9f094d7df95d9f461a9c9b49a69db8bcf047633c
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-03-02 13:38:17 +0100

    RN 54

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,2 +1,4 @@
 global.Buffer = require("buffer").Buffer;
 process.browser = true; // for readable-stream/lib/_stream_writable.js
+
+require("ReactFeatureFlags").warnAboutDeprecatedLifecycles = false;
