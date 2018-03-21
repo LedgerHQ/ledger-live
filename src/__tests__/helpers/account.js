19a193361591f31a4511e7778af9106f14550996
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-03-21 17:51:36 +0100

    use deterministic RNG & timetravel to fix the date in tests

--- src/__tests__/helpers/account.js
+++ src/__tests__/helpers/account.js
@@ -3,5 +3,8 @@ import {getBalanceHistory, getBalanceHistorySum} from "../../helpers/account";
 import {genAccount} from "../../mock/account";
 
 test("getBalanceHistory(*,7) returns an array of 7 items", () => {
-  expect(getBalanceHistory(genAccount(0), 7).length).toBe(7);
+  const history = getBalanceHistory(genAccount(0, "seed_1"), 30);
+  expect(history).toBeInstanceOf(Array);
+  expect(history.length).toBe(30);
+  expect(history).toMatchSnapshot();
 });
--- src/mock/account.js
+++ src/mock/account.js
@@ -1,52 +1,25 @@
 // @flow
 import { listCurrencies } from "@ledgerhq/currencies";
+import Prando from "prando";
 import type { Currency } from "@ledgerhq/currencies";
 import type { Account, Operation } from "../types";
 
 const currencies = listCurrencies();
 
-export function genBalanceDataNext(data: *, dateIncrement: number) {
-  function mix(a, b, m) {
-    return (1 - m) * a + m * b;
-  }
-  return {
-    date: new Date(data[data.length - 1].date.getTime() + dateIncrement),
-    value: mix(
-      data[data.length - 1].value,
-      Math.floor(6000000 * Math.random()),
-      0.5
-    )
-  };
-}
-
-export function genBalanceData(n: number, dateIncrement: number) {
-  const arr = [{ date: new Date(2018, 2, 1), value: 1234567 }];
-  for (let i = 1; i < n; i++) {
-    arr.push(genBalanceDataNext(arr, dateIncrement));
-  }
-  return arr;
-}
-
-export function genBitcoinAddressLike() {
+export function genBitcoinAddressLike(rng: Prando) {
   const charset = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-  return `1${Array(Math.floor(25 + 9 * Math.random()))
-    .fill(null)
-    .map(() => charset[Math.floor(Math.random() * charset.length)])
-    .join("")}`;
+  return `1${rng.nextString(rng.nextInt(25, 34), charset)}`;
 }
 
-export function genHex(length: number) {
-  return Array(length)
-    .fill(null)
-    .map(() => Math.floor(16 * Math.random()).toString(16))
-    .join("");
+export function genHex(length: number, rng: Prando) {
+  return rng.nextString(length, "0123456789ABCDEF");
 }
 
-export function genAddress(currency: Currency) {
+export function genAddress(currency: Currency, rng: Prando) {
   if (currency.coinType === 60 || currency.coinType === 61) {
-    return `0x${genHex(40)}`;
+    return `0x${genHex(40, rng)}`;
   }
-  return genBitcoinAddressLike();
+  return genBitcoinAddressLike(rng);
 }
 
 // TODO fix the mock to never generate negative balance...
@@ -54,36 +27,40 @@ export function genAddress(currency: Currency) {
 export function genOperation(
   account: Account,
   ops: *,
-  currency: Currency
+  currency: Currency,
+  rng: Prando
 ): Operation {
   const lastOp = ops[ops.length - 1];
   const date = new Date(
-    lastOp
-      ? lastOp.date - Math.floor(100000000 * Math.random() * Math.random())
-      : Date.now() - Math.floor(1000000000 * Math.random() * Math.random())
+    (lastOp ? lastOp.date : Date.now()) -
+      rng.nextInt(0, 100000000 * rng.next() * rng.next())
   );
   return {
     id: String(`mock_op_${ops.length}_${account.id}`),
     accountId: account.id,
-    address: genAddress(currency),
+    address: genAddress(currency, rng),
     amount:
-      (Math.random() < 0.3 ? -1 : 1) *
-      Math.floor(100000000 * Math.random() * Math.random()),
-    hash: genHex(64),
+      (rng.next() < 0.3 ? -1 : 1) *
+      rng.nextInt(0, 100000000 * rng.next() * rng.next()),
+    hash: genHex(64, rng),
     date,
     confirmations: Math.floor((Date.now() - date) / 900000)
   };
 }
 
+/**
+ */
 export function genAddingOperationsInAccount(
   account: Account,
-  count: number
+  count: number,
+  seed: ?(number | string)
 ): Account {
+  const rng = new Prando(seed);
   const copy = { ...account };
   copy.operations = Array(count)
     .fill(null)
     .reduce(ops => {
-      const op = genOperation(copy, ops, copy.currency);
+      const op = genOperation(copy, ops, copy.currency, rng);
       return ops.concat(op);
     }, copy.operations);
   copy.balance = account.operations.reduce((sum, op) => sum + op.amount, 0);
@@ -92,32 +69,26 @@ export function genAddingOperationsInAccount(
 
 /**
  */
-export function genAccount(accountIndex: number) {
-  const currency = currencies[Math.floor(currencies.length * Math.random())];
+export function genAccount(accountIndex: number, seed: ?(number | string)) {
+  const rng = new Prando(seed);
+  const currency = rng.nextArrayItem(currencies);
   const account: Account = {
     id: String(`mock_account_${accountIndex}`),
-    xpub: genHex(64),
+    xpub: genHex(64, rng),
     archived: false,
-    data: genBalanceData(8, 86400000),
     currency,
     coinType: currency.coinType,
     unit: currency.units[0],
     balance: 0,
-    address: genAddress(currency),
+    address: genAddress(currency, rng),
     operations: [],
     minConfirmations: 2,
-    name:
-      String.fromCharCode(Math.floor(65 + 26 * Math.random())) +
-      Array(Math.floor(4 + 30 * Math.random()))
-        .fill("")
-        .map(() => String.fromCharCode(Math.floor(65 + 26 * Math.random())))
-        .join("")
-        .toLowerCase()
+    name: rng.nextString(rng.nextInt(4, 34))
   };
-  account.operations = Array(Math.floor(1 + 200 * Math.random()))
+  account.operations = Array(rng.nextInt(1, 200))
     .fill(null)
     .reduce(ops => {
-      const op = genOperation(account, ops, currency);
+      const op = genOperation(account, ops, currency, rng);
       return ops.concat(op);
     }, []);
   account.balance = account.operations.reduce((sum, op) => sum + op.amount, 0);
