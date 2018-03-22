737215b7086911d7b73edfaac3d676f4d91569a9
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-03-22 09:49:02 +0100

    add opRetentionStategy on the accuont model
    by default, it would always keep 100 last operations AND always keep a one year old history so we can graph things up to 1 year without need to re-pull data

--- src/__tests__/models/account.js
+++ src/__tests__/models/account.js
@@ -1,13 +1,12 @@
 // @flow
 import { getFiatUnit } from "@ledgerhq/currencies";
-import AccountModel from "../../models/account";
+import createAccountModel from "../../models/account";
 import { genAccount } from "../../mock/account";
 
-const { encode, decode } = AccountModel;
-
 test("for current accountModel, decode(encode(...)) is pseudo-identity", () => {
+  const {version,encode,decode} = createAccountModel();
   const account = genAccount("model1");
-  const model = { data: account, version: AccountModel.version };
+  const model = { data: account, version: version };
   const modelMirror = decode(encode(model));
   // the encode method is allowed to chunk the potentially big operations array
   model.data.operations.splice(modelMirror.data.operations.length);
--- src/models/account.js
+++ src/models/account.js
@@ -1,49 +1,70 @@
-// @flow
-
+/**
+ * @module models/account
+ * @flow
+ */
 import { getCurrencyByCoinType } from "@ledgerhq/currencies";
 import { createDataModel } from "../DataModel";
 import type { DataModel } from "../DataModel";
-import type { Account, AccountRaw } from "../types";
+import type { Account, AccountRaw, Operation } from "../types";
 
-const MAX_OP_SIZE_TO_KEEP = 100;
+/**
+ * @memberof models/account
+ */
+export const opRetentionStategy = (maxDaysOld: number, keepFirst: number) => (
+  op: Operation,
+  index: number
+): boolean =>
+  index < keepFirst || Date.now() - op.date < 1000 * 60 * 60 * 24 * maxDaysOld;
 
-const accountModel: DataModel<AccountRaw, Account> = createDataModel({
-  migrations: [
-    // Each time a modification is brought to the model, add here a migration function
-  ],
+/**
+ * @memberof models/account
+ * @example
+ * import createAccountModel from "@ledgerhq/wallet-common/lib/models/account";
+ * const AccountModel = createAccountModel();
+ * const raw = AccountModel.encode({ data: account, version: AccountModel.version });
+ * const decoded = AccountModel.decode(raw);
+ */
+export default (
+  opRetentionFilter: (
+    op: Operation,
+    index: number
+  ) => boolean = opRetentionStategy(366, 100)
+): DataModel<AccountRaw, Account> =>
+  createDataModel({
+    migrations: [
+      // Each time a modification is brought to the model, add here a migration function
+    ],
 
-  wrap: ({
-    coinType,
-    unitMagnitude,
-    operations,
-    ...acc
-  }: AccountRaw): Account => {
-    const currency = getCurrencyByCoinType(coinType);
-    const unit =
-      currency.units.find(u => u.magnitude === unitMagnitude) ||
-      currency.units[0];
-    return {
+    wrap: ({
+      coinType,
+      unitMagnitude,
+      operations,
+      ...acc
+    }: AccountRaw): Account => {
+      const currency = getCurrencyByCoinType(coinType);
+      const unit =
+        currency.units.find(u => u.magnitude === unitMagnitude) ||
+        currency.units[0];
+      return {
+        ...acc,
+        operations: operations.map(({ date, ...op }) => ({
+          ...op,
+          date: new Date(date)
+        })),
+        unit,
+        currency
+      };
+    },
+
+    unwrap: ({ currency, operations, unit, ...acc }: Account): AccountRaw => ({
       ...acc,
-      operations: operations.map(({ date, ...op }) => ({
-        ...op,
-        date: new Date(date)
-      })),
-      unit,
-      currency
-    };
-  },
-
-  unwrap: ({ currency, operations, unit, ...acc }: Account): AccountRaw => ({
-    ...acc,
-    operations: operations
-      .slice(0, MAX_OP_SIZE_TO_KEEP)
-      .map(({ date, ...op }) => ({
-        ...op,
-        date: date.toISOString()
-      })),
-    coinType: currency.coinType,
-    unitMagnitude: unit.magnitude
-  })
-});
-
-export default accountModel;
+      operations: operations
+        .filter(opRetentionFilter)
+        .map(({ date, ...op }) => ({
+          ...op,
+          date: date.toISOString()
+        })),
+      coinType: currency.coinType,
+      unitMagnitude: unit.magnitude
+    })
+  });
