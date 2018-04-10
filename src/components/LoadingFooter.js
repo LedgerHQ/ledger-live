46a9911aac8d3a961a20177fdddf44469a50278f
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-04-10 19:40:48 +0200

    add LoadingFooter back. but can't use it for now until we track 'end'

---
+++ src/components/LoadingFooter.js
@@ -0,0 +1,8 @@
+/* @flow */
+import React from "react";
+import { ActivityIndicator } from "react-native";
+import colors from "../colors";
+
+const el = <ActivityIndicator style={{ margin: 40 }} color={colors.blue} />;
+
+export default () => el;
