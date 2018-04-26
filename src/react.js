ae6676018b17efff4ce6110570d2430aeb393e50
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-04-26 18:39:25 +0200

    for react, color is actually optional

--- src/react.js
+++ src/react.js
@@ -3,7 +3,7 @@ import * as icons from "./data/icons/react";
 import type { CryptoCurrency } from "./types";
 import iconNameById from "./iconNameById";
 
-type Icon = React$ComponentType<{ size: number, color: string }>;
+type Icon = React$ComponentType<{ size: number, color?: string }>;
 
 export function getCryptoCurrencyIcon(currency: CryptoCurrency): ?Icon {
   return icons[iconNameById(currency.id)];
