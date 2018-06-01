f0cbf28ed3fd6408d70f811b44ffc4b14b8bd568
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-06-01 16:51:17 +0200

    Fix formatShort(0)

--- src/helpers/currencies/formatShort.js
+++ src/helpers/currencies/formatShort.js
@@ -10,9 +10,12 @@ import { formatCurrencyUnit } from "./formatCurrencyUnit";
 export function formatShort(unit: Unit, value: number): string {
   const { magnitude } = unit;
   const floatValue = value / 10 ** magnitude;
-  if (floatValue < 1) {
+  if (floatValue === 0) {
+    return "0";
+  }
+  if (-1 < floatValue && floatValue < 1) {
+    // numeral have issues with low values, fallback on formatCurrencyUnit
     return formatCurrencyUnit(unit, value);
   }
-
   return numeral(floatValue).format("0[.]0a");
 }
