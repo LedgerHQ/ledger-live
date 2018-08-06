8a0769008f0ee0536bb98a20b7043f54f67fed32
meriadec <meriadec.pillet@gmail.com>
2018-08-06 18:57:32 +0200

    Add Rubik font

--- src/components/LText/getFontStyle.ios.js
+++ src/components/LText/getFontStyle.ios.js
@@ -3,14 +3,15 @@ type Opts = {
   bold?: boolean,
   semiBold?: boolean,
   secondary?: boolean,
+  tertiary?: boolean,
 };
 type Res = {
   fontFamily: string,
   fontWeight: string,
 };
 
-export default ({ bold, semiBold, secondary }: Opts = {}): Res => {
-  const fontFamily = secondary ? "Museo Sans" : "Open Sans";
+export default ({ bold, semiBold, secondary, tertiary }: Opts = {}): Res => {
+  const fontFamily = secondary ? "Museo Sans" : tertiary ? "Rubik" : "Open Sans";
   let fontWeight = secondary ? -200 : 0; // Fix for Museo weights being off by 200;
   if (semiBold) {
     fontWeight += 600;
