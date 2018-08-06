d556d66cbaae835a83b7d6400f2821a119d50325
meriadec <meriadec.pillet@gmail.com>
2018-08-06 13:14:42 +0200

    Ignore timeout warning

--- src/polyfill.js
+++ src/polyfill.js
@@ -8,7 +8,8 @@ if (__DEV__) {
     try {
       // just for tests
       require("react-native").YellowBox.ignoreWarnings([
-        "Warning: isMounted(...) is deprecated in plain JavaScript React classes. Instead, make sure to clean up subscriptions and pending requests in componentWillUnmount to prevent memory leaks."
+        "Warning: isMounted(...) is deprecated in plain JavaScript React classes. Instead, make sure to clean up subscriptions and pending requests in componentWillUnmount to prevent memory leaks.",
+        "Setting a timer for a long period of time, i.e. multiple minutes, is a performance and correctness issue on Android as it keeps the timer module awake, and timers can only be called when the app is in the foreground. See https://github.com/facebook/react-native/issues/12981 for more info."
       ]);
     } catch (e) {
       console.warn(e);
