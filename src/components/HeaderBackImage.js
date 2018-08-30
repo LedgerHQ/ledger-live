31bb4df4e44c8ae84f3a8d43cb292d6de1e5d5ec
Thibaut Boustany <thibaut@kastor.fr>
2018-08-30 14:23:35 +0200

    Use StyleSheet

--- src/components/HeaderBackImage.js
+++ src/components/HeaderBackImage.js
@@ -1,17 +1,19 @@
 /* @flow */
 import React from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, StyleSheet } from "react-native";
 import ArrowLeft from "../images/icons/ArrowLeft";
 import colors from "../colors";
 
-const HeaderBackImage = () => {
-  const paddingLeft = Platform.OS === "ios" ? 16 : 3;
-
-  return (
-    <View style={{ paddingLeft }}>
+const HeaderBackImage = () => (
+    <View style={styles.root}>
       <ArrowLeft size={16} color={colors.darkBlue} />
     </View>
   );
-};
 
 export default HeaderBackImage;
+
+const styles = StyleSheet.create({
+  root: {
+    paddingLeft: Platform.OS === "ios" ? 16 : 3,
+  },
+});
