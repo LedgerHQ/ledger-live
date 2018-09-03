494d2388a4417129942f4722331bd0e73d4bdc3e
Anastasia Poupeney <anastasiasergienko@gmail.com>
2018-09-03 10:29:55 +0200

    remove mock hardcoded data from operation details

--- src/screens/OperationDetails/Content.js
+++ src/screens/OperationDetails/Content.js
@@ -3,11 +3,16 @@ import React, { PureComponent, Fragment } from "react";
 import { View, StyleSheet } from "react-native";
 import type { Account, Operation } from "@ledgerhq/live-common/lib/types";
 import uniq from "lodash/uniq";
+import { connect } from "react-redux";
+import { compose } from "redux";
 import { translate } from "react-i18next";
 import LText from "../../components/LText";
 import OperationIcon from "../../components/OperationIcon";
 import CurrencyUnitValue from "../../components/CurrencyUnitValue";
 import CounterValue from "../../components/CounterValue";
+import type { CurrencySettings } from "../../reducers/settings";
+import { currencySettingsSelector } from "../../reducers/settings";
+import type { State } from "../../reducers";
 import colors from "../../colors";
 import type { T } from "../../types/common";
 import DataList from "./DataList";
@@ -16,28 +21,24 @@ type Props = {
   t: T,
   account: Account,
   operation: Operation,
+  currencySettings: CurrencySettings,
 };
 
+const mapStateToProps = (state: State, props: { account: Account }) => ({
+  currencySettings: currencySettingsSelector(state, props.account.currency),
+});
+
 class Content extends PureComponent<Props, *> {
-  // NOTE some const temp hardcoded for testing purposes
   render() {
-    const { account, operation, t } = this.props;
+    const { account, operation, t, currencySettings } = this.props;
     const valueColor = operation.type === "IN" ? colors.green : colors.smoke;
     const confirmations = operation.blockHeight
       ? account.blockHeight - operation.blockHeight
       : 0;
-    // const uniqueSenders = uniq(operation.senders);
-    const uniqueSenders = [
-      "15YftVBGtvLtW3Bshd7wkyNqJw1WnCudEcH",
-      "77YftVBGtvLtW3Bshd7wkyNqJw1WnCudTyh",
-      "88YftVBGtvLtW3Bshd7wkyNqJw1WnCudTyh",
-      "99YftVBGtvLtW3Bshd7wkyNqJw1WnCudTyh",
-    ];
+    const uniqueSenders = uniq(operation.senders);
     const uniqueRecipients = uniq(operation.recipients);
 
-    // const isConfirmed = confirmations >= currencySettings.confirmationsNb
-    const isConfirmed = confirmations >= 2;
-
+    const isConfirmed = confirmations >= currencySettings.confirmationsNb;
     return (
       <Fragment>
         <View style={styles.header}>
@@ -149,7 +150,10 @@ class Content extends PureComponent<Props, *> {
   }
 }
 
-export default translate()(Content);
+export default compose(
+  connect(mapStateToProps),
+  translate(),
+)(Content);
 
 const styles = StyleSheet.create({
   root: {
