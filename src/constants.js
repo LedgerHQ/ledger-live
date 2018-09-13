4111c23c7e8d4d90071a7503746fbac09984503a
meriadec <meriadec.pillet@gmail.com>
2018-09-13 17:26:28 +0200

    Build Account from coreAccount

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,5 @@ export const SYNC_BOOT_DELAY = 2 * 1000;
 export const SYNC_PENDING_INTERVAL = 10 * 1000;
 export const SYNC_MAX_CONCURRENT = 1;
 export const SYNC_TIMEOUT = 60 * 1000;
+
+export const MAX_ACCOUNT_NAME_SIZE = 50;
