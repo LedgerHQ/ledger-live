eee285d325f577792b19e9b973bde36764e5d8e1
meriadec <meriadec.pillet@gmail.com>
2018-09-14 17:00:35 +0200

    Add cancel mechanism on libcore sync

--- src/bridge/RNLibcoreAccountBridge.js
+++ src/bridge/RNLibcoreAccountBridge.js
@@ -15,51 +15,61 @@ function operationsDiffer(a, b) {
 }
 
 const startSync = (initialAccount, _observation) =>
-  Observable.create(async o => {
-    try {
-      const syncedAccount = await syncAccount({ account: initialAccount });
+  Observable.create(o => {
+    let cancelled = false;
+    const isCancelled = () => cancelled;
+    const cancel = () => (cancelled = true);
 
-      o.next(initialAccount => {
-        const patch = {
-          ...initialAccount,
-          id: syncedAccount.id,
-          freshAddress: syncedAccount.freshAddress,
-          freshAddressPath: syncedAccount.freshAddressPath,
-          balance: syncedAccount.balance,
-          blockHeight: syncedAccount.blockHeight,
-          lastSyncDate: new Date(),
-          operations: initialAccount.operations,
-          pendingOperations: [],
-        };
+    (async () => {
+      try {
+        const syncedAccount = await syncAccount({
+          account: initialAccount,
+          cancel,
+        });
+        if (isCancelled()) return;
 
-        const oldOpsLength = initialAccount.operations.length;
-        const newOpsLength = syncedAccount.operations.length;
+        o.next(initialAccount => {
+          const patch = {
+            ...initialAccount,
+            id: syncedAccount.id,
+            freshAddress: syncedAccount.freshAddress,
+            freshAddressPath: syncedAccount.freshAddressPath,
+            balance: syncedAccount.balance,
+            blockHeight: syncedAccount.blockHeight,
+            lastSyncDate: new Date(),
+            operations: initialAccount.operations,
+            pendingOperations: [],
+          };
 
-        const patchedOperations = [];
-        let hasChanged = false;
+          const oldOpsLength = initialAccount.operations.length;
+          const newOpsLength = syncedAccount.operations.length;
 
-        for (let i = 0; i < newOpsLength; i++) {
-          const newOp = syncedAccount.operations[newOpsLength - 1 - i];
-          const oldOp = initialAccount.operations[oldOpsLength - 1 - i];
-          if (operationsDiffer(newOp, oldOp)) {
-            hasChanged = true;
-            patchedOperations.push(newOp);
-          } else {
-            patchedOperations.push(oldOp);
+          const patchedOperations = [];
+          let hasChanged = false;
+
+          for (let i = 0; i < newOpsLength; i++) {
+            const newOp = syncedAccount.operations[newOpsLength - 1 - i];
+            const oldOp = initialAccount.operations[oldOpsLength - 1 - i];
+            if (operationsDiffer(newOp, oldOp)) {
+              hasChanged = true;
+              patchedOperations.push(newOp);
+            } else {
+              patchedOperations.push(oldOp);
+            }
           }
-        }
 
-        if (hasChanged) {
-          patch.operations = patchedOperations
-        }
+          if (hasChanged) {
+            patch.operations = patchedOperations;
+          }
 
-        return patch;
-      });
-      o.complete();
-    } catch (err) {
-      o.error(err);
-    }
-    return () => {};
+          return patch;
+        });
+        o.complete();
+      } catch (err) {
+        o.error(err);
+      }
+    })();
+    return cancel;
   });
 
 const checkValidRecipient = (_currency, _recipient) =>
