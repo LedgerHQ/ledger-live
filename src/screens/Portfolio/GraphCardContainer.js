768b5573fbbfdac149324cc13e79c6a237168018
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-09-21 16:21:59 +0200

    Integrate react-native-gesture-handler
    the tradeoff here is to only start graph interaction after a X panning. the idea is to not trigger it at all if you are just scrolling or pull to refresh, which is most of the time.
    The change here:
    - simplify code
    - fix race condition
    - remove the hack to disable the scroll & solve it on native side

--- src/screens/Portfolio/GraphCardContainer.js
+++ src/screens/Portfolio/GraphCardContainer.js
@@ -5,23 +5,10 @@ import type { Summary } from "../../components/provideSummary";
 import GraphCard from "../../components/GraphCard";
 import Greetings from "./Greetings";
 
-const GraphCardContainer = ({
-  summary,
-  onPanResponderStart,
-  onPanResponderRelease,
-}: {
-  summary: Summary,
-  onPanResponderStart: () => *,
-  onPanResponderRelease: () => *,
-}) => (
+const GraphCardContainer = ({ summary }: { summary: Summary }) => (
   <View>
     <Greetings nbAccounts={summary.accounts.length} />
-    <GraphCard
-      summary={summary}
-      useCounterValue
-      onPanResponderStart={onPanResponderStart}
-      onPanResponderRelease={onPanResponderRelease}
-    />
+    <GraphCard summary={summary} useCounterValue />
   </View>
 );
 
