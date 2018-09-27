595c9a6e6a8c1b1341e0e883a413b78b1bb9c0ad
Thibaut Boustany <thibaut@kastor.fr>
2018-09-27 19:40:40 +0200

    Fix #199 back button hitbox too small

--- src/components/HeaderBackImage.js
+++ src/components/HeaderBackImage.js
@@ -1,27 +1,24 @@
 /* @flow */
-import React from "react";
+import React, { PureComponent } from "react";
 import { View, Platform, StyleSheet } from "react-native";
 import ArrowLeft from "../icons/ArrowLeft";
 import colors from "../colors";
 
-const HeaderBackImage = () => (
-  <View
-    style={styles.root}
-    hitSlop={{
-      top: 16,
-      left: 16,
-      right: 16,
-      bottom: 16,
-    }}
-  >
-    <ArrowLeft size={16} color={colors.grey} />
-  </View>
-);
+class HeaderBackImage extends PureComponent<*> {
+  render() {
+    return (
+      <View style={styles.root}>
+        <ArrowLeft size={16} color={colors.grey} />
+      </View>
+    );
+  }
+}
 
 export default HeaderBackImage;
 
 const styles = StyleSheet.create({
   root: {
-    paddingLeft: Platform.OS === "ios" ? 16 : 3,
+    marginLeft: Platform.OS === "ios" ? 0 : -13,
+    padding: 16,
   },
 });
