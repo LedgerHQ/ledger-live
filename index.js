77a9e826c7cd9a7ffbb518cc931fedf4f85c0653
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-18 08:05:08 +0200

    Upgrade to latest react-native & remove useless rn device-info

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { AppRegistry } from "react-native";
 import { Sentry } from "react-native-sentry";
 import Config from "react-native-config";
-import DeviceInfo from "react-native-device-info";
 
 import App from "./src";
 
@@ -13,7 +12,6 @@ if (Config.SENTRY_DSN) {
   }).install();
 
   Sentry.setUserContext({
-    userID: DeviceInfo.getUniqueID(),
     ip_address: null,
   });
 }
