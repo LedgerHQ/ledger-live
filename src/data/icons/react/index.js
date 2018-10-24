461b57f568e0a250b305b073b6cd34dd73994426
David <37987466+dalijolijo@users.noreply.github.com>
2018-10-24 18:56:07 +0200

    Add BitCore (BTX) support

--- src/data/icons/react/index.js
+++ src/data/icons/react/index.js
@@ -1,6 +1,7 @@
 export { default as ark } from "./ark";
 export { default as bitcloud } from "./bitcloud";
 export { default as bitcoin } from "./bitcoin";
+export { default as bitcore } from "./bitcore";
 export { default as bitsend } from "./bitsend";
 export { default as clubcoin } from "./clubcoin";
 export { default as dascoin } from "./dascoin";
