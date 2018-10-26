5d1017f2299d6230faa9599b475f821781654161
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-10-26 16:33:18 +0200

    Add missing add account modal for the '+'

--- src/screens/Accounts/AddAccount.js
+++ src/screens/Accounts/AddAccount.js
@@ -3,19 +3,41 @@
 import React, { PureComponent } from "react";
 import { withNavigation } from "react-navigation";
 import Icon from "react-native-vector-icons/dist/Feather";
+import AddAccountsModal from "../../components/AddAccountsModal";
 import Touchable from "../../components/Touchable";
 import colors from "../../colors";
 
-class AddAccount extends PureComponent<{ navigation: * }> {
-  onPress = () => {
-    this.props.navigation.navigate("ImportAccounts");
+class AddAccount extends PureComponent<
+  { navigation: * },
+  { isAddModalOpened: boolean },
+> {
+  state = {
+    isAddModalOpened: false,
+  };
+
+  closeAddModal = () => {
+    this.setState({ isAddModalOpened: false });
+  };
+
+  openAddModal = () => {
+    this.setState({ isAddModalOpened: true });
   };
 
   render() {
+    const { navigation } = this.props;
+    const { isAddModalOpened } = this.state;
     return (
-      <Touchable style={{ marginHorizontal: 16 }} onPress={this.onPress}>
-        <Icon name="plus" color={colors.grey} size={20} />
-      </Touchable>
+      <>
+        <Touchable style={{ marginHorizontal: 16 }} onPress={this.openAddModal}>
+          <Icon name="plus" color={colors.grey} size={20} />
+        </Touchable>
+
+        <AddAccountsModal
+          navigation={navigation}
+          isOpened={isAddModalOpened}
+          onClose={this.closeAddModal}
+        />
+      </>
     );
   }
 }
