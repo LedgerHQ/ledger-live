30c4b6160f44d969b4dd07b79890b57821feefc5
Thibaut Boustany <thibaut@kastor.fr>
2018-11-07 19:14:39 +0100

    PIN code screen

--- src/screens/Onboarding/steps/04-setup-pin.js
+++ src/screens/Onboarding/steps/04-setup-pin.js
@@ -1,11 +1,14 @@
 // @flow
 
 import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
 
-import LText from "../../../components/LText";
 import Button from "../../../components/Button";
+import BulletList from "../../../components/BulletList";
 import OnboardingLayout from "../OnboardingLayout";
 import { withOnboardingContext } from "../onboardingContext";
+import NanoXPincode from "../assets/NanoXPincode";
+import colors from "../../../colors";
 
 import type { OnboardingStepProps } from "../types";
 
@@ -17,12 +20,38 @@ class OnboardingStep04SetupPin extends Component<OnboardingStepProps> {
 
   render() {
     return (
-      <OnboardingLayout header="OnboardingStep04SetupPin" Footer={this.Footer}>
-        <LText>OnboardingStep04SetupPin</LText>
-        <LText>OnboardingStep04SetupPin</LText>
+      <OnboardingLayout
+        header="OnboardingStep04SetupPin"
+        Footer={this.Footer}
+        noHorizontalPadding
+      >
+        <View style={styles.hero}>
+          <NanoXPincode />
+        </View>
+        <View style={styles.wrapper}>
+          <BulletList
+            list={[
+              "Turn on your Ledger Nano X.",
+              "Press both device buttons to continue.",
+              "Press left or right button to select a digit. Press both to validate.",
+              "Select ✓ to confirm your PIN code.\nSelect 🔙 to erase a digit.",
+            ]}
+          />
+        </View>
       </OnboardingLayout>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  hero: {
+    paddingVertical: 60,
+    backgroundColor: colors.lightGrey,
+    alignItems: "center",
+  },
+  wrapper: {
+    padding: 16,
+  },
+});
 
 export default withOnboardingContext(OnboardingStep04SetupPin);
