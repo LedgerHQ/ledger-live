bb5ac4e8f6cbba4df286a65712c411d220214f9b
Thibaut Boustany <thibaut@kastor.fr>
2018-11-16 19:09:14 +0100

    i18n tab labels + fix Transfer label not being tappable

---
+++ src/components/TabIcon.js
@@ -0,0 +1,48 @@
+/* @flow */
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import { Trans, translate } from "react-i18next";
+import LText from "./LText";
+
+type Props = {
+  tintColor: string,
+  focused: boolean,
+  i18nKey: string,
+  Icon: *,
+};
+
+class TabIcon extends Component<Props> {
+  render() {
+    const { Icon, i18nKey, tintColor, focused } = this.props;
+
+    return (
+      <View style={styles.root}>
+        <Icon size={18} color={tintColor} />
+        <LText
+          semiBold={!focused}
+          bold={focused}
+          secondary
+          style={[styles.text, { color: tintColor }]}
+        >
+          <Trans i18nKey={i18nKey} />
+        </LText>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 4,
+  },
+  text: {
+    fontSize: 12,
+    textAlign: "center",
+    marginTop: 6,
+  },
+});
+
+export default translate()(TabIcon);
