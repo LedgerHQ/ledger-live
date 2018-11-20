fbc8769b8a8538c7552dc3ad9ef8143f417a920d
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2018-11-20 20:59:29 +0100

    Add timeout

--- src/context/AuthPass/index.js
+++ src/context/AuthPass/index.js
@@ -1,9 +1,11 @@
 // @flow
 import React, { PureComponent } from "react";
+import { AppState } from "react-native";
 import { connect } from "react-redux";
 import { translate } from "react-i18next";
 import { createStructuredSelector } from "reselect";
 import { privacySelector } from "../../reducers/settings";
+import { AUTOLOCK_TIMEOUT } from "../../constants";
 
 import auth from "./auth";
 import type { Privacy } from "../../reducers/settings";
@@ -26,14 +28,65 @@ type Props = {
 
 class AuthPass extends PureComponent<Props, State> {
   state = {
-    isLocked: !!this.props.privacy,
+    isLocked: this.props.privacy.authSecurityEnabled,
     biometricsError: null,
   };
 
-  componentDidMount() {
-    this.auth();
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this.handleAppStateChange);
   }
 
+  componentDidMount() {
+    this.auth();
+    AppState.addEventListener("change", this.handleAppStateChange);
+  }
+
+  timeout: *;
+  handleAppStateChange = appState => {
+    clearTimeout(this.timeout);
+    if (appState === "active") {
+      this.auth();
+    } else {
+      this.timeout = setTimeout(() => {
+        if (!this.state.isLocked) {
+          this.lock();
+        }
+      }, AUTOLOCK_TIMEOUT);
+    }
+  };
+
+  // auth: try to auth with biometrics and fallback on password
+  authPending = false;
+  auth = () => {
+    const { privacy, t } = this.props;
+    const { isLocked } = this.state;
+    if (isLocked && privacy.biometricsEnabled) {
+      if (this.authPending) return;
+      this.authPending = true;
+      auth(t("auth.unlock.biometricsTitle"))
+        .then(() => {
+          this.authPending = false;
+          this.unlock();
+        })
+        .catch(error => {
+          this.authPending = false;
+          this.setState({
+            biometricsError: error,
+          });
+        });
+    }
+  };
+
+  // lock the app
+  lock = () => {
+    if (!this.props.privacy) return;
+    this.setState({
+      isLocked: true,
+      biometricsError: null,
+    });
+  };
+
+  // unlock the app
   unlock = () => {
     this.setState({
       isLocked: false,
@@ -41,19 +94,6 @@ class AuthPass extends PureComponent<Props, State> {
     });
   };
 
-  auth = () => {
-    const { privacy, t } = this.props;
-    const { isLocked } = this.state;
-    if (isLocked && privacy.biometricsEnabled) {
-      auth(t("auth.unlock.biometricsTitle"))
-        .then(this.unlock)
-        .catch(error => {
-          this.setState({
-            biometricsError: error,
-          });
-        });
-    }
-  };
   render() {
     const { children, privacy } = this.props;
     const { isLocked, biometricsError } = this.state;
