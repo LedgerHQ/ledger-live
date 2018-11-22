f2673bfdb0d8c9ef447129369064bd95b8ef4eb7
Thibaut Boustany <thibaut@kastor.fr>
2018-11-22 16:53:32 +0100

    Use real apdu for rename

--- src/logic/hw/editDeviceName.js
+++ src/logic/hw/editDeviceName.js
@@ -2,18 +2,5 @@
 import Transport from "@ledgerhq/hw-transport";
 
 export default async (transport: Transport<*>, name: string): Promise<void> => {
-  // Temporary BLE implementation for demoing rename,
-  // should use real APDU in final release
-  // $FlowFixMe
-  if (transport.renameCharacteristic) {
-    const formattedName = Buffer.concat([
-      Buffer.alloc(1),
-      Buffer.from(name),
-    ]).toString("base64");
-    // $FlowFixMe
-    await transport.renameCharacteristic.writeWithResponse(formattedName);
-    return;
-  }
-
   await transport.send(0xe0, 0xd4, 0x00, 0x00, Buffer.from(name));
 };
