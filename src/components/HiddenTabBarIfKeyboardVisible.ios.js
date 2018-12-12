5badf48ccbe5ad5843dfc67dac52d2ec7b02a68d
Juan Cortes Ross <juan@bohem.io>
2018-12-12 21:55:40 +0100

    Update HiddenTabBarIfKeyboardVisible.ios.js

--- src/components/HiddenTabBarIfKeyboardVisible.ios.js
+++ src/components/HiddenTabBarIfKeyboardVisible.ios.js
@@ -1,12 +1,4 @@
 // @flow
-import React from "react";
 import { BottomTabBar } from "react-navigation-tabs";
 
-export default class HiddenTabBarIfKeyboardVisible extends React.PureComponent<
-  *,
-  *,
-> {
-  render() {
-    return <BottomTabBar {...this.props} />;
-  }
-}
+export default BottomTabBar;
