5493a830cf10a7fc935ca4182b474aa57f06159d
Juan Cortes Ross <juan@bohem.io>
2018-12-18 22:51:31 +0100

    Adds a clipboard watcher inside the app to remove non-text content

--- android/app/src/main/java/com/ledger/live/MainActivity.java
+++ android/app/src/main/java/com/ledger/live/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ledger.live;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.facebook.react.ReactFragmentActivity;
@@ -27,6 +30,28 @@ public class MainActivity extends ReactFragmentActivity {
             SplashScreen.show(this, true);
         }
         super.onCreate(savedInstanceState);
+
+        /**
+         * Addresses an inconvenient side-effect of using `password-visible`, that allowed styled
+         * texts to be pasted (receiver's address for instance) retaining the styles of the source
+         * text.
+         */
+        final ClipboardManager clipboard = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.addPrimaryClipChangedListener( new ClipboardManager.OnPrimaryClipChangedListener() {
+            boolean breakLoop = false;
+            public void onPrimaryClipChanged() {
+                if(breakLoop){
+                    breakLoop = false;
+                    return;
+                }
+
+                ClipData clipData = clipboard.getPrimaryClip();
+                ClipData.Item item = clipData.getItemAt(0);
+                ClipData clip = ClipData.newPlainText("overriden text", item.coerceToText(MainActivity.this).toString());
+                breakLoop = true;
+                clipboard.setPrimaryClip(clip);
+            }
+        });
     }
 
     @Override
