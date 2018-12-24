cf302e51f8ede0eafa57f5f4492e27a8ee1fa34e
Juan Cortes Ross <juan@bohem.io>
2018-12-24 22:41:37 +0100

    Removes the preview for the app for both iOS and Android

--- ios/ledgerlivemobile/AppDelegate.m
+++ ios/ledgerlivemobile/AppDelegate.m
@@ -58,4 +58,47 @@
                      restorationHandler:restorationHandler];
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application {
+  
+  // fill screen with our own colour
+  UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleRegular];
+  UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+  UIImageView *logoView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"Blurry"]];
+  logoView.contentMode = UIViewContentModeScaleAspectFit;
+  blurEffectView.frame = [self.window bounds];
+  blurEffectView.tag = 12345;
+  logoView.tag = 12346;
+  
+  blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  [self.window addSubview:blurEffectView];
+  [self.window addSubview:logoView];
+  [self.window bringSubviewToFront:logoView];
+  
+  [logoView setContentHuggingPriority:251 forAxis:UILayoutConstraintAxisHorizontal];
+  [logoView setContentHuggingPriority:251 forAxis:UILayoutConstraintAxisVertical];
+  logoView.frame = CGRectMake(0, 0, 128, 128);
+
+  
+  logoView.center = CGPointMake(self.window.frame.size.width  / 2,self.window.frame.size.height / 2);
+
+
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  
+  // grab a reference to our coloured view
+  UIView *blurEffectView = [self.window viewWithTag:12345];
+  UIView *logoView = [self.window viewWithTag:12346];
+  
+  // fade away colour view from main view
+  [UIView animateWithDuration:0.5 animations:^{
+    blurEffectView.alpha = 0;
+    logoView.alpha = 0;
+  } completion:^(BOOL finished) {
+    // remove when finished fading
+    [blurEffectView removeFromSuperview];
+    [logoView removeFromSuperview];
+  }];
+}
+
 @end
