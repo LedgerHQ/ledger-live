f461948d30a6b0e08ac69d7d2896759cd1389876
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2019-02-04 19:58:32 +0100

    update live-common and dedup all react*

--- scripts/check-no-dups.sh
+++ scripts/check-no-dups.sh
@@ -6,3 +6,10 @@ if [ $? -eq 0 ]; then
   echo "Found duplicates in @ledgerhq/* – fix it with yarn-deduplicate"
   exit 1
 fi
+
+yarn-deduplicate -l | grep \"react
+
+if [ $? -eq 0 ]; then
+  echo "Found duplicates in some react packages – fix it with yarn-deduplicate"
+  exit 1
+fi
