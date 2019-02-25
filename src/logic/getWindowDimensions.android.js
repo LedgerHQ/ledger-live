245b885ad4a7bf2d655b3181ad367861dd3feef5
Juan Cortes Ross <juan@bohem.io>
2019-02-25 21:36:21 +0100

    Take into account softmenubarheight on account scan

--- src/logic/getWindowDimensions.android.js
+++ src/logic/getWindowDimensions.android.js
@@ -4,3 +4,6 @@ export default () => ({
   width: ExtraDimensions.get("REAL_WINDOW_WIDTH"),
   height: ExtraDimensions.get("REAL_WINDOW_HEIGHT"),
 });
+
+export const softMenuBarHeight = () =>
+  ExtraDimensions.get("SOFT_MENU_BAR_HEIGHT");
