a0b9d25912eb2b43726179a0e3a59ff2cf84b563
Juan Cortes Ross <juan@bohem.io>
2019-03-12 17:12:58 +0100

    Handle User refusal of device rename

--- src/hw/editDeviceName.js
+++ src/hw/editDeviceName.js
@@ -1,6 +1,13 @@
 // @flow
 import Transport from "@ledgerhq/hw-transport";
+import { StatusCodes } from "@ledgerhq/errors";
+import { UserRefusedDeviceNameChange } from "@ledgerhq/errors";
 
 export default async (transport: Transport<*>, name: string): Promise<void> => {
-  await transport.send(0xe0, 0xd4, 0x00, 0x00, Buffer.from(name));
+  await transport.send(0xe0, 0xd4, 0x00, 0x00, Buffer.from(name)).catch(e => {
+    if (e.statusCode === StatusCodes.CONDITIONS_OF_USE_NOT_SATISFIED) {
+      throw new UserRefusedDeviceNameChange();
+    }
+    throw e;
+  });
 };
