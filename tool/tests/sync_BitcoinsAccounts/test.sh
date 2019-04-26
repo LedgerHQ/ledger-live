a6af5bb6d5e6d77c7f067bc03b0523ca93905c7c
Arnaud97234 <arnaud.ulric@gmail.com>
2019-04-26 19:24:02 +0200

    Test Sync  BTC like accounts

---
+++ tool/tests/sync_BitcoinsAccounts/test.sh
@@ -0,0 +1,12 @@
+IFS=$'\n'
+for c in `cat currencies.txt`; do
+  echo "$c start Sync..."
+  ledger-live app -o $c
+  ledger-live deviceAppVersion >> output/${c}_app.json
+  ledger-live sync --device --currency $c -f json > output/$c.json
+  cat output/$c.json | jq 'del(.lastSyncDate,.blockHeight,.operations)' > output/${c}_fields.json
+  cat output/$c.json | jq '.operations | sort_by(.id)[] | del(.date)' > output/${c}_operations.json
+  rm output/$c.json
+  ledger-live app -q
+  echo "$c Sync SUCCESS"
+done
