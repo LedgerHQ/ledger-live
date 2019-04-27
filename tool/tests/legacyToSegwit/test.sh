4d8d5f3ff81cf258cc73d685835c900e2ac43fa1
Arnaud97234 <arnaud.ulric@gmail.com>
2019-04-27 06:30:47 +0200

    send tBTC legacy/segwit

--- tool/tests/legacyToSegwit/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-recipientAddress=`ledger-live receive --currency 'bitcoin_testnet' --scheme 'segwit' --index 0`
-echo $recipientAddress
-
-ledger-live send --device --currency 'bitcoin_testnet' --scheme '' --index 0 --recipient $recipientAddress --amount 0.001 --feePerByte 3 --format json |
-    jq 'del(.operation.date)' > output/raw.json
