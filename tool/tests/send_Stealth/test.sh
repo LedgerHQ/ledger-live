37876d0dd573e3b4ccba851e42a805988ebc1733
Arnaud97234 <arnaud.ulric@gmail.com>
2019-04-30 13:51:56 +0200

    Add test send horizen

---
+++ tool/tests/send_Stealth/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dataLength=`cat data.json | jq '. | length'`
+data=`cat data.json | jq '.'`
+
+echo "Opening Stealth wallet app ..."
+ledger-live app -o stealth
+
+for((i=0; i<$dataLength; i++))
+do
+    currency=`echo $data | jq -r --argjson id $i '.[$id].currency'`
+    echo $currency
+    indexSender=`echo $data | jq -r --argjson id $i '.[$id].indexSender'`
+    amount=`echo $data | jq -r --argjson id $i '.[$id].amount'`
+    fees=`echo $data | jq -r --argjson id $i '.[$id].fees'`
+    type=`echo $data | jq -r --argjson id $i '.[$id].type'`
+    recipientAddress=`echo $data | jq -r --argjson id $i '.[$id].recipientAddress'`
+    type=`echo $data | jq -r --argjson id $i '.[$id].type'`
+
+    echo "Sending $currency $type..."
+    ledger-live send --device --currency $currency --index $indexSender --recipient $recipientAddress --amount $amount --feePerByte $feePerByte --format json |
+        jq 'del(.operation.date)' > output/${currency}_$type.json
+    sleep 2
+done
+ledger-live app -q
