1bf8c8652be491969b1e77a6ad40dfa028a015fd
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-05-03 18:32:46 +0200

    Factorize sync tests and add more sync cases

--- tool/tests/sync_BitcoinGold/test.sh
+++ tool/tests/sync_BitcoinGold/test.sh
@@ -1,12 +1,1 @@
-IFS=$'\n'
-c="Bitcoin Gold"
-echo "$c start Sync..."
-ledger-live app -o $c
-ledger-live deviceAppVersion >> output/${c}_app.json
-ledger-live sync --device --currency $c -f json > output/$c.json
-cat output/$c.json | jq 'del(.lastSyncDate,.blockHeight,.operations)' > output/${c}_fields.json
-cat output/$c.json | jq '.operations | sort_by(.id)[] | del(.date)' > output/${c}_operations.json
-rm output/$c.json
-echo "Closing $c wallet app..."
-ledger-live app -q
-echo "$c Sync SUCCESS"
+../shared_sync.sh "Bitcoin Gold"
---
+++ tool/tests/sync_Ethereum_v2/test.sh
@@ -0,0 +1,1 @@
+../shared_sync.sh "Ethereum"
