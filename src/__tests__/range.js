6c18dcf649b9d99ec74974ad7bdc56bc043c1209
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-05-21 14:32:01 +0200

    Use BigNumber

--- src/__tests__/range.js
+++ src/__tests__/range.js
@@ -1,25 +1,40 @@
 // @flow
+import { BigNumber } from "bignumber.js";
 import {
   inferDynamicRange,
   projectRangeIndex,
   reverseRangeIndex
 } from "../range";
 
+function asString(bn) {
+  if (BigNumber.isBigNumber(bn)) return bn.toString();
+  if (typeof bn === "object" && bn) {
+    const obj = {};
+    for (let k in bn) {
+      obj[k] = asString(bn[k]);
+    }
+    return obj;
+  }
+  return bn;
+}
+
 test("inferDynamicRange", () => {
-  expect(inferDynamicRange(0.4)).toMatchSnapshot();
-  expect(inferDynamicRange(1)).toMatchSnapshot();
-  expect(inferDynamicRange(1.01)).toMatchSnapshot();
-  expect(inferDynamicRange(10)).toMatchSnapshot();
-  expect(inferDynamicRange(100)).toMatchSnapshot();
-  expect(inferDynamicRange(99)).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(0.4)))).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(1)))).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(1.01)))).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(10)))).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(100)))).toMatchSnapshot();
+  expect(asString(inferDynamicRange(BigNumber(99)))).toMatchSnapshot();
 });
 
 test("projectRangeIndex", () => {
-  expect(projectRangeIndex(inferDynamicRange(0.4), 2)).toMatchSnapshot();
+  expect(
+    asString(projectRangeIndex(inferDynamicRange(BigNumber(0.4)), BigNumber(2)))
+  ).toMatchSnapshot();
 });
 
 test("reverseRangeIndex", () => {
-  const range = inferDynamicRange(0.4);
+  const range = inferDynamicRange(BigNumber(0.4));
   for (let i = 0; i < range.steps; i++) {
     const n = projectRangeIndex(range, i);
     expect(reverseRangeIndex(range, n)).toBe(i);
--- src/range.js
+++ src/range.js
@@ -1,11 +1,12 @@
 // @flow
 // Utility to deal with ranges
+import { BigNumber } from "bignumber.js";
 
 export type Range = {
-  initial: number,
-  min: number,
-  max: number,
-  step: number,
+  initial: BigNumber,
+  min: BigNumber,
+  max: BigNumber,
+  step: BigNumber,
   steps: number
 };
 
@@ -22,56 +23,58 @@ export type InferDynamicRangeOpts = typeof defaultOpts;
 // to test this:
 // for (let i = 0.1; i < 10; i += 0.1) console.log(i, inferDynamicRange(i));
 export function inferDynamicRange(
-  amount: number,
+  amount: BigNumber,
   opts: $Shape<InferDynamicRangeOpts> = {}
 ): Range {
   const { minMult, maxMult, targetSteps } = { ...defaultOpts, ...opts };
-  const targetMin = amount * minMult;
-  const targetMax = amount * maxMult;
+  const targetMin = amount.times(minMult);
+  const targetMax = amount.times(maxMult);
   const step = findBestRangeStep(targetMin, targetMax, targetSteps);
   if (Number.isNaN(step) || step <= 0) {
     throw new Error("inferDynamicRange: invalid parameters");
   }
-  const initial = round(amount, step);
-  const min = floor(targetMin, step);
-  const max = ceil(targetMax, step);
-  const steps = 1 + (max - min) / step;
+  const initial = stepping(amount, step, BigNumber.ROUND_HALF_UP);
+  const min = stepping(targetMin, step, BigNumber.ROUND_FLOOR);
+  const max = stepping(targetMax, step, BigNumber.ROUND_CEIL);
+  const steps = max
+    .minus(min)
+    .div(step)
+    .plus(1)
+    .toNumber();
   return { initial, min, max, step, steps };
 }
 
-export function projectRangeIndex(range: Range, index: number): number {
-  return range.min + index * range.step;
+export function projectRangeIndex(range: Range, index: number): BigNumber {
+  return range.min.plus(range.step.times(index));
 }
 
-export function reverseRangeIndex(range: Range, n: number): number {
-  const x = (n - range.min) / (range.max - range.min);
-  const i = Math.floor(range.steps * x);
+export function reverseRangeIndex(range: Range, n: BigNumber): number {
+  const x = n.minus(range.min).div(range.max.minus(range.min));
+  const i = x
+    .times(range.steps)
+    .integerValue(BigNumber.ROUND_FLOOR)
+    .toNumber();
   return Math.max(0, Math.min(i, range.steps - 1));
 }
 
-function floor(n, step) {
-  return Math.floor(n / step) * step;
-}
-
-function ceil(n, step) {
-  return Math.ceil(n / step) * step;
-}
-
-function round(n, step) {
-  return Math.round(n / step) * step;
+function stepping(n, step, roundingMode) {
+  return n
+    .div(step)
+    .integerValue(roundingMode)
+    .times(step);
 }
 
 const log10 = Math.log(10);
 
 // return step to use for a [min,max] with a planned number of steps (will approx).
 function findBestRangeStep(min, max, steps) {
-  const nonRoundedStep = (max - min) / steps;
+  const nonRoundedStep = (max.toNumber() - min.toNumber()) / steps;
   const mag = Math.log(nonRoundedStep) / log10;
   const magInt = Math.floor(mag);
   const remain = mag - magInt;
-  let step = 10 ** (magInt + 1);
+  let step = BigNumber(10).pow(magInt + 1);
   // heuristics
-  if (remain < 0.3) step /= 5;
-  else if (remain < 0.7) step /= 2;
+  if (remain < 0.3) step = step.div(5);
+  else if (remain < 0.7) step = step.div(2);
   return step;
 }
