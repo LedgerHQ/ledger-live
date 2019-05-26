92f27dd82b4b9df13f69850329bbefd51dae4448
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-05-26 10:23:34 +0200

    Update to latest libcore that fixes encryption test

--- tool/tests/encryption/test.sh
+++ tool/tests/encryption/test.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
-echo "ENCRYPTION IS LIKELY TO FAIL, IGNORING ERRORS FOR NOW"
-# Unsetting the -e flag set by parent
-set +e
+set -e
 
-ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f json | jq '.operations[] | length' >> output/res
+ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f summary
 
 echo set password for first time
 ledger-live libcoreSetPassword --password foobar
 export LIBCORE_PASSWORD=foobar
 
-ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f json | jq '.operations[] | length' >> output/res
+echo try a sync with the new password
+ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f summary
 
 echo change password
 ledger-live libcoreSetPassword --password foo
 export LIBCORE_PASSWORD=foo
 
-ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f json | jq '.operations[] | length' >> output/res
+echo try a sync with the changed password
+ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f summary
 
 echo decrypt the libcore
 ledger-live libcoreSetPassword --password ""
 export LIBCORE_PASSWORD=
 
-ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f json | jq '.operations[] | length' >> output/res
-
+echo try a sync after removing the password
+ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f summary
 
 echo set again password and try to not use encryption
 ledger-live libcoreSetPassword --password foo
 
-LIBCORE_PASSWORD=mistake ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj || true
-
-echo "check encrypted data can be descripted later"
-
-LIBCORE_PASSWORD=foo ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f json | jq '.operations[] | length' >> output/res
-
-# restore -e flag
+echo try a sync with a wrong password
+set +e
+LIBCORE_PASSWORD=mistake ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj
+RES=$?
 set -e
+if [ $RES -eq 0 ]; then
+  echo expected failure with wrong password
+  exit 1
+fi
 
-true
+echo check encrypted data can be descripted later
+LIBCORE_PASSWORD=foo ledger-live sync -c bitcoin --xpub xpub6BosfCnifzxcFwrSzQiqu2DBVTshkCXacvNsWGYJVVhhawA7d4R5WSWGFNbi8Aw6ZRc1brxMyWMzG3DSSSSoekkudhUd9yLb6qx39T9nMdj -f summary
