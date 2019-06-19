91bc7aa161757fe168265b5924ccc46421dfad32
Juan Cortes Ross <juan@bohem.io>
2019-06-19 16:18:26 +0200

    Added getTransactionStatus

---
+++ src/transaction.js
@@ -0,0 +1,53 @@
+// @flow
+import type { BigNumber } from "bignumber.js";
+import type { Account, Transaction } from "./types";
+import type { AccountBridge } from "./bridge/types";
+
+export async function getTransactionStatus(
+  bridge: AccountBridge<any>,
+  account: Account,
+  transaction: Transaction
+): Promise<{
+  showFeeWarning: boolean,
+  estimatedFees: BigNumber,
+  amount: BigNumber,
+  totalSpent: BigNumber,
+  useAllAmount: boolean,
+  maxAmount: ?BigNumber
+}> {
+  const totalSpent = await bridge.getTotalSpent(account, transaction);
+  const amount = bridge.getTransactionAmount(account, transaction);
+  const useAllAmount = bridge.getTransactionExtra(
+    account,
+    transaction,
+    "useAllAmount"
+  );
+
+  let maxAmount;
+  if (useAllAmount) maxAmount = await bridge.getMaxAmount(account, transaction);
+
+  const estimatedFees = totalSpent - (maxAmount || amount);
+
+  const showFeeWarning =
+    (amount &&
+      amount.gt(0) &&
+      totalSpent
+        .minus(amount)
+        .times(10)
+        .gt(amount)) ||
+    (maxAmount &&
+      maxAmount.gt(0) &&
+      totalSpent
+        .minus(maxAmount)
+        .times(10)
+        .gt(maxAmount));
+
+  return {
+    showFeeWarning,
+    totalSpent,
+    amount,
+    estimatedFees,
+    useAllAmount,
+    maxAmount
+  };
+}
