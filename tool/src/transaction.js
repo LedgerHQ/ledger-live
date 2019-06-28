30641af1a182c6db67cc50ef1d3dcb8ac74e1fd6
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-06-28 23:41:00 +0200

    ability to do more than one tx at a time

--- tool/src/transaction.js
+++ tool/src/transaction.js
@@ -1,5 +1,8 @@
 // @flow
 
+import "lodash.product";
+import { product } from "lodash";
+import shuffle from "lodash/shuffle";
 import { BigNumber } from "bignumber.js";
 import { getAccountCurrency } from "@ledgerhq/live-common/lib/account";
 import { parseCurrencyUnit } from "@ledgerhq/live-common/lib/currencies";
@@ -22,7 +25,7 @@ const inferAmount = (account, str) => {
   return parseCurrencyUnit(units[0], str);
 };
 
-export const inferTransactionOpts = [
+export const inferTransactionsOpts = [
   {
     name: "self-transaction",
     type: Boolean,
@@ -36,7 +39,8 @@ export const inferTransactionOpts = [
   {
     name: "recipient",
     type: String,
-    desc: "the address to send funds to"
+    desc: "the address to send funds to",
+    multiple: true
   },
   {
     name: "amount",
@@ -59,85 +63,98 @@ export const inferTransactionOpts = [
     name: "token",
     alias: "t",
     type: String,
-    desc: "use an token account children of the account"
+    desc: "use an token account children of the account",
+    multiple: true
+  },
+  {
+    name: "shuffle",
+    type: Boolean,
+    desc: "if using multiple token or recipient, order will be randomized"
   }
 ];
-export async function inferTransaction(account, opts) {
-  const tShared =
-    account.currency.family === "bitcoin"
-      ? {
-          feePerByte: new BigNumber(
+export function inferTransactions(account, opts) {
+  let pairs = product(opts.token || [null], opts.recipient || [null]);
+
+  if (opts.shuffle) {
+    pairs = shuffle(pairs);
+  }
+
+  return Promise.all(
+    pairs.map(async ([token, recipient]) => {
+      const tShared = {};
+
+      let acc;
+      if (token) {
+        const tkn = token.toLowerCase();
+        const tokenAccounts = account.tokenAccounts || [];
+        const tokenAccount = tokenAccounts.find(
+          t => tkn === t.token.ticker.toLowerCase() || tkn === t.token.id
+        );
+        if (!tokenAccount) {
+          throw new Error(
+            "token account '" +
+              token +
+              "' not found. Available: " +
+              tokenAccounts.map(t => t.token.ticker).join(", ")
+          );
+        }
+        acc = tokenAccount;
+        tShared.tokenAccountId = tokenAccount.id;
+      } else {
+        acc = account;
+      }
+
+      switch (account.currency.family) {
+        case "bitcoin":
+          tShared.feePerByte = new BigNumber(
             opts.feePerByte === undefined ? 1 : opts.feePerByte
-          )
+          );
+
+        case "ethereum": {
+          tShared.gasPrice = inferAmount(account, opts.gasPrice || "2gwei");
+          if (opts.gasLimit) {
+            tShared.gasLimit = new BigNumber(opts.gasLimit);
+          }
+          if (!tShared.gasLimit) {
+            tShared.gasLimit = BigNumber(
+              await apiForCurrency(account.currency).estimateGasLimitForERC20(
+                acc.type === "TokenAccount"
+                  ? acc.token.contractAddress
+                  : recipient
+              )
+            );
+          }
         }
-      : account.currency.family === "ethereum"
-      ? {
-          gasPrice: inferAmount(account, opts.gasPrice || "2gwei"),
-          gasLimit: opts.gasLimit && new BigNumber(opts.gasLimit)
+      }
+
+      if (opts["use-all-amount"]) {
+        tShared.useAllAmount = true;
+      }
+
+      if (opts["self-transaction"]) {
+        return {
+          transaction: {
+            amount: opts.amount
+              ? inferAmount(acc, opts.amount)
+              : inferAmount(acc, "0.001"),
+            recipient: account.freshAddress,
+            ...tShared
+          }
+        };
+      }
+
+      if (!opts.amount && !tShared.useAllAmount)
+        throw new Error("amount is required");
+      if (!recipient) throw new Error("recipient is required");
+      return {
+        transaction: {
+          amount: tShared.useAllAmount
+            ? BigNumber(0)
+            : inferAmount(acc, opts.amount),
+          recipient,
+          ...tShared
         }
-      : null;
-
-  let res;
-
-  if (opts["use-all-amount"]) {
-    tShared.useAllAmount = true;
-  }
-
-  let acc;
-  if (opts.token) {
-    const tkn = opts.token.toLowerCase();
-    const tokenAccounts = account.tokenAccounts || [];
-    const tokenAccount = tokenAccounts.find(
-      t => tkn === t.token.ticker.toLowerCase() || tkn === t.token.id
-    );
-    if (!tokenAccount) {
-      throw new Error(
-        "token account '" +
-          opts.token +
-          "' not found. Available: " +
-          tokenAccounts.map(t => t.token.ticker).join(", ")
-      );
-    }
-    acc = tokenAccount;
-    tShared.tokenAccountId = tokenAccount.id;
-  } else {
-    acc = account;
-  }
-
-  if (opts["self-transaction"]) {
-    res = {
-      transaction: {
-        amount: opts.amount
-          ? inferAmount(acc, opts.amount)
-          : inferAmount(acc, "0.001"),
-        recipient: account.freshAddress,
-        ...tShared
-      }
-    };
-  } else {
-    if (!opts.amount && !tShared.useAllAmount)
-      throw new Error("amount is required");
-    if (!opts.recipient) throw new Error("recipient is required");
-    res = {
-      transaction: {
-        amount: tShared.useAllAmount
-          ? BigNumber(0)
-          : inferAmount(acc, opts.amount),
-        recipient: opts.recipient,
-        ...tShared
-      }
-    };
-  }
-
-  if (account.currency.family === "ethereum" && !res.transaction.gasLimit) {
-    res.transaction.gasLimit = BigNumber(
-      await apiForCurrency(account.currency).estimateGasLimitForERC20(
-        acc.type === "TokenAccount"
-          ? acc.token.contractAddress
-          : res.transaction.recipient
-      )
-    );
-  }
-
-  return res;
+      };
+    })
+  );
 }
