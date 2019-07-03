39c5bb75e3ebe01c565b1b76bb6e23d2996f5419
Thibaut Boustany <thibaut@kastor.fr>
2019-07-03 17:19:08 +0200

    Env variable for hiding empty token accounts

--- src/families/ethereum/libcore-buildTokenAccounts.js
+++ src/families/ethereum/libcore-buildTokenAccounts.js
@@ -10,6 +10,7 @@ import {
   findTokenByAddress,
   listTokensForCryptoCurrency
 } from "../../currencies";
+import { getEnv } from "../../env";
 
 async function buildERC20TokenAccount({
   parentAccountId,
@@ -20,6 +21,11 @@ async function buildERC20TokenAccount({
   const balance = await libcoreBigIntToBigNumber(
     await coreTokenAccount.getBalance()
   );
+
+  if (getEnv("HIDE_EMPTY_TOKEN_ACCOUNTS") && balance.isZero()) {
+    return null;
+  }
+
   const coreOperations = await coreTokenAccount.getOperations();
   const id = parentAccountId + "+" + token.contractAddress;
 
@@ -85,7 +91,7 @@ async function ethereumBuildTokenAccounts({
         token,
         coreTokenAccount: coreTA
       });
-      tokenAccounts.push(tokenAccount);
+      if (tokenAccount) tokenAccounts.push(tokenAccount);
     }
   }
 
