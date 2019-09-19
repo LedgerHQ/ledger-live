ddc65d235337b3476a809f05dfd53b7c36b73463
Juan Cortes Ross <juan@bohem.io>
2019-09-19 15:29:20 +0200

    LL-1895 Rollback reset functionality

--- src/components/HardResetModal.js
+++ src/components/HardResetModal.js
@@ -1,24 +1,18 @@
 /* @flow */
 
 import React, { PureComponent } from "react";
-import { StyleSheet, View } from "react-native";
 import { Trans } from "react-i18next";
 
 import colors from "../colors";
 import ModalBottomAction from "./ModalBottomAction";
 import Trash from "../icons/Trash";
-import Button from "./Button";
 import Circle from "./Circle";
 
-type Props = {
-  onRequestClose: () => void,
-  onHardReset: () => *,
-};
+type Props = {};
 
 // FIXME this is not a modal \o/
 class HardResetModal extends PureComponent<Props> {
   render() {
-    const { onRequestClose, onHardReset } = this.props;
     return (
       <ModalBottomAction
         title={null}
@@ -28,39 +22,10 @@ class HardResetModal extends PureComponent<Props> {
           </Circle>
         }
         description={<Trans i18nKey="reset.description" />}
-        footer={
-          <View style={styles.footerContainer}>
-            <Button
-              event="HardResetModalCancel"
-              type="secondary"
-              title={<Trans i18nKey="common.cancel" />}
-              onPress={onRequestClose}
-              containerStyle={styles.buttonContainer}
-            />
-            <Button
-              event="HardResetModalAction"
-              type="alert"
-              title={<Trans i18nKey="reset.button" />}
-              onPress={onHardReset}
-              containerStyle={[styles.buttonContainer, styles.buttonMarginLeft]}
-            />
-          </View>
-        }
+        footer={null}
       />
     );
   }
 }
 
 export default HardResetModal;
-
-const styles = StyleSheet.create({
-  footerContainer: {
-    flexDirection: "row",
-  },
-  buttonContainer: {
-    flex: 1,
-  },
-  buttonMarginLeft: {
-    marginLeft: 16,
-  },
-});
