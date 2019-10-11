90baa7ff1f3ab3a5949fbf18a118729df2e72505
Thibaut Boustany <thibaut@kastor.fr>
2019-10-11 17:49:12 +0200

    [iOS] Move native dependencies to CocoaPods

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see https://github.com/react-native-community/react-native-camera#face-detection-steps
-if [ -e node_modules/react-native-camera/ios/FaceDetector ] ; then
+if [ -e node_modules/react-native-camera/ios/FaceDetector ]; then
   rm -rf node_modules/react-native-camera/ios/FaceDetector
 fi
 cp node_modules/react-native-camera/postinstall_project/projectWithoutFaceDetection.pbxproj node_modules/react-native-camera/ios/RNCamera.xcodeproj/project.pbxproj
@@ -15,9 +15,18 @@ rn-nodeify --hack
 
 # Create the dev .env file with APP_NAME if it doesn't exist
 if ! [ -f .env ]; then
-  echo 'APP_NAME="LL [DEV]"' > .env;
+  echo 'APP_NAME="LL [DEV]"' >.env
 fi
 
-if [[ $DEBUG_RNDEBUGGER == "1" ]] ; then
+if [[ $DEBUG_RNDEBUGGER == "1" ]]; then
   rndebugger-open
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  if ! [ -x "$(command -v pod)" ]; then
+    echo 'Error: `pod` command is missing. Please install CocoaPods.' >&2
+    exit 1
+  fi
+
+  cd ios && pod install
 fi
