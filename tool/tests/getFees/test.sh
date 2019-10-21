234dfb7bfa250150c6a3bf6ee6f86b084ee20be7
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-10-21 15:33:41 +0200

    remove getFees test

--- tool/tests/getFees/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# just testing it doesn't crash here
-
-ledger-live getFees -c eth --xpub xpub6BemYiVNp19aDejXFVmety2Sv1qruzhbE7p1zAkY6mjbEShEo4dkxyawqhXDuzQhpi83W6fZ463mcqdW1ZVJj9Y3V5hTqUB4YgBcw7UCFWp
-ledger-live getFees -c xrp --xpub xpub6BemYiVNp19ZzgoVip83uhipVPKjNMjM4QYFeSJdA5EAUzQjNF66swQqH4afV15848dNnMLfcdrKkWZjNMAx2CZk1bfcukrdFGM3zzDfv1z
-ledger-live getFees -c btc --xpub xpub6Bm5P7Xyx2UYrVBAgb54gEswXhbZaryZSWsPjeJ1jpb9K9S5UTD5z5cXW4EREkTqkNjSHQHxwHKZJVE7TFvftySnKabMAXAQCMSVJBdJxMC
