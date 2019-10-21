a7ba37b2b9d777f58860d378826f0ca671d6f982
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-10-21 09:53:35 +0200

    Fixes ethereum optimistic update

--- src/families/ethereum/libcore-postSyncPatch.js
+++ src/families/ethereum/libcore-postSyncPatch.js
@@ -1,5 +1,6 @@
 // @flow
-import type { AccountLike, Account } from "../../types";
+import type { AccountLike, Account, Operation } from "../../types";
+import { shouldRetainPendingOperation } from "../../account";
 
 // we need to preserve ETH pendingOperations because there is no mempool to do this
 // we assume we need to preserve until:
@@ -9,16 +10,24 @@ import type { AccountLike, Account } from "../../types";
 const postSyncPatchGen = <T: AccountLike>(
   initial: T,
   synced: T,
-  latestNonce: number // latest eth op nonce
+  latestNonce: number, // latest eth op nonce
+  mainAccount: Account,
+  parentPendingOperation?: Operation[]
 ): T => {
   let pendingOperations = initial.pendingOperations || [];
   if (pendingOperations.length === 0) return synced;
   const { operations } = synced;
   synced.pendingOperations = pendingOperations.filter(
-    ({ transactionSequenceNumber, hash }) =>
-      transactionSequenceNumber &&
-      transactionSequenceNumber >= latestNonce &&
-      !operations.some(op => op.hash === hash)
+    op =>
+      (!parentPendingOperation ||
+        // a child pending parent need to disappear if parent eth op disappear
+        parentPendingOperation.some(o => o.hash === op.hash)) &&
+      op.transactionSequenceNumber &&
+      op.transactionSequenceNumber >= latestNonce &&
+      // retain logic
+      (shouldRetainPendingOperation(mainAccount, op) ||
+        // after retain logic, we need operation to appear
+        !operations.some(o => o.hash === op.hash))
   );
   return synced;
 };
@@ -27,14 +36,22 @@ const postSyncPatch = (initial: Account, synced: Account): Account => {
   const sendingOps = (synced.operations || []).filter(op => op.type === "OUT");
   const latestNonce =
     (sendingOps.length > 0 && sendingOps[0].transactionSequenceNumber) || 0;
-  const acc = postSyncPatchGen(initial, synced, latestNonce);
-  const { subAccounts } = acc;
-  const initialTAs = initial.subAccounts;
-  if (subAccounts && initialTAs) {
-    acc.subAccounts = subAccounts.map(ta => {
-      const initialTA = initialTAs.find(t => t.id === ta.id);
-      if (!initialTA) return ta;
-      return postSyncPatchGen(initialTA, ta, latestNonce);
+  const acc = postSyncPatchGen(initial, synced, latestNonce, synced);
+  const { subAccounts, pendingOperations } = acc;
+  const initialSubAccounts = initial.subAccounts;
+  if (subAccounts && initialSubAccounts) {
+    acc.subAccounts = subAccounts.map(subAccount => {
+      const initialSubAccount = initialSubAccounts.find(
+        a => a.id === subAccount.id
+      );
+      if (!initialSubAccount) return subAccount;
+      return postSyncPatchGen(
+        initialSubAccount,
+        subAccount,
+        latestNonce,
+        acc,
+        pendingOperations
+      );
     });
   }
   return acc;
