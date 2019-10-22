bd3bf02c0646723e3c27885edc65c0b1d84303ad
Juan Cortes Ross <juan@bohem.io>
2019-10-22 14:51:02 +0200

    Change from react-native-touch-id to react-native-fingerprint-scanner

--- src/context/AuthPass/auth.js
+++ src/context/AuthPass/auth.js
@@ -3,8 +3,9 @@ import biometry from "./methods/biometry";
 
 export default async (reason: string) => {
   let success = false;
-  if (await biometry.isSupported()) {
-    success = await biometry.authenticate(reason);
+  if (await biometry.isSensorAvailable()) {
+    success = await biometry.authenticate({description:reason});
+    biometry.release();
   }
   return success;
 };
--- src/context/AuthPass/methods/biometry.js
+++ src/context/AuthPass/methods/biometry.js
@@ -1,7 +1,7 @@
-import TouchID from "@ledgerhq/react-native-touch-id";
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 import { Platform } from "react-native";
 import unsupported from "./unsupported";
 
 export default Platform.OS === "android" && Platform.Version < 23
   ? unsupported
-  : TouchID;
+  : FingerprintScanner;
