e62e4ecdaa4f830680d8ea98a0035c5f11df9adb
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-10-24 00:35:13 +0200

    listApps multi page

--- src/hw/listApps.js
+++ src/hw/listApps.js
@@ -4,37 +4,44 @@ import type Transport from "@ledgerhq/hw-transport";
 export default async (transport: Transport<*>) => {
   const payload = await transport.send(0xe0, 0xde, 0, 0);
 
-  if (payload[0] !== 0x01) {
-    throw new Error("unknown listApps format");
+  const apps = [];
+  let data = payload;
+  // more than the status bytes
+  while (data.length > 2) {
+    if (payload[0] !== 0x01) {
+      throw new Error("unknown listApps format");
+    }
+    let i = 1;
+    while (i < data.length - 2) {
+      const length = data[i];
+      i++;
+      const blocks = data.readUInt16BE(i);
+      i += 2;
+      const flags = data.readUInt16BE(i);
+      i += 2;
+      const hashCodeData = data.slice(i, i + 32).toString("hex");
+      i += 32;
+      const hash = data.slice(i, i + 32).toString("hex");
+      i += 32;
+      const nameLength = data[i];
+      i++;
+      if (length !== nameLength + 70) {
+        throw new Error("invalid listApps length data");
+      }
+      const name = data.slice(i, i + nameLength).toString("ascii");
+      i += nameLength;
+      apps.push({
+        name,
+        hash,
+        hashCodeData,
+        blocks,
+        flags
+      });
+    }
+
+    // continue
+    data = await transport.send(0xe0, 0xdf, 0, 0);
   }
 
-  let i = 1;
-  const apps = [];
-  while (i < payload.length - 2) {
-    const length = payload[i];
-    i++;
-    const blocks = payload.readUInt16BE(i);
-    i += 2;
-    const flags = payload.readUInt16BE(i);
-    i += 2;
-    const hashCodeData = payload.slice(i, i + 32).toString("hex");
-    i += 32;
-    const hash = payload.slice(i, i + 32).toString("hex");
-    i += 32;
-    const nameLength = payload[i];
-    i++;
-    if (length !== nameLength + 70) {
-      throw new Error("invalid listApps length data");
-    }
-    const name = payload.slice(i, i + nameLength).toString("ascii");
-    i += nameLength;
-    apps.push({
-      name,
-      hash,
-      hashCodeData,
-      blocks,
-      flags
-    });
-  }
   return apps;
 };
