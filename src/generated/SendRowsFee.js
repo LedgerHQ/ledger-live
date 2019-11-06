37debf3f3f0936b6f988634667fbaf600c037091
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-11-06 10:20:05 +0100

    live-common and libcore bump
    - bridge to drop deprecated code
    - preload mecanism

---
+++ src/generated/SendRowsFee.js
@@ -0,0 +1,12 @@
+// @flow
+import bitcoin from "../families/bitcoin/SendRowsFee.js";
+import ethereum from "../families/ethereum/SendRowsFee.js";
+import ripple from "../families/ripple/SendRowsFee.js";
+import tezos from "../families/tezos/SendRowsFee.js";
+
+export default {
+  bitcoin,
+  ethereum,
+  ripple,
+  tezos,
+};
---
+++ src/generated/screens.js
@@ -0,0 +1,10 @@
+// @flow
+import bitcoin from "../families/bitcoin/screens.js";
+import ethereum from "../families/ethereum/screens.js";
+import ripple from "../families/ripple/screens.js";
+
+export default {
+  bitcoin,
+  ethereum,
+  ripple,
+};
