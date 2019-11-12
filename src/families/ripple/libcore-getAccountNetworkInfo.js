37ddb4481f1c3f8a34d3f28192041522e7f05536
Juan Cortes Ross <juan@bohem.io>
2019-11-12 13:37:32 +0100

    Remove hack to convert baseReserve to drop, its now provided in drops

--- src/families/ripple/libcore-getAccountNetworkInfo.js
+++ src/families/ripple/libcore-getAccountNetworkInfo.js
@@ -1,26 +1,19 @@
 // @flow
-import { BigNumber } from "bignumber.js";
-import type { Account } from "../../types";
 import type { NetworkInfo } from "./types";
 import type { CoreAccount } from "../../libcore/types";
 import { libcoreAmountToBigNumber } from "../../libcore/buildBigNumber";
 
 type Input = {
   coreAccount: CoreAccount,
-  account: Account
 };
 
 type Output = Promise<NetworkInfo>;
 
-async function ripple({ coreAccount, account }: Input): Output {
+async function ripple({ coreAccount }: Input): Output {
   const rippleLikeAccount = await coreAccount.asRippleLikeAccount();
   const feesRaw = await rippleLikeAccount.getFees();
   const baseReserveRaw = await rippleLikeAccount.getBaseReserve();
-  let baseReserve = await libcoreAmountToBigNumber(baseReserveRaw);
-  // Bug in libcore, that value is incorrectly in XRP unit value..
-  baseReserve = baseReserve.times(
-    BigNumber(10).pow(account.currency.units[0].magnitude)
-  );
+  const baseReserve = await libcoreAmountToBigNumber(baseReserveRaw);
   const serverFee = await libcoreAmountToBigNumber(feesRaw);
 
   return {
