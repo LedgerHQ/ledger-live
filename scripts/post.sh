bdf660a29f0c3a6466218043f646bc5877755fb2
Thibaut Boustany <thibaut@kastor.fr>
2019-11-26 14:39:50 +0100

    Clean-up obsolete patches

--- scripts/post.sh
+++ scripts/post.sh
@@ -4,15 +4,7 @@ cd $(dirname $0)/..
 
 ./scripts/sync-families-dispatch.sh
 
-# see https://github.com/react-native-community/react-native-camera#face-detection-steps
-if [ -e node_modules/react-native-camera/ios/FaceDetector ]; then
-  rm -rf node_modules/react-native-camera/ios/FaceDetector
-fi
-cp node_modules/react-native-camera/postinstall_project/projectWithoutFaceDetection.pbxproj node_modules/react-native-camera/ios/RNCamera.xcodeproj/project.pbxproj
-
 rm -f 'node_modules/@segment/analytics-ios/.clang-format' 'third-party/glog-0.3.5/test-driver'
-
-patch --forward -i scripts/rnc-RNCameraManager.patch node_modules/react-native-camera/ios/RN/RNCameraManager.m
 
 rn-nodeify --hack
 
