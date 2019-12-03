b0e75a47df6c1813a7c0f41cb8ae49436f6c18ee
Thibaut Boustany <thibaut@kastor.fr>
2019-12-03 16:26:45 +0100

    Refactor biometric auth

--- src/screens/Settings/General/BiometricsRow.js
+++ src/screens/Settings/General/BiometricsRow.js
@@ -1,5 +1,5 @@
 /* @flow */
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { createStructuredSelector } from "reselect";
 import { Switch, Alert } from "react-native";
 import { connect } from "react-redux";
@@ -9,8 +9,8 @@ import type { T } from "../../../types/common";
 import { setPrivacyBiometrics } from "../../../actions/settings";
 import { privacySelector } from "../../../reducers/settings";
 import type { Privacy } from "../../../reducers/settings";
-import auth from "../../../context/AuthPass/auth";
 import SettingsRow from "../../../components/SettingsRow";
+import RequestBiometricModal from "../../../components/RequestBiometricModal";
 
 type Props = {
   privacy: ?Privacy,
@@ -36,66 +36,78 @@ class BiometricsRow extends Component<Props, State> {
   };
 
   onValueChange = async (biometricsEnabled: boolean) => {
-    const { t } = this.props;
     if (this.state.validationPending) return;
     if (biometricsEnabled) {
       this.setState({ validationPending: true });
-      let success = false;
-      let error;
-      try {
-        success = await auth(t("auth.failed.biometrics.authenticate"));
-      } catch (e) {
-        error = e;
-      }
-      this.setState({ validationPending: false });
-      if (!success) {
-        Alert.alert(
-          t("auth.failed.title"),
-          `${t("auth.failed.denied")}\n${String(error || "")}`,
-        );
-        return;
-      }
+    } else {
+      this.props.setPrivacyBiometrics(false);
     }
-    this.props.setPrivacyBiometrics(biometricsEnabled);
+  };
+
+  onSuccess = () => {
+    this.setState({ validationPending: false });
+    this.props.setPrivacyBiometrics(true);
+  };
+
+  onError = error => {
+    const { t } = this.props;
+    this.setState({ validationPending: false });
+    Alert.alert(
+      t("auth.failed.title"),
+      `${t("auth.failed.denied")}\n${String(error || "")}`,
+    );
+  };
+
+  onCancel = () => {
+    this.setState({ validationPending: false });
   };
 
   render() {
     const { privacy, iconLeft } = this.props;
     const { validationPending } = this.state;
     if (!privacy) return null;
+
     return (
-      <Fragment>
+      <>
         {privacy.biometricsType && (
-          <SettingsRow
-            event="BiometricsRow"
-            iconLeft={iconLeft}
-            centeredIcon
-            title={
-              <Trans
-                i18nKey="auth.enableBiometrics.title"
-                values={{
-                  ...privacy,
-                  biometricsType: privacy.biometricsType,
-                }}
+          <>
+            <SettingsRow
+              event="BiometricsRow"
+              iconLeft={iconLeft}
+              centeredIcon
+              title={
+                <Trans
+                  i18nKey="auth.enableBiometrics.title"
+                  values={{
+                    ...privacy,
+                    biometricsType: privacy.biometricsType,
+                  }}
+                />
+              }
+              desc={
+                <Trans
+                  i18nKey="auth.enableBiometrics.desc"
+                  values={{
+                    ...privacy,
+                    biometricsType: privacy.biometricsType,
+                  }}
+                />
+              }
+            >
+              <Switch
+                value={privacy.biometricsEnabled || validationPending}
+                onValueChange={this.onValueChange}
               />
-            }
-            desc={
-              <Trans
-                i18nKey="auth.enableBiometrics.desc"
-                values={{
-                  ...privacy,
-                  biometricsType: privacy.biometricsType,
-                }}
-              />
-            }
-          >
-            <Switch
-              value={privacy.biometricsEnabled || validationPending}
-              onValueChange={this.onValueChange}
+            </SettingsRow>
+            <RequestBiometricModal
+              isVisible={validationPending}
+              onSuccess={this.onSuccess}
+              onError={this.onError}
+              onCancel={this.onCancel}
             />
-          </SettingsRow>
+          </>
         )}
-      </Fragment>
+      </>
     );
   }
 }
