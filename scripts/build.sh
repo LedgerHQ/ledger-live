dcde3f9ab1e5cd752f8c5bdde84d356309fcbb82
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2019-12-23 10:17:07 +0100

    es target

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+rm -rf lib src/data/icons/react*
+node scripts/buildPartnersReactLogos.js
+bash ./scripts/sync-families-dispatch.sh
+node scripts/buildReactIcons.js
+
+BABEL_ENV=cjs babel --ignore __tests__ -s -d lib src
+flow-copy-source -i \"__tests__/**\" src lib
+
+BABEL_ENV=es babel --ignore __tests__ -s -d lib-es src
+flow-copy-source -i \"__tests__/**\" src lib-es
