8bd373c659cb887f0d2c13b9125ca699c00e9612
Arnaud97234 <arnaud.ulric@gmail.com>
2019-12-23 16:15:41 +0100

    update script device-appsUpdateTestAll

--- cli/tests/device-appsUpdateTestAll.sh
+++ cli/tests/device-appsUpdateTestAll.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ~/Dev/ledger-live-common &&
 git clean -xdf &&
 git fetch Arnaud && git checkout device-appsUpdateTestAll &&
 git pull Arnaud device-appsUpdateTestAll &&
@@ -12,4 +11,13 @@ yalc add @ledgerhq/live-common &&
 yarn &&
 yarn link &&
 yarn build &&
-ledger-live appsUpdateTestAll
+ledger-live appsUpdateTestAll 2> tests/tmp/error.log
+
+if grep "FAILED" .tests/tmp/error.log; then
+    echo "An error has occured"
+    curl -i -H 'Authorization: token 6fd7ff181cd04c8d9ba780945ac52a0fc06e0b69' \
+-d '{"title": "device appsUpdateTestAll failed", "body":"An error has occured during the process. Find logs enclosed"}' \
+https://api.github.com/repos/Arnaud97234/ledger-live-common/issues
+else
+    echo "Success. All the app have been installed / updated / removed properly"
+fi
