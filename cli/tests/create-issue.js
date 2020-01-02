b5a6de39f95258b5ea03137ab6a6726f28763ddf
Arnaud97234 <arnaud.ulric@gmail.com>
2020-01-02 18:27:50 +0100

    Update log file retrieval

--- cli/tests/create-issue.js
+++ cli/tests/create-issue.js
@@ -1,6 +1,7 @@
 const Octokit = require('@octokit/rest')
-const content = process.argv[2]
+const fs = require('fs')
 
+const content = fs.readFileSync(process.argv[2] , 'utf-8')
 const github = new Octokit({
     auth: process.env.GH_TOKEN
 })
--- cli/tests/device-appsUpdateTestAll.sh
+++ cli/tests/device-appsUpdateTestAll.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e
+
 GIT_REMOTE=git@github.com:LedgerHQ/ledger-live-common.git
 
-git clean -xdf &&
-git fetch $GIT_REMOTE && git checkout master &&
-git pull &&
-rm -rf node_modules &&
-yarn &&
-yalc publish &&
-cd cli &&
-yalc add @ledgerhq/live-common &&
-yarn &&
-yarn link &&
-yarn build &&
-mkdir -p cli/tests/tmp/ &&
+git clean -xdf
+git fetch $GIT_REMOTE && git checkout master
+git pull
+rm -rf node_modules
+yarn
+yalc publish
+cd cli
+yalc add @ledgerhq/live-common
+yarn
+yarn link
+yarn build
+mkdir -p tests/tmp/
 ledger-live appsUpdateTestAll 2> cli/tests/tmp/error.log
 
 LOG_FILE=cli/tests/tmp/error.log
@@ -24,7 +26,7 @@ elif grep -i "NoDevice" $LOG_FILE; then
     echo "A Nano device needs to be connected"
 elif grep -i -E "FAILED|ERROR" $LOG_FILE; then
     echo "An error has occured. Creating an issue..."
-    node cli/tests/create-issue.js "`cat $LOG_FILE`"
+    node cli/tests/create-issue.js $LOG_FILE
 else
     echo "Success. All the apps have been installed / updated / removed properly"
 fi
