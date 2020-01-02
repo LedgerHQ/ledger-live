07123b0edd5268f9e5425f16a5c4db395905394f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-01-02 18:17:57 +0100

    Update cli/tool/mobile-test-app

--- mobile-test-app/scripts/copy.sh
+++ mobile-test-app/scripts/copy.sh
@@ -3,7 +3,7 @@
 set -e
 cd $(dirname $0)/..
 rm -rf ledger/tests
-cp -R ../tool/src/__tests__ ledger/tests
+cp -R ../cli/src/__tests__ ledger/tests
 cd ledger/tests
 for f in *.js; do
   if [ "$f" != "index.js" ]; then
