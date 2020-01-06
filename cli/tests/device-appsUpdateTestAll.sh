80ec64f58165806513019912283753090419810f
Arnaud97234 <arnaud.ulric@gmail.com>
2020-01-06 16:57:15 +0100

    update create issue script

--- cli/tests/device-appsUpdateTestAll.sh
+++ cli/tests/device-appsUpdateTestAll.sh
@@ -26,7 +26,7 @@ elif grep -i "NoDevice" $LOG_FILE; then
     echo "A Nano device needs to be connected"
 elif grep -i -E "FAILED|ERROR" $LOG_FILE; then
     echo "An error has occured. Creating an issue..."
-    node cli/tests/create-issue.js $LOG_FILE
+    node tests/create-issue.js $LOG_FILE
 else
     echo "Success. All the apps have been installed / updated / removed properly"
 fi
