a8e430579abbb96f60b9b7cfd86a6f36ed33044a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-01-10 12:52:07 +0100

    prettier

--- src/apps/formatting.test.js
+++ src/apps/formatting.test.js
@@ -1,16 +1,10 @@
 // @flow
-import {
-  formatSize
-} from "./formatting";
+import { formatSize } from "./formatting";
 
 test("Scenario: Formatting - format bytes", () => {
   const defaultSize = "default";
-  const bytes = [
-    [1024 * 10],
-    [0],
-    [undefined, defaultSize]
-  ];
-  const formattedBytes = bytes.map((args) => formatSize(...args));
+  const bytes = [[1024 * 10], [0], [undefined, defaultSize]];
+  const formattedBytes = bytes.map(args => formatSize(...args));
   const expectedSizes = ["10Kb", "", defaultSize];
   expect(formattedBytes).toEqual(expectedSizes);
 });
