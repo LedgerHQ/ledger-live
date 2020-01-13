1cec9cd23678b8f9ea217d043a4699c7c13df3ad
LFBarreto <lfb.access@gmail.com>
2020-01-13 09:00:51 +0100

    (apps): formatSize takes in account block size as param

--- src/apps/formatting.js
+++ src/apps/formatting.js
@@ -1,8 +1,12 @@
 // @flow
 import type { AppOp, InstalledItem } from "./types";
 
-export const formatSize = (size?: number, defaultString?: string = "") =>
-  !size ? defaultString : Math.round(size / 1024) + "Kb";
+/** format bytes size according to given block size */
+export const formatSize = (size?: number, blockSize: number = 1) => {
+  const formatedSize =
+    size && Math.ceil((Math.ceil(size / blockSize) * blockSize) / 1024);
+  return formatedSize ? `${formatedSize}Kb` : "";
+};
 
 export const prettyActionPlan = (ops: AppOp[]) =>
   ops.map(op => (op.type === "install" ? "+" : "-") + op.name).join(", ");
--- src/apps/formatting.test.js
+++ src/apps/formatting.test.js
@@ -2,9 +2,8 @@
 import { formatSize } from "./formatting";
 
 test("Scenario: Formatting - format bytes", () => {
-  const defaultSize = "default";
-  const bytes = [[1024 * 10], [0], [undefined, defaultSize]];
+  const bytes = [[4096], [1], [1025], [4097, 4096], [undefined], [0]];
   const formattedBytes = bytes.map(args => formatSize(...args));
-  const expectedSizes = ["10Kb", "", defaultSize];
+  const expectedSizes = ["4Kb", "1Kb", "2Kb", "8Kb", "", ""];
   expect(formattedBytes).toEqual(expectedSizes);
 });
