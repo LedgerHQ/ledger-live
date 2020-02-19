1121de590ce98d8cefddb8834dea8f647a54824e
Arnaud <13007150+Arnaud97234@users.noreply.github.com>
2020-02-19 13:35:43 +0100

    update XRP dataset (#521)
    * add eth token operation test
    
    * update XRP tests
    
    * drop ws explicit dep
    
    * prettier
    
    Co-authored-by: Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>

--- src/families/ripple/test-dataset.js
+++ src/families/ripple/test-dataset.js
@@ -22,13 +22,13 @@ const dataset: DatasetTest<Transaction> = {
         {
           name: "ripple seed 1",
           apdus: `
-      => e002004015058000002c80000090800000000000000000000000
-      <= 21023555225257bcd4649a451a6f5d1600a37c86e727b3c56427f2df2d085fbbad8a22724a667a524a48634d397147754d64554c474d376d553452696b71525934374678529000
-      => e002004015058000002c80000090800000010000000000000000
-      <= 210267925c331fcd8db90f1a95287eea7ce8ef73f9e5c0444ddcbe3aadfa9923a20c2272423670776f76737972465768505955736a6a3956334348636b393835516a6958699000
-      => e002004015058000002c80000090800000020000000000000000
-      <= 21037e1a0c07bcd5531b268f48f18fb3b8acb49058f9126180d7febc53ae839f058a22724d4c675159503775703578503366396f353146396b3171314a456639646f6141699000
-      `
+          => e002004015058000002c80000090800000000000000000000000
+          <= 2103d1adcff3e0cf1232b1416a75cd6f23b49dd6a25c69bc291a1f6783ec6825ec062272616765584842365134566276765764547a4b414e776a65435434485846434b58379000
+          => e002004015058000002c80000090800000010000000000000000
+          <= 21036da109ee84825eab0f55fb57bcf9ef0b05621e71fb0400266fb42d6f68f9487c2272425065393169766d67384347573450414e6f657555555173756d337470786a55469000
+          => e002004015058000002c80000090800000020000000000000000
+          <= 2102df9a55b79fb3668dac70fee7372806195841cd713ab8da9fba82240f9db8a23921725a76426335653259523141396f745333723944794768334e445038584c4c70349000
+          `
         }
       ],
       accounts: [
@@ -83,7 +83,7 @@ const dataset: DatasetTest<Transaction> = {
               name: "recipient and sender must not be the same",
               transaction: fromTransactionRaw({
                 family: "ripple",
-                recipient: "rJfzRJHcM9qGuMdULGM7mU4RikqRY47FxR",
+                recipient: "rageXHB6Q4VbvvWdTzKANwjeCT4HXFCKX7",
                 amount: "10000000",
                 tag: null,
                 fee: "1",
@@ -121,30 +121,21 @@ const dataset: DatasetTest<Transaction> = {
             }
           ],
           raw: {
-            id:
-              "libcore:1:ripple:xpub6BemYiVNp19a2SqH5MuUUuMUsiMU4ZLcXQgfoFxbRSRjPEuzcwcjx5SXezUhwcmgCTKGzuGAqHxRFSCn6YLAqydEdq11LVYENwxNC6ctwrv:",
-            seedIdentifier:
-              "02b96ea039567968d11d12e3195e4b6194a016c18511e51814e5cca03fcd800a29",
+            id: "ripplejs:2:ripple:rageXHB6Q4VbvvWdTzKANwjeCT4HXFCKX7:",
+            seedIdentifier: "rageXHB6Q4VbvvWdTzKANwjeCT4HXFCKX7",
             name: "XRP 1",
             derivationMode: "",
             index: 0,
-            freshAddress: "rJfzRJHcM9qGuMdULGM7mU4RikqRY47FxR",
+            freshAddress: "rageXHB6Q4VbvvWdTzKANwjeCT4HXFCKX7",
             freshAddressPath: "44'/144'/0'/0/0",
-            freshAddresses: [
-              {
-                address: "rJfzRJHcM9qGuMdULGM7mU4RikqRY47FxR",
-                derivationPath: "44'/144'/0'/0/0"
-              }
-            ],
+            freshAddresses: [],
             blockHeight: 0,
             operations: [],
             pendingOperations: [],
             currencyId: "ripple",
             unitMagnitude: 6,
             lastSyncDate: "",
-            balance: "31209950",
-            xpub:
-              "xpub6BemYiVNp19a2SqH5MuUUuMUsiMU4ZLcXQgfoFxbRSRjPEuzcwcjx5SXezUhwcmgCTKGzuGAqHxRFSCn6YLAqydEdq11LVYENwxNC6ctwrv"
+            balance: "21000310"
           }
         }
       ]
