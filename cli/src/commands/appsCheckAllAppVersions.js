8421b745111e66608ffa9c6cc7dac07558dd1629
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-02-21 14:34:07 +0100

    Improve cli command appsCheckAllAppVersions

--- cli/src/commands/appsCheckAllAppVersions.js
+++ cli/src/commands/appsCheckAllAppVersions.js
@@ -1,12 +1,19 @@
 /* eslint-disable no-console */
 // @flow
 
-import { from, empty, concat, defer } from "rxjs";
-import { mergeMap, ignoreElements, catchError } from "rxjs/operators";
+import fs from "fs";
+import { from, empty, concat, defer, of } from "rxjs";
+import {
+  mergeMap,
+  ignoreElements,
+  catchError,
+  filter,
+  map
+} from "rxjs/operators";
 import { withDevice } from "@ledgerhq/live-common/lib/hw/deviceAccess";
 import getDeviceInfo from "@ledgerhq/live-common/lib/hw/getDeviceInfo";
 import ManagerAPI from "@ledgerhq/live-common/lib/api/Manager";
-import { listApps } from "@ledgerhq/live-common/lib/apps/hw";
+import network from "@ledgerhq/live-common/lib/network";
 import installApp from "@ledgerhq/live-common/lib/hw/installApp";
 import uninstallApp from "@ledgerhq/live-common/lib/hw/uninstallApp";
 import type {
@@ -14,10 +21,94 @@ import type {
   ApplicationVersion,
   Application
 } from "@ledgerhq/live-common/lib/types/manager";
+import { initState, reducer, runAll } from "@ledgerhq/live-common/lib/apps";
+import { listApps, execWithTransport } from "@ledgerhq/live-common/lib/apps/hw";
 import { delay } from "@ledgerhq/live-common/lib/promise";
 import { getEnv } from "@ledgerhq/live-common/lib/env";
 import { getDependencies } from "@ledgerhq/live-common/lib/apps/polyfill";
 import { deviceOpt } from "../scan";
+type ResultCommon = {
+  versionId: number,
+  appPath: string
+};
+
+type Result =
+  | {
+      ...ResultCommon,
+      status: "OK"
+    }
+  | {
+      ...ResultCommon,
+      status: "KO",
+      error: string
+    };
+
+class MemoFile {
+  file: string;
+  constructor(file: string) {
+    this.file = file;
+  }
+
+  readResults(): Promise<Result[]> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this.file, "utf8", (err, data) => {
+        if (err) return reject(err);
+        resolve(
+          data
+            .split("\n")
+            .map((line: string): ?Result => {
+              const [versionIdStr, appPath, status, ...rest] = line
+                .split(":")
+                .map(s => s.trim());
+              const error = rest.join(": ");
+              const versionId = parseInt(versionIdStr, 10);
+              if (isNaN(versionId) || !isFinite(versionId) || versionId <= 0)
+                return;
+              if (versionId && appPath) {
+                if (status === "OK") {
+                  return {
+                    versionId,
+                    appPath,
+                    status: "OK"
+                  };
+                } else if (status === "KO" && typeof error === "string") {
+                  return {
+                    versionId,
+                    appPath,
+                    status: "KO",
+                    error
+                  };
+                }
+              }
+            })
+            .filter(Boolean)
+        );
+      });
+    });
+  }
+
+  writeResults(results: Result[]): Promise<void> {
+    const data = results
+      .map(result =>
+        [
+          result.versionId,
+          result.appPath,
+          result.status,
+          result.error || ""
+        ].join(": ")
+      )
+      .join("\n");
+    return new Promise((resolve, reject) => {
+      fs.writeFile(this.file, data, "utf8", err => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+}
+
+let results = [];
+let memoFile;
 
 type Candidate = {
   app: Application,
@@ -25,20 +116,36 @@ type Candidate = {
   installQueue: ApplicationVersion[]
 };
 
-const findCandidates = (
+const getAPIDeviceVersionId = async (
+  deviceInfo: DeviceInfo
+): Promise<?number> => {
+  const targetId = String(deviceInfo.targetId);
+  const { data } = await network({
+    method: "GET",
+    url: `${getEnv("MANAGER_API_BASE")}/devices`
+  });
+  for (const device of data) {
+    for (const deviceVersion of device.device_versions) {
+      if (deviceVersion.target_id === targetId) {
+        return deviceVersion.id;
+      }
+    }
+  }
+};
+
+const findCandidates = async (
   deviceModel,
   applications: Application[],
   deviceInfo: DeviceInfo
-): Candidate[] => {
+): Promise<Candidate[]> => {
+  const deviceVersionId = await getAPIDeviceVersionId(deviceInfo);
+  if (!deviceVersionId) throw new Error("unknown device version plugged");
   const candidates = applications.flatMap(app => {
     const deps = getDependencies(app.name);
     return app.application_versions
       .filter(
         v =>
-          v.providers.includes(1) &&
-          v.firmware.startsWith(
-            deviceModel.id.toLowerCase() + "/" + deviceInfo.version
-          )
+          v.providers.includes(1) && v.device_versions.includes(deviceVersionId)
       )
       .map(version => {
         return {
@@ -108,34 +215,142 @@ const checkInstalled = (installed, candidate: Candidate) => {
   }
   const hashMatches = ins.hash === candidate.version.hash;
   const hasBytes = !!candidate.version.bytes;
+  let result;
+
   if (hashMatches && hasBytes) {
+    result = {
+      versionId: candidate.version.id,
+      appPath: candidate.version.firmware,
+      status: "OK"
+    };
     console.log("OK " + name);
   } else {
-    console.error(
-      "FAIL " +
-        name +
-        (hashMatches
-          ? ""
-          : " have BAD HASH: API have " +
-            candidate.version.hash +
-            " but device have " +
-            ins.hash) +
-        (hasBytes ? "" : " DOES NOT have bytes defined!")
-    );
+    const message =
+      (hashMatches
+        ? ""
+        : " have BAD HASH. API have " +
+          candidate.version.hash +
+          " but device have " +
+          ins.hash) + (hasBytes ? "" : " DOES NOT have bytes defined!");
+
+    result = {
+      versionId: candidate.version.id,
+      appPath: candidate.version.firmware,
+      status: "KO",
+      error: message
+    };
+    console.error("FAIL " + name + message);
   }
+
+  results = results
+    .filter(r => r.versionId !== result.versionId)
+    .concat(result);
+
+  if (memoFile) {
+    return from(memoFile.writeResults(results)).pipe(ignoreElements());
+  }
+
+  return empty();
 };
+
+const wipeAll = (t, deviceInfo) =>
+  listApps(t, deviceInfo).pipe(
+    filter(e => e.type === "result"),
+    map(e => e.result),
+    mergeMap(listAppsResult => {
+      const exec = execWithTransport(t);
+      let s = initState(listAppsResult);
+      s = reducer(s, { type: "wipe" });
+      if (s.uninstallQueue.length) {
+        console.log("Uninstall " + s.uninstallQueue.length + " app(s)");
+      }
+      return runAll(s, exec);
+    })
+  );
 
 export default {
   description:
     "install/uninstall all possible apps available on our API to check all is good (even old app versions)",
-  args: [deviceOpt],
-  job: ({ device }: $Shape<{ device: string }>) =>
+  args: [
+    deviceOpt,
+    {
+      name: "memo",
+      alias: "m",
+      desc:
+        "a file to memorize the previously saved result so we don't run again from the start"
+    }
+  ],
+  job: ({ device, memo }: $Shape<{ device: string, memo: string }>) =>
     withDevice(device || "")(t => {
       // $FlowFixMe
-      return from(Promise.all([getDeviceInfo(t), ManagerAPI.listApps()])).pipe(
-        mergeMap(([deviceInfo, applications]) =>
-          // $FlowFixMe
-          findCandidates(t.deviceModel, applications, deviceInfo).reduce(
+      return from(
+        Promise.all([getDeviceInfo(t), ManagerAPI.listApps()]).then(
+          async ([deviceInfo, applications]) => {
+            const candidates = await findCandidates(
+              // $FlowFixMe
+              t.deviceModel,
+              applications,
+              deviceInfo
+            );
+            let candidatesErrors = [];
+            let candidatesNew = [...candidates];
+
+            if (memo) {
+              memoFile = new MemoFile(memo);
+              results = await memoFile.readResults();
+              candidatesErrors = [];
+              candidatesNew = [];
+              candidates.forEach(c => {
+                const result = results.find(r => r.versionId === c.version.id);
+                if (process.env.VERBOSE) {
+                  console.log(
+                    c.version.id,
+                    c.version.firmware,
+                    result
+                      ? "result was " +
+                          result.status +
+                          (result.status === "KO" ? " " + result.error : "")
+                      : ""
+                  );
+                }
+                if (result) {
+                  if (result.status === "KO") {
+                    candidatesErrors.push(c);
+                  }
+                } else {
+                  candidatesNew.push(c);
+                }
+              });
+            }
+
+            const all = [...candidatesNew, ...candidatesErrors];
+
+            console.log(
+              (
+                (100 * (candidates.length - candidatesNew.length)) /
+                candidates.length
+              ).toFixed(0) +
+                "% of apps versions remaining to test. (" +
+                candidates.length +
+                " in total. " +
+                candidatesNew.length +
+                " new. " +
+                candidatesErrors.length +
+                " errors)"
+            );
+
+            return [deviceInfo, all];
+          }
+        )
+      ).pipe(
+        mergeMap(([deviceInfo, candidates]) =>
+          concat(
+            wipeAll(t, deviceInfo).pipe(ignoreElements()),
+            of([deviceInfo, candidates])
+          )
+        ),
+        mergeMap(([deviceInfo, candidates]) =>
+          candidates.reduce(
             (acc, candidate) =>
               concat(
                 acc,
@@ -143,10 +358,24 @@ export default {
                   installCandidate(t, deviceInfo, candidate).pipe(
                     ignoreElements(),
                     catchError(e => {
+                      const result = {
+                        versionId: candidate.version.id,
+                        appPath: candidate.version.firmware,
+                        status: "KO",
+                        error: "FAILED installing"
+                      };
+                      results = results
+                        .filter(r => r.versionId !== result.versionId)
+                        .concat(result);
                       console.error(
                         "FAILED installing " + getCandidateName(candidate),
                         e
                       );
+                      if (memoFile) {
+                        return from(memoFile.writeResults(results)).pipe(
+                          ignoreElements()
+                        );
+                      }
                       return empty();
                     })
                   )
@@ -168,12 +397,12 @@ export default {
                       });
                     })
                   ).pipe(
-                    mergeMap(installed => {
-                      checkInstalled(installed, candidate);
-                      return uninstallCandidate(t, deviceInfo, candidate).pipe(
-                        ignoreElements()
-                      );
-                    })
+                    mergeMap(installed =>
+                      concat(
+                        checkInstalled(installed, candidate),
+                        uninstallCandidate(t, deviceInfo, candidate)
+                      ).pipe(ignoreElements())
+                    )
                   )
                 )
               ),
