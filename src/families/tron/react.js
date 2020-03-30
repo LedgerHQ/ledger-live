14a93872cbfbc51415b273b41cef14dc4ed4708b
LFBarreto <lfb.access@gmail.com>
2020-03-30 16:34:32 +0200

    (Tron): added voting flow react hooks + some tests

--- src/families/tron/react.js
+++ src/families/tron/react.js
@@ -1,18 +1,36 @@
 // @flow
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useReducer, useRef } from "react";
 import { getTronSuperRepresentatives, getNextVotingDate } from "../../api/Tron";
 
 import { BigNumber } from "bignumber.js";
 
-import type { SuperRepresentative, Vote } from "./types";
+import type { SuperRepresentative, Vote, TronResources } from "./types";
 import type { Account } from "../../types";
 
 import { getCryptoCurrencyById } from "../../currencies";
+
+export type Action = {
+  type: "updateVote" | "resetVotes" | "clearVotes",
+  address: string,
+  value: string
+};
+
+export type State = {
+  votes: { [address: string]: number }, // formatted Map of votes
+  votesAvailable: number, // total of available TP
+  votesUsed: number, // total of TP used
+  votesSelected: number, // number of SR votes selected
+  max: number, // votes remaining
+  initialVotes: { [address: string]: number } // initial Map of votes
+};
 
 const oneTrx = BigNumber(10).pow(
   getCryptoCurrencyById("tron").units[0].magnitude
 );
 export const MIN_TRANSACTION_AMOUNT = oneTrx;
+
+export const SR_THRESHOLD = 27;
+export const SR_MAX_VOTES = 5;
 
 /** Fetch the list of super representatives */
 export const useTronSuperRepresentatives = (): Array<SuperRepresentative> => {
@@ -68,3 +86,160 @@ export const formatVotes = (
       }))
     : [];
 };
+
+/** Search filters for SR list */
+const searchFilter = (query?: string) => ({
+  name,
+  address
+}: {
+  name: ?string,
+  address: string
+}) => {
+  if (!query) return true;
+  const terms = `${name || ""} ${address}`;
+  return terms.toLowerCase().includes(query.toLowerCase().trim());
+};
+
+/** Hook to search and sort SR list according to initial votes and query */
+export function useSortedSr(
+  search: string,
+  superRepresentatives: SuperRepresentative[],
+  votes: Vote[]
+): {
+  sr: SuperRepresentative,
+  name: ?string,
+  address: string,
+  rank: number,
+  isSR: boolean
+}[] {
+  const { current: initialVotes } = useRef(votes.map(({ address }) => address));
+
+  const SR = useMemo(
+    () =>
+      superRepresentatives.map((sr, rank) => ({
+        sr,
+        name: sr.name,
+        address: sr.address,
+        rank: rank + 1,
+        isSR: rank <= SR_THRESHOLD
+      })),
+    [superRepresentatives]
+  );
+
+  const sortedVotes = useMemo(
+    () =>
+      SR.filter(({ address }) => initialVotes.includes(address)).concat(
+        SR.filter(({ address }) => !initialVotes.includes(address))
+      ),
+    [SR, initialVotes]
+  );
+
+  const sr = useMemo(
+    () => (search ? SR.filter(searchFilter(search)) : sortedVotes),
+    [search, SR, sortedVotes]
+  );
+
+  return sr;
+}
+
+/** Tron vote flow state reducer */
+function votesReducer(state: State, { type, address, value }: Action) {
+  switch (type) {
+    case "updateVote": {
+      const voteCount = value
+        ? parseInt(Number(value.replace(/[^0-9]/g, "")))
+        : 0;
+      const currentVotes = Object.values({
+        ...state.votes,
+        [address]: voteCount
+      }).filter(Boolean);
+
+      const votes = {
+        ...state.votes,
+        [address]:
+          voteCount <= 0 || currentVotes.length > SR_MAX_VOTES ? 0 : voteCount
+      };
+
+      const votesUsed = Object.values(votes).reduce(
+        (sum, count) => sum + Number(count),
+        0
+      );
+
+      return {
+        ...state,
+        votes,
+        votesUsed,
+        votesSelected: Object.values(votes).filter(Boolean).length,
+        max: Math.max(0, state.votesAvailable - votesUsed)
+      };
+    }
+    case "resetVotes": {
+      const { initialVotes, votesAvailable } = state;
+      const votesUsed = Object.values(initialVotes).reduce(
+        (sum, voteCount) => sum + Number(voteCount),
+        0
+      );
+      return {
+        ...state,
+        votes: initialVotes,
+        votesUsed,
+        votesSelected: Object.keys(initialVotes).length,
+        max: Math.max(0, votesAvailable - votesUsed)
+      };
+    }
+    case "clearVotes": {
+      return {
+        ...state,
+        votes: {},
+        votesUsed: 0,
+        votesSelected: 0,
+        max: state.votesAvailable
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+/** Vote flow init state */
+function initState(initialVotes, tronResources): State {
+  const votes = initialVotes.reduce(
+    (sum, { voteCount, address }) => ({ ...sum, [address]: voteCount }),
+    {}
+  );
+  const votesAvailable = tronResources ? tronResources.tronPower : 0;
+  const votesUsed = Object.values(votes).reduce(
+    (sum, voteCount) => sum + Number(voteCount),
+    0
+  );
+
+  return {
+    votes,
+    votesAvailable,
+    votesUsed,
+    votesSelected: initialVotes.length,
+    max: Math.max(0, votesAvailable - votesUsed),
+    initialVotes: votes
+  };
+}
+
+export function toTransactionVotes(votes: {
+  [address: string]: number
+}): Vote[] {
+  return Object.keys(votes)
+    .map(address => ({ address, voteCount: votes[address] }))
+    .filter(({ voteCount }) => voteCount > 0);
+}
+
+/** Hook to retrieve and update voting flow state */
+export function useVotesReducer(
+  initialVotes: Vote[],
+  tronResources: ?TronResources
+): [State, (action: Action) => void] {
+  const [state, dispatch]: [State, (action: Action) => void] = useReducer(
+    votesReducer,
+    initState(initialVotes, tronResources)
+  );
+
+  return [state, dispatch];
+}
--- src/families/tron/react.test.js
+++ src/families/tron/react.test.js
@@ -4,7 +4,9 @@ import {
   useTronSuperRepresentatives,
   useNextVotingDate,
   getNextRewardDate,
-  formatVotes
+  formatVotes,
+  useSortedSr,
+  useVotesReducer
 } from "./react";
 
 import { __NEXT_REWARD_DATE__, mockAccount, mockAccountNoReward } from "./mock";
@@ -54,4 +56,135 @@ test("Tron format votes - formatVotes - Expect to get formatted votes", () => {
   expect(formatVotes(__VOTES__, superRepresentatives)).toStrictEqual(
     __FORMATTED_VOTES__
   );
+});
+
+const SR_INDEX_1 = 9;
+const SR_INDEX_2 = 2;
+const SR_INDEX_3 = 1;
+
+const VOTE_AMOUNT_1 = 10;
+const VOTE_AMOUNT_2 = 50;
+const VOTE_AMOUNT_3 = 60;
+
+const votes = [
+  {
+    address: superRepresentatives[SR_INDEX_1].address,
+    voteCount: VOTE_AMOUNT_1
+  },
+  {
+    address: superRepresentatives[SR_INDEX_2].address,
+    voteCount: VOTE_AMOUNT_2
+  }
+];
+
+const formattedVotes = {
+  [superRepresentatives[SR_INDEX_1].address]: VOTE_AMOUNT_1,
+  [superRepresentatives[SR_INDEX_2].address]: VOTE_AMOUNT_2
+};
+
+test("Tron search SR - search SR in the list - Expect to retrieve a specific list SR", () => {
+  const { result } = renderHook(() =>
+    useSortedSr(
+      superRepresentatives[SR_INDEX_1].name,
+      superRepresentatives,
+      votes
+    )
+  );
+  act(() => {
+    expect(Array.isArray(result.current)).toBe(true);
+    expect(result.current[0].address).toBe(
+      superRepresentatives[SR_INDEX_1].address
+    );
+  });
+});
+
+test("Tron search SR - search SR in the list - Expect to retrieve all the list if no search provided and sorted by votes", () => {
+  const { result } = renderHook(() =>
+    useSortedSr("", superRepresentatives, votes)
+  );
+  act(() => {
+    expect(Array.isArray(result.current)).toBe(true);
+    expect(result.current.length).toBe(superRepresentatives.length);
+    expect(result.current[0].address).toBe(votes[1].address);
+    expect(result.current[1].address).toBe(votes[0].address);
+  });
+});
+
+test("Tron vote State - manage tron voting state - Expect to retrieve the current voting state", () => {
+  const { tronResources } = mockAccount;
+  const tronPower = tronResources ? tronResources.tronPower : 0;
+
+  const UPDATE_ACTION = {
+    type: "updateVote",
+    address: superRepresentatives[SR_INDEX_3].address,
+    value: String(VOTE_AMOUNT_3)
+  };
+
+  const RESET_ACTION = {
+    type: "resetVotes"
+  };
+
+  const CLEAR_ACTION = {
+    type: "clearVotes"
+  };
+
+  const DEFAULT_ACTION = {};
+
+  const INIT_STATE = {
+    initialVotes: formattedVotes,
+    max: tronPower - (VOTE_AMOUNT_1 + VOTE_AMOUNT_2),
+    votes: formattedVotes,
+    votesAvailable: tronPower,
+    votesSelected: 2,
+    votesUsed: VOTE_AMOUNT_1 + VOTE_AMOUNT_2
+  };
+
+  const UPDATED_STATE = {
+    initialVotes: formattedVotes,
+    max: tronPower - (VOTE_AMOUNT_1 + VOTE_AMOUNT_2 + VOTE_AMOUNT_3),
+    votes: {
+      ...formattedVotes,
+      [superRepresentatives[SR_INDEX_3].address]: VOTE_AMOUNT_3
+    },
+    votesAvailable: tronPower,
+    votesSelected: 3,
+    votesUsed: VOTE_AMOUNT_1 + VOTE_AMOUNT_2 + VOTE_AMOUNT_3
+  };
+
+  const CLEAR_STATE = {
+    initialVotes: formattedVotes,
+    max: tronPower,
+    votes: {},
+    votesAvailable: tronPower,
+    votesSelected: 0,
+    votesUsed: 0
+  };
+
+  const { result } = renderHook(() => useVotesReducer(votes, tronResources));
+
+  expect(Array.isArray(result.current)).toStrictEqual(true);
+  expect(result.current[0]).toStrictEqual(INIT_STATE);
+  expect(typeof result.current[1] === "function").toBe(true);
+
+  act(() => {
+    result.current[1](UPDATE_ACTION);
+  });
+  expect(result.current[0]).toStrictEqual(UPDATED_STATE);
+
+  act(() => {
+    result.current[1](CLEAR_ACTION);
+  });
+
+  expect(result.current[0]).toStrictEqual(CLEAR_STATE);
+
+  act(() => {
+    result.current[1](RESET_ACTION);
+  });
+
+  expect(result.current[0]).toStrictEqual(INIT_STATE);
+
+  act(() => {
+    result.current[1](DEFAULT_ACTION);
+  });
+  expect(result.current[0]).toStrictEqual(INIT_STATE);
 });
