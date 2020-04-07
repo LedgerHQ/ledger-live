14a70f60c0799ca1b5dd4e5928dce7a4355868bd
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-04-07 16:49:17 +0200

    prettier

--- src/families/tron/bridge/js.js
+++ src/families/tron/bridge/js.js
@@ -378,7 +378,12 @@ const getAccountShape = async (info, syncConfig) => {
     subAccounts.map(s => s.operations)
   )
     .filter(o => o.type === "OUT" && o.fee.isGreaterThan(0))
-    .map(o => ({ ...o, accountId: info.id, value: o.fee, id: `${info.id}-${o.hash}-OUT` }));
+    .map(o => ({
+      ...o,
+      accountId: info.id,
+      value: o.fee,
+      id: `${info.id}-${o.hash}-OUT`
+    }));
 
   // add them to the parent operations and sort by date desc
   const parentOpsAndSubOutOpsWithFee = parentOperations
