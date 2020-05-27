d01b0da6a91e8fad22ad83ec137f1f02646d2bfc
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2020-05-27 18:44:22 +0200

    Ethereum specs bot (#673)
    * add a way to filter on one or more coin families for the bot
    
    * first spec with erc20 tokens

--- src/families/ethereum/specs.js
+++ src/families/ethereum/specs.js
@@ -1,6 +1,7 @@
 // @flow
 import expect from "expect";
 import invariant from "invariant";
+import sample from "lodash/sample";
 import type { Transaction } from "./types";
 import { getCryptoCurrencyById, parseCurrencyUnit } from "../../currencies";
 import { pickSiblings } from "../../bot/specs";
@@ -35,6 +36,39 @@ const ethereum: AppSpec<Transaction> = {
         expect(account.balance.toString()).toBe(
           accountBeforeTransaction.balance.minus(operation.value).toString()
         );
+      },
+    },
+    {
+      name: "move all of ERC20 tokens to another account",
+      transaction: ({ account, siblings, bridge }) => {
+        invariant(account.subAccounts, "no erc20 tokens available");
+        invariant(
+          account.balance.gt(minimalAmount),
+          "parent balance is too low"
+        );
+        const erc20Account = sample(account.subAccounts);
+        invariant(erc20Account.balance.gt(0), "balance is too low");
+        let t = bridge.createTransaction(account);
+        const sibling = pickSiblings(siblings);
+        const recipient = sibling.freshAddress;
+        t = bridge.updateTransaction(t, {
+          useAllAmount: true,
+          recipient,
+          subAccountId: erc20Account.id,
+        });
+        return t;
+      },
+      deviceAction: deviceActions.acceptTransaction,
+      test: ({ account, transaction }) => {
+        if (account.subAccounts) {
+          const erc20account = account.subAccounts.find(
+            (s) => s.id === transaction.subAccountId
+          );
+
+          if (erc20account) {
+            expect(erc20account.balance.toString()).toBe("0");
+          }
+        }
       },
     },
   ],
