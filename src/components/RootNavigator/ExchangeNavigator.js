2b2ef2ce2dd6ed102cb13ab7ff10c5b4d035c255
LFBarreto <lfb.access@gmail.com>
2020-06-22 15:30:31 +0200

    (BuyCrypto): minor style polishes

--- src/components/RootNavigator/ExchangeNavigator.js
+++ src/components/RootNavigator/ExchangeNavigator.js
@@ -6,6 +6,12 @@ import { ScreenName } from "../../const";
 import styles from "../../navigation/styles";
 import Buy from "../../screens/Exchange/Buy";
 import History from "../../screens/Exchange/History";
+import LText from "../LText";
+
+type TabLabelProps = {
+  focused: boolean,
+  color: string,
+};
 
 export default function CryptoAssetsSettingsNavigator() {
   const { t } = useTranslation();
@@ -19,12 +25,27 @@ export default function CryptoAssetsSettingsNavigator() {
       <Tab.Screen
         name={ScreenName.ExchangeBuy}
         component={Buy}
-        options={{ title: t("exchange.buy.tabTitle") }}
+        options={{
+          title: t("exchange.buy.tabTitle"),
+          tabBarLabel: ({ focused, color }: TabLabelProps) => (
+            /** width has to be a little bigger to accomodate the switch in size between semibold to regular */
+            <LText style={{ width: "110%", color }} semiBold={focused}>
+              {t("exchange.buy.tabTitle")}
+            </LText>
+          ),
+        }}
       />
       <Tab.Screen
         name={ScreenName.ExchangeHistory}
         component={History}
-        options={{ title: t("exchange.history.tabTitle") }}
+        options={{
+          title: t("exchange.history.tabTitle"),
+          tabBarLabel: ({ focused, color }: TabLabelProps) => (
+            <LText style={{ width: "110%", color }} semiBold={focused}>
+              {t("exchange.history.tabTitle")}
+            </LText>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
