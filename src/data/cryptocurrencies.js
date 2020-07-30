4c2fd31f94d3ccc016b807a35b5128d1dfd17818
LFBarreto <lfb.access@gmail.com>
2020-07-30 11:18:35 +0200

    (CryptoCurrencies): add algorand data

--- src/data/cryptocurrencies.js
+++ src/data/cryptocurrencies.js
@@ -156,6 +156,35 @@ const cryptocurrenciesById: { [name: string]: CryptoCurrency } = {
       },
     ],
   },
+  algorand: {
+    type: "CryptoCurrency",
+    id: "algorand",
+    coinType: 283,
+    name: "Algorand",
+    managerAppName: "Algorand",
+    ticker: "ALGO",
+    scheme: "algorand",
+    color: "#000000",
+    family: "algorand",
+    units: [
+      {
+        name: "ALGO",
+        code: "ALGO",
+        magnitude: 6,
+      },
+      {
+        name: "uALGO",
+        code: "uALGO",
+        magnitude: 0,
+      },
+    ],
+    explorerViews: [
+      {
+        tx:
+          "https://goalseeker.purestake.io/algorand/mainnet/transaction/$hash",
+      },
+    ],
+  },
   ark: {
     type: "CryptoCurrency",
     id: "ark",
