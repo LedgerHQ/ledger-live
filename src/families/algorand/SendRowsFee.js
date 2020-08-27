cc666c0bdd7d1f359a7e66b809f4a33e54e58562
LFBarreto <lfb.access@gmail.com>
2020-08-27 10:06:20 +0200

    (Algo): send fees summary unit updated with correct value for ASA

--- src/families/algorand/SendRowsFee.js
+++ src/families/algorand/SendRowsFee.js
@@ -1,13 +1,14 @@
 // @flow
 import React, { useCallback } from "react";
 import { View, StyleSheet, Linking } from "react-native";
-import type { AccountLike } from "@ledgerhq/live-common/lib/types";
+import type { AccountLike, Account } from "@ledgerhq/live-common/lib/types";
 import { Trans } from "react-i18next";
 import type { Transaction } from "@ledgerhq/live-common/lib/families/algorand/types";
 import {
   getAccountUnit,
   getAccountCurrency,
 } from "@ledgerhq/live-common/lib/account";
+import { getMainAccount } from "@ledgerhq/live-common/lib/account/helpers";
 import SummaryRow from "../../screens/SendFunds/SummaryRow";
 import LText from "../../components/LText";
 import CurrencyUnitValue from "../../components/CurrencyUnitValue";
@@ -17,17 +18,24 @@ import { urls } from "../../config/urls";
 import colors from "../../colors";
 
 type Props = {
-  account: AccountLike,
   transaction: Transaction,
+  account: AccountLike,
+  parentAccount: ?Account,
+  navigation: *,
 };
 
-export default function AlgorandFeeRow({ account, transaction }: Props) {
+export default function AlgorandFeeRow({
+  account,
+  parentAccount,
+  transaction,
+}: Props) {
   const extraInfoFees = useCallback(() => {
     Linking.openURL(urls.feesMoreInfo);
   }, []);
 
   const fees = transaction.fees;
-  const unit = getAccountUnit(account);
+  const mainAccount = getMainAccount(account, parentAccount);
+  const unit = getAccountUnit(mainAccount);
   const currency = getAccountCurrency(account);
 
   return (
