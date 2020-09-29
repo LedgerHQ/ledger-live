410601d26fd1602bdcc2e37ce2cdc1ec6001799a
Juan Cortes Ross <juan@bohem.io>
2020-09-29 09:41:03 +0200

    LL-3397 Prevent fees from being the same

--- src/families/bitcoin/libcore-getAccountNetworkInfo.js
+++ src/families/bitcoin/libcore-getAccountNetworkInfo.js
@@ -15,6 +15,16 @@ type Output = Promise<NetworkInfo>;
 
 const speeds = ["high", "standard", "low"];
 
+export function avoidDups(nums: Array<BigNumber>): Array<BigNumber> {
+  nums = nums.slice(0);
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (nums[i + 1].gte(nums[i])) {
+      nums[i] = nums[i + 1].plus(1);
+    }
+  }
+  return nums;
+}
+
 async function bitcoin({ coreAccount }: Input): Output {
   const bitcoinLikeAccount = await coreAccount.asBitcoinLikeAccount();
   const bigInts = await bitcoinLikeAccount.getFees();
@@ -23,15 +33,9 @@ async function bitcoin({ coreAccount }: Input): Output {
     bigInts,
     libcoreBigIntToBigNumber
   );
-  let normalized = bigNumbers.map((bn) =>
-    bn.div(1000).integerValue(BigNumber.ROUND_CEIL)
+  const normalized = avoidDups(
+    bigNumbers.map((bn) => bn.div(1000).integerValue(BigNumber.ROUND_CEIL))
   );
-
-  if (normalized[1].eq(normalized[2]) || normalized[1].eq(normalized[0])) {
-    // NB if two of the fees are the same, use the lowest as a base and increase the rest
-    normalized = [normalized[2].plus(2), normalized[2].plus(1), normalized[2]];
-  }
-
   const feeItems = {
     items: normalized.map((feePerByte, i) => ({
       key: String(i),
