bf98c8b55db69ead73f9c881ba770e2551316923
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-10-28 18:24:57 +0100

    Update ethereum bots logic

--- src/families/ethereum/specs.js
+++ src/families/ethereum/specs.js
@@ -1,9 +1,18 @@
 // @flow
 import expect from "expect";
+import { BigNumber } from "bignumber.js";
 import invariant from "invariant";
 import sample from "lodash/sample";
 import type { Transaction } from "./types";
-import { getCryptoCurrencyById, parseCurrencyUnit } from "../../currencies";
+import {
+  getCryptoCurrencyById,
+  parseCurrencyUnit,
+  findCompoundToken,
+} from "../../currencies";
+import {
+  makeCompoundSummaryForAccount,
+  getAccountCapabilities,
+} from "../../compound/logic";
 import { pickSiblings } from "../../bot/specs";
 import type { AppSpec } from "../../bot/types";
 import { getGasLimit } from "./transaction";
@@ -40,6 +49,58 @@ const ethereumBasicMutations = ({ maxAccount }) => [
   },
 ];
 
+function findCompoundAccount(account, f: Function) {
+  return sample(
+    (account.subAccounts || []).filter((a) => {
+      if (
+        a.type === "TokenAccount" &&
+        a.balance.gt(0) &&
+        findCompoundToken(a.token)
+      ) {
+        const c = getAccountCapabilities(a);
+        return c && f(c, a);
+      }
+      return false;
+    })
+  );
+}
+
+function getCompoundResult({ account, transaction, accountBeforeTransaction }) {
+  const a = account.subAccounts?.find((a) => a.id === transaction.subAccountId);
+  const aBefore = accountBeforeTransaction.subAccounts?.find(
+    (a) => a.id === transaction.subAccountId
+  );
+  invariant(
+    a && a.type === "TokenAccount",
+    "account %s found",
+    transaction.subAccountId
+  );
+  invariant(
+    aBefore && aBefore.type === "TokenAccount",
+    "account %s found",
+    transaction.subAccountId
+  );
+  const capabilities = getAccountCapabilities(a);
+  const summary = makeCompoundSummaryForAccount(a, account);
+
+  const capabilitiesBefore = getAccountCapabilities(aBefore);
+  const summaryBefore = makeCompoundSummaryForAccount(
+    aBefore,
+    accountBeforeTransaction
+  );
+  invariant(
+    capabilities,
+    "account %s have no capabilities found",
+    transaction.subAccountId
+  );
+  return {
+    a,
+    capabilities,
+    summary,
+    previous: { summary: summaryBefore, capabilities: capabilitiesBefore },
+  };
+}
+
 const ethereum: AppSpec<Transaction> = {
   name: "Ethereum",
   currency: getCryptoCurrencyById("ethereum"),
@@ -57,6 +118,155 @@ const ethereum: AppSpec<Transaction> = {
     );
   },
   mutations: ethereumBasicMutations({ maxAccount: 3 }).concat([
+    {
+      name: "allow MAX a compound token",
+      maxRun: 1,
+      transaction: ({ account, bridge }) => {
+        // find an existing token which was not yet allowed
+        const a = findCompoundAccount(account, (c) => c.enabledAmount.eq(0));
+        invariant(a, "no compound account to allow");
+        const ctoken = findCompoundToken(a.token);
+        invariant(ctoken, "ctoken found");
+        return {
+          transaction: bridge.createTransaction(account),
+          updates: [
+            {
+              mode: "erc20.approve",
+              subAccountId: a.id,
+              recipient: ctoken.contractAddress,
+            },
+            { useAllAmount: true },
+          ],
+        };
+      },
+      test: (arg) => {
+        const { capabilities } = getCompoundResult(arg);
+        expect(capabilities.enabledAmountIsUnlimited).toBe(true);
+      },
+    },
+
+    {
+      name: "supply some compound token",
+      maxRun: 1,
+      transaction: ({ account, bridge }) => {
+        const a = findCompoundAccount(account, (c) => c.canSupply);
+        invariant(a, "no compound account to supply");
+        const ctoken = findCompoundToken(a.token);
+        invariant(ctoken, "ctoken found");
+        const amount = a.spendableBalance
+          .times(0.5 + 0.5 * Math.random())
+          .integerValue();
+        return {
+          transaction: bridge.createTransaction(account),
+          updates: [
+            {
+              mode: "compound.supply",
+              subAccountId: a.id,
+              amount,
+            },
+          ],
+        };
+      },
+      test: (arg) => {
+        const { transaction } = arg;
+        const { summary, previous } = getCompoundResult(arg);
+        invariant(
+          summary,
+          "could not find compound summary for account %s",
+          transaction.subAccountId
+        );
+        expect(
+          summary.totalSupplied.gt(
+            previous.summary?.totalSupplied || BigNumber(0)
+          )
+        ).toBe(true);
+      },
+    },
+    {
+      name: "withdraw some compound token",
+      maxRun: 1,
+      transaction: ({ account, bridge }) => {
+        const a = findCompoundAccount(
+          account,
+          (c, a) =>
+            c.canWithdraw &&
+            a.operations.length > 0 &&
+            // 7 days has passed since last operation
+            Date.now() - a.operations[0].date > 7 * 24 * 60 * 60 * 1000
+        );
+        invariant(a, "no compound account to withdraw");
+        const ctoken = findCompoundToken(a.token);
+        invariant(ctoken, "ctoken found");
+        const nonSpendableBalance = a.balance.minus(a.spendableBalance);
+        return {
+          transaction: bridge.createTransaction(account),
+          updates: [
+            { mode: "compound.withdraw", subAccountId: a.id },
+            Math.random() < 0.5
+              ? { useAllAmount: true }
+              : {
+                  amount: nonSpendableBalance
+                    .times(Math.random())
+                    .integerValue(),
+                },
+          ],
+        };
+      },
+      test: (arg) => {
+        const { transaction } = arg;
+        const { summary, previous } = getCompoundResult(arg);
+        invariant(
+          summary,
+          "could not find compound summary for account %s",
+          transaction.subAccountId
+        );
+        invariant(
+          previous.summary,
+          "could not find a previous compound summary for account %s",
+          transaction.subAccountId
+        );
+        if (arg.transaction.useAllAmount) {
+          expect(summary.totalSupplied.eq(0)).toBe(true);
+        } else {
+          expect(
+            summary.totalSupplied.lt(
+              previous.summary.totalSupplied || BigNumber(0)
+            )
+          ).toBe(true);
+        }
+      },
+    },
+    {
+      name: "disallow a compound token",
+      maxRun: 1,
+      transaction: ({ account, bridge }) => {
+        // find an existing token which was allowed
+        // set it back to zero
+        // ? IDEA: only do it if there is nothing to withdraw
+        const a = findCompoundAccount(
+          account,
+          (c) => c.enabledAmount.gt(0) || c.enabledAmountIsUnlimited
+        );
+        invariant(a, "no compound account to disallow");
+        const ctoken = findCompoundToken(a.token);
+        invariant(ctoken, "ctoken found");
+        return {
+          transaction: bridge.createTransaction(account),
+          updates: [
+            {
+              mode: "erc20.approve",
+              subAccountId: a.id,
+              recipient: ctoken.contractAddress,
+            },
+            { amount: BigNumber(0) },
+          ],
+        };
+      },
+      test: (arg) => {
+        const { capabilities } = getCompoundResult(arg);
+        expect(capabilities.enabledAmount.eq(0)).toBe(true);
+      },
+    },
     {
       name: "move some ERC20",
       maxRun: 1,
--- src/families/ethereum/speculos-deviceActions.js
+++ src/families/ethereum/speculos-deviceActions.js
@@ -1,22 +1,85 @@
 // @flow
 import type { DeviceAction } from "../../bot/types";
 import type { Transaction } from "./types";
+import { formatCurrencyUnit } from "../../currencies";
+import { deviceActionFlow } from "../../bot/specs";
 
-const acceptTransaction: DeviceAction<Transaction, {}> = ({
-  transport,
-  event,
-}) => {
-  if (event.text.startsWith("Accept")) {
-    transport.button("LRlr");
-  } else if (
-    event.text.startsWith("Review") ||
-    event.text.startsWith("Amount") ||
-    event.text.startsWith("Address") ||
-    event.text.startsWith("Confirm") ||
-    event.text.includes("Fees")
-  ) {
-    transport.button("Rr");
-  }
-};
+function subAccount(subAccountId, account) {
+  const sub = (account.subAccounts || []).find((a) => a.id === subAccountId);
+  if (!sub || sub.type !== "TokenAccount")
+    throw new Error("expected sub account id " + String(subAccountId));
+  return sub;
+}
+
+const acceptTransaction: DeviceAction<Transaction, *> = deviceActionFlow({
+  steps: [
+    {
+      title: "Review",
+      button: "Rr",
+    },
+    {
+      title: "Type",
+      button: "Rr",
+      expectedValue: ({ transaction }) => {
+        if (transaction.mode === "erc20.approve") return "Approve";
+        if (transaction.mode === "compound.supply") return "Supply";
+        if (transaction.mode === "compound.withdraw") return "Redeem Assets";
+        return "";
+      },
+    },
+    {
+      title: "Amount",
+      button: "Rr",
+      expectedValue: ({ account, status, transaction }) =>
+        transaction.mode === "erc20.approve" && transaction.useAllAmount
+          ? "Unlimited " +
+            subAccount(transaction.subAccountId, account).token.ticker
+          : formatCurrencyUnit(
+              {
+                ...(transaction.subAccountId
+                  ? subAccount(transaction.subAccountId, account).token.units[0]
+                  : account.unit),
+                prefixCode: true,
+              },
+              status.amount,
+              {
+                showCode: true,
+                disableRounding: true,
+                joinFragmentsSeparator: " ",
+              }
+            ).replace(/\s/g, " "),
+    },
+    {
+      title: "Contract",
+      button: "Rr",
+    },
+    {
+      title: "Max fees",
+      button: "Rr",
+      expectedValue: ({ account, status }) =>
+        formatCurrencyUnit(
+          {
+            ...account.unit,
+            prefixCode: true,
+          },
+          status.estimatedFees,
+          {
+            showCode: true,
+            disableRounding: true,
+            joinFragmentsSeparator: " ",
+          }
+        ).replace(/\s/g, " "),
+    },
+    {
+      title: "Address",
+      button: "Rr",
+      expectedValue: ({ transaction }) => transaction.recipient,
+    },
+    {
+      title: "Accept",
+      button: "LRlr",
+    },
+  ],
+});
 
 export default { acceptTransaction };
