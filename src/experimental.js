LedgerHQ/ledger-live

commit e735d4b47bd0c16b64b740a88f6119976fea5de9

(Onboarding): pair new device bluetooth updated

src/screens/Onboarding/steps/pair-new.js
@@ -72,7 +72,7 @@ export default function OnboardingStepPairNew() {
         withArrows
         usbOnly={usbOnly}
         deviceModelId={deviceModelId}
-        onSelect={setDevice}
+        onSelect={usbOnly ? setDevice : next}
         autoSelectOnAdd
       />
       <DeviceActionModal

src/components/SelectDevice/index.js
@@ -24,6 +24,7 @@ type Props = {
   withArrows?: boolean,
   usbOnly?: boolean,
   filter?: (transportModule: TransportModule) => boolean,
+  autoSelectOnAdd: boolean,
 };
 
 export default function SelectDevice({
@@ -32,6 +33,7 @@ export default function SelectDevice({
   filter = () => true,
   onSelect,
   onBluetoothDeviceAction,
+  autoSelectOnAdd,
 }: Props) {
   const navigation = useNavigation();
   const knownDevices = useSelector(knownDevicesSelector);
@@ -39,8 +41,10 @@ export default function SelectDevice({
   const [devices, setDevices] = useState([]);
 
   const onPairNewDevice = useCallback(() => {
-    navigation.navigate(ScreenName.PairDevices);
-  }, [navigation]);
+    navigation.navigate(ScreenName.PairDevices, {
+      onDone: autoSelectOnAdd ? onSelect : null,
+    });
+  }, [autoSelectOnAdd, navigation, onSelect]);
 
   const renderItem = useCallback(
     (item: Device) => (

commit 31e01a2e70b3a11d53f6c9d516836173ab3ecae5

live-common 15.7.0 (#1432)
* live-common 15.7.0

* 15.7.1

* live-common 15.7.2

* wording

* Fixes saving non read-only mode on usb select

fixes regression introduced by https://github.com/LFBarreto/ledger-live-mobile/commit/e735d4b47bd0c16b64b740a88f6119976fea5de9

src/experimental.js
@@ -54,17 +54,13 @@ export const experimentalFeatures: Feature[] = [
     description:
       "Try an upcoming version of Ledger's blockchain explorers. Changing this setting may affect the account balance and synchronization as well as the send feature.",
   },
-  /*
   {
     type: "toggle",
-    name: "API_TEZOS_NODE",
-    valueOn: "https://xtz-node.explorers.prod.aws.ledger.fr/",
-    valueOff: "https://mainnet.tezrpc.me/",
-    title: "Experimental Tezos Send",
+    name: "EXPERIMENTAL_QUIT_APP",
+    title: "Quit apps automatically",
     description:
-      "Workaround for Sending Tezos. Switch to another experimental node.",
+      "Enabling this setting will automatically quit an application on your device when another one needs to be opened. This will make switching between apps more seamless.",
   },
-*/
 ];
 
 const storageKey = "experimentalFlags";

src/screens/Onboarding/steps/pair-new.js
@@ -34,6 +34,11 @@ export default function OnboardingStepPairNew() {
       />
     ) : null;
 
+  const directNext = useCallback(() => {
+    dispatch(setReadOnlyMode(false));
+    next();
+  }, [dispatch, next]);
+
   const onResult = useCallback(
     (info: any) => {
       /** if list apps succeed we update settings with state of apps installed */
@@ -72,7 +77,7 @@ export default function OnboardingStepPairNew() {
         withArrows
         usbOnly={usbOnly}
         deviceModelId={deviceModelId}
-        onSelect={usbOnly ? setDevice : next}
+        onSelect={usbOnly ? setDevice : directNext}
         autoSelectOnAdd
       />
       <DeviceActionModal