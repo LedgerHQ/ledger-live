14be4b59a645512cf848ca46113d76892fa4fc26
LFBarreto <lfb.access@gmail.com>
2020-11-03 15:57:34 +0100

    (Compound): add useCompoundSummaries react helper

--- src/compound/react.js
+++ src/compound/react.js
@@ -1,8 +1,14 @@
 // @flow
 import { useMemo } from "react";
 import { BigNumber } from "bignumber.js";
-import type { TokenAccount } from "../types";
-import { getSupplyMax } from "../families/ethereum/modules/compound";
+import type { AccountLikeArray, TokenAccount } from "../types";
+import {
+  getSupplyMax,
+  isCompoundTokenSupported,
+} from "../families/ethereum/modules/compound";
+import type { CompoundAccountSummary } from "./types";
+import { makeCompoundSummaryForAccount } from "./logic";
+import { findCompoundToken } from "../currencies";
 
 export function useSupplyMax(account: TokenAccount): BigNumber {
   return useMemo(() => getSupplyMax(account), [account]);
@@ -33,3 +39,26 @@ export const useSupplyMaxChoiceButtons = (
     [supplyMax]
   );
 };
+
+const makeSummaries = (accounts: AccountLikeArray): CompoundAccountSummary[] =>
+  accounts
+    .map((acc) => {
+      if (acc.type !== "TokenAccount") return;
+      const ctoken = findCompoundToken(acc.token);
+      if (!ctoken) return;
+
+      if (!isCompoundTokenSupported(ctoken)) return;
+
+      const parentAccount = accounts.find((a) => a.id === acc.parentId);
+      if (!parentAccount || parentAccount.type !== "Account") return;
+      const summary = makeCompoundSummaryForAccount(acc, parentAccount);
+      return summary;
+    })
+    .filter(Boolean);
+
+export function useCompoundSummaries(
+  accounts: AccountLikeArray
+): CompoundAccountSummary[] {
+  const summaries = useMemo(() => makeSummaries(accounts), [accounts]);
+  return summaries;
+}
