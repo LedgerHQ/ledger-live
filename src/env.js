3e6bade6afdb7f687e60fb9b3f1c21fff74e003b
Wietse Wind <mail@wietse.com>
2021-01-21 12:30:35 +0100

    Ripple » XRP ledger
    Ripple = Company in the US
    XRPL = Open blockchain where the XRP asset lives

--- src/env.js
+++ src/env.js
@@ -55,7 +55,7 @@ const envDefinitions = {
   API_RIPPLE_WS: {
     parser: stringParser,
     def: "wss://xrpl.ws",
-    desc: "websockets api for ripple",
+    desc: "XRP Ledger full history open WebSocket endpoint",
   },
   API_POLKADOT_INDEXER: {
     parser: stringParser,
