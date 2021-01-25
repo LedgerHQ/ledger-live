d329522e71e53977b35760198ac4353578e0ddb5
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-01-25 18:58:23 +0100

    Squashed commit of the following:
    commit f9b05ea06d8dd4c3155afa3b7edb39539ca10e9f
    Author: LFBarreto <lfb.access@gmail.com>
    Date:   Thu Jan 21 11:00:00 2021 +0100
    
        (Cosmos): add stargate update warning banner

--- src/components/CurrencyDownStatusAlert.js
+++ src/components/CurrencyDownStatusAlert.js
@@ -1,13 +1,25 @@
 // @flow
+import React from "react";
+import { createCustomErrorClass } from "@ledgerhq/errors";
 import type {
   TokenCurrency,
   CryptoCurrency,
 } from "@ledgerhq/live-common/lib/types";
+import WarningBanner from "./WarningBanner";
 
 type Props = {
   currencies: Array<CryptoCurrency | TokenCurrency>,
 };
 
-const CurrencyDownStatusAlert = (_: Props) => null;
+const CosmosStargateFeb2021Warning = createCustomErrorClass(
+  "CosmosStargateFeb2021Warning",
+);
+
+const CurrencyDownStatusAlert = ({ currencies }: Props) => {
+  if (currencies.some(c => c.id === "cosmos")) {
+    return <WarningBanner error={new CosmosStargateFeb2021Warning()} />;
+  }
+  return null;
+};
 
 export default CurrencyDownStatusAlert;
--- src/config/urls.js
+++ src/config/urls.js
@@ -28,6 +28,8 @@ export const urls = {
       "https://support.ledger.com/hc/en-us/articles/360012109220?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=error_syncerror",
     LedgerAPIErrorWithMessage:
       "https://status.ledger.com?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=error_apierrorwithmessage",
+    CosmosStargateFeb2021Warning:
+      "https://support.ledger.com/hc/en-us/articles/360013713840-Cosmos-ATOM-",
   },
   multipleAddresses:
     "https://support.ledger.com/hc/en-us/articles/360033802154?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=ops_details_change",
