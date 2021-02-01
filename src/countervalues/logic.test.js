c84e7f80e0a8361e161539bcfab65bdbc6e92340
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-02-01 13:09:57 +0100

    add sanity test for countervalues api (#1043)

--- src/countervalues/logic.test.js
+++ src/countervalues/logic.test.js
@@ -1,192 +1,60 @@
 // @flow
-import "../__tests__/test-helpers/staticTime";
 import { initialState, loadCountervalues, calculate } from "./logic";
-import CountervaluesAPI from "./api";
-import { setEnv } from "../env";
-import {
-  getFiatCurrencyByTicker,
-  getTokenById,
-  getCryptoCurrencyById,
-} from "../currencies";
-import {
-  formatCounterValueDay,
-  formatCounterValueHour,
-  parseFormattedDate,
-} from "./helpers";
+import { getFiatCurrencyByTicker, getCryptoCurrencyById } from "../currencies";
 
-setEnv("MOCK", "1");
+const bitcoin = getCryptoCurrencyById("bitcoin");
+const usd = getFiatCurrencyByTicker("USD");
+const now = Date.now();
 
-test("helpers", () => {
-  expect(formatCounterValueDay(new Date())).toBe("2018-03-14");
-  expect(formatCounterValueHour(new Date())).toBe("2018-03-14T17");
-
-  expect(formatCounterValueDay(parseFormattedDate("2018-03-14"))).toBe(
-    "2018-03-14"
-  );
-  expect(formatCounterValueHour(parseFormattedDate("2018-03-14T17"))).toBe(
-    "2018-03-14T17"
-  );
-});
-
-test("mock load with nothing to track", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [],
-    autofillGaps: true,
+describe("API sanity", () => {
+  test("recent days have rate for BTC USD", async () => {
+    const state = await loadCountervalues(initialState, {
+      trackingPairs: [
+        {
+          from: bitcoin,
+          to: usd,
+          startDate: new Date(now - 200 * 24 * 60 * 60 * 1000),
+        },
+      ],
+      autofillGaps: false,
+    });
+    for (let i = 0; i < 7; i++) {
+      const value = calculate(state, {
+        date: new Date(now - i * 24 * 60 * 60 * 1000),
+        disableRounding: true,
+        from: bitcoin,
+        to: usd,
+        value: 1000000,
+      });
+      expect(value).toBeDefined();
+    }
   });
-  expect(state).toBeDefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-    })
-  ).toBeUndefined();
-});
-
-test("mock fetchMarketcapTickers", async () => {
-  expect(await CountervaluesAPI.fetchMarketcapTickers()).toBeDefined();
-});
-
-test("mock load with btc-usd to track", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [
-      {
-        from: getCryptoCurrencyById("bitcoin"),
-        to: getFiatCurrencyByTicker("USD"),
-        startDate: new Date(Date.now() - 200 * 24 * 60 * 60 * 1000),
-      },
-    ],
-    autofillGaps: false,
+  test("recent days have different rates for BTC USD", async () => {
+    const state = await loadCountervalues(initialState, {
+      trackingPairs: [
+        {
+          from: bitcoin,
+          to: usd,
+          startDate: new Date(now - 200 * 24 * 60 * 60 * 1000),
+        },
+      ],
+      autofillGaps: true,
+    });
+    let currentValue = calculate(state, {
+      disableRounding: true,
+      from: bitcoin,
+      to: usd,
+      value: 1000000,
+    });
+    for (let i = 1; i < 7; i++) {
+      const value = calculate(state, {
+        date: new Date(now - i * 24 * 60 * 60 * 1000),
+        disableRounding: true,
+        from: bitcoin,
+        to: usd,
+        value: 1000000,
+      });
+      expect(value).not.toEqual(currentValue);
+    }
   });
-  expect(state).toBeDefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-      date: new Date(Date.now() - 210 * 24 * 60 * 60 * 1000),
-    })
-  ).toBeUndefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-    })
-  ).toBe(261944);
-  expect(
-    calculate(state, {
-      value: 10000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-    })
-  ).toBe(26194);
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("EUR"),
-    })
-  ).toBeUndefined();
 });
-
-test("DAI EUR latest price", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [
-      {
-        from: getTokenById("ethereum/erc20/dai_stablecoin_v2_0"),
-        to: getFiatCurrencyByTicker("EUR"),
-      },
-    ],
-    autofillGaps: false,
-  });
-  expect(state).toBeDefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getTokenById("ethereum/erc20/dai_stablecoin_v2_0"),
-      to: getFiatCurrencyByTicker("EUR"),
-    })
-  ).toBeUndefined();
-});
-
-test("calculate(now()) is calculate(null)", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [
-      {
-        from: getTokenById("ethereum/erc20/dai_stablecoin_v2_0"),
-        to: getFiatCurrencyByTicker("EUR"),
-      },
-    ],
-    autofillGaps: false,
-  });
-  expect(state).toBeDefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getTokenById("ethereum/erc20/dai_stablecoin_v2_0"),
-      to: getFiatCurrencyByTicker("EUR"),
-    })
-  ).toEqual(
-    calculate(state, {
-      value: 100000000,
-      from: getTokenById("ethereum/erc20/dai_stablecoin_v2_0"),
-      to: getFiatCurrencyByTicker("EUR"),
-      date: new Date(),
-    })
-  );
-});
-
-test("missing rate in mock", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [
-      {
-        from: getCryptoCurrencyById("bitcoin"),
-        to: getFiatCurrencyByTicker("USD"),
-        startDate: new Date(Date.now() - 200 * 24 * 60 * 60 * 1000),
-      },
-    ],
-    autofillGaps: false,
-  });
-  expect(state).toBeDefined();
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-      date: new Date("2018-01-07T19:00"),
-    })
-  ).toBeUndefined();
-});
-
-test("missing rate in mock is filled by autofillGaps", async () => {
-  const state = await loadCountervalues(initialState, {
-    trackingPairs: [
-      {
-        from: getCryptoCurrencyById("bitcoin"),
-        to: getFiatCurrencyByTicker("USD"),
-        startDate: new Date(Date.now() - 200 * 24 * 60 * 60 * 1000),
-      },
-    ],
-    autofillGaps: true,
-  });
-  expect(state).toBeDefined();
-  // shifting rates to the right. hole in data looks up in older datapoint
-  expect(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-      date: new Date("2018-01-07T19:00"),
-    })
-  ).toBe(
-    calculate(state, {
-      value: 100000000,
-      from: getCryptoCurrencyById("bitcoin"),
-      to: getFiatCurrencyByTicker("USD"),
-      date: new Date("2018-01-06T19:00"),
-    })
-  );
-});
-
-// TODO test the incremental aspect of loadCountervalues
