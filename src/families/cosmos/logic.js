27ae1f267360269dda48ac860498d161497ba4e3
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2021-02-12 10:22:52 +0100

    Make the LRUCaches aware of their ID

--- src/families/cosmos/logic.js
+++ src/families/cosmos/logic.js
@@ -185,11 +185,13 @@ export const calculateFees: CacheRes<
     });
   },
   ({ a, t }) =>
-    `${a.id}_${t.amount.toString()}_${t.recipient}_${String(t.useAllAmount)}_${
-      t.mode
-    }_${t.validators ? t.validators.map((v) => v.address).join("-") : ""}_${
-      t.memo ? t.memo.toString() : ""
-    }_${t.cosmosSourceValidator ? t.cosmosSourceValidator : ""}`
+    `${a.id}_${a.currency.id}_${t.amount.toString()}_${t.recipient}_${String(
+      t.useAllAmount
+    )}_${t.mode}_${
+      t.validators ? t.validators.map((v) => v.address).join("-") : ""
+    }_${t.memo ? t.memo.toString() : ""}_${
+      t.cosmosSourceValidator ? t.cosmosSourceValidator : ""
+    }`
 );
 
 export function canUndelegate(account: Account): boolean {
