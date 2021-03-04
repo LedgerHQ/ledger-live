e1871a9172bf541d49d90222200e1b18fffc642a
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-03-04 19:27:24 +0100

    improve logsviewer

--- tools/src/demos/logsviewer/index.js
+++ tools/src/demos/logsviewer/index.js
@@ -2,39 +2,204 @@
 import React, { Fragment, Component, useMemo } from "react";
 import { ObjectInspector } from "react-inspector";
 import ReactTable from "react-table";
+import styled from "styled-components";
 import "react-table/react-table.css";
+import { BigNumber } from "bignumber.js";
+import {
+  decodeAccountId,
+  toAccountRaw,
+} from "@ledgerhq/live-common/lib/account";
+import { getCryptoCurrencyById } from "@ledgerhq/live-common/lib/currencies";
+import { shortAddressPreview } from "@ledgerhq/live-common/lib/account/helpers";
+import {
+  runDerivationScheme,
+  getDerivationScheme,
+} from "@ledgerhq/live-common/lib/derivation";
 
 const messageLenses = {
   libcore: ({ message }) => {
     const i = message.indexOf("I: ");
     return i === -1 ? message : message.slice(i + 3);
-  }
+  },
 };
 
-const extractAPDUs = logs =>
-  logs
-    .slice(0)
-    .reverse()
-    .filter(l => l.type === "apdu")
-    .reduce((all, l) => {
-      const last = all[all.length - 1];
-      if (last && last.message === l.message) return all;
-      return all.concat(l);
-    }, [])
-    .map(l => l.message)
-    .join("\n");
+const Button = styled.a`
+  cursor: pointer;
+  padding: 12px 16px;
+  border: none;
+  background: ${(p) =>
+    p.danger
+      ? "#FF483820"
+      : p.primary
+      ? "#6490F1"
+      : "rgba(100, 144, 241, 0.1)"};
+  color: ${(p) => (p.danger ? "#FF4838" : p.primary ? "#fff" : "#6490F1")};
+  border-radius: 4px;
+  opacity: ${(p) => (p.disabled ? 0.3 : 1)};
+  &:hover {
+    opacity: ${(p) => (p.disabled ? 0.3 : 0.8)};
+  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  > svg {
+    padding-right: 5px;
+  }
+`;
 
-const Header = ({ logs }: *) => {
-  const href = useMemo(
-    () => "data:text/plain;base64," + btoa(extractAPDUs(logs)),
+const HeaderWrapper = styled.div``;
+const HeaderRow = styled.div`
+  display: flex;
+  padding: 10px;
+  > * + * {
+    margin-right: 10px;
+  }
+`;
+
+const Header = ({ logs, logsMeta, onFiles }: *) => {
+  const apdusLogs = useMemo(
+    () =>
+      logs
+        .slice(0)
+        .reverse()
+        .filter((l) => l.type === "apdu")
+        .reduce((all, l) => {
+          const last = all[all.length - 1];
+          if (last && last.message === l.message) return all;
+          return all.concat(l);
+        }, []),
     [logs]
   );
+  const apdus = apdusLogs.map((l) => l.message).join("\n");
+  const href = useMemo(() => "data:text/plain;base64," + btoa(apdus), [apdus]);
+  const onChange = (e: *) => onFiles(e.target.files);
+  const errors = logs.filter((l) => l.error);
+  const accountsIds = logsMeta?.accountsIds;
+  let accounts;
+  try {
+    accounts = accountsIds.map((id) => {
+      const { derivationMode, xpubOrAddress, currencyId } = decodeAccountId(id);
+      const currency = getCryptoCurrencyById(currencyId);
+      const scheme = getDerivationScheme({
+        derivationMode,
+        currency,
+      });
+      const index = 0;
+      const freshAddressPath = runDerivationScheme(scheme, currency, {
+        account: index,
+        node: 0,
+        address: 0,
+      });
+
+      return {
+        type: "Account",
+        name:
+          currency.name +
+          " " +
+          (derivationMode || "legacy") +
+          " " +
+          shortAddressPreview(xpubOrAddress),
+        xpub: xpubOrAddress,
+        seedIdentifier: xpubOrAddress,
+        starred: true,
+        swapHistory: [],
+        id,
+        derivationMode,
+        currency,
+        unit: currency.units[0],
+        index,
+        freshAddress: xpubOrAddress,
+        freshAddressPath,
+        freshAddresses: [],
+        creationDate: new Date(),
+        lastSyncDate: new Date(0),
+        blockHeight: 0,
+        balance: new BigNumber(0),
+        spendableBalance: new BigNumber(0),
+        operationsCount: 0,
+        operations: [],
+        pendingOperations: [],
+      };
+    });
+  } catch (e) {
+    console.error(e);
+  }
+
+  const appJsonHref = useMemo(() => {
+    try {
+      return !accounts
+        ? ""
+        : "data:text/plain;base64," +
+            btoa(
+              JSON.stringify({
+                data: {
+                  settings: { hasCompletedOnboarding: true },
+                  accounts: accounts.map((a) => ({ data: toAccountRaw(a) })),
+                },
+              })
+            );
+    } catch (e) {
+      console.error(e);
+    }
+  }, [accounts]);
+
   return (
-    <div>
-      <a download="apdus" href={href}>
-        extract APDUs
-      </a>
-    </div>
+    <HeaderWrapper>
+      <HeaderRow>
+        <input type="file" onChange={onChange} accept=".json" />
+
+        {apdusLogs.length ? (
+          <Button download="apdus" href={href}>
+            {apdusLogs.length} APDUs
+          </Button>
+        ) : null}
+
+        <Button download="app.json" href={appJsonHref}>
+          app.json with user accounts
+        </Button>
+      </HeaderRow>
+
+      {logsMeta ? (
+        <HeaderRow>
+          <strong>user is on</strong> {logsMeta.userAgent}
+        </HeaderRow>
+      ) : null}
+
+      {accountsIds ? (
+        <HeaderRow>
+          <details>
+            <summary>
+              <strong>user have {accountsIds.length} accounts</strong>
+            </summary>
+            <ul>
+              {accountsIds.map((id, i) => (
+                <li key={i}>
+                  <pre>
+                    <code>{id}</code>
+                  </pre>
+                </li>
+              ))}
+            </ul>
+          </details>
+        </HeaderRow>
+      ) : null}
+
+      <HeaderRow>
+        <details>
+          <summary>
+            <strong>this logs have {errors.length} errors</strong>
+          </summary>
+          <ul>
+            {errors.map((e, i) => (
+              <li key={i}>
+                <ObjectInspector data={e.error} expandLevel={3} />
+              </li>
+            ))}
+          </ul>
+        </details>
+      </HeaderRow>
+    </HeaderWrapper>
   );
 };
 
@@ -65,30 +230,30 @@ const columns = [
     Header: "index",
     accessor: "index",
     minWidth: 80,
-    maxWidth: 80
+    maxWidth: 80,
   },
   {
     id: "time",
     Header: "time",
     accessor: "timestamp",
-    maxWidth: 220
+    maxWidth: 220,
   },
   {
     Header: "process",
     accessor: "pname",
-    maxWidth: 100
+    maxWidth: 100,
   },
   {
     Header: "type",
     accessor: "type",
-    maxWidth: 150
+    maxWidth: 150,
   },
   {
     id: "content",
     Header: "Content",
     accessor: "message",
-    Cell: ContentCell
-  }
+    Cell: ContentCell,
+  },
 ];
 
 /*
@@ -151,7 +316,7 @@ class HeaderEmptyState extends Component<*> {
 
 class LogsViewer extends Component<*, *> {
   state = {
-    logs: null
+    logs: null,
   };
   onDragOver = (evt: *) => {
     evt.stopPropagation();
@@ -169,10 +334,10 @@ class LogsViewer extends Component<*, *> {
         continue;
       }
       var reader = new FileReader();
-      reader.onload = e => {
+      reader.onload = (e) => {
         const logs = JSON.parse(e.target.result).map((l, index) => ({
           index,
-          ...l
+          ...l,
         }));
         console.log({ logs }); // eslint-disable-line no-console
         this.setState({ logs });
@@ -192,7 +357,11 @@ class LogsViewer extends Component<*, *> {
           <HeaderEmptyState onFiles={this.onFiles} />
         ) : (
           <>
-            <Header logs={logs} />
+            <Header
+              onFiles={this.onFiles}
+              logs={logs}
+              logsMeta={logs.find((l) => l.message === "exportLogsMeta")}
+            />
             <Logs logs={logs} />
           </>
         )}
@@ -204,7 +373,7 @@ class LogsViewer extends Component<*, *> {
 // $FlowFixMe
 LogsViewer.demo = {
   title: "Logs viewer",
-  url: "/logsviewer"
+  url: "/logsviewer",
 };
 
 export default LogsViewer;
