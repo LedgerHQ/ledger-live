f060023702707bbf13def763e9b1df603cdf6335
LFBarreto <lfb.access@gmail.com>
2021-03-24 17:29:25 +0100

    (PrivacyPolicy): add translated url for privacy policy (#1633)

--- src/config/urls.js
+++ src/config/urls.js
@@ -6,8 +6,16 @@ export const urls = {
   contact:
     "https://support.ledger.com/hc/en-us/requests/new?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=contact",
   terms: "https://github.com/LedgerHQ/ledger-live-mobile/blob/master/TERMS.md",
-  privacyPolicy:
-    "https://shop.ledger.com/pages/privacy-policy?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=privacy",
+  privacyPolicy: {
+    en:
+      "https://shop.ledger.com/pages/privacy-policy?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=privacy",
+    fr:
+      "https://shop.ledger.com/pages/politique-de-confidentialite?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=privacy",
+    es:
+      "https://shop.ledger.com/pages/politica-de-privacidad?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=privacy",
+    ru:
+      "https://shop.ledger.com/pages/nasha-politika-konfidentsialnosti?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=privacy",
+  },
   buyNanoX:
     "https://www.ledger.com/products/ledger-nano-x?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=manager_emptystate",
   playstore: "https://play.google.com/store/apps/details?id=com.ledger.live",
