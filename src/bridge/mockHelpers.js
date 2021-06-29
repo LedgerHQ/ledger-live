899bea15f34f40729549f28b8cadcdf2dd532836
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-06-29 15:09:06 +0200

    Workaround for test flakiness

--- src/bridge/mockHelpers.js
+++ src/bridge/mockHelpers.js
@@ -40,7 +40,7 @@ export const sync: $PropertyType<AccountBridge<*>, "sync"> = (initialAccount) =>
 
         const nextAcc = {
           ...acc,
-          blockHeight: acc.blockHeight + 1,
+          blockHeight: acc.blockHeight + 1000, // make a sync move a lot by blockHeight to avoid flawky tests issue on op confirm.
           lastSyncDate: new Date(),
           operations: ops.concat(acc.operations.slice(0)),
           pendingOperations: [],
@@ -59,7 +59,7 @@ export const sync: $PropertyType<AccountBridge<*>, "sync"> = (initialAccount) =>
       o.complete();
     };
 
-    syncTimeouts[accountId] = setTimeout(sync, 2000);
+    syncTimeouts[accountId] = setTimeout(sync, 500);
 
     return () => {
       clearTimeout(syncTimeouts[accountId]);
