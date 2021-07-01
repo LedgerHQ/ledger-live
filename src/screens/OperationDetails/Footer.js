035498c1741a823d00b58472dd9daa6329b583d0
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2021-07-01 09:28:40 +0200

    use eventProperties

--- src/screens/OperationDetails/Footer.js
+++ src/screens/OperationDetails/Footer.js
@@ -1,12 +1,11 @@
 /* @flow */
-import React, { memo, useCallback } from "react";
+import React, { memo } from "react";
 import { View, StyleSheet, Linking } from "react-native";
 import { Trans } from "react-i18next";
 import { useTheme } from "@react-navigation/native";
 import type { Account } from "@ledgerhq/live-common/lib/types/account";
 import ExternalLink from "../../icons/ExternalLink";
 import Button from "../../components/Button";
-import { track } from "../../analytics/segment";
 
 type Props = {
   url: ?string,
@@ -18,13 +17,6 @@ function Footer({ url, urlWhatIsThis, account }: Props) {
   const { colors } = useTheme();
 
   const currencyId = account.currency.name;
-
-  const viewInExplorer = useCallback(() => {
-    if (url) {
-      Linking.openURL(url);
-      track("ViewInExplorer", { currencyId });
-    }
-  }, [url, currencyId]);
   return (
     <View
       style={[
@@ -46,7 +38,10 @@ function Footer({ url, urlWhatIsThis, account }: Props) {
           event="OperationDetailViewInExplorer"
           type="primary"
           title={<Trans i18nKey="operationDetails.viewInExplorer" />}
-          onPress={viewInExplorer}
+          onPress={() => Linking.openURL(url)}
+          eventProperties={{
+            currencyId,
+          }}
         />
       ) : null}
     </View>
