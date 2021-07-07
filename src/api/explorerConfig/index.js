7c7788f1f366446e3dd841fb2b0bec07b6ae6c06
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-07-07 15:47:44 +0200

    Update index.js

--- src/api/explorerConfig/index.js
+++ src/api/explorerConfig/index.js
@@ -98,10 +98,12 @@ const initialExplorerConfig: FullConfig = {
       base: "EXPLORER",
       version: "v2",
     },
+    /*
     experimental: {
       base: "EXPLORER",
       version: "v3",
     },
+    */
   },
   litecoin: {
     id: "ltc",
