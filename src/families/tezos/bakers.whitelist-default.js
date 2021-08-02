c1337c147fa265d23128d2db5c89bd167e95a834
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-08-02 13:23:17 +0200

    LL-6530

--- src/families/tezos/bakers.whitelist-default.js
+++ src/families/tezos/bakers.whitelist-default.js
@@ -3,8 +3,10 @@
 // unfinished list...
 const whitelist = `\
 tz1WCd2jm4uSt4vntk4vSuUWoZQGhLcDuR9q
+tz1Z3KCf8CLGAYfvVWPEr562jDDyWkwNF7sT
 tz1Scdr2HsZiQjc7bHMeBbmDRXYVvdhjJbBh
 tz1VmiY38m3y95HqQLjMwqnMS7sdMfGomzKi
+tz1bHzftcTKZMTZgLLtnrXydCm6UEqf4ivca
 tz1g8vkmcde6sWKaG2NN9WKzCkDM6Rziq194
 tz1PWCDnz783NNGGQjEFFsHtrcK5yBW4E2rm
 tz1d6Fx42mYgVFnHUW8T8A7WBfJ6nD9pVok8
@@ -13,10 +15,13 @@ tz1KfEsrtDaA1sX7vdM4qmEPWuSytuqCDp5j
 tz1QLXqnfN51dkjeghXvKHkJfhvGiM5gK4tc
 tz1V4qCyvPKZ5UeqdH14HN42rxvNPQfc9UZg
 tz1Lhf4J9Qxoe3DZ2nfe8FGDnvVj7oKjnMY6
+tz1Pwgj6j55akKCyvTwwr9X4np1RskSXpQY4
 tz1Ldzz6k1BHdhuKvAtMRX7h5kJSMHESMHLC
 tz1NortRftucvAkD1J58L32EhSVrQEWJCEnB
 tz1Kf25fX1VdmYGSEzwFy1wNmkbSEZ2V83sY
 tz1RCFbB9GpALpsZtu6J58sb74dm8qe6XBzv
+tz1eEnQhbwf6trb8Q8mPb2RaPkNk2rN7BKi8
+tz1RSWMYKGAykpizFteowByYMueCYv9TMn1L
 tz1Tnjaxk6tbAeC2TmMApPh8UsrEVQvhHvx5
 tz1LLNkQK4UQV6QcFShiXJ2vT2ELw449MzAA
 tz1dbfppLAAxXZNtf2SDps7rch3qfUznKSoK
@@ -31,6 +36,7 @@ tz1P2Po7YM526ughEsRbY4oR9zaUPDZjxFrb
 tz1KzSC1J9aBxKp7u8TUnpN8L7S65PBRkgdF
 tz1STeamwbp68THcny9zk3LsbG3H36DMvbRK
 tz1bLwpPfr3xqy1gWBF4sGvv8bLJyPHR11kx
+tz1TaLYBeGZD3yKVHQGBM857CcNnFFNceLYh
 tz1fJHFn6sWEd3NnBPngACuw2dggTv6nQZ7g
 tz1NEKxGEHsFufk87CVZcrqWu8o22qh46GK6
 tz1V3yg82mcrPJbegqVCPn6bC8w1CSTRp3f8
@@ -38,8 +44,12 @@ tz1aRoaRhSpRYvFdyvgWLL6TGyRoGF51wDjM
 tz1WpeqFaBG9Jm73Dmgqamy8eF8NWLz9JCoY
 tz1MQJPGNMijnXnVoBENFz9rUhaPt3S7rWoz
 tz1dRKU4FQ9QRRQPdaH4zCR6gmCmXfcvcgtB
+tz1hTFcQk2KJRPzZyHkCwbj7E1zY1xBkiHsk
 tz1ZcTRk5uxD86EFEn1vvNffWWqJy7q5eVhc
-tz1aDiEJf9ztRrAJEXZfcG3CKimoKsGhwVAi`.split("\n");
+tz1aDiEJf9ztRrAJEXZfcG3CKimoKsGhwVAi
+tz1NMNQYrYrfNpmUqQrMhJypf6ovnaDZhJHM
+tz1QXrzaHrGACVn15UiCg33aLCJ6npWdQb3J
+tz1axcnVN9tZnCe4sQQhC6f3tfSEXdjPaXPY`.split("\n");
 
 // we give no ordering preference. it's settled at module load time
 whitelist.sort(() => Math.random() - 0.5);
