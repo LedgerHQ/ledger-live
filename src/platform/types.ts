376bf92e0b422fff235c17d60aa72d9133176024
Alexandre Chabrolin <87011321+achabrolin-ledger@users.noreply.github.com>
2021-08-25 10:41:43 +0200

    [LL-6935] Update LLC to use live-app-sdk (#1328)
    * leverage LLP-SDK to remove duplicated code
    
    * Update ledger-live-platform-sdk dep
    
    Point to current last commit on main
    
    * use @ledgerhq/live-app-sdk package
    
    * add tests for platform serializers

--- src/platform/types.ts
+++ src/platform/types.ts
@@ -1,5 +1,11 @@
-import type { BigNumber } from "bignumber.js";
 import type { SignedOperation } from "../types";
+
+export type {
+  Account as PlatformAccount,
+  Currency as PlatformCurrency,
+  Unit as PlatformUnit,
+  Transaction as PlatformTransaction,
+} from "@ledgerhq/live-app-sdk";
 
 export type TranslatableString = {
   en: string;
@@ -45,46 +51,4 @@ export type PlatformApi = {
   fetchManifest: () => Promise<AppManifest[]>;
 };
 
-export type PlatformAccount = {
-  id: string;
-  name: string;
-  address: string;
-  currency: string;
-  balance: BigNumber;
-  spendableBalance: BigNumber;
-  blockHeight: number;
-  lastSyncDate: Date;
-};
-export type PlatformUnit = {
-  name: string;
-  code: string;
-  magnitude: number;
-};
-export type PlatformCurrency = {
-  type: string;
-  color: string;
-  ticker: string;
-  id: string;
-  name: string;
-  family: string;
-  units: PlatformUnit[];
-};
-export interface PlatformTransactionCommon {
-  amount: BigNumber;
-  recipient: string;
-}
-export interface PlatformEthereumTransaction extends PlatformTransactionCommon {
-  family: "ethereum";
-  nonce: number | null | undefined;
-  data: Buffer | null | undefined;
-  gasPrice: BigNumber | null | undefined;
-  gasLimit: BigNumber | null | undefined;
-}
-export interface PlatformBitcoinTransaction extends PlatformTransactionCommon {
-  family: "bitcoin";
-  feePerByte: BigNumber | null | undefined;
-}
-export type PlatformTransaction =
-  | PlatformEthereumTransaction
-  | PlatformBitcoinTransaction;
 export type PlatformSignedTransaction = SignedOperation;
