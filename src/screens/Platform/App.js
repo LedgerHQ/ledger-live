bc41fb10b4cd75f9311506ea660c6fbac32ed585
Kevin Lambert <86959861+klambert-ledger@users.noreply.github.com>
2021-09-22 14:39:12 +0200

    LL 6884 - Add discover deeplink (#1889)
    * Add root deeplink for the platform catalog & dapp
    
    * Add platform & dapp deeplink in useDeepLinkHandler
    
    * Update Platform to open dapp from route param
    
    * Update deep links test page to add platform

--- src/screens/Platform/App.js
+++ src/screens/Platform/App.js
@@ -1,28 +1,31 @@
+// @flow
+
 import React from "react";
 import { usePlatformApp } from "@ledgerhq/live-common/lib/platform/PlatformAppProvider";
-
+import type { StackScreenProps } from "@react-navigation/stack";
 import { useTheme } from "@react-navigation/native";
 import TrackScreen from "../../analytics/TrackScreen";
 import WebPlatformPlayer from "../../components/WebPlatformPlayer";
 
-const PlatformApp = ({ route }: { route: { params: Props } }) => {
+const PlatformApp = ({ route }: StackScreenProps) => {
   const { dark } = useTheme();
-  const appId = route.params.platform;
+  const { platform: appId, ...params } = route.params;
   const { manifests } = usePlatformApp();
   const manifest = manifests.get(appId);
   const themeType = dark ? "dark" : "light";
 
-  return (
+  return manifest ? (
     <>
       <TrackScreen category="Platform" name="App" />
       <WebPlatformPlayer
         manifest={manifest}
         inputs={{
           theme: themeType,
+          ...params,
         }}
       />
     </>
-  );
+  ) : null;
 };
 
 export default PlatformApp;
