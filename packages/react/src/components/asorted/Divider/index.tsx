758b4028249f6d728587f8b03450010ea3661d39
LFBarreto <lfb.access@gmail.com>
2021-09-24 18:03:58 +0200

    init ledger react ui lib

---
+++ packages/react/src/components/asorted/Divider/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Divider = styled.div`
+  display: block;
+  background: ${(p) => p.theme.colors.palette.neutral.c90};
+  height: 1px;
+`;
+
+export default Divider;
