1480f2806222fe3e24f28b5c169637e080377577
LFBarreto <lfb.access@gmail.com>
2021-09-29 14:18:07 +0200

    (Rebrand): removal of all design system resources from main repo -> moving to ledherHQ/ui (#1921)

--- uilib/storybook/stories/index.ts
+++
@@ -1,9 +0,0 @@
-import "./Button/Button.stories";
-import "./Text/Text.stories";
-import "./Layout/Modal/Modal.stories";
-import "./Layout/Modal/Tooltip.stories";
-import "./Layout/ScrollContainer.stories";
-import "./Layout/Flex.stories";
-import "./Navigation/SlideIndicator/SlideIndicator.stories";
-import "./Icon/IconBox.stories";
-import "./Loader/Loader.stories";
