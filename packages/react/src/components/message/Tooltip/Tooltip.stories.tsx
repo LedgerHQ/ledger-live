46d2051c2d48e8c4a5ad5acc05844492980b2923
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-10-06 15:11:01 +0200

    design(react): add tooltip component

---
+++ packages/react/src/components/message/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,55 @@
+import Text from "@ui/components/asorted/Text";
+import FlexBox from "@ui/components/layout/Flex";
+import React from "react";
+import TooltipComponent, { Props } from "./index";
+
+export default {
+  title: "Messages/Tooltip",
+  component: TooltipComponent,
+  argTypes: {
+    content: { type: "string", defaultValue: "Hello World!" },
+    visible: { type: "boolean" },
+    disabled: { type: "boolean" },
+    placement: {
+      control: { type: "radio" },
+      options: [
+        "top",
+        "top-start",
+        "top-end",
+        "right",
+        "right-start",
+        "right-end",
+        "bottom",
+        "bottom-start",
+        "bottom-end",
+        "left",
+        "left-start",
+        "left-end",
+        "auto",
+        "auto-start",
+        "auto-end",
+      ],
+    },
+  },
+};
+
+export const Tooltip = (args: Props): JSX.Element => (
+  <FlexBox alignItems="center" justifyContent="center" style={{ height: "100vh" }}>
+    <TooltipComponent {...args}>
+      <div style={{ padding: "5px" }}>
+        <Text
+          as="div"
+          ff="Inter|SemiBold"
+          color="palette.neutral.c100"
+          style={{ border: "2px solid #AAA", borderRadius: "5px" }}
+          p={10}
+          textAlign="center"
+        >
+          Hover
+          <br />
+          me
+        </Text>
+      </div>
+    </TooltipComponent>
+  </FlexBox>
+);
