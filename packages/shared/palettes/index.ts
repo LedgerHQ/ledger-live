24956a166eb7d94054603c035ca41c0996bbc0cb
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-10-18 11:37:48 +0200

    chore(shared): shorten exported palette type

--- packages/shared/palettes/index.ts
+++ packages/shared/palettes/index.ts
@@ -1,72 +1,10 @@
 import dark from "./dark";
 import light from "./light";
 
-export type ColorPalette = {
-  type: string;
-  primary: {
-    c10: string;
-    c20: string;
-    c30: string;
-    c40: string;
-    c50: string;    
-    c60: string;
-    c70: string;
-    c80: string;
-    c90: string;
-    c100: string;
-  };
-  neutral: {
-    c00: string;
-    c20: string;
-    c30: string;
-    c40: string;
-    c50: string;
-    c60: string;
-    c70: string;
-    c80: string;
-    c90: string;
-    c100: string;
-    c100a07: string;
-  };
-  success: {
-    c10: string;
-    c30: string;
-    c40: string;
-    c50: string;
-    c60: string;
-    c80: string;
-    c100: string;
-  };
-  warning: {
-    c10: string;
-    c30: string;
-    c40: string;
-    c50: string;
-    c60: string;
-    c80: string;
-    c100: string;
-  };
-  error: {
-    c10: string;
-    c30: string;
-    c40: string;
-    c50: string;
-    c60: string;
-    c80: string;
-    c100: string;
-  };
-  constant: {
-    overlay: string;
-    white: string;
-  };
-  background: {
-    main: string;
-  };
-};
+export type ThemeNames = keyof typeof palettes;
+export type ColorPalette = typeof dark | typeof light;
 
 export const palettes: { dark: ColorPalette; light: ColorPalette } = {
   dark,
   light,
 };
-
-export type ThemeNames = keyof typeof palettes;
