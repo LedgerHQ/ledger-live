7fcd5eaadb3f7f9f750637b63dc51a79096a3974
LFBarreto <lfb.access@gmail.com>
2021-10-27 16:31:25 +0200

    LL-6608 (Banner): update banenr with family pack + remove polkastake (#1940)

--- src/config/urls.js
+++ src/config/urls.js
@@ -102,14 +102,12 @@ export const urls = {
   banners: {
     blackfriday:
       "https://shop.ledger.com/pages/black-friday?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
-    backupPack:
-      "https://shop.ledger.com/products/ledger-backup-pack?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
+    familyPack:
+      "https://shop.ledger.com/products/ledger-nano-s-3pack?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
     ledgerAcademy:
       "https://www.ledger.com/academy?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
     valentine:
       "https://shop.ledger.com/pages/valentines-day-special-offers?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
-    polkaStake:
-      "https://www.ledger.com/staking-polkadot?utm_source=ledger_live_mobile&utm_medium=self_referral&utm_content=banner_carousel",
     twitterIntent: "https://twitter.com/intent/tweet",
   },
   platform: {
