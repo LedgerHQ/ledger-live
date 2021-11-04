b6428586d387f2e18a482cfe5b2c17ab4a883d48
Junichi Sugiura <jun.sugiura.jp@gmail.com>
2021-11-04 12:38:34 +0100

    fix manager link configuration (#1969)

--- src/index.js
+++ src/index.js
@@ -249,19 +249,19 @@ const linkingOptions = {
                   [ScreenName.PlatformCatalog]: "discover/:platform?",
                 },
               },
-            },
-            [NavigatorName.Manager]: {
-              screens: {
-                /**
-                 * ie: "ledgerlive://manager" will open the manager
-                 *
-                 * @params ?installApp: string
-                 * ie: "ledgerlive://manager?installApp=bitcoin" will open the manager with "bitcoin" prefilled in the search input
-                 *
-                 * * @params ?searchQuery: string
-                 * ie: "ledgerlive://manager?searchQuery=bitcoin" will open the manager with "bitcoin" prefilled in the search input
-                 */
-                [ScreenName.Manager]: "manager",
+              [NavigatorName.Manager]: {
+                screens: {
+                  /**
+                   * ie: "ledgerlive://manager" will open the manager
+                   *
+                   * @params ?installApp: string
+                   * ie: "ledgerlive://manager?installApp=bitcoin" will open the manager with "bitcoin" prefilled in the search input
+                   *
+                   * * @params ?searchQuery: string
+                   * ie: "ledgerlive://manager?searchQuery=bitcoin" will open the manager with "bitcoin" prefilled in the search input
+                   */
+                  [ScreenName.Manager]: "manager",
+                },
               },
             },
           },
