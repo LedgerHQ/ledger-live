5ca9708a07a8117f1541d3a9df1f130a5a3cf319
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-11-04 18:19:08 +0100

    fix(react): add a ref forwarding wraper for the tippyjs component

--- packages/react/src/components/message/Tooltip/index.tsx
+++ packages/react/src/components/message/Tooltip/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { forwardRef } from "react";
+import { isForwardRef } from "react-is";
 import Tippy, { TippyProps } from "@tippyjs/react";
 import Text from "../../asorted/Text";
 
@@ -26,8 +27,32 @@ export type Props = TippyProps & {
   placement?: Placement;
 };
 
+// Tippyjs need the ref to be forwarded to the DOM element wrapping the children.
+// This component has been created to add a wrapping span and use its ref when needed.
+// See: https://github.com/atomiks/tippyjs-react#component-children
+const Wrapper: React.ComponentType<React.PropsWithChildren<unknown>> = forwardRef(
+  (props, ref: React.LegacyRef<HTMLElement>) => {
+    const childrenCount = React.Children.count(props.children);
+
+    try {
+      const child = React.Children.only(props.children);
+      const isValidElement = React.isValidElement(child);
+      const isForwardingRef = isForwardRef(child);
+      const isDomElement = isValidElement && typeof child.type === "string";
+
+      if (isForwardingRef || isDomElement) {
+        return React.cloneElement(child, { ref });
+      } else {
+        return <span ref={ref}>{props.children}</span>;
+      }
+    } catch (e) {
+      return childrenCount < 1 ? null : <span ref={ref}>{props.children}</span>;
+    }
+  },
+);
+
 export default function Tooltip(props: Props): JSX.Element | null {
-  const { content, placement = "auto", ...rest } = props;
+  const { content, placement = "auto", children, ...rest } = props;
   return (
     <Tippy
       {...rest}
@@ -37,6 +62,8 @@ export default function Tooltip(props: Props): JSX.Element | null {
           {content}
         </Text>
       }
-    />
+    >
+      <Wrapper children={children} />
+    </Tippy>
   );
 }
