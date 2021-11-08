4f2bd13771a5c10cb4ff3f60611cc8a1ab2bf66a
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-11-08 16:25:07 +0100

    chore(react): transpile to commonjs

---
+++ packages/react/scripts/transpile.js
@@ -0,0 +1,28 @@
+const esbuild = require("esbuild");
+const glob = require("tiny-glob");
+
+const globEntryPoints = () =>
+  Promise.all([`src/**/*.ts`, `src/**/*.tsx`].map((path) => glob(path))).then(([ts, tsx]) => [
+    ...ts,
+    ...tsx,
+  ]);
+
+const commonConfig = {
+  entryPoints: ["src/index.ts"],
+  sourcemap: true,
+};
+
+const commonjs = () =>
+  globEntryPoints().then((entryPoints) =>
+    esbuild.build({
+      ...commonConfig,
+      entryPoints,
+      outdir: "lib/cjs",
+      format: "cjs",
+    }),
+  );
+
+commonjs().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
