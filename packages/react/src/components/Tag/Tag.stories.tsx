788b8ab3edf664c37d7ace0eff9af04d6267bb47
ofreyssinet-ledger <91890529+ofreyssinet-ledger@users.noreply.github.com>
2021-11-10 14:16:33 +0100

    LLD - Tag complete implementation of design spec (#77)
    * Update typo style for Tag component
    
    https://www.figma.com/file/WXCEmRMwW47ELrFonzPTGg/LLD-%2F-Library?node-id=967%3A279
    
    * Add different sizes of Tags
    
    * Document Tag props
    
    * Add size options to Tag story
    
    * Add outlineOpacity type, disabled prop and fix all colors
    
    * Add story with all combinations in 1 page
    
    * Cleanup tag story
    
    * Rename tag main story to "Overview"

--- packages/react/src/components/Tag/Tag.stories.tsx
+++ packages/react/src/components/Tag/Tag.stories.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 import { StoryTemplate } from "../helpers";
-import Tag, { Props } from "./index";
+import Tag, { Type, Size, Props } from "./index";
+import Text from "../asorted/Text";
+import Flex from "../layout/Flex";
+
+const sizes: Size[] = ["large", "medium", "small"];
+const types: Type[] = ["plain", "opacity", "outlined", "outlinedOpacity"];
+const states = [{}, { disabled: true }];
 
 export default {
   title: "Toasts/Tag",
@@ -13,13 +19,46 @@ export default {
       },
     },
     type: {
-      options: ["plain", "opacity", "outlined"],
+      options: types,
+    },
+    size: {
+      options: sizes,
     },
     children: {
       type: "string",
     },
   },
 };
+
+export const Overview = ((): JSX.Element => (
+  <Flex flexDirection="column">
+    {sizes.map((size) => (
+      <Flex flexDirection="column" marginBottom={5}>
+        <Text variant="h5" marginBottom={5}>
+          size="{size}"
+        </Text>
+        {types.map((type) => (
+          <Flex flexDirection="row" alignItems="center" mb={5}>
+            <div style={{ width: "150px" }}>
+              <Text variant="small" color="palette.neutral.c70">
+                type="{type}"
+              </Text>
+            </div>
+            <Flex flexDirection="row" mt="5px" columnGap="16px">
+              {states.map((state) =>
+                [true, false].map((active: boolean) => (
+                  <Tag size={size} type={type} active={active} {...state}>
+                    {active ? "Active" : "Inactive"}
+                  </Tag>
+                )),
+              )}
+            </Flex>
+          </Flex>
+        ))}
+      </Flex>
+    ))}
+  </Flex>
+)).bind({});
 
 const Template: StoryTemplate<Props> = (args: Props): JSX.Element => (
   <Tag {...args}>{args.children}</Tag>
@@ -28,7 +67,7 @@ const Template: StoryTemplate<Props> = (args: Props): JSX.Element => (
 const defaultArgs = {
   active: true,
   children: "Label",
-  type: "",
+  type: "large",
 };
 
 export const Plain = Template.bind({});
@@ -45,4 +84,9 @@ export const Outlined = Template.bind({});
 Outlined.args = {
   ...defaultArgs,
   type: "outlined",
+};
+export const OutlinedOpacity = Template.bind({});
+Outlined.args = {
+  ...defaultArgs,
+  type: "outlinedOpacity",
 };
--- packages/react/src/components/Tag/index.tsx
+++ packages/react/src/components/Tag/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { TextVariants } from "src/styles/theme";
 import styled from "styled-components";
 import { border, BorderProps, space, SpaceProps, color, ColorProps } from "styled-system";
-import Text from "../asorted/Text";
+import Text, { TextProps } from "../asorted/Text";
+
+export type Size = "large" | "medium" | "small";
+export type Type = "plain" | "opacity" | "outlined" | "outlinedOpacity";
 
 export type Props = React.PropsWithChildren<
   {
@@ -12,34 +16,83 @@ export type Props = React.PropsWithChildren<
     /**
      * Tag style.
      */
-    type?: "plain" | "opacity" | "outlined";
+    type?: Type;
+    /**
+     * Size of the tag, affects the padding and the casing (uppercase for small and medium)
+     */
+    size?: Size;
+    /**
+     * Props passed to the text component, overriding props set internally by Tag component
+     */
+    textProps?: TextProps;
+
+    disabled?: boolean;
   } & BorderProps &
     ColorProps
 >;
 
-function getColor({ type, active }: Props) {
+function getColor({ type, active, disabled }: Props) {
   switch (type) {
-    case "opacity":
-    case "outlined":
-      return "palette.primary.c90";
-    default:
+    case "plain":
+      if (disabled) return active ? "palette.neutral.c00" : "palette.neutral.c70";
       return active ? "palette.neutral.c00" : "palette.primary.c90";
-  }
-}
-function getBgColor({ type, active }: Props) {
-  switch (type) {
-    case "opacity":
-      return active ? "palette.primary.c20" : undefined;
-    case "outlined":
-      return;
     default:
-      return active ? "palette.primary.c90" : undefined;
+      return disabled ? "palette.neutral.c70" : "palette.primary.c90";
   }
 }
 
-function getBorderColor({ type, active }: Props) {
-  if (type === "outlined" && active) {
-    return "palette.primary.c90";
+function getBgColor({ type, active, disabled }: Props) {
+  switch (type) {
+    case "plain":
+      return active ? (disabled ? "palette.neutral.c70" : "palette.primary.c90") : undefined;
+    case "opacity":
+      return active ? (disabled ? "palette.neutral.c30" : "palette.primary.c20") : undefined;
+    default:
+      return;
+  }
+}
+
+function getBorderColor({ type, active, disabled }: Props) {
+  if (!active) return;
+  switch (type) {
+    case "outlined":
+      return disabled ? "palette.neutral.c70" : "palette.primary.c90";
+    case "outlinedOpacity":
+      return disabled ? "palette.neutral.c40" : "palette.primary.c40";
+  }
+}
+
+function getPadding({ size }: Props) {
+  switch (size) {
+    case "small":
+      return "3px 5px";
+    case "medium":
+      return "6px 8px";
+    case "large":
+    default:
+      return "9px 10px 10px";
+  }
+}
+
+function getTextProps({ size }: Props): {
+  variant: TextVariants;
+  fontWeight?: string;
+  uppercase?: boolean;
+} {
+  switch (size) {
+    case "small":
+    case "medium":
+      return {
+        variant: "tiny",
+        fontWeight: "semiBold",
+        uppercase: true,
+      };
+    case "large":
+    default:
+      return {
+        variant: "extraSmall",
+        fontWeight: "semiBold",
+      };
   }
 }
 
@@ -47,22 +100,23 @@ const TagContainer = styled.div.attrs((props: Props) => ({
   backgroundColor: props.bg || props.backgroundColor || getBgColor(props),
   color: props.color || getColor(props),
   borderColor: getBorderColor(props),
-  p: "7px", // TODO: use spacing from the theme when it gets updated
 }))<Props & BorderProps & SpaceProps & ColorProps>`
   display: inline-flex;
   justify-content: center;
   border: 1px solid transparent;
   border-radius: ${(p) => `${p.theme.radii[1]}px`};
+  padding: ${(p) => getPadding(p)};
   ${border}
   ${space}
   ${color}
 `;
 
-export default function Tag({ children, ...props }: Props): JSX.Element {
+export default function Tag({ children, textProps, size = "large", ...props }: Props): JSX.Element {
   const textColor = getColor(props);
+  const baseTextProps = getTextProps({ size, ...props });
   return (
-    <TagContainer {...props}>
-      <Text variant="tiny" color={textColor}>
+    <TagContainer size={size} {...props}>
+      <Text {...baseTextProps} color={textColor} {...(textProps ? textProps : {})}>
         {children}
       </Text>
     </TagContainer>
