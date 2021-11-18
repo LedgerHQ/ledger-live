71ed1d62e7d6c06ab6ee01ea714f6506b08fb6d8
Gabriel R. Soares <gabriel.soares@ledger.fr>
2021-11-18 14:29:59 +0100

    Export notification Badge component in the library

--- packages/react/src/components/message/index.ts
+++ packages/react/src/components/message/index.ts
@@ -1,5 +1,6 @@
 export { default as Alert } from "./Alert";
 export { default as Log } from "./Log";
 export { default as Notification } from "./Notification";
+export { default as Badge } from "./Notification/Badge";
 export { default as Tooltip } from "./Tooltip";
 export { default as Tip } from "./Tip";
