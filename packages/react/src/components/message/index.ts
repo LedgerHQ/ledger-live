2b2057a1d660cec8b6dd712dae11706ee9356b34
Gabriel R. Soares <gabriel.soares@ledger.fr>
2021-11-19 14:54:35 +0100

    Export Major notification component externally

--- packages/react/src/components/message/index.ts
+++ packages/react/src/components/message/index.ts
@@ -4,3 +4,4 @@ export { default as Notification } from "./Notification";
 export { default as Badge } from "./Notification/Badge";
 export { default as Tooltip } from "./Tooltip";
 export { default as Tip } from "./Tip";
+export { default as MajorNotification } from "./MajorNotification";
