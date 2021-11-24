0878d02670072c971591821e12ebd8dc8fe9a83d
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-11-24 12:15:36 +0100

    chore(native): export Accordion component

--- packages/native/src/components/Layout/index.ts
+++ packages/native/src/components/Layout/index.ts
@@ -1,4 +1,6 @@
+export { default as Accordion } from "./Collapse/Accordion";
 export { default as Flex } from "./Flex";
 export * from "./Modals";
 export { default as ScrollContainer } from "./ScrollContainer";
+export { default as ScrollContainerHeader } from "./ScrollContainerHeader";
 export { default as Row } from "./Table/Row";
