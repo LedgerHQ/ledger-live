01cb70e9a7516711a42e22e5596748e2a13349c5
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-11-30 11:48:48 +0100

    Add more documentation to the Link component & storybook

--- packages/react/src/components/cta/Link/index.tsx
+++ packages/react/src/components/cta/Link/index.tsx
@@ -8,16 +8,41 @@ import baseStyled, { BaseStyledProps } from "../../styled";
 
 export type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> &
   BaseStyledProps & {
+    /**
+     * Component that takes `{size: number; color?: string}` as props
+     */
     Icon?: React.ComponentType<{ size: number; color?: string }>;
+    /**
+     * Affects the colors of the text, icon & underline, can be overriden by the `color` prop
+     */
     type?: "main" | "shade" | "color";
+    /**
+     * Affect the font variant & icon size
+     */
     size?: "small" | "medium" | "large";
+    /**
+     * Color of the link, overrides colors defined by the `type` prop
+     */
     color?: string;
+    /**
+     * Props passed to the rendered text
+     */
     textProps?: TextProps;
+    /**
+     * If true text will always be underlined, else it will be underlined only on hover
+     */
     alwaysUnderline?: boolean;
+    /**
+     * Position of the icon relative to the text
+     */
     iconPosition?: "right" | "left";
     disabled?: boolean;
     children?: React.ReactNode;
   };
+
+export const DEFAULT_ICON_POSITION = "right";
+export const DEFAULT_SIZE = "medium";
+export const DEFAULT_TYPE = "main";
 
 const IconContainer = styled.div<{
   iconPosition: "right" | "left";
@@ -53,9 +78,14 @@ export const Base = baseStyled.a<LinkProps>`
 
 `;
 
-const LinkContainer = (props: LinkProps): React.ReactElement => {
-  const { Icon, iconPosition = "right", children, color, size = "medium", textProps } = props;
-
+const LinkContainer = ({
+  Icon,
+  iconPosition = DEFAULT_ICON_POSITION,
+  children,
+  color,
+  size = DEFAULT_SIZE,
+  textProps,
+}: LinkProps): React.ReactElement => {
   const text = (
     <Text
       variant={ctaTextType[size]}
@@ -81,7 +111,7 @@ const LinkContainer = (props: LinkProps): React.ReactElement => {
 };
 
 const Link = (props: LinkProps): React.ReactElement => {
-  const { type = "main", size = "medium", color } = props;
+  const { type = DEFAULT_TYPE, size = DEFAULT_SIZE, color } = props;
   return (
     <Base color={color} {...props}>
       <LinkContainer {...props} type={type} size={size} />
