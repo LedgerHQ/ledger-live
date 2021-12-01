928545768845df75995d23790e709a14f146a56c
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-12-01 10:28:28 +0100

    Add "control: false" to Icon prop in Link stories

--- packages/react/src/components/cta/Link/Link.stories.tsx
+++ packages/react/src/components/cta/Link/Link.stories.tsx
@@ -8,7 +8,7 @@ export default {
   parameters: { actions: { argTypesRegex: false } },
   argTypes: {
     Icon: {
-      options: [],
+      control: false,
     },
     type: {
       options: ["main", "shade", "color", undefined],
