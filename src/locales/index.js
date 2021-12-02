2146d2d969c48b7bbad3899c711d88be866bb6ed
LFBarreto <lfb.access@gmail.com>
2021-12-02 17:05:09 +0100

    LL-8349 (Languages): activate de tr ja ko (#2037)

--- src/locales/index.js
+++ src/locales/index.js
@@ -3,3 +3,7 @@ exports.fr = { common: require("./fr/common.json"), };
 exports.es = { common: require("./es/common.json"), };
 exports.zh = { common: require("./zh/common.json"), };
 exports.ru = { common: require("./ru/common.json"), };
+exports.de = { common: require("./de/common.json"), };
+exports.tr = { common: require("./tr/common.json"), };
+exports.ja = { common: require("./ja/common.json"), };
+exports.ko = { common: require("./ko/common.json"), };
