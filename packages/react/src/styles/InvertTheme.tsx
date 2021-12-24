fd8303b89f85aa95d3321f8edf23682f992e318d
elbywan <julien.el-baz@ledger.fr>
2021-12-24 15:55:28 +0100

    feat(react): add "if" prop to invert theme

--- packages/react/src/styles/InvertTheme.tsx
+++ packages/react/src/styles/InvertTheme.tsx
@@ -4,9 +4,16 @@ import { defaultTheme } from ".";
 import { palettes } from "@ledgerhq/ui-shared";
 import { Theme } from "./theme";
 
-export const InvertTheme = ({ children }: React.PropsWithChildren<unknown>): React.ReactElement => {
-  const { theme } = useTheme();
-  const revertTheme = theme === "light" ? "dark" : "light";
+export type Props = {
+  if?: boolean;
+};
+
+export const InvertTheme = ({
+  if: condition,
+  children,
+}: React.PropsWithChildren<Props>): React.ReactElement => {
+  const theme = useTheme();
+  const revertTheme = theme.theme === "light" ? "dark" : "light";
   const newTheme: Theme = useMemo(
     () => ({
       ...defaultTheme,
@@ -16,5 +23,5 @@ export const InvertTheme = ({ children }: React.PropsWithChildren<unknown>): Rea
     [revertTheme],
   );
 
-  return <ThemeProvider theme={newTheme}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={condition ? newTheme : theme}>{children}</ThemeProvider>;
 };
