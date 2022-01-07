bf8d2ce9d868fd0c85ca5e3e5a9895d4f040dc2f
jgiamporcaro-ledger <jonathan.giamporcaro@gmail.com>
2022-01-07 17:16:00 +0100

    feat(iconbox): add props to override the container

--- packages/native/src/components/Icon/IconBox/index.tsx
+++ packages/native/src/components/Icon/IconBox/index.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import styled, { useTheme } from "styled-components/native";
 import { IconOrElementType } from "../type";
+import Flex, { FlexBoxProps } from "../../Layout/Flex";
 
 type Props = {
   Icon: IconOrElementType;
   color?: string;
   boxSize?: number;
   iconSize?: number;
-};
-
-type IconContainerProps = {
-  size?: number;
+  /**
+   * Additional props to pass to the container component.
+   * This component is a Flex element.
+   */
+  iconContainerProps?: FlexBoxProps;
 };
 
 const DEFAULT_BOX_SIZE = 56;
 const DEFAULT_ICON_SIZE = 24;
 
-const IconContainer = styled.View`
-  width: ${(p: IconContainerProps) => (p.size ? p.size : DEFAULT_BOX_SIZE)}px;
-  height: ${(p: IconContainerProps) => (p.size ? p.size : DEFAULT_BOX_SIZE)}px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-width: 1px;
-  border-color: ${(p) => p.theme.colors.neutral.c40};
+const IconContainer = styled(Flex).attrs<{ size: Props["boxSize"] }>(
+  ({ size = DEFAULT_BOX_SIZE }) => ({
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: "1px",
+    borderColor: "neutral.c40",
+    width: `${size}px`,
+    height: `${size}px`,
+  }),
+)`
   border-radius: ${(p) => `${p.theme.radii[1]}px`};
 `;
 
@@ -32,10 +36,11 @@ export default function IconBox({
   color,
   boxSize = DEFAULT_BOX_SIZE,
   iconSize = DEFAULT_ICON_SIZE,
+  iconContainerProps,
 }: Props): React.ReactElement {
   const { colors } = useTheme();
   return (
-    <IconContainer size={boxSize}>
+    <IconContainer size={boxSize} {...iconContainerProps}>
       {React.isValidElement(Icon) ? (
         Icon
       ) : (
