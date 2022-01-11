892d6a3e34693a330d05b49e2351b605415ec85a
ofreyssinet-ledger <91890529+ofreyssinet-ledger@users.noreply.github.com>
2022-01-11 17:10:18 +0100

    LL-8928 - Fix "Receive page is blank" (#2107)
    * Fix ReceiveFunds/ConnectDevice missing params when navigating in onSkipDevice
    
    * Revert something I tried during debugging

--- src/screens/ReceiveFunds/02-ConnectDevice.js
+++ src/screens/ReceiveFunds/02-ConnectDevice.js
@@ -94,8 +94,7 @@ export default function ConnectDevice({ navigation, route }: Props) {
   const onSkipDevice = useCallback(() => {
     if (!account) return;
     navigation.navigate(ScreenName.ReceiveConfirmation, {
-      accountId: account.id,
-      parentId: parentAccount && parentAccount.id,
+      ...route.params,
     });
   }, [account, navigation, parentAccount]);
 
