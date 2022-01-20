97bdaa7f3a4997afb718783b7b03e41c35f13b1d
LFBarreto <lfb.access@gmail.com>
2022-01-20 14:51:31 +0100

    fix build resolving

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,4 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        root: ["./src"],
-        extensions: [
-          ".ios.js",
-          ".android.js",
-          ".js",
-          ".json",
-          ".ts",
-          ".tsx",
-          ".ios.tsx",
-          ".android.tsx",
-        ],
-      },
-    ],
-    "react-native-reanimated/plugin",
-  ],
+  plugins: ["react-native-reanimated/plugin"],
 };
