880e35195bf5f2b9eac8bc6e2c78a8b3762f7296
elbywan <julien.el-baz@ledger.fr>
2022-02-02 11:57:13 +0100

    ci: attempt to harmonize tests across all libs

--- apps/ledger-live-desktop/babel.plugins.js
+++ apps/ledger-live-desktop/babel.plugins.js
@@ -1,4 +1,4 @@
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
 module.exports = [
   ...(isDevelopment ? [require.resolve("react-refresh/babel")] : []),
