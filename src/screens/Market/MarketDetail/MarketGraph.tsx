3f0a5b5cb402456f103581987ab1cbc7ec164904
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-02-08 10:41:49 +0100

    Fix graph vertical amplitude

--- src/screens/Market/MarketDetail/MarketGraph.tsx
+++ src/screens/Market/MarketDetail/MarketGraph.tsx
@@ -72,6 +72,7 @@ export default function MarketGraph({
               data={data}
               mapValue={mapGraphValue}
               onItemHover={setHoverItem}
+              verticalRangeRatio={10}
             />
           </Animatable.View>
         ) : (
