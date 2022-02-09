78f8b24de52f5c366e44ee2833b5b6c291e9a611
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-02-09 17:44:40 +0100

    log why build breaks

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,7 +1,13 @@
 function readPackage(pkg, context) {
+  if (!pkg?.name) {
+    console.log("no name found");
+    console.log({ pkg }, { context });
+  }
   // Prevents duplicate packages.
   if (
-    ["react-redux", "styled-components"].some((pkgName) => pkgName === pkg.name)
+    ["react-redux", "styled-components"].some(
+      (pkgName) => pkgName === pkg?.name
+    )
   ) {
     delete pkg.peerDependencies;
     delete pkg.peerDependenciesMeta;
