2ea8fd8ad59f1abc004e635a480537a413e97671
elbywan <julien.el-baz@ledger.fr>
2022-02-09 14:00:28 +0100

    chore: use metro 0.67 and fix native-ui packaging

--- apps/ledger-live-mobile/react-native.config.js
+++ apps/ledger-live-mobile/react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependencies: {},
   assets: [
     "./assets/fonts/",
-    "node_modules/@ledgerhq/native-ui/assets/fonts/alpha",
-    "node_modules/@ledgerhq/native-ui/assets/fonts/inter",
+    "node_modules/@ledgerhq/native-ui/lib/assets/fonts/alpha",
+    "node_modules/@ledgerhq/native-ui/lib/assets/fonts/inter",
   ],
 };
