d629d19b30cfcdd79d92667e103236f36e62b747
henrily-ledger <33897120+henrily-ledger@users.noreply.github.com>
2022-02-14 16:25:58 +0100

    LL-9214 remove metro.config.js (#2194)

--- metro.config.js
+++ metro.config.js
@@ -19,29 +19,6 @@ module.exports = {
     }),
   },
   resolver: {
-    sourceExts: [...defaultSourceExts, 'cjs'],
-    resolveRequest: (context, realModuleName, platform, moduleName) => {
-      const { resolveRequest: removed, ...restContext} = context;
-
-      let module = realModuleName;
-      // When resolveRequest exist, ripple-lib doesn't seems to be able to find ws.
-      if (moduleName === 'ws') {
-        return {
-          type: 'sourceFile',
-          filePath: `${__dirname}/node_modules/ws/browser.js`
-        }
-      }
-
-      // Patch for polkadot, because inside of packageInfo.js they used
-      // import.meta that is still not avaible
-      if (
-        context.originModulePath.includes("@polkadot") &&
-        moduleName === "./packageInfo.js"
-      ) {
-        module = module.replace(".js", ".cjs");
-      }
-
-      return resolve(restContext, module, platform);
-    },
+    sourceExts: [...defaultSourceExts, 'cjs']
   }
 };
