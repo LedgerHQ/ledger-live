b331bfdfbc6acb2426fdd8bc843206d82d073f0b
ofreyssinet-ledger <91890529+ofreyssinet-ledger@users.noreply.github.com>
2022-02-16 11:05:28 +0100

    Reenable basic Flipper config on iOS (#2165)
    * Reenable basic Flipper config on iOS
    
    * chore: update podfile
    
    * post rebase yarn && yarn pod
    
    Co-authored-by: elbywan <julien.el-baz@ledger.fr>

--- ios/ledgerlivemobile/AppDelegate.m
+++ ios/ledgerlivemobile/AppDelegate.m
@@ -7,33 +7,23 @@
 #import "RNSplashScreen.h"  // here
 
 #import <Firebase.h>
-
-/* #ifdef FB_SONARKIT_ENABLED */
-/* #import <FlipperKit/FlipperClient.h> */
-/* #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h> */
-/* #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h> */
-/* #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h> */
-/* #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h> */
-/* #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h> */
-
-/* static void InitializeFlipper(UIApplication *application) { */
-/*   FlipperClient *client = [FlipperClient sharedClient]; */
-/*   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults]; */
-/*   [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]]; */
-/*   [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]]; */
-/*   [client addPlugin:[FlipperKitReactPlugin new]]; */
-/*   [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]]; */
-/*   [client start]; */
-/* } */
-/* #endif */
+#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#endif
+#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-/* #ifdef FB_SONARKIT_ENABLED */
-/*   InitializeFlipper(application); */
-/* #endif */
+  [self initializeFlipper:application];
 
   [FIRApp configure];
 
@@ -56,6 +46,20 @@
   [super application:application didFinishLaunchingWithOptions:launchOptions];
 
   return YES;
+}
+
+- (void) initializeFlipper:(UIApplication *)application {
+  #if DEBUG
+  #ifdef FB_SONARKIT_ENABLED
+    FlipperClient *client = [FlipperClient sharedClient];
+    SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+    [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
+    [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+    [client addPlugin: [FlipperKitReactPlugin new]];
+    [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+    [client start];
+  #endif
+  #endif
 }
 
 - (void) showOverlay{
