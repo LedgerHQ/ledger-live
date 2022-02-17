c2eec95ba633f05bd379df01a77d162bb4762977
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-02-17 17:57:36 +0100

    update comment body action and all actions syntax

--- tools/actions/prepare-comment-body/main.ts
+++ tools/actions/prepare-comment-body/main.ts
@@ -1,17 +1,17 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 
 // TODO: REWORK
 const main = async (): Promise<void> => {
-  const images = core.getInput('images');
+  const images = core.getInput("images");
   const imgArr = JSON.parse(images);
 
-  let str = '';
+  let str = "";
   if (imgArr.length) {
     imgArr.forEach((image) => {
-      str += '![](' + image + ') \n';
+      str += "![](" + image + ") \n";
     });
   }
-  core.setOutput('body', str);
+  core.setOutput("body", str);
 };
 
 main().catch((err) => core.setFailed(err));
