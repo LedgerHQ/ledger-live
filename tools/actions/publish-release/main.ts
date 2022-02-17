9e2b1bf6894ef85fae4ec356bc9b33ccc349d9f7
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-02-17 17:57:36 +0100

    update comment body action and all actions syntax

--- tools/actions/publish-release/main.ts
+++ tools/actions/publish-release/main.ts
@@ -1,15 +1,15 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
+import * as github from "@actions/github";
+import * as core from "@actions/core";
 
 const main = async (): Promise<void> => {
   try {
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-    const owner: string = core.getInput('owner');
-    const repo: string = core.getInput('repo');
-    const id: string = core.getInput('id');
-    const body: string = core.getInput('body');
+    const owner: string = core.getInput("owner");
+    const repo: string = core.getInput("repo");
+    const id: string = core.getInput("id");
+    const body: string = core.getInput("body");
 
-    core.info('=== Release notes ===');
+    core.info("=== Release notes ===");
     core.info(body);
 
     const { data } = await octokit.rest.repos.updateRelease({
--- tools/actions/upload-images/main.ts
+++ tools/actions/upload-images/main.ts
@@ -1,39 +1,39 @@
-import fetch from 'isomorphic-unfetch';
-import * as core from '@actions/core';
-import * as fs from 'fs';
-import * as FormData from 'form-data';
-import * as path from 'path';
+import fetch from "isomorphic-unfetch";
+import * as core from "@actions/core";
+import * as fs from "fs";
+import * as FormData from "form-data";
+import * as path from "path";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const clean = (str: string): string =>
   str
-    .replace('-expected.png', '')
-    .replace('-actual.png', '')
-    .replace('-diff.png', '');
+    .replace("-expected.png", "")
+    .replace("-actual.png", "")
+    .replace("-diff.png", "");
 
-const isDiff = (str): boolean => str.includes('diff');
-const isActual = (str): boolean => str.includes('actual');
+const isDiff = (str): boolean => str.includes("diff");
+const isActual = (str): boolean => str.includes("actual");
 
 const uploadImage = async () => {
-  const p = core.getInput('path');
-  const os = core.getInput('os');
-  const workspace = core.getInput('workspace');
+  const p = core.getInput("path");
+  const os = core.getInput("os");
+  const workspace = core.getInput("workspace");
   const fullPath = path.resolve(p);
 
   const upload = async (file, i = 0) => {
     if (i > 2) {
-      return 'error';
+      return "error";
     }
     const body = new FormData();
-    body.append('type', 'file');
-    body.append('image', file);
+    body.append("type", "file");
+    body.append("image", file);
 
     try {
-      const res = await fetch('https://api.imgur.com/3/image', {
-        method: 'POST',
+      const res = await fetch("https://api.imgur.com/3/image", {
+        method: "POST",
         headers: {
-          Accept: 'application/json',
+          Accept: "application/json",
           Authorization: `Client-ID 11eb8a62f4c7927`,
         },
         // @ts-expect-error correct types are not found. FormData are acceptable
@@ -42,7 +42,7 @@ const uploadImage = async () => {
 
       const link = (await res.json()).data.link;
       if (!link) {
-        throw new Error('no link');
+        throw new Error("no link");
       }
       return link;
     } catch (e) {
@@ -55,7 +55,7 @@ const uploadImage = async () => {
     let results = [];
     const dirents = fs.readdirSync(currentPath, { withFileTypes: true });
     dirents.forEach((dirent) => {
-      if (dirent.name.toLocaleLowerCase().includes('retry')) return;
+      if (dirent.name.toLocaleLowerCase().includes("retry")) return;
       const newPath = path.resolve(currentPath, dirent.name);
       const stat = fs.statSync(newPath);
       if (stat && stat.isDirectory()) {
@@ -72,7 +72,7 @@ const uploadImage = async () => {
     files = getAllFiles(fullPath);
   } catch {
     fs.writeFileSync(`${workspace}/images-${os}.json`, JSON.stringify([]));
-    return core.setOutput('images', []);
+    return core.setOutput("images", []);
   }
 
   const resultsP = files.map(async (file) => {
@@ -91,10 +91,10 @@ const uploadImage = async () => {
       formatted[key] = { actual: {}, diff: {}, expected: {} };
 
     const subKey = isActual(file)
-      ? 'actual'
+      ? "actual"
       : isDiff(file)
-      ? 'diff'
-      : 'expected';
+      ? "diff"
+      : "expected";
     const name = path.parse(file).name;
 
     formatted[key][subKey] = {
@@ -108,7 +108,7 @@ const uploadImage = async () => {
   console.log(final);
 
   fs.writeFileSync(`${workspace}/images-${os}.json`, final);
-  core.setOutput('images', final);
+  core.setOutput("images", final);
 };
 
 uploadImage().catch((err) => {
