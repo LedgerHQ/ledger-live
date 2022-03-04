4ded5d21554743167930e1113a21098c32ae6193
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-03-04 15:21:47 +0100

    Add utils functions common to LLD / LLM

--- src/exchange/swap/utils/index.ts
+++ src/exchange/swap/utils/index.ts
@@ -1,9 +1,12 @@
+import jwtDecode from "jwt-decode";
 import { CryptoCurrency, TokenCurrency } from "@ledgerhq/cryptoassets";
 import { makeEmptyTokenAccount } from "../../../account";
 import { Account, SubAccount, TokenAccount } from "../../../types";
-import type { ExchangeRate } from "../types";
+import type { CheckQuoteStatus, ExchangeRate } from "../types";
 import { getAccountCurrency } from "../../../account";
+import { getProviderNameAndSignature } from "../";
 
+// Note: looks like we can't use an enum because this is used in LLD js code
 export const KYC_STATUS = {
   pending: "pending",
   rejected: "closed",
@@ -77,3 +80,112 @@ export const getAvailableAccountsById = (
   accounts
     .filter((acc) => getAccountCurrency(acc)?.id === id && !acc.disabled)
     .sort((a, b) => b.balance.minus(a.balance).toNumber());
+
+// Note: used in UI (LLD / LLM)
+export const isJwtExpired = (jwtToken: string): boolean => {
+  /**
+   * Note:
+   * The JWT token should have at least an exp property (the expiration date of the token)
+   */
+  const { exp } = jwtDecode<{ exp: number }>(jwtToken);
+
+  const currentTime = new Date().getTime() / 1000;
+
+  return currentTime > exp;
+};
+
+// Note: used in UI (LLD / LLM)
+export const getKYCStatusFromCheckQuoteStatus = (
+  checkQuoteStatus: CheckQuoteStatus
+): KYCStatus | null => {
+  switch (checkQuoteStatus.codeName) {
+    case "KYC_PENDING":
+      return KYC_STATUS.pending as KYCStatus;
+
+    case "KYC_FAILED":
+      return KYC_STATUS.rejected as KYCStatus;
+
+    case "KYC_UNDEFINED":
+    case "KYC_UPGRADE_REQUIRED":
+      return KYC_STATUS.upgradeRequierd as KYCStatus;
+
+    case "RATE_VALID":
+      return KYC_STATUS.approved as KYCStatus;
+
+    // FIXME: should handle all other non KYC related error cases somewhere
+    default:
+      return null;
+  }
+};
+
+// Note: used in UI (LLD / LLM)
+export type WidgetTypes = "login" | "kyc";
+export type FTXProviders = "ftx" | "ftxus";
+export const getFTXURL = ({
+  type,
+  provider,
+}: {
+  type: WidgetTypes;
+  provider: FTXProviders;
+}): string => {
+  const domain = (() => {
+    switch (provider) {
+      case "ftx":
+        return "ftx.com";
+
+      case "ftxus":
+        return "ftx.us";
+
+      default:
+        break;
+    }
+  })();
+
+  if (!domain) {
+    throw new Error(`Could not find domain for ${provider}`);
+  }
+
+  return `https://${domain}/${type}?hideFrame=true&ledgerLive=true`;
+};
+
+// Node: used in UI (LLD / LLM)
+export const shouldShowLoginBanner = ({
+  provider,
+  token,
+}: {
+  provider?: string;
+  token?: string;
+}): boolean => {
+  if (!provider) {
+    return false;
+  }
+
+  const providerConfig = getProviderNameAndSignature(provider);
+
+  if (!providerConfig.needsBearerToken) {
+    return false;
+  }
+
+  return !token || isJwtExpired(token);
+};
+
+// Node: used in UI (LLD / LLM)
+export const shouldShowKYCBanner = ({
+  provider,
+  kycStatus,
+}: {
+  provider?: string;
+  kycStatus: KYCStatus;
+}): boolean => {
+  if (!provider) {
+    return false;
+  }
+
+  const providerConfig = getProviderNameAndSignature(provider);
+
+  if (!providerConfig.needsKYC) {
+    return false;
+  }
+
+  return kycStatus !== KYC_STATUS.approved;
+};
