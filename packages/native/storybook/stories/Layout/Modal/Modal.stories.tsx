5d12951c8def092c7f0c075b22bfc07b9be69926
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-03-07 15:38:12 +0100

    Add a BottomModal story with no header props, move to Layout/Modal

--- packages/native/storybook/stories/Layout/Modal/Modal.stories.tsx
+++ packages/native/storybook/stories/Layout/Modal/Modal.stories.tsx
@@ -2,36 +2,42 @@ import React, { useState, useCallback } from "react";
 import { storiesOf } from "../../storiesOf";
 import { text, button, boolean } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import BaseModal from "../../../../src/components/Layout/Modals/BaseModal";
-import Text from "../../../../src/components/Text";
-import { Icons } from "../../../../src/assets";
-import IconBox from "../../../../src/components/Icon/IconBox";
+import { Alert, BaseModal, IconBox, Icons } from "../../../../src";
 
-const ModalStory = () => {
-  const [isOpen, setIsOpen] = useState(true);
+const MakeModalStory = ({ noHeaderProps = false }: { noHeaderProps?: boolean }) =>
+  function ModalStory() {
+    const [isOpen, setIsOpen] = useState(true);
 
-  const openModal = useCallback(() => {
-    setIsOpen(true);
-  }, []);
+    const openModal = useCallback(() => {
+      setIsOpen(true);
+    }, []);
 
-  button("Open modal", openModal);
+    button("Open modal", openModal);
 
-  return (
-    <BaseModal
-      isOpen={isOpen}
-      onClose={() => {
-        action("onClose")();
-        setIsOpen(false);
-      }}
-      title={text("title", "title")}
-      description={text("description", "Description")}
-      subtitle={text("subtitle", "Subtitle")}
-      Icon={<IconBox Icon={Icons.TrashMedium} />}
-      noCloseButton={boolean("noCloseButton", false)}
-    >
-      <Text>{text("content", "Ledger")}</Text>
-    </BaseModal>
-  );
-};
+    return (
+      <BaseModal
+        isOpen={isOpen}
+        onClose={() => {
+          action("onClose")();
+          setIsOpen(false);
+        }}
+        {...(noHeaderProps
+          ? {}
+          : {
+              title: text("title", "title"),
+              description: text("description", "Description"),
+              subtitle: text("subtitle", "Subtitle"),
+              Icon: <IconBox Icon={Icons.TrashMedium} />,
+            })}
+        noCloseButton={boolean("noCloseButton", false)}
+      >
+        <Alert type="info" title="Example children (Alert component)" />
+      </BaseModal>
+    );
+  };
 
-storiesOf((story) => story("Layout", module).add("Modal", ModalStory));
+storiesOf((story) =>
+  story("Layout/Modal", module)
+    .add("BaseModal", MakeModalStory({ noHeaderProps: false }))
+    .add("BaseModal without header props", MakeModalStory({ noHeaderProps: true })),
+);
