4127ec84ec758854566c1d4bc39c3f058d79710c
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-03-08 12:47:02 +0100

    Remove default colors from story as they are not being reapplied when theme changes

--- packages/native/storybook/stories/Loader/ProgressLoader.stories.tsx
+++ packages/native/storybook/stories/Loader/ProgressLoader.stories.tsx
@@ -2,21 +2,17 @@ import React from "react";
 import { storiesOf } from "../storiesOf";
 import { number, color, boolean, select } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import { useTheme } from "styled-components/native";
 import { Text, ProgressLoader } from "../../../src";
 
 const LoaderSample = () => {
-  const { colors } = useTheme();
-  const mainColor = colors.primary.c80;
-  const secondaryColor = colors.primary.c40;
   const progress = number("progress", 0.5, { min: 0, max: 1, step: 0.1 });
   return (
     <ProgressLoader
       progress={progress}
       infinite={boolean("infinite", false)}
       onPress={action("onPress")}
-      mainColor={color("mainColor", mainColor)}
-      secondaryColor={color("secondaryColor", secondaryColor)}
+      mainColor={color("mainColor", "")}
+      secondaryColor={color("secondaryColor", "")}
       radius={number("radius", 48)}
       strokeWidth={number("strokeWidth", 10)}
       frontStrokeLinecap={select("frontStrokeLinecap", ["butt", "round"], "butt")}
