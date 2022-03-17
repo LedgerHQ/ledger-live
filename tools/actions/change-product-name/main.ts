cf89f3294c9fe5d463db44ecf5e2923917b5c16f
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-03-17 18:14:01 +0100

    update action

--- tools/actions/change-product-name/main.ts
+++ tools/actions/change-product-name/main.ts
@@ -3,11 +3,12 @@ import * as fs from "fs";
 
 const main = async () => {
   const filePath = `${core.getInput("path") || ""}/package.json`;
+  const suffix = core.getInput("suffix");
   if (fs.existsSync(filePath)) {
     try {
       const file = fs.readFileSync(filePath, "utf8");
       const json = JSON.parse(file);
-      json.productName = `${json.productName} Beta`;
+      json.productName = `${json.productName} ${suffix}`;
       fs.writeFileSync(filePath, JSON.stringify(json), {
         encoding: "utf8",
         flag: "w",
