bbe62b32d17c7b235f5730a1dc8e2cb42fe2af40
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-03-23 18:23:19 +0100

    add doc on create-release-hash script

--- tools/create-release-hash/index.js
+++ tools/create-release-hash/index.js
@@ -2,6 +2,21 @@ const fs = require("fs/promises");
 const path = require("path");
 const crypto = require("crypto");
 
+/**
+ * createHashFile
+ *
+ * Reads the `dist` folder located at the same root where this script is run,
+ * it creates a sha512 of each binary in that folder,
+ * then create a ${name}-${version}.sha512sum file containing the sha512 of each binary
+ *
+ * The contents of `dist` folder should look like this
+ * | dist
+ * |--
+ *   | ${name}-${version}-linux-x86_64.AppImage
+ *   | ${name}-${version}-mac.dmg
+ *   | ${name}-${version}-mac.zip
+ *   | ${name}-${version}-win-x64.exe
+ */
 async function createHashFile() {
   const files = await fs.readdir(path.join(process.cwd(), "dist"));
   let fileName = files
