ce2cef312dc4fc4943c4b39db53d6f11553fa12e
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-03-25 10:20:11 +0100

    remove unnecesary checks

--- apps/ledger-live-desktop/tools/dist/health-checks.js
+++ apps/ledger-live-desktop/tools/dist/health-checks.js
@@ -93,19 +93,19 @@ module.exports = args => {
       title: "Check for required environment variables",
       task: checkEnv(args.nightly),
     },
-    {
-      title: "Check that git remote branch matches package.json `repository`",
-      task: checkRemote(args.nightly),
-    },
     // TODO: make sure this is not needed anymore
+    // {
+    //   title: "Check that git remote branch matches package.json `repository`",
+    //   task: checkRemote(args.nightly),
+    // },
     // {
     //   title: "Check that the local git repository is clean",
     //   task: isClean,
     //   skip: () => !!args.nightly,
     // },
-    {
-      title: "Check that HEAD is tagged",
-      task: isTagged(args.nightly),
-    },
+    // {
+    //   title: "Check that HEAD is tagged",
+    //   task: isTagged(args.nightly),
+    // },
   ];
 };
