77b290804853c71e6792eaeba5eb3642f6d8eaaa
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-03-31 18:17:41 +0200

    LIVE-545 Use FF and experimental setting to decide which FW update modal to show

--- src/components/FirmwareUpdateBanner.tsx
+++ src/components/FirmwareUpdateBanner.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext, useCallback } from "react";
-
+import { Platform } from "react-native";
 import manager from "@ledgerhq/live-common/lib/manager";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -11,10 +11,7 @@ import { useSelector } from "react-redux";
 import { ScreenName, NavigatorName } from "../const";
 import { BottomDrawer, Flex, Text } from "@ledgerhq/native-ui";
 import { useTheme } from "styled-components";
-import {
-  DownloadMedium,
-  NanoFirmwareUpdateMedium,
-} from "@ledgerhq/native-ui/assets/icons";
+import { DownloadMedium } from "@ledgerhq/native-ui/assets/icons";
 
 import {
   lastSeenDeviceSelector,
@@ -22,6 +19,8 @@ import {
 } from "../reducers/settings";
 import { hasConnectedDeviceSelector } from "../reducers/appstate";
 import Button from "./Button";
+import useEnv from "@ledgerhq/live-common/lib/hooks/useEnv";
+import { useFeature } from "@ledgerhq/live-common/lib/featureFlags";
 
 const FirmwareUpdateBanner = () => {
   const lastSeenDevice: DeviceModelInfo | null = useSelector(
@@ -71,7 +70,15 @@ const FirmwareUpdateBanner = () => {
     setShowDrawer(false);
   };
 
-  return showBanner && hasCompletedOnboarding ? (
+  const usbFwUpdateExperimental = useEnv("USB_FW_UPDATE");
+  const usbFwUpdateFeatureFlag = useFeature("llmUsbFirmwareUpdate");
+  const usbFwUpdateActivated =
+    (usbFwUpdateExperimental || usbFwUpdateFeatureFlag?.enabled) &&
+    Platform.OS === "android";
+
+  // TODO: use the correct checks below to display the banner
+  // showBanner && hasCompletedOnboarding && hasConnectedDevice
+  return true ? (
     <>
       <Flex
         mx={6}
@@ -87,11 +94,10 @@ const FirmwareUpdateBanner = () => {
         </Text>
         <Button
           ml={5}
-          Icon={NanoFirmwareUpdateMedium}
           iconPosition="left"
           type="color"
           title={"Update"}
-          onPress={onPress}
+          onPress={usbFwUpdateActivated ? onExperimentalFirmwareUpdate : onPress}
           outline={false}
         />
       </Flex>
@@ -107,11 +113,6 @@ const FirmwareUpdateBanner = () => {
           type="primary"
           title={t("common.close")}
           onPress={onCloseDrawer}
-        />
-        <Button
-          type="alert"
-          title={"Or... do it here 😯"}
-          onPress={onExperimentalFirmwareUpdate}
         />
       </BottomDrawer>
     </>
