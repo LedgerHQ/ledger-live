78b185cf59737ea60db9707983829cf04ee88bb9
Côme Grellard <come.grellard@ledger.fr>
2022-04-06 09:16:04 +0200

    bugfix/swap-operation-details-navigation-modified Back button on swap operation details now redirects to previous page instead of closing the whole swap feature (#2390)

--- src/components/RootNavigator/BaseNavigator.tsx
+++ src/components/RootNavigator/BaseNavigator.tsx
@@ -72,6 +72,7 @@ import SwapFormSelectAccount from "../../screens/Swap/FormSelection/SelectAccoun
 import SwapFormSelectCurrency from "../../screens/Swap/FormSelection/SelectCurrencyScreen";
 import SwapFormSelectFees from "../../screens/Swap/FormSelection/SelectFeesScreen";
 import SwapFormSelectProviderRate from "../../screens/Swap/FormSelection/SelectProviderRateScreen";
+import SwapOperationDetails from "../../screens/Swap/OperationDetails";
 
 import BuyDeviceScreen from "../../screens/BuyDeviceScreen";
 import { readOnlyModeEnabledSelector } from "../../reducers/settings";
@@ -177,6 +178,14 @@ export default function BaseNavigator() {
           ),
           headerRight: null,
         })}
+      />
+      <Stack.Screen
+        name={ScreenName.SwapOperationDetails}
+        component={SwapOperationDetails}
+        options={{
+          title: t("transfer.swap.form.tab"),
+          headerRight: null,
+        }}
       />
       <Stack.Screen
         name={ScreenName.SwapV2FormSelectCurrency}
