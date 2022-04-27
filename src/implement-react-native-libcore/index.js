47b4ca340b8606704d01c6759fcd8f5c85cfe232
@greweb <renaudeau.gaetan@gmail.com>
2022-04-27 17:31:30 +0100

    Sunset libcore (#2329)
    * tezos js: Fixes NotEnoughBalance errors to display
    
    https://github.com/LedgerHQ/ledger-live-common/pull/1819
    
    * Fixes .toString() unsafe call
    
    * update incoming dep
    
    * Update live-common
    
    * Sunset libcore
    
    * lint
    
    * remove unused patch
    
    * Revert unrelated package bump
    
    * Remove experimental JS setting
    
    * Target LLC branch sunset-libcore
    
    * Update Podfile.lock
    
    * Update yarn.lock after merge on develop
    
    Co-authored-by: haammar-ledger <hakim.aammar-ext@ledger.fr>

--- src/implement-react-native-libcore/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import implementLibcore from "@ledgerhq/live-common/lib/libcore/platforms/react-native";
-import { getNativeModule } from "./specific";
-
-implementLibcore({ getNativeModule });
