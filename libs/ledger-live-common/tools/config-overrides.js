3b658719bcc70a1bf7627fbffdeb68b2122791f6
elbywan <julien.el-baz@ledger.fr>
2022-04-27 15:43:56 +0200

    chore: make llc tools work

--- libs/ledger-live-common/tools/config-overrides.js
+++ libs/ledger-live-common/tools/config-overrides.js
@@ -1,9 +1,18 @@
 module.exports = function override(webpackConfig) {
+  const babelRule = webpackConfig.module.rules[2].oneOf[2];
+  babelRule.options.plugins = ["@babel/plugin-proposal-class-properties"];
+  // console.log(babelRule.options);
+
+  webpackConfig.module.rules.push({
+    test: /\.js$/i,
+    loader: require.resolve("@open-wc/webpack-import-meta-loader"),
+  });
+
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
-    type: "javascript/auto"
+    type: "javascript/auto",
   });
 
   return webpackConfig;
-}
+};
