2241239027e3f92a43343ba769e37baffe0621a1
elbywan <julien.el-baz@ledger.fr>
2022-05-02 12:08:04 +0200

    fix: LLD pnpm7 compat

--- tools/pnpm-utils/index.js
+++ tools/pnpm-utils/index.js
@@ -1,72 +1,87 @@
 function bold(str, colorCode = "") {
-  return "\033" + `[1${colorCode ? `;${colorCode}`: ""}m` + str + "\033[0;0m";
+  return "\033" + `[1${colorCode ? `;${colorCode}` : ""}m` + str + "\033[0;0m";
 }
 
 function field(str, { length = 36, bolden = true } = {}) {
-  const paddedField = ("[" + str + "]").padEnd(length)
-  return bolden ? bold(paddedField) : paddedField
+  const paddedField = ("[" + str + "]").padEnd(length);
+  return bolden ? bold(paddedField) : paddedField;
 }
 
-const memoMap = new Map()
-function addDependencies(filter, dependencies, {
-  kind = "dependencies",
-  matchDevVersion = true,
-  ignoreExisting = false
-} = {}) {
+const memoMap = new Map();
+function addDependencies(
+  filter,
+  dependencies,
+  {
+    kind = "dependencies",
+    matchDevVersion = true,
+    ignoreExisting = false,
+    filterOnPkg = null,
+  } = {}
+) {
   return (pkg, context) => {
-    if (filter instanceof RegExp ? filter.test(pkg?.name) : pkg.name === filter) {
-      const key = `${pkg.name}@${pkg.version}`
-      if (!memoMap.has(key))
-        memoMap.set(key, new Set())
-      const visitedDeps = memoMap.get(key)
+    if (
+      filter instanceof RegExp ? filter.test(pkg?.name) : pkg.name === filter
+    ) {
+      if (filterOnPkg && !filterOnPkg(pkg)) {
+        return;
+      }
+      const key = `${pkg.name}@${pkg.version}`;
+      if (!memoMap.has(key)) memoMap.set(key, new Set());
+      const visitedDeps = memoMap.get(key);
 
-      if (!pkg[kind])
-        pkg[kind] = {}
+      if (!pkg[kind]) pkg[kind] = {};
 
       Object.entries(dependencies).forEach(([dep, depVersion]) => {
-        const devVersion = pkg?.devDependencies?.[dep]
-        const version = devVersion ?? depVersion ?? "*"
-        const depKey = `${dep}@${version}`
+        const devVersion = pkg?.devDependencies?.[dep];
+        const version = (matchDevVersion && devVersion) ?? depVersion ?? "*";
+        const depKey = `${dep}@${version}`;
 
-        if (visitedDeps.has(depKey))
-          return
-        visitedDeps.add(depKey)
+        if (visitedDeps.has(depKey)) return;
+        visitedDeps.add(depKey);
 
         if (pkg[kind][dep]) {
           if (!ignoreExisting) {
-            console.log(`${bold("[!]", 33)} ${field(depKey)} | ${field(key, { length: 0 })} already declares ${dep}@${pkg[kind][dep]} (${kind})`)
-            return
+            console.log(
+              `${bold("[!]", 33)} ${field(depKey)} | ${field(key, {
+                length: 0,
+              })} already declares ${dep}@${pkg[kind][dep]} (${kind})`
+            );
+            return;
           }
         }
 
-        console.log(`${bold("[+]", 32)} ${field(depKey)} | ${field(key)} (${kind})`)
+        console.log(
+          `${bold("[+]", 32)} ${field(depKey)} | ${field(key)} (${kind})`
+        );
 
         pkg[kind] = {
           ...pkg[kind],
-          [dep]: version
-        }
-      })
+          [dep]: version,
+        };
+      });
     }
-  }
+  };
 }
 
 function removePeerDeps(filter) {
   return (pkg, context) => {
-    const key = `${pkg.name}@${pkg.version}`
-    if (filter instanceof RegExp ? filter.test(pkg?.name) : pkg.name === filter) {
-      console.log(`${bold("[-]", 31)} ${field(key)} | (peerDependencies)`)
+    const key = `${pkg.name}@${pkg.version}`;
+    if (
+      filter instanceof RegExp ? filter.test(pkg?.name) : pkg.name === filter
+    ) {
+      console.log(`${bold("[-]", 31)} ${field(key)} | (peerDependencies)`);
       delete pkg.peerDependencies;
       delete pkg.peerDependenciesMeta;
     }
-  }
+  };
 }
 
 function process(fns, pkg, context) {
-  fns.forEach(fn => fn(pkg, context))
+  fns.forEach((fn) => fn(pkg, context));
 }
 
 module.exports = {
   process,
   addDependencies,
-  removePeerDeps
-}
+  removePeerDeps,
+};
