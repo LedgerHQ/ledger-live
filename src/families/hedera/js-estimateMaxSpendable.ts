74cf436586ab8fd373a2f5ee23899a887de5825f
Jack Ta <jack.th.ta@gmail.com>
2022-05-05 00:36:03 -0700

    feat: add edge case for max spendable (#1937)
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/families/hedera/js-estimateMaxSpendable.ts
+++ src/families/hedera/js-estimateMaxSpendable.ts
@@ -19,6 +19,14 @@ export default function estimateMaxSpendable({
   // as fees are based on a currency conversion, we stay
   // on the safe side here and double the estimate for "max spendable"
   const estimatedFee = estimatedFees.multipliedBy(estimatedFeeSafetyRate);
+  
+  let maxSpendable = balance.minus(estimatedFee);
 
-  return Promise.resolve(balance.minus(estimatedFee));
+  // set max spendable to 0 if negative
+  // for cases where the user's account balance is smaller than the estimated fee
+  if (maxSpendable.isLessThan(0)) {
+    maxSpendable = new BigNumber(0);
+  }
+
+  return Promise.resolve(maxSpendable);
 }
