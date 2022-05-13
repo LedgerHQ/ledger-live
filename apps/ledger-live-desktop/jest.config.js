9c01c5da69471de53270e2ddd5d6298e2553ddd8
elbywan <julien.el-baz@ledger.fr>
2022-05-13 13:16:27 +0200

    fix(lld): add polkadot mapper to jest config file

--- apps/ledger-live-desktop/jest.config.js
+++ apps/ledger-live-desktop/jest.config.js
@@ -9,5 +9,14 @@ module.exports = {
   },
   globalSetup: "<rootDir>/tests/setup.js",
   setupFiles: ["<rootDir>/tests/jestSetup.js"],
-  transformIgnorePatterns: ["/node_modules/(?!(@polkadot|@babel/runtime)/)"],
+  moduleNameMapper: {
+    "^@polkadot/([^/]+)/(.+)$": [
+      "@polkadot/$1/index.cjs",
+      "@polkadot/$1/node.cjs",
+      "@polkadot/$1/$2.cjs",
+      "@polkadot/$1/cjs/$2",
+      "@polkadot/$1/$2",
+    ],
+  },
+  // transformIgnorePatterns: ["/node_modules/(?!(@polkadot|@babel/runtime)/)"],
 };
