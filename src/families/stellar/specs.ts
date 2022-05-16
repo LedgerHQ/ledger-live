b8b26e50951594e9528b220458e6474e53f7f8c3
Iveta <quietbits@gmail.com>
2022-05-16 11:17:46 -0400

    Stellar: update specs.ts + speculos

--- src/families/stellar/specs.ts
+++ src/families/stellar/specs.ts
@@ -1,13 +1,24 @@
 import expect from "expect";
 import invariant from "invariant";
+import BigNumber from "bignumber.js";
 import type { Transaction } from "./types";
-import { getCryptoCurrencyById, parseCurrencyUnit } from "../../currencies";
+import {
+  getCryptoCurrencyById,
+  parseCurrencyUnit,
+  listTokensForCryptoCurrency,
+} from "../../currencies";
 import { pickSiblings } from "../../bot/specs";
 import type { AppSpec } from "../../bot/types";
 import { DeviceModelId } from "@ledgerhq/devices";
+
 const currency = getCryptoCurrencyById("stellar");
 const minAmountCutoff = parseCurrencyUnit(currency.units[0], "0.1");
-const reserve = parseCurrencyUnit(currency.units[0], "1");
+const reserve = parseCurrencyUnit(currency.units[0], "1.5");
+
+const MAX_FEE = 5000;
+const USDC_CODE = "USDC";
+const USDC_ISSUER = "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN";
+
 const stellar: AppSpec<Transaction> = {
   name: "Stellar",
   currency,
@@ -20,7 +31,8 @@ const stellar: AppSpec<Transaction> = {
       name: "move ~50%",
       maxRun: 2,
       transaction: ({ account, siblings, bridge, maxSpendable }) => {
-        invariant(maxSpendable.gt(minAmountCutoff), "balance is too low");
+        invariant(maxSpendable.gt(minAmountCutoff), "XLM balance is too low");
+
         const transaction = bridge.createTransaction(account);
         const sibling = pickSiblings(siblings, 4);
         const recipient = sibling.freshAddress;
@@ -29,7 +41,7 @@ const stellar: AppSpec<Transaction> = {
         if (!sibling.used && amount.lt(reserve)) {
           invariant(
             maxSpendable.gt(reserve.plus(minAmountCutoff)),
-            "not enough funds to send to new account"
+            "not enough XLM funds to send to new account"
           );
           amount = reserve;
         }
@@ -40,6 +52,9 @@ const stellar: AppSpec<Transaction> = {
           },
           {
             recipient,
+            // Setting higher max fee here to make sure transaction doesn't
+            // time out.
+            fees: new BigNumber(MAX_FEE),
           },
         ];
 
@@ -56,15 +71,74 @@ const stellar: AppSpec<Transaction> = {
         };
       },
       test: ({ account, accountBeforeTransaction, operation, transaction }) => {
-        expect(account.balance.toString()).toBe(
-          accountBeforeTransaction.balance.minus(operation.value).toString()
+        // We don't know what the final fee will be until after the tx is
+        // submitted. Using higher max fee to make sure tx doesn't time out.
+        expect(account.balance.toNumber()).toBeLessThanOrEqual(
+          accountBeforeTransaction.balance.minus(operation.value).toNumber()
         );
 
         if (transaction.memoValue) {
-          expect(operation.extra).toEqual({
+          expect(operation.extra).toMatchObject({
             memo: transaction.memoValue,
           });
         }
+
+        // Transaction type "payment" can be either "payment" or "create_account"
+        // (if account is not created).
+        const getType = () => {
+          switch (transaction.operationType) {
+            case "payment":
+              return /payment|create_account/;
+            case "changeTrust":
+              return /change_trust/;
+            default:
+              return "";
+          }
+        };
+
+        expect(transaction.operationType).toMatch(getType());
+      },
+    },
+    {
+      name: "add USDC asset",
+      maxRun: 1,
+      transaction: ({ account, bridge, maxSpendable }) => {
+        invariant(maxSpendable.gt(reserve), "balance is too low");
+
+        const transaction = bridge.createTransaction(account);
+        const assets = listTokensForCryptoCurrency(account.currency).map(
+          (a) => a.id
+        );
+        const assetUSDC = assets.find((a) =>
+          a.endsWith(`${USDC_CODE}:${USDC_ISSUER}`)
+        );
+
+        invariant(assetUSDC, "USDC asset not found");
+
+        const updates: Array<Partial<Transaction>> = [
+          {
+            operationType: "changeTrust",
+            // Setting higher max fee here to make sure transaction doesn't
+            // time out.
+            fees: new BigNumber(MAX_FEE),
+          },
+          {
+            assetCode: USDC_CODE,
+            assetIssuer: USDC_ISSUER,
+          },
+        ];
+
+        return {
+          transaction,
+          updates,
+        };
+      },
+      test: ({ account }) => {
+        const assetId = `${USDC_CODE}:${USDC_ISSUER}`;
+        const hasAsset = account.subAccounts?.find((a) =>
+          a.id.endsWith(assetId)
+        );
+        expect(hasAsset).toBeTruthy();
       },
     },
   ],
