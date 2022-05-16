64ad66f723270f7bd7ded2fb19e8352950e989aa
nparigi-ledger <nicolas.parigi@ledger.fr>
2022-05-16 18:41:47 +0200

    LL-2240 - Reborn onboarding

--- apps/ledger-live-mobile/src/screens/Onboarding/steps/postWelcomeSelection.tsx
+++ apps/ledger-live-mobile/src/screens/Onboarding/steps/postWelcomeSelection.tsx
@@ -1,8 +1,7 @@
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigation } from "@react-navigation/native";
+import { RouteProp, useNavigation } from "@react-navigation/native";
 import { track, TrackScreen } from "../../../analytics";
-import ChoiceCard from "../../../components/ChoiceCard";
 import { ScreenName } from "../../../const";
 import OnboardingView from "../OnboardingView";
 import StyledStatusBar from "../../../components/StyledStatusBar";
@@ -10,10 +9,16 @@ import Illustration from "../../../images/illustration/Illustration";
 import DiscoverCard from "../../Discover/DiscoverCard";
 
 const setupLedgerImg = require("../../../images/illustration/Shared/_SetupLedger.png");
-
 const buyNanoImg = require("../../../images/illustration/Shared/_BuyNanoX.png");
+const discoverLiveImg = require("../../../images/illustration/Shared/_DiscoverLive.png");
 
-function PostWelcomeSelection() {
+function PostWelcomeSelection({
+  route,
+}: {
+  route: RouteProp<{ params: { userHasDevice: boolean } }, "params">;
+}) {
+  const { userHasDevice } = route.params;
+
   const navigation = useNavigation();
   const { t } = useTranslation();
 
@@ -25,8 +30,17 @@ function PostWelcomeSelection() {
 
   const buyLedger = useCallback(() => {
     track("Onboarding PostWelcome - Buy Ledger");
+    // TODO: FIX @react-navigation/native using Typescript
+    // @ts-ignore next-line
     navigation.navigate(ScreenName.BuyDeviceScreen);
-  }, []);
+  }, [navigation]);
+
+  const exploreLedger = useCallback(() => {
+    track("Onboarding PostWelcome - Explore Live");
+    // TODO: FIX @react-navigation/native using Typescript
+    // @ts-ignore next-line
+    navigation.navigate(ScreenName.OnboardingModalDiscoverLive);
+  }, [navigation]);
 
   return (
     <OnboardingView
@@ -35,38 +49,58 @@ function PostWelcomeSelection() {
       subTitle={t("onboarding.postWelcomeStep.subtitle")}
     >
       <StyledStatusBar barStyle="dark-content" />
+      {userHasDevice && (
+        <DiscoverCard
+          title={t("onboarding.postWelcomeStep.setupLedger.title")}
+          titleProps={{ variant: "h3" }}
+          subTitle={t("onboarding.postWelcomeStep.setupLedger.subtitle")}
+          event="Onboarding PostWelcome - Setup Ledger"
+          testID={`Onboarding PostWelcome - Selection|SetupLedger`}
+          onPress={setupLedger}
+          cardProps={{ mx: 0 }}
+          Image={
+            <Illustration
+              size={130}
+              darkSource={setupLedgerImg}
+              lightSource={setupLedgerImg}
+            />
+          }
+        />
+      )}
       <DiscoverCard
-        title={t("onboarding.postWelcomeStep.setupLedger.title")}
+        title={t("onboarding.postWelcomeStep.discoverLedger.title")}
         titleProps={{ variant: "h3" }}
-        subTitle={t("onboarding.postWelcomeStep.setupLedger.subtitle")}
-        event="Onboarding PostWelcome - Setup Ledger"
-        testID={`Onboarding PostWelcome - Selection|SetupLedger`}
-        onPress={setupLedger}
+        subTitle={t("onboarding.postWelcomeStep.discoverLedger.subtitle")}
+        event="Onboarding PostWelcome - Explore Ledger"
+        testID={`Onboarding PostWelcome - Selection|ExploreLedger`}
+        onPress={exploreLedger}
         cardProps={{ mx: 0 }}
         Image={
           <Illustration
             size={130}
-            darkSource={setupLedgerImg}
-            lightSource={setupLedgerImg}
+            darkSource={discoverLiveImg}
+            lightSource={discoverLiveImg}
           />
         }
       />
-      <DiscoverCard
-        title={t("onboarding.postWelcomeStep.buyNano.title")}
-        titleProps={{ variant: "h3" }}
-        subTitle={t("onboarding.postWelcomeStep.buyNano.subtitle")}
-        event="Onboarding PostWelcome - Buy Nano"
-        testID={`Onboarding PostWelcome - Selection|BuyNano`}
-        onPress={buyLedger}
-        cardProps={{ mx: 0 }}
-        Image={
-          <Illustration
-            size={130}
-            darkSource={buyNanoImg}
-            lightSource={buyNanoImg}
-          />
-        }
-      />
+      {!userHasDevice && (
+        <DiscoverCard
+          title={t("onboarding.postWelcomeStep.buyNano.title")}
+          titleProps={{ variant: "h3" }}
+          subTitle={t("onboarding.postWelcomeStep.buyNano.subtitle")}
+          event="Onboarding PostWelcome - Buy Nano"
+          testID={`Onboarding PostWelcome - Selection|BuyNano`}
+          onPress={buyLedger}
+          cardProps={{ mx: 0 }}
+          Image={
+            <Illustration
+              size={130}
+              darkSource={buyNanoImg}
+              lightSource={buyNanoImg}
+            />
+          }
+        />
+      )}
       <TrackScreen category="Onboarding" name="SelectDevice" />
     </OnboardingView>
   );
