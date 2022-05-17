42c554cd2fb3e2920504324795b09c4d0aeba311
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-05-17 14:37:28 +0200

    Disable Hedera dataset tests because need frozen accounts

--- src/families/hedera/test-dataset.ts
+++ src/families/hedera/test-dataset.ts
@@ -1,4 +1,4 @@
-import type { DatasetTest } from "../../types";
+import type { CurrenciesData, DatasetTest } from "../../types";
 import type { Transaction } from "./types";
 import {
   NotEnoughBalance,
@@ -7,86 +7,87 @@ import {
 } from "@ledgerhq/errors";
 import { fromTransactionRaw } from "./transaction";
 
-const dataset: DatasetTest<Transaction> = {
-  implementations: ["js"],
-  currencies: {
-    hedera: {
-      scanAccounts: [
-        {
-          name: "hedera seed 1",
-          apdus: `
+const hedera: CurrenciesData<Transaction> = {
+  scanAccounts: [
+    {
+      name: "hedera seed 1",
+      apdus: `
           => e002010009000000002c00000bd6
           <= 9e92a312233d5fd6b5a723875aeea2cea81a8e48ffc00341cff6dffcfd3ab7f29000
           `,
-        },
-      ],
-      accounts: [
+    },
+  ],
+  accounts: [
+    {
+      raw: {
+        id: `js:2:hedera:0.0.751515:`,
+        seedIdentifier: "",
+        name: "Hedera 1",
+        derivationMode: "hederaBip44",
+        index: 0,
+        freshAddress: "0.0.751515",
+        freshAddressPath: "44/3030/0/0/0",
+        freshAddresses: [],
+        blockHeight: 0,
+        operations: [],
+        pendingOperations: [],
+        currencyId: "hedera",
+        unitMagnitude: 8,
+        lastSyncDate: "",
+        balance: "0",
+      },
+      transactions: [
         {
-          raw: {
-            id: `js:2:hedera:0.0.751515:`,
-            seedIdentifier: "",
-            name: "Hedera 1",
-            derivationMode: "",
-            index: 0,
-            freshAddress: "0.0.751515",
-            freshAddressPath: "44/3030/0/0/0",
-            freshAddresses: [],
-            blockHeight: 0,
-            operations: [],
-            pendingOperations: [],
-            currencyId: "hedera",
-            unitMagnitude: 8,
-            lastSyncDate: "",
-            balance: "0",
+          name: "Recipient and sender must not be the same",
+          transaction: fromTransactionRaw({
+            family: "hedera",
+            recipient: "0.0.751515",
+            amount: "100000000",
+          }),
+          expectedStatus: {
+            errors: {
+              recipient: new InvalidAddressBecauseDestinationIsAlsoSource(),
+            },
+            warnings: {},
           },
-          transactions: [
-            {
-              name: "Recipient and sender must not be the same",
-              transaction: fromTransactionRaw({
-                family: "hedera",
-                recipient: "0.0.751515",
-                amount: "100000000",
-              }),
-              expectedStatus: {
-                errors: {
-                  recipient: new InvalidAddressBecauseDestinationIsAlsoSource(),
-                },
-                warnings: {},
-              },
+        },
+        {
+          name: "Amount Required",
+          transaction: fromTransactionRaw({
+            family: "hedera",
+            recipient: "0.0.751515",
+            amount: "0",
+          }),
+          expectedStatus: {
+            errors: {
+              amount: new AmountRequired(),
             },
-            {
-              name: "Amount Required",
-              transaction: fromTransactionRaw({
-                family: "hedera",
-                recipient: "0.0.751515",
-                amount: "0",
-              }),
-              expectedStatus: {
-                errors: {
-                  amount: new AmountRequired(),
-                },
-                warnings: {},
-              },
+            warnings: {},
+          },
+        },
+        {
+          name: "Not enough balance",
+          transaction: fromTransactionRaw({
+            family: "hedera",
+            recipient: "0.0.751515",
+            amount: "1000000000000000",
+          }),
+          expectedStatus: {
+            errors: {
+              amount: new NotEnoughBalance(),
             },
-            {
-              name: "Not enough balance",
-              transaction: fromTransactionRaw({
-                family: "hedera",
-                recipient: "0.0.751515",
-                amount: "1000000000000000",
-              }),
-              expectedStatus: {
-                errors: {
-                  amount: new NotEnoughBalance(),
-                },
-                warnings: {},
-              },
-            },
-          ],
+            warnings: {},
+          },
         },
       ],
     },
-  },
+  ],
 };
 
+const dataset: DatasetTest<Transaction> = {
+  implementations: ["js"],
+  currencies: {
+    // hedera, // LIVE-2376
+  },
+};
 export default dataset;
