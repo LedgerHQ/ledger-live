02b0a687c8aacd8366b594ced04f9de61249899f
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2022-05-23 15:41:58 +0200

    allow multiple release branches (#135)

--- tools/github-bot/src/tools.ts
+++ tools/github-bot/src/tools.ts
@@ -1,7 +1,7 @@
 import { Probot } from "probot";
 
 export const isValidBranchName = (branch: string): boolean =>
-  /^(feat|support|bugfix)\/.+/.test(branch) ||
+  /^(feat|support|bugfix|releases)\/.+/.test(branch) ||
   ["release", "hotfix"].includes(branch);
 
 export const isValidUser = (user: string): boolean =>
