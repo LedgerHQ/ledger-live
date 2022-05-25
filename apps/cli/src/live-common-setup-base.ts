287457cc58448434580c2938b28c32d5106687be
Henri Ly <henri.ly-ext@ledger.fr>
2022-05-25 16:31:20 +0200

    add osmosis to supported coin in cli

--- apps/cli/src/live-common-setup-base.ts
+++ apps/cli/src/live-common-setup-base.ts
@@ -47,7 +47,8 @@ setSupportedCurrencies([
   "celo",
   "hedera",
   "cardano",
-  "solana"
+  "solana",
+  "osmosis"
 ]);
 
 for (const k in process.env) setEnvUnsafe(k as EnvName, process.env[k]);
