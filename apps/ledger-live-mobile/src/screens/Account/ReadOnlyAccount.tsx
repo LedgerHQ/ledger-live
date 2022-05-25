c028b5e6d03c9e855138da93cb431d8ad844348d
Jules Grenier <jules.grenier-ext@ledger.fr>
2022-05-25 18:07:59 +0200

    LIVE-1982 - Gradient container in Account page

--- apps/ledger-live-mobile/src/screens/Account/ReadOnlyAccount.tsx
+++ apps/ledger-live-mobile/src/screens/Account/ReadOnlyAccount.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { FlatList } from "react-native";
-import { Box } from "@ledgerhq/native-ui";
+import { Trans, useTranslation } from "react-i18next";
+import { Box, Flex, Text } from "@ledgerhq/native-ui";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getCryptoCurrencyById } from "@ledgerhq/live-common/lib/currencies";
 
 import { TAB_BAR_SAFE_HEIGHT } from "../../components/TabBar/TabBarSafeAreaView";
 import ReadOnlyAccountGraphCard from "../../components/ReadOnlyAccountGraphCard";
 import ReadOnlyFabActions from "../../components/ReadOnlyFabActions";
+import GradientContainer from "../../components/GradientContainer";
 import { TrackScreen } from "../../analytics";
 
 import { withDiscreetMode } from "../../context/DiscreetModeContext";
@@ -27,6 +29,7 @@ const analytics = (
 function ReadOnlyAccount({ route }: Props) {
   const { currencyId } = route.params;
   const currency = getCryptoCurrencyById(currencyId);
+  const { t } = useTranslation();
 
   const data = [
     <Box mx={6} my={6}>
@@ -38,8 +41,37 @@ function ReadOnlyAccount({ route }: Props) {
     <Box py={3}>
       <ReadOnlyFabActions />
     </Box>,
-    <Box mx={6} mt={8}>
-      {/* Gradient box here */}
+    <Box mt={8}>
+      <GradientContainer containerStyle={{ marginHorizontal: 16 }}>
+        <Flex
+          flex={1}
+          px={10}
+          py={11}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            variant="large"
+            fontWeight="semiBold"
+            color="neutral.c100"
+            textAlign="center"
+          >
+            {t("account.readOnly.noTransaction.title")}
+          </Text>
+          <Text
+            variant="small"
+            fontWeight="medium"
+            color="neutral.c70"
+            textAlign="center"
+            mt={3}
+          >
+            <Trans
+              i18nKey={"account.readOnly.noTransaction.title"}
+              values={currency.name}
+            />
+          </Text>
+        </Flex>
+      </GradientContainer>
     </Box>,
   ];
 
