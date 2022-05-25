6dc53ec1d0c300492d395f6482ca7067aa7b92ed
Thomas Roger Lux <thomas@rogerlux.dev>
2022-05-25 17:44:22 +0200

    feat(common): add icon variant and shape props to IconBox components

--- libs/ui/packages/native/src/components/Icon/IconBox/index.tsx
+++ libs/ui/packages/native/src/components/Icon/IconBox/index.tsx
@@ -5,6 +5,8 @@ import Flex, { FlexBoxProps } from "../../Layout/Flex";
 
 type Props = {
   Icon: IconOrElementType;
+  variant?: "outlined" | "plain";
+  shape?: "square" | "circle";
   color?: string;
   boxSize?: number;
   iconSize?: number;
@@ -15,36 +17,44 @@ type Props = {
   iconContainerProps?: FlexBoxProps;
 };
 
+const DEFAULT_BOX_VARIANT = "outlined";
+const DEFAULT_BOX_SHAPE = "square";
 const DEFAULT_BOX_SIZE = 56;
 const DEFAULT_ICON_SIZE = 24;
 
-const IconContainer = styled(Flex).attrs<{ size: Props["boxSize"] }>(
-  ({ size = DEFAULT_BOX_SIZE }) => ({
+const IconContainer = styled(Flex).attrs<{ size: Props["boxSize"]; variant: Props["variant"] }>(
+  ({ size = DEFAULT_BOX_SIZE, variant = DEFAULT_BOX_VARIANT }) => ({
     justifyContent: "center",
     alignItems: "center",
-    borderWidth: "1px",
+    borderWidth: variant === "outlined" ? "1px" : 0,
     borderColor: "neutral.c40",
     width: `${size}px`,
     height: `${size}px`,
   }),
-)`
-  border-radius: ${(p) => `${p.theme.radii[1]}px`};
+)<{ variant: Props["variant"]; shape: Props["shape"] }>`
+  border-radius: ${(p) => (p.shape === "square" ? `${p.theme.radii[1]}px` : "9999px")};
+  background: ${(p) => (p.variant === "plain" ? p.theme.colors.primary.c10 : "transparent")};
 `;
 
 export default function IconBox({
   Icon,
   color,
+  variant = DEFAULT_BOX_VARIANT,
+  shape = DEFAULT_BOX_SHAPE,
   boxSize = DEFAULT_BOX_SIZE,
   iconSize = DEFAULT_ICON_SIZE,
   iconContainerProps,
 }: Props): React.ReactElement {
   const { colors } = useTheme();
   return (
-    <IconContainer size={boxSize} {...iconContainerProps}>
+    <IconContainer size={boxSize} variant={variant} shape={shape} {...iconContainerProps}>
       {React.isValidElement(Icon) ? (
         Icon
       ) : (
-        <Icon size={iconSize} color={color || colors.neutral.c100} />
+        <Icon
+          size={iconSize}
+          color={color || (variant === "plain" ? colors.primary.c70 : colors.neutral.c100)}
+        />
       )}
     </IconContainer>
   );
