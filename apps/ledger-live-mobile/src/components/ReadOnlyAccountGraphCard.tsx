44e690ca2257353af009fbbd04f0d2bd9171fd1b
Jules Grenier <jules.grenier-ext@ledger.fr>
2022-05-27 11:47:18 +0200

    LIVE-1982 - Account graph card discreet mode

--- apps/ledger-live-mobile/src/components/ReadOnlyAccountGraphCard.tsx
+++ apps/ledger-live-mobile/src/components/ReadOnlyAccountGraphCard.tsx
@@ -1,5 +1,6 @@
 import React, { memo } from "react";
 import { useTheme } from "styled-components/native";
+import { useSelector } from "react-redux";
 import { Unit, CryptoCurrency } from "@ledgerhq/live-common/lib/types";
 import { ValueChange } from "@ledgerhq/live-common/lib/portfolio/v2/types";
 import { Flex, Text } from "@ledgerhq/native-ui";
@@ -7,6 +8,10 @@ import { Flex, Text } from "@ledgerhq/native-ui";
 import Delta from "./Delta";
 import getWindowDimensions from "../logic/getWindowDimensions";
 import Graph from "../icons/Graph";
+import { withDiscreetMode } from "../context/DiscreetModeContext";
+import DiscreetModeButton from "./DiscreetModeButton";
+import CurrencyUnitValue from "./CurrencyUnitValue";
+import { counterValueCurrencySelector } from "../reducers/settings";
 
 const { width } = getWindowDimensions();
 
@@ -19,13 +24,26 @@ const GraphCardHeader = ({
   cryptoCurrencyUnit,
   valueChange,
 }: HeaderTitleProps) => {
+  const counterValueCurrency = useSelector(counterValueCurrencySelector);
+  const counterCurrencyUnit = counterValueCurrency.units[0];
+
   return (
     <Flex flexDirection={"row"} px={6} justifyContent={"space-between"}>
       <Flex>
         <Flex flexDirection={"row"}>
-          <Text variant={"large"} fontWeight={"medium"} color={"neutral.c70"}>
-            {`0 ${cryptoCurrencyUnit.code}`}
+          <Text
+            variant={"large"}
+            fontWeight={"medium"}
+            color={"neutral.c70"}
+            mr={2}
+          >
+            <CurrencyUnitValue
+              unit={cryptoCurrencyUnit}
+              value={0}
+              joinFragmentsSeparator=" "
+            />
           </Text>
+          <DiscreetModeButton size={20} />
         </Flex>
         <Text
           fontFamily="Inter"
@@ -34,7 +52,11 @@ const GraphCardHeader = ({
           numberOfLines={1}
           adjustsFontSizeToFit
         >
-          $0.00
+          <CurrencyUnitValue
+            unit={counterCurrencyUnit}
+            value={0}
+            joinFragmentsSeparator=" "
+          />
         </Text>
         <Flex flexDirection="row" alignItems="center">
           <Delta percent valueChange={valueChange} />
@@ -76,4 +98,4 @@ function ReadOnlyAccountGraphCard({ currency, valueChange }: Props) {
   );
 }
 
-export default memo(ReadOnlyAccountGraphCard);
+export default memo(withDiscreetMode(ReadOnlyAccountGraphCard));
