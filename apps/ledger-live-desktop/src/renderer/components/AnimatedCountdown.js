ab65231821a70fc7fcc4804c5e310ff1a1502986
Nabil Bourenane <nabil.bourenane@icloud.com>
2022-05-30 01:07:49 +0200

    removed conditions when playwright run

--- apps/ledger-live-desktop/src/renderer/components/AnimatedCountdown.js
+++ apps/ledger-live-desktop/src/renderer/components/AnimatedCountdown.js
@@ -26,15 +26,13 @@ const AnimatedCountdown = ({
   return (
     <WrappedSvg bgColor={bgColor} width={size} height={size} viewBox={"0 0 32 32"}>
       <circle r={16} cx={16} cy={16} strokeWidth={32} fill={fillColor} stroke={bgColor}>
-        {process.env.PLAYWRIGHT_RUN ? null : (
-          <animate
-            attributeName="stroke-dasharray"
-            from="0 100"
-            to="100 100"
-            repeatCount="1"
-            dur={`${duration}ms`}
-          />
-        )}
+        <animate
+          attributeName="stroke-dasharray"
+          from="0 100"
+          to="100 100"
+          repeatCount="1"
+          dur={`${duration}ms`}
+        />
       </circle>
       <circle r={16} cx={16} cy={16} strokeWidth={8} fill="none" stroke={bgColor} />
     </WrappedSvg>
