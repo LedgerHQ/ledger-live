a522317c98e83f0fc68e68930b0296249f114c83
nparigi-ledger <nicolas.parigi@ledger.fr>
2022-05-30 18:49:12 +0200

    LIVE-1813 - Refactor ReadOnlyAccountRow and AccountRow with a common layout component

--- apps/ledger-live-mobile/src/screens/Accounts/ReadOnly/ReadOnlyAccountRow.tsx
+++ apps/ledger-live-mobile/src/screens/Accounts/ReadOnly/ReadOnlyAccountRow.tsx
@@ -1,29 +1,16 @@
-import React, { useCallback, useMemo } from "react";
-import { TouchableOpacity } from "react-native";
-import { getCurrencyColor } from "@ledgerhq/live-common/lib/currencies";
-import { Flex, Text, ProgressLoader } from "@ledgerhq/native-ui";
-import { Currency } from "@ledgerhq/live-common/lib/types";
-import { useTheme } from "styled-components/native";
-import CurrencyUnitValue from "../../../components/CurrencyUnitValue";
-import CounterValue from "../../../components/CounterValue";
-import { ensureContrast } from "../../../colors";
+import React, { useCallback } from "react";
+import { CryptoCurrency } from "@ledgerhq/live-common/lib/types";
 import { NavigatorName, ScreenName } from "../../../const";
-import ParentCurrencyIcon from "../../../components/ParentCurrencyIcon";
+import AccountRowLayout from "../../../components/AccountRowLayout";
+import { BigNumber } from "bignumber.js";
 
 type Props = {
-  currency: Currency;
+  currency: CryptoCurrency;
   navigation: any;
 };
 
 const ReadOnlyAccountRow = ({ navigation, currency }: Props) => {
-  const { colors } = useTheme();
-
   const { name, units, id, type } = currency;
-
-  const color = useMemo(
-    () => ensureContrast(getCurrencyColor(currency), colors.constant.white),
-    [colors, currency],
-  );
 
   const onAccountPress = useCallback(() => {
     navigation.navigate(NavigatorName.Portfolio, {
@@ -39,57 +26,14 @@ const ReadOnlyAccountRow = ({ navigation, currency }: Props) => {
   }, [navigation, id, type]);
 
   return (
-    <TouchableOpacity onPress={onAccountPress}>
-      <Flex flexDirection="row" pt={6} pb={6}>
-        <Flex pr={4}>
-          <ProgressLoader
-            strokeWidth={2}
-            mainColor={color}
-            secondaryColor={colors.neutral.c40}
-            progress={0}
-            radius={22}
-          >
-            <ParentCurrencyIcon currency={currency} size={32} />
-          </ProgressLoader>
-        </Flex>
-        <Flex flex={1} justifyContent="center">
-          <Flex mb={1} flexDirection="row" justifyContent="space-between">
-            <Flex
-              flexGrow={1}
-              flexShrink={1}
-              flexDirection="row"
-              alignItems="center"
-            >
-              <Flex flexShrink={1}>
-                <Text
-                  variant="large"
-                  fontWeight="semiBold"
-                  color="neutral.c100"
-                  numberOfLines={1}
-                  flexShrink={1}
-                >
-                  {name}
-                </Text>
-              </Flex>
-            </Flex>
-            <Flex flexDirection="row" alignItems="flex-end" flexShrink={0}>
-              <Text variant="large" fontWeight="semiBold" color="neutral.c100">
-                <CounterValue
-                  currency={currency}
-                  value={0}
-                  joinFragmentsSeparator=""
-                />
-              </Text>
-            </Flex>
-          </Flex>
-          <Flex flexDirection="row" justifyContent="space-between">
-            <Text variant="body" fontWeight="medium" color="neutral.c70">
-              <CurrencyUnitValue showCode unit={units[0]} value={0} />
-            </Text>
-          </Flex>
-        </Flex>
-      </Flex>
-    </TouchableOpacity>
+    <AccountRowLayout
+      onPress={onAccountPress}
+      currency={currency}
+      currencyUnit={units[0]}
+      balance={new BigNumber(0)}
+      name={name}
+      progress={0}
+    />
   );
 };
 
