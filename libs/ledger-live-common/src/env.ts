f6bb45eb12a2b399c79df10a969fec3286eb64fb
Pavan Vora <pavanvora.cs@gmail.com>
2022-05-31 20:24:38 +0530

    fix cardano api endpoint

--- libs/ledger-live-common/src/env.ts
+++ libs/ledger-live-common/src/env.ts
@@ -161,12 +161,12 @@ const envDefinitions = {
     desc: "bot's default timeout for scanAccounts",
   },
   CARDANO_API_ENDPOINT: {
-    def: "https://cardano.coin.ledger.com",
+    def: "https://cardano.coin.ledger.com/api",
     parser: stringParser,
     desc: "Cardano API url",
   },
   CARDANO_TESTNET_API_ENDPOINT: {
-    def: "https://apitestnet.typhonwallet.io/api/ledger",
+    def: "https://testnet-ledger.cardanoscan.io/api",
     parser: stringParser,
     desc: "Cardano API url",
   },
