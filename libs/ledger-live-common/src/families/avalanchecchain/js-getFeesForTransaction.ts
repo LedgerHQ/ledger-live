141eca0a86c0762528cb81845e7f8ebb3c9472f8
Trent Rogers <trentkrogers@gmail.com>
2022-06-06 12:31:39 -0500

    Fees WIP

--- libs/ledger-live-common/src/families/avalanchecchain/js-getFeesForTransaction.ts
+++ libs/ledger-live-common/src/families/avalanchecchain/js-getFeesForTransaction.ts
@@ -1,28 +1,28 @@
 import { avalancheClient } from "./api/client";
 import { BigNumber } from "bignumber.js";
 
-export const DEFAULT_GAS_LIMIT = new BigNumber(21000);
+export const DEFAULT_GAS_LIMIT = 21000;
 
 const getFeesForTransaction = async (): Promise<BigNumber> => {
     const gasPrice = await getGasPrice();
 
-    return DEFAULT_GAS_LIMIT.multipliedBy(gasPrice);
+    return new BigNumber(DEFAULT_GAS_LIMIT).multipliedBy(gasPrice);
 }
 
 export const getGasPrice = async () => {
     const cChain = avalancheClient().CChain();
 
     const baseFee = await cChain.getBaseFee();
+    const adjustedBaseFee = getAdjustedBaseFee(baseFee);
     const priorityFee = await cChain.getMaxPriorityFeePerGas();
 
-    const adjustedBaseFee = getAdjustedBaseFee(baseFee);
     return new BigNumber(adjustedBaseFee).plus(priorityFee);
 }
 
 /**
- * In accordance with the Avalance wallet, return the gas price + 25%
+ * In accordance with the Avalance wallet, adjust the gas price by +25%
  * @param baseFee 
  */
-const getAdjustedBaseFee = baseFee => new BigNumber(baseFee).multipliedBy(1.25);
+const getAdjustedBaseFee = (baseFee: string) => new BigNumber(baseFee).multipliedBy(1.25);
 
 export default getFeesForTransaction;
