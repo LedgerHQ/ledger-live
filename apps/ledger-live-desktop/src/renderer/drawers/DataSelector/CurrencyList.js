5d1d044c4d354fd2fcff795c82489be265a2b511
Hugo Morosini <hugo.morosini@gmail.com>
2022-06-08 17:17:24 +0100

    fixed currency list drawer height calculation

--- apps/ledger-live-desktop/src/renderer/drawers/DataSelector/CurrencyList.js
+++ apps/ledger-live-desktop/src/renderer/drawers/DataSelector/CurrencyList.js
@@ -15,6 +15,11 @@ type Props = {
   onCurrencySelect: (currency: Currency) => void,
 };
 
+const CurrencyListContainer = styled.div`
+  flex: 1 1 auto;
+  width: 100%;
+`;
+
 const CustomListContainer = styled.div`
   &::-webkit-scrollbar {
     display: none;
@@ -26,62 +31,64 @@ outerElementType.displayName = "outerElementType";
 
 export function CurrencyList({ currencies, onCurrencySelect }: Props) {
   return (
-    <AutoSizer style={{ flex: "1 1 auto", width: "100%" }}>
-      {({ height }: { height: number }) => (
-        <List
-          outerElementType={outerElementType}
-          height={height}
-          itemCount={currencies.length}
-          itemSize={52}
-          width="100%"
-        >
-          {({ index, style }: any) => {
-            const currency = currencies[index];
-            return (
-              <RowContainer key={index} style={style} onClick={() => onCurrencySelect(currency)}>
-                <RowInnerContainer>
-                  <Box
-                    horizontal
-                    alignItems="center"
-                    style={{
-                      flexShrink: 1,
-                    }}
-                  >
-                    <CryptoCurrencyIcon circle currency={currency} size={24} />
-                    <Text
-                      ff="Inter|SemiBold"
-                      color="inherit"
-                      fontSize="13px"
+    <CurrencyListContainer>
+      <AutoSizer style={{ height: "100%", width: "100%" }}>
+        {({ height }: { height: number }) => (
+          <List
+            outerElementType={outerElementType}
+            height={height}
+            itemCount={currencies.length}
+            itemSize={52}
+            width="100%"
+          >
+            {({ index, style }: any) => {
+              const currency = currencies[index];
+              return (
+                <RowContainer key={index} style={style} onClick={() => onCurrencySelect(currency)}>
+                  <RowInnerContainer>
+                    <Box
+                      horizontal
+                      alignItems="center"
                       style={{
-                        marginLeft: 12,
-                        textOverflow: "ellipsis",
                         flexShrink: 1,
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
                       }}
                     >
-                      {currency.name}
-                    </Text>
-                    <Text
-                      ff="Inter|Medium"
-                      color="palette.text.shade50"
-                      fontSize="13px"
-                      style={{ marginLeft: 4, textTransform: "uppercase" }}
-                    >
-                      {currency.ticker}
-                    </Text>
-                  </Box>
-                  {currency.parentCurrency ? (
-                    <Box horizontal alignItems="center" marginLeft="12px">
-                      <CurrencyLabel>{currency.parentCurrency.name}</CurrencyLabel>
+                      <CryptoCurrencyIcon circle currency={currency} size={24} />
+                      <Text
+                        ff="Inter|SemiBold"
+                        color="inherit"
+                        fontSize="13px"
+                        style={{
+                          marginLeft: 12,
+                          textOverflow: "ellipsis",
+                          flexShrink: 1,
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                        }}
+                      >
+                        {currency.name}
+                      </Text>
+                      <Text
+                        ff="Inter|Medium"
+                        color="palette.text.shade50"
+                        fontSize="13px"
+                        style={{ marginLeft: 4, textTransform: "uppercase" }}
+                      >
+                        {currency.ticker}
+                      </Text>
                     </Box>
-                  ) : null}
-                </RowInnerContainer>
-              </RowContainer>
-            );
-          }}
-        </List>
-      )}
-    </AutoSizer>
+                    {currency.parentCurrency ? (
+                      <Box horizontal alignItems="center" marginLeft="12px">
+                        <CurrencyLabel>{currency.parentCurrency.name}</CurrencyLabel>
+                      </Box>
+                    ) : null}
+                  </RowInnerContainer>
+                </RowContainer>
+              );
+            }}
+          </List>
+        )}
+      </AutoSizer>
+    </CurrencyListContainer>
   );
 }
