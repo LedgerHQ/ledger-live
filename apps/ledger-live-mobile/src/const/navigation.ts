63951a11d23b6b6ffb16934ce29ee86b63d76b5d
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-06-13 17:12:45 +0200

    feat(llm): navigator and main screen to new synchronous onboarding flow

--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -476,6 +476,7 @@ export const NavigatorName = {
   MigrateAccountsFlow: "MigrateAccountsFlow",
   NftNavigator: "NftNavigator",
   Onboarding: "Onboarding",
+  SyncOnboarding: "SyncOnboarding",
   PasswordAddFlow: "PasswordAddFlow",
   PasswordModifyFlow: "PasswordModifyFlow",
   Platform: "Platform",
