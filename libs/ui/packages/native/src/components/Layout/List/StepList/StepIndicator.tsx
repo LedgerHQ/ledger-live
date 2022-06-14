0ec02b3a0abd1f972d0186e8c2f1295742bc4fb5
Thomas Roger Lux <thomas@rogerlux.dev>
2022-06-14 18:00:55 +0200

    refactor(native-ui): add animations to StepList component

--- libs/ui/packages/native/src/components/Layout/List/StepList/StepIndicator.tsx
+++ libs/ui/packages/native/src/components/Layout/List/StepList/StepIndicator.tsx
@@ -6,13 +6,9 @@ import Flex, { Props as FlexProps } from "../../Flex";
 import { ItemStatus } from ".";
 
 const TopSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
-  /* For seemless continuity between the dashed lines we need a 3px margin,
-  but for solid lines we don't want margins. */
-  height: ${(p) => (p.status === "inactive" ? 17 : 20)}px;
-  margin-top: ${(p) => (p.status === "inactive" ? 3 : 0)}px;
-
+  height: 20px;
   border-width: ${(p) => (p.hidden ? 0 : 1)}px;
-  border-style: ${(p) => (p.status === "inactive" ? "dashed" : "solid")};
+  border-style: solid;
   border-color: ${(p) =>
     p.status === "inactive" ? p.theme.colors.neutral.c40 : p.theme.colors.primary.c80};
 `;
@@ -20,7 +16,7 @@ const TopSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
 const BottomSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
   flex: 1;
   border-width: ${(p) => (p.hidden ? 0 : 1)}px;
-  border-style: ${(p) => (p.status === "completed" ? "solid" : "dashed")};
+  border-style: solid;
   border-color: ${(p) =>
     p.status === "completed" ? p.theme.colors.primary.c80 : p.theme.colors.neutral.c40};
 `;
