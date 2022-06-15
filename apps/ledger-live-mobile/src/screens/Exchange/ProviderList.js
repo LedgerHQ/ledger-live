e9ca61c33c3ee238d0116d5945952c07775ef8ce
Kant <quentin.jaccarino@ledger.fr>
2022-06-15 15:34:26 +0200

    fix(multibuy): remove fiat filter

--- apps/ledger-live-mobile/src/screens/Exchange/ProviderList.js
+++ apps/ledger-live-mobile/src/screens/Exchange/ProviderList.js
@@ -143,9 +143,6 @@ export default function ProviderList({ route }: Props) {
 
   const filteredProviders = filterRampCatalogEntries(rampCatalog.value[type], {
     cryptoCurrencies: currency.id ? [currency.id] : undefined,
-    fiatCurrencies: fiatCurrency.ticker
-      ? [fiatCurrency.ticker.toLowerCase()]
-      : undefined,
   });
 
   const onProviderClick = useCallback(
