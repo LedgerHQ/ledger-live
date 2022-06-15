e8610a7299c2db965a467d5d2b52aa517c146c4e
Thomas Roger Lux <thomas@rogerlux.dev>
2022-06-15 18:01:56 +0200

    fix(mobile):update steps status computation

--- apps/ledger-live-mobile/src/screens/SyncOnboarding/index.tsx
+++ apps/ledger-live-mobile/src/screens/SyncOnboarding/index.tsx
@@ -1,58 +1,66 @@
-import React from "react";
-import type { StackScreenProps } from "@react-navigation/stack";
+import React, { useEffect, useState, useCallback, ReactNode } from "react";
 import { Button, Flex, StepList, Text } from "@ledgerhq/native-ui";
-import type { Device } from "@ledgerhq/live-common/lib/hw/actions/types";
+import { StackScreenProps } from "@react-navigation/stack";
+import { Device } from "@ledgerhq/live-common/lib/hw/actions/types";
 import { useOnboardingStatePolling } from "@ledgerhq/live-common/src/onboarding/hooks/useOnboardingStatePolling";
 import { CloseMedium } from "@ledgerhq/native-ui/assets/icons";
 import { OnboardingStep } from "@ledgerhq/live-common/src/hw/extractOnboardingState";
 import { useTheme } from "styled-components/native";
 
 import { ScreenName } from "../../const";
-import type { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
-import { useOnboardingStatePolling } from "@ledgerhq/live-common/lib/onboarding/hooks/useOnboardingStatePolling";
-import type { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
+import { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
 
-const pollingPeriodMs = 1000;
+type Step = {
+  status: "completed" | "active" | "inactive";
+  deviceStates: OnboardingStep[];
+  title: string;
+  renderBody?: () => ReactNode;
+};
 
-const defaultOnboardingSteps = [
-    {
-      status: "inactive",
-      deviceStates: [OnboardingStep.WelcomeScreen, OnboardingStep.SetupChoice],
-      title: "Nano paired",
-    },
-    {
-      status: "inactive",
-      deviceStates: [OnboardingStep.Pin],
-      title: "Set your PIN",
-      renderBody: () => (
-        <Text>
-          {`Your PIN can be 4 to 8 digits long. Anyone with access to your Nano and to your PIN can also access all your crypto and NFT assets.`}
-        </Text>
-      ),
-    },
-    {
-      status: "inactive",
-      deviceStates: [OnboardingStep.NewDevice, OnboardingStep.NewDeviceConfirming, OnboardingStep.RestoreSeed, OnboardingStep.SafetyWarning],
-      title: "Recovery phrase",
-      renderBody: () => (
-        <Text>
-          {`Your recovery phrase is a secret list of 24 words that backs up your private keys. Your Nano generates a unique recovery phrase. Ledger does not keep a copy of it.`}
-        </Text>
-      ),
-    },
-    {
-      status: "inactive",
-      deviceStates: [],
-      title: "Software check",
-      renderBody: () => (
-        <Text>{`We'll verify whether your Nano is genuine. This should be quick and easy!`}</Text>
-      ),
-    },
-    {
-      status: "inactive",
-      deviceStates: [OnboardingStep.Ready],
-      title: "Nano is ready",
-    },
+const defaultOnboardingSteps: Step[] = [
+  {
+    status: "inactive",
+    deviceStates: [OnboardingStep.WelcomeScreen, OnboardingStep.SetupChoice],
+    title: "Nano paired",
+  },
+  {
+    status: "inactive",
+    deviceStates: [OnboardingStep.Pin],
+    title: "Set your PIN",
+    renderBody: () => (
+      <Text>
+        {`Your PIN can be 4 to 8 digits long. Anyone with access to your Nano and to your PIN can also access all your crypto and NFT assets.`}
+      </Text>
+    ),
+  },
+  {
+    status: "inactive",
+    deviceStates: [
+      OnboardingStep.NewDevice,
+      OnboardingStep.NewDeviceConfirming,
+      OnboardingStep.RestoreSeed,
+      OnboardingStep.SafetyWarning,
+    ],
+    title: "Recovery phrase",
+    renderBody: () => (
+      <Text>
+        {`Your recovery phrase is a secret list of 24 words that backs up your private keys. Your Nano generates a unique recovery phrase. Ledger does not keep a copy of it.`}
+      </Text>
+    ),
+  },
+  {
+    status: "inactive",
+    deviceStates: [],
+    title: "Software check",
+    renderBody: () => (
+      <Text>{`We'll verify whether your Nano is genuine. This should be quick and easy!`}</Text>
+    ),
+  },
+  {
+    status: "inactive",
+    deviceStates: [OnboardingStep.Ready],
+    title: "Nano is ready",
+  },
 ];
 
 type Props = StackScreenProps<
@@ -65,11 +73,17 @@ const pollingPeriodMs = 1000;
 export const SyncOnboarding = ({ navigation, route }: Props) => {
   const { colors } = useTheme();
   const [device, setDevice] = useState<Device | null>(null);
-  const [onboardingSteps, setOnboardingSteps] = useState(defaultOnboardingSteps);
+  const [onboardingSteps, setOnboardingSteps] = useState(
+    defaultOnboardingSteps,
+  );
 
-  const { pairedDevice } = route.params; 
+  const { pairedDevice } = route.params;
 
-  const { onboardingState, allowedError, fatalError } = useOnboardingStatePolling({ device, pollingPeriodMs });
+  const {
+    onboardingState,
+    allowedError,
+    fatalError,
+  } = useOnboardingStatePolling({ device, pollingPeriodMs });
 
   // Triggers the pairing if no pairedDevice was given
   useEffect(() => {
@@ -82,24 +96,32 @@ export const SyncOnboarding = ({ navigation, route }: Props) => {
     }
   }, [navigation, pairedDevice]);
 
-  console.log("Allowed Error:", allowedError);
-
-  console.log("Fatal Error:", fatalError)
-
   useEffect(() => {
-    console.log(onboardingState)
-    setOnboardingSteps(defaultOnboardingSteps.map(step => {
-      if (onboardingState && onboardingState.currentOnboardingStep && step.deviceStates.includes(onboardingState.currentOnboardingStep)) {
-        return {
-          ...step,
-          status: "active"
-        }
-      }
-      return step;
-    }));
+    const newStepState: Step[] = [];
 
+    defaultOnboardingSteps.some((step, index, steps) => {
+      if (
+        onboardingState &&
+        onboardingState.currentOnboardingStep &&
+        step.deviceStates.includes(onboardingState.currentOnboardingStep)
+      ) {
+        newStepState.push({
+          ...step,
+          status: "active",
+        });
+        newStepState.push(...steps.slice(index + 1));
+        return true;
+      }
+
+      newStepState.push({
+        ...step,
+        status: "completed",
+      });
+      return false;
+    });
+
+    setOnboardingSteps(newStepState);
   }, [onboardingState]);
- 
 
   const handleOnPaired = useCallback((pairedDevice: Device) => {
     setDevice(pairedDevice);
@@ -114,16 +136,18 @@ export const SyncOnboarding = ({ navigation, route }: Props) => {
   }, [pairedDevice, handleOnPaired]);
 
   return (
-    <Flex
-      px={7}
-      py={8}
-    >
+    <Flex px={7} py={8}>
       <Flex flexDirection="row" justifyContent="space-between" mt={6} mb={8}>
-        <Button type="main" outline>EN</Button>
+        <Button type="main" outline>
+          EN
+        </Button>
         <Button Icon={CloseMedium} />
       </Flex>
       <Text variant="h4">Setup Manual</Text>
-      <Text mb={8} variant="body">Continue setting up on your Nano. Check back here for tips and information.</Text>
+      <Text mb={8} variant="body">
+        Continue setting up on your Nano. Check back here for tips and
+        information.
+      </Text>
       <StepList items={onboardingSteps} />
     </Flex>
   );
