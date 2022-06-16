81efd89ea18a8018a6f063ba34b7fc8e9dda8d9e
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-06-16 16:09:45 -0400

    remove default value for MOCK_SWAP_CHECK_QUOTE env
    If there is a default value, we can't conditionally check that that the env is set
    or not because it will always be set to the default value

--- libs/ledger-live-common/src/env.ts
+++ libs/ledger-live-common/src/env.ts
@@ -386,7 +386,7 @@ const envDefinitions = {
     desc: "mock the server response for the exchange KYC check, options are 'open', 'pending', 'closed' or 'approved'.",
   },
   MOCK_SWAP_CHECK_QUOTE: {
-    def: "RATE_VALID",
+    def: "",
     parser: stringParser,
     desc: "mock the server response for the exchange check quote, options are 'RATE_VALID', 'KYC_FAILED', 'KYC_PENDING', 'KYC_UNDEFINED', 'KYC_UPGRADE_REQUIRED', 'MFA_REQUIRED', 'OVER_TRADE_LIMIT', 'UNKNOW_USER' or 'UNKNOWN_ERROR'.",
   },
