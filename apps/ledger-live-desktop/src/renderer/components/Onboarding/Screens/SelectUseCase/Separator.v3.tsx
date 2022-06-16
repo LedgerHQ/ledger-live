2b3ba709aef5471bceb5250e800b7f85582b2290
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-06-16 16:51:43 +0200

    support: Migrate V3 Onboarding

---
+++ apps/ledger-live-desktop/src/renderer/components/Onboarding/Screens/SelectUseCase/Separator.v3.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const SeparatorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 40px 0px;
+  width: 100%;
+  align-items: center;
+`;
+
+const SeparatorLine = styled.div`
+  background-color: ${p => p.theme.colors.palette.neutral.c40};
+  height: 1px;
+  flex: 1;
+`;
+
+interface SeparatorProps {
+  label: string;
+}
+
+export function Separator({ label }: SeparatorProps) {
+  return (
+    <SeparatorContainer>
+      <SeparatorLine />
+    </SeparatorContainer>
+  );
+}
