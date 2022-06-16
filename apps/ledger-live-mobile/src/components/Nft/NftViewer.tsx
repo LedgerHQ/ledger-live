dcc1fe2ab30dead706835384370fec7a5c60828c
laure-lebon <laure.lebon-ext@ledger.fr>
2022-06-16 15:30:55 +0200

    LIVE-1035 and LIVE-1036 NFT counter value refacto

--- apps/ledger-live-mobile/src/components/Nft/NftViewer.tsx
+++ apps/ledger-live-mobile/src/components/Nft/NftViewer.tsx
@@ -12,16 +12,16 @@ import {
   decodeNftId,
   getNftCapabilities,
   useNftCollectionMetadata,
+  getFloorPrice
 } from "@ledgerhq/live-common/lib/nft";
 import { BigNumber } from "bignumber.js";
 import { useSelector } from "react-redux";
 import { Button, Icons } from "@ledgerhq/native-ui";
 import { useTranslation, Trans } from "react-i18next";
 import Clipboard from "@react-native-community/clipboard";
-import { ProtoNFT, Currency } from "@ledgerhq/live-common/lib/types";
+import { ProtoNFT, FloorPrice } from "@ledgerhq/live-common/lib/types";
 import { FeatureToggle } from "@ledgerhq/live-common/lib/featureFlags";
 import { getCryptoCurrencyById } from "@ledgerhq/live-common/lib/currencies";
-import network from "@ledgerhq/live-common/lib/network";
 import { useNavigation, useTheme } from "@react-navigation/native";
 import { getAccountBridge } from "@ledgerhq/live-common/lib/bridge";
 import { accountSelector } from "../../reducers/accounts";
@@ -43,20 +43,6 @@ type RouteParams = {
 };
 
 type TimeoutReturn = ReturnType<typeof setTimeout>;
-
-const FLOOR_PRICE_CURRENCIES = new Set(["ethereum"]);
-const getFloorPrice = async (nft: ProtoNFT, currency: Currency): Promise<any> => {
-  if (!FLOOR_PRICE_CURRENCIES.has(nft.currencyId)) {
-    return null;
-  }
-
-  const { data } = await network({
-    method: "GET",
-    url: `https://nft.api.live.ledger.com/v1/marketdata/${nft.currencyId}/${currency.ethereumLikeInfo.chainId}/contract/${nft.contract}/floor-price`,
-  });
-
-  return data;
-};
 
 const Section = ({
   title,
@@ -150,9 +136,9 @@ const NftViewer = ({ route }: Props) => {
 
   useEffect(() => {
     setFloorPriceLoading(true);
-    getFloorPrice(nft, currency)
+    getFloorPrice(nft, currency?.ethereumLikeInfo.chainId)
       .then(
-        result => {
+        (result: FloorPrice) => {
           if (result) {
             setTicker(result.ticker);
             setFloorPrice(result.value);
