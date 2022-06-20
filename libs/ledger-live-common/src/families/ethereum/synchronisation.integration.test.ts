180e73eb72b8f4fc6419b16000ee2bc3f40fe52a
lambertkevin <kevin.lambert@ledger.fr>
2022-06-20 22:49:15 +0200

    Update reorg integration test to ensure bug is fixed

--- libs/ledger-live-common/src/families/ethereum/synchronisation.integration.test.ts
+++ libs/ledger-live-common/src/families/ethereum/synchronisation.integration.test.ts
@@ -2,7 +2,7 @@ import "../../__tests__/test-helpers/setup";
 import { reduce } from "rxjs/operators";
 import { fromAccountRaw } from "../../account";
 import { getAccountCurrency } from "../../account/helpers";
-import type { Account, SubAccount } from "../../types";
+import type { Account, OperationRaw, SubAccount } from "../../types";
 import { getAccountBridge } from "../../bridge";
 import { makeBridgeCacheSystem } from "../../bridge/cache";
 import { ethereum1 } from "./datasets/ethereum1";
@@ -95,6 +95,7 @@ describe("sync on reorg", () => {
     const opHash = "NotImportant";
     const accountId = ethereum2.id;
     const opType = "OUT";
+    const opId = encodeOperationId(accountId, opHash, opType);
     const blockHeight = 10042069;
     const currency = getCryptoCurrencyById(ethereum2.currencyId);
     const syncHash =
@@ -104,40 +105,43 @@ describe("sync on reorg", () => {
         withDelisted: true,
       }).length;
 
+    const operation: OperationRaw = {
+      id: opId,
+      hash: opHash,
+      type: opType,
+      value: "1",
+      fee: "string",
+      senders: ["0x789d2f10826BF8f3a56Ec524359bBA4e738Af5bF"],
+      recipients: ["0xdA9EDcC3CF66bc18050dB55D376407Cf85e0617B"],
+      blockHeight: blockHeight - 81, // 80 is defined today as the threshold number of confirmation to be safe for a reorg. @see SAFE_REORG_THRESHOLD
+      blockHash:
+        "0x00000000000000000000000000000000000Th1sH4shSh0u1dN0t3x1sTOnCh4iN",
+      transactionSequenceNumber: 1,
+      date: "May-11-2020 01:50:10 UTC",
+      extra: {},
+      accountId,
+    };
+
     const account = fromAccountRaw({
       ...ethereum2,
       blockHeight,
       syncHash,
-      operations: [
-        {
-          id: encodeOperationId(accountId, opHash, opType),
-          hash: opHash,
-          type: opType,
-          value: "1",
-          fee: "string",
-          senders: ["0x0E3F0bb9516F01f2C34c25E0957518b8aC9414c5"],
-          recipients: ["0xdA9EDcC3CF66bc18050dB55D376407Cf85e0617B"],
-          blockHeight: blockHeight - 81, // 80 is defined today as the threshold number of confirmation to be safe for a reorg. @see SAFE_REORG_THRESHOLD
-          blockHash:
-            "0x00000000000000000000000000000000000Th1sH4shSh0u1dN0t3x1sTOnCh4iN",
-          transactionSequenceNumber: 1,
-          date: new Date(Date.now()).toISOString(),
-          extra: {},
-          accountId,
-        },
-      ],
+      operations: [operation],
       operationsCount: 1,
     });
     const bridge = getAccountBridge(account);
-
-    await expect(
-      bridge
-        .sync(account, {
-          paginationConfig: {},
-          blacklistedTokenIds: [],
-        })
-        .pipe(reduce((a, f: (arg0: Account) => Account) => f(a), account))
-        .toPromise()
-    ).rejects.toThrow();
+    const syncPromise = await bridge
+      .sync(account, {
+        paginationConfig: {},
+        blacklistedTokenIds: [],
+      })
+      .pipe(reduce((a, f: (arg0: Account) => Account) => f(a), account))
+      .toPromise();
+    // expect not to throw ok if you reach this point
+    // + expect operations to be from full sync instead of incremental
+    // so the fake op should be removed
+    expect(syncPromise.operations.map((o) => o.id)).not.toContain(opId);
+    // To this date (June 20th 2022) this account has 160 ops, we should at least get them all
+    expect(syncPromise.operations.length >= 160).toBe(true);
   });
 });
