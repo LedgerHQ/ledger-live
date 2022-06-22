a143b058be316b52f96cef3dd2c9c058ca4c5606
ThomasLaforge <thomas.laforge.38@gmail.com>
2022-06-22 11:06:47 +0200

    select crypto view

--- apps/ledger-live-mobile/src/screens/SelectCrypto.tsx
+++ apps/ledger-live-mobile/src/screens/SelectCrypto.tsx
@@ -1,6 +1,21 @@
-import React, { useCallback } from "react";
-import { Flex, Text, Button } from "@ledgerhq/native-ui";
+import React, { useCallback, useMemo } from "react";
+import { Text } from "@ledgerhq/native-ui";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+
+import { Trans } from "react-i18next";
+import { CryptoCurrency, AccountLike } from "@ledgerhq/live-common/lib/types";
+import {
+  useCurrenciesByMarketcap,
+  listCryptoCurrencies,
+} from "@ledgerhq/live-common/lib/currencies";
+
+import { useTheme } from "@react-navigation/native";
 import { ScreenName } from "../const";
+import LText from "../components/LText";
+import CurrencyRow from "../components/CurrencyRow";
+import { TrackScreen } from "../analytics";
+import KeyboardView from "../components/KeyboardView";
+import FilteredSearchBar from "../components/FilteredSearchBar";
 
 type Props = {
   navigation: any;
@@ -9,17 +24,89 @@ type Props = {
   };
 };
 
+const SEARCH_KEYS = ["name", "ticker"];
+
+type RouteParams = {
+  currencies: string[];
+  allowAddAccount?: boolean;
+  accounts: AccountLike[];
+};
+
+const keyExtractor = (currency: CryptoCurrency) => currency.id;
+
+const renderEmptyList = () => (
+  <View style={styles.emptySearch}>
+    <LText style={styles.emptySearchText}>
+      <Trans i18nKey="common.noCryptoFound" />
+    </LText>
+  </View>
+);
+
 export default function SelectCrypto({ navigation, route }: Props) {
-  const goToNext = useCallback(() => {
-    navigation.navigate(ScreenName.ReceiveSelectAccount, {
-      ...route.params,
-    });
-  }, [navigation, route]);
+  const { colors } = useTheme();
+  const cryptoCurrencies = useMemo(() => listCryptoCurrencies(), []);
+  const sortedCryptoCurrencies = useCurrenciesByMarketcap(cryptoCurrencies);
+
+  const onPressCurrency = useCallback(
+    (selectedCurrency: CryptoCurrency) => {
+      navigation.navigate(ScreenName.ReceiveSelectAccount, {
+        ...route.params,
+        selectedCurrency,
+      });
+    },
+    [navigation, route.params],
+  );
+
+  const renderList = (items: CryptoCurrency[]) => (
+    <FlatList
+      contentContainerStyle={styles.list}
+      data={items}
+      renderItem={({ item }) => (
+        <CurrencyRow currency={item} onPress={onPressCurrency} />
+      )}
+      keyExtractor={keyExtractor}
+      showsVerticalScrollIndicator={false}
+      keyboardDismissMode="on-drag"
+    />
+  );
 
   return (
-    <Flex flex={1} color="background.main">
+    <SafeAreaView style={[styles.root, { backgroundColor: colors.background }]}>
+      <TrackScreen category="ReceiveFlow" name="SelectCrypto" />
       <Text>Select Crypto</Text>
-      <Button onPress={goToNext}>Next</Button>
-    </Flex>
+      <KeyboardView style={{ flex: 1 }}>
+        <View style={styles.searchContainer}>
+          <FilteredSearchBar
+            keys={SEARCH_KEYS}
+            inputWrapperStyle={styles.filteredSearchInputWrapperStyle}
+            list={sortedCryptoCurrencies}
+            renderList={renderList}
+            renderEmptySearch={renderEmptyList}
+          />
+        </View>
+      </KeyboardView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  searchContainer: {
+    paddingTop: 16,
+    flex: 1,
+  },
+  list: {
+    paddingBottom: 32,
+  },
+  filteredSearchInputWrapperStyle: {
+    marginHorizontal: 16,
+  },
+  emptySearch: {
+    paddingHorizontal: 16,
+  },
+  emptySearchText: {
+    textAlign: "center",
+  },
+});
