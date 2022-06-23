0305b76283743f16d31e626ef26653c9b256d61c
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-06-23 12:56:01 -0400

    Add specific FTX case when KYC rejected

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/FormKYCBanner/index.js
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/FormKYCBanner/index.js
@@ -1,8 +1,10 @@
 // @flow
 import type { KYCStatus } from "@ledgerhq/live-common/lib/exchange/swap/utils";
-import { KYC_STATUS } from "@ledgerhq/live-common/lib/exchange/swap/utils";
+import { getProviderName, KYC_STATUS } from "@ledgerhq/live-common/lib/exchange/swap/utils";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { urls } from "~/config/urls";
+import { openURL } from "~/renderer/linking";
 import SectionInformative from "~/renderer/screens/exchange/Swap2/Form/FormSummary/SectionInformative";
 
 const FormKYCBanner = ({
@@ -20,30 +22,46 @@ const FormKYCBanner = ({
   // i.e: we don't render it if the KYC is "pending".
   if (!provider || (status && status === KYC_STATUS.pending)) return null;
 
-  const { message, cta } = (() => {
+  let onClickAction = onClick;
+
+  let { message, cta } = (() => {
     switch (status) {
       case KYC_STATUS.rejected:
         return {
-          message: "swap2.form.providers.kyc.rejected",
-          cta: "swap2.form.providers.kyc.update",
+          message: t("swap2.form.providers.kyc.rejected"),
+          cta: t("swap2.form.providers.kyc.update"),
         };
 
       case KYC_STATUS.upgradeRequired:
         return {
-          message: "swap2.form.providers.kyc.updateRequired",
-          cta: "swap2.form.providers.kyc.complete",
+          message: t("swap2.form.providers.kyc.updateRequired"),
+          cta: t("swap2.form.providers.kyc.complete"),
         };
 
       // If the status is _undefined_, this means the user has not yet completed KYC (for Wyre only)
       default:
         return {
-          message: "swap2.form.providers.kyc.required",
-          cta: "swap2.form.providers.kyc.complete",
+          message: t("swap2.form.providers.kyc.required"),
+          cta: t("swap2.form.providers.kyc.complete"),
         };
     }
   })();
 
-  return <SectionInformative message={t(message)} ctaLabel={t(cta)} onClick={onClick} />;
+  /**
+   * For FTX, the user needs to contact FTX support if his KYC has been rejected
+   * This is specific partner logic
+   */
+  if (["ftx", "ftxus"].includes(provider) && status === KYC_STATUS.rejected) {
+    const openProviderSupport = () => openURL(urls.swap.providers[provider]?.support);
+
+    message = t("swap2.form.providers.kyc.rejectedContactProviderSupport", {
+      providerName: getProviderName(provider),
+    });
+    cta = t("common.getSupport");
+    onClickAction = openProviderSupport;
+  }
+
+  return <SectionInformative message={message} ctaLabel={cta} onClick={onClickAction} />;
 };
 
 export default FormKYCBanner;
