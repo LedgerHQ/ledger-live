70b566d0d64e0241237117969519541bfbac9b49
Come Grellard <come.grellard@ledger.fr>
2022-06-23 17:17:59 +0200

    feat/LIVE-2451-receive-flow-receive-screen some text added to the toast on the receive screen

--- apps/ledger-live-mobile/src/screens/ReceiveFunds/ReceiveSecurityModal/index.tsx
+++ apps/ledger-live-mobile/src/screens/ReceiveFunds/ReceiveSecurityModal/index.tsx
@@ -37,7 +37,7 @@ const ReceiveSecurityModal = ({
 
   useEffect(() => {
     getShouldNotRemindUserAgain().then(shouldNotRemindUserAgain => {
-      if (!shouldNotRemindUserAgain || true) {
+      if (!shouldNotRemindUserAgain) {
         setTimeout(() => {
           setIsModalOpen(true);
         }, 800);
