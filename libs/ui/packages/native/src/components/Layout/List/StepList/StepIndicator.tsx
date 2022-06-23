c517ce6ba1470669e88cd850a1431e5f02222e66
Thomas Roger Lux <thomas@rogerlux.dev>
2022-06-23 14:47:35 +0200

    refactor(native-ui): pixel polish on vertical steppers

--- libs/ui/packages/native/src/components/Layout/List/StepList/StepIndicator.tsx
+++ libs/ui/packages/native/src/components/Layout/List/StepList/StepIndicator.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CheckAloneMedium } from "@ledgerhq/icons-ui/native";
+import { CircledCheckSolidMedium } from "@ledgerhq/icons-ui/native";
 import styled, { useTheme } from "styled-components/native";
 
 import Flex, { Props as FlexProps } from "../../Flex";
@@ -7,10 +7,13 @@ import { ItemStatus } from ".";
 import { Theme } from "src/styles/theme";
 
 const TopSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
+  /* width: 4px; */
   height: 20px;
   border-width: ${(p) => (p.hidden ? 0 : 1)}px;
   border-style: solid;
   border-color: ${(p) =>
+    p.status === "inactive" ? p.theme.colors.neutral.c50 : p.theme.colors.primary.c80};
+  background: ${(p) =>
     p.status === "inactive" ? p.theme.colors.neutral.c50 : p.theme.colors.primary.c80};
 `;
 
@@ -20,15 +23,15 @@ const BottomSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
   border-style: solid;
   border-color: ${(p) =>
     p.status === "completed" ? p.theme.colors.primary.c80 : p.theme.colors.neutral.c50};
+  background: ${(p) =>
+    p.status === "completed" ? p.theme.colors.primary.c80 : p.theme.colors.neutral.c50};
 `;
 
 const getIconBackground = (theme: Theme, status: ItemStatus, isLastItem?: boolean) => {
-  if (isLastItem && status === "completed") {
-    return theme.colors.success.c100;
+  if (status === "completed") {
+    return "transparent";
   } else if (isLastItem) {
     return theme.colors.success.c10;
-  } else if (status === "completed") {
-    return theme.colors.primary.c80;
   } else if (status === "active") {
     return theme.colors.neutral.c40;
   }
@@ -50,6 +53,8 @@ const MiddleIcon = styled(Flex)<{ status: ItemStatus; isLastItem?: boolean }>`
   height: 20px;
   background: ${(p) => getIconBackground(p.theme, p.status, p.isLastItem)};
   border: 2px solid ${(p) => getIconBorder(p.theme, p.status, p.isLastItem)};
+  align-items: center;
+  justify-content: center;
 `;
 
 export type Props = FlexProps & {
@@ -64,9 +69,16 @@ export default function StepIndicator({ status, isFirstItem, isLastItem, ...prop
   return (
     <Flex flexDirection="column" alignItems="center" {...props}>
       <TopSegment status={status} hidden={isFirstItem} />
-      <MiddleIcon status={status} isLastItem={isLastItem}>
-        {status === "completed" && <CheckAloneMedium color={colors.neutral.c40} size={16} />}
-      </MiddleIcon>
+      {status === "completed" ? (
+        <MiddleIcon status={status} isLastItem={isLastItem}>
+          <CircledCheckSolidMedium
+            color={isLastItem ? colors.success.c100 : colors.primary.c80}
+            size={24}
+          />
+        </MiddleIcon>
+      ) : (
+        <MiddleIcon status={status} isLastItem={isLastItem} />
+      )}
       <BottomSegment status={status} hidden={isLastItem} />
     </Flex>
   );
