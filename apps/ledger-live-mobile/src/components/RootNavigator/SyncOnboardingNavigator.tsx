e3fbf6faf5f74b287eb3e51d7132df7f135001e9
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-06-23 16:02:39 +0200

    feat(llm): DeviceModelSelection onboarding screen

--- apps/ledger-live-mobile/src/components/RootNavigator/SyncOnboardingNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/SyncOnboardingNavigator.tsx
@@ -3,10 +3,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import type { Device } from "@ledgerhq/live-common/lib/hw/actions/types";
 import { ScreenName } from "../../const";
 import { SyncOnboarding } from "../../screens/SyncOnboarding";
+import { DeviceModelSelection } from "../../screens/SyncOnboarding/DeviceModelSelection";
 
 // TODO: handle usb-connected device ?
 export type SyncOnboardingStackParamList = {
   SyncOnboardingCompanion: { pairedDevice: Device | null };
+  DeviceModelSelection: undefined; 
 };
 
 const Stack = createStackNavigator<SyncOnboardingStackParamList>();
@@ -19,6 +21,11 @@ export const SyncOnboardingNavigator = () => (
       headerShadowVisible: false,
     }}
   >
+    <Stack.Screen
+      name={ScreenName.DeviceModelSelection as "DeviceModelSelection"}
+      component={DeviceModelSelection}
+    />
+
     <Stack.Screen
       name={ScreenName.SyncOnboardingCompanion as "SyncOnboardingCompanion"}
       component={SyncOnboarding}
