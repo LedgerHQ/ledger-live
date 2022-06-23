6b37abb417a4fee3162fb1b25b8d4d21ec0606a7
Thomas Roger Lux <thomas@rogerlux.dev>
2022-06-23 10:41:50 +0200

    feat(mobile): add desync loader when nano is disconnected

--- apps/ledger-live-mobile/src/screens/SyncOnboarding/index.tsx
+++ apps/ledger-live-mobile/src/screens/SyncOnboarding/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useCallback, ReactNode } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
-  BottomDrawer,
   Button,
   Flex,
   ScrollContainer,
@@ -18,13 +17,14 @@ import {
 } from "@ledgerhq/native-ui/assets/icons";
 import { OnboardingStep } from "@ledgerhq/live-common/src/hw/extractOnboardingState";
 import { useTheme } from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ScreenName } from "../../const";
 import { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Question from "../../icons/Question";
 import HelpDrawer from "./HelpDrawer";
 import DesyncDrawer from "./DesyncDrawer";
+import ResyncOverlay from "./ResyncOverlay";
 
 type Step = {
   status: "completed" | "active" | "inactive";
@@ -36,12 +36,16 @@ type Step = {
 const defaultOnboardingSteps: Step[] = [
   {
     status: "inactive",
-    deviceStates: [OnboardingStep.WelcomeScreen, OnboardingStep.SetupChoice],
+    deviceStates: [],
     title: "Nano paired",
   },
   {
     status: "inactive",
-    deviceStates: [OnboardingStep.Pin],
+    deviceStates: [
+      OnboardingStep.WelcomeScreen,
+      OnboardingStep.SetupChoice,
+      OnboardingStep.Pin,
+    ],
     title: "Set your PIN",
     renderBody: () => (
       <Flex>
@@ -91,7 +95,6 @@ const pollingPeriodMs = 1000;
 const pollingTimeoutMs = 5000;
 
 export const SyncOnboarding = ({ navigation, route }: Props) => {
-  const { colors } = useTheme();
   const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
   const [stopPolling, setStopPolling] = useState<boolean>(false);
   const [isHelpDrawerOpen, setHelpDrawerOpen] = useState<boolean>(false);
@@ -164,7 +167,6 @@ export const SyncOnboarding = ({ navigation, route }: Props) => {
   }, []);
 
   const handleTimerRunsOut = useCallback(() => {
-    console.log("Timeout done");
     setDesyncDrawerOpen(true);
   }, [setDesyncDrawerOpen]);
 
@@ -176,68 +178,74 @@ export const SyncOnboarding = ({ navigation, route }: Props) => {
     }
   }, [pairedDevice, handleOnPaired]);
 
-  useEffect(() => {
-    console.log("Fatal error");
-    setDesyncDrawerOpen(true);
-  }, [fatalError]);
+  // useEffect(() => {
+  //   console.log("Fatal error");
+  //   setDesyncDrawerOpen(true);
+  // }, [fatalError]);
 
   useEffect(() => {
     if (allowedError && !timer) {
-      console.log("Set timeout");
       setTimer(setTimeout(handleTimerRunsOut, pollingTimeoutMs));
     } else if (!allowedError && timer) {
-      console.log("Clear timeout");
       clearTimeout(timer);
+      setTimer(null);
     }
   }, [allowedError, timer]);
 
   useEffect(() => {
     if (isDesyncDrawerOpen) {
-      console.log("Stopped polling");
       setStopPolling(false);
     }
   }, [isDesyncDrawerOpen]);
 
+  if (!pairedDevice) {
+    // TODO: do something better here
+    return <Flex />;
+  }
+
   return (
     <SafeAreaView>
-      <HelpDrawer
-        isOpen={isHelpDrawerOpen}
-        onClose={() => setHelpDrawerOpen(false)}
-      />
-      <DesyncDrawer
-        isOpen={isDesyncDrawerOpen}
-        onClose={() => setDesyncDrawerOpen(false)}
-      />
-      <ScrollContainer>
-        <ScrollContainerHeader>
-          <Flex
-            flexDirection="row"
-            justifyContent="space-between"
-            pt={7}
-            px={7}
-          >
-            <Button type="main" outline>
-              EN
-            </Button>
-            <Button type="default" Icon={CloseMedium} onPress={handleClose} />
+      <Flex height="100%" position="relative">
+        <HelpDrawer
+          isOpen={isHelpDrawerOpen}
+          onClose={() => setHelpDrawerOpen(false)}
+        />
+        <DesyncDrawer
+          isOpen={isDesyncDrawerOpen}
+          onClose={() => setDesyncDrawerOpen(false)}
+        />
+        <ResyncOverlay isOpen={!!timer && !isDesyncDrawerOpen} />
+        <ScrollContainer>
+          <ScrollContainerHeader>
+            <Flex
+              flexDirection="row"
+              justifyContent="space-between"
+              pt={7}
+              px={7}
+            >
+              <Button type="main" outline>
+                EN
+              </Button>
+              <Button type="default" Icon={CloseMedium} onPress={handleClose} />
+            </Flex>
+          </ScrollContainerHeader>
+          <Flex px={7} pt={7}>
+            <Flex flexDirection="row" alignItems="center">
+              <Text variant="h4">Setup Manual</Text>
+              <Button
+                ml={2}
+                Icon={Question}
+                onPress={() => setHelpDrawerOpen(true)}
+              />
+            </Flex>
+            <Text variant="body">Continue setting up on your Nano.</Text>
+            <Text mb={8} variant="body">
+              Check back here for tips and information.
+            </Text>
+            <StepList items={onboardingSteps} />
           </Flex>
-        </ScrollContainerHeader>
-        <Flex px={7} pt={7}>
-          <Flex flexDirection="row" alignItems="center">
-            <Text variant="h4">Setup Manual</Text>
-            <Button
-              ml={2}
-              Icon={Question}
-              onPress={() => setHelpDrawerOpen(true)}
-            />
-          </Flex>
-          <Text variant="body">Continue setting up on your Nano.</Text>
-          <Text mb={8} variant="body">
-            Check back here for tips and information.
-          </Text>
-          <StepList items={onboardingSteps} />
-        </Flex>
-      </ScrollContainer>
+        </ScrollContainer>
+      </Flex>
     </SafeAreaView>
   );
 };
