41afd699aff2de780f2078a8980fbb09c8ca16a2
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-06-23 16:02:39 +0200

    feat(llm): DeviceModelSelection onboarding screen

--- apps/ledger-live-mobile/src/const/navigation.js
+++ apps/ledger-live-mobile/src/const/navigation.js
@@ -336,6 +336,7 @@ export const ScreenName = {
   OnboardingModalRecoveryPhraseWarning: "OnboardingModalRecoveryPhraseWarning",
 
   SyncOnboardingCompanion: "SyncOnboardingCompanion",
+  DeviceModelSelection: "DeviceModelSelection",
 
   PlatformCatalog: "PlatformCatalog",
   PlatformApp: "PlatformApp",
