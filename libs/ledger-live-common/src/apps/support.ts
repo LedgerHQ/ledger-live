42e43e4ced2c6cf79e5f6c046ea520f8c8a7f5ea
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-06-24 13:27:10 +0200

    Use correct cardano Nano app name

--- libs/ledger-live-common/src/apps/support.ts
+++ libs/ledger-live-common/src/apps/support.ts
@@ -35,7 +35,7 @@ const appVersionsRequired = {
   Elrond: ">= 1.0.11",
   Ethereum: ">= 1.9.17",
   Solana: ">= 1.2.0",
-  CardanoADA: ">= 4.1.0",
+  "Cardano ADA": ">= 4.1.0",
 };
 export function mustUpgrade(
   deviceModel: DeviceModelId,
