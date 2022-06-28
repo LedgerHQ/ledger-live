14590061d796122861f8f0d578fc8afd65c80941
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-06-28 14:33:52 +0200

    add: trigger drawer during onboarding

--- apps/ledger-live-desktop/src/renderer/components/Onboarding/Screens/Tutorial/screens/DeviceHowTo.tsx
+++ apps/ledger-live-desktop/src/renderer/components/Onboarding/Screens/Tutorial/screens/DeviceHowTo.tsx
@@ -29,7 +29,7 @@ export function DeviceHowTo() {
     <Column>
       <Title>{t("onboarding.screens.tutorial.screens.howToGetStarted.title")}</Title>
       {steps.map((step, index) => (
-        <Bullet key={index} bulletText={index} text={t(step.text)} subText={t(step.subText)} />
+        <Bullet key={index} bulletText={index + 1} text={t(step.text)} subText={t(step.subText)} />
       ))}
     </Column>
   );
