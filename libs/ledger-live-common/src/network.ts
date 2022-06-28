96f80b50730c37c214d90590efed740864a0cb51
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-06-28 17:55:12 +0200

    Simplify network.ts errors handling

--- libs/ledger-live-common/src/network.ts
+++ libs/ledger-live-common/src/network.ts
@@ -2,7 +2,7 @@ import invariant from "invariant";
 import axios, { AxiosResponse } from "axios";
 import type { AxiosError, AxiosRequestConfig } from "axios";
 import { log } from "@ledgerhq/logs";
-import { NetworkDown, LedgerAPI5xx, LedgerAPI4xx } from "@ledgerhq/errors";
+import { NetworkDown } from "@ledgerhq/errors";
 import { retry } from "./promise";
 import { getEnv } from "./env";
 
@@ -49,36 +49,15 @@ export const errorInterceptor = (error: AxiosError<any>) => {
   if (!config) throw error;
   const { baseURL, url, method = "", metadata } = config;
   const { startTime = 0 } = metadata || {};
-
-  let errorToThrow;
   if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
     const { data, status } = error.response;
-    let msg;
-    try {
-      if (data && typeof data === "string") {
-        msg = extractErrorMessage(data);
-      } else if (data && typeof data === "object") {
-        msg = getErrorMessage(data);
-      }
-    } catch (e) {
-      log("warn", "can't parse server result " + String(e));
-    }
-
-    if (msg) {
-      errorToThrow = makeError(msg, status, url, method);
-    } else {
-      errorToThrow = makeError(`API HTTP ${status}`, status, url, method);
-    }
     log(
       "network-error",
       `${status} ${method} ${baseURL || ""}${url} (${(
         Date.now() - startTime
-      ).toFixed(0)}ms): ${errorToThrow.message}`,
+      ).toFixed(0)}ms)`,
       getEnv("DEBUG_HTTP_RESPONSE") ? { data: data } : {}
     );
-    throw errorToThrow;
   } else if (error.request) {
     log(
       "network-down",
@@ -95,52 +74,6 @@ axios.interceptors.request.use(requestInterceptor);
 
 // $FlowFixMe LLD raise issues here
 axios.interceptors.response.use(responseInterceptor, errorInterceptor);
-
-const makeError = (msg, status, url, method) => {
-  const obj = {
-    status,
-    url,
-    method,
-  };
-  return (status || "").toString().startsWith("4")
-    ? new LedgerAPI4xx(msg, obj)
-    : new LedgerAPI5xx(msg, obj);
-};
-
-const getErrorMessage = (
-  data: Record<string, any>
-): string | null | undefined => {
-  if (!data) return "";
-  if (typeof data === "string") return data;
-  if (data.errors) {
-    return getErrorMessage(data.errors[0]);
-  }
-  return data.message || data.error_message || data.error || data.msg;
-};
-
-const extractErrorMessage = (raw: string): string | undefined => {
-  let data = JSON.parse(raw);
-  if (data && Array.isArray(data)) data = data[0];
-  let msg = getErrorMessage(data);
-
-  if (typeof msg === "string") {
-    const m = msg.match(/^JsDefined\((.*)\)$/);
-    const innerPart = m ? m[1] : msg;
-
-    const r = JSON.parse(innerPart);
-    let message = r.message;
-    if (typeof message === "object") {
-      message = message.message;
-    }
-    if (typeof message === "string") {
-      msg = message;
-    }
-
-    return msg ? String(msg) : undefined;
-  }
-
-  return;
-};
 
 const implementation = (arg: any): Promise<any> => {
   invariant(typeof arg === "object", "network takes an object as parameter");
