f0d75df57cce14c11da866a708315dd27fd50218
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-06-28 14:45:28 +0200

    fix: drawers design in onboaring

--- apps/ledger-live-desktop/src/renderer/components/Onboarding/Help/HideRecoverySeed.tsx
+++ apps/ledger-live-desktop/src/renderer/components/Onboarding/Help/HideRecoverySeed.tsx
@@ -2,55 +2,85 @@ import React from "react";
 import { useTranslation, Trans } from "react-i18next";
 import { Text, Button, Icons } from "@ledgerhq/react-ui";
 import styled from "styled-components";
+import IconCross from "~/renderer/icons/Cross";
+import IconCheck from "~/renderer/icons/Check";
+import IconTriangleWarning from "~/renderer/icons/TriangleWarning";
+import Color from "color";
 import { ScrollArea } from "~/renderer/components/Onboarding/ScrollArea";
-import ChevronRight from "~/renderer/icons/ChevronRight";
 
-const PointContainer = styled.div`
+const RuleContainer = styled.div`
   display: flex;
   flex-direction: row;
   margin-bottom: 16px;
-  align-items: flex-start;
+  align-items: center;
 `;
 
-const PointIconContainer = styled.div`
+const RuleIconContainer = styled.div`
   width: 28px;
   height: 28px;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 50%;
-  color: #6490f1;
-  background-color: #6490f110;
 `;
 
-type PointProps = {
+const ruleTypes = {
+  success: {
+    color: "#66BE54",
+    bgColor: Color("#66BE54")
+      .alpha(0.1)
+      .toString(),
+    Icon: IconCheck,
+  },
+  error: {
+    color: "#EA2E49",
+    bgColor: Color("#EA2E49")
+      .alpha(0.1)
+      .toString(),
+    Icon: IconCross,
+  },
+  warning: {
+    color: "#FF6E33",
+    bgColor: Color("#FF6E33")
+      .alpha(0.1)
+      .toString(),
+    Icon: IconTriangleWarning,
+  },
+};
+
+type RuleProps = {
+  type: "success" | "error" | "warning";
   children: any;
 };
 
-function Point({ children }: PointProps) {
+function Rule({ type, children }: RuleProps) {
+  const RuleIcon = ruleTypes[type].Icon;
   return (
-    <PointContainer>
-      <PointIconContainer>
-        <ChevronRight size={12} />
-      </PointIconContainer>
+    <RuleContainer>
+      <RuleIconContainer
+        style={{
+          color: ruleTypes[type].color,
+          backgroundColor: ruleTypes[type].bgColor,
+        }}
+      >
+        <RuleIcon size={12} />
+      </RuleIconContainer>
       <Text
         ml="16px"
         color="palette.text.shade100"
         ff="Inter|Regular"
-        fontSize="14px"
-        lineHeight="21px"
+        fontSize={13}
         style={{ flex: 1 }}
       >
         {children}
       </Text>
-    </PointContainer>
+    </RuleContainer>
   );
 }
 
 const HideRecoverySeedContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 76px;
 `;
 
 export function HideRecoverySeed(props: { handleNextInDrawer: () => void }) {
@@ -59,32 +89,34 @@ export function HideRecoverySeed(props: { handleNextInDrawer: () => void }) {
   return (
     <ScrollArea>
       <HideRecoverySeedContainer>
-        <Text
-          mb="32px"
-          color="palette.text.shade100"
-          ff="Inter|SemiBold"
-          fontSize="22px"
-          lineHeight="26.63px"
-        >
+        <Text mb="32px" color="palette.text.shade100" variant="h3">
           {t("onboarding.drawers.whereToHide.title")}
         </Text>
-        <Point>
+        <Rule type="error">
           <Trans i18nKey="onboarding.drawers.whereToHide.points.1">
             <Text ff="Inter|Bold" />
           </Trans>
-        </Point>
-        <Point>
+        </Rule>
+        <Rule type="error">
           <Trans i18nKey="onboarding.drawers.whereToHide.points.2">
             <Text ff="Inter|Bold" />
           </Trans>
-        </Point>
-        <Point>
+        </Rule>
+        <Rule type="success">
           <Trans i18nKey="onboarding.drawers.whereToHide.points.3">
             <Text ff="Inter|Bold" />
           </Trans>
-        </Point>
-        <Point>{t("onboarding.drawers.whereToHide.points.4")}</Point>
-        <Point>{t("onboarding.drawers.whereToHide.points.5")}</Point>
+        </Rule>
+        <Rule type="warning">
+          <Trans i18nKey="onboarding.drawers.whereToHide.points.4">
+            <Text ff="Inter|Bold" />
+          </Trans>
+        </Rule>
+        <Rule type="warning">
+          <Trans i18nKey="onboarding.drawers.whereToHide.points.5">
+            <Text ff="Inter|Bold" />
+          </Trans>
+        </Rule>
         <Button
           variant="main"
           onClick={props.handleNextInDrawer}
