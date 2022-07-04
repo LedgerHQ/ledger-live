c51cb4b07a1b0058de1e1241e3a3e98ca2e40652
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-07-04 19:16:24 +0200

    Custom UI component for Storyly

--- apps/ledger-live-mobile/src/components/Storyly/StorylyWrapper.tsx
+++ apps/ledger-live-mobile/src/components/Storyly/StorylyWrapper.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, {
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 import { Platform } from "react-native";
 import { useSelector } from "react-redux";
 import { Storyly } from "storyly-react-native";
@@ -6,81 +12,106 @@ import styled from "styled-components/native";
 import { languageSelector } from "../../reducers/settings";
 import { StorylyInstanceID } from "./shared"; // eslint-disable-line no-unused-vars, @typescript-eslint/no-unused-vars
 
-type Props = {
+export type Props = {
   instanceID: StorylyInstanceID;
-  shouldBlockVideoContentOnAndroid?: boolean; // videos not supported on Android for now, causing a crash if they load
+  /**
+   * Videos not supported on Android for now, causing a crash if they load.
+   * As a better-than-nothing safety net, if this props is set to true
+   * it will fully disable the current instance in case it's Android and some
+   * video content is detected.
+   *
+   * Default is true.
+   */
+  shouldBlockVideoContentOnAndroid?: boolean;
+  /**
+   * If there are no story groups loaded, it will fallback to loading the
+   * english version.
+   *
+   * Default is true.
+   */
   shouldFallbackToEnglishIfEmpty?: boolean;
 } & Omit<Storyly.Props, "storylyId">;
 
+// @ts-ignore idk how to fix this
 const StyledStoryly = styled(Storyly)`
-  flex: 1; // necessary for touches to work
+  flex: 1; // necessary for touches to work;
 `;
 
-const StorylyWrapper: React.FC<Props> = props => {
-  const {
-    instanceID,
-    storylySegments,
-    onLoad,
-    onFail,
-    shouldBlockVideoContentOnAndroid = true,
-    shouldFallbackToEnglishIfEmpty = true,
-  } = props;
-
-  const [fallbackToEnglish, setFallbackToEnglish] = useState(false);
-  const [blocked, setBlocked] = useState(false);
-
-  const language = useSelector(languageSelector);
-
-  const segments = useMemo(() => {
-    const languageSegments = [
-      language,
-      ...(fallbackToEnglish && language !== "en" ? ["en"] : []),
-    ].map(l => `lang_${l}`);
-    return [...languageSegments, ...(storylySegments ?? [])];
-  }, [language, storylySegments, fallbackToEnglish]);
-
-  const handleLoad = useCallback(
-    (event: Storyly.StoryLoadEvent) => {
-      console.log("StorylyWrapper handleLoad", event);
-      if (shouldFallbackToEnglishIfEmpty && event.storyGroupList.length === 0)
-        setFallbackToEnglish(true);
-      if (shouldBlockVideoContentOnAndroid && Platform.OS === "android") {
-        if (
-          event.storyGroupList.find(
-            storyGroup =>
-              !!storyGroup.stories.find(story => story.media.type !== 1),
-          )
-        )
-          setBlocked(true);
-      }
-      onLoad && onLoad(event);
-    },
-    [
+const StorylyWrapper = forwardRef(
+  (props: Props, ref: ForwardedRef<Storyly>) => {
+    const {
+      instanceID,
+      storylySegments,
       onLoad,
-      shouldFallbackToEnglishIfEmpty,
-      setFallbackToEnglish,
-      shouldBlockVideoContentOnAndroid,
-      setBlocked,
-    ],
-  );
+      onFail,
+      onEvent,
+      shouldBlockVideoContentOnAndroid = true,
+      shouldFallbackToEnglishIfEmpty = true,
+    } = props;
 
-  const handleFail = useCallback(
-    (event: String) => {
-      console.log("StorylyWrapper handleFail", event);
-      onFail && onFail(event);
-    },
-    [onFail],
-  );
+    const [fallbackToEnglish, setFallbackToEnglish] = useState(false);
+    const [blocked, setBlocked] = useState(false);
 
-  return (
-    <StyledStoryly
-      {...props}
-      storylyId={blocked ? "" : instanceID}
-      storylySegments={segments}
-      onLoad={handleLoad}
-      onFail={handleFail}
-    />
-  );
-};
+    const language = useSelector(languageSelector);
+
+    const segments = useMemo(() => {
+      const languageSegments = [
+        language,
+        ...(fallbackToEnglish && language !== "en" ? ["en"] : []),
+      ].map(l => `lang_${l}`);
+      return [...languageSegments, ...(storylySegments ?? [])];
+    }, [language, storylySegments, fallbackToEnglish]);
+
+    const handleLoad = useCallback(
+      (event: Storyly.StoryLoadEvent) => {
+        if (shouldFallbackToEnglishIfEmpty && event.storyGroupList.length === 0)
+          setFallbackToEnglish(true);
+        if (shouldBlockVideoContentOnAndroid && Platform.OS === "android") {
+          if (
+            event.storyGroupList.find(
+              storyGroup =>
+                !!storyGroup.stories.find(story => story.media.type !== 1),
+            )
+          )
+            setBlocked(true);
+        }
+        onLoad && onLoad(event);
+      },
+      [
+        onLoad,
+        shouldFallbackToEnglishIfEmpty,
+        setFallbackToEnglish,
+        shouldBlockVideoContentOnAndroid,
+        setBlocked,
+      ],
+    );
+
+    const handleFail = useCallback(
+      (event: String) => {
+        onFail && onFail(event);
+      },
+      [onFail],
+    );
+
+    const handleEvent = useCallback(
+      (event: Storyly.StoryEvent) => {
+        onEvent && onEvent(event);
+      },
+      [onEvent],
+    );
+
+    return (
+      <StyledStoryly
+        ref={ref}
+        {...props}
+        storylyId={blocked ? "" : instanceID}
+        storylySegments={segments}
+        onLoad={handleLoad}
+        onFail={handleFail}
+        onEvent={handleEvent}
+      />
+    );
+  },
+);
 
 export default StorylyWrapper;
