8a27e8dbda64c0a4ecba5129e6ed3725c65f91ea
Marco - Figment <98154129+marco-figment@users.noreply.github.com>
2022-07-08 12:25:53 +0200

    bug: fix missing info link in drawer

--- apps/ledger-live-desktop/src/config/urls.js
+++ apps/ledger-live-desktop/src/config/urls.js
@@ -128,7 +128,8 @@ export const urls = {
     "https://support.ledger.com/hc/en-us/articles/360018131260?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=polkadot",
   cosmosStakingRewards:
     "https://support.ledger.com/hc/en-us/articles/360014339340-Earn-Cosmos-staking-rewards,?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=cosmos",
-  osmosisStakingRewards: "TODO",
+  osmosisStakingRewards:
+    "https://support.ledger.com/hc/en-us/sections/4404369637521-Crypto-assets?docs=true",
   algorandStakingRewards:
     "https://support.ledger.com/hc/en-us/articles/360015897740?utm_source=ledger_live_desktop&utm_medium=self_referral&utm_content=algorand",
   polkadotFeesInfo:
