59d488e1dc50dd8e4e6b0deef217f91080f99c26
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-07-11 09:46:06 +0200

    fix background color for kyc status tag

--- apps/ledger-live-mobile/src/screens/Swap/Form/Summary/StatusTag.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/Form/Summary/StatusTag.tsx
@@ -7,18 +7,22 @@ import { KYCStatus } from "@ledgerhq/live-common/src/exchange/swap/types";
 const statusThemeMap = {
   pending: {
     color: "warning.c100",
+    backgroundColor: "warning.c10",
     icon: "Clock",
   },
   approved: {
     color: "success.c100",
+    backgroundColor: "success.c10",
     icon: "CircledCheck",
   },
   closed: {
     color: "error.c100",
+    backgroundColor: "error.c10",
     icon: "Info",
   },
   upgradeRequired: {
     color: "warning.c100",
+    backgroundColor: "warning.c10",
     icon: "Clock",
   },
 };
@@ -31,11 +35,13 @@ export function StatusTag({ kyc }: { kyc?: KYCStatus }) {
   }
 
   // @ts-expect-error
-  const { color, icon } = statusThemeMap[kyc.status] || {
+  const { color, backgroundColor, icon } = statusThemeMap[kyc.status] || {
     color: null,
+    backgroundColor: null,
     icon: null,
   };
 
+  // TODO swap: replace with Tag component
   return (
     <Flex
       alignItems="center"
@@ -43,8 +49,9 @@ export function StatusTag({ kyc }: { kyc?: KYCStatus }) {
       borderRadius={4}
       padding={2}
       marginRight={2}
+      backgroundColor={backgroundColor}
     >
-      <Flex marginRight={2}>
+      <Flex marginRight={2} borderRadius={4}>
         <Text variant="small" color={color}>
           {t(`transfer.swap2.form.providers.kyc.status.${kyc.status}`)}
         </Text>
