e704a6f837183f4d2fc3996b1b1e38072f7ce7a1
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-07-11 09:59:45 +0200

    handle from amount error

--- apps/ledger-live-mobile/src/screens/Swap/Form/TxForm/AmountInput.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/Form/TxForm/AmountInput.tsx
@@ -1,33 +1,47 @@
 import React from "react";
-import { Text } from "@ledgerhq/native-ui";
+import { Flex, Text } from "@ledgerhq/native-ui";
 import { StyleSheet } from "react-native";
 import { BigNumber } from "bignumber.js";
 import { Unit } from "@ledgerhq/live-common/lib/types";
 import CurrencyInput from "../../../../components/CurrencyInput";
+import TranslatedError from "../../../../components/TranslatedError";
 
 interface Props {
   value?: BigNumber;
   editable: boolean;
   unit?: Unit;
   onChange: (val: BigNumber) => void;
+  fromAmountError?: string;
 }
 
-export function AmountInput({ value, onChange, editable, unit }: Props) {
+export function AmountInput({
+  value,
+  onChange,
+  editable,
+  unit,
+  fromAmountError,
+}: Props) {
   if (!unit) {
     return <Text>0</Text>;
   }
 
   return (
-    <CurrencyInput
-      editable={editable}
-      onChange={onChange}
-      unit={unit}
-      value={value}
-      inputStyle={styles.inputText}
-      // TODO
-      // hasError={!hideError && !!fromAmountError}
-      dynamicFontRatio={0.3}
-    />
+    <Flex alignItems="flex-end">
+      <CurrencyInput
+        editable={editable}
+        onChange={onChange}
+        unit={unit}
+        value={value}
+        inputStyle={styles.inputText}
+        hasError={!!fromAmountError}
+        dynamicFontRatio={0.3}
+      />
+
+      <Text color="error.c100">
+        {/* @ts-expect-error */}
+        <TranslatedError error={fromAmountError} />
+      </Text>
+    </Flex>
   );
 }
 
