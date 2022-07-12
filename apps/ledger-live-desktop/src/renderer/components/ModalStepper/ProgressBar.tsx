f7d352e52b467fb90bc0572d16f08488114856ea
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-07-12 20:00:49 +0200

    Fix some styling not using design system

--- apps/ledger-live-desktop/src/renderer/components/ModalStepper/ProgressBar.tsx
+++ apps/ledger-live-desktop/src/renderer/components/ModalStepper/ProgressBar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { Box } from "@ledgerhq/react-ui";
 
-const Bar = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 4px;
-  width: ${p => p.percentage}%;
-  background-color: ${p => p.theme.colors.palette.neutral.c100};
+const Bar = styled(Box).attrs((p: {percentage: number}) => ({
+  "position": "absolute",
+  "top": 0,
+  "left": 0,
+  "right": 0,
+  "height": "4px",
+  "width": `${p.percentage}%`,
+  backgroundColor: "neutral.c100",
+}))`
   transition: width ease-out 200ms;
 `;
 
-const ProgressBar = ({ stepIndex, stepCount }) => {
+const ProgressBar = ({ stepIndex, stepCount }: {stepIndex: number, stepCount: number}) => {
   return <Bar percentage={100 * ((stepIndex + 1) / stepCount)} />;
 };
 
