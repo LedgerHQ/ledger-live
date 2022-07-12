0748c7a6907253c1b0e6dd7554fdc1c5a0e1b38b
ThomasLaforge <thomas.laforge.38@gmail.com>
2022-07-12 19:42:55 +0200

    select crypto analytics

--- apps/ledger-live-mobile/src/components/FilteredSearchBar.tsx
+++ apps/ledger-live-mobile/src/components/FilteredSearchBar.tsx
@@ -1,9 +1,13 @@
-import React, { ReactNode, useState, memo } from "react";
+import React, { ReactNode, useState, memo, useCallback } from "react";
 import { SearchInput, Flex } from "@ledgerhq/native-ui";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "styled-components/native";
 
+import { useRoute } from "@react-navigation/native";
+import { useDebounce } from "@ledgerhq/live-common/lib/hooks/useDebounce";
 import Search from "./Search";
+import { track } from "../analytics";
+import { ScreenName } from "../const";
 
 type Props = {
   initialQuery?: string;
@@ -25,13 +29,21 @@ const FilteredSearchBar = ({
   const { t } = useTranslation();
   const { colors } = useTheme();
   const [query, setQuery] = useState<string>(initialQuery || "");
+  const route = useRoute();
+
+  const onChange = useDebounce((newQuery: string) => {
+    setQuery(newQuery);
+    if (route.name === ScreenName.ReceiveSelectCrypto) {
+      track("search_clicked", { input: newQuery, screen: route.name });
+    }
+  }, 200);
 
   return (
     <>
       <Flex style={inputWrapperStyle}>
         <SearchInput
           value={query}
-          onChange={setQuery}
+          onChange={onChange}
           placeholder={t("common.search")}
           placeholderTextColor={colors.neutral.c70}
           color={colors.neutral.c100}
