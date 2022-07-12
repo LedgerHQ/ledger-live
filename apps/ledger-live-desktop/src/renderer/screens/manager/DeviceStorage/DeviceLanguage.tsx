e00da89a57b4963cd94c2e2918e55702f58f8b96
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-07-12 09:58:10 +0200

    FAT-126 Success screen + refresh language when it's installed

--- apps/ledger-live-desktop/src/renderer/screens/manager/DeviceStorage/DeviceLanguage.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/manager/DeviceStorage/DeviceLanguage.tsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Button, Flex, Icons } from "@ledgerhq/react-ui";
 import Text from "~/renderer/components/Text";
 import DeviceLanguageInstallation from "./DeviceLanguageInstallation";
 import { DeviceInfo } from "@ledgerhq/live-common/types/manager";
 import { Device } from "@ledgerhq/live-common/hw/actions/types";
+import { idsToLanguage, Language } from "@ledgerhq/live-common/types/languages";
+import { useTranslation } from "react-i18next";
 
 type Props = {
-  deviceInfo: DeviceInfo,
-  device: Device
-}
+  deviceInfo: DeviceInfo;
+  device: Device;
+};
 
 const DeviceLanguage: React.FC<Props> = ({ deviceInfo, device }: Props) => {
+  if (!deviceInfo.languageId) return null;
+
   const [isLanguageInstallationOpen, setIsLanguageInstallation] = useState(false);
+  const [deviceLanguage, setDeviceLanguage] = useState<Language>(
+    idsToLanguage[deviceInfo.languageId],
+  );
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>(deviceLanguage);
+
+  const refreshDeviceLanguage = useCallback(() => {
+    setDeviceLanguage(selectedLanguage);
+  }, [setDeviceLanguage, selectedLanguage]);
+
+  const { t } = useTranslation();
 
   return (
     <Flex>
@@ -21,12 +35,17 @@ const DeviceLanguage: React.FC<Props> = ({ deviceInfo, device }: Props) => {
           Language
         </Text>
       </Flex>
-      <Button Icon={Icons.ChevronRightMedium} onClick={() => setIsLanguageInstallation(true)}>English</Button>
+      <Button Icon={Icons.ChevronRightMedium} onClick={() => setIsLanguageInstallation(true)}>
+        {t(`deviceLocalization.languages.${deviceLanguage}`)}
+      </Button>
       <DeviceLanguageInstallation
         isOpen={isLanguageInstallationOpen}
         onClose={() => setIsLanguageInstallation(false)}
         deviceInfo={deviceInfo}
         device={device}
+        onSelectLanguage={setSelectedLanguage}
+        selectedLanguage={selectedLanguage}
+        onSuccess={refreshDeviceLanguage}
       />
     </Flex>
   );
--- apps/ledger-live-desktop/src/renderer/screens/manager/DeviceStorage/DeviceLanguageInstallation.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/manager/DeviceStorage/DeviceLanguageInstallation.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from "react";
-import { Button, Flex, Icons, Drawer, Radio } from "@ledgerhq/react-ui";
-import Text from "~/renderer/components/Text";
+import React, { useState, useCallback, useEffect } from "react";
+import { Button, Flex, Icons, Drawer, Radio, BoxedIcon, Text } from "@ledgerhq/react-ui";
 import { DeviceInfo } from "@ledgerhq/live-common/types/manager";
 import { useAvailableLanguagesForDevice } from "@ledgerhq/live-common/manager/hooks";
 import { Language } from "@ledgerhq/live-common/types/languages";
@@ -10,6 +9,7 @@ import { getEnv } from "@ledgerhq/live-common/env";
 import { mockedEventEmitter } from "~/renderer/components/debug/DebugMock";
 import DeviceAction from "~/renderer/components/DeviceAction";
 import { Device } from "@ledgerhq/live-common/hw/actions/types";
+import { useTranslation } from "react-i18next";
 
 const installLanguageExec = command("installLanguage");
 const action = createAction(getEnv("MOCK") ? mockedEventEmitter : installLanguageExec);
@@ -18,40 +18,91 @@ type Props = {
   isOpen: boolean;
   onClose: () => void;
   deviceInfo: DeviceInfo;
+  onSuccess: () => void;
+  selectedLanguage: Language;
+  onSelectLanguage: (language: Language) => void;
   device: Device;
 };
 
-const DeviceLanguageInstallation: React.FC<Props> = ({ isOpen, onClose, deviceInfo, device }: Props) => {
+const DeviceLanguageInstalled = ({
+  onContinue,
+  onMount,
+  installedLanguage,
+}: {
+  onContinue: () => void;
+  onMount: () => void;
+  installedLanguage: Language;
+}) => {
+  useEffect(() => onMount(), [onMount]);
+
+  const { t } = useTranslation();
+
+  return (
+    <Flex flexDirection="column" alignItems="center" height="100%" justifyContent="center">
+      <BoxedIcon Icon={Icons.CheckAloneMedium} iconColor="success.c100" size={48} iconSize={24} />
+      <Text variant="h4" textAlign="center" my={7}>
+        {t("deviceLocalization.languageInstalled", {
+          language: t(`deviceLocalization.languages.${installedLanguage}`),
+        })}
+      </Text>
+      <Button variant="main" alignSelf="stretch" onClick={onContinue}>
+        {t("common.continue")}
+      </Button>
+    </Flex>
+  );
+};
+
+const DeviceLanguageInstallation: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  deviceInfo,
+  selectedLanguage,
+  onSelectLanguage,
+  onSuccess,
+}: Props) => {
   const availableLanguages = useAvailableLanguagesForDevice(deviceInfo);
 
-  const [selectedLanguage, setSelectedLanguage] = useState<Language>(availableLanguages[0]);
   const [installing, setInstalling] = useState(false);
+
+  const { t } = useTranslation();
 
   const onCloseDrawer = useCallback(() => {
     onClose();
     setInstalling(false);
   }, [onClose, setInstalling]);
 
+  const Result = useCallback(() => {
+    return (
+      <DeviceLanguageInstalled
+        installedLanguage={selectedLanguage}
+        onContinue={onCloseDrawer}
+        onMount={onSuccess}
+      />
+    );
+  }, [selectedLanguage, onCloseDrawer, onSuccess]);
+
+  const onChange = useCallback(
+    (radioValue?: string | string[] | number) => onSelectLanguage(radioValue as Language),
+    [onSelectLanguage],
+  );
+
   return (
     <Drawer isOpen={isOpen} onClose={onCloseDrawer} title="Device Language" big>
       <Flex flex={1} p={10} flexDirection="column" justifyContent="space-between">
         {installing ? (
-          <DeviceAction
-            action={action}
-            request={selectedLanguage}
-          />
+          <DeviceAction action={action} request={selectedLanguage} Result={Result} />
         ) : (
           <>
             <Radio
               currentValue={selectedLanguage}
-              onChange={setSelectedLanguage}
+              onChange={onChange}
               name="LanguageSelection"
               containerProps={{ flexDirection: "column", rowGap: "1rem", flex: 1 }}
             >
               {availableLanguages.map(language => (
                 <Radio.ListElement
                   containerProps={{ flex: 1, padding: 0 }}
-                  label={language}
+                  label={t(`deviceLocalization.languages.${language}`)}
                   value={language}
                   key={language}
                 />
