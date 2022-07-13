35cc5a072d108c678e30f31642748d74148597bc
Henri Ly <henri.ly-ext@ledger.fr>
2022-07-13 13:55:46 +0200

    linter 2

--- apps/ledger-live-mobile/src/families/stellar/StellarFeeRow.js
+++ apps/ledger-live-mobile/src/families/stellar/StellarFeeRow.js
@@ -82,7 +82,7 @@ export default function StellarFeeRow({
     fee,
     onSelect,
   }: {
-    label: React.ReactNode,
+    label: ReactNode,
     isSelected: boolean,
     fee: BigNumber | null,
     onSelect: (isCustom: boolean) => void,
