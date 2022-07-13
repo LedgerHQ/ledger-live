380fa50f98a631b20bcddab16bad3d7b0237e376
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-07-13 14:13:11 +0200

    FAT-127 Fix bug in supported target id identification

--- libs/ledger-live-common/src/manager/localization.ts
+++ libs/ledger-live-common/src/manager/localization.ts
@@ -1,17 +1,11 @@
 import { DeviceModelId } from "@ledgerhq/devices";
 import { satisfies as versionSatisfies } from "semver";
 
-const deviceVersionRangesForLocalization: { [key in DeviceModelId]?: string } =
-  {
-    nanoX: "=2.1.0-lo2",
-  };
+const deviceVersionRangesForLocalization: { [key in DeviceModelId]?: string } = {
+  nanoX: "=2.1.0-lo2",
+};
 
-export const isDeviceLocalizationSupported = (
-  seVersion: string,
-  modelId: DeviceModelId
-) =>
+export const isDeviceLocalizationSupported = (seVersion: string, modelId?: DeviceModelId) =>
+  modelId &&
   deviceVersionRangesForLocalization[modelId] &&
-  versionSatisfies(
-    seVersion,
-    deviceVersionRangesForLocalization[modelId] as string
-  );
+  versionSatisfies(seVersion, deviceVersionRangesForLocalization[modelId] as string);
