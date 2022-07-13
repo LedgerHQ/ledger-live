79e6fd3d8c1f87ac3eea623475616c4681d4ecf5
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-07-13 13:44:47 +0200

    fix e2e

--- apps/ledger-live-desktop/tests/models/OnboardingPage.ts
+++ apps/ledger-live-desktop/tests/models/OnboardingPage.ts
@@ -1,27 +1,32 @@
-import { Locator, Page } from "@playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
+import { DeviceAction } from "../DeviceAction";
 
 export class OnboardingPage {
   readonly page: Page;
+  readonly deviceAction: DeviceAction;
   readonly getStartedButton: Locator;
-  readonly termsCheckbox: Locator;
-  readonly termsSubmitButton: Locator;
+  readonly selectDeviceContainer: Function;
   readonly selectDeviceButton: Function;
   readonly newDeviceButton: Locator;
   readonly connectDeviceButton: Locator;
   readonly restoreDeviceButton: Locator;
   readonly pedagogyModal: Locator;
+  readonly stepperContinueButton: Locator;
+  readonly stepperEndButton: Locator;
   readonly checkMyNanoButton: Locator;
   readonly continueButton: Locator;
   readonly leftArrowBasicsButton: Locator;
   readonly rightArrowBasicsButton: Locator;
+  readonly tutorialContinueButton: Locator;
   readonly setupWalletButton: Locator;
   readonly getStartedCtaButton: Locator;
-  readonly beCarefulButton: Locator;
   readonly startSetupButton: Locator;
-  readonly pincodeCheckbox: Locator;
-  readonly setupPincodeButton: Locator;
-  readonly confirmPincodeButton: Locator;
+  readonly pinCodeCheckbox: Locator;
+  readonly pinCodeDrawer: Locator;
   readonly recoveryPhraseCheckbox: Locator;
+  readonly recoverySeedDrawer: Locator;
+  readonly hideSeedDrawer: Locator;
+  readonly recoveryPhraseLossCheckbox: Locator;
   readonly recoverySetupButton: Locator;
   readonly writeRecoveryPhraseButton: Locator;
   readonly confirmRecoveryPhraseButton: Locator;
@@ -30,54 +35,55 @@ export class OnboardingPage {
   readonly quizStartButton: Locator;
   readonly quizAnswerTopButton: Locator;
   readonly quizAnswerBottomButton: Locator;
-  readonly quizNextButton: Locator;
-  readonly quizSuccessButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.getStartedButton = page.locator("data-test-id=onboarding-get-started-button");
-    this.termsCheckbox = page.locator("data-test-id=onboarding-terms-checkbox");
-    this.termsSubmitButton = page.locator("data-test-id=onboarding-terms-submit");
-    this.selectDeviceButton = (device: "nanoS" | "nanoX" | "nanoSP"): Locator =>
-      page.locator(`data-test-id=device-${device}`);
+    this.deviceAction = new DeviceAction(page);
+    this.getStartedButton = page.locator("data-test-id=v3-onboarding-get-started-button");
+    this.selectDeviceContainer = (deviceId: string): Locator => {
+      return page.locator(`data-test-id=v3-container-device-${deviceId}`);
+    };
+    this.selectDeviceButton = (deviceId: string): Locator =>
+      page.locator(`data-test-id=v3-device-${deviceId}`);
     this.checkMyNanoButton = page.locator('button:has-text("Check my Nano")');
     this.continueButton = page.locator('button:has-text("Continue")');
-    this.newDeviceButton = page.locator("data-test-id=onboarding-new-device");
-    this.connectDeviceButton = page.locator("data-test-id=onboarding-initialized-device");
-    this.restoreDeviceButton = page.locator("data-test-id=onboarding-restore-device");
-    this.pedagogyModal = page.locator("data-test-id=onboarding-pedagogy-modal");
-    this.leftArrowBasicsButton = page.locator("data-test-id=pedagogy-left");
-    this.rightArrowBasicsButton = page.locator("data-test-id=pedagogy-right");
-    this.setupWalletButton = page.locator("data-test-id=setup-nano-wallet-cta");
-    this.getStartedCtaButton = page.locator("data-test-id=get-started-cta");
-    this.beCarefulButton = page.locator("data-test-id=be-careful-cta");
-    this.startSetupButton = page.locator("data-test-id=device-howto-cta");
-    this.pincodeCheckbox = page.locator("data-test-id=pincode-private-cb");
-    this.setupPincodeButton = page.locator("data-test-id=device-pincode-cta");
-    this.confirmPincodeButton = page.locator("data-test-id=pincode-howto-cta");
-    this.recoveryPhraseCheckbox = page.locator("data-test-id=recoveryphrase-private-cb");
-    this.recoverySetupButton = page.locator("data-test-id=device-recoveryphrase-cta");
-    this.writeRecoveryPhraseButton = page.locator("data-test-id=use-recovery-sheet");
-    this.confirmRecoveryPhraseButton = page.locator("data-test-id=recovery-howto-3");
-    this.hideRecoveryPhraseButton = page.locator("data-test-id=hide-recovery-cta");
-    this.quizContainer = page.locator("data-test-id=quiz-container");
-    this.quizStartButton = page.locator("data-test-id=quiz-start-cta");
-    this.quizAnswerTopButton = page.locator("data-test-id=quiz-answer-0");
-    this.quizAnswerBottomButton = page.locator("data-test-id=quiz-answer-1");
-    this.quizNextButton = page.locator("data-test-id=quiz-next-cta");
-    this.quizSuccessButton = page.locator("data-test-id=quiz-success-cta");
+    this.newDeviceButton = page.locator("data-test-id=v3-onboarding-new-device");
+    this.connectDeviceButton = page.locator("data-test-id=v3-onboarding-initialized-device");
+    this.restoreDeviceButton = page.locator("data-test-id=v3-onboarding-restore-device");
+    this.pedagogyModal = page.locator("data-test-id=v3-onboarding-pedagogy-modal");
+    this.stepperContinueButton = page.locator("data-test-id=v3-modal-stepper-continue");
+    this.stepperEndButton = page.locator("data-test-id=v3-modal-stepper-end");
+    this.leftArrowBasicsButton = page.locator("data-test-id=v3-pedagogy-left");
+    this.rightArrowBasicsButton = page.locator("data-test-id=v3-pedagogy-right");
+    this.tutorialContinueButton = page.locator("data-test-id=v3-tutorial-continue");
+    this.setupWalletButton = page.locator("data-test-id=v3-setup-nano-wallet-cta");
+    this.getStartedCtaButton = page.locator("data-test-id=v3-get-started-cta");
+    this.startSetupButton = page.locator("data-test-id=v3-device-howto-cta");
+    this.pinCodeCheckbox = page.locator("data-test-id=v3-private-pin-code-checkbox");
+    this.pinCodeDrawer = page.locator("data-test-id=v3-pin-code-drawer");
+    this.recoveryPhraseCheckbox = page.locator("data-test-id=v3-recovery-phrase-checkbox");
+    this.recoveryPhraseLossCheckbox = page.locator("data-test-id=v3-recovery-phrase-loss-checkbox");
+    this.recoverySeedDrawer = page.locator("data-test-id=v3-seed-drawer");
+    this.hideSeedDrawer = page.locator("data-test-id=v3-hide-seed-drawer");
+    this.recoverySetupButton = page.locator("data-test-id=v3-device-recoveryphrase-cta");
+    this.writeRecoveryPhraseButton = page.locator("data-test-id=v3-use-recovery-sheet");
+    this.confirmRecoveryPhraseButton = page.locator("data-test-id=v3-recovery-howto-3");
+    this.hideRecoveryPhraseButton = page.locator("data-test-id=v3-hide-recovery-cta");
+    this.quizContainer = page.locator("data-test-id=v3-quiz-container");
+    this.quizStartButton = page.locator("data-test-id=v3-quiz-start-button");
+    this.quizAnswerTopButton = page.locator("data-test-id=v3-quiz-answer-0");
+    this.quizAnswerBottomButton = page.locator("data-test-id=v3-quiz-answer-1");
   }
 
   async getStarted() {
+    await this.getStartedButton.waitFor({ state: "visible" });
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-get-started.png");
     await this.getStartedButton.click();
   }
 
-  async acceptTerms() {
-    await this.termsCheckbox.click();
-    await this.termsSubmitButton.click();
-  }
-
-  async selectDevice(device: "nanoS" | "nanoSP" | "nanoX" | string) {
+  async selectDevice(device: "nanoS" | "nanoX" | "nanoSPlus") {
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-device-selection.png");
+    await this.page.hover(`[data-test-id=v3-container-device-${device}]`);
     await this.selectDeviceButton(device).click();
   }
 
@@ -89,32 +95,66 @@ export class OnboardingPage {
     await this.newDeviceButton.click();
   }
 
-  async basicsCarrouselRight() {
-    await this.rightArrowBasicsButton.click();
+  async restoreDevice() {
+    await this.restoreDeviceButton.click();
   }
 
-  async startSetup() {
-    await this.getStartedCtaButton.click();
-    await this.beCarefulButton.click();
-    await this.startSetupButton.click();
+  async pedagogyContinue() {
+    await this.stepperContinueButton.click();
   }
 
-  async setPincode() {
-    await this.pincodeCheckbox.click();
-    await this.setupPincodeButton.click();
-    await this.confirmPincodeButton.click();
+  async pedagogyEnd() {
+    await this.stepperEndButton.click();
   }
 
-  async setPassphrase() {
+  async startTutorial(group: string) {
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "get-started-1.png"]);
+    await this.continueTutorial();
+
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "get-started-2.png"]);
+    await this.continueTutorial();
+  }
+
+  async setPinCode(group: string) {
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "pin-code-1.png"]);
+    await this.acceptPrivatePinCode();
+
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "pin-code-2.png"]);
+    await this.continueTutorial();
+
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "pin-code-3.png"]);
+    await this.continueTutorial();
+
+    expect(await this.page.screenshot()).toMatchSnapshot([group, "pin-code-4.png"]);
+    await this.continuePinDrawer();
+  }
+
+  async continuePinDrawer() {
+    await this.pinCodeDrawer.click();
+  }
+
+  async continueRecoverySeedDrawer() {
+    await this.recoverySeedDrawer.click();
+  }
+
+  async continueHideSeedDrawer() {
+    await this.hideSeedDrawer.click();
+  }
+
+  async continueTutorial() {
+    await this.tutorialContinueButton.click();
+  }
+
+  async acceptPrivatePinCode() {
+    await this.pinCodeCheckbox.click();
+  }
+
+  async acceptRecoveryPhrase() {
     await this.recoveryPhraseCheckbox.click();
-    await this.recoverySetupButton.click();
-    await this.writeRecoveryPhraseButton.click();
-    await this.confirmRecoveryPhraseButton.click();
-    await this.hideRecoveryPhraseButton.click();
   }
 
-  async setupWallet() {
-    await this.setupWalletButton.click();
+  async acceptRecoveryPhraseLoss() {
+    await this.recoveryPhraseLossCheckbox.click();
   }
 
   async startQuiz() {
@@ -130,18 +170,31 @@ export class OnboardingPage {
   }
 
   async quizNextQuestion() {
-    await this.quizNextButton.click();
+    await this.stepperContinueButton.click();
   }
 
   async quizEnd() {
-    await this.quizSuccessButton.click();
+    await this.stepperEndButton.click();
   }
 
   async checkDevice() {
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-genuine-check.png");
     await this.checkMyNanoButton.click();
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-before-genuine-check.png");
+  }
+
+  async genuineCheck() {
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-genuine-checking.png");
+    await this.deviceAction.genuineCheck();
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-genuine-check-done.png");
   }
 
   async continue() {
     await this.continueButton.click();
+  }
+
+  async reachApp() {
+    await this.continue();
+    expect(await this.page.screenshot()).toMatchSnapshot("v3-onboarding-complete.png");
   }
 }
--- apps/ledger-live-desktop/tests/specs/onboarding/connect-device.spec.ts
+++ apps/ledger-live-desktop/tests/specs/onboarding/connect-device.spec.ts
@@ -1,23 +1,22 @@
-import test from "../../fixtures/common";
+import test from "../../../fixtures/common";
 import { expect } from "@playwright/test";
-import { OnboardingPage } from "../../models/OnboardingPage";
-import { DeviceAction } from "../../models/DeviceAction";
+import { OnboardingPage } from "../../../models/OnboardingPage";
 
-const nanos = ["nanoS", "nanoSP", "nanoX"];
+enum Nano {
+  nanoX = "nanoX",
+  nanoS = "nanoS",
+  nanoSP = "nanoSP",
+}
+
+const nanos = [Nano.nanoX, Nano.nanoS, Nano.nanoSP];
 
 test.describe.parallel("Onboarding", () => {
   for (const nano of nanos) {
     test(`[${nano}] Onboarding flow already set up`, async ({ page }) => {
       const onboardingPage = new OnboardingPage(page);
-      const deviceAction = new DeviceAction(page);
 
       await test.step("Get started", async () => {
-        expect(await onboardingPage.getStartedButton).toBeVisible();
         await onboardingPage.getStarted();
-      });
-
-      await test.step("Terms of service", async () => {
-        await onboardingPage.acceptTerms();
       });
 
       await test.step(`[${nano}] Select Device`, async () => {
@@ -25,20 +24,20 @@ test.describe.parallel("Onboarding", () => {
       });
 
       await test.step(`[${nano}] Already set up`, async () => {
+        expect(await page.screenshot()).toMatchSnapshot(`v3-device-connection-${nano}.png`);
         await onboardingPage.connectDevice();
       });
 
       await test.step(`[${nano}] Device genuine check`, async () => {
-        await onboardingPage.continue();
         await onboardingPage.checkDevice();
       });
 
       await test.step("Pass genuine check", async () => {
-        await deviceAction.genuineCheck();
+        await onboardingPage.genuineCheck();
       });
 
       await test.step("Reach app", async () => {
-        await onboardingPage.continue();
+        await onboardingPage.reachApp();
       });
     });
   }
