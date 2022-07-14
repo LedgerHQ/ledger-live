4a70a4dda3b588b628a77d0617bf4132625316c6
Trent Rogers <trentkrogers@gmail.com>
2022-07-14 15:30:58 -0500

    Add estimateMaxSpendable

---
+++ libs/ledger-live-common/src/families/avalanchepchain/js-estimateMaxSpendable.ts
@@ -0,0 +1,26 @@
+import { BigNumber } from "bignumber.js";
+
+import type { AccountLike, Account } from "../../types";
+import { getMainAccount } from "../../account";
+
+import type { Transaction } from "./types";
+
+/**
+ * Returns the maximum possible amount for transaction
+ *
+ * @param {Object} param - the account, parentAccount and transaction
+ */
+const estimateMaxSpendable = async ({
+  account,
+  parentAccount,
+  transaction,
+}: {
+  account: AccountLike;
+  parentAccount?: Account;
+  transaction?: Transaction;
+}): Promise<BigNumber> => {
+  const a = getMainAccount(account, parentAccount);
+  return await a.spendableBalance;
+};
+
+export default estimateMaxSpendable;
