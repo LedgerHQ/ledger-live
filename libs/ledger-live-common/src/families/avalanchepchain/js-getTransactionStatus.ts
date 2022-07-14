93e00d95a13dc3e3cf5d9e430e57453462fbc7d4
Trent Rogers <trentkrogers@gmail.com>
2022-07-14 15:32:17 -0500

    Formatting; add NotEnoughBalanceToDelegate error

--- libs/ledger-live-common/src/families/avalanchepchain/js-getTransactionStatus.ts
+++ libs/ledger-live-common/src/families/avalanchepchain/js-getTransactionStatus.ts
@@ -1,65 +1,67 @@
 import { BigNumber } from "bignumber.js";
 import {
-    NotEnoughBalance,
-    RecipientRequired,
-    InvalidAddress,
-    FeeNotLoaded,
-    AmountRequired,
-    InvalidAddressBecauseDestinationIsAlsoSource,
+  NotEnoughBalance,
+  RecipientRequired,
+  InvalidAddress,
+  FeeNotLoaded,
+  AmountRequired,
+  InvalidAddressBecauseDestinationIsAlsoSource,
+  NotEnoughBalanceToDelegate,
 } from "@ledgerhq/errors";
 import { Account, TransactionStatus } from "../../types";
 import { Transaction } from "./types";
 import { isValidAddress } from "./logic";
+import { AVAX_MINIMUM_STAKE_AMOUNT } from "./utils";
 
 const getTransactionStatus = async (
-    account: Account,
-    transaction: Transaction
+  account: Account,
+  transaction: Transaction
 ): Promise<TransactionStatus> => {
-    const errors: any = {};
-    const warnings: any = {};
-    const useAllAmount = !!transaction.useAllAmount;
+  const errors: any = {};
+  const warnings: any = {};
+  const useAllAmount = !!transaction.useAllAmount;
 
-    if (account.freshAddress === transaction.recipient) {
-        errors.recipient = new InvalidAddressBecauseDestinationIsAlsoSource();
-    }
+  if (account.freshAddress === transaction.recipient) {
+    errors.recipient = new InvalidAddressBecauseDestinationIsAlsoSource();
+  }
 
-    const estimatedFees = transaction.fees || new BigNumber(0);
+  const estimatedFees = transaction.fees || new BigNumber(0);
 
-    const amount = useAllAmount
-        ? account.spendableBalance.minus(estimatedFees)
-        : new BigNumber(transaction.amount);
+  const amount = useAllAmount
+    ? account.spendableBalance.minus(estimatedFees)
+    : new BigNumber(transaction.amount);
 
-    if (amount.lte(0) && !transaction.useAllAmount) {
-        errors.amount = new AmountRequired();
-    }
+  if (amount.lte(0) && !transaction.useAllAmount) {
+    errors.amount = new AmountRequired();
+  }
 
-    const totalSpent = amount.plus(estimatedFees);
+  if (amount.lte(AVAX_MINIMUM_STAKE_AMOUNT)) {
+    console.log("SPENDABLE BALANCE: ", account.spendableBalance);
+    console.log("AMOUNT: ", amount);
+    errors.amount = new NotEnoughBalanceToDelegate(); //switched this from NotEnoughBalanceToDelegate
+  }
 
-    if (totalSpent.gt(account.spendableBalance)) {
-        errors.amount = new NotEnoughBalance();
-    }
+  const totalSpent = amount.plus(estimatedFees);
 
-    if (!errors.amount && account.spendableBalance.lt(estimatedFees)) {
-        errors.amount = new NotEnoughBalance();
-    }
+  if (totalSpent.gt(account.spendableBalance)) {
+    errors.amount = new NotEnoughBalance();
+  }
 
-    if (transaction.mode === "send") {
-        if (!transaction.recipient) {
-            errors.recipient = new RecipientRequired();
-        } else if (!isValidAddress(transaction.recipient)) {
-            errors.recipient = new InvalidAddress("", {
-                currencyName: account.currency.name,
-            });
-        }
-    }
+  if (!errors.amount && account.spendableBalance.lt(estimatedFees)) {
+    errors.amount = new NotEnoughBalance();
+  }
 
-    return Promise.resolve({
-        errors,
-        warnings,
-        estimatedFees,
-        amount,
-        totalSpent,
-    });
+  if (!transaction.recipient) {
+    errors.recipient = new RecipientRequired();
+  }
+
+  return Promise.resolve({
+    errors,
+    warnings,
+    estimatedFees,
+    amount,
+    totalSpent,
+  });
 };
 
 export default getTransactionStatus;
