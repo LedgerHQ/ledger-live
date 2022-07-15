e0c47abccaacaadc4ab692b7f3ec1ec096851260
Trent Rogers <trentkrogers@gmail.com>
2022-07-15 14:55:58 -0500

    P-Chain staking dashboard and modal WIP

---
+++ apps/ledger-live-desktop/src/renderer/families/avalanchepchain/Delegation/Header.js
@@ -0,0 +1,53 @@
+// @flow
+
+import React from "react";
+import { Trans } from "react-i18next";
+import styled from "styled-components";
+import Box from "~/renderer/components/Box/Box";
+import { HeaderWrapper } from "~/renderer/components/TableContainer";
+import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
+
+export const TableLine: ThemedComponent<{}> = styled(Box).attrs(() => ({
+  ff: "Inter|SemiBold",
+  color: "palette.text.shade60",
+  horizontal: true,
+  alignItems: "center",
+  justifyContent: "flex-start",
+  fontSize: 3,
+  flex: 1.125,
+  pr: 2,
+}))`
+  box-sizing: border-box;
+  &:last-child {
+    justify-content: flex-end;
+    flex: 0.5;
+    text-align: right;
+    white-space: nowrap;
+  }
+`;
+
+export const Header = () => (
+    <HeaderWrapper>
+      <TableLine>
+        <Trans i18nKey="avalanchepchain.delegation.validator" />
+      </TableLine>
+      <TableLine>
+        <Trans i18nKey="avalanchepchain.delegation.startTime" />
+      </TableLine>
+      <TableLine>
+        <Trans i18nKey="avalanchepchain.delegation.endTime" />
+      </TableLine>
+      <TableLine>
+        <Trans i18nKey="avalanchepchain.delegation.stakeAmount" />
+      </TableLine>
+      <TableLine>
+        <Trans i18nKey="avalanchepchain.delegation.potentialReward" />
+      </TableLine>
+    </HeaderWrapper>
+  );
+
+  //node id
+  //stake amount
+  //start time
+  //end time
+  //potential reward
