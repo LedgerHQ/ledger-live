3820999a2d0a6ba5544fe18a95ca7e1e43e9c92d
Trent Rogers <trentkrogers@gmail.com>
2022-07-15 14:56:58 -0500

    Update imports

--- libs/ledger-live-common/src/families/avalanchepchain/bridge/js.ts
+++ libs/ledger-live-common/src/families/avalanchepchain/bridge/js.ts
@@ -2,23 +2,18 @@ import type { AccountBridge, CurrencyBridge } from "../../../types";
 import type { Transaction } from '../types';
 import { makeAccountBridgeReceive } from "../../../bridge/jsHelpers";
 import { sync, scanAccounts } from "../js-synchronisation";
-import { createTransaction, prepareTransaction } from '../js-transaction';
+import {
+  createTransaction,
+  prepareTransaction,
+  updateTransaction,
+} from "../js-transaction";
 import getTransactionStatus from "../js-getTransactionStatus";
 import signOperation from "../js-signOperation";
 import broadcast from "../js-broadcast";
-
-const preload = () => Promise.resolve({});
-const hydrate = (): void => { };
+import { preload, hydrate } from "../js-preload-data";
+import estimateMaxSpendable from "../js-estimateMaxSpendable";
 
 const receive = makeAccountBridgeReceive();
-
-const updateTransaction = () => {
-    throw new Error("updateTransaction not implemented");
-};
-
-const estimateMaxSpendable = () => {
-    throw new Error("estimateMaxSpendable not implemented");
-};
 
 const currencyBridge: CurrencyBridge = {
     preload,
