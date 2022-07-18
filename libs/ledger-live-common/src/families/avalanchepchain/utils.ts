bd361471fb88ff5bdbe6d579cc41165eb98bf485
Trent Rogers <trentkrogers@gmail.com>
2022-07-18 10:21:20 -0500

    Add canDelegate

--- libs/ledger-live-common/src/families/avalanchepchain/utils.ts
+++ libs/ledger-live-common/src/families/avalanchepchain/utils.ts
@@ -1,10 +1,15 @@
+import type { Account } from "../../types";
+
 export const FIGMENT_AVALANCHE_VALIDATOR_NODES = [
   "NodeID-GW7CvXwkPFDsfFHrix1SA148NHF5ZnSXs",
   "NodeID-CRg2AxBCkLjY6T7oiS7ijrCFsdAVmPNuY",
 ];
 
+export const AVAX_MINIMUM_STAKE_AMOUNT = 1000000000; //fuji testnet
+// export const AVAX_MINIMUM_STAKE_AMOUNT = 25000000000; //mainnet
+
 export const isDefaultValidatorNode = (nodeID: string): boolean =>
   FIGMENT_AVALANCHE_VALIDATOR_NODES.includes(nodeID);
 
-// export const AVAX_MINIMUM_STAKE_AMOUNT = 25000000000; //mainnet
-export const AVAX_MINIMUM_STAKE_AMOUNT = 1000000000; //fuji testnet
+export const canDelegate = (account: Account) =>
+  account.spendableBalance.gt(AVAX_MINIMUM_STAKE_AMOUNT);
