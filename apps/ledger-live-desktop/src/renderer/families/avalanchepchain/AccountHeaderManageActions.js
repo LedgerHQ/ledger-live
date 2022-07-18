e033250a20f523e389aa6adac9c8927948d17fbb
Trent Rogers <trentkrogers@gmail.com>
2022-07-18 10:18:56 -0500

    Add AccountHeaderManageActions

---
+++ apps/ledger-live-desktop/src/renderer/families/avalanchepchain/AccountHeaderManageActions.js
@@ -0,0 +1,49 @@
+// @flow
+import { getMainAccount } from "@ledgerhq/live-common/lib/account";
+import type { Account, AccountLike } from "@ledgerhq/live-common/lib/types";
+import invariant from "invariant";
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { openModal } from "~/renderer/actions/modals";
+import IconCoins from "~/renderer/icons/Coins";
+import { canDelegate } from "@ledgerhq/live-common/lib/families/avalanchepchain/utils";
+
+type Props = {
+  account: AccountLike,
+  parentAccount: ?Account,
+};
+
+const AccountHeaderActions = ({ account, parentAccount }: Props) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const mainAccount = getMainAccount(account, parentAccount);
+
+  const { avalanchePChainResources } = mainAccount;
+  invariant(avalanchePChainResources, "Avalanche account with avalanchePChainResources expected");
+  const isDelegationEnabled = canDelegate(account);
+
+  const hasDelegations = avalanchePChainResources.delegations.length > 0;
+
+  const onClick = useCallback(() => {
+    dispatch(
+      openModal("MODAL_AVALANCHE_DELEGATE", {
+        account,
+      }),
+    );
+  }, [dispatch, account, hasDelegations]);
+
+  if (parentAccount) return null;
+
+  return [
+    {
+      key: "Stake",
+      onClick: onClick,
+      icon: IconCoins,
+      disabled: !isDelegationEnabled,
+      label: t("account.stake"),
+    },
+  ];
+};
+
+export default AccountHeaderActions;
