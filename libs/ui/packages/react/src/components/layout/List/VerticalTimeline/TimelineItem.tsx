a9da2596ee15bb55449c4a12acfc2dd5432857e5
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-07-19 12:08:18 +0200

    support: migrate Vertical timeline component from native to UI

---
+++ libs/ui/packages/react/src/components/layout/List/VerticalTimeline/TimelineItem.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { Theme } from "src/styles/theme";
+import styled from "styled-components";
+
+import { Item, ItemStatus } from ".";
+import { Flex } from "../..";
+import { Text } from "../../..";
+import TimelineIndicator from "./TimelineIndicator";
+
+export type Props = {
+  item: Item;
+  isFirstItem?: boolean;
+  isLastItem?: boolean;
+};
+
+const getContainerBackground = (theme: Theme, status: ItemStatus, isLastItem?: boolean) => {
+  if (isLastItem && status === "completed") {
+    return theme.colors.success.c30;
+  } else if (status === "completed") {
+    return theme.colors.primary.c20;
+  } else if (status === "active") {
+    return theme.colors.neutral.c20;
+  }
+  return theme.colors.neutral.c30;
+};
+
+const getContainerBorder = (theme: Theme, status: ItemStatus, isLastItem?: boolean) => {
+  if (isLastItem && status === "completed") {
+    return theme.colors.success.c30;
+  } else if (isLastItem && status === "active") {
+    return theme.colors.success.c100;
+  } else if (status === "completed") {
+    return theme.colors.primary.c20;
+  } else if (status === "active") {
+    return theme.colors.primary.c80;
+  }
+  return theme.colors.neutral.c30;
+};
+
+const Container = styled(Flex)<{ status: ItemStatus; isLastItem?: boolean }>`
+  flex: 1;
+  border-radius: ${(p) => p.theme.radii[2]}px;
+  background: ${(p) => getContainerBackground(p.theme, p.status, p.isLastItem)};
+  border: 1px solid ${(p) => getContainerBorder(p.theme, p.status, p.isLastItem)};
+  padding: 20px 16px;
+`;
+
+export default function TimelineItem({ item, isFirstItem, isLastItem }: Props) {
+  return (
+    <Flex flexDirection="row">
+      <TimelineIndicator
+        status={item.status}
+        isFirstItem={isFirstItem}
+        isLastItem={isLastItem}
+        mr={4}
+      />
+      <Container status={item.status} isLastItem={isLastItem} mb={4}>
+        <Text
+          variant="body"
+          color={
+            item.status === "inactive" ? "neutral.c80" : isLastItem ? "success.c100" : "primary.c90"
+          }
+        >
+          {item.title}
+        </Text>
+        {item.renderBody && (
+          <Flex position="relative">
+            <Flex pt={6} position="absolute" opacity={0}>
+              {item.renderBody(false)}
+            </Flex>
+            <Flex pt={6}>{item.renderBody(item.status === "active")}</Flex>
+          </Flex>
+        )}
+      </Container>
+    </Flex>
+  );
+}
