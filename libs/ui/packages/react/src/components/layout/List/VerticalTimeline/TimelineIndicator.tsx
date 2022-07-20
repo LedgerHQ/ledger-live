407709c418a46baa2bdb7a25fc049ffa285150bf
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-07-20 11:21:56 +0200

    fix: top segment height

--- libs/ui/packages/react/src/components/layout/List/VerticalTimeline/TimelineIndicator.tsx
+++ libs/ui/packages/react/src/components/layout/List/VerticalTimeline/TimelineIndicator.tsx
@@ -7,7 +7,7 @@ import { ItemStatus } from "./index";
 import { Theme } from "src/styles/theme";
 
 const TopSegment = styled(Flex)<{ status: ItemStatus; hidden?: boolean }>`
-  height: 20px;
+  height: 18px;
   border-width: ${(p) => (p.hidden ? 0 : 1)}px;
   border-style: dashed;
   border-color: ${(p) =>
