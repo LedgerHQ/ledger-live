6db5ae69595f73e661eef6e2c67026051f75cf5a
Thomas Roger Lux <thomas@rogerlux.dev>
2022-07-20 17:26:07 +0200

    feat(desktop): WIP add new sync onboarding flow entry point

---
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Flex } from "@ledgerhq/react-ui";
+import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
+
+import SyncOnboardingPairing from "./Pairing";
+import SyncOnboardingManual from "./Manual";
+
+const SyncOnboarding = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <Flex width="100%" height="100%" position="relative">
+      <Switch>
+        <Route
+          exact
+          path={path}
+          render={() => <Redirect to={`${path}/manual` /* TODO put pairing instead */} />}
+        />
+        <Route path={`${path}/pairing`} render={props => <SyncOnboardingPairing {...props} />} />
+        <Route path={`${path}/manual`} render={props => <SyncOnboardingManual {...props} />} />
+      </Switch>
+    </Flex>
+  );
+};
+
+export default SyncOnboarding;
