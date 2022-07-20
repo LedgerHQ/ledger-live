9909d5a6062c4de19dd0cdd53ecf99db0ddfcfd0
Juan Cortés Ross <juan@bohem.io>
2022-07-20 11:51:14 +0200

    feat/LIVE-537 Changes to iOS implementation after Android

--- libs/ledgerjs/packages/hw-transport-react-native-ble/ios/HwTransportReactNativeBle.m
+++ libs/ledgerjs/packages/hw-transport-react-native-ble/ios/HwTransportReactNativeBle.m
@@ -4,7 +4,7 @@
 @interface RCT_EXTERN_MODULE(HwTransportReactNativeBle, RCTEventEmitter)
     RCT_EXTERN_METHOD(listen: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
     RCT_EXTERN_METHOD(stop: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-    RCT_EXTERN_METHOD(connect: (NSString *)string serviceUUID: (NSString *)serviceUUID resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+    RCT_EXTERN_METHOD(connect: (NSString *)string resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
     RCT_EXTERN_METHOD(disconnect: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
     RCT_EXTERN_METHOD(exchange: (NSString *)apdu resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
     RCT_EXTERN_METHOD(isConnected: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
