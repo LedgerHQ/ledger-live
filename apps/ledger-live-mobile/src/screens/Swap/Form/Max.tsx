617959b92ef39d0f7904705c7f88f8dd42aab2a0
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-07-25 10:32:27 +0200

    show max spendable

--- apps/ledger-live-mobile/src/screens/Swap/Form/Max.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/Form/Max.tsx
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BigNumber } from "bignumber.js";
 import { useTranslation } from "react-i18next";
 import { Flex, Text, Switch } from "@ledgerhq/native-ui";
 import { SwapTransactionType } from "@ledgerhq/live-common/exchange/swap/types";
+import { getAccountBridge } from "@ledgerhq/live-common/bridge/index";
+import CurrencyUnitValue from "../../../components/CurrencyUnitValue";
 
 export function Max({ swapTx }: { swapTx: SwapTransactionType }) {
   const { t } = useTranslation();
+
+  const [max, setMax] = useState<BigNumber>();
+
+  useEffect(() => {
+    if (!swapTx.swap.from.account) {
+      return;
+    }
+
+    getAccountBridge(swapTx.swap.from.account, swapTx.swap.from.parentAccount)
+      .estimateMaxSpendable({
+        account: swapTx.swap.from.account,
+        parentAccount: swapTx.swap.from.parentAccount,
+      })
+      .then(estimate => {
+        setMax(estimate);
+      });
+  }, [swapTx.swap.from]);
+
   return (
     <Flex
       flexDirection="row"
       alignItems="center"
-      justifyContent="flex-end"
-      paddingY={4}
+      justifyContent="space-between"
     >
-      <Flex paddingRight={2}>
-        <Text variant="small">{t("transfer.swap2.form.amount.useMax")}</Text>
+      <Flex>
+        <Text variant="small">{t("transfer.swap2.form.amount.available")}</Text>
+
+        {max ? (
+          <Text variant="small">
+            <CurrencyUnitValue
+              showCode
+              unit={swapTx.swap.from!.currency!.units[0]}
+              value={max}
+            />
+          </Text>
+        ) : (
+          <Text variant="small">-</Text>
+        )}
       </Flex>
-      <Switch checked={swapTx.swap.isMaxEnabled} onChange={swapTx.toggleMax} />
+
+      <Flex flexDirection="row" alignItems="center" paddingY={4}>
+        <Text variant="small" paddingRight={2}>
+          {t("transfer.swap2.form.amount.useMax")}
+        </Text>
+
+        <Switch
+          checked={swapTx.swap.isMaxEnabled}
+          onChange={swapTx.toggleMax}
+        />
+      </Flex>
     </Flex>
   );
 }
