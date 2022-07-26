ac7dd8f9f353e10b6b6d84ec6f7ece7ebd373679
lambertkevin <kevin.lambert@ledger.fr>
2022-07-26 15:36:09 +0200

    Add final EVM family bridge

---
+++ libs/ledger-live-common/src/families/evm/bridge/js.ts
@@ -0,0 +1,45 @@
+import type { AccountBridge, CurrencyBridge } from "@ledgerhq/types-live";
+import { makeAccountBridgeReceive } from "../../../bridge/jsHelpers";
+import { getTransactionStatus } from "../getTransactionStatus";
+import type { Transaction as EvmTransaction } from "../types";
+import { prepareTransaction } from "../prepareTransaction";
+import { createTransaction } from "../createTransaction";
+import { sync, scanAccounts } from "../synchronization";
+import { signOperation } from "../signOperation";
+import { broadcast } from "../broadcast";
+
+const receive = makeAccountBridgeReceive();
+
+const updateTransaction: AccountBridge<EvmTransaction>["updateTransaction"] = (
+  transaction,
+  patch
+) => {
+  return { ...transaction, ...patch } as EvmTransaction;
+};
+
+const preload = async (): Promise<Record<any, any>> => Promise.resolve({});
+
+const hydrate = (): void => {};
+
+const currencyBridge: CurrencyBridge = {
+  preload,
+  hydrate,
+  scanAccounts,
+};
+
+const accountBridge: AccountBridge<EvmTransaction> = {
+  createTransaction,
+  updateTransaction,
+  prepareTransaction,
+  getTransactionStatus,
+  sync,
+  receive,
+  signOperation,
+  broadcast,
+  estimateMaxSpendable: async () => ({} as any),
+};
+
+export default {
+  currencyBridge,
+  accountBridge,
+};
