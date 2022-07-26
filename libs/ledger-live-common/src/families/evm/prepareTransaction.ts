e4e063abf4951ac978092e138311c3f762c91538
lambertkevin <kevin.lambert@ledger.fr>
2022-07-26 15:23:56 +0200

    Add EVM family related files for transactions

---
+++ libs/ledger-live-common/src/families/evm/prepareTransaction.ts
@@ -0,0 +1,63 @@
+import BigNumber from "bignumber.js";
+import { getFeesEstimation, getGasEstimation } from "../../api/Evm";
+import {
+  EvmTransactionEIP1559,
+  EvmTransactionLegacy,
+  Transaction as EvmTransaction,
+} from "./types";
+import { Account } from "@ledgerhq/types-live";
+import { estimateMaxSpendable } from "./estimateMaxSpendable";
+import { validateRecipient } from "./getTransactionStatus";
+
+export const prepareTransaction = async (
+  account: Account,
+  transaction: EvmTransaction
+): Promise<EvmTransaction> => {
+  const { currency } = account;
+
+  const [validationErrors] = validateRecipient(account, transaction);
+  const { recipient: recipientErrors } = validationErrors || {};
+  const [gasLimit, feeData] = await Promise.all([
+    // Validating recipient first cause estimating a transaction with a wrong recipient will throw an error
+    recipientErrors
+      ? Promise.resolve(new BigNumber(21000))
+      : getGasEstimation(currency, transaction),
+    // Fee data is not dependent on the gasEstimation so they can be triggered in parallel
+    getFeesEstimation(currency),
+  ]);
+
+  // First the transaction is creating with its correct type, in order for the `estimateMaxSpendable` to be correct
+  // Doing the `estimateMaxSpendable` first would give a potentially incorrect amount if the transaction if transform from a
+  // type 2 to a type 0 (1559 to Legacy)
+  const typedTransaction = (() => {
+    // If the blockchain is supporting EIP-1559, use maxFeePerGas & maxPriorityFeePerGas
+    if (feeData.maxFeePerGas && feeData.maxPriorityFeePerGas) {
+      delete transaction.gasPrice;
+      return {
+        ...transaction,
+        gasLimit,
+        maxFeePerGas: feeData.maxFeePerGas || undefined,
+        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas || undefined,
+        type: 2,
+      } as EvmTransactionEIP1559;
+    }
+
+    // Else just use a legacy transaction
+    delete transaction.maxFeePerGas;
+    delete transaction.maxPriorityFeePerGas;
+    return {
+      ...transaction,
+      gasLimit,
+      gasPrice: feeData.gasPrice || new BigNumber(0),
+      type: 0,
+    } as EvmTransactionLegacy;
+  })();
+  const amount = transaction.useAllAmount
+    ? await estimateMaxSpendable({ account, transaction: typedTransaction })
+    : transaction.amount;
+
+  return {
+    ...typedTransaction,
+    amount,
+  };
+};
---
+++ libs/ledger-live-common/src/families/evm/transaction.ts
@@ -0,0 +1,175 @@
+import { ethers } from "ethers";
+import type {
+  EvmTransactionEIP1559,
+  EvmTransactionLegacy,
+  Transaction as EvmTransaction,
+  TransactionRaw as EvmTransactionRaw,
+} from "./types";
+import { BigNumber } from "bignumber.js";
+import type { Account } from "@ledgerhq/types-live";
+import {
+  formatTransactionStatusCommon as formatTransactionStatus,
+  fromTransactionCommonRaw,
+  fromTransactionStatusRawCommon as fromTransactionStatusRaw,
+  toTransactionCommonRaw,
+  toTransactionStatusRawCommon as toTransactionStatusRaw,
+} from "../../transaction/common";
+import { getAccountUnit } from "../../account";
+import { formatCurrencyUnit } from "../../currencies";
+import { getTransactionCount } from "../../api/Evm";
+
+/**
+ * Format the transaction for the CLI
+ */
+export const formatTransaction = (
+  { mode, amount, recipient, useAllAmount }: EvmTransaction,
+  account: Account
+): string =>
+  `
+${mode.toUpperCase()} ${
+    useAllAmount
+      ? "MAX"
+      : amount.isZero()
+      ? ""
+      : " " +
+        formatCurrencyUnit(getAccountUnit(account), amount, {
+          showCode: true,
+          disableRounding: true,
+        })
+  }${recipient ? `\nTO ${recipient}` : ""}`;
+
+/**
+ * Serializer raw to transaction
+ */
+export const fromTransactionRaw = (
+  rawTx: EvmTransactionRaw
+): EvmTransaction => {
+  const common = fromTransactionCommonRaw(rawTx);
+  const tx: Partial<EvmTransaction> = {
+    ...common,
+    family: rawTx.family,
+    mode: rawTx.mode,
+    chainId: rawTx.chainId,
+    nonce: rawTx.nonce,
+    gasLimit: new BigNumber(rawTx.gasLimit),
+    feesStrategy: rawTx.feesStrategy,
+    type: Number(rawTx.type), // if rawTx.type is undefined, transaction will be considered legacy and therefore type 0
+  };
+
+  if (rawTx.data) {
+    tx.data = Buffer.from(rawTx.data, "hex");
+  }
+
+  if (rawTx.gasPrice) {
+    tx.gasPrice = new BigNumber(rawTx.gasPrice);
+  }
+
+  if (rawTx.maxFeePerGas) {
+    tx.maxFeePerGas = new BigNumber(rawTx.maxFeePerGas);
+  }
+
+  if (rawTx.maxPriorityFeePerGas) {
+    tx.maxPriorityFeePerGas = new BigNumber(rawTx.maxPriorityFeePerGas);
+  }
+
+  return tx as EvmTransaction;
+};
+
+/**
+ * Serializer transaction to raw
+ */
+export const toTransactionRaw = (tx: EvmTransaction): EvmTransactionRaw => {
+  const common = toTransactionCommonRaw(tx);
+  const txRaw: Partial<EvmTransactionRaw> = {
+    ...common,
+    family: tx.family,
+    mode: tx.mode,
+    chainId: tx.chainId,
+    nonce: tx.nonce,
+    gasLimit: tx.gasLimit.toFixed(),
+    feesStrategy: tx.feesStrategy,
+    type: tx.type,
+  };
+
+  if (tx.data) {
+    txRaw.data = Buffer.from(tx.data).toString("hex");
+  }
+
+  if (tx.gasPrice) {
+    txRaw.gasPrice = tx.gasPrice?.toFixed();
+  }
+
+  if (tx.maxFeePerGas) {
+    txRaw.maxFeePerGas = tx.maxFeePerGas?.toFixed();
+  }
+
+  if (tx.maxPriorityFeePerGas) {
+    txRaw.maxPriorityFeePerGas = tx.maxPriorityFeePerGas?.toFixed();
+  }
+
+  return txRaw as EvmTransactionRaw;
+};
+
+/**
+ * Adapter to convert a Ledger Live transaction to an Ethers transaction
+ */
+export const transactionToEthersTransaction = (
+  tx: EvmTransaction
+): ethers.Transaction => {
+  const ethersTx = {
+    to: tx.recipient,
+    value: tx.amount
+      ? ethers.BigNumber.from(tx.amount.toFixed())
+      : ethers.BigNumber.from(0),
+    data: tx.data ? `0x${tx.data.toString("hex")}` : undefined,
+    gasLimit: ethers.BigNumber.from(tx.gasLimit.toFixed()),
+    nonce: tx.nonce,
+    chainId: tx.chainId,
+    type: tx.type,
+  } as Partial<ethers.Transaction>;
+
+  // is EIP-1559 transaction (type 2)
+  if (tx.type === 2) {
+    ethersTx.maxFeePerGas = ethers.BigNumber.from(
+      (tx as EvmTransactionEIP1559).maxFeePerGas.toFixed()
+    );
+    ethersTx.maxPriorityFeePerGas = ethers.BigNumber.from(
+      (tx as EvmTransactionEIP1559).maxPriorityFeePerGas.toFixed()
+    );
+  } else {
+    // is Legacy transaction (type 0)
+    ethersTx.gasPrice = ethers.BigNumber.from(
+      (tx as EvmTransactionLegacy).gasPrice.toFixed()
+    );
+  }
+
+  return ethersTx as ethers.Transaction;
+};
+
+/**
+ * Create an unsigned transaction from a Ledger Live transaction.
+ * Usually called "buildTransaction"
+ */
+export const transactionToUnsignedTransaction = async (
+  account: Account,
+  tx: EvmTransaction
+): Promise<EvmTransaction> => {
+  const { currency, freshAddress } = account;
+  const nonce = await getTransactionCount(currency, freshAddress);
+
+  return {
+    ...tx,
+    nonce,
+  };
+};
+
+export default {
+  formatTransaction,
+  fromTransactionRaw,
+  toTransactionRaw,
+  toTransactionStatusRaw,
+  transactionToEthersTransaction,
+  transactionToUnsignedTransaction,
+  formatTransactionStatus,
+  fromTransactionStatusRaw,
+};
