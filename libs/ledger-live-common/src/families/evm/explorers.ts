dee6bfebca48e7f63d9b88d6b5224c962f73871a
lambertkevin <kevin.lambert@ledger.fr>
2022-07-26 13:13:31 +0200

    Add EVM api for RPC communication

---
+++ libs/ledger-live-common/src/families/evm/explorers.ts
@@ -0,0 +1,12 @@
+export default {
+  ethereum: "https://api.etherscan.io",
+  avalanche: "https://api.snowtrace.io",
+  polygon: "https://api.polygonscan.com",
+  cronos: "https://api.cronoscan.com",
+  fantom: "https://api.ftmscan.com",
+  arbitrum: "https://api.arbiscan.io",
+  optimism: "https://api-optimistic.etherscan.io",
+  moonbeam: "https://api-moonbeam.moonscan.io",
+  moonriver: "https://api-moonriver.moonscan.io",
+  celo: "https://api.celoscan.io",
+};
