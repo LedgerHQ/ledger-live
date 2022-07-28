84404f0dd17677ff1a561acfb333173b64a6f936
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-07-28 09:37:14 +0200

    chore(lld): renaming and configuring router

--- apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/index.tsx
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/index.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { Flex } from "@ledgerhq/react-ui";
-import { Redirect, Route, Switch, useRouteMatch, RouteComponentProps, useParams } from "react-router-dom";
+import { Route, Switch, useRouteMatch, RouteComponentProps } from "react-router-dom";
 import { withV3StyleProvider } from "~/renderer/styles/StyleProviderV3";
-import SyncOnboardingPairing, { SyncOnboardingPairingProps } from "./Pairing";
+import SyncOnboardingDeviceConnection, { SyncOnboardingDeviceConnectionProps } from "./DeviceConnection";
 import SyncOnboardingManual from "./Manual";
 import CompletionScreen from "./Manual/CompletionScreen";
 
-export type SyncOnboardingPairingRouteProps = RouteComponentProps<SyncOnboardingPairingProps>;
+export type SyncOnboardingDeviceConnectionRouteProps = RouteComponentProps<SyncOnboardingDeviceConnectionProps>;
 
 const SyncOnboarding = () => {
   const { path } = useRouteMatch();
-
   return (
     <Flex width="100%" height="100%" position="relative">
       <Switch>
-        <Route
-          exact
-          path={`${path}/:deviceModelId`}
-          render={(props: SyncOnboardingPairingRouteProps) => <Redirect to={`${path}/pairing/${props.match.params.deviceModelId}`} />}
-        />
-        <Route path={`${path}/pairing/:deviceModelId`} render={(props: SyncOnboardingPairingRouteProps) => <SyncOnboardingPairing {...props.match.params} />} />
-        <Route path={`${path}/manual`} render={props => <SyncOnboardingManual {...props} />} />
+        <Route exact path={[`${path}/manual`]} render={() => <SyncOnboardingManual />} />
+        <Route exact path={[`${path}/:deviceModelId`, `${path}/connection/:deviceModelId`]} render={(routeProps: SyncOnboardingDeviceConnectionRouteProps) => <SyncOnboardingDeviceConnection {...routeProps.match.params} />} />
         <Route path={`${path}/completion`} render={props => <CompletionScreen {...props} />} />
       </Switch>
     </Flex>
