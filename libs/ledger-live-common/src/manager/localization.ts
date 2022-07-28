c2f714a91241ce8aa380f03264201b2e3a2fad0e
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-07-28 17:27:39 +0200

    FAT-127 Support for lo4 test firmware + use of prod URLs

--- libs/ledger-live-common/src/manager/localization.ts
+++ libs/ledger-live-common/src/manager/localization.ts
@@ -2,7 +2,7 @@ import { DeviceModelId } from "@ledgerhq/devices";
 import { satisfies as versionSatisfies } from "semver";
 
 const deviceVersionRangesForLocalization: { [key in DeviceModelId]?: string } = {
-  nanoX: "=2.1.0-lo2",
+  nanoX: "=2.1.0-lo2 || =2.1.0-lo4",
 };
 
 export const isDeviceLocalizationSupported = (seVersion: string, modelId?: DeviceModelId) =>
