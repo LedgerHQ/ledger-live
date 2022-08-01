321edc6eb44265e1a4b898a94f046f00beacf467
mdeidda-ledger <marco.deidda@ledger.fr>
2022-08-01 16:24:00 +0200

    add open Ada account

--- apps/ledger-live-desktop/tests/models/AccountsPage.ts
+++ apps/ledger-live-desktop/tests/models/AccountsPage.ts
@@ -1,15 +1,22 @@
 import { Page, Locator } from '@playwright/test';
+import { syncBuiltinESMExports } from 'module';
 
 export class AccountsPage {
   readonly page: Page;
   readonly addAccountButton: Locator;
+  readonly accountTile : Locator;
   
   constructor(page: Page) {
     this.page = page;
     this.addAccountButton = page.locator('data-test-id=accounts-add-account-button');
+    this.accountTile = page.locator(`text=Cardano 1`); //(accountName: string) : Locator => 
   }
 
   async openAddAccountModal() {
     await this.addAccountButton.click();
+  }
+
+  async clickAccountTile() {
+    await this.accountTile.click();
   }
 }
