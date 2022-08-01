e5e16eacdcb4908e41c983169c76cd9336cf9c34
mdeidda-ledger <marco.deidda@ledger.fr>
2022-08-01 16:25:10 +0200

    add open ada account + select send button

--- apps/ledger-live-desktop/tests/specs/accounts/send.spec.ts
+++ apps/ledger-live-desktop/tests/specs/accounts/send.spec.ts
@@ -2,14 +2,23 @@ import test from "../../fixtures/common";
 import { expect } from "@playwright/test";
 import { DeviceAction } from "../../models/DeviceAction";
 import { Layout } from "../../models/Layout";
+import { AccountsPage } from "tests/models/AccountsPage";
+import { AccountPage } from "tests/models/AccountPage";
 
 //test.use({ userdata: "skip-onboarding" });
+test.use({ userdata: "adaAccount" })
 
 test(`ADA send`, async ({ page }) => {
   const deviceAction = new DeviceAction(page);
+  const accountsPage = new AccountsPage(page);
   const layout = new Layout(page);
+  const accountPage = new AccountPage(page);
 
   await test.step(`Open Account`, async () => {
-  
+    await layout.goToAccounts();
+    await accountsPage.clickAccountTile();
+    await accountPage.clickBtnSend();
   });
+
+  //recipient adresse: addr1qxpah604p4hgnvqd4ql5azz9vkzzptvg7u2wjvupk3x59h63qunfksfflaezaqfwysefz7cs2pd7gcd4jqc77hqyu6ms5lkec9
 });
