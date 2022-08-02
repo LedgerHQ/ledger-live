29fbdc423eecee297ed994e265f1069768f1fd18
JunichiSugiura <jun.sugiura.jp@gmail.com>
2022-08-02 17:31:00 +0200

    reset authToken on localStorage(webview) when opening login widget

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Login/FTX.jsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Login/FTX.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { useRef, useMemo } from "react";
+import React, { useRef, useMemo, useCallback, useEffect } from "react";
 
 import type { FTXProviders } from "@ledgerhq/live-common/exchange/swap/utils/index";
 import { getFTXURL } from "@ledgerhq/live-common/exchange/swap/utils/index";
@@ -13,6 +13,24 @@ const FTXLogin = ({ onClose, provider }: Props) => {
   const url = useMemo(() => getFTXURL({ type: "login", provider }), [provider]);
 
   const webviewRef = useRef(null);
+
+  const handleExecuteJavaScript = useCallback(() => {
+    const webview = webviewRef.current;
+    webview.executeJavaScript(`localStorage.setItem('authToken', "")`);
+  }, []);
+
+  useEffect(() => {
+    const webview = webviewRef.current;
+    if (webview) {
+      webview.addEventListener("dom-ready", handleExecuteJavaScript);
+    }
+
+    return () => {
+      if (webview) {
+        webview.removeEventListener("dom-ready", handleExecuteJavaScript);
+      }
+    };
+  }, [handleExecuteJavaScript]);
 
   return <SwapConnectWidget provider={provider} onClose={onClose} url={url} ref={webviewRef} />;
 };
