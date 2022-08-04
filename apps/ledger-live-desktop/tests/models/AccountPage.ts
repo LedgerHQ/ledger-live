a4a603c17f219feba7f1769ddb6b09c429084e7a
mdeidda-ledger <marco.deidda@ledger.fr>
2022-08-04 15:46:01 +0200

    page object

--- apps/ledger-live-desktop/tests/models/AccountPage.ts
+++ apps/ledger-live-desktop/tests/models/AccountPage.ts
@@ -4,10 +4,16 @@ export class AccountPage {
   readonly page: Page;
   readonly buttonsGroup: Locator;
   readonly settingsButton: Locator;
-  
+  readonly buttonSend: Locator;
+
   constructor(page: Page) {
     this.page = page;
-    this.buttonsGroup = page.locator('data-test-id=account-buttons-group');
-    this.settingsButton = page.locator('data-test-id=account-settings-button');
+    this.buttonsGroup = page.locator("data-test-id=account-buttons-group");
+    this.settingsButton = page.locator("data-test-id=account-settings-button");
+    this.buttonSend = page.locator(`text=Send`);
+  }
+
+  async clickBtnSend() {
+    await this.buttonSend.click();
   }
 }
