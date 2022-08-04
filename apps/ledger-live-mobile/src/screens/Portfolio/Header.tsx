e838452da349482b49374cd8186e889deac756f4
Come Grellard <come.grellard@ledger.fr>
2022-08-04 12:08:22 +0200

    feat/LIVE-2808-market-price-section wallet screen header title modified

--- apps/ledger-live-mobile/src/screens/Portfolio/Header.tsx
+++ apps/ledger-live-mobile/src/screens/Portfolio/Header.tsx
@@ -214,7 +214,7 @@ function PortfolioHeader({
                 fontSize="16px"
                 mr={2}
               >
-                {t("tabs.portfolio")}
+                {t("portfolio.walletBalance")}
               </Text>
               {!hidePortfolio && <DiscreetModeButton size={20} />}
             </Flex>
