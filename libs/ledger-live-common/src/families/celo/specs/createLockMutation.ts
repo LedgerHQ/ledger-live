ebbc7ea85065ee28e30c8f8f1d5e8a1ab96081c8
Andy Hass <ah@andrewhass.net>
2022-08-04 08:21:40 -0500

    bugfix: check that Celo bot account is registered before locking tokens

--- libs/ledger-live-common/src/families/celo/specs/createLockMutation.ts
+++ libs/ledger-live-common/src/families/celo/specs/createLockMutation.ts
@@ -1,7 +1,7 @@
 import invariant from "invariant";
 import { getCryptoCurrencyById, parseCurrencyUnit } from "../../../currencies";
 import { MutationSpec } from "../../../bot/types";
-import type { Transaction } from "../types";
+import type { CeloAccount, Transaction } from "../types";
 
 const currency = getCryptoCurrencyById("celo");
 const minimalAmount = parseCurrencyUnit(currency.units[0], "0.005");
@@ -10,6 +10,12 @@ export const createLockMutation = (): MutationSpec<Transaction> => ({
   name: "Celo: Lock",
   maxRun: 1,
   transaction: ({ account, bridge, maxSpendable }) => {
+    const { celoResources } = account as CeloAccount;
+    invariant(
+      celoResources?.registrationStatus,
+      "Celo: Lock Vote | Account is not registered"
+    );
+
     invariant(
       maxSpendable.gt(minimalAmount),
       "Celo:  Lock | balance is too low"
