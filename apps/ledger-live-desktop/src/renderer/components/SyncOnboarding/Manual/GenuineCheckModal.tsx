777ef19fbe3b4f0cca15773890334fb8cce7daa7
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-08-05 15:28:10 +0200

    Feat/software check sync onboarding (#822)
    * add: software check sync onboarding
    
    * add: translations and fix UI
    
    * change typo
    
    * re fix typo

---
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/GenuineCheckModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Flex, Popin, Text } from "@ledgerhq/react-ui";
+import { useTranslation } from "react-i18next";
+
+export type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const GenuineCheckModal = ({ isOpen, onClose }: Props) => {
+  const { t } = useTranslation();
+
+  return (
+    <Popin position="relative" isOpen={isOpen}>
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+        height="100%"
+        padding="40px"
+      >
+        <Text variant="h2" fontSize="20px" color="palette.primary.c80">
+          {t("syncOnboarding.manual.genuineCheckModal.title")}
+        </Text>
+        <Text variant="paragraph" fontSize="25px" textAlign="center">
+          {t("syncOnboarding.manual.genuineCheckModal.paragraph1")}
+        </Text>
+        <Text variant="paragraph" fontSize="18px" color="palette.neutral.c80" textAlign="center">
+          {t("syncOnboarding.manual.genuineCheckModal.paragraph2")}
+        </Text>
+        <Button variant="main" width="100%" onClick={onClose}>
+          {t("syncOnboarding.manual.genuineCheckModal.checkDeviceButton")}
+        </Button>
+      </Flex>
+    </Popin>
+  );
+};
+
+export default GenuineCheckModal;
