f05886ea77169a64b68d39870b050e6506610522
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-08-08 17:40:31 +0200

    Feat/add completion screen onboarding (#865)
    add completion screen to onboarding

--- apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/index.tsx
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/index.tsx
@@ -5,6 +5,7 @@ import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 import { withV3StyleProvider } from "~/renderer/styles/StyleProviderV3";
 import SyncOnboardingPairing from "./Pairing";
 import SyncOnboardingManual from "./Manual";
+import CompletionScreen from "./Manual/CompletionScreen";
 
 const SyncOnboarding = () => {
   const { path } = useRouteMatch();
@@ -19,6 +20,7 @@ const SyncOnboarding = () => {
         />
         <Route path={`${path}/pairing`} render={props => <SyncOnboardingPairing {...props} />} />
         <Route path={`${path}/manual`} render={props => <SyncOnboardingManual {...props} />} />
+        <Route path={`${path}/completion`} render={props => <CompletionScreen {...props} />} />
       </Switch>
     </Flex>
   );
