89bd6a8b5710fa0de4827a099cfdb86ece03a7a8
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-08-08 14:16:38 +0200

    add one more test that verify parallelisation of sync

--- libs/ledger-live-common/src/bridge/react/BridgeSync.test.tsx
+++ libs/ledger-live-common/src/bridge/react/BridgeSync.test.tsx
@@ -3,11 +3,13 @@
  */
 import "../../__tests__/test-helpers/dom-polyfill";
 import React from "react";
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets";
 import { genAccount } from "../../mock/account";
 import { BridgeSync } from "./BridgeSync";
 import { setSupportedCurrencies } from "../../currencies";
+
+jest.setTimeout(30000);
 
 const defaultsBridgeSyncOpts = {
   accounts: [],
@@ -19,7 +21,7 @@ const defaultsBridgeSyncOpts = {
   blacklistedTokenIds: [],
 };
 
-setSupportedCurrencies(["bitcoin"]);
+setSupportedCurrencies(["bitcoin", "ethereum"]);
 
 describe("BridgeSync", () => {
   test("initialize without an error", async () => {
@@ -40,17 +42,67 @@ describe("BridgeSync", () => {
           resolve(null);
         }
       }
-      act(() => {
-        render(
-          <BridgeSync
-            {...defaultsBridgeSyncOpts}
-            accounts={accounts}
-            trackAnalytics={track}
-          >
-            {null}
-          </BridgeSync>
-        );
-      });
+      render(
+        <BridgeSync
+          {...defaultsBridgeSyncOpts}
+          accounts={accounts}
+          trackAnalytics={track}
+        >
+          {null}
+        </BridgeSync>
+      );
+    });
+  });
+
+  test("sync all accounts in parallel at start tracked as reason=initial", async () => {
+    await new Promise((resolve) => {
+      const BTC = getCryptoCurrencyById("bitcoin");
+      const ETH = getCryptoCurrencyById("ethereum");
+      const accounts = [
+        genAccount("2btc1", { currency: BTC }),
+        genAccount("2btc2", { currency: BTC }),
+        genAccount("2eth1", { currency: ETH }),
+      ];
+      const synced: Array<any> = [];
+      let resolveFirst;
+      function prepareCurrency(c) {
+        if (!resolveFirst) {
+          return new Promise((resolve, reject) => {
+            resolveFirst = resolve;
+            setTimeout(
+              reject,
+              5000,
+              new Error("prepareCurrency doesn't seem to be called in parallel")
+            );
+          });
+        }
+        // if we reach here, it means, we managed to have
+        // a SECOND sync that need to prepare currency
+        // so it's a proof that sync correctly runs in parallel
+        // otherwise it would timeout
+        resolveFirst();
+        return Promise.resolve();
+      }
+      function track(type, opts) {
+        expect(type).not.toEqual("SyncError");
+        if (type === "SyncSuccess") {
+          synced.push(opts);
+          expect(opts).toMatchObject({
+            reason: "initial",
+          });
+          if (synced.length === accounts.length) resolve(null);
+        }
+      }
+      render(
+        <BridgeSync
+          {...defaultsBridgeSyncOpts}
+          prepareCurrency={prepareCurrency}
+          accounts={accounts}
+          trackAnalytics={track}
+        >
+          {null}
+        </BridgeSync>
+      );
     });
   });
 });
