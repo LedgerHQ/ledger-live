b214bd7dcc88f6c07c57553a298fe58c04a9ce7e
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-08-16 17:28:39 +0200

    feat(llm): BLE scanning and paring safe area view with headers

--- apps/ledger-live-mobile/src/screens/SyncOnboarding/DeviceModelSelection.tsx
+++ apps/ledger-live-mobile/src/screens/SyncOnboarding/DeviceModelSelection.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback } from "react";
 import { StackScreenProps } from "@react-navigation/stack";
+import { useTranslation } from "react-i18next";
 import { CompositeScreenProps } from "@react-navigation/native";
-import { DeviceModelId } from "@ledgerhq/types-devices";
-import type { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
+import { Flex, Text, Button } from "@ledgerhq/native-ui";
+import { ArrowLeftMedium } from "@ledgerhq/native-ui/assets/icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import type { DeviceModelId } from "@ledgerhq/devices";
+import { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
 import { NavigatorName, ScreenName } from "../../const";
 // TODO: to put OnboardingView in components/move it in root ?
-import OnboardingView from "../Onboarding/OnboardingView";
 import DiscoverCard from "../Discover/DiscoverCard";
 import Illustration from "../../images/illustration/Illustration";
 import type { BaseNavigatorStackParamList } from "../../components/RootNavigator/BaseNavigator";
@@ -48,44 +51,59 @@ export const DeviceModelSelection = ({ navigation }: Props) => {
     });
   }, [navigation]);
 
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const setupNanoX = () => {};
 
   return (
-    <OnboardingView
-      hasBackButton
-      title={t("syncOnboarding.deviceSelection.title")}
-      subTitle={t("syncOnboarding.deviceSelection.subtitle")}
-    >
-      <DiscoverCard
-        title={t("syncOnboarding.deviceSelection.nanoFTS.title")}
-        titleProps={{ variant: "h3" }}
-        subTitle={t("syncOnboarding.deviceSelection.nanoFTS.description")}
-        onPress={setupNanoFTS}
-        cardProps={{ mx: 0 }}
-        Image={
-          <Illustration
-            size={130}
-            darkSource={setupLedgerImg}
-            lightSource={setupLedgerImg}
+    <SafeAreaView>
+      <Flex bg="background.main" height="100%">
+        <Flex flexDirection="row" justifyContent="space-between">
+          <Button
+            Icon={ArrowLeftMedium}
+            size="medium"
+            onPress={handleNavigateBack}
           />
-        }
-      />
+        </Flex>
 
-      <DiscoverCard
-        title={t("syncOnboarding.deviceSelection.nanoX.title")}
-        titleProps={{ variant: "h3" }}
-        subTitle={t("syncOnboarding.deviceSelection.nanoX.description")}
-        onPress={setupNanoX}
-        cardProps={{ mx: 0 }}
-        Image={
-          <Illustration
-            size={130}
-            darkSource={setupLedgerImg}
-            lightSource={setupLedgerImg}
+        <Flex px={3}>
+          <Text mb={4} mt={2} variant="h4" fontWeight="semiBold">
+            {t("syncOnboarding.deviceSelection.title")}
+          </Text>
+          <Text mb={8}>{t("syncOnboarding.deviceSelection.subtitle")}</Text>
+          <DiscoverCard
+            title={t("syncOnboarding.deviceSelection.nanoFTS.title")}
+            titleProps={{ variant: "h3" }}
+            subTitle={t("syncOnboarding.deviceSelection.nanoFTS.description")}
+            onPress={setupNanoFTS}
+            cardProps={{ mx: 0, mb: 4 }}
+            Image={
+              <Illustration
+                size={130}
+                darkSource={setupLedgerImg}
+                lightSource={setupLedgerImg}
+              />
+            }
           />
-        }
-      />
-    </OnboardingView>
+          <DiscoverCard
+            title={t("syncOnboarding.deviceSelection.nanoX.title")}
+            titleProps={{ variant: "h3" }}
+            subTitle={t("syncOnboarding.deviceSelection.nanoX.description")}
+            onPress={setupNanoX}
+            cardProps={{ mx: 0 }}
+            Image={
+              <Illustration
+                size={130}
+                darkSource={setupLedgerImg}
+                lightSource={setupLedgerImg}
+              />
+            }
+          />
+        </Flex>
+      </Flex>
+    </SafeAreaView>
   );
 };
