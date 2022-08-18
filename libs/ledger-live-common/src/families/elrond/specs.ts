6d3d7444949ef6710bd3b892c2c4af1f634280e3
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-08-18 12:45:39 +0300

    add esdt transfer device action

--- libs/ledger-live-common/src/families/elrond/specs.ts
+++ libs/ledger-live-common/src/families/elrond/specs.ts
@@ -2,14 +2,27 @@ import type { Transaction } from "../../families/elrond/types";
 import invariant from "invariant";
 import { getCryptoCurrencyById } from "../../currencies";
 import { pickSiblings } from "../../bot/specs";
-import type { AppSpec } from "../../bot/types";
+import type { AppSpec, TransactionTestInput } from "../../bot/types";
 import { toOperationRaw } from "../../account";
 import { DeviceModelId } from "@ledgerhq/devices";
 import BigNumber from "bignumber.js";
 import expect from "expect";
+import {
+  acceptEsdtTransferTransaction,
+  acceptMoveBalanceTransaction,
+} from "./speculos-deviceActions";
 import { sample } from "lodash";
+import { SubAccount } from "@ledgerhq/types-live";
+
+function expectCorrectBalanceChange(input: TransactionTestInput<Transaction>) {
+  const { account, operation, accountBeforeTransaction } = input;
+  expect(account.balance.toNumber()).toBe(
+    accountBeforeTransaction.balance.minus(operation.value).toNumber()
+  );
+}
 
 const ELROND_MIN_SAFE = new BigNumber(10000);
+
 const elrondSpec: AppSpec<Transaction> = {
   name: "Elrond",
   currency: getCryptoCurrencyById("elrond"),
@@ -33,11 +46,15 @@ const elrondSpec: AppSpec<Transaction> = {
     delete opExpected.blockHeight;
     expect(toOperationRaw(operation)).toMatchObject(opExpected);
   },
+
   mutations: [
     {
       name: "send 50%~",
       maxRun: 1,
+      deviceAction: acceptMoveBalanceTransaction,
       transaction: ({ account, siblings, bridge }) => {
+        invariant(account.spendableBalance.gt(0), "balance is 0");
+
         const sibling = pickSiblings(siblings, 2);
         let amount = account.spendableBalance
           .div(1.9 + 0.2 * Math.random())
@@ -63,16 +80,22 @@ const elrondSpec: AppSpec<Transaction> = {
           ],
         };
       },
+      test: (input) => {
+        expectCorrectBalanceChange(input);
+      },
     },
 
     {
       name: "move some ESDT",
       maxRun: 1,
+      deviceAction: acceptEsdtTransferTransaction,
       transaction: ({ account, siblings, bridge }) => {
-        const esdtAccount: any = sample(
+        const esdtAccount: SubAccount | undefined = sample(
           (account.subAccounts || []).filter((a) => a.balance.gt(0))
         );
         invariant(esdtAccount, "no esdt account");
+
+        invariant(esdtAccount?.balance.gt(0), "esdt balance is 0");
 
         const sibling = pickSiblings(siblings, 2);
         const recipient = sibling.freshAddress;
@@ -82,10 +105,10 @@ const elrondSpec: AppSpec<Transaction> = {
           updates: [
             {
               recipient,
-              subAccountId: esdtAccount.id,
+              subAccountId: esdtAccount?.id,
             },
             {
-              amount: esdtAccount.balance.times(Math.random()).integerValue(),
+              amount: esdtAccount?.balance.times(Math.random()).integerValue(),
             },
           ],
         };
