9553e6bb083b39526a02e49c6d51f94566306726
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2022-08-18 14:21:12 +0200

    feat: Add test for broadcastTransactionLogic
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/platform/logic.test.ts
+++ libs/ledger-live-common/src/platform/logic.test.ts
@@ -1,24 +1,17 @@
-import { receiveOnAccountLogic } from "./logic";
+import { broadcastTransactionLogic, receiveOnAccountLogic } from "./logic";
 
-import { createAccount } from "../__tests__/accounts/fixtures/index";
 import { AppManifest } from "./types";
+import { createAccount } from "../mock/fixtures/cryptoCurrencies";
+import { OperationType, SignedOperation, SignedOperationRaw } from "@ledgerhq/types-live";
+import BigNumber from "bignumber.js";
 
 describe("receiveOnAccountLogic", () => {
   it("calls uiNavigation callback with an accountAddress", async () => {
     // Given
     const accountId = "12";
-    const context = {
-      manifest: createAppManifest(),
-      accounts: [createAccount(accountId), createAccount()],
-      tracking: {
-        platformReceiveRequested: jest.fn(),
-        platformReceiveFail: jest.fn(),
-      },
-    };
+    const context = createContextContainingAccountId(accountId);
     const expectedResult = "Function called";
-    const uiNavigation = jest.fn((_acc, _pAcc, _add) =>
-      Promise.resolve(expectedResult)
-    );
+    const uiNavigation = jest.fn().mockResolvedValueOnce(expectedResult);
 
     // When
     const result = await receiveOnAccountLogic(
@@ -38,15 +31,8 @@ describe("receiveOnAccountLogic", () => {
   it("returns an error when account cannot be found", async () => {
     // Given
     const accountId = "12";
-    const context = {
-      manifest: createAppManifest(),
-      accounts: [createAccount("10"), createAccount("11")],
-      tracking: {
-        platformReceiveRequested: jest.fn(),
-        platformReceiveFail: jest.fn(),
-      },
-    };
-    const uiNavigation = jest.fn(() => Promise.resolve("Function called"));
+    const context = createContextContainingAccountId("10", "11");
+    const uiNavigation = jest.fn().mockResolvedValueOnce("Function called");
 
     // When
     await expect(async () => {
@@ -57,6 +43,37 @@ describe("receiveOnAccountLogic", () => {
     expect(uiNavigation).toBeCalledTimes(0);
     expect(context.tracking.platformReceiveRequested).toBeCalledTimes(1);
     expect(context.tracking.platformReceiveFail).toBeCalledTimes(1);
+  });
+});
+
+const signedOperation = createSignedOperation()
+jest.mock("./serializers", () => {
+  return {
+    deserializePlatformSignedTransaction: jest.fn(() => signedOperation),
+  };
+});
+
+describe("broadcastTransactionLogic", () => {
+  it("calls uiNavigation callback with a signedOperation", async () => {
+    // Given
+    const accountId = "12";
+    const context = createContextContainingAccountId(accountId);
+    const rawSignedTransaction = createSignedOperationRaw();
+    const expectedResult = "Function called";
+    const uiNavigation = jest.fn().mockResolvedValueOnce(expectedResult);
+
+    // When
+    const result = await broadcastTransactionLogic(
+      context,
+      accountId,
+      rawSignedTransaction,
+      uiNavigation
+    );
+
+    // Then
+    expect(uiNavigation).toBeCalledTimes(1);
+    expect(uiNavigation.mock.calls[0][2]).toEqual(signedOperation);
+    expect(result).toEqual(expectedResult);
   });
 });
 
@@ -86,5 +103,60 @@ function createAppManifest(id = "1"): AppManifest {
     },
     permissions: [],
     domains: [],
+  };
+}
+
+function createContextContainingAccountId(...accountIds: string[]) {
+  return {
+    manifest: createAppManifest(),
+    accounts: [...accountIds.map((val) => createAccount(val)), createAccount()],
+    tracking: {
+      platformReceiveRequested: jest.fn(),
+      platformReceiveFail: jest.fn(),
+    },
+  };
+}
+
+function createSignedOperation(): SignedOperation {
+  const operation = {
+    id: "42",
+    hash: "hashed",
+    type: "IN" as OperationType,
+    value: new BigNumber(0),
+    fee: new BigNumber(0),
+    senders: [],
+    recipients: [],
+    blockHeight: null,
+    blockHash: null,
+    accountId: "14",
+    date: new Date(),
+    extra: {},
+  };
+  return {
+    operation,
+    signature: "Signature",
+    expirationDate: null,
+  };
+}
+
+function createSignedOperationRaw(): SignedOperationRaw {
+  const rawOperation = {
+    id: "12",
+    hash: "123456",
+    type: "CREATE" as OperationType,
+    value: "0",
+    fee: "0",
+    senders: [],
+    recipients: [],
+    blockHeight: null,
+    blockHash: null,
+    accountId: "12",
+    date: "01/01/1970",
+    extra: {},
+  };
+  return {
+    operation: rawOperation,
+    signature: "Signature",
+    expirationDate: null,
   };
 }
