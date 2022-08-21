4d0a7aaf0047ebe42224226f49b97937711de1ca
Juan Cortés Ross <juan@bohem.io>
2022-08-21 21:03:17 +0200

    js2ts removed unused assets (rollback if needed)

--- apps/ledger-live-mobile/src/logic/anonymizer.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  url: (url: string): string =>
-    url
-      .replace(/\/addresses\/[^/]+/g, "/addresses/<HIDDEN>")
-      .replace(/blockHash=[^&]+/g, "blockHash=<HIDDEN>"),
-};
--- apps/ledger-live-mobile/src/screens/Portfolio/events.ts
+++
@@ -1,3 +0,0 @@
-import { Subject } from "rxjs";
-
-export const scrollToTopIntent: Subject<any> = new Subject();
