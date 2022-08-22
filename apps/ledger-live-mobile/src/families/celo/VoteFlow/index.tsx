b73e47c4bc3df20f75a5870fd47176ed5acb8611
Trent Rogers <trentkrogers@gmail.com>
2022-08-22 16:26:42 -0500

    Celo Vote WIP

---
+++ apps/ledger-live-mobile/src/families/celo/VoteFlow/index.tsx
@@ -0,0 +1,148 @@
+import { CeloValidatorGroup } from "@ledgerhq/live-common/families/celo/types";
+import { useTheme } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { Platform } from "react-native";
+import StepHeader from "../../../components/StepHeader";
+import { ScreenName } from "../../../const";
+import { getStackNavigatorConfig } from "../../../navigation/navigatorConfig";
+import ConnectDevice from "../../../screens/ConnectDevice";
+import SelectDevice from "../../../screens/SelectDevice";
+import VoteAmount from "./VoteAmount";
+import VoteStarted from "./01-Started";
+import SelectValidator from "./SelectValidator";
+import VoteSummary from "./02-Summary";
+import DelegationValidationError from "./ValidationError";
+import DelegationValidationSuccess from "./ValidationSuccess";
+
+const totalSteps = "3";
+
+function VoteFlow() {
+  const { t } = useTranslation();
+  const { colors } = useTheme();
+  const stackNavigationConfig = useMemo(
+    () => getStackNavigatorConfig(colors, true),
+    [colors],
+  );
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        ...stackNavigationConfig,
+        gestureEnabled: Platform.OS === "ios",
+      }}
+    >
+      <Stack.Screen
+        name={ScreenName.CeloVoteStarted}
+        component={VoteStarted}
+        options={{
+          headerTitle: () => (
+            <StepHeader title={t("delegation.started.title")} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name={ScreenName.CeloVoteSummary}
+        component={VoteSummary}
+        options={{
+          gestureEnabled: false,
+          headerTitle: () => (
+            <StepHeader
+              title={t("delegation.summaryTitle")}
+              subtitle={t("send.stepperHeader.stepRange", {
+                currentStep: "1",
+                totalSteps,
+              })}
+            />
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name={ScreenName.CeloVoteValidatorSelect}
+        component={SelectValidator}
+        options={{
+          gestureEnabled: false,
+          headerTitle: () => (
+            <StepHeader title={t("delegation.selectValidatorTitle")} />
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name={ScreenName.CeloVoteAmount}
+        component={VoteAmount}
+        options={({
+          route,
+        }: {
+          route: { params: { validator: CeloValidatorGroup } };
+        }) => ({
+          headerRight: null,
+          headerTitle: () => (
+            <StepHeader title={t("send.stepperHeader.selectAmount")} />
+          ),
+        })}
+      />
+
+      <Stack.Screen
+        name={ScreenName.CeloVoteSelectDevice}
+        component={SelectDevice}
+        options={{
+          headerTitle: () => (
+            <StepHeader
+              title={t("celo.vote.stepperHeader.selectDevice")}
+              subtitle={t("celo.vote.stepperHeader.stepRange", {
+                currentStep: "2",
+                totalSteps,
+              })}
+            />
+          ),
+        }}
+      />
+
+      <Stack.Screen
+        name={ScreenName.CeloVoteConnectDevice}
+        component={ConnectDevice}
+        options={{
+          headerLeft: undefined,
+          gestureEnabled: false,
+          headerTitle: () => (
+            <StepHeader
+              title={t("celo.vote.stepperHeader.connectDevice")}
+              subtitle={t("celo.vote.stepperHeader.stepRange", {
+                currentStep: "3",
+                totalSteps,
+              })}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name={ScreenName.CeloVoteValidationError}
+        component={DelegationValidationError}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name={ScreenName.CeloVoteValidationSuccess}
+        component={DelegationValidationSuccess}
+        options={{
+          headerLeft: undefined,
+          headerRight: undefined,
+          headerTitle: "",
+          gestureEnabled: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const options = {
+  headerShown: false,
+};
+
+export { VoteFlow as component, options };
+
+const Stack = createStackNavigator();
