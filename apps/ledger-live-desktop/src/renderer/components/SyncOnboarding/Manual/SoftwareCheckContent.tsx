546ee0797911a5451ec1240de5e14c1e3128fec1
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-08-22 16:34:59 +0200

    add UI for update available in sync onboarding (#1012)

--- apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/SoftwareCheckContent.tsx
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/SoftwareCheckContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Flex, Text, InfiniteLoader } from "@ledgerhq/react-ui";
+import { Button, Flex, Text, InfiniteLoader } from "@ledgerhq/react-ui";
 import { useTranslation } from "react-i18next";
 import styled, { useTheme } from "styled-components";
 
 import Check from "~/renderer/icons/Check";
+import InfoCircle from "~/renderer/icons/InfoCircle";
 
 export const BorderFlex = styled(Flex)`
   background-color: ${p => p.theme.colors.palette.neutral.c30};
@@ -32,50 +33,65 @@ export const Column = styled(Flex).attrs({
   alignItems: "stretch",
 })``;
 
+enum Status {
+  inactive = "inactive",
+  active = "active",
+  updateAvailable = "updateAvailable",
+  completed = "completed",
+}
+
+type StatusType = "inactive" | "active" | "updateAvailable" | "completed";
+
 const Bullet = ({
-  isActive,
-  isCompleted,
+  status,
   bulletText,
   text,
   subText,
 }: {
-  isActive: boolean;
-  isCompleted: boolean;
+  status: StatusType;
   bulletText?: string | number;
   text: string;
   subText?: string;
 }) => {
   const theme = useTheme();
+  const { t } = useTranslation();
+
   return (
-    <Row mb={8}>
-      <IconContainer>
-        {isActive ? (
-          <InfiniteLoader />
-        ) : isCompleted ? (
-          <Check size={24} color={theme.colors.palette.success.c50} />
-        ) : (
-          <Text fontSize="20px">{bulletText}</Text>
-        )}
-      </IconContainer>
-      <Column flex="1" ml={4}>
-        <Text variant="body">{text}</Text>
-        {subText && (
-          <Text mt={2} variant="small" color="palette.neutral.c80">
-            {subText}
-          </Text>
-        )}
-      </Column>
-    </Row>
+    <>
+      <Row mb={8}>
+        <IconContainer>
+          {status === Status.active ? (
+            <InfiniteLoader />
+          ) : status === Status.completed ? (
+            <Check size={24} color={theme.colors.palette.success.c50} />
+          ) : status === Status.updateAvailable ? (
+            <InfoCircle size={24} color={theme.colors.palette.constant.purple} />
+          ) : (
+            <Text fontSize="20px">{bulletText}</Text>
+          )}
+        </IconContainer>
+        <Column flex="1" ml={4}>
+          <Text variant="body">{text}</Text>
+          {subText && (
+            <Text mt={2} variant="small" color="palette.neutral.c80">
+              {subText}
+            </Text>
+          )}
+        </Column>
+      </Row>
+      {status === Status.updateAvailable && (
+        <Flex mt={2} flex="1" justifyContent="space-around">
+          <Button variant="main" width="45%" padding="10px 20px">
+            {t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.downloadUpdate")}
+          </Button>
+          <Button variant="main" outline width="45%" padding="10px 20px">
+            {t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.skipUpdate")}
+          </Button>
+        </Flex>
+      )}
+    </>
   );
 };
-
-enum Status {
-  inactive = "inactive",
-  active = "active",
-  completed = "completed",
-}
-
-type StatusType = "inactive" | "active" | "completed";
 
 const SoftwareCheckContent = ({
   genuineCheckStatus,
@@ -90,8 +106,7 @@ const SoftwareCheckContent = ({
     <>
       <Bullet
         bulletText="1"
-        isActive={genuineCheckStatus === Status.active && true}
-        isCompleted={genuineCheckStatus === Status.completed && true}
+        status={genuineCheckStatus}
         text={
           genuineCheckStatus === Status.completed
             ? t("syncOnboarding.manual.softwareCheckContent.genuineCheck.completed")
@@ -100,14 +115,22 @@ const SoftwareCheckContent = ({
       />
       <Bullet
         bulletText="2"
-        isActive={firmwareUpdateStatus === Status.active && true}
-        isCompleted={firmwareUpdateStatus === Status.completed && true}
+        status={firmwareUpdateStatus}
         text={
           firmwareUpdateStatus === Status.inactive
             ? t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.inactive")
             : firmwareUpdateStatus === Status.active
             ? t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.active")
+            : firmwareUpdateStatus === Status.updateAvailable
+            ? t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.updateAvailable", {
+                firmwareVersion: "2.0.2", // FIX HERE TO REMOVE HARDCODED VERSION
+              })
             : t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.completed")
+        }
+        subText={
+          firmwareUpdateStatus === Status.updateAvailable
+            ? t("syncOnboarding.manual.softwareCheckContent.firmwareUpdate.recommendation")
+            : undefined
         }
       />
     </>
