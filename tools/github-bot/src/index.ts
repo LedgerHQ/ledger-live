22f02227fc4f804a99d07d95664d9d35415a62e0
elbywan <julien.el-baz@ledger.fr>
2022-08-23 15:55:27 +0200

    ci: add /regen-pods bot command and action

--- tools/github-bot/src/index.ts
+++ tools/github-bot/src/index.ts
@@ -24,6 +24,28 @@ export default (app: Probot) => {
     });
   });
 
+  commands(app, "regen-pods", async (context, data) => {
+    const { octokit, payload } = context;
+
+    if (context.isBot) return;
+
+    await octokit.rest.reactions.createForIssueComment({
+      ...context.repo(),
+      comment_id: context.payload.comment.id,
+      content: "rocket",
+    });
+
+    await octokit.actions.createWorkflowDispatch({
+      ...context.repo(),
+      workflow_id: "regen-pods.yml",
+      ref: "develop",
+      inputs: {
+        number: `${data.number}`,
+        login: `${payload.comment.user.login}`,
+      },
+    });
+  });
+
   // trigger to autoclose PR when not respecting guidelines
   app.on(["pull_request.opened", "pull_request.reopened"], async (context) => {
     const { payload, octokit } = context;
