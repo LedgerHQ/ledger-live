8f541073193fa691e166b86ebb12d8fb4a4de120
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-08-24 15:33:35 +0200

    fix: typo in getGenuineCheckFromDeviceId comment

--- libs/ledger-live-common/src/hw/getGenuineCheckFromDeviceId.ts
+++ libs/ledger-live-common/src/hw/getGenuineCheckFromDeviceId.ts
@@ -23,7 +23,7 @@ export type GetGenuineCheckFromDeviceIdOutput =
  * @param deviceId A device id, or an empty string if device is usb plugged
  * @param lockedDeviceTimeoutMs Time of no response from device after which the device is considered locked, in ms. Default 1000ms.
  * @returns An Observable pushing objects containing:
- * - sockerEvent: a SocketEvent giving the current status of the genuine check,
+ * - socketEvent: a SocketEvent giving the current status of the genuine check,
  *     null if the genuine check process did not reach any state yet
  * - deviceIsLocked: a boolean set to true if the device is currently locked, false otherwise
  */
