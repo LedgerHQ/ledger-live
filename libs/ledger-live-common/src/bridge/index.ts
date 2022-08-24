216d5ec8c4a752f1348720c59f88905063f8bb8f
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2022-08-24 13:45:00 +0200

    feat: remove 'any' type from getAccountBridge to avoid type lose
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/bridge/index.ts
+++ libs/ledger-live-common/src/bridge/index.ts
@@ -6,6 +6,7 @@ import type {
   CurrencyBridge,
   ScanAccountEvent,
   ScanAccountEventRaw,
+  TransactionCommon,
 } from "@ledgerhq/types-live";
 import { fromAccountRaw, toAccountRaw } from "../account";
 import * as impl from "./impl";
@@ -25,11 +26,13 @@ export const setBridgeProxy = (p: Proxy | null | undefined) => {
 };
 export const getCurrencyBridge = (currency: CryptoCurrency): CurrencyBridge =>
   (proxy || impl).getCurrencyBridge(currency);
-export const getAccountBridge = (
+export function getAccountBridge<T extends TransactionCommon>(
   account: AccountLike,
   parentAccount?: Account | null | undefined
-): AccountBridge<any> =>
-  (proxy || impl).getAccountBridge(account, parentAccount);
+): AccountBridge<T> {
+  return (proxy || impl).getAccountBridge(account, parentAccount);
+}
+
 export function fromScanAccountEventRaw(
   raw: ScanAccountEventRaw
 ): ScanAccountEvent {
