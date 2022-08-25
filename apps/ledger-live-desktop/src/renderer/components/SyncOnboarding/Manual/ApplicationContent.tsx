f26b9768b7e442b6a461e0067bd7e401f06b4682
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2022-08-25 18:03:36 +0200

    Add UI for recommended apps step in sync onboarding (#1048)

---
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/ApplicationContent.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Button, Flex, Text } from "@ledgerhq/react-ui";
+
+import Illustration from "~/renderer/components/Illustration";
+import { Bullet, Status } from "./shared";
+import Coins from "./assets/coins.png";
+
+// TODO: this is a placeholder, implement the real logic
+
+const InstallingApplicationContent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Flex flexDirection="column" rowGap="24px">
+      <Text variant="h5">{"Installing apps 22%"}</Text>
+      <Flex flexDirection="column">
+        <Bullet bulletText="1" status={Status.completed} text={"Bitcoin"} />
+        <Bullet bulletText="2" status={Status.active} text={"Ethereum"} />
+        <Bullet bulletText="3" status={Status.inactive} text={"Polygon"} />
+      </Flex>
+      <Text color="palette.neutral.c70" variant="paragraph">
+        {t("syncOnboarding.manual.installApplications.info")}
+      </Text>
+    </Flex>
+  );
+};
+
+const ApplicationContent = () => {
+  const { t } = useTranslation();
+
+  const [isInstallingApplications, setIsInstallingApplications] = useState<boolean>(false);
+
+  if (isInstallingApplications) {
+    return <InstallingApplicationContent />;
+  }
+
+  return (
+    <Flex flexDirection="column" alignItems="center" rowGap="24px">
+      <Illustration lightSource={Coins} darkSource={Coins} size={106} height={40} />
+      <Text variant="h5">{"Nano uses apps to enable secure blockchain transactions"}</Text>
+      <Text color="palette.neutral.c70" variant="paragraph">
+        {t("syncOnboarding.manual.installApplications.title")}
+      </Text>
+      <Flex flex="1" justifyContent="space-around" width="100%">
+        <Button
+          variant="main"
+          width="45%"
+          padding="10px 20px"
+          onClick={() => setIsInstallingApplications(true)}
+        >
+          {t("syncOnboarding.manual.installApplications.install")}
+        </Button>
+        <Button variant="main" outline width="45%" padding="10px 20px">
+          {t("syncOnboarding.manual.installApplications.skip")}
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ApplicationContent;
