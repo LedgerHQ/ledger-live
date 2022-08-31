5554b068967538963d26abadec78a6b9fb4fb982
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-08-31 14:00:31 +0200

    feat(llm): use of the new BLE pairing flow

--- apps/ledger-live-mobile/src/screens/SyncOnboarding/DeviceModelSelection.tsx
+++ apps/ledger-live-mobile/src/screens/SyncOnboarding/DeviceModelSelection.tsx
@@ -1,29 +1,49 @@
 import React, { useCallback } from "react";
-import { NativeModules } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
+import { CompositeScreenProps } from "@react-navigation/native";
+import { DeviceModelId } from "@ledgerhq/types-devices";
 import type { SyncOnboardingStackParamList } from "../../components/RootNavigator/SyncOnboardingNavigator";
-import { ScreenName } from "../../const";
+import { NavigatorName, ScreenName } from "../../const";
 // TODO: to put OnboardingView in components/move it in root ?
 import OnboardingView from "../Onboarding/OnboardingView";
 import DiscoverCard from "../Discover/DiscoverCard";
 import Illustration from "../../images/illustration/Illustration";
+import type { BaseNavigatorStackParamList } from "../../components/RootNavigator/BaseNavigator";
 
 const setupLedgerImg = require("../../images/illustration/Shared/_SetupLedger.png");
 
-type Props = StackScreenProps<
-  SyncOnboardingStackParamList,
-  "DeviceModelSelection"
+type Props = CompositeScreenProps<
+  StackScreenProps<SyncOnboardingStackParamList, "DeviceModelSelection">,
+  StackScreenProps<BaseNavigatorStackParamList>
 >;
 
 export const DeviceModelSelection = ({ navigation }: Props) => {
   // const { t } = useTranslation();
 
   const setupNanoFTS = useCallback(() => {
-    // Prompts user to enable bluetooth. Not necessary as next screen handles the ble requirement, but it smooths the transition
-    NativeModules.BluetoothHelperModule.prompt()
-      .then(() => navigation.navigate(ScreenName.BleDevicesScanning as "BleDevicesScanning"))
-      .catch(() => {
-      // ignore
+    // On pairing success, navigate to the Sync Onboarding Companion
+    navigation.navigate(NavigatorName.Base as "Base", {
+      screen: ScreenName.BleDevicePairingFlow as "BleDevicePairingFlow",
+      params: {
+        filterByDeviceModelId: DeviceModelId.nanoFTS,
+        areKnownDevicesDisplayed: false,
+        onSuccessAddToKnownDevices: false,
+        onSuccessNavigateToConfig: {
+          navigateInput: {
+            name: NavigatorName.BaseOnboarding,
+            params: {
+              screen: NavigatorName.SyncOnboarding,
+              params: {
+                screen: ScreenName.SyncOnboardingCompanion,
+                params: {
+                  device: null,
+                },
+              },
+            },
+          },
+          pathToDeviceParam: "params.params.params.device",
+        },
+      },
     });
   }, [navigation]);
 
@@ -49,7 +69,7 @@ export const DeviceModelSelection = ({ navigation }: Props) => {
           />
         }
       />
-    
+
       <DiscoverCard
         title="Nano X"
         titleProps={{ variant: "h3" }}
@@ -66,4 +86,4 @@ export const DeviceModelSelection = ({ navigation }: Props) => {
       />
     </OnboardingView>
   );
-}
+};
