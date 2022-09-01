294fdeab00aedf06926a90ad9c802073c713f183
nparigi-ledger <nicolas.parigi@ledger.fr>
2022-09-01 14:50:59 +0200

    Clicking on + inside a specific crypto account list add the said crypto

--- apps/ledger-live-mobile/src/screens/Accounts/AddAccount.tsx
+++ apps/ledger-live-mobile/src/screens/Accounts/AddAccount.tsx
@@ -2,14 +2,16 @@ import React, { memo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Flex } from "@ledgerhq/native-ui";
 import { PlusMedium } from "@ledgerhq/native-ui/assets/icons";
+import { findCryptoCurrencyById } from "@ledgerhq/live-common/currencies/index";
 import Touchable from "../../components/Touchable";
 import AddAccountsModal from "../AddAccounts/AddAccountsModal";
 import { useCurrentRouteName } from "../../helpers/routeHooks";
 import { track } from "../../analytics";
 
-function AddAccount() {
+function AddAccount({ currencyId }: { currencyId?: string }) {
   const navigation = useNavigation();
   const currentScreen = useCurrentRouteName();
+  const currency = currencyId ? findCryptoCurrencyById(currencyId) : undefined;
   const [isAddModalOpened, setIsAddModalOpened] = useState(false);
 
   function openAddModal() {
@@ -44,6 +46,7 @@ function AddAccount() {
         navigation={navigation}
         isOpened={isAddModalOpened}
         onClose={closeAddModal}
+        currency={currency}
       />
     </Touchable>
   );
