a19ee6415dba7c8cba88cfb13a6d8493c5df3dc0
Thomas Roger Lux <thomas@rogerlux.dev>
2022-09-02 15:11:50 +0200

    fix(cli): remove missing merge conflicts

--- apps/cli/src/commands/synchronousOnboarding.ts
+++ apps/cli/src/commands/synchronousOnboarding.ts
@@ -1,12 +1,9 @@
 import {
   getOnboardingStatePolling,
   OnboardingStatePollingResult,
-<<<<<<< HEAD
-} from "@ledgerhq/live-common/hw/getOnboardingStatePolling";
-=======
 } from "@ledgerhq/live-common/lib/hw/getOnboardingStatePolling";
->>>>>>> 3f5518e1c9 (feat(llc): getOnboardingStatePolling logic)
 import { Observable } from "rxjs";
+
 import { deviceOpt } from "../scan";
 
 export default {
