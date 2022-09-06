c98d4b646b9e308e5ae276c35bb542bca8e9fe0c
Juan Cortés Ross <juan@bohem.io>
2022-09-06 17:46:03 +0200

    feat/LIVE-537 Regenerated docs

--- libs/ledgerjs/packages/hw-transport/src/Transport.ts
+++ libs/ledgerjs/packages/hw-transport/src/Transport.ts
@@ -72,7 +72,7 @@ export default class Transport {
 
   /**
    * Listen all device events for a given Transport. The method takes an Obverver of DescriptorEvent and returns a Subscription (according to Observable paradigm https://github.com/tc39/proposal-observable )
-   * a DescriptorEvent is a `{ descriptor, type }` object. type can be `"add"` or `"remove"` and descriptor is a value you can pass to `open(descriptor)`.
+   * a DescriptorEvent is a `{ descriptor, type }` object. type can be `"add"`, `"remove"`, or `"flush"` and descriptor is a value you can pass to `open(descriptor)`.
    * each listen() call will first emit all potential device already connected and then will emit events can come over times,
    * for instance if you plug a USB device after listen() or a bluetooth device become discoverable.
    * @param observer is an object with a next, error and complete function (compatible with observer pattern)
