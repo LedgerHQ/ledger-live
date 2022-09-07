0b5a948fcade44dad4268303a1e9b9e9780f744e
JesseKuntz <jessekuntz96@gmail.com>
2022-09-07 15:29:04 -0400

    test: add assertions to speculos-deviceActions

--- libs/ledger-live-common/src/families/near/speculos-deviceActions.ts
+++ libs/ledger-live-common/src/families/near/speculos-deviceActions.ts
@@ -1,6 +1,15 @@
+import * as nearAPI from "near-api-js";
+import { getAccountUnit } from "../../account";
+import { formatCurrencyUnit } from "../../currencies";
 import type { DeviceAction } from "../../bot/types";
 import type { Transaction } from "./types";
 import { deviceActionFlow } from "../../bot/specs";
+const confirmWording = {
+  send: "transfer",
+  stake: "deposit_and_stake",
+  unstake: "unstake",
+  withdraw: "withdraw",
+};
 
 export const acceptTransaction: DeviceAction<Transaction, any> =
   deviceActionFlow({
@@ -8,26 +17,59 @@ export const acceptTransaction: DeviceAction<Transaction, any> =
       {
         title: "Confirm",
         button: "Rr",
+        expectedValue: ({ transaction }) => confirmWording[transaction.mode],
       },
       {
         title: "Deposit",
         button: "Rr",
+        expectedValue: ({ transaction, account }) => {
+          if (transaction.mode === "stake") {
+            return formatCurrencyUnit(
+              getAccountUnit(account),
+              transaction.amount,
+              {
+                disableRounding: true,
+              }
+            );
+          }
+
+          return "0";
+        },
       },
       {
         title: "Amount",
         button: "Rr",
+        expectedValue: ({ transaction, account }) =>
+          formatCurrencyUnit(getAccountUnit(account), transaction.amount, {
+            disableRounding: true,
+          }),
       },
       {
         title: "To",
         button: "Rr",
+        expectedValue: ({ transaction }) => transaction.recipient,
       },
       {
         title: "From",
         button: "Rr",
+        expectedValue: ({ account }) => account.freshAddress,
       },
       {
         title: "Args",
         button: "Rr",
+        expectedValue: ({ transaction, account }) => {
+          if (transaction.mode === "stake") {
+            return "{}";
+          }
+
+          return JSON.stringify({
+            amount: nearAPI.utils.format.parseNearAmount(
+              formatCurrencyUnit(getAccountUnit(account), transaction.amount, {
+                disableRounding: true,
+              })
+            ),
+          });
+        },
       },
       {
         title: "Approve",
