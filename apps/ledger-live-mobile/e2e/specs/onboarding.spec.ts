8151c883fa42fef548ac586d3f2e6569b9061e15
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-09-09 16:32:36 +0200

    updating detox to run with monorepo
    removing redundant ios logs - they come as part of the detox artifacts
    
    changing text to validate on
    
    [B2CQA-752] Enabling Android Detox CI flow (#1350)
    
    Enabling Android Detox CI flow
    Tweaking some timeouts
    Force exit jest
    
    trying test id for password test
    
    updated lockfile and pods
    
    removed redundant avd caching and altered forceexit override
    
    removing perf test for now as it's redundant
    
    patching install timeout
    
    Trigger Build
    
    detect open handles and make android headless
    
    whoops, actually headless now
    
    removing cache to force pnpm reinstall
    
    trigger CI
    
    updating patch file to correct version
    
    updating to trace logs
    
    removing cleanup as a test
    
    trigger CI
    
    trigger CI
    
    reducing android log level and improved start script in docs
    
    updated docs and test mobile workflow
    
    fixed typo
    
    trigger CI
    
    updated ruby and removed comments
    
    doing trace logging again
    
    increasing detox timeout to match jest timeout and splitting mobile test and codecheck into separate workflows

--- apps/ledger-live-mobile/e2e/specs/onboarding.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/onboarding.spec.ts
@@ -2,13 +2,10 @@ import OnboardingSteps from "../models/onboarding/onboardingSteps";
 import PortfolioPage from "../models/portfolioPage";
 
 describe("Onboarding", () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
-
   it("should be able to connect a Nano X", async () => {
     await OnboardingSteps.waitForPageToBeVisible();
     await OnboardingSteps.startOnboarding();
+    await OnboardingSteps.DoIOwnDevice(true);
     await OnboardingSteps.chooseToSetupLedger();
     await OnboardingSteps.selectYourDevice("NANO X");
     await OnboardingSteps.chooseToConnectYourNano();
@@ -17,6 +14,7 @@ describe("Onboarding", () => {
     await OnboardingSteps.selectPairWithBluetooth();
     await OnboardingSteps.addDeviceViaBluetooth();
     await OnboardingSteps.openLedgerLive();
+    await OnboardingSteps.declineNotifications();
 
     await PortfolioPage.waitForPageToBeVisible();
     await PortfolioPage.emptyPortfolioIsVisible();
