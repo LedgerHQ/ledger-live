9370aa2fcea9f7c54ee221bdb970c3ad19e4db49
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-09-09 16:32:36 +0200

    updating detox to run with monorepo
    removing redundant ios logs - they come as part of the detox artifacts
    
    changing text to validate on
    
    [B2CQA-752] Enabling Android Detox CI flow (#1350)
    
    Enabling Android Detox CI flow
    Tweaking some timeouts
    Force exit jest
    
    trying test id for password test
    
    updated lockfile and pods
    
    removed redundant avd caching and altered forceexit override
    
    removing perf test for now as it's redundant
    
    patching install timeout
    
    Trigger Build
    
    detect open handles and make android headless
    
    whoops, actually headless now
    
    removing cache to force pnpm reinstall
    
    trigger CI
    
    updating patch file to correct version
    
    updating to trace logs
    
    removing cleanup as a test
    
    trigger CI
    
    trigger CI
    
    reducing android log level and improved start script in docs
    
    updated docs and test mobile workflow
    
    fixed typo
    
    trigger CI
    
    updated ruby and removed comments
    
    doing trace logging again
    
    increasing detox timeout to match jest timeout and splitting mobile test and codecheck into separate workflows

--- apps/ledger-live-mobile/e2e/environment.js
+++ apps/ledger-live-mobile/e2e/environment.js
@@ -1,15 +1,16 @@
-const {
+import {
   DetoxCircusEnvironment,
   SpecReporter,
   WorkerAssignReporter,
-} = require("detox/runners/jest-circus");
+} from "detox/runners/jest-circus";
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
-  constructor(config, context) {
-    super(config, context);
+  constructor({ globalConfig, projectConfig }, context) {
+    super({ globalConfig, projectConfig }, context);
+    // const config = projectConfig;
 
     // Can be safely removed, if you are content with the default value (=300000ms)
-    this.initTimeout = 300000;
+    this.initTimeout = 600000;
 
     // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
     // This is strictly optional.
@@ -20,4 +21,4 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
   }
 }
 
-module.exports = CustomDetoxEnvironment;
+export default CustomDetoxEnvironment;
