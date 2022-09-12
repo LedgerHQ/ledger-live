921ae77448c5396a2154fbd6a08eac50af9cd1a8
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-09-12 19:45:28 +0200

    Replace just testing with auto-removing duplicates from the list

--- apps/ledger-live-desktop/src/live-common-set-supported-currencies.test.js
+++
@@ -1,8 +0,0 @@
-import "./live-common-set-supported-currencies";
-import { listSupportedCurrencies } from "@ledgerhq/live-common/currencies/index";
-
-test("No duplicate in the list of supported currencies on LLD", () => {
-  const supportedCurrenciesIds = listSupportedCurrencies().map(c => c.id);
-  const hasDuplicates = new Set(supportedCurrenciesIds).size !== supportedCurrenciesIds.length;
-  expect(hasDuplicates).toBeFalsy();
-});
