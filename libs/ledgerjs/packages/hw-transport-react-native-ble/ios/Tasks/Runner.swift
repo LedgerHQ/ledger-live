84b91b1eb1ed9f4bf20ee4307254e5f6225c665e
Juan Cortés Ross <juan@bohem.io>
2022-09-13 15:58:46 +0200

    feat/LIVE-537 FAT-393 Disconnects/Locks during bulk progress error handling

--- libs/ledgerjs/packages/hw-transport-react-native-ble/ios/Tasks/Runner.swift
+++ libs/ledgerjs/packages/hw-transport-react-native-ble/ios/Tasks/Runner.swift
@@ -276,15 +276,23 @@ class Runner: NSObject  {
         switch result {
         case .success(let response):
             let data = response.dropLast(4)
-            
+            let status = response.suffix(4)
+
             if self.isInBulkMode {
-                let index = self.APDUMaxCount - self.APDUQueue.count
-                let progress = ((Double(index))/Double(self.APDUMaxCount))
-                self.onEmit!(
-                    RunnerAction.runProgress,
-                    ExtraData(progress: progress, index: index, total: self.APDUMaxCount)
-                )
-                self.handleNextAPDU()
+                if status != "9000" {
+                    self.onEmit!(
+                        RunnerAction.runError,
+                        ExtraData(code: "transport-error", message: "transport-error (\(status))")
+                    )
+                } else {
+                    let index = self.APDUMaxCount - self.APDUQueue.count
+                    let progress = ((Double(index))/Double(self.APDUMaxCount))
+                    self.onEmit!(
+                        RunnerAction.runProgress,
+                        ExtraData(progress: progress, index: index, total: self.APDUMaxCount)
+                    )
+                    self.handleNextAPDU()
+                }
             } else {
                 /// Send message back to the scriptrunner
                 let response = "{\"nonce\":\(self.HSMNonce),\"response\":\"success\",\"data\":\"\(data)\"}"
