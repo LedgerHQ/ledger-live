cc3e8986969dce7bf95f37f0efd42b3fcf05b245
lambertkevin <kevin.lambert@ledger.fr>
2022-09-14 23:50:35 +0200

    Add EIP712 tests with filters

---
+++ libs/ledgerjs/packages/hw-app-eth/tests/fixtures/CAL.js
@@ -0,0 +1,276 @@
+module.exports = {
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:1fd77fb96a82b9c1de173fa836e20da791158ea832a3d07c6725f47c":
+    {
+      contractName: {
+        label: "Anakin",
+        signature:
+          "3044022063587552c233fa3a5cadbd298f6120cb77e29f4d4a2483831e9fb732edabe7f8022034a88f97e8fdc208800960968f7fbcc0c1d3b71aecdb241b2dd36dfca7ea6b32",
+      },
+      fields: [
+        {
+          label: "From",
+          path: "from.name",
+          signature:
+            "3045022100f915a23c2654d761fb810d16c05757e8c1663c99a457b3903d1a5a76fbcb058d02205636516e2e5ab90d5879ad141c329f828d576b5c4cbdf7938b2cf86cb153dce5",
+        },
+        {
+          label: "To",
+          path: "to.name",
+          signature:
+            "3046022100df57f9370b82d1a144d988d085e721cc5e45ec5dab85c906787ed3d6c72d6e22022100cbe041b439cca1784299f42b0e646e03f7c5795f48645c60a0f3b21224167d0c",
+        },
+      ],
+    },
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:5b2c066ad15a55058a74d5698b12785338559cb982cab547e5d93e3d":
+    {
+      contractName: {
+        label: "Chewie",
+        signature:
+          "304402200a68076edda783f20939d29795a2fa64a822ebfb06bf06af9c4deca472fd908002201c7e72fcb7e38bfcefb674cdcdc6a42beabb2d6557b18df9293eb2dea8994bd9",
+      },
+      fields: [
+        {
+          label: "Sender",
+          path: "from.name",
+          signature:
+            "304402200c8991afadb1dbf395edaec90a709e3b4659794ec3f759b5f08e0b25d0656f9802201f50addfd159f669d8c2eb6273fb3c366ee8c5c707c9a59089bbc9f2b15304ac",
+        },
+        {
+          label: "Recipients",
+          path: "to.[].wallets.[]",
+          signature:
+            "3044022022827c43a60563768826f7d1003273828f373b2ddac58fab7f46398133b4e2360220293e311194fc604aac695663c0768e5174dbc82588a3ba445ade5a0513a6569d",
+        },
+        {
+          label: "Message",
+          path: "contents",
+          signature:
+            "3045022074c6e91513f214590fb7ca8e62a207f6c945a0a02ab96010d8e401eb391f2a36022100f5cfd20bb8903cbc1d6a686ab23764fae1319a6e10856443faaf1953ac9c6b7f",
+        },
+      ],
+    },
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:a6dfffb1ad68d66636f07152a977b48093ca6f0761283b54d3699419":
+    {
+      contractName: {
+        label: "Padme",
+        signature:
+          "3045022100d68fd98d552a9e28ae4eadb265346b6a9a512ce7d9e64ab5281ab2db8a690bc502201f360b06b06a4b669f54bb632d4b84c155b173b6cce6793cb0ba4ed52dcd3afa",
+      },
+      fields: [
+        {
+          label: "Value ma man",
+          path: "val",
+          signature:
+            "3044022020f273ed63ba6c5699829a8e40ec675d3ada63d50eb76a0dfbdf2a4b7ed0240402206f467c1fcef99b15206ba2ce88b4cf1c43df9da953868b9d4fb69ff8e1b69600",
+        },
+      ],
+    },
+  "1:0x7f268357a8c2552623316e2562d90e642bb538e5:d8e4f2bd77f7562e99ea5df4adb127291a2bfbc225ae55450038f27f":
+    {
+      contractName: {
+        label: "Mace Windu",
+        signature:
+          "304502205098e783a362b1e3d33ae4d8a6ad4073dd5ad97da72d7ffe6ad41af17869a1f0022100c50ba9335788f82a2ef6d0d5e8b29ea83990346cccb73e887398b0a72ed4a2a1",
+      },
+      fields: [
+        {
+          label: "Maker",
+          path: "maker",
+          signature:
+            "30450220476cc3309dd8104c87102df5fda3c6a760492676c2da3a9f4b4763156c97cf31022100bdaee8bcce4123b59d177ff9e15acfdc8d9a4286b4812479a0f3667ed5cefa4f",
+        },
+        {
+          label: "Taker",
+          path: "taker",
+          signature:
+            "3045022100ad6d6a772ddea2ad4ed7f1e2799846f8d520200418fb435eeb34ea35c46c445502200e602c58f398e5eefa8eef759940ceafd51bdc79b85b5b510dd48dccafe0660c",
+        },
+        {
+          label: "Base Price",
+          path: "basePrice",
+          signature:
+            "3045022100dfead31f8603678ae2eed97f0ea35963f7a00701067be4a58a9c95a03ce73c7702200ed8e835bfbfd336cc047341e7782c0e917ebc4768bdb59b594bf7edbf71885f",
+        },
+        {
+          label: "Expiration Time",
+          path: "expirationTime",
+          signature:
+            "304402207221ee5c1ad62447ae56bc01dd27057bdfd1e807deeaa335f225bc080b9f304f022030a6b8e6eb42287f757e3eac9695abfc68f31c8c358dbd814a4df62bb547ff1d",
+        },
+      ],
+    },
+  "1284:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:087da43b1d5a46a4acfe9437df4197f443a524ec658fbd0e8d305798":
+    {
+      contractName: {
+        label: "Yoda",
+        signature:
+          "304402204a67f5c38317705c31acd936153d627756202370e6412bea13cd43e3cd7d5da802200e8debc34e70160ccc192e92d720d42399faad243740673e945ab3438f6a2ff6",
+      },
+      fields: [
+        {
+          label: "Sender",
+          path: "from",
+          signature:
+            "30450220277c73383cfe014c12504b991339378123b9a339f005470060747b8ddb2ecfb1022100e29196c745c3b3fb4b6eab097e8972f06a390a2d31ff8deafde2f4ab4bb47392",
+        },
+        {
+          label: "Recipients",
+          path: "to.[]",
+          signature:
+            "304502203f4c1125e3133841e17fb6b579b69c4274d38eb2ca363cb0a2c52b30c52a53f7022100bbd38ad455363801af516ad7fc2dccbdf163cc547d1476113928c23079c67bcd",
+        },
+        {
+          label: "Message",
+          path: "contents",
+          signature:
+            "304502206cc88eb3eab3b55d5e6358d46e0fbbaefbd1590d01e77396a15a9f7887bbaaae02210080fab66195f75656134c67007761abfcc207afc075aaf3894e30fdf29e3daa7d",
+        },
+        {
+          label: "Hello this is ID",
+          path: "id",
+          signature:
+            "30450221008dcd615a78440f0ce412717d47903f71b7e0131766a72ef6ab189b02af36ac2802205f9ad7f6688f6e36f96442202f2d9f46532319ebf5f12ba3c92ca263ff08143a",
+        },
+      ],
+    },
+  "1:0x9757f2d2b135150bbeb65308d4a91804107cd8d6:139c059f886c2b9b41f05a6c4ec2578a048d18aaadbc095609e5df4b":
+    {
+      contractName: {
+        label: "Boba",
+        signature:
+          "304502202f9e99e99544e9971c6e8c1f58901debd762fc96b6c388c24e921a72f3588a4a022100a9f80217965c99362424017d0fedfd25060fd4e5aae2daf85ac8cbef033fe7e2",
+      },
+      fields: [
+        {
+          label: "Maker",
+          path: "maker",
+          signature:
+            "3045022100a2dd1bab1c723f5c4978ddfc3416bc32a3e665e1a2a798061ace5a171a821e66022012cd32cbb142b366548c46c0ae4688a174da5f63ece1ac88db5e9d82627daf8e",
+        },
+        {
+          label: "Taker",
+          path: "taker",
+          signature:
+            "3046022100cb5a8695a4c498dfd5e44b6af9238efb7baed439870b7534c81fd70cea8d4b23022100a49eec86927485697a41362455972926a7b6312ad78c1352f3051b69c1dc4f4f",
+        },
+      ],
+    },
+  "0:0x0000000000000000000000000000000000000000:e30e691e8ad018c90b84c64217c2e4abfe9881d27bcd0f8dd999f6b4":
+    {
+      contractName: {
+        label: "R2D2",
+        signature:
+          "3044022056df947a834ef2bf6502e3e1ce611a34ff1b959c0ae366c2d1c8c361077d5a2d022045e7f8afc13d6cde7da3e0d95d834c2ce2d7cab49483d96b14cc35f1150f7215",
+      },
+      fields: [
+        {
+          label: "Document",
+          path: "document.[].[]",
+          signature:
+            "3044022045a0a7d7b9a85be7b63e17872f1155be77330e2b3da5c892bc22baf853f9de4d022050136b6b284c317158e9c83135c83d3d9a692acfbc2a2e12c460878b260a4b5b",
+        },
+        {
+          label: "Proof",
+          path: "proof.[].[]",
+          signature:
+            "3046022100940e7b62f50c6dfc4944e381e218bb928190978ff5b131cb942a5d96be59bd3402210086e99e21dfdd95d4c6e3414e3e58e4a693e928c46ffa21bc768a9027ef1bd09f",
+        },
+        {
+          label: "Depth",
+          path: "depthy.[].[].[].[]",
+          signature:
+            "304402207a5f12624b8f5f6628f64520246d31ac24893876a33010307f8b500d212088be02205ea516cde61a61d6dece3d33949b91bfebf31427e199b7c6f94746bb2e81b7d8",
+        },
+      ],
+    },
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCccccccc1:5b2c066ad15a55058a74d5698b12785338559cb982cab547e5d93e3d":
+    {
+      contractName: {
+        label: "Leia",
+        signature:
+          "3046022100d786a7581ca1bd313e8271cca5635e4b35ae69c13cce9664b2f61886979a5e94022100a45a87afd52f3ae630510dcde9139e98f1979f84efba29b85b5dac583605e225",
+      },
+      fields: [
+        {
+          label: "Sender",
+          path: "from.name",
+          signature:
+            "3046022100fdf2641839d9eb35211a1dff0a642efffcdada1b4a735fed1dc8286cfef3a69f0221009ab91a098be77ed3fa385bb33300c3f6050f1871c2df1a420411b9049e018cf2",
+        },
+        {
+          label: "Should be Alice & Bob",
+          path: "to.[].name",
+          signature:
+            "3046022100ec0bb0ee7962ea0b89a4c1b81e7141c38f4fb3841dc254611b39357e98fa13710221009d38375810801157565b9e7338feb8f44babf4c3bfd46f9dff7be412684323d9",
+        },
+        {
+          label: "Message",
+          path: "contents",
+          signature:
+            "304502210084741fe267c0e68f588c2b46b7e9a33490912bfff0075fa0f35ea807707beba802204692e9380005cd613e0bd8177de9a9189fef1bbeee77a483df65f87b86dfc5a3",
+        },
+      ],
+    },
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:3863b554dca8a87c0367c3967cf1726802cdb6b7d4d1788301d35398":
+    {
+      contractName: {
+        label: "Han",
+        signature:
+          "304602210090e6002a4b5b2a96409fbbfce11510a5ec3efc52139b43fb313e195ad7cab1a2022100eccaf2257b8f9e35c42c064ade707bfff9e04399730efa4e1460ccd444cdad1f",
+      },
+      fields: [
+        {
+          label: "Sender",
+          path: "from.name",
+          signature:
+            "3045022076c5578debbf0c30422d46f546272bbee51d1983691b7f24098991b892f0fb05022100ee5aee20b96e7b60e8cb9d95a185140c5427d7f3b8c8795c9267cc5c52e0a962",
+        },
+        {
+          label: "Recipients",
+          path: "to.[].wallets.[]",
+          signature:
+            "3046022100ba2e37ab42de23f088924639d0c340352a013dd8258882f2fed42d2516313b68022100ec2474e9d083a7935251da520b35b08fe125d2153c7102afc8572135b42c3d0d",
+        },
+        {
+          label: "Message",
+          path: "contents",
+          signature:
+            "3044022052996b835132bf8248277b443439382bd3af11d4614052e9e03e22ab093a36dc02201ed76b7f35f1fd8d61a36abd65d70295d7db02a8e89623ecf4fbe0d01ef1106a",
+        },
+      ],
+    },
+  "5:0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC:7e916a5dd34dd8da7436fa22a4b79f250d77275e11273b38cdf5387c":
+    {
+      contractName: {
+        label: "C3P0",
+        signature:
+          "304502203b0768bde7f99183d3687336d63e314ea6806bf125dfa9261d55b7319ee18fa3022100f9b711b626ec116318ef2ee2ac96a31746cd071c5caac3655adfcc1934161d51",
+      },
+      fields: [
+        {
+          label: "Sender",
+          path: "from.name",
+          signature:
+            "3045022100ced76e0e61411b1ef5642586834e29ae9189974ce4338a9a9a2516e43edbde7302200a36170372164d778f873a4f5e95923ab09f48ac0cb9fa106f7f7d097e891eb9",
+        },
+        {
+          label: "Recipient",
+          path: "to.members.[].name",
+          signature:
+            "3044022100a9a05f12fbe643750f0c8a60b30dbc793077c2104dcf999a62303dba8458d65d021f6edd5f71a9c32b411f17df3fb1aa4721b356259cf77a48c4a44319806a5435",
+        },
+        {
+          label: "Message",
+          path: "contents",
+          signature:
+            "304502200f6f7be32f617791e4ed639ba294b8ab38e9a4f9c39e3fe415c4e03ab4674168022100853409de5fc10e80c2a406439ba184b28762dc74797e1e383b167676d8fae959",
+        },
+        {
+          label: "Attachment",
+          path: "attach.list.[].name",
+          signature:
+            "304502206650c083b2c895fba0fdb8edf952f56f443a6cdde0086aab469c38032f7aeb320221009c7acee25f280180c8cbf0038c1b3692a671cdab0651501948e6a2b59783c539",
+        },
+      ],
+    },
+};
