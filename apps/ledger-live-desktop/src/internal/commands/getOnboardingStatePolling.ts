f3c49c75ec29ef21f05d72354b8750a145db286e
Juan Cortés Ross <juan@bohem.io>
2022-09-15 14:14:39 +0200

    Fix broken import that was breaking everything (ノಠ益ಠ)ノ彡┻━┻

--- apps/ledger-live-desktop/src/internal/commands/getOnboardingStatePolling.ts
+++ apps/ledger-live-desktop/src/internal/commands/getOnboardingStatePolling.ts
@@ -2,7 +2,7 @@ import {
   getOnboardingStatePolling,
   GetOnboardingStatePollingArgs,
   GetOnboardingStatePollingResult,
-} from "@ledgerhq/live-common/lib/hw/getOnboardingStatePolling";
+} from "@ledgerhq/live-common/hw/getOnboardingStatePolling";
 
 // Makes getOnboardingStatePolling run on the internal thread
 const cmd = (args: GetOnboardingStatePollingArgs): GetOnboardingStatePollingResult =>
