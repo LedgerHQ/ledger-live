965cebebf40b7d932c9ed17aac87b0cceb4c1add
sarneijim <sarneijim@gmail.com>
2022-09-16 15:45:17 +0100

    fix: change dex order

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/utils.js
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/utils.js
@@ -1,17 +1,17 @@
 export const DEX_PROVIDERS = [
   {
-    provider: "paraswap",
-    icon: "paraswap",
-    navigation: {
-      pathname: `/platform/paraswap`,
-      params: {},
-    },
-  },
-  {
     provider: "1inch",
     icon: "oneinch",
     navigation: {
       pathname: `/platform/1inch-lld`,
+      params: {},
+    },
+  },
+  {
+    provider: "paraswap",
+    icon: "paraswap",
+    navigation: {
+      pathname: `/platform/paraswap`,
       params: {},
     },
   },
