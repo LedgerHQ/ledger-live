88e56bb986ecca4b900006233f8d06e505642ec5
Emmanuel <emmanuelm41@gmail.com>
2022-09-16 10:43:46 -0300

    add SubHeader for zondax integration

---
+++ apps/ledger-live-desktop/src/renderer/families/stacks/AccountSubHeader.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader/index";
+
+export default function StacksAccountSubHeader() {
+  return <AccountSubHeader family="Stacks" team="Zondax"></AccountSubHeader>;
+}
