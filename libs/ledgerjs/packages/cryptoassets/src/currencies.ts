2eb71a6fa7af1161a7c1c174d8e3897da0d7b361
Emmanuel <emmanuelm41@gmail.com>
2022-09-16 10:46:29 -0300

    add new unit measurement

--- libs/ledgerjs/packages/cryptoassets/src/currencies.ts
+++ libs/ledgerjs/packages/cryptoassets/src/currencies.ts
@@ -3160,6 +3160,11 @@ export const cryptocurrenciesById: Record<string, CryptoCurrency> = {
         code: "STX",
         magnitude: 6,
       },
+      {
+        name: "uSTX",
+        code: "uSTX",
+        magnitude: 0,
+      },
     ],
     explorerViews: [
       {
