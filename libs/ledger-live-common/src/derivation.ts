ceba3809357f35ecd6378efb51a963ac025fd37b
Emmanuel <emmanuelm41@gmail.com>
2022-09-16 09:12:06 -0300

    remove derivation schema not required

--- libs/ledger-live-common/src/derivation.ts
+++ libs/ledger-live-common/src/derivation.ts
@@ -167,9 +167,6 @@ const modes = Object.freeze({
     startsAt: 1,
     tag: "third-party",
   },
-  stacks: {
-    overridesDerivation: "44'/5757'/0'/0/<account>",
-  },
   solanaMain: {
     isNonIterable: true,
     overridesDerivation: "44'/501'",
@@ -207,7 +204,6 @@ const legacyDerivations: Record<CryptoCurrencyIds, DerivationMode[]> = {
   cardano: ["cardano"],
   cardano_testnet: ["cardano"],
   near: ["nearbip44h"],
-  stacks: ["stacks"],
 };
 
 const legacyDerivationsPerFamily: Record<string, DerivationMode[]> = {
