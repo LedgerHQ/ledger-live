783f91b13acbb304a4bde324a5100c19dc94fa51
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-09-19 15:21:36 +0200

    filter cardano spec to uses v4

--- libs/ledger-live-common/src/families/cardano/specs.ts
+++ libs/ledger-live-common/src/families/cardano/specs.ts
@@ -27,6 +27,7 @@ const cardano: AppSpec<Transaction> = {
   appQuery: {
     model: DeviceModelId.nanoS,
     appName: "CardanoADA",
+    appVersion: "4",
   },
   minViableAmount: minBalanceRequired,
   genericDeviceAction: acceptTransaction,
