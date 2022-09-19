11d59178fe11787b2a7209723eaef4713bfcd7ef
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2022-09-19 10:42:46 +0200

    support(UI): Update text and ui for Add Account modal (#1277)
    * support(UI): Update text and ui for Add Account modal
    
    * 💄 Add margin bottom

---
+++ apps/ledger-live-mobile/src/screens/AddAccounts/AddAccountsModalCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ImageSourcePropType } from "react-native";
+import Illustration from "../../images/illustration/Illustration";
+import DiscoverCard from "../Discover/DiscoverCard";
+
+type Props = {
+  title: string;
+  subTitle: string;
+  onPress: () => void;
+  imageSource: ImageSourcePropType;
+  hasMarginBottom?: boolean;
+};
+export default function AddAccountsModalCard({
+  title,
+  subTitle,
+  onPress,
+  imageSource,
+  hasMarginBottom = false,
+}: Props) {
+  return (
+    <DiscoverCard
+      title={title}
+      titleProps={{ variant: "large" }}
+      subTitle={subTitle}
+      subTitleProps={{ variant: "paragraph" }}
+      onPress={onPress}
+      cardProps={{
+        mx: 0,
+        mb: hasMarginBottom ? "16px" : 0,
+      }}
+      Image={
+        <Illustration
+          size={130}
+          darkSource={imageSource}
+          lightSource={imageSource}
+        />
+      }
+    />
+  );
+}
