aff1ab214d451780c16d465e603bbbc457681cb5
lambertkevin <kevin.lambert@ledger.fr>
2022-09-21 16:50:45 +0200

    Update isEIP712Message to allow string as param

--- libs/ledgerjs/packages/hw-app-eth/src/modules/EIP712/EIP712.utils.ts
+++ libs/ledgerjs/packages/hw-app-eth/src/modules/EIP712/EIP712.utils.ts
@@ -274,9 +274,10 @@ export const makeTypeEntryStructBuffer = ({
 
 // As defined in [spec](https://eips.ethereum.org/EIPS/eip-712), the properties below are all required.
 export function isEIP712Message(
-  message: Record<string, unknown>
+  message: Record<string, unknown> | string
 ): message is EIP712Message {
   return (
+    typeof message === "object" &&
     "types" in message &&
     "primaryType" in message &&
     "domain" in message &&
