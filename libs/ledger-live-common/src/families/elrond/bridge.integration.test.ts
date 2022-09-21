19ef09bd05b6276015f14c899aadeab6eb2bafe3
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-09-21 16:12:47 +0300

    do not ignore fields on bridge tests

--- libs/ledger-live-common/src/families/elrond/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/elrond/bridge.integration.test.ts
@@ -27,10 +27,6 @@ const elrond: CurrenciesData<Transaction> = {
     `,
     },
   ],
-  FIXME_ignoreAccountFields: [
-    "elrondResources.nonce",
-    "elrondResources.delegations",
-  ],
   accounts: [
     {
       raw: {
