a6225d64eef32c483a13d060f58e2fd53d00b504
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-09-22 14:15:16 +0200

    Different permissions for different versions of Android

--- apps/ledger-live-mobile/src/components/RequiresBLE/RequiresLocationOnAndroid.android.tsx
+++ apps/ledger-live-mobile/src/components/RequiresBLE/RequiresLocationOnAndroid.android.tsx
@@ -1,15 +1,23 @@
 import React, { ReactNode, useCallback, useEffect, useState } from "react";
-import { PermissionsAndroid, Platform } from "react-native";
+import { Permission, PermissionsAndroid, Platform } from "react-native";
 import LocationRequired from "../../screens/LocationRequired";
 
-const permission = PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION;
-
 /**
- * Location permission is not required to perform a bluetooth scan on devices
- * with Android 12 (API 31) and above
- * https://developer.android.com/guide/topics/connectivity/bluetooth/permissions#assert-never-for-location
- * */
-const requiresLocation = Platform.OS === "android" && Platform.Version < 31;
+ * https://developer.android.com/guide/topics/connectivity/bluetooth/permissions#declare
+ */
+const locationPermission: Permission | undefined =
+  Platform.OS === "android"
+    ? Platform.Version <= 28
+      ? PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+      : Platform.Version <= 30
+      ? PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      : /**
+         * Location permission is not required to perform a bluetooth scan on devices
+         * with Android 12 (API 31) and above
+         * https://developer.android.com/guide/topics/connectivity/bluetooth/permissions#assert-never-for-location
+         * */
+        undefined
+    : undefined;
 
 /**
  * Renders an error if location is required & not available,
@@ -21,21 +29,23 @@ const RequiresLocationOnAndroid: React.FC<{
   const [granted, setGranted] = useState<boolean | null | undefined>(null);
 
   const request = useCallback(async () => {
-    const result = await PermissionsAndroid.request(permission);
+    if (!locationPermission) return;
+    const result = await PermissionsAndroid.request(locationPermission);
     setGranted(result === PermissionsAndroid.RESULTS.GRANTED);
   }, [setGranted]);
 
   const recheck = useCallback(async () => {
-    const granted = await PermissionsAndroid.check(permission);
+    if (!locationPermission) return;
+    const granted = await PermissionsAndroid.check(locationPermission);
     setGranted(granted);
   }, [setGranted]);
 
   useEffect(() => {
-    if (!requiresLocation) return;
+    if (!locationPermission) return;
     request();
   }, [request]);
 
-  if (!requiresLocation) return <>{children}</>;
+  if (!locationPermission) return <>{children}</>;
   if (granted === null) return null; // suspense PLZ
   if (granted === true) return <>{children}</>;
   return <LocationRequired errorType="unauthorized" onRetry={recheck} />;
