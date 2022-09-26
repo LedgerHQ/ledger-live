00d83e0fc51194d2a63babffb31069b2f42c6dfa
sarneijim <sarneijim@gmail.com>
2022-09-26 15:26:01 +0100

    feat: add filter to swap

---
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/Rates/Filter.jsx
@@ -0,0 +1,80 @@
+// @flow
+import React, { useState } from "react";
+import { Trans } from "react-i18next";
+import Box from "~/renderer/components/Box";
+import styled from "styled-components";
+import { FILTER } from "../utils";
+import Button from "~/renderer/components/Button";
+import { Icons } from "@ledgerhq/react-ui";
+
+type Props = {
+  onClick: () => void,
+};
+
+const Container: ThemedComponent<{}> = styled(Box).attrs(() => ({}))`
+  flex-direction: row;
+  margin: 10px 0;
+`;
+
+export const Btn: ThemedComponent<{}> = styled(Button).attrs((p) => {
+  return {
+    key: p.key,
+    primary: p.selected,
+    outline: p.selected,
+    onClick: () => p.updateFilter(p.type),
+    iconPosition: "left",
+  };
+})`
+  border-radius: 500px;
+  margin-right: 12px;
+  padding: 0 12px;
+  height: 32px;
+  color: ${(p) => (p.selected ? p.theme.colors.neutral.c00 : p.theme.colors.neutral.c100)};
+  background-color: ${(p) =>
+    p.selected ? p.theme.colors.primary.c80 : p.theme.colors.neutral.c30};
+  &:active,
+  &:hover {
+    color: ${(p) => (p.selected ? p.theme.colors.primary.c80 : p.theme.colors.neutral.c100)};
+  }
+`;
+
+export default function ProviderRate({ onClick }: Props) {
+  const [filter, setFilter] = useState([]);
+
+  const updateFilter = (type) => {
+    let newFilter = [];
+    if (filter.includes(type)) {
+      newFilter = filter.filter((e) => e !== type);
+      setFilter(newFilter);
+    } else {
+      setFilter([type, ...filter]);
+      newFilter = [type, ...filter];
+    }
+    if ([FILTER.centralised, FILTER.decentralised].every((elem) => newFilter.includes(elem))) {
+      newFilter = newFilter.filter((f) => ![FILTER.centralised, FILTER.decentralised].includes(f));
+    }
+    if ([FILTER.fixed, FILTER.float].every((elem) => newFilter.includes(elem))) {
+      newFilter = newFilter.filter((f) => ![FILTER.fixed, FILTER.float].includes(f));
+    }
+    onClick(newFilter);
+  };
+
+  return (
+    <Container>
+      {[FILTER.centralised, FILTER.decentralised, FILTER.float, FILTER.fixed].map((type, i) => {
+        const selected = filter.includes(type);
+        const props = { selected, key: type, updateFilter, type };
+        return (
+          <Btn key={type} {...props}>
+            {selected ? (
+              <Box mr={1}>
+                <Icons.CloseMedium size={16} />
+              </Box>
+            ) : null}
+            {<Trans i18nKey={`swap.providers.filter.${type}`} />}
+          </Btn>
+        );
+      })}
+    </Container>
+  );
+}
--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/Rates/index.jsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/Rates/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Trans } from "react-i18next";
 import Box from "~/renderer/components/Box";
@@ -7,6 +7,8 @@ import Text from "~/renderer/components/Text";
 import DecentralisedRate from "./DecentralisedRate";
 import CentralisedRate from "./CentralisedRate";
 import Countdown from "./Countdown";
+import EmptyState from "./EmptyState";
+import Filter from "./Filter";
 import type {
   SwapSelectorStateType,
   RatesReducerState,
@@ -17,7 +19,7 @@ import { SWAP_VERSION } from "../../utils/index";
 import styled from "styled-components";
 import Tooltip from "~/renderer/components/Tooltip";
 import IconInfoCircle from "~/renderer/icons/InfoCircle";
-import { DEX_PROVIDERS } from "../utils";
+import { DEX_PROVIDERS, FILTER } from "../utils";
 
 type Props = {
   fromCurrency: $PropertyType<SwapSelectorStateType, "currency">,
@@ -43,7 +45,7 @@ const TableHeader: ThemedComponent<{}> = styled(Box).attrs({
   mt: 3,
   pb: 10,
 })`
-  border-bottom: 1px solid ${p => p.theme.colors.neutral.c30};
+  border-bottom: 1px solid ${(p) => p.theme.colors.neutral.c30};
 `;
 
 export default function ProviderRate({
@@ -58,10 +60,14 @@ export default function ProviderRate({
 }: Props) {
   const dispatch = useDispatch();
   const [dexSelected, setDexSelected] = useState(null);
+  const [filter, setFilter] = useState([]);
+  const [emptyState, setEmptyState] = useState(false);
   const selectedRate = useSelector(rateSelector);
 
+  const providerRef = useRef(null);
+
   const setRate = useCallback(
-    rate => {
+    (rate) => {
       setDexSelected(null);
       updateSelection(rate);
       dispatch(updateRateAction(rate));
@@ -70,12 +76,29 @@ export default function ProviderRate({
   );
 
   const setDexRate = useCallback(
-    provider => {
+    (provider) => {
       setDexSelected(provider);
       updateSelection(provider);
     },
     [updateSelection],
   );
+
+  useEffect(() => {
+    if (filter.includes(FILTER.decentralised)) {
+      setDexRate(DEX_PROVIDERS[0]);
+    } else {
+      if (filter.includes(FILTER.float)) {
+        const selectedRate = rates.find((rate) => rate.tradeMethod === FILTER.float);
+        setRate(selectedRate || rates[0]);
+      } else if (filter.includes(FILTER.fixed)) {
+        const selectedRate = rates.find((rate) => rate.tradeMethod === FILTER.fixed);
+        setRate(selectedRate || rates[0]);
+      } else {
+        setRate(rates[0]);
+      }
+    }
+    setEmptyState(!providerRef.current?.children.length);
+  }, [filter, rates, setDexRate, setRate]);
 
   return (
     <Box height="100%" width="100%">
@@ -99,6 +122,7 @@ export default function ProviderRate({
           </Box>
         )}
       </Box>
+      <Filter onClick={(type) => setFilter(type)} />
       <TableHeader>
         <Box horizontal width="215px" alignItems="center" pr="38px">
           <Text alignItems="center" display="flex" mr={1}>
@@ -151,28 +175,48 @@ export default function ProviderRate({
           </Tooltip>
         </Box>
       </TableHeader>
-      <Box mt={3}>
-        {rates?.map((rate, index) => (
-          <CentralisedRate
-            key={`${rate.provider}-${rate.tradeMethod}`}
-            value={rate}
-            selected={!dexSelected && rate === selectedRate}
-            onSelect={setRate}
-            fromCurrency={fromCurrency}
-            toCurrency={toCurrency}
-          />
-        ))}
+      <Box mt={3} ref={providerRef}>
+        {rates?.map((rate, index) => {
+          const valid = filter.every((item) =>
+            [FILTER.centralised, rate.tradeMethod].includes(item),
+          );
+          if (valid) {
+            return (
+              <CentralisedRate
+                key={`${rate.provider}-${rate.tradeMethod}`}
+                value={rate}
+                selected={!dexSelected && rate === selectedRate}
+                onSelect={setRate}
+                fromCurrency={fromCurrency}
+                toCurrency={toCurrency}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
         {decentralizedSwapAvailable &&
-          DEX_PROVIDERS.map((rate, index) => (
-            <DecentralisedRate
-              key={rate.id}
-              value={rate}
-              selected={dexSelected && rate.id === dexSelected.id}
-              onSelect={setDexRate}
-              icon={rate.icon}
-            />
-          ))}
+          DEX_PROVIDERS.map((rate, index) => {
+            const valid = filter.every((item) =>
+              [FILTER.decentralised, FILTER.float].includes(item),
+            );
+            if (valid) {
+              return (
+                <DecentralisedRate
+                  filter={filter}
+                  key={rate.id}
+                  value={rate}
+                  selected={dexSelected && rate.id === dexSelected.id}
+                  onSelect={setDexRate}
+                  icon={rate.icon}
+                />
+              );
+            } else {
+              return null;
+            }
+          })}
       </Box>
+      {emptyState && <EmptyState />}
     </Box>
   );
 }
