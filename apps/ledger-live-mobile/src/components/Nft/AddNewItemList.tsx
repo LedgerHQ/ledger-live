e05d47dee851a3da72b2145230e95460dcef63c2
Martin Cayuelas <martin.cayuelas@ledger.fr>
2022-10-03 18:23:36 +0200

    💄 Polishing

--- apps/ledger-live-mobile/src/components/Nft/AddNewItemList.tsx
+++ apps/ledger-live-mobile/src/components/Nft/AddNewItemList.tsx
@@ -1,24 +1,28 @@
-import { Flex, Icons } from "@ledgerhq/native-ui";
+import { Flex, Icons, Box, Text } from "@ledgerhq/native-ui";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { View, StyleSheet } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
-import { useTheme } from "styled-components/native";
-import LText from "../LText";
+import styled, { useTheme } from "styled-components/native";
 
-export const AddNewItem = ({ style }: { style?: any }) => {
+const StyledBox = styled(Box)`
+  background-color: ${props => props.theme.colors.background.main};
+  margin-bottom: 8;
+  border-radius: 4;
+  width: 160;
+  height: 160;
+  border-width: 1;
+  border-color: ${props => props.theme.colors.neutral.c60};
+`;
+
+export const AddNewItem = () => {
   const { colors } = useTheme();
   const { t } = useTranslation();
   return (
-    <View
-      style={[
-        style,
-        styles.card,
-        {
-          backgroundColor: colors.background.main,
-          borderColor: colors.neutral.c60,
-        },
-      ]}
+    <StyledBox
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      borderStyle="dashed"
     >
       <RectButton onPress={() => console.log("Add new NFT")}>
         <Flex
@@ -27,34 +31,18 @@ export const AddNewItem = ({ style }: { style?: any }) => {
           justifyContent="center"
         >
           <Icons.PlusMedium size={24} color={colors.neutral.c100} />
-          <LText
-            semiBold
+          <Text
+            variant="body"
+            fontWeight="semiBold"
             color={colors.neutral.c100}
-            style={styles.text}
             mt={2}
+            fontSize={14}
+            lineHeight="18px"
           >
             {t("nft.gallery.addNew")}
-          </LText>
+          </Text>
         </Flex>
       </RectButton>
-    </View>
+    </StyledBox>
   );
 };
-
-const styles = StyleSheet.create({
-  card: {
-    marginBottom: 8,
-    borderRadius: 4,
-    width: 160,
-    height: 160,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderStyle: "dashed",
-    borderWidth: 1,
-  },
-  text: {
-    fontSize: 14,
-    lineHeight: 18,
-  },
-});
