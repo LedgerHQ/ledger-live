6f7a4a5a9047ff491c9df54979880191398f3635
mdeidda-ledger <marco.deidda@ledger.fr>
2022-10-03 17:58:57 +0200

    no message

--- apps/ledger-live-desktop/tests/models/AccountPage.ts
+++ apps/ledger-live-desktop/tests/models/AccountPage.ts
@@ -5,15 +5,21 @@ export class AccountPage {
   readonly buttonsGroup: Locator;
   readonly settingsButton: Locator;
   readonly buttonSend: Locator;
+  readonly accountRow: Function;
 
   constructor(page: Page) {
     this.page = page;
     this.buttonsGroup = page.locator("data-test-id=account-buttons-group");
     this.settingsButton = page.locator("data-test-id=account-settings-button");
     this.buttonSend = page.locator("data-test-id=account-send-button");
+    this.accountRow = (name: string): Locator => page.locator(`text=${name}`);
   }
 
   async clickBtnSend() {
     await this.buttonSend.click();
+  }
+
+  async goToTokenAccount(name: string) {
+    await this.accountRow(name).click();
   }
 }
--- apps/ledger-live-desktop/tests/specs/accounts/tokenaccount.spec.ts
+++ apps/ledger-live-desktop/tests/specs/accounts/tokenaccount.spec.ts
@@ -1,6 +1,5 @@
 import test from "../../fixtures/common";
 import { expect } from "@playwright/test";
-import { DeviceAction } from "../../models/DeviceAction";
 import { Layout } from "../../models/Layout";
 import { AccountsPage } from "../../models/AccountsPage";
 import { AccountPage } from "../../models/AccountPage";
@@ -8,21 +7,21 @@ import { AccountPage } from "../../models/AccountPage";
 test.use({ userdata: "adaAccount" });
 
 test(`Open token account`, async ({ page }) => {
-  const deviceAction = new DeviceAction(page);
   const layout = new Layout(page);
   const accountsPage = new AccountsPage(page);
   const accountPage = new AccountPage(page);
-  const sendModal = new SendModal(page);
 
-  await test.step(`Open Account`, async () => {
+  await test.step(`Open account`, async () => {
     // TODO: Remove changelog modal
     await layout.goToAccounts();
     await accountsPage.goToAccount("ethereum-1");
   });
 
-  await test.step(`Open token Account`, async () => {
-    // TODO: Remove changelog modal
-    await layout.goToAccounts();
-    await accountsPage.goToAccount("usd-coin");
+  await test.step(`Open token account`, async () => {
+    await accountPage.goToTokenAccount("USD Coin");
+  });
+
+  await test.step(`Verify token account is opened`, async () => {
+    expect.soft(page.locator("id=account-header-name")).toHaveValue("USD Coin");
   });
 });
