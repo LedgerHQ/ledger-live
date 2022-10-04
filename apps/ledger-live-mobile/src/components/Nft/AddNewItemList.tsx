9340f72389d1aeefabeeb9cd83a0c36a90b9812b
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2022-10-04 10:54:02 +0200

    ✨ feat(Add): Add new button (#1449)
    * ✨ feat(Add): Add new button
    
    * 💄 Polishing

---
+++ apps/ledger-live-mobile/src/components/Nft/AddNewItemList.tsx
@@ -0,0 +1,48 @@
+import { Flex, Icons, Box, Text } from "@ledgerhq/native-ui";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { RectButton } from "react-native-gesture-handler";
+import styled, { useTheme } from "styled-components/native";
+
+const StyledBox = styled(Box)`
+  background-color: ${props => props.theme.colors.background.main};
+  margin-bottom: 8;
+  border-radius: 4;
+  width: 160;
+  height: 160;
+  border-width: 1;
+  border-color: ${props => props.theme.colors.neutral.c60};
+`;
+
+export const AddNewItem = () => {
+  const { colors } = useTheme();
+  const { t } = useTranslation();
+  return (
+    <StyledBox
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      borderStyle="dashed"
+    >
+      <RectButton onPress={() => console.log("Add new NFT")}>
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Icons.PlusMedium size={24} color={colors.neutral.c100} />
+          <Text
+            variant="body"
+            fontWeight="semiBold"
+            color={colors.neutral.c100}
+            mt={2}
+            fontSize={14}
+            lineHeight="18px"
+          >
+            {t("nft.gallery.addNew")}
+          </Text>
+        </Flex>
+      </RectButton>
+    </StyledBox>
+  );
+};
