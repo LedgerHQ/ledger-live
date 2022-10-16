b0063b8cd557b1b192994ca4536a396821e39db0
yaqiong-Ledger <yaqiong.chen@ledger.fr>
2022-10-16 22:29:05 +0200

    selector update

--- apps/ledger-live-desktop/tests/models/SettingsPage.ts
+++ apps/ledger-live-desktop/tests/models/SettingsPage.ts
@@ -9,16 +9,34 @@ export class SettingsPage {
   readonly experimentalDevModeToggle: Locator;
   readonly carouselSwitchButton: Locator;
   readonly counterValueSelector: Locator;
+  readonly counterValueSearchBar: Locator;
+  readonly counterValuedropdownChoiceEuro: Locator;
+  readonly languageSelector: Locator;
+  readonly languageChoiceFrench: Locator;
+  readonly themeSelector: Locator;
+  readonly themeChoiceLight: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.accountsTab = page.locator('data-test-id=settings-accounts-tab');
-    this.aboutTab = page.locator('data-test-id=settings-about-tab');
-    this.helpTab = page.locator('data-test-id=settings-help-tab');
-    this.experimentalTab = page.locator('data-test-id=settings-experimental-tab');
-    this.experimentalDevModeToggle = page.locator('data-test-id=MANAGER_DEV_MODE-button');
-    this.carouselSwitchButton = page.locator('data-test-id=settings-carousel-switch-button');
-    this.counterValueSelector = page.locator('.select__value-container').first();
+    this.accountsTab = page.locator("data-test-id=settings-accounts-tab");
+    this.aboutTab = page.locator("data-test-id=settings-about-tab");
+    this.helpTab = page.locator("data-test-id=settings-help-tab");
+    this.experimentalTab = page.locator("data-test-id=settings-experimental-tab");
+    this.experimentalDevModeToggle = page.locator("data-test-id=MANAGER_DEV_MODE-button");
+    this.carouselSwitchButton = page.locator("data-test-id=settings-carousel-switch-button");
+    this.counterValueSelector = page.locator(".select__value-container").first();
+    this.counterValueSearchBar = page.locator('[placeholder="Search"]');
+    this.counterValuedropdownChoiceEuro = page.locator(
+      "#react-select-2-option-15 >> text=Euro - EUR",
+    );
+    this.languageSelector = page.locator(
+      "div:nth-child(2) > .sc-dkzDqf.uXYMl > .css-198krsd-container > .select__control > .select__value-container",
+    );
+    this.languageChoiceFrench = page.locator("#react-select-3-option-2");
+    this.themeSelector = page.locator(
+      "div:nth-child(4) > .sc-dkzDqf.uXYMl > .css-198krsd-container > .select__control > .select__value-container",
+    );
+    this.themeChoiceLight = page.locator("#react-select-5-option-1");
   }
 
   async goToAccountsTab() {
@@ -43,19 +61,17 @@ export class SettingsPage {
 
   async changeCounterValue() {
     await this.counterValueSelector.click();
-    await this.page.locator('[placeholder="Search"]').fill("euro");
-    await this.page.locator("#react-select-2-option-15 >> text=Euro - EUR").click();
+    await this.counterValueSearchBar.fill("euro");
+    await this.counterValuedropdownChoiceEuro.click();
   }
 
   async changeLanguage() {
-    await this.page
-      .locator("div:nth-child(2) > .sc-dkzDqf.uXYMl > .css-198krsd-container > .select__control")
-      .click();
-    await this.page.locator(".css-1dsbpcp").click();
+    await this.languageSelector.click();
+    await this.languageChoiceFrench.click();
   }
 
   async changeTheme() {
-    await this.page.locator("#react-select-3-option-2").click();
-    await this.page.locator("text=Clair").click();
+    await this.themeSelector.click();
+    await this.themeChoiceLight.click();
   }
 }
--- apps/ledger-live-desktop/tests/specs/general/userParametersChange.spec.ts
+++ apps/ledger-live-desktop/tests/specs/general/userParametersChange.spec.ts
@@ -12,24 +12,16 @@ test("User retrive data", async ({ page }) => {
   await test.step("user change currency", async () => {
     await layout.goToSettings();
     await settingsPage.changeCounterValue();
-    await expect.soft(page.locator(".select__value-container").first()).toHaveText("Euro - EUR");
+    await expect.soft(settingsPage.counterValueSelector).toHaveText("Euro - EUR");
   });
 
-  // await test.step("user change language", async () => {
-  //   await layout.goToSettings();
-  //   await settingsPage.changeLanguage();
-  //   await expect
-  //     .soft(
-  //       page.locator(
-  //         "div:nth-child(2) > .sc-dkzDqf.uXYMl > .css-198krsd-container > .select__control",
-  //       ),
-  //     )
-  //     .toHaveText("Français");
-  // });
+  await test.step("user change language", async () => {
+    await settingsPage.changeLanguage();
+    await expect.soft(settingsPage.languageSelector).toHaveText("Français");
+  });
 
-  // await test.step("user change Theme", async () => {
-  //   await layout.goToSettings();
-  //   await settingsPage.changeTheme();
-  //   await expect.soft(page.locator("#react-select-3-option-2")).toHaveText("Clair");
-  // });
+  await test.step("user change Theme", async () => {
+    await settingsPage.changeTheme();
+    await expect.soft(settingsPage.themeSelector).toHaveText("Clair");
+  });
 });
