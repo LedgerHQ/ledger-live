56727955d8cd1ed9daded3691e20af23de5b5b95
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-10-25 19:00:31 +0200

    TS migration compliance

--- apps/ledger-live-mobile/src/families/elrond/components/Flows/Undelegate/03-ValidationError.tsx
+++ apps/ledger-live-mobile/src/families/elrond/components/Flows/Undelegate/03-ValidationError.tsx
@@ -1,7 +1,7 @@
 // @flow
 import React, { useCallback } from "react";
 import { StyleSheet, Linking } from "react-native";
-import SafeAreaView from "react-native-safe-area-view";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "@react-navigation/native";
 
 import { TrackScreen } from "../../../../../analytics";
--- apps/ledger-live-mobile/src/families/elrond/shared/02-SelectAmount.tsx
+++ apps/ledger-live-mobile/src/families/elrond/shared/02-SelectAmount.tsx
@@ -1,7 +1,7 @@
 // @flow
 import React, { useCallback, useState, useMemo } from "react";
 import { View, StyleSheet, Keyboard, TouchableOpacity } from "react-native";
-import SafeAreaView from "react-native-safe-area-view";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Trans } from "react-i18next";
 import { useSelector } from "react-redux";
 import { BigNumber } from "bignumber.js";
