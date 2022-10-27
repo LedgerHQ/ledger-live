3d690117929c8c011c419132ac58d360fc5937fc
Kant <quentin.jaccarino@ledger.fr>
2022-10-27 16:49:50 +0200

    feat: split webview for v2 api version
    Co-authored-by: Junichi Sugiura <JunichiSugiura@users.noreply.github.com>
    
    fix: webview v2 export
    
    chore: update podfile.lock
    
    fix: import issues
    
    feat: init Wallet API v2
    
    fix: wallet-api-server import
    
    fix: bump version of wallet-api
    
    feat: migrate LLC platform to walletAPI
    
    feat: implement more handlers
    
    feat: wallet-api integration
    added account.request support using the accounts$ observable
    fix transaction.sign return value
    remove unneeded serialization/deserialization in transaction.broadcast
    cleanup some wallet-api files in LLC
    fix platform broadcastTransaction deadlock when DISABLE_TRANSACTION_BROADCAST is enabled
    
    refactor: types and some cleanup
    
    fix: added permissions
    bump wallet-api versions
    cleanup
    
    test: fix logic test
    
    fix: use constants and setWalletAPIVersion
    
    Introduce WebViewV2 for WebPlatformPlayer
    
    [WIP] Request account handler
    
    Add drawer for request acount handler
    
    Give wallet-api permissions
    
    Pass account list observable to select account drawer
    
    Cleanup
    
    Remove Webview.tsx
    
    Add WebView.tsx
    
    Cleanup
    
    Cleanup
    
    test: fix version tests
    
    refactor: cleanup unused code
    
    refactor: cleanup console.log
    
    fix: add missing permissions

--- libs/ledger-live-common/src/wallet-api/types.ts
+++ libs/ledger-live-common/src/wallet-api/types.ts
@@ -3,25 +3,20 @@ import type {
   CryptoCurrency,
   TokenCurrency,
 } from "@ledgerhq/types-cryptoassets";
-import { AccountFilters, CurrencyFilters } from "./filters";
-import {
-  Account as PlatformAccount,
-  Currency as PlatformCurrency,
-} from "@ledgerhq/live-app-sdk";
 
 export type {
-  Account as PlatformAccount,
-  Currency as PlatformCurrency,
-  Unit as PlatformUnit,
-  Transaction as PlatformTransaction,
-  CryptoCurrency as PlatformCryptoCurrency,
-  ERC20TokenCurrency as PlatformERC20TokenCurrency,
-} from "@ledgerhq/live-app-sdk";
+  Families as WalletAPIFamilies,
+  Account as WalletAPIAccount,
+  Currency as WalletAPICurrency,
+  Transaction as WalletAPITransaction,
+  CryptoCurrency as WalletAPICryptoCurrency,
+  ERC20TokenCurrency as WalletAPIERC20TokenCurrency,
+} from "@ledgerhq/wallet-api-core";
 
 export {
-  CurrencyType as PlatformCurrencyType,
-  TokenStandard as PlatformTokenStandard,
-} from "@ledgerhq/live-app-sdk";
+  CurrencyType as WalletAPICurrencyType,
+  TokenStandard as WalletAPITokenStandard,
+} from "@ledgerhq/wallet-api-core";
 
 export type TranslatableString = {
   en: string;
@@ -40,6 +35,7 @@ export type AppPermission = {
   params?: any;
 };
 
+// TODO update to the new manifest types from wallet-api when released
 export type AppManifest = {
   id: string;
   private?: boolean;
@@ -63,18 +59,6 @@ export type AppManifest = {
   domains: string[];
 };
 
-export type PlatformApi = {
-  fetchManifest: () => Promise<AppManifest[]>;
-};
+export type WalletAPISignedTransaction = SignedOperation;
 
-export type PlatformSignedTransaction = SignedOperation;
-
-export type ListPlatformAccount = (
-  filters?: AccountFilters
-) => PlatformAccount[];
-
-export type ListPlatformCurrency = (
-  filters?: CurrencyFilters
-) => PlatformCurrency[];
-
-export type PlatformSupportedCurrency = CryptoCurrency | TokenCurrency;
+export type WalletAPISupportedCurrency = CryptoCurrency | TokenCurrency;
