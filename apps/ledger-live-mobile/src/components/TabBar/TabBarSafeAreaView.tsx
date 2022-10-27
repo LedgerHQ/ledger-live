c3e9f704652b9c04cd8e67ca8588af060a415d73
nparigi-ledger <89014981+nparigi-ledger@users.noreply.github.com>
2022-10-27 12:07:59 +0200

    Fix/LIVE-4302 - Fix update firmware notif breaking layout (#1624)
    * Fix update firmware notif breaking layout by putting it sticky again. It was mostly due to a react-native glitch with flex layout

--- apps/ledger-live-mobile/src/components/TabBar/TabBarSafeAreaView.tsx
+++ apps/ledger-live-mobile/src/components/TabBar/TabBarSafeAreaView.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-import {
-  NativeSafeAreaViewProps,
-  SafeAreaView,
-} from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
 export { TAB_BAR_SAFE_HEIGHT } from "./shared";
@@ -11,8 +7,4 @@ const StyledSafeAreaView = styled(SafeAreaView)`
   flex: 1;
 `;
 
-const TabBarSafeAreaView = (props: NativeSafeAreaViewProps) => (
-  <StyledSafeAreaView {...props} />
-);
-
-export default TabBarSafeAreaView;
+export default StyledSafeAreaView;
