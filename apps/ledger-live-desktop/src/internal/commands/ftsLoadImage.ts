5acd83123a2e61fc2d43a708afcff6394a051259
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-10-27 17:15:58 +0200

    fix bad naming

--- apps/ledger-live-desktop/src/internal/commands/ftsLoadImage.ts
+++ apps/ledger-live-desktop/src/internal/commands/ftsLoadImage.ts
@@ -1,8 +1,8 @@
 import { Observable, from } from "rxjs";
-import installLanguage, {
+import ftsLoadImage, {
   LoadImageEvent,
   LoadImageRequest,
 } from "@ledgerhq/live-common/hw/ftsLoadImage";
-const cmd = (input: LoadImageRequest): Observable<LoadImageEvent> => from(installLanguage(input));
+const cmd = (input: LoadImageRequest): Observable<LoadImageEvent> => from(ftsLoadImage(input));
 
 export default cmd;
