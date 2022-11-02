d71d6b39b1b1a4341596a6fab6cbe104b5433962
Martin Cayuelas <martin.cayuelas@ledger.fr>
2022-11-02 15:06:24 +0100

    🐛 Catch Upsell modal before receive flow in NFT Galeery

--- apps/ledger-live-mobile/src/screens/Nft/NftGallery/ReceiveNFTsModal.hook.ts
+++ apps/ledger-live-mobile/src/screens/Nft/NftGallery/ReceiveNFTsModal.hook.ts
@@ -1,15 +1,26 @@
+import { useNavigation } from "@react-navigation/native";
 import { useState, useCallback } from "react";
+import { useSelector } from "react-redux";
 import { track } from "../../../analytics";
+import { NavigatorName } from "../../../const";
+import { readOnlyModeEnabledSelector } from "../../../reducers/settings";
 
 export function useReceiveNFTsModal() {
   const [isModalOpened, setModalOpened] = useState<boolean>(false);
+  const readOnlyModeEnabled = useSelector(readOnlyModeEnabledSelector);
+  const navigation = useNavigation();
 
   const openModal = useCallback(() => {
-    track("button_clicked", {
-      button: "Add my NFTs",
-    });
-    setModalOpened(true);
-  }, [setModalOpened]);
+    if (readOnlyModeEnabled) {
+      navigation.navigate(NavigatorName.BuyDevice);
+    } else {
+      track("button_clicked", {
+        button: "Add my NFTs",
+      });
+      setModalOpened(true);
+    }
+  }, [navigation, readOnlyModeEnabled]);
+
   const closeModal = useCallback(() => setModalOpened(false), [setModalOpened]);
 
   return {
