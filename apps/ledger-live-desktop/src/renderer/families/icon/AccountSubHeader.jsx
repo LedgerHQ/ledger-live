5baec422295a57f1c4ed0827dd5b5b744e32e696
heirland <adam.heirland@gmail.com>
2022-11-04 00:42:16 +0700

    feat: add account sub header for icon

---
+++ apps/ledger-live-desktop/src/renderer/families/icon/AccountSubHeader.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader/index";
+
+export default function IconAccountSubHeader() {
+  return <AccountSubHeader family="ICON" team="Techiast"></AccountSubHeader>;
+}
