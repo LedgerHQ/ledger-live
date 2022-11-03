baa22e0b1a91d5491126e63acad7989feb71d7b4
Emmanuel <emmanuelm41@gmail.com>
2022-11-03 14:53:26 -0300

    add headers on mobile and desktop (#4)
    * add headers on mobile and desktop
    * set name correctly

---
+++ apps/ledger-live-mobile/src/families/casper/AccountSubHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader";
+
+function CasperAccountSubHeader() {
+  return <AccountSubHeader family="Casper" team="Zondax" />;
+}
+
+export default CasperAccountSubHeader;
