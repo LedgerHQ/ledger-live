69c32eaf8efe19233a9f70c9fd93c4d50c8ebd6a
Prateek Rathod <prateek@zondax.ch>
2022-11-04 19:29:02 +0530

    fix: validation address logic for casper

--- libs/ledger-live-common/src/families/casper/consts.ts
+++ libs/ledger-live-common/src/families/casper/consts.ts
@@ -3,3 +3,4 @@ export const FEE_CL_TYPE = "U512";
 export const MINIMUM_VALID_AMOUNT = 2.5 * 1e9;
 export const SMALL_BYTES_COUNT = 75;
 export const CASPER_NETWORK = "casper";
+export const CHECKSUM_HEX_LEN = 32;
