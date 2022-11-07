bea305a6a4eccdf5b283c69a8559512b28a980eb
Come Grellard <come.grellard@ledger.fr>
2022-11-07 09:44:27 +0100

    feat/LIVE-3675-braze-integration lint fix

--- apps/ledger-live-mobile/src/analytics/AnonymousIpPlugin.tsx
+++ apps/ledger-live-mobile/src/analytics/AnonymousIpPlugin.tsx
@@ -1,11 +1,14 @@
-import { Plugin, PluginType, SegmentEvent } from '@segment/analytics-react-native';
-  
+import {
+  Plugin,
+  PluginType,
+  SegmentEvent,
+} from "@segment/analytics-react-native";
+
 export class AnonymousIpPlugin extends Plugin {
-    
-    type = PluginType.enrichment;
-  
-    execute(event: SegmentEvent) {
-      (event as any).context.ip = "0.0.0.0";
-      return event;
-    }
+  type = PluginType.enrichment;
+
+  execute(event: SegmentEvent) {
+    (event as any).context.ip = "0.0.0.0";
+    return event;
+  }
 }
