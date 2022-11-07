4c5579e59b5e5ead69b43b81f1bdcd39ccfa56ad
Prateek Rathod <prateek@zondax.ch>
2022-11-07 20:06:43 +0530

    fix: wrong error returned on casper minAmount

--- libs/ledgerjs/packages/errors/src/index.ts
+++ libs/ledgerjs/packages/errors/src/index.ts
@@ -116,6 +116,9 @@ export const ManagerUninstallBTCDep = createCustomErrorClass(
 export const NetworkDown = createCustomErrorClass("NetworkDown");
 export const NoAddressesFound = createCustomErrorClass("NoAddressesFound");
 export const NotEnoughBalance = createCustomErrorClass("NotEnoughBalance");
+export const InvalidAmountTransfer = createCustomErrorClass(
+  "InvalidAmountTransfer"
+);
 export const NotEnoughBalanceToDelegate = createCustomErrorClass(
   "NotEnoughBalanceToDelegate"
 );
