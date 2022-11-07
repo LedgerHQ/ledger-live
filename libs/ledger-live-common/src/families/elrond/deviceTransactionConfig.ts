439f0416dbc28f68557d2b94ed419f99a7245800
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-11-07 15:36:03 +0200

    show amount for unDelegate txs

--- libs/ledger-live-common/src/families/elrond/deviceTransactionConfig.ts
+++ libs/ledger-live-common/src/families/elrond/deviceTransactionConfig.ts
@@ -1,11 +1,17 @@
 import type { DeviceTransactionField } from "../../transaction";
 import type { TransactionStatus, Transaction } from "./types";
 import { findTokenById } from "@ledgerhq/cryptoassets";
+import BigNumber from "bignumber.js";
+import { formatCurrencyUnit } from "../../currencies";
+import { getAccountUnit } from "../../account";
+import { Account } from "@ledgerhq/types-live";
 
 function getDeviceTransactionConfig({
+  account,
   transaction,
   status: { estimatedFees },
 }: {
+  account: Account;
   transaction: Transaction;
   status: TransactionStatus;
 }): Array<DeviceTransactionField> {
@@ -38,11 +44,22 @@ function getDeviceTransactionConfig({
     address: transaction.recipient,
   });
 
-  if (!isEsdtTransfer && transaction.mode !== "unDelegate") {
-    fields.push({
-      type: "amount",
-      label: "Amount",
-    });
+  if (!isEsdtTransfer) {
+    if (transaction.mode === "unDelegate") {
+      fields.push({
+        type: "text",
+        label: "Amount",
+        value: formatCurrencyUnit(getAccountUnit(account), new BigNumber(0), {
+          showCode: true,
+          disableRounding: true,
+        }),
+      });
+    } else {
+      fields.push({
+        type: "amount",
+        label: "Amount",
+      });
+    }
   }
 
   if (!estimatedFees.isZero()) {
