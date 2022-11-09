362d5d0c6462d736d9af6358461c969dee91d079
lambertkevin <kevin.lambert@ledger.fr>
2022-11-09 18:12:22 +0100

    Add clear signed selectors lists to hw-app-eth

--- libs/ledgerjs/packages/hw-app-eth/src/utils.ts
+++ libs/ledgerjs/packages/hw-app-eth/src/utils.ts
@@ -115,3 +115,28 @@ export const decodeTxInfo = (rawTx: Buffer) => {
  */
 export const intAsHexBytes = (int: number, bytes: number): string =>
   int.toString(16).padStart(2 * bytes, "0");
+
+export enum ERC20_CLEAR_SIGNED_SELECTORS {
+  APPROVE = "0x095ea7b3",
+  TRANSFER = "0xa9059cbb",
+}
+
+export enum ERC721_CLEAR_SIGNED_SELECTORS {
+  APPROVE = "0x095ea7b3",
+  SET_APPROVAL_FOR_ALL = "0xa22cb465",
+  TRANSFER_FROM = "0x23b872dd",
+  SAFE_TRANSFER_FROM = "0x42842e0e",
+  SAFE_TRANSFER_FROM_WITH_DATA = "0xb88d4fde",
+}
+
+export enum ERC1155_CLEAR_SIGNED_SELECTORS {
+  SET_APPROVAL_FOR_ALL = "0xa22cb465",
+  SAFE_TRANSFER_FROM = "0xf242432a",
+  SAFE_BATCH_TRANSFER_FROM = "0x2eb2c2d6",
+}
+
+export const tokenSelectors = Object.values(ERC20_CLEAR_SIGNED_SELECTORS);
+export const nftSelectors = [
+  ...Object.values(ERC721_CLEAR_SIGNED_SELECTORS),
+  ...Object.values(ERC1155_CLEAR_SIGNED_SELECTORS),
+];
