6189a9c378de8b36e3c847cd2981d7c2e6893ba1
Juan Cortés Ross <juan@bohem.io>
2022-11-10 15:20:02 +0100

    feat/FAT-505 Updated OpenDebugFetchCustomImage.tsx

--- apps/ledger-live-mobile/src/screens/Settings/Debug/OpenDebugFetchCustomImage.tsx
+++ apps/ledger-live-mobile/src/screens/Settings/Debug/OpenDebugFetchCustomImage.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { FeatureToggle } from "@ledgerhq/live-common/featureFlags/index";
 import { ScreenName } from "../../../const";
 import SettingsRow from "../../../components/SettingsRow";
 import { StackNavigatorNavigation } from "../../../components/RootNavigator/types/helpers";
 import { SettingsNavigatorStackParamList } from "../../../components/RootNavigator/types/SettingsNavigator";
 
-export default function OpenDebugPlayground() {
+export default function OpenDebugFetchCustomImage() {
   const navigation =
     useNavigation<StackNavigatorNavigation<SettingsNavigatorStackParamList>>();
 
   return (
-    <SettingsRow
-      title="Debug Custom Image - Fetch & Backup"
-      desc="Backing up and skipping included"
-      onPress={() => navigation.navigate(ScreenName.DebugFetchCustomImage)}
-    />
+    <FeatureToggle feature="customImage" fallback={null}>
+      <SettingsRow
+        title="Debug Custom Image - Fetch & Backup"
+        desc="Backing up and skipping included"
+        onPress={() => navigation.navigate(ScreenName.DebugFetchCustomImage)}
+      />
+    </FeatureToggle>
   );
 }
