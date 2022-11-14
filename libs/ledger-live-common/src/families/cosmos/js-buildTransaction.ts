4238d1847f0e2e215419d42dc3e292f085a575a2
hzheng-ledger <huiqi.zheng@ledger.fr>
2022-11-14 14:56:58 +0100

    refactoring

--- libs/ledger-live-common/src/families/cosmos/js-buildTransaction.ts
+++ libs/ledger-live-common/src/families/cosmos/js-buildTransaction.ts
@@ -1,9 +1,4 @@
-import { CosmosAccount, Transaction } from "./types";
-import {
-  makeAuthInfoBytes,
-  Registry,
-  TxBodyEncodeObject,
-} from "@cosmjs/proto-signing";
+import { Transaction } from "./types";
 import {
   MsgDelegate,
   MsgUndelegate,
@@ -12,8 +7,6 @@ import {
 import { MsgWithdrawDelegatorReward } from "cosmjs-types/cosmos/distribution/v1beta1/tx";
 import { SignMode } from "cosmjs-types/cosmos/tx/signing/v1beta1/signing";
 import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
-import { defaultCosmosAPI } from "./api/Cosmos";
-import BigNumber from "bignumber.js";
 import type { Account } from "@ledgerhq/types-live";
 import { AminoMsg, AminoSignResponse } from "@cosmjs/amino";
 import {
@@ -36,170 +29,6 @@ type ProtoMsg = {
 };
 
 export const buildTransaction = async (
-  account: CosmosAccount,
-  transaction: Transaction
-): Promise<any> => {
-  const msg: Array<{ typeUrl: string; value: any }> = [];
-
-  // Ledger Live is able to build transaction atomically,
-  // Take care expected data are complete before push msg.
-  // Otherwise, the transaction is silently returned intact.
-
-  let isComplete = true;
-
-  switch (transaction.mode) {
-    case "send":
-      if (!transaction.recipient || transaction.amount.lte(0)) {
-        isComplete = false;
-      } else {
-        msg.push({
-          typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-          value: {
-            fromAddress: account.freshAddress,
-            toAddress: transaction.recipient,
-            amount: [
-              {
-                denom: account.currency.units[1].code,
-                amount: transaction.amount.toString(),
-              },
-            ],
-          },
-        });
-      }
-      break;
-
-    case "delegate":
-      if (!transaction.validators || transaction.validators.length < 1) {
-        isComplete = false;
-      } else {
-        const validator = transaction.validators[0];
-        if (!validator) {
-          isComplete = false;
-          break;
-        } else if (!validator.address || transaction.amount.lte(0)) {
-          isComplete = false;
-        }
-
-        msg.push({
-          typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
-          value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: validator.address,
-            amount: {
-              denom: account.currency.units[1].code,
-              amount: transaction.amount.toString(),
-            },
-          },
-        });
-      }
-      break;
-
-    case "undelegate":
-      if (
-        !transaction.validators ||
-        transaction.validators.length < 1 ||
-        !transaction.validators[0].address ||
-        transaction.validators[0].amount.lte(0)
-      ) {
-        isComplete = false;
-      } else {
-        msg.push({
-          typeUrl: "/cosmos.staking.v1beta1.MsgUndelegate",
-          value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: transaction.validators[0].address,
-            amount: {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          },
-        });
-      }
-      break;
-
-    case "redelegate":
-      if (
-        !transaction.sourceValidator ||
-        !transaction.validators ||
-        transaction.validators.length < 1 ||
-        !transaction.validators[0].address ||
-        transaction.validators[0].amount.lte(0)
-      ) {
-        isComplete = false;
-      } else {
-        msg.push({
-          typeUrl: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
-          value: {
-            validatorSrcAddress: transaction.sourceValidator,
-            delegatorAddress: account.freshAddress,
-            validatorDstAddress: transaction.validators[0].address,
-            amount: {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          },
-        });
-      }
-      break;
-
-    case "claimReward":
-      if (
-        !transaction.validators ||
-        transaction.validators.length < 1 ||
-        !transaction.validators[0].address
-      ) {
-        isComplete = false;
-      } else {
-        msg.push({
-          typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-          value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: transaction.validators[0].address,
-          },
-        });
-      }
-      break;
-
-    case "claimRewardCompound":
-      if (
-        !transaction.validators ||
-        transaction.validators.length < 1 ||
-        !transaction.validators[0].address ||
-        transaction.validators[0].amount.lte(0)
-      ) {
-        isComplete = false;
-      } else {
-        msg.push({
-          typeUrl: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-          value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: transaction.validators[0].address,
-          },
-        });
-
-        msg.push({
-          typeUrl: "/cosmos.staking.v1beta1.MsgDelegate",
-          value: {
-            delegatorAddress: account.freshAddress,
-            validatorAddress: transaction.validators[0].address,
-            amount: {
-              denom: account.currency.units[1].code,
-              amount: transaction.validators[0].amount.toString(),
-            },
-          },
-        });
-      }
-      break;
-  }
-
-  if (!isComplete) {
-    return [];
-  }
-
-  return msg;
-};
-
-export const buildTransaction2 = async (
   account: Account,
   transaction: Transaction
 ): Promise<{ aminoMsgs: AminoMsg[]; protoMsgs: ProtoMsg[] }> => {
@@ -427,59 +256,6 @@ export const buildTransaction2 = async (
 };
 
 export const postBuildTransaction = async (
-  account: CosmosAccount,
-  transaction: Transaction,
-  pubkey: any,
-  unsignedPayload: any,
-  signature: Uint8Array
-): Promise<any> => {
-  const txBodyFields: TxBodyEncodeObject = {
-    typeUrl: "/cosmos.tx.v1beta1.TxBody",
-    value: {
-      messages: unsignedPayload,
-      memo: transaction.memo || "",
-    },
-  };
-
-  // @ts-expect-error TODO: monorepo detected this error
-  const registry = new Registry([
-    ["/cosmos.staking.v1beta1.MsgDelegate", MsgDelegate],
-    ["/cosmos.staking.v1beta1.MsgUndelegate", MsgUndelegate],
-    ["/cosmos.staking.v1beta1.MsgBeginRedelegate", MsgBeginRedelegate],
-    [
-      "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-      MsgWithdrawDelegatorReward,
-    ],
-  ]);
-
-  const { sequence } = await defaultCosmosAPI.getAccount(account.freshAddress);
-
-  const txBodyBytes = registry.encode(txBodyFields);
-
-  const authInfoBytes = makeAuthInfoBytes(
-    [{ pubkey, sequence }],
-    [
-      {
-        amount: transaction.fees?.toString() || new BigNumber(2500).toString(),
-        denom: account.currency.units[1].code,
-      },
-    ],
-    transaction.gas?.toNumber() || new BigNumber(250000).toNumber(),
-    SignMode.SIGN_MODE_LEGACY_AMINO_JSON
-  );
-
-  const txRaw = TxRaw.fromPartial({
-    bodyBytes: txBodyBytes,
-    authInfoBytes,
-    signatures: [signature],
-  });
-
-  const tx_bytes = Array.from(Uint8Array.from(TxRaw.encode(txRaw).finish()));
-
-  return tx_bytes;
-};
-
-export const postBuildTransaction2 = async (
   signResponse: AminoSignResponse,
   protoMsgs: Array<ProtoMsg>
 ): Promise<Uint8Array> => {
--- libs/ledger-live-common/src/families/cosmos/js-signOperation.ts
+++ libs/ledger-live-common/src/families/cosmos/js-signOperation.ts
@@ -1,19 +1,11 @@
-import type { CosmosAccount, Transaction } from "./types";
+import type { Transaction } from "./types";
 import { Observable } from "rxjs";
 import { withDevice } from "../../hw/deviceAccess";
 import { encodeOperationId } from "../../operation";
-import Cosmos from "@ledgerhq/hw-app-cosmos";
-// import { AminoTypes } from "@cosmjs/stargate";
 import { LedgerSigner } from "@cosmjs/ledger-amino";
 import { stringToPath } from "@cosmjs/crypto";
-import {
-  buildTransaction,
-  buildTransaction2,
-  postBuildTransaction,
-  postBuildTransaction2,
-} from "./js-buildTransaction";
+import { buildTransaction, postBuildTransaction } from "./js-buildTransaction";
 import BigNumber from "bignumber.js";
-import { Secp256k1Signature } from "@cosmjs/crypto";
 import { makeSignDoc } from "@cosmjs/launchpad";
 
 import type {
@@ -38,16 +30,12 @@ const signOperation = ({
       let cancelled;
 
       async function main() {
-        const hwApp = new Cosmos(transport);
         const cosmosAPI = new CosmosAPI(account.currency.id);
         const { accountNumber, sequence } = await cosmosAPI.getAccount(
           account.freshAddress
         );
-
-        const chainId = await cosmosAPI.getChainId();
-
         o.next({ type: "device-signature-requested" });
-        const { aminoMsgs, protoMsgs } = await buildTransaction2(
+        const { aminoMsgs, protoMsgs } = await buildTransaction(
           account,
           transaction
         );
@@ -64,72 +52,25 @@ const signOperation = ({
         // Note:
         // Cosmos Nano App sign data in Amino way only, not Protobuf.
         // This is a legacy outdated standard and a long-term blocking point.
+        const chainId = await cosmosAPI.getChainId();
+        const signDoc = makeSignDoc(
+          aminoMsgs,
+          feeToEncode,
+          chainId,
+          transaction.memo || "",
+          accountNumber.toString(),
+          sequence.toString()
+        );
+        const ledgerSigner = new LedgerSigner(transport, {
+          hdPaths: [stringToPath("m/" + account.freshAddressPath)],
+          prefix: account.currency.id,
+        });
 
-        let tx_bytes: Uint8Array;
-        if (account.currency.id === "cosmos") {
-          const { publicKey } = await hwApp.getAddress(
-            account.freshAddressPath,
-            "cosmos",
-            false
-          );
-          const pubkey = {
-            typeUrl: "/cosmos.crypto.secp256k1.PubKey",
-            value: new Uint8Array([
-              ...new Uint8Array([10, 33]),
-              ...new Uint8Array(Buffer.from(publicKey, "hex")),
-            ]),
-          };
-          const unsignedPayload = await buildTransaction(
-            account as CosmosAccount,
-            transaction
-          );
-          const message = {
-            chain_id: chainId,
-            account_number: accountNumber.toString(),
-            sequence: sequence.toString(),
-            fee: feeToEncode,
-            msgs: aminoMsgs,
-            memo: transaction.memo || "",
-          };
-          const { signature } = await hwApp.sign(
-            account.freshAddressPath,
-            JSON.stringify(sortedObject(message))
-          );
-          if (!signature) {
-            throw new Error("Cosmos: no Signature Found");
-          }
-          const secp256k1Signature = Secp256k1Signature.fromDer(
-            new Uint8Array(signature)
-          ).toFixedLength();
-
-          tx_bytes = await postBuildTransaction(
-            account as CosmosAccount,
-            transaction,
-            pubkey,
-            unsignedPayload,
-            secp256k1Signature
-          );
-        } else {
-          const signDoc = makeSignDoc(
-            aminoMsgs,
-            feeToEncode,
-            chainId,
-            transaction.memo || "",
-            accountNumber.toString(),
-            sequence.toString()
-          );
-          const ledgerSigner = new LedgerSigner(transport, {
-            hdPaths: [stringToPath("m/" + account.freshAddressPath)],
-            prefix: account.currency.id,
-          });
-
-          const signResponse = await ledgerSigner.signAmino(
-            account.freshAddress,
-            signDoc
-          );
-
-          tx_bytes = await postBuildTransaction2(signResponse, protoMsgs);
-        }
+        const signResponse = await ledgerSigner.signAmino(
+          account.freshAddress,
+          signDoc
+        );
+        const tx_bytes = await postBuildTransaction(signResponse, protoMsgs);
         const signed = Buffer.from(tx_bytes).toString("hex");
 
         if (cancelled) {
@@ -216,24 +157,5 @@ const signOperation = ({
       };
     })
   );
-
-const sortedObject = (obj) => {
-  if (typeof obj !== "object" || obj === null) {
-    return obj;
-  }
-
-  if (Array.isArray(obj)) {
-    return obj.map(sortedObject);
-  }
-
-  const sortedKeys = Object.keys(obj).sort();
-  const result = {};
-
-  sortedKeys.forEach((key) => {
-    result[key] = sortedObject(obj[key]);
-  });
-
-  return result;
-};
 
 export default signOperation;
