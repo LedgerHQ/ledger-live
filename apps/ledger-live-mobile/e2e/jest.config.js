cbcd497063f0dd50af15a3cd727933ed79896c30
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-11-15 18:08:19 +0100

    chore(deps): update detox to v20

---
+++ apps/ledger-live-mobile/e2e/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  maxWorkers: 1,
+  preset: "ts-jest",
+  // globalSetup: "./global-setup.ts",
+  // globalTeardown: "./global-teardown.ts",
+  setupFilesAfterEnv: ["<rootDir>/e2e/setup.ts"],
+  testTimeout: 600000,
+  rootDir: "..",
+  testMatch: ["<rootDir>/e2e/**/*.spec.ts"],
+  reporters: ["detox/runners/jest/reporter"],
+  globalSetup: "detox/runners/jest/globalSetup",
+  globalTeardown: "detox/runners/jest/globalTeardown",
+  testEnvironment: "detox/runners/jest/testEnvironment",
+  verbose: true,
+};
