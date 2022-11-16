d9ad1d059d75d504128cb9fbd21dfbf851e48d4a
Come Grellard <come.grellard@ledger.fr>
2022-11-16 16:27:34 +0100

    ui:native Cards empty components fixed

--- libs/ui/packages/native/src/components/Cards/CardA/index.tsx
+++ libs/ui/packages/native/src/components/Cards/CardA/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  test?: string;
+};
 
 const CardA = (props: Props): React.ReactElement => {
-  const {} = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { test } = props;
 
   return <></>;
 };
--- libs/ui/packages/native/storybook/stories/Cards/Cards.stories.tsx
+++ libs/ui/packages/native/storybook/stories/Cards/Cards.stories.tsx
@@ -1,11 +1,7 @@
-import { text } from "@storybook/addon-knobs";
 import { storiesOf } from "../storiesOf";
-import { select, boolean } from "@storybook/addon-knobs";
-import { action } from "@storybook/addon-actions";
 import React from "react";
 import CardA from "../../../src/components/Cards/CardA";
 import CardB from "../../../src/components/Cards/CardB";
-import { InfoMedium } from "@ledgerhq/icons-ui/native";
 
 const CardAStory = () => <CardA></CardA>;
 
