2fa4d32d16533531fc418ff848978d22b2c9809f
Henri Ly <henri.ly-ext@ledger.fr>
2022-11-16 14:02:11 +0100

    lint fix

--- libs/ledger-live-common/src/families/cosmos/logic.ts
+++ libs/ledger-live-common/src/families/cosmos/logic.ts
@@ -15,7 +15,6 @@ import type {
   CosmosAccount,
 } from "./types";
 import type { Transaction } from "../../generated/types";
-import { calculateFees } from "./js-prepareTransaction";
 import type { Unit } from "@ledgerhq/types-cryptoassets";
 
 export const COSMOS_MAX_REDELEGATIONS = 7;
