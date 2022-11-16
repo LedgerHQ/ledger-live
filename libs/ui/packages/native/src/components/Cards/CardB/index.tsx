edd02d103674cfea1f33e2d7b595ad63ea088270
Martin Cayuelas <martin.cayuelas@ledger.fr>
2022-11-16 17:17:19 +0100

    🚧 WIP : Asset content Card (1)

--- libs/ui/packages/native/src/components/Cards/CardB/index.tsx
+++ libs/ui/packages/native/src/components/Cards/CardB/index.tsx
@@ -1,11 +1,111 @@
 import React from "react";
+import { ArrowRightMedium, CloseMedium } from "@ledgerhq/icons-ui/native";
 
-type Props = {};
+import { TouchableOpacityProps, TouchableOpacity } from "react-native";
+import styled, { useTheme } from "styled-components/native";
 
-const CardB = (props: Props): React.ReactElement => {
-  const {} = props;
+import { Text, Flex, Link, rgba } from "@ledgerhq/native-ui";
 
-  return <></>;
+export type CardProps = TouchableOpacityProps & {
+  tag?: string;
+  title?: string;
+  cta?: string;
+  image?: string;
+  onPressDismiss?: () => void;
+};
+
+export const Base = styled(TouchableOpacity)`
+  border-radius: 8px;
+`;
+
+export const Container = styled(Flex)`
+  background: ${(p) => rgba(p.theme.colors.neutral.c20, 0.4)};
+  flex-direction: row;
+  border-radius: 8px;
+`;
+
+export const ImageContainer = styled(Flex)`
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  position: relative;
+`;
+
+export const CloseContainer = styled(TouchableOpacity)`
+  background-color: ${(p) => p.theme.colors.neutral.c30};
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  border-radius: 50%;
+  height: 24px;
+  width: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CloseButton = (props: CardProps) => {
+  const { colors } = useTheme();
+  return (
+    <CloseContainer onPress={props.onPressDismiss}>
+      <CloseMedium size={14} color={colors.neutral.c100} />
+    </CloseContainer>
+  );
+};
+
+const CardContainer = (props: CardProps): React.ReactElement => {
+  const { tag, title, cta, onPress } = props;
+
+  return (
+    <Container height={125} width={"100%"}>
+      <Flex justifyContent={"space-between"} alignItems={"flex-start"} p={"12px"} flexShrink={1}>
+        <Flex>
+          <Text
+            variant={"small"}
+            fontWeight={"semiBold"}
+            color="neutral.c70"
+            uppercase
+            numberOfLines={1}
+          >
+            {tag}
+          </Text>
+
+          <Text
+            variant={"large"}
+            numberOfLines={3}
+            fontWeight={"semiBold"}
+            color="neutral.c100"
+            ellipsizeMode="tail"
+            mt={"8px"}
+          >
+            {title}
+          </Text>
+        </Flex>
+        <Link
+          type={"main"}
+          size={"medium"}
+          iconPosition="right"
+          Icon={() => <ArrowRightMedium color="primary.c80" />}
+          onPress={onPress}
+        >
+          <Text variant={"paragraph"} fontWeight={"semiBold"} color="primary.c80" numberOfLines={1}>
+            {cta}
+          </Text>
+        </Link>
+      </Flex>
+
+      <ImageContainer width={115} backgroundColor="red">
+        <CloseButton />
+      </ImageContainer>
+    </Container>
+  );
+};
+
+const CardB = (props: CardProps): React.ReactElement => {
+  return (
+    <Base {...props} activeOpacity={0.5}>
+      <CardContainer {...props} />
+    </Base>
+  );
 };
 
 export default CardB;
--- libs/ui/packages/native/storybook/stories/Cards/Cards.stories.tsx
+++ libs/ui/packages/native/storybook/stories/Cards/Cards.stories.tsx
@@ -1,14 +1,40 @@
-import { text } from "@storybook/addon-knobs";
 import { storiesOf } from "../storiesOf";
-import { select, boolean } from "@storybook/addon-knobs";
-import { action } from "@storybook/addon-actions";
 import React from "react";
 import CardA from "../../../src/components/Cards/CardA";
 import CardB from "../../../src/components/Cards/CardB";
-import { InfoMedium } from "@ledgerhq/icons-ui/native";
+import Flex from "../../../src/components/Layout/Flex";
 
 const CardAStory = () => <CardA></CardA>;
 
-const CardBStory = () => <CardB></CardB>;
+const CardBStory = () => (
+  <>
+    <Flex
+      backgroundColor={"primary.c70"}
+      height={300}
+      width={500}
+      alignItems="center"
+      justifyContent={"center"}
+      p={"16px"}
+    >
+      <CardB
+        tag="ANNouncement"
+        title="This is a card title which spans multiple lines and if takes more lines the text will go out.This is a card title which spans multiple lines and if takes more lines the text will go out"
+        cta="My cta to click"
+      />
+    </Flex>
+
+    <Flex
+      p={"16px"}
+      mt={18}
+      backgroundColor={"primary.c70"}
+      height={300}
+      width={500}
+      alignItems="center"
+      justifyContent={"center"}
+    >
+      <CardB tag="stack" title="This is a short message" cta="My cta to click" />
+    </Flex>
+  </>
+);
 
 storiesOf((story) => story("Cards", module).add("Card A", CardAStory).add("Card B", CardBStory));
