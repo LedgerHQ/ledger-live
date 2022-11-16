c2b42dfcd6a7edd2c8eae55389ab0677aac37462
Come Grellard <come.grellard@ledger.fr>
2022-11-16 16:12:25 +0100

    ui:native Cards components files created

---
+++ libs/ui/packages/native/src/components/Cards/CardA/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const CardA = (props: Props): React.ReactElement => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default CardA;
---
+++ libs/ui/packages/native/src/components/Cards/CardB/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const CardB = (props: Props): React.ReactElement => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default CardB;
