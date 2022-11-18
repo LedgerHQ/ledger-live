2f207c95e877d8c86a35dca98a3f29c9e4a7e4d8
lvndry <lvndry@proton.me>
2022-11-18 12:39:57 +0100

    remove trailing slash for env

--- libs/ledger-live-common/src/env.ts
+++ libs/ledger-live-common/src/env.ts
@@ -358,7 +358,7 @@ const envDefinitions = {
     desc: "enable an experimental swap interface",
   },
   EXPLORER: {
-    def: "https://explorers.api-01.live.ledger-stg.com/",
+    def: "https://explorers.api-01.live.ledger-stg.com",
     parser: stringParser,
     desc: "Ledger generic explorer API (STAGING - CHANGE TO PRODUCTION BEFORE MERGE)",
   },
