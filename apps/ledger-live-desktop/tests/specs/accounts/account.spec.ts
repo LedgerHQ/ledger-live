c74d52f783ea496dd0f906730190b19d6e5ef570
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-11-18 11:22:14 +0100

    chore: testing with fewer pages - without Layout

--- apps/ledger-live-desktop/tests/specs/accounts/account.spec.ts
+++ apps/ledger-live-desktop/tests/specs/accounts/account.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-done-callback */
 import test from "../../fixtures/common";
 import { expect } from "@playwright/test";
 import { PortfolioPage } from "../../models/PortfolioPage";
@@ -7,15 +8,16 @@ import { Layout } from "../../models/Layout";
 
 test.use({ userdata: "skip-onboarding" });
 
-const currencies = ["BTC", "LTC", "ETH", "ATOM", "XTZ", "XRP"];
+const currencies = ["BTC", "LTC"]; // , "ETH", "ATOM", "XTZ", "XRP"];
 
+// TODO: put back parallel
 test.describe.serial("Accounts", () => {
   for (const currency of currencies) {
     test(`[${currency}] Add account`, async ({ page }) => {
       const portfolioPage = new PortfolioPage(page);
       const addAccountModal = new AddAccountModal(page);
-      const deviceAction = new DeviceAction(page);
-      const layout = new Layout(page);
+      // const deviceAction = new DeviceAction(page);
+      // const layout = new Layout(page);
 
       await test.step(`[${currency}] Open modal`, async () => {
         await portfolioPage.openAddAccountModal();
@@ -23,34 +25,32 @@ test.describe.serial("Accounts", () => {
         await expect.soft(addAccountModal.container).toHaveScreenshot(`open-modal.png`);
       });
 
-      await test.step(`[${currency}] Select currency`, async () => {
-        await addAccountModal.select(currency);
-        await expect.soft(addAccountModal.container).toHaveScreenshot(
-          `${currency}-select.png`,
-        );
-        await addAccountModal.continue();
-      });
+      // await test.step(`[${currency}] Select currency`, async () => {
+      //   await addAccountModal.select(currency);
+      //   await expect.soft(addAccountModal.container).toHaveScreenshot(`${currency}-select.png`);
+      //   await addAccountModal.continue();
+      // });
 
-      await test.step(`[${currency}] Open device app`, async () => {
-        await deviceAction.openApp();
-        await addAccountModal.waitForSync();
-        await expect.soft(addAccountModal.container).toHaveScreenshot(
-          `${currency}-accounts-list.png`,
-        );
-      });
+      // await test.step(`[${currency}] Open device app`, async () => {
+      //   await deviceAction.openApp();
+      //   await addAccountModal.waitForSync();
+      //   await expect
+      //     .soft(addAccountModal.container)
+      //     .toHaveScreenshot(`${currency}-accounts-list.png`);
+      // });
 
-      await test.step(`[${currency}] Scan and add accounts`, async () => {
-        await addAccountModal.addAccounts();
-        await expect.soft(addAccountModal.container).toHaveScreenshot(
-          `${currency}-success.png`,
-        );
-      });
+      // await test.step(`[${currency}] Scan and add accounts`, async () => {
+      //   await addAccountModal.addAccounts();
+      //   await expect.soft(addAccountModal.container).toHaveScreenshot(`${currency}-success.png`);
+      // });
 
-      await test.step(`[${currency}] Done`, async () => {
-        await addAccountModal.done();
-        await layout.totalBalance.waitFor({ state: "visible" });
-        await expect.soft(page).toHaveScreenshot(`${currency}-complete.png`, { mask: [page.locator('canvas')] });
-      });
+      // await test.step(`[${currency}] Done`, async () => {
+      //   await addAccountModal.done();
+      //   await layout.totalBalance.waitFor({ state: "visible" });
+      //   await expect
+      //     .soft(page)
+      //     .toHaveScreenshot(`${currency}-complete.png`, { mask: [page.locator("canvas")] });
+      // });
     });
   }
 });
