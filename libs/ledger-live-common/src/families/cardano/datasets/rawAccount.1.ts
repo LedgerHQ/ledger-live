09db09980b4c5ea6805b70406aa37c9224806011
Pavan Vora <pavanvora.cs@gmail.com>
2022-11-18 20:10:45 +0530

    lint fix

--- libs/ledger-live-common/src/families/cardano/datasets/rawAccount.1.ts
+++ libs/ledger-live-common/src/families/cardano/datasets/rawAccount.1.ts
@@ -6,13 +6,15 @@ export const cardanoRawAccount1: AccountRaw = {
   name: "Cardano (Testnet) 1",
   derivationMode: "cardano",
   index: 0,
-  freshAddress: "addr_test1qpj84xm4jlpcsvmrnldj86zz7g4mwzr7qg90wdjpn64uea44mcsd3mzvmr9qhfscxkyla3aqnzttwepay9mt3fz84smqyjvhh4",
+  freshAddress:
+    "addr_test1qpj84xm4jlpcsvmrnldj86zz7g4mwzr7qg90wdjpn64uea44mcsd3mzvmr9qhfscxkyla3aqnzttwepay9mt3fz84smqyjvhh4",
   freshAddressPath: "1852'/1815'/0'/0/2",
   freshAddresses: [
     {
       derivationPath: "1852'/1815'/0'/0/2",
-      address: "addr_test1qpj84xm4jlpcsvmrnldj86zz7g4mwzr7qg90wdjpn64uea44mcsd3mzvmr9qhfscxkyla3aqnzttwepay9mt3fz84smqyjvhh4"
-    }
+      address:
+        "addr_test1qpj84xm4jlpcsvmrnldj86zz7g4mwzr7qg90wdjpn64uea44mcsd3mzvmr9qhfscxkyla3aqnzttwepay9mt3fz84smqyjvhh4",
+    },
   ],
   blockHeight: 0,
   operations: [],
@@ -21,5 +23,5 @@ export const cardanoRawAccount1: AccountRaw = {
   unitMagnitude: 6,
   lastSyncDate: "",
   balance: "10098829375",
-  xpub: "806499588e0c4a58f4119f7e6e096bf42c3f774a528d2acec9e82ceebf87d1ceb3d4f3622dd2c77c65cc89c123f79337db22cf8a69f122e36dab1bf5083bf82d"
+  xpub: "806499588e0c4a58f4119f7e6e096bf42c3f774a528d2acec9e82ceebf87d1ceb3d4f3622dd2c77c65cc89c123f79337db22cf8a69f122e36dab1bf5083bf82d",
 };
