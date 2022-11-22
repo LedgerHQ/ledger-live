8ec2063061beb4a77fdf66c2ea99287f04193d71
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-11-22 19:20:35 +0100

    Create proper error types for custom transaction errors

--- libs/ledger-live-common/src/families/elrond/errors.ts
+++ libs/ledger-live-common/src/families/elrond/errors.ts
@@ -3,3 +3,11 @@ import { createCustomErrorClass } from "@ledgerhq/errors";
 export const DecimalsLimitReached = createCustomErrorClass(
   "DecimalsLimitReached"
 );
+
+export const MinDelegatedAmountError = createCustomErrorClass(
+  "MinDelegatedAmountError"
+);
+
+export const MinUndelegatedAmountError = createCustomErrorClass(
+  "MinUndelegatedAmountError"
+);
