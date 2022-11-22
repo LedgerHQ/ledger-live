63fc8007f1311e9ff80239a45540751112ad1a83
haammar-ledger <hakim.aammar-ext@ledger.fr>
2022-11-22 19:27:08 +0100

    Proper transaction validation (except undelegate)

--- libs/ledger-live-common/src/families/elrond/js-getTransactionStatus.ts
+++ libs/ledger-live-common/src/families/elrond/js-getTransactionStatus.ts
@@ -1,5 +1,7 @@
+import BigNumber from "bignumber.js";
 import {
   NotEnoughBalance,
+  NotEnoughBalanceInParentAccount,
   RecipientRequired,
   InvalidAddress,
   FeeNotLoaded,
@@ -7,14 +9,19 @@ import {
   FeeTooHigh,
   AmountRequired,
 } from "@ledgerhq/errors";
-import { DECIMALS_LIMIT } from "./constants";
+import { getCryptoCurrencyById, formatCurrencyUnit } from "../../currencies";
+import { DECIMALS_LIMIT, MIN_DELEGATION_AMOUNT } from "./constants";
 import type { ElrondAccount, Transaction, TransactionStatus } from "./types";
 import {
   isValidAddress,
   isSelfTransaction,
-  computeTransactionValue,
+  isAmountSpentFromBalance,
 } from "./logic";
-import { DecimalsLimitReached } from "./errors";
+import {
+  DecimalsLimitReached,
+  MinDelegatedAmountError,
+  MinUndelegatedAmountError,
+} from "./errors";
 
 const getTransactionStatus = async (
   a: ElrondAccount,
@@ -37,49 +44,68 @@ const getTransactionStatus = async (
     errors.fees = new FeeNotLoaded();
   }
 
+  if (!errors.amount && t.amount.eq(0) && !t.useAllAmount) {
+    errors.amount = new AmountRequired();
+  }
+
+  let totalSpent = new BigNumber(0);
+
   const tokenAccount =
     (t.subAccountId &&
       a.subAccounts &&
       a.subAccounts.find((ta) => ta.id === t.subAccountId)) ||
     null;
 
-  if (!errors.amount && t.amount.eq(0) && !t.useAllAmount) {
-    errors.amount = new AmountRequired();
-  }
-
-  const { amount, totalSpent, estimatedFees } = await computeTransactionValue(
-    t,
-    a,
-    tokenAccount
-  );
-
-  if (estimatedFees.gt(a.balance)) {
-    errors.amount = new NotEnoughBalance();
-  }
-
   if (tokenAccount) {
-    if (totalSpent.gt(tokenAccount.balance)) {
+    totalSpent = t.fees || new BigNumber(0); // totalSpent in EGLD, not token currency
+
+    if (!errors.amount && t.amount.gt(tokenAccount.balance)) {
       errors.amount = new NotEnoughBalance();
     }
-    if (!totalSpent.decimalPlaces(DECIMALS_LIMIT).isEqualTo(totalSpent)) {
+
+    if (
+      !errors.amount &&
+      !totalSpent.decimalPlaces(DECIMALS_LIMIT).isEqualTo(totalSpent)
+    ) {
       errors.amount = new DecimalsLimitReached();
     }
   } else {
-    if (totalSpent.gt(a.balance)) {
-      errors.amount = new NotEnoughBalance();
-    }
+    totalSpent = isAmountSpentFromBalance(t.mode)
+      ? t.fees?.plus(t.amount) || t.amount
+      : t.fees || new BigNumber(0);
 
-    const isZeroAmountOperation = t.mode !== "send" && t.mode !== "delegate";
-    if (!isZeroAmountOperation && amount.div(10).lt(estimatedFees)) {
+    if (t.mode === "send" && t.fees && t.amount.div(10).lt(t.fees)) {
       warnings.feeTooHigh = new FeeTooHigh();
     }
+
+    if (!errors.amount && t.amount.lt(MIN_DELEGATION_AMOUNT)) {
+      const egld = getCryptoCurrencyById("elrond").units[0];
+      const formattedAmount = formatCurrencyUnit(egld, MIN_DELEGATION_AMOUNT, {
+        showCode: true,
+      });
+      if (t.mode === "delegate") {
+        errors.amount = new MinDelegatedAmountError("", {
+          formattedAmount,
+        });
+      } else if (t.mode === "unDelegate") {
+        errors.amount = new MinUndelegatedAmountError("", {
+          formattedAmount,
+        });
+      }
+    }
+  }
+
+  if (!errors.amount && totalSpent.gt(a.spendableBalance)) {
+    errors.amount = tokenAccount
+      ? new NotEnoughBalanceInParentAccount()
+      : new NotEnoughBalance();
   }
 
   return Promise.resolve({
     errors,
     warnings,
-    estimatedFees,
-    amount,
+    estimatedFees: t.fees || new BigNumber(0),
+    amount: t.amount,
     totalSpent,
   });
 };
