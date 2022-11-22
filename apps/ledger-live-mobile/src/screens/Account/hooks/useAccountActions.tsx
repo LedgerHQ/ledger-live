f197fd7c22e0074836409ab8a163295b97f26df5
Trent Rogers <trentkrogers@gmail.com>
2022-11-22 12:24:41 -0600

    disable send and receive for p chain accounts

--- apps/ledger-live-mobile/src/screens/Account/hooks/useAccountActions.tsx
+++ apps/ledger-live-mobile/src/screens/Account/hooks/useAccountActions.tsx
@@ -96,6 +96,12 @@ export default function useAccountActions({
     [account, parentAccount, decorators],
   );
 
+  const canSend = useMemo(() => currency?.id !== "avalanchepchain", [currency]);
+  const canReceive = useMemo(
+    () => currency?.id !== "avalanchepchain",
+    [currency],
+  );
+
   const actionButtonSwap: ActionButtonEvent = {
     id: "swap",
     navigationParams: [
@@ -212,8 +218,8 @@ export default function useAccountActions({
     ...(availableOnSwap ? [actionButtonSwap] : []),
     ...(!readOnlyModeEnabled && canBeBought ? [actionButtonBuy] : []),
     ...(!readOnlyModeEnabled && canBeSold ? [actionButtonSell] : []),
-    ...(!readOnlyModeEnabled ? [SendAction] : []),
-    ReceiveAction,
+    ...(!readOnlyModeEnabled && canSend ? [SendAction] : []),
+    ...(canReceive ? [ReceiveAction] : []),
   ];
   const secondaryActions = [
     ...baseActions,
