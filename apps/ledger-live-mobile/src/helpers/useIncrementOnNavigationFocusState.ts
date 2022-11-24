eb02f6c79fec633717711c35881cd7663dcd6ae4
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-11-24 12:06:14 +0100

    chore: fix types on helpers for navigation-on-focus actions

--- apps/ledger-live-mobile/src/helpers/useIncrementOnNavigationFocusState.ts
+++ apps/ledger-live-mobile/src/helpers/useIncrementOnNavigationFocusState.ts
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import { NavigationProp } from "@react-navigation/native";
 
-// TODO: fix type
-// A state that increments a counter on focus from the navigation mechanism
+// Returns a counter that increments itself when the component using it
+// gets a new focus from the navigation mechanism
 export function useIncrementOnNavigationFocusState<
-  NavigationType extends NavigationProp<ReactNavigation.RootParamList>,
+  NavigationType extends NavigationProp<Record<string, unknown>>,
 >(navigation: NavigationType): number {
   const [nonce, setNonce] = useState<number>(0);
 
   useEffect(() => {
-    return navigation.addListener("focus", () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       setNonce(prev => prev + 1);
     });
+
+    return unsubscribe;
   }, [navigation]);
 
   return nonce;
