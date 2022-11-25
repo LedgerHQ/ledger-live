0036ee6c91f5abc2d9eff9193e9153ef4df4b3ad
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-11-25 17:05:58 +0100

    feat: BLE pairing flow as components

---
+++ apps/ledger-live-mobile/src/components/BleDevicePairingFlow/BleDeviceItem.tsx
@@ -0,0 +1,77 @@
+import React, { useMemo } from "react";
+import { Device } from "@ledgerhq/live-common/hw/actions/types";
+import { Flex, Icons, Text } from "@ledgerhq/native-ui";
+import { ChevronRightMedium } from "@ledgerhq/native-ui/assets/icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useTranslation } from "react-i18next";
+import { DeviceModelId } from "@ledgerhq/types-devices";
+
+export type RenderedDevice = Device & {
+  isAlreadyKnown: boolean;
+};
+
+type Props = {
+  deviceMeta: RenderedDevice;
+  onSelect: () => void;
+};
+
+const BleDeviceItem = ({ deviceMeta, onSelect }: Props) => {
+  const { t } = useTranslation();
+  const { deviceName, isAlreadyKnown } = deviceMeta;
+
+  const deviceIcon = useMemo(() => {
+    switch (deviceMeta.modelId) {
+      case DeviceModelId.nanoFTS:
+        return <Icons.PowerMedium size={20} />;
+      case DeviceModelId.nanoX:
+      default:
+        return <Icons.NanoXFoldedMedium size={20} />;
+    }
+  }, [deviceMeta.modelId]);
+
+  if (isAlreadyKnown) {
+    return (
+      <Flex mb={3} opacity="0.5">
+        <Flex
+          alignItems="center"
+          flexDirection="row"
+          backgroundColor="neutral.c30"
+          borderRadius={8}
+          padding={6}
+        >
+          {deviceIcon}
+          <Flex flexDirection="column">
+            <Text flex={1} ml={4} variant="large" fontWeight="semiBold">
+              {deviceName}
+            </Text>
+            <Text flex={1} ml={4} variant="small">
+              {t("blePairingFlow.scanning.alreadyPaired")}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex mb={3}>
+      <TouchableOpacity onPress={onSelect}>
+        <Flex
+          alignItems="center"
+          flexDirection="row"
+          backgroundColor="neutral.c30"
+          borderRadius={8}
+          padding={6}
+        >
+          {deviceIcon}
+          <Text flex={1} ml={4} variant="large" fontWeight="semiBold">
+            {deviceName}
+          </Text>
+          <ChevronRightMedium size={20} color="primary.c80" />
+        </Flex>
+      </TouchableOpacity>
+    </Flex>
+  );
+};
+
+export default BleDeviceItem;
---
+++ apps/ledger-live-mobile/src/components/BleDevicePairingFlow/BleDevicePairing.tsx
@@ -0,0 +1,154 @@
+import React, { ReactNode, useCallback, useEffect } from "react";
+import { useTheme } from "styled-components/native";
+import { useBleDevicePairing } from "@ledgerhq/live-common/ble/hooks/useBleDevicePairing";
+import { Device } from "@ledgerhq/live-common/hw/actions/types";
+import { useTranslation } from "react-i18next";
+import { getDeviceModel } from "@ledgerhq/devices";
+import { Flex, InfiniteLoader, Text, Button } from "@ledgerhq/native-ui";
+import {
+  CircledCheckSolidMedium,
+  CircledCrossSolidMedium,
+} from "@ledgerhq/native-ui/assets/icons";
+
+import { getDeviceAnimation } from "../../helpers/getDeviceAnimation";
+import Animation from "../Animation";
+import DeviceSetupView from "../DeviceSetupView";
+
+const TIMEOUT_AFTER_PAIRED_MS = 2000;
+
+export type BleDevicePairingProps = {
+  onPaired: (device: Device) => void;
+  onRetry: () => void;
+  deviceToPair: Device;
+};
+
+/**
+ * Runs a BLE pairing with the given device. Displays pairing, success or error steps.
+ *
+ * A closing cross is displayed to the user during the pairing, which either calls onPaired if
+ * the device is already paired, or onRetry otherwise.
+ *
+ * @param deviceToPair Device to pair
+ * @param onPaired Function called when pairing was successful
+ * @param onRetry Function called when the user chooses to retry on unsuccessful pairing
+ */
+const BleDevicePairing = ({
+  deviceToPair,
+  onPaired,
+  onRetry,
+}: BleDevicePairingProps) => {
+  const { t } = useTranslation();
+  const { colors } = useTheme();
+  const theme = colors.type as "dark" | "light";
+
+  const productName =
+    getDeviceModel(deviceToPair.modelId).productName || deviceToPair.modelId;
+  const deviceName = deviceToPair.deviceName || productName;
+
+  const { isPaired, pairingError } = useBleDevicePairing({
+    deviceId: deviceToPair.deviceId,
+  });
+
+  useEffect(() => {
+    if (isPaired) {
+      // To display the success to the user
+      setTimeout(() => {
+        onPaired(deviceToPair);
+      }, TIMEOUT_AFTER_PAIRED_MS);
+    }
+  }, [isPaired, deviceToPair, onPaired]);
+
+  const handleClose = useCallback(() => {
+    if (isPaired) {
+      onPaired(deviceToPair);
+    } else {
+      onRetry();
+    }
+  }, [deviceToPair, isPaired, onPaired, onRetry]);
+
+  let content: ReactNode;
+
+  if (isPaired) {
+    content = (
+      <>
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          p={1}
+          borderWidth={2}
+          borderRadius="9999px"
+          borderColor={colors.success.c80}
+          mb={9}
+        >
+          <CircledCheckSolidMedium color={colors.success.c80} size={48} />
+        </Flex>
+        <Text mb={4} textAlign="center" variant="h4" fontWeight="semiBold">
+          {t("blePairingFlow.pairing.success.title", {
+            deviceName,
+          })}
+        </Text>
+        {/* Transparent text in order to have a smooth transition between loading and success */}
+        <Text variant="body" textAlign="center" mb={8} color="transparent">
+          {t("blePairingFlow.pairing.loading.subtitle", { productName })}
+        </Text>
+        <Animation
+          source={getDeviceAnimation({
+            device: deviceToPair,
+            key: "blePaired",
+            theme,
+          })}
+        />
+      </>
+    );
+  } else if (pairingError) {
+    content = (
+      <Flex>
+        <Flex flex={1} alignItems="center" justifyContent="center">
+          <Flex alignItems="center" justifyContent="center" mb={8}>
+            <CircledCrossSolidMedium color={colors.error.c80} size={56} />
+          </Flex>
+          <Text mb={4} textAlign="center" variant="h4" fontWeight="semiBold">
+            {t("blePairingFlow.pairing.error.title")}
+          </Text>
+          <Text variant="body" mb={8} color="neutral.c80" textAlign="center">
+            {t("blePairingFlow.pairing.error.subtitle", { productName })}
+          </Text>
+        </Flex>
+        <Button type="main" onPress={onRetry} mb={8}>
+          {t("blePairingFlow.pairing.error.retryCta")}
+        </Button>
+      </Flex>
+    );
+  } else {
+    content = (
+      <>
+        <Flex mb={10}>
+          <InfiniteLoader color="primary.c80" size={48} />
+        </Flex>
+        <Text variant="h4" fontWeight="semiBold" textAlign="center" mb={4}>
+          {t("blePairingFlow.pairing.loading.title", { deviceName })}
+        </Text>
+        <Text variant="body" textAlign="center" mb={8} color="neutral.c80">
+          {t("blePairingFlow.pairing.loading.subtitle", { productName })}
+        </Text>
+        <Animation
+          source={getDeviceAnimation({
+            device: deviceToPair,
+            key: "blePairing",
+            theme,
+          })}
+        />
+      </>
+    );
+  }
+
+  return (
+    <DeviceSetupView onClose={handleClose}>
+      <Flex flex={1} px={10} pt={36} alignItems="center">
+        {content}
+      </Flex>
+    </DeviceSetupView>
+  );
+};
+
+export default BleDevicePairing;
