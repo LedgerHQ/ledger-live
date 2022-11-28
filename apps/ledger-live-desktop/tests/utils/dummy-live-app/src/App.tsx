d8d4cb37f9b7b4f607f562709b889d5ff0d29bde
Kant <quentin.jaccarino@ledger.fr>
2022-11-28 10:11:52 +0100

    refactor: revert dummy-live-app changes already present in dummy-wallet-app

--- apps/ledger-live-desktop/tests/utils/dummy-live-app/src/App.tsx
+++ apps/ledger-live-desktop/tests/utils/dummy-live-app/src/App.tsx
@@ -1,40 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import HWTransport from "@ledgerhq/hw-transport";
-import Eth from "@ledgerhq/hw-app-eth";
-import hwGetDeviceInfo from "@ledgerhq/live-common/hw/getDeviceInfo";
-import {
-  Transaction,
-  TransactionSign,
-  WalletAPIClient,
-  WindowMessageTransport,
-} from "@ledgerhq/wallet-api-client";
-import BigNumber from "bignumber.js";
-import { Buffer } from "buffer";
+import LedgerLiveApi, { WindowMessageTransport } from "@ledgerhq/live-app-sdk";
 import logo from "./ledger-logo.png";
 import "./App.css";
-
-global.Buffer = Buffer;
 
 const prettyJSON = (payload: any) => JSON.stringify(payload, null, 2);
 
 const App = () => {
   // Define the Ledger Live API variable used to call api methods
-  const api = useRef<WalletAPIClient>();
-  const transport = useRef<HWTransport>();
+  const api = useRef<LedgerLiveApi>();
 
   const [output, setOutput] = useState<any>(null);
 
   // Instantiate the Ledger Live API on component mount
   useEffect(() => {
-    const windowTransport = new WindowMessageTransport();
-    const client = new WalletAPIClient(windowTransport);
-    windowTransport.connect();
-    api.current = client;
+    const llapi = new LedgerLiveApi(new WindowMessageTransport());
+    llapi.connect();
+    if (llapi) {
+      api.current = llapi;
+    }
 
     // Cleanup the Ledger Live API on component unmount
     return () => {
       api.current = undefined;
-      windowTransport.disconnect();
+      llapi.disconnect();
     };
   }, []);
 
@@ -42,48 +30,48 @@ const App = () => {
     if (!api.current) {
       return;
     }
-    try {
-      const action = await api.current.listAccounts({ currencyIds: ["bitcoin", "ethereum"] });
-      setOutput(action);
-    } catch (error) {
-      console.error(error);
-    }
+    const action = await api.current.listAccounts().catch(error => console.error({ error }));
+    setOutput(action);
   };
 
   const requestAccount = async () => {
     if (!api.current) {
       return;
     }
-    try {
-      const action = await api.current.requestAccount({ currencyIds: ["bitcoin", "ethereum"] });
-      setOutput(action);
-    } catch (error) {
-      console.error(error);
+    const action = await api.current.requestAccount().catch(error => console.error({ error }));
+    setOutput(action);
+  };
+
+  const verifyAddress = async () => {
+    if (!api.current) {
+      return;
     }
+    const action = await api.current.receive("mock:1:bitcoin:true_bitcoin_0:");
+    setOutput(action);
   };
 
   const signTransaction = async () => {
     if (!api.current) {
       return;
     }
-    const transaction: Transaction = {
-      amount: new BigNumber(1230),
+    const transaction: any = {
+      amount: 1230,
       recipient: "1Cz2ZXb6Y6AacXJTpo4RBjQMLEmscuxD8e",
       family: "bitcoin",
-      feePerByte: new BigNumber(1),
+      feePerByte: 1,
     };
 
-    const params: TransactionSign["params"]["options"] = { hwAppId: undefined };
+    const params: any = { useApp: null };
 
     const action = await api.current.signTransaction(
       "mock:1:bitcoin:true_bitcoin_0:",
       transaction,
       params,
     );
-    setOutput(action.toString());
+    setOutput(action);
   };
 
-  const signAndBroadcastTransaction = async () => {
+  const broadcastTransaction = async () => {
     // if (!api.current) {
     //   return;
     // }
@@ -95,53 +83,20 @@ const App = () => {
     if (!api.current) {
       return;
     }
-    try {
-      const action = await api.current.listCurrencies({ currencyIds: ["bitcoin", "ethereum"] });
-      setOutput(action);
-    } catch (error) {
-      console.error(error);
-    }
+    const action = await api.current.listCurrencies();
+    setOutput(action);
   };
 
-  const getTransport = (appName?: string) => async () => {
-    if (!api.current) {
-      return;
-    }
-    try {
-      transport.current = await api.current.deviceTransport({ appName });
-    } catch (error) {
-      console.error(error);
-    }
+  const swap = async () => {
+    // not implemented
   };
 
-  const getDeviceInfo = async () => {
-    if (!transport.current) {
-      return;
-    }
-    try {
-      const res = await hwGetDeviceInfo(transport.current);
-      setOutput(res);
-    } catch (error) {
-      console.error(error);
-    }
+  const fund = async () => {
+    // not implemented
   };
 
-  const ethGetAppConfiguration = async () => {
-    if (!transport.current) {
-      return;
-    }
-    try {
-      const eth = new Eth(transport.current);
-      const res = await eth.getAppConfiguration();
-      setOutput(res);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const closeTransport = () => {
-    transport.current?.close();
-    transport.current = undefined;
+  const sell = async () => {
+    // not implemented
   };
 
   return (
@@ -157,32 +112,26 @@ const App = () => {
           <button onClick={requestAccount} data-test-id="request-single-account-button">
             Request account
           </button>
+          <button onClick={verifyAddress} data-test-id="verify-address-button">
+            Verify Address
+          </button>
           <button onClick={signTransaction} data-test-id="sign-transaction-button">
             Sign Transaction
           </button>
-          <button
-            onClick={signAndBroadcastTransaction}
-            data-test-id="sign-broadcast-transaction-button"
-          >
+          <button onClick={broadcastTransaction} data-test-id="broadcast-transaction-button">
             Broadcast Transaction (Not yet implemented)
           </button>
           <button onClick={listCurrencies} data-test-id="list-currencies-button">
             List Currencies
           </button>
-          <button onClick={getTransport()} data-test-id="get-bolos-transport-button">
-            getBolosTransport
+          <button onClick={swap} data-test-id="swap-button">
+            Swap (Not yet implemented)
           </button>
-          <button onClick={getDeviceInfo} data-test-id="get-device-info-button">
-            getDeviceInfo
+          <button onClick={fund} data-test-id="fund-button">
+            Fund (Not yet implemented)
           </button>
-          <button onClick={getTransport("Ethereum")} data-test-id="get-eth-transport-button">
-            getEthTransport
-          </button>
-          <button onClick={ethGetAppConfiguration} data-test-id="eth-get-app-configuration-button">
-            ethGetAppConfiguration
-          </button>
-          <button onClick={closeTransport} data-test-id="get-eth-transport-button">
-            closeTransport
+          <button onClick={sell} data-test-id="sell-button">
+            Sell (Not yet implemented)
           </button>
         </div>
         <pre className="output-container">{output ? prettyJSON(output) : ""}</pre>
