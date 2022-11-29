2e2727c175456994df8573faa68c798fcebff63d
@greweb <renaudeau.gaetan@gmail.com>
2022-11-29 15:27:42 +0100

    Introduce REPL in monorepo (#1977)
    * Introduce REPL in monorepo
    
    * chore(repl): override webpack 4 to match cryptoassets lib-es export
    
    * chore(repl): fix linter errors
    
    Co-authored-by: elbywan <julien.el-baz@ledger.fr>

---
+++ tools/common-tools/src/demos/repl/components/fields/DerivationModeField.js
@@ -0,0 +1,37 @@
+// @flow
+import React from "react";
+import Select from "react-select";
+import { getAllDerivationModes } from "@ledgerhq/live-common/lib/derivation";
+import type { DerivationMode } from "@ledgerhq/live-common/lib/derivation";
+
+export type DataTypeDerivationMode = {
+  type: "derivationMode",
+  default?: DerivationMode,
+};
+
+type Props = {
+  value: ?DerivationMode,
+  onChange: (?DerivationMode) => void,
+};
+
+const DerivationModeField = ({ value, onChange }: Props) => {
+  const derivationModes: DerivationMode[] = getAllDerivationModes();
+  const options = derivationModes.map((value) => ({
+    label: value || "(default)",
+    value,
+  }));
+  const option = options.find((o) => o.value === value);
+  return (
+    <Select
+      value={option}
+      options={options}
+      onChange={(o) => {
+        console.log(o);
+        onChange((o && o.value) || "");
+      }}
+      placeholder="Select a derivationMode"
+    />
+  );
+};
+
+export default DerivationModeField;
