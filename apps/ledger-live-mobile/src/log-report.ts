94d9942c49ea1fae2e871ab24f8dffe50011f008
Juan Cortés Ross <juan@bohem.io>
2022-12-02 14:36:18 +0100

    Get the max export log  env at runtime

--- apps/ledger-live-mobile/src/log-report.ts
+++ apps/ledger-live-mobile/src/log-report.ts
@@ -2,16 +2,16 @@ import { listen, Log } from "@ledgerhq/logs";
 import { getEnv } from "@ledgerhq/live-common/env";
 
 const logs: Log[] = [];
-const logLimit = getEnv("EXPORT_MAX_LOGS");
-const excludedLogTypes = getEnv("EXPORT_EXCLUDED_LOG_TYPES").split(",");
-
 export default {
   logReportInit: () => {
     listen(log => {
+      const logLimit = getEnv("EXPORT_MAX_LOGS");
+      const excludedLogTypes = getEnv("EXPORT_EXCLUDED_LOG_TYPES").split(",");
+
       if (!excludedLogTypes.includes(log.type)) {
         logs.unshift(log);
 
-        if (logs.length > logLimit) {
+        while (logs.length > logLimit) {
           logs.pop();
         }
       }
