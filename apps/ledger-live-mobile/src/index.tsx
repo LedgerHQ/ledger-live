2aa58247746ff2944f49f81205cc618b578dc904
Jules Grenier <jules.grenier-ext@ledger.fr>
2022-12-02 16:17:58 +0100

    LIVE-4767 - LLM - Protect - remove ProtectProvider

--- apps/ledger-live-mobile/src/index.tsx
+++ apps/ledger-live-mobile/src/index.tsx
@@ -43,7 +43,6 @@ import {
   useRemoteLiveAppContext,
 } from "@ledgerhq/live-common/platform/providers/RemoteLiveAppProvider/index";
 import { LocalLiveAppProvider } from "@ledgerhq/live-common/platform/providers/LocalLiveAppProvider/index";
-import { ProtectProvider } from "@ledgerhq/live-common/platform/providers/ProtectProvider/index";
 
 import { isEqual } from "lodash";
 import { postOnboardingSelector } from "@ledgerhq/live-common/postOnboarding/reducer";
@@ -727,13 +726,11 @@ export default class Root extends Component<{
                                                         <SnackbarContainer />
                                                         <NftMetadataProvider>
                                                           <MarketDataProvider>
-                                                            <ProtectProvider>
-                                                              <App
-                                                                importDataString={
-                                                                  importDataString
-                                                                }
-                                                              />
-                                                            </ProtectProvider>
+                                                            <App
+                                                              importDataString={
+                                                                importDataString
+                                                              }
+                                                            />
                                                           </MarketDataProvider>
                                                         </NftMetadataProvider>
                                                       </NotificationsProvider>
