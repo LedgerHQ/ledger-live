e5398e69356a72e0ef9255b42c6a73473708eb19
Emmanuel <emmanuelm41@gmail.com>
2022-12-02 13:44:07 -0300

    display amount and fees with both units

--- apps/ledger-live-desktop/src/renderer/families/stacks/TransactionConfirmFields.jsx
+++ apps/ledger-live-desktop/src/renderer/families/stacks/TransactionConfirmFields.jsx
@@ -5,7 +5,10 @@ import React from "react";
 import TransactionConfirmField from "~/renderer/components/TransactionConfirm/TransactionConfirmField";
 import Text from "~/renderer/components/Text";
 import type { DeviceTransactionField } from "@ledgerhq/live-common/transaction/index";
+import { getAccountCurrency, getAccountUnit } from "@ledgerhq/live-common/account/index";
 import type { Transaction } from "@ledgerhq/live-common/generated/types";
+import Box from "~/renderer/components/Box";
+import FormattedVal from "~/renderer/components/FormattedVal";
 
 const addressStyle = {
   wordBreak: "break-all",
@@ -13,7 +16,48 @@ const addressStyle = {
   maxWidth: "70%",
 };
 
-const StacksField = ({
+const StacksExtendedAmountField = ({
+  account,
+  status: { amount },
+  transaction,
+  field,
+}: FieldComponentProps) => {
+  invariant(transaction.family === "stacks", "stacks transaction");
+  const currency = getAccountCurrency(account);
+  const unit = getAccountUnit(account);
+  const specifiedAmount = field.value != null ? field.value : null;
+  return (
+    <TransactionConfirmField label={field.label}>
+      <Box textAlign="right">
+        <FormattedVal
+          color={"palette.text.shade80"}
+          disableRounding={true}
+          unit={unit}
+          val={specifiedAmount ?? amount}
+          fontSize={3}
+          inline
+          showCode
+        />
+        <Box textAlign="right">
+          <FormattedVal
+            color={"palette.text.shade40"}
+            disableRounding={true}
+            unit={currency.units[1]}
+            subMagnitude={1}
+            prefix={"("}
+            val={specifiedAmount ?? amount}
+            suffix={")"}
+            fontSize={3}
+            inline
+            showCode
+          />
+        </Box>
+      </Box>
+    </TransactionConfirmField>
+  );
+};
+
+const StacksMemoField = ({
   transaction,
   field,
 }: {
@@ -32,7 +76,8 @@ const StacksField = ({
 };
 
 const fieldComponents = {
-  "stacks.memo": StacksField,
+  "stacks.extendedAmount": StacksExtendedAmountField,
+  "stacks.memo": StacksMemoField,
 };
 
 export default {
