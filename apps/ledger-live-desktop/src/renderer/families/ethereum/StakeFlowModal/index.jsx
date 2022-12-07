6bebd1f6dd227107f773a35ed01862ff798cc2b5
sarneijim <sarneijim@gmail.com>
2022-12-07 11:53:26 +0000

    chore: convert func component

--- apps/ledger-live-desktop/src/renderer/families/ethereum/StakeFlowModal/index.jsx
+++ apps/ledger-live-desktop/src/renderer/families/ethereum/StakeFlowModal/index.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { PureComponent } from "react";
+import React from "react";
 import { Trans } from "react-i18next";
 import Modal, { ModalBody } from "~/renderer/components/Modal";
 import Body from "./Body";
@@ -9,28 +9,28 @@ import { Flex } from "@ledgerhq/react-ui";
 import type { Account } from "@ledgerhq/types-live";
 
 type Props = { name: string, account: Account, checkbox?: boolean, source?: string };
-export default class DelegationModal extends PureComponent<Props> {
-  render() {
-    const { name, account, checkbox, source } = this.props;
-    return (
-      <Modal
-        name={name}
-        centered
-        title={<Trans i18nKey="ethereum.stake.title" />}
-        width={500}
-        render={({ onClose }) => (
-          <ModalBody
-            title={<Trans i18nKey="ethereum.stake.title" />}
-            onClose={onClose}
-            render={() => (
-              <Flex justifyContent={"center"}>
-                <TrackPage category="ETH Stake Modal" name="Main Modal" />
-                <Body onClose={onClose} account={account} checkbox={checkbox} source={source} />
-              </Flex>
-            )}
-          />
-        )}
-      />
-    );
-  }
-}
+
+const DelegationModal = ({ name, account, checkbox, source }: Props) => {
+  return (
+    <Modal
+      name={name}
+      centered
+      title={<Trans i18nKey="ethereum.stake.title" />}
+      width={500}
+      render={({ onClose }) => (
+        <ModalBody
+          title={<Trans i18nKey="ethereum.stake.title" />}
+          onClose={onClose}
+          render={() => (
+            <Flex justifyContent={"center"}>
+              <TrackPage category="ETH Stake Modal" name="Main Modal" />
+              <Body onClose={onClose} account={account} checkbox={checkbox} source={source} />
+            </Flex>
+          )}
+        />
+      )}
+    />
+  );
+};
+
+export default DelegationModal;
