e104abefa6e3f7ade067524799f42829db7a56d8
lvndry <lvndry@proton.me>
2022-12-08 18:18:40 +0100

    type 2 tests

--- libs/ledger-live-common/src/families/ethereum/__tests__/signOperation.test.ts
+++ libs/ledger-live-common/src/families/ethereum/__tests__/signOperation.test.ts
@@ -1,14 +1,15 @@
 import { ethers } from "ethers";
 import { BigNumber } from "bignumber.js";
-import { cryptocurrenciesById } from "@ledgerhq/cryptoassets/currencies";
-import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+import {
+  cryptocurrenciesById,
+  getCryptoCurrencyById,
+} from "@ledgerhq/cryptoassets/currencies";
 
 import createTransaction from "../createTransaction";
 import { fromAccountRaw } from "../../../account";
 import { ethereum1 } from "../datasets/ethereum1";
 import { signOperation } from "../signOperation";
 import { setEnv } from "../../../env";
-import { EIP1559ShouldBeUsed } from "../transaction";
 
 const signTransaction = jest.fn(() => {
   return Promise.resolve({
@@ -86,71 +87,118 @@ describe("signOperation", () => {
     });
 
     describe("Transaction type 2", () => {
-      const unsupportedCurrencies: CryptoCurrency["id"][] = [
-        "bsc",
-        "ethereum_classic",
-        "ethereum_ropsten",
-      ];
-
-      beforeAll(() => {
-        setEnv(
-          "EIP1559_ENABLED_CURRENCIES",
-          currencies
-            .map((currency) => currency.id)
-            .filter((currencyId) => !unsupportedCurrencies.includes(currencyId))
-            .join(",")
-        );
-      });
-
-      currencies.forEach((currency) => {
-        beforeEach(() => {
-          signTransaction.mockImplementationOnce(() => {
-            return Promise.resolve({
-              r: "006c000371dc04c5752287a9901b1fac4b069eb1410173db39c407ae725e4a6e",
-              s: "4f445c94cc869f01e194478a3b876052716ae7676247664acec371b6e6ad16e4",
-              v: "01",
-            });
+      it("Should use EIP155 for ethereum", async () => {
+        setEnv("EIP1559_ENABLED_CURRENCIES", "ethereum");
+        const currency = getCryptoCurrencyById("ethereum");
+        signTransaction.mockImplementationOnce(() => {
+          return Promise.resolve({
+            r: "006c000371dc04c5752287a9901b1fac4b069eb1410173db39c407ae725e4a6e",
+            s: "4f445c94cc869f01e194478a3b876052716ae7676247664acec371b6e6ad16e4",
+            v: "01",
           });
         });
 
-        it(`should use EIP155 for ${currency.id} transaction`, async () => {
-          await signOperation({
-            account: {
-              ...dummyAccount,
-              currency,
-            },
-            deviceId: "",
-            transaction: {
-              ...createTransaction(),
-              recipient: "0xc3f95102D5c8F2c83e49Ce3Acfb905eDfb7f37dE",
-              maxFeePerGas: new BigNumber(0),
-              maxPriorityFeePerGas: new BigNumber(0),
-              nonce: 0,
-            },
-          })
-            .toPromise()
-            .catch((err) => {
-              if (err.message === "EIP-1559 not enabled on Common") {
-                console.log(`EIP 1559 not supported for ${currency.id}`);
-                return;
-              }
+        await signOperation({
+          account: {
+            ...dummyAccount,
+            currency,
+          },
+          deviceId: "",
+          transaction: {
+            ...createTransaction(),
+            recipient: "0xc3f95102D5c8F2c83e49Ce3Acfb905eDfb7f37dE",
+            maxFeePerGas: new BigNumber(0),
+            maxPriorityFeePerGas: new BigNumber(0),
+            nonce: 0,
+          },
+        }).toPromise();
 
-              return; // TODO: should be throw err
-            });
+        const txHashProvidedToAppBindings = (
+          signTransaction.mock.calls[
+            signTransaction.mock.calls.length - 1
+          ] as unknown[]
+        )[1];
 
-          const txHashProvidedToAppBindings = (
-            signTransaction.mock.calls[
-              signTransaction.mock.calls.length - 1
-            ] as unknown[]
-          )[1];
+        expect(
+          ethers.utils.parseTransaction(`0x${txHashProvidedToAppBindings}`)
+            .chainId
+        ).toBe(currency.ethereumLikeInfo?.chainId);
+      });
 
-          if (EIP1559ShouldBeUsed(currency)) {
-            expect(
-              ethers.utils.parseTransaction(`0x${txHashProvidedToAppBindings}`)
-                .chainId
-            ).toBe(currency.ethereumLikeInfo?.chainId);
-          }
+      it("Should use EIP155 for ethereum_goerli", async () => {
+        setEnv("EIP1559_ENABLED_CURRENCIES", "ethereum_goerli");
+        const currency = getCryptoCurrencyById("ethereum_goerli");
+        signTransaction.mockImplementationOnce(() => {
+          return Promise.resolve({
+            r: "006c000371dc04c5752287a9901b1fac4b069eb1410173db39c407ae725e4a6e",
+            s: "4f445c94cc869f01e194478a3b876052716ae7676247664acec371b6e6ad16e4",
+            v: "01",
+          });
         });
+
+        await signOperation({
+          account: {
+            ...dummyAccount,
+            currency,
+          },
+          deviceId: "",
+          transaction: {
+            ...createTransaction(),
+            recipient: "0xc3f95102D5c8F2c83e49Ce3Acfb905eDfb7f37dE",
+            maxFeePerGas: new BigNumber(0),
+            maxPriorityFeePerGas: new BigNumber(0),
+            nonce: 0,
+          },
+        }).toPromise();
+
+        const txHashProvidedToAppBindings = (
+          signTransaction.mock.calls[
+            signTransaction.mock.calls.length - 1
+          ] as unknown[]
+        )[1];
+
+        expect(
+          ethers.utils.parseTransaction(`0x${txHashProvidedToAppBindings}`)
+            .chainId
+        ).toBe(currency.ethereumLikeInfo?.chainId);
+      });
+
+      it("Should use EIP155 for polygon", async () => {
+        setEnv("EIP1559_ENABLED_CURRENCIES", "polygon");
+        const currency = getCryptoCurrencyById("polygon");
+        signTransaction.mockImplementationOnce(() => {
+          return Promise.resolve({
+            r: "006c000371dc04c5752287a9901b1fac4b069eb1410173db39c407ae725e4a6e",
+            s: "4f445c94cc869f01e194478a3b876052716ae7676247664acec371b6e6ad16e4",
+            v: "01",
+          });
+        });
+
+        await signOperation({
+          account: {
+            ...dummyAccount,
+            currency,
+          },
+          deviceId: "",
+          transaction: {
+            ...createTransaction(),
+            recipient: "0xc3f95102D5c8F2c83e49Ce3Acfb905eDfb7f37dE",
+            maxFeePerGas: new BigNumber(0),
+            maxPriorityFeePerGas: new BigNumber(0),
+            nonce: 0,
+          },
+        }).toPromise();
+
+        const txHashProvidedToAppBindings = (
+          signTransaction.mock.calls[
+            signTransaction.mock.calls.length - 1
+          ] as unknown[]
+        )[1];
+
+        expect(
+          ethers.utils.parseTransaction(`0x${txHashProvidedToAppBindings}`)
+            .chainId
+        ).toBe(currency.ethereumLikeInfo?.chainId);
       });
     });
   });
