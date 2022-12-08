cda0f2cb5be2c57ba8ba569acb6a8469c23eeb2f
BlockchainSimplified.com <mail@blockchainsimplified.com>
2022-12-09 01:21:45 +0530

    Added developed by banner.

---
+++ apps/ledger-live-mobile/src/families/nervos/AccountSubHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader";
+
+function NervosAccountSubHeader() {
+  return <AccountSubHeader family="Nervos" team="Blockchain Simplified" />;
+}
+
+export default NervosAccountSubHeader;
