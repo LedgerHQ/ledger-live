9a34a9ed0e12a675f888f1b45fdba569bcce076b
Martin Cayuelas <martin.cayuelas@ledger.fr>
2022-12-08 17:05:23 +0100

    💄 V0 - Learn CC

---
+++ libs/ui/packages/native/src/components/Cards/CardD/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { TouchableOpacityProps, TouchableOpacity, Image } from "react-native";
+import styled from "styled-components/native";
+
+import Text from "../../Text";
+import Flex from "../../Layout/Flex";
+
+export type CardProps = TouchableOpacityProps & {
+  tag?: string;
+  title?: string;
+  imageUrl?: string;
+  onClickCard?: () => void;
+};
+
+const Base = styled(TouchableOpacity)``;
+const ImageContent = styled(Image)`
+  border-radius: 8px;
+`;
+
+const CardContainer = (props: CardProps): React.ReactElement => {
+  const { imageUrl, title, tag } = props;
+
+  return (
+    <Flex flexDirection="row">
+      <Flex justifyContent="space-between" alignItems="flex-start" p="12px" flex={1}>
+        <Text
+          variant="body"
+          fontWeight="medium"
+          color="neutral.c100"
+          numberOfLines={2}
+          maxWidth={"95%"}
+        >
+          {title}
+        </Text>
+        <Text
+          variant="paragraph"
+          fontWeight="medium"
+          color="neutral.c70"
+          numberOfLines={1}
+          maxWidth={"95%"}
+          mt={4}
+        >
+          {tag}
+        </Text>
+      </Flex>
+
+      <Flex width={100} height={75}>
+        <ImageContent
+          source={{
+            uri: imageUrl,
+          }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+const CardD = (props: CardProps): React.ReactElement => {
+  return (
+    <Base {...props} activeOpacity={0.5} onPress={props.onClickCard}>
+      <CardContainer {...props} />
+    </Base>
+  );
+};
+
+export default CardD;
