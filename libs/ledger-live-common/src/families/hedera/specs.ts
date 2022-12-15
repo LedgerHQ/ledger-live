80572fb83d98b26fbf33be1f29f57ba1fe23fa2b
lvndry <lvndry@proton.me>
2022-12-15 14:19:41 +0100

    original mutations order

--- libs/ledger-live-common/src/families/hedera/specs.ts
+++ libs/ledger-live-common/src/families/hedera/specs.ts
@@ -84,6 +84,32 @@ const hedera: AppSpec<Transaction> = {
       },
     },
     {
+      name: "Send max",
+      maxRun: 1,
+      testDestination: genericTestDestination,
+      transaction: ({
+        account,
+        siblings,
+        bridge,
+      }: TransactionArg<Transaction>): TransactionRes<Transaction> => {
+        const sibling = pickSiblings(siblings, 4);
+        const recipient = sibling.freshAddress;
+        const transaction = bridge.createTransaction(account);
+
+        return {
+          transaction,
+          updates: [{ recipient }, { useAllAmount: true }],
+        };
+      },
+      test: ({ account, accountBeforeTransaction, operation }) => {
+        botTest("Account balance should have decreased", () => {
+          expect(account.balance.toNumber()).toBeLessThanOrEqual(
+            accountBeforeTransaction.balance.minus(operation.value).toNumber()
+          );
+        });
+      },
+    },
+    {
       name: "Memo",
       maxRun: 2,
       transaction: ({
@@ -112,32 +138,6 @@ const hedera: AppSpec<Transaction> = {
         );
       },
       testDestination: genericTestDestination,
-    },
-    {
-      name: "Send max",
-      maxRun: 1,
-      testDestination: genericTestDestination,
-      transaction: ({
-        account,
-        siblings,
-        bridge,
-      }: TransactionArg<Transaction>): TransactionRes<Transaction> => {
-        const sibling = pickSiblings(siblings, 4);
-        const recipient = sibling.freshAddress;
-        const transaction = bridge.createTransaction(account);
-
-        return {
-          transaction,
-          updates: [{ recipient }, { useAllAmount: true }],
-        };
-      },
-      test: ({ account, accountBeforeTransaction, operation }) => {
-        botTest("Account balance should have decreased", () => {
-          expect(account.balance.toNumber()).toBeLessThanOrEqual(
-            accountBeforeTransaction.balance.minus(operation.value).toNumber()
-          );
-        });
-      },
     },
   ],
 };
