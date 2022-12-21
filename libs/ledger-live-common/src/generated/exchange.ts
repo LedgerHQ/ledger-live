d3666170d46d1c0786e47c5d5ac78e0d3bd77d67
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2022-12-21 15:50:30 +0100

    LIVE-3475: regenerate exchange.ts

--- libs/ledger-live-common/src/generated/exchange.ts
+++ libs/ledger-live-common/src/generated/exchange.ts
@@ -1,15 +1,15 @@
 import bitcoin from "../families/bitcoin/exchange";
 import ethereum from "../families/ethereum/exchange";
 import ripple from "../families/ripple/exchange";
+import solana from "../families/solana/exchange";
 import stellar from "../families/stellar/exchange";
 import tezos from "../families/tezos/exchange";
-import solana from "../families/solana/exchange";
 
 export default {
   bitcoin,
   ethereum,
   ripple,
+  solana,
   stellar,
   tezos,
-  solana
 };
