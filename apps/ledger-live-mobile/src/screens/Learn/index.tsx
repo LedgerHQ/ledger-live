ceded28796afbef975a8d9e5b354511c0fad6dae
Come Grellard <come.grellard@ledger.fr>
2022-12-21 11:25:08 +0100

    LearWebView header added to be able to go back to the learn screen

--- apps/ledger-live-mobile/src/screens/Learn/index.tsx
+++ apps/ledger-live-mobile/src/screens/Learn/index.tsx
@@ -2,12 +2,14 @@ import React, { memo, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import styled, { useTheme } from "styled-components/native";
 
-import { Flex, InfiniteLoader } from "@ledgerhq/native-ui";
+import { Flex, InfiniteLoader, Text } from "@ledgerhq/native-ui";
 
 import WebViewScreen from "../../components/WebViewScreen";
 import { BaseNavigatorStackParamList } from "../../components/RootNavigator/types/BaseNavigator";
 import { StackNavigatorProps } from "../../components/RootNavigator/types/helpers";
+import HeaderRightClose from "../../components/HeaderRightClose";
 import { ScreenName } from "../../const/navigation";
+import { useNavigation } from "@react-navigation/native";
 
 export type NavigationProps = StackNavigatorProps<
   BaseNavigatorStackParamList,
@@ -17,6 +19,7 @@ export type NavigationProps = StackNavigatorProps<
 function LearnWebView({ route }: NavigationProps) {
   const { uri: uriFromRoute } = route.params;
   const { i18n, t } = useTranslation();
+  const navigation = useNavigation();
   const {
     colors: { type: themeType },
   } = useTheme();
@@ -37,12 +40,36 @@ function LearnWebView({ route }: NavigationProps) {
     [],
   );
 
+  const goBack = useCallback(() => {
+    navigation.navigate(ScreenName.Learn);
+  }, [navigation]);
+
   return (
     <WebViewScreen
       screenName={t("learn.pageTitle")}
       uri={uri}
       trackEventName="Page Learn"
       renderLoading={renderLoading}
+      renderHeader={() => (
+        <Flex
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          width="100%"
+          height={48}
+          zIndex={1}
+        >
+          <Flex width="20%" />
+          <Flex flex={1}>
+            <Text textAlign="center" variant="h5" fontWeight="semiBold">
+              {t("help.ledgerAcademy.title")}
+            </Text>
+          </Flex>
+          <Flex width="20%" alignItems="flex-end">
+            <HeaderRightClose onClose={goBack} />
+          </Flex>
+        </Flex>
+      )}
     />
   );
 }
