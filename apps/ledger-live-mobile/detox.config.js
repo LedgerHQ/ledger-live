9c21919788d091833e2507b001500f3028769313
Gregor Gilchrist <87659842+ggilchrist-ledger@users.noreply.github.com>
2022-12-27 18:06:38 +0100

    Set Detox debug level to Debug on CI runs (#2204)
    * Setting to Detox debug level to verbose so we have more info for debugging issues on the CI runners
    
    * its debug, not verbose...

--- apps/ledger-live-mobile/detox.config.js
+++ apps/ledger-live-mobile/detox.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
   },
   logger: {
-    level: process.env.DEBUG_DETOX ? "trace" : "info",
+    level: process.env.DEBUG_DETOX ? "trace" : "debug",
   },
   behavior: {
     // NOTE: https://github.com/wix/Detox/blob/master/docs/APIRef.Configuration.md#behavior-configuration
