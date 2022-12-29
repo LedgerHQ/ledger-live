c4ac96786c7ec968e92530c6852383d4bfff475a
Valentin D. Pinkman <valentin.d.pinkman@hey.com>
2022-12-29 14:36:01 +0100

    regen pods requested action

--- tools/github-bot/src/commands/regen-pods.ts
+++ tools/github-bot/src/commands/regen-pods.ts
@@ -1,5 +1,8 @@
-import { Probot } from "probot";
+import { Context, Probot } from "probot";
 import { commands } from "./tools";
+
+// Keep this in sync with the workflow file
+const ACTION_ID = "regen_pods";
 
 /**
  * Slash commands `/regen-pods`
@@ -8,6 +11,26 @@ import { commands } from "./tools";
  * @param app The Probot application.
  */
 export function regenPods(app: Probot) {
+  async function triggerWorkflow({
+    context,
+    number,
+    login,
+  }: {
+    context: Context;
+    number: string;
+    login: string;
+  }) {
+    await context.octokit.actions.createWorkflowDispatch({
+      ...context.repo(),
+      workflow_id: "regen-pods.yml",
+      ref: "develop",
+      inputs: {
+        number,
+        login,
+      },
+    });
+  }
+
   commands(app, "regen-pods", async (context, data) => {
     const { octokit, payload } = context;
 
@@ -19,14 +42,27 @@ export function regenPods(app: Probot) {
       content: "rocket",
     });
 
-    await octokit.actions.createWorkflowDispatch({
-      ...context.repo(),
-      workflow_id: "regen-pods.yml",
-      ref: "develop",
-      inputs: {
-        number: `${data.number}`,
-        login: `${payload.comment.user.login}`,
-      },
+    await triggerWorkflow({
+      context,
+      number: `${data.number}`,
+      login: `${payload.comment.user.login}`,
+    });
+  });
+
+  app.on("check_run.requested_action", async (context) => {
+    const { payload } = context;
+
+    if (payload.requested_action.identifier !== ACTION_ID) return;
+
+    const number = payload.check_run.pull_requests[0]?.number;
+    const login = payload.sender.login;
+
+    if (!number) return;
+
+    await triggerWorkflow({
+      context,
+      number: `${number}`,
+      login,
     });
   });
 }
