d8508302738a7e632cafd1b3b53c03fee7bb7d2c
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-05 17:04:32 +0100

    feat: proper first definition of commands, tasks and actions
    Co-authored-by: Gabriel Restori Soares <grsoares21@users.noreply.github.com>
    Co-authored-by: Juan Cortés Ross <juan-cortes@users.noreply.github.com>

--- libs/ledger-live-common/src/deviceSDK/actions/getDeviceInfo.ts
+++ libs/ledger-live-common/src/deviceSDK/actions/getDeviceInfo.ts
@@ -1,76 +1,30 @@
-//import { LockedDeviceError } from "@ledgerhq/errors";
 import { DeviceId, DeviceInfo } from "@ledgerhq/types-live";
 import { Observable } from "rxjs";
 import { scan, tap } from "rxjs/operators";
 import {
+  SharedActionState,
+  getSharedInitialState,
+  sharedReducer,
+} from "./core";
+import {
   getDeviceInfoTask,
+  GetDeviceInfoTaskError,
   GetDeviceInfoTaskEvent,
 } from "../tasks/getDeviceInfo";
 
 export type GetDeviceInfoActionArgs = { deviceId: DeviceId };
 
-// TODO: Bad idea ?
-// Should it be: SpeficActionErrors | CommonErrors
-export type DeviceActionError = Error;
+// Union of all the tasks specific errors
+export type GetDeviceInfoActionErrorType = GetDeviceInfoTaskError;
 
-// TODO: Should we create a "general state" (scared we would end up with the same big existing state by doing this)
-// for all the lockedDevice etc. to be consistent ?
-// What would be in it ?
-// lockedDevice (and unresponsive would be handle with lockedDevice)
-// error ? meaning an action can throw an error, but it's always handled in the state ? could be interesting
-// If only those, it's ok ? But what happens if we device to add a new prop to this "general state" ? We will need to add it everywhere ?
-type State = {
+export type GetDeviceInfoActionState = {
   deviceInfo: DeviceInfo | null;
-};
+} & SharedActionState<GetDeviceInfoActionErrorType>;
 
-// TODO: put it somewhere else: in the type lib
-export type GeneralState = {
-  lockedDevice: boolean;
-  error: DeviceActionError | null;
-};
-
-// Maybe here, or in the type lib
-export type GetDeviceInfoActionState = State & GeneralState;
-
-// export function getDeviceInfoAction({
-//   deviceId,
-// }: GetDeviceInfoActionArgs): Observable<GetDeviceInfoActionState> {
-//   // or getDeviceInfoTask({}).pipe(scan(reducer)).subscribe(o) ? See below
-//   return new Observable((o) => {
-//     getDeviceInfoTask({ deviceId }).subscribe({
-//       next: (event) => {
-//         o.next({ deviceInfo: event, lockedDevice: false, error: null });
-//       },
-//       error: (error) => {
-//         // TODO: handle device locked for ex ?
-//         if (error instanceof LockedDeviceError) {
-//           o.next({ deviceInfo: null, lockedDevice: true, error: null });
-//           return;
-//         }
-
-//         o.next({ deviceInfo: null, lockedDevice: false, error });
-//       },
-//       complete: () => o.complete(),
-//     });
-//   });
-// }
-
-const generalInitialState: GeneralState = { lockedDevice: false, error: null };
 export const initialState: GetDeviceInfoActionState = {
   deviceInfo: null,
-  ...generalInitialState,
+  ...getSharedInitialState<GetDeviceInfoActionErrorType>(),
 };
-
-// const generalReducer = (currentState: GeneralState, event: GeneralTaskEvent) => {
-//   switch (event.type) {
-//     case "error":
-//       if (event.error instanceof LockedDeviceError) {
-//         return { ...currentState, lockedDevice: true };
-//       }
-//     default:
-//       return currentState;
-//   }
-// };
 
 export function getDeviceInfoAction({
   deviceId,
@@ -85,12 +39,21 @@ export function getDeviceInfoAction({
       ) => {
         switch (event.type) {
           case "taskError":
-            return { ...initialState, error: event.error };
+            return { ...initialState, error: { type: event.error } };
           case "data":
-            return { ...currentState, deviceInfo: event.deviceInfo };
-          default:
-            // TODO: define a general reducer
-            return { ...currentState, error: event.error }; // ...generalReducer(currentState, event) };
+            return {
+              ...currentState,
+              error: null,
+              deviceInfo: event.deviceInfo,
+            };
+          case "error":
+            return {
+              ...currentState,
+              ...sharedReducer<GetDeviceInfoActionErrorType>({
+                currentState,
+                event,
+              }),
+            };
         }
       },
       initialState
--- libs/ledger-live-common/src/deviceSDK/tasks/core.ts
+++ libs/ledger-live-common/src/deviceSDK/tasks/core.ts
@@ -3,31 +3,37 @@ import { Observable, of } from "rxjs";
 import { catchError, retryWhen, timeout } from "rxjs/operators";
 import { retryWhileErrors } from "../../hw/deviceAccess";
 
-export type GeneralTaskEvent = { type: "error"; error: Error };
+export type SharedTaskEvent = { type: "error"; error: Error };
 
-const NO_RESPONSE_TIMEOUT_MS = 10000;
+const NO_RESPONSE_TIMEOUT_MS = 30000;
 
 export function wrappedTask<TaskArgumentsType, TaskEventsType>(
   task: (
     args: TaskArgumentsType
-  ) => Observable<TaskEventsType | GeneralTaskEvent>
+  ) => Observable<TaskEventsType | SharedTaskEvent>
 ) {
   return (
     args: TaskArgumentsType
-  ): Observable<TaskEventsType | GeneralTaskEvent> =>
+  ): Observable<TaskEventsType | SharedTaskEvent> =>
     task(args).pipe(
       timeout(NO_RESPONSE_TIMEOUT_MS),
       retryWhen(
         retryWhileErrors((error) => {
+          // TODO: emits here an event to the action of type error, error: LockedDeviceError ? Yes
           if (error instanceof LockedDeviceError) {
             return true;
           }
+
+          // TODO: other errors on which to retry:
+          // - CantOpenDevice: seems to be when device is USB plugged, powered on, but not yet unlocked for the 1st time
+          //   since being powered on.
 
           return false;
         })
       ),
       catchError((error: Error) => {
-        return of<GeneralTaskEvent>({ type: "error", error });
+        // Emits the error to the action, without throwing
+        return of<SharedTaskEvent>({ type: "error", error });
       })
     );
 }
