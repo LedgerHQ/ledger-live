e80d6b322a57c1de93d163a6d11a9e2897856c4d
Troels F. Rønnow <troels@wonop.com>
2023-01-05 17:17:20 +0100

    Fixing linting errors

--- libs/ledger-live-common/src/families/zilliqa/js-getFeesForTransaction.ts
+++ libs/ledger-live-common/src/families/zilliqa/js-getFeesForTransaction.ts
@@ -1,16 +1,8 @@
 import { ZILLIQA_TX_GAS_LIMIT, getMinimumGasPrice } from "./api";
-import type { ZilliqaAccount, Transaction } from "./types";
 import { BigNumber } from "bignumber.js";
 import { BN } from "@zilliqa-js/util";
 
-export const getEstimatedFees = async ({}: /*
-  a,
-  t,
-  */
-{
-  a: ZilliqaAccount;
-  t: Transaction;
-}): Promise<BigNumber> => {
+export const getEstimatedFees = async (): Promise<BigNumber> => {
   const gasPrice = await getMinimumGasPrice();
   const gasLimit = new BN(ZILLIQA_TX_GAS_LIMIT);
   return new BigNumber(gasPrice.mul(gasLimit).toString());
