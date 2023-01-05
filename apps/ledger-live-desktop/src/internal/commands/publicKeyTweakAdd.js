9bfedda6880ae974ee6a37ede8f5fcc087a3904f
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2023-01-05 18:36:05 +0100

    introduce commands/publicKeyTweakAdd as workaround for perf
    when experimental is on, bitcoin family will run on renderer and we want to run the secp256k1 impl on another thread still
    because it was hurting performance badly otherwise
    
    in future, we will need to make this run in a webworker when we actually sunset internal process

---
+++ apps/ledger-live-desktop/src/internal/commands/publicKeyTweakAdd.js
@@ -0,0 +1,15 @@
+// @flow
+import { from } from "rxjs";
+import { getSecp256k1Instance } from "@ledgerhq/live-common/families/bitcoin/wallet-btc/crypto/secp256k1";
+
+const cmd = ({ publicKey, tweak }) =>
+  from(
+    getSecp256k1Instance()
+      .publicKeyTweakAdd(
+        new Uint8Array(Buffer.from(publicKey, "hex").toJSON().data),
+        new Uint8Array(Buffer.from(tweak, "hex").toJSON().data),
+      )
+      .then(r => Buffer.from(r).toString("hex")),
+  );
+
+export default cmd;
