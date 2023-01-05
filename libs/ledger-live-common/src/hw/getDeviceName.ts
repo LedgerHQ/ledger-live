b1d949b06b2af2295892f4ac13bb320a3e19df5b
Gabriel Soares <gabriel.soares@ledger.fr>
2023-01-05 15:58:25 +0100

    LIVE-5098 Stop throwing when device is not on onboarded

--- libs/ledger-live-common/src/hw/getDeviceName.ts
+++ libs/ledger-live-common/src/hw/getDeviceName.ts
@@ -1,5 +1,22 @@
-import Transport from "@ledgerhq/hw-transport";
+import Transport, {
+  StatusCodes,
+  TransportStatusError,
+} from "@ledgerhq/hw-transport";
+
 export default async (transport: Transport): Promise<string> => {
-  const res = await transport.send(0xe0, 0xd2, 0x00, 0x00);
-  return res.slice(0, res.length - 2).toString("utf-8");
+  const res = await transport.send(0xe0, 0xd2, 0x00, 0x00, Buffer.from([]), [
+    StatusCodes.OK,
+    StatusCodes.DEVICE_NOT_ONBOARDED,
+  ]);
+
+  const status = res.readUInt16BE(res.length - 2);
+
+  switch (status) {
+    case StatusCodes.OK:
+      return res.slice(0, res.length - 2).toString("utf-8");
+    case StatusCodes.DEVICE_NOT_ONBOARDED:
+      return "";
+  }
+
+  throw new TransportStatusError(status);
 };
