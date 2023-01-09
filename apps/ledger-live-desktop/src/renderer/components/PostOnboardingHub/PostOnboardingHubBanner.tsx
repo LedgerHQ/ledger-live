bc6ed2930393a96dc840f467d8e96c6cf45542d4
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2023-01-09 15:35:32 +0100

    handle hide banner

--- apps/ledger-live-desktop/src/renderer/components/PostOnboardingHub/PostOnboardingHubBanner.tsx
+++ apps/ledger-live-desktop/src/renderer/components/PostOnboardingHub/PostOnboardingHubBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
@@ -11,32 +11,33 @@ import bannerStaxDark from "./assets/bannerStaxDark.svg";
 
 import { useHistory } from "react-router-dom";
 
-const PostOnboardingHubBannerWrapper = styled(Flex)`
-  cursor: pointer;
-`;
-
 const CloseButtonWrapper = styled(Box).attrs(() => ({
   top: 4,
   right: 4,
   position: "absolute",
-}))``;
+}))`
+  cursor: pointer;
+`;
 
 const PostOnboardingHubBanner = () => {
   const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState<boolean>(true);
   const navigateToPostOnboardingHub = useNavigateToPostOnboardingHubCallback();
 
   const handleNavigateToPostOnboardingHub = useCallback(() => navigateToPostOnboardingHub(), [
     navigateToPostOnboardingHub,
   ]);
 
+  const handleHidePostOnboardingHubBanner = useCallback(() => setIsVisible(false), []);
+
+  if (!isVisible) return null;
   return (
-    <PostOnboardingHubBannerWrapper
+    <Flex
       backgroundColor="neutral.c100"
       borderRadius="8px"
       justifyContent="space-between"
       px={"40px"}
       mb="20px"
-      onClick={handleNavigateToPostOnboardingHub}
     >
       <Flex flexDirection="column" justifyContent="center" alignItems="flex-start">
         <Text color="neutral.c00" variant="paragraph" fontSize="18px">
@@ -45,15 +46,17 @@ const PostOnboardingHubBanner = () => {
         <Text mt="8px" mb="18px" color="neutral.c50" whiteSpace="pre-wrap" fontSize="14px">
           {t("postOnboarding.postOnboardingBanner.description")}
         </Text>
-        <Link color="neutral.c00">{t("postOnboarding.postOnboardingBanner.link")}</Link>
+        <Link color="neutral.c00" onClick={handleNavigateToPostOnboardingHub}>
+          {t("postOnboarding.postOnboardingBanner.link")}
+        </Link>
       </Flex>
       <Flex>
         <Illustration lightSource={bannerStaxLight} darkSource={bannerStaxDark} size={240} />
       </Flex>
-      <CloseButtonWrapper onClick={handleNavigateToPostOnboardingHub}>
+      <CloseButtonWrapper onClick={handleHidePostOnboardingHubBanner}>
         <Icons.CloseMedium color="neutral.c00" size={30} />
       </CloseButtonWrapper>
-    </PostOnboardingHubBannerWrapper>
+    </Flex>
   );
 };
 
