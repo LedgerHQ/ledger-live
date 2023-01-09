aa53c485e21bedc386db78776a44cdda7de14abb
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-09 12:12:23 +0100

    fix: locked device error handled in sharedLogicTaskWrapper

--- libs/ledger-live-common/src/deviceSDK/tasks/core.ts
+++ libs/ledger-live-common/src/deviceSDK/tasks/core.ts
@@ -7,33 +7,48 @@ export type SharedTaskEvent = { type: "error"; error: Error };
 
 const NO_RESPONSE_TIMEOUT_MS = 30000;
 
-export function wrappedTask<TaskArgumentsType, TaskEventsType>(
+/**
+ * Wraps a task function to add some common logic to it:
+ * - Timeout for no response
+ * - Retry strategy on specific errors
+ * - Catch errors and emit them as events
+ *
+ * @param task The task function to wrap
+ * @returns A wrapped task function
+ */
+export function sharedLogicTaskWrapper<TaskArgumentsType, TaskEventsType>(
   task: (
     args: TaskArgumentsType
   ) => Observable<TaskEventsType | SharedTaskEvent>
 ) {
   return (
     args: TaskArgumentsType
-  ): Observable<TaskEventsType | SharedTaskEvent> =>
-    task(args).pipe(
-      timeout(NO_RESPONSE_TIMEOUT_MS),
-      retryWhen(
-        retryWhileErrors((error) => {
-          // TODO: emits here an event to the action of type error, error: LockedDeviceError ? Yes
-          if (error instanceof LockedDeviceError) {
-            return true;
-          }
+  ): Observable<TaskEventsType | SharedTaskEvent> => {
+    return new Observable((subscriber) => {
+      return task(args)
+        .pipe(
+          timeout(NO_RESPONSE_TIMEOUT_MS),
+          retryWhen(
+            retryWhileErrors((error) => {
+              if (error instanceof LockedDeviceError) {
+                // Emits to the action a locked device error event so it is aware of it before retrying
+                subscriber.next({ type: "error", error });
+                return true;
+              }
 
-          // TODO: other errors on which to retry:
-          // - CantOpenDevice: seems to be when device is USB plugged, powered on, but not yet unlocked for the 1st time
-          //   since being powered on.
+              // TODO: other errors on which to retry:
+              // - CantOpenDevice: seems to be when device is USB plugged, powered on, but not yet unlocked for the 1st time
+              //   since being powered on.
 
-          return false;
-        })
-      ),
-      catchError((error: Error) => {
-        // Emits the error to the action, without throwing
-        return of<SharedTaskEvent>({ type: "error", error });
-      })
-    );
+              return false;
+            })
+          ),
+          catchError((error: Error) => {
+            // Emits the error to the action, without throwing
+            return of<SharedTaskEvent>({ type: "error", error });
+          })
+        )
+        .subscribe(subscriber);
+    });
+  };
 }
--- libs/ledger-live-common/src/deviceSDK/tasks/getDeviceInfo.ts
+++ libs/ledger-live-common/src/deviceSDK/tasks/getDeviceInfo.ts
@@ -11,7 +11,7 @@ import { withDevice } from "../../hw/deviceAccess";
 import { PROVIDERS } from "../../manager/provider";
 import { Observable } from "rxjs";
 import { map, filter, switchMap } from "rxjs/operators";
-import { SharedTaskEvent, wrappedTask } from "./core";
+import { SharedTaskEvent, sharedLogicTaskWrapper } from "./core";
 
 const ManagerAllowedFlag = 0x08;
 const PinValidatedFlag = 0x80;
@@ -38,104 +38,103 @@ function internalGetDeviceInfoTask({
   deviceId,
 }: GetDeviceInfoTaskArgs): Observable<GetDeviceInfoTaskEvent> {
   return new Observable((subscriber) => {
-    return withDevice(deviceId)((transport) =>
-      getAppAndVersion(transport).pipe(
-        filter(({ appAndVersion: { name } }) => {
-          if (!isDashboardName(name)) {
-            subscriber.next({
-              type: "taskError",
-              error: "DeviceOnDashboardExpected",
-            });
-            return false;
-          }
-          return true;
-        }),
-        switchMap(() => {
-          return getVersion(transport);
-        }),
-        map((value) => {
-          if (value.type === "unresponsive") {
-            return { type: "error" as const, error: new LockedDeviceError() };
-          }
+    return (
+      withDevice(deviceId)((transport) =>
+        getAppAndVersion(transport).pipe(
+          filter(({ appAndVersion: { name } }) => {
+            if (!isDashboardName(name)) {
+              subscriber.next({
+                type: "taskError",
+                error: "DeviceOnDashboardExpected",
+              });
+              return false;
+            }
+            return true;
+          }),
+          switchMap(() => {
+            return getVersion(transport);
+          }),
+          map((value) => {
+            if (value.type === "unresponsive") {
+              return { type: "error" as const, error: new LockedDeviceError() };
+            }
 
-          const { firmwareInfo } = value;
+            const { firmwareInfo } = value;
 
-          const {
-            isBootloader,
-            rawVersion,
-            targetId,
-            seVersion,
-            seTargetId,
-            mcuBlVersion,
-            mcuVersion,
-            mcuTargetId,
-            flags,
-            bootloaderVersion,
-            hardwareVersion,
-            languageId,
-          } = firmwareInfo;
+            const {
+              isBootloader,
+              rawVersion,
+              targetId,
+              seVersion,
+              seTargetId,
+              mcuBlVersion,
+              mcuVersion,
+              mcuTargetId,
+              flags,
+              bootloaderVersion,
+              hardwareVersion,
+              languageId,
+            } = firmwareInfo;
 
-          const isOSU = rawVersion.includes("-osu");
-          const version = rawVersion.replace("-osu", "");
-          const m = rawVersion.match(/([0-9]+.[0-9]+)(.[0-9]+)?(-(.*))?/);
-          const [, majMin, , , postDash] = m || [];
-          const providerName = PROVIDERS[postDash] ? postDash : null;
-          const flag = flags.length > 0 ? flags[0] : 0;
-          const managerAllowed = !!(flag & ManagerAllowedFlag);
-          const pinValidated = !!(flag & PinValidatedFlag);
+            const isOSU = rawVersion.includes("-osu");
+            const version = rawVersion.replace("-osu", "");
+            const m = rawVersion.match(/([0-9]+.[0-9]+)(.[0-9]+)?(-(.*))?/);
+            const [, majMin, , , postDash] = m || [];
+            const providerName = PROVIDERS[postDash] ? postDash : null;
+            const flag = flags.length > 0 ? flags[0] : 0;
+            const managerAllowed = !!(flag & ManagerAllowedFlag);
+            const pinValidated = !!(flag & PinValidatedFlag);
 
-          let isRecoveryMode = false;
-          let onboarded = true;
-          if (flags.length === 4) {
-            // Nb Since LNS+ unseeded devices are visible + extra flags
-            isRecoveryMode = !!(flags[0] & 0x01);
-            onboarded = !!(flags[0] & 0x04);
-          }
+            let isRecoveryMode = false;
+            let onboarded = true;
+            if (flags.length === 4) {
+              // Nb Since LNS+ unseeded devices are visible + extra flags
+              isRecoveryMode = !!(flags[0] & 0x01);
+              onboarded = !!(flags[0] & 0x04);
+            }
 
-          log(
-            "hw",
-            "deviceInfo: se@" +
-              version +
-              " mcu@" +
-              mcuVersion +
-              (isOSU ? " (osu)" : isBootloader ? " (bootloader)" : "")
-          );
+            log(
+              "hw",
+              "deviceInfo: se@" +
+                version +
+                " mcu@" +
+                mcuVersion +
+                (isOSU ? " (osu)" : isBootloader ? " (bootloader)" : "")
+            );
 
-          const hasDevFirmware = isDevFirmware(seVersion);
-          const deviceInfo: DeviceInfo = {
-            version,
-            mcuVersion,
-            seVersion,
-            mcuBlVersion,
-            majMin,
-            providerName: providerName || null,
-            targetId,
-            hasDevFirmware,
-            seTargetId,
-            mcuTargetId,
-            isOSU,
-            isBootloader,
-            isRecoveryMode,
-            managerAllowed,
-            pinValidated,
-            onboarded,
-            bootloaderVersion,
-            hardwareVersion,
-            languageId,
-          };
+            const hasDevFirmware = isDevFirmware(seVersion);
+            const deviceInfo: DeviceInfo = {
+              version,
+              mcuVersion,
+              seVersion,
+              mcuBlVersion,
+              majMin,
+              providerName: providerName || null,
+              targetId,
+              hasDevFirmware,
+              seTargetId,
+              mcuTargetId,
+              isOSU,
+              isBootloader,
+              isRecoveryMode,
+              managerAllowed,
+              pinValidated,
+              onboarded,
+              bootloaderVersion,
+              hardwareVersion,
+              languageId,
+            };
 
-          return { type: "data" as const, deviceInfo };
-        })
+            return { type: "data" as const, deviceInfo };
+          })
+        )
       )
-    ).subscribe({
-      next: (event) => subscriber.next(event),
-      error: (error) => {
-        // Any other erro
-        subscriber.next({ type: "error", error });
-      },
-      complete: () => subscriber.complete(),
-    });
+        // Any error will be handled by the sharedLogicTaskWrapper, which will map it a relevant event
+        .subscribe(subscriber)
+    );
   });
 }
 
-export const getDeviceInfoTask = wrappedTask(internalGetDeviceInfoTask);
+export const getDeviceInfoTask = sharedLogicTaskWrapper(
+  internalGetDeviceInfoTask
+);
