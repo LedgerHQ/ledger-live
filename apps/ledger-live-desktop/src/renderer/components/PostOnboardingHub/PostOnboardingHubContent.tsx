b54b4ec27fef4aa7f989e0b69ca190ed36d4ff6d
Pierre-Louis Crescitz <pierrelouis.crescitz@gmail.com>
2023-01-11 10:11:28 +0100

    add timeout

--- apps/ledger-live-desktop/src/renderer/components/PostOnboardingHub/PostOnboardingHubContent.tsx
+++ apps/ledger-live-desktop/src/renderer/components/PostOnboardingHub/PostOnboardingHubContent.tsx
@@ -1,18 +1,27 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { Flex, Link } from "@ledgerhq/react-ui";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
+import { useAllPostOnboardingActionsCompleted } from "@ledgerhq/live-common/postOnboarding/hooks/index";
 import PostOnboardingHub from ".";
 import { withV3StyleProvider } from "~/renderer/styles/StyleProviderV3";
 
 const PostOnboardingHubContent = () => {
   const { t } = useTranslation();
   const history = useHistory();
+  const allDone = useAllPostOnboardingActionsCompleted();
 
   const handleSkipButton = useCallback(() => {
     history.push("/");
   }, [history]);
+
+  useEffect(() => {
+    if (allDone) {
+      const timeout = setTimeout(() => history.push("/"), 4000);
+      return () => clearTimeout(timeout);
+    }
+  }, [allDone, history]);
 
   return (
     <Flex
@@ -25,9 +34,11 @@ const PostOnboardingHubContent = () => {
       maxWidth={450}
     >
       <PostOnboardingHub />
-      <Link onClick={handleSkipButton} data-test-id={"postonboarding-hub-skip-button"} mt={5}>
-        {t("postOnboarding.postOnboardingContent.skipLink")}
-      </Link>
+      {!allDone ? (
+        <Link onClick={handleSkipButton} data-test-id={"postonboarding-hub-skip-button"} mt={5}>
+          {t("postOnboarding.postOnboardingContent.skipLink")}
+        </Link>
+      ) : null}
     </Flex>
   );
 };
