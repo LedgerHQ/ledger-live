9e04f63f820900aa1f6276cf2675f4cdca74a5f9
Troels F. Rønnow <troels@wonop.com>
2023-01-11 08:17:06 +0100

    Linting

--- libs/ledger-live-common/src/families/zilliqa/speculos-deviceActions.ts
+++ libs/ledger-live-common/src/families/zilliqa/speculos-deviceActions.ts
@@ -2,52 +2,50 @@ import type { DeviceAction } from "../../bot/types";
 import type { Transaction } from "./types";
 import { deviceActionFlow, formatDeviceAmount } from "../../bot/specs";
 
-export const acceptTransaction: DeviceAction<
-	Transaction,
-	any
-> = deviceActionFlow({
-	steps: [
-		{
-			title: "Transaction Type",
-			button: "Rr",
-			expectedValue: () => "Payment",
-		},
-		{
-			title: "Amount",
-			button: "Rr",
-			expectedValue: ({ account, status }) =>
-				formatDeviceAmount(account.currency, status.amount),
-		},
-		{
-			title: "Fee",
-			button: "Rr",
-			expectedValue: ({ account, status }) =>
-				formatDeviceAmount(account.currency, status.estimatedFees),
-		},
-		{
-			title: "Destination Tag",
-			button: "Rr",
-			expectedValue: ({ transaction }) => "",
-		},
-		{
-			title: "Destination",
-			button: "Rr",
-			trimValue: true,
-			expectedValue: ({ transaction }) => transaction.recipient,
-		},
-		{
-			title: "Account",
-			button: "Rr",
-			trimValue: true,
-			expectedValue: ({ account }) => account.freshAddress,
-		},
-		{
-			title: "Accept",
-			button: "LRlr",
-		},
-		{
-			title: "Sign transaction",
-			button: "LRlr",
-		},
-	],
-});
+export const acceptTransaction: DeviceAction<Transaction, any> =
+  deviceActionFlow({
+    steps: [
+      {
+        title: "Transaction Type",
+        button: "Rr",
+        expectedValue: () => "Payment",
+      },
+      {
+        title: "Amount",
+        button: "Rr",
+        expectedValue: ({ account, status }) =>
+          formatDeviceAmount(account.currency, status.amount),
+      },
+      {
+        title: "Fee",
+        button: "Rr",
+        expectedValue: ({ account, status }) =>
+          formatDeviceAmount(account.currency, status.estimatedFees),
+      },
+      {
+        title: "Destination Tag",
+        button: "Rr",
+        expectedValue: (/*{transaction}*/) => "",
+      },
+      {
+        title: "Destination",
+        button: "Rr",
+        trimValue: true,
+        expectedValue: ({ transaction }) => transaction.recipient,
+      },
+      {
+        title: "Account",
+        button: "Rr",
+        trimValue: true,
+        expectedValue: ({ account }) => account.freshAddress,
+      },
+      {
+        title: "Accept",
+        button: "LRlr",
+      },
+      {
+        title: "Sign transaction",
+        button: "LRlr",
+      },
+    ],
+  });
