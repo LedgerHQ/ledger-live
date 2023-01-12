33b0def90a3b0f3c4b7efe05e3cadfd92392be9a
lambertkevin <kevin.lambert@ledger.fr>
2023-01-12 11:25:21 +0100

    Add tokens to evm sync

--- libs/ledger-live-common/src/families/evm/logic.ts
+++ libs/ledger-live-common/src/families/evm/logic.ts
@@ -1,4 +1,6 @@
 import BigNumber from "bignumber.js";
+import { Account, SubAccount } from "@ledgerhq/types-live";
+import { mergeOps } from "../../bridge/jsHelpers";
 import {
   Transaction as EvmTransaction,
   EvmTransactionEIP1559,
@@ -23,12 +25,84 @@ export const eip1559TransactionHasFees = (tx: EvmTransactionEIP1559): boolean =>
 export const getEstimatedFees = (tx: EvmTransaction): BigNumber => {
   if (tx.type !== 2) {
     return (
-      (tx as EvmTransactionLegacy)?.gasPrice?.multipliedBy(tx.gasLimit) ||
+      (tx as EvmTransactionLegacy).gasPrice?.multipliedBy(tx.gasLimit) ||
       new BigNumber(0)
     );
   }
   return (
-    (tx as EvmTransactionEIP1559)?.maxFeePerGas?.multipliedBy(tx.gasLimit) ||
+    (tx as EvmTransactionEIP1559).maxFeePerGas?.multipliedBy(tx.gasLimit) ||
     new BigNumber(0)
   );
+};
+
+/**
+ * List of properties of a sub account that can be updated when 2 "identical" accounts are found
+ */
+const updatableSubAccountProperties = [
+  { name: "balance", isOps: false },
+  { name: "spendableBalance", isOps: false },
+  { name: "balanceHistoryCache", isOps: false },
+  { name: "operations", isOps: true },
+  { name: "pendingOperations", isOps: true },
+];
+
+/**
+ * In charge of smartly merging sub accounts while maintaining references as much as possible
+ */
+export const mergeSubAccounts = (
+  initialAccount: Account | undefined,
+  newSubAccounts: Partial<SubAccount>[]
+): Array<Partial<SubAccount> | SubAccount> => {
+  const oldSubAccounts: Array<Partial<SubAccount> | SubAccount> | undefined =
+    initialAccount?.subAccounts;
+  if (!oldSubAccounts) {
+    if (initialAccount) {
+      initialAccount.subAccounts = newSubAccounts as SubAccount[];
+    }
+    return newSubAccounts;
+  }
+
+  // Creating a map of already existing sub accounts by id
+  const oldSubAccountsById: { [key: string]: Partial<SubAccount> } = {};
+  for (const oldSubAccount of oldSubAccounts) {
+    oldSubAccountsById[oldSubAccount.id!] = oldSubAccount;
+  }
+
+  // Looping on new sub accounts to compare them with already existing ones
+  // Already existing will be updated if necessary (see `updatableSubAccountProperties`)
+  // Fresh new sub accounts will be added/pushed after already existing
+  const newAccountsToAdd: Partial<SubAccount>[] = [];
+  for (const newSubAccount of newSubAccounts) {
+    const duplicatedAccount: Partial<SubAccount> | undefined =
+      oldSubAccountsById[newSubAccount.id!];
+
+    // If this sub account was not already in the initialAccount
+    if (!duplicatedAccount) {
+      // We'll add it later
+      newAccountsToAdd.push(newSubAccount);
+      continue;
+    }
+
+    updatableSubAccountProperties.forEach(({ name, isOps }) => {
+      if (!isOps) {
+        if (newSubAccount[name] !== duplicatedAccount[name]) {
+          duplicatedAccount[name] = newSubAccount[name];
+        }
+      } else {
+        duplicatedAccount[name] = mergeOps(
+          duplicatedAccount[name],
+          newSubAccount[name]
+        );
+      }
+    });
+
+    // Updating the operationsCount in case the mergeOps changed it
+    duplicatedAccount.operationsCount =
+      duplicatedAccount.operations?.length || 0;
+  }
+
+  // We add the potential new subaccounts to the already existing ones
+  oldSubAccounts.push(...newAccountsToAdd);
+
+  return oldSubAccounts;
 };
--- libs/ledger-live-common/src/families/evm/synchronization.ts
+++ libs/ledger-live-common/src/families/evm/synchronization.ts
@@ -1,15 +1,30 @@
-import { Account, Operation } from "@ledgerhq/types-live";
-import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
-import { getAccount, getBlock, getTransaction } from "./api/rpc";
-import { encodeAccountId } from "../../account";
+import { log } from "@ledgerhq/logs";
+import { Account, Operation, SubAccount } from "@ledgerhq/types-live";
+import { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import etherscanLikeApi from "./api/etherscan";
+import { mergeSubAccounts } from "./logic";
 import {
   makeSync,
   makeScanAccounts,
   mergeOps,
   GetAccountShape,
+  AccountShapeInfos,
 } from "../../bridge/jsHelpers";
+import {
+  emptyHistoryCache,
+  encodeAccountId,
+  encodeTokenAccountId,
+} from "../../account";
+import {
+  getAccount,
+  getBlock,
+  getTokenBalance,
+  getTransaction,
+} from "./api/rpc";
 
+/**
+ * Switch to select one of the compatible explorer
+ */
 const getExplorerApi = (currency: CryptoCurrency) => {
   const apiType = currency?.ethereumLikeInfo?.explorer?.type;
 
@@ -24,10 +39,11 @@ const getExplorerApi = (currency: CryptoCurrency) => {
 };
 
 /**
- * Synchronization process
+ * Main synchronization process
+ * Get the main Account and the potential TokenAccounts linked to it
  */
-export const getAccountShape: GetAccountShape = async (info) => {
-  const { initialAccount, address, derivationMode, currency } = info;
+export const getAccountShape: GetAccountShape = async (infos) => {
+  const { initialAccount, address, derivationMode, currency } = infos;
   const { blockHeight, balance, nonce } = await getAccount(currency, address);
   const accountId = encodeAccountId({
     type: "js",
@@ -38,29 +54,41 @@ export const getAccountShape: GetAccountShape = async (info) => {
   });
 
   // Get the latest stored operation to know where to start the new sync
-  const latestOperation = initialAccount?.operations?.reduce((acc, curr) => {
-    if (!acc) {
-      return curr;
-    }
-    return (acc?.blockHeight || 0) > (curr?.blockHeight || 0) ? acc : curr;
-  }, null as Operation | null);
+  const latestCoinOperation =
+    initialAccount?.operations?.reduce<Operation | null>((acc, curr) => {
+      if (!acc) {
+        return curr;
+      }
+      return (acc?.blockHeight || 0) > (curr?.blockHeight || 0) ? acc : curr;
+    }, null);
+
   // This method could not be working if the integration doesn't have an API to retreive the operations
-  const lastOperations = await (async () => {
+  const lastCoinOperations = await (async () => {
     try {
-      const { getLatestTransactions } = await getExplorerApi(currency);
-      return await getLatestTransactions(
+      const { getLatestCoinOperations } = await getExplorerApi(currency);
+      return await getLatestCoinOperations(
         currency,
         address,
         accountId,
-        latestOperation?.blockHeight ? latestOperation.blockHeight : 0
+        latestCoinOperation?.blockHeight ? latestCoinOperation.blockHeight : 0
       );
     } catch (e) {
-      return [];
+      log("EVM Family", "Failed to get latest transactions", {
+        address,
+        currency,
+        error: e,
+      });
+      throw e;
     }
   })();
 
+  const newSubAccounts = await getSubAccounts(infos, accountId, nonce);
+  // Merging potential new subAccouns while preserving the reference (returned value will be initialAccount.subAccounts)
+  const subAccounts = mergeSubAccounts(initialAccount, newSubAccounts);
+
   // Trying to confirm pending operations that we are sure of
   // because they were made in the live
+  // Useful for integrations without explorers
   const confirmPendingOperations =
     initialAccount?.pendingOperations?.map((op) =>
       getOperationStatus(currency, op)
@@ -69,17 +97,119 @@ export const getAccountShape: GetAccountShape = async (info) => {
     (ops) => ops.filter((op): op is Operation => !!op)
   );
 
-  const newOperations = [...confirmedOperations, ...lastOperations];
+  const newOperations = [...confirmedOperations, ...lastCoinOperations];
   const operations = mergeOps(initialAccount?.operations || [], newOperations);
+  const lastSyncDate = new Date();
 
   return {
+    type: "Account",
     id: accountId,
     balance,
     spendableBalance: balance,
     operationsCount: nonce,
     blockHeight,
     operations,
+    subAccounts,
+    lastSyncDate,
   } as Partial<Account>;
+};
+
+/**
+ * Getting all token related operations in order to provide TokenAccounts
+ */
+export const getSubAccounts = async (
+  infos: AccountShapeInfos,
+  accountId: string,
+  nonce: number
+): Promise<Partial<SubAccount>[]> => {
+  const { initialAccount, address, currency } = infos;
+
+  // Get the latest token operation from all subaccounts
+  const latestTokenOperation = initialAccount?.subAccounts
+    ?.flatMap(({ operations }) => operations)
+    .reduce<Operation | null>((acc, curr) => {
+      if (!acc) {
+        return curr;
+      }
+      return (acc?.blockHeight || 0) > (curr?.blockHeight || 0) ? acc : curr;
+    }, null);
+
+  // This method could not be working if the integration doesn't have an API to retreive the operations
+  const lastERC20OperationsAndCurrencies = await (async () => {
+    try {
+      const { getLatestTokenOperations } = await getExplorerApi(currency);
+      return await getLatestTokenOperations(
+        currency,
+        address,
+        accountId,
+        latestTokenOperation?.blockHeight ? latestTokenOperation.blockHeight : 0
+      );
+    } catch (e) {
+      log("EVM Family", "Failed to get latest ERC20 transactions", {
+        address,
+        currency,
+        error: e,
+      });
+      throw e;
+    }
+  })();
+
+  // Creating a Map of Operations by TokenCurrencies in order to know which TokenAccounts should be synced as well
+  const erc20OperationsByToken = lastERC20OperationsAndCurrencies.reduce<
+    Map<TokenCurrency, Operation[]>
+  >((acc, { tokenCurrency, operation }) => {
+    if (!tokenCurrency) return acc;
+
+    if (!acc.has(tokenCurrency)) {
+      acc.set(tokenCurrency, []);
+    }
+    acc.get(tokenCurrency)?.push(operation);
+
+    return acc;
+  }, new Map<TokenCurrency, Operation[]>());
+
+  // Fetching all TokenAccounts possible and providing already filtered operations
+  const subAccountsPromises: Promise<Partial<SubAccount>>[] = [];
+  for (const [token, ops] of erc20OperationsByToken.entries()) {
+    subAccountsPromises.push(
+      getSubAccountShape(currency, accountId, address, token, ops, nonce)
+    );
+  }
+
+  return Promise.all(subAccountsPromises);
+};
+
+/**
+ * Fetch the balance for a token and creates a TokenAccount based on this and the provided operations
+ */
+export const getSubAccountShape = async (
+  currency: CryptoCurrency,
+  parentId: string,
+  address: string,
+  token: TokenCurrency,
+  operations: Operation[],
+  nonce: number
+): Promise<Partial<SubAccount>> => {
+  const tokenAccountId = encodeTokenAccountId(parentId, token);
+  const balance = await getTokenBalance(
+    currency,
+    address,
+    token.contractAddress
+  );
+
+  return {
+    type: "TokenAccount",
+    id: tokenAccountId,
+    parentId,
+    token,
+    balance,
+    spendableBalance: balance,
+    creationDate: new Date(),
+    operationsCount: nonce,
+    operations,
+    pendingOperations: [],
+    balanceHistoryCache: emptyHistoryCache,
+  };
 };
 
 /**
