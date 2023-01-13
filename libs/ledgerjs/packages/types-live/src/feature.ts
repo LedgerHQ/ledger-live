99abba0b5a591d475d0be554e21eb9b406f2eb39
Henri Ly <henri.ly-ext@ledger.fr>
2023-01-13 15:38:01 +0100

    add feature flag for objkt on tezos account

--- libs/ledgerjs/packages/types-live/src/feature.ts
+++ libs/ledgerjs/packages/types-live/src/feature.ts
@@ -43,7 +43,8 @@ export type FeatureId =
   | "postOnboardingAssetsTransfer"
   | "firebaseEnvironmentReadOnly"
   | "protectServicesMobile"
-  | "swapShowDexQuotes";
+  | "swapShowDexQuotes"
+  | "objkt";
 
 /**  We use objects instead of direct booleans for potential future improvements
 like feature versioning etc */
