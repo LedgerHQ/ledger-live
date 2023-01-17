7b44c280c4d2673d1ac9667abd333737e325600d
lambertkevin <kevin.lambert@ledger.fr>
2023-01-17 22:07:19 +0100

    Add full code coverage for EVM family

--- libs/ledger-live-common/src/families/evm/__tests__/logic.unit.test.ts
+++ libs/ledger-live-common/src/families/evm/__tests__/logic.unit.test.ts
@@ -1,10 +1,13 @@
 import BigNumber from "bignumber.js";
+import { getCryptoCurrencyById, getTokenById } from "@ledgerhq/cryptoassets";
+import { EvmTransactionEIP1559, EvmTransactionLegacy } from "../types";
+import { makeAccount, makeOperation, makeTokenAccount } from "../testUtils";
 import {
   eip1559TransactionHasFees,
   getEstimatedFees,
   legacyTransactionHasFees,
+  mergeSubAccounts,
 } from "../logic";
-import { EvmTransactionEIP1559, EvmTransactionLegacy } from "../types";
 
 describe("EVM Family", () => {
   describe("logic.ts", () => {
@@ -12,6 +15,15 @@ describe("EVM Family", () => {
       it("should return true for legacy tx with fees", () => {
         const tx: Partial<EvmTransactionLegacy> = {
           type: 0,
+          gasPrice: new BigNumber(100),
+        };
+
+        expect(legacyTransactionHasFees(tx as EvmTransactionLegacy)).toBe(true);
+      });
+
+      it("should return true for type 1 (unused in the live for now) tx with fees", () => {
+        const tx: Partial<EvmTransactionLegacy> = {
+          type: 1,
           gasPrice: new BigNumber(100),
         };
 
@@ -87,6 +99,19 @@ describe("EVM Family", () => {
         expect(getEstimatedFees(tx as any)).toEqual(new BigNumber(69));
       });
 
+      it("should fallback with tx without type", () => {
+        const tx = {};
+        expect(getEstimatedFees(tx as any)).toEqual(new BigNumber(0));
+      });
+
+      it("should fallback with badly formatted legacy tx", () => {
+        const tx = {
+          type: 0,
+        };
+
+        expect(getEstimatedFees(tx as any)).toEqual(new BigNumber(0));
+      });
+
       it("should return the right fee estimation for a 1559 tx", () => {
         const tx = {
           type: 2,
@@ -97,6 +122,144 @@ describe("EVM Family", () => {
         };
 
         expect(getEstimatedFees(tx as any)).toEqual(new BigNumber(420));
+      });
+
+      it("should fallback with badly formatted 1559 tx", () => {
+        const tx = {
+          type: 2,
+        };
+
+        expect(getEstimatedFees(tx as any)).toEqual(new BigNumber(0));
+      });
+    });
+
+    describe("mergeSubAccounts", () => {
+      it("should merge 2 different sub accounts", () => {
+        const tokenAccount1 = {
+          ...makeTokenAccount(
+            "0xkvn",
+            getTokenById("ethereum/erc20/usd__coin")
+          ),
+          balance: new BigNumber(1),
+          operations: [],
+        };
+        const tokenAccount2 = {
+          ...makeTokenAccount("0xkvn", getTokenById("ethereum/erc20/weth")),
+          balance: new BigNumber(2),
+          operations: [],
+        };
+        const account = makeAccount(
+          "0xkvn",
+          getCryptoCurrencyById("ethereum"),
+          [tokenAccount1]
+        );
+
+        expect(mergeSubAccounts(account, [tokenAccount2])).toEqual([
+          tokenAccount1,
+          tokenAccount2,
+        ]);
+        expect(account.subAccounts).toEqual([tokenAccount1, tokenAccount2]);
+        expect(account?.subAccounts?.[0]).toBe(tokenAccount1); // keeping the reference
+      });
+
+      it("should merge 2 different sub accounts and update the first one", () => {
+        const tokenAccount1 = {
+          ...makeTokenAccount(
+            "0xkvn",
+            getTokenById("ethereum/erc20/usd__coin")
+          ),
+          balance: new BigNumber(1),
+          operations: [],
+        };
+        const tokenAccount1Bis = {
+          ...tokenAccount1,
+          balance: new BigNumber(10),
+          spendableBalance: new BigNumber(11),
+          operationsCount: 0,
+          balanceHistoryCache: {
+            HOUR: {
+              latestDate: 123,
+              balances: [123],
+            },
+            DAY: {
+              latestDate: 234,
+              balances: [234],
+            },
+            WEEK: {
+              latestDate: 345,
+              balances: [345],
+            },
+          },
+          operations: [],
+        };
+        const tokenAccount2 = {
+          ...makeTokenAccount("0xkvn", getTokenById("ethereum/erc20/weth")),
+          balance: new BigNumber(2),
+          operations: [],
+        };
+        const account = makeAccount(
+          "0xkvn",
+          getCryptoCurrencyById("ethereum"),
+          [tokenAccount1]
+        );
+
+        expect(
+          mergeSubAccounts(account, [tokenAccount1Bis, tokenAccount2])
+        ).toEqual([tokenAccount1Bis, tokenAccount2]);
+        expect(account.subAccounts).toEqual([tokenAccount1Bis, tokenAccount2]);
+        expect(account?.subAccounts?.[0]).toBe(tokenAccount1); // keeping the reference
+      });
+
+      it("should update subAccount ops", () => {
+        const op1 = makeOperation();
+        const op2 = makeOperation({
+          hash: "0xdiffHash",
+        });
+        const op3 = makeOperation({
+          hash: "0xAgAinAnotHeRH4sh",
+        });
+        const tokenAccount1 = {
+          ...makeTokenAccount(
+            "0xkvn",
+            getTokenById("ethereum/erc20/usd__coin")
+          ),
+          balance: new BigNumber(1),
+          operations: [op1, op2],
+          operationsCount: 2,
+        };
+        const tokenAccount1Bis = {
+          ...tokenAccount1,
+          operations: [op3, op1, op2],
+          operationsCount: 3,
+        };
+        const account = makeAccount(
+          "0xkvn",
+          getCryptoCurrencyById("ethereum"),
+          [tokenAccount1]
+        );
+
+        expect(mergeSubAccounts(account, [tokenAccount1Bis])).toEqual([
+          tokenAccount1Bis,
+        ]);
+        expect(account.subAccounts).toEqual([tokenAccount1Bis]);
+        expect(account?.subAccounts?.[0]).toBe(tokenAccount1); // keeping the reference
+      });
+
+      it("should return only new sub accounts", () => {
+        const tokenAccount = {
+          ...makeTokenAccount(
+            "0xkvn",
+            getTokenById("ethereum/erc20/usd__coin")
+          ),
+          balance: new BigNumber(1),
+        };
+        const account = makeAccount("0xkvn", getCryptoCurrencyById("ethereum"));
+        delete account.subAccounts;
+
+        expect(mergeSubAccounts(account, [tokenAccount])).toEqual([
+          tokenAccount,
+        ]);
+        expect(account.subAccounts).toEqual([tokenAccount]);
       });
     });
   });
