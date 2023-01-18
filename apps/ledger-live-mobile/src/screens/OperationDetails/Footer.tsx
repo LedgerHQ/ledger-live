0d7c6158f7d14e1325886a80b37764e8e531e725
lvndry <lvndry@proton.me>
2023-01-18 10:31:56 +0100

    improve props passed in components in OperationDetails

--- apps/ledger-live-mobile/src/screens/OperationDetails/Footer.tsx
+++ apps/ledger-live-mobile/src/screens/OperationDetails/Footer.tsx
@@ -1,20 +1,21 @@
 import React, { memo } from "react";
 import { Linking } from "react-native";
 import { Trans } from "react-i18next";
-import { AccountLike } from "@ledgerhq/types-live";
-import { getAccountCurrency } from "@ledgerhq/live-common/account/helpers";
+import { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { Flex } from "@ledgerhq/native-ui";
+
 import ExternalLink from "../../icons/ExternalLink";
 import Button from "../../components/wrappedUi/Button";
 
 type Props = {
   url?: string | null;
   urlWhatIsThis?: string | null;
-  account: AccountLike;
+  currency: CryptoCurrency | TokenCurrency;
 };
 
-function Footer({ url, urlWhatIsThis, account }: Props) {
-  const currencyId = getAccountCurrency(account).name;
+function Footer({ url, urlWhatIsThis, currency }: Props) {
+  const { name } = currency;
+
   return (
     <Flex bg={"background.main"} px={6} my={6}>
       {urlWhatIsThis ? (
@@ -34,7 +35,7 @@ function Footer({ url, urlWhatIsThis, account }: Props) {
           type="main"
           onPress={() => Linking.openURL(url)}
           eventProperties={{
-            currencyId,
+            currencyId: name,
           }}
           outline
           mt={4}
