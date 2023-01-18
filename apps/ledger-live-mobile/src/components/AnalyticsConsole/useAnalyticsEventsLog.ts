40c4375ff740e061a9081ab5962f0e08cfe0c0df
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-01-18 17:47:05 +0100

    remove useless filter function

--- apps/ledger-live-mobile/src/components/AnalyticsConsole/useAnalyticsEventsLog.ts
+++ apps/ledger-live-mobile/src/components/AnalyticsConsole/useAnalyticsEventsLog.ts
@@ -3,27 +3,9 @@ import { map } from "rxjs/operators";
 import { trackSubject } from "../../analytics/segment";
 import { LoggableEventRenderable } from "./types";
 
-type FilterFunction = (
-  curr: LoggableEventRenderable,
-  prev: LoggableEventRenderable,
-) => LoggableEventRenderable;
-
 export default function useAnalyticsEventsLog(limit = 40) {
   const id = useRef(0);
   const [items, setItems] = useState<LoggableEventRenderable[]>([]);
-  const filter: FilterFunction = useCallback((curr, prev) => {
-    if (!prev || !curr.eventProperties) return curr;
-    // We repeat a bunch of data over and over again, filter that out unless it changes.
-    // Will only work for flat props.
-    const keys = Object.keys(curr.eventProperties).filter(
-      k => curr?.eventProperties[k] !== prev?.eventProperties[k],
-    );
-    const eventProperties = keys.reduce(
-      (ret, key) => ({ ...ret, [key]: curr.eventProperties[key] }),
-      {},
-    );
-    return { ...curr, eventProperties };
-  }, []);
   const addItem = useCallback(
     item => {
       setItems(currentItems => [...currentItems.slice(-(limit - 1)), item]);
@@ -39,6 +21,5 @@ export default function useAnalyticsEventsLog(limit = 40) {
 
   return {
     items,
-    filter,
   };
 }
