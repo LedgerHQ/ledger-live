1ff73ff02653769c359536f8ef8e6db7b6e9205f
Trent Rogers <trentkrogers@gmail.com>
2023-01-19 12:54:57 -0600

    Update integration info drawer

--- apps/ledger-live-desktop/src/renderer/families/avalanchepchain/AccountSubHeader.jsx
+++ apps/ledger-live-desktop/src/renderer/families/avalanchepchain/AccountSubHeader.jsx
@@ -1,3 +1,6 @@
-// @flow
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader/index";
 
-export { default } from "./AccountSubHeader/AccountSubHeader";
+export default function AvalancheAccountSubHeader() {
+  return <AccountSubHeader family="Avalanche" team="Figment"></AccountSubHeader>;
+}
