31666465a5a03c631edc29315ad59ce543445426
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-19 08:59:45 +0100

    feat: BLE service enable: for both Android and iOS
    Hook:
     * exposing a method which checks the bluetooth service state and requests (if necessary) the user to enable it.
     * listening to an update of the bluetooth service state from BleTransport, and updates the state accordingly.
     * It can also indicate if bluetooth has enough permissions to be enabled, which is useful for iOS as we don't have an easy way to implement a useIosBluetoothPermissions currently.

--- apps/ledger-live-mobile/src/components/RequiresBLE/RequiresBluetoothEnabled.tsx
+++ apps/ledger-live-mobile/src/components/RequiresBLE/RequiresBluetoothEnabled.tsx
@@ -1,46 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { Observable } from "rxjs";
-import TransportBLE from "../../react-native-hw-transport-ble";
-import {
-  BluetoothPromptResult,
-  usePromptBluetoothCallbackWithState,
-} from "../../logic/usePromptBluetoothCallback";
+import React from "react";
+import { useEnableBluetooth } from "./hooks/useEnableBluetooth";
 import BluetoothDisabled from "./BluetoothDisabled";
 import BluetoothPermissionDenied from "./BluetoothPermissionDenied";
 
 type Props = {
   children?: React.ReactNode;
+  hasBackButtonOnError?: boolean;
+  openSettingsOnErrorButton?: boolean;
 };
 
-const RequiresBluetoothEnabled: React.FC<Props> = ({ children }) => {
-  const [type, setType] = useState<string>("Unknown");
+/**
+ * Renders an error if bluetooth is required but is not enabled.
+ * Otherwise renders children.
+ *
+ * Can be used for both Android and iOS.
+ *
+ * @param hasBackButtonOnError If true, the back button will be displayed on the permission denied or disabled error screens.
+ * Defaults to false.
+ * @param openSettingsOnErrorButton Used for debug purposes. If true, on a bluetooth service disabled, pressing the button on
+ *   the error component will make the user go to the settings. Otherwise it will try to prompt the user to enable their bluetooth
+ *   service if possible. Defaults to false.
+ */
+const RequiresBluetoothEnabled: React.FC<Props> = ({
+  children,
+  hasBackButtonOnError = false,
+  openSettingsOnErrorButton = false,
+}) => {
+  const { bluetoothEnableState, checkAndRequestAgain } = useEnableBluetooth();
 
-  const { prompt, bluetoothPromptedOnce, bluetoothPromptResult } =
-    usePromptBluetoothCallbackWithState();
-
-  useEffect(() => {
-    prompt();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    const sub = new Observable(TransportBLE.observeState).subscribe({
-      next: ({ type }: { type: string }) => setType(type),
-    });
-    return () => sub.unsubscribe();
-  }, []);
-
-  if (type === "Unknown" || !bluetoothPromptedOnce) return null; // suspense PLZ
-
-  if (bluetoothPromptResult === BluetoothPromptResult.UNAUTHORIZED) {
-    return <BluetoothPermissionDenied />;
+  switch (bluetoothEnableState) {
+    case "unknown":
+      // As long as we don't know the state of bluetooth, we don't render anything
+      return null;
+    case "enabled":
+      return <>{children}</>;
+    case "unauthorized":
+      return <BluetoothPermissionDenied hasBackButton={hasBackButtonOnError} />;
+    default:
+    case "disabled":
+      return (
+        <BluetoothDisabled
+          onRetry={checkAndRequestAgain}
+          hasBackButton={hasBackButtonOnError}
+          openSettings={openSettingsOnErrorButton}
+        />
+      );
   }
-
-  if (type === "PoweredOn") {
-    return <>{children}</>;
-  }
-
-  return <BluetoothDisabled onRetry={prompt} />;
 };
 
 export default RequiresBluetoothEnabled;
