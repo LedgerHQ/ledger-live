cd002f8c664d642a6c07b946f880cc6bfb02e2fa
sarneijim <sarneijim@gmail.com>
2023-01-23 20:11:33 +0000

    feat: add centralise stake flow

---
+++ apps/ledger-live-desktop/src/renderer/screens/stake/index.tsx
@@ -0,0 +1,92 @@
+import { useState, useEffect, useMemo } from "react";
+import SelectAccountAndCurrencyDrawer from "~/renderer/drawers/DataSelector/SelectAccountAndCurrencyDrawer";
+import { setDrawer } from "~/renderer/drawers/Provider";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { Account } from "@ledgerhq/types-live";
+import perFamilyManageActions from "~/renderer/generated/AccountHeaderManageActions";
+import { useHistory } from "react-router-dom";
+import { stakeDefaultTrack } from "./constants";
+import { track, page } from "~/renderer/analytics/segment";
+
+type Props = {
+  account?: Account;
+  parentAccount?: Account;
+};
+
+const StakeFlow = (props: Props) => {
+  const history = useHistory();
+  const [accountSelection, setAccountSelection] = useState({});
+  const { enabled: stakeFlag, list: flagList } = useFeature("stakePrograms");
+  const { account = {}, parentAccount = null } = accountSelection;
+  const family = account?.currency?.family || "ethereum";
+  const manage = perFamilyManageActions[family];
+
+  const familyManageActions = useMemo(() => {
+    return manage({ account, parentAccount });
+  }, [account, manage, parentAccount]);
+
+  const action = useMemo(() => {
+    const manageList =
+      familyManageActions && familyManageActions.length > 0 ? familyManageActions : [];
+    return manageList && manageList.find(item => item.key === "Stake");
+  }, [familyManageActions]);
+
+  useEffect(() => {
+    if (action && action.onClick) {
+      action.onClick();
+      history.push({
+        pathname: `/account/${account.id}`,
+      });
+    }
+  }, [account.id, action, history]);
+
+  const getStakeDrawer = () => {
+    page("Stake", "Drawer - Choose Asset", {
+      page: history.location.pathname,
+      ...stakeDefaultTrack,
+    });
+    setDrawer(
+      SelectAccountAndCurrencyDrawer,
+      {
+        currencies: flagList || [],
+        onAccountSelected: (account: Account, parentAccount: Account | null = null) => {
+          setDrawer();
+          setAccountSelection({ account, parentAccount });
+          track("button_clicked", {
+            button: "asset",
+            page: history.location.pathname,
+            currency: account?.currency?.family,
+            account,
+            parentAccount,
+            ...stakeDefaultTrack,
+          });
+        },
+      },
+      {
+        onRequestClose: () => {
+          setDrawer();
+          track("button_clicked", {
+            button: "close",
+            page: history.location.pathname,
+            currency: account?.currency?.family,
+            account,
+            parentAccount,
+            ...stakeDefaultTrack,
+          });
+        },
+      },
+    );
+  };
+
+  const getStakeFlow = () => {
+    if (props) {
+      setAccountSelection(props);
+    } else if (stakeFlag) {
+      getStakeDrawer();
+    }
+  };
+
+  return getStakeFlow;
+};
+
+export default StakeFlow;
