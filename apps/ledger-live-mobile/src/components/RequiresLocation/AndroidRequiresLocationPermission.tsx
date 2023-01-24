f9058032fb69e312a6b7dc59997219dcca1b5719
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-24 15:48:02 +0100

    chore: simplify android location permissions logic
    And clearer documentation

--- apps/ledger-live-mobile/src/components/RequiresLocation/AndroidRequiresLocationPermission.tsx
+++ apps/ledger-live-mobile/src/components/RequiresLocation/AndroidRequiresLocationPermission.tsx
@@ -9,36 +9,37 @@ type Props = {
 };
 
 /**
- * Android: Renders an error if location is required & its associated permissions are not
- * set. Otherwise renders children.
+ * Renders children if the permission for the location services is granted.
+ * Otherwise, tries to request the permission. And if the user denies, renders an error.
  *
  * Should only be used for Android.
  *
+ * @param children The children to render if bluetooth has its associated permissions granted
  * @param hasBackButtonOnDenied If true, the back button will be displayed on the permission denied screen. Defaults to false.
  * @param openSettingsOnErrorButton Used for debug purposes. If true, on a permission denied, pressing the button on
  *   the error component will make the user go to the settings. Otherwise it will try to prompt the user to allow permission
  *   if possible. Defaults to false.
  */
-const AndroidRequiresLocationPermissions: React.FC<Props> = ({
+const AndroidRequiresLocationPermission: React.FC<Props> = ({
   children,
   hasBackButtonOnDenied = false,
   openSettingsOnErrorButton = false,
 }) => {
-  const { renderChildren, hasPermission, neverAskAgain, requestAgain } =
+  const { hasPermission, neverAskAgain, requestForPermissionAgain } =
     useAndroidLocationPermission();
 
-  if (hasPermission === undefined) return null; // Prevents blink
+  if (hasPermission === "unknown") return null; // Prevents blink
 
-  return renderChildren ? (
+  return hasPermission === "granted" ? (
     <>{children}</>
   ) : (
     <LocationPermissionDenied
       neverAskAgain={neverAskAgain}
-      onRetry={requestAgain}
+      onRetry={requestForPermissionAgain}
       hasBackButton={hasBackButtonOnDenied}
       openSettings={openSettingsOnErrorButton}
     />
   );
 };
 
-export default AndroidRequiresLocationPermissions;
+export default AndroidRequiresLocationPermission;
