6b85956cea1081e89d8e0f0e892aa179869deb44
lvndry <lvndry@proton.me>
2023-01-24 13:58:51 +0100

    show message in account if pending transaction

--- apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
@@ -85,6 +85,7 @@ import { hasNoAccountsSelector } from "../../reducers/accounts";
 import { BaseNavigatorStackParamList } from "./types/BaseNavigator";
 import DeviceConnect from "../../screens/DeviceConnect";
 import ExploreTabNavigator from "./ExploreTabNavigator";
+import EditTransactionNavigator from "./EditTransactionNavigator";
 
 const Stack = createStackNavigator<BaseNavigatorStackParamList>();
 
@@ -592,6 +593,11 @@ export default function BaseNavigator() {
             }
           },
         })}
+      />
+      <Stack.Screen
+        name={NavigatorName.EthereumEditTransaction}
+        options={{ headerShown: false }}
+        component={EditTransactionNavigator}
       />
     </Stack.Navigator>
   );
