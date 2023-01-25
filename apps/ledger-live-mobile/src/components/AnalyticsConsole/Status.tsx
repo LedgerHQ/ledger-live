de1bf99157b069f8ae6746172cf0a929c2764d3c
ofreyssinet-ledger <91890529+ofreyssinet-ledger@users.noreply.github.com>
2023-01-25 10:56:45 +0100

    Feat/fat 788 LLM Custom lockscreen analytics + analytics console improvements (#2350)
    * wip
    
    * cleanup logs
    
    * add subscription cleanup in useAnalyticsEventsLog
    
    * add limit param in useAnalyticsEventsLog
    
    * Refactor DebugTheme: pull out reusable "FloatingDebugButton" component
    
    * Add event date at source, highlight last events
    
    * Fix initial position of FloatingDebugButton
    
    * Add visibility toggle with floating button, sort event properties
    
    * Add status indicator
    
    * Add possibility to toggle extra props for 1 event, fix wrapping
    
    * Add bottom toast + adjust colors
    
    * increase log size
    
    * wording
    
    * Add all events of CLS tracking plan
    
    * changesets
    
    * remove useless filter function
    
    * Fix event name for "Choose from my picture gallery"
    
    * Add slider to change the height of the transparent overlay
    
    * Replace all uses of components/Button in CLS
    
    * lint

---
+++ apps/ledger-live-mobile/src/components/AnalyticsConsole/Status.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Flex, Text } from "@ledgerhq/native-ui";
+import { useSelector } from "react-redux";
+import { getIsTracking } from "../../analytics";
+import { State } from "../../reducers/types";
+
+const trackingSelector = (state: State) => getIsTracking(state, false);
+
+const Status = () => {
+  const isTracking = useSelector(trackingSelector);
+  return (
+    <Flex>
+      <Text color="black">
+        Tracking status: {isTracking.enabled ? "enabled" : "disabled"}
+        {!isTracking.enabled ? ": " + isTracking.reason : ""}
+      </Text>
+    </Flex>
+  );
+};
+
+export default Status;
