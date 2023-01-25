ea1e1d2c2c2480d2d111498e8f939089b7a9c986
Bruno Spolidorio <bruno.bachega-spolidorio-ext@ledger.fr>
2023-01-25 16:00:28 +0000

    feat: add deeplinks

--- apps/ledger-live-desktop/src/renderer/modals/Stake/index.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/Stake/index.tsx
@@ -5,12 +5,13 @@ import { Icon, Text } from "@ledgerhq/react-ui";
 import { useRampCatalog } from "@ledgerhq/live-common/platform/providers/RampCatalogProvider/index";
 import { getAllSupportedCryptoCurrencyTickers } from "@ledgerhq/live-common/platform/providers/RampCatalogProvider/helpers";
 import { Account, AccountLike } from "~/../../../libs/ledgerjs/packages/types-live/lib";
-import { closeModal } from "~/renderer/actions/modals";
+import { closeModal, openModal } from "~/renderer/actions/modals";
 import { useProviders } from "~/renderer/screens/exchange/Swap2/Form";
 import Modal, { ModalBody } from "~/renderer/components/Modal";
 import Box from "~/renderer/components/Box";
 import EntryButton from "~/renderer/components/EntryButton/EntryButton";
 import CoinsIcon from "./assets/CoinsIcon";
+import { useHistory } from "react-router-dom";
 
 interface StakeModalProps {
   account: AccountLike;
@@ -20,6 +21,7 @@ interface StakeModalProps {
 const StakeModal = ({ account, parentAccount }: StakeModalProps) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const rampCatalog = useRampCatalog();
   const { providers, storedProviders } = useProviders();
@@ -41,8 +43,37 @@ const StakeModal = ({ account, parentAccount }: StakeModalProps) => {
   const currency = parentAccount?.currency || account.currency;
   const availableOnBuy = currency && onRampAvailableTickers.includes(currency.ticker.toUpperCase());
   const availableOnSwap = currency && swapAvailableIds.includes(currency.id);
+  const availableOnReceive = true;
 
   const modalName = "MODAL_STAKE";
+
+  const onBuy = useCallback(() => {
+    dispatch(closeModal(modalName));
+    history.push({
+      pathname: "/exchange",
+      state: {
+        currency: currency.id,
+        mode: "buy",
+      },
+    });
+  }, [currency, history, dispatch]);
+
+  const onSwap = useCallback(() => {
+    dispatch(closeModal(modalName));
+    history.push({
+      pathname: "/swap",
+      state: {
+        defaultCurrency: currency,
+        defaultAccount: account,
+        defaultParentAccount: parentAccount,
+      },
+    });
+  }, [currency, account, parentAccount, history, dispatch]);
+
+  const onReceive = useCallback(() => {
+    dispatch(closeModal(modalName));
+    dispatch(openModal("MODAL_RECEIVE", { parentAccount, account }));
+  }, [parentAccount, account, dispatch]);
 
   const onClose = useCallback(() => {
     dispatch(closeModal(modalName));
@@ -59,10 +90,7 @@ const StakeModal = ({ account, parentAccount }: StakeModalProps) => {
               <CoinsIcon />
             </Box>
             <Box textAlign="center" mb={16}>
-              <Text variant="subhead" fontWeight="semiBold">
-                {" "}
-                {t("stake.modal.text", { coin: currency.ticker })}
-              </Text>
+              <Text fontWeight="semiBold">{t("stake.modal.text", { coin: currency.ticker })}</Text>
             </Box>
             <Box textAlign="center" mb={24}>
               <Text fontSize={13} fontWeight="regular" color="neutral.c70">
@@ -70,14 +98,13 @@ const StakeModal = ({ account, parentAccount }: StakeModalProps) => {
               </Text>
             </Box>
             {availableOnBuy && (
-              <Box>
-                <EntryButton
-                  Icon={() => <Icon name="BuyCryptoAlt" />}
-                  title={t("stake.modal.options.buy.title")}
-                  body={t("stake.modal.options.buy.body")}
-                  showChevron
-                />
-              </Box>
+              <EntryButton
+                Icon={() => <Icon name="BuyCryptoAlt" />}
+                title={t("stake.modal.options.buy.title")}
+                body={t("stake.modal.options.buy.body")}
+                onClick={onBuy}
+                showChevron
+              />
             )}
             {availableOnSwap && (
               <EntryButton
@@ -85,15 +112,19 @@ const StakeModal = ({ account, parentAccount }: StakeModalProps) => {
                 title={t("stake.modal.options.swap.title")}
                 label={t("stake.modal.options.swap.label")}
                 body={t("stake.modal.options.swap.body")}
+                onClick={onSwap}
                 showChevron
               />
             )}
-            <EntryButton
-              Icon={() => <Icon name="ArrowToBottom" />}
-              title={t("stake.modal.options.receive.title")}
-              body={t("stake.modal.options.receive.body")}
-              showChevron
-            />
+            {availableOnReceive && (
+              <EntryButton
+                Icon={() => <Icon name="ArrowToBottom" />}
+                title={t("stake.modal.options.receive.title")}
+                body={t("stake.modal.options.receive.body")}
+                onClick={onReceive}
+                showChevron
+              />
+            )}
           </Box>
         )}
       />
