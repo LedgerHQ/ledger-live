55b3f1816be7ef642029a4fa05383a5e8261d182
cjordan-ledger <chris.jordan-ext@ledger.fr>
2023-01-25 14:01:59 +0000

    feat: links exchange buttons to correct places

--- apps/ledger-live-desktop/src/renderer/screens/dashboard/FeaturedButtons.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/dashboard/FeaturedButtons.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Grid } from "@ledgerhq/react-ui";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
@@ -6,6 +6,9 @@ import EntryButton from "~/renderer/components/EntryButton/EntryButton";
 import Swap from "~/renderer/icons/Swap";
 import Exchange from "~/renderer/icons/Exchange";
 import Growth from "~/renderer/icons/Growth";
+import { useHistory } from "react-router-dom";
+import useStakeFlow from "~/renderer/screens/stake";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 
 const ButtonGrid = styled(Grid).attrs(() => ({
   columns: 3,
@@ -18,7 +21,20 @@ const ButtonGrid = styled(Grid).attrs(() => ({
 const devFeatureFlag = false;
 
 const FeaturedButtons = () => {
+  const history = useHistory();
   const { t } = useTranslation();
+
+  const handleClickExchange = useCallback(() => {
+    history.push("/exchange");
+  }, [history]);
+
+  const handleClickSwap = useCallback(() => {
+    history.push("/swap");
+  }, [history]);
+
+  const stakeProgramsFeatureFlag = useFeature("stakePrograms");
+  const stakeDisabled = stakeProgramsFeatureFlag?.params.list.length === 0 ?? true;
+  const startStakeFlow = useStakeFlow({});
 
   if (!devFeatureFlag) return null;
 
@@ -28,27 +44,21 @@ const FeaturedButtons = () => {
         Icon={() => <Exchange />}
         title={t("dashboard.featuredButtons.buySell.title")}
         body={t("dashboard.featuredButtons.buySell.description")}
-        onClick={() => {
-          // to be implemented
-        }}
+        onClick={handleClickExchange}
       />
       <EntryButton
         Icon={() => <Swap />}
         title={t("dashboard.featuredButtons.swap.title")}
         body={t("dashboard.featuredButtons.swap.description")}
         label={t("dashboard.featuredButtons.swap.label")}
-        onClick={() => {
-          // to be implemented
-        }}
+        onClick={handleClickSwap}
       />
       <EntryButton
         Icon={() => <Growth />}
+        disabled={stakeDisabled}
         title={t("dashboard.featuredButtons.earn.title")}
         body={t("dashboard.featuredButtons.earn.description")}
-        disabled={true}
-        onClick={() => {
-          // to be implemented
-        }}
+        onClick={startStakeFlow}
       />
     </ButtonGrid>
   );
