a59bef14f2f47d57c517e57075b90deafad79f43
Prateek Rathod <prateek@zondax.ch>
2023-01-26 21:56:45 +0530

    refac(icp): improve bot tests

--- libs/ledger-live-common/src/families/internet_computer/specs.ts
+++ libs/ledger-live-common/src/families/internet_computer/specs.ts
@@ -7,7 +7,7 @@ import { getCryptoCurrencyById } from "../../currencies";
 import { genericTestDestination, pickSiblings } from "../../bot/specs";
 import type { AppSpec } from "../../bot/types";
 import { acceptTransaction } from "./speculos-deviceActions";
-import { ICP_FEES } from "./consts";
+// import { ICP_FEES } from "./consts";
 
 const MIN_SAFE = new BigNumber(10);
 const maxAccount = 6;
@@ -27,15 +27,12 @@ const internetComputerSpecs: AppSpec<Transaction> = {
   },
   mutations: [
     {
-      name: "Send 50%~",
+      name: "Send minimum",
       maxRun: 1,
       testDestination: genericTestDestination,
       transaction: ({ account, siblings, bridge }) => {
         const sibling = pickSiblings(siblings, maxAccount);
-        let amount = account.spendableBalance
-          .minus(ICP_FEES)
-          .div(0.5)
-          .integerValue();
+        let amount = MIN_SAFE;
 
         if (!sibling.used && amount.lt(MIN_SAFE)) {
           invariant(
@@ -58,23 +55,23 @@ const internetComputerSpecs: AppSpec<Transaction> = {
         };
       },
     },
-    {
-      name: "Transfer Max",
-      maxRun: 1,
-      transaction: ({ account, siblings, bridge }) => {
-        return {
-          transaction: bridge.createTransaction(account),
-          updates: [
-            {
-              recipient: pickSiblings(siblings, maxAccount).freshAddress,
-            },
-            {
-              useAllAmount: true,
-            },
-          ],
-        };
-      },
-    },
+    // {
+    //   name: "Transfer Max",
+    //   maxRun: 1,
+    //   transaction: ({ account, siblings, bridge }) => {
+    //     return {
+    //       transaction: bridge.createTransaction(account),
+    //       updates: [
+    //         {
+    //           recipient: pickSiblings(siblings, maxAccount).freshAddress,
+    //         },
+    //         {
+    //           useAllAmount: true,
+    //         },
+    //       ],
+    //     };
+    //   },
+    // },
   ],
 };
 
