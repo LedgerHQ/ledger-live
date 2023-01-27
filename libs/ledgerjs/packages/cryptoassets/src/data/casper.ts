68b73ba3d4c1d052cad0571e55312eeac935bbe6
Prateek Rathod <prateek@zondax.ch>
2023-01-27 21:30:56 +0530

    fix(casper): fix msc typos

--- libs/ledgerjs/packages/cryptoassets/src/data/casper.ts
+++ libs/ledgerjs/packages/cryptoassets/src/data/casper.ts
@@ -3,7 +3,7 @@ export type CasperToken = [string, string, string, string, number, boolean];
 const tokens: CasperToken[] = [
   // [assetCode, assetIssuer, asset type, name, precision, enableCountervalues]
   // Note: asset type is only used in Receive asset message and always should be
-  // "Stellar"
+  // "Casper"
   [
     "USDC",
     "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
