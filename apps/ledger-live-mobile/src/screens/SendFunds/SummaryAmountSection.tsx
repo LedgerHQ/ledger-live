3d6e1f4b816d27f90813d4713a28857b8ea8d27f
lvndry <lvndry@proton.me>
2023-01-31 08:16:05 +0100

    wip

--- apps/ledger-live-mobile/src/screens/SendFunds/SummaryAmountSection.tsx
+++ apps/ledger-live-mobile/src/screens/SendFunds/SummaryAmountSection.tsx
@@ -18,6 +18,7 @@ type Props = {
   amount: number | BigNumber;
   overrideAmountLabel?: string;
 };
+
 export default class SummaryAmountSection extends PureComponent<Props> {
   render() {
     const { account, amount, overrideAmountLabel } = this.props;
