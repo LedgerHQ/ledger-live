352600db6e4b32b26f53c5fc607a494d30903234
lvndry <lvndry@proton.me>
2023-01-31 17:57:53 +0100

    cancel transaction

--- apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
@@ -9,6 +9,7 @@ import { useTranslation } from "react-i18next";
 import { useTheme } from "styled-components/native";
 import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
 import { useSelector } from "react-redux";
+
 import { ScreenName, NavigatorName } from "../../const";
 import * as families from "../../families";
 import OperationDetails from "../../screens/OperationDetails";
@@ -76,7 +77,6 @@ import { hasNoAccountsSelector } from "../../reducers/accounts";
 import { BaseNavigatorStackParamList } from "./types/BaseNavigator";
 import DeviceConnect, { deviceConnectHeaderOptions } from "../../screens/DeviceConnect";
 import ExploreTabNavigator from "./ExploreTabNavigator";
-import NoFundsFlowNavigator from "./NoFundsFlowNavigator";
 import StakeFlowNavigator from "./StakeFlowNavigator";
 import { RecoverPlayer } from "../../screens/Protect/Player";
 import { RedirectToOnboardingRecoverFlowScreen } from "../../screens/Protect/RedirectToOnboardingRecoverFlow";
@@ -87,6 +87,7 @@ import {
 } from "../NavigationHeaderCloseButton";
 import { RedirectToRecoverStaxFlowScreen } from "../../screens/Protect/RedirectToRecoverStaxFlow";
 import EditTransactionNavigator from "./EditTransactionNavigator";
+import NoFundsFlowNavigator from "./NoFundsFlowNavigator";
 
 const Stack = createStackNavigator<BaseNavigatorStackParamList>();
 
--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -74,7 +74,7 @@ export enum ScreenName {
   EditAccountName = "EditAccountName",
   EditAccountUnits = "EditAccountUnits",
   EditDeviceName = "EditDeviceName",
-  EditTransactionOptions = "EditTransactionOptions",
+  EditEthereumTransactionMethodSelection = "EditEthereumTransactionMethodSelection",
   Exchange = "Exchange",
   ExchangeBuy = "ExchangeBuy",
   ExchangeHistory = "ExchangeHistory",
