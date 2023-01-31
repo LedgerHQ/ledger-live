cea8e93d1cfc1e8bc1c01ab885f0609d97ba13c1
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-31 11:57:48 +0100

    feat: debug screen for useRequireBluetooth and associated drawer

--- apps/ledger-live-mobile/src/screens/Settings/Debug/Debugging/BluetoothAndLocationServices.tsx
+++ apps/ledger-live-mobile/src/screens/Settings/Debug/Debugging/BluetoothAndLocationServices.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Flex, Alert, Switch } from "@ledgerhq/native-ui";
 import NavigationScrollView from "../../../../components/NavigationScrollView";
 import RequiresBLE from "../../../../components/RequiresBLE";
+import { useRequireBluetooth } from "../../../../components/RequiresBLE/hooks/useRequireBluetooth";
+import RequiresBluetoothDrawer from "../../../../components/RequiresBLE/RequiresBluetoothDrawer";
 
 /**
  * Debugging screen to test:
@@ -10,13 +11,47 @@ import RequiresBLE from "../../../../components/RequiresBLE";
  * - reaction on enabling/disabling the bluetooth service
  * - for Android: permission of the location service
  * - for Android: reaction on enabling/disabling the location services
+ *
+ * 2 types of requirements enforcement strategy are available:
+ * - use the RequiresBLE component
+ * - use the useRequireBluetooth hook with the RequiresBluetoothDrawer component
  */
 export default function DebugBluetoothAndLocationServices() {
   const [openSettings, setOpenSettings] = useState(false);
+  const [useCustomizedRequirements, setUseCustomizedRequirements] =
+    useState(true);
+  const [isBleRequired, setIsBleRequired] = useState(false);
+  const [doesBleNeedScanning, setDoesBleNeedScanning] = useState(false);
+
+  const isRequireCustomizedBluetoothEnabled =
+    useCustomizedRequirements && isBleRequired;
+
+  // Makes sure we are not using the custom requirements hook and the RequiresBLE component
+  useEffect(() => {
+    if (!useCustomizedRequirements) {
+      setIsBleRequired(false);
+    }
+  }, [useCustomizedRequirements, isBleRequired]);
+
+  const {
+    bluetoothRequirementsState,
+    retryRequestOnIssue,
+    cannotRetryRequest,
+  } = useRequireBluetooth({
+    requiredFor: doesBleNeedScanning ? "scanning" : "connecting",
+    isHookEnabled: isRequireCustomizedBluetoothEnabled,
+  });
 
   return (
     <NavigationScrollView>
-      <Flex style={styles.root}>
+      <Flex p="2">
+        <Switch
+          checked={useCustomizedRequirements}
+          onChange={val => setUseCustomizedRequirements(val)}
+          label={
+            "Use customizable bluetooth requirements - a drawer is used to display issues"
+          }
+        />
         <Switch
           checked={openSettings}
           onChange={val => setOpenSettings(val)}
@@ -24,24 +59,61 @@ export default function DebugBluetoothAndLocationServices() {
             "Open settings: on a permission denied or service disabled, go to settings and does not prompt the user"
           }
         />
-        <Flex mt={5}>
-          <RequiresBLE
-            hasBackButtonOnError={false}
-            openSettingsOnErrorButton={openSettings}
-          >
-            <Alert
-              type="info"
-              title="We have all the permissions for the services and they are enabled ✅"
+        {useCustomizedRequirements ? (
+          <>
+            <Switch
+              checked={isBleRequired}
+              onChange={val => setIsBleRequired(val)}
+              label={`Need bluetooth ${isBleRequired ? "🔵" : "🔴"}`}
             />
-          </RequiresBLE>
-        </Flex>
+            <Switch
+              checked={doesBleNeedScanning}
+              onChange={val => setDoesBleNeedScanning(val)}
+              label={`Need bluetooth for scanning ? ${
+                doesBleNeedScanning ? "📡" : "🗣"
+              }`}
+            />
+
+            <Flex mt={5}>
+              <RequiresBluetoothDrawer
+                bluetoothRequirementsState={bluetoothRequirementsState}
+                retryRequestOnIssue={retryRequestOnIssue}
+                isOpen={isRequireCustomizedBluetoothEnabled}
+                cannotRetryRequest={cannotRetryRequest}
+              />
+
+              {bluetoothRequirementsState === "all_respected" ? (
+                <Alert
+                  type="info"
+                  title="We have all the permissions for the services and they are enabled ✅"
+                />
+              ) : bluetoothRequirementsState === "unknown" ? (
+                <Alert
+                  type="info"
+                  title="Unknwon state on permissions/services 🤷"
+                />
+              ) : (
+                <Alert
+                  type="error"
+                  title="Missing some permissions/services ..."
+                />
+              )}
+            </Flex>
+          </>
+        ) : (
+          <Flex mt={5}>
+            <RequiresBLE
+              hasBackButtonOnError={false}
+              openSettingsOnErrorButton={openSettings}
+            >
+              <Alert
+                type="info"
+                title="We have all the permissions for the services and they are enabled ✅"
+              />
+            </RequiresBLE>
+          </Flex>
+        )}
       </Flex>
     </NavigationScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  root: {
-    padding: 16,
-  },
-});
