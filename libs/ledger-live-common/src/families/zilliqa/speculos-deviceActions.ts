161ad6f31779b53efc64551041f9743aceb1d3a0
Troels F. Rønnow <troels@wonop.com>
2023-01-31 15:56:59 +0100

    Updating bot tests

--- libs/ledger-live-common/src/families/zilliqa/speculos-deviceActions.ts
+++ libs/ledger-live-common/src/families/zilliqa/speculos-deviceActions.ts
@@ -2,12 +2,20 @@ import type { DeviceAction } from "../../bot/types";
 import type { Transaction } from "./types";
 import { deviceActionFlow /*, formatDeviceAmount*/ } from "../../bot/specs";
 
-export const acceptTransaction: DeviceAction<Transaction, any> =
-  deviceActionFlow({
-    steps: [
-      {
-        title: "Value",
-        button: "Rr",
-      },
-    ],
-  });
+export const acceptTransaction: DeviceAction<
+  Transaction,
+  any
+> = deviceActionFlow({
+  steps: [
+    {
+      title: "Sign Txn",
+      button: "Rr",
+    },
+    { title: "To (1/3)", button: "Rr" },
+    { title: "To (2/3)", button: "Rr" },
+    { title: "To (3/3)", button: "Rr" },
+    { title: "Amount (ZIL)", button: "Rr" },
+    { title: "Gasprice (ZIL)", button: "Rr" },
+    { title: "Sign", button: "LRlr" },
+  ],
+});
