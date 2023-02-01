12017d14a778d3221f33cf601cae29352b6ea160
henrily-ledger <33897120+henrily-ledger@users.noreply.github.com>
2023-02-01 14:52:33 +0100

    Update libs/ledger-live-common/src/naming-service/api.ts
    Co-authored-by: Landry Monga <lvndry@protonmail.com>

--- libs/ledger-live-common/src/naming-service/api.ts
+++ libs/ledger-live-common/src/naming-service/api.ts
@@ -5,7 +5,7 @@ import network from "../network";
 export const getAddressByName = async (name: string): Promise<string> => {
   const currency = getCryptoCurrencyById("ethereum");
   const url = `${blockchainBaseURL(currency)}/ens/resolve/${name}`;
-  const address = (await network({ method: "GET", url })).data;
+  const { data: address } = await network({ method: "GET", url });
 
   return address;
 };
