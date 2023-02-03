e26155d3b2f9611923b1008c928f9fb1fc18f351
sarneijim <38540290+sarneijim@users.noreply.github.com>
2023-02-03 17:49:20 +0000

    fix: fix deeplink problem launching lld (#2523)

--- apps/ledger-live-desktop/src/renderer/screens/platform/App.jsx
+++ apps/ledger-live-desktop/src/renderer/screens/platform/App.jsx
@@ -7,7 +7,9 @@ import { Card } from "~/renderer/components/Box";
 import WebPlatformPlayer from "~/renderer/components/WebPlatformPlayer";
 import { languageSelector } from "~/renderer/reducers/settings";
 import { useSelector } from "react-redux";
-import { useGetManifest } from "./utils";
+import { useRemoteLiveAppManifest } from "@ledgerhq/live-common/platform/providers/RemoteLiveAppProvider/index";
+import { useLocalLiveAppManifest } from "@ledgerhq/live-common/platform/providers/LocalLiveAppProvider/index";
+import { getCustomDappUrl } from "./utils";
 
 type Props = {
   match: {
@@ -51,7 +53,14 @@ export default function PlatformApp({ match, appId: propsAppId, location }: Prop
     ...internalParams,
   };
 
-  const manifest = useGetManifest(appId, params, pathname);
+  const localManifest = useLocalLiveAppManifest(appId);
+  const remoteManifest = useRemoteLiveAppManifest(appId);
+  let manifest = localManifest || remoteManifest;
+
+  const customDappUrl = getCustomDappUrl(manifest, appId, params, pathname);
+  if (customDappUrl) {
+    manifest = { ...manifest, params: { ...manifest.params, dappUrl: customDappUrl } };
+  }
 
   // TODO for next urlscheme evolutions:
   // - check if local settings allow to launch an app from this branch, else display an error
