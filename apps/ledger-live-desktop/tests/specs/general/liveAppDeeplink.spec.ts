7eb2e178e5adc5b26d558043d4941e39cdbd2beb
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-02-03 11:58:24 +0100

    tidied up test

--- apps/ledger-live-desktop/tests/specs/general/liveAppDeeplink.spec.ts
+++ apps/ledger-live-desktop/tests/specs/general/liveAppDeeplink.spec.ts
@@ -3,10 +3,10 @@ import { expect } from "@playwright/test";
 
 test.use({
   userdata: "1AccountBTC1AccountETH",
-  // deeplink: "ledgerlive://discover/lido"
-  deeplink: "ledgerlive://receive?currency=btc",
+  deeplink: "ledgerlive://discover/lido",
+  // deeplink: "ledgerlive://receive?currency=btc",
 });
 
 test("Can open correct live app via a deeplink", async ({ page }) => {
-  await page.pause();
+  expect(await page.textContent("div")).toContain("Lido");
 });
