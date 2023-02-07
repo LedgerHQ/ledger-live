48d52f7e30fc3483b441dd79ac3fc84066b537e2
nparigi-ledger <nicolas.parigi@ledger.fr>
2023-02-07 18:27:50 +0100

    discover protect : add analytics

--- apps/ledger-live-desktop/src/renderer/modals/ProtectDiscover/ProtectDiscoverBody.jsx
+++ apps/ledger-live-desktop/src/renderer/modals/ProtectDiscover/ProtectDiscoverBody.jsx
@@ -1,19 +1,18 @@
 // @flow
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useTheme } from "styled-components";
 import { Flex, Button as BaseButton, Text, Tag, Icons } from "@ledgerhq/react-ui";
 import styled from "@ledgerhq/react-ui/components/styled";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 
 import { ModalBody } from "~/renderer/components/Modal";
-import TrackPage from "~/renderer/analytics/TrackPage";
 import { withV3StyleProvider } from "~/renderer/styles/StyleProviderV3";
 import GetItOnGooglePlayImage from "./images/get_it_on_google_play.png";
 import GetItOnAppleStoreImage from "./images/get_it_on_apple_store.png";
 import QrCodeLLMImages from "./images/QRcode_LLM.png";
 import { openURL } from "~/renderer/linking";
 import { urls } from "~/config/urls";
+import { track } from "~/renderer/analytics/segment";
 
 type Props = {
   onClose: () => void,
@@ -29,16 +28,18 @@ const ProtectDiscoverBody = ({ onClose }: Props) => {
 
   const onAppStoreClick = () => openURL(urls.ledgerLiveMobile.appStore);
   const onPlayStoreClick = () => openURL(urls.ledgerLiveMobile.playStore);
-  const onDiscoverClick = () =>
+  const onDiscoverClick = () => {
+    track("button_clicked", {
+      button: "Discover the benefits",
+    });
     openURL(protectServicesDiscoverDesktopFeature?.params?.discoverTheBenefitsLink);
-
+  };
   return (
     <ModalBody
       onClose={onClose}
       p={1}
       render={() => (
         <Flex flexDirection={"column"} px={7}>
-          <TrackPage category="Modal" name="ReleaseNotes" />
           <Tag active type="plain" size={"medium"} alignSelf={"center"} mb={8}>
             {t("discoverProtect.new")}
           </Tag>
