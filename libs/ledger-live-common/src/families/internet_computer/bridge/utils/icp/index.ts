954bca198e30dcb17b3306248bfaa3d61339d349
Prateek Rathod <prateek@zondax.ch>
2023-02-08 15:55:16 +0530

    chore(icp): post rebase fixes/refac

--- libs/ledger-live-common/src/families/internet_computer/bridge/utils/icp/index.ts
+++ libs/ledger-live-common/src/families/internet_computer/bridge/utils/icp/index.ts
@@ -74,7 +74,7 @@ export const signICPTransaction = async ({
   signatures: { txnSig: string; readSig: string };
   signedTxn: string;
 }> => {
-  const icp = new ICP(transport);
+  const icp = new ICP(transport as any);
   const decodedTxn: any = Cbor.decode(Buffer.from(unsignedTxn, "hex"));
   const txnReqFromCbor = decodedTxn.updates[0][1];
   const expiry = new ingressExpiry(
