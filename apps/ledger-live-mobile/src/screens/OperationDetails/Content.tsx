89bb1af74c7ff3399cd028da7a4a4739cd4b0122
lvndry <lvndry@proton.me>
2023-02-10 16:15:41 +0100

    add optimistic operation timeout in env variables

--- apps/ledger-live-mobile/src/screens/OperationDetails/Content.tsx
+++ apps/ledger-live-mobile/src/screens/OperationDetails/Content.tsx
@@ -27,6 +27,7 @@ import {
 import { NFTResource } from "@ledgerhq/live-common/nft/NftMetadataProvider/types";
 import { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { Box, Flex, Link } from "@ledgerhq/native-ui";
+import { getEnv } from "@ledgerhq/live-common/env";
 
 import { NavigatorName, ScreenName } from "../../const";
 import LText from "../../components/LText";
@@ -53,8 +54,6 @@ import type {
   StackNavigatorNavigation,
 } from "../../components/RootNavigator/types/helpers";
 import type { BaseNavigatorStackParamList } from "../../components/RootNavigator/types/BaseNavigator";
-
-const FIVE_MINUTES = 5 * 60 * 1000;
 
 type HelpLinkProps = {
   event: string;
@@ -159,7 +158,8 @@ export default function Content({
   );
 
   const isOperationStuck =
-    operation.date.getTime() <= new Date().getTime() - FIVE_MINUTES;
+    operation.date.getTime() <=
+    new Date().getTime() - getEnv("ETHEREUM_STUCK_TRANSACTION_TIMEOUT");
 
   const isEditable =
     mainAccount.currency.family === "ethereum" &&
