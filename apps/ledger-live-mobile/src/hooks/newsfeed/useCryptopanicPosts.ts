e3e94e23fc3d65acec38115735e16a01b2080f55
nparigi-ledger <nicolas.parigi@ledger.fr>
2023-02-13 15:09:06 +0100

    depecrate old button component add useCryptopanicPosts hook

--- apps/ledger-live-mobile/src/hooks/newsfeed/useCryptopanicPosts.ts
+++ apps/ledger-live-mobile/src/hooks/newsfeed/useCryptopanicPosts.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   CryptopanicGetParams,
   CryptopanicNewsWithMetadata,
@@ -12,38 +12,43 @@ export async function useCryptopanicPosts(params: CryptopanicGetParams) {
   const [currentPage, setCurrentPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
 
-  const getCryptoPanicPosts = async ({ page = 1, concatPosts = false }) => {
-    try {
-      const apiResult = await getPosts({ ...params, page });
-      if (concatPosts) {
-        setPosts(currentPosts => currentPosts.concat(apiResult.results));
-      } else {
-        setPosts(apiResult.results);
+  const getCryptoPanicPosts = useCallback(
+    async ({ page = 1, concatPosts = false }) => {
+      try {
+        setIsLoading(true);
+        const apiResult = await getPosts({ ...params, page });
+        if (concatPosts) {
+          setPosts(currentPosts => currentPosts.concat(apiResult.results));
+        } else {
+          setPosts(apiResult.results);
+        }
+        setHasMore(!!apiResult.next);
+        setCurrentPage(page);
+      } catch (e) {
+        // handle error
       }
-      setHasMore(!!apiResult.next);
-      setCurrentPage(page);
-    } catch (e) {
-      // handle error
-    }
-    setIsLoading(false);
-    setReady(true);
-  };
+      setIsLoading(false);
+      setReady(true);
+    },
+    // maybe spread params object to array to reduce re-render
+    [params],
+  );
 
   // Init
   useEffect(() => {
     if (ready || isLoading) return;
     getCryptoPanicPosts({ page: 1 });
-  }, []);
+  }, [getCryptoPanicPosts, isLoading, ready]);
 
-  const refresh = () => {
+  const refresh = useCallback(async () => {
     if (!ready || isLoading) return;
-    getCryptoPanicPosts({ page: 1 });
-  };
+    await getCryptoPanicPosts({ page: 1 });
+  }, [getCryptoPanicPosts, isLoading, ready]);
 
-  const loadMore = () => {
+  const loadMore = useCallback(async () => {
     if (!ready || isLoading || !hasMore) return;
-    getCryptoPanicPosts({ page: currentPage + 1, concatPosts: true });
-  };
+    await getCryptoPanicPosts({ page: currentPage + 1, concatPosts: true });
+  }, [currentPage, getCryptoPanicPosts, hasMore, isLoading, ready]);
 
   return { posts, hasMore, ready, isLoading, refresh, loadMore };
 }
