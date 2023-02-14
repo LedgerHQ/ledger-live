2f360dc57ee4d500e6d8c5f42670c023d192b71e
cjordan-ledger <chris.jordan-ext@ledger.fr>
2023-02-14 14:42:45 +0000

    chore: converts usePageState to typescript

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/hooks/usePageState.ts
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/hooks/usePageState.ts
@@ -1,8 +1,12 @@
-// @flow
-
 import { useEffect, useState } from "react";
+import {
+  RatesReducerState,
+  SwapTransactionType,
+} from "@ledgerhq/live-common/lib/exchange/swap/types";
 
 type PageState = "initial" | "empty" | "loading" | "loaded";
+
+type SwapError = SwapTransactionType["fromAmountError"] | RatesReducerState["error"];
 
 // when the user first lands on this screen, they are seeing the initial state
 // when the user fetches data, they are seeing the loading state
@@ -11,7 +15,10 @@ type PageState = "initial" | "empty" | "loading" | "loaded";
 // when the user fetches data and there is an error, they see the empty state
 // when the user fetches data and there is no data, they see the empty state
 // when the user resets their from search, they see the initial state
-const usePageState = (swapTransaction, swapError): PageState => {
+const usePageState = (
+  swapTransaction: SwapTransactionType,
+  swapError: SwapError | undefined,
+): PageState => {
   const [pageState, setPageState] = useState("initial");
   const fromFieldIsZero = swapTransaction.swap.from.amount?.isZero() ?? true;
   const isDataLoading =
