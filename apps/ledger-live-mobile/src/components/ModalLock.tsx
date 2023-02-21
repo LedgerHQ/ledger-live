3b49215a5d9533e0f0442ddcb69fac169b3b42e8
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-02-21 15:49:58 +0100

    chore: reset and set modal lock on react-navigation focus changes

--- apps/ledger-live-mobile/src/components/ModalLock.tsx
+++ apps/ledger-live-mobile/src/components/ModalLock.tsx
@@ -1,24 +1,34 @@
-import { useEffect } from "react";
+import { useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
 import { setModalLock } from "../actions/appstate";
 
 /**
+ * If mounted, lock all drawers using the modalLock redux app state (components/QueuedDrawer)
+ *
+ * The modal lock is reset when loosing the react-navigation screen focus.
+ *
+ * Used in some DeviceAction displayed in drawers:
  * All steps of a DeviceAction that have an ongoing operation on
  * the device should disable the closing of the modal. If we are
  * transfering something (installing an app, a language, CLS, etc)
  * we don't want to break the flow.
  */
-const LockModal = () => {
+const ModalLock = () => {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(setModalLock(true));
-    return () => {
-      dispatch(setModalLock(false));
-    };
-  }, [dispatch]);
+  // Triggered when the screen gets the (react-navigation) focus
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(setModalLock(true));
+
+      return () => {
+        dispatch(setModalLock(false));
+      };
+    }, [dispatch]),
+  );
 
   return null;
 };
 
-export default LockModal;
+export default ModalLock;
