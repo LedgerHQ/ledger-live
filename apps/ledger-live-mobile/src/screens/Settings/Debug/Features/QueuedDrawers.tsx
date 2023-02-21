5b311a84a22a4aacdb24255c982442d3c22573f8
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-02-21 15:52:57 +0100

    chore: added modal lock in queued drawers debug screen

--- apps/ledger-live-mobile/src/screens/Settings/Debug/Features/QueuedDrawers.tsx
+++ apps/ledger-live-mobile/src/screens/Settings/Debug/Features/QueuedDrawers.tsx
@@ -8,6 +8,8 @@ import { SettingsNavigatorStackParamList } from "../../../../components/RootNavi
 import { ScreenName } from "../../../../const";
 import { setDebugAppLelevelDrawerOpened } from "../../../../actions/settings";
 import { debugAppLevelDrawerOpenedSelector } from "../../../../reducers/settings";
+import LockModal from "../../../../components/ModalLock";
+import NavigationScrollView from "../../../../components/NavigationScrollView";
 
 /**
  * Debugging screen to test:
@@ -40,6 +42,9 @@ export default function DebugQueuedDrawers() {
 
   const [isDrawer6AOpen, setIsDrawer6AOpen] = useState(false);
   const [isDrawer6BOpen, setIsDrawer6BOpen] = useState(false);
+
+  const [areDrawersLockedStarted, setAreDrawersLockedStarted] = useState(false);
+  const [areDrawersLocked, setAreDrawersLocked] = useState(false);
 
   // Handles 4th drawer opening after x seconds
   useEffect(() => {
@@ -93,6 +98,27 @@ export default function DebugQueuedDrawers() {
     };
   }, [isNavigatingAfter3s, navigation]);
 
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+
+    if (areDrawersLockedStarted) {
+      setAreDrawersLocked(true);
+
+      timeoutId = setTimeout(() => {
+        setAreDrawersLocked(false);
+        setAreDrawersLockedStarted(false);
+      }, 10000);
+    }
+
+    return () => {
+      if (timeoutId) clearTimeout(timeoutId);
+      if (areDrawersLockedStarted) {
+        setAreDrawersLockedStarted(false);
+        setAreDrawersLocked(false);
+      }
+    };
+  }, [areDrawersLockedStarted]);
+
   const handleDrawer1Close = useCallback(() => {
     setIsDrawer1Open(false);
   }, []);
@@ -133,110 +159,122 @@ export default function DebugQueuedDrawers() {
 
   return (
     <>
-      <Flex p="4">
-        <Flex mb="2">
-          <Switch
-            checked={isDrawer1Open}
-            onChange={val => setIsDrawer1Open(val)}
-            label={"Open the 1st drawer"}
+      <NavigationScrollView>
+        <Flex p="4">
+          <Flex mb="2">
+            <Switch
+              checked={isDrawer1Open}
+              onChange={val => setIsDrawer1Open(val)}
+              label={"Open the 1st drawer"}
+            />
+          </Flex>
+          <Flex mb="2">
+            <Switch
+              checked={isDrawer2Open}
+              onChange={val => {
+                setIsDrawer1Open(val);
+                setIsDrawer2Open(val);
+              }}
+              label={"Open the 1st then 2nd drawers"}
+            />
+          </Flex>
+          <Flex mb="2">
+            <Switch
+              checked={isDrawer3Open}
+              onChange={val => {
+                setIsDrawer1Open(val);
+                setIsDrawer2Open(val);
+                setIsDrawer3Open(val);
+              }}
+              label={"Open the 1st then 2nd then 3rd drawers"}
+            />
+          </Flex>
+          <Flex mt="6" mb="6">
+            <Switch
+              checked={isDrawer4Started}
+              onChange={val => {
+                setIsDrawer4Started(val);
+              }}
+              label={
+                "Open a 4th drawer after 3sec that adds itself to the queue of drawers, without forcing 🧑‍🎓"
+              }
+            />
+          </Flex>
+          <Flex mb="6">
+            <Switch
+              checked={isDrawer5Started}
+              onChange={val => {
+                setIsDrawer5Started(val);
+              }}
+              label={
+                "Open a 5th drawer after 3sec that forces all the other opened drawers to close ⚔️"
+              }
+            />
+          </Flex>
+          <Flex mb="6">
+            <Switch
+              checked={isDebugAppLevelDrawerOpened}
+              onChange={handleDebugAppLevelDrawerOpenedChange}
+              label={
+                "Open a drawer equivalent to the notification or ratings drawer 📲"
+              }
+            />
+          </Flex>
+          <Flex mb="6">
+            <Switch
+              checked={isDrawer6AOpen || isDrawer6BOpen}
+              onChange={val => {
+                // Only opens the 1st drawer, the 2nd one will be opened on user close
+                setIsDrawer6AOpen(val);
+              }}
+              label={
+                "Open a 1st drawer, and on user close, open a 2nd drawer after closing the 1st one. To test on iOS"
+              }
+            />
+          </Flex>
+          <Flex mb="6">
+            <Switch
+              checked={areDrawersLockedStarted}
+              onChange={val => {
+                setAreDrawersLockedStarted(val);
+              }}
+              label={`Lock all drawer for 10sec, then unlock them: currently ${
+                areDrawersLocked ? "locked 🔒" : "unlocked 🔓"
+              }`}
+            />
+          </Flex>
+          <Alert
+            type="info"
+            title="Hey 🧙‍♀️ Test successive opening/closing of drawers !"
           />
-        </Flex>
-        <Flex mb="2">
-          <Switch
-            checked={isDrawer2Open}
-            onChange={val => {
-              setIsDrawer1Open(val);
-              setIsDrawer2Open(val);
-            }}
-            label={"Open the 1st then 2nd drawers"}
-          />
-        </Flex>
-        <Flex mb="2">
-          <Switch
-            checked={isDrawer3Open}
-            onChange={val => {
-              setIsDrawer1Open(val);
-              setIsDrawer2Open(val);
-              setIsDrawer3Open(val);
-            }}
-            label={"Open the 1st then 2nd then 3rd drawers"}
-          />
-        </Flex>
-        <Flex mt="6" mb="6">
-          <Switch
-            checked={isDrawer4Started}
-            onChange={val => {
-              setIsDrawer4Started(val);
-            }}
-            label={
-              "Open a 4th drawer after 3sec that adds itself to the queue of drawers, without forcing 🧑‍🎓"
-            }
-          />
-        </Flex>
-        <Flex mb="6">
-          <Switch
-            checked={isDrawer5Started}
-            onChange={val => {
-              setIsDrawer5Started(val);
-            }}
-            label={
-              "Open a 5th drawer after 3sec that forces all the other opened drawers to close ⚔️"
-            }
-          />
-        </Flex>
-        <Flex mb="6">
-          <Switch
-            checked={isDebugAppLevelDrawerOpened}
-            onChange={handleDebugAppLevelDrawerOpenedChange}
-            label={
-              "Open a drawer equivalent to the notification or ratings drawer 📲"
-            }
-          />
-        </Flex>
-        <Flex mb="6">
-          <Switch
-            checked={isDrawer6AOpen || isDrawer6BOpen}
-            onChange={val => {
-              // Only opens the 1st drawer, the 2nd one will be opened on user close
-              setIsDrawer6AOpen(val);
-            }}
-            label={
-              "Open a 1st drawer, and on user close, open a 2nd drawer after closing the 1st one. To test on iOS"
-            }
-          />
-        </Flex>
-        <Alert
-          type="info"
-          title="Hey 🧙‍♀️ Test successive opening/closing of drawers !"
-        />
-        <Flex mt="8">
-          <Text>
-            Also, try navigating to a new screen by clicking on one of the
-            buttons below and come back here with the back arrow. To check if
-            the drawers are still working.
-          </Text>
+          <Flex mt="8">
+            <Text>
+              Also, try navigating to a new screen by clicking on one of the
+              buttons below and come back here with the back arrow. To check if
+              the drawers are still working.
+            </Text>
 
-          <Button
-            type="main"
-            onPress={() => navigation.navigate(ScreenName.DebugLottie)}
-            mt="8"
-            size="small"
-            outline
-          >
-            Navigate somewhere 🧞‍♂️
-          </Button>
-          <Button
-            type="main"
-            onPress={() => setIsNavigatingAfter3s(true)}
-            mt="8"
-            size="small"
-            outline
-          >
-            Navigate somewhere after 3s ⏳
-          </Button>
+            <Button
+              type="main"
+              onPress={() => navigation.navigate(ScreenName.DebugLottie)}
+              mt="8"
+              size="small"
+              outline
+            >
+              Navigate somewhere 🧞‍♂️
+            </Button>
+            <Button
+              type="main"
+              onPress={() => setIsNavigatingAfter3s(true)}
+              mt="8"
+              size="small"
+              outline
+            >
+              Navigate somewhere after 3s ⏳
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
-
+      </NavigationScrollView>
       <QueuedDrawer
         isRequestingToBeOpened={isDrawer1Open}
         onClose={handleDrawer1Close}
@@ -309,6 +347,8 @@ export default function DebugQueuedDrawers() {
           <Alert type="info" title="Drawer B 🥳" />
         </Flex>
       </QueuedDrawer>
+
+      {areDrawersLocked && <LockModal />}
     </>
   );
 }
