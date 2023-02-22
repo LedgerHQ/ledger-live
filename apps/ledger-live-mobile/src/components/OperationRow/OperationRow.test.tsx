81150d5aa49d7d1cf9f5ba340668bc7be75e4dfa
elbywan <julien.el-baz@ledger.fr>
2023-02-22 11:58:47 +0100

    hore(llm): fix jest setup

--- apps/ledger-live-mobile/src/components/OperationRow/OperationRow.test.tsx
+++ apps/ledger-live-mobile/src/components/OperationRow/OperationRow.test.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import * as React from "react";
 import { Account, Operation } from "@ledgerhq/types-live";
 import { getCryptoCurrencyById } from "@ledgerhq/live-common/currencies/index";
 import BigNumber from "bignumber.js";
 import { render } from "@testing-library/react-native";
 
 import OperationRow from ".";
+
+jest.mock("react-native-localize", () => ({
+  getTimeZone: jest.fn(),
+  getLocales: jest.fn(),
+  getNumberFormatSettings: jest.fn(),
+  getCalendar: jest.fn(),
+  getCountry: jest.fn(),
+  getTemperatureUnit: jest.fn(),
+  getFirstWeekDay: jest.fn(),
+  uses24HourClock: jest.fn(),
+  findBestAvailableLanguage: jest.fn(),
+}));
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+}));
+jest.mock("react-native-gesture-handler", () => ({
+  GestureHandlerRootView: jest.fn(),
+  ScrollView: jest.fn(),
+  RectButton: jest.fn(),
+  TouchableOpacity: jest.fn(),
+  TouchableWithoutFeedback: jest.fn(),
+  TouchableHighlight: jest.fn(),
+  Swipeable: jest.fn(),
+}));
+jest.mock("react-native-version-number", () => ({
+  appVersion: "1.0.0",
+  buildVersion: "1",
+}));
+jest.mock("react-native-startup-time", () => ({
+  getStartupTime: jest.fn(),
+}));
 
 const mockedOperation: Operation = {
   accountId: "js:1:ethereum:0xaccount",
