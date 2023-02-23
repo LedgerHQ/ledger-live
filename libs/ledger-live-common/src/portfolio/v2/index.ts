94d9c67924140e1ca4888fb2caf35b6f0d17753d
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2023-02-23 19:32:37 +0100

    fix(coin): add missing jest config file

--- libs/ledger-live-common/src/portfolio/v2/index.ts
+++ libs/ledger-live-common/src/portfolio/v2/index.ts
@@ -6,10 +6,7 @@ import {
   getAccountHistoryBalances,
 } from "../../account";
 import { getEnv } from "../../env";
-import {
-  getPortfolioRangeConfig,
-  getDates,
-} from "@ledgerhq/live-common-date";
+import { getPortfolioRangeConfig, getDates } from "@ledgerhq/live-common-date";
 
 export { getDates, getRanges } from "@ledgerhq/live-common-date";
 
