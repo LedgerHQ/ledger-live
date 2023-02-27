016d2a77acd51d3bf73ccd0246d83e50637ab998
lvndry <lvndry@proton.me>
2023-02-27 10:40:44 +0100

    remove useless guard.ts

--- libs/ledger-live-common/src/families/ethereum/guards.ts
+++
@@ -1,7 +0,0 @@
-import { CryptoCurrency, Currency } from "@ledgerhq/types-cryptoassets";
-
-export const isEthereumFamily = (
-  currency: Currency
-): currency is CryptoCurrency => {
-  return currency.type === "CryptoCurrency" && currency.family === "ethereum";
-};
--- libs/ledger-live-common/src/operation.ts
+++ libs/ledger-live-common/src/operation.ts
@@ -1,12 +1,8 @@
-import type { AccountLike, NFTStandard, Operation } from "@ledgerhq/types-live";
+import type { NFTStandard, Operation } from "@ledgerhq/types-live";
 import { decodeAccountId } from "./account";
 import { encodeNftId } from "@ledgerhq/coin-framework/nft/nftId";
-import { encodeERC1155OperationId, encodeERC721OperationId } from "./nft/nftOperationId";
 
-const nftOperationIdEncoderPerStandard: Record<NFTStandard, (...args: any[]) => string> = {
-  ERC721: encodeERC721OperationId,
-  ERC1155: encodeERC1155OperationId,
-};
+import { encodeERC1155OperationId, encodeERC721OperationId } from "./nft/nftOperationId";
 
 import {
   findOperationInAccount,
@@ -23,6 +19,11 @@ import {
   patchOperationWithHash as commonPatchOperationWithHash,
   isAddressPoisoningOperation,
 } from "@ledgerhq/coin-framework/operation";
+
+const nftOperationIdEncoderPerStandard: Record<NFTStandard, (...args: any[]) => string> = {
+  ERC721: encodeERC721OperationId,
+  ERC1155: encodeERC1155OperationId,
+};
 
 export {
   findOperationInAccount,
@@ -66,18 +67,3 @@ export function patchOperationWithHash(operation: Operation, hash: string): Oper
       }),
   };
 }
-
-export const isEditableOperation = (account: AccountLike, operation: Operation): boolean => {
-  let isEthFamily = false;
-  if (account.type === "Account") {
-    isEthFamily = account.currency.family === "ethereum";
-  } else if (account.type === "TokenAccount") {
-    isEthFamily = account.token.parentCurrency.family === "ethereum";
-  }
-
-  return (
-    isEthFamily &&
-    operation.blockHeight === null &&
-    (operation.type === "OUT" || operation.type === "NFT_OUT")
-  );
-};
