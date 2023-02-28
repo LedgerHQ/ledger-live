7b17361b587d97eec1284e5d126e68b72d37853c
lvndry <lvndry@proton.me>
2023-02-28 14:13:30 +0100

    babel config for detox

---
+++ apps/ledger-live-mobile/e2e/babel.config.detox.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    "module:metro-react-native-babel-preset",
+    ["@babel/preset-react", { runtime: "automatic" }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [],
+};
