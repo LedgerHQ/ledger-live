9cd5fa6e7b7b90865aff4afce7fb64083d0dcb41
Jules Grenier <jules.grenier-ext@ledger.fr>
2023-02-28 18:06:30 +0100

    chore: review fix

--- apps/ledger-live-mobile/src/components/WebPlatformPlayer/index.tsx
+++ apps/ledger-live-mobile/src/components/WebPlatformPlayer/index.tsx
@@ -1,7 +1,7 @@
 import semver from "semver";
 import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
 import { WALLET_API_VERSION } from "@ledgerhq/live-common/wallet-api/constants";
-import React from "react";
+import React, { useMemo } from "react";
 import { WebView as WebViewV2 } from "./WebViewV2";
 import { WebView } from "./WebView";
 
@@ -17,7 +17,10 @@ const ledgerRecoverIds = [
 ];
 
 const WebViewWrapper = ({ manifest, inputs }: Props) => {
-  const isManifestOfLedgerRecover = ledgerRecoverIds.includes(manifest.id);
+  const isManifestOfLedgerRecover = useMemo(
+    () => ledgerRecoverIds.includes(manifest.id),
+    [manifest.id],
+  );
 
   if (semver.satisfies(WALLET_API_VERSION, manifest.apiVersion)) {
     return (
