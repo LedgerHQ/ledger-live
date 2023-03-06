3e71ef2918cb8cd69d69cddcbae06f92f0233868
lvndry <lvndry@proton.me>
2023-03-06 16:18:41 +0100

    remove useless babel config param

--- apps/ledger-live-mobile/babel.config.js
+++ apps/ledger-live-mobile/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   presets: [
-    [
-      "module:metro-react-native-babel-preset",
-      { useTransformReactJSXExperimental: true },
-    ],
+    "module:metro-react-native-babel-preset",
     "@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }],
     "@babel/preset-typescript",
