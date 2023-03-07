e2971a4f8c2e9811bc7252b66c270bdc2c6903b9
lvndry <lvndry@proton.me>
2023-03-07 10:15:46 +0100

    wip

--- apps/ledger-live-mobile/babel.config.js
+++ apps/ledger-live-mobile/babel.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset",
-    "@babel/preset-env",
-    ["@babel/preset-react", { runtime: "automatic" }],
-    "@babel/preset-typescript",
-  ],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
-    "react-native-reanimated/plugin",
     "@babel/plugin-transform-named-capturing-groups-regex",
     "@babel/plugin-proposal-export-namespace-from",
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    "react-native-reanimated/plugin",
   ],
 };
