fb4e463ab0117e2da25fefefec1bd26e46c963ea
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-03-10 09:52:14 +0100

    feat: re-work of bluetooth permissions and services requirements
    New hooks to simplify bluetooth requirements checks and requests
    Re-work of associated components using those hooks
    
    Co-authored-by: Juan Cortes <juan.cortes.ross@gmail.com>

--- apps/ledger-live-mobile/src/components/RequiresBLE/index.tsx
+++ apps/ledger-live-mobile/src/components/RequiresBLE/index.tsx
@@ -1,31 +1,60 @@
-// renders children if BLE is available
-// otherwise render an error
 import React from "react";
 import { Platform } from "react-native";
 import AndroidRequiresBluetoothPermissions from "./AndroidRequiresBluetoothPermissions";
-import AndroidRequiresLocationPermissions from "./AndroidRequiresLocationPermissions";
+import AndroidRequiresLocationPermission from "../RequiresLocation/AndroidRequiresLocationPermission";
 import RequiresBluetoothEnabled from "./RequiresBluetoothEnabled";
+import AndroidRequiresLocationEnabled from "../RequiresLocation/AndroidRequiresLocationEnabled";
 
 type Props = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  forceOpenSettingsOnErrorButton?: boolean;
 };
 
+/**
+ * Renders children if bluetooth is enabled and its associated needed permissions are set.
+ * Otherwise, renders a relevant error component.
+ *
+ * @param children The children to render if bluetooth (and location on Android) has the correct granted permissions and is enabled
+ * @param hasBackButtonOnError If true, the back button will be displayed on the different error component. Default to false.
+ * @param forceOpenSettingsOnErrorButton Used mainly for debug purposes. If true, on a permission denied or service disabled,
+ *   pressing the button on the error component will make the user go to the settings.
+ *   Otherwise it will try to prompt the user (to allow permission, or enable the service) if possible.
+ *   Defaults to false.
+ */
 const RequiresBLE: React.FC<Props> = ({
   children,
-}: {
-  children?: React.ReactNode;
+  forceOpenSettingsOnErrorButton = false,
 }) => {
   if (Platform.OS === "android") {
     return (
-      <AndroidRequiresBluetoothPermissions>
-        <AndroidRequiresLocationPermissions>
-          <RequiresBluetoothEnabled>{children}</RequiresBluetoothEnabled>
-        </AndroidRequiresLocationPermissions>
+      <AndroidRequiresBluetoothPermissions
+        forceOpenSettingsOnErrorButton={forceOpenSettingsOnErrorButton}
+      >
+        <RequiresBluetoothEnabled
+          forceOpenSettingsOnErrorButton={forceOpenSettingsOnErrorButton}
+        >
+          <AndroidRequiresLocationPermission
+            forceOpenSettingsOnErrorButton={forceOpenSettingsOnErrorButton}
+          >
+            <AndroidRequiresLocationEnabled
+              forceOpenSettingsOnErrorButton={forceOpenSettingsOnErrorButton}
+            >
+              {children}
+            </AndroidRequiresLocationEnabled>
+          </AndroidRequiresLocationPermission>
+        </RequiresBluetoothEnabled>
       </AndroidRequiresBluetoothPermissions>
     );
   }
 
-  return <RequiresBluetoothEnabled>{children}</RequiresBluetoothEnabled>;
+  // On iOS, only Bluetooth service is needed. Its permission is directly handled by `RequiresBluetoothEnabled`.
+  return (
+    <RequiresBluetoothEnabled
+      forceOpenSettingsOnErrorButton={forceOpenSettingsOnErrorButton}
+    >
+      {children}
+    </RequiresBluetoothEnabled>
+  );
 };
 
-export default RequiresBLE;
+export default React.memo(RequiresBLE);
