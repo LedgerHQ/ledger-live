5311071d907e20226f0f0e80bba0ed7959ab31da
lvndry <lvndry@proton.me>
2023-03-13 15:30:59 +0100

    wip

--- libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
+++ libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
@@ -50,4 +50,4 @@ export type ExtraDeviceTransactionField =
   | ExtraDeviceTransactionField_stellar
   | ExtraDeviceTransactionField_tezos
   | ExtraDeviceTransactionField_tron
-  | ExtraDeviceTransactionField_polkadot;
+    | ExtraDeviceTransactionField_polkadot;
