1840436c23c4bd4e0cbc4e3ba1f3f6c619b1fe00
lvndry <lvndry@proton.me>
2023-03-15 16:14:24 +0100

    improved display of current network fee

--- apps/ledger-live-mobile/src/components/RootNavigator/types/SendFundsNavigator.ts
+++ apps/ledger-live-mobile/src/components/RootNavigator/types/SendFundsNavigator.ts
@@ -89,7 +89,6 @@ export type SendFundsNavigatorStackParamList = {
       | ScreenName.SwapForm;
     overrideAmountLabel?: string;
     operation?: Operation;
-    isEdit?: string;
     appName?: string;
   };
   [ScreenName.SendSelectDevice]: {
@@ -149,7 +148,6 @@ export type SendFundsNavigatorStackParamList = {
     parentId?: string;
     transaction: BitcoinTransaction;
     status?: BitcoinTransactionStatus;
-    isEdit?: boolean;
     currentNavigation:
       | ScreenName.SignTransactionSummary
       | ScreenName.SignTransactionSummary
