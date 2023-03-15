b0a4fb7d7ff717489bd56a328c54f3793ccd51a3
lvndry <lvndry@proton.me>
2023-03-15 11:53:51 +0100

    moved isEditableOperation

--- libs/ledger-live-common/src/operation.ts
+++ libs/ledger-live-common/src/operation.ts
@@ -1,4 +1,4 @@
-import type { NFTStandard, Operation } from "@ledgerhq/types-live";
+import type { AccountLike, NFTStandard, Operation } from "@ledgerhq/types-live";
 import { decodeAccountId } from "./account";
 import { encodeNftId } from "@ledgerhq/coin-framework/nft/nftId";
 
@@ -21,7 +21,6 @@ import {
   isConfirmedOperation,
   patchOperationWithHash as commonPatchOperationWithHash,
   isAddressPoisoningOperation,
-  isEditableOperation,
 } from "@ledgerhq/coin-framework/operation";
 
 const nftOperationIdEncoderPerStandard: Record<
@@ -45,7 +44,6 @@ export {
   getOperationConfirmationDisplayableNumber,
   isConfirmedOperation,
   isAddressPoisoningOperation,
-  isEditableOperation,
 };
 
 export function patchOperationWithHash(
@@ -78,3 +76,21 @@ export function patchOperationWithHash(
       }),
   };
 }
+
+export const isEditableOperation = (
+  account: AccountLike,
+  operation: Operation
+): boolean => {
+  let isEthFamily = false;
+  if (account.type === "Account") {
+    isEthFamily = account.currency.family === "ethereum";
+  } else if (account.type === "TokenAccount") {
+    isEthFamily = account.token.parentCurrency.family === "ethereum";
+  }
+
+  return (
+    isEthFamily &&
+    operation.blockHeight === null &&
+    (operation.type === "OUT" || operation.type === "NFT_OUT")
+  );
+};
