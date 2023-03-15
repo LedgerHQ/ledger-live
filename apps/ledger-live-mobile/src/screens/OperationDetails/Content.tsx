f8aa4787fd262ccf5bd3ee3e573b94df46c56538
lvndry <lvndry@proton.me>
2023-03-15 16:33:45 +0100

    panel show different message if stuck or not

--- apps/ledger-live-mobile/src/screens/OperationDetails/Content.tsx
+++ apps/ledger-live-mobile/src/screens/OperationDetails/Content.tsx
@@ -372,7 +372,7 @@ export default function Content({
           {isOperationStuck ? (
             <Box>
               <LText color="neutral.c20">
-                {t("editTransaction.panel.description")}
+                {t("editTransaction.panel.stuckMessage")}
               </LText>
               <LText marginTop={4}>
                 <Link onPress={onEditTxPress}>
@@ -390,7 +390,7 @@ export default function Content({
             <>
               <Box>
                 <LText color="neutral.c20">
-                  {t("editTransaction.panel.speedupTxMessage")}
+                  {t("editTransaction.panel.speedupMessage")}
                 </LText>
                 <LText marginTop={4}>
                   <Link onPress={onEditTxPress}>
