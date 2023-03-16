9cd709ee153ab047184b786ff98eca70f62d68d9
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2023-03-16 10:23:26 +0100

    chore(coin): rename date package to live-portfolio
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/portfolio/v2/index.ts
+++ libs/ledger-live-common/src/portfolio/v2/index.ts
@@ -6,9 +6,9 @@ import {
   getAccountHistoryBalances,
 } from "../../account";
 import { getEnv } from "../../env";
-import { getPortfolioRangeConfig, getDates } from "@ledgerhq/live-date";
+import { getPortfolioRangeConfig, getDates } from "@ledgerhq/live-portfolio";
 
-export { getDates, getRanges } from "@ledgerhq/live-date";
+export { getDates, getRanges } from "@ledgerhq/live-portfolio";
 
 export const defaultAssetsDistribution = {
   minShowFirst: 1,
