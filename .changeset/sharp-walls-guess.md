8559045d4b7c58dce8e7e5c7565e941b0d92531b
JunichiSugiura <jun.sugiura.jp@gmail.com>
2023-03-21 11:30:33 +0100

    feat: support Tezos family via Wallet API

---
+++ .changeset/sharp-walls-guess.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/live-common": minor
+---
+
+Support Tezos family via Wallet API
--- libs/ledger-live-common/src/generated/walletApiAdapter.ts
+++ libs/ledger-live-common/src/generated/walletApiAdapter.ts
@@ -2,10 +2,12 @@ import bitcoin from "../families/bitcoin/walletApiAdapter";
 import ethereum from "../families/ethereum/walletApiAdapter";
 import polkadot from "../families/polkadot/walletApiAdapter";
 import ripple from "../families/ripple/walletApiAdapter";
+import tezos from "../families/tezos/walletApiAdapter";
 
 export default {
   bitcoin,
   ethereum,
   polkadot,
   ripple,
+  tezos,
 };
