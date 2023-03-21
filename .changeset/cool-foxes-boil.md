77605ba29833117b798e179d732ff8b7e4303560
JunichiSugiura <jun.sugiura.jp@gmail.com>
2023-03-21 11:19:25 +0100

    feat: support Stellar family via Wallet API

---
+++ .changeset/cool-foxes-boil.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/live-common": minor
+---
+
+Support Stellar family via Wallet API
---
+++ libs/ledger-live-common/src/families/stellar/walletApiAdapter.ts
@@ -0,0 +1,22 @@
+import { StellarTransaction as WalletAPITransaction } from "@ledgerhq/wallet-api-core";
+import {
+  AreFeesProvided,
+  GetWalletAPITransactionSignFlowInfos,
+} from "../../wallet-api/types";
+import { Transaction } from "./types";
+
+const CAN_EDIT_FEES = true;
+
+const areFeesProvided: AreFeesProvided<WalletAPITransaction> = (tx) =>
+  !!tx.fees;
+
+const getWalletAPITransactionSignFlowInfos: GetWalletAPITransactionSignFlowInfos<
+  WalletAPITransaction,
+  Transaction
+> = (tx) => ({
+  canEditFees: CAN_EDIT_FEES,
+  liveTx: tx,
+  hasFeesProvided: areFeesProvided(tx),
+});
+
+export default { getWalletAPITransactionSignFlowInfos };
