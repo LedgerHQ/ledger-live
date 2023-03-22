7f8c1f82d3da3e2e97cc6bfe44cd6de887cedcc0
RamyEB <ramyelb@gmail.com>
2023-03-22 12:18:56 +0100

    fix: typecheck

--- libs/ledger-live-common/src/platform/filters.ts
+++ libs/ledger-live-common/src/platform/filters.ts
@@ -4,7 +4,7 @@ import { isPlatformTokenCurrency } from "./helpers";
 
 export type FilterParams = {
   branches?: string[];
-  platform?: AppPlatform;
+  platforms?: AppPlatform;
   private?: boolean;
   version?: string;
 };
