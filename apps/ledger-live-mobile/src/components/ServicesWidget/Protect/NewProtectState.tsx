65b487430a1232722ac6592453f712daba58e9e7
nparigi-ledger <nicolas.parigi@ledger.fr>
2023-03-22 16:59:05 +0100

    LIVE-5856 recover screens pixel polish LIVE-6849 fix support link

--- apps/ledger-live-mobile/src/components/ServicesWidget/Protect/NewProtectState.tsx
+++ apps/ledger-live-mobile/src/components/ServicesWidget/Protect/NewProtectState.tsx
@@ -1,9 +1,8 @@
-import { Tag, Text } from "@ledgerhq/native-ui";
+import { Button, Tag, Text } from "@ledgerhq/native-ui";
 import React, { useCallback } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { GestureResponderEvent, Linking } from "react-native";
 import { urls } from "../../../config/urls";
-import Button from "../../Button";
 
 function NewProtectState({ params }: { params: Record<string, string> }) {
   const { t } = useTranslation();
@@ -26,16 +25,28 @@ function NewProtectState({ params }: { params: Record<string, string> }) {
 
   return (
     <>
-      <Button type="main" outline={false} onPress={onLearnMore} mb={6}>
+      <Button
+        type="main"
+        outline={false}
+        size={"small"}
+        onPress={onLearnMore}
+        mb={6}
+      >
         {t(`servicesWidget.protect.status.new.actions.learnMore`)}
       </Button>
-      <Text variant="body" color="neutral.c100" mb={6}>
+      <Text
+        variant="paragraph"
+        fontWeight={"semiBold"}
+        color="neutral.c100"
+        textAlign={"center"}
+      >
         <Trans
           i18nKey="servicesWidget.protect.status.new.actions.alreadySubscribed"
           components={{
             LinkAccount: (
               <Text
-                variant="body"
+                variant="paragraph"
+                fontWeight="semiBold"
                 color="neutral.c100"
                 onPressIn={onPressInAlreadySubscribed}
                 onPress={onAlreadySubscribe}
@@ -60,6 +71,7 @@ const StateTag = () => {
       textColor="neutral.c90"
       ellipsizeMode="tail"
       size="medium"
+      uppercase={false}
     >
       {t(`servicesWidget.protect.status.new.title`)}
     </Tag>
--- apps/ledger-live-mobile/src/screens/Onboarding/steps/setupDevice/scenes/RestoreWithProtect.tsx
+++ apps/ledger-live-mobile/src/screens/Onboarding/steps/setupDevice/scenes/RestoreWithProtect.tsx
@@ -3,10 +3,10 @@ import { useTranslation } from "react-i18next";
 import { Icons, NumberedList, Text } from "@ledgerhq/native-ui";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import { Linking } from "react-native";
-import InfoModal from "../../../../../modals/Info";
 import Button from "../../../../../components/wrappedUi/Button";
 import { TrackScreen } from "../../../../../analytics";
 import Touchable from "../../../../../components/Touchable";
+import QueuedDrawer from "../../../../../components/QueuedDrawer";
 
 const RestoreWithProtectScene = () => {
   const { t } = useTranslation();
@@ -46,7 +46,7 @@ const Next = ({ onNext }: { onNext: () => void }) => {
   const updateFirmwareLink =
     "https://support.ledger.com/hc/en-us/articles/360013349800-Update-Ledger-Nano-X-firmware?docs=true";
 
-  const supportLink = restoreInfoDrawer?.supportLink;
+  const supportLink = restoreInfoDrawer?.supportLinkURI;
 
   const onOpen = useCallback(() => setIsOpened(true), []);
   const onClose = useCallback(() => setIsOpened(false), []);
@@ -68,49 +68,40 @@ const Next = ({ onNext }: { onNext: () => void }) => {
       <Button type="main" size="large" onPress={onNext} mb={7}>
         {t("onboarding.stepProtect.nextStep")}
       </Button>
-      <InfoModal
-        isOpened={isOpened}
+      <QueuedDrawer
+        isRequestingToBeOpened={isOpened}
         onClose={onClose}
-        data={[
-          {
-            title: t("onboarding.stepProtect.extraInfo.title"),
-          },
-          {
-            description: t("onboarding.stepProtect.extraInfo.desc"),
-            footer: (
-              <>
-                <Button
-                  type="main"
-                  size="large"
-                  onPress={onLearnToUpdate}
-                  Icon={Icons.ExternalLinkMedium}
-                  mt={8}
-                  mb={6}
-                  event={"button_clicked"}
-                  eventProperties={{ button: "Go through manual steps" }}
-                >
-                  {t("onboarding.stepProtect.extraInfo.cta")}
-                </Button>
-                <Button
-                  type="default"
-                  size="large"
-                  onPress={onSupportLink}
-                  Icon={Icons.ExternalLinkMedium}
-                  event={"button_clicked"}
-                  eventProperties={{ button: "Contact Ledger support" }}
-                >
-                  {t("onboarding.stepProtect.extraInfo.supportLink")}
-                </Button>
-                <TrackScreen
-                  category="Why can I not see Restore with Protect on my Ledger"
-                  refreshSource={false}
-                  type="drawer"
-                />
-              </>
-            ),
-          },
-        ]}
-      />
+        title={t("onboarding.stepProtect.extraInfo.title")}
+        description={t("onboarding.stepProtect.extraInfo.desc")}
+      >
+        <Button
+          type="main"
+          size="large"
+          onPress={onLearnToUpdate}
+          Icon={Icons.ExternalLinkMedium}
+          mt={0}
+          mb={6}
+          event={"button_clicked"}
+          eventProperties={{ button: "Go through manual steps" }}
+        >
+          {t("onboarding.stepProtect.extraInfo.cta")}
+        </Button>
+        <Button
+          type="default"
+          size="large"
+          onPress={onSupportLink}
+          Icon={Icons.ExternalLinkMedium}
+          event={"button_clicked"}
+          eventProperties={{ button: "Contact Ledger support" }}
+        >
+          {t("onboarding.stepProtect.extraInfo.supportLink")}
+        </Button>
+        <TrackScreen
+          category="Why can I not see Restore with Protect on my Ledger"
+          refreshSource={false}
+          type="drawer"
+        />
+      </QueuedDrawer>
       {restoreInfoDrawer?.enabled ? (
         <Touchable onPress={onOpen}>
           <Text textAlign="center" variant="large">
