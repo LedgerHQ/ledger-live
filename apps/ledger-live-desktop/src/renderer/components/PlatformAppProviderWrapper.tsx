6d1fcb88207a5885d7d089887449e64677fd8a7b
RamyEB <ramyelb@gmail.com>
2023-03-22 11:46:59 +0100

    feat: filter by platform

--- apps/ledger-live-desktop/src/renderer/components/PlatformAppProviderWrapper.tsx
+++ apps/ledger-live-desktop/src/renderer/components/PlatformAppProviderWrapper.tsx
@@ -26,7 +26,7 @@ export function PlatformAppProviderWrapper({ children }: PlatformAppProviderWrap
       parameters={{
         version: getPlatformVersion(),
         private: false,
-        platform: "desktop",
+        platforms: "desktop",
         allowDebugApps,
         allowExperimentalApps,
       }}
