ec3a33aff423933548d201a68349408749c1a044
male-gal <maxence.le-gall@ledger.fr>
2023-03-23 12:38:57 +0100

    fix lint issue

--- apps/ledger-live-mobile/src/logic/internetReachable.ts
+++ apps/ledger-live-mobile/src/logic/internetReachable.ts
@@ -2,7 +2,7 @@ import NetInfo from "@react-native-community/netinfo";
 
 export const internetReachable = async () => {
   // workaround : on iOS, netInfo returns null on the first call, see ; https://github.com/react-native-netinfo/react-native-netinfo/issues/572
-const isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
-if (isInternetReachable !== null) return isInternetReachable
-return (await NetfInfo.fetch()).isInternetReachable
+  const isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
+  if (isInternetReachable !== null) return isInternetReachable;
+  return (await NetfInfo.fetch()).isInternetReachable;
 };
