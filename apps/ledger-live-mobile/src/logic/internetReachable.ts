c6b31bc0d906ebd05f3fa1c5df8d90aa99c38bf8
male-gal <maxence.le-gall@ledger.fr>
2023-03-23 15:08:42 +0100

    adding timeout to InternetReachable to prevent call from being null on iOS

--- apps/ledger-live-mobile/src/logic/internetReachable.ts
+++ apps/ledger-live-mobile/src/logic/internetReachable.ts
@@ -2,7 +2,16 @@ import NetInfo from "@react-native-community/netinfo";
 
 export const internetReachable = async () => {
   // workaround : on iOS, netInfo returns null on the first call, see ; https://github.com/react-native-netinfo/react-native-netinfo/issues/572
-  const isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
+  const timeout = (milliseconds: number) =>
+    new Promise(resolve => setTimeout(resolve, milliseconds));
+
+  let isInternetReachable: boolean | null = false;
+
+  isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
+
   if (isInternetReachable !== null) return isInternetReachable;
-  return (await NetInfo.fetch()).isInternetReachable;
+
+  await timeout(200);
+
+  isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
 };
