e38424f8e4527a7a795d924ac4dbc2843a7bdce0
male-gal <maxence.le-gall@ledger.fr>
2023-03-23 13:44:36 +0100

    Fix internetReachable helper

--- apps/ledger-live-mobile/src/logic/internetReachable.ts
+++ apps/ledger-live-mobile/src/logic/internetReachable.ts
@@ -4,5 +4,5 @@ export const internetReachable = async () => {
   // workaround : on iOS, netInfo returns null on the first call, see ; https://github.com/react-native-netinfo/react-native-netinfo/issues/572
   const isInternetReachable = (await NetInfo.fetch()).isInternetReachable;
   if (isInternetReachable !== null) return isInternetReachable;
-  return (await NetfInfo.fetch()).isInternetReachable;
+  return (await NetInfo.fetch()).isInternetReachable;
 };
