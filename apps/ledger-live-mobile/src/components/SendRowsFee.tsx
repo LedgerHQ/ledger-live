0649dbdcc9edf355229f39be82f295f0904a12cb
lvndry <lvndry@proton.me>
2023-03-30 16:57:04 +0200

    remove lonely if lint rule

--- apps/ledger-live-mobile/src/components/SendRowsFee.tsx
+++ apps/ledger-live-mobile/src/components/SendRowsFee.tsx
@@ -14,7 +14,7 @@ import type { SendFundsNavigatorStackParamList } from "./RootNavigator/types/Sen
 import type { SignTransactionNavigatorParamList } from "./RootNavigator/types/SignTransactionNavigator";
 import type { BaseNavigatorStackParamList } from "./RootNavigator/types/BaseNavigator";
 import type { SwapNavigatorParamList } from "./RootNavigator/types/SwapNavigator";
-import { EditTransactionParamList } from "./RootNavigator/types/EditTransactionNavigator";
+import { EditTransactionParamList } from "../families/ethereum/EditTransactionNavigator";
 import { ScreenName } from "../const";
 
 type Props = {
