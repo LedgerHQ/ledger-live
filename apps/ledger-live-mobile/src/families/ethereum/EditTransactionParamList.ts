c715bca8c0cd62dcf7c2117455080b02a8e927e8
lvndry <lvndry@proton.me>
2023-03-31 16:24:40 +0200

    remove unused navigator props

--- apps/ledger-live-mobile/src/families/ethereum/EditTransactionParamList.ts
+++ apps/ledger-live-mobile/src/families/ethereum/EditTransactionParamList.ts
@@ -1,6 +1,6 @@
 import {
   TransactionRaw,
-  Transaction as ETHTransaction,
+  Transaction as EthereumTransaction,
 } from "@ledgerhq/live-common/families/ethereum/types";
 import { Transaction, TransactionStatus } from "@ledgerhq/live-common/generated/types";
 import { Device } from "@ledgerhq/types-devices";
@@ -18,7 +18,7 @@ export type EditTransactionParamList = {
   [ScreenName.EthereumCustomFees]: {
     accountId: string;
     parentId?: string;
-    transaction: Transaction;
+    transaction: EthereumTransaction;
     currentNavigation: ScreenName.EditTransactionMethodSelection;
     nextNavigation: ScreenName.SendSelectDevice;
     setTransaction: (transaction: Transaction) => void;
@@ -29,7 +29,7 @@ export type EditTransactionParamList = {
     deviceId?: string;
     transaction: Transaction;
     transactionRaw?: TransactionRaw;
-    setTransaction: Result<ETHTransaction>["setTransaction"];
+    setTransaction: Result<EthereumTransaction>["setTransaction"];
     operation?: Operation;
     currentNavigation: ScreenName.EditTransactionMethodSelection;
     nextNavigation: ScreenName.SignTransactionSelectDevice | ScreenName.SendSelectDevice;
