1a51532119fde04a4fab5d32f5a3fa7f3ce963e2
lvndry <lvndry@proton.me>
2023-03-31 12:09:19 +0200

    remove unused import

--- apps/ledger-live-mobile/src/screens/Account/ListHeaderComponent.tsx
+++ apps/ledger-live-mobile/src/screens/Account/ListHeaderComponent.tsx
@@ -10,7 +10,7 @@ import {
   Operation,
 } from "@ledgerhq/types-live";
 import { Currency } from "@ledgerhq/types-cryptoassets";
-import { Box, ColorPalette, SideImageCard } from "@ledgerhq/native-ui";
+import { Box, ColorPalette } from "@ledgerhq/native-ui";
 import { isNFTActive } from "@ledgerhq/live-common/nft/index";
 import { TFunction } from "react-i18next";
 import { CosmosAccount } from "@ledgerhq/live-common/families/cosmos/types";
@@ -120,9 +120,7 @@ export function getListHeaderComponents({
     .filter(pendingOperation => {
       return isEditableOperation(account, pendingOperation);
     })
-    .sort(
-      (a, b) => a.transactionSequenceNumber! - b.transactionSequenceNumber!,
-    );
+    .sort((a, b) => a.transactionSequenceNumber! - b.transactionSequenceNumber!);
 
   const isOperationStuck =
     oldestEditableOperation &&
