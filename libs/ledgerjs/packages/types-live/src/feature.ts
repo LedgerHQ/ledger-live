9ff6a0623b1d98e029f5668f79a4583ff6c7f2e4
lvndry <lvndry@proton.me>
2023-03-31 11:56:50 +0200

    add feature flag

--- libs/ledgerjs/packages/types-live/src/feature.ts
+++ libs/ledgerjs/packages/types-live/src/feature.ts
@@ -67,7 +67,8 @@ export type FeatureId =
   | "discover"
   | "protectServicesDiscoverDesktop"
   | "protectServicesDesktop"
-  | "listAppsV2";
+  | "listAppsV2"
+  | "editEthTx";
 
 /**  We use objects instead of direct booleans for potential future improvements
 like feature versioning etc */
