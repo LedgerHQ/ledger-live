e3cf1c7dee833af1813eda4ad7eecf1719956c82
lvndry <lvndry@proton.me>
2023-03-31 16:24:40 +0200

    remove unused navigator props

--- apps/ledger-live-mobile/src/components/SendRowsCustom.tsx
+++ apps/ledger-live-mobile/src/components/SendRowsCustom.tsx
@@ -7,13 +7,11 @@ import type { SendFundsNavigatorStackParamList } from "./RootNavigator/types/Sen
 import type { SignTransactionNavigatorParamList } from "./RootNavigator/types/SignTransactionNavigator";
 import type { SwapNavigatorParamList } from "./RootNavigator/types/SwapNavigator";
 import { ScreenName } from "../const";
-import { EditTransactionParamList } from "../families/ethereum/editTransactionNavigatorParamsList";
 
 type Navigation = BaseComposite<
   | StackNavigatorProps<SendFundsNavigatorStackParamList, ScreenName.SendSummary>
   | StackNavigatorProps<SignTransactionNavigatorParamList, ScreenName.SignTransactionSummary>
   | StackNavigatorProps<SwapNavigatorParamList, ScreenName.SwapSelectFees>
-  | StackNavigatorProps<EditTransactionParamList, ScreenName.SendSummary>
 >;
 
 export default ({
