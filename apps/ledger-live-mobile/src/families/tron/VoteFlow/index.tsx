9454250a264038d96bfb3d04e192b3113496e5c4
hzheng-ledger <huiqi.zheng@ledger.fr>
2023-04-04 01:34:49 +0200

    fix: remove unnecessary code

--- apps/ledger-live-mobile/src/families/tron/VoteFlow/index.tsx
+++ apps/ledger-live-mobile/src/families/tron/VoteFlow/index.tsx
@@ -1,33 +1,10 @@
-import React, { useMemo } from "react";
-import { Platform } from "react-native";
-import { useTranslation } from "react-i18next";
-import { createStackNavigator } from "@react-navigation/stack";
-import { useTheme } from "@react-navigation/native";
-import {
-  getStackNavigatorConfig,
-} from "../../../navigation/navigatorConfig";
-import { TronVoteFlowParamList } from "./types";
+import React from "react";
 
 function VoteFlow() {
-  const { t } = useTranslation();
-  const { colors } = useTheme();
-  const stackNavigationConfig = useMemo(
-    () => getStackNavigatorConfig(colors, true),
-    [colors],
-  );
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        ...stackNavigationConfig,
-        gestureEnabled: Platform.OS === "ios",
-      }}
-    >
-    </Stack.Navigator>
-  );
+  return <></>;
 }
 
 const options = {
   headerShown: false,
 };
 export { VoteFlow as component, options };
-const Stack = createStackNavigator<TronVoteFlowParamList>();
