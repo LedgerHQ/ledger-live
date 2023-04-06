9399871fb14d2f15da41504d2b1b355a7fca95fb
Prashanth <prashanthsoordelu@gmail.com>
2023-04-06 10:26:49 +0530

    register modal

--- apps/ledger-live-desktop/src/renderer/families/cardano/modals.ts
+++ apps/ledger-live-desktop/src/renderer/families/cardano/modals.ts
@@ -1,7 +1,9 @@
 import MODAL_CARDANO_DELEGATE from "./DelegationFlowModal";
 import MODAL_CARDANO_UNDELEGATE from "./UndelegateFlowModal";
+import MODAL_CARDANO_REWARDS_INFO from "./DelegationFlowModal/Info";
 
 export default {
   MODAL_CARDANO_DELEGATE,
   MODAL_CARDANO_UNDELEGATE,
+  MODAL_CARDANO_REWARDS_INFO,
 };
