17df2c9d5665aa0d5ee4b80137c001863f6a84d0
Jules Grenier <jules.grenier-ext@ledger.fr>
2023-04-12 12:14:29 +0200

    fix: recover label and icon in sidebar

--- apps/ledger-live-desktop/src/renderer/components/MainSideBar/index.tsx
+++ apps/ledger-live-desktop/src/renderer/components/MainSideBar/index.tsx
@@ -483,8 +483,8 @@ const MainSideBar = () => {
                 <FeatureToggle feature="protectServicesDiscoverDesktop">
                   <SideBarListItem
                     id={"send"}
-                    label={t("sidebar.protect")}
-                    icon={Icons.LockMedium}
+                    label={t("sidebar.recover")}
+                    icon={Icons.ShieldCheckMedium}
                     iconSize={20}
                     iconActiveColor="wallet"
                     onClick={handleOpenProtectDiscoverModal}
