319785c085aae11b85aa9ed0aa991850bd364f38
lvndry <lvndry@proton.me>
2023-04-13 21:04:05 +0200

    typecheck

--- apps/ledger-live-mobile/src/families/polkadot/SendRowsFee.tsx
+++ apps/ledger-live-mobile/src/families/polkadot/SendRowsFee.tsx
@@ -22,12 +22,14 @@ import { ScreenName } from "../../const";
 import { SignTransactionNavigatorParamList } from "../../components/RootNavigator/types/SignTransactionNavigator";
 import { SwapNavigatorParamList } from "../../components/RootNavigator/types/SwapNavigator";
 import { PolkadotBondFlowParamList } from "./BondFlow/types";
+import { EditTransactionParamList } from "../ethereum/editTransactionNavigatorParamsList";
 
 type Navigation = CompositeScreenProps<
   | StackNavigatorProps<
       SendFundsNavigatorStackParamList,
       ScreenName.SendSummary
     >
+  | StackNavigatorProps<EditTransactionParamList, ScreenName.SendSummary>
   | StackNavigatorProps<
       SignTransactionNavigatorParamList,
       ScreenName.SignTransactionSummary
