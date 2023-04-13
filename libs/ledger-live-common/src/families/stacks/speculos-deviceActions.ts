74c090e44ff3c3d37e54327a5ce06a76111de513
haammar-ledger <hakim.aammar-ext@ledger.fr>
2023-04-13 19:55:00 +0200

    Additional changes after rebase

--- libs/ledger-live-common/src/families/stacks/speculos-deviceActions.ts
+++ libs/ledger-live-common/src/families/stacks/speculos-deviceActions.ts
@@ -1,45 +1,45 @@
 import type { DeviceAction } from "../../bot/types";
 import type { Transaction } from "./types";
-import { deviceActionFlow } from "../../bot/specs";
+import { deviceActionFlow, SpeculosButton } from "../../bot/specs";
 
 export const acceptTransaction: DeviceAction<Transaction, any> =
   deviceActionFlow({
     steps: [
       {
         title: "Origin",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ account }) => account.freshAddress,
       },
       {
         title: "Nonce",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ transaction }) =>
           transaction.nonce ? transaction.nonce.toFixed() : "0",
       },
       {
         title: "Fee (uSTX)",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ transaction }) =>
           transaction.fee ? transaction.fee.toFixed() : "0",
       },
       {
         title: "Amount uSTX",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ status }) => status.amount.toFixed(),
       },
       {
         title: "To",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ transaction }) => transaction.recipient,
       },
       {
         title: "Memo",
-        button: "Rr",
+        button: SpeculosButton.RIGHT,
         expectedValue: ({ transaction }) => transaction.memo || "",
       },
       {
         title: "APPROVE",
-        button: "LRlr",
+        button: SpeculosButton.BOTH,
       },
     ],
   });
