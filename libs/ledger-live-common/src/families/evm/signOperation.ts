aa3fe2c46729665ca095c9cb0a5267afd6d15536
lambertkevin <kevin.lambert@ledger.fr>
2023-04-17 14:03:04 +0200

    Reorg steps of EVM signOperation

--- libs/ledger-live-common/src/families/evm/signOperation.ts
+++ libs/ledger-live-common/src/families/evm/signOperation.ts
@@ -1,7 +1,8 @@
 import { Observable } from "rxjs";
 import Eth, { ledgerService } from "@ledgerhq/hw-app-eth";
-import { buildOptimisticOperation } from "./buildOptimisticOperation";
+import { AccountBridge, SignOperationEvent } from "@ledgerhq/types-live";
 import { ResolutionConfig } from "@ledgerhq/hw-app-eth/lib/services/types";
+import { buildOptimisticOperation } from "./buildOptimisticOperation";
 import { prepareForSignOperation } from "./prepareTransaction";
 import { getSerializedTransaction } from "./transaction";
 import { Transaction as EvmTransaction } from "./types";
@@ -45,39 +46,34 @@ export const signOperation: AccountBridge<EvmTransaction>["signOperation"] = ({
     (transport) =>
       new Observable((o) => {
         async function main() {
-          o.next({
-            type: "device-signature-requested",
-          });
-
           const preparedTransaction = await prepareForSignOperation(
             account,
             transaction
           );
-          const serializedTxHexString = getSerializedTransaction(
-            account,
-            preparedTransaction
-          ).slice(2); // Remove 0x prefix
+          const serializedTxHexString =
+            getSerializedTransaction(preparedTransaction).slice(2); // Remove 0x prefix
 
-          // Instanciate Eth app bindings
-          const eth = new Eth(transport);
           // Configure type of resolutions necessary for the clear signing
           const resolutionConfig: ResolutionConfig = {
             externalPlugins: true,
             erc20: true,
+            domains: transaction.recipientDomain
+              ? [transaction.recipientDomain]
+              : [],
           };
-          // Add potential domains to clear sign
-          if (transaction.recipientDomain?.type === "forward") {
-            resolutionConfig.domains = [transaction.recipientDomain];
-          }
           // Look for resolutions for external plugins and ERC20
           const resolution = await ledgerService.resolveTransaction(
             serializedTxHexString,
-            eth.loadConfig,
-            {
-              externalPlugins: true,
-              erc20: true,
-            }
+            {},
+            resolutionConfig
           );
+
+          o.next({
+            type: "device-signature-requested",
+          });
+
+          // Instanciate Eth app bindings
+          const eth = new Eth(transport);
           // Request signature on the nano
           const sig = await eth.signTransaction(
             account.freshAddressPath,
