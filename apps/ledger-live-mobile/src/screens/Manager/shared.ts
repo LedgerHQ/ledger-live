0f8cfb8287367ffa4dfad564753680eda8327743
Juan Cortés Ross <juan.cortes.ross@gmail.com>
2023-04-19 10:07:19 +0000

    chore: added a retry to the install/uninstall for disconnects

--- apps/ledger-live-mobile/src/screens/Manager/shared.ts
+++ apps/ledger-live-mobile/src/screens/Manager/shared.ts
@@ -6,7 +6,7 @@ import { isDashboardName } from "@ledgerhq/live-common/hw/isDashboardName";
 
 import { withDevice } from "@ledgerhq/live-common/hw/deviceAccess";
 import quitApp from "@ledgerhq/live-common/hw/quitApp";
-import { concatMap } from "rxjs/operators";
+import { concatMap, retry } from "rxjs/operators";
 import { from, of, timer } from "rxjs";
 import getAppAndVersion from "@ledgerhq/live-common/hw/getAppAndVersion";
 
@@ -17,6 +17,8 @@ export function useApps(
 ) {
   // Nb Two device jobs are chained due to the BLE stack getting reset when we enter/exit,
   // an application. A reconnectable transport paradigm could simplify this.
+  // The `retry` is present because some devices take longer to become available for BLE
+  // connection than others.
   const exec: Exec = useCallback(
     (...args) =>
       withDevice(deviceId)(transport =>
@@ -29,11 +31,11 @@ export function useApps(
         ),
       ).pipe(
         concatMap(alreadyInDashboard =>
-          timer(alreadyInDashboard ? 0 : 4000).pipe(
+          timer(alreadyInDashboard ? 0 : 2000).pipe(
             concatMap(_ =>
               withDevice(deviceId)(transport =>
                 execWithTransport(transport)(...args),
-              ),
+              ).pipe(retry(3)),
             ),
           ),
         ),
