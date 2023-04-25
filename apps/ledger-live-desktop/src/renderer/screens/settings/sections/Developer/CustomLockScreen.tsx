6c102b3bfa262b55b64e8a317fb517654692f10e
Juan Cortés Ross <juan.cortes.ross@gmail.com>
2023-04-25 21:48:49 +0000

    chore: no longer scalable assets for CLS in debug screen

--- apps/ledger-live-desktop/src/renderer/screens/settings/sections/Developer/CustomLockScreen.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/settings/sections/Developer/CustomLockScreen.tsx
@@ -18,7 +18,6 @@ const CustomLockScreen = () => {
   }, [history]);
 
   const [percentage, setPercentage] = useState<number>(0);
-  const [scale, setScale] = useState<number>(0);
 
   const fixedPercentage = percentage === 99 ? 100 : percentage; // Nb there's a bug in the slider
   return (
@@ -34,7 +33,6 @@ const CustomLockScreen = () => {
           <FramedImage
             source={source}
             loadingProgress={0}
-            scale={scale}
             background={
               <Animation
                 animation={getDeviceAnimation(DeviceModelId.stax, "light", "allowManager", true)}
@@ -46,7 +44,6 @@ const CustomLockScreen = () => {
           <FramedImage
             source={source}
             loadingProgress={1}
-            scale={scale}
             background={
               <Animation
                 animation={getDeviceAnimation(
@@ -59,11 +56,7 @@ const CustomLockScreen = () => {
             }
           />
         </Flex>
-        <FramedImage
-          source={source}
-          loadingProgress={+(fixedPercentage / 100).toFixed(2)}
-          scale={scale}
-        />
+        <FramedImage source={source} loadingProgress={+(fixedPercentage / 100).toFixed(2)} />
       </Box>
       <Slider
         value={percentage}
@@ -73,11 +66,12 @@ const CustomLockScreen = () => {
         steps={100}
         error={undefined}
       />
-      <Box horizontal>
+      {/* Murdered the scaling concept until it's done to all lotties, sorry. */}
+      {/* <Box horizontal>
+        <Button onClick={() => setScale(0.8)}>{"x0.8"}</Button>
         <Button onClick={() => setScale(1)}>{"x1"}</Button>
-        <Button onClick={() => setScale(1.5)}>{"x1.5"}</Button>
         <Button onClick={() => setScale(2)}>{"x2"}</Button>
-      </Box>
+      </Box> */}
     </Box>
   );
 };
