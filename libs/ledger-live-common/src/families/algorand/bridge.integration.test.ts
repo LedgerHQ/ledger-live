e9e8024eda5f31707a32b2bc33edd61d5babb662
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-04-25 17:19:21 +0200

    update paths

--- libs/ledger-live-common/src/families/algorand/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/algorand/bridge.integration.test.ts
@@ -3,6 +3,6 @@ import { testBridge } from "../../__tests__/test-helpers/bridge";
 import { dataset } from "@ledgerhq/coin-algorand/bridge.integration.test";
 
 // FIXME: why is this needed? cf. polkadot
-import from "@ledgerhq/coin-algorand/bridge.integration.test";
+import "@ledgerhq/coin-algorand/bridge.integration.test";
 
 testBridge(dataset);
