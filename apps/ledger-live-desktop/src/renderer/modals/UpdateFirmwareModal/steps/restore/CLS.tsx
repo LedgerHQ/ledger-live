46b1ef46450b87d1168da0aa0d62e6b41819033e
Juan Cortés Ross <juan.cortes.ross@gmail.com>
2023-04-25 21:43:59 +0000

    feat: turn the firmware update into a drawer

---
+++ apps/ledger-live-desktop/src/renderer/modals/UpdateFirmwareModal/steps/restore/CLS.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { StepProps } from "../..";
+import CustomImageDeviceAction from "~/renderer/components/CustomImage/CustomImageDeviceAction";
+import { getCurrentDevice } from "~/renderer/reducers/devices";
+import { useSelector } from "react-redux";
+import { reconstructImage } from "~/renderer/components/CustomImage/TestImage";
+
+type Props = Partial<StepProps> & { onDone: () => void };
+const CLS = ({ onDone, CLSBackup }: Props) => {
+  const device = useSelector(getCurrentDevice);
+
+  useEffect(() => {
+    if (!CLSBackup) onDone();
+  }, [CLSBackup, onDone]);
+
+  return CLSBackup ? (
+    <CustomImageDeviceAction
+      device={device}
+      hexImage={CLSBackup}
+      source={reconstructImage({ hexData: CLSBackup, width: 400, height: 672 }).imageBase64DataUri}
+      padImage={false}
+      onResult={onDone}
+      onSkip={onDone}
+      onTryAnotherImage={() => {}}
+      blockNavigation={() => {}}
+    />
+  ) : null;
+};
+
+export default CLS;
