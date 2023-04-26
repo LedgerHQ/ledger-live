93871b6c8b45f795dae558b68aec5998f82a1701
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2023-04-26 09:23:06 +0200

    remove nyx lcd

--- libs/ledger-live-common/src/families/cosmos/chain/Nyx.ts
+++ libs/ledger-live-common/src/families/cosmos/chain/Nyx.ts
@@ -10,13 +10,12 @@ class Nyx extends CosmosBase {
   ledgerValidator!: string;
   constructor() {
     super();
-    this.lcd = "https://api.nyx.nodes.guru";
     this.stakingDocUrl =
       "https://support.ledger.com/hc/en-us/articles/9604704817821?support=true";
     this.unbondingPeriod = 28;
     this.prefix = "n";
     this.validatorPrefix = `${this.prefix}valoper`;
-    this.minGasprice = 0.25;
+    this.minGasprice = 0;
   }
 }
 
