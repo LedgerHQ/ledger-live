55b9a3c627076df1cb3e66236e1c2bb6be887eeb
Henri Ly <henri.ly-ext@ledger.fr>
2023-05-05 13:21:09 +0200

    fix: speculos bot polkadot

--- libs/coin-polkadot/src/specs.ts
+++ libs/coin-polkadot/src/specs.ts
@@ -77,9 +77,11 @@ const polkadot: AppSpec<Transaction> = {
           .div(1.9 + 0.2 * Math.random())
           .integerValue();
 
-        if (!sibling.used && amount.lt(EXISTENTIAL_DEPOSIT)) {
+        invariant(amount.gte(0), "not enough balance to do a transfer")
+
+        if (sibling.balance.eq(0) && amount.lt(EXISTENTIAL_DEPOSIT)) {
           invariant(
-            account.spendableBalance.gt(
+            account.spendableBalance.gte(
               EXISTENTIAL_DEPOSIT.plus(POLKADOT_MIN_SAFE)
             ),
             "send is too low to activate account"
