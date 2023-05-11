bbda58635240e28dbbdcadb33af9a1a31c0e0bd3
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-05-11 13:14:33 +0200

    fix(llm/EditDeviceName): custom autoFocus implementation (blur if modal)

--- apps/ledger-live-mobile/src/screens/EditDeviceName.tsx
+++ apps/ledger-live-mobile/src/screens/EditDeviceName.tsx
@@ -1,7 +1,15 @@
-import React, { memo, useCallback, useMemo, useState } from "react";
+import React, {
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Trans, useTranslation } from "react-i18next";
 import { connect } from "react-redux";
+import { TextInput as NativeTextInput } from "react-native";
 import { DeviceNameInvalid } from "@ledgerhq/errors";
 import { useToasts } from "@ledgerhq/live-common/notifications/ToastProvider/index";
 import { Button, Text, Icons, Flex } from "@ledgerhq/native-ui";
@@ -45,6 +53,8 @@ function EditDeviceName({ navigation, route, saveBleDeviceName }: Props) {
   const originalName = route.params?.deviceName;
   const device = route.params?.device;
   const deviceInfo = route.params?.deviceInfo;
+
+  const textInputRef = useRef<NativeTextInput | null>(null);
 
   const { t } = useTranslation();
   const { pushToast } = useToasts();
@@ -117,6 +127,17 @@ function EditDeviceName({ navigation, route, saveBleDeviceName }: Props) {
   const disabled =
     !cleanName || !!error || running || cleanName === originalName;
 
+  useEffect(() => {
+    let handle: number;
+    if (running) textInputRef.current?.blur();
+    else {
+      handle = requestAnimationFrame(() => textInputRef.current?.focus());
+    }
+    return () => {
+      handle && cancelAnimationFrame(handle);
+    };
+  }, [running]);
+
   return (
     <KeyboardBackgroundDismiss>
       <SafeAreaView style={{ flex: 1 }}>
@@ -124,10 +145,10 @@ function EditDeviceName({ navigation, route, saveBleDeviceName }: Props) {
           <TrackScreen category="EditDeviceName" />
           <Flex flex={1} p={6} bg="background.main">
             <TextInput
+              ref={textInputRef}
               value={name}
               onChangeText={onChangeText}
               maxLength={maxDeviceName}
-              autoFocus
               selectTextOnFocus
               blurOnSubmit={true}
               clearButtonMode="always"
