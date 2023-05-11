428d54e2ab6d5d7912a269e8a9a146cc62abd781
Henri Ly <henri.ly-ext@ledger.fr>
2023-05-11 16:59:39 +0200

    fix: original image

--- libs/ledger-live-common/src/load/speculos.ts
+++ libs/ledger-live-common/src/load/speculos.ts
@@ -111,7 +111,7 @@ export async function createSpeculosDevice(
     `SPECULOS_APPNAME=${appName}:${appVersion}`,
     "--name",
     `${speculosID}`,
-    "speculos",
+    "ghcr.io/ledgerhq/speculos",
     "--model",
     model.toLowerCase(),
     appPath,
