3e2997474f3fbc3279e2cc011478c034c6d7a41d
Emmanuel <emmanuelm41@gmail.com>
2023-05-12 15:56:16 -0300

    renaming files

--- libs/ledger-live-common/src/families/stacks/bridge/utils/misc.ts
+++ libs/ledger-live-common/src/families/stacks/bridge/utils/misc.ts
@@ -5,20 +5,20 @@ import { Account, Address, Operation } from "@ledgerhq/types-live";
 import {
   makeUnsignedSTXTokenTransfer,
   UnsignedTokenTransferOptions,
-  createMessageSignature,
+  createMessageSignature
 } from "@stacks/transactions";
 
 import {
   GetAccountShape,
-  AccountShapeInfo,
+  AccountShapeInfo
 } from "../../../../bridge/jsHelpers";
 import { decodeAccountId, encodeAccountId } from "../../../../account";
 import {
   fetchBalances,
   fetchBlockHeight,
-  fetchFullTxs,
+  fetchFullTxs
 } from "../../bridge/utils/api";
-import { StacksNetwork, TransactionResponse } from "./types";
+import { StacksNetwork, TransactionResponse } from "./api.types";
 import { getCryptoCurrencyById } from "../../../../currencies";
 import { encodeOperationId } from "../../../../operation";
 
@@ -30,7 +30,7 @@ export const getTxToBroadcast = async (
     value,
     recipients,
     fee,
-    extra: { xpub, nonce, anchorMode, network, memo },
+    extra: { xpub, nonce, anchorMode, network, memo }
   } = operation;
 
   const options: UnsignedTokenTransferOptions = {
@@ -41,7 +41,7 @@ export const getTxToBroadcast = async (
     network: StacksNetwork[network],
     publicKey: xpub,
     fee: new BN(fee.toFixed()),
-    nonce: new BN(nonce.toFixed()),
+    nonce: new BN(nonce.toFixed())
   };
 
   const tx = await makeUnsignedSTXTokenTransfer(options);
@@ -60,69 +60,74 @@ export const getAddress = (a: Account): Address =>
     ? a.freshAddresses[0]
     : { address: a.freshAddress, derivationPath: a.freshAddressPath };
 
-export const mapTxToOps =
-  (accountID, { address }: AccountShapeInfo) =>
-  (tx: TransactionResponse): Operation[] => {
-    const { sender, recipient, amount } = tx.stx_transfers[0];
-    const { tx_id, fee_rate, block_height, burn_block_time, token_transfer } =
-      tx.tx;
-    const { memo: memoHex } = token_transfer;
+export const mapTxToOps = (accountID, { address }: AccountShapeInfo) => (
+  tx: TransactionResponse
+): Operation[] => {
+  const { sender, recipient, amount } = tx.stx_transfers[0];
+  const {
+    tx_id,
+    fee_rate,
+    block_height,
+    burn_block_time,
+    token_transfer
+  } = tx.tx;
+  const { memo: memoHex } = token_transfer;
 
-    const ops: Operation[] = [];
+  const ops: Operation[] = [];
 
-    const date = new Date(burn_block_time * 1000);
-    const value = new BigNumber(amount || "0");
-    const feeToUse = new BigNumber(fee_rate || "0");
+  const date = new Date(burn_block_time * 1000);
+  const value = new BigNumber(amount || "0");
+  const feeToUse = new BigNumber(fee_rate || "0");
 
-    const isSending = address === sender;
-    const isReceiving = address === recipient;
+  const isSending = address === sender;
+  const isReceiving = address === recipient;
 
-    const memo = Buffer.from(memoHex.substring(2), "hex")
-      .toString()
-      .replaceAll("\x00", "");
+  const memo = Buffer.from(memoHex.substring(2), "hex")
+    .toString()
+    .replaceAll("\x00", "");
 
-    if (isSending) {
-      ops.push({
-        id: encodeOperationId(accountID, tx_id, "OUT"),
-        hash: tx_id,
-        type: "OUT",
-        value: value.plus(feeToUse),
-        fee: feeToUse,
-        blockHeight: block_height,
-        blockHash: null,
-        accountId: accountID,
-        senders: [sender],
-        recipients: [recipient],
-        date,
-        extra: {
-          memo,
-        },
-      });
-    }
+  if (isSending) {
+    ops.push({
+      id: encodeOperationId(accountID, tx_id, "OUT"),
+      hash: tx_id,
+      type: "OUT",
+      value: value.plus(feeToUse),
+      fee: feeToUse,
+      blockHeight: block_height,
+      blockHash: null,
+      accountId: accountID,
+      senders: [sender],
+      recipients: [recipient],
+      date,
+      extra: {
+        memo
+      }
+    });
+  }
 
-    if (isReceiving) {
-      ops.push({
-        id: encodeOperationId(accountID, tx_id, "IN"),
-        hash: tx_id,
-        type: "IN",
-        value,
-        fee: feeToUse,
-        blockHeight: block_height,
-        blockHash: null,
-        accountId: accountID,
-        senders: [sender],
-        recipients: [recipient],
-        date,
-        extra: {
-          memo,
-        },
-      });
-    }
+  if (isReceiving) {
+    ops.push({
+      id: encodeOperationId(accountID, tx_id, "IN"),
+      hash: tx_id,
+      type: "IN",
+      value,
+      fee: feeToUse,
+      blockHeight: block_height,
+      blockHash: null,
+      accountId: accountID,
+      senders: [sender],
+      recipients: [recipient],
+      date,
+      extra: {
+        memo
+      }
+    });
+  }
 
-    return ops;
-  };
+  return ops;
+};
 
-export const getAccountShape: GetAccountShape = async (info) => {
+export const getAccountShape: GetAccountShape = async info => {
   const { initialAccount, address, currency, rest = {}, derivationMode } = info;
 
   const publicKey = reconciliatePublicKey(rest.publicKey, initialAccount);
@@ -132,7 +137,7 @@ export const getAccountShape: GetAccountShape = async (info) => {
     version: "2",
     currencyId: currency.id,
     xpubOrAddress: publicKey,
-    derivationMode,
+    derivationMode
   });
 
   const blockHeight = await fetchBlockHeight();
@@ -146,7 +151,7 @@ export const getAccountShape: GetAccountShape = async (info) => {
     balance: new BigNumber(balance.balance),
     spendableBalance: new BigNumber(balance.balance),
     operations: flatMap(rawTxs, mapTxToOps(accountId, info)),
-    blockHeight: blockHeight.chain_tip.block_height,
+    blockHeight: blockHeight.chain_tip.block_height
   };
 
   return result;
