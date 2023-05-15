25859bf39b996808d0ec6228a0e9c2f1ab09eaad
lvndry <lvndry@proton.me>
2023-05-15 16:56:08 +0200

    screeename typo

--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -19,7 +19,7 @@ export enum ScreenName {
   ClaimNftQrScan = "ClaimNftQrScan",
   ClaimRewardsConnectDevice = "ClaimRewardsConnectDevice",
   ClaimRewardsSelectDevice = "ClaimRewardsSelectDevice",
-  ClaimRewardsSendValidationErrorionError = "ClaimRewardsValidationError",
+  ClaimRewardsValidationError = "ClaimRewardsValidationError",
   ClaimRewardsValidationSuccess = "ClaimRewardsValidationSuccess",
   ConfirmPassword = "ConfirmPassword",
   CountervalueSettings = "CountervalueSettings",
@@ -284,6 +284,7 @@ export enum ScreenName {
   ElrondWithdrawValidationError = "ElrondWithdrawValidationError",
   ElrondWithdrawValidationSuccess = "ElrondWithdrawValidationSuccess",
 
+  // Osmosis
   OsmosisDelegationStarted = "OsmosisDelegationStarted",
   OsmosisDelegationValidator = "OsmosisDelegationValidator",
   OsmosisDelegationValidatorSelect = "OsmosisDelegationValidatorSelect",
@@ -312,6 +313,8 @@ export enum ScreenName {
 
   // crypto_org
   CryptoOrgEditMemo = "CryptoOrgEditMemo",
+
+  // Algorand
   AlgorandEditMemo = "AlgorandEditMemo",
   AlgorandClaimRewardsInfo = "AlgorandClaimRewardsInfo",
   AlgorandClaimRewardsStarted = "AlgorandClaimRewardsStarted",
