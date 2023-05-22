d81b267a1cad19ba76aeb3cb575b5438ee16ca43
kallen-ledger <kieran.allen@ledger.fr>
2023-05-22 10:07:21 +0100

    fix: update tests with better naming

--- apps/ledger-live-desktop/src/helpers/generateValidDappURLWithParams.test.ts
+++ apps/ledger-live-desktop/src/helpers/generateValidDappURLWithParams.test.ts
@@ -1,36 +1,34 @@
 import { generateValidDappURLWithParams } from "./generateValidDappURLWithParams";
 
 describe("generateValidDappURLWithParams.test", () => {
-  const manifest1 = {
+  const manifestWithValidURLWithQueryString = {
     params: {
       dappUrl: "https://example.com?existingParam=123",
     },
   };
 
-  const manifest2 = {
+  const manifestWithInvalidURL = {
     params: {
       dappUrl: "invalid-url",
     },
   };
 
-  const manifest3 = {
+  const manifestWithValidURL = {
     params: {
       dappUrl: "https://example.com",
     },
   };
 
-  const queryString1 = {
+  const queryStringToAdd = {
     param1: "value1",
     param2: ["value2", "value3"],
   };
 
-  const queryString2 = {
-    foo: "bar",
-    baz: ["qux", "quux"],
-  };
-
   test("should append query strings to an existing URL and not remove existing query parameters when they exist", () => {
-    const result = generateValidDappURLWithParams(manifest1, queryString1);
+    const result = generateValidDappURLWithParams(
+      manifestWithValidURLWithQueryString,
+      queryStringToAdd,
+    );
     const expected =
       "https://example.com/?existingParam=123&param1=value1&param2=value2&param2=value3";
 
@@ -38,16 +36,8 @@ describe("generateValidDappURLWithParams.test", () => {
     expect(result!.toString()).toBe(expected);
   });
 
-  test("should handle array values in query strings", () => {
-    const result = generateValidDappURLWithParams(manifest1, queryString2);
-    const expected = "https://example.com/?existingParam=123&foo=bar&baz=qux&baz=quux";
-
-    expect(result).toBeDefined();
-    expect(result!.toString()).toBe(expected);
-  });
-
   test("should append a query string to the new url", () => {
-    const result = generateValidDappURLWithParams(manifest3, queryString1);
+    const result = generateValidDappURLWithParams(manifestWithValidURL, queryStringToAdd);
     const expected = "https://example.com/?param1=value1&param2=value2&param2=value3";
 
     expect(result).toBeDefined();
@@ -55,7 +45,7 @@ describe("generateValidDappURLWithParams.test", () => {
   });
 
   test("should not append any additional queries if the query string parameter is undefined", () => {
-    const result = generateValidDappURLWithParams(manifest1);
+    const result = generateValidDappURLWithParams(manifestWithValidURLWithQueryString);
     const expected = "https://example.com/?existingParam=123";
 
     expect(result).toBeDefined();
@@ -63,12 +53,12 @@ describe("generateValidDappURLWithParams.test", () => {
   });
 
   test("should return undefined for an invalid URL", () => {
-    const result = generateValidDappURLWithParams(manifest2, queryString1);
+    const result = generateValidDappURLWithParams(manifestWithInvalidURL, queryStringToAdd);
     expect(result).toBeUndefined();
   });
 
   test("should return undefined for undefined manifest", () => {
-    const result = generateValidDappURLWithParams(undefined, queryString1);
+    const result = generateValidDappURLWithParams(undefined, queryStringToAdd);
 
     expect(result).toBeUndefined();
   });
