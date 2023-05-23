89ca5dda9d068e3c5f6ec87c451f6e27ea6aee06
lambertkevin <kevin.lambert@ledger.fr>
2023-05-23 21:04:43 +0200

    Make Eth/Matic evms able to have tokens in preload

--- libs/coin-evm/src/preload.ts
+++ libs/coin-evm/src/preload.ts
@@ -33,7 +33,14 @@ export const fetchERC20Tokens: (
 
 export async function preload(currency: CryptoCurrency): Promise<ERC20Token[]> {
   const erc20 = await fetchERC20Tokens(currency);
-  addTokens(erc20.map(convertERC20));
+  // This weird thing is here to help the "clone" currencies like
+  // ethereum_as_evm_test_only & polygon_as_evm_test_only
+  // to get tokens despite the different currency_id
+  // registered in the CAL for those chain ids
+  // We should remove this after the merge
+  addTokens(
+    erc20.map(([, ...tokenRest]) => convertERC20([currency.id, ...tokenRest]))
+  );
   return erc20;
 }
 
