cdb1bc043f4363c5983bd31a846ccf99c8ff9d4e
Emmanuel <emmanuelm41@gmail.com>
2023-05-24 12:54:15 -0300

    add abandon seed

--- libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
+++ libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
@@ -84,6 +84,7 @@ const abandonSeedAddresses: Partial<Record<CryptoCurrency["id"], string>> = {
   onomy: "cosmos19rl4cm2hmr8afy4kldpxz3fka4jguq0auqdal4",
   quicksilver: "cosmos19rl4cm2hmr8afy4kldpxz3fka4jguq0auqdal4",
   persistence: "cosmos19rl4cm2hmr8afy4kldpxz3fka4jguq0auqdal4",
+  stacks: "SP3KS7VMY2ZNE6SB88PHR4SKRK2EEPHS8N8MCCBR9",
 };
 
 /**
