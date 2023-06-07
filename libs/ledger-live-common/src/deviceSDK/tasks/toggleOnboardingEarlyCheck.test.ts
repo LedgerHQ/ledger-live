d14ff0bb2b4569bf09a37b449cf2b47650d4cbd1
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-06-07 10:52:27 +0200

    feat: new toggleOnboardingEarlyCheckAction device action (#3497)
    Introducing a new device action (implemented in the device SDK): toggleOnboardingEarlyCheckAction
    with:
    
    - its associated new command and task
    - its associated new onboarding state
    - a hook useToggleOnboardingEarlyCheck for simple usage on LLM and LLD
    - unit tests
    - its associated cli command deviceSDKToggleOnboardingEarlyCheck
    
    This new action uses a new APDU to enter and exit the "early security check"
    blocking step during the onboarding of Stax.

---
+++ libs/ledger-live-common/src/deviceSDK/tasks/toggleOnboardingEarlyCheck.test.ts
@@ -0,0 +1,111 @@
+import { from, of, throwError } from "rxjs";
+import { StatusCodes, TransportStatusError } from "@ledgerhq/errors";
+import { toggleOnboardingEarlyCheckTask } from "./toggleOnboardingEarlyCheck";
+import { toggleOnboardingEarlyCheckCmd } from "../commands/toggleOnboardingEarlyCheck";
+import { withTransport } from "../transports/core";
+import { aTransportRefBuilder } from "../mocks/aTransportRef";
+
+jest.mock("../commands/toggleOnboardingEarlyCheck");
+const mockedToggleOnboardingEarlyCheckCmd = jest.mocked(
+  toggleOnboardingEarlyCheckCmd
+);
+
+jest.mock("../transports/core");
+const mockedWithTransport = jest.mocked(withTransport);
+
+describe("@deviceSDK/tasks/toggleOnboardingEarlyCheckTask", () => {
+  beforeAll(async () => {
+    const transportRef = await aTransportRefBuilder();
+    mockedWithTransport.mockReturnValue((job) => from(job({ transportRef })));
+  });
+
+  describe("When the device is in the expected onboarding state", () => {
+    it("should emit a success event after being able to enter or exit the onboarding early checks step successfully", (done) => {
+      mockedToggleOnboardingEarlyCheckCmd.mockReturnValue(
+        of({ type: "success" })
+      );
+
+      // `enter` and `exit` have the same behavior
+      toggleOnboardingEarlyCheckTask({
+        deviceId: "",
+        toggleType: "exit",
+      }).subscribe({
+        next: (event) => {
+          expect(event.type).toBe("success");
+          done();
+        },
+        error: (error: unknown) => {
+          done(`No error should have been thrown: ${JSON.stringify(error)}`);
+        },
+      });
+    });
+  });
+
+  describe("When the device is neither in the WELCOME and WELCOME_STEP2 onboarding state, and it returns 0x6982", () => {
+    it("should emit a task error event with the correct error type", (done) => {
+      mockedToggleOnboardingEarlyCheckCmd.mockReturnValue(
+        throwError(
+          new TransportStatusError(StatusCodes.SECURITY_STATUS_NOT_SATISFIED)
+        )
+      );
+
+      toggleOnboardingEarlyCheckTask({
+        deviceId: "",
+        toggleType: "enter",
+      }).subscribe({
+        next: (event) => {
+          try {
+            if (event.type === "taskError") {
+              expect(event.error).toBe("DeviceInInvalidState");
+              done();
+            } else {
+              done(`The event is incorrect: ${JSON.stringify(event)}`);
+            }
+          } catch (expectError) {
+            done(expectError);
+          }
+        },
+        error: (error: unknown) => {
+          done(
+            `The error should have been mapped to an event, not thrown: ${JSON.stringify(
+              error
+            )}`
+          );
+        },
+      });
+    });
+  });
+
+  describe("When the command is not respected the expected APDU format", () => {
+    it("should emit a task error event with the correct error type", (done) => {
+      mockedToggleOnboardingEarlyCheckCmd.mockReturnValue(
+        throwError(new TransportStatusError(StatusCodes.INCORRECT_LENGTH))
+      );
+
+      toggleOnboardingEarlyCheckTask({
+        deviceId: "",
+        toggleType: "enter",
+      }).subscribe({
+        next: (event) => {
+          try {
+            if (event.type === "taskError") {
+              expect(event.error).toBe("InternalError");
+              done();
+            } else {
+              done(`The event is incorrect: ${JSON.stringify(event)}`);
+            }
+          } catch (expectError) {
+            done(expectError);
+          }
+        },
+        error: (error: unknown) => {
+          done(
+            `The error should have been mapped to an event, not thrown: ${JSON.stringify(
+              error
+            )}`
+          );
+        },
+      });
+    });
+  });
+});
