b27c91b707636bbb77f8a5ce0551c5a9926e9d3d
lambertkevin <kevin.lambert@ledger.fr>
2023-06-09 11:01:58 +0200

    Move Batcher & metadataCallBatcher to coin-framework

--- libs/ledger-live-common/src/nft/helpers.ts
+++ libs/ledger-live-common/src/nft/helpers.ts
@@ -1,15 +1,5 @@
 import { groupAccountsOperationsByDay } from "../account";
-import type { Batch, BatchElement, Batcher } from "./NftMetadataProvider/types";
-import type {
-  NFTMetadataResponse,
-  Operation,
-  ProtoNFT,
-  NFT,
-  NFTCollectionMetadataResponse,
-  Account,
-} from "@ledgerhq/types-live";
-import { API, apiForCurrency } from "../families/ethereum/api";
-import type { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+import type { Operation, ProtoNFT, NFT, Account } from "@ledgerhq/types-live";
 
 /**
  * Helper to group NFTs by their collection/contract.
@@ -44,105 +34,6 @@ export const getNftKey = (contract: string, tokenId: string, currencyId: string)
 
 export const getNftCollectionKey = (contract: string, currencyId: string): string => {
   return `${currencyId}-${contract}`;
-};
-
-/**
- * Factory to make a metadata API call batcher.
- *
- * It will wait for a complete `tick` to accumulate all the metadata requests
- * before sending it as one call to a given API.
- * Once the response is received, it will then spread the metadata to each request Promise,
- * just like if each request had been made separately.
- */
-const makeBatcher = (
-  call: API["getNFTMetadata"] | API["getNFTCollectionMetadata"],
-  chainId: number,
-): Batcher =>
-  (() => {
-    const queue: BatchElement[] = [];
-
-    let debounce;
-    const timeoutBatchCall = () => {
-      // Clear the previous scheduled call if it was existing
-      clearTimeout(debounce);
-
-      // Schedule a new call with the whole batch
-      debounce = setTimeout(() => {
-        // Seperate each batch element properties into arrays by type and index
-        const { elements, resolvers, rejecters } = queue.reduce(
-          (acc, { element, resolve, reject }) => {
-            acc.elements.push(element);
-            acc.resolvers.push(resolve);
-            acc.rejecters.push(reject);
-
-            return acc;
-          },
-          { elements: [], resolvers: [], rejecters: [] } as Batch,
-        );
-        // Empty the queue
-        queue.length = 0;
-
-        // Make the call with all the couples of contract and tokenId at once
-        call(elements, chainId.toString())
-          .then((res: any) => {
-            // Resolve each batch element with its own resolver and only its response
-            res.forEach((metadata, index) => resolvers[index](metadata));
-          })
-          .catch(err => {
-            // Reject all batch element with the error
-            rejecters.forEach(reject => reject(err));
-          });
-      });
-    };
-
-    return {
-      // Load the metadata for a given couple contract + tokenId
-      load(
-        element:
-          | {
-              contract: string;
-              tokenId: string;
-            }
-          | { contract: string },
-      ): Promise<NFTMetadataResponse | NFTCollectionMetadataResponse> {
-        return new Promise((resolve, reject) => {
-          queue.push({ element, resolve, reject });
-          timeoutBatchCall();
-        });
-      },
-    };
-  })();
-
-const batchersMap = new Map();
-
-/**
- * In order to `instanciate`/make only 1 batcher by currency,
- * they're `cached` in a Map and retrieved by this method
- * This method is still EVM based for now but can be improved
- * to implement an even more generic solution
- */
-export const metadataCallBatcher = (
-  currency: CryptoCurrency,
-): { loadNft: Batcher["load"]; loadCollection: Batcher["load"] } => {
-  const api: API = apiForCurrency(currency);
-  const chainId = currency?.ethereumLikeInfo?.chainId;
-
-  if (!chainId || !api) {
-    throw new Error("Ethereum: No API or chainId for this Currency");
-  }
-
-  if (!batchersMap.has(currency.id)) {
-    batchersMap.set(currency.id, {
-      nft: makeBatcher(api.getNFTMetadata, chainId),
-      collection: makeBatcher(api.getNFTCollectionMetadata, chainId),
-    });
-  }
-
-  const batchers = batchersMap.get(currency.id);
-  return {
-    loadNft: batchers.nft.load,
-    loadCollection: batchers.collection.load,
-  };
 };
 
 export const getNFT = (
