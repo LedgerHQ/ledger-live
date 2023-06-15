690f5c1f8b27ca1c94822a7f0d703c547da9b86f
Juan Cortés Ross <juan.cortes.ross@gmail.com>
2023-06-15 16:26:35 +0200

    feat: make listAppsV2 feature flagged

--- libs/ledger-live-common/src/apps/hw.ts
+++ libs/ledger-live-common/src/apps/hw.ts
@@ -2,7 +2,6 @@ import Transport from "@ledgerhq/hw-transport";
 import { App, DeviceInfo } from "@ledgerhq/types-live";
 import { Observable } from "rxjs";
 import { AppOp, Exec } from "./types";
-import { getEnv } from "../env";
 import installApp from "../hw/installApp";
 import uninstallApp from "../hw/uninstallApp";
 import type { ListAppsEvent } from "./types";
@@ -24,8 +23,9 @@ export const execWithTransport =
  *   currency and parent application. And therefor we no longer need the version
  *   check that broke that dependency after a certain version for ETH and BTC.
  * - Remove all the legacy v1 code, and tests.
- * - Remove the env definition and all the forks based on it here.
+ * - Cleanup the feature flag that governs this.
  */
-// Nb Written this way to respect runtime changes to the env.
+let listAppsV2Enabled = false;
+export const enableListAppsV2 = (enabled: boolean) => (listAppsV2Enabled = enabled);
 export const listApps = (transport: Transport, deviceInfo: DeviceInfo): Observable<ListAppsEvent> =>
-  getEnv("LIST_APPS_V2") ? listAppsV2(transport, deviceInfo) : listAppsV1(transport, deviceInfo);
+  listAppsV2Enabled ? listAppsV2(transport, deviceInfo) : listAppsV1(transport, deviceInfo);
