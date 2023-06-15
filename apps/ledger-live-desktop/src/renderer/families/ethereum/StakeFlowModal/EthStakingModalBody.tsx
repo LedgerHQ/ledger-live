d7b70666a1dd3ca4c625de6f3ceaa9a7b59614f8
sarneijim <sarneijim@gmail.com>
2023-06-15 11:07:07 +0100

    chore: change img source path

--- apps/ledger-live-desktop/src/renderer/families/ethereum/StakeFlowModal/EthStakingModalBody.tsx
+++ apps/ledger-live-desktop/src/renderer/families/ethereum/StakeFlowModal/EthStakingModalBody.tsx
@@ -12,7 +12,7 @@ import {
 } from "~/renderer/modals/Receive/steps/StepReceiveStakingFlow";
 import CheckBox from "~/renderer/components/CheckBox";
 import { appendQueryParamsToDappURL } from "@ledgerhq/live-common/platform/utils/appendQueryParamsToDappURL";
-import EthStakeIllustration from "./assets/EthStakeIllustration";
+import EthStakeIllustration from "~/renderer/components";
 import { ListProvider, ListProviders } from "./types";
 import { getTrackProperties } from "./utils/getTrackProperties";
 
