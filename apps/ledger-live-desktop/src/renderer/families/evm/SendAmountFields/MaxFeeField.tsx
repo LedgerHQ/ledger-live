db616a21cdd6c37119081111be4bd17f084c4629
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-06-15 16:27:24 +0200

    add nextBaseFee to feeData
    o make it available to UI

--- apps/ledger-live-desktop/src/renderer/families/evm/SendAmountFields/MaxFeeField.tsx
+++ apps/ledger-live-desktop/src/renderer/families/evm/SendAmountFields/MaxFeeField.tsx
@@ -1,19 +1,19 @@
 // FIXME: removed nextBaseFeeValue for now because data not available at the time
 import { getMainAccount } from "@ledgerhq/live-common/account/index";
 import { getAccountBridge } from "@ledgerhq/live-common/bridge/index";
-// import { formatCurrencyUnit } from "@ledgerhq/live-common/currencies/index";
+import { formatCurrencyUnit } from "@ledgerhq/live-common/currencies/index";
 import { Transaction } from "@ledgerhq/coin-evm/types";
 import { AccountBridge } from "@ledgerhq/types-live";
 import BigNumber from "bignumber.js";
 import invariant from "invariant";
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { urls } from "~/config/urls";
 import { track } from "~/renderer/analytics/segment";
 import Box from "~/renderer/components/Box";
 import InputCurrency from "~/renderer/components/InputCurrency";
-// import Label from "~/renderer/components/Label";
+import Label from "~/renderer/components/Label";
 import LabelWithExternalIcon from "~/renderer/components/LabelWithExternalIcon";
 import TranslatedError from "~/renderer/components/TranslatedError";
 import { openURL } from "~/renderer/linking";
@@ -37,14 +37,14 @@ const WarningDisplay = styled(Box)`
   color: ${p => p.theme.colors.warning};
 `;
 
-// const FeesValues = styled.span`
-//   color: ${p => p.theme.colors.neutral.c90};
-// `;
+const FeesValues = styled.span`
+  color: ${p => p.theme.colors.neutral.c90};
+`;
 
-// const WhiteSpacedLabel = styled(Label)`
-//   white-space: pre;
-//   color: ${p => p.theme.colors.neutral.c60};
-// `;
+const WhiteSpacedLabel = styled(Label)`
+  white-space: pre;
+  color: ${p => p.theme.colors.neutral.c60};
+`;
 
 const FeesField: NonNullable<EvmFamily["sendAmountFields"]>["component"] = ({
   account,
@@ -70,28 +70,19 @@ const FeesField: NonNullable<EvmFamily["sendAmountFields"]>["component"] = ({
     [bridge, transaction, updateTransaction],
   );
 
-  // const defaultMaxFeePerGas = useMemo(
-  //   () =>
-  //     transaction.gasOptions?.medium.maxFeePerGas
-  //       ?.times(getEnv("EIP1559_BASE_FEE_MULTIPLIER"))
-  //       .plus(transaction?.maxPriorityFeePerGas || 0)
-  //       .integerValue(),
-  //   [transaction.maxPriorityFeePerGas, transaction.gasOptions?.medium.maxFeePerGas],
-  // );
-
   const maxFeePerGas = transaction.maxFeePerGas || transaction.gasOptions?.medium.maxFeePerGas;
   const { units } = mainAccount.currency;
   const unit = units.length > 1 ? units[1] : units[0];
   const unitName = unit.code;
 
-  // const nextBaseFeeValue = useMemo(
-  //   () =>
-  //     formatCurrencyUnit(unit, transaction.gasOptions?.medium.maxFeePerGas || new BigNumber(0), {
-  //       showCode: true,
-  //       disableRounding: true,
-  //     }),
-  //   [transaction.gasOptions?.medium.maxFeePerGas, unit],
-  // );
+  const nextBaseFeeValue = useMemo(
+    () =>
+      formatCurrencyUnit(unit, transaction.gasOptions?.medium.nextBaseFee || new BigNumber(0), {
+        showCode: true,
+        disableRounding: true,
+      }),
+    [transaction.gasOptions?.medium.nextBaseFee, unit],
+  );
 
   const validTransactionError = status.errors.maxFee;
   const validTransactionWarning = status.warnings.maxFee;
@@ -127,10 +118,10 @@ const FeesField: NonNullable<EvmFamily["sendAmountFields"]>["component"] = ({
           </WarningDisplay>
         ) : null}
       </ErrorContainer>
-      {/* <WhiteSpacedLabel>
+      <WhiteSpacedLabel>
         {`${t("send.steps.details.nextBlock")} : `}
         <FeesValues>{nextBaseFeeValue}</FeesValues>
-      </WhiteSpacedLabel> */}
+      </WhiteSpacedLabel>
     </Box>
   );
 };
