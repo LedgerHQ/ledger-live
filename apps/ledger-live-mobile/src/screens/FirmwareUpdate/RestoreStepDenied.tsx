f44c990bf3506f7c27648924bea8e4812511297a
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-06-15 16:48:30 +0200

    add(llm/fwupdate): restore step denied analytics

--- apps/ledger-live-mobile/src/screens/FirmwareUpdate/RestoreStepDenied.tsx
+++ apps/ledger-live-mobile/src/screens/FirmwareUpdate/RestoreStepDenied.tsx
@@ -5,6 +5,13 @@ import { TFunction } from "i18next";
 import { getDeviceModel } from "@ledgerhq/devices";
 import Button from "../../components/wrappedUi/Button";
 import Link from "../../components/wrappedUi/Link";
+import { TrackScreen } from "../../analytics";
+import { UserRefusedAllowManager } from "@ledgerhq/errors";
+import {
+  LanguageInstallRefusedOnDevice,
+  ImageLoadRefusedOnDevice,
+  ImageCommitRefusedOnDevice,
+} from "@ledgerhq/live-common/errors";
 
 export const RestoreStepDenied = ({
   t,
@@ -19,8 +26,19 @@ export const RestoreStepDenied = ({
   onPressSkip: () => void;
   stepDeniedError: Error;
 }) => {
+  const deviceName = getDeviceModel(device.modelId).productName;
+  const analyticsDrawerName =
+    stepDeniedError instanceof LanguageInstallRefusedOnDevice
+      ? `Error: the language change was cancelled on ${deviceName}`
+      : stepDeniedError instanceof ImageLoadRefusedOnDevice ||
+        stepDeniedError instanceof ImageCommitRefusedOnDevice
+      ? `Error: the restoration of lock screen picture was cancelled on ${deviceName}`
+      : stepDeniedError instanceof UserRefusedAllowManager
+      ? `Error: the restoration of apps was cancelled on ${deviceName}`
+      : `Error: ${stepDeniedError.name}`;
   return (
     <Flex alignItems="center" justifyContent="center" px={1}>
+      <TrackScreen category={analyticsDrawerName} refreshSource={false} />
       <IconBadge iconColor="primary.c100" iconSize={32} Icon={Icons.InfoAltFillMedium} />
       <Text fontSize={7} fontWeight="semiBold" textAlign="center" mt={6}>
         {t(`FirmwareUpdate.steps.restoreSettings.errors.${stepDeniedError.name}`, {
@@ -30,11 +48,33 @@ export const RestoreStepDenied = ({
       <Text fontSize={4} textAlign="center" color="neutral.c80" mt={6}>
         {t("FirmwareUpdate.steps.restoreSettings.errors.description")}
       </Text>
-      <Button type="main" outline={false} onPress={onPressRetry} mt={8} alignSelf="stretch">
+      <Button
+        event="button_clicked"
+        eventProperties={{
+          button: "Retry",
+          screen: "Firmware update",
+          drawer: analyticsDrawerName,
+        }}
+        type="main"
+        outline={false}
+        onPress={onPressRetry}
+        mt={8}
+        alignSelf="stretch"
+      >
         {t("common.retry")}
       </Button>
       <Flex mt={8} mb={6} alignSelf="stretch">
-        <Link onPress={onPressSkip}>{t("common.skip")}</Link>
+        <Link
+          event="button_clicked"
+          eventProperties={{
+            button: "Skip",
+            screen: "Firmware update",
+            drawer: analyticsDrawerName,
+          }}
+          onPress={onPressSkip}
+        >
+          {t("common.skip")}
+        </Link>
       </Flex>
     </Flex>
   );
