3e3ce1d99db940479e4cd16edc0f3c890ef8b1d3
lambertkevin <kevin.lambert@ledger.fr>
2023-06-16 21:17:18 +0200

    Missing import in bridge

--- libs/coin-evm/src/bridge/js.ts
+++ libs/coin-evm/src/bridge/js.ts
@@ -16,8 +16,8 @@ import { createTransaction } from "../createTransaction";
 import { buildSignOperation } from "../signOperation";
 import { hydrate, preload } from "../preload";
 import nftResolvers from "../nftResolvers";
-import getAddress from "../hw-getAddress";
 import { broadcast } from "../broadcast";
+import resolver from "../hw-getAddress";
 
 const updateTransaction: AccountBridge<EvmTransaction>["updateTransaction"] = (
   transaction,
