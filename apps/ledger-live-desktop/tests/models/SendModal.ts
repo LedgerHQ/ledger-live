2e75c9950d8fe5e494e88db6ce5f026b56b9734e
Henri Ly <henri.ly-ext@ledger.fr>
2023-06-22 17:08:43 +0200

    fix: lint

--- apps/ledger-live-desktop/tests/models/SendModal.ts
+++ apps/ledger-live-desktop/tests/models/SendModal.ts
@@ -11,7 +11,7 @@ export class SendModal extends Modal {
     super(page);
     this.page = page;
     this.drowdownAccount = this.page.locator('[data-test-id="modal-content"] svg').nth(1);
-    this.recipientInput = this.page.getByPlaceholder('Enter');
+    this.recipientInput = this.page.getByPlaceholder("Enter");
   }
 
   async selectAccount(name: string) {
