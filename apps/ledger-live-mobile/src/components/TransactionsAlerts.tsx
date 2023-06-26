f83b468a5e41e72c0274362f7e28dfa1f5eff1e0
Come Grellard <come.grellard@ledger.fr>
2023-06-26 16:37:31 +0200

    feat(llm/llc): tx alerts fix issue when registering address imported from lld

--- apps/ledger-live-mobile/src/components/TransactionsAlerts.tsx
+++ apps/ledger-live-mobile/src/components/TransactionsAlerts.tsx
@@ -26,30 +26,25 @@ const TransactionsAlerts = () => {
     [accounts, supportedChainsIds],
   );
   const refAccounts = useRef<Account[]>([]);
-
-  const syncTransactionsAlerts = async (newAccounts: Account[], removedAccounts: Account[]) => {
-    if (chainwatchBaseUrl) {
-      const { user } = await getOrCreateUser();
-      updateTransactionsAlertsAddresses(
-        user.id,
-        chainwatchBaseUrl,
-        supportedChains,
-        newAccounts,
-        removedAccounts,
-      );
-    }
-  };
+  const refFeatureEnabled = useRef<boolean>(false);
+  const refNotifSettings = useRef<boolean>(false);
 
   useEffect(() => {
     if (!chainwatchBaseUrl) return;
 
     // If the FF is disabled or if the transactionsAlerts toggle is turned off in the settings we stop tracking all addresses for this user
-    if (!featureTransactionsAlerts?.enabled || !notifications.transactionsAlertsCategory) {
+    if (
+      (!featureTransactionsAlerts?.enabled && refFeatureEnabled.current) ||
+      (!notifications.transactionsAlertsCategory && refNotifSettings.current)
+    ) {
       getOrCreateUser().then(({ user }) => {
         deleteUserChainwatchAccounts(user.id, chainwatchBaseUrl, supportedChains);
       });
-      return;
     }
+    refFeatureEnabled.current = featureTransactionsAlerts?.enabled;
+    refNotifSettings.current = notifications.transactionsAlertsCategory;
+
+    if (!featureTransactionsAlerts?.enabled || !notifications.transactionsAlertsCategory) return;
 
     const newAccounts = accountsFilteredBySupportedChains.filter(
       account => !refAccounts.current.find(refAccount => refAccount.id === account.id),
@@ -60,9 +55,19 @@ const TransactionsAlerts = () => {
     );
 
     if (newAccounts.length > 0 || removedAccounts.length > 0) {
-      syncTransactionsAlerts(newAccounts, removedAccounts);
+      getOrCreateUser().then(({ user }) => {
+        updateTransactionsAlertsAddresses(
+          user.id,
+          chainwatchBaseUrl,
+          supportedChains,
+          newAccounts,
+          removedAccounts,
+        );
+      });
     }
     refAccounts.current = accountsFilteredBySupportedChains;
+    refFeatureEnabled.current = featureTransactionsAlerts?.enabled;
+    refNotifSettings.current = notifications.transactionsAlertsCategory;
   }, [
     featureTransactionsAlerts?.enabled,
     chainwatchBaseUrl,
