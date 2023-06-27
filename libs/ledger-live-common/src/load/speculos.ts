62b9c05199b835c6d5d9d84a78130177fa8960c8
Henri Ly <henri.ly-ext@ledger.fr>
2023-06-27 11:47:59 +0200

    fix: with good docker image

--- libs/ledger-live-common/src/load/speculos.ts
+++ libs/ledger-live-common/src/load/speculos.ts
@@ -138,7 +138,7 @@ export async function createSpeculosDevice(
     `SPECULOS_APPNAME=${appName}:${appVersion}`,
     "--name",
     `${speculosID}`,
-    "speculos",
+    "ghcr.io/ledgerhq/speculos",
     "--model",
     model.toLowerCase(),
     appPath,
