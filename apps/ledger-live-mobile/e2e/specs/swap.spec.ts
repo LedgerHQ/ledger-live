0b5d222b300415c2dc6297d03b8dcbab784f2d21
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-06-27 14:10:56 +0200

    feat: fixed account selection step and made some prettier changes

--- apps/ledger-live-mobile/e2e/specs/swap.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/swap.spec.ts
@@ -2,13 +2,14 @@ import { expect } from "detox";
 import { loadConfig } from "../bridge/server";
 import PortfolioPage from "../models/wallet/portfolioPage";
 import SwapFormPage from "../models/trade/swapFormPage";
+import { delay } from "../helpers";
 
 let portfolioPage: PortfolioPage;
 let swapPage: SwapFormPage;
 
 describe("Swap", () => {
   beforeAll(async () => {
-    await loadConfig("1AccountBTC1AccountETHReadOnlyFalse", true);
+    loadConfig("1AccountBTC1AccountETHReadOnlyFalse", true);
     portfolioPage = new PortfolioPage();
     swapPage = new SwapFormPage();
   });
