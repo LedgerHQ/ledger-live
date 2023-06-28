7c4d0d167578155564fe10e744ef20d579c7f44f
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-06-28 12:29:09 +0200

    feat(llm/debug): debug setting to simulate LL has seen Stax once

--- apps/ledger-live-mobile/src/actions/settings.ts
+++ apps/ledger-live-mobile/src/actions/settings.ts
@@ -63,6 +63,7 @@ import {
   SettingsSetCustomImageTypePayload,
   SettingsSetGeneralTermsVersionAccepted,
   SettingsSetOnboardingTypePayload,
+  SettingsSetKnownDeviceModelIdsPayload,
 } from "./types";
 import { ImageType } from "../components/CustomImage/types";
 
@@ -179,6 +180,9 @@ export const setLastSeenDeviceInfo = createAction<SettingsLastSeenDeviceInfoPayl
 );
 export const setLastSeenDeviceLanguageId = createAction<SettingsLastSeenDeviceLanguagePayload>(
   SettingsActionTypes.LAST_SEEN_DEVICE_LANGUAGE_ID,
+);
+export const setKnownDeviceModelIds = createAction<SettingsSetKnownDeviceModelIdsPayload>(
+  SettingsActionTypes.SET_KNOWN_DEVICE_MODEL_IDS,
 );
 const setHasSeenStaxEnabledNftsPopupAction =
   createAction<SettingsSetHasSeenStaxEnabledNftsPopupPayload>(
