53945e1dab860a56e6c7db88f8e69894c1bc4a74
KVNLS <kevin.leseigle@gmail.com>
2023-06-30 13:02:36 +0200

    fix: remove INITAL_STATE from createStore as already handled by handleActions method

--- apps/ledger-live-mobile/src/context/LedgerStore.tsx
+++ apps/ledger-live-mobile/src/context/LedgerStore.tsx
@@ -4,7 +4,6 @@ import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import { importPostOnboardingState } from "@ledgerhq/live-common/postOnboarding/actions";
 import { CounterValuesStateRaw } from "@ledgerhq/live-common/countervalues/types";
-import { initialState as postOnboardingState } from "@ledgerhq/live-common/postOnboarding/reducer";
 import { findCryptoCurrencyById } from "@ledgerhq/live-common/currencies/index";
 import {
   getAccounts,
@@ -20,33 +19,11 @@ import { importStore as importAccounts } from "../actions/accounts";
 import { importBle } from "../actions/ble";
 import { updateProtectData, updateProtectStatus } from "../actions/protect";
 import { INITIAL_STATE as settingsState, supportedCountervalues } from "../reducers/settings";
-import { INITIAL_STATE as accountsState } from "../reducers/accounts";
-import { INITIAL_STATE as appstateState } from "../reducers/appstate";
-import { INITIAL_STATE as bleState } from "../reducers/ble";
-import { INITIAL_STATE as notificationsState } from "../reducers/notifications";
-import { INITIAL_STATE as swapState } from "../reducers/swap";
-import { INITIAL_STATE as ratingsState } from "../reducers/ratings";
-import { INITIAL_STATE as walletconnectState } from "../reducers/walletconnect";
-import { INITIAL_STATE as dynamicContentState } from "../reducers/dynamicContent";
-import { INITIAL_STATE as protectState } from "../reducers/protect";
-import type { State } from "../reducers/types";
 import { listCachedCurrencyIds, hydrateCurrency } from "../bridge/cache";
 
-const INITIAL_STATE: State = {
-  accounts: accountsState,
-  appstate: appstateState,
-  ble: bleState,
-  notifications: notificationsState,
-  ratings: ratingsState,
-  settings: settingsState,
-  swap: swapState,
-  walletconnect: walletconnectState,
-  postOnboarding: postOnboardingState,
-  dynamicContent: dynamicContentState,
-  protect: protectState,
-};
 
-export const store = createStore(reducers, INITIAL_STATE, compose(applyMiddleware(thunk)));
+
+export const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 export type StoreType = typeof store;
 
