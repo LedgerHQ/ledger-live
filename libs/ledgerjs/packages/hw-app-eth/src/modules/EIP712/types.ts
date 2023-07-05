94f2e5e58209ee9ebf8d86bb9ded2becfcca3271
lambertkevin <kevin.lambert@ledger.fr>
2023-07-05 21:27:05 +0200

    Use `evm-tools` in hw-app-eth for EIP712 CAL

--- libs/ledgerjs/packages/hw-app-eth/src/modules/EIP712/types.ts
+++ libs/ledgerjs/packages/hw-app-eth/src/modules/EIP712/types.ts
@@ -1,39 +1,3 @@
-export type EIP712Message = {
-  domain: EIP712MessageDomain;
-  types: EIP712MessageTypes;
-  primaryType: string;
-  message: Record<string, unknown>;
-};
-
-export type EIP712MessageDomain = Partial<{
-  name: string;
-  chainId: number;
-  version: string;
-  verifyingContract: string;
-  salt: string;
-}>;
-
-export type EIP712MessageTypesEntry = {
-  name: string;
-  type: string;
-};
-
-export type EIP712MessageTypes = {
-  EIP712Domain: EIP712MessageTypesEntry[];
-  [key: string]: EIP712MessageTypesEntry[];
-};
-
-export type StructDefData = Required<
-  | {
-      structType: "name";
-      value: string;
-    }
-  | {
-      structType: "field";
-      value: Buffer;
-    }
->;
-
 export type StructImplemData = Required<
   | {
       structType: "root";
@@ -50,6 +14,17 @@ export type StructImplemData = Required<
         type: string;
         sizeInBits: number | undefined;
       }>;
+    }
+>;
+
+export type StructDefData = Required<
+  | {
+      structType: "name";
+      value: string;
+    }
+  | {
+      structType: "field";
+      value: Buffer;
     }
 >;
 
