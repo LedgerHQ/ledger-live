2be7d88a10e0dc9dd40a4b856837baa66ecacaf3
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-07-05 16:28:52 +0200

    feat(lld/earlysecuritychecks): allow manager & locked device drawers

---
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/SoftwareCheckLockedDeviceDrawer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Flex, Text } from "@ledgerhq/react-ui";
+import { DeviceModelId } from "@ledgerhq/devices";
+import { useTheme } from "styled-components";
+import Animation from "~/renderer/animations";
+import { getDeviceAnimation } from "~/renderer/components/DeviceAction/animations";
+
+export type Props = {
+  deviceModelId: DeviceModelId;
+  productName: string;
+};
+
+const SoftwareCheckLockedDeviceModal = ({ deviceModelId, productName }: Props) => {
+  const { t } = useTranslation();
+  const theme = useTheme();
+
+  return (
+    <Flex flexDirection="column" justifyContent="center" alignItems="center" height="100%">
+      <Animation
+        animation={getDeviceAnimation(deviceModelId, theme.theme, "enterPinCode") as object}
+      />
+      <Text variant="h5Inter" fontWeight="semiBold" mt={6}>
+        {t("syncOnboarding.manual.softwareCheckLockedDeviceModal.title", {
+          deviceName: productName,
+        })}
+      </Text>
+    </Flex>
+  );
+};
+
+export default SoftwareCheckLockedDeviceModal;
