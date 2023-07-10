88a516e8889104468f566a54df9d8986a7142075
haammar-ledger <hakim.aammar-ext@ledger.fr>
2023-07-10 11:35:56 +0200

    Adapt signMessage.ts following merge of develop

--- libs/ledger-live-common/src/families/stacks/hw-signMessage.ts
+++ libs/ledger-live-common/src/families/stacks/hw-signMessage.ts
@@ -3,13 +3,13 @@ import BlockstackApp from "@zondax/ledger-stacks";
 import type { SignMessage, Result } from "../../hw/signMessage/types";
 import { getBufferFromString, getPath, throwIfError } from "./utils";
 
-const signMessage: SignMessage = async (transport, { path, message }): Promise<Result> => {
+const signMessage: SignMessage = async (transport, account, { message }): Promise<Result> => {
   const blockstack = new BlockstackApp(transport);
 
   if (!message) throw new Error(`Message cannot be empty`);
   if (typeof message !== "string") throw new Error(`Message must be string`);
 
-  const r = await blockstack.sign(getPath(path), getBufferFromString(message));
+  const r = await blockstack.sign(getPath(account.freshAddressPath), getBufferFromString(message));
   throwIfError(r);
 
   return {
