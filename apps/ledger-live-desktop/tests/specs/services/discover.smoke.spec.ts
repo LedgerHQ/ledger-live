0f99498e21276979d024577b0989e6879227afec
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-07-11 10:46:01 +0200

    feat: added live app webview page object model

--- apps/ledger-live-desktop/tests/specs/services/discover.smoke.spec.ts
+++ apps/ledger-live-desktop/tests/specs/services/discover.smoke.spec.ts
@@ -5,48 +5,29 @@ import { Layout } from "../../models/Layout";
 import { Drawer } from "../../models/Drawer";
 import { Modal } from "../../models/Modal";
 import { DeviceAction } from "../../models/DeviceAction";
-import { startDummyServer, getLiveAppManifest, stopDummyServer } from "@ledgerhq/test-utils";
+import { stopDummyServer } from "@ledgerhq/test-utils";
+import { LiveAppWebview } from "../../models/LiveAppWebview";
 
 test.use({ userdata: "1AccountBTC1AccountETH" });
 
 let continueTest = false;
 
-test.beforeAll(async ({ request }) => {
+test.beforeAll(async () => {
   // Check that dummy app in tests/utils/dummy-app-build has been started successfully
-  try {
-    const port = await startDummyServer("dummy-live-app/build");
-    const url = `http://localhost:${port}`;
-    const response = await request.get(url);
-    if (response.ok()) {
-      continueTest = true;
-      console.info(`========> Dummy test app successfully running on port ${port}! <=========`);
-      process.env.MOCK_REMOTE_LIVE_MANIFEST = JSON.stringify(
-        getLiveAppManifest({
-          id: "dummy-live-app",
-          url,
-          permissions: [{ method: "*" }],
-          content: {
-            shortDescription: {
-              en: "App to test the Live App SDK",
-            },
-            description: {
-              en: "App to test the Live App SDK with Playwright",
-            },
-          },
-        }),
-      );
-    } else {
-      throw new Error("Ping response != 200, got: " + response.status);
-    }
-  } catch (error) {
-    console.warn(`========> Dummy test app not running! <=========`);
-    console.error(error);
+  continueTest = await LiveAppWebview.startLiveApp("dummy-live-app/build", {
+    name: "Dummy Live App",
+    id: "dummy-live-app",
+    permissions: [{ method: "*" }],
+  });
+
+  if (!continueTest) {
+    console.warn("Stopping Buy/Sell test setup");
+    return; // need to make this a proper ignore/jest warning
   }
 });
 
 test.afterAll(async () => {
   await stopDummyServer();
-  console.info(`========> Dummy test app stopped <=========`);
   delete process.env.MOCK_REMOTE_LIVE_MANIFEST;
 });
 
@@ -56,6 +37,7 @@ test("Discover", async ({ page }) => {
   if (!continueTest) return;
 
   const discoverPage = new DiscoverPage(page);
+  const liveAppWebview = new LiveAppWebview(page);
   const drawer = new Drawer(page);
   const modal = new Modal(page);
   const layout = new Layout(page);
@@ -66,59 +48,59 @@ test("Discover", async ({ page }) => {
     await discoverPage.openTestApp();
     await drawer.continue();
     await drawer.waitForDrawerToDisappear(); // macos runner was having screenshot issues here because the drawer wasn't disappearing fast enough
-    await discoverPage.waitForCorrectTextInWebview("Ledger Live Dummy Test App");
+    await liveAppWebview.waitForCorrectTextInWebview("Ledger Live Dummy Test App");
   });
 
   await test.step("List all accounts", async () => {
-    await discoverPage.getAccountsList();
-    await discoverPage.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_0:");
-    await discoverPage.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_1:");
+    await liveAppWebview.getAccountsList();
+    await liveAppWebview.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_0:");
+    await liveAppWebview.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_1:");
   });
 
   await test.step("Request Account drawer - open", async () => {
-    await discoverPage.requestAsset();
-    await expect(discoverPage.selectAssetTitle).toBeVisible();
+    await liveAppWebview.requestAsset();
+    await expect(liveAppWebview.selectAssetTitle).toBeVisible();
   });
 
   await test.step("Request Account - select asset", async () => {
-    await discoverPage.selectAsset();
-    await expect(discoverPage.selectAccountTitle).toBeVisible();
-    await expect(discoverPage.selectAssetSearchBar).toBeEnabled();
+    await liveAppWebview.selectAsset();
+    await expect(liveAppWebview.selectAccountTitle).toBeVisible();
+    await expect(liveAppWebview.selectAssetSearchBar).toBeEnabled();
   });
 
   await test.step("Request Account - select BTC", async () => {
-    await discoverPage.selectAccount();
+    await liveAppWebview.selectAccount();
     await drawer.waitForDrawerToDisappear();
-    await discoverPage.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_0:");
+    await liveAppWebview.waitForCorrectTextInWebview("mock:1:bitcoin:true_bitcoin_0:");
   });
 
   await test.step("List currencies", async () => {
-    await discoverPage.listCurrencies();
-    await discoverPage.waitForCorrectTextInWebview("CryptoCurrency");
+    await liveAppWebview.listCurrencies();
+    await liveAppWebview.waitForCorrectTextInWebview("CryptoCurrency");
   });
 
   await test.step("Verify Address - modal", async () => {
-    await discoverPage.verifyAddress();
+    await liveAppWebview.verifyAddress();
     await deviceAction.openApp();
     await expect.soft(page).toHaveScreenshot("live-app-verify-address.png");
   });
 
   await test.step("Verify Address - address output", async () => {
     await modal.waitForModalToDisappear();
-    await discoverPage.waitForCorrectTextInWebview("1xey");
+    await liveAppWebview.waitForCorrectTextInWebview("1xey");
   });
 
   await test.step("Sign Transaction - info modal", async () => {
-    await discoverPage.signTransaction();
+    await liveAppWebview.signTransaction();
     await expect.soft(page).toHaveScreenshot("live-app-sign-transaction-info.png", {
       timeout: 20000,
     });
   });
 
   await test.step("Sign Transaction - confirmation modal", async () => {
-    await discoverPage.continueToSignTransaction();
+    await liveAppWebview.continueToSignTransaction();
     await layout.waitForLoadingSpinnerToHaveDisappeared();
-    await discoverPage.waitForConfirmationScreenToBeDisplayed();
+    await liveAppWebview.waitForConfirmationScreenToBeDisplayed();
     await expect(page.locator("text=0.0000123")).toBeVisible();
     await expect(page.locator("text=0.0000025")).toBeVisible();
     // This screenshot is flaky as the loading spinner appears again after this confirm modal, and on slow CI runners the screenshot can take a picture of this instead of the confirm.
@@ -127,6 +109,6 @@ test("Discover", async ({ page }) => {
 
   await test.step("Sign Transaction - signature output", async () => {
     await modal.waitForModalToDisappear();
-    await discoverPage.waitForCorrectTextInWebview("mock_op_100_mock:1:bitcoin:true_bitcoin_0:");
+    await liveAppWebview.waitForCorrectTextInWebview("mock_op_100_mock:1:bitcoin:true_bitcoin_0:");
   });
 });
