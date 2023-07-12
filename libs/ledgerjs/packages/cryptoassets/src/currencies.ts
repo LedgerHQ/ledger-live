9a67168839f3176d177159add1f0ae6091034538
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2023-07-12 11:54:01 +0200

    edit deviceTicker

--- libs/ledgerjs/packages/cryptoassets/src/currencies.ts
+++ libs/ledgerjs/packages/cryptoassets/src/currencies.ts
@@ -4212,7 +4212,7 @@ export const cryptocurrenciesById: Record<CryptoCurrencyId, CryptoCurrency> = {
     name: "Telos",
     managerAppName: "Ethereum",
     ticker: "TLOS",
-    deviceTicker: "TLOS",
+    deviceTicker: "ETH",
     scheme: "telos_evm",
     color: "#AC72F9",
     family: "evm",
