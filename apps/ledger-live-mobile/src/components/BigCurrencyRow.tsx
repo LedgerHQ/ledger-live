2b89ba908895a5ecd14d84c070cf7857a9999f75
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2023-07-17 10:54:51 +0200

    Update flow entry (#4027)

--- apps/ledger-live-mobile/src/components/BigCurrencyRow.tsx
+++ apps/ledger-live-mobile/src/components/BigCurrencyRow.tsx
@@ -8,10 +8,10 @@ import styled from "styled-components/native";
 const RowContainer = styled(TouchableOpacity)`
   flex-direction: row;
   justify-content: center;
-  padding-left: 16;
-  padding-right: 16;
-  padding-top: 12;
-  padding-bottom: 12;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 12px;
+  padding-bottom: 12px;
 `;
 
 type Props = {
