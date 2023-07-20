d3200a8eb3d93a3c8795bae2502c905a7ca89738
Kieran Allen <kieran.allen@ledger.fr>
2023-07-20 14:20:08 +0100

    feat: add initial drawers to root drawer

---
+++ apps/ledger-live-mobile/src/components/RootDrawer/InitialDrawers/PTXServicesAppleWarning.tsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import QueuedDrawer from "../../QueuedDrawer";
+import { useRootDrawerContext } from "../../../context/RootDrawerContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { InitialDrawerID } from "../types";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { Button, Checkbox, Flex, Icons, Link, Text } from "@ledgerhq/native-ui";
+import { View } from "react-native";
+import styled from "styled-components/native";
+import { Trans, useTranslation } from "react-i18next";
+
+const CheckboxContainer = styled(View)(
+  ({ theme }) => `
+  padding: ${theme.space[4]}px ${theme.space[5]}px;
+  background-color: ${theme.colors.neutral.c30};
+  border-radius: 4px;
+`,
+);
+
+export function PTXServicesAppleWarning() {
+  const { t } = useTranslation();
+  const [persistentHide, setPersistentHide] = useState(false);
+  const exchangeDrawer = useFeature<{ enabled: boolean }>("ptxServiceCtaExchangeDrawer");
+  const ctaScreens = useFeature<{ enabled: boolean }>("ptxServiceCtaScreens");
+
+  const exchangeDrawerEnabled = useMemo(() => exchangeDrawer?.enabled ?? true, [exchangeDrawer]);
+  const ctaScreensEnabled = useMemo(() => ctaScreens?.enabled ?? true, [ctaScreens]);
+
+  const { isOpen, onModalHide, openDrawer, onClose } = useRootDrawerContext();
+  useEffect(() => {
+    if (!(exchangeDrawerEnabled && ctaScreensEnabled)) {
+      openDrawer();
+    }
+  }, [openDrawer, exchangeDrawerEnabled, ctaScreensEnabled]);
+
+  const _onClose = useCallback(
+    () =>
+      onClose(() => {
+        if (persistentHide) {
+          AsyncStorage.setItem(InitialDrawerID.PTXServicesAppleDrawerKey, "true");
+        }
+      }),
+    [persistentHide, onClose],
+  );
+
+  const onLinkPress = useCallback(() => {
+    // TO-DO
+    console.log(
+      '%cPTXServicesAppleWarning.tsx line:45 "link clicked"',
+      "color: #007acc;",
+      "link clicked",
+    );
+  }, []);
+
+  return (
+    <QueuedDrawer isRequestingToBeOpened={isOpen} onClose={_onClose} onModalHide={onModalHide}>
+      <Flex rowGap={24}>
+        <Flex rowGap={24} alignItems="center">
+          <Text
+            maxWidth={295}
+            textAlign="center"
+            fontFamily="inter"
+            fontWeight="semiBold"
+            fontSize="h4"
+          >
+            {t("notifications.ptxServices.drawer.title")}
+          </Text>
+          <Text color="neutral.c70" maxWidth={295} fontSize="body" textAlign="center">
+            <Trans
+              i18nKey="notifications.ptxServices.drawer.description"
+              components={{ emphasized: <Text color="neutral.c100" fontSize="body" /> }}
+            />
+          </Text>
+        </Flex>
+        <CheckboxContainer>
+          <Checkbox
+            checked={persistentHide}
+            label={t("notifications.ptxServices.drawer.checkboxLabel")}
+            onChange={() => setPersistentHide(prev => !prev)}
+          />
+        </CheckboxContainer>
+        <Button type="main" onPress={_onClose}>
+          {t("notifications.ptxServices.drawer.cta")}
+        </Button>
+        <Link onPress={onLinkPress} Icon={Icons.ExternalLinkMedium}>
+          {t("notifications.ptxServices.drawer.link")}
+        </Link>
+      </Flex>
+    </QueuedDrawer>
+  );
+}
---
+++ apps/ledger-live-mobile/src/components/RootDrawer/RootDrawer.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+
+import { EthereumStakingDrawer } from "../../families/ethereum/EthereumStakingDrawer";
+
+import { RootDrawerProvider, useRootDrawerContext } from "../../context/RootDrawerContext";
+import { InitialDrawerID, RootDrawerProps } from "./types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { NavigatorName } from "../../const";
+import { PTXServicesAppleWarning } from "./InitialDrawers/PTXServicesAppleWarning";
+export async function getInitialDrawersToShow(initialDrawers: InitialDrawerID[]) {
+  const initialDrawersToShow = await AsyncStorage.multiGet(initialDrawers);
+
+  // if we have a value then the drawer should not be shown
+  return initialDrawersToShow
+    .map(([key, value]) => (value ? undefined : key))
+    .filter((drawer): drawer is InitialDrawerID => !!drawer);
+}
+
+export function RootDrawerSelector() {
+  const { drawer } = useRootDrawerContext();
+  switch (drawer.id) {
+    case "EthStakingDrawer":
+      return <EthereumStakingDrawer />;
+    case InitialDrawerID.PTXServicesAppleDrawerKey:
+      return <PTXServicesAppleWarning />;
+    default:
+      return null;
+  }
+}
+
+export function RootDrawer({ drawer }: RootDrawerProps) {
+  // this keeps track initial drawers closed during the session.
+  const [initialDrawers, setInitialDrawers] = useState(Object.values(InitialDrawerID));
+  const { navigate } = useNavigation<StackNavigationProp<Record<string, object | undefined>>>();
+
+  useEffect(() => {
+    if (!drawer) {
+      getInitialDrawersToShow(initialDrawers)
+        .then(([id]) => {
+          if (id) {
+            setInitialDrawers(prev => prev.filter(d => d !== id));
+            navigate(NavigatorName.Base, {
+              drawer: {
+                id,
+              },
+            });
+          }
+        })
+        .catch(e => {
+          console.log("%cRootDrawer.tsx line:43 e", "color: #007acc;", e);
+        });
+    }
+  }, [drawer, navigate, setInitialDrawers, initialDrawers]);
+
+  return (
+    <RootDrawerProvider drawer={drawer}>
+      <RootDrawerSelector />
+    </RootDrawerProvider>
+  );
+}
