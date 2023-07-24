7ae89bc743d1717e876fe5265fc4e6dca90463ba
lvndry <lvndry@proton.me>
2023-07-24 16:32:50 +0200

    moved getTransactionByHash in explorer api

--- libs/coin-evm/src/api/transaction/index.ts
+++ libs/coin-evm/src/api/transaction/index.ts
@@ -1,17 +1,14 @@
-import { getEnv } from "@ledgerhq/live-env";
-import network from "@ledgerhq/live-network/network";
 import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+import { getExplorerApi } from "../explorer";
 
 export const getTransactionByHash = async (
   currency: CryptoCurrency,
   transactionHash: string,
 ): Promise<{ confirmations?: number }> => {
-  const explorerId = currency.explorerId ?? currency.id;
+  const { getTransactionByHash: apiGetTransactionByHash } = getExplorerApi(currency);
+  if (!apiGetTransactionByHash) {
+    return {};
+  }
 
-  const { data } = await network({
-    method: "GET",
-    url: `${getEnv("EXPLORER")}/blockchain/v4/${explorerId}/tx/${transactionHash}`,
-  });
-
-  return data;
+  return apiGetTransactionByHash(currency, transactionHash);
 };
