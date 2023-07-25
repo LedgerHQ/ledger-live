d561b3acc031a1c4189097cc4b7d106d3824137f
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-07-25 16:55:41 +0200

    chore: reverted llc changes

---
+++ libs/ledger-live-common/src/exchange/swap/checkQuote.ts
@@ -0,0 +1,35 @@
+import network from "@ledgerhq/live-network/network";
+import { getEnv } from "../../env";
+import { getSwapAPIBaseURL } from "./";
+import { mockCheckQuote } from "./mock";
+import type { CheckQuote } from "./types";
+
+// This code is specifically for FTX
+const checkQuote: CheckQuote = async ({ provider, quoteId, bearerToken }) => {
+  if (getEnv("MOCK") || getEnv("MOCK_SWAP_CHECK_QUOTE")) {
+    return mockCheckQuote({ provider, quoteId, bearerToken });
+  }
+
+  const request = {
+    provider,
+    rateId: quoteId,
+  };
+
+  const { data } = await network({
+    method: "POST",
+    url: `${getSwapAPIBaseURL()}/rate/check`,
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    data: request,
+  });
+
+  /**
+   * TODO: we might want to handle various status and error here
+   * cf. https://github.com/LedgerHQ/swap/blob/master/modules/model/src/main/scala/co/ledger/swap/model/RateStatus.scala
+   */
+
+  return data;
+};
+
+export default checkQuote;
