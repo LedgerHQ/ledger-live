07cbcfb0419f1bfea5ceccd5a5449656db1a125c
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-07-26 13:16:09 +0200

    fix: deeplink nano manager flakiness

--- apps/ledger-live-mobile/e2e/models/manager/managerPage.ts
+++ apps/ledger-live-mobile/e2e/models/manager/managerPage.ts
@@ -12,30 +12,26 @@ import * as bridge from "../../bridge/server";
 const baseLink = "myledger";
 
 export default class ManagerPage {
-  proceedButtonId = "onboarding-paired-continue";
-  getManagerTitle = () => getElementById("manager-title");
-  getPairDeviceButton = () => getElementById("pair-device");
-  getProceedButton = () => getElementById(this.proceedButtonId);
-  waitProceedButton = () => waitForElementById(this.proceedButtonId);
+  managerTitle = () => getElementById("manager-title");
+  pairDeviceButton = () => getElementById("pair-device");
 
   async openViaDeeplink() {
     await openDeeplink(baseLink);
   }
 
   async expectManagerPage() {
-    await expect(this.getManagerTitle()).toBeVisible();
+    await expect(this.managerTitle()).toBeVisible();
   }
 
   async addDevice(deviceName: string) {
     await this.expectManagerPage();
     await device.disableSynchronization(); // Scanning animation prevents launching mocks
-    await tapByElement(this.getPairDeviceButton());
+    await tapByElement(this.pairDeviceButton());
     bridge.addDevices();
     await device.enableSynchronization();
     await tapByText(deviceName);
     bridge.setInstalledApps(); // tell LLM what apps the mock device has
     bridge.open();
-    await this.waitProceedButton();
-    await this.getProceedButton();
+    await waitForElementById("onboarding-paired-continue");
   }
 }
