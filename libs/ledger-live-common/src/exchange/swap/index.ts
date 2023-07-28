45e5564aeb60ba2768474becd84b0ac72cdb36ef
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-07-28 17:14:51 +0200

    chore: removing llc code - logic, mocks, types, tests and indexes

--- libs/ledger-live-common/src/exchange/swap/index.ts
+++ libs/ledger-live-common/src/exchange/swap/index.ts
@@ -48,23 +48,8 @@ const getSwapAPIVersion: () => number = () => {
   return version;
 };
 
-const ftx: ProviderConfig = {
-  ...createExchangeProviderNameAndSignature({
-    name: "FTX",
-    publicKey:
-      "04c89f3e48cde252f6cd6fcccc47c2f6ca6cf05f9f921703d31b7a7dddbf0bd6a690744662fe599f8761612021ba1fc0e8a5a4b7d5910c625b6dd09aa40762e5cd",
-    signature:
-      "3044022029c0fb80d6e524f811f30cc04a349fa7f8896ce1ba84010da55f7be5eb9d528802202727985361cab969ad9b4f56570f3f6120c1d77d04ba10e5d99366d8eecee8e2",
-  }),
-  needsKYC: true,
-  needsBearerToken: true,
-  type: "CEX",
-};
-
 type CEXProviderConfig = SwapProviderConfig & { type: "CEX" };
 type DEXProviderConfig = {
-  needsKYC: boolean;
-  needsBearerToken: boolean;
   type: "DEX";
 };
 type ProviderConfig = CEXProviderConfig | DEXProviderConfig;
@@ -78,8 +63,6 @@ const swapProviders: Record<string, ProviderConfig> = {
       signature:
         "3045022100e73339e5071b5d232e8cacecbd7c118c919122a43f8abb8b2062d4bfcd58274e022050b11605d8b7e199f791266146227c43fd11d7645b1d881f705a2f8841d21de5",
     }),
-    needsKYC: false,
-    needsBearerToken: false,
     type: "CEX",
   },
   cic: {
@@ -90,32 +73,12 @@ const swapProviders: Record<string, ProviderConfig> = {
       signature:
         "3044022078a73433ab6289027b7a169a260f180d16346f7ab55b06a22109f68a756d691d0220190edd6e1214c3309dc1b0afe90d217b728377491561383f2ee543e2c90188eb",
     }),
-    needsKYC: false,
-    needsBearerToken: false,
     type: "CEX",
   },
-  wyre: {
-    ...createExchangeProviderNameAndSignature({
-      name: "Wyre",
-      publicKey:
-        "04AD01A6241929A5EC331046868FBACB424696FD7C8A4D824FEE61268374E9F4F87FFC5301F0E0A84CEA69FFED46E14C771F9CA1EEA345F6531994291C816E8AE6",
-      signature:
-        "304402207b49e46d458a55daee9bc8ed96e1b404c2d99dbbc3d3c3c15430026eb7e01a05022011ab86db08a4c956874a83f23d918319a073fdd9df23a1c7eed8a0a22c98b1e3",
-    }),
-    needsKYC: true,
-    needsBearerToken: false,
-    type: "CEX",
-  },
-  ftx,
-  ftxus: ftx,
   oneinch: {
-    needsKYC: false,
-    needsBearerToken: false,
     type: "DEX",
   },
   paraswap: {
-    needsKYC: false,
-    needsBearerToken: false,
     type: "DEX",
   },
 };
