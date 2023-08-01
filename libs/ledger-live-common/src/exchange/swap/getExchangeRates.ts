1a7a247746bae32ed024bb384a56b2ab53a8ed46
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-08-01 16:15:14 +0200

    feat: updated getExchangeRate types and removed kyc copy

--- libs/ledger-live-common/src/exchange/swap/getExchangeRates.ts
+++ libs/ledger-live-common/src/exchange/swap/getExchangeRates.ts
@@ -1,5 +1,5 @@
 import network from "@ledgerhq/live-network/network";
-import type { CryptoCurrency, TokenCurrency, Unit } from "@ledgerhq/types-cryptoassets";
+import type { Unit } from "@ledgerhq/types-cryptoassets";
 import { BigNumber } from "bignumber.js";
 import { getAccountCurrency, getAccountUnit } from "../../account";
 import { formatCurrencyUnit } from "../../currencies";
@@ -9,19 +9,18 @@ import {
   SwapExchangeRateAmountTooLow,
   SwapExchangeRateAmountTooLowOrTooHigh,
 } from "../../errors";
-import type { Transaction } from "../../generated/types";
 import { getProviderConfig, getSwapAPIBaseURL, getSwapAPIError } from "./";
 import { mockGetExchangeRates } from "./mock";
-import type { AvailableProviderV3, CustomMinOrMaxError, Exchange, GetExchangeRates } from "./types";
+import type { CustomMinOrMaxError, GetExchangeRates } from "./types";
 
-const getExchangeRates: GetExchangeRates = async (
-  exchange: Exchange,
-  transaction: Transaction,
-  currencyTo?: TokenCurrency | CryptoCurrency | undefined | null,
-  providers: AvailableProviderV3[] = [],
-  timeout = undefined,
-  timeoutErrorMessage = undefined,
-) => {
+const getExchangeRates: GetExchangeRates = async ({
+  exchange,
+  transaction,
+  currencyTo,
+  providers = [],
+  timeout,
+  timeoutErrorMessage,
+}) => {
   if (getEnv("MOCK") && !getEnv("PLAYWRIGHT_RUN"))
     return mockGetExchangeRates(exchange, transaction, currencyTo);
 
