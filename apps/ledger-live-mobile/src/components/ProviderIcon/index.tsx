0f54c32b6e4ec496b445dcd01310093f0850a1c1
Chuck Ng <cheuk-man.ng@ledger.fr>
2023-08-02 19:18:33 +0100

    bugfix(LIVE-8841): default icon to boxed
    bugfix(LIVE-8841): wrap llm icon with a ccontainer
    
    bugfix(LIVE-8841): use svguri instead
    
    changes to svg itself has been made to ledger-live-asset repo

--- apps/ledger-live-mobile/src/components/ProviderIcon/index.tsx
+++ apps/ledger-live-mobile/src/components/ProviderIcon/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { getProviderIconUrl } from "@ledgerhq/live-common/icons/providers/providers";
 import { ProviderIconSize } from "@ledgerhq/live-common/icons/providers/sizes";
-import * as Styles from "./styles";
+import * as Styles from "./styles";;
 
 export type Props = {
   name: string;
@@ -9,7 +9,7 @@ export type Props = {
   boxed?: boolean;
 };
 
-const ProviderIcon = ({ name, size = "S", boxed = false }: Props): JSX.Element | null => {
+const ProviderIcon = ({ name, size = "S", boxed = true }: Props): JSX.Element | null => {
   const iconUrl = getProviderIconUrl({ boxed, name });
   return <Styles.Icon uri={iconUrl} size={size} />;
 };
