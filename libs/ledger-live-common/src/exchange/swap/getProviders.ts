5ee6536418ba2ebb04dbedc6ce586229280add18
lvndry <lvndry@proton.me>
2023-08-03 14:37:08 +0200

    wip swap pairs

--- libs/ledger-live-common/src/exchange/swap/getProviders.ts
+++ libs/ledger-live-common/src/exchange/swap/getProviders.ts
@@ -1,5 +1,6 @@
 import network from "@ledgerhq/live-network/network";
 import qs from "qs";
+
 import { getEnv } from "../../env";
 import { SwapNoAvailableProviders } from "../../errors";
 import { getAvailableProviders, getSwapAPIBaseURL } from "./";
@@ -7,33 +8,37 @@ import { mockGetProviders } from "./mock";
 import type { GetProviders, ProvidersResponseV4 } from "./types";
 
 const getProviders: GetProviders = async () => {
-  if (getEnv("MOCK") && !getEnv("PLAYWRIGHT_RUN")) return mockGetProviders();
+  if (getEnv("MOCK") && !getEnv("PLAYWRIGHT_RUN")) {
+    return mockGetProviders();
+  }
 
-  const res = await network({
+  const { data: responseV4 } = await network<ProvidersResponseV4>({
     method: "GET",
     url: `${getSwapAPIBaseURL()}/providers`,
     params: { whitelist: getAvailableProviders() },
     paramsSerializer: params => qs.stringify(params, { arrayFormat: "comma" }),
   });
 
-  const responseV4 = res.data as ProvidersResponseV4;
   if (!responseV4.providers || !Object.keys(responseV4.providers).length) {
     throw new SwapNoAvailableProviders();
   }
-  return Object.entries(responseV4.providers).flatMap(([provider, groups]) => ({
-    provider: provider,
-    pairs: groups.flatMap(group =>
-      group.methods.flatMap(tradeMethod =>
-        Object.entries(group.pairs).flatMap(([from, toArray]) =>
-          (toArray as number[]).map(to => ({
-            from: responseV4.currencies[from],
-            to: responseV4.currencies[to.toString()],
-            tradeMethod,
-          })),
+
+  return Object.entries(responseV4.providers).flatMap(([provider, groups]) => {
+    return {
+      provider: provider,
+      pairs: groups.flatMap(group =>
+        group.methods.flatMap(tradeMethod =>
+          Object.entries(group.pairs).flatMap(([from, toArray]) =>
+            toArray.map(to => ({
+              from: responseV4.currencies[from],
+              to: responseV4.currencies[to.toString()],
+              tradeMethod,
+            })),
+          ),
         ),
       ),
-    ),
-  }));
+    };
+  });
 };
 
 export default getProviders;
