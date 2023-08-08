bd1fa5360f5e93854d03dcfcbc7498e8a16f4993
Chuck Ng <cheuk-man.ng@ledger.fr>
2023-08-08 13:36:47 +0100

    feat(LIVE-8241): add api call and mock data for swap/from endpoint

---
+++ libs/ledger-live-common/src/exchange/swap/api/v5/fetchCurrencyFrom.ts
@@ -0,0 +1,43 @@
+import network from "@ledgerhq/live-network/network";
+import { isPlaywrightEnv } from "../../utils/isPlaywrightEnv";
+import { getEnv } from "../../../../env";
+import { DEFAULT_SWAP_TIMEOUT_MS } from "../../const/timeout";
+
+type Props = {
+  providers: Array<string>;
+  currencyTo?: string;
+  additionalCoinsFlag?: boolean;
+};
+
+export type ResponseData = {
+  currencyGroups: Array<{
+    network: string;
+    supportedCurrencies: Array<string>;
+  }>;
+};
+
+export async function fetchCurrencyFrom({
+  providers,
+  currencyTo,
+  additionalCoinsFlag = false,
+}: Props) {
+  if (isPlaywrightEnv()) return Promise.resolve(fetchCurrencyFrom);
+
+  const url = new URL(`${getEnv("SWAP_API_BASE_V5")}/currencies/from`);
+  url.searchParams.append("providers-whitelist", providers.join(","));
+  url.searchParams.append("additional-coins-flag", additionalCoinsFlag.toString());
+  if (currencyTo) {
+    url.searchParams.append("currencyTo", currencyTo);
+  }
+
+  try {
+    const { data } = await network<ResponseData>({
+      method: "GET",
+      url: url.toString(),
+      timeout: DEFAULT_SWAP_TIMEOUT_MS,
+    });
+    return data;
+  } catch (e) {
+    throw Error("Something went wrong in fetchCurrencyFrom call");
+  }
+}
