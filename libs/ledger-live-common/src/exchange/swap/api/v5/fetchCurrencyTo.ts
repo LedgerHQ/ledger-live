c9de0b9c0e03364c46b0ab746c4ddc37e723dbab
Kieran Allen <kieran.allen@ledger.fr>
2023-08-08 12:43:38 +0100

    feat: add api call and mock data for swap/to endpoint

---
+++ libs/ledger-live-common/src/exchange/swap/api/v5/fetchCurrencyTo.ts
@@ -0,0 +1,41 @@
+import network from "@ledgerhq/live-network/network";
+import { getEnv } from "../../../../env";
+import { fetchCurrencyToMock } from "./__mocks__/fetchCurrencyTo.mocks";
+
+type Props = {
+  currencyFrom: string;
+  additionalCoinsFlag?: boolean;
+};
+
+export type ResponseData = {
+  currencyGroups: CurrencyGroup[];
+};
+type CurrencyGroup = {
+  network: string;
+  supportedCurrencies: string[];
+};
+
+export async function fetchCurrencyTo({ currencyFrom, additionalCoinsFlag = false }: Props) {
+  if (getEnv("MOCK") && !getEnv("PLAYWRIGHT_RUN")) return Promise.resolve(fetchCurrencyToMock);
+
+  const url = new URL(`${getEnv("SWAP_API_BASE_V5")}/currencies/to`);
+
+  url.searchParams.append("currencyFrom", currencyFrom);
+  url.searchParams.append("additional-coins-flag", additionalCoinsFlag.toString());
+
+  try {
+    const { data } = await network<ResponseData>({
+      method: "GET",
+      url: url.toString(),
+      timeout: 10000,
+    });
+    return data;
+  } catch (e) {
+    console.log(
+      "%cerror fetchCurrencyTo.ts line:37 ",
+      "color: red; display: block; width: 100%;",
+      e,
+    );
+    throw Error();
+  }
+}
