157dde91c723ad7e07e3402ed87ece6cd82f81c4
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-08-11 16:00:18 +0200

    remove ethereum family from generated files

--- libs/ledger-live-common/src/generated/bridge/mock.ts
+++ libs/ledger-live-common/src/generated/bridge/mock.ts
@@ -2,7 +2,6 @@ import algorand from "../../families/algorand/bridge/mock";
 import bitcoin from "../../families/bitcoin/bridge/mock";
 import cardano from "../../families/cardano/bridge/mock";
 import cosmos from "../../families/cosmos/bridge/mock";
-import ethereum from "../../families/ethereum/bridge/mock";
 import evm from "../../families/evm/bridge/mock";
 import polkadot from "../../families/polkadot/bridge/mock";
 import ripple from "../../families/ripple/bridge/mock";
@@ -16,7 +15,6 @@ export default {
   bitcoin,
   cardano,
   cosmos,
-  ethereum,
   evm,
   polkadot,
   ripple,
