85b778c3f66663305e60d9dac372de490928b073
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-08-11 16:00:18 +0200

    remove ethereum family from generated files

--- libs/ledger-live-common/src/generated/bridge/js.ts
+++ libs/ledger-live-common/src/generated/bridge/js.ts
@@ -4,7 +4,6 @@ import celo from "../../families/celo/bridge/js";
 import cosmos from "../../families/cosmos/bridge/js";
 import crypto_org from "../../families/crypto_org/bridge/js";
 import elrond from "../../families/elrond/bridge/js";
-import ethereum from "../../families/ethereum/bridge/js";
 import filecoin from "../../families/filecoin/bridge/js";
 import hedera from "../../families/hedera/bridge/js";
 import internet_computer from "../../families/internet_computer/bridge/js";
@@ -26,7 +25,6 @@ export default {
   cosmos,
   crypto_org,
   elrond,
-  ethereum,
   filecoin,
   hedera,
   internet_computer,
