2c9001d5f56d6135d35e9d92ebabd9772ececdb9
Beth Swingler <beth.swingler-ext@ledger.fr>
2023-08-11 18:11:17 +0100

    fix: 18next packages; dynamic NoFundsModal title

--- apps/ledger-live-desktop/src/renderer/families/polkadot/RebondFlowModal/fields/AmountField.tsx
+++ apps/ledger-live-desktop/src/renderer/families/polkadot/RebondFlowModal/fields/AmountField.tsx
@@ -1,6 +1,7 @@
 import invariant from "invariant";
 import React, { useCallback } from "react";
-import { Trans, TFunction } from "react-i18next";
+import { Trans } from "react-i18next";
+import { TFunction } from "i18next";
 import styled from "styled-components";
 import { BigNumber } from "bignumber.js";
 import { getAccountBridge } from "@ledgerhq/live-common/bridge/index";
