15361154121e52f46715c501b58c531f491d777a
hzheng-ledger <huiqi.zheng@ledger.fr>
2023-08-16 11:03:41 +0200

    fix: unit tests

--- libs/coin-framework/src/account/balanceHistoryCache.test.ts
+++ libs/coin-framework/src/account/balanceHistoryCache.test.ts
@@ -8,12 +8,12 @@ describe("date utils", () => {
   });
   describe("startOfDay", () => {
     test("basic test", () => {
-      expect(startOfDay(new Date(1655827384305)).toISOString()).toBe("2022-06-20T22:00:00.000Z");
+      expect(startOfDay(new Date(1655827384305)).toISOString()).toBe("2022-06-21T04:00:00.000Z");
     });
   });
   describe("startOfWeek", () => {
     test("basic test", () => {
-      expect(startOfWeek(new Date(1655827384305)).toISOString()).toBe("2022-06-18T22:00:00.000Z");
+      expect(startOfWeek(new Date(1655827384305)).toISOString()).toBe("2022-06-19T04:00:00.000Z");
     });
   });
 });
