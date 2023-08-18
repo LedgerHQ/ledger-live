8b205d4124afd98c25f5818f3fd89e826b148e2a
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-08-18 17:16:19 +0200

    chore(llm): migrate AppDelegate to objective c++ to match RN template

--- apps/ledger-live-mobile/ios/ledgerlivemobile/AppDelegate.h
+++ apps/ledger-live-mobile/ios/ledgerlivemobile/AppDelegate.h
@@ -1,10 +1,10 @@
 #import <RCTAppDelegate.h>
 #import <Expo/Expo.h>
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate : RCTAppDelegate
+@interface AppDelegate : EXAppDelegateWrapper <UNUserNotificationCenterDelegate>
 
-- (void)showOverlay;
-- (void)hideOverlay;
+
 
 @end
