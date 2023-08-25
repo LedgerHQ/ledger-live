722c85474e2390f393093a4a8de048f75da604af
heirland <adam.heirland@gmail.com>
2023-08-25 14:21:36 +0700

    refactor: change jsx file to tsx file

---
+++ apps/ledger-live-desktop/src/renderer/families/icon/modals.ts
@@ -0,0 +1,19 @@
+import MODAL_ICON_REWARDS_INFO from "./EarnRewardsInfoModal";
+import MODAL_ICON_FREEZE from "./Freeze";
+import MODAL_MANAGE_ICON from "./ManageIcon";
+import MODAL_VOTE_ICON from "./VoteIcon";
+import MODAL_VOTE_ICON_INFO from "./VoteIcon/Info";
+import MODAL_ICON_UNFREEZE from "./Unfreeze";
+import MODAL_ICON_CLAIM_REWARDS from "./ClaimRewards";
+import { MakeModalsType } from "~/renderer/modals/types";
+
+
+export type ModalsData = {
+
+};
+
+const modals: MakeModalsType<ModalsData> = {
+
+};
+
+export default modals;
