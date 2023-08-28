4858d5aa869fb4c679675ef91d139958c4ebf730
Prateek Rathod <prateek@zondax.ch>
2023-08-28 19:42:53 +0530

    fix(cspr): lld type errors

--- apps/ledger-live-desktop/src/renderer/families/casper/types.ts
+++ apps/ledger-live-desktop/src/renderer/families/casper/types.ts
@@ -1,11 +1,17 @@
 import {
   CasperAccount,
+  CasperOperation,
   Transaction,
   TransactionStatus,
 } from "@ledgerhq/live-common/families/casper/types";
 import { FieldComponentProps, LLDCoinFamily } from "../types";
 
-export type CasperFamily = LLDCoinFamily<CasperAccount, Transaction, TransactionStatus>;
+export type CasperFamily = LLDCoinFamily<
+  CasperAccount,
+  Transaction,
+  TransactionStatus,
+  CasperOperation
+>;
 export type CasperFieldComponentProps = FieldComponentProps<
   CasperAccount,
   Transaction,
