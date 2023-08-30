cb43f148a4a43283a5aec4058f8174cb72c64863
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-08-30 17:15:51 +0200

    fixup! add platform and wallet-api adapters to evm family

--- libs/ledger-live-common/src/families/evm/walletApiAdapter.ts
+++ libs/ledger-live-common/src/families/evm/walletApiAdapter.ts
@@ -29,14 +29,34 @@ const convertToLiveTransaction: ConvertToLiveTransaction<
   };
 
   // We create a type 2 tx by default, and fallback to type 0 if gasPrice is provided
+
+  /**
+   * We explicitly set unrelated type specific fields to undefined to avoid transaction
+   * type assertion errors during the `prepareTransaction` logic (libs/coin-evm/src/prepareTransaction.ts)
+   * when performing an `updateTransaction` with a newly created tx
+   * Which is what happen in this case (in apps/ledger-live-desktop/src/renderer/modals/SignTransaction/Body.tsx):
+      // `createTransaction` will create a type 2 tx by default with `maxFeePerGas` and `maxPriorityFeePerGas` set to 0
+      const tx = bridge.createTransaction(account);
+      const { recipient, ...txData } = transactionData;
+      const tx2 = bridge.updateTransaction(tx, {
+        recipient,
+        subAccountId: isTokenAccount(account) ? account.id : undefined,
+      });
+      // `transaction` will be of type 2 here if `maxFeePerGas` and `maxPriorityFeePerGas` are not undefined
+      const transaction = bridge.updateTransaction(tx2, txData);
+   */
+
   const liveTx: Partial<Transaction> = tx.gasPrice
     ? {
         ...params,
         gasPrice: tx.gasPrice,
+        maxFeePerGas: undefined,
+        maxPriorityFeePerGas: undefined,
         type: 0,
       }
     : {
         ...params,
+        gasPrice: undefined,
         maxFeePerGas: tx.maxFeePerGas,
         maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
         type: 2,
