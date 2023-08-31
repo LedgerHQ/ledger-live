e5f6755aa36a122f1b410350e0da6726b5467dcb
Maxime Aubanel <sshmaxime@gmail.com>
2023-08-31 11:02:09 +0200

    Renamed basic feature to default feature + useFeature returns DEFAULT_FEATURE is returned value is null or undefined

--- libs/ledger-live-common/src/featureFlags/useFeature.ts
+++ libs/ledger-live-common/src/featureFlags/useFeature.ts
@@ -1,12 +1,21 @@
 import { useMemo } from "react";
 import { useFeatureFlags } from "./provider";
 import { FeatureId, Feature } from "@ledgerhq/types-live";
-import { DEFAULT_FEATURES } from "./defaultFeatures";
+import { DEFAULT_FEATURES, DEFAULT_FEATURE } from "./defaultFeatures";
 
+/**
+ * Hook that returns feature information based on its `featureId`.
+ *
+ * @dev If the value returned by `featureFlags.getFeature` is undefined
+ * or null it will return `DEFAULT_FEATURE` instead.
+ *
+ * @param featureId
+ * @returns a feature.
+ */
 const useFeature = <T extends FeatureId>(featureId: T) => {
   const featureFlags = useFeatureFlags();
   const value = useMemo(() => featureFlags.getFeature(featureId), [featureFlags, featureId]);
-  return value as Feature<(typeof DEFAULT_FEATURES)[T]["params"]>;
+  return (value || DEFAULT_FEATURE) as Feature<(typeof DEFAULT_FEATURES)[T]["params"]>;
 };
 
 export default useFeature;
