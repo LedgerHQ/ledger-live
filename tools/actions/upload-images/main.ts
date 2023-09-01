02d95b29707d378dc97195dd20ffe88288d84ee6
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2023-09-01 10:58:21 +0200

    remove useless fetch

--- tools/actions/upload-images/main.ts
+++ tools/actions/upload-images/main.ts
@@ -2,7 +2,6 @@ import * as core from "@actions/core";
 import * as fs from "fs";
 import FormData from "form-data";
 import * as path from "path";
-import fetch from "node-fetch";
 
 function handleErrors(response: Response) {
   if (!response.ok) {
