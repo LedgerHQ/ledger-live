760aecfc1356730b7a07e8c0b9f76ba176151969
Kieran Allen <kieran.allen@ledger.fr>
2023-09-04 16:13:34 +0100

    fix: update test return of currency all

--- libs/ledger-live-common/src/exchange/swap/api/v5/__tests__/fetchCurrencyAll.spec.ts
+++ libs/ledger-live-common/src/exchange/swap/api/v5/__tests__/fetchCurrencyAll.spec.ts
@@ -2,6 +2,7 @@ import network from "@ledgerhq/live-network/network";
 import { fetchCurrencyAll } from "../fetchCurrencyAll";
 import { fetchCurrencyAllMock } from "../__mocks__/fetchCurrencyAll.mocks";
 import { DEFAULT_SWAP_TIMEOUT_MS } from "../../../const/timeout";
+import { flattenV5CurrenciesAll } from "../../../utils/flattenV5CurrenciesAll";
 
 jest.mock("@ledgerhq/live-network/network");
 
@@ -15,7 +16,7 @@ describe("fetchCurrencyAll", () => {
       providers: ["oneinch"],
     });
 
-    expect(result).toStrictEqual(fetchCurrencyAllMock);
+    expect(result).toStrictEqual(flattenV5CurrenciesAll(fetchCurrencyAllMock));
     expect(network as jest.Mock).toHaveBeenCalledWith({
       method: "GET",
       timeout: DEFAULT_SWAP_TIMEOUT_MS,
