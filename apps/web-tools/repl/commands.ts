83c502a650fb568aed9c1f551e46bccce3310a25
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2023-09-05 16:44:26 +0200

    chore: introduce apps/web-tools ported from common-tools

---
+++ apps/web-tools/repl/commands.ts
@@ -0,0 +1,155 @@
+import editDeviceName from "@ledgerhq/live-common/lib/hw/editDeviceName";
+import getDeviceName from "@ledgerhq/live-common/lib/hw/getDeviceName";
+import getDeviceInfo from "@ledgerhq/live-common/lib/hw/getDeviceInfo";
+import getBatteryStatus from "@ledgerhq/live-common/lib/hw/getBatteryStatus";
+import getAddress from "@ledgerhq/live-common/lib/hw/getAddress/index";
+import getVersion from "@ledgerhq/live-common/lib/hw/getVersion";
+import { createDeviceSocket as socket } from "@ledgerhq/live-common/lib/socket/index";
+import getAppAndVersion from "@ledgerhq/live-common/lib/hw/getAppAndVersion";
+import genuineCheck from "@ledgerhq/live-common/lib/hw/genuineCheck";
+import openApp from "@ledgerhq/live-common/lib/hw/openApp";
+import quitApp from "@ledgerhq/live-common/lib/hw/quitApp";
+import installApp from "@ledgerhq/live-common/lib/hw/installApp";
+import uninstallApp from "@ledgerhq/live-common/lib/hw/uninstallApp";
+import installFinalFirmware from "@ledgerhq/live-common/lib/hw/installFinalFirmware";
+import { getCryptoCurrencyById } from "@ledgerhq/live-common/lib/currencies/index";
+import type { Command } from "./helpers/commands";
+import type { DeviceInfo } from "@ledgerhq/types-live";
+
+const bitcoinCurrency = getCryptoCurrencyById("bitcoin");
+const mapDeviceInfo = (
+  args: unknown[],
+  {
+    deviceInfo,
+  }: {
+    deviceInfo: DeviceInfo;
+  },
+) => [deviceInfo, ...args];
+const mapTargetId = (args: unknown[], { deviceInfo }: { deviceInfo: DeviceInfo }) => [
+  deviceInfo.targetId,
+  ...args,
+];
+const mapP2 = (p2: unknown): [number] => [Number(p2)];
+
+export const commands: Command<any, any>[] = [
+  {
+    id: "getVersion",
+    exec: getVersion,
+    form: [],
+  },
+
+  {
+    id: "getAppAndVersion",
+    exec: getAppAndVersion,
+    form: [],
+  },
+
+  {
+    id: "getDeviceInfo",
+    exec: getDeviceInfo,
+    form: [],
+  },
+
+  {
+    id: "getBatteryStatus",
+    exec: getBatteryStatus,
+    mapArgs: mapP2,
+    form: [
+      {
+        type: "ascii",
+        default: "4",
+        maxlength: 1,
+      },
+    ],
+  },
+
+  {
+    id: "genuineCheck",
+    exec: genuineCheck,
+    mapArgs: mapDeviceInfo,
+    dependencies: {
+      deviceInfo: getDeviceInfo,
+    },
+    form: [],
+  },
+
+  { id: "installFinalFirmware", exec: installFinalFirmware, form: [] },
+
+  {
+    id: "installApp",
+    exec: installApp,
+    mapArgs: mapTargetId,
+    dependencies: {
+      deviceInfo: getDeviceInfo,
+    },
+    form: [
+      {
+        type: "application",
+      },
+    ],
+  },
+
+  {
+    id: "uninstallApp",
+    exec: uninstallApp,
+    mapArgs: mapTargetId,
+    dependencies: {
+      deviceInfo: getDeviceInfo,
+    },
+    form: [
+      {
+        type: "application",
+      },
+    ],
+  },
+
+  {
+    id: "socket",
+    exec: socket,
+    form: [{ url: { type: "ascii" } }],
+  },
+
+  {
+    id: "openApp",
+    exec: openApp,
+    form: [{ type: "ascii" }],
+  },
+
+  {
+    id: "quitApp",
+    exec: quitApp,
+    form: [],
+  },
+
+  {
+    id: "getDeviceName",
+    exec: getDeviceName,
+    form: [],
+  },
+
+  {
+    id: "editDeviceName",
+    exec: editDeviceName,
+    form: [
+      {
+        type: "ascii",
+        default: "",
+        maxlength: 32,
+      },
+    ],
+  },
+
+  {
+    id: "getAddress",
+    exec: getAddress,
+    form: [
+      {
+        currency: { type: "cryptocurrency", default: bitcoinCurrency },
+        path: { type: "derivationPath", default: "44'/0'/0'/0/0" },
+        derivationMode: { type: "derivationMode", default: "" },
+        verify: { type: "checkbox", label: "Verify" },
+        askChainCode: { type: "checkbox", label: "ask chainCode" },
+      },
+    ],
+  },
+];
---
+++ apps/web-tools/repl/components/fields/CryptoCurrencySelect.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Select from "react-select";
+import {
+  listCryptoCurrencies,
+  useCurrenciesByMarketcap,
+} from "@ledgerhq/live-common/lib/currencies/index";
+import type { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+
+export type DataTypeCryptoCurrency = {
+  type: "cryptocurrency";
+  default: CryptoCurrency;
+};
+
+type Props = {
+  value?: CryptoCurrency;
+  onChange: (_: null | CryptoCurrency) => void;
+  filterFamilies?: string[];
+};
+
+const TokenSelect = ({ value, onChange, filterFamilies }: Props) => {
+  let currencies = listCryptoCurrencies(true);
+  if (filterFamilies) {
+    currencies = currencies.filter(c => filterFamilies.includes(c.family));
+  }
+  currencies = useCurrenciesByMarketcap(currencies);
+  return (
+    <Select
+      value={value}
+      options={currencies}
+      onChange={onChange}
+      placeholder="Select a crypto currency"
+      getOptionLabel={token => `${token.name} (${token.ticker})`}
+      getOptionValue={token => token.ticker}
+    />
+  );
+};
+
+export default TokenSelect;
