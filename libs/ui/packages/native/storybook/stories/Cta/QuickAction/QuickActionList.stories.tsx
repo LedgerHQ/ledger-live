56062cd7a65a5dc0942ea7aa137e6cfb16f82ef3
KVNLS <kevin.leseigle@gmail.com>
2023-09-06 11:34:03 +0200

    feat: introduce variant for QuickActions component

--- libs/ui/packages/native/storybook/stories/Cta/QuickAction/QuickActionList.stories.tsx
+++ libs/ui/packages/native/storybook/stories/Cta/QuickAction/QuickActionList.stories.tsx
@@ -17,46 +17,61 @@ export default {
   component: QuickActionList,
 };
 
-export const List = (): JSX.Element => {
-  const data: QuickActionButtonProps[] = [
-    {
-      Icon: PlusMedium,
-      children: "Buy",
-      onPress: action("onPress"),
-    },
-    {
-      Icon: ArrowBottomMedium,
-      children: "Receive",
-      onPress: action("onPress"),
-    },
-    {
-      Icon: LendMedium,
-      children: "Earn",
-      onPress: action("onPress"),
-    },
-    {
-      Icon: MinusMedium,
-      children: "Sell",
-      onPress: action("onPress"),
-    },
-    {
-      Icon: ArrowTopMedium,
-      children: "Send",
-      disabled: true,
-      onPress: action("onPress"),
-    },
-    {
-      Icon: DelegateMedium,
-      children: "Convert",
-      disabled: true,
-      onPress: action("onPress"),
-    },
-  ];
+const DATA: QuickActionButtonProps[] = [
+  {
+    Icon: PlusMedium,
+    children: "Buy",
+    onPress: action("onPress"),
+  },
+  {
+    Icon: ArrowBottomMedium,
+    children: "Receive",
+    onPress: action("onPress"),
+  },
+  {
+    Icon: LendMedium,
+    children: "Earn",
+    onPress: action("onPress"),
+  },
+  {
+    Icon: MinusMedium,
+    children: "Sell",
+    onPress: action("onPress"),
+  },
+  {
+    Icon: ArrowTopMedium,
+    children: "Send",
+    disabled: true,
+    onPress: action("onPress"),
+  },
+  {
+    Icon: DelegateMedium,
+    children: "Convert",
+    disabled: true,
+    onPress: action("onPress"),
+  },
+];
 
+export const List = (): JSX.Element => {
   return (
     <Box width={"100%"} px={7}>
-      <QuickActionList data={data} />
+      <QuickActionList data={DATA} key="test" />
     </Box>
   );
 };
+
 List.storyName = "QuickActionList";
+
+export const ListSmall = (): JSX.Element => {
+  return (
+    <Box width={"100%"} px={7}>
+      <QuickActionList
+        data={DATA.map((element) => ({ ...element, variant: "small", textVariant: "small" }))}
+        key="test"
+        numColumns={6}
+      />
+    </Box>
+  );
+};
+
+ListSmall.storyName = "QuickActionListSmall";
