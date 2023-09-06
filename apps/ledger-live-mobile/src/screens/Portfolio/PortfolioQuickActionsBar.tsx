2683b328e08fea4db529d06935473caccc3556cb
KVNLS <kevin.leseigle@gmail.com>
2023-09-06 13:52:00 +0200

    feat: add analytics on quick action CTAs

--- apps/ledger-live-mobile/src/screens/Portfolio/PortfolioQuickActionsBar.tsx
+++ apps/ledger-live-mobile/src/screens/Portfolio/PortfolioQuickActionsBar.tsx
@@ -7,6 +7,7 @@ import { useTranslation } from "react-i18next";
 import useQuickActions from "../../hooks/useQuickActions";
 import { QuickActionList, type QuickActionButtonProps } from "@ledgerhq/native-ui";
 import { TextVariants } from "@ledgerhq/native-ui/styles/theme";
+import { track, useAnalytics } from "../../analytics";
 
 const SHARED_CONFIG = {
   variant: "small" as const,
@@ -20,19 +21,21 @@ const getIcon =
 
 function PortfolioQuickActionsBar() {
   const navigation = useNavigation();
+  const { page } = useAnalytics();
   const { t } = useTranslation();
   const {
     quickActionsList: { SEND, RECEIVE, BUY, SWAP, STAKE },
   } = useQuickActions();
 
   const onNavigate = useCallback(
-    (name: string, options?: object) => {
+    (name: string, options: object, eventButton: string) => {
+      track("button_clicked", { button: eventButton, page });
       (navigation as StackNavigationProp<{ [key: string]: object | undefined }>).navigate(
         name,
         options,
       );
     },
-    [navigation],
+    [navigation, page],
   );
 
   const quickActionsData: QuickActionButtonProps[] = [
@@ -40,28 +43,28 @@ function PortfolioQuickActionsBar() {
       ...SHARED_CONFIG,
       Icon: getIcon(Plus),
       children: t("portfolio.quickActions.buy"),
-      onPress: () => onNavigate(...BUY.route),
+      onPress: () => onNavigate(...BUY.route, "quick_action_buy"),
       disabled: BUY.disabled,
     },
     {
       ...SHARED_CONFIG,
       Icon: getIcon(Exchange),
       children: t("portfolio.quickActions.swap"),
-      onPress: () => onNavigate(...SWAP.route),
+      onPress: () => onNavigate(...SWAP.route, "quick_action_swap"),
       disabled: SWAP.disabled,
     },
     {
       ...SHARED_CONFIG,
       Icon: getIcon(ArrowUp),
       children: t("portfolio.quickActions.send"),
-      onPress: () => onNavigate(...SEND.route),
+      onPress: () => onNavigate(...SEND.route, "quick_action_send"),
       disabled: SEND.disabled,
     },
     {
       ...SHARED_CONFIG,
       Icon: getIcon(ArrowDown),
       children: t("portfolio.quickActions.deposit"),
-      onPress: () => onNavigate(...RECEIVE.route),
+      onPress: () => onNavigate(...RECEIVE.route, "quick_action_receive"),
       disabled: RECEIVE.disabled,
     },
   ];
@@ -70,7 +73,7 @@ function PortfolioQuickActionsBar() {
       ...SHARED_CONFIG,
       Icon: getIcon(CoinPercent),
       children: t("portfolio.quickActions.stake"),
-      onPress: () => onNavigate(...STAKE.route),
+      onPress: () => onNavigate(...STAKE.route, "quick_action_stake"),
       disabled: STAKE.disabled,
     });
   }
