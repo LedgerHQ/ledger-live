8286981551f9bd0a813298f3b06acd3e7fb72caf
lvndry <lvndry@proton.me>
2023-09-08 14:48:50 +0200

    token test

--- libs/ledgerjs/packages/cryptoassets/src/tokens.ts
+++ libs/ledgerjs/packages/cryptoassets/src/tokens.ts
@@ -343,7 +343,7 @@ function convertTRONTokens(type: "trc10" | "trc20") {
     name,
     ticker: abbr,
     delisted,
-    disableCountervalue: !enableCountervalues,
+    disableCountervalue: enableCountervalues !== null ? !enableCountervalues : false,
     ledgerSignature,
     units: [
       {
