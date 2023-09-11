192afce47c9a0aa5aaf527c54ff86c3076a7be5a
Kieran Allen <kieran.allen@ledger.fr>
2023-09-11 16:19:01 +0100

    feat: add feature flag to currency endpoint calls

--- libs/ledger-live-common/src/exchange/swap/hooks/v5/useFetchCurrencyFrom.ts
+++ libs/ledger-live-common/src/exchange/swap/hooks/v5/useFetchCurrencyFrom.ts
@@ -1,20 +1,21 @@
 import { getAvailableProviders } from "../..";
+import useFeature from "../../../../featureFlags/useFeature";
 import { useAPI } from "../../../../hooks/useAPI";
 import { fetchCurrencyFrom } from "../../api/v5/fetchCurrencyFrom";
 
 type Props = {
   currencyTo?: string;
-  additionalCoinsFlag?: boolean;
   enabled?: boolean;
 };
 
-export function useFetchCurrencyFrom({ currencyTo, additionalCoinsFlag, enabled }: Props = {}) {
+export function useFetchCurrencyFrom({ currencyTo, enabled }: Props = {}) {
+  const fetchAdditionalCoins = useFeature("fetchAdditionalCoins");
   return useAPI({
     queryFn: fetchCurrencyFrom,
     queryProps: {
       providers: getAvailableProviders(),
       currencyTo,
-      additionalCoinsFlag,
+      additionalCoinsFlag: fetchAdditionalCoins?.enabled,
     },
     // assume a currency list for the given props won't change during a users session.
     staleTimeout: Infinity,
