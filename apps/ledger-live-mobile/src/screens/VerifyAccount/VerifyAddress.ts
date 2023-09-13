e198f71c91ca09a69d2956d2ebf03d78b2cb16c0
Kant <quentin.jaccarino@ledger.fr>
2023-09-13 15:09:41 +0200

    fix: lint issue

--- apps/ledger-live-mobile/src/screens/VerifyAccount/VerifyAddress.ts
+++ apps/ledger-live-mobile/src/screens/VerifyAccount/VerifyAddress.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "styled-components/native";
 import type { Account } from "@ledgerhq/types-live";
