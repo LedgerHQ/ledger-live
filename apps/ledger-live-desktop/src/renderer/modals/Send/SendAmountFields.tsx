40a00d3eaeb0b071e010f9db73b5f2dc24bafa86
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-09-14 14:44:48 +0200

    Disable invalid strategies
    Disable strategies with fees too low (bellow the TransactionUnderpriced
    threshold)

--- apps/ledger-live-desktop/src/renderer/modals/Send/SendAmountFields.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/Send/SendAmountFields.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Account, FeeStrategy, Operation, TransactionCommonRaw } from "@ledgerhq/types-live";
 import { Transaction, TransactionStatus } from "@ledgerhq/live-common/generated/types";
 import { getLLDCoinFamily } from "~/renderer/families";
+import BigNumber from "bignumber.js";
 
 type Props = {
   account: Account;
@@ -13,6 +14,13 @@ type Props = {
   mapStrategies?: (a: FeeStrategy) => FeeStrategy;
   bridgePending?: boolean;
   trackProperties?: Record<string, unknown>;
+  // FIXME: shity typing
+  minFees?: {
+    maxFeePerGas?: BigNumber;
+    maxPriorityFeePerGas?: BigNumber;
+    gasPrice?: BigNumber;
+  };
+  // TODO: is it used anywhere? to remove?
   transactionRaw?: TransactionCommonRaw;
 };
 
