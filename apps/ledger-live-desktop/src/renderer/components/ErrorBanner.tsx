663d66d1442969a0cfeeb355d739a00cef88d123
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-09-14 14:57:59 +0200

    refacto error display in EditTransaction flow
    - Use ErrorBanner for each error to improve concistency
    - Display errors in summary step as well
    - Add optionnal fallback prop to error banner to be passed to underlying
    TranslatedError component

--- apps/ledger-live-desktop/src/renderer/components/ErrorBanner.tsx
+++ apps/ledger-live-desktop/src/renderer/components/ErrorBanner.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import { urls } from "~/config/urls";
 import TranslatedError from "./TranslatedError";
 import Alert from "./Alert";
+
 type Props = {
   error: Error;
   warning?: boolean;
+  fallback?: {
+    title?: React.ReactNode;
+    description?: React.ReactNode;
+  };
 };
-const ErrorBanner = ({ error, warning }: Props) => {
+
+const ErrorBanner = ({ error, warning, fallback }: Props) => {
   const maybeUrl = error ? urls.errors[String(error?.name)] : null;
   return (
     <Alert
       type={warning ? "warning" : "error"}
-      title={<TranslatedError error={error} />}
+      title={<TranslatedError error={error} field="title" fallback={fallback?.title} />}
       learnMoreUrl={maybeUrl}
       mb={4}
     >
-      <TranslatedError error={error} field="description" />
+      <TranslatedError error={error} field="description" fallback={fallback?.description} />
     </Alert>
   );
 };
