9398a8239512b038d0c4d4aabafb72806fc7e616
Louis Aussedat <louis.aussedat@ledger.fr>
2023-09-18 09:59:43 +0200

    feat: skip cancel modal on disclaimer

--- apps/ledger-live-desktop/src/renderer/modals/UpdateFirmwareModal/index.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/UpdateFirmwareModal/index.tsx
@@ -106,8 +106,8 @@ const UpdateModal = ({
   const [cancel, setCancel] = useState<boolean>(false);
 
   const onRequestCancel = useCallback(() => {
-    setCancel(state => !state);
-  }, []);
+    showDisclaimer ? onRequestClose() : setCancel(state => !state);
+  }, [showDisclaimer, onRequestClose]);
 
   const createSteps = useCallback(
     ({ withResetStep }: { withResetStep: boolean }) => {
