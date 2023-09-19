9787c04e56099c5a85cfcb7c5571eb816299893b
Prateek Rathod <prateek@zondax.ch>
2023-09-19 18:07:11 +0530

    fix(cspr): node path on reverse proxy update

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -69,7 +69,7 @@ const envDefinitions = {
   },
   API_CASPER_NODE_ENDPOINT: {
     parser: stringParser,
-    def: "https://casper.coin.ledger.com/node",
+    def: "https://casper.coin.ledger.com/node/",
     desc: "Casper API url node",
   },
   API_ALGORAND_BLOCKCHAIN_EXPLORER_API_ENDPOINT: {
