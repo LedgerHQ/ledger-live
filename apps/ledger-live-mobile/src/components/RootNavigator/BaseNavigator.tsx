a75b21a98cfe0cb892e237ca1c81d5224c088fd3
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-09-20 18:14:31 +0200

    add back edit evm tx logic in LLM as is

--- apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
@@ -1,5 +1,3 @@
-// FIXME: to update when implementing edit transaction on evm
-
 import React, { useMemo } from "react";
 import {
   createStackNavigator,
@@ -84,8 +82,7 @@ import {
   NavigationHeaderCloseButtonAdvanced,
 } from "../NavigationHeaderCloseButton";
 import { RootDrawer } from "../RootDrawer/RootDrawer";
-// to keep until edit transaction on evm is implemented
-// import EditTransactionNavigator from "../../families/ethereum/EditTransactionFlow/EditTransactionNavigator";
+import EditTransactionNavigator from "../../families/evm/EditTransactionFlow/EditTransactionNavigator";
 import { DrawerProps } from "../RootDrawer/types";
 
 const Stack = createStackNavigator<BaseNavigatorStackParamList>();
@@ -535,12 +532,11 @@ export default function BaseNavigator() {
             headerLeft: () => null,
           }}
         />
-        {/* to keep until edit transaction on evm is implemented */}
-        {/* <Stack.Screen
+        <Stack.Screen
           name={NavigatorName.EditTransaction}
           options={{ headerShown: false }}
           component={EditTransactionNavigator}
-        /> */}
+        />
       </Stack.Navigator>
     </>
   );
