c518ab8b75ac9f3418c1f82016bc89b331ac8970
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-09-22 15:47:42 +0200

    Revamp Service in Manager

--- apps/ledger-live-mobile/src/components/ServicesWidget/index.tsx
+++ apps/ledger-live-mobile/src/components/ServicesWidget/index.tsx
@@ -1,66 +1,84 @@
-import { Flex, Text } from "@ledgerhq/native-ui";
-import React, { memo, useCallback } from "react";
-import { Linking, Image } from "react-native";
+import { Button, Flex, Text } from "@ledgerhq/native-ui";
+import React, { memo } from "react";
+import { Image, ImageBackground } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useTheme } from "styled-components/native";
-import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
-import {
-  useAlreadySubscribedURI,
-  useLearnMoreURI,
-} from "@ledgerhq/live-common/hooks/recoverFeatureFlag";
-import NewProtectState from "./Protect/NewProtectState";
+import styled, { useTheme } from "styled-components/native";
+
 import Touchable from "../Touchable";
 
 import LedgerRecoverLogoLight from "../../images/ledger_recover_light.png";
-import LedgerRecoverLogoDark from "../../images/ledger_recover_dark.png";
-
-import LedgerRecoverCardTopImage from "../../images/ledger_recover_card_top.png";
+import LedgerRecoverBackground from "../../images/ledger_recover_card_background.png";
+import { useProtect } from "./useProtect";
 
 function ServicesWidget() {
   const { t } = useTranslation();
-  const servicesConfig = useFeature("protectServicesMobile");
   const theme = useTheme();
+  const { displayService, onClickFreeTrial, onClickCard, onClickSignIn } = useProtect();
 
-  const learnMoreURI = useLearnMoreURI(servicesConfig);
-  const alreadySubscribedURI = useAlreadySubscribedURI(servicesConfig);
-
-  const onCardPress = useCallback(() => {
-    if (alreadySubscribedURI) {
-      Linking.canOpenURL(alreadySubscribedURI).then(() => Linking.openURL(alreadySubscribedURI));
-    }
-  }, [alreadySubscribedURI]);
-
-  return servicesConfig?.enabled && learnMoreURI && alreadySubscribedURI ? (
+  return displayService ? (
     <>
-      <Text mt={10} fontWeight="semiBold" variant="h5" mb={6}>
-        {t("servicesWidget.title")}
-      </Text>
-      <Touchable onPress={onCardPress}>
-        <Flex bg="neutral.c30" borderRadius={8} mb={13} overflow="hidden">
-          <Image
-            source={LedgerRecoverCardTopImage}
-            resizeMode={"stretch"}
-            style={{ width: "100%", height: 8 }}
-          />
-          <Flex p={7}>
-            <Flex flexDirection="row" justifyContent="space-between" alignItems="center" mb={6}>
-              <Image
-                source={
-                  theme.colors.type === "light" ? LedgerRecoverLogoLight : LedgerRecoverLogoDark
-                }
-                style={{ width: 90, height: 26 }}
-              />
-              <NewProtectState.StatusTag />
-            </Flex>
-            <Text variant="body" color="neutral.c80" mb={7}>
-              {t(`servicesWidget.protect.status.new.desc`)}
+      <Flex flexDirection="row" justifyContent="space-between" alignItems="center" mb={6} mt={8}>
+        <Text variant="h5" fontWeight="semiBold">
+          {t("servicesWidget.title")}
+        </Text>
+
+        <Touchable onPress={onClickSignIn}>
+          <Text color="primary.c90" mr={3} variant="body">
+            {t("servicesWidget.signIn")}
+          </Text>
+        </Touchable>
+      </Flex>
+      <StyledTouchable onPress={onClickCard}>
+        <ImageBackground
+          source={LedgerRecoverBackground}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 30,
+          }}
+          imageStyle={{
+            borderRadius: theme.space[4],
+          }}
+        >
+          <TagComponent bg="opacityReverse.c50" borderRadius={1} px={2} py={1}>
+            <Text variant="small" fontWeight="semiBold" color="neutral.c100" numberOfLines={1}>
+              {t("servicesWidget.recover.tag")}
             </Text>
-            <NewProtectState params={{ learnMoreURI, alreadySubscribedURI }} />
-          </Flex>
-        </Flex>
-      </Touchable>
+          </TagComponent>
+
+          <ImageComponent>
+            <Image source={LedgerRecoverLogoLight} style={{ width: 90, height: 26 }} />
+          </ImageComponent>
+        </ImageBackground>
+      </StyledTouchable>
+
+      <Text color="neutral.c80" fontWeight="semiBold" my={4}>
+        {t("servicesWidget.recover.desc")}
+      </Text>
+
+      <Button type="main" onPress={onClickFreeTrial} style={{ alignSelf: "flex-start" }}>
+        <Text variant="body" fontWeight="semiBold" color="neutral.c00">
+          {t("servicesWidget.recover.cta")}
+        </Text>
+      </Button>
     </>
   ) : null;
 }
+
+const StyledTouchable = styled(Touchable)`
+  height: 110px;
+`;
+
+const TagComponent = styled(Flex)`
+  position: absolute;
+  right: 9px;
+  top: 9px;
+`;
+
+const ImageComponent = styled(Flex)``;
 
 export default memo(ServicesWidget);
