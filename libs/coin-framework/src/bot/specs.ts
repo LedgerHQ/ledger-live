5a76ee68fd5f0f9d4a88af5f6444ddb8ea9bdc9b
lvndry <lvndry@proton.me>
2023-09-25 16:54:46 +0200

    fix(bot) remove replace S bot hack

--- libs/coin-framework/src/bot/specs.ts
+++ libs/coin-framework/src/bot/specs.ts
@@ -84,11 +84,7 @@ export function deviceActionFlow<T extends TransactionCommon>(
               expect({
                 [stepTitle]: stepValueTransform(stepValue),
               }).toMatchObject({
-                // FIXME: OCR of speculos couldn't retrieve S properly
-                // Issue on speculos repository : https://github.com/LedgerHQ/speculos/issues/204
-                [stepTitle]: expectedValue(arg, acc)
-                  .replace(/S/g, arg.appCandidate.model === DeviceModelId.nanoS ? "S" : "")
-                  .trim(),
+                [stepTitle]: expectedValue(arg, acc).trim(),
               });
             });
           }
