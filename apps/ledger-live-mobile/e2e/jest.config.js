5c186bd1641f4c6f8dae73552a3c076d1fe94cc6
KVNLS <kevin.leseigle@gmail.com>
2023-09-25 13:47:44 +0200

    fix: add parallel execution for jest and detox

--- apps/ledger-live-mobile/e2e/jest.config.js
+++ apps/ledger-live-mobile/e2e/jest.config.js
@@ -1,23 +1,29 @@
-module.exports = {
-  rootDir: "..",
-  maxWorkers: 1,
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(js|jsx)?$": "babel-jest",
-    "^.+\\.(ts|tsx)?$": [
-      "ts-jest",
-      {
-        babelConfig: "<rootDir>/e2e/babel.config.detox.js",
-        tsconfig: "<rootDir>/e2e/tsconfig.test.json",
-      },
-    ],
-  },
-  setupFilesAfterEnv: ["<rootDir>/e2e/setup.ts"],
-  testTimeout: 120000,
-  testMatch: ["<rootDir>/e2e/**/*.spec.ts"],
-  reporters: ["detox/runners/jest/reporter"],
-  globalSetup: "detox/runners/jest/globalSetup",
-  globalTeardown: "detox/runners/jest/globalTeardown",
-  testEnvironment: "<rootDir>/e2e/environment.js",
-  verbose: true,
+const { resolveConfig } = require("detox/internals");
+
+module.exports = async () => {
+  const { device } = await resolveConfig();
+
+  return {
+    rootDir: "..",
+    maxWorkers: process.env.CI ? (device.type === "ios.simulator" ? 3 : 2) : 1,
+    preset: "ts-jest",
+    transform: {
+      "^.+\\.(js|jsx)?$": "babel-jest",
+      "^.+\\.(ts|tsx)?$": [
+        "ts-jest",
+        {
+          babelConfig: "<rootDir>/e2e/babel.config.detox.js",
+          tsconfig: "<rootDir>/e2e/tsconfig.test.json",
+        },
+      ],
+    },
+    setupFilesAfterEnv: ["<rootDir>/e2e/setup.ts"],
+    testTimeout: 120000,
+    testMatch: ["<rootDir>/e2e/**/*.spec.ts"],
+    reporters: ["detox/runners/jest/reporter"],
+    globalSetup: "detox/runners/jest/globalSetup",
+    globalTeardown: "detox/runners/jest/globalTeardown",
+    testEnvironment: "<rootDir>/e2e/environment.js",
+    verbose: true,
+  };
 };
