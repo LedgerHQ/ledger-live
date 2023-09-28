93a4750b53693a8a96a7f2fd503042f89743c047
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-09-28 15:30:05 +0200

    Disable invalid startegies in fee selection on LLM

--- libs/ledger-live-common/src/families/evm/getUpdateTransactionPatch.ts
+++ libs/ledger-live-common/src/families/evm/getUpdateTransactionPatch.ts
@@ -5,6 +5,7 @@ import { getEstimatedFees } from "@ledgerhq/coin-evm/logic";
 import type {
   EvmTransactionEIP1559,
   EvmTransactionLegacy,
+  FeeData,
   GasOptions,
   Transaction,
   TransactionStatus,
@@ -484,4 +485,44 @@ export const getEditTransactionStatus = ({
   };
 
   return updatedStatus;
+};
+
+export const isStrategyDisabled = ({
+  isEIP1559,
+  feeData,
+  minFees,
+}: {
+  isEIP1559: boolean;
+  feeData: FeeData;
+  minFees?: {
+    maxFeePerGas?: BigNumber;
+    maxPriorityFeePerGas?: BigNumber;
+    gasPrice?: BigNumber;
+  };
+}): boolean => {
+  if (!minFees) {
+    return false;
+  }
+
+  if (isEIP1559) {
+    if (
+      !feeData.maxFeePerGas ||
+      !feeData.maxPriorityFeePerGas ||
+      !minFees.maxFeePerGas ||
+      !minFees.maxPriorityFeePerGas
+    ) {
+      return false;
+    }
+
+    return (
+      feeData.maxFeePerGas.isLessThan(minFees.maxFeePerGas) ||
+      feeData.maxPriorityFeePerGas.isLessThan(minFees.maxPriorityFeePerGas)
+    );
+  } else {
+    if (!feeData.gasPrice || !minFees.gasPrice) {
+      return false;
+    }
+
+    return feeData.gasPrice.isLessThan(minFees.gasPrice);
+  }
 };
