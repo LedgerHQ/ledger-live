7ed924471f5efbe44c52fbc368dad2d47d791c9f
Stephane Lieumont <stephane.lieumont-ext@ledger.fr>
2023-10-03 13:44:36 +0200

    Revert "fix(protect-25575): set completed onboarding before redirection"
    This reverts commit 500948d61497118cfc200a9e23c8f6c0e0477b64.

--- apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/SyncOnboardingCompanion.tsx
+++ apps/ledger-live-desktop/src/renderer/components/SyncOnboarding/Manual/SyncOnboardingCompanion.tsx
@@ -10,7 +10,7 @@ import React, {
 import { useHistory } from "react-router-dom";
 import { Box, Flex, Text, VerticalTimeline } from "@ledgerhq/react-ui";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useOnboardingStatePolling } from "@ledgerhq/live-common/onboarding/hooks/useOnboardingStatePolling";
 import { getDeviceModel } from "@ledgerhq/devices";
 import { DeviceModelInfo, SeedPhraseType } from "@ledgerhq/types-live";
@@ -29,12 +29,11 @@ import { getOnboardingStatePolling } from "@ledgerhq/live-common/hw/getOnboardin
 import ContinueOnDeviceWithAnim from "./ContinueOnDeviceWithAnim";
 import { RecoverState } from "~/renderer/screens/recover/Player";
 import TrackPage from "~/renderer/analytics/TrackPage";
-import { track, trackPage } from "~/renderer/analytics/segment";
+import { trackPage } from "~/renderer/analytics/segment";
 import { Device } from "@ledgerhq/live-common/hw/actions/types";
 import { setDrawer } from "~/renderer/drawers/Provider";
 import LockedDeviceDrawer, { Props as LockedDeviceDrawerProps } from "./LockedDeviceDrawer";
 import { LockedDeviceError } from "@ledgerhq/errors";
-import { saveSettings } from "~/renderer/actions/settings";
 
 const READY_REDIRECT_DELAY_MS = 2500;
 const POLLING_PERIOD_MS = 1000;
@@ -101,7 +100,6 @@ const SyncOnboardingCompanion: React.FC<SyncOnboardingCompanionProps> = ({
   notifySyncOnboardingShouldReset,
 }) => {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
   const history = useHistory<RecoverState>();
   const [stepKey, setStepKey] = useState<StepKey>(StepKey.Paired);
   const [shouldRestoreApps, setShouldRestoreApps] = useState<boolean>(false);
@@ -466,19 +464,13 @@ const SyncOnboardingCompanion: React.FC<SyncOnboardingCompanionProps> = ({
   useEffect(() => {
     if (seedPathStatus === "recover_seed" && postOnboardingPath) {
       const [pathname, search] = postOnboardingPath.split("?");
-      dispatch(
-        saveSettings({
-          hasCompletedOnboarding: true,
-        }),
-      );
-      track("Onboarding - End");
       history.push({
         pathname,
         search: search ? `?${search}` : undefined,
         state: { fromOnboarding: true },
       });
     }
-  }, [dispatch, history, postOnboardingPath, seedPathStatus]);
+  }, [history, postOnboardingPath, seedPathStatus]);
 
   return (
     <Flex width="100%" height="100%" flexDirection="column" justifyContent="flex-start">
