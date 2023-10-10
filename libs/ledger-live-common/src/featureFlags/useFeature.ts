350bc6c4d773613b8d8a321ee1b13bbcb8ee5433
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-10-10 16:36:28 +0200

    docs(useFeature): more comments

--- libs/ledger-live-common/src/featureFlags/useFeature.ts
+++ libs/ledger-live-common/src/featureFlags/useFeature.ts
@@ -8,6 +8,9 @@ import { FeatureId, Feature, FeatureParam } from "@ledgerhq/types-live";
  * @dev If the value returned by `featureFlags.getFeature` is null it will
  * return null.
  *
+ * @dev do not modify this function to make it return a default value instead
+ * of null, this should not be handled at this level.
+ *
  * @param featureId
  * @returns a feature flag value or null if the feature flag is not found.
  */
