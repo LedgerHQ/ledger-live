7187701648cc2f4b8ea0abe292fb83ee3be9669f
Davide Carpini <carpini.davide@gmail.com>
2023-10-10 12:15:46 +0200

    fix: dependencies

--- libs/ledgerjs/packages/hw-app-vet/tests/Vechain.test.ts
+++ libs/ledgerjs/packages/hw-app-vet/tests/Vechain.test.ts
@@ -1,7 +1,4 @@
-import {
-  openTransportReplayer,
-  RecordStore,
-} from "@ledgerhq/hw-transport-mocker";
+import { openTransportReplayer, RecordStore } from "@ledgerhq/hw-transport-mocker";
 import Vet from "../src/Vet";
 import { Transaction as ThorTransaction } from "thor-devkit";
 
@@ -10,7 +7,7 @@ test("getAppConfiguration", async () => {
     RecordStore.fromString(`
     => e006000000
     <= 030100079000
-    `)
+    `),
   );
   const vet = new Vet(transport);
   const result = await vet.getAppConfiguration();
@@ -24,7 +21,7 @@ test("Get address", async () => {
     RecordStore.fromString(`
           => e002000015058000002c80000332800000000000000000000000
           <= 410482c44bc99cdf08d4360c97fbf62d387288ab75c576926943ad90059002720e93f58799391393c98ad41136aa4ac871b103d25cb9a88f1aadd7dbbe3c7794888928333234373631393364346133323438383332326666426239383335613763463263376532323032439000
-      `)
+      `),
   );
   const vet = new Vet(transport);
   const { address } = await vet.getAddress("44'/818'/0'/0/0");
@@ -36,7 +33,7 @@ test("signMessage", async () => {
     RecordStore.fromString(`
         => e004000037058000002c80000332800000000000000000000000e1278012d8d7940000000000000000000000000000000000000000808080808080c0
         <= dd0ad907f6cfdd068226999eee0d20789732905270a7c4ed385872599f40978e043a5d7a15e396e59e9c9e921be5f4e02e3b6284b8cf835443f4123ca20b5b35009000
-    `)
+    `),
   );
 
   const unsigned = new ThorTransaction({
@@ -57,12 +54,9 @@ test("signMessage", async () => {
   });
 
   const vet = new Vet(transport);
-  const res = await vet.signTransaction(
-    "44'/818'/0'/0/0",
-    unsigned.encode().toString("hex")
-  );
+  const res = await vet.signTransaction("44'/818'/0'/0/0", unsigned.encode().toString("hex"));
 
   expect(res.toString("hex")).toEqual(
-    "dd0ad907f6cfdd068226999eee0d20789732905270a7c4ed385872599f40978e043a5d7a15e396e59e9c9e921be5f4e02e3b6284b8cf835443f4123ca20b5b3500"
+    "dd0ad907f6cfdd068226999eee0d20789732905270a7c4ed385872599f40978e043a5d7a15e396e59e9c9e921be5f4e02e3b6284b8cf835443f4123ca20b5b3500",
   );
 });
