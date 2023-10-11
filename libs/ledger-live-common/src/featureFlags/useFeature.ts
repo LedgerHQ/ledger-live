4ef19d09dab35ccb8f795e83aa2474bdd9d78b22
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-10-11 15:27:50 +0200

    docs(common/useFeature): improve doc

--- libs/ledger-live-common/src/featureFlags/useFeature.ts
+++ libs/ledger-live-common/src/featureFlags/useFeature.ts
@@ -5,14 +5,12 @@ import { FeatureId, Feature, FeatureParam } from "@ledgerhq/types-live";
 /**
  * Hook that returns the value of a feature flag based on its `featureId`.
  *
- * @dev If the value returned by `featureFlags.getFeature` is null it will
- * return null.
- *
- * @dev do not modify this function to make it return a default value instead
- * of null, this should not be handled at this level.
+ * @dev do not modify this function to make it return a default arbitrary value
+ * instead of null, this should not be handled at this level.
  *
  * @param featureId
- * @returns a feature flag value or null if the feature flag is not found.
+ * @returns a feature flag value or null if the feature flag is not found
+ * (neither in the remote configuration, in the cache or in the local defaults).
  */
 const useFeature = <T extends FeatureId>(featureId: T): Feature<FeatureParam<T>> | null => {
   const featureFlags = useFeatureFlags();
