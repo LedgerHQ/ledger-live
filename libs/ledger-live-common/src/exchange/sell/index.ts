80a6b7a8798a0d6979f0a44c5dd8a46a1d2c9792
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2023-10-17 17:34:45 +0200

    fix: lint issue
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/exchange/sell/index.ts
+++ libs/ledger-live-common/src/exchange/sell/index.ts
@@ -3,7 +3,7 @@ import { ExchangeProviderNameAndSignature } from "../";
 export const sellProviders: Record<string, ExchangeProviderNameAndSignature> = {
   coinify: {
     name: "Coinify",
-    publicKey:{
+    publicKey: {
       curve: "secp256r1",
       data: Buffer.from(
         "04CEA7DC8B189FA0D7A5A97530B50556CB0C14079C39CD44532D7037F2B96F0FA9C2DE588E1840B351B71114EE4021FC260F790A6F2D0CDF1C3E1899CCF97D3CCB",
