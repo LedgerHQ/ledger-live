f36b260d4f0da5644a5bd4a9eb85117a9614f72e
Beth Swingler <beth.swingler-ext@ledger.fr>
2023-10-23 11:37:38 +0100

    fix: stop waiting for too long on accountpage test

--- apps/ledger-live-mobile/e2e/models/accounts/accountPage.ts
+++ apps/ledger-live-mobile/e2e/models/accounts/accountPage.ts
@@ -1,11 +1,8 @@
-import { currencyParam, openDeeplink, waitForElementToExistWithUniqueId } from "../../helpers";
+import { currencyParam, openDeeplink } from "../../helpers";
 
 const hostname = "account";
 
 export default class accountPage {
-  waitForAssetPageToLoad = (coin: string) =>
-    waitForElementToExistWithUniqueId(`account-assets-${coin}`);
-
   async openViaDeeplink(currencyLong?: string) {
     const link = currencyLong ? hostname + currencyParam + currencyLong : hostname;
     await openDeeplink(link);
