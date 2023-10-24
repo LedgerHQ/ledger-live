8fe6a1c92be03a02622a69fcd50882bec6c50e7f
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2023-10-24 13:58:24 +0200

    fix: revert using android sdk api 33

--- apps/ledger-live-mobile/detox.config.js
+++ apps/ledger-live-mobile/detox.config.js
@@ -71,7 +71,7 @@ module.exports = {
     emulator: {
       type: "android.emulator",
       device: {
-        avdName: "Pixel_5_API_33",
+        avdName: "Pixel_5_API_31",
       },
       gpuMode: "swiftshader_indirect",
       headless: process.env.CI ? true : false,
