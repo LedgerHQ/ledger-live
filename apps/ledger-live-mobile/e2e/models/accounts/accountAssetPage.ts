727e4418b7bfc013f38d39485d250f61492c9c02
Beth Swingler <beth.swingler-ext@ledger.fr>
2023-10-24 11:37:53 +0100

    test: try debug config, extended timeout, it only

--- apps/ledger-live-mobile/e2e/models/accounts/accountAssetPage.ts
+++ apps/ledger-live-mobile/e2e/models/accounts/accountAssetPage.ts
@@ -2,16 +2,13 @@ import { currencyParam, openDeeplink, waitForElementById } from "../../helpers";
 
 const baseLink = "account";
 
-export default class accountPage {
-  async openViaDeeplink() {
-    await openDeeplink(baseLink);
-  }
+export default class accountAssetPage {
   async openAssetScreenViaDeeplink(currencyName: string) {
     const link = baseLink + currencyParam + currencyName;
     await openDeeplink(link);
   }
 
   waitForAccountAssetsToLoad(currencyName: string) {
-    return waitForElementById(`account-assets-${currencyName}`);
+    return waitForElementById(`account-assets-${currencyName}`, 240000);
   }
 }
