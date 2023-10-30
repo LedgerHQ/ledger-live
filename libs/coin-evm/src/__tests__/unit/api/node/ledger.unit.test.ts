d89aa26ad0ee13da4cba6ad97146c8369cf19c5a
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-10-30 14:29:00 +0100

    fix block/{blockHeight} explorer api call return shape
    This specific endpoint returns an array instead of a block object

--- libs/coin-evm/src/__tests__/unit/api/node/ledger.unit.test.ts
+++ libs/coin-evm/src/__tests__/unit/api/node/ledger.unit.test.ts
@@ -431,12 +431,12 @@ describe("EVM Family", () => {
         expect(await LEDGER_API.getBlockByHeight(currency, 12)).toEqual({
           hash: "0xhash",
           height: 123,
-          timestamp: Date.now(),
+          timestamp: Math.floor(Date.now() / 1000),
         });
         expect(await LEDGER_API.getBlockByHeight(currency, "latest")).toEqual({
           hash: "0xhashLatest",
           height: 456,
-          timestamp: Date.now(),
+          timestamp: Math.floor(Date.now() / 1000),
         });
       });
     });
