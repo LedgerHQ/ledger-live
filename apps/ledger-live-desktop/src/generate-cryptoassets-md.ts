bedb166a3dd46355bb43ec06aa2d64a4a87e5d5d
lvndry <lvndry@proton.me>
2023-11-03 11:49:55 +0100

    generate:cryptoassets-md script

--- apps/ledger-live-desktop/src/generate-cryptoassets-md.ts
+++ apps/ledger-live-desktop/src/generate-cryptoassets-md.ts
@@ -1,8 +1,3 @@
-// it is a bit a hack but it's a test that run in jest
-// to access the libs / babel ecosystem and generate a markdown file!
-// it passes if the file doesn't change (like a snapshot!)
-// to run this test: pnpm desktop test:jest src/generate-cryptoassets-md.test.ts
-import { test, expect } from "@jest/globals";
 import fs from "fs";
 import "./live-common-set-supported-currencies";
 import {
@@ -10,14 +5,17 @@ import {
   isCurrencySupported,
   listTokens,
 } from "@ledgerhq/live-common/currencies/index";
+
 const outputFile = "cryptoassets.md";
 
 function gen() {
-  let md = "# Supported crypto assets\n\n";
   const currencies = listCryptoCurrencies();
+
+  let md = "# Supported crypto assets\n\n";
   md += "## Crypto currencies (" + currencies.length + ")\n";
   md += "| name | ticker | supported on Ledger Live? | ledger id |\n";
   md += "|--|--|--|--|\n";
+
   currencies
     .slice(0)
     .sort((a, b) => {
@@ -26,16 +24,22 @@ function gen() {
       }
       return isCurrencySupported(b) ? 1 : -1;
     })
-    .forEach(c => {
-      md += `| ${c.name} | ${c.ticker} | ${isCurrencySupported(c) ? "YES" : "NO"} | ${c.id} |\n`;
+    .forEach(currency => {
+      md += `| ${currency.name} | ${currency.ticker} | ${
+        isCurrencySupported(currency) ? "YES" : "NO"
+      } | ${currency.id} |\n`;
     });
+
   md += "\n";
+
   const tokens = listTokens({
     withDelisted: true,
   });
+
   md += "## Tokens (" + tokens.length + ")\n";
   md += "| parent currency | ticker | contract | name | status | ledger id |\n";
   md += "|--|--|--|--|--|--|\n";
+
   tokens
     .slice(0)
     .sort((a, b) => {
@@ -44,22 +48,20 @@ function gen() {
       }
       return a.parentCurrency.name > b.parentCurrency.name ? 1 : -1;
     })
-    .forEach(t => {
+    .forEach(token => {
       const status = [];
-      if (t.delisted) {
+      if (token.delisted) {
         status.push("delisted");
       }
-      md += `| ${t.parentCurrency.name} | ${t.ticker} | ${t.contractAddress || ""} | ${
-        t.name
-      } | ${status.join(", ")} | ${t.id} |\n`;
+      md += `| ${token.parentCurrency.name} | ${token.ticker} | ${token.contractAddress || ""} | ${
+        token.name
+      } | ${status.join(", ")} | ${token.id} |\n`;
     });
+
   md += "\n";
+
   return md;
 }
 
-test("generate cryptoassets.md", () => {
-  const md = gen();
-  const existing = fs.readFileSync(outputFile, "utf-8");
-  fs.writeFileSync(outputFile, md);
-  expect(existing).toBe(md);
-});
+const md = gen();
+fs.writeFileSync(outputFile, md);
