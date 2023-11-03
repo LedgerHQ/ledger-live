af8559ecfb174bdf7085b8b9b7dc666d501a4ec8
lvndry <lvndry@proton.me>
2023-11-03 12:35:45 +0100

    remove file generation from test

--- apps/ledger-live-desktop/src/generate-cryptoassets-md.test.ts
+++ apps/ledger-live-desktop/src/generate-cryptoassets-md.test.ts
@@ -5,61 +5,10 @@
 import { test, expect } from "@jest/globals";
 import fs from "fs";
 import "./live-common-set-supported-currencies";
-import {
-  listCryptoCurrencies,
-  isCurrencySupported,
-  listTokens,
-} from "@ledgerhq/live-common/currencies/index";
-const outputFile = "cryptoassets.md";
-
-function gen() {
-  let md = "# Supported crypto assets\n\n";
-  const currencies = listCryptoCurrencies();
-  md += "## Crypto currencies (" + currencies.length + ")\n";
-  md += "| name | ticker | supported on Ledger Live? | ledger id |\n";
-  md += "|--|--|--|--|\n";
-  currencies
-    .slice(0)
-    .sort((a, b) => {
-      if (isCurrencySupported(a) === isCurrencySupported(b)) {
-        return a.name < b.name ? -1 : 1;
-      }
-      return isCurrencySupported(b) ? 1 : -1;
-    })
-    .forEach(c => {
-      md += `| ${c.name} | ${c.ticker} | ${isCurrencySupported(c) ? "YES" : "NO"} | ${c.id} |\n`;
-    });
-  md += "\n";
-  const tokens = listTokens({
-    withDelisted: true,
-  });
-  md += "## Tokens (" + tokens.length + ")\n";
-  md += "| parent currency | ticker | contract | name | status | ledger id |\n";
-  md += "|--|--|--|--|--|--|\n";
-  tokens
-    .slice(0)
-    .sort((a, b) => {
-      if (a.parentCurrency === b.parentCurrency) {
-        return a.name < b.name ? -1 : 1;
-      }
-      return a.parentCurrency.name > b.parentCurrency.name ? 1 : -1;
-    })
-    .forEach(t => {
-      const status = [];
-      if (t.delisted) {
-        status.push("delisted");
-      }
-      md += `| ${t.parentCurrency.name} | ${t.ticker} | ${t.contractAddress || ""} | ${
-        t.name
-      } | ${status.join(", ")} | ${t.id} |\n`;
-    });
-  md += "\n";
-  return md;
-}
+import { generateFile, outputFile } from "./generate-cryptoassets-md";
 
 test("generate cryptoassets.md", () => {
-  const md = gen();
+  const md = generateFile();
   const existing = fs.readFileSync(outputFile, "utf-8");
-  fs.writeFileSync(outputFile, md);
   expect(existing).toBe(md);
 });
