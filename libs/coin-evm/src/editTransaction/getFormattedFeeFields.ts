cc9a195634c41e4d0a96b888015b32660ca63561
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-11-06 12:19:56 +0100

    [BUGFIX] Fix an out of bound access in the units array of the currencies (#5258)

--- libs/coin-evm/src/editTransaction/getFormattedFeeFields.ts
+++ libs/coin-evm/src/editTransaction/getFormattedFeeFields.ts
@@ -23,29 +23,32 @@ export const getFormattedFeeFields = ({
   formattedMaxFeePerGas: string;
   formattedGasPrice: string;
 } => {
+  const unit = getAccountUnit(mainAccount);
   const feeValue = getEstimatedFees(transaction);
-  const formattedFeeValue = formatCurrencyUnit(getAccountUnit(mainAccount), feeValue, {
+  const formattedFeeValue = formatCurrencyUnit(unit, feeValue, {
     showCode: true,
     locale,
   });
 
   const { currency } = mainAccount;
-  const gweiUnit = currency.units[1];
+  // If a lower unit is available, use it to display the fees (e.g. ETH -> GWEI)
+  // This might not be the case for all currencies, so we fallback to the account unit
+  const feesUnit = currency.units.length > 1 ? currency.units[1] : unit;
 
   const maxPriorityFeePerGas = transaction.maxPriorityFeePerGas ?? new BigNumber(0);
-  const formattedMaxPriorityFeePerGas = formatCurrencyUnit(gweiUnit, maxPriorityFeePerGas, {
+  const formattedMaxPriorityFeePerGas = formatCurrencyUnit(feesUnit, maxPriorityFeePerGas, {
     showCode: true,
     locale,
   });
 
   const maxFeePerGas = transaction.maxFeePerGas ?? new BigNumber(0);
-  const formattedMaxFeePerGas = formatCurrencyUnit(gweiUnit, maxFeePerGas, {
+  const formattedMaxFeePerGas = formatCurrencyUnit(feesUnit, maxFeePerGas, {
     showCode: true,
     locale,
   });
 
   const gasPrice = transaction.gasPrice ?? new BigNumber(0);
-  const formattedGasPrice = formatCurrencyUnit(gweiUnit, gasPrice, {
+  const formattedGasPrice = formatCurrencyUnit(feesUnit, gasPrice, {
     showCode: true,
     locale,
   });
