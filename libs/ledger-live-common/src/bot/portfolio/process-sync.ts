ed9a8dfe0626edf9a11fe16676a684197def88b8
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2023-11-06 15:50:55 +0100

    change onspeculoscreated hook

--- libs/ledger-live-common/src/bot/portfolio/process-sync.ts
+++ libs/ledger-live-common/src/bot/portfolio/process-sync.ts
@@ -52,7 +52,7 @@ async function main(): Promise<Report> {
 
   const appCandidates = await listAppCandidates(COINAPPS);
 
-  const { appQuery, currency, dependency } = spec;
+  const { appQuery, currency, dependency, onSpeculosDeviceCreated } = spec;
   const appCandidate = findAppCandidate(appCandidates, appQuery);
   if (!appCandidate) {
     console.warn("no app found for " + spec.name);
@@ -68,13 +68,10 @@ async function main(): Promise<Report> {
     seed: SEED,
     dependency,
     coinapps: COINAPPS,
+    onSpeculosDeviceCreated,
   };
 
   const device = await createSpeculosDevice(deviceParams);
-
-  if (spec.onSpeculosDeviceCreated) {
-    await spec.onSpeculosDeviceCreated(device);
-  }
 
   try {
     const audit = new Audit();
