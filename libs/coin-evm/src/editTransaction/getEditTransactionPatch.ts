974772be875362235c24cd48f04f2cf11ba12fa3
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2023-11-06 11:42:57 +0100

    [BUGFIX] usage of explorer block endpoint in coin evm (#5245)

--- libs/coin-evm/src/editTransaction/getEditTransactionPatch.ts
+++ libs/coin-evm/src/editTransaction/getEditTransactionPatch.ts
@@ -1,14 +1,14 @@
 import type { Account } from "@ledgerhq/types-live";
 import { BigNumber } from "bignumber.js";
 import invariant from "invariant";
-import { getGasTracker } from "..//api/gasTracker/index";
+import { getGasTracker } from "../api/gasTracker/index";
 import type {
   EditType,
   EvmTransactionEIP1559,
   EvmTransactionLegacy,
   GasOptions,
   Transaction,
-} from "..//types/index";
+} from "../types/index";
 import { getMinEip1559Fees, getMinLegacyFees } from "./getMinEditTransactionFees";
 
 /**
