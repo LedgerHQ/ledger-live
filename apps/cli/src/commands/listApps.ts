2fc47a108c1fcca605a41b232ccb1d5dc81a01b9
Louis Aussedat <louis.aussedat@ledger.fr>
2023-11-07 14:55:14 +0100

    feat(cli): add listApps to display all installed apps on device

---
+++ apps/cli/src/commands/listApps.ts
@@ -0,0 +1,20 @@
+import { from, tap } from "rxjs";
+import { withDevice } from "@ledgerhq/live-common/hw/deviceAccess";
+import listApps from "@ledgerhq/live-common/hw/listApps";
+import { deviceOpt } from "../scan";
+
+export default {
+  args: [deviceOpt],
+  job: ({
+    device,
+  }: Partial<{
+    device: string;
+  }>) =>
+    withDevice(device || "")(t =>
+      from(listApps(t)).pipe(
+        tap(res => {
+          console.log(res);
+        }),
+      ),
+    ),
+};
