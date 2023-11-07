eb9c10198b2ddb70fbeb4d05ff97406286ff7586
lvndry <lvndry@proton.me>
2023-11-07 17:26:47 +0100

    sort chainId

--- libs/ledgerjs/packages/cryptoassets/src/data/evm/index.ts
+++ libs/ledgerjs/packages/cryptoassets/src/data/evm/index.ts
@@ -1,144 +1,144 @@
-import ethereum_goerli_tokens from "./43114/erc20.json";
-import arbitrum_goerli_tokens from "./56/erc20.json";
-import arbitrum_tokens from "./1/erc20.json";
-import cronos_tokens from "./61/erc20.json";
-import fantom_tokens from "./137/erc20.json";
-import bittorrent_tokens from "./3/erc20.json";
-import rsk_tokens from "./5/erc20.json";
-import moonbeam_tokens from "./42161/erc20.json";
-import ethereum_ropsten_tokens from "./421613/erc20.json";
-import optimism_tokens from "./25/erc20.json";
-import energy_web_tokens from "./250/erc20.json";
-import velas_evm_tokens from "./14/erc20.json";
-import avalanche_c_chain_tokens from "./19/erc20.json";
-import syscoin_tokens from "./1284/erc20.json";
-import metis_tokens from "./30/erc20.json";
-import astar_tokens from "./199/erc20.json";
-import telos_evm_tokens from "./2222/erc20.json";
-import base_tokens from "./9001/erc20.json";
-import base_goerli_tokens from "./10/erc20.json";
-import polygon_zk_evm_testnet_tokens from "./420/erc20.json";
-import polygon_zk_evm_tokens from "./246/erc20.json";
+import ethereum_ropsten_tokens from "./1/erc20.json";
+import cronos_tokens from "./3/erc20.json";
+import syscoin_tokens from "./5/erc20.json";
+import telos_evm_tokens from "./10/erc20.json";
+import rsk_tokens from "./14/erc20.json";
+import bittorrent_tokens from "./19/erc20.json";
+import velas_evm_tokens from "./25/erc20.json";
+import energy_web_tokens from "./30/erc20.json";
+import ethereum_goerli_tokens from "./40/erc20.json";
+import optimism_tokens from "./56/erc20.json";
+import fantom_tokens from "./57/erc20.json";
+import metis_tokens from "./61/erc20.json";
+import optimism_goerli_tokens from "./106/erc20.json";
+import polygon_zk_evm_tokens from "./137/erc20.json";
+import moonbeam_tokens from "./199/erc20.json";
+import polygon_zk_evm_testnet_tokens from "./246/erc20.json";
+import astar_tokens from "./250/erc20.json";
+import base_tokens from "./288/erc20.json";
+import base_goerli_tokens from "./420/erc20.json";
 import neon_evm_tokens from "./592/erc20.json";
-import klaytn_tokens from "./1088/erc20.json";
-import polygon_tokens from "./288/erc20.json";
-import bsc_tokens from "./1285/erc20.json";
-import ethereum_tokens from "./106/erc20.json";
-import undefined_tokens from "./57/erc20.json";
-import undefined_tokens from "./40/erc20.json";
-import undefined_tokens from "./1101/erc20.json";
-import undefined_tokens from "./1442/erc20.json";
-import undefined_tokens from "./8453/erc20.json";
-import undefined_tokens from "./84531/erc20.json";
+import arbitrum_goerli_tokens from "./1088/erc20.json";
+import arbitrum_tokens from "./1101/erc20.json";
+import avalanche_c_chain_tokens from "./1284/erc20.json";
+import polygon_tokens from "./1285/erc20.json";
+import bsc_tokens from "./1442/erc20.json";
+import ethereum_tokens from "./2222/erc20.json";
 import undefined_tokens from "./8217/erc20.json";
+import undefined_tokens from "./8453/erc20.json";
+import undefined_tokens from "./9001/erc20.json";
+import undefined_tokens from "./42161/erc20.json";
+import undefined_tokens from "./43114/erc20.json";
+import undefined_tokens from "./84531/erc20.json";
+import undefined_tokens from "./421613/erc20.json";
 import undefined_tokens from "./245022934/erc20.json";
 
-import ethereum_goerli_signatures from "./43114/erc20-signatures.json";
-import arbitrum_goerli_signatures from "./56/erc20-signatures.json";
-import arbitrum_signatures from "./1/erc20-signatures.json";
-import cronos_signatures from "./61/erc20-signatures.json";
-import fantom_signatures from "./137/erc20-signatures.json";
-import bittorrent_signatures from "./3/erc20-signatures.json";
-import rsk_signatures from "./5/erc20-signatures.json";
-import moonbeam_signatures from "./42161/erc20-signatures.json";
-import ethereum_ropsten_signatures from "./421613/erc20-signatures.json";
-import optimism_signatures from "./25/erc20-signatures.json";
-import energy_web_signatures from "./250/erc20-signatures.json";
-import velas_evm_signatures from "./14/erc20-signatures.json";
-import avalanche_c_chain_signatures from "./19/erc20-signatures.json";
-import syscoin_signatures from "./1284/erc20-signatures.json";
-import metis_signatures from "./30/erc20-signatures.json";
-import astar_signatures from "./199/erc20-signatures.json";
-import telos_evm_signatures from "./2222/erc20-signatures.json";
-import base_signatures from "./9001/erc20-signatures.json";
-import base_goerli_signatures from "./10/erc20-signatures.json";
-import polygon_zk_evm_testnet_signatures from "./420/erc20-signatures.json";
-import polygon_zk_evm_signatures from "./246/erc20-signatures.json";
+import ethereum_ropsten_signatures from "./1/erc20-signatures.json";
+import cronos_signatures from "./3/erc20-signatures.json";
+import syscoin_signatures from "./5/erc20-signatures.json";
+import telos_evm_signatures from "./10/erc20-signatures.json";
+import rsk_signatures from "./14/erc20-signatures.json";
+import bittorrent_signatures from "./19/erc20-signatures.json";
+import velas_evm_signatures from "./25/erc20-signatures.json";
+import energy_web_signatures from "./30/erc20-signatures.json";
+import ethereum_goerli_signatures from "./40/erc20-signatures.json";
+import optimism_signatures from "./56/erc20-signatures.json";
+import fantom_signatures from "./57/erc20-signatures.json";
+import metis_signatures from "./61/erc20-signatures.json";
+import optimism_goerli_signatures from "./106/erc20-signatures.json";
+import polygon_zk_evm_signatures from "./137/erc20-signatures.json";
+import moonbeam_signatures from "./199/erc20-signatures.json";
+import polygon_zk_evm_testnet_signatures from "./246/erc20-signatures.json";
+import astar_signatures from "./250/erc20-signatures.json";
+import base_signatures from "./288/erc20-signatures.json";
+import base_goerli_signatures from "./420/erc20-signatures.json";
 import neon_evm_signatures from "./592/erc20-signatures.json";
-import klaytn_signatures from "./1088/erc20-signatures.json";
-import polygon_signatures from "./288/erc20-signatures.json";
-import bsc_signatures from "./1285/erc20-signatures.json";
-import ethereum_signatures from "./106/erc20-signatures.json";
-import undefined_signatures from "./57/erc20-signatures.json";
-import undefined_signatures from "./40/erc20-signatures.json";
-import undefined_signatures from "./1101/erc20-signatures.json";
-import undefined_signatures from "./1442/erc20-signatures.json";
-import undefined_signatures from "./8453/erc20-signatures.json";
-import undefined_signatures from "./84531/erc20-signatures.json";
+import arbitrum_goerli_signatures from "./1088/erc20-signatures.json";
+import arbitrum_signatures from "./1101/erc20-signatures.json";
+import avalanche_c_chain_signatures from "./1284/erc20-signatures.json";
+import polygon_signatures from "./1285/erc20-signatures.json";
+import bsc_signatures from "./1442/erc20-signatures.json";
+import ethereum_signatures from "./2222/erc20-signatures.json";
 import undefined_signatures from "./8217/erc20-signatures.json";
+import undefined_signatures from "./8453/erc20-signatures.json";
+import undefined_signatures from "./9001/erc20-signatures.json";
+import undefined_signatures from "./42161/erc20-signatures.json";
+import undefined_signatures from "./43114/erc20-signatures.json";
+import undefined_signatures from "./84531/erc20-signatures.json";
+import undefined_signatures from "./421613/erc20-signatures.json";
 import undefined_signatures from "./245022934/erc20-signatures.json";
 
 export const tokens = {
-  43114: ethereum_goerli_tokens,
-  56: arbitrum_goerli_tokens,
-  1: arbitrum_tokens,
-  61: cronos_tokens,
-  137: fantom_tokens,
-  3: bittorrent_tokens,
-  5: rsk_tokens,
-  42161: moonbeam_tokens,
-  421613: ethereum_ropsten_tokens,
-  25: optimism_tokens,
-  250: energy_web_tokens,
-  14: velas_evm_tokens,
-  19: avalanche_c_chain_tokens,
-  1284: syscoin_tokens,
-  30: metis_tokens,
-  199: astar_tokens,
-  2222: telos_evm_tokens,
-  9001: base_tokens,
-  10: base_goerli_tokens,
-  420: polygon_zk_evm_testnet_tokens,
-  246: polygon_zk_evm_tokens,
+  1: ethereum_ropsten_tokens,
+  3: cronos_tokens,
+  5: syscoin_tokens,
+  10: telos_evm_tokens,
+  14: rsk_tokens,
+  19: bittorrent_tokens,
+  25: velas_evm_tokens,
+  30: energy_web_tokens,
+  40: ethereum_goerli_tokens,
+  56: optimism_tokens,
+  57: fantom_tokens,
+  61: metis_tokens,
+  106: optimism_goerli_tokens,
+  137: polygon_zk_evm_tokens,
+  199: moonbeam_tokens,
+  246: polygon_zk_evm_testnet_tokens,
+  250: astar_tokens,
+  288: base_tokens,
+  420: base_goerli_tokens,
   592: neon_evm_tokens,
-  1088: klaytn_tokens,
-  288: polygon_tokens,
-  1285: bsc_tokens,
-  106: ethereum_tokens,
-  57: undefined_tokens,
-  40: undefined_tokens,
-  1101: undefined_tokens,
-  1442: undefined_tokens,
-  8453: undefined_tokens,
-  84531: undefined_tokens,
+  1088: arbitrum_goerli_tokens,
+  1101: arbitrum_tokens,
+  1284: avalanche_c_chain_tokens,
+  1285: polygon_tokens,
+  1442: bsc_tokens,
+  2222: ethereum_tokens,
   8217: undefined_tokens,
+  8453: undefined_tokens,
+  9001: undefined_tokens,
+  42161: undefined_tokens,
+  43114: undefined_tokens,
+  84531: undefined_tokens,
+  421613: undefined_tokens,
   245022934: undefined_tokens,
 };
 
 export const signatures = {
-  43114: ethereum_goerli_signatures,
-  56: arbitrum_goerli_signatures,
-  1: arbitrum_signatures,
-  61: cronos_signatures,
-  137: fantom_signatures,
-  3: bittorrent_signatures,
-  5: rsk_signatures,
-  42161: moonbeam_signatures,
-  421613: ethereum_ropsten_signatures,
-  25: optimism_signatures,
-  250: energy_web_signatures,
-  14: velas_evm_signatures,
-  19: avalanche_c_chain_signatures,
-  1284: syscoin_signatures,
-  30: metis_signatures,
-  199: astar_signatures,
-  2222: telos_evm_signatures,
-  9001: base_signatures,
-  10: base_goerli_signatures,
-  420: polygon_zk_evm_testnet_signatures,
-  246: polygon_zk_evm_signatures,
+  1: ethereum_ropsten_signatures,
+  3: cronos_signatures,
+  5: syscoin_signatures,
+  10: telos_evm_signatures,
+  14: rsk_signatures,
+  19: bittorrent_signatures,
+  25: velas_evm_signatures,
+  30: energy_web_signatures,
+  40: ethereum_goerli_signatures,
+  56: optimism_signatures,
+  57: fantom_signatures,
+  61: metis_signatures,
+  106: optimism_goerli_signatures,
+  137: polygon_zk_evm_signatures,
+  199: moonbeam_signatures,
+  246: polygon_zk_evm_testnet_signatures,
+  250: astar_signatures,
+  288: base_signatures,
+  420: base_goerli_signatures,
   592: neon_evm_signatures,
-  1088: klaytn_signatures,
-  288: polygon_signatures,
-  1285: bsc_signatures,
-  106: ethereum_signatures,
-  57: undefined_signatures,
-  40: undefined_signatures,
-  1101: undefined_signatures,
-  1442: undefined_signatures,
-  8453: undefined_signatures,
-  84531: undefined_signatures,
+  1088: arbitrum_goerli_signatures,
+  1101: arbitrum_signatures,
+  1284: avalanche_c_chain_signatures,
+  1285: polygon_signatures,
+  1442: bsc_signatures,
+  2222: ethereum_signatures,
   8217: undefined_signatures,
+  8453: undefined_signatures,
+  9001: undefined_signatures,
+  42161: undefined_signatures,
+  43114: undefined_signatures,
+  84531: undefined_signatures,
+  421613: undefined_signatures,
   245022934: undefined_signatures,
 };
 
