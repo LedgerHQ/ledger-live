08a2e295ec5bc2f9c789c08de881de7a95349309
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2023-11-07 14:08:00 +0100

    refactor(common/manager/api): clearer code + remove 4 year old FIXME

--- libs/ledger-live-common/src/manager/api.ts
+++ libs/ledger-live-common/src/manager/api.ts
@@ -377,7 +377,7 @@ const getCurrentFirmware: (input: {
         provider: input.provider,
       },
     }).catch(error => {
-      const status = error && (error.status || (error.response && error.response.status)); // FIXME LLD is doing error remapping already. we probably need to move the remapping in live-common
+      const status = error?.status || error?.response?.status;
 
       if (status === 404 && error.message === "not found entity") {
         throw new FirmwareNotRecognized();
@@ -465,7 +465,7 @@ const getDeviceVersion: (targetId: string | number, provider: number) => Promise
           target_id: targetId,
         },
       }).catch(error => {
-        const status = error && (error.status || (error.response && error.response.status)); // FIXME LLD is doing error remapping already. we probably need to move the remapping in live-common
+        const status = error?.status || error?.response?.status;
 
         if (status === 404 && error.message === "not found entity") {
           throw new FirmwareNotRecognized("manager api did not recognize targetId=" + targetId, {
