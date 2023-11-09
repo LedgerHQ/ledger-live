f9f92bf6b946b6736b52c4282a08ad4e36fd7fad
hzheng-ledger <huiqi.zheng@ledger.fr>
2023-11-09 09:58:17 +0100

    fix: refactoring

--- apps/ledger-live-mobile/src/components/FirebaseRemoteConfig.tsx
+++ apps/ledger-live-mobile/src/components/FirebaseRemoteConfig.tsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from "react";
 import remoteConfig from "@react-native-firebase/remote-config";
-<<<<<<< HEAD
 import { DEFAULT_FEATURES, formatDefaultFeatures } from "@ledgerhq/live-config/featureFlags/index";
 import type { FirebaseFeatureFlagsProviderProps as Props } from "@ledgerhq/live-config/featureFlags/index";
 import { LiveConfig } from "@ledgerhq/live-config/LiveConfig";
-=======
-import {
-  DEFAULT_FEATURES,
-  formatDefaultFeatures,
-  formatToFirebaseFeatureId,
-} from "@ledgerhq/live-config/FeatureFlags/index";
-import type { FirebaseFeatureFlagsProviderProps as Props } from "@ledgerhq/live-config/FeatureFlags/index";
-import { LiveConfig } from "@ledgerhq/live-config/FeatureFlags/index";
-import VersionNumber from "react-native-version-number";
-import { Platform } from "react-native";
->>>>>>> f8e0133b13 (fix: refactoring)
 
 export const FirebaseRemoteConfigProvider = ({ children }: Props): JSX.Element | null => {
   const [loaded, setLoaded] = useState<boolean>(false);
