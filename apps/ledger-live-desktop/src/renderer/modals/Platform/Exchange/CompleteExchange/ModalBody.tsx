193f4a08624caccc67b48f4b31f3de9ee8b1893b
sarneijim <sarneijim@gmail.com>
2023-11-13 13:50:15 +0000

    fix: fix lint

--- apps/ledger-live-desktop/src/renderer/modals/Platform/Exchange/CompleteExchange/ModalBody.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/Platform/Exchange/CompleteExchange/ModalBody.tsx
@@ -1,18 +1,5 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Exchange, ExchangeSwap } from "@ledgerhq/live-common/exchange/platform/types";
-import { Exchange as SwapExchange } from "@ledgerhq/live-common/exchange/swap/types";
-import { setBroadcastTransaction } from "@ledgerhq/live-common/exchange/swap/setBroadcastTransaction";
-import { getUpdateAccountWithUpdaterParams } from "@ledgerhq/live-common/exchange/swap/getUpdateAccountWithUpdaterParams";
-import { Operation, SignedOperation } from "@ledgerhq/types-live";
-import { Transaction } from "@ledgerhq/live-common/generated/types";
-import { TokenCurrency } from "@ledgerhq/types-cryptoassets";
-import { updateAccountWithUpdater } from "~/renderer/actions/accounts";
+import React from "react";
 import { ModalBody } from "~/renderer/components/Modal";
-import Box from "~/renderer/components/Box";
-import { useBroadcast } from "~/renderer/hooks/useBroadcast";
-import { BodyContent } from "./BodyContent";
-import { getMagnitudeAwareRate } from "@ledgerhq/live-common/exchange/swap/webApp/index";
 import LogicContent, { Data } from "./Body";
 
 const Body = ({ data, onClose }: { data: Data; onClose?: () => void | undefined }) => {
