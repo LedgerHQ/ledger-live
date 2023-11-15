b561c7cce0aab5d558494de74e60aa37625088b4
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-11-15 15:46:34 +0100

    chore: finalize unit test thanks to Interface

--- libs/ledger-live-common/src/device-core/use-cases/getLatestFirmwareForDevice.test.ts
+++ libs/ledger-live-common/src/device-core/use-cases/getLatestFirmwareForDevice.test.ts
@@ -1,28 +1,48 @@
 import { getLatestFirmwareForDevice } from "./getLatestFirmwareForDevice";
-import { ManagerApiRepository } from "../repositories/ManagerApiRepository";
+import { ManagerApiPort } from "../repositories/ManagerApiRepository";
 import { DeviceInfoEntity } from "../entities/DeviceInfoEntity";
+import { aDeviceInfoBuilder } from "../../mock/fixtures/aDeviceInfo";
+import { DeviceVersion, OsuFirmware } from "@ledgerhq/types-live";
+import { UnknownMCU } from "@ledgerhq/errors";
 jest.mock("../repositories/ManagerApiRepository");
 
-describe("getLatestFirmwareForDevice", () => {
-  let managerApiRepository: ManagerApiRepository;
+const mockedFetchMcus = jest.fn();
+const mockedGetDeviceVersion = jest.fn();
+const mockedGetCurrentOSU = jest.fn();
+const mockedManagerApiRepository: ManagerApiPort = {
+  fetchLatestFirmware: jest.fn(),
+  fetchMcus: mockedFetchMcus,
+  getDeviceVersion: mockedGetDeviceVersion,
+  getCurrentOSU: mockedGetCurrentOSU,
+  getCurrentFirmware: jest.fn(),
+  getFinalFirmwareById: jest.fn(),
+};
 
+describe("getLatestFirmwareForDevice", () => {
   beforeEach(() => {
-    managerApiRepository = new ManagerApiRepository("", "");
+    // Clear the methods we are using in our test
+    mockedFetchMcus.mockClear();
   });
 
   test("Error throw for unknown mcu", async () => {
     // given
-    const deviceInfo = { mcuVersion: "42" } as DeviceInfoEntity;
-    jest.spyOn(managerApiRepository, "fetchMcus").mockResolvedValue([]);
+    // Using a fixture builder - we could create that for all our entities
+    const deviceInfo = aDeviceInfoBuilder({ mcuVersion: "42", isOSU: true });
+
+    mockedFetchMcus.mockResolvedValue([]);
+    mockedGetDeviceVersion.mockResolvedValue({ id: 42 } as DeviceVersion);
+    mockedGetCurrentOSU.mockResolvedValue({} as OsuFirmware);
+
     const params = {
       deviceInfo,
       providerId: 42,
       userId: "",
-      managerApiRepository,
+      managerApiRepository: mockedManagerApiRepository,
     };
+
     // when
     const response = getLatestFirmwareForDevice(params);
     // then
-    expect(response).rejects.toContain({ name: "UnknownMCU" });
+    expect(response).rejects.toBeInstanceOf(UnknownMCU);
   });
 });
