9d35080944a6a63c78f54a545734f4cf3cbded63
Louis Aussedat <aussedat.louis@gmail.com>
2023-12-01 17:31:33 +0100

    Bugfix/8850 add battery check during ESC (#5138)
    * feat(libs): add LOW_BATTERY_PERCENTAGE environment variable
    
    * feat(common): add low battery check in battery statuses hook
    
    * feat(llm): move battery check drawer into firmware update screen
    
    * feat(llm): prevent re-display of fw update drawer post-cancellation
    
    * feat(llm,common,env): add changeset
    
    * feat(llm): don't open fw update drawer only when cancelled
    
    * feat(llm): remove getEnv from battery warning drawer
    
    * chore: add logs to getBatteryStatus
    
    * feat(common): return custom task event for unknown apdu for battery stat
    
    * feat(llm): fix eslint warning
    
    * feat(llm): fix typo
    
    * feat(common): improve tracer for get battery statuses
    
    * feat(common): remove error when UnknownApdu is set for battery check
    
    ---------
    
    Co-authored-by: Alexandre Magaud <alexandre.magaud@gmail.com>

---
+++ apps/ledger-live-mobile/src/screens/FirmwareUpdate/BatteryWarningDrawer.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import QueuedDrawer, { Props as QueuedDrawerProps } from "../../components/QueuedDrawer";
+import { Button, Flex, IconBadge, IconsLegacy, Text } from "@ledgerhq/native-ui";
+import { useTranslation } from "react-i18next";
+import { GetBatteryStatusesActionState } from "@ledgerhq/live-common/deviceSDK/actions/getBatteryStatuses";
+import { Device } from "@ledgerhq/types-devices";
+
+type Props = QueuedDrawerProps & {
+  state: GetBatteryStatusesActionState;
+  lowBatteryPercentage: number;
+  device: Device;
+  onRetry: () => void;
+  onQuit: () => void;
+};
+
+const BatteryWarningDrawer: React.FC<Props> = ({
+  device,
+  state,
+  lowBatteryPercentage,
+  onRetry,
+  onQuit,
+  ...props
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <QueuedDrawer noCloseButton {...props}>
+        <Flex>
+          <Flex alignItems="center" justifyContent="center" mb={8}>
+            <IconBadge
+              iconColor="primary.c100"
+              iconSize={32}
+              Icon={IconsLegacy.BatteryHalfMedium}
+            />
+            <Text fontSize={7} fontWeight="semiBold" textAlign="center" mt={6}>
+              {t("FirmwareUpdate.staxBatteryLow")}
+            </Text>
+            <Text fontSize={4} textAlign="center" color="neutral.c80" mt={6}>
+              {t("FirmwareUpdate.staxBatteryLowDescription", {
+                lowBatteryPercentage,
+              })}
+            </Text>
+          </Flex>
+
+          <Button type="main" outline={false} onPress={onRetry} mt={6} alignSelf="stretch">
+            {t("FirmwareUpdate.retryBatteryCheck")}
+          </Button>
+          <Button type="default" outline={false} onPress={onQuit} mt={6}>
+            {t("FirmwareUpdate.quitUpdate")}
+          </Button>
+        </Flex>
+      </QueuedDrawer>
+    </>
+  );
+};
+
+export default BatteryWarningDrawer;
