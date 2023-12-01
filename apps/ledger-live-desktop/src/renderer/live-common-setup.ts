f4310971985532ad5a2b6120058fa1e9f29816d3
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-12-01 17:43:20 +0100

    chore: renderer setup - retry question and tracing

--- apps/ledger-live-desktop/src/renderer/live-common-setup.ts
+++ apps/ledger-live-desktop/src/renderer/live-common-setup.ts
@@ -5,7 +5,7 @@ import "./families"; // families may set up their own things
 import VaultTransport from "@ledgerhq/hw-transport-vault";
 import { registerTransportModule } from "@ledgerhq/live-common/hw/index";
 import { retry } from "@ledgerhq/live-common/promise";
-import { listen as listenLogs } from "@ledgerhq/logs";
+import { TraceContext, listen as listenLogs, trace } from "@ledgerhq/logs";
 import { getUserId } from "~/helpers/user";
 import { setEnvOnAllThreads } from "./../helpers/env";
 import { IPCTransport } from "./IPCTransport";
@@ -27,14 +27,30 @@ listenLogs(({ id, date, ...log }) => {
 // This defines our IPC Transport that will proxy to an internal process (we shouldn't use node-hid on renderer)
 registerTransportModule({
   id: "ipc",
-  open: (id: string) => {
+  open: (id: string, timeoutMs?: number, context?: TraceContext) => {
     // id could be another type of transport such as vault-transport
     if (id.startsWith(vaultTransportPrefixID)) return;
 
     if (originalDeviceMode !== currentMode) {
       setDeviceMode(originalDeviceMode);
     }
-    return retry(() => IPCTransport.open(id));
+
+    trace({
+      type: "renderer-setup",
+      message: "Open called on registered module",
+      data: {
+        transport: "IPCTransport",
+        timeoutMs,
+      },
+      context: {
+        openContext: context,
+      },
+    });
+
+    // TODO: is it retrying on the renderer side and on the internal side ?
+    // Yes: for each retry here, 4 retries on the internal thread so 16 retries ?
+    return retry(() => IPCTransport.open(id, timeoutMs, context));
+    // return IPCTransport.open(id, timeoutMs, context);
   },
   disconnect: () => Promise.resolve(),
 });
