6d4da6e0eff9249b7241cfb90a93425c285b6e09
sarneijim <sarneijim@gmail.com>
2023-12-05 21:34:48 +0000

    feat: changing env variable to test

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -591,7 +591,7 @@ const envDefinitions = {
     desc: "Use speculos websocket interface instead of Rest API",
   },
   SWAP_API_BASE: {
-    def: "https://swap.ledger.com/v5",
+    def: "https://swap.aws.stg.ldg-tech.com/v5",
     parser: stringParser,
     desc: "Swap API base",
   },
