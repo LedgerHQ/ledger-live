529db5d65ce112957914ba1574e8a877b045188d
Kieran Allen <132384348+kallen-ledger@users.noreply.github.com>
2023-12-08 12:41:22 +0000

    bugfix: customDappUrl for non moonpay redirect providers (#5685)
    * fix: customDappUrl for non moonpay redirect providers
    
    * fix: address comments
    
    * chore: add changeset
    
    * fix: use customDappUrl in state
    
    * fix: append string

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/index.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/index.tsx
@@ -189,7 +189,7 @@ const SwapForm = () => {
   const generateMoonpayUrl = useCallback(
     ({ base = "", args = {} }: { base: string; args: { [key: string]: string | undefined } }) => {
       const moonpayURL = new URL(base || "");
-      moonpayURL.searchParams.append("ledgerlive", true);
+      moonpayURL.searchParams.append("ledgerlive", `${true}`);
       Object.entries(args).forEach(
         ([key, value]) =>
           // customFeeConfig is an object
@@ -269,10 +269,15 @@ const SwapForm = () => {
           ? accountToWalletAPIAccount(account, parentAccount)?.id
           : fromAccountId;
 
-      const state = {
+      const state: {
+        returnTo: string;
+        accountId?: string;
+        goToURL?: string;
+        customDappUrl?: string;
+      } = {
         returnTo: "/swap",
         accountId,
-        goToURL: providerURL,
+        customDappUrl: providerURL,
       };
 
       if (provider === "moonpay") {
@@ -280,12 +285,12 @@ const SwapForm = () => {
           base: exchangeRate?.providerURL || "",
           args: getExchangeSDKParams(),
         });
+        state.customDappUrl = undefined;
         state.goToURL = moonpayURL.toString();
       }
 
       history.push({
         // This looks like an issue, the proper signature is: push(path, [state]) - (function) Pushes a new entry onto the history stack
-        // It seems possible to also pass a LocationDescriptorObject but it does not expect extra properties
         pathname,
         state,
       });
