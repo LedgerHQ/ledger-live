033c2c7e0b7d8a4498d8361b19e361035b4451ab
sarneijim <sarneijim@gmail.com>
2023-12-13 18:30:16 +0000

    fix: fix exchange funds

--- apps/ledger-live-desktop/src/renderer/components/Web3AppWebview/WalletAPIWebview.tsx
+++ apps/ledger-live-desktop/src/renderer/components/Web3AppWebview/WalletAPIWebview.tsx
@@ -29,7 +29,7 @@ import { useWebviewState } from "./helpers";
 import { getStoreValue, setStoreValue } from "~/renderer/store";
 import { NetworkErrorScreen } from "./NetworkError";
 import getUser from "~/helpers/user";
-import { openExchangeDrawer } from "~/renderer/actions/UI";
+import { openExchangeDrawer, closePlatformAppDrawer } from "~/renderer/actions/UI";
 import { currentRouteNameRef } from "~/renderer/analytics/screenRefs";
 import { TrackFunction } from "@ledgerhq/live-common/platform/tracking";
 
@@ -163,9 +163,11 @@ function useUiHook(
             exchangeType: ExchangeType[exchangeType],
             onResult: (nonce: string) => {
               onSuccess(nonce);
+              dispatch(closePlatformAppDrawer());
             },
             onCancel: (error: Error) => {
               onCancel(error);
+              dispatch(closePlatformAppDrawer());
             },
           }),
         );
