5cf047576422bf4faf3c4aea3f216a50be7f782a
hzheng-ledger <huiqi.zheng@ledger.fr>
2023-12-13 10:42:55 +0100

    fix: build errors

--- apps/ledger-live-mobile/src/config/configInit.ts
+++ apps/ledger-live-mobile/src/config/configInit.ts
@@ -1,7 +1,6 @@
-import { LiveConfig, FirebaseRemoteConfigProvider } from "@ledgerhq/live-config";
+import { LiveConfig } from "@ledgerhq/live-config/LiveConfig";
 import { Platform } from "react-native";
 import VersionNumber from "react-native-version-number";
-import remoteConfig from "@react-native-firebase/remote-config";
 import { sharedConfig } from "@ledgerhq/live-common/config/sharedConfig";
 
 LiveConfig.init({
@@ -9,13 +8,5 @@ LiveConfig.init({
   platform: Platform.OS,
   environment: process.env.NODE_ENV ?? "development",
 });
-
-LiveConfig.setProvider(
-  new FirebaseRemoteConfigProvider({
-    getValue: (key: string) => {
-      return remoteConfig().getValue(key);
-    },
-  }),
-);
 
 LiveConfig.setConfig(sharedConfig);
