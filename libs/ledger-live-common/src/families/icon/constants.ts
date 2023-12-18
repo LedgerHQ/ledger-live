219d9a9b92cd4deebeb95429269d99b6b5ff1d65
heirland <adam.heirland@gmail.com>
2023-12-18 23:18:57 +0700

    fix: wrong after fix review

--- libs/ledger-live-common/src/families/icon/constants.ts
+++ libs/ledger-live-common/src/families/icon/constants.ts
@@ -3,7 +3,7 @@ import { convertLoopToIcx } from "./logic";
 export const GOVERNANCE_SCORE_ADDRESS = "cx0000000000000000000000000000000000000001";
 export const IISS_SCORE_ADDRESS = "cx0000000000000000000000000000000000000000";
 export const LIMIT = 20;
-export const BERLIN_TESTNET_NID = 2;
+export const BERLIN_TESTNET_NID = 7;
 export const MAINNET_NID = 1;
 export const STEP_LIMIT = 200000;
 export const I_SCORE_UNIT = 1000;
