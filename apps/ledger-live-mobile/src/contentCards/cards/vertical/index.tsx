225c5704e7680586659173a19e6925323a733caa
Maxime Aubanel <sshmaxime@gmail.com>
2023-12-18 11:14:54 +0100

    Rename sizes to style and add container to elements for vertical card

--- apps/ledger-live-mobile/src/contentCards/cards/vertical/index.tsx
+++ apps/ledger-live-mobile/src/contentCards/cards/vertical/index.tsx
@@ -1,9 +1,16 @@
 import { Flex } from "@ledgerhq/native-ui";
 import React, { useEffect } from "react";
-import { TouchableOpacity, View } from "react-native";
-import { useTheme } from "styled-components/native";
+import { TouchableOpacity } from "react-native";
 import { ContentCardBuilder } from "~/contentCards/cards/utils";
-import { Close, Image, Price, Subtitle, Tag, Title } from "~/contentCards/cards/vertical/elements";
+import {
+  Close,
+  Container,
+  Image,
+  Price,
+  Subtitle,
+  Tag,
+  Title,
+} from "~/contentCards/cards/vertical/elements";
 import { Size } from "~/contentCards/cards/vertical/types";
 
 type Props = {
@@ -15,48 +22,8 @@ type Props = {
   tag?: string;
 };
 
-export const SIZES: {
-  [key in Size]: {
-    height: number;
-
-    paddingTop: number;
-    paddingBottom: number;
-
-    radius: number;
-  };
-} = {
-  L: {
-    height: 306,
-
-    paddingTop: 24,
-    paddingBottom: 24,
-
-    radius: 12,
-  },
-  M: {
-    height: 206,
-
-    paddingTop: 16,
-    paddingBottom: 8,
-
-    radius: 16,
-  },
-  S: {
-    height: 156,
-
-    paddingTop: 16,
-    paddingBottom: 8,
-
-    radius: 16,
-  },
-};
-
 const VerticalCard = ContentCardBuilder<Props>(
   ({ title, description: subtitle, price, image, tag, size, metadata }) => {
-    const { colors } = useTheme();
-
-    const styles = SIZES[size];
-
     useEffect(() => metadata.actions?.onView?.());
 
     return (
@@ -64,19 +31,7 @@ const VerticalCard = ContentCardBuilder<Props>(
         {tag && <Tag size={size} label={tag} />}
         {metadata.actions?.onDismiss && <Close onPress={metadata.actions?.onDismiss} />}
 
-        <View
-          style={{
-            position: "relative",
-            justifyContent: "space-between",
-            alignItems: "center",
-
-            backgroundColor: colors.opacityDefault.c05,
-            paddingTop: styles.paddingTop,
-            paddingBottom: styles.paddingBottom,
-            borderRadius: styles.radius,
-            height: styles.height,
-          }}
-        >
+        <Container size={size}>
           <Image uri={image} />
 
           <Flex alignItems="center">
@@ -84,7 +39,7 @@ const VerticalCard = ContentCardBuilder<Props>(
             <Subtitle size={size} label={subtitle} />
             <Price size={size} label={price} />
           </Flex>
-        </View>
+        </Container>
       </TouchableOpacity>
     );
   },
