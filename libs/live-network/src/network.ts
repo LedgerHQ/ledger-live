08788efac4144bbb8067277e218eee02c9312b47
lvndry <lvndry@proton.me>
2023-12-21 15:59:33 +0100

    set https agent if in electron

--- libs/live-network/src/network.ts
+++ libs/live-network/src/network.ts
@@ -99,6 +99,15 @@ export const errorInterceptor = (error: InterceptedError): InterceptedError => {
 axios.interceptors.request.use(requestInterceptor);
 axios.interceptors.response.use(responseInterceptor, errorInterceptor);
 
+const userAgent = navigator.userAgent.toLowerCase();
+if (userAgent.includes(" electron/")) {
+  // the keepAlive is necessary when we make a lot of request in in parallel, especially for bitcoin sync. Otherwise, it may raise "connect ETIMEDOUT" error
+  // refer to https://stackoverflow.com/questions/63064393/getting-axios-error-connect-etimedout-when-making-high-volume-of-calls
+  // eslint-disable-next-line global-require,@typescript-eslint/no-var-requires
+  const https = require("https");
+  axios.defaults.httpsAgent = new https.Agent({ keepAlive: true });
+}
+
 const makeError = (msg: string, status: number, url: string | undefined, method: Method | "") => {
   const obj = {
     status,
