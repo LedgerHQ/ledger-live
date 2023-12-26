09759bda49ada92561ccc89c43c19970ca708528
jdabbech-ledger <jalil.dabbech@ledger.com>
2023-12-26 12:24:00 +0100

    fix(mobile): e2e script cache option

--- apps/ledger-live-mobile/scripts/e2e-ci.mjs
+++ apps/ledger-live-mobile/scripts/e2e-ci.mjs
@@ -1,24 +1,32 @@
 #!/usr/bin/env zx
-import { basename } from 'path'
+import { basename } from 'path';
 
-const usage = () => {
-  console.log(`Usage: ${basename(__filename)} [-h] [-p <ios|android>] [-t] [-b]`);
-  process.exit(1);
+let platform, test, build, bundle;
+let cache = true;
+
+const usage = (exitCode = 1) => {
+  console.log(`Usage: ${basename(__filename)} -p --platform <ios|android> [-h --help]  [-t --test] [-b --build] [--bundle] [--cache | --no-cache]`);
+  process.exit(exitCode);
 }
 
 const build_ios = async () => {
+  await $`pnpm mobile e2e:build -c ios.sim.release`;
+}
+
+const bundle_ios = async () => {
   await $`pnpm mobile bundle:ios --dev false --minify false`;
+}
+
+const bundle_ios_with_cache = async () => {
+  await bundle_ios();
+
   await $`pnpm mobile exec detox clean-framework-cache`;
   await $`pnpm mobile exec detox build-framework-cache`;
   within(async () => {
     cd('apps/ledger-live-mobile');
     await $`cp main.jsbundle ios/build/Build/Products/Release-iphonesimulator/ledgerlivemobile.app/main.jsbundle`;
     await $`mv main.jsbundle ios/build/Build/Products/Release-iphonesimulator/main.jsbundle`;
-  })
-}
-
-const build_android = async () => {
-  await $`pnpm mobile e2e:build -c android.emu.release`;
+  });
 }
 
 const test_ios = async () => {
@@ -33,6 +41,10 @@ const test_ios = async () => {
     --record-performance all`;
 }
 
+const build_android = async () => {
+  await $`pnpm mobile e2e:build -c android.emu.release`;
+}
+
 const test_android = async () => {
   await $`pnpm mobile e2e:test \\
     -c android.emu.release \\
@@ -45,53 +57,68 @@ const test_android = async () => {
     --cleanup`;
 }
 
-const platformToTask = {
+const getTasksFrom = {
   ios: {
     build: build_ios,
+    bundle: async () => cache ? await bundle_ios_with_cache() : await bundle_ios(),
     test: test_ios,
   },
   android: {
     build: build_android,
+    bundle: () => undefined,
     test: test_android
   }
 }
 
-let platform;
-let test, build;
-
 for (const argName in argv) {
   switch (argName) {
+    case 'help':
     case 'h':
-      usage();
+      usage(0);
       break;
+    case 'platform':
     case 'p':
       if (argv[argName] !== 'ios' && argv[argName] !== 'android') {
-        usage()
+        usage(1);
       } else {
-        platform = argv[argName]
+        platform = argv[argName];
       }
       break;
+    case 'test':
     case 't':
-      test = true
+      test = true;
       break;
+    case 'build':
     case 'b':
       build = true;
+      break;
+    case 'bundle':
+      bundle = true;
+      break;
+    case 'cache':
+      cache = argv[argName];
       break;
     case '_':
       break;
     default:
-      usage()
+      usage(42);
       break;
   }
 }
 
 within(async () => {
-  cd('../../')
+  if (!platform) {
+    usage(2)
+  }
 
+  cd('../../');
   if (build) {
-    await platformToTask[platform].build()
+    await getTasksFrom[platform].build();
+  }
+  if (bundle) {
+    await getTasksFrom[platform].bundle();
   }
   if (test) {
-    await platformToTask[platform].test()
+    await getTasksFrom[platform].test();
   }
-})
+});
