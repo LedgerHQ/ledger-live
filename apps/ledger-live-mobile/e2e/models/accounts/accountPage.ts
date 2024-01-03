e416d076b8afb44ab90df8e0d2c0c3fedb1ffa5d
alalmi-ledger <96071577+alalmi-ledger@users.noreply.github.com>
2024-01-03 15:59:48 +0100

    test: add test for adding account from modal + correction in doc

--- apps/ledger-live-mobile/e2e/models/accounts/accountPage.ts
+++ apps/ledger-live-mobile/e2e/models/accounts/accountPage.ts
@@ -1,19 +1,20 @@
-import { currencyParam, openDeeplink, waitForElementById } from "../../helpers";
+import { currencyParam, openDeeplink, waitForElementById, getElementById } from "../../helpers";
 
 const baseLink = "account";
 
 export default class accountPage {
-  async openViaDeeplink(currencyName?: string) {
-    const link = currencyName ? baseLink + currencyParam + currencyName : baseLink;
+  accountSettingsButton = () => getElementById("accounts-settings");
+
+  async waitForAccountPageToLoad(coin: string) {
+    await waitForElementById(`accounts-title-${coin}`);
+  }
+
+  async openViaDeeplink(currencyLong?: string) {
+    const link = currencyLong ? baseLink + currencyParam + currencyLong : baseLink;
     await openDeeplink(link);
   }
 
-  async waitForAccountsPageToLoad() {
-    await waitForElementById("accounts-list-title");
-  }
-
-  async waitForAccountAssetsToLoad(currencyName: string) {
-    await waitForElementById(`account-assets-${currencyName}`);
-    await waitForElementById(`accounts-title-${currencyName}`);
+  async openAccountSettings() {
+    await this.accountSettingsButton().tap();
   }
 }
