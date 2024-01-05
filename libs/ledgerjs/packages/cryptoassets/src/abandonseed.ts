c2691d280e1c757aaebe83265625734bfa02db35
lvndry <lvndry@proton.me>
2024-01-05 17:25:16 +0100

    linea_goerli dead address

--- libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
+++ libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
@@ -109,6 +109,7 @@ const abandonSeedAddresses: Partial<Record<CryptoCurrency["id"], string>> = {
   vechain: EVM_DEAD_ADDRESS,
   lukso: EVM_DEAD_ADDRESS,
   linea: EVM_DEAD_ADDRESS,
+  linea_goerli: EVM_DEAD_ADDRESS,
 };
 
 /**
