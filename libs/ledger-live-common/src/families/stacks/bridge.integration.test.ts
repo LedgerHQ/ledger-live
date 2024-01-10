770d6a961e434d981ce34c731218dab06c88b325
Prateek Rathod <prateek@zondax.ch>
2024-01-10 16:26:33 +0100

    tests(stx): ignore `fee` on integration tests snapshots

--- libs/ledger-live-common/src/families/stacks/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/stacks/bridge.integration.test.ts
@@ -18,6 +18,7 @@ const ACCOUNT_1 = "SP2DV2RVZP1A69Q6VAG5PHEQ6ZHQHZPCV84TMYNGN";
 
 const stacks: CurrenciesData<Transaction> = {
   FIXME_ignoreAccountFields: [],
+  IgnorePrepareTransactionFields: ["fee"],
   scanAccounts: [
     {
       name: "stacks seed 1",
