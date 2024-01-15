bbbee2c10d07d805c1f1fbaf0b91ab62ac51d84c
Carlo Sala <carlosalag@protonmail.com>
2024-01-15 11:59:24 +0100

    feat(ton): add LLD integration

---
+++ apps/ledger-live-desktop/src/renderer/families/ton/AccountSubHeader.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader/index";
+
+export default function TonAccountSubHeader() {
+  return <AccountSubHeader family="TON" team="Zondax"></AccountSubHeader>;
+}
