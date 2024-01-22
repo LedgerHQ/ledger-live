499367c2c5c89a67cf3859f885930942843ecc0d
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2024-01-22 17:23:40 +0100

    refactor(common/device/fetchLatestFirmwareUseCase): use repository

--- libs/ledger-live-common/src/device/use-cases/managerApi/fetchLatestFirmwareUseCase.ts
+++ libs/ledger-live-common/src/device/use-cases/managerApi/fetchLatestFirmwareUseCase.ts
@@ -1,28 +1,25 @@
-import { version } from "../../../../package.json";
 import { getEnv } from "@ledgerhq/live-env";
-import { fetchLatestFirmware } from "../../../device-core/use-cases/managerApi/fetchLatestFirmware";
 import { Id } from "@ledgerhq/types-live";
+import { version } from "../../../../package.json";
+import { ManagerApiHttpRepository } from "../../../device-core/repositories/ManagerApiRepository";
 
 export type FetchLatestFirmwareUseCaseParams = {
   current_se_firmware_final_version: Id;
   device_version: Id;
   provider: number;
+  managerApiRepository: ManagerApiHttpRepository;
 };
 
-/**
- * TODO: not used anywhere in the codebase. Only used in `libs/ledger-live-common/src/device-core/use-cases/getLatestFirmwareForDevice.ts`
- */
 export default function fetchLatestFirmwareUseCase({
   current_se_firmware_final_version,
   device_version,
   provider,
+  managerApiRepository = new ManagerApiHttpRepository(getEnv("MANAGER_API_BASE"), version),
 }: FetchLatestFirmwareUseCaseParams) {
-  return fetchLatestFirmware({
+  return managerApiRepository.fetchLatestFirmware({
     current_se_firmware_final_version,
     device_version,
     providerId: provider,
-    managerApiBase: getEnv("MANAGER_API_BASE"),
     userId: getEnv("USER_ID"),
-    liveCommonVersion: version,
   });
 }
