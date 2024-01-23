68cdac1338b633b37bb140feda482b6ec1b8b56f
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2024-01-23 19:02:52 +0100

    refactor(live-common/device): move fetchMcusUseCase out of subfolder

---
+++ libs/ledger-live-common/src/device/use-cases/fetchMcusUseCase.ts
@@ -0,0 +1,10 @@
+import { version } from "../../../package.json";
+import { getEnv } from "@ledgerhq/live-env";
+import { HttpManagerApiRepository } from "../../device-core/managerApi/repositories/HttpManagerApiRepository";
+import { fetchMcus } from "../../device-core/managerApi/use-cases/fetchMcus";
+
+export default function fetchMcusUseCase(
+  managerApiRepository = new HttpManagerApiRepository(getEnv("MANAGER_API_BASE"), version),
+) {
+  return fetchMcus(managerApiRepository);
+}
