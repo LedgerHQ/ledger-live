2c44553eb6fbc7626de1eae4137eab95bbb52c60
ofreyssinet-ledger <olivier.freyssinet@ledger.fr>
2024-01-24 17:29:32 +0100

    fix(common/HttpManagerApiRepository): cached functions created at instanciation

--- libs/ledger-live-common/src/device-core/managerApi/repositories/HttpManagerApiRepository.ts
+++ libs/ledger-live-common/src/device-core/managerApi/repositories/HttpManagerApiRepository.ts
@@ -5,7 +5,6 @@ import URL from "url";
 import { FirmwareNotRecognized } from "@ledgerhq/errors";
 import { ManagerApiRepository } from "./ManagerApiRepository";
 import { FinalFirmware, OsuFirmware } from "../entities/FirmwareUpdateContextEntity";
-import { IdEntity } from "../entities/IdEntity";
 import { DeviceVersionEntity } from "../entities/DeviceVersionEntity";
 
 export class HttpManagerApiRepository implements ManagerApiRepository {
@@ -17,185 +16,154 @@ export class HttpManagerApiRepository implements ManagerApiRepository {
     this.liveCommonVersion = liveCommonVersion;
   }
 
-  async fetchLatestFirmware(params: {
-    current_se_firmware_final_version: IdEntity;
-    device_version: IdEntity;
-    providerId: number;
-    userId: string;
-  }): Promise<OsuFirmware | null | undefined> {
-    return makeLRUCache(
-      async ({ current_se_firmware_final_version, device_version, providerId, userId }) => {
-        const salt = getUserHashes(userId).firmwareSalt;
-        const {
-          data,
-        }: {
-          data: {
-            result: string;
-            se_firmware_osu_version: OsuFirmware;
-          };
-        } = await network({
-          method: "POST",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/get_latest_firmware`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-              salt,
-            },
-          }),
-          data: {
-            current_se_firmware_final_version,
-            device_version,
-            provider: providerId,
+  readonly fetchLatestFirmware = makeLRUCache(
+    async ({ current_se_firmware_final_version, device_version, providerId, userId }) => {
+      const salt = getUserHashes(userId).firmwareSalt;
+      const {
+        data,
+      }: {
+        data: {
+          result: string;
+          se_firmware_osu_version: OsuFirmware;
+        };
+      } = await network({
+        method: "POST",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/get_latest_firmware`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
+            salt,
           },
-        });
+        }),
+        data: {
+          current_se_firmware_final_version,
+          device_version,
+          provider: providerId,
+        },
+      });
 
-        if (data.result === "null") {
-          return null;
+      if (data.result === "null") {
+        return null;
+      }
+
+      return data.se_firmware_osu_version;
+    },
+    a => `${a.current_se_firmware_final_version}_${a.device_version}_${a.providerId}`,
+  );
+
+  readonly fetchMcus = makeLRUCache(
+    async () => {
+      const { data } = await network({
+        method: "GET",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/mcu_versions`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
+          },
+        }),
+      });
+      return data;
+    },
+    () => "",
+  );
+
+  readonly getDeviceVersion = makeLRUCache(
+    async ({ targetId, providerId }) => {
+      const {
+        data,
+      }: {
+        data: DeviceVersionEntity;
+      } = await network({
+        method: "POST",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/get_device_version`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
+          },
+        }),
+        data: {
+          provider: providerId,
+          target_id: targetId,
+        },
+      }).catch(error => {
+        const status = error && (error.status || (error.response && error.response.status)); // FIXME LLD is doing error remapping already. we probably need to move the remapping in live-common
+
+        if (status === 404) {
+          throw new FirmwareNotRecognized("manager api did not recognize targetId=" + targetId, {
+            targetId,
+          });
         }
 
-        return data.se_firmware_osu_version;
-      },
-      a => `${a.current_se_firmware_final_version}_${a.device_version}_${a.providerId}`,
-    )(params);
-  }
+        throw error;
+      });
+      return data;
+    },
+    ({ targetId, providerId }) => `${targetId}_${providerId}`,
+  );
 
-  fetchMcus(): Promise<any> {
-    return makeLRUCache(
-      async () => {
-        const { data } = await network({
-          method: "GET",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/mcu_versions`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-            },
-          }),
-        });
-        return data;
-      },
-      () => "",
-    )();
-  }
-
-  async getDeviceVersion({
-    targetId,
-    providerId,
-  }: {
-    targetId: string | number;
-    providerId: number;
-  }): Promise<DeviceVersionEntity> {
-    return makeLRUCache(
-      async ({ targetId, providerId }) => {
-        const {
-          data,
-        }: {
-          data: DeviceVersionEntity;
-        } = await network({
-          method: "POST",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/get_device_version`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-            },
-          }),
-          data: {
-            provider: providerId,
-            target_id: targetId,
+  readonly getCurrentOSU = makeLRUCache(
+    async input => {
+      const { data } = await network({
+        method: "POST",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/get_osu_version`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
           },
-        }).catch(error => {
-          const status = error && (error.status || (error.response && error.response.status)); // FIXME LLD is doing error remapping already. we probably need to move the remapping in live-common
+        }),
+        data: {
+          device_version: input.deviceId,
+          version_name: `${input.version}-osu`,
+          provider: input.providerId,
+        },
+      });
+      return data;
+    },
+    a => `${a.version}_${a.deviceId}_${a.providerId}`,
+  );
 
-          if (status === 404) {
-            throw new FirmwareNotRecognized("manager api did not recognize targetId=" + targetId, {
-              targetId,
-            });
-          }
-
-          throw error;
-        });
-        return data;
-      },
-      ({ targetId, providerId }) => `${targetId}_${providerId}`,
-    )({ targetId, providerId });
-  }
-
-  async getCurrentOSU(params: {
-    deviceId: string | number;
-    providerId: string | number;
-    version: string;
-  }): Promise<OsuFirmware> {
-    return makeLRUCache(
-      async input => {
-        const { data } = await network({
-          method: "POST",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/get_osu_version`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-            },
-          }),
-          data: {
-            device_version: input.deviceId,
-            version_name: `${input.version}-osu`,
-            provider: input.providerId,
+  readonly getCurrentFirmware = makeLRUCache(
+    async input => {
+      const {
+        data,
+      }: {
+        data: FinalFirmware;
+      } = await network({
+        method: "POST",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/get_firmware_version`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
           },
-        });
-        return data;
-      },
-      a => `${a.version}_${a.deviceId}_${a.providerId}`,
-    )(params);
-  }
+        }),
+        data: {
+          device_version: input.deviceId,
+          version_name: input.version,
+          provider: input.providerId,
+        },
+      });
+      return data;
+    },
+    a => `${a.version}_${a.deviceId}_${a.providerId}`,
+  );
 
-  async getCurrentFirmware(params: {
-    version: string;
-    deviceId: string | number;
-    providerId: number;
-  }): Promise<FinalFirmware> {
-    return makeLRUCache(
-      async input => {
-        const {
-          data,
-        }: {
-          data: FinalFirmware;
-        } = await network({
-          method: "POST",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/get_firmware_version`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-            },
-          }),
-          data: {
-            device_version: input.deviceId,
-            version_name: input.version,
-            provider: input.providerId,
+  readonly getFinalFirmwareById = makeLRUCache(
+    async id => {
+      const {
+        data,
+      }: {
+        data: FinalFirmware;
+      } = await network({
+        method: "GET",
+        url: URL.format({
+          pathname: `${this.managerApiBase}/firmware_final_versions/${id}`,
+          query: {
+            livecommonversion: this.liveCommonVersion,
           },
-        });
-        return data;
-      },
-      a => `${a.version}_${a.deviceId}_${a.providerId}`,
-    )(params);
-  }
-
-  async getFinalFirmwareById(id: number): Promise<FinalFirmware> {
-    return makeLRUCache(
-      async id => {
-        const {
-          data,
-        }: {
-          data: FinalFirmware;
-        } = await network({
-          method: "GET",
-          url: URL.format({
-            pathname: `${this.managerApiBase}/firmware_final_versions/${id}`,
-            query: {
-              livecommonversion: this.liveCommonVersion,
-            },
-          }),
-        });
-        return data;
-      },
-      id => String(id),
-    )(id);
-  }
+        }),
+      });
+      return data;
+    },
+    id => String(id),
+  );
 }
