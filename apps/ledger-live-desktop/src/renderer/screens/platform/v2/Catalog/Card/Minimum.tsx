4c89eb349dea4c89aac44047770095b4415b85c7
RamyEB <ramyelb@gmail.com>
2024-01-29 19:31:59 +0900

    feat: change type + improvements

--- apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/Minimum.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/Minimum.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Flex, Text } from "@ledgerhq/react-ui";
 import { Logo } from "./Logo";
 import { PropsRaw } from "./types";
@@ -11,7 +11,11 @@ export function MinimumCard(props: PropsRaw) {
   const { disabled, onClick } = useCard(props);
   const { manifest } = props;
 
-  const rtf = new Intl.RelativeTimeFormat(useSelector(languageSelector));
+  const lang = useSelector(languageSelector);
+  const usedAt = useMemo(() => {
+    const rtf = new Intl.RelativeTimeFormat(lang);
+    return rtf.format(-manifest.usedAt.diff, manifest.usedAt.unit);
+  }, [lang, manifest.usedAt.diff, manifest.usedAt.unit]);
 
   return (
     <Container disabled={disabled} onClick={onClick} width={300}>
@@ -22,7 +26,7 @@ export function MinimumCard(props: PropsRaw) {
           <Text overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis" fontSize={14}>
             {manifest.name}
           </Text>
-          <Subtitle>{rtf.format(-manifest.usedAt.diff, manifest.usedAt.unit)}</Subtitle>
+          <Subtitle>{usedAt}</Subtitle>
         </Flex>
       </Flex>
     </Container>
