b31ce9048c703e909cb8cc73d03f66cfcd187c48
Beth Swingler <beth.swingler-ext@ledger.fr>
2024-01-29 19:11:19 +0000

    feat: style scrollbar, update translations, add stader figment icons

--- apps/ledger-live-mobile/src/families/evm/StakingDrawer/index.tsx
+++ apps/ledger-live-mobile/src/families/evm/StakingDrawer/index.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from "react";
-import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
-import { Button, Flex } from "@ledgerhq/native-ui";
+import useFeature from "@ledgerhq/live-config/featureFlags/useFeature";
+import { Button, Flex, ScrollListContainer } from "@ledgerhq/native-ui";
 import { useTranslation } from "react-i18next";
 import { EvmStakingDrawerBody } from "./EvmStakingDrawerBody";
 import { Track } from "~/analytics";
 import QueuedDrawer from "~/components/QueuedDrawer";
 import { useRootDrawerContext } from "~/context/RootDrawerContext";
 
+import { useTheme } from "styled-components/native";
+import type { ListProvider } from "./types";
+
+const ascending = (a: ListProvider, b: ListProvider) => (a?.min || 0) - (b?.min || 0);
+const descending = (a: ListProvider, b: ListProvider) => (b?.min || 0) - (a?.min || 0);
+
 export function EvmStakingDrawer() {
   const { t } = useTranslation();
   const { isOpen, onModalHide, openDrawer, onClose, drawer } = useRootDrawerContext();
   const ethStakingProviders = useFeature("ethStakingProviders");
+  const { theme: themeName } = useTheme();
 
   useEffect(() => {
     if (
@@ -23,19 +30,33 @@ export function EvmStakingDrawer() {
 
   if (!ethStakingProviders || drawer.id !== "EvmStakingDrawer") return null;
 
+  const has32Eth = drawer.props.has32Eth ?? false;
+
+  const listProvidersSorted = ethStakingProviders.params!.listProvider.sort(
+    has32Eth ? descending : ascending,
+  );
+
   return (
     <QueuedDrawer isRequestingToBeOpened={isOpen} onClose={onClose} onModalHide={onModalHide}>
-      <Flex rowGap={52}>
+      <Flex justifyContent={"center"}>
         <Track onMount event="ETH Stake Modal" />
-        <EvmStakingDrawerBody
-          onClose={onClose}
-          singleProviderRedirectMode={drawer.props.singleProviderRedirectMode ?? true}
-          accountId={drawer.props.accountId}
-          providers={ethStakingProviders.params!.listProvider}
-        />
-        <Button onPress={() => onClose()} type="main">
-          {t("stake.ethereum.close")}
-        </Button>
+        <ScrollListContainer
+          fadingEdgeLength={36}
+          paddingX={4}
+          alwaysBounceVertical={false}
+          indicatorStyle={themeName === "dark" ? "white" : "default"}
+          scrollIndicatorInsets={{ left: 2, bottom: 64 }}
+        >
+          <EvmStakingDrawerBody
+            onClose={onClose}
+            singleProviderRedirectMode={drawer.props.singleProviderRedirectMode ?? true}
+            accountId={drawer.props.accountId}
+            providers={listProvidersSorted}
+          />
+          <Button onPress={() => onClose()} type="main">
+            {t("stake.ethereum.close")}
+          </Button>
+        </ScrollListContainer>
       </Flex>
     </QueuedDrawer>
   );
