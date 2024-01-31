088ea0f056bdf9b108454346e97006cbfd0b9cb7
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2024-01-31 18:16:12 +0100

    test(common/listApps/v2): test basic error cases
    test(common/listApps/v2): more error cases

---
+++ libs/ledger-live-common/src/mock/fixtures/aDeviceVersion.ts
@@ -0,0 +1,20 @@
+import { DeviceVersion } from "@ledgerhq/types-live";
+
+export const aDeviceVersionBuilder = (props?: Partial<DeviceVersion>): DeviceVersion => {
+  return {
+    name: "Ledger Nano S",
+    device: 3,
+    providers: [],
+    id: 5,
+    display_name: "Ledger Nano S",
+    target_id: "0x31100004",
+    description: "Ledger Nano S",
+    mcu_versions: [1.7],
+    se_firmware_final_versions: [1.2],
+    osu_versions: [],
+    application_versions: [],
+    date_creation: "2020-04-30T13:50:00.000Z",
+    date_last_modified: "2020-04-30T13:50:00.000Z",
+    ...props,
+  };
+};
