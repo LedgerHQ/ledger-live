606ffa5369bc1e64ed868c43fc2486c65f539a1f
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2024-02-01 17:10:43 +0100

    refactor(StubManagerApiRepository): replace mocks

--- libs/ledger-live-common/src/device-core/managerApi/repositories/StubManagerApiRepository.ts
+++ libs/ledger-live-common/src/device-core/managerApi/repositories/StubManagerApiRepository.ts
@@ -1,28 +1,13 @@
-import { DeviceInfoEntity } from "../entities/DeviceInfoEntity";
 import { DeviceVersionEntity } from "../entities/DeviceVersionEntity";
 import { FinalFirmware, OsuFirmware } from "../entities/FirmwareUpdateContextEntity";
-import { LanguagePackageEntity } from "../entities/LanguagePackageEntity";
+import { aDeviceVersionEntityBuilder } from "../entities/mocks/aDeviceVersionEntityBuilder";
+import { aFinalFirmwareBuilder } from "../entities/mocks/aFinalFirmware";
+import { aOsuFirmwareBuilder } from "../entities/mocks/aOsuFirmware";
 import { ManagerApiRepository } from "./ManagerApiRepository";
 
 export class StubManagerApiRepository implements ManagerApiRepository {
   readonly fetchLatestFirmware = () => {
-    const result: OsuFirmware = {
-      id: 0,
-      name: "",
-      display_name: "",
-      notes: "",
-      perso: "",
-      firmware: "",
-      firmware_key: "",
-      hash: "",
-      device_versions: [],
-      next_se_firmware_final_version: 0,
-      providers: [],
-      date_creation: "",
-      date_last_modified: "",
-      description: "",
-      previous_se_firmware_final_version: [],
-    };
+    const result: OsuFirmware = aOsuFirmwareBuilder();
     return Promise.resolve(result);
   };
 
@@ -31,90 +16,22 @@ export class StubManagerApiRepository implements ManagerApiRepository {
   };
 
   readonly getDeviceVersion = () => {
-    const result: DeviceVersionEntity = {
-      name: "Ledger Nano S",
-      device: 3,
-      providers: [],
-      id: 5,
-      display_name: "Ledger Nano S",
-      target_id: "0x31100004",
-      description: "Ledger Nano S",
-      mcu_versions: [1.7],
-      se_firmware_final_versions: [1.2],
-      osu_versions: [],
-      application_versions: [],
-      date_creation: "2020-04-30T13:50:00.000Z",
-      date_last_modified: "2020-04-30T13:50:00.000Z",
-    };
+    const result: DeviceVersionEntity = aDeviceVersionEntityBuilder();
     return Promise.resolve(result);
   };
 
   readonly getCurrentOSU = () => {
-    const result: OsuFirmware = {
-      id: 0,
-      name: "",
-      display_name: "",
-      notes: "",
-      perso: "",
-      firmware: "",
-      firmware_key: "",
-      hash: "",
-      device_versions: [],
-      next_se_firmware_final_version: 0,
-      providers: [],
-      date_creation: "",
-      date_last_modified: "",
-      description: "",
-      previous_se_firmware_final_version: [],
-    };
+    const result: OsuFirmware = aOsuFirmwareBuilder();
     return Promise.resolve(result);
   };
 
   readonly getCurrentFirmware = () => {
-    const result: FinalFirmware = {
-      id: 1,
-      name: "FINAL",
-      description: null,
-      display_name: null,
-      notes: null,
-      perso: "",
-      firmware: "",
-      firmware_key: "",
-      hash: "",
-      date_creation: "",
-      date_last_modified: "",
-      device_versions: [],
-      providers: [],
-      version: "0",
-      se_firmware: 1,
-      osu_versions: [],
-      mcu_versions: [],
-      application_versions: [],
-    };
+    const result: FinalFirmware = aFinalFirmwareBuilder();
     return Promise.resolve(result);
   };
 
   readonly getFinalFirmwareById = () => {
-    const result: FinalFirmware = {
-      id: 1,
-      name: "FINAL",
-      description: null,
-      display_name: null,
-      notes: null,
-      perso: "",
-      firmware: "",
-      firmware_key: "",
-      hash: "",
-      date_creation: "",
-      date_last_modified: "",
-      device_versions: [],
-      providers: [],
-      version: "0",
-      se_firmware: 1,
-      osu_versions: [],
-      mcu_versions: [],
-      application_versions: [],
-    };
+    const result: FinalFirmware = aFinalFirmwareBuilder();
     return Promise.resolve(result);
   };
 
