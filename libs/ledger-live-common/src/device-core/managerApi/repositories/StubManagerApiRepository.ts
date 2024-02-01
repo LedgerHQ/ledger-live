f0928310e06e79b2d9d5415b04f540136a84b929
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2024-02-01 14:48:59 +0100

    refactor(ManagerApiRepository): implement StubManagerApiRepository

---
+++ libs/ledger-live-common/src/device-core/managerApi/repositories/StubManagerApiRepository.ts
@@ -0,0 +1,122 @@
+import { DeviceVersionEntity } from "../entities/DeviceVersionEntity";
+import { FinalFirmware, OsuFirmware } from "../entities/FirmwareUpdateContextEntity";
+import { ManagerApiRepository } from "./ManagerApiRepository";
+
+export class StubManagerApiRepository implements ManagerApiRepository {
+  readonly fetchLatestFirmware = () => {
+    const result: OsuFirmware = {
+      id: 0,
+      name: "",
+      display_name: "",
+      notes: "",
+      perso: "",
+      firmware: "",
+      firmware_key: "",
+      hash: "",
+      device_versions: [],
+      next_se_firmware_final_version: 0,
+      providers: [],
+      date_creation: "",
+      date_last_modified: "",
+      description: "",
+      previous_se_firmware_final_version: [],
+    };
+    return Promise.resolve(result);
+  };
+
+  readonly fetchMcus = () => {
+    return Promise.resolve([]);
+  };
+
+  readonly getDeviceVersion = () => {
+    const result: DeviceVersionEntity = {
+      name: "Ledger Nano S",
+      device: 3,
+      providers: [],
+      id: 5,
+      display_name: "Ledger Nano S",
+      target_id: "0x31100004",
+      description: "Ledger Nano S",
+      mcu_versions: [1.7],
+      se_firmware_final_versions: [1.2],
+      osu_versions: [],
+      application_versions: [],
+      date_creation: "2020-04-30T13:50:00.000Z",
+      date_last_modified: "2020-04-30T13:50:00.000Z",
+    };
+    return Promise.resolve(result);
+  };
+
+  readonly getCurrentOSU = () => {
+    const result: OsuFirmware = {
+      id: 0,
+      name: "",
+      display_name: "",
+      notes: "",
+      perso: "",
+      firmware: "",
+      firmware_key: "",
+      hash: "",
+      device_versions: [],
+      next_se_firmware_final_version: 0,
+      providers: [],
+      date_creation: "",
+      date_last_modified: "",
+      description: "",
+      previous_se_firmware_final_version: [],
+    };
+    return Promise.resolve(result);
+  };
+
+  readonly getCurrentFirmware = () => {
+    const result: FinalFirmware = {
+      id: 1,
+      name: "FINAL",
+      description: null,
+      display_name: null,
+      notes: null,
+      perso: "",
+      firmware: "",
+      firmware_key: "",
+      hash: "",
+      date_creation: "",
+      date_last_modified: "",
+      device_versions: [],
+      providers: [],
+      version: "0",
+      se_firmware: 1,
+      osu_versions: [],
+      mcu_versions: [],
+      application_versions: [],
+    };
+    return Promise.resolve(result);
+  };
+
+  readonly getFinalFirmwareById = () => {
+    const result: FinalFirmware = {
+      id: 1,
+      name: "FINAL",
+      description: null,
+      display_name: null,
+      notes: null,
+      perso: "",
+      firmware: "",
+      firmware_key: "",
+      hash: "",
+      date_creation: "",
+      date_last_modified: "",
+      device_versions: [],
+      providers: [],
+      version: "0",
+      se_firmware: 1,
+      osu_versions: [],
+      mcu_versions: [],
+      application_versions: [],
+    };
+    return Promise.resolve(result);
+  };
+
+  readonly getAppsByHash = () => {
+    return Promise.resolve([]);
+  };
+}
