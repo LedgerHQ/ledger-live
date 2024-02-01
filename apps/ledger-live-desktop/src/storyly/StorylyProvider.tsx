90554c5f695b6dc89a11d531e9d7f026589752ed
male-gal <maxence.le-gall@ledger.fr>
2024-02-01 15:31:59 +0100

    feat(StorylyLLD): Upgrade storyly lib

--- apps/ledger-live-desktop/src/storyly/StorylyProvider.tsx
+++ apps/ledger-live-desktop/src/storyly/StorylyProvider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, ReactNode, useRef, useEffect } from "react";
-import useFeature from "@ledgerhq/live-config/featureFlags/useFeature";
+import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
 import { Feature_Storyly, StorylyInstanceType } from "@ledgerhq/types-live";
 
 import { StorylyRef } from "storyly-web";
@@ -29,15 +29,11 @@ const StorylyProvider: React.FC<StorylyProviderProps> = ({ children }) => {
 
   const storylyRef = useRef<StorylyRef>(null);
 
-  const {
-    // @ts-expect-error TYPINGS
-    params: { stories },
-  } = useFeature("storyly") || {};
-
-  const storyly = useFeature("storyly");
+  const { params } = useFeature("storyly") || {};
+  const stories = params?.stories;
 
   useEffect(() => {
-    if (!storyly || !token || !query) return;
+    if (!params || !token || !query) return;
     storylyRef.current?.init({
       layout: "classic",
       token: token,
@@ -46,7 +42,7 @@ const StorylyProvider: React.FC<StorylyProviderProps> = ({ children }) => {
       },
     });
     storylyRef.current?.openStory({ group: query?.g, story: query?.s, playMode: query?.play });
-  }, [storyly, token, query]);
+  }, [params, token, query]);
 
   useEffect(() => {
     if (url) {
