b1f7d8a8a95d434a1b39576d52f43678d11ac33b
Louis PAQUET <louis.paquet@ledger.fr>
2024-02-02 13:50:00 +0100

    feat(LIVE-9363): add semi colomn

--- libs/ledgerjs/packages/hw-app-exchange/src/ReturnCode.ts
+++ libs/ledgerjs/packages/hw-app-exchange/src/ReturnCode.ts
@@ -31,7 +31,7 @@ export function getExchangeErrorMessage(errorCode: number, step?: string): strin
       return "Wrond transaction id";
     case ErrorStatus.INVALID_ADDRESS:
       if (step === "CHECK_PAYOUT_ADDRESS")
-        return "This receiving account does not belong to your device. Please change and retry."
+        return "This receiving account does not belong to your device. Please change and retry.";
       else if (step === "CHECK_REFUND_ADDRESS")
         return "This receiving account does not belong to your device for the refund. Please change and retry.";
       return "Invalid address";
