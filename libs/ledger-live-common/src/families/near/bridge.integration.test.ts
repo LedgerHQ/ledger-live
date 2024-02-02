d32d6ad0d12d25edbe495cc860e82ed8aeaeff0a
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-02-02 14:22:35 +0100

    fix: error during rebase
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/families/near/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/near/bridge.integration.test.ts
@@ -2,6 +2,4 @@ import "../../__tests__/test-helpers/setup";
 import { testBridge } from "../../__tests__/test-helpers/bridge";
 import { dataset } from "@ledgerhq/coin-near/bridge.integration.test";
 
-import "@ledgerhq/coin-polkadot/bridge.integration.test";
-
 testBridge(dataset);
--- libs/ledger-live-common/src/generated/account.ts
+++ libs/ledger-live-common/src/generated/account.ts
@@ -1,21 +1,13 @@
 import bitcoin from "../families/bitcoin/account";
 import cardano from "../families/cardano/account";
 import crypto_org from "../families/crypto_org/account";
-import elrond from "../families/elrond/account";
-import tron from "../families/tron/account";
 import vechain from "../families/vechain/account";
-import algorand from "@ledgerhq/coin-algorand/account";
 import near from "@ledgerhq/coin-near/account";
-import polkadot from "@ledgerhq/coin-polkadot/account";
 
 export default {
   bitcoin,
   cardano,
   crypto_org,
-  elrond,
-  tron,
   vechain,
-  algorand,
   near,
-  polkadot,
 };
