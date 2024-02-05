8be966a4d94bcbc253f89654125a1dcac1f56ac4
lambertkevin <kevin.lambert@ledger.fr>
2024-02-05 16:07:28 +0100

    Lint imports

--- libs/coin-evm/src/hw-getAddress.ts
+++ libs/coin-evm/src/hw-getAddress.ts
@@ -1,7 +1,7 @@
 import eip55 from "eip55";
-import { GetAddressFn } from "@ledgerhq/coin-framework/bridge/getAddressWrapper";
 import { SignerContext } from "@ledgerhq/coin-framework/signer";
 import { GetAddressOptions } from "@ledgerhq/coin-framework/derivation";
+import { GetAddressFn } from "@ledgerhq/coin-framework/bridge/getAddressWrapper";
 import { EvmAddress, EvmSignature, EvmSigner } from "./types/signer";
 
 const resolver = (
