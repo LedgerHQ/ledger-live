088b38d11bcee53cbd45c8bd74ea07736752f2a7
Beth Swingler <beth.swingler-ext@ledger.fr>
2024-02-06 14:31:48 +0000

    refactor: rm duplicate type, show tag only if has min required eth,

--- apps/ledger-live-mobile/src/families/evm/StakingDrawer/types.ts
+++ apps/ledger-live-mobile/src/families/evm/StakingDrawer/types.ts
@@ -1,13 +1,3 @@
-export type EthStakingProviders = {
-  listProvider: Array<{
-    id: string;
-    name: string;
-    liveAppId: string;
-    min?: number;
-    supportLink?: string;
-    icon?: string;
-    queryParams?: Record<string, string>;
-  }>;
-};
+import { EthStakingProviders } from "~/types/featureFlags";
 
 export type ListProvider = EthStakingProviders["listProvider"][number];
