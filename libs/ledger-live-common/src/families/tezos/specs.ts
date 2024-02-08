3233d41844c6803dcaf489a9f2530404581fabeb
lvndry <lvndry@proton.me>
2024-02-08 12:31:42 +0100

    force bot appVersion

--- libs/ledger-live-common/src/families/tezos/specs.ts
+++ libs/ledger-live-common/src/families/tezos/specs.ts
@@ -30,6 +30,8 @@ const tezos: AppSpec<Transaction> = {
   appQuery: {
     model: DeviceModelId.nanoS,
     appName: "TezosWallet",
+    // TODO: delete before merge
+    appVersion: "2.4.5",
   },
   genericDeviceAction: acceptTransaction,
   testTimeout: 2 * 60 * 1000,
