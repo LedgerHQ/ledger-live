d7a80f74d296b4ab98d907b81ba915f9a2046119
RamyEB <ramyelb@gmail.com>
2024-02-08 16:37:33 +0700

    feat: fix type

--- apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/types.ts
+++ apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/types.ts
@@ -1,13 +1,9 @@
 import { RecentlyUsedManifest } from "@ledgerhq/live-common/wallet-api/react";
 import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
 
-export interface PropsRawFullCard {
-  manifest: LiveAppManifest;
-  onClick: (manifest: LiveAppManifest) => void;
-}
-export interface PropsRawMinimumCard {
-  manifest: RecentlyUsedManifest;
-  onClick: (manifest: RecentlyUsedManifest) => void;
+export interface PropsCard<T extends RecentlyUsedManifest | LiveAppManifest> {
+  manifest: T;
+  onClick: (manifest: T) => void;
 }
 
 export interface PropsCta {
