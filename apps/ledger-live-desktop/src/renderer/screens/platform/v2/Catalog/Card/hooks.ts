a454fe3f8495bd00331f56c4d8dbed1b2db01d14
Kant <quentin.jaccarino@ledger.fr>
2024-02-08 16:22:42 +0100

    fix: type

--- apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/hooks.ts
+++ apps/ledger-live-desktop/src/renderer/screens/platform/v2/Catalog/Card/hooks.ts
@@ -1,17 +1,13 @@
 import { useMemo, useCallback } from "react";
 import { LiveAppManifest, LiveAppManifestParams } from "@ledgerhq/live-common/platform/types";
 import { LiveAppManifestParamsDapp } from "@ledgerhq/live-common/platform/types";
-import { RecentlyUsedManifest } from "@ledgerhq/live-common/wallet-api/react";
 import { PropsCard } from "./types";
 
 const hasDappUrl = (params: LiveAppManifestParams): params is LiveAppManifestParamsDapp => {
   return "dappUrl" in params;
 };
 
-export function useCard({
-  manifest,
-  onClick: onClickProp,
-}: PropsCard<RecentlyUsedManifest | LiveAppManifest>) {
+export function useCard({ manifest, onClick: onClickProp }: PropsCard<LiveAppManifest>) {
   let url = manifest.url;
   if (manifest.params && hasDappUrl(manifest.params)) {
     url = manifest.params.dappUrl;
@@ -27,6 +23,5 @@ export function useCard({
     hostname,
     disabled: manifest.branch === "soon",
     onClick,
-    ...("usedAt" in manifest ? { usedAt: manifest.usedAt } : {}),
   };
 }
