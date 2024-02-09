7eaf65c9c3f2ce7ef6dd9f08126ab6d5aeeac7e5
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-02-09 16:40:13 +0100

    fix type

--- libs/live-nft-react/src/hooks/useNftGalleryFilter.ts
+++ libs/live-nft-react/src/hooks/useNftGalleryFilter.ts
@@ -72,6 +72,6 @@ function hashProtoNFT(contract: string, tokenId: string, currencyId: string): st
   return `${contract}|${tokenId}|${currencyId}`;
 }
 
-export function isThresholdValid(threshold: string | number): boolean {
+export function isThresholdValid(threshold?: string | number): boolean {
   return Number(threshold) >= 0 && Number(threshold) <= 100;
 }
