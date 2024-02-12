2c1e81f1eaa2bcae91d831aa0a40a50b3e3152eb
Thomas Brillard <thomas.brillard@ledger.fr>
2024-02-12 18:10:41 +0100

    chore: new check

--- libs/ledger-live-common/src/exchange/swap/maybeTezosAccountUnrevealedAccount.ts
+++ libs/ledger-live-common/src/exchange/swap/maybeTezosAccountUnrevealedAccount.ts
@@ -3,21 +3,14 @@ import { SwapTransactionType } from "./types";
 export const maybeTezosAccountUnrevealedAccount = (
   swapTransaction: SwapTransactionType,
 ): Error | undefined => {
-  const operations = swapTransaction?.account?.operations;
-
   if (
     swapTransaction.swap?.rates.status === "success" &&
     swapTransaction?.transaction?.family === "tezos" &&
-    operations
+    // @ts-ignore we're in a tezos account check above
+    !swapTransaction?.account?.tezosResources.revealed
   ) {
-    // if there is more than 42 operations we can assume the account is revealed, no need to check thousands of operations
-    const hasRevealOperation =
-      operations.length < 42 ? operations.some(op => op.type === "REVEAL") : true;
-
-    if (!hasRevealOperation) {
-      const tezosError = new Error("Cannot swap with an unrevealed Tezos account");
-      tezosError.name = "TezosUnrevealedAccount";
-      return tezosError;
-    }
+    const tezosError = new Error("Cannot swap with an unrevealed Tezos account");
+    tezosError.name = "TezosUnrevealedAccount";
+    return tezosError;
   }
 };
