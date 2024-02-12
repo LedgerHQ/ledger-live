ba6b42e76a0af09f1a7a67efbb3c80621deb2e1a
Thomas Brillard <thomas.brillard@ledger.fr>
2024-02-12 16:58:46 +0100

    bugfix: better tezos reveal check

--- libs/ledger-live-common/src/exchange/swap/maybeTezosAccountUnrevealedAccount.ts
+++ libs/ledger-live-common/src/exchange/swap/maybeTezosAccountUnrevealedAccount.ts
@@ -3,14 +3,21 @@ import { SwapTransactionType } from "./types";
 export const maybeTezosAccountUnrevealedAccount = (
   swapTransaction: SwapTransactionType,
 ): Error | undefined => {
+  const operations = swapTransaction?.account?.operations;
+
   if (
-    swapTransaction.swap?.rates.status == "success" &&
-    swapTransaction?.transaction?.family == "tezos" &&
-    swapTransaction?.transaction?.estimatedFees &&
-    swapTransaction?.transaction?.fees !== swapTransaction?.transaction?.estimatedFees
+    swapTransaction.swap?.rates.status === "success" &&
+    swapTransaction?.transaction?.family === "tezos" &&
+    operations
   ) {
-    const tezosError = new Error("Cannot swap with an unrevealed Tezos account");
-    tezosError.name = "TezosUnrevealedAccount";
-    return tezosError;
+    // if there is more than 42 operations we can assume the account is revealed, no need to check thousands of operations
+    const hasRevealOperation =
+      operations.length < 42 ? operations.some(op => op.type === "REVEAL") : true;
+
+    if (!hasRevealOperation) {
+      const tezosError = new Error("Cannot swap with an unrevealed Tezos account");
+      tezosError.name = "TezosUnrevealedAccount";
+      return tezosError;
+    }
   }
 };
