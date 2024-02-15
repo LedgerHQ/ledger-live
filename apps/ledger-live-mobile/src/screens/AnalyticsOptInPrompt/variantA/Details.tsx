ac0c71de8bd75b768f5557480b681a193fcb3349
Come Grellard <come.grellard@ledger.fr>
2024-02-15 18:31:32 +0100

    feat(llm): update copy and style of the analytics opt in prompt

--- apps/ledger-live-mobile/src/screens/AnalyticsOptInPrompt/variantA/Details.tsx
+++ apps/ledger-live-mobile/src/screens/AnalyticsOptInPrompt/variantA/Details.tsx
@@ -7,6 +7,7 @@ import Button from "~/components/Button";
 import Switch from "~/components/Switch";
 import { View, Container, Titles, Content, Bottom } from "../Common";
 import useAnalyticsOptInPrompt from "~/hooks/useAnalyticsOptInPromptVariantA";
+import { ScrollView } from "react-native";
 
 const OptionContainer = styled(Flex).attrs({
   width: "100%",
@@ -53,53 +54,55 @@ function Details() {
   const { clickOnMoreOptionsConfirm, clickOnLearnMore } = useAnalyticsOptInPrompt();
 
   return (
-    <Container alignItems="center">
-      <View>
-        <Titles>
-          <Text variant="h3Inter" fontSize={24} fontWeight="semiBold" color="neutral.c100">
-            {t("analyticsOptIn.variantA.details.title")}
-          </Text>
-        </Titles>
-        <Content>
-          <Option
-            title={t("analyticsOptIn.variantA.details.analytics.title")}
-            description={t("analyticsOptIn.variantA.details.analytics.description")}
-            checked={isAnalyticsEnabled}
-            onToggle={setIsAnalyticsEnabled}
-          />
-          <Flex pt={7}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <Container alignItems="center">
+        <View>
+          <Titles>
+            <Text variant="h3Inter" fontSize={24} fontWeight="semiBold" color="neutral.c100">
+              {t("analyticsOptIn.variantA.details.title")}
+            </Text>
+          </Titles>
+          <Content>
             <Option
-              title={t("analyticsOptIn.variantA.details.personalizedExp.title")}
-              description={t("analyticsOptIn.variantA.details.personalizedExp.description")}
-              checked={isPersonalRecommendationsEnabled}
-              onToggle={setIsPersonalRecommendationsEnabled}
+              title={t("analyticsOptIn.variantA.details.analytics.title")}
+              description={t("analyticsOptIn.variantA.details.analytics.description")}
+              checked={isAnalyticsEnabled}
+              onToggle={setIsAnalyticsEnabled}
+            />
+            <Flex pt={7}>
+              <Option
+                title={t("analyticsOptIn.variantA.details.personalizedExp.title")}
+                description={t("analyticsOptIn.variantA.details.personalizedExp.description")}
+                checked={isPersonalRecommendationsEnabled}
+                onToggle={setIsPersonalRecommendationsEnabled}
+              />
+            </Flex>
+          </Content>
+        </View>
+        <Bottom>
+          <Flex flexDirection="row" py="20px">
+            <Button
+              title={t("analyticsOptIn.variantA.details.ctas.confirm")}
+              onPress={() =>
+                clickOnMoreOptionsConfirm(isAnalyticsEnabled, isPersonalRecommendationsEnabled)
+              }
+              type="main"
+              size="large"
+              outline={false}
+              ml="2"
+              flex={1}
             />
           </Flex>
-        </Content>
-      </View>
-      <Bottom>
-        <Flex flexDirection="row" py="20px">
-          <Button
-            title={t("analyticsOptIn.variantA.details.ctas.confirm")}
-            onPress={() =>
-              clickOnMoreOptionsConfirm(isAnalyticsEnabled, isPersonalRecommendationsEnabled)
-            }
-            type="main"
-            size="large"
-            outline={false}
-            ml="2"
-            flex={1}
-          />
-        </Flex>
-        <Text fontWeight="semiBold" pt={2} color="neutral.c70" textAlign="center" pb="2">
-          {t("analyticsOptIn.variantA.details.infoText.info")}
-        </Text>
-        <Link size="small" type="color" onPress={clickOnLearnMore}>
-          {t("analyticsOptIn.variantA.details.infoText.link")}
-        </Link>
-      </Bottom>
-      <TrackScreen category="Analytics Opt In Prompt" name="Details" variant="A" />
-    </Container>
+          <Text fontWeight="semiBold" pt={2} color="neutral.c70" textAlign="center" pb="2">
+            {t("analyticsOptIn.variantA.details.infoText.info")}
+          </Text>
+          <Link size="small" type="color" onPress={clickOnLearnMore}>
+            {t("analyticsOptIn.variantA.details.infoText.link")}
+          </Link>
+        </Bottom>
+        <TrackScreen category="Analytics Opt In Prompt" name="Details" variant="A" />
+      </Container>
+    </ScrollView>
   );
 }
 
--- apps/ledger-live-mobile/src/screens/AnalyticsOptInPrompt/variantB/Details.tsx
+++ apps/ledger-live-mobile/src/screens/AnalyticsOptInPrompt/variantB/Details.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 import Button from "~/components/Button";
 import { View, Container, Titles, Content, Bottom } from "../Common";
 import headerPersonalized from "./illustrations/header_personalized.png";
-import { Image } from "react-native";
+import { Image, ScrollView } from "react-native";
 import useAnalyticsOptInPrompt from "~/hooks/useAnalyticsOptInPromptVariantB";
 
 function Details() {
@@ -22,60 +22,62 @@ function Details() {
   ];
 
   return (
-    <Container alignItems="center">
-      <View>
-        <Image
-          source={headerPersonalized}
-          style={{ width: "100%", height: 180 }}
-          resizeMode="contain"
-        />
-        <Titles>
-          <Text variant="h3Inter" fontSize={24} fontWeight="semiBold" color="neutral.c100">
-            {t("analyticsOptIn.variantB.details.title")}
-          </Text>
-        </Titles>
-        <Content>
-          <Text color="neutral.c80" fontSize={14}>
-            {t("analyticsOptIn.variantB.details.description")}
-          </Text>
-          <Flex pl={2}>
-            {bulletPoints.map((item, index) => (
-              <View key={index}>
-                <Text pt={6} color="neutral.c80" fontSize={14}>{`\u2022 ${item}`}</Text>
-              </View>
-            ))}
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <Container alignItems="center">
+        <View>
+          <Image
+            source={headerPersonalized}
+            style={{ width: "100%", height: 180 }}
+            resizeMode="contain"
+          />
+          <Titles>
+            <Text variant="h3Inter" fontSize={24} fontWeight="semiBold" color="neutral.c100">
+              {t("analyticsOptIn.variantB.details.title")}
+            </Text>
+          </Titles>
+          <Content>
+            <Text color="neutral.c80" fontSize={14}>
+              {t("analyticsOptIn.variantB.details.description")}
+            </Text>
+            <Flex pl={2}>
+              {bulletPoints.map((item, index) => (
+                <View key={index}>
+                  <Text pt={6} color="neutral.c80" fontSize={14}>{`\u2022 ${item}`}</Text>
+                </View>
+              ))}
+            </Flex>
+          </Content>
+        </View>
+        <Bottom>
+          <Flex flexDirection="row" py="20px">
+            <Button
+              title={t("analyticsOptIn.variantB.details.ctas.refuse")}
+              onPress={clickOnRefusePersonalizedExperience}
+              type="shade"
+              size="large"
+              mr="2"
+              flex={1}
+            />
+            <Button
+              title={t("analyticsOptIn.variantB.details.ctas.allow")}
+              onPress={clickOnAllowPersonalizedExperience}
+              type="main"
+              size="large"
+              outline={false}
+              ml="2"
+              flex={1}
+            />
           </Flex>
-        </Content>
-      </View>
-      <Bottom>
-        <Flex flexDirection="row" py="20px">
-          <Button
-            title={t("analyticsOptIn.variantB.details.ctas.refuse")}
-            onPress={clickOnRefusePersonalizedExperience}
-            type="shade"
-            size="large"
-            mr="2"
-            flex={1}
-          />
-          <Button
-            title={t("analyticsOptIn.variantB.details.ctas.allow")}
-            onPress={clickOnAllowPersonalizedExperience}
-            type="main"
-            size="large"
-            outline={false}
-            ml="2"
-            flex={1}
-          />
-        </Flex>
-        <Text fontWeight="semiBold" pt={2} color="neutral.c70" textAlign="center" pb="2">
-          {t("analyticsOptIn.variantB.details.infoText.info")}
-        </Text>
-        <Link size="small" type="color" onPress={clickOnLearnMore}>
-          {t("analyticsOptIn.variantB.details.infoText.link")}
-        </Link>
-      </Bottom>
-      <TrackScreen category="Analytics Opt In Prompt" name="Details" variant="B" />
-    </Container>
+          <Text fontWeight="semiBold" pt={2} color="neutral.c70" textAlign="center" pb="2">
+            {t("analyticsOptIn.variantB.details.infoText.info")}
+          </Text>
+          <Link size="small" type="color" onPress={clickOnLearnMore}>
+            {t("analyticsOptIn.variantB.details.infoText.link")}
+          </Link>
+        </Bottom>
+        <TrackScreen category="Analytics Opt In Prompt" name="Details" variant="B" />
+      </Container>
+    </ScrollView>
   );
 }
 
