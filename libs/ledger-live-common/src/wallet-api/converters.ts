3b6b53800e29a47ff5792c17221fbfba31cd8500
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-02-16 15:30:19 +0100

    update to latest version of wallet-api-* packages remove ts-ignore of parentAccountId set uuidToAccount of parentAccountId update dummy-wallet-app versions update playwright test update changeset

--- libs/ledger-live-common/src/wallet-api/converters.ts
+++ libs/ledger-live-common/src/wallet-api/converters.ts
@@ -31,11 +31,12 @@ export function accountToWalletAPIAccount(
       throw new Error("No 'parentAccount' account provided for token account");
     }
 
+    const parentWalletApiId = uuidv5(parentAccount.id, NAMESPACE);
+    uuidToAccountId.set(parentWalletApiId, parentAccount.id);
+
     return {
       id: walletApiId,
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore Remove once wallet-api
-      parentAccountId: uuidv5(parentAccount.id, NAMESPACE),
+      parentAccountId: parentWalletApiId,
       balance: account.balance,
       address: parentAccount.freshAddress,
       blockHeight: parentAccount.blockHeight,
