9f9ba1e5b36c0b2be63294586e3c8353b6b670df
lvndry <lvndry@proton.me>
2024-02-22 15:18:29 +0100

    all families config support

--- libs/ledger-live-common/src/config/sharedConfig.ts
+++ libs/ledger-live-common/src/config/sharedConfig.ts
@@ -9,19 +9,41 @@ import { celoConfig } from "../families/celo/config";
 import { cryptoOrgConfig } from "../families/crypto_org/config";
 import { elrondConfig } from "../families/elrond/config";
 import { fileCoinConfig } from "../families/filecoin/config";
+import { hederaConfig } from "../families/hedera/config";
+import { internetComputerConfig } from "../families/internet_computer/config";
+import { polkadotConfig } from "../families/polkadot/config";
+import { rippleConfig } from "../families/ripple/config";
+import { nearConfig } from "../families/near/config";
+import { solanaConfig } from "../families/solana/config";
+import { stacksConfig } from "../families/stacks/config";
+import { stellarConfig } from "../families/stellar/config";
+import { tezosConfig } from "../families/tezos/config";
+import { tronConfig } from "../families/tron/config";
+import { vechainConfig } from "../families/vechain/config";
 
 const liveCommonConfig: ConfigSchema = {};
 
 export default {
   ...liveCommonConfig,
-  ...cosmosConfig,
-  ...bitcoinConfig,
-  ...evmConfig,
   ...algorandConfig,
+  ...bitcoinConfig,
+  ...cosmosConfig,
   ...cardanoConfig,
   ...casperConfig,
   ...celoConfig,
   ...cryptoOrgConfig,
   ...elrondConfig,
+  ...evmConfig,
   ...fileCoinConfig,
+  ...hederaConfig,
+  ...internetComputerConfig,
+  ...nearConfig,
+  ...polkadotConfig,
+  ...rippleConfig,
+  ...solanaConfig,
+  ...stacksConfig,
+  ...stellarConfig,
+  ...tezosConfig,
+  ...tronConfig,
+  ...vechainConfig,
 } as ConfigSchema;
