e7bff4e3e767c2d50c5f57938cd34378359776cd
Landry Monga <lvndry@protonmail.com>
2024-02-23 14:09:30 +0100

    [coin-config] Under maintenance status (#6212)
    * add status in cosmos default config
    
    * bitcoin + evm configs
    
    * algorand config
    
    * cardano config
    
    * casper config
    
    * celoConfig
    
    * crypto org config
    
    * elrond + filecoin
    
    * all families config support

---
+++ libs/ledger-live-common/src/families/bitcoin/config.ts
@@ -0,0 +1,254 @@
+import { ConfigInfo } from "@ledgerhq/live-config/LiveConfig";
+
+const bitcoinConfig: Record<string, ConfigInfo> = {
+  config_currency_bitcoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_bitcoin_cash: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_bitcoin_gold: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_bitcoin_private: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_clubcoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_dash: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_decred: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_digibyte: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_dogecoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_game_credits: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_gochain: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_hcash: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_komodo: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_lbry: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_litecoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_nix: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_peercoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_pivx: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_poswallet: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_qtum: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_ravencoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_resistance: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_stakenet: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_stratis: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_stealthcoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_vertcoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_viacoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_zcash: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_zclassic: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_zcoin: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+  config_currency_zencash: {
+    type: "object",
+    default: {
+      status: {
+        type: "active",
+      },
+    },
+  },
+};
+
+export { bitcoinConfig };
