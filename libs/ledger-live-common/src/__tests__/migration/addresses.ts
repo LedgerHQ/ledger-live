83aa5e2e4917d61177bcb5917e14b1ac0d8e913b
lvndry <lvndry@proton.me>
2024-02-28 11:56:50 +0100

    github action checkout on branches

--- libs/ledger-live-common/src/__tests__/migration/addresses.ts
+++ libs/ledger-live-common/src/__tests__/migration/addresses.ts
@@ -1,4 +1,6 @@
-export const migrationAddresses: { currencyId: string; address: string }[] = [
+import { CryptoCurrencyId } from "@ledgerhq/types-cryptoassets";
+
+export const migrationAddresses: { currencyId: CryptoCurrencyId; address: string }[] = [
   {
     currencyId: "ethereum",
     address: "0x0E3F0bb9516F01f2C34c25E0957518b8aC9414c5",
