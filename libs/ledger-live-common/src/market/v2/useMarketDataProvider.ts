91da5a7a431d37eb4de2c242004cf9f6847f3566
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-02-28 11:14:19 +0100

    WIP 2

--- libs/ledger-live-common/src/market/v2/useMarketDataProvider.ts
+++ libs/ledger-live-common/src/market/v2/useMarketDataProvider.ts
@@ -1,47 +1,56 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQueries, useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import {
   fetchCurrency,
   fetchCurrencyChartData,
   fetchCurrencyData,
+  setSupportedCoinsList,
   supportedCounterCurrencies,
 } from "../api/api";
 import { MarketCurrencyRequestParams } from "../types";
 import { QUERY_KEY } from "./queryKeys";
+import { useState } from "react";
 
 // 3min
 const STALE_TIME = 3 * 60000;
-// export function useMarketDataProvider() {
-//   // TO DO
-//   // api.setSupportedCoinsList()
 
-//   const { isPending, error, data, isFetching } = useQuery({
-//     queryKey: [QUERY_KEY.SupportedCounterCurrencies],
-//     queryFn: () => supportedCounterCurrencies(),
-//   });
+export function useMarketDataProvider() {
+  const [counterCurrency, setCounterCurrency] = useState<string>("usd");
+  const [range, setRange] = useState<string>("24h");
+  const { data: supportedCounterCurrencies } = useSupportedCounterCurrencies();
+  const { data: supportedCurrencies } = useSupportedCurrencies();
 
-//   // useEffect(() => {
-//   //   if (countervalue) {
-//   //     const ticker = countervalue.ticker.toLowerCase();
-//   //     api.supportedCounterCurrencies().then(
-//   //       supportedCounterCurrencies =>
-//   //       .then((coins: SupportedCoins) => {
-//   //           dispatch({
-//   //             type: ACTIONS.IS_READY,
-//   //             payload: {
-//   //               totalCoinsAvailable: coins.length,
-//   //               supportedCounterCurrencies,
-//   //             },
-//   //           });
-//   //           dispatch({
-//   //             type: ACTIONS.UPDATE_COUNTERVALUE,
-//   //             payload: supportedCounterCurrencies.includes(ticker) ? ticker : "usd",
-//   //           });
-//   //         }, handleError),
-//   //       handleError,
-//   //     );
-//   //   }
-//   // }, [api, countervalue, handleError]);
-// }
+  // useEffect(() => {
+  //   if (countervalue) {
+  //     const ticker = countervalue.ticker.toLowerCase();
+  //     api.supportedCounterCurrencies().then(
+  //       supportedCounterCurrencies =>
+  //       .then((coins: SupportedCoins) => {
+  //           dispatch({
+  //             type: ACTIONS.IS_READY,
+  //             payload: {
+  //               totalCoinsAvailable: coins.length,
+  //               supportedCounterCurrencies,
+  //             },
+  //           });
+  //           dispatch({
+  //             type: ACTIONS.UPDATE_COUNTERVALUE,
+  //             payload: supportedCounterCurrencies.includes(ticker) ? ticker : "usd",
+  //           });
+  //         }, handleError),
+  //       handleError,
+  //     );
+  //   }
+  // }, [api, countervalue, handleError]);
+
+  return {
+    counterCurrency,
+    setCounterCurrency,
+    range,
+    setRange,
+    supportedCounterCurrencies,
+    supportedCurrencies,
+  };
+}
 
 export function useCurrencyChartData({ id, counterCurrency, ranges }: MarketCurrencyRequestParams) {
   const results = useQueries({
@@ -64,7 +73,7 @@ export function useCurrencyData({ id, counterCurrency, ranges }: MarketCurrencyR
     })),
   });
 
-  const resultCurrency = useQuery({
+  const resultCurrency = useSuspenseQuery({
     queryKey: [QUERY_KEY.CurrencyDataRaw, id],
     queryFn: () => fetchCurrency({ id }),
     staleTime: STALE_TIME,
@@ -77,6 +86,22 @@ export function useSupportedCounterCurrencies() {
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: [QUERY_KEY.SupportedCounterCurrencies],
     queryFn: () => supportedCounterCurrencies(),
+    staleTime: Infinity,
+  });
+
+  return {
+    isPending,
+    error,
+    data,
+    isFetching,
+  };
+}
+
+export function useSupportedCurrencies() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: [QUERY_KEY.SupportedCounterCurrencies],
+    queryFn: () => setSupportedCoinsList(),
+    staleTime: Infinity,
   });
 
   return {
