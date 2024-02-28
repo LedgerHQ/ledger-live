1846aec14be961bad976ffdd65314eb64c0228e9
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-02-28 12:58:53 +0100

    fix indent + missing import

--- apps/ledger-live-desktop/src/renderer/components/TransactionConfirm/ConfirmTitle.tsx
+++ apps/ledger-live-desktop/src/renderer/components/TransactionConfirm/ConfirmTitle.tsx
@@ -3,6 +3,7 @@ import { TFunction } from "i18next";
 import { withTranslation } from "react-i18next";
 import { Account, AccountLike } from "@ledgerhq/types-live";
 import { Transaction, TransactionStatus } from "@ledgerhq/live-common/generated/types";
+import Text from "~/renderer/components/Text";
 
 type Props = {
   t: TFunction;
@@ -14,11 +15,7 @@ type Props = {
   typeTransaction: string | undefined;
 };
 
-const ConfirmTitle = ({
-  t,
-  title,
-  typeTransaction,
-}: Props) => {
+const ConfirmTitle = ({ t, title, typeTransaction }: Props) => {
   const description: string = useMemo(() => {
     if (typeTransaction === "Approve") return t("approve.description");
     else return t("sign.description");
@@ -26,7 +23,6 @@ const ConfirmTitle = ({
 
   if (title) {
     return title;
-  };
   }
   return (
     <Text ff={"Inter|Medium"} textAlign={"center"} fontSize={22} marginBottom={12}>
