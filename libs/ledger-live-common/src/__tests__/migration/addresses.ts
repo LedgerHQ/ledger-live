1e1ae4535ab710845bd8c41637bb574f154e5523
lvndry <lvndry@proton.me>
2024-02-29 11:46:51 +0100

    support more currencies

--- libs/ledger-live-common/src/__tests__/migration/addresses.ts
+++ libs/ledger-live-common/src/__tests__/migration/addresses.ts
@@ -5,4 +5,153 @@ export const migrationAddresses: { currencyId: CryptoCurrencyId; address: string
     currencyId: "ethereum",
     address: "0x0E3F0bb9516F01f2C34c25E0957518b8aC9414c5",
   },
+  {
+    currencyId: "bitcoin",
+    address: "17gPmBH8b6UkvSmxMfVjuLNAqzgAroiPSe",
+  },
+  {
+    currencyId: "bitcoin",
+    address: "bc1q8vp7v5wyv8nvhsh5p2dvkgalep4q325kd5xk4e",
+  },
+  {
+    currencyId: "digibyte",
+    address: "dgb1qs45s7zq4gn6uygzrthyh067crzphvhwu33a8sl",
+  },
+  {
+    currencyId: "litecoin",
+    address: "ltc1qx2wxzwmpg4m8tr9d7rharerxaqj50jkdasvxmx",
+  },
+  {
+    currencyId: "algorand",
+    address: "ZC3HFULMJF53BF5ER4E2TTGPBRGH2Y4RVS32JZ6NH4RW7LN67HCE6UBS3Q",
+  },
+  {
+    currencyId: "algorand",
+    address: "MECOWMKPKH2NWVZTS5V5RQDGFFYBT25KNLOPHG2KUMMNKU6FOHGJT24WBI",
+  },
+  {
+    currencyId: "cardano",
+    address:
+      "addr_test1qpj84xm4jlpcsvmrnldj86zz7g4mwzr7qg90wdjpn64uea44mcsd3mzvmr9qhfscxkyla3aqnzttwepay9mt3fz84smqyjvhh4",
+  },
+  {
+    currencyId: "casper",
+    address: "0202ba6dc98cbe677711a45bf028a03646f9e588996eb223fad2485e8bc391b01581",
+  },
+  {
+    currencyId: "axelar",
+    address: "axelar1gyauvl44q2apn3u3aujm36q8zrj74vry3kglyg",
+  },
+  {
+    currencyId: "coreum",
+    address: "core1sel9ys8a7jlsmrxp9692zfjncnmhxkws0qr3g9",
+  },
+  {
+    currencyId: "cosmos",
+    address: "cosmos1g84934jpu3v5de5yqukkkhxmcvsw3u2ajxvpdl",
+  },
+  {
+    currencyId: "desmos",
+    address: "desmos1gyauvl44q2apn3u3aujm36q8zrj74vrypqn8c3",
+  },
+  {
+    currencyId: "dydx",
+    address: "dydx1gyauvl44q2apn3u3aujm36q8zrj74vryupsn07",
+  },
+  {
+    currencyId: "injective",
+    address: "inj1hn46zvx43mxq47vsecvw84k5chjhuhwp6d62dt",
+  },
+  {
+    currencyId: "onomy",
+    address: "onomy1gyauvl44q2apn3u3aujm36q8zrj74vry0e2p7v",
+  },
+  {
+    currencyId: "osmosis",
+    address: "osmo10h50supk4en682vrjkc6wkgkpcyxyqn4vxjy2c",
+  },
+  {
+    currencyId: "persistence",
+    address: "persistence1gyauvl44q2apn3u3aujm36q8zrj74vrym5cypd",
+  },
+  {
+    currencyId: "quicksilver",
+    address: "quick1gyauvl44q2apn3u3aujm36q8zrj74vry7uw9km",
+  },
+  {
+    currencyId: "secret_network",
+    address: "secret1gyauvl44q2apn3u3aujm36q8zrj74vryha27j4",
+  },
+  {
+    currencyId: "sei_network",
+    address: "sei1gyauvl44q2apn3u3aujm36q8zrj74vryc50pfg",
+  },
+  {
+    currencyId: "stargaze",
+    address: "stars1gyauvl44q2apn3u3aujm36q8zrj74vrypyf2yc",
+  },
+  {
+    currencyId: "umee",
+    address: "umee1gyauvl44q2apn3u3aujm36q8zrj74vry8wrgtm",
+  },
+  {
+    currencyId: "polkadot",
+    address: "12YA86tRQhHgwU3SSj56aesUKB7GKvdnZTTTXRop4vd3YgDV",
+  },
+  {
+    currencyId: "polkadot",
+    address: "13jAJfhpFkRZj1TSSdFopaiFeKnof2q7g4GNdcxcg8Lvx6QN",
+  },
+  {
+    currencyId: "polkadot",
+    address: "15oodc5d8DWJodZhTD6qsxxSQRYWhdkWCrwqNHajDirXRrAD",
+  },
+  {
+    currencyId: "polkadot",
+    address: "12EsPA79dvhtjp1bYvCiEWPsQmmdKGss44GzE3CT9tTo9g4Q",
+  },
+  {
+    currencyId: "solana",
+    address: "AQbkEagmPgmsdAfS4X8V8UyJnXXjVPMvjeD15etqQ3Jh",
+  },
+  {
+    currencyId: "solana",
+    address: "7b6Q3ap8qRzfyvDw1Qce3fUV8C7WgFNzJQwYNTJm3KQo",
+  },
+  {
+    currencyId: "stellar",
+    address: "GAT4LBXYJGJJJRSNK74NPFLO55CDDXSYVMQODSEAAH3M6EY4S7LPH5GV",
+  },
+  {
+    currencyId: "stellar",
+    address: "GCDDN6T2LJN3T7SPWJQV6BCCL5KNY5GBN7X4CMSZLDEXDHXAH32TOAHS",
+  },
+  {
+    currencyId: "tezos",
+    address: "tz1boBHAVpwcvKkNFAQHYr7mjxAz1PpVgKq7",
+  },
+  {
+    currencyId: "tezos",
+    address: "tz1VSichevvJSNkSSntgwKDKikWNB6iqNJii",
+  },
+  {
+    currencyId: "tron",
+    address: "THAe4BNVxp293qgyQEqXEkHMpPcqtG73bi",
+  },
+  {
+    currencyId: "tron",
+    address: "TRqkRnAj6ceJFYAn2p1eE7aWrgBBwtdhS9",
+  },
+  {
+    currencyId: "vechain",
+    address: "0x0fe6688548f0C303932bB197B0A96034f1d74dba",
+  },
+  {
+    currencyId: "vechain",
+    address: "0xC0dFC490f8fba6A573C55a68dd9023f999ccaDA0",
+  },
+  {
+    currencyId: "ripple",
+    address: "rageXHB6Q4VbvvWdTzKANwjeCT4HXFCKX7",
+  },
 ];
