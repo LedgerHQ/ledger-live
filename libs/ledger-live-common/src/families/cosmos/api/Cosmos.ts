36a2dd3dd65af4789fa357b8148641ca59e30397
lvndry <lvndry@proton.me>
2024-03-01 11:27:14 +0100

    can checkout on base_ref

--- libs/ledger-live-common/src/families/cosmos/api/Cosmos.ts
+++ libs/ledger-live-common/src/families/cosmos/api/Cosmos.ts
@@ -27,7 +27,6 @@ export class CosmosAPI {
     const crypto = cryptoFactory(currencyId);
     this.chainInstance = crypto;
     this.defaultEndpoint = crypto.lcd;
-    console.log({ lcd: crypto.lcd });
     this.version = crypto.version;
   }
 
