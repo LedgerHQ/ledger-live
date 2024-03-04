96759fd552cba38362b720c6cc904a96cf17edb2
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-03-04 19:00:39 +0100

    feat: use msw instead of mocking network lib calls
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-polkadot/src/logic/getFeesForTransaction.test.ts
+++ libs/coin-polkadot/src/logic/getFeesForTransaction.test.ts
@@ -1,35 +1,34 @@
 import BigNumber from "bignumber.js";
+import { HttpResponse, http } from "msw";
 import { loadPolkadotCrypto } from "./polkadot-crypto";
 import getEstimatedFees from "./getFeesForTransaction";
-import { fixtureChainSpec, fixtureTxMaterialWithMetadata } from "../network/sidecar.fixture";
+import mockServer from "../network/sidecar.mock";
 import { createFixtureAccount, createFixtureTransaction } from "../types/model.fixture";
 
 jest.mock("./polkadot-crypto");
-const mockPaymentInfo = jest.fn();
-jest.mock("../network/sidecar", () => ({
-  ...jest.requireActual("../network/sidecar"),
-  fetchChainSpec: () => jest.fn().mockResolvedValue(fixtureChainSpec),
-  getTransactionMaterialWithMetadata: () =>
-    jest.fn().mockResolvedValue(fixtureTxMaterialWithMetadata),
-  paymentInfo: (args: any) => mockPaymentInfo(args),
-}));
-mockPaymentInfo.mockResolvedValue({
-  weight: "WHATEVER",
-  class: "WHATEVER",
-  partialFee: "155099814",
-});
 
 describe("getEstimatedFees", () => {
+  const transaction = createFixtureTransaction();
+
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
   beforeEach(() => {
-    mockPaymentInfo.mockClear();
+    mockServer.resetHandlers();
+  });
+
+  afterAll(() => {
+    mockServer.close();
   });
 
   it("calls loadPolkadotCrypto (WASM check)", async () => {
     // Given
+    const account = createFixtureAccount();
     const mockLoadPolkadotCrypto = jest.mocked(loadPolkadotCrypto);
 
     // When
-    await getEstimatedFees({ a: createFixtureAccount(), t: createFixtureTransaction() });
+    await getEstimatedFees({ a: account, t: transaction });
 
     // Then
     // Test to comply with existing code. Should be 1 time only.
@@ -37,16 +36,26 @@ describe("getEstimatedFees", () => {
   });
 
   it("returns estimation from Polkadot explorer", async () => {
+    // Given
+    const account = createFixtureAccount();
+    const partialFee = "155099814";
+    mockServer.use(
+      http.post("https://polkadot-sidecar.coin.ledger.com/transaction/fee-estimate", () => {
+        return HttpResponse.json({
+          weight: "WHATEVER",
+          class: "WHATEVER",
+          partialFee,
+        });
+      }),
+    );
+
     // When
     const result = await getEstimatedFees({
-      a: createFixtureAccount(),
-      t: createFixtureTransaction(),
+      a: account,
+      t: transaction,
     });
 
     // Then
-    expect(mockPaymentInfo).toHaveBeenCalledTimes(1);
-    // Receive hex signature computed by Polkadot lib
-    expect(mockPaymentInfo.mock.lastCall).not.toBeNull();
-    expect(result).toEqual(new BigNumber(155099814));
+    expect(result).toEqual(new BigNumber(partialFee));
   });
 });
--- libs/coin-polkadot/src/network/sidecar.unit.test.ts
+++ libs/coin-polkadot/src/network/sidecar.unit.test.ts
@@ -1,83 +1,86 @@
 import BigNumber from "bignumber.js";
-import network from "@ledgerhq/live-network/network";
+import { HttpResponse, http } from "msw";
 import { getAccount, getRegistry } from "./sidecar";
-import { fixtureChainSpec, fixtureTxMaterialWithMetadata } from "./sidecar.fixture";
-
-jest.mock("@ledgerhq/live-network/network");
-const networkApiMock = jest.mocked(network);
+import mockServer from "./sidecar.mock";
 
 describe("getAccount", () => {
+  let balanceResponseStub = {};
+
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
+  beforeEach(() => {
+    mockServer.resetHandlers();
+    mockServer.use(
+      http.get("https://polkadot-sidecar.coin.ledger.com/accounts/:addr/balance-info", () => {
+        return HttpResponse.json(balanceResponseStub);
+      }),
+    );
+  });
+
+  afterAll(() => {
+    mockServer.close();
+  });
+
   it("should estimate lockedBalance correctly with 1 locked balance type", async () => {
-    networkApiMock.mockResolvedValue({
-      data: {
-        at: {
-          height: "0",
-        },
-        locks: [
-          {
-            amount: "60000000000",
-            reasons: "All",
-          },
-        ],
-        targets: [],
+    balanceResponseStub = {
+      at: {
+        height: "0",
       },
-      status: 200,
-      statusText: "",
-      headers: {},
-      config: {},
-    });
+      locks: [
+        {
+          amount: "60000000000",
+          reasons: "All",
+        },
+      ],
+      targets: [],
+    };
+
     const { lockedBalance } = await getAccount("addr");
     expect(lockedBalance).toEqual(new BigNumber("60000000000"));
   });
 
   it("should estimate lockedBalance when one locked balance is higher than others", async () => {
-    networkApiMock.mockResolvedValue({
-      data: {
-        at: {
-          height: "0",
-        },
-        locks: [
-          {
-            amount: "1",
-            reasons: "reason 1",
-          },
-          {
-            amount: "5",
-            reasons: "reason 2",
-          },
-          {
-            amount: "3",
-            reasons: "reason 3",
-          },
-        ],
-        targets: [],
+    balanceResponseStub = {
+      at: {
+        height: "0",
       },
-      status: 200,
-      statusText: "",
-      headers: {},
-      config: {},
-    });
+      locks: [
+        {
+          amount: "1",
+          reasons: "reason 1",
+        },
+        {
+          amount: "5",
+          reasons: "reason 2",
+        },
+        {
+          amount: "3",
+          reasons: "reason 3",
+        },
+      ],
+      targets: [],
+    };
     const { lockedBalance } = await getAccount("addr");
     expect(lockedBalance).toEqual(new BigNumber("5"));
   });
 });
 
 describe("getRegistry", () => {
+  beforeAll(() => {
+    mockServer.listen();
+  });
+
+  beforeEach(() => {
+    mockServer.resetHandlers();
+  });
+
+  afterAll(() => {
+    mockServer.close();
+  });
+
   it("works", async () => {
-    networkApiMock.mockResolvedValueOnce({
-      data: fixtureTxMaterialWithMetadata,
-      status: 200,
-      statusText: "",
-      headers: {},
-      config: {},
-    });
-    networkApiMock.mockResolvedValueOnce({
-      data: fixtureChainSpec,
-      status: 200,
-      statusText: "",
-      headers: {},
-      config: {},
-    });
     const { registry, extrinsics } = await getRegistry();
     expect(registry).not.toBeNull();
     expect(extrinsics).not.toBeNull();
