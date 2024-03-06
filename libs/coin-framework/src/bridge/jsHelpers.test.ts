d68e96eeee44123177e55992cc39fdd4f1c339d8
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-03-06 11:53:48 +0100

    chore: add more tests
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-framework/src/bridge/jsHelpers.test.ts
+++ libs/coin-framework/src/bridge/jsHelpers.test.ts
@@ -1,6 +1,8 @@
 import BigNumber from "bignumber.js";
-import { defaultUpdateTransaction } from "./jsHelpers";
-import type { TransactionCommon } from "@ledgerhq/types-live";
+import { AccountShapeInfo, defaultUpdateTransaction, makeSync } from "./jsHelpers";
+import type { Account, SyncConfig, TransactionCommon } from "@ledgerhq/types-live";
+import { TestScheduler } from "rxjs/testing";
+import { listCryptoCurrencies } from "../currencies";
 
 describe("jsHelpers", () => {
   describe("defaultUpdateTransaction", () => {
@@ -37,4 +39,89 @@ describe("jsHelpers", () => {
       });
     });
   });
+
+  describe("makeSync", () => {
+    it("returns a function to update account that give a new instance of account", done => {
+      // Given
+      const account = createAccount("12");
+
+      // When
+      const accountUpdater = makeSync({
+        getAccountShape: (_accountShape: AccountShapeInfo) => Promise.resolve({} as Account),
+      })(account, {} as SyncConfig);
+
+      accountUpdater.subscribe(updater => {
+        // Then
+        expect(updater(account)).not.toBe(account);
+        done();
+      });
+    });
+
+    it("returns a account with a corrected formatted id", done => {
+      // Given
+      const account = createAccount("12");
+
+      // When
+      const accountUpdater = makeSync({
+        getAccountShape: (_accountShape: AccountShapeInfo) => Promise.resolve({} as Account),
+      })(account, {} as SyncConfig);
+
+      const expectedAccount = {
+        ...account,
+        id: "js:2:bitcoin::",
+        subAccounts: undefined,
+      };
+      accountUpdater.subscribe(updater => {
+        // Then
+        expect(updater(account).id).toEqual(expectedAccount.id);
+        done();
+      });
+    });
+  });
 });
+
+const emptyHistoryCache = {
+  HOUR: {
+    latestDate: null,
+    balances: [],
+  },
+  DAY: {
+    latestDate: null,
+    balances: [],
+  },
+  WEEK: {
+    latestDate: null,
+    balances: [],
+  },
+};
+
+function createAccount(id: string): Account {
+  const currency = listCryptoCurrencies(true).find(c => c.id === "bitcoin")!;
+
+  return {
+    type: "Account",
+    id,
+    seedIdentifier: "",
+    derivationMode: "",
+    index: 0,
+    freshAddress: "",
+    freshAddressPath: "",
+    freshAddresses: [],
+    name: "",
+    starred: false,
+    used: true,
+    balance: new BigNumber(0),
+    spendableBalance: new BigNumber(0),
+    creationDate: new Date(),
+    blockHeight: 0,
+    currency,
+    unit: currency.units[0],
+    operationsCount: 0,
+    operations: [],
+    pendingOperations: [],
+    lastSyncDate: new Date(),
+    // subAccounts: [],
+    balanceHistoryCache: emptyHistoryCache,
+    swapHistory: [],
+  };
+}
