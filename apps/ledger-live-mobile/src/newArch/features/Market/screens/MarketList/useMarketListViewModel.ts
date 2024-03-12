092b766f57355dbc60383a8acfb646869d099af7
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-03-12 13:32:34 +0100

    WIP

--- apps/ledger-live-mobile/src/newArch/features/Market/screens/MarketList/useMarketListViewModel.ts
+++ apps/ledger-live-mobile/src/newArch/features/Market/screens/MarketList/useMarketListViewModel.ts
@@ -1,5 +1,5 @@
 import { useCallback, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BaseComposite, StackNavigatorProps } from "~/components/RootNavigator/types/helpers";
 import { MarketNavigatorStackParamList } from "LLM/features/Market/Navigator";
 import { ScreenName } from "~/const";
@@ -7,8 +7,16 @@ import { useRoute } from "@react-navigation/native";
 import { useMarketData } from "@ledgerhq/live-common/market/MarketDataProvider";
 import {
   marketFilterByStarredAccountsSelector,
+  marketRequestParamsSelector,
   starredMarketCoinsSelector,
 } from "~/reducers/market";
+
+import {
+  useMarketDataProvider,
+  useMarketData as useMarketDataHook,
+} from "@ledgerhq/live-common/market/v2/useMarketDataProvider";
+import { setMarketRequestParams } from "~/actions/market";
+import { MarketListRequestParams } from "@ledgerhq/live-common/market/types";
 
 type NavigationProps = BaseComposite<
   StackNavigatorProps<MarketNavigatorStackParamList, ScreenName.MarketList>
@@ -17,26 +25,33 @@ type NavigationProps = BaseComposite<
 function useMarketListViewModel() {
   const { params } = useRoute<NavigationProps["route"]>();
   const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
   const initialTop100 = params?.top100;
   const starredMarketCoins: string[] = useSelector(starredMarketCoinsSelector);
   const filterByStarredAccount: boolean = useSelector(marketFilterByStarredAccountsSelector);
+  const marketParams = useSelector(marketRequestParamsSelector);
+  const { counterCurrency, loading, selectCurrency } = useMarketData();
 
-  const {
-    requestParams,
-    refresh,
-    counterCurrency,
-    marketData,
-    loadNextPage,
-    loading,
-    page,
-    selectCurrency,
-  } = useMarketData();
+  const refresh = useCallback(
+    (payload?: MarketListRequestParams) => {
+      dispatch(setMarketRequestParams(payload ?? {}));
+    },
+    [dispatch],
+  );
 
-  const { limit, search, range, top100 } = requestParams;
+  const { supportedCurrencies, liveCoinsList } = useMarketDataProvider();
+
+  const { limit, search, range, top100, page } = marketParams;
+
+  const marketResult = useMarketDataHook({
+    ...marketParams,
+    liveCoinsList,
+    supportedCoinsList: supportedCurrencies,
+  });
 
   const marketDataFiltered = filterByStarredAccount
-    ? marketData?.filter(d => starredMarketCoins.includes(d.id)) ?? undefined
-    : marketData;
+    ? marketResult.data?.filter(d => starredMarketCoins.includes(d.id)) ?? undefined
+    : marketResult.data;
 
   useEffect(() => {
     if (initialTop100) {
@@ -60,26 +75,21 @@ function useMarketListViewModel() {
     }
   }, [refresh, starredMarketCoins, filterByStarredAccount]);
 
+  const onLoadNextPage = useCallback(() => {
+    dispatch(setMarketRequestParams({ page: (marketParams?.page || 1) + 1 }));
+  }, [dispatch, marketParams?.page]);
+
   const onEndReached = useCallback(() => {
     if (
-      !limit ||
-      isNaN(limit) ||
-      !marketData ||
-      page * limit > marketData.length ||
-      loading ||
-      top100
+      (!limit || isNaN(limit) || !marketResult.data || page) ??
+      (0 * limit > marketResult.data.length || loading || top100)
     ) {
       setIsLoading(false);
       return Promise.resolve();
     }
     setIsLoading(true);
-    const next = loadNextPage();
-    return next
-      ?.then(() => {
-        // do nothing
-      })
-      .finally(() => setIsLoading(false));
-  }, [limit, marketData, page, loading, top100, loadNextPage]);
+    onLoadNextPage();
+  }, [limit, marketResult.data, page, loading, top100, onLoadNextPage]);
 
   return {
     marketData: marketDataFiltered,
