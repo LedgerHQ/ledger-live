f34d2b5e85cc7937fb6f6bfb65e6e8a22e824c02
Chuck Ng <cheuk-man.ng@ledger.fr>
2024-03-19 09:56:38 +0000

    fix(LIVE-11791): ci

--- libs/ledger-live-common/src/exchange/swap/const/timeout.ts
+++ libs/ledger-live-common/src/exchange/swap/const/timeout.ts
@@ -4,3 +4,8 @@ export const DEFAULT_SWAP_TIMEOUT_MS = 10000;
  * Default to 20 seconds for quotes refresh interval in millisecond
  */
 export const DEFAULT_SWAP_RATES_INTERVAL_MS = 20 * 1000;
+
+/**
+ * Default to 60 seconds for quotes refresh interval in millisecond
+ */
+export const DEFAULT_LLM_SWAP_RATES_INTERVAL_MS = 60 * 1000;
