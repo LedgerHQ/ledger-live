2b8a75faae20a1190a3513e708a9b70a9e88247f
Lucas Werey <lucas.werey@ledger.fr>
2024-03-25 13:05:24 +0100

    feat(llm): optimize the ui of stacked braze cards

--- apps/ledger-live-mobile/src/contentCards/layouts/grid/index.tsx
+++ apps/ledger-live-mobile/src/contentCards/layouts/grid/index.tsx
@@ -3,7 +3,8 @@ import { useWindowDimensions } from "react-native";
 import { ContentLayoutBuilder } from "~/contentCards/layouts/utils";
 import { Flex } from "@ledgerhq/native-ui";
 import { WidthFactor } from "~/contentCards/layouts/types";
-import { useTheme } from "styled-components/native";
+import { useItemStyle as getItemStyle } from "./utils";
+import { ContentCardsType } from "~/dynamicContent/types";
 
 type Props = {
   styles?: {
@@ -13,77 +14,41 @@ type Props = {
 };
 
 const defaultStyles = {
-  gap: 12,
+  gap: 16,
   widthFactor: WidthFactor.Half,
 };
 
-const stylesMap = {
-  firstStackCard: {
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-  },
-  lastStackCard: {
-    borderBottomLeftRadius: 12,
-    borderBottomRightRadius: 12,
-  },
-  middleCards: {
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-  },
-};
-
 const Grid = ContentLayoutBuilder<Props>(({ items, styles: _styles = defaultStyles }) => {
+  const { width: windowWidth } = useWindowDimensions();
   const styles = {
     gap: _styles.gap ?? defaultStyles.gap,
     widthFactor: _styles.widthFactor ?? defaultStyles.widthFactor,
   };
-  const { colors } = useTheme();
-
-  const width = useWindowDimensions().width * styles.widthFactor - 20;
-  const marginStack = (useWindowDimensions().width - width) / 2;
-  const marginSquare = (useWindowDimensions().width - 2 * width - styles.gap) / 2;
-  const isStack = styles.widthFactor === WidthFactor.Full;
-
-  const getItemStyle = (isNotSingleCard: boolean, isFirstItem: boolean, isLastItem: boolean) => {
-    if (isNotSingleCard) {
-      if (isLastItem) {
-        return stylesMap.lastStackCard;
-      }
-      if (isFirstItem) {
-        return {
-          ...stylesMap.firstStackCard,
-          ...(items.length === 2
-            ? { borderBottomWidth: 1, borderBottomColor: colors.opacityDefault.c10 }
-            : {}),
-        };
-      }
-      return {
-        ...stylesMap.middleCards,
-        borderTopColor: colors.opacityDefault.c10,
-        borderBottomColor: colors.opacityDefault.c10,
-      };
-    }
-    return { borderRadius: 12 };
-  };
+  const cardWidth =
+    styles.widthFactor === WidthFactor.Full
+      ? windowWidth * styles.widthFactor - styles.gap * 2
+      : windowWidth * styles.widthFactor - styles.gap * 1.5;
+  const isStack =
+    styles.widthFactor === WidthFactor.Full && items[0].props.type === ContentCardsType.action;
 
   return (
     <Flex
       style={{
-        marginLeft: isStack ? marginStack : marginSquare,
+        marginHorizontal: styles.gap,
         justifyContent: "flex-start",
         width: "100%",
         flexDirection: "row",
         flexWrap: "wrap",
-        gap: isStack ? 0 : 16,
+        gap: isStack ? 0 : styles.gap,
       }}
     >
       {items.map((item, index) => {
-        const isLastItem = index === items.length - 1;
-        const isFirstItem = index === 0;
-        const itemStyle = getItemStyle(items.length > 1, isFirstItem, isLastItem);
         return (
-          <Flex key={item.props.metadata.id} style={{ width }}>
-            <item.component {...item.props} itemStyle={itemStyle} />
+          <Flex key={item.props.metadata.id} style={{ width: cardWidth }}>
+            <item.component
+              {...item.props}
+              itemStyle={getItemStyle(items.length === 1, index === 0, index === items.length - 1)}
+            />
           </Flex>
         );
       })}
