6101897acaf15dffe6d7810131da58d6b1c8b2d4
Lucas Werey <lucas.werey@ledger.fr>
2024-03-25 11:27:22 +0100

    feat(llm): poc to see if we can get the correct ui for braze stack card

--- apps/ledger-live-mobile/src/contentCards/cards/horizontal/index.tsx
+++ apps/ledger-live-mobile/src/contentCards/cards/horizontal/index.tsx
@@ -10,48 +10,52 @@ type Props = {
   description: string;
   image: string;
   tag?: string;
+  medata: Array<string>;
+  itemStyle?: Array<string>;
 };
 
-const HorizontalCard = ContentCardBuilder<Props>(({ title, description, image, tag, metadata }) => {
-  const { colors, space } = useTheme();
+const HorizontalCard = ContentCardBuilder<Props>(
+  ({ title, description, image, tag, metadata, itemStyle }) => {
+    const { colors, space } = useTheme();
 
-  const isDismissable = !!metadata.actions?.onDismiss;
-  const isTag = !!tag;
+    const isDismissable = !!metadata.actions?.onDismiss;
+    const isTag = !!tag;
 
-  useEffect(() => metadata.actions?.onView?.());
+    useEffect(() => metadata.actions?.onView?.());
 
-  return (
-    <TouchableOpacity onPress={metadata.actions?.onClick} key={metadata.id}>
-      <Flex
-        bg={colors.opacityDefault.c05}
-        p="13px"
-        borderRadius="12px"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        columnGap={13}
-      >
-        {image ? <Image uri={image} /> : null}
+    return (
+      <TouchableOpacity onPress={metadata.actions?.onClick} key={metadata.id}>
+        <Flex
+          bg={colors.opacityDefault.c05}
+          p="13px"
+          {...(itemStyle ?? { borderRadius: 12 })}
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          columnGap={13}
+        >
+          {image ? <Image uri={image} /> : null}
 
-        <Flex flex={1} rowGap={space[2]}>
-          <Flex flexDirection="row" justifyContent="space-between" columnGap={space[3]}>
-            <Flex overflow={"hidden"} flex={1}>
-              <Title label={title} />
+          <Flex flex={1} rowGap={space[2]}>
+            <Flex flexDirection="row" justifyContent="space-between" columnGap={space[3]}>
+              <Flex overflow={"hidden"} flex={1}>
+                <Title label={title} />
+              </Flex>
+
+              <Flex alignSelf="center" height="16px">
+                {isDismissable ? (
+                  <Close onPress={metadata.actions?.onDismiss} />
+                ) : (
+                  isTag && <Tag label={tag} />
+                )}
+              </Flex>
             </Flex>
-
-            <Flex alignSelf="center" height="16px">
-              {isDismissable ? (
-                <Close onPress={metadata.actions?.onDismiss} />
-              ) : (
-                isTag && <Tag label={tag} />
-              )}
-            </Flex>
+            {description ? <Subtitle label={description} /> : null}
           </Flex>
-          {description ? <Subtitle label={description} /> : null}
         </Flex>
-      </Flex>
-    </TouchableOpacity>
-  );
-});
+      </TouchableOpacity>
+    );
+  },
+);
 
 export default HorizontalCard;
--- apps/ledger-live-mobile/src/contentCards/layouts/grid/index.tsx
+++ apps/ledger-live-mobile/src/contentCards/layouts/grid/index.tsx
@@ -3,6 +3,7 @@ import { useWindowDimensions } from "react-native";
 import { ContentLayoutBuilder } from "~/contentCards/layouts/utils";
 import { Flex } from "@ledgerhq/native-ui";
 import { WidthFactor } from "~/contentCards/layouts/types";
+import { useTheme } from "styled-components/native";
 
 type Props = {
   styles?: {
@@ -16,16 +17,54 @@ const defaultStyles = {
   widthFactor: WidthFactor.Half,
 };
 
+const stylesMap = {
+  firstStackCard: {
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+  },
+  lastStackCard: {
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+  middleCards: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+};
+
 const Grid = ContentLayoutBuilder<Props>(({ items, styles: _styles = defaultStyles }) => {
   const styles = {
     gap: _styles.gap ?? defaultStyles.gap,
     widthFactor: _styles.widthFactor ?? defaultStyles.widthFactor,
   };
+  const { colors } = useTheme();
 
   const width = useWindowDimensions().width * styles.widthFactor - 20;
   const marginStack = (useWindowDimensions().width - width) / 2;
   const marginSquare = (useWindowDimensions().width - 2 * width - styles.gap) / 2;
   const isStack = styles.widthFactor === WidthFactor.Full;
+
+  const getItemStyle = (isNotSingleCard: boolean, isFirstItem: boolean, isLastItem: boolean) => {
+    if (isNotSingleCard) {
+      if (isLastItem) {
+        return stylesMap.lastStackCard;
+      }
+      if (isFirstItem) {
+        return {
+          ...stylesMap.firstStackCard,
+          ...(items.length === 2
+            ? { borderBottomWidth: 1, borderBottomColor: colors.opacityDefault.c10 }
+            : {}),
+        };
+      }
+      return {
+        ...stylesMap.middleCards,
+        borderTopColor: colors.opacityDefault.c10,
+        borderBottomColor: colors.opacityDefault.c10,
+      };
+    }
+    return { borderRadius: 12 };
+  };
 
   return (
     <Flex
@@ -35,13 +74,16 @@ const Grid = ContentLayoutBuilder<Props>(({ items, styles: _styles = defaultStyl
         width: "100%",
         flexDirection: "row",
         flexWrap: "wrap",
-        gap: isStack ? styles.gap : 16,
+        gap: isStack ? 0 : 16,
       }}
     >
-      {items.map(item => {
+      {items.map((item, index) => {
+        const isLastItem = index === items.length - 1;
+        const isFirstItem = index === 0;
+        const itemStyle = getItemStyle(items.length > 1, isFirstItem, isLastItem);
         return (
           <Flex key={item.props.metadata.id} style={{ width }}>
-            <item.component {...item.props} />
+            <item.component {...item.props} itemStyle={itemStyle} />
           </Flex>
         );
       })}
