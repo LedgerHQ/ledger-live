2669db746ccb8da9b1851d96a0a7652eff0572f3
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2024-03-27 14:47:12 +0100

    test: fix flow for adding BT device with new onboarding

--- apps/ledger-live-mobile/e2e/models/onboarding/onboardingSteps.ts
+++ apps/ledger-live-mobile/e2e/models/onboarding/onboardingSteps.ts
@@ -8,12 +8,11 @@ import {
 } from "../../helpers";
 import * as bridge from "../../bridge/server";
 import DeviceAction from "../DeviceAction";
-import { ModelId, getUSBDevice } from "../devices";
+import { ModelId, getUSBDevice, knownDevice } from "../devices";
 import { expect } from "detox";
 
 export default class OnboardingSteps {
   getStartedButtonId = "onboarding-getStarted-button";
-  devicePairedContinueButtonId = "onboarding-paired-continue";
   exploreWithoutDeviceButtonId = "discoverLive-exploreWithoutADevice";
   readyToScanButtonID = "onboarding-scan-button";
   scanAndImportAccountsPageID = "onboarding-import-accounts-title";
@@ -27,8 +26,7 @@ export default class OnboardingSteps {
   connectLedgerButton = () => getElementById("Existing Wallet | Connect");
   syncWithLedgerLiveDesktop = () => getElementById("Existing Wallet | Sync");
   readyToScanButton = () => getElementById(this.readyToScanButtonID);
-  continueButton = () => getElementById(this.devicePairedContinueButtonId);
-  pairDeviceButton = () => getElementById("pair-device");
+  addDeviceButton = () => getElementById("connect-with-bluetooth");
   pairNanoButton = () => getElementById("Onboarding-PairNewNano");
   maybeLaterButton = () => getElementById("notifications-prompt-later");
 
@@ -66,8 +64,6 @@ export default class OnboardingSteps {
   quizzSuccessRegex = /onboarding-quizz-.*-success/;
   quizzCta = "onboarding-quizz-cta";
   quizzFinalCta = "onboarding-quizz-final-cta";
-
-  defaultName = "Nano X de David";
 
   async startOnboarding() {
     await waitForElementById(this.getStartedButtonId);
@@ -176,14 +172,18 @@ export default class OnboardingSteps {
     await tapByElement(this.pairNanoButton());
   }
 
-  async addDeviceViaBluetooth(name = this.defaultName) {
-    await tapByElement(getElementById("connect-with-bluetooth"));
+  async selectAddDevice() {
+    await tapByElement(this.addDeviceButton());
+  }
+
+  async addDeviceViaBluetooth(name = knownDevice.name) {
+    const deviceAction = new DeviceAction(knownDevice);
     await bridge.addDevicesBT(name);
-    await waitForElementByText(name); // Error here
+    await waitForElementByText(name);
     await tapByText(name);
-    await bridge.setInstalledApps(); // tell LLM what apps the mock device has
     await bridge.open(); // Mocked action open ledger manager on the Nano
-    await waitForElementById(this.devicePairedContinue);
+    await deviceAction.waitForSpinner();
+    await deviceAction.accessManager();
   }
 
   async addDeviceViaUSB(device: ModelId) {
@@ -192,11 +192,6 @@ export default class OnboardingSteps {
     await waitForElementByText(nano.deviceName);
     await tapByText(nano.deviceName);
     await new DeviceAction(nano).accessManager();
-  }
-
-  async openLedgerLive() {
-    await waitForElementById(this.devicePairedContinueButtonId);
-    await tapByElement(this.continueButton());
   }
 
   async declineNotifications() {
