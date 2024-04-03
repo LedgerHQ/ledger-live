d09bcae6c99441645d131419c06dca6c0fc0d7bd
RamyEB <ramyelb@gmail.com>
2024-04-03 14:52:40 +0200

    feat: add select and input array

--- apps/ledger-live-desktop/src/renderer/modals/CreateLocalManifest/FormLiveAppInput.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/CreateLocalManifest/FormLiveAppInput.tsx
@@ -11,7 +11,6 @@ type Props = {
   optional: boolean;
   parseCheck: boolean;
   path: string;
-  isArray?: boolean;
   autoFocus?: boolean;
   disabled?: boolean;
   handleChange: (path: string, value: unknown) => void;
@@ -25,15 +24,17 @@ function FormLiveAppInput({
   parseCheck,
   path,
   handleChange,
-  isArray = false,
   autoFocus = false,
   disabled = false,
 }: Props) {
   return (
     <Flex flexDirection={"column"}>
-      <Text marginBottom={1} marginLeft={1} ff="Inter|Medium" fontSize={4}>
+      <Text marginLeft={1} ff="Inter|Medium" fontSize={4}>
         {`${fieldName} (${type}) `}
         {!optional && <span style={{ color: "red" }}>*</span>}
+      </Text>
+      <Text marginBottom={1} color={"grey"} marginLeft={1} ff="Inter|Medium" fontSize={2}>
+        {`Here is my description`}
       </Text>
       {typeof value === "boolean" ? (
         <Flex width={"max-content"} marginLeft={1}>
@@ -55,8 +56,7 @@ function FormLiveAppInput({
               !isNaN(Number(value)) && handleChange(path, Number(value));
               return;
             }
-            typeof value !== "number" &&
-              handleChange(path, isArray ? (value ? value.split(",") : []) : value);
+            typeof value !== "number" && handleChange(path, value);
           }}
           value={String(value)}
         />
