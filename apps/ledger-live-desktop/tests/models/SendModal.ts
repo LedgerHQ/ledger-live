1151cac33aa7c954564f671d244e6f71cc5a152f
Victor Alber <victor.alber@ledger.fr>
2024-04-16 16:31:45 +0200

    remove userdata from commit

--- apps/ledger-live-desktop/tests/models/SendModal.ts
+++ apps/ledger-live-desktop/tests/models/SendModal.ts
@@ -27,4 +27,8 @@ export class SendModal extends Modal {
     await this.recipientInput.clear();
     await this.recipientInput.fill(recipient);
   }
+
+  async sendMax() {
+    await this.page.locator('[data-test-id="modal-max-checkbox"]').first().check();
+  }
 }
