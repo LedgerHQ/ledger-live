5cb70c598a6a70a193d7d68a1d59e7cd69f6f207
lvndry <lvndry@proton.me>
2024-04-16 12:20:05 +0200

    coin-evm coin-tester implementation

---
+++ libs/coin-modules/coin-evm/src/__tests__/coin-tester/scenario.test.ts
@@ -0,0 +1,42 @@
+import { killSpeculos } from "@ledgerhq/coin-tester/docker";
+import { executeScenario } from "@ledgerhq/coin-tester/main";
+import { killAnvil } from "./docker";
+import { scenarioPolygon } from "./scenarios/polygon";
+import { scenarioEthereum } from "./scenarios/ethereum";
+
+jest.setTimeout(600_000); // 10 Min
+global.console = require("console");
+
+afterAll(async () => {
+  await Promise.all([killSpeculos(), killAnvil()]);
+});
+
+describe("EVM Deterministic Tester", () => {
+  it("scenario Ethereum", async () => {
+    try {
+      await executeScenario(scenarioEthereum);
+    } catch (e) {
+      if (e != "done") {
+        await Promise.all([killSpeculos(), killAnvil()]);
+        throw e;
+      }
+    }
+  });
+
+  it("scenario polygon", async () => {
+    try {
+      await executeScenario(scenarioPolygon);
+    } catch (e) {
+      if (e != "done") {
+        await Promise.all([killSpeculos(), killAnvil()]);
+        throw e;
+      }
+    }
+  });
+});
+
+["exit", "SIGINT", "SIGQUIT", "SIGTERM", "SIGUSR1", "SIGUSR2", "uncaughtException"].map(e =>
+  process.on(e, async () => {
+    await Promise.all([killSpeculos(), killAnvil()]);
+  }),
+);
