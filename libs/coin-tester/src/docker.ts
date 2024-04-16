2d633b2e76aa0ddf1ab01cbea95613f00989223d
lvndry <lvndry@proton.me>
2024-04-16 12:17:25 +0200

    coin-tester engine

---
+++ libs/coin-tester/src/docker.ts
@@ -0,0 +1,96 @@
+import path from "path";
+import axios from "axios";
+import fs from "fs/promises";
+import { v2 as compose } from "docker-compose";
+import SpeculosTransportHttp from "@ledgerhq/hw-transport-node-speculos-http";
+import { ENV } from "./types";
+import chalk from "chalk";
+import { SignOperationEvent } from "@ledgerhq/types-live";
+
+const { API_PORT } = process.env as ENV;
+const cwd = path.join(__dirname);
+
+const delay = (timing: number) => new Promise(resolve => setTimeout(resolve, timing));
+
+export const spawnSigner = async (
+  service: "speculos",
+  nanoAppEndpoint: `/${string}`,
+): Promise<{
+  transport: SpeculosTransportHttp;
+  onSignerConfirmation: (e?: SignOperationEvent) => Promise<void>;
+}> => {
+  console.log(`Starting ${service}...`);
+
+  const { data: blob } = await axios({
+    url: `https://raw.githubusercontent.com/LedgerHQ/coin-apps/master/nanox${nanoAppEndpoint}`,
+    method: "GET",
+    responseType: "stream",
+    headers: {
+      Authorization: `Bearer ${process.env.GH_TOKEN}`,
+    },
+  });
+
+  await fs.mkdir(path.resolve(cwd, "tmp"), { recursive: true });
+  await fs.writeFile(path.resolve(cwd, "tmp/app.elf"), blob, "binary");
+
+  await compose.upOne("speculos", {
+    cwd,
+    // log: true,
+    env: process.env,
+  });
+
+  const checkSpeculosLogs = async (): Promise<SpeculosTransportHttp> => {
+    const { out } = await compose.logs("speculos", { cwd, env: process.env });
+
+    if (out.includes("Running on all addresses (0.0.0.0)")) {
+      console.log(chalk.bgYellowBright.black(" -  SPECULOS READY ✅  - "));
+      return SpeculosTransportHttp.open({
+        apiPort: API_PORT,
+      });
+    }
+
+    await delay(200);
+    return checkSpeculosLogs();
+  };
+
+  const onSpeculosConfirmation = async (e?: SignOperationEvent): Promise<void> => {
+    if (e?.type === "device-signature-requested") {
+      const { data } = await axios.get(
+        `http://localhost:${process.env.API_PORT}/events?currentscreenonly=true`,
+      );
+
+      if (data.events[0].text !== "Accept") {
+        await axios.post(`http://localhost:${process.env.API_PORT}/button/right`, {
+          action: "press-and-release",
+        });
+        onSpeculosConfirmation(e);
+      } else {
+        await axios.post(`http://localhost:${process.env.API_PORT}/button/both`, {
+          action: "press-and-release",
+        });
+      }
+    }
+  };
+
+  return checkSpeculosLogs().then(transport => {
+    return {
+      transport,
+      onSignerConfirmation: onSpeculosConfirmation,
+    };
+  });
+};
+
+export const killSpeculos = async () => {
+  console.log("Stopping speculos...");
+  await compose.down({
+    cwd,
+    // log: true,
+    env: process.env,
+  });
+};
+
+["exit", "SIGINT", "SIGQUIT", "SIGTERM", "SIGUSR1", "SIGUSR2", "uncaughtException"].map(e =>
+  process.on(e, async () => {
+    await killSpeculos();
+  }),
+);
