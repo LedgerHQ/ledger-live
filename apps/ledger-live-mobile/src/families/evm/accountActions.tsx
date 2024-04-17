92455de4d4c870deef251fbdbe75594a48a7b676
Philip Zudemberg <philip.zdb@gmail.com>
2024-04-17 12:40:42 +0400

    feat(llm): enable matic staking via stakekit on mobile

--- apps/ledger-live-mobile/src/families/evm/accountActions.tsx
+++ apps/ledger-live-mobile/src/families/evm/accountActions.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { Account } from "@ledgerhq/types-live";
+import type { Account, TokenAccount } from "@ledgerhq/types-live";
 import { IconsLegacy } from "@ledgerhq/native-ui";
 import { Trans } from "react-i18next";
 import { isAccountEmpty } from "@ledgerhq/live-common/account/index";
@@ -14,12 +14,15 @@ const ethMagnitude = getCryptoCurrencyById("ethereum").units[0].magnitude ?? 18;
 const ETH_LIMIT = BigNumber(32).times(BigNumber(10).pow(ethMagnitude));
 
 type Props = {
-  account: Account;
+  account: Account | TokenAccount;
   parentAccount: Account;
   parentRoute: RouteProp<ParamListBase, ScreenName>;
 };
 
-function getNavigatorParams({ parentRoute, account, parentAccount }: Props): NavigationParamsType {
+function getNavigatorParams(
+  { parentRoute, account, parentAccount }: Props,
+  isEthereumMaticTokenAccount: boolean,
+): NavigationParamsType {
   if (isAccountEmpty(account)) {
     return [
       NavigatorName.NoFundsFlow,
@@ -28,6 +31,20 @@ function getNavigatorParams({ parentRoute, account, parentAccount }: Props): Nav
         params: {
           account,
           parentAccount,
+        },
+      },
+    ];
+  }
+
+  if (isEthereumMaticTokenAccount) {
+    return [
+      ScreenName.PlatformApp,
+      {
+        params: {
+          platform: "stakekit",
+          name: "StakeKit",
+          accountId: account.id,
+          yieldId: "ethereum-matic-native-staking",
         },
       },
     ];
@@ -63,12 +80,19 @@ function getNavigatorParams({ parentRoute, account, parentAccount }: Props): Nav
 }
 
 const getMainActions = ({ account, parentAccount, parentRoute }: Props): ActionButtonEvent[] => {
-  if (account.type === "Account" && account.currency.id === "ethereum") {
-    const navigationParams = getNavigatorParams({
-      account,
-      parentAccount,
-      parentRoute,
-    });
+  const isEthereumAccount = account.type === "Account" && account.currency.id === "ethereum";
+  const isEthereumMaticTokenAccount =
+    account.type === "TokenAccount" && account.token.id === "ethereum/erc20/matic";
+
+  if (isEthereumAccount || isEthereumMaticTokenAccount) {
+    const navigationParams = getNavigatorParams(
+      {
+        account,
+        parentAccount,
+        parentRoute,
+      },
+      isEthereumMaticTokenAccount,
+    );
 
     return [
       {
@@ -77,7 +101,7 @@ const getMainActions = ({ account, parentAccount, parentRoute }: Props): ActionB
         label: <Trans i18nKey="account.stake" />,
         Icon: IconsLegacy.CoinsMedium,
         eventProperties: {
-          currency: "ETH",
+          currency: isEthereumMaticTokenAccount ? "ETH" : "MATIC",
         },
       },
     ];
