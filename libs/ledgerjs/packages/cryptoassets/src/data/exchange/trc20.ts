8edf031c4ecc5a9994861eb69c53733143aab9a2
Chuck Ng <cheuk-man.ng@ledger.fr>
2024-04-22 09:39:44 +0100

    feat(LIVE-10131): fix types

--- libs/ledgerjs/packages/cryptoassets/src/data/exchange/trc20.ts
+++ libs/ledgerjs/packages/cryptoassets/src/data/exchange/trc20.ts
@@ -2,4 +2,4 @@ export type TRC20Exchange = [string, string, string];
 
 import exchanges from "./trc20.json";
 
-export default exchanges as TRC20Exchange[];
+export default exchanges[0] as TRC20Exchange[];
