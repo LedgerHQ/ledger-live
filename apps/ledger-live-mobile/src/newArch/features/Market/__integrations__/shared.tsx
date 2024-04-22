c872992aaadf8a92f7dac5fdb84c645deb1d3905
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-04-22 14:15:57 +0200

    Fix typecheck

--- apps/ledger-live-mobile/src/newArch/features/Market/__integrations__/shared.tsx
+++ apps/ledger-live-mobile/src/newArch/features/Market/__integrations__/shared.tsx
@@ -6,17 +6,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { ScreenName } from "~/const";
 import MarketList from "../screens/MarketList";
 import { BaseNavigatorStackParamList } from "~/components/RootNavigator/types/BaseNavigator";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Stack = createStackNavigator<BaseNavigatorStackParamList>();
 const StackWalletTab = createStackNavigator<MarketNavigatorStackParamList>();
 
 export function MarketPages() {
   return (
-    <WalletTabNavigatorScrollManager>
-      <StackWalletTab.Navigator initialRouteName={ScreenName.MarketList}>
-        <StackWalletTab.Screen name={ScreenName.MarketList} component={MarketList} />
-        {MarketNavigator({ Stack })}
-      </StackWalletTab.Navigator>
-    </WalletTabNavigatorScrollManager>
+    <QueryClientProvider client={new QueryClient()}>
+      <WalletTabNavigatorScrollManager>
+        <StackWalletTab.Navigator initialRouteName={ScreenName.MarketList}>
+          <StackWalletTab.Screen name={ScreenName.MarketList} component={MarketList} />
+          {MarketNavigator({ Stack })}
+        </StackWalletTab.Navigator>
+      </WalletTabNavigatorScrollManager>
+    </QueryClientProvider>
   );
 }
