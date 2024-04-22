fe28f0677caf5b45730c4af7019d7df4111193b5
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-04-22 17:20:28 +0200

    fix typecheck

--- apps/ledger-live-desktop/src/renderer/actions/market.ts
+++ apps/ledger-live-desktop/src/renderer/actions/market.ts
@@ -1,4 +1,4 @@
-import { MarketListRequestParams } from "@ledgerhq/live-common/market/utils/types";
+import { MarketListRequestParams } from "@ledgerhq/live-common/market/types";
 
 export const setMarketOptions = (payload: MarketListRequestParams) => ({
   type: "MARKET_SET_VALUES",
