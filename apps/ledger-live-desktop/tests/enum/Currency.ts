fea9cc01ef2873ee152c9a3fc2a360c5e3fb1df0
Victor Alber <victor.alber@ledger.fr>
2024-04-22 16:15:55 +0200

    update Speculos right Click method

--- apps/ledger-live-desktop/tests/enum/Currency.ts
+++ apps/ledger-live-desktop/tests/enum/Currency.ts
@@ -9,4 +9,8 @@ export class Currency {
   static readonly ETH = new Currency("Ethereum", "ETH");
   static readonly tETH = new Currency("Ethereum Holesky", "tETH");
   static readonly SOL = new Currency("Solana", "SOL");
+  static readonly ADA = new Currency("Cardano", "ADA");
+  static readonly DOT = new Currency("Polkadot", "DOT");
+  static readonly TRX = new Currency("Tron", "TRX");
+  static readonly Ripple = new Currency("Ripple", "XRP");
 }
