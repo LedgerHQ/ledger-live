75a4852e3cba0051a0c6228bff8c0419b87d26e8
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-04-22 17:08:24 +0200

    [FIX]: Starred coins were not saved anymore since rework

--- apps/ledger-live-desktop/src/renderer/actions/market.ts
+++ apps/ledger-live-desktop/src/renderer/actions/market.ts
@@ -1,16 +1,7 @@
-import { MarketListRequestParams } from "@ledgerhq/live-common/market/types";
+import { MarketListRequestParams } from "@ledgerhq/live-common/market/utils/types";
 
 export const setMarketOptions = (payload: MarketListRequestParams) => ({
   type: "MARKET_SET_VALUES",
-  payload,
-});
-
-export const addStarredMarketCoins = (payload: string) => ({
-  type: "ADD_STARRED_MARKET_COINS",
-  payload,
-});
-export const removeStarredMarketCoins = (payload: string) => ({
-  type: "REMOVE_STARRED_MARKET_COINS",
   payload,
 });
 
