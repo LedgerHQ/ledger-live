a115d6cd5dcbcc753d02dedb80f5eb1693d1a249
Come Grellard <come.grellard@ledger.fr>
2024-04-24 16:00:02 +0200

    feat(llc): automatically dismisses the post onboarding entry point on both LLM and LLD if it has been displayed for more than seven days

--- libs/ledger-live-common/src/postOnboarding/hooks/usePostOnboardingEntryPointVisibleOnWallet.test.ts
+++ libs/ledger-live-common/src/postOnboarding/hooks/usePostOnboardingEntryPointVisibleOnWallet.test.ts
@@ -5,13 +5,16 @@ import { renderHook } from "@testing-library/react";
 import {
   postOnboardingDeviceModelIdSelector,
   walletPostOnboardingEntryPointDismissedSelector,
+  entryPointFirstDisplayedDateSelector,
 } from "../reducer";
 import { useAllPostOnboardingActionsCompleted } from "./useAllPostOnboardingActionsCompleted";
 import { usePostOnboardingEntryPointVisibleOnWallet } from "./usePostOnboardingEntryPointVisibleOnWallet";
 import { DeviceModelId } from "@ledgerhq/types-devices";
 
+const mockedDispatch = jest.fn();
 jest.mock("react-redux", () => ({
   useSelector: fun => fun(),
+  useDispatch: () => mockedDispatch,
 }));
 jest.mock("../reducer");
 jest.mock("./useAllPostOnboardingActionsCompleted");
@@ -19,12 +22,25 @@ jest.mock("./useAllPostOnboardingActionsCompleted");
 const mockedUseAllCompleted = jest.mocked(useAllPostOnboardingActionsCompleted);
 const mockedDismissedSelector = jest.mocked(walletPostOnboardingEntryPointDismissedSelector);
 const mockedDeviceModelIdSelector = jest.mocked(postOnboardingDeviceModelIdSelector);
+const mockedEntryPointFirstDisplayedDateSelector = jest.mocked(
+  entryPointFirstDisplayedDateSelector,
+);
 
 describe("usePostOnboardingEntryPointVisibleOnWallet", () => {
+  beforeAll(() => {
+    jest.useFakeTimers().setSystemTime(new Date("2020-01-20"));
+  });
+
+  afterAll(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
   beforeEach(() => {
     mockedUseAllCompleted.mockClear();
     mockedDismissedSelector.mockClear();
     mockedDeviceModelIdSelector.mockClear();
+    mockedDispatch.mockClear();
   });
 
   it("should be false if deviceModelId is null", () => {
@@ -68,5 +84,30 @@ describe("usePostOnboardingEntryPointVisibleOnWallet", () => {
     const { result } = renderHook(() => usePostOnboardingEntryPointVisibleOnWallet());
 
     expect(result.current).toBe(true);
+  });
+
+  it("should dismiss the entry point if it has been displayed more than 7 days ago", () => {
+    mockedDismissedSelector.mockReturnValue(false);
+    mockedUseAllCompleted.mockReturnValue(false);
+    mockedDeviceModelIdSelector.mockReturnValue(DeviceModelId.nanoX);
+    mockedEntryPointFirstDisplayedDateSelector.mockReturnValue(new Date("2020-01-12"));
+
+    renderHook(() => usePostOnboardingEntryPointVisibleOnWallet());
+
+    expect(mockedDispatch).toHaveBeenCalledWith({
+      type: "POST_ONBOARDING_HIDE_WALLET_ENTRY_POINT",
+    });
+  });
+
+  it("should be true if the entry point has been displayed less than 7 days ago", () => {
+    mockedDismissedSelector.mockReturnValue(false);
+    mockedUseAllCompleted.mockReturnValue(false);
+    mockedDeviceModelIdSelector.mockReturnValue(DeviceModelId.nanoX);
+    mockedEntryPointFirstDisplayedDateSelector.mockReturnValue(new Date("2020-01-14"));
+
+    const { result } = renderHook(() => usePostOnboardingEntryPointVisibleOnWallet());
+
+    expect(result.current).toBe(true);
+    expect(mockedDispatch).not.toHaveBeenCalled();
   });
 });
--- libs/ledger-live-common/src/postOnboarding/hooks/usePostOnboardingEntryPointVisibleOnWallet.ts
+++ libs/ledger-live-common/src/postOnboarding/hooks/usePostOnboardingEntryPointVisibleOnWallet.ts
@@ -1,9 +1,23 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
+  entryPointFirstDisplayedDateSelector,
   postOnboardingDeviceModelIdSelector,
   walletPostOnboardingEntryPointDismissedSelector,
 } from "../reducer";
 import { useAllPostOnboardingActionsCompleted } from "./useAllPostOnboardingActionsCompleted";
+import { useEffect } from "react";
+import { hidePostOnboardingWalletEntryPoint } from "../actions";
+
+const millisecondsInADay = 1000 * 60 * 60 * 24;
+
+const isMoreThanSevenDaysAgo = (date: Date) => {
+  if (!date) return false;
+
+  const datePlusSevenDays = new Date(date).getTime() + 7 * millisecondsInADay;
+  const today = new Date().getTime();
+
+  return datePlusSevenDays < today;
+};
 
 /**
  *
@@ -11,9 +25,20 @@ import { useAllPostOnboardingActionsCompleted } from "./useAllPostOnboardingActi
  * should be visible on the wallet page.
  */
 export function usePostOnboardingEntryPointVisibleOnWallet(): boolean {
+  const dispatch = useDispatch();
   const deviceModelId = useSelector(postOnboardingDeviceModelIdSelector);
   const dismissed = useSelector(walletPostOnboardingEntryPointDismissedSelector);
   const allCompleted = useAllPostOnboardingActionsCompleted();
+  const entryPointFirstDisplayedDate = useSelector(entryPointFirstDisplayedDateSelector);
 
-  return !!deviceModelId && !(dismissed || allCompleted);
+  const isPostOnboardingEntryPointVisible = !!deviceModelId && !(dismissed || allCompleted);
+
+  useEffect(() => {
+    if (isPostOnboardingEntryPointVisible && isMoreThanSevenDaysAgo(entryPointFirstDisplayedDate)) {
+      dispatch(hidePostOnboardingWalletEntryPoint());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return isPostOnboardingEntryPointVisible;
 }
