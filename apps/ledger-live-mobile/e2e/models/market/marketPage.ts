b8f4b565a987a153f5069f44293a796eb5ac296a
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-04-25 15:46:46 +0200

    Revert "fix e2e"
    This reverts commit 39b4161b6a90093859b46d9cabbdc66de905e3f3.

--- apps/ledger-live-mobile/e2e/models/market/marketPage.ts
+++ apps/ledger-live-mobile/e2e/models/market/marketPage.ts
@@ -4,7 +4,7 @@ export default class MarketPage {
   searchBar = () => getElementById("search-box");
   starButton = () => getElementById("star-asset");
   assetCardBackBtn = () => getElementById("market-back-btn");
-  starMarketListButton = () => getElementById("toggle-starred-currencies");
+  starMarketListButton = () => getElementById("starred");
   buyAssetButton = () => getElementById("market-buy-btn");
 
   searchAsset(asset: string) {
