48d6da08dca794baf55d5f20b445df100693d62c
weeddotv <weeddotv@gmail.com>
2024-04-27 15:39:40 +0700

    fix: revert lint

--- libs/ledger-live-common/src/generated/account.ts
+++ libs/ledger-live-common/src/generated/account.ts
@@ -1,8 +1,10 @@
 import crypto_org from "../families/crypto_org/account";
+import icon from "../families/icon/account";
 import vechain from "../families/vechain/account";
 import bitcoin from "@ledgerhq/coin-bitcoin/account";
 import cardano from "@ledgerhq/coin-cardano/account";
 import near from "@ledgerhq/coin-near/account";
+<<<<<<< HEAD
 import elrond from "../families/elrond/account";
 import filecoin from "../families/filecoin/account";
 import icon from "../families/icon/account";
@@ -10,13 +12,17 @@ import polkadot from "@ledgerhq/coin-polkadot/account";
 import internet_computer from "../families/internet_computer/account";
 import stacks from "../families/stacks/account";
 import algorand from "@ledgerhq/coin-algorand/account";
+=======
+>>>>>>> 6a02bf4d45 (fix: revert lint)
 
 export default {
   crypto_org,
+  icon,
   vechain,
   bitcoin,
   cardano,
   near,
+<<<<<<< HEAD
   elrond,
   filecoin,
   icon,
@@ -24,4 +30,6 @@ export default {
   stacks,
   algorand,
   polkadot,
+=======
+>>>>>>> 6a02bf4d45 (fix: revert lint)
 };
