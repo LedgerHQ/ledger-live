de5de2d273ed6966c82bde2c3a95b98ba594204f
Stéphane Prohaszka <104785785+sprohaszka-ledger@users.noreply.github.com>
2024-04-29 15:27:00 +0200

    Move transaction serialization in correct directory (#6663)
    * chore: move transaction serialization functions into serialization dir
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    * fix: update unimported file
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    * chore: feedback
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    * chore: changeset
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    ---------
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/families/crypto_org/transaction.ts
+++ libs/ledger-live-common/src/families/crypto_org/transaction.ts
@@ -1,12 +1,12 @@
 import type { Transaction, TransactionRaw } from "./types";
 import { BigNumber } from "bignumber.js";
+import { formatTransactionStatus } from "@ledgerhq/coin-framework/formatters";
 import {
-  formatTransactionStatusCommon as formatTransactionStatus,
   fromTransactionCommonRaw,
   fromTransactionStatusRawCommon as fromTransactionStatusRaw,
   toTransactionCommonRaw,
   toTransactionStatusRawCommon as toTransactionStatusRaw,
-} from "@ledgerhq/coin-framework/transaction/common";
+} from "@ledgerhq/coin-framework/serialization";
 import type { Account } from "@ledgerhq/types-live";
 import { getAccountUnit } from "../../account";
 import { formatCurrencyUnit } from "../../currencies";
--- libs/ledger-live-common/src/families/vechain/transaction.ts
+++ libs/ledger-live-common/src/families/vechain/transaction.ts
@@ -1,11 +1,11 @@
 import type { Transaction, TransactionRaw, TransactionStatus, TransactionStatusRaw } from "./types";
+import { formatTransactionStatus } from "@ledgerhq/coin-framework/formatters";
 import {
-  formatTransactionStatusCommon as formatTransactionStatus,
   fromTransactionCommonRaw,
   fromTransactionStatusRawCommon,
   toTransactionCommonRaw,
   toTransactionStatusRawCommon,
-} from "@ledgerhq/coin-framework/transaction/common";
+} from "@ledgerhq/coin-framework/serialization";
 import type { Account } from "@ledgerhq/types-live";
 import { getAccountUnit } from "../../account";
 import { formatCurrencyUnit } from "../../currencies";
