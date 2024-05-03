16b30382ba047745f1993922bf9f1004c5de00f7
Come Grellard <come.grellard@ledger.fr>
2024-05-03 16:25:25 +0200

    feat(llm/lld): add a deeplink redirecting to the post onboarding

--- libs/ledger-live-common/src/postOnboarding/hooks/index.ts
+++ libs/ledger-live-common/src/postOnboarding/hooks/index.ts
@@ -4,3 +4,4 @@ export * from "./usePostOnboardingEntryPointVisibleOnWallet";
 export * from "./usePostOnboardingHubState";
 export * from "./useStartPostOnboardingCallback";
 export * from "./useAutoDismissPostOnboardingEntryPoint";
+export * from "./usePostOnboardingDeeplinkHandler";
