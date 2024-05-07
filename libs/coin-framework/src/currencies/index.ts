17ab18af37bb6fd6f2ccf90525879c6758861257
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-05-07 10:26:32 +0200

    chore: remove unused chopCurrencyUnitDecimals function
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-framework/src/currencies/index.ts
+++ libs/coin-framework/src/currencies/index.ts
@@ -1,17 +1,16 @@
-import { encodeURIScheme, decodeURIScheme } from "./CurrencyURIScheme";
-import { sanitizeValueString } from "./sanitizeValueString";
-export * from "./support";
-import { parseCurrencyUnit } from "./parseCurrencyUnit";
-import { chopCurrencyUnitDecimals } from "./chopCurrencyUnitDecimals";
-import { formatCurrencyUnit, formatCurrencyUnitFragment } from "./formatCurrencyUnit";
-import { formatShort } from "./formatShort";
-import { valueFromUnit } from "./valueFromUnit";
 import type { Currency } from "@ledgerhq/types-cryptoassets";
 import {
   findCryptoCurrencyByTicker,
   findFiatCurrencyByTicker,
   findTokenByTicker,
 } from "@ledgerhq/cryptoassets/index";
+import { encodeURIScheme, decodeURIScheme } from "./CurrencyURIScheme";
+import { sanitizeValueString } from "./sanitizeValueString";
+export * from "./support";
+import { parseCurrencyUnit } from "./parseCurrencyUnit";
+import { formatCurrencyUnit, formatCurrencyUnitFragment } from "./formatCurrencyUnit";
+import { formatShort } from "./formatShort";
+import { valueFromUnit } from "./valueFromUnit";
 
 const findCurrencyByTicker = (ticker: string): Currency | null | undefined =>
   findCryptoCurrencyByTicker(ticker) ||
@@ -21,7 +20,6 @@ const findCurrencyByTicker = (ticker: string): Currency | null | undefined =>
 export {
   findCurrencyByTicker,
   parseCurrencyUnit,
-  chopCurrencyUnitDecimals,
   formatCurrencyUnit,
   formatCurrencyUnitFragment,
   formatShort,
