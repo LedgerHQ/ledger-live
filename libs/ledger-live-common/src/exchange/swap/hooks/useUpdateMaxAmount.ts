cc88da48e189334ec9093f4fec643952559d5824
Chuck Ng <cheuk-man.ng@ledger.fr>
2024-05-09 15:07:20 +0100

    feat(LIVE-10131): pr review

--- libs/ledger-live-common/src/exchange/swap/hooks/useUpdateMaxAmount.ts
+++ libs/ledger-live-common/src/exchange/swap/hooks/useUpdateMaxAmount.ts
@@ -41,17 +41,12 @@ export const useUpdateMaxAmount = ({
         }
         bridge.updateTransaction(tx => {
           let additionalFees;
-          switch (true) {
-            case tx.family === "evm" && next:
-              additionalFees = new BigNumber(5000000000000000); // 0,005 ETH/BNB/MATIC
-              break;
-            case tx.family === "tron" && next:
-              // https://github.com/tronprotocol/documentation/blob/master/TRX/Tron-overview.md
-              // 1 TRX=1,000,000 sun.
-              additionalFees = new BigNumber(1).multipliedBy(1_000_000); // 1 TRX
-              break;
-            default:
-              break;
+          if (tx.family === "evm" && next) {
+            additionalFees = new BigNumber(5000000000000000); // 0,005 ETH/BNB/MATIC
+          } else if (tx.family === "tron" && next) {
+            // https://github.com/tronprotocol/documentation/blob/master/TRX/Tron-overview.md
+            // 1 TRX=1,000,000 sun.
+            additionalFees = new BigNumber(1).multipliedBy(1_000_000); // 1 TRX
           }
           return {
             ...tx,
