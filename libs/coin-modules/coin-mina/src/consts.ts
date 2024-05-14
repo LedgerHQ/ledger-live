cc2edd96701eed468faea5aada469c98d144613d
Prateek Rathod <prateek@zondax.ch>
2024-05-14 14:23:11 +0200

    feat(mina): add staking

--- libs/coin-modules/coin-mina/src/consts.ts
+++ libs/coin-modules/coin-mina/src/consts.ts
@@ -5,8 +5,9 @@ export const MAINNET_NETWORK_IDENTIFIER = {
   },
 };
 
+export const MINA_VALID_UNTIL_DEFAULT = 4294967295;
+
 export const MINA_MAINNET_NETWORK_ID = 1;
-export const MINA_PAYMENT_TYPE_ID = 0;
 export const MINA_TOKEN_ID = "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf";
 export const MINA_DECIMALS = 9;
 export const MINA_SYMBOL = "MINA";
@@ -15,3 +16,7 @@ export const MINA_CURVE_TYPE = "pallas";
 export const MINA_DECODED_ADDRESS_LENGTH = 72;
 
 export const MAX_TRANSACTIONS_PER_PAGE = 100;
+export const MINA_ROSETTA_TIMEOUT = 30000;
+export const MINA_API_RETRY_COUNT = 3;
+
+export const MINA_CANCEL_RETURN_CODE = "27013";
