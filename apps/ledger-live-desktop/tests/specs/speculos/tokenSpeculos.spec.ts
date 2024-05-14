98df1a4b9089ea60cc3062b88802be832a988498
Victor Alber <victor.alber@ledger.fr>
2024-05-14 12:15:24 +0200

    WIP: Speculos on the fly

--- apps/ledger-live-desktop/tests/specs/speculos/tokenSpeculos.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/tokenSpeculos.spec.ts
@@ -3,23 +3,20 @@ import { expect } from "@playwright/test";
 import { PortfolioPage } from "../../models/PortfolioPage";
 import { Layout } from "../../models/Layout";
 import { AccountPage } from "../../models/AccountPage";
-import { Device, specs, startSpeculos, stopSpeculos } from "../../utils/speculos";
+import { specs } from "../../utils/speculos";
 
-test.use({ userdata: "speculos" });
-const token = "Tether USD";
-
-let device: Device | undefined;
-
-test.afterEach(async () => {
-  await stopSpeculos(device);
+test.use({
+  userdata: "speculos",
+  testName: "tokenERC20",
+  speculosCurrency: specs["Ethereum".replace(/ /g, "_")],
 });
+const token = "Tether USD";
 
 test.describe.parallel("ERC20 token", () => {
   test(`Ckeck ERC20 token`, async ({ page }) => {
     const portfolioPage = new PortfolioPage(page);
     const layout = new Layout(page);
     const accountPage = new AccountPage(page);
-    device = await startSpeculos(test.name, specs["Ethereum".replace(/ /g, "_")]);
 
     await test.step(`navigate to portfolio`, async () => {
       await layout.goToPortfolio();
