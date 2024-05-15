cfb5d27ee2e44335717ff4c11a2ea4a8dd86441b
Stéphane Prohaszka <104785785+sprohaszka-ledger@users.noreply.github.com>
2024-05-15 13:36:31 +0200

    Adding coverage for coin modules (#6859)
    * feat: start adding coverage for coins
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    * feat: set coverage on all files
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    ---------
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-modules/coin-near/jest.config.js
+++ libs/coin-modules/coin-near/jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["lib/", "lib-es/"],
