aa6f77883a23d20ba6eb38fcfd5bb45d6e945628
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-05-16 23:12:31 +0200

    Apply improved types & architecture to Evm

--- libs/coin-modules/coin-evm/src/__tests__/unit/preload.unit.test.ts
+++ libs/coin-modules/coin-evm/src/__tests__/unit/preload.unit.test.ts
@@ -18,8 +18,8 @@ const usdcDefinition: ERC20Token = [
   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   false,
   false,
-  undefined,
-  undefined,
+  null,
+  null,
 ];
 const usdtDefinition: ERC20Token = [
   "ethereum",
@@ -31,8 +31,8 @@ const usdtDefinition: ERC20Token = [
   "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   false,
   false,
-  undefined,
-  undefined,
+  null,
+  null,
 ];
 const binanceDaiDefinition: BEP20Token = [
   "bsc",
