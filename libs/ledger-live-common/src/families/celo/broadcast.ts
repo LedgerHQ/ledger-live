d5e042cd181a414b3335f70448a583f6216020e0
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-05-16 23:13:51 +0200

    Apply improved types & architecture to Celo

--- libs/ledger-live-common/src/families/celo/broadcast.ts
+++ libs/ledger-live-common/src/families/celo/broadcast.ts
@@ -1,7 +1,11 @@
+import { AccountBridge } from "@ledgerhq/types-live";
 import { patchOperationWithHash } from "../../operation";
+import { Transaction } from "./types";
 import { celoKit } from "./api/sdk";
 
-const broadcast = async ({ signedOperation: { operation, signature } }) => {
+export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
+  signedOperation: { operation, signature },
+}) => {
   const kit = celoKit();
   const { transactionHash } = await kit.web3.eth.sendSignedTransaction(signature);
   return patchOperationWithHash(operation, transactionHash);
