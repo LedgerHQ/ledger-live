16731a55571f54aed2e15e27f77a6505ac57def2
Mikhail <mikhail@hoodies.company>
2024-05-20 12:51:46 +0300

    feat: display solana tokens in LLD

--- apps/ledger-live-desktop/src/renderer/families/solana/AccountSubHeader.tsx
+++ apps/ledger-live-desktop/src/renderer/families/solana/AccountSubHeader.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Trans } from "react-i18next";
+import { SolanaAccount, SolanaTokenAccount } from "@ledgerhq/live-common/families/solana/types";
+import { isTokenAccountFrozen } from "@ledgerhq/live-common/families/solana/logic";
+import { SubAccount } from "@ledgerhq/types-live";
+
+import Box from "~/renderer/components/Box";
+import Alert from "~/renderer/components/Alert";
 import AccountSubHeader from "../../components/AccountSubHeader/index";
-export default function SolanaAccountSubHeader() {
-  return <AccountSubHeader family="Solana" team="Solana Labs"></AccountSubHeader>;
+
+type Account = SolanaAccount | SolanaTokenAccount | SubAccount;
+
+type Props = {
+  account: Account;
+};
+
+export default function SolanaAccountSubHeader({ account }: Props) {
+  return (
+    <>
+      {isTokenAccountFrozen(account) && (
+        <Box mb={10}>
+          <Alert type="warning">
+            <Trans i18nKey="solana.token.frozenStateWarning" />
+          </Alert>
+        </Box>
+      )}
+      <AccountSubHeader family="Solana" team="Solana Labs"></AccountSubHeader>
+    </>
+  );
 }
