2e52a5282c968b8d18f3123419f6dbdaaa2c1e68
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-05-20 11:56:36 +0200

    [WIP]: WS activation flow (connect device)

--- apps/ledger-live-desktop/src/newArch/WalletSync/SideContent/Activation/index.tsx
+++ apps/ledger-live-desktop/src/newArch/WalletSync/SideContent/Activation/index.tsx
@@ -1,27 +1,37 @@
-import { Box, Flex, Icons, Text } from "@ledgerhq/react-ui";
-import React, { PropsWithChildren } from "react";
-import { useTranslation } from "react-i18next";
-import ButtonV3 from "~/renderer/components/ButtonV3";
-import useTheme from "~/renderer/hooks/useTheme";
-
-const LogoWrapper = ({ children, opacity = "70%" }: PropsWithChildren & { opacity?: string }) => (
-  <Box>
-    <Flex padding="7px" borderRadius="13px" border="1px solid hsla(0, 0%, 100%, 0.05)">
-      <Flex
-        borderRadius="9px"
-        backgroundColor="hsla(248, 100%, 85%, 0.08)"
-        padding="5px"
-        opacity={opacity}
-      >
-        {children}
-      </Flex>
-    </Flex>
-  </Box>
-);
+import React from "react";
+import { useFlows } from "../Flows/useFlows";
+import { StepOne } from "./stepA";
+import { Flow } from "~/renderer/reducers/walletSync";
+import { useDispatch } from "react-redux";
+import { setFlow } from "~/renderer/actions/walletSync";
+import { StepTwo } from "./stepB";
+import { Flex } from "@ledgerhq/react-ui";
+import { StepThree } from "./stepC";
 
 const WalletSyncActivation = () => {
-  const { colors } = useTheme();
-  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const { currentStep, goToNextScene, setCurrentStep } = useFlows({ flow: Flow.Activation });
+
+  const goToSync = () => {
+    dispatch(setFlow(Flow.Sync));
+  };
+
+  const goBeginnning = () => {
+    setCurrentStep(1);
+  };
+
+  const getStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <StepOne goToCreateBackup={goToNextScene} goToSync={goToSync} />;
+      case 2:
+        return <StepTwo goNext={goToNextScene} />;
+      case 3:
+        return <StepThree goNext={goBeginnning} />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <Flex
@@ -32,48 +42,7 @@ const WalletSyncActivation = () => {
       justifyContent="center"
       rowGap="48px"
     >
-      <Flex flexDirection="column" alignSelf="center" justifyContent="center" rowGap="24px">
-        <Flex justifyContent="center" alignItems="center">
-          <LogoWrapper>
-            <Icons.Mobile color={colors.constant.purple} />
-          </LogoWrapper>
-
-          <LogoWrapper opacity="100%">
-            <Icons.Refresh size="L" color={colors.constant.purple} />
-          </LogoWrapper>
-
-          <LogoWrapper>
-            <Icons.Desktop color={colors.constant.purple} />
-          </LogoWrapper>
-        </Flex>
-
-        <Text fontSize={24} variant="h4Inter" textAlign="center">
-          {t("walletSync.activate.title")}
-        </Text>
-        <Text fontSize={14} variant="body" color="hsla(0, 0%, 58%, 1)" textAlign="center">
-          {t("walletSync.activate.description")}
-        </Text>
-        <Flex justifyContent="center">
-          <ButtonV3 variant="main"> {t("walletSync.activate.cta")}</ButtonV3>
-        </Flex>
-      </Flex>
-
-      <Box>
-        <Flex
-          flexDirection="row"
-          padding="18px"
-          borderRadius="12px"
-          backgroundColor={colors.opacityDefault.c05}
-          justifyContent="space-between"
-        >
-          <Text variant="body" fontSize={14} flexShrink={1}>
-            {t("walletSync.alreadySync.title")}
-          </Text>
-          <Box>
-            <ButtonV3 variant="shade">{t("walletSync.alreadySync.cta")}</ButtonV3>
-          </Box>
-        </Flex>
-      </Box>
+      {getStep()}
     </Flex>
   );
 };
--- apps/ledger-live-desktop/src/renderer/reducers/walletSync.ts
+++ apps/ledger-live-desktop/src/renderer/reducers/walletSync.ts
@@ -1,17 +1,26 @@
 import { handleActions } from "redux-actions";
 import { Handlers } from "./types";
+export enum Flow {
+  Activation = "Activation",
+  Sync = "Sync",
+  ManageInstances = "ManageInstances",
+  ManageBackup = "ManageBackup",
+}
 
 export type WalletSyncState = {
   activated: boolean;
+  flow: Flow;
 };
 
 const initialState: WalletSyncState = {
   activated: false,
+  flow: Flow.Activation,
 };
 
 type HandlersPayloads = {
   WALLET_SYNC_ACTIVATE: boolean;
   WALLET_SYNC_DEACTIVATE: boolean;
+  WALLET_SYNC_CHANGE_FLOW: Flow;
 };
 
 type MarketHandlers<PreciseKey = true> = Handlers<WalletSyncState, HandlersPayloads, PreciseKey>;
@@ -25,10 +34,19 @@ const handlers: MarketHandlers = {
     ...state,
     activated: false,
   }),
+  WALLET_SYNC_CHANGE_FLOW: (state: WalletSyncState, { payload }: { payload: Flow }) => ({
+    ...state,
+    flow: payload,
+  }),
 };
 
 // Selectors
 export const walletSyncSelector = (state: { walletSync: WalletSyncState }) => state.walletSync;
+
+export const walletSyncFlowSelector = (state: { walletSync: WalletSyncState }) =>
+  state.walletSync.flow;
+export const walletSyncStateSelector = (state: { walletSync: WalletSyncState }) =>
+  state.walletSync.activated;
 
 export default handleActions<WalletSyncState, HandlersPayloads[keyof HandlersPayloads]>(
   handlers as unknown as MarketHandlers<false>,
