c431002bfab27a10f3aa680edd8b709d8ee402ee
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-05-21 11:41:43 +0200

    Reorg files using newArch folder
    update imports

---
+++ apps/ledger-live-desktop/src/newArch/WalletSync/utils/shared.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import WalletSyncRow from "~/renderer/screens/settings/sections/General/WalletSync";
+
+export function WalletSyncPages() {
+  return (
+    <>
+      <div id="modals"></div>
+      <WalletSyncRow />
+    </>
+  );
+}
