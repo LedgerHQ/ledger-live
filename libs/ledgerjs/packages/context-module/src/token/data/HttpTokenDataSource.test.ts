3251bebec988f5a175beeeb6d9375a6430dc48d5
Louis Aussedat <louis.aussedat@ledger.fr>
2024-05-22 10:07:08 +0200

    feat(context-module): add unit tests for token datasource

--- libs/ledgerjs/packages/context-module/src/token/data/HttpTokenDataSource.test.ts
+++ libs/ledgerjs/packages/context-module/src/token/data/HttpTokenDataSource.test.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { TokenDataSource } from "./TokenDataSource";
 import { HttpTokenDataSource } from "./HttpTokenDataSource";
+import { TokenDto } from "./TokenDto";
 import PACKAGE from "../../../package.json";
 
 jest.mock("axios");
@@ -26,5 +27,39 @@ describe("HttpTokenDataSource", () => {
     expect(requestSpy).toHaveBeenCalledWith(
       expect.objectContaining({ headers: { "X-Ledger-Client-Version": version } }),
     );
+  });
+
+  it("should return a string when axios response is correct", async () => {
+    // GIVEN
+    const tokenDTO: TokenDto = { live_signature: "0x0" };
+    jest.spyOn(axios, "request").mockResolvedValue({ data: [tokenDTO] });
+
+    // WHEN
+    const result = await datasource.getTokenInfosPayload({ address: "0x00", chainId: 1 });
+
+    // THEN
+    expect(result).toEqual("0x0");
+  });
+
+  it("should return undefined when data is empty", async () => {
+    // GIVEN
+    jest.spyOn(axios, "request").mockResolvedValue({ data: undefined });
+
+    // WHEN
+    const result = await datasource.getTokenInfosPayload({ address: "0x00", chainId: 1 });
+
+    // THEN
+    expect(result).toEqual(undefined);
+  });
+
+  it("should return undefined when no signature", async () => {
+    // GIVEN
+    jest.spyOn(axios, "request").mockResolvedValue({ data: [{}] });
+
+    // WHEN
+    const result = await datasource.getTokenInfosPayload({ address: "0x00", chainId: 1 });
+
+    // THEN
+    expect(result).toEqual(undefined);
   });
 });
