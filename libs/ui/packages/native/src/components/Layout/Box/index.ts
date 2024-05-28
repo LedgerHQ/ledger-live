8d9dcc0539aa9e02c333ff3aa1fa8fd9853f4459
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2024-05-28 16:55:36 +0200

    fix: sample commit to test diff

--- libs/ui/packages/native/src/components/Layout/Box/index.ts
+++ libs/ui/packages/native/src/components/Layout/Box/index.ts
@@ -2,6 +2,7 @@ import baseStyled, { BaseStyledProps } from "../../styled";
 
 export type BoxProps = BaseStyledProps;
 
+console.log("hello");
 const Box = baseStyled.View<BaseStyledProps>``;
 
 export default Box;
