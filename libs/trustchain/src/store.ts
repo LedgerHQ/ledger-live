09a843d7d91b1c64d2244277b06711e5706656e1
Come Grellard <come.grellard@ledger.fr>
2024-05-29 17:17:02 +0200

    added store functions to manage trustchain state

--- libs/trustchain/src/store.ts
+++ libs/trustchain/src/store.ts
@@ -1,24 +1,69 @@
 /**
- * helpers to init and handle the trustchain store (what data are stored on instances)
+ * This exports all the logic related to the Trustchain store.
+ * The Trustchain store is a store that contains the data related to trustchain.
+ * It essentially is the ledger live's credentials that are only stored on the
+ * client side and the trustchain returned by the backend.
  */
-
 import { LiveCredentials, Trustchain } from "./types";
+import { sdk } from ".";
 
 export type TrustchainStore = {
   trustchain: Trustchain | null;
   liveCredentials: LiveCredentials | null;
 };
 
-export const initStore = (): TrustchainStore => {
+export const getInitialStore = (): TrustchainStore => {
   return {
     trustchain: null,
     liveCredentials: null,
   };
 };
 
-/**
- * TODO reducers with:
- * clean action (disable this instance locally)
- * init live credentials action
- * lenses to get the live credentials and trustchain
- */
+export enum WalletHandlerType {
+  RESET_TRUSTCHAIN_STORE = "RESET_TRUSTCHAIN_STORE",
+  SET_TRUSTCHAIN = "SET_TRUSTCHAIN",
+}
+
+export type HandlersPayloads = {
+  RESET_TRUSTCHAIN_STORE: {};
+  SET_TRUSTCHAIN: { trustchain: Trustchain };
+};
+
+type Handlers<State, Types, PreciseKey = true> = {
+  [Key in keyof Types]: (
+    state: State,
+    body: { payload: Types[PreciseKey extends true ? Key : keyof Types] },
+  ) => State;
+};
+
+export type WalletHandlers<PreciseKey = true> = Handlers<TrustchainStore, HandlersPayloads, PreciseKey>;
+
+export const handlers: WalletHandlers = {
+  RESET_TRUSTCHAIN_STORE: (): TrustchainStore => {
+    return { ...getInitialStore() };
+  },
+  SET_TRUSTCHAIN: (state, { payload: { trustchain } }) => {
+    return { ...state, trustchain };
+  },
+};
+
+// actions
+
+export const resetTrustchainStore = () => ({
+  type: "RESET_TRUSTCHAIN_STORE",
+});
+
+export const setTrustchain = (trustchain: Trustchain) => ({
+  type: "SET_TRUSTCHAIN",
+  payload: { trustchain },
+});
+
+// Local Selectors
+
+export const trustchainSelector = (
+  state: TrustchainStore,
+): Trustchain | null => state.trustchain;
+
+export const liveCredentialsSelector = (
+  state: TrustchainStore,
+): LiveCredentials | null => state.liveCredentials;
