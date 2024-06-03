b52edce7f893ce3af59ae897e1770953330b89f3
Maxime Aubanel <sshmaxime@gmail.com>
2024-06-03 10:51:52 +0200

    Add unsecured Error

---
+++ apps/ledger-live-desktop/src/newArch/WalletSync/components/UnsecuredError.tsx
@@ -0,0 +1,52 @@
+import { Box, Flex, Icons, Text } from "@ledgerhq/react-ui";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import styled, { useTheme } from "styled-components";
+import ButtonV3 from "~/renderer/components/ButtonV3";
+
+const Container = styled(Box)`
+  background-color: ${p => p.theme.colors.opacityDefault.c05};
+  border-radius: 100%;
+  height: 72px;
+  width: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const UnsecuredError = () => {
+  const tryAgain = () => console.log("try again");
+  const { t } = useTranslation();
+  const { colors } = useTheme();
+  return (
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100%"
+      rowGap="24px"
+      paddingX={50}
+    >
+      <Container>
+        <Icons.DeleteCircleFill size={"L"} color={colors.error.c60} />
+      </Container>
+      <Text fontSize={24} variant="h4Inter" color="neutral.c100" textAlign="center">
+        {t("walletSync.unsecuredError.title")}
+      </Text>
+
+      <Flex flexDirection="column" rowGap="16px">
+        <Text variant="bodyLineHeight" color="neutral.c70" textAlign="center" fontSize={14}>
+          {t("walletSync.unsecuredError.description")}
+        </Text>
+        <Text variant="bodyLineHeight" color="neutral.c70" textAlign="center" fontSize={14}>
+          {t("walletSync.unsecuredError.info")}
+        </Text>
+      </Flex>
+
+      <ButtonV3 variant="shade" onClick={tryAgain}>
+        {t("walletSync.unsecuredError.cta")}
+      </ButtonV3>
+      <ButtonV3 onClick={tryAgain}>{t("walletSync.unsecuredError.ctaDelete")}</ButtonV3>
+    </Flex>
+  );
+};
