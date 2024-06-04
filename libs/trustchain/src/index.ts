af94a85baed528661509fe4f94a394296ec56f8f
Come Grellard <come.grellard@ledger.fr>
2024-06-04 12:32:41 +0200

    removed direct export of the trustchain sdk, we now use the getSdk function

--- libs/trustchain/src/index.ts
+++ libs/trustchain/src/index.ts
@@ -2,5 +2,4 @@ import { getEnv } from "@ledgerhq/live-env";
 import { sdk as normalSdk } from "./sdk";
 import { mockSdk } from "./mockSdk";
 
-export const sdk = getEnv("MOCK") ? mockSdk : normalSdk;
 export const getSdk = () => getEnv("MOCK") ? mockSdk : normalSdk;
