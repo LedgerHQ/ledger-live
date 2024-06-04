1e20d39a297b3b232d4d97c05b2df1694a23f5ff
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-06-04 10:28:55 +0200

    Fix tests

--- apps/ledger-live-desktop/tests/jestSetup.ts
+++ apps/ledger-live-desktop/tests/jestSetup.ts
@@ -19,32 +19,9 @@ jest.mock("src/sentry/install", () => ({
   setTags: jest.fn(),
 }));
 
-class Worker {
-  url: string;
-  onmessage: (msg: string) => void;
-  constructor(stringUrl: string) {
-    this.url = stringUrl;
-    this.onmessage = jest.fn();
-  }
-  postMessage(msg: string): void {
-    this.onmessage(msg);
-  }
-}
-
-Object.defineProperty(window, "Worker", {
-  writable: true,
-  value: Worker,
-});
-
 global.TextEncoder = TextEncoder;
 // @ts-expect-error weird compatibility
 global.TextDecoder = TextDecoder;
 global.setImmediate =
   global.setImmediate ||
   ((fn: (...args: any[]) => void, ...args: any) => global.setTimeout(fn, 0, ...args));
-
-const analytics = {
-  identify: jest.fn(),
-};
-
-global.window.analytics = analytics;
