1d7b4d5bbf7eabc085cab39542b1a227a67de4d1
Victor Alber <victor.alber@ledger.fr>
2024-06-06 10:12:09 +0200

    Xray with Json

--- apps/ledger-live-desktop/tests/utils/global-teardown.ts
+++ apps/ledger-live-desktop/tests/utils/global-teardown.ts
@@ -1,5 +1,7 @@
 export default async function globalTeardown() {
-  if (process.env.CI) {
-    //Appeler l'export vers XRAY
+  if (process.env.CI && process.env.XRAY === 1) {
+    //Appeler l'export vers XRAY (publish to xray)
+    // XRAY-REPORTER
+    //CE Fichier = CustomTestExecutionListener.java du Vault
   }
 }
