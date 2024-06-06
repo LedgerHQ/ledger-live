9602e8350266e3bd5a0fd57d4d128694fcb8706b
Louis PAQUET <louis.paquet@ledger.fr>
2024-06-06 14:22:46 +0200

    feat(LIVE-12808): remove targetCurrencyId and currenyCurrencyId that are sending wrong data

--- apps/ledger-live-mobile/src/screens/Swap/Form/Modal/Confirmation.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/Form/Modal/Confirmation.tsx
@@ -109,6 +109,8 @@ export function Confirmation({
           swapStep: "SIGN_COIN_TRANSACTION",
           statusCode: "DISABLE_TRANSACTION_BROADCAST",
           errorMessage: "DISABLE_TRANSACTION_BROADCAST",
+          sourceCurrencyId: undefined,
+          targetCurrencyId: undefined,
           hardwareWalletType: deviceMeta.device.modelId,
           swapType: exchangeRate.current.tradeMethod,
         });
@@ -117,6 +119,8 @@ export function Confirmation({
           provider,
           swapId,
           transactionId: operation.hash,
+          sourceCurrencyId: undefined,
+          targetCurrencyId: undefined,
           hardwareWalletType: deviceMeta.device.modelId,
           swapType: exchangeRate.current.tradeMethod,
         });
