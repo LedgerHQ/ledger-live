06627bcfeb278d349342e8c9ab7af322d7bf41ba
Prateek Rathod <prateek@zondax.ch>
2024-06-10 01:26:09 +0200

    refac(mina): using new conventions

--- libs/coin-modules/coin-mina/src/estimateMaxSpendable.ts
+++ libs/coin-modules/coin-mina/src/estimateMaxSpendable.ts
@@ -1,30 +1,23 @@
 import { BigNumber } from "bignumber.js";
-import type { AccountLike, Account } from "@ledgerhq/types-live";
+import type { AccountBridge } from "@ledgerhq/types-live";
 import type { Transaction, MinaAccount } from "./types";
 import { getMainAccount } from "@ledgerhq/coin-framework/account/index";
-import { createTransaction } from "./js-transaction";
-import getEstimatedFees from "./js-getFeesForTransaction";
 import { getMaxAmount } from "./logic";
+import { createTransaction } from "./createTransaction";
+import getEstimatedFees from "./getFeesForTransaction";
 
-const estimateMaxSpendable = async ({
-  account,
-  parentAccount,
-  transaction,
-  calculatedFees,
-}: {
-  account: AccountLike;
-  parentAccount?: Account | null;
-  transaction?: Transaction | null;
-  calculatedFees?: BigNumber;
-}): Promise<BigNumber> => {
+const estimateMaxSpendable: AccountBridge<
+  Transaction,
+  MinaAccount
+>["estimateMaxSpendable"] = async ({ account, parentAccount, transaction }): Promise<BigNumber> => {
   const a = getMainAccount(account, parentAccount) as MinaAccount;
   const t = {
-    ...createTransaction(),
+    ...createTransaction(account),
     ...transaction,
     amount: a.spendableBalance,
   };
 
-  const fees = calculatedFees ?? (await getEstimatedFees(t));
+  const fees = await getEstimatedFees(t);
 
   const maxSpendable = getMaxAmount(a, t, fees);
 
