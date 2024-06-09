c35a48a9cedcac495cbb223b48fbf6fa9f27a001
Prateek Rathod <prateek@zondax.ch>
2024-06-10 01:26:09 +0200

    refac(mina): using new conventions

--- libs/coin-modules/coin-mina/src/bridge/js.ts
+++ libs/coin-modules/coin-mina/src/bridge/js.ts
@@ -8,14 +8,15 @@ import { SignerContext } from "@ledgerhq/coin-framework/signer";
 import getAddressWrapper from "@ledgerhq/coin-framework/bridge/getAddressWrapper";
 import type { Transaction } from "../types";
 import resolver from "../hw-getAddress";
-import { sync, getAccountShape } from "../js-synchronisation";
-import { createTransaction, prepareTransaction } from "../js-transaction";
-import getTransactionStatus from "../js-getTransactionStatus";
-import { buildSignOperation } from "../js-signOperation";
-import broadcast from "../js-broadcast";
-import estimateMaxSpendable from "../js-estimateMaxSpendable";
+import { sync, getAccountShape } from "../synchronisation";
 import { MinaSigner } from "../signer";
 import { MinaCoinConfig, setCoinConfig } from "../config";
+import broadcast from "../broadcast";
+import { createTransaction } from "../createTransaction";
+import estimateMaxSpendable from "../estimateMaxSpendable";
+import getTransactionStatus from "../getTransactionStatus";
+import { prepareTransaction } from "../prepareTransaction";
+import buildSignOperation from "../signOperation";
 
 export function buildCurrencyBridge(signerContext: SignerContext<MinaSigner>): CurrencyBridge {
   const getAddress = resolver(signerContext);
