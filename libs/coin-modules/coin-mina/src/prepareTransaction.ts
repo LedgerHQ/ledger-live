405de69a4a8ef29d4282aa365608c6a5041fbf29
Prateek Rathod <prateek@zondax.ch>
2024-06-10 01:26:09 +0200

    refac(mina): using new conventions

---
+++ libs/coin-modules/coin-mina/src/prepareTransaction.ts
@@ -0,0 +1,21 @@
+import { defaultUpdateTransaction } from "@ledgerhq/coin-framework/lib/bridge/jsHelpers";
+import { Account, AccountBridge } from "@ledgerhq/types-live";
+import estimateMaxSpendable from "./estimateMaxSpendable";
+import getEstimatedFees from "./getFeesForTransaction";
+import { MinaAccount, Transaction } from "./types";
+
+export const prepareTransaction: AccountBridge<
+  Transaction,
+  MinaAccount
+>["prepareTransaction"] = async (a: Account, t: Transaction) => {
+  const fees = await getEstimatedFees(t);
+
+  const amount = t.useAllAmount
+    ? await estimateMaxSpendable({
+        account: a,
+        transaction: t,
+      })
+    : t.amount;
+
+  return defaultUpdateTransaction(t, { fees, amount });
+};
