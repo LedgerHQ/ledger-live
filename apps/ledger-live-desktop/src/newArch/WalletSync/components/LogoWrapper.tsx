f11f1be3205e6ec6cc3fa5922232b5183ad07a4d
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2024-06-11 15:59:23 +0200

    💄: Rework Icons on WelcomeScreen on WalletSync (#7062)

--- apps/ledger-live-desktop/src/newArch/WalletSync/components/LogoWrapper.tsx
+++ apps/ledger-live-desktop/src/newArch/WalletSync/components/LogoWrapper.tsx
@@ -1,19 +1,30 @@
 import { Flex } from "@ledgerhq/react-ui";
 import React, { PropsWithChildren } from "react";
-import { useTheme } from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { rgba } from "~/renderer/styles/helpers";
 
 export const LogoWrapper = ({
   children,
   opacity = "70%",
-}: PropsWithChildren & { opacity?: string }) => {
+  deg = 0,
+}: PropsWithChildren & { opacity?: string; deg?: number }) => {
   const { colors } = useTheme();
   const bg = rgba(colors.primary.c80, 0.08);
   return (
-    <Flex padding="7px" borderRadius="13px" border={`1px solid ${colors.opacityDefault.c05}`}>
+    <GradientDiv padding="7px" borderRadius="13px" deg={deg}>
       <Flex borderRadius="9px" backgroundColor={bg} padding="5px" opacity={opacity}>
         {children}
       </Flex>
-    </Flex>
+    </GradientDiv>
   );
 };
+
+const GradientDiv = styled(Flex).attrs<{
+  deg?: number;
+}>({})<{ deg: number }>`
+  background: linear-gradient(
+    ${props => props.deg || 0}deg,
+    ${props => props.theme.colors.background.drawer} 5%,
+    ${props => props.theme.colors.opacityDefault.c05} 95%
+  );
+`;
