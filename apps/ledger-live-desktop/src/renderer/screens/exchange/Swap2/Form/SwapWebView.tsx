d9d884563a5bee4e244dafb88d6e3d9a558f03ab
Louis PAQUET <louis.paquet@ledger.fr>
2024-06-13 16:05:36 +0200

    feat(LIVE-12808): changeset

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/SwapWebView.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/SwapWebView.tsx
@@ -304,6 +304,7 @@ const SwapWebView = ({
   }
 
   const onSwapWebviewError = (error?: SwapLiveError) => {
+    console.log('%capps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/SwapWebView.tsx:248 ', 'color: #007acc;');
     console.error("onSwapWebviewError", error);
     setDrawer(WebviewErrorDrawer, error);
   };
