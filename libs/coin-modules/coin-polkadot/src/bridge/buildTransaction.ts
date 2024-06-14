4b7f19c96d95d86d5b6fbb480032d77532bf755e
Hedi EDELBLOUTE <hedi.edelbloute@ledger.fr>
2024-06-14 08:17:17 +0200

    Polkadot generic app support (#6326)
    * getAddress support
    
    * fix: add metadata support
    
    * fix: get polkadot app version
    
    * fix: refactoring
    
    * fix: refactoring
    
    * fix: refactoring
    
    * fix: refactoring
    
    * fix: small refactoring
    
    * fix: update doc
    
    * feat: add Polkadot coin configuration
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    
    * polkadot config location
    
    * fix: polkadot transaction craft
    
    * remove useless code
    
    * fix: refactoring
    
    * fix: runtime upgrade transaction
    
    * fix: update doc
    
    * fix: unit tests
    
    * fix: update changelog
    
    * fix: update pnpm-lock
    
    * fix: update polkadot integration test APDU
    
    ---------
    
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    Co-authored-by: hzheng-ledger <huiqi.zheng@ledger.fr>
    Co-authored-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
    Co-authored-by: lvndry <lvndry@proton.me>

--- libs/coin-modules/coin-polkadot/src/bridge/buildTransaction.ts
+++ libs/coin-modules/coin-polkadot/src/bridge/buildTransaction.ts
@@ -1,6 +1,7 @@
 import type { PolkadotAccount, Transaction } from "../types";
 import { craftTransaction, type CreateExtrinsicArg } from "../logic";
 import { isFirstBond, getNonce } from "./utils";
+import { getCoinConfig } from "../config";
 
 export const extractExtrinsicArg = (
   account: PolkadotAccount,
@@ -28,10 +29,12 @@ export const buildTransaction = async (
   transaction: Transaction,
   forceLatestParams = false,
 ) => {
+  const runtimeUpgraded = getCoinConfig().runtimeUpgraded;
   return craftTransaction(
     account.freshAddress,
     getNonce(account),
     extractExtrinsicArg(account, transaction),
     forceLatestParams,
+    runtimeUpgraded,
   );
 };
--- libs/ledger-live-common/src/families/polkadot/config.ts
+++ libs/ledger-live-common/src/families/polkadot/config.ts
@@ -1,4 +1,5 @@
 import { ConfigInfo } from "@ledgerhq/live-config/LiveConfig";
+import { getEnv } from "@ledgerhq/live-env";
 
 export const polkadotConfig: Record<string, ConfigInfo> = {
   config_currency_polkadot: {
@@ -7,6 +8,20 @@ export const polkadotConfig: Record<string, ConfigInfo> = {
       status: {
         type: "active",
       },
+      sidecar: {
+        url: getEnv("API_POLKADOT_SIDECAR"),
+        credentials: getEnv("API_POLKADOT_SIDECAR_CREDENTIALS"),
+      },
+      staking: {
+        electionStatusThreshold: getEnv("POLKADOT_ELECTION_STATUS_THRESHOLD"),
+      },
+      metadataShortener: {
+        url: "https://api.zondax.ch/polkadot/transaction/metadata",
+      },
+      metadataHash: {
+        url: "https://api.zondax.ch/polkadot/node/metadata/hash",
+      },
+      runtimeUpgraded: false,
     },
   },
 };
