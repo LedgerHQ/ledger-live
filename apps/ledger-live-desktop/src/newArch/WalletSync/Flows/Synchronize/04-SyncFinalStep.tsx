6a5c86e283908a71415900176fb72b25caa4efa6
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-06-17 14:31:03 +0200

    💄 Fix Final step display in Sync

--- apps/ledger-live-desktop/src/newArch/WalletSync/Flows/Synchronize/04-SyncFinalStep.tsx
+++ apps/ledger-live-desktop/src/newArch/WalletSync/Flows/Synchronize/04-SyncFinalStep.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-//import { Error } from "../../components/Error";
 import { Success } from "../../components/Success";
 import { useTranslation } from "react-i18next";
-import { Flex } from "@ledgerhq/react-ui";
 
 export default function SyncFinalStep() {
   const { t } = useTranslation();
