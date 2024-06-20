ebcdefcedd6c8ac247a83cb38d9e7bfccf146515
lvndry <lvndry@proton.me>
2024-06-20 15:58:27 +0200

    coin polkadot support ws node

--- libs/coin-modules/coin-polkadot/src/network/node/apiPromise.ts
+++ libs/coin-modules/coin-polkadot/src/network/node/apiPromise.ts
@@ -1,8 +1,9 @@
-import { ApiPromise, HttpProvider } from "@polkadot/api";
+import { ApiPromise, HttpProvider, WsProvider } from "@polkadot/api";
 import { getCoinConfig, PolkadotCoinConfig } from "../../config";
 
 let coinConfig: PolkadotCoinConfig | undefined;
 let api: ApiPromise | undefined;
+
 export default async function () {
   const config = getCoinConfig();
   // Need to constantly check if a new config is setted
@@ -11,10 +12,22 @@ export default async function () {
     const headers = coinConfig.node.credentials
       ? { Authorization: "Basic " + coinConfig.node.credentials }
       : undefined;
-    api = await ApiPromise.create({
-      provider: new HttpProvider(coinConfig.node.url, headers),
-      noInitWarn: true, //to avoid undesired warning (ex: "API/INIT: polkadot/1002000: Not decorating unknown runtime apis")
-    });
+
+    const nodeURL = coinConfig.node.url;
+
+    if (nodeURL.startsWith("ws://") || nodeURL.startsWith("wss://")) {
+      api = await ApiPromise.create({
+        provider: new WsProvider(nodeURL),
+        noInitWarn: true,
+      });
+    } else if (nodeURL.startsWith("http://") || nodeURL.startsWith("https://")) {
+      api = await ApiPromise.create({
+        provider: new HttpProvider(coinConfig.node.url, headers),
+        noInitWarn: true, //to avoid undesired warning (ex: "API/INIT: polkadot/1002000: Not decorating unknown runtime apis")
+      });
+    } else {
+      throw new Error("[Polkadot] Invalid node URL");
+    }
   }
 
   return api;
