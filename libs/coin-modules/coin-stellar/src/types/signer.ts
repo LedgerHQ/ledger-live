106417d8039346704bd0e40ce56eedda43bab89d
lvndry <lvndry@proton.me>
2024-06-26 15:43:41 +0200

    delete hw-app-str dependency

--- libs/coin-modules/coin-stellar/src/types/signer.ts
+++ libs/coin-modules/coin-stellar/src/types/signer.ts
@@ -1,3 +1,26 @@
-import type Stellar from "@ledgerhq/hw-app-str";
-
-export interface StellarSigner extends Stellar {}
+export interface StellarSigner {
+  getAppConfiguration(): Promise<{
+    version: string;
+    hashSigningEnabled: boolean;
+    maxDataSize?: number;
+  }>;
+  getPublicKey(path: string, display?: boolean): Promise<{ rawPublicKey: Buffer }>;
+  signTransaction(
+    path: string,
+    transaction: Buffer,
+  ): Promise<{
+    signature: Buffer;
+  }>;
+  signSorobanAuthorization(
+    path: string,
+    hashIdPreimage: Buffer,
+  ): Promise<{
+    signature: Buffer;
+  }>;
+  signHash(
+    path: string,
+    hash: Buffer,
+  ): Promise<{
+    signature: Buffer;
+  }>;
+}
