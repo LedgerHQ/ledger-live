bb496284261b99d54fef1f1c6105a629636f749b
heirland <adam.heirland@gmail.com>
2024-06-28 00:33:30 +0700

    test: update unit test for getTransactionStatus.ts

--- libs/coin-modules/coin-icon/jest.config.js
+++ libs/coin-modules/coin-icon/jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["lib/", "lib-es/"],
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
 };
