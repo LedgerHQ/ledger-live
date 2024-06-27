05fa46524515ca7f224121ea84b1bdc59f712151
lvndry <lvndry@proton.me>
2024-06-27 15:23:40 +0200

    delete json provider

--- libs/live-config/src/providers/json/parser.ts
+++
@@ -1,9 +0,0 @@
-export function parser(value: any, type: string) {
-  if (type === "number") {
-    return Number(value);
-  } else if (type === "boolean") {
-    return Boolean(value);
-  } else {
-    return value;
-  }
-}
