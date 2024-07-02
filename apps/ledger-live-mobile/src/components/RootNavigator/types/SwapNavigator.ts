7ef11946556300be46c96242967ecb8411d459db
Prateek Rathod <prateek@zondax.ch>
2024-07-02 14:11:44 +0200

    feat(mina): add support for llm

--- apps/ledger-live-mobile/src/components/RootNavigator/types/SwapNavigator.ts
+++ apps/ledger-live-mobile/src/components/RootNavigator/types/SwapNavigator.ts
@@ -34,6 +34,7 @@ import {
 import { Transaction as HederaTransaction } from "@ledgerhq/live-common/families/hedera/types";
 import type { Transaction as RippleTransaction } from "@ledgerhq/live-common/families/xrp/types";
 import type { Transaction as ICPTransaction } from "@ledgerhq/live-common/families/internet_computer/types";
+import type { Transaction as MinaTransaction } from "@ledgerhq/live-common/families/mina/types";
 import type { Transaction as StellarTransaction } from "@ledgerhq/live-common/families/stellar/types";
 import type { Transaction as StacksTransaction } from "@ledgerhq/live-common/families/stacks/types";
 import type { Transaction as CasperTransaction } from "@ledgerhq/live-common/families/casper/types";
@@ -267,6 +268,13 @@ export type SwapNavigatorParamList = {
     account: Account;
     parentId?: string;
     transaction: ICPTransaction;
+    currentNavigation: ScreenName.SignTransactionSummary | ScreenName.SignTransactionSummary;
+  };
+  [ScreenName.MinaEditMemo]: {
+    accountId: string;
+    account: Account;
+    parentId?: string;
+    transaction: MinaTransaction;
     currentNavigation: ScreenName.SignTransactionSummary | ScreenName.SignTransactionSummary;
   };
 
