0d3851b5be71d294f46d23d6cbc4130ccd5a7f3d
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-07-03 10:16:39 +0200

    fix: remove live-env from test
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-modules/coin-tezos/src/bridge/synchronization.integ.test.ts
+++ libs/coin-modules/coin-tezos/src/bridge/synchronization.integ.test.ts
@@ -1,4 +1,4 @@
-import { getEnv, setEnv } from "@ledgerhq/live-env";
+import coinConfig, { TezosCoinConfig } from "../config";
 import { fetchAllTransactions } from "../network/tzkt";
 
 jest.setTimeout(2 * 60 * 1000);
@@ -6,17 +6,45 @@ jest.setTimeout(2 * 60 * 1000);
 describe("TEZOS_MAX_TX_QUERIES", () => {
   const bigAccount = "tz1boBHAVpwcvKkNFAQHYr7mjxAz1PpVgKq7";
   test("default have more than 100 txs", async () => {
+    // Given
+    coinConfig.setCoinConfig(
+      (): TezosCoinConfig => ({
+        status: { type: "active" },
+        baker: {
+          url: "https://tezos-bakers.api.live.ledger.com",
+        },
+        explorer: {
+          url: "https://xtz-tzkt-explorer.api.live.ledger.com",
+          maxTxQuery: 100,
+        },
+        node: {
+          url: "https://xtz-node.api.live.ledger.com",
+        },
+      }),
+    );
+
     const txs = await fetchAllTransactions(bigAccount);
     expect(txs.length).toBeGreaterThan(100);
   });
   test("lowering it to 1 will only fetch a few txs", async () => {
-    const cur = getEnv("TEZOS_MAX_TX_QUERIES");
-    setEnv("TEZOS_MAX_TX_QUERIES", 1);
-    try {
-      const txs = await fetchAllTransactions(bigAccount);
-      expect(txs.length).toBeLessThanOrEqual(100);
-    } finally {
-      setEnv("TEZOS_MAX_TX_QUERIES", cur);
-    }
+    // Given
+    coinConfig.setCoinConfig(
+      (): TezosCoinConfig => ({
+        status: { type: "active" },
+        baker: {
+          url: "https://tezos-bakers.api.live.ledger.com",
+        },
+        explorer: {
+          url: "https://xtz-tzkt-explorer.api.live.ledger.com",
+          maxTxQuery: 1,
+        },
+        node: {
+          url: "https://xtz-node.api.live.ledger.com",
+        },
+      }),
+    );
+
+    const txs = await fetchAllTransactions(bigAccount);
+    expect(txs.length).toBeLessThanOrEqual(100);
   });
 });
