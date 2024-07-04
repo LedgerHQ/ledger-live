ccac0f414805eb89c0654dc40f2d5736211bee61
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2024-07-04 14:29:15 +0200

    chore(trustchain): simplify test folders

--- libs/trustchain/src/__tests__/sdk/membersManySelfAdd.ts
+++
@@ -1,4 +0,0 @@
-import json from "./membersManySelfAdd.json";
-import { replayTrustchainSdkTests } from "../../test-helpers/replayTrustchainSdkTests";
-import { scenario } from "../../test-scenarios/membersManySelfAdd";
-replayTrustchainSdkTests(json, scenario);
