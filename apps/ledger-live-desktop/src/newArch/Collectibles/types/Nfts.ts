5f4e68dc373ac4e31ecd52ef77f435a931504e4c
Lucas Werey <73439207+LucasWerey@users.noreply.github.com>
2024-07-05 16:12:04 +0200

    :boom:feat(lld): nft breadcrumb (#7249)
    feat(lld): refactor of breadcrumb for nft gallery

--- apps/ledger-live-desktop/src/newArch/Collectibles/types/Nfts.ts
+++ apps/ledger-live-desktop/src/newArch/Collectibles/types/Nfts.ts
@@ -1,4 +1,5 @@
-import { Account } from "@ledgerhq/types-live";
+import { Account, ProtoNFT } from "@ledgerhq/types-live";
+import { DropDownItemType } from "~/renderer/components/DropDownSelector";
 
 export type NftsDetailDrawerProps = {
   account: Account;
@@ -18,4 +19,12 @@ export type NftComponentData = {
   isLoading: boolean;
   setUseFallback: () => void;
   closeCollectiblesPanAndZoom: () => void;
+};
+
+export type NftBreadcrumbProps = {
+  activeItem: DropDownItemType<ProtoNFT>;
+  items: DropDownItemType<ProtoNFT>[];
+  collectionAddress: string | undefined;
+  onCollectionSelected: (item: DropDownItemType<ProtoNFT>) => void;
+  onSeeAll: () => void;
 };
