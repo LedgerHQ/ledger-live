7946a5adda0c2ec8a831e2b6fc60d7444e405eea
Thomas Brillard <thomas.brillard@ledger.fr>
2024-07-05 10:58:11 +0200

    chore: remarks

--- libs/ledger-live-common/src/exchange/swap/maybeTronEmptyAccount.ts
+++ libs/ledger-live-common/src/exchange/swap/maybeTronEmptyAccount.ts
@@ -6,7 +6,7 @@ export const maybeTronEmptyAccount = (swapTransaction: SwapTransactionType): Err
     swapTransaction?.swap?.to?.currency?.tokenType === "trc20" &&
     swapTransaction?.swap?.to?.parentAccount?.balance.lt(100_000)
   ) {
-    return new TronEmptyAccount(undefined, {
+    return new TronEmptyAccount("PREVENT_RECEIVING_TRC20_ON_EMPTY_ACCOUNT", {
       links: [
         "https://support.ledger.com/hc/en-us/articles/6516823445533-Activate-Tron-account-to-send-or-receive-Tron-tokens?support=true",
       ],
