ae89ddd30a01281e33f4b71466aeb0fc89e60ea9
Prateek Rathod <prateek@zondax.ch>
2024-07-08 13:02:35 +0200

    refac(kda): use updated `hw-app-kda`

--- libs/ledger-live-common/src/families/kadena/setup.ts
+++ libs/ledger-live-common/src/families/kadena/setup.ts
@@ -4,7 +4,7 @@ import { createBridges } from "@ledgerhq/coin-kadena/bridge/js";
 import makeCliTools from "@ledgerhq/coin-kadena/cli-transaction";
 import kadenaResolver from "@ledgerhq/coin-kadena/hw-getAddress";
 import { Transaction } from "@ledgerhq/coin-kadena/types";
-import Kadena from "hw-app-kda";
+import Kadena from "@zondax/hw-app-kda";
 import Transport from "@ledgerhq/hw-transport";
 import type { Bridge } from "@ledgerhq/types-live";
 import { CreateSigner, createResolver, executeWithSigner } from "../../bridge/setup";
