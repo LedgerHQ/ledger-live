0b450545cfb0a411e52a318eaae0ffa1bb577998
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-07-09 09:22:48 +0200

    :green_heart: (llm): CI script replace assets

---
+++ apps/ledger-live-mobile/scripts/replace-assets.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env zx
+import "zx/globals";
+import fs from "fs";
+import path from "path";
+
+const rootPath = path.join(__dirname, "..", "..", "..");
+const basePath = path.join(__dirname, "..");
+const replaceAssetsPath = path.join(rootPath, "ledger-live-internal-assets", "apps", "ledger-live-mobile");
+
+async function replaceAssets(){
+  await fs.promises.cp(replaceAssetsPath, basePath, { recursive: true, force: true});
+}
+
+await replaceAssets();
