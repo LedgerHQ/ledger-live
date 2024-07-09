43f391a7b79117077c1bc4020095bb1119487ab7
Yaroslava Polishchuk <yaroslava.polishchuk@ledger.fr>
2024-07-09 15:45:40 +0200

    Removing appendFileErrorHandler mathod

--- apps/ledger-live-desktop/tests/utils/fileUtils.ts
+++ apps/ledger-live-desktop/tests/utils/fileUtils.ts
@@ -5,14 +5,10 @@ export function generateUUID(): string {
   return crypto.randomBytes(16).toString("hex");
 }
 
-export function appendFileErrorHandler(e: Error | null) {
-  if (e) console.error("couldn't append file", e);
-}
-
 export async function safeAppendFile(filePath: string, data: string) {
   try {
     await appendFile(filePath, data);
   } catch (e: any) {
-    appendFileErrorHandler(e);
+    if (e) console.error("couldn't append file", e);
   }
 }
