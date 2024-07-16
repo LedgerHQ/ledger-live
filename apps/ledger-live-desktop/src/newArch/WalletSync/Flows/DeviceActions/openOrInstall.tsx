584d20f69eccb1e25f4de3310fae210423869e0b
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2024-07-16 14:27:21 +0200

    [CHORE] : Update TRUSTCHAIN_APP_NAME on both platforms (#7338)
    * Add FollowInstruction Drawer
    
    * ActivationProcess
    
    * Rework Success arch
    
    * Changeset
    
    * Integration test for ActivationProcess
    
    * Improve Activation Test
    
    * [CHORE] : Update TRUSTCHAIN_APP_NAME on both platforms

--- apps/ledger-live-desktop/src/newArch/WalletSync/Flows/DeviceActions/openOrInstall.tsx
+++ apps/ledger-live-desktop/src/newArch/WalletSync/Flows/DeviceActions/openOrInstall.tsx
@@ -5,8 +5,7 @@ import { mockedEventEmitter } from "~/renderer/components/debug/DebugMock";
 import connectApp from "@ledgerhq/live-common/hw/connectApp";
 import { Device } from "@ledgerhq/live-common/hw/actions/types";
 import { createAction } from "@ledgerhq/live-common/hw/actions/app";
-
-const APP_NAME = "Ledger Sync";
+import { TRUSTCHAIN_APP_NAME } from "@ledgerhq/hw-trustchain";
 
 const action = createAction(getEnv("MOCK") ? mockedEventEmitter : connectApp);
 
@@ -15,7 +14,7 @@ type Props = {
 };
 
 export default function OpenOrInstallTrustChainApp({ goNext }: Props) {
-  const request = { appName: APP_NAME };
+  const request = { appName: TRUSTCHAIN_APP_NAME };
   return (
     <DeviceAction action={action} request={request} onResult={({ device }) => goNext(device)} />
   );
