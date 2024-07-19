8f4dab5df01f0c80d514f27ad173d1695e1a0bf7
hzheng-ledger <huiqi.zheng@ledger.fr>
2024-07-19 14:25:11 +0200

    fix: unit test

--- libs/coin-modules/coin-polkadot/src/network/sidecar.ts
+++ libs/coin-modules/coin-polkadot/src/network/sidecar.ts
@@ -15,7 +15,6 @@ import type {
   SidecarAccountBalanceInfo,
   SidecarPalletStorageItem,
   SidecarStakingInfo,
-  SidecarNominations,
   SidecarValidatorsParamStatus,
   SidecarValidatorsParamAddresses,
   SidecarValidators,
@@ -126,18 +125,6 @@ const fetchStakingInfo = async (addr: string): Promise<SidecarStakingInfo> => {
     data: SidecarStakingInfo;
   } = await callSidecar(`/accounts/${addr}/staking-info`);
   return data;
-};
-
-/**
- * Returns the list of nominations for an account, with status and associated stake if relevant.
- *
- * @async
- * @param {string} addr
- *
- * @returns {SidecarNominations}
- */
-const fetchNominations = async (addr: string): Promise<SidecarNominations> => {
-  return node.fetchNominations(addr);
 };
 
 /**
@@ -445,7 +432,7 @@ export const getStakingInfo = async (addr: string) => {
  * @returns {PolkadotNomination[}
  */
 const getNominations = async (addr: string): Promise<PolkadotNomination[]> => {
-  const nominations = await fetchNominations(addr);
+  const nominations = await node.fetchNominations(addr);
 
   if (!nominations) {
     return [];
