2630e9b34caef6ffe3601ed960156d8650314457
chrisduma-ledger <cristian.duma-ext@ledger.fr>
2024-07-22 13:47:42 +0300

    fix: code review

--- apps/ledger-live-mobile/src/screens/CustomError/index.tsx
+++ apps/ledger-live-mobile/src/screens/CustomError/index.tsx
@@ -2,11 +2,11 @@ import { Flex, Text } from "@ledgerhq/native-ui";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import ErrorSvg from "./ErrorSvg";
 import { StackNavigatorProps } from "~/components/RootNavigator/types/helpers";
 import { CustomErrorNavigatorParamList } from "~/components/RootNavigator/types/CustomErrorNavigator";
 import { ScreenName } from "~/const";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
+import { Icons } from "@ledgerhq/native-ui";
 
 type CustomErrorPropsProps = StackNavigatorProps<
   CustomErrorNavigatorParamList,
@@ -20,14 +20,18 @@ export default function CustomError({ route }: CustomErrorPropsProps) {
   return (
     <SafeAreaView style={styles.root}>
       <Flex justifyContent="center" alignItems="center" flex={1}>
-        <ErrorSvg />
+        <Icons.Close color="red" size="L" />
         <Text variant="h3Inter" fontWeight="bold" fontSize={25} textAlign={"center"}>
           {t("errors.CustomError.title")}
           {"\n"}
         </Text>
         <Text variant="body" textAlign={"center"}>
           {t("errors.CustomError.description")}
-          {`Code: ${error?.cause.swapCode}`}
+
+          <Trans
+            i18nKey="errors.CustomError.errorCode"
+            values={{ errorCode: error?.cause.swapCode }}
+          />
         </Text>
       </Flex>
     </SafeAreaView>
