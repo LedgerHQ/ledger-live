18c94d8e6f98e6ab3b50d696702333eae22ec093
Liviu Ciulinaru <liviuciulinaru@users.noreply.github.com>
2024-07-31 11:30:47 +0300

    fix(live-13499): get counterValue from swap-live-app in demo1 (#7460)
    * fix: demo1 get counterValue from swap-live-app
    
    * chore: add changeset
    
    * chore: clean

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/index.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/index.tsx
@@ -467,6 +467,7 @@ const SwapForm = () => {
   const [quoteState, setQuoteState] = useSwapLiveAppQuoteState({
     amountTo: exchangeRate?.toAmount,
     swapError,
+    counterValue: undefined,
   });
 
   return (
@@ -484,6 +485,7 @@ const SwapForm = () => {
         isMaxEnabled={swapTransaction.swap.isMaxEnabled}
         toggleMax={toggleMax}
         fromAmountError={!isDemo1Enabled ? swapError : quoteState.swapError}
+        counterValue={isDemo1Enabled ? quoteState.counterValue : undefined}
         fromAmountWarning={swapWarning}
         isSwapReversable={swapTransaction.swap.isSwapReversable}
         reverseSwap={reverseSwap}
