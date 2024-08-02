38597743bfc3301b86609b582a661329eeeedd08
Theophile Sandoz <Theophile Sandoz>
2024-08-02 16:35:05 +0200

    Test remove member

--- libs/trustchain/src/__tests__/unit/sdk.test.ts
+++ libs/trustchain/src/__tests__/unit/sdk.test.ts
@@ -1,8 +1,10 @@
 import { http, HttpResponse } from "msw";
 import { setupServer, SetupServerApi } from "msw/node";
-import { StreamTree } from "@ledgerhq/hw-trustchain";
+import { crypto, Device, Permissions, SoftwareDevice, StreamTree } from "@ledgerhq/hw-trustchain";
+import { getEnv } from "@ledgerhq/live-env";
+import { PutCommandsRequest } from "../../api";
 import { HWDeviceProvider } from "../../HWDeviceProvider";
-import { SDK } from "../../sdk";
+import { convertLiveCredentialsToKeyPair, SDK } from "../../sdk";
 import { TrustchainResultType } from "../../types";
 
 describe("Trustchain SDK", () => {
@@ -13,88 +15,168 @@ describe("Trustchain SDK", () => {
     clearJwt: jest.fn(),
   } satisfies Partial<HWDeviceProvider>;
 
+  const deviceCallbacks = {
+    onStartRequestUserInteraction: jest.fn(),
+    onEndRequestUserInteraction: jest.fn(),
+  };
+
   const hwDeviceProviderMock = HWDeviceProviderMethodsMocks as unknown as HWDeviceProvider;
 
   let mswServer: SetupServerApi | undefined;
 
+  const apiBaseUrl = getEnv("TRUSTCHAIN_API_STAGING");
+  const sdkContext = { applicationId: 16, name: "Foo", apiBaseUrl };
+
   beforeEach(() => {
     mswServer?.close();
-    HWDeviceProviderMethodsMocks.withJwt.mockClear();
     HWDeviceProviderMethodsMocks.withHw.mockClear();
     HWDeviceProviderMethodsMocks.refreshJwt.mockClear();
     HWDeviceProviderMethodsMocks.clearJwt.mockClear();
+    HWDeviceProviderMethodsMocks.withJwt
+      .mockClear()
+      .mockImplementation(async (deviceId, job) => job({ accessToken: "ACCESS TOKEN" }));
+    deviceCallbacks.onStartRequestUserInteraction.mockClear();
+    deviceCallbacks.onEndRequestUserInteraction.mockClear();
   });
 
   it("should create Trustchain", async () => {
-    const callbacks = {
-      onStartRequestUserInteraction: jest.fn(),
-      onEndRequestUserInteraction: jest.fn(),
-    };
+    // Mock trustchain states:
+    const device = new SoftwareDevice(convertLiveCredentialsToKeyPair(MOCK_DATA.members.alice));
+    const initialTree = await createTrustChain(device);
+    const oneMemberTree = await addMember(device, "m/0'/16'/0'", "alice")(initialTree);
 
-    HWDeviceProviderMethodsMocks.withJwt.mockImplementation(async (transport, job) =>
-      job({ accessToken: "ACCESS TOKEN" }),
-    );
-
+    // Mock API calls:
     const reqCount = { trustchains: 0 };
     mswServer = setupServer(
       http.get("*/v1/trustchains", () => {
         switch (reqCount.trustchains++) {
-          case 0: {
+          case 0:
             return HttpResponse.json({});
-          }
-          case 1: {
+          case 1:
             return HttpResponse.json({ ROOTID: { "m/": [] } });
-          }
         }
       }),
-      http.get("*/v1/trustchain/ROOTID", () =>
-        HttpResponse.json({ "m/": MOCK_DATA.trustChains.tree1.root }),
-      ),
+      http.get("*/v1/trustchain/ROOTID", () => HttpResponse.json(initialTree.serialize())),
       http.all("*", () => HttpResponse.json({})),
     );
     mswServer.listen();
 
-    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(
-      StreamTree.deserialize({ "m/": MOCK_DATA.trustChains.tree1.root }),
-    );
-    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(
-      StreamTree.deserialize({
-        "m/": MOCK_DATA.trustChains.tree1.root,
-        "m/0'/16'/0':": MOCK_DATA.trustChains.tree1.pushMember,
-      }),
-    );
+    // Mock HW device interactions
+    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(initialTree);
+    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(oneMemberTree);
 
-    const sdk = new SDK({ applicationId: 16, name: "Foo" }, hwDeviceProviderMock);
+    // Run the test
+    const sdk = new SDK(sdkContext, hwDeviceProviderMock);
+    const { alice } = MOCK_DATA.members;
+    const { type, trustchain } = await sdk.getOrCreateTrustchain("foo", alice, deviceCallbacks);
 
-    const { type, trustchain } = await sdk.getOrCreateTrustchain(
-      "foo",
-      MOCK_DATA.credentials.member1,
-      callbacks,
-      new Uint8Array(0x1234),
-    );
-
+    // Check expectations
     expect(type).toBe(TrustchainResultType.created);
     expect(trustchain).toEqual({
       applicationPath: "m/0'/16'/0'",
       rootId: "ROOTID",
-      walletSyncEncryptionKey: expect.any(String),
+      walletSyncEncryptionKey: expect.stringMatching(/^[0-9a-f]{64}$/),
     });
+    // TODO add more expectations
+  });
+
+  it("should remove a member from the Trustchain", async () => {
+    // Mock trustchain states:
+    const device = new SoftwareDevice(convertLiveCredentialsToKeyPair(MOCK_DATA.members.alice));
+    const threeMembersTree = await createTrustChain(device)
+      .then(addMember(device, "m/0'/16'/0'", "alice"))
+      .then(addMember(device, "m/0'/16'/0'", "bob"))
+      .then(addMember(device, "m/0'/16'/0'", "charlie"));
+    const rmMembersTree = await addMember(device, "m/0'/16'/1'", "alice")(threeMembersTree);
+
+    // Mock API calls
+    const pushedCommands: PutCommandsRequest[] = [];
+    mswServer = setupServer(
+      http.get("*/v1/trustchain/ROOTID", () => HttpResponse.json(threeMembersTree.serialize())),
+      http.get("*/v1/challenge", () =>
+        HttpResponse.json({ json: {}, tlv: MOCK_DATA.challengeTlv }),
+      ),
+
+      http.post("*/v1/authenticate", () =>
+        HttpResponse.json({
+          accessToken: "BACKEND JWT",
+          permissions: { ROOTID: { "m/0'/16'/1'": ["owner"] } },
+        }),
+      ),
+
+      http.put("*/v1/trustchain/ROOTID/commands", async ({ request }) => {
+        const content = await request.json();
+        pushedCommands.push(content as PutCommandsRequest);
+        return HttpResponse.json({});
+      }),
+
+      http.all("*", async () => HttpResponse.json({})),
+    );
+    mswServer.listen();
+
+    // Mock HW device interactions
+    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(threeMembersTree);
+    HWDeviceProviderMethodsMocks.withHw.mockResolvedValueOnce(rmMembersTree);
+
+    // Run the test
+    const sdk = new SDK(sdkContext, hwDeviceProviderMock);
+
+    const { alice, bob, charlie } = MOCK_DATA.members;
+    const trustchain = {
+      applicationPath: "m/0'/16'/0'",
+      rootId: "ROOTID",
+      walletSyncEncryptionKey: "",
+    };
+    const memberToRemove = {
+      name: "bob",
+      id: bob.pubkey,
+      permissions: Permissions.OWNER,
+    };
+    const res = await sdk.removeMember("foo", trustchain, alice, memberToRemove, deviceCallbacks);
+
+    // Check expectations
+    expect(res).toEqual({
+      applicationPath: "m/0'/16'/1'",
+      rootId: "ROOTID",
+      walletSyncEncryptionKey: expect.stringMatching(/^[0-9a-f]{64}$/),
+    });
+
+    const lastCommand = pushedCommands.filter(({ path }) => path === "m/0'/16'/1'").at(-1)
+      ?.blocks[0];
+    expect(lastCommand).toContain(alice.pubkey);
+    expect(lastCommand).not.toContain(bob.pubkey);
+    expect(lastCommand).toContain(charlie.pubkey);
     // TODO add more expectations
   });
 });
 
 const MOCK_DATA = {
-  credentials: {
-    member1: {
-      pubkey: "0306af506179ca737dbd45a699aa5ea5d2fe5f32a5517b60e642f3e0f4f8aae882",
-      privatekey: "75e25449353cfd6952d61b88f257dc529d579d6e590bec175980449bcb49627e",
+  members: {
+    alice: {
+      pubkey: "02e3311a12c450604725f02d1a775ef5cdb4a1b832eb41ac6b1302adbe92a612fc",
+      privatekey: "873f500bd20783224f7e78d4f8cce3d2bf69eb8008fbd697d20bbea31a721a03",
+    },
+    bob: {
+      pubkey: "034ac6813695b0d5e033a2a19061c83951e2241aad62ec8fa347a944831c07ea82",
+    },
+    charlie: {
+      pubkey: "03b4165cddf39e58f3a89682fdf1ccba213167084fecdbb3b86669d40d201df1cf",
     },
   },
-  trustChains: {
-    tree1: {
-      root: "0101010220ce28e53bc4b6b5266f5850ab5a712de83346e4e32f598cd457ba051408b321d10621032348f4c7d507c5aa2c00e9e9b71f0cdc137b59afe62a220bded59311df26cc9301010110b005000102000006210324d6d2be4dc02bec8014226e8dabd0df745605e3421d4f988241e6892578ca360510395f2994be710a27992193c8fd62578005502629b9325c9635157f7c49514a66c0a0d42af5210192ecb2e5f46bf7dcc77ab179f9e3633d5cefd0972d285dd18683742cf804b6ec4a68882033b9a9cdff7c3ddc78bca5f2ca6036d0bb01bc5a307f19062102b293202376dd8c70732cae2ab39f04beb321e5cbc8b77b2220862801f6006941034630440220238b0e5614e485ba1f227519e7f9f084f8c1f62caa389c2e72aa2a744942895202205637356693f558f71915396ba7a43db12eaaa7edbe53d3165aaab96ce22bf157",
-      pushMember:
-        "0101010220f21394bdef8bc27a0695e80896c1de7242a586c403274e20abbbd75c5eb7cc990621032348f4c7d507c5aa2c00e9e9b71f0cdc137b59afe62a220bded59311df26cc9301010315b8050c800000008000001080000000062102846c94806532b7102712aa37a223c9709a2f16e0602f8cc752fac4764ade82e10510f70f41fe07c988db7030e9b1fc5b177505502e0b33584d7bcf45afb682e8a47461186d226d5e5d64188a1dba8e29933bbcd984956137173e31703195b346bfc60cd1a3d93a2f239cea8b8b800a50843aafa2d8b14314819af251f459e45bdd1b508d062102fe743822595843b7e69df5ca96c6cbc477400bcd97c519f546753c0226aaae54113304084d656d626572203106210306af506179ca737dbd45a699aa5ea5d2fe5f32a5517b60e642f3e0f4f8aae8820104ffffffff12aa0510cc9af7d206117c3f4cc0e523a8be7140055078dff588a71eea1994e14ea50b8db7c500d267a62cde4c30cf56ce4a3c6ad80d05600f9c8d68d5dd20194408a2e906ebc8d20972a77fc9b1d8d95d4baf2b794ad92ff2f287255eb90ce8b4be68b3e70106210306af506179ca737dbd45a699aa5ea5d2fe5f32a5517b60e642f3e0f4f8aae882062102275f88d0c6deb9ddd8b9184cd7a871827be1879e6add14c5deec077384361e3b03473045022100e5934425144f62896d0dfe12dae62a17bf019c21d12a1c57b08ae9844d38a67e02204c7b86d44a8cd27a9d56c244423340635a55afc94714fabb4c966ab7dfd46c8d",
-    },
-  },
+
+  challengeTlv:
+    "010107020100121053801a35c2e24b627d6e4925ce318980140101154630440220319b42a416512437e48d9c9bf204daea7da03d452c50a8caa4c2d152407ffd0c02201f121b0e99df1d30f4757b6a00b8d974d70996771893ac49c4a245c147cc1d8f160466a90248202b7472757374636861696e2d6261636b656e642e6170692e6177732e7374672e6c64672d746563682e636f6d320121332103cb7628e7248ddf9c07da54b979f16bf081fb3d173aac0992ad2a44ef6a388ae2600401000000",
 };
+
+function createTrustChain(device: Device): Promise<StreamTree> {
+  return StreamTree.createNewTree(device);
+}
+
+function addMember(
+  device: Device,
+  path: string,
+  name: keyof typeof MOCK_DATA.members,
+): (tree: StreamTree) => Promise<StreamTree> {
+  const memberId = crypto.from_hex(MOCK_DATA.members[name].pubkey);
+  return (tree: StreamTree) => tree.share(path, device, memberId, name, Permissions.OWNER);
+}
