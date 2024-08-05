1a576dc2ddf0e647ce19799e68df3bf788b797f1
Thomas Brillard <thomas.brillard@ledger.fr>
2024-08-05 16:28:58 +0200

    Revert "feat: run importer"
    This reverts commit de625d03d2e322ff4e81b1adf047daea7d698b7e.

--- libs/ledgerjs/packages/cryptoassets/src/crypto-assets-importer/exchange/polygon-erc20.ts
+++ libs/ledgerjs/packages/cryptoassets/src/crypto-assets-importer/exchange/polygon-erc20.ts
@@ -7,7 +7,7 @@ export const importPolygonERC20Exchange = async (outputDir: string) => {
   console.log("importing Polygon-ERC20 exchange...");
   try {
     const [polygonERC20Exchange, hash] = await fetchTokens<PolygonERC20Exchange>(
-      "evm/137/erc20-exchange.json",
+      "exchange/polygon-erc20.json",
     );
     fs.writeFileSync(
       `${outputDir}/exchange/polygon-erc20.json`,
