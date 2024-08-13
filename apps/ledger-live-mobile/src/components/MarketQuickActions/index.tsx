afac39d933f060139f6a6e399abbc5a9415143cb
Theophile Sandoz <Theophile Sandoz>
2024-08-13 13:04:19 +0200

    Add analytics

--- apps/ledger-live-mobile/src/components/MarketQuickActions/index.tsx
+++ apps/ledger-live-mobile/src/components/MarketQuickActions/index.tsx
@@ -1,9 +1,10 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { QuickActionButtonProps, QuickActionList } from "@ledgerhq/native-ui";
 import { CryptoOrTokenCurrency } from "@ledgerhq/types-cryptoassets";
+import { track } from "~/analytics";
 import { EntryOf } from "~/types/helpers";
 import useQuickActions from "../../hooks/useQuickActions";
 import { BaseNavigatorStackParamList } from "../RootNavigator/types/BaseNavigator";
@@ -13,6 +14,7 @@ type Props = { currency: CryptoOrTokenCurrency };
 export const MarketQuickActions = ({ currency }: Props) => {
   const { t } = useTranslation();
   const navigation = useNavigation<StackNavigationProp<BaseNavigatorStackParamList>>();
+  const router = useRoute();
   const { quickActionsList } = useQuickActions({ currency });
 
   const quickActionsData: QuickActionButtonProps[] = useMemo(
@@ -26,12 +28,14 @@ export const MarketQuickActions = ({ currency }: Props) => {
           textVariant: "small",
           Icon: quickActionsItem.icon,
           children: t(prop.name),
-          onPress: () =>
-            navigation.navigate<keyof BaseNavigatorStackParamList>(...quickActionsItem.route),
+          onPress: () => {
+            track("button_clicked", { button: prop.analytics, page: router.name });
+            navigation.navigate<keyof BaseNavigatorStackParamList>(...quickActionsItem.route);
+          },
           disabled: quickActionsItem.disabled,
         };
       }),
-    [quickActionsList, t, navigation],
+    [quickActionsList, t, navigation, router.name],
   );
 
   return <QuickActionList data={quickActionsData} numColumns={5} id="asset_five_columns" />;
@@ -40,20 +44,26 @@ export const MarketQuickActions = ({ currency }: Props) => {
 const QUICK_ACTIONS = {
   SEND: {
     name: "portfolio.quickActions.send",
+    analytics: "quick_action_send",
   },
   RECEIVE: {
     name: "portfolio.quickActions.deposit",
+    analytics: "quick_action_receive",
   },
   BUY: {
     name: "portfolio.quickActions.buy",
+    analytics: "quick_action_buy",
   },
   SELL: {
     name: "portfolio.quickActions.sell",
+    analytics: "quick_action_sell",
   },
   SWAP: {
     name: "portfolio.quickActions.swap",
+    analytics: "quick_action_swap",
   },
   STAKE: {
     name: "portfolio.quickActions.stake",
+    analytics: "quick_action_stake",
   },
 } as const;
