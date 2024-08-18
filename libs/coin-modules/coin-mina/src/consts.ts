689cefbb701ad32b7758c46bc3a193aab8962e6e
Prateek Rathod <prateek@zondax.ch>
2024-08-18 19:41:34 +0200

    refac(mina): finalize mainnet usage

--- libs/coin-modules/coin-mina/src/consts.ts
+++ libs/coin-modules/coin-mina/src/consts.ts
@@ -1,11 +1,11 @@
 export const TESTNET_NETWORK_IDENTIFIER = {
   network_identifier: {
     blockchain: "mina",
-    network: "testnet",
+    network: "mainnet",
   },
 };
 
-export const MINA_DEVNET_NETWORK_ID = 0;
+export const MINA_MAINNET_NETWORK_ID = 1;
 export const MINA_PAYMENT_TYPE_ID = 0;
 export const MINA_TOKEN_ID = "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf";
 export const MINA_DECIMALS = 9;
