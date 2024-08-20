aa0126ae3edf0c7604cb9e4027473ba86f151f01
Kant <quentin.jaccarino@ledger.fr>
2024-08-20 09:43:14 +0200

    fix: hidden header should show again when searching [LIVE-13727]

--- apps/ledger-live-mobile/src/newArch/features/Web3Hub/screens/Web3HubSearch/index.tsx
+++ apps/ledger-live-mobile/src/newArch/features/Web3Hub/screens/Web3HubSearch/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import type { SearchProps } from "LLM/features/Web3Hub/types";
@@ -12,6 +12,11 @@ const edges = ["top", "bottom", "left", "right"] as const;
 export default function Web3HubSearch({ navigation }: SearchProps) {
   const [search, setSearch] = useState("");
   const { layoutY, scrollHandler } = useScrollHandler(TOTAL_HEADER_HEIGHT);
+
+  // Reset the layoutY value when search changes
+  useEffect(() => {
+    layoutY.value = 0;
+  }, [layoutY, search]);
 
   return (
     <SafeAreaView edges={edges} style={{ flex: 1 }}>
