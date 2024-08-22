7ff526d94538a61b95739ef101377d1b2ad0f7ed
Theophile Sandoz <Theophile Sandoz>
2024-08-22 11:57:36 +0200

    Add more descriptive mobile device name to the trustchain

--- apps/ledger-live-mobile/src/newArch/features/WalletSync/hooks/useInstanceName.ts
+++ apps/ledger-live-mobile/src/newArch/features/WalletSync/hooks/useInstanceName.ts
@@ -1,12 +1,15 @@
+import { getDeviceNameSync } from "react-native-device-info";
 import { getEnv } from "@ledgerhq/live-env";
 import { Platform } from "react-native";
 
 const platformMap: Record<string, string | undefined> = {
-  ios: "iOS",
+  ios: "iPhone iOS",
   android: "Android",
 };
 
 export function useInstanceName(): string {
   const hash = getEnv("USER_ID").slice(0, 5);
-  return `${platformMap[Platform.OS] ?? Platform.OS} ${Platform.Version} ${hash ? " " + hash : ""}`;
+  const os = platformMap[Platform.OS] ?? Platform.OS;
+  const deviceName = getDeviceNameSync() ?? `${os} ${Platform.Version}`;
+  return `${deviceName} ${hash ? " " + hash : ""}`;
 }
