a1da8696c083cc07ecc02a2a47a66eb97f887928
chrisduma-ledger <cristian.duma-ext@ledger.fr>
2024-08-28 16:24:48 +0300

    chore: tidy up

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Sell/SellCompleted.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Sell/SellCompleted.tsx
@@ -1,33 +1,10 @@
 import React from "react";
 import { Trans } from "react-i18next";
-import styled from "styled-components";
 import Box from "~/renderer/components/Box";
 import Text from "~/renderer/components/Text";
 import IconCheck from "~/renderer/icons/Check";
 import IconClock from "~/renderer/icons/Clock";
-import { colors } from "~/renderer/styles/theme";
-
-const IconWrapper = styled(Box)`
-  background: ${colors.lightGreen};
-  color: ${colors.positiveGreen};
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`;
-
-const WrapperClock = styled(Box).attrs(() => ({
-  bg: "palette.background.paper",
-  color: "palette.text.shade60",
-}))`
-  border-radius: 50%;
-  position: absolute;
-  bottom: -2px;
-  right: -2px;
-  padding: 2px;
-`;
+import { IconWrapper, WrapperClock } from "../shared/shared-styles";
 
 const SellCompleted = () => {
   return (
