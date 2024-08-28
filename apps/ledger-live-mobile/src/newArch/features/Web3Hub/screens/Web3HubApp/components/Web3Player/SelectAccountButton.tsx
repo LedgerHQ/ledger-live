248901b235ea2ace10bebea77453592494636371
Kant <quentin.jaccarino@ledger.fr>
2024-08-28 17:49:46 +0200

    feat(web3hub): new bottom modal to select network and account with dapp browser v3 Small perf improvement for dapp browser v3 dapps initial load Lots of small visual improvements, polish and bugfixes Update app screen header url

--- apps/ledger-live-mobile/src/newArch/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountButton.tsx
+++ apps/ledger-live-mobile/src/newArch/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountButton.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Trans } from "react-i18next";
 import { Text } from "@ledgerhq/native-ui";
 import { AppManifest } from "@ledgerhq/live-common/wallet-api/types";
 import { CurrentAccountHistDB } from "@ledgerhq/live-common/wallet-api/react";
+import { useDappCurrentAccount } from "@ledgerhq/live-common/wallet-api/useDappLogic";
 import Button from "~/components/Button";
 import CircleCurrencyIcon from "~/components/CircleCurrencyIcon";
-import { useSelectAccount } from "~/components/Web3AppWebview/helpers";
 import { useMaybeAccountName } from "~/reducers/wallet";
+import SelectAccountModal from "./SelectAccountModal";
 
 type SelectAccountButtonProps = {
   manifest: AppManifest;
@@ -17,36 +18,55 @@ export default function SelectAccountButton({
   manifest,
   currentAccountHistDb,
 }: SelectAccountButtonProps) {
-  const { onSelectAccount, currentAccount } = useSelectAccount({ manifest, currentAccountHistDb });
+  const { currentAccount } = useDappCurrentAccount(currentAccountHistDb);
 
   const currentAccountName = useMaybeAccountName(currentAccount);
 
+  const [modalOpened, setModalOpened] = useState(false);
+
+  const onSelectAccount = useCallback(() => {
+    setModalOpened(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setModalOpened(false);
+  }, []);
+
   return (
-    <Button
-      Icon={
-        !currentAccount ? undefined : (
-          <CircleCurrencyIcon
-            size={24}
-            currency={
-              currentAccount.type === "TokenAccount"
-                ? currentAccount.token
-                : currentAccount.currency
-            }
-          />
-        )
-      }
-      iconPosition={"left"}
-      type="primary"
-      onPress={onSelectAccount}
-      isNewIcon
-    >
-      {!currentAccount ? (
-        <Text>
-          <Trans i18nKey="common.selectAccount" />
-        </Text>
-      ) : (
-        <Text color={"neutral.c20"}>{currentAccountName}</Text>
-      )}
-    </Button>
+    <>
+      <SelectAccountModal
+        manifest={manifest}
+        currentAccountHistDb={currentAccountHistDb}
+        isOpened={modalOpened}
+        onSelectAccount={onSelectAccount}
+        onClose={onClose}
+      />
+      <Button
+        Icon={
+          !currentAccount ? undefined : (
+            <CircleCurrencyIcon
+              size={24}
+              currency={
+                currentAccount.type === "TokenAccount"
+                  ? currentAccount.token
+                  : currentAccount.currency
+              }
+            />
+          )
+        }
+        iconPosition={"left"}
+        type="primary"
+        onPress={onSelectAccount}
+        isNewIcon
+      >
+        {!currentAccount ? (
+          <Text>
+            <Trans i18nKey="common.selectAccount" />
+          </Text>
+        ) : (
+          <Text color={"neutral.c20"}>{currentAccountName}</Text>
+        )}
+      </Button>
+    </>
   );
 }
