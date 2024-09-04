d3407bd6f8eef37f4829c0090602fcc97f1cb377
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-09-04 19:00:47 +0200

    generated sort

--- libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
+++ libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
@@ -1,20 +1,20 @@
+import casper from "../families/casper/deviceTransactionConfig";
+import celo from "../families/celo/deviceTransactionConfig";
+import crypto_org from "../families/crypto_org/deviceTransactionConfig";
+import filecoin from "../families/filecoin/deviceTransactionConfig";
+import hedera from "../families/hedera/deviceTransactionConfig";
+import internet_computer from "../families/internet_computer/deviceTransactionConfig";
+import stacks from "../families/stacks/deviceTransactionConfig";
 import algorand from "@ledgerhq/coin-algorand/deviceTransactionConfig";
 import bitcoin from "@ledgerhq/coin-bitcoin/deviceTransactionConfig";
 import cardano from "@ledgerhq/coin-cardano/deviceTransactionConfig";
-import casper from "../families/casper/deviceTransactionConfig";
-import celo from "../families/celo/deviceTransactionConfig";
 import cosmos from "@ledgerhq/coin-cosmos/deviceTransactionConfig";
-import crypto_org from "../families/crypto_org/deviceTransactionConfig";
 import elrond from "@ledgerhq/coin-elrond/deviceTransactionConfig";
 import evm from "@ledgerhq/coin-evm/deviceTransactionConfig";
-import filecoin from "../families/filecoin/deviceTransactionConfig";
-import hedera from "../families/hedera/deviceTransactionConfig";
 import icon from "@ledgerhq/coin-icon/deviceTransactionConfig";
-import internet_computer from "../families/internet_computer/deviceTransactionConfig";
 import near from "@ledgerhq/coin-near/deviceTransactionConfig";
 import polkadot from "@ledgerhq/coin-polkadot/deviceTransactionConfig";
 import solana from "@ledgerhq/coin-solana/deviceTransactionConfig";
-import stacks from "../families/stacks/deviceTransactionConfig";
 import stellar from "@ledgerhq/coin-stellar/deviceTransactionConfig";
 import tezos from "@ledgerhq/coin-tezos/deviceTransactionConfig";
 import ton from "@ledgerhq/coin-ton/deviceTransactionConfig";
@@ -22,23 +22,23 @@ import tron from "@ledgerhq/coin-tron/deviceTransactionConfig";
 import xrp from "@ledgerhq/coin-xrp/deviceTransactionConfig";
 
 export default {
+  casper,
+  celo,
+  crypto_org,
+  filecoin,
+  hedera,
+  internet_computer,
+  stacks,
   algorand,
   bitcoin,
   cardano,
-  casper,
-  celo,
   cosmos,
-  crypto_org,
   elrond,
   evm,
-  filecoin,
-  hedera,
   icon,
-  internet_computer,
   near,
   polkadot,
   solana,
-  stacks,
   stellar,
   tezos,
   ton,
--- libs/ledger-live-common/src/generated/transaction.ts
+++ libs/ledger-live-common/src/generated/transaction.ts
@@ -1,49 +1,49 @@
+import casper from "../families/casper/transaction";
+import celo from "../families/celo/transaction";
+import crypto_org from "../families/crypto_org/transaction";
+import filecoin from "../families/filecoin/transaction";
+import hedera from "../families/hedera/transaction";
+import internet_computer from "../families/internet_computer/transaction";
+import stacks from "../families/stacks/transaction";
+import vechain from "../families/vechain/transaction";
 import algorand from "@ledgerhq/coin-algorand/transaction";
 import bitcoin from "@ledgerhq/coin-bitcoin/transaction";
 import cardano from "@ledgerhq/coin-cardano/transaction";
-import casper from "../families/casper/transaction";
-import celo from "../families/celo/transaction";
 import cosmos from "@ledgerhq/coin-cosmos/transaction";
-import crypto_org from "../families/crypto_org/transaction";
 import elrond from "@ledgerhq/coin-elrond/transaction";
 import evm from "@ledgerhq/coin-evm/transaction";
-import filecoin from "../families/filecoin/transaction";
-import hedera from "../families/hedera/transaction";
 import icon from "@ledgerhq/coin-icon/transaction";
-import internet_computer from "../families/internet_computer/transaction";
 import near from "@ledgerhq/coin-near/transaction";
 import polkadot from "@ledgerhq/coin-polkadot/transaction";
 import solana from "@ledgerhq/coin-solana/transaction";
-import stacks from "../families/stacks/transaction";
 import stellar from "@ledgerhq/coin-stellar/transaction";
 import tezos from "@ledgerhq/coin-tezos/transaction";
 import ton from "@ledgerhq/coin-ton/transaction";
 import tron from "@ledgerhq/coin-tron/transaction";
-import vechain from "../families/vechain/transaction";
 import xrp from "@ledgerhq/coin-xrp/transaction";
 
 export default {
+  casper,
+  celo,
+  crypto_org,
+  filecoin,
+  hedera,
+  internet_computer,
+  stacks,
+  vechain,
   algorand,
   bitcoin,
   cardano,
-  casper,
-  celo,
   cosmos,
-  crypto_org,
   elrond,
   evm,
-  filecoin,
-  hedera,
   icon,
-  internet_computer,
   near,
   polkadot,
   solana,
-  stacks,
   stellar,
   tezos,
   ton,
   tron,
-  vechain,
   xrp,
 };
