a7a608a27c74181be5a096bb475883afe5f43be8
Angus Bayley <angusbayley@gmail.com>
2024-09-09 17:14:15 +0100

    temp: debugs to prepareExample

--- libs/ui/tests/react.script.js
+++ libs/ui/tests/react.script.js
@@ -4,7 +4,7 @@ const exampleName = process.argv[2] || "";
 
 const UPDATE_SNAPSHOTS = process.env["UPDATE_SNAPSHOTS"];
 
-const prepareExample = `pnpm clean:full && echo "DEBUG cleaned" && pnpm i --filter="${exampleName}-example" && echo "DEBUG installed" && pnpm build && echo "DEBUG built"`;
+const prepareExample = `pnpm clean:full 2>&1 | sed 's/^/[DEBUG: PNPM CLEAN] /' && echo "DEBUG cleaned" && pnpm i --filter="${exampleName}-example" 2>&1 | sed 's/^/[DEBUG: PNPM INSTALL] /' && echo "DEBUG installed" && pnpm build 2>&1 | sed 's/^/[DEBUG: PNPM BUILD] /' && echo "DEBUG built"`;
 const runTest = `pnpm concurrently -s=first -k "pnpm --dir examples/${exampleName} serve" "playwright test ${
   UPDATE_SNAPSHOTS ? "--update-snapshots" : ""
 }"`;
@@ -14,6 +14,7 @@ try {
     stdio: "inherit",
     cwd: `./examples/${exampleName}`,
   });
+  child_process.execSync(`echo "DEBUG prepareExample finished"`, { stdio: "inherit" });
   child_process.execSync(runTest, { stdio: "inherit" });
 } catch (error) {
   console.error(error.message);
