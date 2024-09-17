0a463b9417e9a37d41c306d7a75fa0bd5a0e2275
Victor Alber <victor.alber@ledger.fr>
2024-09-17 09:53:24 +0200

    test: 🎭 Updating TMS link for xray execution

--- apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
@@ -3,38 +3,41 @@ import { Currency } from "../../enum/Currency";
 import { addTmsLink } from "tests/utils/allureUtils";
 import { getDescription } from "../../utils/customJsonReporter";
 
-const currencies: Currency[] = [
-  Currency.BTC,
-  Currency.ETH,
-  Currency.ETC,
-  Currency.XRP,
+const currencies = [
+  {
+    currency: Currency.BTC,
+    xrayTicket: "B2CQA-2499, B2CQA-2644, B2CQA-2658, B2CQA-2672, B2CQA-929, B2CQA-786",
+  },
+  { currency: Currency.ETH, xrayTicket: "B2CQA-2503, B2CQA-2645, B2CQA-2659, B2CQA-2673" },
+  { currency: Currency.ETC, xrayTicket: "B2CQA-2502, B2CQA-2646, B2CQA-2660, B2CQA-2674" },
+  { currency: Currency.XRP, xrayTicket: "B2CQA-2505, B2CQA-2647, B2CQA-2661, B2CQA-2675" },
   //todo: Reactivate after DOT API issue is resolved - TSD-3603
-  //Currency.DOT,
-  Currency.TRX,
-  Currency.ADA,
-  Currency.XLM,
-  Currency.BCH,
-  Currency.ALGO,
-  Currency.ATOM,
-  Currency.XTZ,
-  Currency.SOL,
-  Currency.TON,
+  //{ currency: Currency.DOT, xrayTicket: "B2CQA-2504, B2CQA-2648, B2CQA-2662, B2CQA-2676" },
+  { currency: Currency.TRX, xrayTicket: "B2CQA-2508, B2CQA-2649, B2CQA-2663, B2CQA-2677" },
+  { currency: Currency.ADA, xrayTicket: "B2CQA-2500, B2CQA-2650, B2CQA-2664, B2CQA-2678" },
+  { currency: Currency.XLM, xrayTicket: "B2CQA-2506, B2CQA-2651, B2CQA-2665, B2CQA-2679" },
+  { currency: Currency.BCH, xrayTicket: "B2CQA-2498, B2CQA-2652, B2CQA-2666, B2CQA-2680" },
+  { currency: Currency.ALGO, xrayTicket: "B2CQA-2497, B2CQA-2653, B2CQA-2667, B2CQA-2681" },
+  { currency: Currency.ATOM, xrayTicket: "B2CQA-2501, B2CQA-2654, B2CQA-2668, B2CQA-2682" },
+  { currency: Currency.XTZ, xrayTicket: "B2CQA-2507, B2CQA-2655, B2CQA-2669, B2CQA-2683" },
+  { currency: Currency.SOL, xrayTicket: "B2CQA-2642, B2CQA-2656, B2CQA-2670, B2CQA-2684" },
+  { currency: Currency.TON, xrayTicket: "B2CQA-2643, B2CQA-2657, B2CQA-2671, B2CQA-2685" },
 ];
 
 for (const currency of currencies) {
   test.describe("Add Accounts", () => {
     test.use({
       userdata: "skip-onboarding",
-      speculosApp: currency.speculosApp,
+      speculosApp: currency.currency.speculosApp,
     });
     let firstAccountName = "NO ACCOUNT NAME YET";
 
     test(
-      `[${currency.name}] Add account`,
+      `[${currency.currency.name}] Add account`,
       {
         annotation: {
           type: "TMS",
-          description: "B2CQA-101, B2CQA-102, B2CQA-314, B2CQA-330, B2CQA-929, B2CQA-786",
+          description: currency.xrayTicket,
         },
       },
       async ({ app }) => {
@@ -42,13 +45,13 @@ for (const currency of currencies) {
 
         await app.portfolio.openAddAccountModal();
         await app.addAccount.expectModalVisiblity();
-        await app.addAccount.selectCurrency(currency);
+        await app.addAccount.selectCurrency(currency.currency);
         firstAccountName = await app.addAccount.getFirstAccountName();
 
         await app.addAccount.addAccounts();
         await app.addAccount.done();
         // Todo: Remove 'if' when CounterValue is fixed for $TON - LIVE-13685
-        if (currency.name !== Currency.TON.name) {
+        if (currency.currency.name !== Currency.TON.name) {
           await app.layout.expectBalanceVisibility();
         }
         await app.layout.goToAccounts();
