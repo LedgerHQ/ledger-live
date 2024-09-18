712e2ad51599c43ecb0d9406f137909885e13d07
Victor Alber <victor.alber@ledger.fr>
2024-09-18 15:43:33 +0200

    changing com

--- apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
@@ -8,7 +8,8 @@ const currencies: Currency[] = [
   Currency.ETH,
   Currency.ETC,
   Currency.XRP,
-  //Currency.DOT, //Reactivate after DOT API issue is resolved - TSD-3603
+  //todo: Reactivate after DOT API issue is resolved - TSD-3603
+  //Currency.DOT,
   Currency.TRX,
   Currency.ADA,
   Currency.XLM,
