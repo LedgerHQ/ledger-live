a611f4729f4c24a2bb8c450b6cd86d18c1da0852
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-09-20 14:44:13 +0200

    feat: ✏️Update Analytics

--- apps/ledger-live-desktop/src/newArch/features/WalletSync/screens/Activation/01-CreateOrSynchronizeStep.tsx
+++ apps/ledger-live-desktop/src/newArch/features/WalletSync/screens/Activation/01-CreateOrSynchronizeStep.tsx
@@ -4,7 +4,11 @@ import { useTranslation } from "react-i18next";
 import { useTheme } from "styled-components";
 import ButtonV3 from "~/renderer/components/ButtonV3";
 import TrackPage from "~/renderer/analytics/TrackPage";
-import { AnalyticsPage } from "../../hooks/useLedgerSyncAnalytics";
+import {
+  AnalyticsFlow,
+  AnalyticsPage,
+  useLedgerSyncAnalytics,
+} from "../../hooks/useLedgerSyncAnalytics";
 import { LogoWrapper } from "../../components/LogoWrapper";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import { openURL } from "~/renderer/linking";
@@ -21,8 +25,15 @@ export default function CreateOrSynchronizeStep({ goToCreateBackup, goToSync }: 
   const learnMoreUrl = ledgerSyncFF?.params?.learnMoreLink;
   const hasLearnMoreLink = !!learnMoreUrl;
 
+  const { onClickTrack } = useLedgerSyncAnalytics();
+
   const onLearnMore = () => {
     if (learnMoreUrl) {
+      onClickTrack({
+        button: "How does Ledger Sync work",
+        page: AnalyticsPage.Activation,
+        flow: AnalyticsFlow,
+      });
       openURL(learnMoreUrl);
     }
   };
