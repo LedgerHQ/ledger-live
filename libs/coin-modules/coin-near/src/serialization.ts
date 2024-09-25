f631b32d3f5c9d82c2da7db0553e7a9f0c541051
qperrot <quentin.perrot-minnot@ledger.fr>
2024-09-25 10:33:47 +0200

    format: fix format, fix: change near snap tests

--- libs/coin-modules/coin-near/src/serialization.ts
+++ libs/coin-modules/coin-near/src/serialization.ts
@@ -10,14 +10,12 @@ export function toNearResourcesRaw(r: NearResources): NearResourcesRaw {
     pendingBalance: pendingBalance.toString(),
     availableBalance: availableBalance.toString(),
     storageUsageBalance: storageUsageBalance.toString(),
-    stakingPositions: stakingPositions.map(
-      ({ staked, validatorId, available, pending }) => ({
-        staked: staked.toString(),
-        available: available.toString(),
-        pending: pending.toString(),
-        validatorId,
-      }),
-    ),
+    stakingPositions: stakingPositions.map(({ staked, validatorId, available, pending }) => ({
+      staked: staked.toString(),
+      available: available.toString(),
+      pending: pending.toString(),
+      validatorId,
+    })),
   };
 }
 
@@ -34,14 +32,12 @@ export function fromNearResourcesRaw(r: NearResourcesRaw): NearResources {
     pendingBalance: new BigNumber(pendingBalance),
     availableBalance: new BigNumber(availableBalance),
     storageUsageBalance: new BigNumber(storageUsageBalance),
-    stakingPositions: stakingPositions.map(
-      ({ staked, validatorId, available, pending }) => ({
-        staked: new BigNumber(staked),
-        available: new BigNumber(available),
-        pending: new BigNumber(pending),
-        validatorId,
-      }),
-    ),
+    stakingPositions: stakingPositions.map(({ staked, validatorId, available, pending }) => ({
+      staked: new BigNumber(staked),
+      available: new BigNumber(available),
+      pending: new BigNumber(pending),
+      validatorId,
+    })),
   };
 }
 
