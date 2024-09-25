ca467ec8191694adf79dc6228c225ac341391f7e
Martin Cayuelas <martin.cayuelas@ledger.fr>
2024-09-25 10:32:27 +0200

    feat: useCheckNftAccount in [LIB]

--- libs/live-nft-react/src/index.ts
+++ libs/live-nft-react/src/index.ts
@@ -6,3 +6,5 @@ export * from "./hooks/useRefreshMetadata";
 export * from "./hooks/useCheckSpamScore";
 export * from "./hooks/useFetchOrdinals";
 export * from "./hooks/helpers/ordinals";
+export * from "./hooks/useCheckNftAccount";
+export * from "./hooks/helpers/index";
--- libs/live-nft-react/src/tools/helperTests.tsx
+++ libs/live-nft-react/src/tools/helperTests.tsx
@@ -6,3 +6,37 @@ const queryClient = new QueryClient();
 export const wrapper = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
+
+import { BigNumber } from "bignumber.js";
+import { NFTs } from "@ledgerhq/coin-framework/mocks/fixtures/nfts";
+import { encodeNftId } from "@ledgerhq/coin-framework/nft/nftId";
+
+export type FakeNFTRaw = {
+  id: string;
+  tokenId: string;
+  amount: BigNumber;
+  contract: string;
+  standard: "ERC721";
+  currencyId: string;
+  metadata: undefined;
+};
+
+export const generateNftsOwned = () => {
+  const nfts: FakeNFTRaw[] = [];
+
+  NFTs.forEach(nft => {
+    for (let i = 1; i <= 20; i++) {
+      nfts.push({
+        id: encodeNftId("foo", nft.collection.contract, String(i), "ethereum"),
+        tokenId: String(i),
+        amount: new BigNumber(0),
+        contract: nft.collection.contract,
+        standard: "ERC721" as const,
+        currencyId: "ethereum",
+        metadata: undefined,
+      });
+    }
+  });
+
+  return nfts;
+};
