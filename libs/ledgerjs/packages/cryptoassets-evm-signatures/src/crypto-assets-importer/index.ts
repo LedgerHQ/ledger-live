5468d5b7a6d12aa6c7a6f897df743d0868114574
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-09-27 09:28:01 +0200

    eip712 data back as used by hw-app-eth-test

--- libs/ledgerjs/packages/cryptoassets-evm-signatures/src/crypto-assets-importer/index.ts
+++ libs/ledgerjs/packages/cryptoassets-evm-signatures/src/crypto-assets-importer/index.ts
@@ -1,21 +1,26 @@
 import path from "path";
 
 import { importEVMTokens } from "./importers/evm";
+import { importEIP712, importEIP712v2 } from "./importers/eip712";
 
 const outputFolder = path.join(__dirname, "../data");
 
 const importTokens = async () => {
-  const promises = [importEVMTokens(outputFolder)];
+  const promises = [
+    importEIP712(outputFolder),
+    importEIP712v2(outputFolder),
+    importEVMTokens(outputFolder),
+  ];
 
   await Promise.allSettled(promises);
 };
 
 const main = async () => {
+  console.group();
   console.log("Starting importing cryptoassets from CDN...");
-
   await importTokens();
-
   console.log("Import of cryptoassets finished successfully");
+  console.groupEnd();
 };
 
 main();
