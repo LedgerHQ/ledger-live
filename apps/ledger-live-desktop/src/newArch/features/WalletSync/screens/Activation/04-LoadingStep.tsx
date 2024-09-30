b8ccbe0156a507d96fc50993126a3e8081fb33bb
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2024-09-30 17:39:22 +0200

    fix: wording for loading screen of Ledger Sync

--- apps/ledger-live-desktop/src/newArch/features/WalletSync/screens/Activation/04-LoadingStep.tsx
+++ apps/ledger-live-desktop/src/newArch/features/WalletSync/screens/Activation/04-LoadingStep.tsx
@@ -4,22 +4,16 @@ import { useTranslation } from "react-i18next";
 import { AnalyticsFlow, AnalyticsPage } from "../../hooks/useLedgerSyncAnalytics";
 import TrackPage from "~/renderer/analytics/TrackPage";
 import { useLoadingStep } from "../../hooks/useLoadingStep";
-import { walletSyncHasTrustchainBeenCreatedSelector } from "~/renderer/reducers/walletSync";
-import { useSelector } from "react-redux";
 
 export default function ActivationLoadingStep() {
   useLoadingStep();
-  const hasTrustchainBeenCreated = useSelector(walletSyncHasTrustchainBeenCreatedSelector);
   const { t } = useTranslation();
   const title = "walletSync.loading.title";
-  const subtitle = hasTrustchainBeenCreated
-    ? "walletSync.loading.activation"
-    : "walletSync.loading.synch";
 
   return (
     <>
       <TrackPage category={String(AnalyticsPage.Loading)} flow={AnalyticsFlow} />
-      <Loading title={t(title)} subtitle={t(subtitle)} />
+      <Loading title={t(title)} subtitle={t("walletSync.loading.activation")} />
     </>
   );
 }
