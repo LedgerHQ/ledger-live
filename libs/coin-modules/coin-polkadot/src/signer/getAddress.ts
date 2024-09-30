307f4672ef4bf2020dff9d29e7f7227f2fb53901
hzheng-ledger <71653044+hzheng-ledger@users.noreply.github.com>
2024-09-30 20:38:53 +0200

    LIVE-13040: polkadot code cleanup (#7812)
    * fix: polkadot code cleanup
    
    * fix: typo
    
    * fix: polkadot integration tests
    
    * fix: polkadot unit tests
    
    * fix: integration tests
    
    * fix: polkadot coin-tester

--- libs/coin-modules/coin-polkadot/src/signer/getAddress.ts
+++ libs/coin-modules/coin-polkadot/src/signer/getAddress.ts
@@ -2,14 +2,10 @@ import { GetAddressFn } from "@ledgerhq/coin-framework/bridge/getAddressWrapper"
 import { SignerContext } from "@ledgerhq/coin-framework/signer";
 import { GetAddressOptions } from "@ledgerhq/coin-framework/derivation";
 import type { PolkadotSigner } from "../types";
-import coinConfig from "../config";
 
 const getAddress = (signerContext: SignerContext<PolkadotSigner>): GetAddressFn => {
   return async (deviceId: string, { path, verify }: GetAddressOptions) => {
-    const runtimeUpgraded = coinConfig.getCoinConfig().runtimeUpgraded;
-    const r = await signerContext(deviceId, signer =>
-      signer.getAddress(path, 0, verify, runtimeUpgraded),
-    );
+    const r = await signerContext(deviceId, signer => signer.getAddress(path, 0, verify));
     return {
       address: r.address,
       publicKey: r.pubKey,
