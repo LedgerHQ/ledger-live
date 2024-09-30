08ae55cfcad35914a6f4fb986a20abf935e7983c
Benjamin Haramboure <benjamin.haramboure@ledger.fr>
2024-09-30 14:05:43 +0200

    swap most common currencies with speculos

--- apps/ledger-live-desktop/tests/specs/speculos/swap.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/swap.spec.ts
@@ -4,36 +4,94 @@ import { AppInfos } from "tests/enum/AppInfos";
 import { setExchangeDependencies } from "@ledgerhq/live-common/e2e/speculos";
 import { Fee } from "tests/enum/Fee";
 import { Swap } from "tests/models/Swap";
-import { Providers, Rates } from "tests/enum/Swap";
+import { Provider, Rates } from "tests/enum/Swap";
 
 const swaps = [
   {
-    swap: new Swap(Account.ETH_1, Account.BTC_NATIVE_SEGWIT_1, "0.015", Fee.MEDIUM),
+    swap: new Swap(
+      Account.ETH_1,
+      Account.BTC_NATIVE_SEGWIT_1,
+      "0.02",
+      Fee.MEDIUM,
+      Provider.CHANGELLY,
+    ),
+  },
+  {
+    swap: new Swap(
+      Account.BTC_NATIVE_SEGWIT_1,
+      Account.ETH_1,
+      "0.00067",
+      Fee.MEDIUM,
+      Provider.CHANGELLY,
+    ),
+  },
+  {
+    swap: new Swap(Account.ETH_USDT_1, Account.ETH_1, "35", Fee.MEDIUM, Provider.CHANGELLY),
+  },
+  {
+    swap: new Swap(Account.ETH_1, Account.SOL_1, "0.018", Fee.MEDIUM, Provider.CHANGELLY),
+  },
+  {
+    swap: new Swap(Account.ETH_1, Account.ETH_USDT_1, "0.02", Fee.MEDIUM, Provider.CHANGELLY),
+  },
+  {
+    swap: new Swap(
+      Account.BTC_NATIVE_SEGWIT_1,
+      Account.SOL_1,
+      "0.0006",
+      Fee.MEDIUM,
+      Provider.CHANGELLY,
+    ),
+  },
+  {
+    swap: new Swap(
+      Account.BTC_NATIVE_SEGWIT_1,
+      Account.ETH_USDT_1,
+      "0.0006",
+      Fee.MEDIUM,
+      Provider.CHANGELLY,
+    ),
+  },
+  {
+    swap: new Swap(
+      Account.ETH_USDT_1,
+      Account.BTC_NATIVE_SEGWIT_1,
+      "35",
+      Fee.MEDIUM,
+      Provider.CHANGELLY,
+    ),
+  },
+  {
+    swap: new Swap(Account.ETH_USDT_1, Account.SOL_1, "40", Fee.MEDIUM, Provider.CHANGELLY),
   },
 ];
 
 const app: AppInfos = AppInfos.EXCHANGE;
 
 for (const { swap } of swaps) {
-  test.beforeAll(async () => {
-    process.env.SWAP_DISABLE_APPS_INSTALL = "true";
-    process.env.SWAP_API_BASE = "https://swap-stg.ledger.com/v5";
-  });
+  test.describe("Swap", () => {
+    test.beforeAll(async () => {
+      process.env.SWAP_DISABLE_APPS_INSTALL = "true";
+      process.env.SWAP_API_BASE = "https://swap-stg.ledger.com/v5";
+    });
 
-  test.afterAll(async () => {
-    delete process.env.SWAP_DISABLE_APPS_INSTALL;
-    delete process.env.SWAP_API_BASE;
-  });
+    test.beforeEach(async () => {
+      setExchangeDependencies(
+        accPair.map(appName => ({
+          name: appName,
+        })),
+      );
+    });
 
-  test.describe.serial("Swap", () => {
+    test.afterAll(async () => {
+      delete process.env.SWAP_DISABLE_APPS_INSTALL;
+      delete process.env.SWAP_API_BASE;
+    });
+
     const accPair: string[] = [swap.accountToDebit, swap.accountToCredit].map(acc =>
-      acc.currency.name.replace(/ /g, "_"),
+      acc.currency.speculosApp.name.replace(/ /g, "_"),
     );
-    setExchangeDependencies(
-      accPair.map(appName => ({
-        name: appName,
-      })),
-    );
+
     test.use({
       userdata: "speculos-tests-app",
       speculosApp: app,
@@ -43,15 +101,13 @@ for (const { swap } of swaps) {
       app,
       electronApp,
     }) => {
-      const provider = Providers.CHANGELLY;
       const rate = Rates.FLOAT;
       await app.layout.goToSwap();
-      await app.swap.selectAccountToSwapFrom(swap.accountToDebit.accountName);
+      await app.swap.selectAccountToSwapFrom(swap.accountToDebit);
       await app.swap.selectCurrencyToSwapTo(swap.accountToCredit.currency.name);
-
       await app.swap.fillInOriginAmount(swap.amount);
-      await app.swap.selectExchangeQuote(provider, rate);
-      await app.swap.clickExchangeButton(electronApp, provider);
+      await app.swap.selectExchangeQuote(swap.provider.name, rate);
+      await app.swap.clickExchangeButton(electronApp, swap.provider.uiName);
       const amountTo = await app.swapDrawer.getAmountToReceive();
       const fees = await app.swapDrawer.getFees();
       swap.setAmountToReceive(amountTo);
@@ -60,7 +116,7 @@ for (const { swap } of swaps) {
       await app.swapDrawer.verifyAmountSent(swap.amount, swap.accountToDebit.currency.ticker);
       await app.swapDrawer.verifySourceAccount(swap.accountToDebit.currency.name);
       await app.swapDrawer.verifyTargetCurrency(swap.accountToCredit.currency.name);
-      await app.swapDrawer.verifyProvider(provider);
+      await app.swapDrawer.verifyProvider(swap.provider.name);
       await app.speculos.verifyAmountsAndRejectSwap(swap);
       await app.swapDrawer.verifyExchangeErrorTextContent("Operation denied on device");
     });
