53d5c579dd3bdfdf94baa912f34b2c2866fd4833
mehulcs <mehul.becs@gmail.com>
2024-10-01 10:29:01 +0530

    tx error update

--- apps/ledger-live-mobile/src/families/cardano/DelegationFlow/02-Summary.tsx
+++ apps/ledger-live-mobile/src/families/cardano/DelegationFlow/02-Summary.tsx
@@ -228,14 +228,6 @@ export default function DelegationSummary({ navigation, route }: Props) {
         />
       </View>
       <GenericErrorBottomModal
-        error={status.errors["feeTooHigh"]}
-        footerButtons={
-          <>
-            <CancelButton containerStyle={styles.button} onPress={onBridgeErrorCancel} />
-          </>
-        }
-      />
-      <GenericErrorBottomModal
         error={bridgeErr}
         onClose={onBridgeErrorRetry}
         footerButtons={
