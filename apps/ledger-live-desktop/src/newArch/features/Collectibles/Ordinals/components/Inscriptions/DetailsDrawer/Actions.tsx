4b0d0f8b3694491143ea9307460384471ca93cc2
Lucas Werey <lucas.werey@ledger.fr>
2024-10-01 17:33:25 +0200

    :sparkles:(lld): add hide inscription feature

--- apps/ledger-live-desktop/src/newArch/features/Collectibles/Ordinals/components/Inscriptions/DetailsDrawer/Actions.tsx
+++ apps/ledger-live-desktop/src/newArch/features/Collectibles/Ordinals/components/Inscriptions/DetailsDrawer/Actions.tsx
@@ -5,23 +5,33 @@ import { useTranslation } from "react-i18next";
 import { ExternalViewerButton } from "LLD/features/Collectibles/components/DetailDrawer/components";
 import { SimpleHashNft } from "@ledgerhq/live-nft/api/types";
 import { Account } from "@ledgerhq/types-live";
+import { useHideInscriptions } from "LLD/features/Collectibles/hooks/useHideInscriptions";
 
 type ActionsProps = {
   inscription: SimpleHashNft;
   account: Account;
+  onModalClose: () => void;
 };
 
-const Actions: React.FC<ActionsProps> = ({ inscription, account }) => {
+const Actions: React.FC<ActionsProps> = ({ inscription, account, onModalClose }) => {
   const { t } = useTranslation();
+  const { openConfirmHideInscriptionModal } = useHideInscriptions();
+
+  const onClick = () => {
+    openConfirmHideInscriptionModal(
+      inscription.name || inscription.contract.name || "",
+      String(inscription.extra_metadata?.ordinal_details?.inscription_id),
+      onModalClose,
+    );
+  };
+
   return (
     <Flex alignItems="center" columnGap={12}>
       <Button
         outlineGrey
         style={{ flex: 1, justifyContent: "center" }}
         my={4}
-        onClick={() => {
-          /* TODO */
-        }}
+        onClick={onClick}
         center
       >
         <Flex columnGap={1}>
