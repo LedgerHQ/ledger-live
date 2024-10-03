f412b48f5734f57df6ba1deded5ce627e2f69d12
Theophile Sandoz <theophile.sandoz@ledger.fr>
2024-10-03 11:42:08 +0200

    feat(llm): style the modal

--- apps/ledger-live-mobile/src/screens/ReceiveFunds/NeedMemoTagModal/NeedMemoTagModal.tsx
+++ apps/ledger-live-mobile/src/screens/ReceiveFunds/NeedMemoTagModal/NeedMemoTagModal.tsx
@@ -1,31 +1,50 @@
+import { useTheme } from "styled-components/native";
 import React, { useState } from "react";
 import { Linking } from "react-native";
-import { BottomDrawer, Button, Icons } from "@ledgerhq/native-ui";
+import { BottomDrawer, Button, Flex, Icons, Text } from "@ledgerhq/native-ui";
 import { useTranslation } from "react-i18next";
 import { urls } from "~/utils/urls";
 
 export function NeedMemoTagModal() {
+  const { colors } = useTheme();
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
   return (
     <>
-      <Button type="accent" onPress={() => setIsOpen(true)}>
+      <Button type="accent" onPress={openModal}>
         {t("transfer.receive.memoTag.link")}
       </Button>
-      <BottomDrawer
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        Icon={() => <Icons.InformationFill size="L" color="primary.c80" />}
-        title={t("transfer.receive.memoTag.title")}
-        description={t("transfer.receive.memoTag.description")}
-      >
-        <Button type="main" size="large" onPress={() => setIsOpen(false)}>
+
+      <BottomDrawer isOpen={isOpen} onClose={closeModal}>
+        <Flex alignItems="center" mb={7}>
+          <Flex
+            backgroundColor={colors.opacityDefault.c05}
+            borderColor="transparent"
+            borderRadius={999}
+            borderWidth={16}
+          >
+            <Icons.InformationFill size="L" color="primary.c80" />
+          </Flex>
+        </Flex>
+
+        <Text variant="h4" textAlign="center" mb={6}>
+          {t("transfer.receive.memoTag.title")}
+        </Text>
+
+        <Text variant="bodyLineHeight" textAlign="center" color="neutral.c80" mb={8}>
+          {t("transfer.receive.memoTag.description")}
+        </Text>
+
+        <Button type="main" size="large" mb={6} onPress={closeModal}>
           {t("transfer.receive.memoTag.cta")}
         </Button>
 
         <Button
           type="accent"
+          size="large"
           Icon={() => <Icons.ExternalLink size="S" color="primary.c80" />}
           onPress={() => Linking.openURL(urls.memoTag)}
         >
