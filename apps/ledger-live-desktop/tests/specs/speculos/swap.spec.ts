68633e0912a7181c8d6180e8b794436c7cd808c7
Benjamin Haramboure <benjamin.haramboure@ledger.fr>
2024-10-04 18:01:32 +0200

    prettier

--- apps/ledger-live-desktop/tests/specs/speculos/swap.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/swap.spec.ts
@@ -5,6 +5,8 @@ import { setExchangeDependencies } from "@ledgerhq/live-common/e2e/speculos";
 import { Fee } from "tests/enum/Fee";
 import { Swap } from "tests/models/Swap";
 import { Provider, Rates } from "tests/enum/Swap";
+import { addTmsLink } from "tests/utils/allureUtils";
+import { getDescription } from "tests/utils/customJsonReporter";
 
 const swaps = [
   {
@@ -15,6 +17,7 @@ const swaps = [
       Fee.MEDIUM,
       Provider.CHANGELLY,
     ),
+    xrayTicket: "B2CQA-2750",
   },
   {
     swap: new Swap(
@@ -24,15 +27,19 @@ const swaps = [
       Fee.MEDIUM,
       Provider.CHANGELLY,
     ),
+    xrayTicket: "B2CQA-2744",
   },
   {
-    swap: new Swap(Account.ETH_USDT_1, Account.ETH_1, "35", Fee.MEDIUM, Provider.CHANGELLY),
+    swap: new Swap(Account.ETH_USDT_1, Account.ETH_1, "40", Fee.MEDIUM, Provider.CHANGELLY),
+    xrayTicket: "B2CQA-2752",
   },
   {
     swap: new Swap(Account.ETH_1, Account.SOL_1, "0.018", Fee.MEDIUM, Provider.CHANGELLY),
+    xrayTicket: "B2CQA-2748",
   },
   {
     swap: new Swap(Account.ETH_1, Account.ETH_USDT_1, "0.02", Fee.MEDIUM, Provider.CHANGELLY),
+    xrayTicket: "B2CQA-2749",
   },
   {
     swap: new Swap(
@@ -42,6 +49,7 @@ const swaps = [
       Fee.MEDIUM,
       Provider.CHANGELLY,
     ),
+    xrayTicket: "B2CQA-2747",
   },
   {
     swap: new Swap(
@@ -51,24 +59,27 @@ const swaps = [
       Fee.MEDIUM,
       Provider.CHANGELLY,
     ),
+    xrayTicket: "B2CQA-2746",
   },
   {
     swap: new Swap(
       Account.ETH_USDT_1,
       Account.BTC_NATIVE_SEGWIT_1,
-      "35",
+      "40",
       Fee.MEDIUM,
       Provider.CHANGELLY,
     ),
+    xrayTicket: "B2CQA-2753",
   },
   {
     swap: new Swap(Account.ETH_USDT_1, Account.SOL_1, "40", Fee.MEDIUM, Provider.CHANGELLY),
+    xrayTicket: "B2CQA-2751",
   },
 ];
 
 const app: AppInfos = AppInfos.EXCHANGE;
 
-for (const { swap } of swaps) {
+for (const { swap, xrayTicket } of swaps) {
   test.describe("Swap", () => {
     test.beforeAll(async () => {
       process.env.SWAP_DISABLE_APPS_INSTALL = "true";
@@ -97,28 +108,36 @@ for (const { swap } of swaps) {
       speculosApp: app,
     });
 
-    test(`Swap ${swap.accountToDebit.currency.name} to ${swap.accountToCredit.currency.name}`, async ({
-      app,
-      electronApp,
-    }) => {
-      const rate = Rates.FLOAT;
-      await app.layout.goToSwap();
-      await app.swap.selectAccountToSwapFrom(swap.accountToDebit);
-      await app.swap.selectCurrencyToSwapTo(swap.accountToCredit.currency.name);
-      await app.swap.fillInOriginAmount(swap.amount);
-      await app.swap.selectExchangeQuote(swap.provider.name, rate);
-      await app.swap.clickExchangeButton(electronApp, swap.provider.uiName);
-      const amountTo = await app.swapDrawer.getAmountToReceive();
-      const fees = await app.swapDrawer.getFees();
-      swap.setAmountToReceive(amountTo);
-      swap.setFeesAmount(fees);
-      await app.swapDrawer.verifyAmountToReceive(amountTo);
-      await app.swapDrawer.verifyAmountSent(swap.amount, swap.accountToDebit.currency.ticker);
-      await app.swapDrawer.verifySourceAccount(swap.accountToDebit.currency.name);
-      await app.swapDrawer.verifyTargetCurrency(swap.accountToCredit.currency.name);
-      await app.swapDrawer.verifyProvider(swap.provider.name);
-      await app.speculos.verifyAmountsAndRejectSwap(swap);
-      await app.swapDrawer.verifyExchangeErrorTextContent("Operation denied on device");
-    });
+    test(
+      `Swap ${swap.accountToDebit.currency.name} to ${swap.accountToCredit.currency.name}`,
+      {
+        annotation: {
+          type: "TMS",
+          description: xrayTicket,
+        },
+      },
+      async ({ app, electronApp }) => {
+        await addTmsLink(getDescription(test.info().annotations).split(", "));
+        const rate = Rates.FLOAT;
+        await app.layout.goToSwap();
+        await app.swap.waitForPageNetworkidleState();
+        await app.swap.selectAccountToSwapFrom(swap.accountToDebit);
+        await app.swap.selectCurrencyToSwapTo(swap.accountToCredit.currency.name);
+        await app.swap.fillInOriginAmount(swap.amount);
+        await app.swap.selectExchangeQuote(swap.provider.name, rate);
+        await app.swap.clickExchangeButton(electronApp, swap.provider.uiName);
+        const amountTo = await app.swapDrawer.getAmountToReceive();
+        const fees = await app.swapDrawer.getFees();
+        swap.setAmountToReceive(amountTo);
+        swap.setFeesAmount(fees);
+        await app.swapDrawer.verifyAmountToReceive(amountTo);
+        await app.swapDrawer.verifyAmountSent(swap.amount, swap.accountToDebit.currency.ticker);
+        await app.swapDrawer.verifySourceAccount(swap.accountToDebit.currency.name);
+        await app.swapDrawer.verifyTargetCurrency(swap.accountToCredit.currency.name);
+        await app.swapDrawer.verifyProvider(swap.provider.name);
+        await app.speculos.verifyAmountsAndRejectSwap(swap);
+        await app.swapDrawer.verifyExchangeErrorTextContent("Operation denied on device");
+      },
+    );
   });
 }
