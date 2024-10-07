590f7702cf1a4b53d7199688f4389588c91f26de
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-07 20:48:17 +0200

    lint

--- apps/ledger-live-desktop/src/renderer/families/stacks/AccountSubHeader.tsx
+++ apps/ledger-live-desktop/src/renderer/families/stacks/AccountSubHeader.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import AccountSubHeader from "../../components/AccountSubHeader/index";
 
 export default function StacksAccountSubHeader() {
-  return <AccountSubHeader family="Stacks" team="Zondax" teamLink="https://zondax.ch/"></AccountSubHeader>;
+  return (
+    <AccountSubHeader
+      family="Stacks"
+      team="Zondax"
+      teamLink="https://zondax.ch/"
+    ></AccountSubHeader>
+  );
 }
