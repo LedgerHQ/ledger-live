4931a7a957e8d89bc26a1ff235972dd93d7d3f16
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-07 10:20:04 +0200

    working display of team link

--- apps/ledger-live-mobile/src/components/AccountSubHeader/AccountSubHeaderDrawer.tsx
+++ apps/ledger-live-mobile/src/components/AccountSubHeader/AccountSubHeaderDrawer.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { Box, Text } from "@ledgerhq/native-ui";
+import { Linking } from "react-native";
+
+import { Box, Flex, Text } from "@ledgerhq/native-ui";
+
+import ExternalLink from "../ExternalLink";
 import QueuedDrawer from "../QueuedDrawer";
 
 type Props = {
@@ -8,9 +12,33 @@ type Props = {
   onClose: () => void;
   family: string;
   team: string;
+  teamLink?: string;
 };
 
-export default function AccountSubHeaderDrawer({ isOpen, onClose, family, team }: Props) {
+type TeamLinkProps = {
+  team: string;
+  teamLink: string;
+};
+
+const TeamLink = ({ team, teamLink }: TeamLinkProps) => {
+  const { t } = useTranslation();
+
+  const onOpenTeam = useCallback(() => {
+    Linking.openURL(teamLink);
+  }, [teamLink]);
+
+  return (
+    <>
+      <Flex flexDirection="row" alignItems="center">
+        <Text>{t("account.subHeader.drawer.descriptionLink.integration")} </Text>
+        <ExternalLink type="color" text={team} onPress={onOpenTeam} />
+      </Flex>
+      <Text>{t("account.subHeader.drawer.descriptionLink.collab")}</Text>
+    </>
+  );
+};
+
+export default function AccountSubHeaderDrawer({ isOpen, onClose, family, team, teamLink }: Props) {
   const { t } = useTranslation();
   return (
     <QueuedDrawer
@@ -26,9 +54,13 @@ export default function AccountSubHeaderDrawer({ isOpen, onClose, family, team }
         <Text variant={"paragraph"} color={"neutral.c100"} mt={2} textAlign="center">
           {t("account.subHeader.drawer.description2")}
         </Text>
-        <Text variant={"paragraph"} color={"neutral.c100"} mt={2} textAlign="center">
-          {t("account.subHeader.drawer.description3", { team })}
-        </Text>
+        {teamLink ? (
+          <TeamLink team={team} teamLink={teamLink} />
+        ) : (
+          <Text variant={"paragraph"} color={"neutral.c100"} mt={2} textAlign="center">
+            {t("account.subHeader.drawer.description3", { team })}
+          </Text>
+        )}
       </Box>
     </QueuedDrawer>
   );
