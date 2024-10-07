408f1cba94ef5c5db647ee48657900eee9f72659
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-07 10:20:04 +0200

    working display of team link

--- apps/ledger-live-mobile/src/families/internet_computer/AccountSubHeader.tsx
+++ apps/ledger-live-mobile/src/families/internet_computer/AccountSubHeader.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import AccountSubHeader from "~/components/AccountSubHeader";
 
 function InternetComputerAccountSubHeader() {
-  return <AccountSubHeader family="Internet Computer" team="Zondax" />;
+  return (
+    <AccountSubHeader family="Internet Computer" team="Zondax" teamLink="https://zondax.ch/" />
+  );
 }
 
 export default InternetComputerAccountSubHeader;
