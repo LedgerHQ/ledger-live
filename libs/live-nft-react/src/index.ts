00cab1db1d67eb0cf35059eeeb9e2d8bd328f8f3
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2024-10-08 09:53:14 +0200

    feat: useCheckNftAccount in [LIB] (#7891)
    * feat: useCheckNftAccount in [LIB]
    
    * :sparkles:(lld): add suggestion
    
    * :sparkles:(lld): fix what rebase broke
    
    ---------
    
    Co-authored-by: Lucas Werey <lucas.werey@ledger.fr>

--- libs/live-nft-react/src/index.ts
+++ libs/live-nft-react/src/index.ts
@@ -7,3 +7,5 @@ export * from "./hooks/useCheckSpamScore";
 export * from "./hooks/useFetchOrdinals";
 export * from "./hooks/useFetchOrdinalByTokenId";
 export * from "./hooks/helpers/ordinals";
+export * from "./hooks/useCheckNftAccount";
+export * from "./hooks/helpers/index";
--- libs/live-nft-react/src/tools/helperTests.tsx
+++ libs/live-nft-react/src/tools/helperTests.tsx
@@ -1,8 +1,41 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { ReactNode } from "react";
+import { BigNumber } from "bignumber.js";
+import { NFTs } from "@ledgerhq/coin-framework/mocks/fixtures/nfts";
+import { encodeNftId } from "@ledgerhq/coin-framework/nft/nftId";
 
 const queryClient = new QueryClient();
 
 export const wrapper = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
+
+export type FakeNFTRaw = {
+  id: string;
+  tokenId: string;
+  amount: BigNumber;
+  contract: string;
+  standard: "ERC721";
+  currencyId: string;
+  metadata: undefined;
+};
+
+export const generateNftsOwned = () => {
+  const nfts: FakeNFTRaw[] = [];
+
+  NFTs.forEach(nft => {
+    for (let i = 1; i <= 20; i++) {
+      nfts.push({
+        id: encodeNftId("foo", nft.collection.contract, String(i), "ethereum"),
+        tokenId: String(i),
+        amount: new BigNumber(0),
+        contract: nft.collection.contract,
+        standard: "ERC721" as const,
+        currencyId: "ethereum",
+        metadata: undefined,
+      });
+    }
+  });
+
+  return nfts;
+};
