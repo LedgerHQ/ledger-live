a0a47235e6de83126d3628b3d7b95caf7892a1a5
Mounir Hamzaoui <mounir.hamzaoui@ledger.fr>
2024-10-08 15:49:47 +0200

    feat: add memo tag information section in LLD recieve flow

---
+++ apps/ledger-live-desktop/src/newArch/features/MemoTag/constants.ts
@@ -0,0 +1,14 @@
+export const MEMO_TAG_LEARN_MORE_LINK = "https://support.ledger.com/article/4409603715217-zd";
+export const MEMO_TAG_COINS: string[] = [
+  "ripple",
+  "stellar",
+  "cosmos",
+  "hedera",
+  "injective",
+  "crypto_org",
+  "crypto_org_croeseid",
+  "stacks",
+  "ton",
+  "eos",
+  "bsc",
+];
