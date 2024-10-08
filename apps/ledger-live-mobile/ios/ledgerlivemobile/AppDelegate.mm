1f6a076027e0b8d1944aa850945c341fb758bbd2
Come Grellard <come.grellard@ledger.fr>
2024-10-08 20:28:50 +0200

    android working

--- apps/ledger-live-mobile/ios/ledgerlivemobile/AppDelegate.mm
+++ apps/ledger-live-mobile/ios/ledgerlivemobile/AppDelegate.mm
@@ -10,18 +10,6 @@
 #import "BrazeReactBridge.h"
 
 #import <Firebase.h>
-#if DEBUG
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitLayoutPlugin/SKDescriptorMapper.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperPerformancePlugin.h> // react-native-performance-plugin
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#endif
-#endif
 
 
 @implementation AppDelegate
