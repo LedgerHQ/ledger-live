cb18661271f8161f3845ba3cbe5a9426752db6fc
Lucas Werey <lucas.werey@ledger.fr>
2024-10-08 18:11:09 +0200

    :sparkles:(lld): rework coin control UI

---
+++ apps/ledger-live-desktop/src/newArch/features/Collectibles/Ordinals/components/CoinControlModal/components/index.ts
@@ -0,0 +1,7 @@
+import { UtxoPickerHeaderSection } from "./UtxoPickerHeaderSection";
+import { UtxoStrategyPicker } from "./UtxoStrategyPicker";
+import { Row } from "./Row/index";
+import Footer from "./Footer";
+import { ExcludeAssetsSection } from "./ExcludeAssetsSection";
+
+export { UtxoPickerHeaderSection, UtxoStrategyPicker, Row, Footer, ExcludeAssetsSection };
