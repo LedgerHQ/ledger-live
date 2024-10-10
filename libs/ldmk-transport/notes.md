cccdd2f1e448e46750779197bc841e0d77b174f1
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2024-10-10 16:51:52 +0200

    feat: init ldmk-transport pkg

---
+++ libs/ldmk-transport/notes.md
@@ -0,0 +1,7 @@
+[x] add alias to pkg to json (you can test by running commands)
+[x] look into device core for other files
+[ ] meat
+[ ] import ldmk
+[ ] like poc5
+
+- to test at the end run pnpm build:libs and check if pkg gets built ()
---
+++ libs/ldmk-transport/src/index.tsx
@@ -0,0 +1,146 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Transport from "@ledgerhq/hw-transport";
+import {
+  DeviceSdkBuilder,
+  ConsoleLogger,
+  type DeviceSdk,
+  type DeviceSessionState,
+  DeviceStatus,
+  LogLevel,
+} from "@ledgerhq/device-management-kit";
+import { BehaviorSubject, firstValueFrom } from "rxjs";
+
+const deviceSdk = new DeviceSdkBuilder().addLogger(new ConsoleLogger(LogLevel.Debug)).build();
+
+export const DeviceSdkContext = createContext<DeviceSdk>(deviceSdk);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const DeviceSdkProvider: React.FC<Props> = ({ children }) => {
+  return <DeviceSdkContext.Provider value={deviceSdk}>{children}</DeviceSdkContext.Provider>;
+};
+
+export const useDeviceSdk = (): DeviceSdk => {
+  return useContext(DeviceSdkContext);
+};
+
+export const useDeviceSessionState = (): DeviceSessionState | undefined => {
+  const sdk = useDeviceSdk();
+  const [sessionState, setSessionState] = useState<DeviceSessionState | undefined>(undefined);
+
+  useEffect(() => {
+    const subscription = activeDeviceSessionSubject.subscribe({
+      next: session => {
+        if (session) {
+          const { sessionId } = session;
+          const stateSubscription = sdk.getDeviceSessionState({ sessionId }).subscribe({
+            next: (state: { deviceStatus: any }) => {
+              state.deviceStatus !== DeviceStatus.NOT_CONNECTED
+                ? setSessionState(state)
+                : setSessionState(undefined);
+            },
+            error: (error: any) => console.error("[useDeviceSessionState] error", error),
+          });
+          return () => stateSubscription.unsubscribe();
+        } else {
+          setSessionState(undefined);
+        }
+      },
+      error: error => console.error("[useDeviceSessionState] subscription error", error),
+    });
+
+    return () => subscription.unsubscribe();
+  }, [sdk]);
+
+  return sessionState;
+};
+
+const activeDeviceSessionSubject: BehaviorSubject<{
+  sessionId: string;
+  transport: DeviceManagementKitTransport;
+} | null> = new BehaviorSubject<{
+  sessionId: string;
+  transport: DeviceManagementKitTransport;
+} | null>(null);
+
+export class DeviceManagementKitTransport extends Transport {
+  readonly sessionId: string;
+  readonly sdk: DeviceSdk;
+
+  constructor(sdk: DeviceSdk, sessionId: string) {
+    super();
+    this.sessionId = sessionId;
+    this.sdk = sdk;
+    this.listenToDisconnect();
+  }
+
+  listenToDisconnect = () => {
+    const subscription = this.sdk.getDeviceSessionState({ sessionId: this.sessionId }).subscribe({
+      next: (state: { deviceStatus: any }) => {
+        if (state.deviceStatus === DeviceStatus.NOT_CONNECTED) {
+          console.log(
+            "[SDKTransport][listenToDisconnect] device not connected, session ended, closing transport",
+          );
+          activeDeviceSessionSubject.next(null);
+          this.emit("disconnect");
+        }
+      },
+      error: (error: any) => {
+        console.error("[SDKTransport][listenToDisconnect] error", error);
+        this.emit("disconnect");
+        subscription.unsubscribe();
+      },
+      complete: () => {
+        console.log("[SDKTransport][listenToDisconnect] complete");
+        this.emit("disconnect");
+        subscription.unsubscribe();
+      },
+    });
+  };
+
+  static async open(): Promise<DeviceManagementKitTransport> {
+    console.log("[SDKTransport][open]");
+    const activeSessionId = activeDeviceSessionSubject.value?.sessionId;
+    if (activeSessionId) {
+      console.log("[SDKTransport][open] checking existing session", activeSessionId);
+      let deviceSessionState: DeviceSessionState | null = null;
+      try {
+        deviceSessionState = await firstValueFrom(
+          deviceSdk.getDeviceSessionState({ sessionId: activeSessionId }),
+        );
+      } catch (e) {
+        console.error("[SDKTransport][open] error getting device session state", e);
+      }
+      if (deviceSessionState?.deviceStatus !== DeviceStatus.NOT_CONNECTED) {
+        console.log(
+          "[SDKTransport][open] reusing existing session and instantiating a new SdkTransport",
+        );
+        return activeDeviceSessionSubject.value.transport;
+      }
+    }
+
+    console.log("[SDKTransport][open] no active session found, starting discovery");
+    const discoveredDevice: { id: string } = await firstValueFrom(deviceSdk.startDiscovering());
+    const connectedSessionId = await deviceSdk.connect({ deviceId: discoveredDevice.id });
+    console.log("[SDKTransport][open] connected");
+    const transport = new DeviceManagementKitTransport(deviceSdk, connectedSessionId);
+    activeDeviceSessionSubject.next({ sessionId: connectedSessionId, transport });
+    return transport;
+  }
+
+  close: () => Promise<void> = () => Promise.resolve();
+
+  async exchange(apdu: Buffer): Promise<Buffer> {
+    console.log("[SDKTransport][exchange] =>", apdu);
+    const apduUint8Array = new Uint8Array(apdu);
+    const apduResponse = await this.sdk.sendApdu({
+      sessionId: this.sessionId,
+      apdu: apduUint8Array,
+    });
+    const response = Buffer.from([...apduResponse.data, ...apduResponse.statusCode]);
+    console.log("[SDKTransport][exchange] <=", response);
+    return response;
+  }
+}
