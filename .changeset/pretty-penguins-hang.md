1a0fa20da7509cdde1141002a2be8e9d8458b27d
Liviu Ciulinaru <liviuciulinaru@users.noreply.github.com>
2024-10-11 16:45:14 +0700

    feat(live-14205): add ptxSwapCoreExperiment flag (#8039)
    feat: add ptxSwapCoreExperiment flag
    
    feat: add unit test
    
    chore: rm logs
    
    chore: rm logs
    
    chore: add changeset
    
    fix: rm the ptxThorswap flag
    
    chore: rm logs
    
    chore: fix lint
    
    fix: types
    
    fix: types
    
    fix: types
    
    fix: pr Bcomm
    
    fix: pr comm
    
    fix: pr comm
    
    fix: pr comm
    
    fix: secp256k1 import
    
    fix: roll back

---
+++ .changeset/pretty-penguins-hang.md
@@ -0,0 +1,7 @@
+---
+"@ledgerhq/types-live": patch
+"ledger-live-desktop": patch
+"@ledgerhq/live-common": patch
+---
+
+add ptxSwapCoreExperiment flag
--- libs/ledger-live-common/src/featureFlags/defaultFeatures.ts
+++ libs/ledger-live-common/src/featureFlags/defaultFeatures.ts
@@ -6,8 +6,8 @@ import {
   Features,
 } from "@ledgerhq/types-live";
 import reduce from "lodash/reduce";
-import { formatToFirebaseFeatureId } from "./firebaseFeatureFlags";
 import { BUY_SELL_UI_APP_ID } from "../wallet-api/constants";
+import { formatToFirebaseFeatureId } from "./firebaseFeatureFlags";
 
 /**
  * Default disabled feature.
@@ -395,6 +395,7 @@ export const DEFAULT_FEATURES: Features = {
     enabled: false,
   },
   ptxCard: DEFAULT_FEATURE,
+
   ptxSwapLiveAppDemoZero: {
     enabled: false,
     params: {
@@ -416,9 +417,16 @@ export const DEFAULT_FEATURES: Features = {
     },
   },
 
+  ptxSwapCoreExperiment: {
+    enabled: false,
+    params: {
+      variant: "Demo0",
+      manifest_id: "swap-live-app-demo-0",
+    },
+  },
+
   ptxSwapMoonpayProvider: DEFAULT_FEATURE,
   ptxSwapExodusProvider: DEFAULT_FEATURE,
-  ptxSwapThorswapProvider: DEFAULT_FEATURE,
 
   llmAnalyticsOptInPrompt: {
     enabled: false,
