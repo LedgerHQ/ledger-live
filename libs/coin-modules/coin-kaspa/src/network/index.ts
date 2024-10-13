23f39da6b7133e030ac25309eb4dec4ab0a4501b
lAmeR1 <lamer1@github.com>
2024-10-13 15:24:16 +0200

    Update network exports to include balance and UTXO functions
    Removed redundant SDK exports and added exports for getting balance and UTXOs. This improves module utility and aligns with new API capabilities.

--- libs/coin-modules/coin-kaspa/src/network/index.ts
+++ libs/coin-modules/coin-kaspa/src/network/index.ts
@@ -1,7 +1,3 @@
-export {
-  fee,
-  getOperations,
-  submit,
-  disconnect, // if using persisting connection
-} from "./sdk";
 export { getFeeEstimate } from "./indexer-api/getFeeEstimate";
+export { getBalanceForAddress } from "./indexer-api/getBalanceForAddress";
+export { getUtxosForAddress } from "./indexer-api/getUtxosForAddress";
