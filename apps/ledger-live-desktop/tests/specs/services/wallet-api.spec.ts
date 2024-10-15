4652e31f4f80df1d6629cd87e071b656995ef5d7
Kieran Allen <kieran.allen@ledger.fr>
2024-10-15 11:30:10 +0100

    fix: update test

--- apps/ledger-live-desktop/tests/specs/services/wallet-api.spec.ts
+++ apps/ledger-live-desktop/tests/specs/services/wallet-api.spec.ts
@@ -240,7 +240,7 @@ test("Wallet API methods @smoke", async ({ page }) => {
     await drawer.selectCurrency("tether usd");
     // Test name and balance for tokens
     await expect(drawer.getAccountButton("tether usd", 2)).toContainText(
-      "Ethereum 3 (USDT)71,8174 USDT", // Special space present in the actual rendered element apparently
+      "Ethereum 3 (USDT)71.8174 USDT", // Special space present in the actual rendered element apparently
     );
     await drawer.back();
 
