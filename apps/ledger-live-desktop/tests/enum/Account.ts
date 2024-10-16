20dfb5edbbe689f6ace4c47b60f0a7045ccad71a
Victor Alber <victor.alber@ledger.fr>
2024-10-16 14:33:13 +0200

    test: ⚡️ Using the cli to build app.json for send e2e scenatios

--- apps/ledger-live-desktop/tests/enum/Account.ts
+++ apps/ledger-live-desktop/tests/enum/Account.ts
@@ -22,66 +22,88 @@ export class Account {
     Currency.BTC,
     "Bitcoin 2",
     "bc1q7ezsfc44adw2gyzqjmwhuh2e83uk8u5hrw590r",
+    undefined,
+    1,
   );
 
   static readonly BTC_LEGACY_1 = new Account(
     Currency.BTC,
     "Bitcoin Legacy 1",
     "1FMx2XwRHKXhbJcaekgPqN111wtRftkHyw",
+    undefined,
+    0,
   );
 
   static readonly BTC_LEGACY_2 = new Account(
     Currency.BTC,
     "Bitcoin Legacy 2",
     "1Nen8hLiZysV6TWi6o6hTBp6dtkRXrJyWh",
+    undefined,
+    1,
   );
 
   static readonly BTC_SEGWIT_1 = new Account(
     Currency.BTC,
     "Bitcoin Segwit 1",
     "3C7fQ47BiZuZN7V2WTgHLq4sqpb5BEp91i",
+    undefined,
+    0,
   );
 
   static readonly BTC_SEGWIT_2 = new Account(
     Currency.BTC,
     "Bitcoin Segwit 2",
     "3B5psxZfUkU6AzPJoirgPQS7dC9vv7ohcK",
+    undefined,
+    1,
   );
 
   static readonly BTC_TAPROOT_1 = new Account(
     Currency.BTC,
     "Bitcoin Taproot 1",
     "bc1pv4aytu7u4pk4nvelymxp65vndxqh4e3xsn0v7cguy6t98k5vynks5td23j",
+    undefined,
+    0,
   );
 
   static readonly BTC_TAPROOT_2 = new Account(
     Currency.BTC,
     "Bitcoin Taproot 2",
     "bc1pywrw64mkvpzxkje5fkcz7vafj03yyzyvx4vg8wy2klvq8wtyfe7sq2jgzd",
+    undefined,
+    1,
   );
 
   static readonly tBTC_1 = new Account(
     Currency.tBTC,
     "Bitcoin Testnet 1",
     "tb1qxmwe6n93fls8r69837cmyt6ua406xaen9hy24d",
+    undefined,
+    0,
   );
 
   static readonly tBTC_2 = new Account(
     Currency.tBTC,
     "Bitcoin Testnet 2",
     "tb1qyjr6hsx3wvsdq998zvn5cusqkdyfvvnpnsz6a5",
+    undefined,
+    1,
   );
 
   static readonly DOGE_1 = new Account(
     Currency.DOGE,
     "Dogecoin 1",
     "DTWxYBoP319u1KBUFE9Z6FQxMKJRT1U7tQ",
+    undefined,
+    0,
   );
 
   static readonly DOGE_2 = new Account(
     Currency.DOGE,
     "Dogecoin 2",
     "DKbRbGP5spDCaRSGLjWGWZGbhE19nQ1LoK",
+    undefined,
+    1,
   );
 
   static readonly ETH_1 = new Account(
@@ -96,36 +118,48 @@ export class Account {
     Currency.ETH,
     "Ethereum 2",
     "0x43047a5023D55a8658Fcb1c1Cea468311AdAA3Ad",
+    undefined,
+    1,
   );
 
   static readonly ETH_3 = new Account(
     Currency.ETH,
     "Ethereum 3",
     "0x50F293e4C3815F5214dac1Bae8F8FC3F24b8BA59",
+    undefined,
+    2,
   );
 
   static readonly tETH_1 = new Account(
     Currency.tETH,
     "Ethereum Holesky 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly tETH_2 = new Account(
     Currency.tETH,
     "Ethereum Holesky 2",
     "0x43047a5023D55a8658Fcb1c1Cea468311AdAA3Ad",
+    undefined,
+    1,
   );
 
   static readonly sep_ETH_1 = new Account(
     Currency.sepETH,
     "Ethereum Sepolia 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly sep_ETH_2 = new Account(
     Currency.sepETH,
     "Ethereum Sepolia 2",
     "0x43047a5023D55a8658Fcb1c1Cea468311AdAA3Ad",
+    undefined,
+    1,
   );
 
   static readonly DOT_1 = new Account(
@@ -140,12 +174,16 @@ export class Account {
     Currency.DOT,
     "Polkadot 2",
     "12fY9vqzD8j1uvqSRx9y3gXRA1S3bwr5xunBVZvx1eeZFaHY",
+    undefined,
+    1,
   );
 
   static readonly DOT_3 = new Account(
     Currency.DOT,
     "Polkadot 3",
     "1532VyvZyyMUmLfhMUYh2KRVLzwYfHcxjtejyX4swkpG82BX",
+    undefined,
+    2,
   );
 
   static readonly SOL_1 = new Account(
@@ -188,7 +226,13 @@ export class Account {
     0,
   );
 
-  static readonly XRP_2 = new Account(Currency.XRP, "XRP 2", "r36cgyrfC1xSQMvjuiSeFJEcBTq31imZS");
+  static readonly XRP_2 = new Account(
+    Currency.XRP,
+    "XRP 2",
+    "r36cgyrfC1xSQMvjuiSeFJEcBTq31imZS",
+    undefined,
+    1,
+  );
 
   static readonly ADA_1 = new Account(
     Currency.ADA,
@@ -201,6 +245,8 @@ export class Account {
     Currency.ADA,
     "Cardano 2",
     "addr1qyjd47qfktpza4ycjddjadaarzwdumwrqws0xage8gvsmrq5ghmxjmdj4eylq78wur2gmm7gtqfq49v6mtdkaqwqzy2qwzv6ac",
+    undefined,
+    1,
   );
 
   static readonly ALGO_1 = new Account(
@@ -215,12 +261,16 @@ export class Account {
     Currency.ALGO,
     "Algorand 2",
     "6TFDU3BYQ2FO32SOYQDTHDW5XKGEYH4FCT34ZQRHFPJRVMLEQWOO2OEUU4",
+    undefined,
+    1,
   );
 
   static readonly ALGO_3 = new Account(
     Currency.ALGO,
     "Algorand 3",
     "3ASRTAN6KCZCICTIFQ5N2UBOSSBOZ7WFSOI2CJEJ4ESK532RODQZ7KCSOA",
+    undefined,
+    2,
   );
 
   static readonly XLM_1 = new Account(
@@ -235,6 +285,8 @@ export class Account {
     Currency.XLM,
     "Stellar 2",
     "GCTGRCFN7AT6NW4DZVI4QN55BRNQA64TXEZSMYPE7BNUZMLMVISXT652",
+    undefined,
+    1,
   );
 
   static readonly BCH_1 = new Account(
@@ -249,6 +301,8 @@ export class Account {
     Currency.BCH,
     "Bitcoin Cash 2",
     "qp2ka732e6h82djvr5ge4vtru0cl3g8lxqtyfmzzl9",
+    undefined,
+    1,
   );
 
   static readonly ATOM_1 = new Account(
@@ -279,11 +333,15 @@ export class Account {
     Currency.XTZ,
     "Tezos 2",
     "tz1g3uEPZ9T3AhUZDTbGW9V43JRfizJmSnPv",
+    undefined,
+    1,
   );
   static readonly POL_1 = new Account(
     Currency.POL,
     "Polygon 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly BSC_1 = new Account(
@@ -315,6 +373,7 @@ export class Account {
     "Ethereum 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
     AccountType.ERC20,
+    0,
   );
 
   static readonly ETH_USDT_2 = new Account(
@@ -322,36 +381,47 @@ export class Account {
     "Ethereum 2",
     "0x43047a5023D55a8658Fcb1c1Cea468311AdAA3Ad",
     AccountType.ERC20,
+    1,
   );
 
   static readonly ETH_LIDO = new Account(
     Currency.ETH_LIDO,
     "Ethereum 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    AccountType.ERC20,
+    0,
   );
 
   static readonly XLM_USCD = new Account(
     Currency.XLM_USCD,
     "Stellar 1",
     "GCAGRZ7XABYSXV7CPFSFWQIUK6XFXECBPWP2SGMVOB2KFWN7YM4TDGSX",
+    undefined,
+    0,
   );
 
   static readonly ALGO_USDT_1 = new Account(
     Currency.ALGO_USDT,
     "Algorand 1",
     "HQ6YJWSVG3KVRE56V6UGWMUJLDVNPQUNXJBY7VJ56VMNMGIKVDTC7JEKOU",
+    undefined,
+    0,
   );
 
   static readonly ALGO_USDT_2 = new Account(
     Currency.ALGO_USDT,
     "Algorand 2",
     "6TFDU3BYQ2FO32SOYQDTHDW5XKGEYH4FCT34ZQRHFPJRVMLEQWOO2OEUU4",
+    undefined,
+    1,
   );
 
   static readonly ALGO_USDT_3 = new Account(
     Currency.ALGO_USDT,
     "Algorand 3",
     "3ASRTAN6KCZCICTIFQ5N2UBOSSBOZ7WFSOI2CJEJ4ESK532RODQZ7KCSOA",
+    undefined,
+    2,
   );
 
   static readonly TRX_USDT = new Account(
@@ -366,36 +436,48 @@ export class Account {
     Currency.TRX_BTT,
     "Tron 1",
     "TDUKFB9wj3P5f2iNvkRuaDDeWVkTdUVhs1",
+    AccountType.TRC20,
+    0,
   );
 
   static readonly BSC_BUSD_1 = new Account(
     Currency.BSC_BUSD,
     "Binance Smart Chain 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly BSC_BUSD_2 = new Account(
     Currency.BSC_BUSD,
     "Binance Smart Chain 2",
     "0x43047a5023D55a8658Fcb1c1Cea468311AdAA3Ad",
+    undefined,
+    1,
   );
 
   static readonly BSC_SHIBA = new Account(
     Currency.BSC_SHIBA,
     "Binance Smart Chain 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly POL_DAI_1 = new Account(
     Currency.POL_DAI,
     "Polygon 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly POL_UNI = new Account(
     Currency.POL_UNI,
     "Polygon 1",
     "0xB9051f83AC6e147924377BBEebd1Aa7aB43a67F6",
+    undefined,
+    0,
   );
 
   static readonly EMPTY = new Account(Currency.BTC, "Empty", " ");
@@ -404,6 +486,8 @@ export class Account {
     Currency.ETH,
     "Ethereum 2",
     "0x43047a5023d55a8658fcb1c1cea468311adaa3ad",
+    undefined,
+    1,
   );
 
   static readonly MULTIVERS_X_1 = new Account(
--- apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
+++ apps/ledger-live-desktop/tests/specs/speculos/add.account.spec.ts
@@ -4,23 +4,20 @@ import { addTmsLink } from "tests/utils/allureUtils";
 import { getDescription } from "../../utils/customJsonReporter";
 
 const currencies = [
-  {
-    currency: Currency.BTC,
-    xrayTicket: "B2CQA-2499, B2CQA-2644, B2CQA-2658, B2CQA-2672, B2CQA-929, B2CQA-786",
-  },
-  { currency: Currency.ETH, xrayTicket: "B2CQA-2503, B2CQA-2645, B2CQA-2659, B2CQA-2673" },
-  { currency: Currency.ETC, xrayTicket: "B2CQA-2502, B2CQA-2646, B2CQA-2660, B2CQA-2674" },
-  { currency: Currency.XRP, xrayTicket: "B2CQA-2505, B2CQA-2647, B2CQA-2661, B2CQA-2675" },
-  { currency: Currency.DOT, xrayTicket: "B2CQA-2504, B2CQA-2648, B2CQA-2662, B2CQA-2676" },
-  { currency: Currency.TRX, xrayTicket: "B2CQA-2508, B2CQA-2649, B2CQA-2663, B2CQA-2677" },
-  { currency: Currency.ADA, xrayTicket: "B2CQA-2500, B2CQA-2650, B2CQA-2664, B2CQA-2678" },
-  { currency: Currency.XLM, xrayTicket: "B2CQA-2506, B2CQA-2651, B2CQA-2665, B2CQA-2679" },
-  { currency: Currency.BCH, xrayTicket: "B2CQA-2498, B2CQA-2652, B2CQA-2666, B2CQA-2680" },
-  { currency: Currency.ALGO, xrayTicket: "B2CQA-2497, B2CQA-2653, B2CQA-2667, B2CQA-2681" },
-  { currency: Currency.ATOM, xrayTicket: "B2CQA-2501, B2CQA-2654, B2CQA-2668, B2CQA-2682" },
-  { currency: Currency.XTZ, xrayTicket: "B2CQA-2507, B2CQA-2655, B2CQA-2669, B2CQA-2683" },
-  { currency: Currency.SOL, xrayTicket: "B2CQA-2642, B2CQA-2656, B2CQA-2670, B2CQA-2684" },
-  { currency: Currency.TON, xrayTicket: "B2CQA-2643, B2CQA-2657, B2CQA-2671, B2CQA-2685" },
+  { currency: Currency.BTC, xrayTicket: "B2CQA-2499, B2CQA-2644, B2CQA-2672, B2CQA-786" },
+  { currency: Currency.ETH, xrayTicket: "B2CQA-2503, B2CQA-2645, B2CQA-2673" },
+  { currency: Currency.ETC, xrayTicket: "B2CQA-2502, B2CQA-2646, B2CQA-2674" },
+  { currency: Currency.XRP, xrayTicket: "B2CQA-2505, B2CQA-2647, B2CQA-2675" },
+  { currency: Currency.DOT, xrayTicket: "B2CQA-2504, B2CQA-2648, B2CQA-2676" },
+  { currency: Currency.TRX, xrayTicket: "B2CQA-2508, B2CQA-2649, B2CQA-2677" },
+  { currency: Currency.ADA, xrayTicket: "B2CQA-2500, B2CQA-2650, B2CQA-2678" },
+  { currency: Currency.XLM, xrayTicket: "B2CQA-2506, B2CQA-2651, B2CQA-2679" },
+  { currency: Currency.BCH, xrayTicket: "B2CQA-2498, B2CQA-2652, B2CQA-2680" },
+  { currency: Currency.ALGO, xrayTicket: "B2CQA-2497, B2CQA-2653, B2CQA-2681" },
+  { currency: Currency.ATOM, xrayTicket: "B2CQA-2501, B2CQA-2654, B2CQA-2682" },
+  { currency: Currency.XTZ, xrayTicket: "B2CQA-2507, B2CQA-2655, B2CQA-2683" },
+  { currency: Currency.SOL, xrayTicket: "B2CQA-2642, B2CQA-2656, B2CQA-2684" },
+  { currency: Currency.TON, xrayTicket: "B2CQA-2643, B2CQA-2657, B2CQA-2685" },
 ];
 
 for (const currency of currencies) {
