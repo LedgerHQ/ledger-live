2ec466e8575f1cfbf569053e02c63e69b9afffb9
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-16 16:36:26 +0200

    fix bridge.integration.test.ts

--- libs/ledger-live-common/src/families/hedera/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/hedera/bridge.integration.test.ts
@@ -1,4 +1,5 @@
 import "../../__tests__/test-helpers/setup";
-import { dataset } from "@ledgerhq/coin-hedera/tests";
+import { testBridge } from "../../__tests__/test-helpers/bridge";
+import { dataset } from "@ledgerhq/coin-hedera/tests/index";
 
 testBridge(dataset);
