b626cfb491eb6fc0f5be8462db427d68cf6c264e
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2024-10-23 14:09:28 +0200

    test: creating DETOX env to decouple flakiness related changes from MOCKing

--- apps/ledger-live-mobile/src/components/BleDevicePairingFlow/BleDevicePairing.tsx
+++ apps/ledger-live-mobile/src/components/BleDevicePairingFlow/BleDevicePairing.tsx
@@ -169,7 +169,7 @@ const BleDevicePairing = ({ deviceToPair, onPaired, onRetry }: BleDevicePairingP
         <Flex width="100%" py={16} alignItems="center">
           <Flex height={100} justifyContent="center">
             <BoxedIcon
-              Icon={<InfiniteLoader color="primary.c80" size={32} mock={Config.MOCK} />}
+              Icon={<InfiniteLoader color="primary.c80" size={32} mock={Config.DETOX} />}
               backgroundColor={colors.opacityDefault.c05}
               size={64}
               variant="circle"
