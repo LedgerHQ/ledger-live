6ca0090292a91006af8ce23e1d08209bb491d4a7
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-23 19:04:27 +0200

    jest.config and tsconfig

--- libs/coin-modules/coin-stacks/jest.config.js
+++ libs/coin-modules/coin-stacks/jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
-// `workerThreads: true` is required for validating object with `bigint` values
 module.exports = {
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["lib/", "lib-es/", ".*\\.integ\\.test\\.[tj]s"],
-  workerThreads: true,
+  testPathIgnorePatterns: ["lib/", "lib-es/", ".integration.test.ts"]
 };
