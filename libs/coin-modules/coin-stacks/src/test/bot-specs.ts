fa1a1baa069d39d39da5e8780ee4e2f26913d801
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-23 17:40:32 +0200

    bot-deviceAction + pnpmlock

--- libs/coin-modules/coin-stacks/src/test/bot-specs.ts
+++ libs/coin-modules/coin-stacks/src/test/bot-specs.ts
@@ -7,7 +7,7 @@ import type { Transaction } from "../types";
 import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets/currencies";
 import { botTest, pickSiblings } from "@ledgerhq/coin-framework/bot/specs";
 import type { AppSpec } from "@ledgerhq/coin-framework/bot/types";
-import { acceptTransaction } from "../speculos-deviceActions";
+import { acceptTransaction } from "./speculos-deviceActions";
 
 const MIN_SAFE = new BigNumber(10000);
 const stacksSpecs: AppSpec<Transaction> = {
