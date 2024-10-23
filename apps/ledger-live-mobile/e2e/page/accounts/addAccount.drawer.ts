44207e171c14f3a54549cd279a8c68f7ff7f9c18
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2024-10-23 14:54:07 +0200

    test: fix Speculos add account test

--- apps/ledger-live-mobile/e2e/page/accounts/addAccount.drawer.ts
+++ apps/ledger-live-mobile/e2e/page/accounts/addAccount.drawer.ts
@@ -1,12 +1,20 @@
 import { expect } from "detox";
-import { getElementById, openDeeplink, tapById, waitForElementById } from "../../helpers";
-import { capitalize } from "../../models/currencies";
+import {
+  getElementById,
+  openDeeplink,
+  scrollToId,
+  tapById,
+  waitForElementById,
+} from "../../helpers";
+import { getEnv } from "@ledgerhq/live-env";
 
 const baseLink = "add-account";
+const isMock = getEnv("MOCK");
 
 export default class AddAccountDrawer {
-  accountCardId = (id: string) => getElementById(`account-card-${id}`);
-  accountId = (currency: string, index: number) => `mock:1:${currency}:MOCK_${currency}_${index}:`;
+  accountCardId = (id: string | RegExp) => getElementById(new RegExp(`account-card-${id}`));
+  accountId = (currency: string, index: number) =>
+    isMock ? `mock:1:${currency}:MOCK_${currency}_${index}:` : `js:2:${currency}:.*`;
   accountTitleId = (accountName: string) => getElementById(`test-id-account-${accountName}`);
   modalButtonId = "add-accounts-modal-add-button";
   currencyRow = (currencyId: string) => `currency-row-${currencyId}`;
@@ -24,17 +32,17 @@ export default class AddAccountDrawer {
 
   async selectCurrency(currencyId: string) {
     const id = this.currencyRow(currencyId);
-    await waitForElementById(id);
+    await scrollToId(id);
     await tapById(id);
   }
 
   async startAccountsDiscovery() {
-    await waitForElementById(this.continueButtonId);
+    await waitForElementById(this.continueButtonId, 120000);
   }
 
-  async expectAccountDiscovery(currency: string, index: number) {
-    const accountName = `${capitalize(currency)} ${index + 1}`;
-    await expect(this.accountCardId(this.accountId(currency, index))).toBeVisible();
+  async expectAccountDiscovery(currencyName: string, currencyId: string, index = 0) {
+    const accountName = `${currencyName} ${index + 1}`;
+    await expect(this.accountCardId(this.accountId(currencyId, index))).toBeVisible();
     await expect(this.accountTitleId(accountName)).toHaveText(accountName);
   }
 
