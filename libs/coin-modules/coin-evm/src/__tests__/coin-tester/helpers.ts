e53be73b5de2447e9a605393d91433743d9a4611
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-10-25 12:26:29 +0200

    Use new `callMyDealer` helper for Eth/Pol scenarii

--- libs/coin-modules/coin-evm/src/__tests__/coin-tester/helpers.ts
+++ libs/coin-modules/coin-evm/src/__tests__/coin-tester/helpers.ts
@@ -2,18 +2,16 @@ import { ethers } from "ethers";
 import { NFTStandard } from "@ledgerhq/types-live";
 import { TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets/currencies";
-import { getTokenById } from "@ledgerhq/cryptoassets/tokens";
-import ERC20ABI from "../../abis/erc20.abi.json";
-import ERC721ABI from "../../abis/erc721.abi.json";
 import ERC1155ABI from "../../abis/erc1155.abi.json";
+import ERC721ABI from "../../abis/erc721.abi.json";
+import ERC20ABI from "../../abis/erc20.abi.json";
 
 export const ethereum = getCryptoCurrencyById("ethereum");
 export const polygon = getCryptoCurrencyById("polygon");
 export const ERC20Interface = new ethers.utils.Interface(ERC20ABI);
 export const ERC721Interface = new ethers.utils.Interface(ERC721ABI);
 export const ERC1155Interface = new ethers.utils.Interface(ERC1155ABI);
-export const USDC_ON_ETHEREUM = getTokenById("ethereum/erc20/usd__coin");
-export const USDC_ON_POLYGON = getTokenById("polygon/erc20/usd_coin_(pos)");
+export const VITALIK = "0x6bfD74C0996F269Bcece59191EFf667b3dFD73b9";
 
 export const impersonnateAccount = async ({
   provider,
