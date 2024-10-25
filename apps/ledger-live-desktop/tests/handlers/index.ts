b7467746ebb3a4e8c0ef7cbcabcc5b54e0c37c11
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2024-10-25 09:55:55 +0200

    test: add integration test for LS QRCode flow

--- apps/ledger-live-desktop/tests/handlers/index.ts
+++ apps/ledger-live-desktop/tests/handlers/index.ts
@@ -5,4 +5,6 @@ export default [...NFTsHandlers];
 export const ALLOWED_UNHANDLED_REQUESTS = [
   "https://nft.api.live.ledger.com/v1/marketdata/ethereum/1/contract/0xe3BE0054Da2F8da5002E8bdD8AA4c7fDf851E86D/floor-price",
   "https://nft.api.live.ledger.com/v1/ethereum/1/contracts/infos",
+  "https://cloud-sync-backend.api.aws.stg.ldg-tech.com/_info",
+  "https://trustchain-backend.api.aws.stg.ldg-tech.com/_info",
 ];
