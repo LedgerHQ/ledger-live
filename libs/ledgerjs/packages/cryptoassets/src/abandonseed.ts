64e811c276fe4127cf650ce975b5407e5ccbecf1
0xkvn <44363395+lambertkevin@users.noreply.github.com>
2024-10-25 12:38:32 +0200

    Add abandonseed for ZKsync currencies

--- libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
+++ libs/ledgerjs/packages/cryptoassets/src/abandonseed.ts
@@ -110,6 +110,8 @@ const abandonSeedAddresses: Partial<Record<CryptoCurrency["id"], string>> = {
   scroll: EVM_DEAD_ADDRESS,
   scroll_sepolia: EVM_DEAD_ADDRESS,
   etherlink: EVM_DEAD_ADDRESS,
+  zksync: EVM_DEAD_ADDRESS,
+  zksync_sepolia: EVM_DEAD_ADDRESS,
 };
 
 /**
