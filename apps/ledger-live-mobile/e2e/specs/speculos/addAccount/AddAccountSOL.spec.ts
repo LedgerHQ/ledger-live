08fd3686036c1ca0d88cb44aa57f2b51837b3d62
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2024-10-28 16:13:24 +0100

    test: separate coin by file

---
+++ apps/ledger-live-mobile/e2e/specs/speculos/addAccount/AddAccountSOL.spec.ts
@@ -0,0 +1,4 @@
+import { runAddAccountTest } from "./AddAccount";
+import { Currency } from "@ledgerhq/live-common/e2e/enum/Currency";
+
+runAddAccountTest(Currency.SOL, "B2CQA-2642, B2CQA-2656, B2CQA-2684");
