4d8eda71dc555bcdf559d20435bf5934670ba421
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-10-29 19:21:57 +0100

    remove types-cryptoassets import

--- libs/coin-modules/coin-stacks/src/config.ts
+++ libs/coin-modules/coin-stacks/src/config.ts
@@ -1,5 +1,4 @@
 import { CurrencyConfig } from "@ledgerhq/coin-framework/config";
-import "@ledgerhq/types-cryptoassets";
 
 export type StacksCoinConfig = () => CurrencyConfig & {
   config_currency_stacks: {
