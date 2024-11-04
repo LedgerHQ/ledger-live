cab1ce5be23ff435e134f0dfbb7d268cdfd12d1f
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-11-04 19:23:31 +0100

    cache control

--- libs/ledger-live-common/src/platform/types.ts
+++ libs/ledger-live-common/src/platform/types.ts
@@ -110,6 +110,7 @@ export type LiveAppManifest = {
   id: string;
   author?: string;
   private?: boolean;
+  nocache?: boolean;
   name: string;
   url: string | URL;
   params?: LiveAppManifestParams;
@@ -157,6 +158,7 @@ export const LiveAppManifestSchema = z
     id: z.string().trim().min(1),
     author: z.string().optional(),
     private: z.boolean().optional(),
+    nocache: z.boolean().optional(),
     name: z.string().trim().min(1),
     url: z.string().trim().min(1),
     dapp: LiveAppManifestDappSchema.optional(),
