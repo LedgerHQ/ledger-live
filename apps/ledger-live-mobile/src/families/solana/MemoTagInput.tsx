e1c5dbd7bd87793d2b9c1791df593bee53a15ee8
Theophile Sandoz <theophile.sandoz@ledger.fr>
2024-11-08 17:18:13 +0100

    fix(llm): truncate Solana's memo too

--- apps/ledger-live-mobile/src/families/solana/MemoTagInput.tsx
+++ apps/ledger-live-mobile/src/families/solana/MemoTagInput.tsx
@@ -1,15 +1,20 @@
 import merge from "lodash/merge";
 import React from "react";
 
-import { Transaction as SolanaTransaction } from "@ledgerhq/live-common/families/solana/types";
+import { MAX_MEMO_LENGTH } from "@ledgerhq/live-common/families/solana/logic";
+import type { Transaction as SolanaTransaction } from "@ledgerhq/live-common/families/solana/types";
 import type { MemoTagInputProps } from "LLM/features/MemoTag/types";
 import { GenericMemoTagInput } from "LLM/features/MemoTag/components/GenericMemoTagInput";
+import { truncateUtf8 } from "LLM/utils/truncateUtf8";
 
-export default (props: MemoTagInputProps<SolanaTransaction>) => (
-  <GenericMemoTagInput
-    {...props}
-    valueToTxPatch={value => tx =>
-      merge({}, tx, { model: { uiState: { memo: value || undefined } } })
-    }
-  />
-);
+export default (props: MemoTagInputProps<SolanaTransaction>) => {
+  return (
+    <GenericMemoTagInput
+      {...props}
+      textToValue={text => truncateUtf8(text, MAX_MEMO_LENGTH)}
+      valueToTxPatch={value => tx =>
+        merge({}, tx, { model: { uiState: { memo: value || undefined } } })
+      }
+    />
+  );
+};
