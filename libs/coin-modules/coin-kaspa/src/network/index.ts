043ea66fc118b46198c0d84f3b8fadff4d525fa6
lAmeR1 <lamer1@github.com>
2024-11-10 01:54:24 +0100

    Add function to derive Kaspa addresses and fetch balances
    Introduce `determineDerivedAddresses` to generate addresses from public keys and fetch their balances. Also, create a test to verify the functionality and ensure active addresses are accurately identified and totaled.

--- libs/coin-modules/coin-kaspa/src/network/index.ts
+++ libs/coin-modules/coin-kaspa/src/network/index.ts
@@ -1,3 +1,132 @@
+import KaspaBIP32 from "../lib/bip32";
+import { getBalancesForAddresses } from "./indexer-api/getBalancesForAddresses";
+import { BigNumber } from "bignumber.js";
+import { getAddressesActive } from "./indexer-api/getAddressesActive";
+
 export { getFeeEstimate } from "./indexer-api/getFeeEstimate";
 export { getBalancesForAddresses } from "./indexer-api/getBalancesForAddresses";
 export { getUtxosForAddresses } from "./indexer-api/getUtxosForAddresses";
+
+const GAP_LIMIT = 50;
+const SCAN_BATCH_SIZE = 100;
+
+type AccountAddress = {
+  type: number;
+  index: number;
+  address: string;
+  balance: BigNumber;
+  active: boolean;
+};
+
+export interface AccountAddresses {
+  usedReceiveAddresses: AccountAddress[];
+  usedChangeAddresses: AccountAddress[];
+  nextChangeAddress: AccountAddress;
+  nextReceiveAddress: AccountAddress;
+  totalBalance: BigNumber;
+}
+
+export async function determineDerivedAddresses(
+  compressedPublicKey: Buffer,
+  chainCode: Buffer,
+  startIndex: number = 0,
+): Promise<AccountAddresses> {
+  const kaspaBip32: KaspaBIP32 = new KaspaBIP32(compressedPublicKey, chainCode);
+
+  const accountAddresses: AccountAddresses = {
+    usedReceiveAddresses: [],
+    usedChangeAddresses: [],
+    nextChangeAddress: { type: 0, index: 0, address: "", balance: BigNumber(0), active: false },
+    nextReceiveAddress: { type: 0, index: 0, address: "", balance: BigNumber(0), active: false },
+    totalBalance: BigNumber(0),
+  };
+
+  // all addresses until the last valid UTXO we assume as used
+  // if wallet has no utxos, fallback via transactions needed.
+  // not yet requestable as batch -> #TODO
+
+  // receive address
+  for (let type = 0; type <= 1; type++) {
+    let keepScanning: boolean = true;
+
+    while (keepScanning) {
+      const addresses: AccountAddress[] = [];
+      for (let index = startIndex; index < startIndex + SCAN_BATCH_SIZE; index++) {
+        const derivedAddress: string = kaspaBip32.getAddress(type, index);
+        addresses.push({
+          type: type,
+          index,
+          address: derivedAddress,
+          balance: BigNumber(0),
+          active: false,
+        } as AccountAddress);
+      }
+
+      // fetch address information via API and update object
+      await updateAddressesActive(addresses);
+
+      // update balance and utxo count
+      for (const addr of addresses) {
+        accountAddresses.totalBalance = accountAddresses.totalBalance.plus(addr.balance);
+      }
+
+      // Check the last GAP_LIMIT addresses for activity
+      const lastAddressesToCheck = addresses.slice(-GAP_LIMIT);
+      const allInactive = lastAddressesToCheck.every(addr => !addr.active);
+
+      if (allInactive) {
+        keepScanning = false;
+      } else {
+        startIndex += SCAN_BATCH_SIZE;
+      }
+
+      if (type === 0) {
+        accountAddresses.usedReceiveAddresses.push(...addresses.filter(addr => addr.active));
+
+        if (!keepScanning) {
+          // last used address + 1
+          // + 1 has to be available, as there needs to be a GAP!
+          accountAddresses.nextReceiveAddress =
+            addresses[
+              addresses.indexOf(<AccountAddress>accountAddresses.usedReceiveAddresses.at(-1)) + 1
+            ];
+        }
+      }
+      // change address
+      if (type === 1) {
+        accountAddresses.usedChangeAddresses.push(...addresses.filter(addr => addr.active));
+
+        if (!keepScanning) {
+          // last used address + 1
+          // + 1 has to be available, as there needs to be a GAP!
+          accountAddresses.nextChangeAddress =
+            addresses[
+              addresses.indexOf(<AccountAddress>accountAddresses.usedChangeAddresses.at(-1)) + 1
+            ];
+        }
+      }
+    }
+  }
+
+  return accountAddresses;
+}
+
+async function updateAddressesActive(addresses: AccountAddress[]) {
+  const balances = await getBalancesForAddresses(addresses.map(addr => addr.address));
+  const addressesActive = await getAddressesActive(addresses.map(addr => addr.address));
+
+  for (const addressBalance of balances) {
+    const addressIndex = addresses.findIndex(addr => addr.address === addressBalance.address);
+    if (addressBalance.balance > 0) {
+      addresses[addressIndex].balance = BigNumber(addressBalance.balance);
+      addresses[addressIndex].active = true;
+    }
+  }
+
+  for (const addressActive of addressesActive) {
+    const addressIndex = addresses.findIndex(addr => addr.address === addressActive.address);
+    if (addressActive.active) {
+      addresses[addressIndex].active = true;
+    }
+  }
+}
---
+++ libs/coin-modules/coin-kaspa/src/network/tests/determineBalanceFromPubKey.test.ts
@@ -0,0 +1,26 @@
+import { parseExtendedPublicKey } from "../../lib/kaspa-util";
+import { AccountAddresses, determineDerivedAddresses } from "../index";
+import { BigNumber } from "bignumber.js";
+
+describe("determineDerivedAddresses function", () => {
+  it("Gets information about addresses being active or not", async () => {
+    const xpub =
+      "410404cd27f15b8a73039972cdd131a93754ef3fa90bee794222737f5ca26a12f887f2fd493acf13230fa42c418d2c1be53a6fc66fbbec3ea9c37a675acc53a65e08203a35a71b1d8c10f7b03cf84c50570ee21af9b830b25bbe16ec661e7de8a51563";
+    const { compressedPublicKey, chainCode } = parseExtendedPublicKey(Buffer.from(xpub, "hex"));
+
+    const accountAddresses: AccountAddresses = await determineDerivedAddresses(
+      compressedPublicKey,
+      chainCode,
+      0,
+    );
+
+    const sumBalance = accountAddresses.usedReceiveAddresses
+      .reduce((acc, v) => acc.plus(v.balance), BigNumber(0))
+      .plus(
+        accountAddresses.usedChangeAddresses.reduce((acc, v) => acc.plus(v.balance), BigNumber(0)),
+      );
+
+    expect(accountAddresses.usedReceiveAddresses.length).toBeGreaterThan(0);
+    expect(accountAddresses.totalBalance.eq(sumBalance)).toBe(true);
+  });
+});
