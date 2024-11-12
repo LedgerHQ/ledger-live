37078ee1779c633cc61b5c9f3ab7883fb51bedc8
lAmeR1 <lamer1@github.com>
2024-11-12 16:23:57 +0100

    refactor name

--- libs/coin-modules/coin-kaspa/src/network/tests/scanAddresses.test.ts
+++ libs/coin-modules/coin-kaspa/src/network/tests/scanAddresses.test.ts
@@ -2,7 +2,7 @@ import { parseExtendedPublicKey } from "../../lib/kaspa-util";
 import { AccountAddresses, scanAddresses } from "../index";
 import { BigNumber } from "bignumber.js";
 
-describe("determineDerivedAddresses function", () => {
+describe("scanAddresses function", () => {
   it("Gets information about addresses being active or not", async () => {
     const xpub =
       "410404cd27f15b8a73039972cdd131a93754ef3fa90bee794222737f5ca26a12f887f2fd493acf13230fa42c418d2c1be53a6fc66fbbec3ea9c37a675acc53a65e08203a35a71b1d8c10f7b03cf84c50570ee21af9b830b25bbe16ec661e7de8a51563";
