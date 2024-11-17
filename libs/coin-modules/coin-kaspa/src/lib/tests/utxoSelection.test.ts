84ddefd7ab2c76f1a3640f2dfa9c3960cd45c7ed
lAmeR1 <lamer1@github.com>
2024-11-17 14:13:52 +0100

    Revise UTXO selection logic for fee strategy robustness
    Refactored UTXO selection to handle various fee rates more effectively, incorporating checks for maximum transaction inputs and ensuring that selected UTXOs meet the required mass constraints. Adjusted the unit tests to validate both low and high-fee strategies, enhancing the reliability of UTXO selection under different conditions.

--- libs/coin-modules/coin-kaspa/src/lib/tests/utxoSelection.test.ts
+++ libs/coin-modules/coin-kaspa/src/lib/tests/utxoSelection.test.ts
@@ -2,12 +2,67 @@ import { selectUtxos } from "../utxoSelection";
 import { KaspaUtxo } from "../../types/bridge";
 import { BigNumber } from "bignumber.js";
 
-describe("selectUtxos function", () => {
-  const DEFAULT_MASS_WITH_ONE_INPUT = 2036;
-  const MASS_PER_UTXO_INPUT = 1118;
-  const ADDTIONAL_MASS_FOR_ECDSA_OUTPUT = 200; // update with actual value if it differs
+const DEFAULT_MASS_WITH_ONE_INPUT = 2036;
+const MASS_PER_UTXO_INPUT = 1118;
+const ADDTIONAL_MASS_FOR_ECDSA_OUTPUT = 200; // update with actual value if it differs
 
-  const sampleUtxo: KaspaUtxo = {
+const sampleUtxo: KaspaUtxo = {
+  address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
+  outpoint: {
+    transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
+    index: 0,
+  },
+  utxoEntry: {
+    amount: BigNumber(1_0000_0000), // 1 KAS
+    scriptPublicKey: {
+      version: 0,
+      scriptPublicKey: "20c3e3cd358cba288acf955a6476b3c34562d0d7e4cb4b8750c12fa4685eb3b882ac",
+    },
+    blockDaaScore: "95538507",
+    isCoinbase: false,
+  },
+  accountType: 0,
+  accountIndex: 5,
+} as KaspaUtxo;
+
+const sampleUtxoSet: KaspaUtxo[] = [
+  {
+    address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
+    outpoint: {
+      transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
+      index: 0,
+    },
+    utxoEntry: {
+      amount: BigNumber(10_0000_0000), // 10 KAS
+      scriptPublicKey: {
+        version: 0,
+        scriptPublicKey: "20c3e3cd358cba288acf955a6476b3c34562d0d7e4cb4b8750c12fa4685eb3b882ac",
+      },
+      blockDaaScore: "95538507",
+      isCoinbase: false,
+    },
+    accountType: 0,
+    accountIndex: 5,
+  } as KaspaUtxo,
+  {
+    address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
+    outpoint: {
+      transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
+      index: 0,
+    },
+    utxoEntry: {
+      amount: BigNumber(5_0000_0000), // 5 KAS
+      scriptPublicKey: {
+        version: 0,
+        scriptPublicKey: "20c3e3cd358cba288acf955a6476b3c34562d0d7e4cb4b8750c12fa4685eb3b882ac",
+      },
+      blockDaaScore: "95538507",
+      isCoinbase: false,
+    },
+    accountType: 0,
+    accountIndex: 5,
+  } as KaspaUtxo,
+  {
     address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
     outpoint: {
       transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
@@ -24,58 +79,10 @@ describe("selectUtxos function", () => {
     },
     accountType: 0,
     accountIndex: 5,
-  } as KaspaUtxo;
+  } as KaspaUtxo,
+];
 
-  const sampleUtxoSet: KaspaUtxo[] = [
-    {
-      address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
-      outpoint: {
-        transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
-        index: 0,
-      },
-      utxoEntry: {
-        amount: BigNumber(10_0000_0000), // 10 KAS
-        scriptPublicKey: {
-          version: 0,
-          scriptPublicKey: "20c3e3cd358cba288acf955a6476b3c34562d0d7e4cb4b8750c12fa4685eb3b882ac",
-        },
-        blockDaaScore: "95538507",
-        isCoinbase: false,
-      },
-      accountType: 0,
-      accountIndex: 5,
-    } as KaspaUtxo,
-    {
-      address: "kaspa:qrp78nf43jaz3zk0j4dxga4ncdzk95xhun95hp6scyh6g6z7kwugy02wfw6ee",
-      outpoint: {
-        transactionId: "44f1881957963cffa85b1ec531e3a195104248b11882229e53df6c6c77becdcc",
-        index: 0,
-      },
-      utxoEntry: {
-        amount: BigNumber(1_0000_0000), // 1 KAS
-        scriptPublicKey: {
-          version: 0,
-          scriptPublicKey: "20c3e3cd358cba288acf955a6476b3c34562d0d7e4cb4b8750c12fa4685eb3b882ac",
-        },
-        blockDaaScore: "95538507",
-        isCoinbase: false,
-      },
-      accountType: 0,
-      accountIndex: 5,
-    } as KaspaUtxo,
-  ];
-
-  test("spending exactly the one UTXO with 2036mass", () => {
-    const utxos = [sampleUtxo];
-    const recipient_is_ecdsa = false;
-    const amount = BigNumber(1_0000_0000 - 2036);
-    const feerate = 1;
-
-    const result = selectUtxos(utxos, recipient_is_ecdsa, amount, feerate);
-
-    expect(result.length).toBe(1);
-  });
-
+describe("selectUtxos function with low-fee strategy", () => {
   test("error if spending more than 9997_964 with 2036 mass", () => {
     const utxos = [sampleUtxo];
     const recipient_is_ecdsa = false;
@@ -110,30 +117,28 @@ describe("selectUtxos function", () => {
     expect(result.length).toBe(2);
     expect(result[0].utxoEntry.amount.eq(BigNumber(1_0000_0000))).toBe(true);
   });
+});
+describe("selectUtxos function with high-fee strategy", () => {
+  const feerate = 2;
+  const utxos = sampleUtxoSet;
+  const recipient_is_ecdsa = false;
 
   test("check ordering - higher fees - descending - 1 utxo needed", () => {
-    const utxos = sampleUtxoSet;
-    const recipient_is_ecdsa = false;
-    const amount = BigNumber(1_0000_0000 - 50_000);
-    const feerate = 2;
-
+    const amount = BigNumber(4_0000_0000);
     const result = selectUtxos(utxos, recipient_is_ecdsa, amount, feerate);
 
+    // only one 5KAS UTXO needed
     expect(result.length).toBe(1);
-    expect(result[0].utxoEntry.amount.eq(BigNumber(10_0000_0000))).toBe(true);
+    expect(result[0].utxoEntry.amount.eq(BigNumber(5_0000_0000))).toBe(true);
   });
 
   test("check ordering - higher fees - descending - 2 utxos needed", () => {
-    const utxos = sampleUtxoSet;
-    const recipient_is_ecdsa = false;
-    const amount = BigNumber(10_0000_0000); // 10 KAS -> need 2 utxos
-    const feerate = 2;
-
+    const amount = BigNumber(5_0000_0000); // 10 KAS -> need 2 utxos
     const result = selectUtxos(utxos, recipient_is_ecdsa, amount, feerate);
 
-    expect(result.length).toBe(2);
+    expect(result.length).toBe(1);
 
-    // check ordering
+    // check utxos, 1 KAS and 5 KAS
     expect(result[0].utxoEntry.amount.eq(BigNumber(10_0000_0000))).toBe(true);
   });
 });
--- libs/coin-modules/coin-kaspa/src/lib/utxoSelection.ts
+++ libs/coin-modules/coin-kaspa/src/lib/utxoSelection.ts
@@ -1,9 +1,11 @@
 import { KaspaUtxo } from "../types/bridge";
 import { BigNumber } from "bignumber.js";
 
-const MAX_TX_MASS = 100_000;
+const MAX_TX_INPUTS = 88; // floor (( 100_000 - 918 (def_size) ) / 1_118 (per_input))
 const MASS_PER_UTXO_INPUT = 1_118;
 const DEFAULT_MASS_WITHOUT_INPUT = 918;
+
+const FEE_RATE_THRESHOLD = 1; // strategy decision based on fee rate threshold
 
 // if one output is ECDSA, scriptPubKey is one byte longer
 // which ends up in 11g mass ( 1 byte + 10g MASS_PER_SCRIPT_PUB_KEY_BYTE )
@@ -29,43 +31,83 @@ export function selectUtxos(
     higher fees -> as less inputs as needed
   */
 
-  if (feerate === 1) {
-    // fee is low, time to compound small utxos
-    // sort utxos ascending
-    utxos.sort((a, b) => a.utxoEntry.amount.minus(b.utxoEntry.amount).toNumber());
-  } else {
-    // fees are higher
-    // sort utxos descending
-    utxos.sort((a, b) => b.utxoEntry.amount.minus(a.utxoEntry.amount).toNumber());
+  utxos.sort((a, b) => a.utxoEntry.amount.minus(b.utxoEntry.amount).toNumber());
+
+  // check if  utxo set is valid
+  const spendableUtxos = calculateUtxoSumInRange(
+    utxos,
+    Math.max(0, utxos.length - MAX_TX_INPUTS),
+    utxos.length,
+  );
+  const maximumMass =
+    DEFAULT_MASS_WITHOUT_INPUT + Math.min(MAX_TX_INPUTS, utxos.length) * MASS_PER_UTXO_INPUT;
+  const maximumSpendableAmount = spendableUtxos.minus(maximumMass * feerate);
+
+  if (amount.gt(maximumSpendableAmount)) {
+    throw Error(`Amount too high for this utxo set. Only ${MAX_TX_INPUTS} inputs are allowed.`);
   }
 
-  const selectedUtxos: KaspaUtxo[] = [];
-  let txTotalInput = BigNumber(0);
-  let txMass = DEFAULT_MASS_WITHOUT_INPUT;
-
+  let txDefaultMass = DEFAULT_MASS_WITHOUT_INPUT;
   if (recipient_is_ecdsa) {
-    txMass += ADDTIONAL_MASS_FOR_ECDSA_OUTPUT;
+    txDefaultMass += ADDTIONAL_MASS_FOR_ECDSA_OUTPUT;
   }
 
-  // Iterate through the UTXOs and select them until the total amount is reached
-  for (const utxo of utxos) {
-    // Calculate the mass when adding this UTXO
-    txMass += MASS_PER_UTXO_INPUT;
+  let from: number = 0;
+  let to: number = 0;
 
-    selectedUtxos.push(utxo);
-    txTotalInput = txTotalInput.plus(utxo.utxoEntry.amount);
+  // fee is low, time to compound small utxos
+  // from smallest to biggest UTXOs
+  if (feerate <= FEE_RATE_THRESHOLD) {
+    let amountNeededWithFees: BigNumber = amount;
 
-    // Check if the amount goal has been reached
-    if (txTotalInput.gte(amount.plus(txMass * feerate))) {
-      break;
+    do {
+      if (to - from === MAX_TX_INPUTS) {
+        from++;
+      }
+      to++;
+
+      amountNeededWithFees = amount.plus(
+        BigNumber(((to - from) * MASS_PER_UTXO_INPUT + txDefaultMass) * feerate),
+      );
+    } while (calculateUtxoSumInRange(utxos, from, to).lt(amountNeededWithFees));
+  }
+  // fee is higher
+  // use as few UTXOs as possible
+  if (feerate > FEE_RATE_THRESHOLD) {
+    from = utxos.length;
+    to = utxos.length;
+    let amountNeededWithFees: BigNumber = amount;
+
+    // first check utxo amount needed
+    do {
+      from--;
+      amountNeededWithFees = amount.plus(
+        BigNumber(((to - from) * MASS_PER_UTXO_INPUT + txDefaultMass) * feerate),
+      );
+    } while (calculateUtxoSumInRange(utxos, from, to).lt(amountNeededWithFees));
+
+    // from and to parameters are now set
+    // now shift to smaller UTXO amounts if possible
+    while (from > 0 && calculateUtxoSumInRange(utxos, from - 1, to - 1).gte(amountNeededWithFees)) {
+      from--;
+      to--;
     }
   }
+  return utxos.slice(from, to);
+}
 
-  if (txTotalInput.lt(amount.plus(txMass * feerate))) {
-    throw Error("Amount too high for inputs.");
+function calculateUtxoSumInRange(utxos: KaspaUtxo[], from: number, to: number): BigNumber {
+  if (from > to) {
+    throw new Error("'from' index must be less than 'to' index");
   }
 
-  console.log("mass", txMass)
-
-  return selectedUtxos;
+  let sum = BigNumber(0);
+  for (let i = from; i < to; i++) {
+    try {
+      sum = sum.plus(utxos[i].utxoEntry.amount);
+    } catch (e) {
+      // just ignore e..
+    }
+  }
+  return sum;
 }
