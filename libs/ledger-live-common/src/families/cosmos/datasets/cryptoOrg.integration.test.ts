567280748ac9c2f1e9e12719f62e41bacf57fb06
qperrot <quentin.perrot-minnot@ledger.fr>
2024-11-18 13:05:02 +0100

    fix: test crypto_org

--- libs/ledger-live-common/src/families/cosmos/datasets/cryptoOrg.integration.test.ts
+++ libs/ledger-live-common/src/families/cosmos/datasets/cryptoOrg.integration.test.ts
@@ -1,13 +1,13 @@
 import { DatasetTest } from "@ledgerhq/types-live";
 import { testBridge } from "../../../__tests__/test-helpers/bridge";
 import "../../../__tests__/test-helpers/setup";
-import crypto_org_croeseid from "./croeseid";
+import crypto_org from "./cryptoOrg";
 import type { Transaction } from "../types";
 
 const dataset: DatasetTest<Transaction> = {
   implementations: ["js"],
   currencies: {
-    crypto_org_croeseid,
+    crypto_org,
   },
 };
 
