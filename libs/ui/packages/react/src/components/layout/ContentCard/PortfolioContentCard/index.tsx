fa0155e5f465e7c11fd2eb3221b87686110e9854
Theophile Sandoz <theophile.sandoz@ledger.fr>
2024-11-18 09:48:15 +0100

    feat(lld): add the PortfolioContentCard component

---
+++ libs/ui/packages/react/src/components/layout/ContentCard/PortfolioContentCard/index.tsx
@@ -0,0 +1,89 @@
+import React, { type ReactEventHandler } from "react";
+import styled from "styled-components";
+
+import { Icons } from "../../../../assets";
+import { Text } from "../../../asorted";
+import { Button } from "../../../cta";
+import Tag from "../../../Tag";
+
+export type PortfolioContentCardProps = {
+  title: string;
+  cta?: string;
+  shortDescription: string;
+  tag?: string;
+  image: string;
+
+  onClick: ReactEventHandler;
+  onClose: ReactEventHandler;
+};
+
+export default function PortfolioContentCard({
+  title,
+  cta,
+  shortDescription,
+  tag,
+  image,
+  onClick,
+  onClose,
+}: PortfolioContentCardProps) {
+  const handleClose: ReactEventHandler = event => {
+    event.stopPropagation();
+    onClose(event);
+  };
+
+  return (
+    <Wrapper image={image} tag={tag} onClick={onClick}>
+      {tag && (
+        <Tag size="medium" type="plain" active>
+          {tag}
+        </Tag>
+      )}
+      <Text variant="h4Inter">{title}</Text>
+      <Text mb={3} variant="small" color="neutral.c70">
+        {shortDescription}
+      </Text>
+      {cta && (
+        <Button variant="main" outline={false} onClick={onClick}>
+          {cta}
+        </Button>
+      )}
+      <CloseButton onClick={handleClose} />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<Pick<PortfolioContentCardProps, "image" | "tag" | "onClick">>`
+  background-color: ${p => p.theme.colors.background.card};
+  background-image: url("${p => p.image}");
+  background-position: right center;
+  background-repeat: no-repeat;
+  background-size: 50% auto;
+
+  padding: 16px;
+  padding-top: ${p => (p.tag ? "16px" : "24px")};
+  padding-right: 50%;
+
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 4px;
+`;
+
+const CloseButton = styled(Button).attrs({
+  Icon: <Icons.Close size="XS" />,
+  iconButton: true,
+  outline: true,
+})`
+  background-color: ${p => p.theme.colors.neutral.c30};
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 24px;
+  height: 24px;
+  svg {
+    width: 12px;
+    height: 12px;
+  }
+`;
