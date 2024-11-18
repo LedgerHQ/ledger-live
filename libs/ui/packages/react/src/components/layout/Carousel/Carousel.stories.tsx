41e9ed9d67015231c4d33be31ee2661ea0ab763c
Theophile Sandoz <theophile.sandoz@ledger.fr>
2024-11-18 11:25:08 +0100

    feat(lld): add a carousel portfolio content cards story

--- libs/ui/packages/react/src/components/layout/Carousel/Carousel.stories.tsx
+++ libs/ui/packages/react/src/components/layout/Carousel/Carousel.stories.tsx
@@ -1,25 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+import React, { FC, ReactElement, useContext } from "react";
+
+import PortfolioContentCard from "../ContentCard/PortfolioContentCard";
 import Carousel from "./";
 import { Props } from "./types";
 
-const CarouselStory = (args: Omit<Props, "children"> & { children: number }) => {
-  const slides = Array.from({ length: args.children }, (_, index) => (
-    <div
-      key={index}
-      style={{
-        backgroundColor: "hsl(" + Math.random() * 360 + ", 100%, 75%)",
-        padding: "16px 24px",
-        borderRadius: "5px",
-      }}
-    >
-      Slide {index}
-    </div>
-  ));
+type Args = Omit<Props, "children"> & { children: number; onSlideAction: (action: string) => void };
+type Parameters = { Slide: FC<{ index: number; onSlideAction: (action: string) => void }> };
 
-  return <Carousel variant={args.variant} children={slides} />;
-};
-
+const SlideContext = React.createContext<ReactElement[]>([]);
 export default {
   title: "Layout/Carousel",
   argTypes: {
@@ -33,6 +22,7 @@ export default {
       defaultValue: "default",
       control: "inline-radio",
     },
+    onSlideAction: {},
   },
   args: {
     variant: "default",
@@ -45,7 +35,48 @@ export default {
       },
     },
   },
-  render: CarouselStory,
-} satisfies Meta;
+  decorators: [
+    (Story: FC, { args, parameters }: { args: Args; parameters: Parameters }) => (
+      <SlideContext.Provider
+        value={Array.from({ length: args.children }, (_, index) => (
+          <parameters.Slide key={index} index={index} onSlideAction={args.onSlideAction} />
+        ))}
+      >
+        <Story />
+      </SlideContext.Provider>
+    ),
+  ],
+  render: (args: Args) => <Carousel variant={args.variant}>{useContext(SlideContext)}</Carousel>,
+} satisfies Meta<Args>;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj<Args> = {
+  parameters: {
+    Slide: (({ index }) => (
+      <div
+        style={{
+          backgroundColor: `hsl(${Math.random() * 360}, 100%, 75%)`,
+          padding: "16px 24px",
+          borderRadius: "5px",
+        }}
+      >
+        Slide {index}
+      </div>
+    )) satisfies Parameters["Slide"],
+  },
+};
+
+export const PortfolioContentCards: StoryObj<Args> = {
+  parameters: {
+    Slide: (({ index, onSlideAction }) => (
+      <PortfolioContentCard
+        title="Ledger Recover"
+        shortDescription="Get peace of mind and start your free trial."
+        cta="Start my free trial"
+        tag="New"
+        image="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='450' height='526' viewBox='0 0 450 526'><defs><linearGradient id='g' x0='0' y0='0' x1='1' y1='1'><stop stop-color='%23000' offset='0%' /><stop stop-color='%23FFF' offset='100%' /></linearGradient></defs><path d='M0,0 H450 V526 Q0,526 0,0 z' fill='url(%23g)' /></svg>"
+        onClick={() => onSlideAction(`click on slide ${index}`)}
+        onClose={() => onSlideAction(`close slide ${index}`)}
+      />
+    )) satisfies Parameters["Slide"],
+  },
+};
