6c580ad1fa8f881f826b9ef17d176cc18382731e
Kant <quentin.jaccarino@ledger.fr>
2024-11-20 18:14:44 +0100

    fix: wallet-api spl token support

---
+++ libs/ledger-live-common/src/families/solana/walletApiAdapter.ts
@@ -0,0 +1,26 @@
+import { SolanaTransaction as WalletAPISolanaTransaction } from "@ledgerhq/wallet-api-core";
+import { GetWalletAPITransactionSignFlowInfos } from "../../wallet-api/types";
+import { Transaction } from "@ledgerhq/coin-solana/types";
+
+const CAN_EDIT_FEES = false;
+
+const HAS_FEES_PROVIDED = false;
+
+const getWalletAPITransactionSignFlowInfos: GetWalletAPITransactionSignFlowInfos<
+  WalletAPISolanaTransaction,
+  Transaction
+> = ({ walletApiTransaction, account }) => {
+  const liveTx: Transaction = { ...walletApiTransaction };
+
+  if (!liveTx.subAccountId && account.type === "TokenAccount") {
+    liveTx.subAccountId = account.id;
+  }
+
+  return {
+    canEditFees: CAN_EDIT_FEES,
+    liveTx,
+    hasFeesProvided: HAS_FEES_PROVIDED,
+  };
+};
+
+export default { getWalletAPITransactionSignFlowInfos };
