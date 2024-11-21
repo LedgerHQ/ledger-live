a546d512ca5bcd2a40b80fbd13f0b0cbf875d2ea
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-11-21 18:41:46 +0100

    skip polyfill lld

--- apps/ledger-live-desktop/jest.polyfills.js
+++ apps/ledger-live-desktop/jest.polyfills.js
@@ -14,7 +14,7 @@ const { Blob, File } = require("node:buffer");
 // subarray logic. It's the same as in ledger-live-mobile
 // Furthermore, importing 'buffer' gets translated to 'node:buffer' so we're
 // using a relative path here
-const { Buffer } = require("./node_modules/buffer");
+// const { Buffer } = require("./node_modules/buffer");
 const { fetch, Headers, FormData, Request, Response } = require("undici");
 
 Object.defineProperties(globalThis, {
@@ -25,5 +25,5 @@ Object.defineProperties(globalThis, {
   FormData: { value: FormData },
   Request: { value: Request },
   Response: { value: Response },
-  Buffer: { value: Buffer },
+  // Buffer: { value: Buffer },
 });
