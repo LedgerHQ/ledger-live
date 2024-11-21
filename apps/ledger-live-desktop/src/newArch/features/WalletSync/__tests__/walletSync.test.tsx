2cf124ccfbf9cb730176a6329dabc9017cfe94bf
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-11-21 18:06:35 +0100

    skip jsdom test

--- apps/ledger-live-desktop/src/newArch/features/WalletSync/__tests__/walletSync.test.tsx
+++ apps/ledger-live-desktop/src/newArch/features/WalletSync/__tests__/walletSync.test.tsx
@@ -15,7 +15,7 @@ jest.mock("../hooks/useTrustchainSdk", () => ({
   }),
 }));
 
-describe.skip("Rendering", () => {
+describe("Rendering", () => {
   it("should loads and displays WalletSync Row", async () => {
     render(<WalletSyncTestApp />);
     expect(screen.getByRole("button", { name: "Manage" })).toBeTruthy();
