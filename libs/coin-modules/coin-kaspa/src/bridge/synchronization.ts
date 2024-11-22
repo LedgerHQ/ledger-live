7927a05496b5b5202a347855ad2c442054718549
lAmeR1 <lamer1@github.com>
2024-11-23 00:09:52 +0100

    Refactor synchronization logic in Kaspa module
    Refactored the account synchronization process by introducing a factory function to handle xpub derivation. Also added debug logs for balancing information. Removed an unnecessary console log in hw-getAddress.ts for cleaner code.

--- libs/coin-modules/coin-kaspa/src/bridge/synchronization.ts
+++ libs/coin-modules/coin-kaspa/src/bridge/synchronization.ts
@@ -1,60 +1,73 @@
 import { encodeAccountId } from "@ledgerhq/coin-framework/account/index";
-import {
-  type GetAccountShape,
-  makeSync,
-  mergeOps,
-} from "@ledgerhq/coin-framework/bridge/jsHelpers";
+import { type GetAccountShape, mergeOps } from "@ledgerhq/coin-framework/bridge/jsHelpers";
 import { KaspaAccount } from "../types/bridge";
 import { parseExtendedPublicKey } from "../lib/kaspa-util";
-import { AccountAddresses, scanAddresses, scanOperations } from "../network";
+import { AccountAddresses, scanAddresses, scanOperations } from "../network/index";
 import { Operation } from "@ledgerhq/types-live";
+import { SignerContext } from "@ledgerhq/coin-framework/lib/signer";
+import { KaspaSigner } from "../signer";
 
-export const getAccountShape: GetAccountShape<KaspaAccount> = async info => {
-  const { initialAccount } = info;
+export const makeGetAccountShape =
+  (signerContext: SignerContext<KaspaSigner>): GetAccountShape<KaspaAccount> =>
+  async info => {
+    const { initialAccount, rest, deviceId, derivationMode, derivationPath, index } = info;
 
-  const xpub = initialAccount?.xpub;
+    let xpub = initialAccount?.xpub;
 
-  if (!xpub) {
-    throw new Error("xpub is empty");
-  }
+    if (!xpub) {
+      // CLI does not deliver account (44'/11111'/0') xpub - need to calculate it..
+      const accountPath = derivationPath.split("/").slice(0, 3).join("/");
+      xpub = (await signerContext(deviceId || "", signer => signer.getAddress(accountPath)))
+        .publicKey;
+    }
 
-  // // Needed for incremental synchronisation
-  const accountId = encodeAccountId({
-    type: "js",
-    version: "2",
-    currencyId: "kaspa",
-    xpubOrAddress: xpub,
-    derivationMode: "",
-  });
+    // // Needed for incremental synchronisation
+    const accountId = encodeAccountId({
+      type: "js",
+      version: "2",
+      currencyId: "kaspa",
+      xpubOrAddress: xpub,
+      derivationMode: "",
+    });
 
-  const { compressedPublicKey, chainCode } = parseExtendedPublicKey(Buffer.from(xpub, "hex"));
-  const accountAddresses: AccountAddresses = await scanAddresses(compressedPublicKey, chainCode, 0);
+    const { compressedPublicKey, chainCode } = parseExtendedPublicKey(Buffer.from(xpub, "hex"));
 
-  const oldOperations = initialAccount?.operations || [];
-  //
-  // // Merge new operations with the previously synced ones
-  const usedAddresses: string[] = [
-    ...accountAddresses.usedReceiveAddresses.map(addr => addr.address),
-    ...accountAddresses.usedChangeAddresses.map(addr => addr.address),
-  ];
-  const allOperations: Operation[] = await scanOperations(usedAddresses);
-  const operations = mergeOps(oldOperations, allOperations);
+    const accountAddresses: AccountAddresses = await scanAddresses(
+      compressedPublicKey,
+      chainCode,
+      0,
+    );
 
-  return {
-    id: accountId,
-    xpub: xpub,
-    blockHeight: 0, // this doesn't really make sense in Kaspa
-    balance: accountAddresses.totalBalance,
-    spendableBalance: accountAddresses.spendableBalance,
-    operations,
-    operationsCount: operations.length,
-    nextChangeAddressIndex: accountAddresses.nextChangeAddress.index,
-    nextChangeAddressType: accountAddresses.nextChangeAddress.type,
-    nextChangeAddress: accountAddresses.nextChangeAddress.address,
-    nextReceiveAddressIndex: accountAddresses.nextReceiveAddress.index,
-    nextReceiveAddressType: accountAddresses.nextReceiveAddress.type,
-    nextReceiveAddress: accountAddresses.nextReceiveAddress.address,
+    const oldOperations = initialAccount?.operations || [];
+    //
+    // // Merge new operations with the previously synced ones
+    const usedAddresses: string[] = [
+      ...accountAddresses.usedReceiveAddresses.map(addr => addr.address),
+      ...accountAddresses.usedChangeAddresses.map(addr => addr.address),
+    ];
+    const allOperations: Operation[] = await scanOperations(usedAddresses);
+    const operations = mergeOps(oldOperations, allOperations);
+
+    console.log({ operations, xpub, accountId, accountAddresses });
+
+    console.log(`Total balance: ${accountAddresses.totalBalance.toNumber()}`);
+    console.log(`Spendable balance: ${accountAddresses.spendableBalance.toNumber()}`);
+
+    return {
+      id: accountId,
+      xpub: xpub,
+      blockHeight: 0, // this doesn't really make sense in Kaspa
+      balance: accountAddresses.totalBalance,
+      spendableBalance: accountAddresses.spendableBalance,
+      operations,
+      operationsCount: operations.length,
+      nextChangeAddressIndex: accountAddresses.nextChangeAddress.index,
+      nextChangeAddressType: accountAddresses.nextChangeAddress.type,
+      nextChangeAddress: accountAddresses.nextChangeAddress.address,
+      nextReceiveAddressIndex: accountAddresses.nextReceiveAddress.index,
+      nextReceiveAddressType: accountAddresses.nextReceiveAddress.type,
+      nextReceiveAddress: accountAddresses.nextReceiveAddress.address,
+    };
   };
-};
 
-export const sync = makeSync({ getAccountShape });
+// export const sync = makeSync({ getAccountShape });
