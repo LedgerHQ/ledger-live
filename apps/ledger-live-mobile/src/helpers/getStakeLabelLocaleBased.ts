0249fbf8f45b4c9f223cfd41252ef4390f84e1de
adammino-ledger <andrea.dammino-ext@ledger.fr>
2024-11-25 17:00:46 +0000

    pass country from locale to Earn dashboard

--- apps/ledger-live-mobile/src/helpers/getStakeLabelLocaleBased.ts
+++ apps/ledger-live-mobile/src/helpers/getStakeLabelLocaleBased.ts
@@ -2,3 +2,5 @@ import RNLocalize from "react-native-localize";
 
 export const getStakeLabelLocaleBased = () =>
   RNLocalize.getCountry() === "GB" ? "account.yield" : "account.earn";
+
+export const getCountryLocale = () => RNLocalize.getCountry();
