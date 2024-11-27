a99ed64b00f9dc5d0065d93663483f6231569de7
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-11-27 10:11:29 +0100

    chore: remove unused code
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledger-live-common/src/families/cardano/exchange.ts
+++ libs/ledger-live-common/src/families/cardano/exchange.ts
@@ -1,7 +1,4 @@
-import { STAKING_ADDRESS_INDEX } from "@ledgerhq/coin-cardano/constants";
-import { StakeChain } from "@ledgerhq/coin-cardano/types";
 import { bip32asBuffer } from "../../crypto";
-import { getBipPathFromString, getBipPathString } from "./logic";
 
 const getSerializedAddressParameters = (
   path: string,
@@ -9,27 +6,10 @@ const getSerializedAddressParameters = (
   addressParameters: Buffer;
 } => {
   const addressPath = bip32asBuffer(path);
-  const stakingPath = getStakingPathFromAddressPath(path);
-  const addressParameters = Buffer.concat([
-    new Uint8Array(addressPath),
-    new Uint8Array(Buffer.from([0x22])),
-    new Uint8Array(stakingPath),
-  ]);
   return {
     addressParameters: addressPath,
   };
 };
-
-function getStakingPathFromAddressPath(path: string): Buffer {
-  const { account } = getBipPathFromString(path);
-  const stakingPathString = getBipPathString({
-    account: account,
-    chain: StakeChain.stake,
-    index: STAKING_ADDRESS_INDEX,
-  });
-
-  return bip32asBuffer(stakingPathString);
-}
 
 export default {
   getSerializedAddressParameters,
