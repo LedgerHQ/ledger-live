4067d101fa05675d42600d01ae9ef357afc7ad37
lAmeR1 <lamer1@github.com>
2024-11-28 02:48:25 +0100

    Add Kaspa support with FIFO UTXO selection strategy
    Integrated Kaspa as a new coin with a FIFO UTXO selection strategy. Defined threshold constants and implemented UTXO selection logic to prioritize lower blockDaaScore entries. Ensured compatibility with ECDSA recipients and included error handling for insufficient UTXOs.

--- libs/ledger-live-common/src/generated/formatters.ts
+++ libs/ledger-live-common/src/generated/formatters.ts
@@ -2,6 +2,7 @@ import cosmos from "../families/cosmos/formatters";
 import elrond from "../families/elrond/formatters";
 import algorand from "@ledgerhq/coin-algorand/formatters";
 import bitcoin from "@ledgerhq/coin-bitcoin/formatters";
+import kaspa from "@ledgerhq/coin-kaspa/formatters";
 import polkadot from "@ledgerhq/coin-polkadot/formatters";
 
 export default {
@@ -9,5 +10,6 @@ export default {
   elrond,
   algorand,
   bitcoin,
+  kaspa,
   polkadot,
 };
