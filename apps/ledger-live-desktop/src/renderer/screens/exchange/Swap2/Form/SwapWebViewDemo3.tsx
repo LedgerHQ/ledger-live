4ab56a71ef4ec7ae5d3b546d38d8d7fce930f893
Luis Couto <luis.couto-ext@ledger.fr>
2024-11-29 09:04:21 +0000

    feat(LIVE-14855): remove console.log

--- apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/SwapWebViewDemo3.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/exchange/Swap2/Form/SwapWebViewDemo3.tsx
@@ -169,7 +169,6 @@ const SwapWebView = ({ manifest, liveAppUnavailable }: SwapWebProps) => {
               },
               onCurrencySelected: currency => {
                 lastCurrencySelected = currency;
-                console.log("onCurrencySelected", currency);
               },
               onAddAccountClick: () => {
                 track("button_clicked", {
@@ -338,14 +337,14 @@ const SwapWebView = ({ manifest, liveAppUnavailable }: SwapWebProps) => {
         };
       }): Promise<
         | {
-            hash: string;
-            blockHeight: number | undefined;
-            blockHash: string | undefined;
-            nonce: number;
-            gasUsed: string;
-            gasPrice: string;
-            value: string;
-          }
+          hash: string;
+          blockHeight: number | undefined;
+          blockHash: string | undefined;
+          nonce: number;
+          gasUsed: string;
+          gasPrice: string;
+          value: string;
+        }
         | {}
       > => {
         const realFromAccountId = getAccountIdFromWalletAccountId(params.fromAccountId);
@@ -448,18 +447,18 @@ const SwapWebView = ({ manifest, liveAppUnavailable }: SwapWebProps) => {
         ...(isOffline ? { isOffline: "true" } : {}),
         ...(state?.defaultAccount
           ? {
-              fromAccountId: accountToWalletAPIAccount(
-                walletState,
-                state?.defaultAccount,
-                state?.defaultParentAccount,
-              ).id,
-            }
+            fromAccountId: accountToWalletAPIAccount(
+              walletState,
+              state?.defaultAccount,
+              state?.defaultParentAccount,
+            ).id,
+          }
           : {}),
         ...(state?.from ? { fromPath: simplifyFromPath(state?.from) } : {}),
         ...(swapLiveEnabledFlag?.params && "variant" in swapLiveEnabledFlag.params
           ? {
-              ptxSwapCoreExperiment: swapLiveEnabledFlag.params?.variant as string,
-            }
+            ptxSwapCoreExperiment: swapLiveEnabledFlag.params?.variant as string,
+          }
           : {}),
       }).toString(),
     [
