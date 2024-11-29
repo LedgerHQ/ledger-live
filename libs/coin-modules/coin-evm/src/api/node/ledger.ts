c45ee457a9f5500ae42f2a8fb7f0cfb7926f319b
Canestin Ndong <100531716+Canestin@users.noreply.github.com>
2024-11-29 10:37:39 +0100

    feat: add mev protection for broadcasting (#8375)
    * feat: add mev protection
    
    * add mev protection to swaps
    
    * add changeset
    
    * use the params parameter of axios
    
    * versioning correction
    
    * mev param documentation
    
    * add unit tests
    
    * add test
    
    * fix tests
    
    * fix test
    
    * fix test

--- libs/coin-modules/coin-evm/src/api/node/ledger.ts
+++ libs/coin-modules/coin-evm/src/api/node/ledger.ts
@@ -254,10 +254,12 @@ export const getFeeData: NodeApi["getFeeData"] = async (currency, transaction) =
 
 /**
  * Broadcast a serialized transaction and returns its hash
+ * @param broadcastConfig.mevProtected - Optional flag indicating whether the transaction should be protected against MEV attacks.
  */
 export const broadcastTransaction: NodeApi["broadcastTransaction"] = async (
   currency,
   signedTxHex,
+  broadcastConfig,
 ) => {
   const config = getCoinConfig(currency).info;
   const { node } = config || /* istanbul ignore next */ {};
@@ -271,8 +273,10 @@ export const broadcastTransaction: NodeApi["broadcastTransaction"] = async (
     method: "POST",
     url: `${getEnv("EXPLORER")}/blockchain/v4/${node.explorerId}/tx/send`,
     data: { tx: signedTxHex },
+    params: {
+      mevProtected: Boolean(broadcastConfig?.mevProtected),
+    },
   });
-
   return hash;
 };
 
