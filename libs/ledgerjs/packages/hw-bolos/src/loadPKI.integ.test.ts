b9b8e98e0b69d1329273bd1eb3074971f30a4005
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-11-29 16:33:40 +0100

    fix: import issue
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/ledgerjs/packages/hw-bolos/src/loadPKI.integ.test.ts
+++ libs/ledgerjs/packages/hw-bolos/src/loadPKI.integ.test.ts
@@ -3,7 +3,7 @@ import TransportNodeHidSingleton from "@ledgerhq/hw-transport-node-hid-singleton
 import loadPKI from "./loadPKI";
 import { PKIFailedVerificationError } from "./PKIError";
 
-describe("loadPKI", () => {
+describe("loadPKI with real connected device", () => {
   let transport: Transport;
 
   beforeAll(async () => {
@@ -22,9 +22,10 @@ describe("loadPKI", () => {
 
     // When & Then
     await expect(loadPKI(transport, "TRUSTED_NAME", descriptor, signature)).rejects.not.toThrow();
+    await (() => new Promise(f => setTimeout(f, 1000)))();
   });
 
-  it.only("returns an Verification Failed error", async () => {
+  it("returns an Verification Failed error", async () => {
     // Given
     const descriptor =
       "0101010201023501043601021004010500001302000314010120086e66745f6d657461300200053101043201213401013321035e6c1020c14dc46442fe89f97c0b68cdb15976dc24f24c316e7b30fe4e8cc76b";
