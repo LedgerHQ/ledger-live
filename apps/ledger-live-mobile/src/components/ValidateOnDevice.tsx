534cdb1eec09e6c2595dc0315da190271c96384e
Kant <quentin.jaccarino@ledger.fr>
2024-12-02 15:45:31 +0100

    feat: update warning message for LNS

--- apps/ledger-live-mobile/src/components/ValidateOnDevice.tsx
+++ apps/ledger-live-mobile/src/components/ValidateOnDevice.tsx
@@ -87,6 +87,7 @@ type SubComponentCommonProps = {
   parentAccount?: Account | null | undefined;
   transaction: Transaction;
   status: TransactionStatus;
+  device: Device;
 };
 
 export default function ValidateOnDevice({
@@ -179,6 +180,7 @@ export default function ValidateOnDevice({
               parentAccount={parentAccount}
               transaction={transaction}
               status={status}
+              device={device}
             />
           ) : (
             <TitleText>{titleWording}</TitleText>
@@ -214,6 +216,7 @@ export default function ValidateOnDevice({
                 transaction={transaction}
                 recipientWording={recipientWording}
                 status={status}
+                device={device}
               />
             ) : null}
           </DataRowsContainer>
