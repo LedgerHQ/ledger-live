4ef4ea1fefe91d5b2f30188389afdca645a00f0d
Theophile Sandoz <theophile.sandoz@ledger.fr>
2024-12-04 17:52:38 +0100

    fix(llm): fix long Memo Tag issue in Stacks

--- apps/ledger-live-mobile/src/families/stacks/MemoTagInput.tsx
+++ apps/ledger-live-mobile/src/families/stacks/MemoTagInput.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { STACKS_MAX_MEMO_SIZE } from "@ledgerhq/live-common/families/stacks/constants";
 import type { Transaction as StacksTransaction } from "@ledgerhq/live-common/families/stacks/types";
 import type { MemoTagInputProps } from "LLM/features/MemoTag/types";
 import { GenericMemoTagInput } from "LLM/features/MemoTag/components/GenericMemoTagInput";
@@ -7,6 +8,7 @@ import { GenericMemoTagInput } from "LLM/features/MemoTag/components/GenericMemo
 export default (props: MemoTagInputProps<StacksTransaction>) => (
   <GenericMemoTagInput
     {...props}
+    textToValue={text => text.slice(0, STACKS_MAX_MEMO_SIZE)}
     valueToTxPatch={value => tx => ({ ...tx, memo: value || undefined })}
   />
 );
