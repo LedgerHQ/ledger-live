ef7b062b89413ac243f506f1fe5fb88e510bbb0f
Artem Varlyha <artem.varlyha-ext@ledger.fr>
2024-12-05 11:16:30 +0200

    Apply ledger-live branch changes

--- libs/ledger-live-common/src/families/near/config.ts
+++ libs/ledger-live-common/src/families/near/config.ts
@@ -6,9 +6,9 @@ const nearConfig: CurrencyLiveConfigDefinition = {
     default: {
       status: { type: "active" },
       infra: {
-        API_NEAR_PRIVATE_NODE: "https://near.coin.ledger.com/node",
+        API_NEAR_PRIVATE_NODE: "https://near.coin.ledger-stg.com/node",
         API_NEAR_PUBLIC_NODE: "https://rpc.mainnet.near.org",
-        API_NEAR_INDEXER: "https://near.coin.ledger.com/indexer",
+        API_NEAR_INDEXER: "https://near.coin.ledger-stg.com/indexer",
       },
     },
   },
