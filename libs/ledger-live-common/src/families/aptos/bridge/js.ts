316a586717ee56fe9692495b43b3378bb4ebd3aa
OleksiiM <omblinda@gmail.com>
2024-12-16 13:44:33 +0000

    Bridge contains all methods

--- libs/ledger-live-common/src/families/aptos/bridge/js.ts
+++ libs/ledger-live-common/src/families/aptos/bridge/js.ts
@@ -11,8 +11,6 @@ import estimateMaxSpendable from "../js-estimateMaxSpendable";
 import signOperation from "../js-signOperation";
 import broadcast from "../js-broadcast";
 
-const receive = makeAccountBridgeReceive();
-
 const currencyBridge: CurrencyBridge = {
   preload: () => Promise.resolve({}),
   hydrate: () => {},
@@ -23,6 +21,8 @@ const updateTransaction = (t: Transaction, patch: Partial<Transaction>): Transac
   ...t,
   ...patch,
 });
+
+const receive = makeAccountBridgeReceive();
 
 const accountBridge: AccountBridge<Transaction> = {
   estimateMaxSpendable,
