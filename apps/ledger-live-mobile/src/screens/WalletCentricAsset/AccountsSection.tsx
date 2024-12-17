6bc229736b1c2d715037683e2f326dba06c595e8
Lucas Werey <lucas.werey@ledger.fr>
2024-12-17 14:05:49 +0100

    feat: :sparkles: (llm)add new accounts list to wallet centric screen

--- apps/ledger-live-mobile/src/screens/WalletCentricAsset/AccountsSection.tsx
+++ apps/ledger-live-mobile/src/screens/WalletCentricAsset/AccountsSection.tsx
@@ -10,6 +10,9 @@ import { NavigatorName, ScreenName } from "~/const";
 import { track } from "~/analytics";
 import { AccountsNavigatorParamList } from "~/components/RootNavigator/types/AccountsNavigator";
 import { BaseComposite, StackNavigatorProps } from "~/components/RootNavigator/types/helpers";
+import AccountsList from "LLM/features/Accounts/components/AccountsListView";
+import { FeatureToggle, useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import AddAccountButton from "LLM/features/Accounts/components/AddAccountButton";
 
 const NB_MAX_ACCOUNTS_TO_DISPLAY = 3;
 
@@ -19,11 +22,13 @@ type ListProps = {
   accounts: Account[] | TokenAccount[];
   currencyId: string;
   currencyTicker: string;
+  onAddAccount: () => void;
 };
 
-const AccountsSection = ({ accounts, currencyId, currencyTicker }: ListProps) => {
+const AccountsSection = ({ accounts, currencyId, currencyTicker, onAddAccount }: ListProps) => {
   const navigation = useNavigation<Navigation["navigation"]>();
   const { t } = useTranslation();
+  const llmAccountListUI = useFeature("llmAccountListUI");
 
   const accountsToDisplay = useMemo(
     () => accounts.slice(0, NB_MAX_ACCOUNTS_TO_DISPLAY),
@@ -46,26 +51,50 @@ const AccountsSection = ({ accounts, currencyId, currencyTicker }: ListProps) =>
     track("button_clicked", {
       button: "See All",
     });
-    navigation.navigate(NavigatorName.Accounts, {
-      screen: ScreenName.Accounts,
-      params: {
-        currencyId,
-        currencyTicker,
-      },
-    });
-  }, [navigation, currencyId, currencyTicker]);
+    if (llmAccountListUI?.enabled) {
+      navigation.navigate(NavigatorName.Accounts, {
+        screen: ScreenName.AccountsList,
+        params: {
+          sourceScreenName: ScreenName.Asset,
+          showHeader: true,
+          canAddAccount: true,
+          isSyncEnabled: true,
+          specificAccounts: accounts as Account[],
+        },
+      });
+    } else {
+      navigation.navigate(NavigatorName.Accounts, {
+        screen: ScreenName.Accounts,
+        params: {
+          currencyId,
+          currencyTicker,
+        },
+      });
+    }
+  }, [llmAccountListUI, navigation, accounts, currencyId, currencyTicker]);
 
   return (
     <>
-      <FlatList<Account | TokenAccount>
-        data={accountsToDisplay}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        contentContainerStyle={{ flex: 1 }}
-      />
+      <FeatureToggle
+        featureId="llmAccountListUI"
+        fallback={
+          <FlatList<Account | TokenAccount>
+            data={accountsToDisplay}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            contentContainerStyle={{ flex: 1 }}
+          />
+        }
+      >
+        <AccountsList
+          limitNumberOfAccounts={NB_MAX_ACCOUNTS_TO_DISPLAY}
+          specificAccounts={accountsToDisplay}
+        />
+        <AddAccountButton sourceScreenName={ScreenName.Asset} onClick={onAddAccount} />
+      </FeatureToggle>
       {accounts.length > NB_MAX_ACCOUNTS_TO_DISPLAY ? (
-        <Button type="shade" size="large" outline mt={6} onPress={goToAccountsScreen}>
-          {t("common.seeAllWithNumber", { number: accounts.length })}
+        <Button type="shade" size="large" outline mt={4} onPress={goToAccountsScreen}>
+          {t("addAccounts.seeAllAccounts")}
         </Button>
       ) : null}
     </>
