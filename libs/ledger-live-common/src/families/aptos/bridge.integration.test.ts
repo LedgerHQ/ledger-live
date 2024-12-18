ab1e665f82daeb47f4c7e9d58a3efb3f2a2b5787
Pedro Semeano <semeano@gmail.com>
2024-12-18 10:39:01 +0000

    test: create new snapshot

--- libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
@@ -26,10 +26,220 @@ const aptos: CurrenciesData<Transaction> = {
       <= 2104c5c84a917f3f8b72a2e8e52ed7bc701fadd6fed638c3fa68c077ccd72773391b20c4fb9828a463809dcaee1965f03071d446658ecf75b3b6932dc122b30c7e2cc09000
       => 5b05000015058000002c8000027d800000028000000080000000
       <= 2104c5c84a917f3f8b72a2e8e52ed7bc701fadd6fed638c3fa68c077ccd72773391b20c4fb9828a463809dcaee1965f03071d446658ecf75b3b6932dc122b30c7e2cc09000
+      => 5b05000015058000002c8000027d800000038000000080000000
+      <= 2104b04acc7b31ed7d49690756383d0f2594bc6053f18456057d29650017514fcdc720de6fccf892eca84c7cd12deb99626e82f1a4515c663a8092dfd2d33a9d15d1409000
+      => 5b05000015058000002c8000027d800000038000000080000000
+      <= 2104b04acc7b31ed7d49690756383d0f2594bc6053f18456057d29650017514fcdc720de6fccf892eca84c7cd12deb99626e82f1a4515c663a8092dfd2d33a9d15d1409000
+      => 5b05000015058000002c8000027d800000048000000080000000
+      <= 21045d9a85f25e0bd8bbedd210407072f13a2e5ea7744497571146682b2db3250ff720cf6739cca4963ecf47419f2672766ef1bf06bc470da6b176d8533896956e46a59000
+      => 5b05000015058000002c8000027d800000048000000080000000
+      <= 21045d9a85f25e0bd8bbedd210407072f13a2e5ea7744497571146682b2db3250ff720cf6739cca4963ecf47419f2672766ef1bf06bc470da6b176d8533896956e46a59000
       `,
     },
   ],
   accounts: [
+    // {
+    //   raw: {
+    //     id: "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
+    //     seedIdentifier: "308656dc38a7ab1f9b5ab966fda3484276fe755da4ecdbcc678e96bf3cb460cf",
+    //     used: true,
+    //     derivationMode: "",
+    //     index: 1,
+    //     freshAddress: "0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1",
+    //     freshAddressPath: "44'/637'/1'/0/0",
+    //     blockHeight: 265578715,
+    //     creationDate: "2024-12-10T17:33:27.634Z",
+    //     operationsCount: 2,
+    //     operations: [],
+    //     pendingOperations: [],
+    //     currencyId: "aptos",
+    //     lastSyncDate: "2024-12-16T16:08:47.017Z",
+    //     balance: "20000",
+    //     spendableBalance: "20000",
+    //     balanceHistoryCache: {
+    //       HOUR: {
+    //         balances: [
+    //           0, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //         ],
+    //         latestDate: 1734364800000,
+    //       },
+    //       DAY: {
+    //         balances: [0, 10000, 10000, 20000, 20000, 20000, 20000],
+    //         latestDate: 1734307200000,
+    //       },
+    //       WEEK: { balances: [0, 20000], latestDate: 1734220800000 },
+    //     },
+    //     xpub: "32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc",
+    //     swapHistory: [],
+    //   },
+    //   transactions: [
+    //     {
+    //       name: "NO_NAME",
+    //       transaction: fromTransactionRaw({
+    //         amount: "10000",
+    //         recipient: "0xa0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec8",
+    //         useAllAmount: false,
+    //         family: "aptos",
+    //         mode: "send",
+    //         fees: "1100",
+    //         options: '{"maxGasAmount":"11","gasUnitPrice":"100"}',
+    //         estimate:
+    //           '{"maxGasAmount":"11","gasUnitPrice":"100","sequenceNumber":"0","expirationTimestampSecs":"1734365447"}',
+    //         firstEmulation: "false",
+    //         errors: "{}",
+    //       }),
+    //       expectedStatus: () =>
+    //         // you can use account and transaction for smart logic. drop the =>fn otherwise
+    //         ({
+    //           errors: {},
+    //           warnings: {},
+    //           estimatedFees: BigNumber("1100"),
+    //           amount: BigNumber("10000"),
+    //           totalSpent: BigNumber("11100"),
+    //         }),
+    //       // WARNING: DO NOT commit this test publicly unless you're ok with possibility tx could leak out. (do self txs)
+    //       testSignedOperation: (expect, signedOperation) => {
+    //         expect(toSignedOperationRaw(signedOperation)).toMatchObject({
+    //           operation: {
+    //             id: "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:--OUT",
+    //             hash: "",
+    //             type: "OUT",
+    //             senders: ["0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1"],
+    //             recipients: ["0xa0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec8"],
+    //             accountId:
+    //               "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
+    //             blockHash: null,
+    //             blockHeight: null,
+    //             extra: {},
+    //             date: "2024-12-16T16:09:11.918Z",
+    //             value: "11100",
+    //             fee: "1100",
+    //             transactionSequenceNumber: 0,
+    //           },
+    //           signature:
+    //             "4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d100000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e73010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220a0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec80810270000000000000b000000000000006400000000000000075160670000000001002032820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc4013c5352850e567a1289b38569bdbd2b1229f2bc6ce24083d25e2d56908d263fab1d374486597cd959e02b3cd79d40934099175e835fdff136c6164823e88860c",
+    //         });
+    //       },
+    //     },
+    //   ],
+    // },
+    // {
+    //   raw: {
+    //     id: "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
+    //     seedIdentifier: "308656dc38a7ab1f9b5ab966fda3484276fe755da4ecdbcc678e96bf3cb460cf",
+    //     used: true,
+    //     derivationMode: "",
+    //     index: 1,
+    //     freshAddress: "0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1",
+    //     freshAddressPath: "44'/637'/1'/0/0",
+    //     blockHeight: 265987327,
+    //     creationDate: "2024-12-10T17:33:27.634Z",
+    //     operationsCount: 9,
+    //     operations: [],
+    //     pendingOperations: [],
+    //     currencyId: "aptos",
+    //     lastSyncDate: "2024-12-17T16:40:10.185Z",
+    //     balance: "89700",
+    //     spendableBalance: "89700",
+    //     balanceHistoryCache: {
+    //       HOUR: {
+    //         balances: [
+    //           0, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000,
+    //           10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+    //           20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20600, 199600, 199600,
+    //         ],
+    //         latestDate: 1734451200000,
+    //       },
+    //       DAY: {
+    //         balances: [0, 10000, 10000, 20000, 20000, 20000, 20000, 20000],
+    //         latestDate: 1734393600000,
+    //       },
+    //       WEEK: { balances: [0, 20000], latestDate: 1734220800000 },
+    //     },
+    //     xpub: "32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc",
+    //     swapHistory: [],
+    //   },
+    //   transactions: [
+    //     {
+    //       name: "NO_NAME",
+    //       transaction: fromTransactionRaw({
+    //         amount: "10000",
+    //         recipient: "0xa26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e4",
+    //         useAllAmount: false,
+    //         family: "aptos",
+    //         mode: "send",
+    //         fees: "1100",
+    //         options: '{"maxGasAmount":"11","gasUnitPrice":"100"}',
+    //         estimate:
+    //           '{"maxGasAmount":"11","gasUnitPrice":"100","sequenceNumber":"4","expirationTimestampSecs":"1734453730"}',
+    //         firstEmulation: "false",
+    //         errors: "{}",
+    //       }),
+    //       expectedStatus: () =>
+    //         // you can use account and transaction for smart logic. drop the =>fn otherwise
+    //         ({
+    //           errors: {},
+    //           warnings: {},
+    //           estimatedFees: BigNumber("1100"),
+    //           amount: BigNumber("10000"),
+    //           totalSpent: BigNumber("11100"),
+    //         }),
+    //       // WARNING: DO NOT commit this test publicly unless you're ok with possibility tx could leak out. (do self txs)
+    //       testSignedOperation: (expect, signedOperation) => {
+    //         expect(toSignedOperationRaw(signedOperation)).toMatchObject({
+    //           operation: {
+    //             id: "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:--OUT",
+    //             hash: "",
+    //             type: "OUT",
+    //             senders: ["0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1"],
+    //             recipients: ["0xa26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e4"],
+    //             accountId:
+    //               "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
+    //             blockHash: null,
+    //             blockHeight: null,
+    //             extra: {},
+    //             date: "2024-12-17T16:40:20.236Z",
+    //             value: "11100",
+    //             fee: "1100",
+    //             transactionSequenceNumber: 4,
+    //           },
+    //           signature:
+    //             "4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d104000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e73010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220a26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e40810270000000000000b000000000000006400000000000000e3a961670000000001002032820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc40d9e80e9e148c6107a331a58ee79f403a7da81e892c0e3abf03b4a88c15439e9eafb1d06379918a2bb62dee3c2b17af2fd93b78fad05853a743370ddb6198ac02",
+    //         });
+    //       },
+    //       apdus: `
+    //   => 5b06008015058000002c8000027d800000018000000080000000
+    //   <= 9000
+    //   => 5b060180ffb5e97db07fa0bd0e5598aa3643a9bc6f6693bddc1a9fec9e674a461eaa00b1934e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d104000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e73010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220a26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e40810270000000000000b000000000000006400000000000000e3a961670000
+    //   <= 9000
+    //   => 5b06020003000001
+    //   <= 40d9e80e9e148c6107a331a58ee79f403a7da81e892c0e3abf03b4a88c15439e9eafb1d06379918a2bb62dee3c2b17af2fd93b78fad05853a743370ddb6198ac029000
+    //   `,
+    //     },
+    //   ],
+    // },
     {
       raw: {
         id: "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
@@ -39,15 +249,15 @@ const aptos: CurrenciesData<Transaction> = {
         index: 1,
         freshAddress: "0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1",
         freshAddressPath: "44'/637'/1'/0/0",
-        blockHeight: 265578715,
+        blockHeight: 266283511,
         creationDate: "2024-12-10T17:33:27.634Z",
-        operationsCount: 2,
+        operationsCount: 9,
         operations: [],
         pendingOperations: [],
         currencyId: "aptos",
-        lastSyncDate: "2024-12-16T16:08:47.017Z",
-        balance: "20000",
-        spendableBalance: "20000",
+        lastSyncDate: "2024-12-18T10:31:45.000Z",
+        balance: "89700",
+        spendableBalance: "89700",
         balanceHistoryCache: {
           HOUR: {
             balances: [
@@ -62,13 +272,17 @@ const aptos: CurrenciesData<Transaction> = {
               20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
               20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
               20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
-              20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+              20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+              20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000,
+              20000, 20000, 20000, 20000, 20000, 20000, 20000, 20000, 20600, 199600, 199600, 89700,
+              89700, 89700, 89700, 89700, 89700, 89700, 89700, 89700, 89700, 89700, 89700, 89700,
+              89700, 89700, 89700, 89700, 89700,
             ],
-            latestDate: 1734364800000,
+            latestDate: 1734516000000,
           },
           DAY: {
-            balances: [0, 10000, 10000, 20000, 20000, 20000, 20000],
-            latestDate: 1734307200000,
+            balances: [0, 10000, 10000, 20000, 20000, 20000, 20000, 20000, 89700],
+            latestDate: 1734480000000,
           },
           WEEK: { balances: [0, 20000], latestDate: 1734220800000 },
         },
@@ -80,14 +294,14 @@ const aptos: CurrenciesData<Transaction> = {
           name: "NO_NAME",
           transaction: fromTransactionRaw({
             amount: "10000",
-            recipient: "0xa0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec8",
+            recipient: "0xa26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e4",
             useAllAmount: false,
             family: "aptos",
             mode: "send",
             fees: "1100",
             options: '{"maxGasAmount":"11","gasUnitPrice":"100"}',
             estimate:
-              '{"maxGasAmount":"11","gasUnitPrice":"100","sequenceNumber":"0","expirationTimestampSecs":"1734365447"}',
+              '{"maxGasAmount":"11","gasUnitPrice":"100","sequenceNumber":"4","expirationTimestampSecs":"1734518025"}',
             firstEmulation: "false",
             errors: "{}",
           }),
@@ -108,19 +322,19 @@ const aptos: CurrenciesData<Transaction> = {
                 hash: "",
                 type: "OUT",
                 senders: ["0x4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d1"],
-                recipients: ["0xa0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec8"],
+                recipients: ["0xa26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e4"],
                 accountId:
                   "js:2:aptos:32820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc:",
                 blockHash: null,
                 blockHeight: null,
                 extra: {},
-                date: "2024-12-16T16:09:11.918Z",
+                date: "2024-12-18T10:31:56.628Z",
                 value: "11100",
                 fee: "1100",
-                transactionSequenceNumber: 0,
+                transactionSequenceNumber: 4,
               },
               signature:
-                "4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d100000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e73010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220a0d8abc262e3321f87d745bd5d687e8f3fb14c87d48f840b6b56867df0026ec80810270000000000000b000000000000006400000000000000075160670000000001002032820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc4013c5352850e567a1289b38569bdbd2b1229f2bc6ce24083d25e2d56908d263fab1d374486597cd959e02b3cd79d40934099175e835fdff136c6164823e88860c",
+                "4e5e65d5c7a3191e4310ecd210e8f0ff53823189123b47086d928bd574a573d104000000000000000200000000000000000000000000000000000000000000000000000000000000010d6170746f735f6163636f756e740e7472616e736665725f636f696e73010700000000000000000000000000000000000000000000000000000000000000010a6170746f735f636f696e094170746f73436f696e000220a26f3fce4d9e54f2a19dc4604f24f2a55aa14d8f0ab5a8721af3e8d8ea4b36e40810270000000000000b00000000000000640000000000000009a562670000000001002032820695b4f7973b278305dc3a1f2d9df91d795b22c811cee46c821b4c91f4cc4070ab5c79121c77fb9c2a57430f433fded0557c20a20d9df380a3f12d2a3bd1a9e8ca4b28c2d4332e6b4612bd70f0379cfe13caca4ff5fed68e8f1efa21fbf605",
             });
           },
         },
