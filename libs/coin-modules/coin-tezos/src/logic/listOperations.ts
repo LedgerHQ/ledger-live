da3ef953ec137a87b9385a769cbaede903cc16bf
sbelkhir-ledger <salim.belkhir@ledger.fr>
2024-12-23 17:03:40 +0100

    correction with Samy comments

--- libs/coin-modules/coin-tezos/src/logic/listOperations.ts
+++ libs/coin-modules/coin-tezos/src/logic/listOperations.ts
@@ -1,6 +1,5 @@
 import { tzkt } from "../network";
 import {
-  APIBlock,
   type APIDelegationType,
   type APITransactionType,
   isAPIDelegationType,
@@ -29,21 +28,17 @@ export async function listOperations(
   { lastId, limit }: { lastId?: number; limit?: number },
 ): Promise<[Operation[], number]> {
   const operations = await tzkt.getAccountOperations(address, { lastId, limit });
-  const operationswithBlock = await Promise.all(
+  return [
     operations
       .filter(op => isAPITransactionType(op) || isAPIDelegationType(op))
-      .map(async op => {
-        const block = await tzkt.getBlockByHash(op.block);
-        return convertOperation(address, op, block);
-      }),
-  );
-  return [operationswithBlock, operations.slice(-1)[0].id];
+      .reduce((acc, op) => acc.concat(convertOperation(address, op)), [] as Operation[]),
+    operations.slice(-1)[0].id,
+  ];
 }
 
 function convertOperation(
   address: string,
   operation: APITransactionType | APIDelegationType,
-  block: APIBlock,
 ): Operation {
   const { amount, hash, storageFee, sender, timestamp, type, counter } = operation;
   let targetAddress = "";
@@ -58,9 +53,9 @@ function convertOperation(
     // storageFee for transaction is always present
     fee: BigInt(storageFee ?? 0),
     block: {
-      hash: block.hash,
-      height: block.level,
-      time: new Date(block.timestamp),
+      hash: operation.block,
+      height: operation.level,
+      time: new Date(operation.timestamp),
     },
     senders: [sender?.address ?? ""],
     recipients: [targetAddress],
