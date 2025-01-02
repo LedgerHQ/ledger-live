56b765cfc6269302433d892e67becda254be86ea
João Guimarães <joao.guimaraes-ext@ledger.fr>
2025-01-02 09:55:47 +0000

    test(aptos): improve unit tests

--- libs/ledger-live-common/src/families/aptos/getTransactionStatus.ts
+++ libs/ledger-live-common/src/families/aptos/getTransactionStatus.ts
@@ -47,7 +47,7 @@ const getTransactionStatus = async (a: Account, t: Transaction): Promise<Transac
   } else if (!isValidAddress(t.recipient)) {
     errors.recipient = new InvalidAddress("", { currencyName: a.currency.name });
   } else if (t.recipient === a.freshAddress) {
-    errors.recepient = new InvalidAddressBecauseDestinationIsAlsoSource();
+    errors.recipient = new InvalidAddressBecauseDestinationIsAlsoSource();
   }
 
   if (
