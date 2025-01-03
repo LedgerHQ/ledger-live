e1f1b5077cb746079b3d35a2493f4beb60216cdf
João Guimarães <joao.guimaraes-ext@ledger.fr>
2025-01-03 15:41:04 +0000

    test(aptos): improve unit tests

--- libs/ledger-live-common/src/families/aptos/buildTransaction.test.ts
+++ libs/ledger-live-common/src/families/aptos/buildTransaction.test.ts
@@ -2,9 +2,17 @@ import { createFixtureAccount } from "../../mock/fixtures/cryptoCurrencies";
 import createTransaction from "./createTransaction";
 import buildTransaction from "./buildTransaction";
 import { AptosAPI } from "./api";
+import { normalizeTransactionOptions } from "./logic";
+import { InputEntryFunctionData } from "@aptos-labs/ts-sdk";
+import { TransactionOptions } from "./types";
+
+const generateTransaction = jest.fn(() => "tx");
 
 jest.mock("./logic", () => ({
-  normalizeTransactionOptions: jest.fn(),
+  normalizeTransactionOptions: jest.fn(() => ({
+    maxGasAmount: "100",
+    gasUnitPrice: "200",
+  })),
   DEFAULT_GAS: 100,
   DEFAULT_GAS_PRICE: 200,
 }));
@@ -13,7 +21,7 @@ jest.mock("./api", () => {
   return {
     AptosAPI: function () {
       return {
-        generateTransaction: jest.fn(() => "tx"),
+        generateTransaction,
       };
     },
   };
@@ -29,5 +37,26 @@ describe("buildTransaction Test", () => {
     const expected = "tx";
 
     expect(result).toBe(expected);
+
+    const mockedNormalizeTransactionOptions = jest.mocked(normalizeTransactionOptions);
+
+    expect(mockedNormalizeTransactionOptions).toHaveBeenCalledTimes(1);
+    expect(generateTransaction).toHaveBeenCalledTimes(1);
+
+    const generateTransactionArgs: [string, InputEntryFunctionData, TransactionOptions][] =
+      generateTransaction.mock.calls[0];
+
+    expect(mockedNormalizeTransactionOptions.mock.calls[0][0]).toEqual({
+      maxGasAmount: "100",
+      gasUnitPrice: "200",
+    });
+
+    expect(generateTransactionArgs[0]).toBe("0x01");
+    expect(generateTransactionArgs[1]).toEqual({
+      function: "0x1::aptos_account::transfer_coins",
+      typeArguments: ["0x1::aptos_coin::AptosCoin"],
+      functionArguments: ["", "0"],
+    });
+    expect(generateTransactionArgs[2]).toEqual({ maxGasAmount: "100", gasUnitPrice: "200" });
   });
 });
