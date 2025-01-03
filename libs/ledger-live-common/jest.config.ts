6ab6d11284dc942a76e33ccca86e09969edefa0d
Pedro Semeano <semeano@gmail.com>
2025-01-03 18:14:54 +0000

    fix: remove jest config

--- libs/ledger-live-common/jest.config.ts
+++ libs/ledger-live-common/jest.config.ts
@@ -69,13 +69,13 @@ export default {
   globalSetup: process.env.UPDATE_BACKEND_MOCKS
     ? "<rootDir>/src/__tests__/test-helpers/bridgeSetupUpdateMocks.ts"
     : process.env.USE_BACKEND_MOCKS
-      ? "<rootDir>/src/__tests__/test-helpers/bridgeSetupUseMocks.ts"
-      : undefined,
+    ? "<rootDir>/src/__tests__/test-helpers/bridgeSetupUseMocks.ts"
+    : undefined,
   globalTeardown: process.env.UPDATE_BACKEND_MOCKS
     ? "<rootDir>/src/__tests__/test-helpers/bridgeTeardownUpdateMocks.ts"
     : process.env.USE_BACKEND_MOCKS
-      ? "<rootDir>/src/__tests__/test-helpers/bridgeTeardownUseMocks.ts"
-      : undefined,
+    ? "<rootDir>/src/__tests__/test-helpers/bridgeTeardownUseMocks.ts"
+    : undefined,
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   coverageReporters: ["json", "lcov", "clover", "json-summary", "html"],
