25765a33cb3911516b0614a2014901a7770d4f3f
Victor Alber <victor.alber@ledger.fr>
2025-01-03 15:29:00 +0100

    test: ✅ Updating method to get the provider - E2E Swap Tests

--- libs/ledger-live-common/src/e2e/models/Swap.ts
+++ libs/ledger-live-common/src/e2e/models/Swap.ts
@@ -1,25 +1,17 @@
 import { Transaction } from "./Transaction";
 import { Fee } from "../enum/Fee";
 import { Account } from "../enum/Account";
-import { Provider, Rate } from "../enum/Swap";
 
 export class Swap extends Transaction {
-  provider: Provider;
-  rate: Rate;
-
   constructor(
     accountToDebit: Account,
     accountToCredit: Account,
     amount: string,
     speed: Fee,
-    provider: Provider,
-    rate: Rate,
     public amountToReceive?: string,
     public feesAmount?: string,
   ) {
     super(accountToDebit, accountToCredit, amount, speed);
-    this.provider = provider;
-    this.rate = rate;
   }
 
   public setAmountToReceive(value: string) {
