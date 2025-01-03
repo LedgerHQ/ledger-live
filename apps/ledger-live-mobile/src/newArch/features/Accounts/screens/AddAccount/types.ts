a070f4ee9ff3d3b5cddfc4e03b703fb289b4cc36
Mounir Hamzaoui <mounir.hamzaoui@ledger.fr>
2025-01-03 17:21:31 +0100

    feat: migrate all entry screen, ctas to new add account v2 (#8670)
    feat: migrate all entry screen, ctas to new add account (uncompleted v2) flow

--- apps/ledger-live-mobile/src/newArch/features/Accounts/screens/AddAccount/types.ts
+++ apps/ledger-live-mobile/src/newArch/features/Accounts/screens/AddAccount/types.ts
@@ -2,12 +2,16 @@ import { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { ScreenName } from "~/const";
 import { Device } from "@ledgerhq/types-devices";
 
+type CommonParams = {
+  context?: "addAccounts" | "receiveFunds";
+  onSuccess?: () => void;
+};
 export type NetworkBasedAddAccountNavigator = {
-  [ScreenName.SelectAccounts]: {
+  [ScreenName.SelectAccounts]: CommonParams & {
     currency: CryptoCurrency | TokenCurrency;
     createTokenAccount?: boolean;
   };
-  [ScreenName.ScanDeviceAccounts]: {
+  [ScreenName.ScanDeviceAccounts]: CommonParams & {
     currency: CryptoCurrency | TokenCurrency;
     device: Device;
     onSuccess?: (_?: unknown) => void;
