71697b7d7831110782a7248731711d64b30b876f
João Martins <joaomartinsfcup@gmail.com>
2025-01-07 14:23:20 +0000

    Added the LedgerAccount test.

--- libs/ledger-live-common/src/families/aptos/LedgerAccount.test.ts
+++ libs/ledger-live-common/src/families/aptos/LedgerAccount.test.ts
@@ -1,37 +1,93 @@
 import LedgerAccount from "./LedgerAccount";
+import { createFixtureAccount } from "../../mock/fixtures/cryptoCurrencies";
+import { AccountAddress, Hex } from "@aptos-labs/ts-sdk";
+import HwAptos from "@ledgerhq/hw-app-aptos";
+import Transport from "@ledgerhq/hw-transport";
+import { AptosAPI } from "./api";
+import { APTOS_ASSET_ID } from "./constants";
+import { Account } from "../../e2e/enum/Account";
 
-jest.mock("./LedgerAccount");
-const mockedLedgerAccount = jest.mocked(LedgerAccount);
+jest.mock("@ledgerhq/hw-app-aptos");
 
 describe("LedgerAccount Test", () => {
-  let account: LedgerAccount;
-
-  beforeAll(() => {
-    account = new LedgerAccount("", "");
+  it("Testing constructor", async () => {
+    const account = createFixtureAccount();
+    const ledger_account = new LedgerAccount(account.freshAddressPath);
+    const expected = {
+      hdPath: account.freshAddressPath,
+      publicKey: Buffer.from([]),
+      accountAddress: new AccountAddress(new Uint8Array(AccountAddress.LENGTH)),
+    };
+    expect(ledger_account).toEqual(expected);
   });
 
-  it("builds the client properly", () => {
-    expect(LedgerAccount.fromLedgerConnection).toBeDefined();
-    expect(typeof LedgerAccount.fromLedgerConnection).toBe("function");
-    expect(account.init).toBeDefined();
-    expect(typeof account.init).toBe("function");
-    expect(account.toAptosAccount).toBeDefined();
-    expect(typeof account.toAptosAccount).toBe("function");
-    expect(account.hdWalletPath).toBeDefined();
-    expect(typeof account.hdWalletPath).toBe("function");
-    expect(account.address).toBeDefined();
-    expect(typeof account.address).toBe("function");
-    expect(account.authKey).toBeDefined();
-    expect(typeof account.authKey).toBe("function");
-    expect(account.pubKey).toBeDefined();
-    expect(typeof account.pubKey).toBe("function");
-    expect(account.asyncSignBuffer).toBeDefined();
-    expect(typeof account.asyncSignBuffer).toBe("function");
-    expect(account.asyncSignHexString).toBeDefined();
-    expect(typeof account.asyncSignHexString).toBe("function");
-    expect(account.signTransaction).toBeDefined();
-    expect(typeof account.signTransaction).toBe("function");
+  it("Testing init method", async () => {
+    const account = createFixtureAccount();
+    const ledger_account = new LedgerAccount(account.freshAddressPath);
+    const transport = {} as Transport;
+    const mockGetAddress = jest.fn().mockResolvedValue({
+      address: account.freshAddress,
+      publicKey: Buffer.from("publicKey"),
+    });
+    HwAptos.prototype.getAddress = mockGetAddress;
 
-    expect(mockedLedgerAccount).toHaveBeenCalledTimes(1);
+    await ledger_account.init(transport);
+
+    expect(mockGetAddress).toHaveBeenCalledWith(account.freshAddressPath, false);
+    expect(ledger_account["publicKey"]).toEqual(Buffer.from("publicKey"));
+    expect(ledger_account["accountAddress"]).toEqual(AccountAddress.from(account.freshAddress));
+  });
+
+  it("Testing authKey method", () => {
+    const account = createFixtureAccount();
+    const ledger_account = new LedgerAccount(account.freshAddressPath, account.freshAddress);
+    const authKey = ledger_account.authKey();
+
+    expect(authKey).toBeInstanceOf(AccountAddress);
+  });
+
+  it("Testing asyncSignBuffer method", async () => {
+    const account = createFixtureAccount();
+    const ledger_account = new LedgerAccount(account.freshAddressPath);
+    const transport = {} as Transport;
+    const mockSignTransaction = jest.fn().mockResolvedValue({
+      signature: Buffer.from("signature"),
+    });
+    HwAptos.prototype.signTransaction = mockSignTransaction;
+
+    await ledger_account.init(transport);
+    const buffer = new Uint8Array([1, 2, 3]);
+    const signature = await ledger_account.asyncSignBuffer(buffer);
+
+    expect(mockSignTransaction).toHaveBeenCalledWith(account.freshAddressPath, Buffer.from(buffer));
+    expect(signature).toEqual(new Hex(new Uint8Array(Buffer.from("signature"))));
+  });
+
+  it("Testing signTransaction method", async () => {
+    const api = new AptosAPI("aptos");
+    const ledger_account = new LedgerAccount(Account.APTOS_1.address);
+    const transport = {} as Transport;
+    const mockSignTransaction = jest.fn().mockResolvedValue({
+      signature: Buffer.from("signature"),
+    });
+    HwAptos.prototype.signTransaction = mockSignTransaction;
+
+    await ledger_account.init(transport);
+    const payload = {
+      function: "0x1::aptos_account::transfer_coins",
+      typeArguments: [APTOS_ASSET_ID],
+      functionArguments: [Account.APTOS_1.address, BigInt(1).toString()],
+    };
+    const options = {
+      maxGasAmount: "100",
+      gasUnitPrice: "50",
+      sequenceNumber: "1",
+      expirationTimestampSecs: "1735639799486",
+    };
+
+    const rawTxn = await api.generateTransaction(Account.APTOS_1.address, payload, options);
+    const signedTxn = await ledger_account.signTransaction(rawTxn);
+
+    expect(signedTxn).toBeInstanceOf(Uint8Array);
   });
 });
