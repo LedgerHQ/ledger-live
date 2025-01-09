6cc0573b0bf3340a2460471dffd68a8251751aa9
OleksiiM <omblinda@gmail.com>
2025-01-09 15:58:02 +0000

    Remove file with unused errors

--- libs/ledger-live-common/src/families/aptos/errors.ts
+++
@@ -1,7 +0,0 @@
-import { createCustomErrorClass } from "@ledgerhq/errors";
-
-export const SequenceNumberTooOldError = createCustomErrorClass("SequenceNumberTooOld");
-
-export const SequenceNumberTooNewError = createCustomErrorClass("SequenceNumberTooNew");
-
-export const TransactionExpiredError = createCustomErrorClass("TransactionExpired");
