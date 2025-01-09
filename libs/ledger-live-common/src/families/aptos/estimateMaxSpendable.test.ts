17ad5fe20320209b88253c8a9e4b32f0fa740184
OleksiiM <omblinda@gmail.com>
2025-01-09 15:44:07 +0000

    code review update

--- libs/ledger-live-common/src/families/aptos/estimateMaxSpendable.test.ts
+++ libs/ledger-live-common/src/families/aptos/estimateMaxSpendable.test.ts
@@ -4,18 +4,14 @@ import estimateMaxSpendable from "./estimateMaxSpendable";
 import BigNumber from "bignumber.js";
 
 jest.mock("./getFeesForTransaction", () => ({
-  getEstimatedGas: jest.fn(() =>
-    Promise.resolve({
-      fees: new BigNumber(0),
-      estimate: {
-        maxGasAmount: 1,
-        gasUnitPrice: 2,
-        sequenceNumber: "",
-        expirationTimestampSecs: "",
-      },
-      errors: {},
-    }),
-  ),
+  getEstimatedGas: jest.fn(() => ({
+    fees: new BigNumber(0),
+    estimate: {
+      maxGasAmount: 1,
+      gasUnitPrice: 2,
+    },
+    errors: {},
+  })),
 }));
 
 describe("estimateMaxSpendable Test", () => {
