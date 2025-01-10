d2fbe968a967f998cc6c0648a261e2219a5c3b7a
OleksiiM <omblinda@gmail.com>
2025-01-10 13:34:47 +0000

    Review update

--- libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
@@ -64,8 +64,7 @@ const aptos: CurrenciesData<Transaction> = {
             family: "aptos",
             mode: "send",
             fees: "1100",
-            options:
-              '{ "maxGasAmount": "11", "gasUnitPrice": "100", "sequenceNumber": "1", "expirationTimestampSecs": "1734535375" }',
+            options: '{ "maxGasAmount": "11", "gasUnitPrice": "100"}',
             errors: "{}",
           }),
           expectedStatus: () =>
