b838cb2f8843d789aeff0381709b5e0cb52e8628
Victor Alber <victor.alber@ledger.fr>
2025-01-15 14:04:23 +0100

    test: ✅ adding ENS test + update NFT test

--- libs/ledger-live-common/src/e2e/families/evm.ts
+++ libs/ledger-live-common/src/e2e/families/evm.ts
@@ -7,8 +7,13 @@ export async function sendEVM(tx: Transaction) {
   const events = await pressUntilTextFound(DeviceLabels.ACCEPT);
   const isAmountCorrect = containsSubstringInEvent(tx.amount, events);
   expect(isAmountCorrect).toBeTruthy();
-  const isAddressCorrect = containsSubstringInEvent(tx.accountToCredit.address, events);
-  expect(isAddressCorrect).toBeTruthy();
+  if (tx.accountToCredit.ensName) {
+    const isENSNameCorrect = containsSubstringInEvent(tx.accountToCredit.ensName, events);
+    expect(isENSNameCorrect).toBeTruthy();
+  } else {
+    const isAddressCorrect = containsSubstringInEvent(tx.accountToCredit.address, events);
+    expect(isAddressCorrect).toBeTruthy();
+  }
 
   await pressBoth();
 }
