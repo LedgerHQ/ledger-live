1f5e6d116312ddb8f60a25edd2cb539524339d0f
lAmeR1 <lamer1@github.com>
2025-01-15 17:31:52 +0100

    Add validation checks for recipient and transaction amount
    This update introduces additional error handling for transaction validation. It ensures the recipient field is mandatory and checks for valid Kaspa addresses. Furthermore, it enforces constraints on transaction amounts, including minimum thresholds and required fields.

--- libs/coin-modules/coin-kaspa/src/bridge/getTransactionStatus.ts
+++ libs/coin-modules/coin-kaspa/src/bridge/getTransactionStatus.ts
@@ -1,4 +1,4 @@
-import { InvalidAddress } from "@ledgerhq/errors";
+import { AmountRequired, DustLimit, InvalidAddress, RecipientRequired } from "@ledgerhq/errors";
 import { BigNumber } from "bignumber.js";
 import {
   calcMaxSpendableAmount,
@@ -18,13 +18,25 @@ const getTransactionStatus = async (
 
   let estimateFee: BigNumber = BigNumber(0);
 
+  if (!transaction.recipient) {
+    errors.recipient = new RecipientRequired("");
+  }
+
   if (!!transaction.recipient && !isValidKaspaAddress(transaction.recipient)) {
     errors.recipient = new InvalidAddress("", {
       currencyName: account.currency.name,
     });
   }
 
-  if ((transaction.amount.gt(0) || transaction.useAllAmount) && !!transaction.recipient) {
+  if ((!transaction.amount || !transaction.amount.isGreaterThan(0)) && !transaction.useAllAmount) {
+    errors.amount = new AmountRequired("");
+  }
+
+  if (transaction.amount.gt(0) && transaction.amount.lt(20000000)) {
+    errors.dustLimit = new DustLimit("");
+  }
+
+  if ((transaction.amount.gte(20000000) || transaction.useAllAmount) && !!transaction.recipient) {
     const { compressedPublicKey, chainCode } = parseExtendedPublicKey(
       Buffer.from(account.xpub, "hex"),
     );
