5d691876be24bd0e2ca5a288cd90d0c1c7929148
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-01-16 11:00:16 +0100

    currency id

--- libs/coin-modules/coin-module-boilerplate/src/common-logic/common.ts
+++ libs/coin-modules/coin-module-boilerplate/src/common-logic/common.ts
@@ -2,5 +2,7 @@ import { BigNumber } from "bignumber.js";
 import { parseCurrencyUnit } from "@ledgerhq/coin-framework/currencies";
 import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets/currencies";
 
-const boilerplateUnit = getCryptoCurrencyById("boilerplate").units[0];
+// NOTE: replace ripple by your currency id, it should be found in
+// libs/ledgerjs/packages/cryptoassets/src/currencies.ts
+const boilerplateUnit = getCryptoCurrencyById("ripple").units[0];
 export const parseAPIValue = (value: string): BigNumber => parseCurrencyUnit(boilerplateUnit, value);
--- libs/coin-modules/coin-module-boilerplate/src/test/index.ts
+++ libs/coin-modules/coin-module-boilerplate/src/test/index.ts
@@ -2,5 +2,5 @@ import makeCliTools from "./cli";
 
 export * from "./bridgeDatasetTest";
 export { makeCliTools };
-export * from "./bot-specs";
 export * from "./bot-deviceActions";
+// export * from "./bot-specs";
