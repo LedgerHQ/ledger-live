50fa8b2687f3fb89e6898ae24b11f2827873cbea
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-01-16 10:35:00 +0100

    cleanup types #2

--- libs/coin-modules/coin-module-boilerplate/src/network/types.ts
+++ libs/coin-modules/coin-module-boilerplate/src/network/types.ts
@@ -40,28 +40,6 @@ export type AccountInfoResponse = {
   account_data: {
     Account: string;
     Balance: string;
-    Flags: number;
-    LedgerEntryType: string;
-    OwnerCount: number;
-    PreviousTxnID: string;
-    PreviousTxnLgrSeq: number;
-    Sequence: number;
-    index: string;
-  };
-  account_flags: {
-    allowTrustLineClawback: boolean;
-    defaultRipple: boolean;
-    depositAuth: boolean;
-    disableMasterKey: boolean;
-    disallowIncomingCheck: boolean;
-    disallowIncomingNFTokenOffer: boolean;
-    disallowIncomingPayChan: boolean;
-    disallowIncomingTrustline: boolean;
-    globalFreeze: boolean;
-    noFreeze: boolean;
-    passwordSpent: boolean;
-    requireAuthorization: boolean;
-    requireDestinationTag: boolean;
   };
   ledger_hash: string;
   ledger_index: number;
@@ -73,68 +51,7 @@ export type SubmitReponse = {
   tx_hash: string;
 };
 
-export type ServerInfoResponse = {
-  info: {
-    build_version: string;
-    complete_ledgers: string;
-    hostid: string;
-    initial_sync_duration_us: string;
-    io_latency_ms: number;
-    jq_trans_overflow: string;
-    last_close: {
-      converge_time_s: number;
-      proposers: number;
-    };
-    load_factor: number;
-    network_id: number;
-    peer_disconnects: string;
-    peer_disconnects_resources: string;
-    peers: number;
-    ports: { port: string; protocol: ["ws" | "http" | "peer"] }[];
-    pubkey_node: string;
-    server_state: string;
-    server_state_duration_us: string;
-    state_accounting: Record<
-      "connected" | "disconnected" | "full" | "syncing" | "tracking",
-      {
-        duration_us: string;
-        transitions: string;
-      }
-    >;
-    time: string;
-    uptime: number;
-    validated_ledger: {
-      age: number;
-      base_fee: number;
-      hash: string;
-      seq: number;
-    };
-    validation_quorum: number;
-  };
-} & ResponseStatus;
-
 export type AccountTxResponse = {
   account: string;
   transactions: BoilerplateOperation[];
-} & ResponseStatus;
-
-export type LedgerResponse = {
-  ledger: {
-    account_hash: string;
-    close_flags: number;
-    close_time: number;
-    close_time_human: string;
-    close_time_iso: string;
-    close_time_resolution: number;
-    closed: boolean;
-    ledger_hash: string;
-    ledger_index: string;
-    parent_close_time: number;
-    parent_hash: string;
-    total_coins: string;
-    transaction_hash: string;
-  };
-  ledger_hash: string;
-  ledger_index: number;
-  validated: boolean;
 } & ResponseStatus;
