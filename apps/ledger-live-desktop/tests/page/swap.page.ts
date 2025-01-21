fe97ba8e5de5ee99af5f1da36820bd642b5d5dc1
Sophie TUAL <sophie.tual@ledger.fr>
2025-01-21 09:22:25 +0100

    fix : leftover merge conflict

--- apps/ledger-live-desktop/tests/page/swap.page.ts
+++ apps/ledger-live-desktop/tests/page/swap.page.ts
@@ -143,17 +143,7 @@ export class SwapPage extends AppPage {
   @step("Click Exchange button")
   async clickExchangeButton(electronApp: ElectronApplication, provider: string) {
     const [, webview] = electronApp.windows();
-<<<<<<< HEAD
-    await webview.getByRole('button',{name: `Swap with ${provider}`}).click();
-  }
-
-  @step("Click Max spend toggle")
-  async sendMax(electronApp: ElectronApplication) {
-    const [, webview] = electronApp.windows();
-    await webview.getByTestId("from-account-max-toggle").click();
-=======
     await webview.getByRole("button", { name: `Swap with ${provider}` }).click();
->>>>>>> origin/develop
   }
 
   async confirmExchange() {
