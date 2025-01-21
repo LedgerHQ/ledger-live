f5196c52453b971a3327d09966edb62bb3d6a293
Liviu Ciulinaru <liviuciulinaru@users.noreply.github.com>
2025-01-21 18:37:30 +0700

    fix(live-1174): llm swap fix to load manifest from feature flag (#8960)
    fix: load manifest from feature flag
    
    chore: changeset
    
    fix: disabled by default

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/index.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { DEFAULT_FEATURES } from "@ledgerhq/live-common/featureFlags/defaultFeatures";
+import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
 import {
   useRemoteLiveAppContext,
   useRemoteLiveAppManifest,
@@ -13,16 +15,28 @@ import { initialWebviewState } from "~/components/Web3AppWebview/helpers";
 import { WebviewState } from "~/components/Web3AppWebview/types";
 import { WebView } from "./WebView";
 
-const DEFAULT_SWAP_APP_ID = "swap-live-app-demo-3";
+// set the default manifest ID for the production swap live app
+// in case the FF is failing to load the manifest ID
+// "swap-live-app-demo-3" points to production vercel URL for the swap live app
+const DEFAULT_MANIFEST_ID =
+  process.env.DEFAULT_SWAP_MANIFEST_ID || DEFAULT_FEATURES.ptxSwapLiveApp.params?.manifest_id;
 
 export function SwapLiveApp() {
   const { t } = useTranslation();
+  const ptxSwapLiveAppMobile = useFeature("ptxSwapLiveAppMobile");
 
   const APP_MANIFEST_NOT_FOUND_ERROR = new Error(t("errors.AppManifestUnknown.title"));
   const APP_MANIFEST_UNKNOWN_ERROR = new Error(t("errors.AppManifestNotFoundError.title"));
 
-  const localManifest: LiveAppManifest | undefined = useLocalLiveAppManifest(DEFAULT_SWAP_APP_ID);
-  const remoteManifest: LiveAppManifest | undefined = useRemoteLiveAppManifest(DEFAULT_SWAP_APP_ID);
+  const swapLiveAppManifestID =
+    (ptxSwapLiveAppMobile?.params?.manifest_id as string) || DEFAULT_MANIFEST_ID;
+
+  const localManifest: LiveAppManifest | undefined = useLocalLiveAppManifest(
+    swapLiveAppManifestID || undefined,
+  );
+  const remoteManifest: LiveAppManifest | undefined = useRemoteLiveAppManifest(
+    swapLiveAppManifestID || undefined,
+  );
   const { state: remoteLiveAppState } = useRemoteLiveAppContext();
 
   const [webviewState, setWebviewState] = useState<WebviewState>(initialWebviewState);
