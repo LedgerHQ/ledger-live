f5910169a720bd92e416b140adcfede34cb97162
Victor Alber <victor.alber@ledger.fr>
2025-01-30 17:16:00 +0100

    test: ✅ Adding e2e test for invalid amount - USDT
    wip

--- apps/ledger-live-mobile/e2e/specs/speculos/send/sendInvalid/sendInvalidTokenAmountETH_USDT.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/speculos/send/sendInvalid/sendInvalidTokenAmountETH_USDT.spec.ts
@@ -3,7 +3,7 @@ import { Transaction } from "@ledgerhq/live-common/e2e/models/Transaction";
 import { runSendInvalidTokenAmountTest } from "../send";
 import { Fee } from "@ledgerhq/live-common/e2e/enum/Fee";
 
-const transaction = new Transaction(Account.ETH_USDT_2, Account.ETH_USDT_1, "1", Fee.MEDIUM);
+const transaction = new Transaction(Account.ETH_USDT_2, Account.ETH_USDT_1, "1", Fee.FAST);
 runSendInvalidTokenAmountTest(
   transaction,
   new RegExp(
