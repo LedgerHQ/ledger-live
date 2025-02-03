6759b1e3e6c46576d3da7a6956654e4b5f98c624
Victor Alber <victor.alber@ledger.fr>
2025-02-03 11:11:18 +0100

    test: ✨ Adding possibility to choose fees (e2e detox)

--- apps/ledger-live-mobile/e2e/specs/speculos/send/send.ts
+++ apps/ledger-live-mobile/e2e/specs/speculos/send/send.ts
@@ -48,7 +48,8 @@ export async function runSendTest(transaction: Transaction, tmsLink: string) {
       const amountWithCode = transaction.amount + " " + transaction.accountToCredit.currency.ticker;
       await app.send.expectSummaryAmount(amountWithCode);
       await app.send.expectSummaryRecipient(addressToCredit);
-      await app.send.expectSummaryMemoTag(transaction.memoTag);
+      await app.send.expectSummaryMemoTag(transaction.memoTag); // inverser ici ?
+      await app.send.chooseFeeStrategy(transaction.speed); // Inverser ici ?
       await app.send.summaryContinue();
       await app.send.dismissHighFeeModal();
 
@@ -170,6 +171,7 @@ export async function runSendInvalidTokenAmountTest(
           transaction.amount + " " + transaction.accountToCredit.currency.ticker;
         await app.send.expectSummaryAmount(amountWithCode);
         await app.send.expectSummaryRecipient(transaction.accountToCredit.address);
+        await app.send.chooseFeeStrategy(transaction.speed);
         await app.send.expectSendSummaryError(expectedErrorMessage);
       } else {
         await app.send.expectSendAmountError(expectedErrorMessage);
