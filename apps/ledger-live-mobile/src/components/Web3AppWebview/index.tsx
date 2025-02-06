46f3cb4329ef467d70600b645f7f858fa1c75019
Elliott Coleman <elliott.coleman@yld.io>
2025-02-06 14:02:26 +0000

    feat(LIVE-16732): Swipe right gesture navigates back on swap

--- apps/ledger-live-mobile/src/components/Web3AppWebview/index.tsx
+++ apps/ledger-live-mobile/src/components/Web3AppWebview/index.tsx
@@ -4,6 +4,14 @@ import React, { forwardRef } from "react";
 import { WalletAPIWebview } from "./WalletAPIWebview";
 import { PlatformAPIWebview } from "./PlatformAPIWebview";
 import { WebviewAPI, WebviewProps } from "./types";
+import {
+  HandlerStateChangeEvent,
+  PanGestureHandler,
+  PanGestureHandlerEventPayload,
+  State,
+} from "react-native-gesture-handler";
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
 export const Web3AppWebview = forwardRef<WebviewAPI, WebviewProps>(
   (
@@ -18,18 +26,31 @@ export const Web3AppWebview = forwardRef<WebviewAPI, WebviewProps>(
     },
     ref,
   ) => {
+    const navigation = useNavigation();
+
+    const onGesture = (event: HandlerStateChangeEvent<PanGestureHandlerEventPayload>) => {
+      // PanGestureHandler callback for swiping left to right to fix issue with <Tab.Navigator>
+      if (event.nativeEvent.state === State.END && event.nativeEvent.translationX > 10) {
+        navigation.goBack();
+      }
+    };
+
     if (semver.satisfies(WALLET_API_VERSION, manifest.apiVersion)) {
       return (
-        <WalletAPIWebview
-          ref={ref}
-          onScroll={onScroll}
-          manifest={manifest}
-          currentAccountHistDb={currentAccountHistDb}
-          inputs={inputs}
-          customHandlers={customHandlers}
-          onStateChange={onStateChange}
-          allowsBackForwardNavigationGestures={allowsBackForwardNavigationGestures}
-        />
+        <PanGestureHandler onHandlerStateChange={onGesture} activeOffsetX={[0, 10]}>
+          <View style={{ flex: 1 }}>
+            <WalletAPIWebview
+              ref={ref}
+              onScroll={onScroll}
+              manifest={manifest}
+              currentAccountHistDb={currentAccountHistDb}
+              inputs={inputs}
+              customHandlers={customHandlers}
+              onStateChange={onStateChange}
+              allowsBackForwardNavigationGestures={allowsBackForwardNavigationGestures}
+            />
+          </View>
+        </PanGestureHandler>
       );
     }
     return (
