556a52cc45e46fd8378f0e63338e70a7159e1f59
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-07 15:31:43 +0100

    refac(mina): use @zondax/ledger-mina-js

--- libs/coin-modules/coin-mina/src/signer.ts
+++ libs/coin-modules/coin-mina/src/signer.ts
@@ -1,10 +1,29 @@
-import { MinaUnsignedTransaction } from "./types";
+interface SignTransactionArgs {
+  txType: number;
+  senderAccount: number;
+  senderAddress: string;
+  receiverAddress: string;
+  amount: number;
+  fee: number;
+  nonce: number;
+  validUntil?: number;
+  memo?: string;
+  networkId: number;
+}
+interface BaseLedgerResponse {
+  returnCode: string;
+  statusText?: string;
+  message?: string;
+}
+interface SignTransactionResponse extends BaseLedgerResponse {
+  signature?: string | null;
+}
 
-export type MinaAddress = {
-  publicKey?: string;
-};
+export interface GetAddressResponse extends BaseLedgerResponse {
+  publicKey?: string | null;
+}
 export type MinaSignature = { signature?: string };
 export interface MinaSigner {
-  getAddress(account?: number): Promise<MinaAddress>;
-  signTransaction(transaction: MinaUnsignedTransaction): Promise<MinaSignature>;
+  getAddress(account?: number, verify?: boolean): Promise<GetAddressResponse>;
+  signTransaction(transaction: SignTransactionArgs): Promise<SignTransactionResponse>;
 }
