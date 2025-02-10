e50b750a362e978046cfa2fde321ac3c3212630e
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2025-02-10 15:05:45 +0100

    feat(tron): add alpaca empty methods
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

---
+++ libs/coin-modules/coin-tron/src/logic/index.ts
@@ -0,0 +1,7 @@
+export * from "./broadcast";
+export * from "./combine";
+export * from "./craftTransaction";
+export * from "./estimateFees";
+export * from "./getBalance";
+export * from "./listOperations";
+export * from "./lastBlock";
