8e2f525bf968b1dba308f316e2406c3a7b1ea28e
Martin Cayuelas <martin.cayuelas@ledger.fr>
2025-02-12 10:42:29 +0100

    bugfix: re-Introduce SpamTX

---
+++ apps/ledger-live-mobile/src/screens/Analytics/Operations/useOperations.ts
@@ -0,0 +1,80 @@
+import { filterTokenOperationsZeroAmountEnabledSelector } from "~/reducers/settings";
+import { useNftCollectionsStatus } from "~/hooks/nfts/useNftCollectionsStatus";
+import { groupAccountsOperationsByDay, flattenAccounts } from "@ledgerhq/live-common/account/index";
+import { isAddressPoisoningOperation } from "@ledgerhq/coin-framework/lib/operation";
+import { Operation, AccountLike } from "@ledgerhq/types-live";
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { BlockchainsType } from "@ledgerhq/live-nft/lib/supported";
+import { useHideSpamCollection } from "~/hooks/nfts/useHideSpamCollection";
+import { useSpamTxFiltering } from "@ledgerhq/live-nft-react";
+import keyBy from "lodash/keyBy";
+
+type Props = {
+  opCount: number;
+  accounts: AccountLike[];
+  withSubAccounts: boolean;
+};
+
+export function useOperations({ accounts, opCount, withSubAccounts }: Props) {
+  const spamFilteringTxFeature = useFeature("llmSpamFilteringTx");
+  const nftsFromSimplehashFeature = useFeature("nftsFromSimplehash");
+  const thresold = Number(nftsFromSimplehashFeature?.params?.threshold) || 40;
+
+  //both features must be enabled to enable spam filtering
+  const spamFilteringTxEnabled =
+    (nftsFromSimplehashFeature?.enabled && spamFilteringTxFeature?.enabled) || false;
+
+  const { hideSpamCollection } = useHideSpamCollection();
+
+  const markNftAsSpam = useCallback(
+    (collectionId: string, blockchain: BlockchainsType, spamScore: number) => {
+      if (spamFilteringTxEnabled && spamScore > thresold) {
+        hideSpamCollection(collectionId, blockchain);
+      }
+    },
+    [hideSpamCollection, spamFilteringTxEnabled, thresold],
+  );
+
+  const { hiddenNftCollections } = useNftCollectionsStatus();
+  const shouldFilterTokenOpsZeroAmount = useSelector(
+    filterTokenOperationsZeroAmountEnabledSelector,
+  );
+  const filterOperation = useCallback(
+    (operation: Operation, account: AccountLike) => {
+      // Remove operations linked to address poisoning
+      const removeZeroAmountTokenOp =
+        shouldFilterTokenOpsZeroAmount && isAddressPoisoningOperation(operation, account);
+      // Remove operations coming from an NFT collection considered spam
+      const opFromBlacklistedNftCollection = operation?.nftOperations?.find(op =>
+        hiddenNftCollections.includes(`${account.id}|${op?.contract}`),
+      );
+      return !opFromBlacklistedNftCollection && !removeZeroAmountTokenOp;
+    },
+    [hiddenNftCollections, shouldFilterTokenOpsZeroAmount],
+  );
+
+  const all = flattenAccounts(accounts || []).filter(Boolean);
+
+  const accountsMap = keyBy(all, "id");
+
+  const groupedOperations = groupAccountsOperationsByDay(accounts, {
+    count: opCount,
+    withSubAccounts,
+    filterOperation,
+  });
+
+  const filteredData = useSpamTxFiltering(
+    spamFilteringTxEnabled,
+    accountsMap,
+    groupedOperations,
+    markNftAsSpam,
+    thresold,
+  );
+
+  return {
+    sections: filteredData?.sections,
+    completed: filteredData?.completed,
+  };
+}
