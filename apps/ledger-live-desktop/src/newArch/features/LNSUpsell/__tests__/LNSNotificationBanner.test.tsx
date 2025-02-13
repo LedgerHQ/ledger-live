a319aa099e6d2dd46c853c389c5a49cc3d3f7852
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-02-13 18:54:23 +0100

    feat(lld): show LNS upsell banners to LNS only users

--- apps/ledger-live-desktop/src/newArch/features/LNSUpsell/__tests__/LNSNotificationBanner.test.tsx
+++ apps/ledger-live-desktop/src/newArch/features/LNSUpsell/__tests__/LNSNotificationBanner.test.tsx
@@ -4,6 +4,7 @@
 
 import { t } from "i18next";
 import React from "react";
+import { DeviceModelId } from "@ledgerhq/types-devices";
 import { render, screen, fireEvent } from "tests/testUtils";
 import { openURL } from "~/renderer/linking";
 import { track } from "~/renderer/analytics/segment";
@@ -33,6 +34,16 @@ describe("LNSNotificationBanner", () => {
     expect(screen.queryByText(t("lnsUpsell.banner.notifications.optIn.cta"))).toBeNull();
   });
 
+  it("should not render if the user uses another device", () => {
+    renderBanner({ devicesModelList: [DeviceModelId.nanoSP] });
+    expect(screen.queryByText(t(`lnsUpsell.banner.${location}.optIn.cta`))).toBeNull();
+  });
+
+  it("should not render if the user has no devices", () => {
+    renderBanner({ devicesModelList: [] });
+    expect(screen.queryByText(t(`lnsUpsell.banner.${location}.optIn.cta`))).toBeNull();
+  });
+
   it("should track click on the cta", () => {
     renderBanner({});
     fireEvent.click(screen.getByText(t("lnsUpsell.banner.notifications.optIn.cta")));
@@ -56,7 +67,12 @@ describe("LNSNotificationBanner", () => {
     // NOTE track will be called but this function has it's own logic not to track opt out users
   });
 
-  function renderBanner({ ffEnabled = true, ffLocationEnabled = true, isOptIn = true }) {
+  function renderBanner({
+    ffEnabled = true,
+    ffLocationEnabled = true,
+    isOptIn = true,
+    devicesModelList = [DeviceModelId.nanoS],
+  }) {
     const defaultParams = { notification_center: ffLocationEnabled, "%": 10, img: "" };
     const ffParams = {
       opted_in: { ...defaultParams, link: "https://example.com/optInCta" },
@@ -68,6 +84,7 @@ describe("LNSNotificationBanner", () => {
         settings: {
           shareAnalytics: true,
           sharePersonalizedRecommandations: isOptIn,
+          devicesModelList,
           overriddenFeatureFlags: {
             lldNanoSUpsellBanners: { enabled: ffEnabled, params: ffParams },
           },
--- apps/ledger-live-desktop/src/newArch/features/LNSUpsell/hooks/useLNSUpsellBannerModel.ts
+++ apps/ledger-live-desktop/src/newArch/features/LNSUpsell/hooks/useLNSUpsellBannerModel.ts
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { DeviceModelId } from "@ledgerhq/types-devices";
 import { track } from "~/renderer/analytics/segment";
 import { openURL } from "~/renderer/linking";
-import { sharePersonalizedRecommendationsSelector } from "~/renderer/reducers/settings";
+import {
+  devicesModelListSelector,
+  sharePersonalizedRecommendationsSelector,
+} from "~/renderer/reducers/settings";
 import {
   AnalyticsButton,
   AnalyticsPage,
@@ -15,9 +19,11 @@ export function useLNSUpsellBannerModel(location: LNSBannerLocation): LNSBannerM
   const ff = useFeature("lldNanoSUpsellBanners");
   const params = ff?.params?.[isOptIn ? "opted_in" : "opted_out"];
 
-  // TODO add the LNS only users filtering logic
+  const devicesModelList = useSelector(devicesModelListSelector);
+  const hasOnlySeenLNS =
+    devicesModelList.length === 1 && devicesModelList[0] === DeviceModelId.nanoS;
 
-  if (!ff?.enabled || !params?.[location as keyof typeof params]) {
+  if (!ff?.enabled || !params?.[location as keyof typeof params] || !hasOnlySeenLNS) {
     return null;
   }
 
