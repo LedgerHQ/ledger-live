6238b7096e0de50a7e143831ff4bc8e1cbc1bba0
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2025-02-14 18:05:06 +0100

    feat(tron): add alpaca getbalance and unit test bridge sync
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- libs/coin-framework/src/api/types.ts
+++ libs/coin-framework/src/api/types.ts
@@ -27,6 +27,15 @@ export type Transaction = {
 } & Record<string, unknown>; // Field containing dedicated value for each blockchain
 
 // TODO add a `token: string` field to the pagination if we really need to support pagination (which is not the case for now)
+export type Asset = {
+  native: bigint;
+};
+
+// TODO rename start to minHeight
+//       and add a `token: string` field to the pagination if we really need to support pagination
+//       (which is not the case for now)
+//       for now start is used as a minHeight from which we want to fetch ALL operations
+//       limit is unused for now
 //       see design document at https://ledgerhq.atlassian.net/wiki/spaces/BE/pages/5446205788/coin-modules+lama-adapter+APIs+refinements
 export type Pagination = { minHeight: number };
 export type Api = {
@@ -34,7 +43,7 @@ export type Api = {
   combine: (tx: string, signature: string, pubkey?: string) => string;
   craftTransaction: (address: string, transaction: Transaction, pubkey?: string) => Promise<string>;
   estimateFees: (addr: string, amount: bigint) => Promise<bigint>;
-  getBalance: (address: string) => Promise<bigint>;
+  getBalance: (address: string) => Promise<Asset | bigint>;
   lastBlock: () => Promise<BlockInfo>;
   listOperations: (address: string, pagination: Pagination) => Promise<[Operation[], string]>;
 };
--- libs/coin-modules/coin-tron/src/network/types.ts
+++ libs/coin-modules/coin-tron/src/network/types.ts
@@ -1,5 +1,42 @@
 import { TrongridTxType } from "../types";
 
+export type AccountTronAPI = {
+  account_name?: string;
+  balance?: number;
+  account_resource?: {
+    delegated_frozenV2_balance_for_energy?: number;
+    frozen_balance_for_energy?: {
+      frozen_balance: number;
+      expire_time: number;
+    };
+  };
+  delegated_frozenV2_balance_for_bandwidth?: number;
+  frozen?: {
+    frozen_balance: number;
+    expire_time: number;
+  }[];
+  frozenV2?: {
+    type?: "ENERGY" | "TRON_POWER" | string;
+    amount?: number;
+  }[];
+  latest_withdraw_time?: number;
+  unfrozenV2?: {
+    type?: "ENERGY" | string;
+    unfreeze_amount: number;
+    unfreeze_expire_time: number;
+  }[];
+  votes?: {
+    vote_address: string;
+    vote_count: number;
+  }[];
+  // TRC10
+  assetV2?: {
+    key: string;
+    value: number;
+  }[];
+  trc20: Record<string, string>[];
+};
+
 export type TransactionResponseTronAPI<T> = {
   data: T[];
   success: boolean;
