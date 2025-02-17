ed4b298d987ecee0162e102ea66af4bf0a128e4e
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-17 16:56:29 +0100

    feat(mina): many fixes, bot tests working and added inc sync

--- libs/coin-modules/coin-mina/src/consts.ts
+++ libs/coin-modules/coin-mina/src/consts.ts
@@ -5,8 +5,6 @@ export const MAINNET_NETWORK_IDENTIFIER = {
   },
 };
 
-export const MINA_ACCOUNT_CREATION_FEE = 1 * 10 ** 9;
-
 export const MINA_MAINNET_NETWORK_ID = 1;
 export const MINA_PAYMENT_TYPE_ID = 0;
 export const MINA_TOKEN_ID = "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf";
@@ -16,3 +14,5 @@ export const MINA_CURVE_TYPE = "pallas";
 
 export const MAX_MEMO_LENGTH = 32;
 export const MINA_DECODED_ADDRESS_LENGTH = 72;
+
+export const MAX_TRANSACTIONS_PER_PAGE = 100;
