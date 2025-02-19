15f6f6ac204c8bb1a50f055b9e3751e2e8bcae1b
jdabbech-ledger <jalil.dabbech@ledger.com>
2025-02-19 16:06:34 +0100

    :construction: --wip-- [skip ci]]

--- libs/live-dmk/src/config/errors.ts
+++ libs/live-dmk/src/config/errors.ts
@@ -1,11 +1,11 @@
 import { DeviceBusyError } from "@ledgerhq/device-management-kit";
-import { WebHidSendReportError } from "@ledgerhq/device-transport-kit-web-hid";
+// import { WebHidSendReportError } from "@ledgerhq/device-transport-kit-web-hid";
 
 export const isAllowedOnboardingStatePollingErrorDmk = (error: unknown): boolean => {
   if (error) {
     return (
       error instanceof DeviceBusyError ||
-      error instanceof WebHidSendReportError ||
+      // error instanceof WebHidSendReportError ||
       (typeof error === "object" && "_tag" in error && error._tag === "DeviceSessionNotFound")
     );
   }
---
+++ libs/live-dmk/src/hooks/useDeviceManagementKit.native.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext } from "react";
+import {
+  DeviceManagementKitBuilder,
+  ConsoleLogger,
+  DeviceManagementKit,
+  LogLevel,
+} from "@ledgerhq/device-management-kit";
+import { RNBleTransportFactory } from "@ledgerhq/device-transport-kit-react-native-ble";
+
+console.log("USING DMK BLE");
+
+export const deviceManagementKit = new DeviceManagementKitBuilder()
+  .addTransport(RNBleTransportFactory)
+  .addLogger(new ConsoleLogger(LogLevel.Debug))
+  .build();
+
+export const DeviceManagementKitContext = createContext<DeviceManagementKit>(deviceManagementKit);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const DeviceManagementKitProvider: React.FC<Props> = ({ children }) => (
+  <DeviceManagementKitContext.Provider value={deviceManagementKit}>
+    {children}
+  </DeviceManagementKitContext.Provider>
+);
+
+export const useDeviceManagementKit = (): DeviceManagementKit =>
+  useContext(DeviceManagementKitContext);
