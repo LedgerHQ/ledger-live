d7e75c03fe813e34afca4d65ec5d051ee5ee7ca1
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2025-02-19 23:08:58 +0100

    test: fix tms links

--- apps/ledger-live-mobile/e2e/specs/speculos/delegate/delegateNEAR.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/speculos/delegate/delegateNEAR.spec.ts
@@ -3,4 +3,4 @@ import { runDelegateTest } from "./delegate";
 import { Delegate } from "@ledgerhq/live-common/e2e/models/Delegate";
 
 const delegation = new Delegate(Account.NEAR_1, "0.01", "ledgerbyfigment.poolv1.near");
-runDelegateTest(delegation, "B2CQA-2741");
+runDelegateTest(delegation, ["B2CQA-2741"]);
--- apps/ledger-live-mobile/e2e/specs/speculos/send/sendValidAddress/sendValidAddressXRP.spec.ts
+++ apps/ledger-live-mobile/e2e/specs/speculos/send/sendValidAddress/sendValidAddressXRP.spec.ts
@@ -3,4 +3,4 @@ import { Transaction } from "@ledgerhq/live-common/e2e/models/Transaction";
 import { runSendValidAddressTest } from "../send";
 
 const transaction = new Transaction(Account.XRP_1, Account.XRP_2, "1", undefined, "123456");
-runSendValidAddressTest(transaction, "B2CQA-2718");
+runSendValidAddressTest(transaction, ["B2CQA-2718"]);
