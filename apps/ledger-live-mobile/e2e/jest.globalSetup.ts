9ba71c7c39193719f5ba342c76a87f1eef3fa261
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2025-02-21 09:34:41 +0100

    test: put speculos screenshot in test body

--- apps/ledger-live-mobile/e2e/jest.globalSetup.ts
+++ apps/ledger-live-mobile/e2e/jest.globalSetup.ts
@@ -1,11 +1,13 @@
+/* eslint-disable no-var */
 const detoxGlobalSetup = require("detox/runners/jest/globalSetup");
 
 declare global {
-  // eslint-disable-next-line no-var
   var IS_FAILED: boolean;
+  var speculosDevices: Map<number, string>;
 }
 
 export default async () => {
   await detoxGlobalSetup();
   global.IS_FAILED = false;
+  global.speculosDevices = new Map<number, string>();
 };
