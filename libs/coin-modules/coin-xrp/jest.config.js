60357c663a13a40c6dda0db4a8db1bd5625e82e2
Canestin <canestin.ndong@ledger.fr>
2025-02-25 10:11:02 +0100

    merge files config

--- libs/coin-modules/coin-xrp/jest.config.js
+++ libs/coin-modules/coin-xrp/jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["lib/", "lib-es/", ".*\\.integ\\.test\\.[tj]s"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+    "!src/test/**/*.ts",
+  ],
+  coverageReporters: ["json", ["lcov", { projectRoot: "../" }], "text"],
   workerThreads: true,
 };
--- libs/ledgerjs/packages/hw-app-aptos/jest.config.ts
+++ libs/ledgerjs/packages/hw-app-aptos/jest.config.ts
@@ -3,4 +3,5 @@ import baseConfig from "../../jest.config";
 export default {
   ...baseConfig,
   rootDir: __dirname,
+  coverageReporters: ["json", ["lcov", { projectRoot: "../" }], "json-summary", "text"],
 };
