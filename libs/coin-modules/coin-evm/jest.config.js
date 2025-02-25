e3e9709a2b96c5d26dd119b29096f732bc9356fd
Canestin <canestin.ndong@ledger.fr>
2025-02-25 10:11:02 +0100

    merge files config

--- libs/coin-modules/coin-evm/jest.config.js
+++ libs/coin-modules/coin-evm/jest.config.js
@@ -1,6 +1,13 @@
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
-  collectCoverageFrom: ["src/**/*.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+    "!src/test/**/*.ts",
+    "!src/__tests__/**/*.ts",
+  ],
+  coverageReporters: ["json", ["lcov", { projectRoot: "../" }], "text"],
   coverageDirectory: "coverage",
   preset: "ts-jest",
   testEnvironment: "node",
