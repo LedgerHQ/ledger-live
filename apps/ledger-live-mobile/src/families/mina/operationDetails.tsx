b3416037272909a1fe77586097bc967c67cbfa4d
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-26 21:19:22 +0100

    refac(mina): format fee add and minor changes

--- apps/ledger-live-mobile/src/families/mina/operationDetails.tsx
+++ apps/ledger-live-mobile/src/families/mina/operationDetails.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Section from "~/screens/OperationDetails/Section";
-import { InternetComputerOperation } from "@ledgerhq/live-common/families/internet_computer/types";
+import { MinaOperation } from "@ledgerhq/live-common/families/mina/types";
+import { formatCurrencyUnit } from "@ledgerhq/live-common/currencies/index";
+import { getCryptoCurrencyById } from "@ledgerhq/live-common/currencies/index";
+import BigNumber from "bignumber.js";
 
 type Props = {
-  operation: InternetComputerOperation;
+  operation: MinaOperation;
 };
 
 function OperationDetailsExtra({ operation }: Props) {
@@ -13,6 +16,16 @@ function OperationDetailsExtra({ operation }: Props) {
     <>
       {operation.extra.memo && (
         <Section title={t("operationDetails.extra.memo")} value={operation.extra.memo} />
+      )}
+      {operation.extra.accountCreationFee && (
+        <Section
+          title={t("operationDetails.extra.accountCreationFee")}
+          value={formatCurrencyUnit(
+            getCryptoCurrencyById("mina").units[0],
+            new BigNumber(operation.extra.accountCreationFee),
+            { showCode: true },
+          )}
+        />
       )}
     </>
   );
