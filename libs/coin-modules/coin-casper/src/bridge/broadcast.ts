e1402dd30ee9eced66b3bd05fa7dca4d508dff01
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-26 19:49:25 +0100

    refac(cspr): fix api types

--- libs/coin-modules/coin-casper/src/bridge/broadcast.ts
+++ libs/coin-modules/coin-casper/src/bridge/broadcast.ts
@@ -3,7 +3,6 @@ import { AccountBridge } from "@ledgerhq/types-live";
 import { patchOperationWithHash } from "@ledgerhq/coin-framework/operation";
 import { Transaction } from "../types";
 import { broadcastTx } from "../api";
-import invariant from "invariant";
 
 export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
   signedOperation: { signature, operation },
@@ -12,7 +11,6 @@ export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
 
   const resp = await broadcastTx(tx);
   const { deploy_hash } = resp;
-  invariant(deploy_hash, "casper: failed to broadcast transaction");
 
   const result = patchOperationWithHash(operation, deploy_hash);
 
