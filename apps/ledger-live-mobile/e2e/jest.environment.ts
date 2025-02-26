19e34f97fbf383007826e7c07cf85265c1e0de1b
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2025-02-26 11:40:50 +0100

    test: fix speculos screenshot when hook failed

--- apps/ledger-live-mobile/e2e/jest.environment.ts
+++ apps/ledger-live-mobile/e2e/jest.environment.ts
@@ -1,15 +1,18 @@
 const detoxEnvironment = require("detox/runners/jest/testEnvironment");
 import { takeSpeculosScreenshot } from "./helpers";
 
-class testEnvironment extends detoxEnvironment {
+class TestEnvironment extends detoxEnvironment {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async handleTestEvent(event: any, state: any) {
     await super.handleTestEvent(event, state);
+
     if (["hook_failure", "test_fn_failure"].includes(event.name)) {
       this.global.IS_FAILED = true;
+    }
+    if (this.global.IS_FAILED && ["test_fn_start", "test_fn_failure"].includes(event.name)) {
       await takeSpeculosScreenshot();
     }
   }
 }
 
-module.exports = testEnvironment;
+export default TestEnvironment;
