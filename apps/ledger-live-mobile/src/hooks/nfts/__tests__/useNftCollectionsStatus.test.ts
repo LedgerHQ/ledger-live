12db45f35e848623b2a2812c2da2f8481c8f881b
Mounir Hamzaoui <mounir.hamzaoui@ledger.fr>
2025-02-26 18:33:46 +0100

    fix: retrocompatibility to show spams that are already marked as hidden collection when spamFilteringTx FF was ON and fallback to OFF

--- apps/ledger-live-mobile/src/hooks/nfts/__tests__/useNftCollectionsStatus.test.ts
+++ apps/ledger-live-mobile/src/hooks/nfts/__tests__/useNftCollectionsStatus.test.ts
@@ -138,4 +138,142 @@ describe("useNftCollectionsStatus", () => {
       "collectionP1",
     ]);
   });
+
+  it("should not return spams in the list for a transaction view only when lldSpamFilteringTx is disabled", () => {
+    const { result } = renderHook(() => useNftCollectionsStatus(true), {
+      overrideInitialState: (state: State) => ({
+        ...state,
+        settings: {
+          ...INITIAL_STATE,
+          overriddenFeatureFlags: {
+            nftsFromSimplehash: {
+              enabled: true,
+            },
+            lldSpamFilteringTx: {
+              enabled: false,
+            },
+          },
+          nftCollectionsStatusByNetwork: {
+            [BlockchainEVM.Ethereum]: {
+              collectionETHA: NftStatus.whitelisted,
+              collectionETHB: NftStatus.blacklisted,
+              collectionETHC: NftStatus.spam,
+              collectionETHD: NftStatus.spam,
+            },
+            [BlockchainEVM.Avalanche]: {
+              collectionAVAX1: NftStatus.blacklisted,
+              collectionAVAX2: NftStatus.spam,
+              collectionAVAX3: NftStatus.blacklisted,
+            },
+            [BlockchainEVM.Polygon]: {
+              collectionP1: NftStatus.blacklisted,
+              collectionP2: NftStatus.whitelisted,
+            },
+            [BlockchainEVM.Arbitrum]: {},
+            [BlockchainEVM.Base]: {},
+            [BlockchainEVM.Blast]: {},
+            [BlockchainEVM.Bsc]: {},
+            [BlockchainEVM.Canto]: {},
+            [BlockchainEVM.Celo]: {},
+            [BlockchainEVM.Cyber]: {},
+            [BlockchainEVM.Degen]: {},
+            [BlockchainEVM.Fantom]: {},
+            [BlockchainEVM.Gnosis]: {},
+            [BlockchainEVM.Godwoken]: {},
+            [BlockchainEVM.Linea]: {},
+            [BlockchainEVM.Loot]: {},
+            [BlockchainEVM.Manta]: {},
+            [BlockchainEVM.Mode]: {},
+            [BlockchainEVM.Moonbeam]: {},
+            [BlockchainEVM.Opbnb]: {},
+            [BlockchainEVM.Optimism]: {},
+            [BlockchainEVM.Palm]: {},
+            [BlockchainEVM.ProofOfPlay]: {},
+            [BlockchainEVM.Rari]: {},
+            [BlockchainEVM.Scroll]: {},
+            [BlockchainEVM.Sei]: {},
+            [BlockchainEVM.Xai]: {},
+            [BlockchainEVM.Zora]: {},
+          },
+        },
+      }),
+    });
+
+    expect(result.current.hiddenNftCollections).toEqual([
+      "collectionETHB",
+      "collectionAVAX1",
+      "collectionAVAX3",
+      "collectionP1",
+    ]);
+  });
+  it("should all spams in the list for a transaction view only when lldSpamFilteringTx is enabled", () => {
+    const { result } = renderHook(() => useNftCollectionsStatus(true), {
+      overrideInitialState: (state: State) => ({
+        ...state,
+        settings: {
+          ...INITIAL_STATE,
+          overriddenFeatureFlags: {
+            nftsFromSimplehash: {
+              enabled: true,
+            },
+            lldSpamFilteringTx: {
+              enabled: true,
+            },
+          },
+          nftCollectionsStatusByNetwork: {
+            [BlockchainEVM.Ethereum]: {
+              collectionETHA: NftStatus.whitelisted,
+              collectionETHB: NftStatus.blacklisted,
+              collectionETHC: NftStatus.spam,
+              collectionETHD: NftStatus.spam,
+            },
+            [BlockchainEVM.Avalanche]: {
+              collectionAVAX1: NftStatus.blacklisted,
+              collectionAVAX2: NftStatus.spam,
+              collectionAVAX3: NftStatus.blacklisted,
+            },
+            [BlockchainEVM.Polygon]: {
+              collectionP1: NftStatus.blacklisted,
+              collectionP2: NftStatus.whitelisted,
+            },
+            [BlockchainEVM.Arbitrum]: {},
+            [BlockchainEVM.Base]: {},
+            [BlockchainEVM.Blast]: {},
+            [BlockchainEVM.Bsc]: {},
+            [BlockchainEVM.Canto]: {},
+            [BlockchainEVM.Celo]: {},
+            [BlockchainEVM.Cyber]: {},
+            [BlockchainEVM.Degen]: {},
+            [BlockchainEVM.Fantom]: {},
+            [BlockchainEVM.Gnosis]: {},
+            [BlockchainEVM.Godwoken]: {},
+            [BlockchainEVM.Linea]: {},
+            [BlockchainEVM.Loot]: {},
+            [BlockchainEVM.Manta]: {},
+            [BlockchainEVM.Mode]: {},
+            [BlockchainEVM.Moonbeam]: {},
+            [BlockchainEVM.Opbnb]: {},
+            [BlockchainEVM.Optimism]: {},
+            [BlockchainEVM.Palm]: {},
+            [BlockchainEVM.ProofOfPlay]: {},
+            [BlockchainEVM.Rari]: {},
+            [BlockchainEVM.Scroll]: {},
+            [BlockchainEVM.Sei]: {},
+            [BlockchainEVM.Xai]: {},
+            [BlockchainEVM.Zora]: {},
+          },
+        },
+      }),
+    });
+
+    expect(result.current.hiddenNftCollections).toEqual([
+      "collectionETHB",
+      "collectionETHC",
+      "collectionETHD",
+      "collectionAVAX1",
+      "collectionAVAX2",
+      "collectionAVAX3",
+      "collectionP1",
+    ]);
+  });
 });
