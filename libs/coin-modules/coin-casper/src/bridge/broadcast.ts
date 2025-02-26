f0bb9732ffb3353ac69f959116b16e0d38b6beb5
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-26 19:54:04 +0100

    refac(cspr): extra check on broadcast

--- libs/coin-modules/coin-casper/src/bridge/broadcast.ts
+++ libs/coin-modules/coin-casper/src/bridge/broadcast.ts
@@ -3,6 +3,7 @@ import { AccountBridge } from "@ledgerhq/types-live";
 import { patchOperationWithHash } from "@ledgerhq/coin-framework/operation";
 import { Transaction } from "../types";
 import { broadcastTx } from "../api";
+import invariant from "invariant";
 
 export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
   signedOperation: { signature, operation },
@@ -10,6 +11,7 @@ export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
   const tx = DeployUtil.deployFromJson(JSON.parse(signature)).unwrap();
 
   const resp = await broadcastTx(tx);
+  invariant(resp.deploy_hash, "casper: failed to broadcast transaction and get deploy hash");
   const { deploy_hash } = resp;
 
   const result = patchOperationWithHash(operation, deploy_hash);
