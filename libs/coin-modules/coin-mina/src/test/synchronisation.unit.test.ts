f5963fc96583fc146c6116c3c6dd03fa5d9abc24
Prateek Rathod <emaillawrathod@gmail.com>
2025-02-26 20:57:42 +0100

    refac(mina): change operation value calc

--- libs/coin-modules/coin-mina/src/test/synchronisation.unit.test.ts
+++ libs/coin-modules/coin-mina/src/test/synchronisation.unit.test.ts
@@ -1,3 +1,4 @@
+import invariant from "invariant";
 import { getTransactions } from "../api";
 import { mapRosettaTxnToOperation } from "../bridge/synchronisation";
 import { setConfig } from "./testUtils";
@@ -14,3 +15,16 @@ describe("synchronisation", () => {
     expect(ops.length).toBeGreaterThan(0);
   }, 1000000);
 });
+
+test("get transaction", async () => {
+  const address = "B62qqP3a8sYEFKnDU3tdhUfmSqAP2MRqpn72jUKVEvSRVPM4bkcAV5c";
+  const txs = await getTransactions(address);
+  const txHash = "5JuL2ErRKXVNqN48KsYJTjsCmhbj5ufgmjDNSdH5rDU8vvC55EBD";
+
+  const tx = txs.find(tx => tx.transaction.transaction_identifier.hash === txHash);
+  invariant(tx, "tx not found");
+
+  const op = await mapRosettaTxnToOperation("accountId", address, tx);
+
+  expect(op).toBeDefined();
+}, 1000000);
