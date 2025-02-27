759845e4ab177d724340a8c0e957dbf662b9d41d
pavanvora <pavanvora.cs@gmail.com>
2025-02-27 10:30:30 +0530

    add test for certificates, withdrawals and delegationInfo

---
+++ libs/coin-modules/coin-cardano/src/buildTransaction.test.ts
@@ -0,0 +1,142 @@
+import BigNumber from "bignumber.js";
+import { getCardanoTestnetAccount1 } from "./fixtures/accounts";
+import { CardanoAccount, Transaction } from "./types";
+import { buildTransaction } from "./buildTransaction";
+import { types as TyphonTypes } from "@stricahq/typhonjs";
+import { getProtocolParams } from "./fixtures/protocolParams";
+
+describe("buildTransaction", () => {
+  let account: CardanoAccount;
+  let txPayload: Transaction;
+
+  beforeEach(() => {
+    account = getCardanoTestnetAccount1();
+    txPayload = {
+      family: "cardano",
+      recipient:
+        "addr_test1qz7jw975stagnvs00wsjny6y6gpazn86yvwcm2vy02j3up7mt68vuzvz4nzgs00x0shrgywvy674v6r2zcs8fxvvq27qfjq8np",
+      amount: new BigNumber(2e6),
+      mode: "send",
+      poolId: undefined,
+      protocolParams: getProtocolParams(),
+    };
+  });
+
+  describe("certificates", () => {
+    it("should not add abstain when there is no delegation", async () => {
+      account.cardanoResources.delegation = undefined; // no delegation
+      const transaction = await buildTransaction(account, txPayload);
+      const certificates = transaction.getCertificates();
+      expect(
+        certificates.some(
+          c =>
+            c.type === TyphonTypes.CertificateType.VOTE_DELEGATION &&
+            c.cert.dRep.type === TyphonTypes.DRepType.ABSTAIN,
+        ),
+      ).toBe(false);
+    });
+
+    it("should not add abstain when there is no rewards", async () => {
+      account.cardanoResources.delegation = {
+        dRepHex: undefined,
+        rewards: new BigNumber(0), // no rewards
+      } as any;
+      const transaction = await buildTransaction(account, txPayload);
+      const certificates = transaction.getCertificates();
+      expect(
+        certificates.some(
+          c =>
+            c.type === TyphonTypes.CertificateType.VOTE_DELEGATION &&
+            c.cert.dRep.type === TyphonTypes.DRepType.ABSTAIN,
+        ),
+      ).toBe(false);
+    });
+
+    it("should not add abstain vote when dRepHex is present", async () => {
+      account.cardanoResources.delegation = {
+        dRepHex: "drepHex", // drepHex present
+        rewards: new BigNumber(10e6),
+      } as any;
+      const transaction = await buildTransaction(account, txPayload);
+      const certificates = transaction.getCertificates();
+      expect(
+        certificates.some(
+          c =>
+            c.type === TyphonTypes.CertificateType.VOTE_DELEGATION &&
+            c.cert.dRep.type === TyphonTypes.DRepType.ABSTAIN,
+        ),
+      ).toBe(false);
+    });
+
+    it("should add abstain when drepHex is absent and rewards is available", async () => {
+      account.cardanoResources.delegation = {
+        status: true,
+        poolId: "poolId",
+        ticker: undefined,
+        name: undefined,
+        dRepHex: undefined, // drepHex absent
+        rewards: new BigNumber(10e6), // rewards available
+      };
+      const transaction = await buildTransaction(account, txPayload);
+      const certificates = transaction.getCertificates();
+      expect(
+        certificates.some(
+          c =>
+            c.type === TyphonTypes.CertificateType.VOTE_DELEGATION &&
+            c.cert.dRep.type === TyphonTypes.DRepType.ABSTAIN,
+        ),
+      ).toBe(true);
+    });
+  });
+
+  describe("withdrawals", () => {
+    it("should not add withdrawal when there is no delegation", async () => {
+      account.cardanoResources.delegation = undefined; // no delegation
+      const transaction = await buildTransaction(account, txPayload);
+      const withdrawals = transaction.getWithdrawals();
+      expect(withdrawals.length).toBe(0);
+    });
+
+    it("should not add withdrawal if no rewards", async () => {
+      account.cardanoResources.delegation = {
+        status: true,
+        poolId: "poolId",
+        ticker: undefined,
+        name: undefined,
+        dRepHex: undefined,
+        rewards: new BigNumber(0), // no rewards
+      };
+      const transaction = await buildTransaction(account, txPayload);
+      const withdrawals = transaction.getWithdrawals();
+      expect(withdrawals.length).toBe(0);
+    });
+
+    it("should not add withdrawal when dRepHex is absent", async () => {
+      account.cardanoResources.delegation = {
+        status: true,
+        poolId: "poolId",
+        ticker: undefined,
+        name: undefined,
+        dRepHex: undefined, // drepHex absent
+        rewards: new BigNumber(10e6),
+      };
+      const transaction = await buildTransaction(account, txPayload);
+      const withdrawals = transaction.getWithdrawals();
+      expect(withdrawals.length).toBe(0);
+    });
+
+    it("should add withdrawal when dRepHex and rewards both are available", async () => {
+      account.cardanoResources.delegation = {
+        status: true,
+        poolId: "poolId",
+        ticker: undefined,
+        name: undefined,
+        dRepHex: "drepHex", // drepHex present
+        rewards: new BigNumber(10e6), // rewards available
+      };
+      const transaction = await buildTransaction(account, txPayload);
+      const withdrawals = transaction.getWithdrawals();
+      expect(withdrawals.length).toBe(1);
+    });
+  });
+});
