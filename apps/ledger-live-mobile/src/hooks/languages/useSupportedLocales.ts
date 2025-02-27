e58351153faa69abea29522c308beb58a6fac6ac
cgrellard-ledger <come.grellard@ledger.fr>
2025-02-27 10:25:56 +0100

    :sparkles: feat(llm): Make useSupportedLocales more generic

--- apps/ledger-live-mobile/src/hooks/languages/useSupportedLocales.ts
+++ apps/ledger-live-mobile/src/hooks/languages/useSupportedLocales.ts
@@ -1,20 +1,28 @@
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import { useMemo } from "react";
-import { supportedLocales } from "~/languages";
+import { LocaleKeys, supportedLocales } from "~/languages";
 
 export const useSupportedLocales = () => {
   const thaiFeature = useFeature("llmThai");
 
+  // When adding a new language, add the feature flag here
+  const languagesFeatureFlags: Partial<Record<LocaleKeys, boolean>> = useMemo(
+    () => ({
+      th: Boolean(thaiFeature?.enabled),
+    }),
+    [thaiFeature?.enabled],
+  );
+
   const supportedLocalesFiltered = useMemo(
     () =>
       supportedLocales.filter(locale => {
-        if (locale === "th" && !thaiFeature?.enabled) {
+        if (locale in languagesFeatureFlags && !languagesFeatureFlags[locale]) {
           return false;
         }
 
         return true;
       }),
-    [thaiFeature?.enabled],
+    [languagesFeatureFlags],
   );
 
   return supportedLocalesFiltered;
