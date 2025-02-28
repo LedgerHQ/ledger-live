4506040e9e9a384209c7e891ed318b087d89cb34
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-02-28 13:49:03 +0100

    feat(lld): improve the notification icon wrapper design

--- libs/ui/packages/react/src/components/layout/Banner/NotificationCard/index.tsx
+++ libs/ui/packages/react/src/components/layout/Banner/NotificationCard/index.tsx
@@ -19,13 +19,29 @@ export type NotificationCardProps = GridBoxProps & {
   onClick: ReactEventHandler;
 };
 
-const colorsByIcon: Partial<
-  Record<IconKey, Partial<{ icon: string; iconBg: string; cta: string }>>
-> = {
+type WrapperType = "round" | "square";
+type IconPallet = { fg: string; bg: string };
+const colorsByIcon: Partial<Record<IconKey, Partial<Record<WrapperType, Partial<IconPallet>>>>> = {
   Warning: {
-    icon: "warning.c80",
-    iconBg: "warning.c30",
-    cta: "warning.c80",
+    round: {
+      fg: "warning.c80",
+      bg: "warning.c30",
+    },
+    square: {
+      fg: "warning.c80",
+      bg: "warning.c30",
+    },
+  },
+};
+
+const defaultDefaultColors: Record<WrapperType, IconPallet> = {
+  round: {
+    fg: "primary.c90",
+    bg: "primary.c30",
+  },
+  square: {
+    fg: "primary.c80",
+    bg: "primary.c10",
   },
 };
 
@@ -45,14 +61,16 @@ export default function NotificationCard({
 
   const safeIcon: IconKey = icon in Icons ? icon : "Information";
   const Icon = Icons[safeIcon];
-  const colors = colorsByIcon[safeIcon];
-  const ctaColor = isHighlighted ? colors?.cta ?? "primary.c80" : "neutral.c100";
-  const iconColor = colors?.icon ?? "primary.c80";
-  const iconBgColor = colors?.iconBg ?? "opacityPurple.c10";
+  const iconType = isHighlighted ? "square" : "round";
+  const colors = colorsByIcon[safeIcon]?.[iconType];
+  const iconColor = colors?.fg ?? defaultDefaultColors[iconType].fg;
+  const iconBgColor = colors?.bg ?? defaultDefaultColors[iconType].bg;
+
+  const ctaColor = isHighlighted ? "primary.c80" : "neutral.c100";
 
   return (
     <Wrapper {...boxProps} isHighlighted={isHighlighted} onClick={handleCTAClick}>
-      <IconWrapper backgroundColor={iconBgColor} isHighlighted={isHighlighted}>
+      <IconWrapper backgroundColor={iconBgColor} type={iconType}>
         <Icon color={iconColor} />
       </IconWrapper>
       <Flex flexDirection="column" rowGap={isHighlighted ? "4px" : "8px"}>
@@ -85,30 +103,43 @@ const Cta = styled(Link).attrs({ alignSelf: "start" })`
   font-size: 13px;
 `;
 
-const IconWrapper = styled(Flex)<Pick<NotificationCardProps, "isHighlighted">>`
-  border-radius: ${p => (p.isHighlighted ? "13.5px" : "50%")};
+const IconWrapper = styled(Flex)<{ type?: WrapperType }>`
   padding: 8px;
-  ${p =>
-    p.isHighlighted &&
-    css`
-      background-clip: padding-box;
-      border-width: 7px;
-      border-style: solid;
-      border-color: transparent;
-      position: relative;
-      &::before {
-        content: "";
-        position: absolute;
-        inset: -7px;
-        border-radius: 13.5px;
-        background: linear-gradient(
-          180deg,
-          ${p.theme.colors.opacityDefault.c05} 0%,
-          rgba(29, 28, 31, 0) 100%
-        );
-      }
-    `}
-          
+  ${p => (p.type === "square" ? squareWrapper : roundWrapper)}
+`;
+
+const roundWrapper = css`
+  border-radius: 50%;
+`;
+
+const squareWrapper = css`
+  border-radius: 13.5px;
+  background-clip: padding-box;
+  border-width: 7px;
+  border-style: solid;
+  border-color: transparent;
+  position: relative;
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    inset: -7px;
+    border-radius: 13.5px;
+  }
+  &::before {
+    background: ${p => css`linear-gradient(
+        ${p.theme.colors.opacityDefault.c10} 0%,
+        rgba(29, 28, 31, 0) 100%
+      );
+  `};
+  }
+  &::after {
+    border: solid 0.5px #fff;
+    mask-image: linear-gradient(
+      rgba(255, 255, 255, 0.15) 0%,
+      rgba(255, 255, 255, 0.01) 60%,
+      rgba(255, 255, 255, 0) 100%
+    );
   }
 `;
 
