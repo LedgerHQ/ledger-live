f44c44e7373c328dfff2a9d9e633d20dea1358f2
cgrellard-ledger <come.grellard@ledger.fr>
2025-03-04 15:08:56 +0100

    :bug: bugfix(lld): Recover Close Button Hitbox Fixed

--- apps/ledger-live-desktop/src/renderer/components/WebRecoverPlayer/index.tsx
+++ apps/ledger-live-desktop/src/renderer/components/WebRecoverPlayer/index.tsx
@@ -16,7 +16,7 @@ export const Container = styled.div`
   flex-direction: column;
   flex: 1;
   height: 100%;
-  margin: 20px;
+  margin-top: 20px;
 `;
 
 export const Wrapper = styled(Box).attrs(() => ({
