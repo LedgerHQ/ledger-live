a2ea8811cb1da0d1f419b3e5a3fb4b9a48110ba7
jdabbech-ledger <jalil.dabbech@ledger.com>
2025-03-05 16:25:00 +0100

    :construction: --wip-- [skip ci]]

--- libs/live-dmk-desktop/src/index.ts
+++ libs/live-dmk-desktop/src/index.ts
@@ -1,4 +1,7 @@
-export { useDeviceSessionRefresherToggle, useDeviceSessionState } from "@ledgerhq/live-dmk-shared";
+export {
+  useDisableDeviceSessionRefresher as useDeviceSessionRefresherToggle,
+  useDeviceSessionState,
+} from "@ledgerhq/live-dmk-shared";
 export * from "./hooks/useDeviceManagementKit";
 export { DeviceManagementKitTransport } from "./transport/DeviceManagementKitTransport";
 export { isAllowedOnboardingStatePollingErrorDmk } from "./errors";
