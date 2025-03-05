e98777c4e5f08a6615196378ff09a857995fb1b0
Canestin <canestin.ndong@ledger.fr>
2025-03-05 09:12:19 +0100

    add executionTests-report.xml files in hw apps

--- libs/ledgerjs/packages/hw-app-aptos/jest.config.ts
+++ libs/ledgerjs/packages/hw-app-aptos/jest.config.ts
@@ -3,5 +3,21 @@ import baseConfig from "../../jest.config";
 export default {
   ...baseConfig,
   rootDir: __dirname,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/*.spec.{ts,tsx}",
+    "!src/**/__tests__/**",
+    "!tests/**",
+  ],
   coverageReporters: ["json", ["lcov", { projectRoot: "../" }], "json-summary", "text"],
+  reporters: [
+    [
+      "jest-sonar",
+      {
+        outputName: "hw-app-aptos-sonar-executionTests-report.xml",
+        reportedFilePath: "absolute",
+      },
+    ],
+  ],
 };
