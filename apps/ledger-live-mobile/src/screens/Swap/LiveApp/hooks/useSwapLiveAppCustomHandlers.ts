dc115be2816ac86939b7052d06234094fa06b94c
Liviu Ciulinaru <liviu.ciulinaru@gmail.com>
2025-03-06 15:30:02 +0700

    fix: search token account in subaccounts
    fix: use flatten accounts
    
    chore: changeset
    
    chore: changeset

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/hooks/useSwapLiveAppCustomHandlers.ts
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/hooks/useSwapLiveAppCustomHandlers.ts
@@ -1,14 +1,14 @@
 import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
 import { WalletAPICustomHandlers } from "@ledgerhq/live-common/wallet-api/types";
+import { useNavigation } from "@react-navigation/native";
 import { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { usePTXCustomHandlers } from "~/components/WebPTXPlayer/CustomHandlers";
-import { accountsSelector } from "~/reducers/accounts";
+import { flattenAccountsSelector } from "~/reducers/accounts";
 import { swapCustomHandlers } from "../customHandlers";
-import { useNavigation } from "@react-navigation/native";
 
 export function useSwapLiveAppCustomHandlers(manifest: LiveAppManifest) {
-  const accounts = useSelector(accountsSelector);
+  const accounts = useSelector(flattenAccountsSelector);
   const ptxCustomHandlers = usePTXCustomHandlers(manifest, accounts);
   const dispatch = useDispatch();
   const navigation = useNavigation();
