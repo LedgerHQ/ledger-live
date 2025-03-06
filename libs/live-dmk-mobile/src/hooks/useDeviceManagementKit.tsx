e18d49c62a7aac16d8ff041ea8e8aee008fb3b2f
Valentin D. Pinkman <valentin.dealmeida@ledger.fr>
2025-03-06 14:58:04 +0100

    chore(logger): fix dmk logger

--- libs/live-dmk-mobile/src/hooks/useDeviceManagementKit.tsx
+++ libs/live-dmk-mobile/src/hooks/useDeviceManagementKit.tsx
@@ -1,15 +1,15 @@
 import React, { createContext, useContext } from "react";
 import {
   DeviceManagementKitBuilder,
-  ConsoleLogger,
   DeviceManagementKit,
   LogLevel,
 } from "@ledgerhq/device-management-kit";
 import { RNBleTransportFactory } from "@ledgerhq/device-transport-kit-react-native-ble";
+import { LedgerLiveLogger } from "@ledgerhq/live-dmk-shared";
 
 export const deviceManagementKit = new DeviceManagementKitBuilder()
   .addTransport(RNBleTransportFactory)
-  .addLogger(new ConsoleLogger(LogLevel.Debug))
+  .addLogger(new LedgerLiveLogger(LogLevel.Debug))
   .build();
 
 export const DeviceManagementKitContext = createContext<DeviceManagementKit>(deviceManagementKit);
--- libs/live-dmk-shared/src/index.ts
+++ libs/live-dmk-shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./hooks";
 export { activeDeviceSessionSubject } from "./config/activeDeviceSession";
 export { dmkToLedgerDeviceIdMap } from "./config/dmkToLedgerDeviceIdMap";
+export { LedgerLiveLogger } from "./services/LedgerLiveLogger";
