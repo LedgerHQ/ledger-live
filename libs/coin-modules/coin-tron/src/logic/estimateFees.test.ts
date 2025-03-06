680012a17857923f140391e495e6a5d6df9c3aa7
qperrot <quentin.perrot-minnot@ledger.fr>
2025-03-06 16:07:45 +0100

    chore: add test for natif TRX

--- libs/coin-modules/coin-tron/src/logic/estimateFees.test.ts
+++ libs/coin-modules/coin-tron/src/logic/estimateFees.test.ts
@@ -3,6 +3,17 @@ import { ACTIVATION_FEES, ACTIVATION_FEES_TRC_20 } from "./constants";
 import { TransactionIntent } from "@ledgerhq/coin-framework/api/index";
 
 describe("estimateFees", () => {
+  it("should calculate fees for natif transactionIntent", async () => {
+    const transactionIntent: TransactionIntent = {
+      sender: "sender1",
+      recipient: "recipient1",
+      amount: BigInt(1000),
+    };
+
+    const result = await estimateFees(transactionIntent);
+
+    expect(result).toEqual(BigInt(ACTIVATION_FEES.toString()));
+  });
   it("should calculate fees for trc10 transactionIntent", async () => {
     const transactionIntent: TransactionIntent = {
       sender: "sender1",
