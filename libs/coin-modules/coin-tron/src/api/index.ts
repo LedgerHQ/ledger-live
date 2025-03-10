cc2d2a95d7fd97caad173e023e5d3d43c41f7bec
qperrot <quentin.perrot-minnot@ledger.fr>
2025-03-10 17:16:51 +0100

    fix: separate in different function in network

--- libs/coin-modules/coin-tron/src/api/index.ts
+++ libs/coin-modules/coin-tron/src/api/index.ts
@@ -1,4 +1,5 @@
 import {
+  TransactionIntent,
   // IncorrectTypeError,
   // Pagination,
   type Api,
@@ -21,7 +22,7 @@ export function createApi(config: TronConfig): Api {
   return {
     broadcast,
     combine,
-    craftTransaction,
+    craftTransaction: craft,
     estimateFees,
     getBalance,
     lastBlock,
--- libs/coin-modules/coin-tron/src/logic/craftTransaction.ts
+++ libs/coin-modules/coin-tron/src/logic/craftTransaction.ts
@@ -1,46 +1,30 @@
 import { TransactionIntent } from "@ledgerhq/coin-framework/api/index";
 import { decode58Check } from "../network/format";
-import { SendTransactionData, SmartContractTransactionData } from "../types";
-import { abiEncodeTrc20Transfer } from "../network/utils";
 import BigNumber from "bignumber.js";
-import { extendTronTxExpirationTimeBy10mn, post } from "../network";
+import { craftStandardTransaction, craftTrc20Transaction } from "../network";
 
 export async function craftTransaction(transactionIntent: TransactionIntent): Promise<string> {
-  // trc20
-  if (transactionIntent.standard === "trc20" && transactionIntent.tokenAddress) {
-    const txData: SmartContractTransactionData = {
-      function_selector: "transfer(address,uint256)",
-      fee_limit: 50000000,
-      call_value: 0,
-      contract_address: decode58Check(transactionIntent.tokenAddress),
-      parameter: abiEncodeTrc20Transfer(
-        decode58Check(transactionIntent.recipient),
-        new BigNumber(transactionIntent.amount.toString()),
-      ),
-      owner_address: decode58Check(transactionIntent.sender),
-    };
-    const url = `/wallet/triggersmartcontract`;
-    const { transaction: preparedTransaction } = await post(url, txData);
-    const { raw_data_hex: rawDataHex } =
-      await extendTronTxExpirationTimeBy10mn(preparedTransaction);
+  const { standard, tokenAddress, recipient, sender, amount } = transactionIntent;
+  const recipientAddress = decode58Check(recipient);
+  const senderAddress = decode58Check(sender);
+
+  if (standard === "trc20" && tokenAddress) {
+    const { raw_data_hex: rawDataHex } = await craftTrc20Transaction(
+      tokenAddress,
+      recipientAddress,
+      senderAddress,
+      new BigNumber(amount.toString()),
+    );
     return rawDataHex as string;
   } else {
-    // trx/trc10
-    const txData: SendTransactionData = {
-      to_address: decode58Check(transactionIntent.recipient),
-      owner_address: decode58Check(transactionIntent.sender),
-      amount: Number(transactionIntent.amount),
-      asset_name:
-        transactionIntent.tokenAddress &&
-        Buffer.from(transactionIntent.tokenAddress).toString("hex"),
-    };
-    const isTransferAsset =
-      transactionIntent.tokenAddress && transactionIntent.tokenAddress.length > 0 ? true : false;
-    const url = isTransferAsset ? `/wallet/transferasset` : `/wallet/createtransaction`;
-    const preparedTransaction = await post(url, txData);
-    // for the ledger Vault we need to increase the expiration
-    const { raw_data_hex: rawDataHex } =
-      await extendTronTxExpirationTimeBy10mn(preparedTransaction);
+    const isTransferAsset = Boolean(tokenAddress && tokenAddress.length > 0);
+    const { raw_data_hex: rawDataHex } = await craftStandardTransaction(
+      tokenAddress,
+      recipientAddress,
+      senderAddress,
+      new BigNumber(amount.toString()),
+      isTransferAsset,
+    );
     return rawDataHex as string;
   }
 }
