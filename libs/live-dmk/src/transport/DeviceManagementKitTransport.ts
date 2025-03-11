1307b0e264eb27e02c28d3ce6b8f810a3da48dc7
Louis Aussedat <louis.aussedat@ledger.fr>
2025-03-11 17:28:39 +0100

    🩹 (dmk): fix compatibility issue with new dmk version

--- libs/live-dmk/src/transport/DeviceManagementKitTransport.ts
+++ libs/live-dmk/src/transport/DeviceManagementKitTransport.ts
@@ -69,12 +69,14 @@ export class DeviceManagementKitTransport extends Transport {
     }
 
     tracer.trace("[open] No active session found, starting discovery");
-    const [discoveredDevice] = await firstValueFrom(deviceManagementKit.listenToKnownDevices());
+    const [discoveredDevice] = await firstValueFrom(
+      deviceManagementKit.listenToAvailableDevices({}),
+    );
     const connectedSessionId = await deviceManagementKit.connect({ device: discoveredDevice });
     // disable session refresher for the connected device as it is not needed and can cause issues
-    deviceManagementKit.toggleDeviceSessionRefresher({
+    deviceManagementKit.disableDeviceSessionRefresher({
       sessionId: connectedSessionId,
-      enabled: false,
+      blockerId: "live-dmk",
     });
 
     tracer.trace("[open] Connected");
@@ -86,7 +88,7 @@ export class DeviceManagementKitTransport extends Transport {
 
   static listen = (observer: Observer<DescriptorEvent<string>>) => {
     const subscription = deviceManagementKit
-      .listenToKnownDevices()
+      .listenToAvailableDevices({})
       .pipe(
         startWith<DiscoveredDevice[]>([]),
         pairwise(),
@@ -146,7 +148,9 @@ export class DeviceManagementKitTransport extends Transport {
 
     // If the device is not connected, connect to new session
     if (!devices.some(device => device.sessionId === this.sessionId)) {
-      const [discoveredDevice] = await firstValueFrom(deviceManagementKit.listenToKnownDevices());
+      const [discoveredDevice] = await firstValueFrom(
+        deviceManagementKit.listenToAvailableDevices({}),
+      );
       const connectedSessionId = await deviceManagementKit.connect({ device: discoveredDevice });
       this.sessionId = connectedSessionId;
     }
