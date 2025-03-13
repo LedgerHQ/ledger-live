100cd5a7aca04ea1f70d58507ca6cd40923fdb8d
Sergey Ponomarev <me@sergey-ponomarev.ru>
2025-03-13 14:58:10 +0300

    chore: leftovers cleanup

--- libs/coin-modules/coin-sui/src/types/model.ts
+++ libs/coin-modules/coin-sui/src/types/model.ts
@@ -1,19 +1,5 @@
 export type SuiOperationMode = "send";
 
-export type TransactionPayloadInfo = {
-  address: string;
-  blockHash: string;
-  era: `0x${string}`;
-  genesisHash: string;
-  method: `0x${string}`;
-  nonce: number;
-  specVersion: `0x${string}`;
-  transactionVersion: `0x${string}`;
-  version: number;
-  mode: number;
-  metadataHash: Uint8Array;
-};
-
 export type CoreTransaction = {
   unsigned: Uint8Array;
 };
