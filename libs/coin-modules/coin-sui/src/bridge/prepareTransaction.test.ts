0be26339660db098d13245068ea51da65cfe102f
Ilya Shabanov <ishaba@me.com>
2025-03-13 16:54:59 +0300

    chore: add prepare transaction tests

---
+++ libs/coin-modules/coin-sui/src/bridge/prepareTransaction.test.ts
@@ -0,0 +1,48 @@
+import BigNumber from "bignumber.js";
+import { createFixtureAccount, createFixtureTransaction } from "../types/bridge.fixture";
+import prepareTransaction from "./prepareTransaction";
+import { faker } from "@faker-js/faker";
+
+const mockEstimateFees = jest.fn();
+jest.mock("../logic", () => ({
+  estimateFees: () => mockEstimateFees(),
+}));
+
+describe("prepareTransaction", () => {
+  afterEach(() => {
+    mockEstimateFees.mockClear();
+  });
+
+  it("returns a new Transaction with new fees", async () => {
+    // Given
+    const fees = new BigNumber(faker.number.int(50));
+    mockEstimateFees.mockResolvedValue(fees);
+    const tx = createFixtureTransaction();
+
+    // When
+    const newTx = await prepareTransaction(createFixtureAccount(), tx);
+
+    // Then
+    expect(mockEstimateFees).toHaveBeenCalledTimes(1);
+    expect(newTx.fees).toEqual(fees);
+    expect(newTx).not.toBe(tx);
+    expect(newTx).toMatchObject({
+      amount: tx.amount,
+      recipient: tx.recipient,
+      mode: tx.mode,
+    });
+  });
+
+  it("returns the passed transaction if fees are the same", async () => {
+    // Given
+    const fees = new BigNumber(faker.number.int(50));
+    mockEstimateFees.mockResolvedValue(fees);
+    const tx = createFixtureTransaction({ fees });
+
+    // When
+    const newTx = await prepareTransaction(createFixtureAccount(), tx);
+
+    // Then
+    expect(newTx).toBe(tx);
+  });
+});
