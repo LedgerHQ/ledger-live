01247d60ed20f3f8d77bf875b6cc0998d574b720
Artem Brilev <razdvajonez@mail.ru>
2025-03-13 12:07:58 +0300

    fix test runner

---
+++ libs/coin-modules/coin-sui/src/bridge/bridge.test.ts
@@ -0,0 +1,9 @@
+describe("signOperation", () => {
+  it("throws an error of transaction has no fees", () => {
+    // GIVEN
+    const account = 1;
+
+    // WHEN & THEN
+    expect(account).toBe(1);
+  });
+});
