9add520c99f4177e2cedb53915c266b41b56c080
Mitchell Vivanti <mitchell.vivanti-ext@ledger.fr>
2025-03-17 11:40:42 +0000

    feat: add Warning rounded fill icon as status tag on LLD sidebar

--- apps/ledger-live-desktop/src/renderer/components/MainSideBar/RecoverStatusDot.tsx
+++ apps/ledger-live-desktop/src/renderer/components/MainSideBar/RecoverStatusDot.tsx
@@ -1,15 +1,65 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Dot } from "~/renderer/components/Dot";
-import { colors } from "~/renderer/styles/theme";
 import { getStoreValue } from "~/renderer/store";
 import { LedgerRecoverSubscriptionStateInProgressEnum } from "~/types/recoverSubscriptionState";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { Icons } from "@ledgerhq/react-ui";
+import useTheme from "~/renderer/hooks/useTheme";
+import styled, { keyframes, css } from "styled-components";
 
 interface RecoverStatusNotificationProps {
   collapsed: boolean;
 }
+const collapseAnim = keyframes`
+  0% {
+    opacity: 0;
+    top: -5px;
+    right: -5px;
+    transform: translateY(0);
+  }
+  100% {
+    opacity: 1;
+    top: -5px;
+    right: -5px;
+    transform: translateY(0);
+  }
+`;
+const openAnim = keyframes`
+  0% {
+    opacity: 0;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+  }
+  100% {
+    opacity: 1;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+  }
+`;
+const StyledIcon = styled(Icons.WarningRoundedFill)<{
+  opacity?: number;
+  collapsed?: boolean | null;
+}>`
+  opacity: ${p => p.opacity};
+  position: absolute;
+  top: -5px;
+  right: -5px;
+  transform: translateY(0);
+  opacity: 0;
+  animation: ${p =>
+      p.collapsed
+        ? css`
+            ${collapseAnim}
+          `
+        : css`
+            ${openAnim}
+          `}
+    200ms 500ms ease forwards;
+`;
 
 const RecoverStatusDot = ({ collapsed }: RecoverStatusNotificationProps) => {
+  const { colors } = useTheme();
   const recoverServices = useFeature("protectServicesDesktop");
   const protectID = recoverServices?.params?.protectId ?? "protect-prod";
   const [displayRecoverDot, setDisplayRecoverDot] = useState<boolean>();
@@ -27,7 +77,17 @@ const RecoverStatusDot = ({ collapsed }: RecoverStatusNotificationProps) => {
     getRecoverSubscriptionState();
   }, [getRecoverSubscriptionState]);
 
-  return displayRecoverDot ? <Dot collapsed={collapsed} color={colors.orange} /> : null;
+  return displayRecoverDot ? (
+    <StyledIcon
+      collapsed={collapsed}
+      size="XS"
+      style={{
+        color: colors.palette.warning.c70,
+        right: "5px",
+        position: "absolute",
+      }}
+    />
+  ) : null;
 };
 
 export default RecoverStatusDot;
