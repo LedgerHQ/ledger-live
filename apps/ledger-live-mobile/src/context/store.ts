28ef4fa38f077ccd7911ecd4da93c02e43b6d178
Mounir Hamzaoui <mounir.hamzaoui@ledger.fr>
2025-03-17 15:13:55 +0100

    fix lint issues

--- apps/ledger-live-mobile/src/context/store.ts
+++ apps/ledger-live-mobile/src/context/store.ts
@@ -2,23 +2,15 @@ import Config from "react-native-config";
 import { configureStore } from "@reduxjs/toolkit";
 import reducers from "~/reducers";
 import Rectotron from "~/ReactotronConfig";
-import { GetDefaultEnhancers } from "@reduxjs/toolkit/dist/getDefaultEnhancers";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const createEnhancers = (getDefaultEnhancers: GetDefaultEnhancers<any>) => {
-  if (__DEV__) {
-    return getDefaultEnhancers().concat(Rectotron.createEnhancer());
-  } else {
-    return getDefaultEnhancers();
-  }
-};
 
 export const store = configureStore({
   reducer: reducers,
   devTools: !!Config.DEBUG_RNDEBUGGER,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }),
-  enhancers: createEnhancers,
+  ...(__DEV__ && {
+    enhancers: getDefaultEnhancers => getDefaultEnhancers().concat(Rectotron.createEnhancer()),
+  }),
 });
 
 export type StoreType = typeof store;
