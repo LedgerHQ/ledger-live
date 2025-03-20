904287dcac164c4992a086df20c6e411a1098f30
Luís Couto <luis.couto-ext@ledger.fr>
2025-03-20 09:14:12 +0000

    feat(LIVE-17141): Swap deeplink (#9601)
    - Support for tokens in Swap deeplink
    - When multiple accounts exist, but none was specified, use the account with the highest balance.

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/hooks/useTranslateToSwapAccount.ts
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/hooks/useTranslateToSwapAccount.ts
@@ -1,21 +1,36 @@
 import { useSelector } from "react-redux";
-import { DefaultAccountSwapParamList } from "../../types";
-import { walletSelector } from "~/reducers/wallet";
-import { accountsSelector } from "~/reducers/accounts";
 import { useMemo } from "react";
 
 import * as walletApi from "@ledgerhq/live-common/wallet-api/converters";
+import { walletSelector } from "~/reducers/wallet";
+import { flattenAccountsSelector } from "~/reducers/accounts";
+
+import { DefaultAccountSwapParamList } from "../../types";
+import type { Account, AccountLike, TokenAccount } from "@ledgerhq/types-live";
 
 type SwapLiveUrlParams = {
   fromAccountId?: string;
+  fromToken?: string;
   amountFrom?: string;
+};
+
+const isTokenAccount = (account: AccountLike | TokenAccount): account is TokenAccount =>
+  (account as TokenAccount).token !== undefined;
+
+const isAccount = (account: AccountLike | undefined): account is Account =>
+  !!account && (account as Account).currency !== undefined;
+
+const getHighestBalanceAccount = (accounts: AccountLike[]): AccountLike => {
+  const sorted = [...accounts];
+  sorted.sort((a, b) => (a.spendableBalance.lt(b.spendableBalance) ? 1 : -1));
+  return sorted[0];
 };
 
 export const useTranslateToSwapAccount = (
   params: DefaultAccountSwapParamList | null,
 ): SwapLiveUrlParams => {
   const walletState = useSelector(walletSelector);
-  const currentAccounts = useSelector(accountsSelector);
+  const currentAccounts: AccountLike[] = useSelector(flattenAccountsSelector);
 
   return useMemo(() => {
     const newParams: SwapLiveUrlParams = {};
@@ -24,30 +39,44 @@ export const useTranslateToSwapAccount = (
       return {};
     }
 
+    const defaultAccount = params.defaultAccount;
+    // @ts-expect-error params.currency comes from market
+    const defaultCurrency = params.defaultCurrency || params.currency;
+
     // A specific account was given
-    if (params.defaultAccount) {
+    if (defaultAccount) {
       newParams.fromAccountId = walletApi.accountToWalletAPIAccount(
         walletState,
-        params.defaultAccount,
+        defaultAccount,
         params?.defaultParentAccount,
       ).id;
+
       return newParams;
     }
 
     // No account was given, but a currency was
-    // We use the first account found of the given currency
-    if (params.defaultCurrency) {
-      const currency = walletApi.currencyToWalletAPICurrency(params.defaultCurrency);
-      const firstAccount = currentAccounts.find(account => account.currency.id === currency.id);
+    if (defaultCurrency) {
+      const currency = walletApi.currencyToWalletAPICurrency(defaultCurrency);
+      const accounts = currentAccounts.filter(account =>
+        isTokenAccount(account)
+          ? account.token.id === currency.id
+          : account.currency?.id === currency.id,
+      );
+      const account = getHighestBalanceAccount(accounts);
 
-      if (firstAccount) {
+      if (account) {
+        const parentAccount = currentAccounts.find(currentAccount =>
+          isTokenAccount(account) ? currentAccount.id === account.parentId : false,
+        );
+
         newParams.fromAccountId = walletApi.accountToWalletAPIAccount(
           walletState,
-          firstAccount,
-          params.defaultParentAccount,
+          account,
+          isAccount(parentAccount) ? parentAccount : undefined,
         ).id;
       }
 
+      newParams.fromToken = currency.id;
       return newParams;
     }
 
