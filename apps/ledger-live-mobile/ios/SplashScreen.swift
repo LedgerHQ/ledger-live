b043853901ead46a46bb251f5f1eeeff44e80eb5
Lucas Werey <lucas.werey@ledger.fr>
2025-03-20 10:38:12 +0100

    Revert ":test_tube: (llm-ios) detox"
    This reverts commit 74596877961a1da6a732ef8674163de0ca013a39.

--- apps/ledger-live-mobile/ios/SplashScreen.swift
+++ apps/ledger-live-mobile/ios/SplashScreen.swift
@@ -12,71 +12,53 @@ import UIKit
 
 @objc(RNSplashScreenModule)
 public class RNSplashScreenModule: NSObject {
-    private static var isRunningTests: Bool {
-        return ProcessInfo.processInfo.environment["RUNNING_TESTS"] == "1"
+
+  @objc
+  static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+
+  @objc func showSplashScreen() {
+    DispatchQueue.main.async {
+      let appDelegate = UIApplication.shared.delegate
+      let viewController = SplashScreenController()
+      viewController.modalPresentationStyle = .fullScreen
+      appDelegate?.window??.rootViewController?.present(
+        viewController, animated: false)
     }
+  }
 
-    @objc
-    static func requiresMainQueueSetup() -> Bool {
-        return true
-    }
+  @objc func hideSplashScreen() {
+    DispatchQueue.main.async {
+      let appDelegate = UIApplication.shared.delegate
+      if let presentedViewController = appDelegate?.window??.rootViewController?
+        .presentedViewController as? SplashScreenController
+      {
+        let fadeOutDuration = 0.2
+        let scaleDownTransform = CGAffineTransform(scaleX: 0.95, y: 0.95)
 
-    @objc func showSplashScreen() {
-        if Self.isRunningTests {
-            return
-        }
-
-        DispatchQueue.main.async {
-            let appDelegate = UIApplication.shared.delegate
-            let viewController = SplashScreenController()
-            viewController.modalPresentationStyle = .fullScreen
-            appDelegate?.window??.rootViewController?.present(
-                viewController, animated: false)
-        }
-    }
-
-    @objc func hideSplashScreen() {
-        if Self.isRunningTests {
-            return
-        }
-
-        DispatchQueue.main.async {
-            let appDelegate = UIApplication.shared.delegate
-            if let presentedViewController = appDelegate?.window??.rootViewController?
-                .presentedViewController as? SplashScreenController
-            {
-                if Self.isRunningTests {
-                    presentedViewController.dismiss(animated: false) {
-                        presentedViewController.view.removeFromSuperview()
-                    }
-                    return
-                }
-
-                let fadeOutDuration = 0.2
-                let scaleDownTransform = CGAffineTransform(scaleX: 0.95, y: 0.95)
-
-                let animateFadeOutAndDismiss = {
-                    UIView.animate(
-                        withDuration: fadeOutDuration,
-                        animations: {
-                            presentedViewController.view.alpha = 0
-                            presentedViewController.view.transform = scaleDownTransform
-                        }
-                    ) { _ in
-                        presentedViewController.dismiss(animated: false) {
-                            presentedViewController.view.removeFromSuperview()
-                        }
-                    }
-                }
-
-                if presentedViewController.gifCompletedOnce {
-                    animateFadeOutAndDismiss()
-                } else {
-                    presentedViewController.onGifCompletion = {
-                        animateFadeOutAndDismiss()
-                    }
-                }
+        let animateFadeOutAndDismiss = {
+          UIView.animate(
+            withDuration: fadeOutDuration,
+            animations: {
+              presentedViewController.view.alpha = 0
+              presentedViewController.view.transform = scaleDownTransform
             }
+          ) { _ in
+            presentedViewController.dismiss(animated: false) {
+              presentedViewController.view.removeFromSuperview()
+            }
+          }
         }
+
+        if presentedViewController.gifCompletedOnce {
+          animateFadeOutAndDismiss()
+        } else {
+          presentedViewController.onGifCompletion = {
+            animateFadeOutAndDismiss()
+          }
+        }
+      }
     }
+  }
 }
