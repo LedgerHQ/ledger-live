f6cdcd38f62e56146ab048a2ef7b28c8aa173f52
RobinVncnt <robin.vincent@ledger.fr>
2025-03-21 14:13:14 +0100

    feat: Add Copy/Paste input to EIP712 Signature
    feat: Add Copy/Paste input to EIP712 Signature
    
    add background
    
    add translation

---
+++ apps/ledger-live-desktop/src/renderer/modals/SignMessage/steps/CopyButton.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useCallback } from "react";
+import styled from "styled-components";
+import { Icons, Button } from "@ledgerhq/react-ui";
+import { t } from "i18next";
+
+type Props = {
+  text: string;
+};
+
+export default function CopyButton({ text }: Props) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 1200);
+    });
+  }, [text]);
+
+  return (
+    <ButtonContainer variant="shade" onClick={handleCopy}>
+      <ContentWrapper>
+        {copied ? (
+          <>
+            <Icons.Check size={"S"} color="success.c50" />
+            <StyledText copied={true}>{t("common.copied")}!</StyledText>
+          </>
+        ) : (
+          <>
+            <Icons.Copy size={"S"} color="neutral.c90" />
+            <StyledText copied={false}>{t("common.copy")}</StyledText>
+          </>
+        )}
+      </ContentWrapper>
+    </ButtonContainer>
+  );
+}
+
+const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledText = styled.span<{ copied: boolean }>`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.neutral.c90};
+  padding-left: 8px;
+`;
+
+const ButtonContainer = styled(Button)`
+  position: sticky;
+  bottom: -15px;
+  left: 50%;
+  transform: translateX(-50%);
+  border: none;
+  display: block;
+  height: 36px;
+  background-color: ${({ theme }) => theme.colors.neutral.c30};
+`;
