4938b98b2b97d16e9358fd5ab9befe54acd01465
Abdurrahman SASTIM <abdurrahman.sastim@ledger.fr>
2025-03-24 09:49:47 +0100

    test: test

--- apps/ledger-live-mobile/e2e/helpers/commonHelpers.ts
+++ apps/ledger-live-mobile/e2e/helpers/commonHelpers.ts
@@ -62,11 +62,20 @@ export async function launchApp() {
 
 export const logMemoryUsage = async () => {
   const pid = process.pid;
+  console.warn(`📦 Detox Memory Usage: Getting memory usage for PID: ${pid}`);
+  exec(`top ${isAndroid() ? "-b -n 1 -p" : "-l 1 -pid"} ${pid}`, async (error, stdout, stderr) => {
+    if (error || stderr) {
+      console.error(`Error getting memory:\n ${error}\n ${stderr}`);
+      return;
+    }
+    const memoryUsed = stdout.trim();
+    log.warn(memoryUsed);
+  });
   exec(
-    `top -pid ${pid} -l 1 | grep -E '^[ ]*${pid}' | awk '{for(i=1;i<=NF;i++) if ($i ~ /[MG]/) {print $i; exit}}'`,
+    `top  ${isAndroid() ? "-b -n 1 -p " : "-l 1 -pid " + pid}  | grep -E '^[ ]*${pid}' | awk '{for(i=1;i<=NF;i++) if ($i ~ /[MG]/) {print $i; exit}}'`,
     async (error, stdout, stderr) => {
       if (error || stderr) {
-        console.error(`Error getting memory usage:\n ${error}\n ${stderr}`);
+        console.error(`Error getting memory usage:\n Error: ${error}\n Stderr: ${stderr}`);
         return;
       }
       const memoryUsed = stdout.trim();
