8921224eee35c4fa17ff96eec1befece8a53dc7f
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-03-25 15:53:59 +0100

    fix(llm): cosmos transactions failing due to undefined memo

--- apps/ledger-live-mobile/src/families/cosmos/MemoTagInput.tsx
+++ apps/ledger-live-mobile/src/families/cosmos/MemoTagInput.tsx
@@ -5,8 +5,5 @@ import type { MemoTagInputProps } from "LLM/features/MemoTag/types";
 import { GenericMemoTagInput } from "LLM/features/MemoTag/components/GenericMemoTagInput";
 
 export default (props: MemoTagInputProps<CosmosTransaction>) => (
-  <GenericMemoTagInput
-    {...props}
-    valueToTxPatch={value => tx => ({ ...tx, memo: value || undefined })}
-  />
+  <GenericMemoTagInput {...props} valueToTxPatch={value => tx => ({ ...tx, memo: value })} />
 );
