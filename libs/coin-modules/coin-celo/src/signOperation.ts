8d7f6cc9e6b802b2ad9a08440767ec34de327738
Antoine Blaise <antoine.blaise@ledger.fr>
2025-03-25 17:29:49 +0100

    feat(coin-celo) fix signOperation.ts

--- libs/coin-modules/coin-celo/src/signOperation.ts
+++ libs/coin-modules/coin-celo/src/signOperation.ts
@@ -3,11 +3,12 @@ import { Observable } from "rxjs";
 import { FeeNotLoaded } from "@ledgerhq/errors";
 import type { Account, AccountBridge, DeviceId, SignOperationEvent } from "@ledgerhq/types-live";
 // import { encodeTransaction, recoverTransaction } from "@celo/wallet-base";
+import { rlpEncodedTx } from "@celo/wallet-base";
 
 import { buildOptimisticOperation } from "./buildOptimisticOperation";
 import type { Transaction, CeloAccount } from "./types";
 // import { withDevice } from "../../../ledger-live-common/src/hw/deviceAccess";
-// import buildTransaction from "./buildTransaction";
+import buildTransaction from "./buildTransaction";
 // import { CeloSigner, CeloSignature } from "./signer";
 import { SignerContext } from "@ledgerhq/coin-framework/signer";
 import { EvmSigner, EvmSignature } from "@ledgerhq/coin-evm/lib/types/signer";
@@ -30,38 +31,22 @@ export const buildSignOperation =
       async function main() {
         const { fees } = transaction;
         if (!fees) throw new FeeNotLoaded();
+        const unsignedTransaction = await buildTransaction(account as CeloAccount, transaction);
+        const rlpEncodedTransaction = await rlpEncodedTx(unsignedTransaction);
 
         o.next({ type: "device-signature-requested" });
 
         const signature = (await signerContext(deviceId, signer =>
-          signer.signTransaction(account.freshAddressPath, "I don't know hat to put"),
+          signer.signTransaction(
+            account.freshAddressPath,
+            trimLeading0x(rlpEncodedTransaction.rlpEncode),
+          ),
         )) as EvmSignature;
-
-        // const response = await celo.signTransaction(
-        //   account.freshAddressPath,
-        //   trimLeading0x(rlpEncodedTransaction.rlpEncode),
-        // );
-        // freshAddressPath is actually a derivation path
-
-        // const { address } = await celo.getAddress(account.freshAddressPath);
-
-        // if (cancelled) return;
-
-        // const signature = parseSigningResponse(response, chainId!, await celo.isAppModern());
 
         o.next({ type: "device-signature-granted" });
 
-        // const encodedTransaction = await encodeTransaction(rlpEncodedTransaction, signature);
-
-        // const [_, recoveredAddress] = recoverTransaction(encodedTransaction.raw);
-        // if (recoveredAddress !== address) {
-        //   throw new Error(
-        //     "celo: there was a signing error, the recovered address doesn't match the your ledger address, the operation was cancelled",
-        //   );
-        // }
-
         const operation = buildOptimisticOperation(
-          account as CeloAccount, // je ne sais pas si je peux faire ça
+          account as CeloAccount,
           transaction,
           transaction.fees ?? new BigNumber(0),
         );
@@ -79,44 +64,8 @@ export const buildSignOperation =
         () => o.complete(),
         e => o.error(e),
       );
-
-      // return () => {
-      //   cancelled = true;
-      // };
     });
 
-// const trimLeading0x = (input: string) => (input.startsWith("0x") ? input.slice(2) : input);
-
-// const parseSigningResponse = (
-//   response: {
-//     s: string;
-//     v: string;
-//     r: string;
-//   },
-//   chainId: number,
-//   isModern: boolean,
-// ): {
-//   s: Buffer;
-//   v: number;
-//   r: Buffer;
-// } => {
-//   // EIP155
-//   const sigV = parseInt(response.v, 16);
-//   let eip155V = chainId * 2 + 35;
-
-//   if (isModern) {
-//     // eip1559 and other enveloppes txs dont need to modify V
-//     // just use what the ledger device returns
-//     eip155V = sigV;
-//   } else if (sigV !== eip155V && (sigV & eip155V) !== sigV) {
-//     eip155V += 1;
-//   }
-
-//   return {
-//     s: Buffer.from(response.s, "hex"),
-//     v: eip155V,
-//     r: Buffer.from(response.r, "hex"),
-//   };
-// };
+const trimLeading0x = (input: string) => (input.startsWith("0x") ? input.slice(2) : input);
 
 export default buildSignOperation;
