30c0f778ca54ee7e6f9380462297651a6c7d186a
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-03-25 12:38:21 +0100

    feat: PR reviews

--- apps/ledger-live-desktop/src/renderer/families/cardano/DelegationFlowModal/fields/ValidatorField.tsx
+++ apps/ledger-live-desktop/src/renderer/families/cardano/DelegationFlowModal/fields/ValidatorField.tsx
@@ -1,5 +1,10 @@
 import { useCardanoFamilyPools } from "@ledgerhq/live-common/families/cardano/react";
-import { StakePool, fetchPoolDetails } from "@ledgerhq/live-common/families/cardano/staking";
+import {
+  DEFAULT_SELECTED_POOL_ID,
+  LEDGER_POOL_IDS,
+  StakePool,
+  fetchPoolDetails,
+} from "@ledgerhq/live-common/families/cardano/staking";
 import { CardanoDelegation } from "@ledgerhq/live-common/families/cardano/types";
 import { TransactionStatus } from "@ledgerhq/live-common/generated/types";
 import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
@@ -25,15 +30,6 @@ type Props = {
   onChangeValidator: (a: StakePool) => void;
   selectedPoolId: string;
 };
-
-const DEFAULT_SELECTED_POOL_ID: string = "4a9c9902c9538da900b10b716d5d1b214487455fdb06028b32ffa180"; // Ledger by Figment 2
-
-// the IDs of ledger stake pools to shows in lld and llm
-const LEDGER_POOL_IDS: Array<string> = [
-  "a314a18528d00c5fbd067ecb4a212cf2f307c83d2c08f44a11ebebf6", // Ledger by Figment 1
-  DEFAULT_SELECTED_POOL_ID, // Ledger by Figment 2
-  "c726c9da5615d5f9f6858c25bb13f81c4741eccd08ce32f3414f323f", // Ledger by Figment 3
-];
 
 export function concatUserAndLedgerPoolIds(
   userLastUsedPool: string | undefined,
