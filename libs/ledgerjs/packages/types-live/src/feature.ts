51546149341271cf3db6dc84cf4b8ede5d6d2224
Luís Couto <luis.couto-ext@ledger.fr>
2025-03-25 10:25:25 +0000

    Revert "feat(LIVE-17514): remove unused Swap feature flags" (#9653)
    Revert "feat(LIVE-17514): remove unused Swap feature flags (#9482)"
    
    This reverts commit 1ec6a03580ea1136dab22747721942056506d589.

--- libs/ledgerjs/packages/types-live/src/feature.ts
+++ libs/ledgerjs/packages/types-live/src/feature.ts
@@ -181,6 +181,7 @@ export type Features = CurrencyFeatures & {
   flexibleContentCards: Feature_FlexibleContentCards;
   llmAnalyticsOptInPrompt: Feature_LlmAnalyticsOptInPrompt;
   ptxSwapMoonpayProvider: Feature_PtxSwapMoonpayProvider;
+  ptxSwapExodusProvider: Feature_PtxSwapExodusProvider;
   lldAnalyticsOptInPrompt: Feature_LldAnalyticsOptInPrompt;
   lldChatbotSupport: Feature_LldChatbotSupport;
   llmChatbotSupport: Feature_LlmChatbotSupport;
@@ -606,6 +607,7 @@ export type Feature_lldnewArchOrdinals = DefaultFeature;
 export type Feature_SpamFilteringTx = DefaultFeature;
 export type Feature_MemoTag = DefaultFeature;
 export type Feature_PtxSwapMoonpayProvider = DefaultFeature;
+export type Feature_PtxSwapExodusProvider = DefaultFeature;
 
 export type Feature_LlmRebornLP = Feature<{
   variant: ABTestingVariants;
