63ebc92dd31958710ccc374d91fc735906c704dd
Antoine Blaise <antoine.blaise@ledger.fr>
2025-03-26 15:46:50 +0100

    feat(coin-module): fix imprt and lld build

--- libs/coin-modules/coin-celo/src/bridge.integration.test.ts
+++ libs/coin-modules/coin-celo/src/bridge.integration.test.ts
@@ -2,7 +2,7 @@ import celo from "./datasets/celo.scanAccounts.1";
 import type { Transaction } from "./types";
 import type { DatasetTest } from "@ledgerhq/types-live";
 
-const dataset: DatasetTest<Transaction> = {
+export const dataset: DatasetTest<Transaction> = {
   implementations: ["js"],
   currencies: {
     celo,
