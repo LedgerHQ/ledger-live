79ded1e5f9d1ce4a0ee24c2ab3a45d25e06e6904
Louis Aussedat <louis.aussedat@ledger.fr>
2025-03-26 12:32:27 +0100

    wip

---
+++ libs/coin-tester-evm/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  setupFiles: ["<rootDir>/src/setup.ts"],
+};
+
+export default config;
