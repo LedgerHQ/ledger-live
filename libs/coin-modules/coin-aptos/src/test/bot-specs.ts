97e1352a54dfd9f8493432fb8801cc99f94bb6f0
Pedro Semeano <semeano@gmail.com>
2025-03-27 10:25:05 +0000

    test: aptos token transaction bot specs (#9548)
    * test: aptos token transaction bot specs
    
    * chore: save work
    
    * chore: save work
    
    * chore: save work
    
    * chore: save work
    
    * chore: save work
    
    * chore: save work
    
    * fix: transaction history with token fees
    
    * chore: remove commented code
    
    * chore: transaction history fixed
    
    * chore: transaction history with total amount with fees fixed
    
    * chore: transaction history order for tokens
    
    * chore: uncomment previous bot tests
    
    * chore: revert changes in project file
    
    * fix: revert changes on project file
    
    * fix: remove unwanted spec configs

--- libs/coin-modules/coin-aptos/src/test/bot-specs.ts
+++ libs/coin-modules/coin-aptos/src/test/bot-specs.ts
@@ -5,8 +5,9 @@ import BigNumber from "bignumber.js";
 import type { Transaction } from "../types";
 import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets/currencies";
 import { genericTestDestination, pickSiblings, botTest } from "@ledgerhq/coin-framework/bot/specs";
-import type { AppSpec } from "@ledgerhq/coin-framework/bot/types";
-import { acceptTransaction } from "./speculos-deviceActions";
+import type { AppSpec, TransactionTestInput } from "@ledgerhq/coin-framework/bot/types";
+import { acceptTokenTransaction, acceptTransaction } from "./speculos-deviceActions";
+import { Account, TokenAccount } from "@ledgerhq/types-live";
 
 const MIN_SAFE = new BigNumber(0.0001);
 const maxAccount = 6;
@@ -19,7 +20,7 @@ const aptosSpecs: AppSpec<Transaction> = {
     appName: "Aptos",
   },
   genericDeviceAction: acceptTransaction,
-  testTimeout: 6 * 60 * 1000,
+  testTimeout: 2 * 60 * 1000,
   minViableAmount: MIN_SAFE,
   transactionCheck: ({ maxSpendable }) => {
     invariant(maxSpendable.gt(MIN_SAFE), "balance is too low");
@@ -84,42 +85,67 @@ const aptosSpecs: AppSpec<Transaction> = {
         );
       },
     },
-    // {
-    //   name: "Send ~50% of stdAPT token",
-    //   feature: "tokens",
-    //   maxRun: 1,
-    //   deviceAction: acceptTokenTransaction,
-    //   testDestination: genericTestDestination,
-    //   transaction: ({ account, siblings, bridge, maxSpendable }) => {
-    //     invariant(maxSpendable.gt(MIN_SAFE), "balance is too low");
-    //     const sibling = pickSiblings(siblings, maxAccount);
-    //     const recipient = sibling.freshAddress;
-    //     const amount = maxSpendable.div(2).integerValue();
+    {
+      name: "Send ~50% of token amount",
+      feature: "tokens",
+      maxRun: 1,
+      deviceAction: acceptTokenTransaction,
+      transaction: ({ account, bridge, siblings, maxSpendable }) => {
+        invariant(maxSpendable.gt(MIN_SAFE), "Balance is too low");
 
-    //     const transaction = bridge.createTransaction(account);
-    //     const updates: Array<Partial<Transaction>> = [
-    //       {
-    //         recipient,
-    //       },
-    //       { amount },
-    //     ];
+        const senderTokenAcc = findTokenSubAccountWithBalance(account);
+        invariant(senderTokenAcc, "Sender token account with available balance not found");
 
-    //     return {
-    //       transaction,
-    //       updates,
-    //     };
-    //   },
+        const sibling = pickSiblings(siblings, maxAccount);
 
-    //   test: ({ accountBeforeTransaction, operation, account }) => {
-    //     botTest("account spendable balance decreased with operation", () =>
-    //       expect(account.spendableBalance).toEqual(
-    //         accountBeforeTransaction.spendableBalance.minus(operation.value),
-    //       ),
-    //     );
-    //   },
-    // },
+        const recipientTokenAcc = findTokenSubAccountWithBalance(sibling);
+        invariant(recipientTokenAcc, "Receiver token account with available balance not found");
+
+        const amount = senderTokenAcc.spendableBalance.div(2).integerValue();
+        const recipient = sibling.freshAddress;
+        const transaction = bridge.createTransaction(account);
+        const subAccountId = senderTokenAcc.id;
+
+        return {
+          transaction,
+          updates: [{ subAccountId }, { recipient }, { amount }],
+        };
+      },
+      test: input => {
+        expectTokenAccountCorrectBalanceChange(input);
+      },
+    },
   ],
 };
+
+function findTokenSubAccountWithBalance(account: Account) {
+  return account.subAccounts?.find(acc => acc.type === "TokenAccount" && acc.balance.gt(0)) as
+    | TokenAccount
+    | undefined;
+}
+
+function expectTokenAccountCorrectBalanceChange({
+  account,
+  accountBeforeTransaction,
+  status,
+  transaction,
+}: TransactionTestInput<Transaction>) {
+  const tokenAccId = transaction.subAccountId;
+  if (!tokenAccId) throw new Error("Wrong subAccountId");
+
+  const tokenAccAfterTx = account.subAccounts?.find(acc => acc.id === tokenAccId);
+  const tokenAccBeforeTx = accountBeforeTransaction.subAccounts?.find(acc => acc.id === tokenAccId);
+
+  if (!tokenAccAfterTx || !tokenAccBeforeTx) {
+    throw new Error("Token sub accounts not found!");
+  }
+
+  botTest("Token balance decreased with operation", () =>
+    expect(tokenAccAfterTx.balance.toString()).toBe(
+      tokenAccBeforeTx.balance.minus(status.amount).toString(),
+    ),
+  );
+}
 
 export default {
   aptosSpecs,
