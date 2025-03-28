0d9c07d428507f61b0c776124df09c273ee99d7a
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-03-28 11:51:00 +0100

    wip

--- libs/coin-modules/coin-cosmos/src/synchronisation.integ.test.ts
+++ libs/coin-modules/coin-cosmos/src/synchronisation.integ.test.ts
@@ -20,20 +20,21 @@ describe("Testing synchronisation", () => {
       } as AccountShapeInfo<CosmosAccount>,
       {} as SyncConfig,
     );
+
     expect(result).not.toBeUndefined();
     expect(result.balance?.isGreaterThanOrEqualTo(0)).toBeTruthy();
     expect(result.blockHeight).toBeGreaterThanOrEqual(0);
     expect(result.id).toEqual("js:2:cosmos:" + addressId + ":");
 
-    if (result.cosmosResources && result.cosmosResources.delegatedBalance > BigNumber(0)) {
+    if (result.cosmosResources) {
       expect(result.cosmosResources.delegatedBalance).toEqual(
         result.cosmosResources.delegations
           .map(delegation => delegation.amount)
-          .reduce((accumulator, currentValue) => accumulator.plus(currentValue)),
+          .reduce((accumulator, currentValue) => accumulator.plus(currentValue), BigNumber(0)),
       );
 
       expect(result.cosmosResources.pendingRewardsBalance.isGreaterThanOrEqualTo(0)).toBeTruthy();
-      expect(result.cosmosResources.sequence).toBeGreaterThanOrEqual(0);
+      expect(result.cosmosResources.sequence).toBeGreaterThanOrEqual(0); // Check definition
 
       expect(result.cosmosResources.unbondingBalance).toEqual(
         result.cosmosResources.unbondings
@@ -53,8 +54,17 @@ describe("Testing synchronisation", () => {
         expect(operation.fee.isGreaterThanOrEqualTo(0)).toBeTruthy();
         expect(operation.hasFailed).not.toBeUndefined();
         expect(operation.hash).not.toBeUndefined();
+        // need to be tested ?
+        // expect(operation.blockHash).not.toBeUndefined();
+        // expect((operation.extra as { memo: string }).memo).not.toBeUndefined();
+
         expect(operation.id).toContain("js:2:cosmos:" + addressId + ":");
         expect(operation.type).toMatch(/IN|REWARD|DELEGATE/);
+        expect(operation.value.isGreaterThanOrEqualTo(0)).toBeTruthy();
+        expect(operation.transactionSequenceNumber).not.toBeUndefined();
+
+        //TODO do we need to check transactionRaw
+        //expect(operation.transactionRaw).not.toBeUndefined();
 
         if (operation.type === "IN") {
           expect(operation.recipients.length).toBeGreaterThan(0);
@@ -67,8 +77,17 @@ describe("Testing synchronisation", () => {
         if (operation.type === "REWARD" || operation.type === "DELEGATE") {
           expect(operation.recipients).toHaveLength(0);
           expect(operation.senders).toHaveLength(0);
+
+          const extra = operation.extra as { validators: { address: string; amount: BigNumber }[] };
+          extra.validators.forEach(validator => {
+            expect(validator.address).not.toBeUndefined();
+            expect(validator.amount.isGreaterThanOrEqualTo(0)).toBeTruthy(); // Check if >= 0
+          });
         }
       });
     }
+
+    expect(result.spendableBalance?.isGreaterThanOrEqualTo(0)).toBeTruthy();
+    expect(result.xpub).toEqual(addressId);
   });
 });
