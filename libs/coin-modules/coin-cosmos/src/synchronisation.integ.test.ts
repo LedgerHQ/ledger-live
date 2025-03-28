e7a91ebb71cab12056553a0533eb31cee04de5ff
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-03-28 11:32:09 +0100

    wip

--- libs/coin-modules/coin-cosmos/src/synchronisation.integ.test.ts
+++ libs/coin-modules/coin-cosmos/src/synchronisation.integ.test.ts
@@ -1,9 +1,9 @@
-import { SyncConfig } from "@ledgerhq/types-live";
-import { getAccountShape } from "./synchronisation";
-import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
 import { AccountShapeInfo } from "@ledgerhq/coin-framework/lib/bridge/jsHelpers";
-import { CosmosAccount } from "./types";
+import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+import { SyncConfig } from "@ledgerhq/types-live";
 import BigNumber from "bignumber.js";
+import { getAccountShape } from "./synchronisation";
+import { CosmosAccount } from "./types";
 
 describe("Testing synchronisation", () => {
   it("should test", async () => {
@@ -24,12 +24,51 @@ describe("Testing synchronisation", () => {
     expect(result.balance?.isGreaterThanOrEqualTo(0)).toBeTruthy();
     expect(result.blockHeight).toBeGreaterThanOrEqual(0);
     expect(result.id).toEqual("js:2:cosmos:" + addressId + ":");
+
     if (result.cosmosResources && result.cosmosResources.delegatedBalance > BigNumber(0)) {
       expect(result.cosmosResources.delegatedBalance).toEqual(
         result.cosmosResources.delegations
           .map(delegation => delegation.amount)
           .reduce((accumulator, currentValue) => accumulator.plus(currentValue)),
       );
+
+      expect(result.cosmosResources.pendingRewardsBalance.isGreaterThanOrEqualTo(0)).toBeTruthy();
+      expect(result.cosmosResources.sequence).toBeGreaterThanOrEqual(0);
+
+      expect(result.cosmosResources.unbondingBalance).toEqual(
+        result.cosmosResources.unbondings
+          .map(unbounding => unbounding.amount)
+          .reduce((accumulator, currentValue) => accumulator.plus(currentValue), BigNumber(0)),
+      );
+
+      expect(result.cosmosResources.withdrawAddress).not.toBeUndefined();
+    }
+
+    if (result.operations) {
+      expect(result.operationsCount).toEqual(result.operations.length);
+      result.operations.forEach(operation => {
+        expect(operation.accountId).not.toBeUndefined();
+        expect(operation.blockHeight).toBeGreaterThanOrEqual(0);
+        expect(operation.date).not.toBeUndefined();
+        expect(operation.fee.isGreaterThanOrEqualTo(0)).toBeTruthy();
+        expect(operation.hasFailed).not.toBeUndefined();
+        expect(operation.hash).not.toBeUndefined();
+        expect(operation.id).toContain("js:2:cosmos:" + addressId + ":");
+        expect(operation.type).toMatch(/IN|REWARD|DELEGATE/);
+
+        if (operation.type === "IN") {
+          expect(operation.recipients.length).toBeGreaterThan(0);
+          expect(operation.recipients.every(recipient => recipient !== undefined)).toBeTruthy();
+
+          expect(operation.senders.length).toBeGreaterThan(0);
+          expect(operation.senders.every(sender => sender !== undefined)).toBeTruthy();
+        }
+
+        if (operation.type === "REWARD" || operation.type === "DELEGATE") {
+          expect(operation.recipients).toHaveLength(0);
+          expect(operation.senders).toHaveLength(0);
+        }
+      });
     }
   });
 });
