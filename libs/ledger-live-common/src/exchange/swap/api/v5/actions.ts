f5105d216264b2fd59fc66cf2b9b71f9ef6eb818
Liviu Ciulinaru <liviu.ciulinaru@gmail.com>
2025-03-28 13:32:20 +0700

    feat: add custom.exchange.swap
    more updates
    
    fix: get parrent currency when swap from token
    
    add custom swap in llm
    
    feat sate to history
    
    fix: mobile history
    
    fix: swap hstory desktop
    
    chore: history failing on mobile
    
    chore: rm comments
    
    fix: imports
    
    chore: logs
    
    fix: move strategy in new file
    
    roll back save history from live app
    
    fix: trying to add cusotm fees
    
    chore: no bignumber
    
    fix: merge
    
    merge
    
    fix: add bignumber and customEchange fee
    
    fix merge
    
    chore: update comments
    
    fix: mobile type
    
    fix: trying to fix TS
    
    fix: build llc
    
    chore: changeset
    
    chore: rm logs
    
    fix: pr comments
    
    fix: navigate to pending operation
    
    fix: hook deps
    
    fix: rm unecessary pop
    
    fix: move custom handlers
    
    chore: rm unsuned fn
    
    chore: rm unsuned fn
    
    fix: lint
    
    fix: lint
    
    fix: redirect
    
    fix: install bignumber
    
    fix: refactor to custom.swapComplete
    
    fix: call without sdk
    
    fix: lld
    
    feat: add useSwapCustomHandlers
    
    chore: clean up
    
    fix: useEarnCustomHandlers
    
    chore: rm comments
    
    fix: call BE on complete
    
    chore: rm logs
    
    chore: update network
    
    chore: fix lint

---
+++ libs/ledger-live-common/src/exchange/swap/api/v5/actions.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { getSwapAPIBaseURL } from "../..";
+import { SwapPayloadRequestData, SwapPayloadResponse } from "../../types";
+
+export async function retrieveSwapPayload(
+  data: SwapPayloadRequestData,
+): Promise<SwapPayloadResponse> {
+  const SWAP_API_BASE = getSwapAPIBaseURL();
+
+  const swapAxiosClient = axios.create({
+    baseURL: SWAP_API_BASE,
+  });
+
+  const request = {
+    provider: data.provider,
+    deviceTransactionId: data.deviceTransactionId,
+    from: data.fromAccountCurrency,
+    to: data.toNewTokenId || data.toAccountCurrency,
+    address: data.toAccountAddress,
+    refundAddress: data.fromAccountAddress,
+    amountFrom: data.amount,
+    amountFromInSmallestDenomination: Number(data.amountInAtomicUnit),
+    rateId: data.quoteId,
+  };
+
+  const res = await swapAxiosClient.post(`${SWAP_API_BASE}/swap`, request);
+
+  return {
+    binaryPayload: res.data?.binaryPayload,
+    signature: res.data?.signature,
+    payinAddress: res.data?.payinAddress,
+    swapId: res.data?.swapId,
+    payinExtraId: res.data?.payinExtraId,
+    extraTransactionParameters: res.data?.extraTransactionParameters,
+  };
+}
