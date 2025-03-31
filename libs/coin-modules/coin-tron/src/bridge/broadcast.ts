5121ca1707b8dc115decfe6a5aeefbd06ab9a9b6
Canestin <canestin.ndong@ledger.fr>
2025-03-31 14:24:20 +0200

    adapt broadcast logic

--- libs/coin-modules/coin-tron/src/bridge/broadcast.ts
+++ libs/coin-modules/coin-tron/src/bridge/broadcast.ts
@@ -1,7 +1,6 @@
 import type { Account, AccountBridge, Operation, SignedOperation } from "@ledgerhq/types-live";
-import type { Transaction } from "../types";
-import { encodeTransaction } from "../logic/utils";
 import { broadcast as broadcastLogic } from "../logic/broadcast";
+import type { Transaction } from "../types";
 
 const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
   signedOperation: { signature, operation, rawData },
@@ -9,13 +8,12 @@ const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
   account: Account;
   signedOperation: SignedOperation;
 }): Promise<Operation> => {
-  try {
-    const tx = await encodeTransaction(rawData!, signature);
-    await broadcastLogic(tx);
-  } catch (e) {
-    console.error("Error caught:", e);
-    throw new Error(JSON.stringify(e));
-  }
+  const transaction = {
+    raw_data: rawData,
+    txID: operation.hash,
+    signature: [signature],
+  };
+  await broadcastLogic(transaction);
 
   return operation;
 };
