243cb5e8ac2eef2af7f2642170f028bb473ba6bb
Kant <quentin.jaccarino@ledger.fr>
2025-03-31 12:00:45 +0200

    fix: error thrown in the wrong case

--- apps/ledger-live-desktop/src/renderer/families/evm/nft.ts
+++ apps/ledger-live-desktop/src/renderer/families/evm/nft.ts
@@ -14,17 +14,30 @@ export const injectNftIntoTransaction = (
   nftProperties: Partial<NftProperties>,
   standard?: NFTStandard,
 ): EvmTransaction => {
-  if (transaction.mode === "send") {
-    throw new Error("Expected an NFT transaction but received a Send mode transactions");
+  if (!standard) {
+    if (transaction.mode === "send") {
+      throw new Error("Expected an NFT transaction but received a Send mode transactions");
+    }
+
+    return {
+      ...transaction,
+      mode: transaction.mode,
+      nft: {
+        collectionName: transaction.nft?.collectionName ?? "",
+        contract: nftProperties?.contract ?? transaction.nft?.contract ?? "",
+        tokenId: nftProperties?.tokenId ?? transaction.nft?.tokenId ?? "",
+        quantity: nftProperties?.quantity ?? transaction.nft?.quantity ?? new BigNumber(NaN),
+      },
+    };
   }
 
-  if (standard && standard === "SPL") {
+  if (standard === "SPL") {
     throw new Error("Expected an ERC721 or ERC1155 standard but received an SPL standard");
   }
 
   return {
     ...transaction,
-    mode: standard ? <Lowercase<typeof standard>>standard.toLowerCase() : transaction.mode,
+    mode: <Lowercase<typeof standard>>standard.toLowerCase(),
     nft: {
       collectionName: transaction.nft?.collectionName ?? "",
       contract: nftProperties?.contract ?? transaction.nft?.contract ?? "",
