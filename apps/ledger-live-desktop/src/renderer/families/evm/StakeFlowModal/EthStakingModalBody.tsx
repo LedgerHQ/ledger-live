96038bfa5d4d8875de42c82ca858b92c53d9b6db
Beth Swingler <beth.swingler@ledger.fr>
2025-03-31 23:25:06 +0200

    chore: try increased timeout on analytics test

--- apps/ledger-live-desktop/src/renderer/families/evm/StakeFlowModal/EthStakingModalBody.tsx
+++ apps/ledger-live-desktop/src/renderer/families/evm/StakeFlowModal/EthStakingModalBody.tsx
@@ -8,6 +8,7 @@ import { track } from "~/renderer/analytics/segment";
 import { ProviderItem } from "./component/ProviderItem";
 import { getTrackProperties } from "./utils/getTrackProperties";
 import { getWalletApiIdFromAccountId } from "@ledgerhq/live-common/wallet-api/converters";
+import { deriveAccountIdForManifest } from "@ledgerhq/live-common/wallet-api/utils/deriveAccountIdForManifest";
 
 type Props = {
   account: AccountLike;
@@ -29,17 +30,25 @@ export function EthStakingModalBody({ source, onClose, account, providers }: Pro
       manifest,
     }: StakeOnClickProps) => {
       const value = `/platform/${liveAppId}`;
-      const customDappUrl = queryParams && appendQueryParamsToManifestURL(manifest, queryParams);
+      const trackProperties = getTrackProperties({
+        value,
+        modal: source,
+      });
       track("button_clicked2", {
         button: providerConfigID,
-        ...getTrackProperties({ value, modal: source }),
+        ...trackProperties,
       });
+      const customDappUrl = queryParams && appendQueryParamsToManifestURL(manifest, queryParams);
 
       history.push({
         pathname: value,
         ...(customDappUrl ? { customDappUrl } : {}),
         state: {
-          accountId: manifest?.dapp ? account.id : getWalletApiIdFromAccountId(account.id),
+          accountId: deriveAccountIdForManifest(
+            account.id,
+            getWalletApiIdFromAccountId(account.id),
+            manifest,
+          ),
         },
       });
       onClose?.();
