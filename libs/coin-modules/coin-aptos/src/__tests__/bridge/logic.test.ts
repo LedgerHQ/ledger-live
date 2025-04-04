ec9bca36f179596a260b4b5ae2aa3b40836b3878
Pedro Semeano <semeano@gmail.com>
2025-04-04 14:40:58 +0100

    chore: remove unused code

--- libs/coin-modules/coin-aptos/src/__tests__/bridge/logic.test.ts
+++ libs/coin-modules/coin-aptos/src/__tests__/bridge/logic.test.ts
@@ -719,66 +719,6 @@ describe("Aptos sync logic ", () => {
       expect(result.coin_id).toEqual("0x44");
     });
 
-    it("should return zero amounts if no matching events are found", () => {
-      const tx = {
-        events: [
-          {
-            type: "0x1::coin::WithdrawEvent",
-            guid: {
-              account_address: "0x11",
-              creation_number: "1",
-            },
-            data: {
-              amount: "100",
-            },
-          },
-          {
-            type: "0x1::coin::DepositEvent",
-            guid: {
-              account_address: "0x11",
-              creation_number: "2",
-            },
-            data: {
-              amount: "50",
-            },
-          },
-        ],
-        changes: [
-          {
-            type: "write_resource",
-            data: {
-              type: APTOS_COIN_CHANGE,
-              data: {
-                withdraw_events: {
-                  guid: {
-                    id: {
-                      addr: "0x12", // should fail by address check
-                      creation_num: "1",
-                    },
-                  },
-                },
-                deposit_events: {
-                  guid: {
-                    id: {
-                      addr: "0x11",
-                      creation_num: "3", // should fail by number check
-                    },
-                  },
-                },
-              },
-            },
-          },
-        ],
-      } as unknown as AptosTransaction;
-
-      const address = "0x11";
-      const result = getCoinAndAmounts(tx, address);
-
-      expect(result.amount_in).toEqual(new BigNumber(0));
-      expect(result.amount_out).toEqual(new BigNumber(0));
-      expect(result.coin_id).toEqual(null);
-    });
-
     it("should handle transactions with other events", () => {
       const tx = {
         events: [
