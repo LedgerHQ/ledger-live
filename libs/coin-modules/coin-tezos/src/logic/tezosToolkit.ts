b7023c57f42e8d86b15050a5a3b4516f42f093e3
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2025-04-04 07:36:33 +0200

    cache by URL

--- libs/coin-modules/coin-tezos/src/logic/tezosToolkit.ts
+++ libs/coin-modules/coin-tezos/src/logic/tezosToolkit.ts
@@ -1,6 +1,15 @@
 import { TezosToolkit } from "@taquito/taquito";
 import coinConfig from "../config";
 
+// Tezos client instance is cached to avoid costly rebuild at every request
+// Watch out: cache key is the URL, coin module can be instantiated several times with different URLs
+const servers = new Map<string, TezosToolkit>();
 export function getTezosToolkit(): TezosToolkit {
-  return new TezosToolkit(coinConfig.getCoinConfig().node.url);
+  const url = coinConfig.getCoinConfig().node.url;
+  let server = servers.get(url);
+  if (server === undefined) {
+    server = new TezosToolkit(url);
+    servers.set(url, server);
+  }
+  return server;
 }
