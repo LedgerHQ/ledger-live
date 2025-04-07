fee0cb118243a5f1e0478233a5dfa94ab23d2446
ysitbon <yoann.sitbon@ledger.fr>
2025-04-07 11:46:15 +0200

    chore(llm): add flipper debug tool for android

--- apps/ledger-live-mobile/android/app/src/main/java/com/ledger/live/MainApplication.kt
+++ apps/ledger-live-mobile/android/app/src/main/java/com/ledger/live/MainApplication.kt
@@ -1,9 +1,22 @@
 package com.ledger.live
 
+import android.content.Context
+import okhttp3.OkHttpClient
 import android.app.Application
 import android.content.res.Configuration
 import com.braze.BrazeActivityLifecycleCallbackListener
 import com.brentvatne.react.ReactVideoPackage
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.leakcanary2.FlipperLeakEventListener
+import com.facebook.flipper.plugins.leakcanary2.LeakCanary2FlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.react.modules.network.NetworkingModule
+import com.facebook.react.ReactInstanceManager
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -16,6 +29,7 @@ import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ExpoModulesPackage
 import expo.modules.ReactNativeHostWrapper
+import leakcanary.LeakCanary
 
 class MainApplication : Application(), ReactApplication {
 
@@ -52,13 +66,33 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+
+    LeakCanary.config = LeakCanary.config.run {
+        copy(eventListeners = eventListeners + FlipperLeakEventListener())
+    }
+
     SoLoader.init(this, false)
     registerActivityLifecycleCallbacks(BrazeActivityLifecycleCallbackListener())
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
+
+    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+      val client = AndroidFlipperClient.getInstance(this)
+      val networkFlipperPlugin = NetworkFlipperPlugin()
+      NetworkingModule.setCustomClientBuilder { builder ->
+        builder.addNetworkInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
+      }
+
+      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+      client.addPlugin(LeakCanary2FlipperPlugin())
+      client.addPlugin(networkFlipperPlugin)
+      client.start()
+    }
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
