31490f140a76ec141b71567768fd177fbbdf4080
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-04-08 16:11:46 +0200

    feat: support RAW tx for Solana

---
+++ libs/coin-modules/coin-solana/src/buildTransaction.test.ts
@@ -0,0 +1,118 @@
+import {
+  BlockhashWithExpiryBlockHeight,
+  PublicKey,
+  RecentPrioritizationFees,
+  TransactionInstruction,
+  VersionedTransaction,
+} from "@solana/web3.js";
+import { buildTransactionWithAPI } from "./buildTransaction";
+import { ChainAPI } from "./network";
+import { Transaction } from "./types";
+import BigNumber from "bignumber.js";
+
+describe("Testing buildTransaction", () => {
+  const ADDRESS = "Hj69wRzkrFuf1Nby4yzPEFHdsmQdMoVYjvDKZSLjZFEp";
+  const BLOCKHASH = "DJRuRgQP3BeBNH8WVdF9cppBDj7pQNiVr4ZDnqupRtTC";
+  const LAST_VALID_BLOCK_HEIGHT = 1;
+  const SIGNATURE = Buffer.from("any random value");
+
+  afterEach(() => jest.clearAllMocks());
+
+  it("should decode raw transaction and use it when user provide it", async () => {
+    const expectedSolanaTransaction = {} as VersionedTransaction;
+    expectedSolanaTransaction.addSignature = jest.fn();
+
+    VersionedTransaction.deserialize = jest.fn(
+      (_serializedTransaction: Uint8Array) => expectedSolanaTransaction,
+    );
+
+    const rawTransaction = transaction("any random value");
+    const [solanaTransaction, recentBlockhash, addSignatureCallback] =
+      await buildTransactionWithAPI(
+        ADDRESS,
+        rawTransaction,
+        api(BLOCKHASH, LAST_VALID_BLOCK_HEIGHT),
+      );
+
+    expect(solanaTransaction).toEqual(expectedSolanaTransaction);
+    expect(recentBlockhash).toEqual(
+      expect.objectContaining({
+        blockhash: BLOCKHASH,
+        lastValidBlockHeight: LAST_VALID_BLOCK_HEIGHT,
+      }),
+    );
+
+    addSignatureCallback(SIGNATURE);
+    expect(expectedSolanaTransaction.addSignature).toHaveBeenCalledTimes(1);
+    expect(expectedSolanaTransaction.addSignature).toHaveBeenCalledWith(
+      new PublicKey(ADDRESS),
+      SIGNATURE,
+    );
+
+    expect(VersionedTransaction.deserialize).toHaveBeenCalledTimes(1);
+    expect(VersionedTransaction.deserialize).toHaveBeenCalledWith(Buffer.from(rawTransaction.raw!));
+  });
+
+  it("should build the transaction when user does not provide raw", async () => {
+    VersionedTransaction.deserialize = jest.fn(
+      (_serializedTransaction: Uint8Array) => ({}) as VersionedTransaction,
+    );
+
+    const nonRawTransaction = transaction();
+    const [solanaTransaction, recentBlockhash, addSignatureCallback] =
+      await buildTransactionWithAPI(
+        ADDRESS,
+        nonRawTransaction,
+        api(BLOCKHASH, LAST_VALID_BLOCK_HEIGHT),
+      );
+
+    expect(recentBlockhash).toEqual(
+      expect.objectContaining({
+        blockhash: BLOCKHASH,
+        lastValidBlockHeight: LAST_VALID_BLOCK_HEIGHT,
+      }),
+    );
+
+    solanaTransaction.addSignature = jest.fn();
+    addSignatureCallback(SIGNATURE);
+    expect(solanaTransaction.addSignature).toHaveBeenCalledTimes(1);
+    expect(solanaTransaction.addSignature).toHaveBeenCalledWith(new PublicKey(ADDRESS), SIGNATURE);
+
+    expect(VersionedTransaction.deserialize).toHaveBeenCalledTimes(0);
+  });
+});
+
+function api(blockhash: string, lastValidBlockHeight: number) {
+  return {
+    getLatestBlockhash: () =>
+      Promise.resolve({
+        blockhash: blockhash,
+        lastValidBlockHeight: lastValidBlockHeight,
+      } as BlockhashWithExpiryBlockHeight),
+    getSimulationComputeUnits: (_instructions: Array<TransactionInstruction>, _payer: PublicKey) =>
+      null,
+    getRecentPrioritizationFees: (_accounts: string[]) => [] as RecentPrioritizationFees[],
+  } as unknown as ChainAPI;
+}
+
+function transaction(raw?: string): Transaction {
+  return {
+    amount: new BigNumber(0),
+    recipient: "",
+    model: {
+      commandDescriptor: {
+        fee: 0,
+        warnings: {},
+        command: {
+          kind: "transfer", // any kind can work, just needed a value for the test
+          sender: "Hj69wRzkrFuf1Nby4yzPEFHdsmQdMoVYjvDKZSLjZFEp",
+          recipient: "DwRL6XkPAtM1bfuySJKZGn2t9WeG25RC39isAu2nwak4",
+          amount: 0,
+        },
+        errors: {},
+      },
+    },
+    raw: raw,
+    family: "solana",
+  } as Transaction;
+}
