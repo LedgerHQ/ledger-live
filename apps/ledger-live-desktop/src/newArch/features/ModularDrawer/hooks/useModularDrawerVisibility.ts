cbc3a2e4379ee9329632486a5faafcc696375cee
Martin CAYUELAS <112866305+mcayuelas-ledger@users.noreply.github.com>
2025-04-11 17:58:14 +0200

    feat: Modular Asset Drawer redirection into the Add Account flow (#9892)
    * feat: Modular Asset Drawer redirection into the Add Account flow
    
    * Review
    
    * chore: test the currency selection of useOpenAssetFlow
    
    ---------
    
    Co-authored-by: Theophile Sandoz <theophile.sandoz@ledger.fr>

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/hooks/useModularDrawerVisibility.ts
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/hooks/useModularDrawerVisibility.ts
@@ -1,18 +1,12 @@
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import { useCallback } from "react";
+import { ModularDrawerLocation } from "../enums";
 
-export enum Location {
-  ADD_ACCOUNT = "add_account",
-  EARN_FLOW = "earn_flow",
-  LIVE_APP = "live_app",
-  RECEIVE_FLOW = "receive_flow",
-  SEND_FLOW = "send_flow",
-}
 export function useModularDrawerVisibility() {
   const featureModularDrawer = useFeature("lldModularDrawer");
 
   const isModularDrawerVisible = useCallback(
-    (location: Location) => {
+    (location: ModularDrawerLocation) => {
       if (!featureModularDrawer?.enabled) return false;
       return featureModularDrawer.params?.[location] ?? false;
     },
