843d72d431bfaf9b4401a28580c9e2b7201b9860
emmanuelm41 <emmanuelm41@gmail.com>
2025-04-14 17:05:42 -0300

    feat: complete buildOptimisticOperation

--- libs/coin-modules/coin-stacks/src/bridge/buildOptimisticOperation.ts
+++ libs/coin-modules/coin-stacks/src/bridge/buildOptimisticOperation.ts
@@ -3,6 +3,7 @@ import { Account, OperationType } from "@ledgerhq/types-live";
 import BigNumber from "bignumber.js";
 import { StacksOperation, Transaction } from "../types";
 import { getAddress } from "./utils/misc";
+import { getSubAccount } from "./utils/token";
 
 export const buildOptimisticOperation = (
   account: Account,
@@ -14,14 +15,27 @@ export const buildOptimisticOperation = (
   const { id: accountId } = account;
   const { address } = getAddress(account);
 
+  // Check if this is a token transfer via subAccountId
+  const subAccount = getSubAccount(account, transaction);
+  const isTokenTransfer = !!subAccount;
+
+  // For token transfers, use the subAccount's id
+  const operationAccountId = isTokenTransfer ? subAccount.id : accountId;
+
+  // Set the correct value based on transaction type
+  // For token transfers, only the fee impacts the main account's balance
+  const value: BigNumber = isTokenTransfer
+    ? transaction.amount
+    : transaction.amount.plus(transaction.fee || new BigNumber(0));
+
   return {
-    id: encodeOperationId(accountId, hash, operatioinType),
+    id: encodeOperationId(operationAccountId, hash, operatioinType),
     hash,
-    type: "OUT",
+    type: operatioinType,
     senders: [address],
     recipients: [transaction.recipient],
-    accountId,
-    value: transaction.amount.plus(transaction.fee || 0),
+    accountId: operationAccountId,
+    value,
     fee: transaction.fee || new BigNumber(0),
     blockHash: null,
     blockHeight: null,
