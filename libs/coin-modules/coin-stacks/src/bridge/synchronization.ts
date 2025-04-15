334aca7b99ffa0ce9225db7029d41b200b330f00
emmanuelm41 <emmanuelm41@gmail.com>
2025-04-15 15:08:54 -0300

    feat: fetch and process token txs to ops

--- libs/coin-modules/coin-stacks/src/bridge/synchronization.ts
+++ libs/coin-modules/coin-stacks/src/bridge/synchronization.ts
@@ -1,11 +1,29 @@
-import { encodeAccountId } from "@ledgerhq/coin-framework/account/index";
+import {
+  emptyHistoryCache,
+  encodeAccountId,
+  encodeTokenAccountId,
+} from "@ledgerhq/coin-framework/account/index";
 import { GetAccountShape, makeSync } from "@ledgerhq/coin-framework/bridge/jsHelpers";
-import { Account } from "@ledgerhq/types-live";
+import { Account, TokenAccount } from "@ledgerhq/types-live";
 import { getAddressFromPublicKey } from "@stacks/transactions";
 import BigNumber from "bignumber.js";
 import invariant from "invariant";
-import { fetchBalances, fetchBlockHeight, fetchFullMempoolTxs, fetchFullTxs } from "../network/api";
-import { mapPendingTxToOps, mapTxToOps, reconciliatePublicKey } from "./utils/misc";
+import {
+  fetchAllTokenBalances,
+  fetchBalances,
+  fetchBlockHeight,
+  fetchFullMempoolTxs,
+  fetchFullTxs,
+} from "../network/api";
+import {
+  mapPendingTxToOps,
+  mapTxToOps,
+  reconciliatePublicKey,
+  sip010TxnToOperation,
+} from "./utils/misc";
+import { log } from "@ledgerhq/logs";
+import { findTokenById } from "@ledgerhq/cryptoassets/lib/tokens";
+import { TransactionResponse } from "../network";
 
 export const getAccountShape: GetAccountShape = async info => {
   const { initialAccount, currency, rest = {}, derivationMode } = info;
@@ -27,6 +45,7 @@ export const getAccountShape: GetAccountShape = async info => {
   const blockHeight = await fetchBlockHeight();
   const balanceResp = await fetchBalances(address);
   const [rawTxs, tokenTxs] = await fetchFullTxs(address);
+  const tokenBalances = await fetchAllTokenBalances(address);
   const mempoolTxs = await fetchFullMempoolTxs(address);
 
   const balance = new BigNumber(balanceResp.balance);
@@ -39,8 +58,17 @@ export const getAccountShape: GetAccountShape = async info => {
 
   const pendingOperations = mempoolTxs.flatMap(mapPendingTxToOps(accountId, address));
 
+  const tokenAccounts = await buildTokenAccounts(
+    address,
+    accountId,
+    tokenTxs,
+    tokenBalances,
+    initialAccount,
+  );
+
   const result: Partial<Account> = {
     id: accountId,
+    subAccounts: tokenAccounts,
     xpub: pubKey,
     freshAddress: address,
     balance,
@@ -51,5 +79,64 @@ export const getAccountShape: GetAccountShape = async info => {
 
   return result;
 };
+
+export async function buildTokenAccounts(
+  address: string,
+  parentAccountId: string,
+  tokenTxs: Record<string, TransactionResponse[]>,
+  tokenBalances: Record<string, string>,
+  initialAccount?: Account,
+): Promise<TokenAccount[]> {
+  try {
+    const subs: TokenAccount[] = [];
+    for (const [tokenId, txns] of Object.entries(tokenTxs)) {
+      const token = findTokenById("stacks/sip010/" + tokenId);
+      if (!tokenId || !token) {
+        log("error", `stacks token not found, addr: ${tokenId}`);
+        continue;
+      }
+
+      const balanceResult = tokenBalances[tokenId];
+      const bnBalance = new BigNumber(balanceResult || "0");
+      const tokenAccountId = encodeTokenAccountId(parentAccountId, token);
+
+      const operations = txns
+        .flatMap(txn => sip010TxnToOperation(txn, address, tokenAccountId))
+        .flat()
+        .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+      if (operations.length === 0 && bnBalance.isZero()) {
+        continue;
+      }
+
+      const maybeExistingSubAccount =
+        initialAccount &&
+        initialAccount.subAccounts &&
+        initialAccount.subAccounts.find(a => a.id === tokenAccountId);
+
+      const sub: TokenAccount = {
+        type: "TokenAccount",
+        id: tokenAccountId,
+        parentId: parentAccountId,
+        token,
+        balance: bnBalance,
+        spendableBalance: bnBalance,
+        operationsCount: operations.length,
+        operations,
+        pendingOperations: maybeExistingSubAccount ? maybeExistingSubAccount.pendingOperations : [],
+        creationDate: operations.length > 0 ? operations[0].date : new Date(),
+        swapHistory: maybeExistingSubAccount ? maybeExistingSubAccount.swapHistory : [],
+        balanceHistoryCache: emptyHistoryCache, // calculated in the jsHelpers
+      };
+
+      subs.push(sub);
+    }
+
+    return subs;
+  } catch (e) {
+    log("error", "stacks error building token accounts", e);
+    return [];
+  }
+}
 
 export const sync = makeSync({ getAccountShape });
