ef551c8a1f483ad2e7a2c855e667d78bb73154a3
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-04-15 16:57:43 +0200

    feat: implements CeloSigner that doesn't use the new EvmSigner

--- libs/coin-modules/coin-celo/src/signer/signer.ts
+++ libs/coin-modules/coin-celo/src/signer/signer.ts
@@ -1,12 +1,36 @@
 import type { CeloTx, RLPEncodedTx, LegacyEncodedTx } from "../types";
-import { EvmSignature, EvmSigner } from "@ledgerhq/coin-evm/types/signer";
-import { LedgerEthTransactionResolution } from "@ledgerhq/hw-app-eth/lib/services/types";
-export interface CeloSigner extends EvmSigner {
-  signTransaction(
+import { EvmSignature, EvmAddress } from "@ledgerhq/coin-evm/types/signer";
+import { LoadConfig, ResolutionConfig } from "@ledgerhq/hw-app-eth/lib/services/types";
+import { EIP712Message } from "@ledgerhq/types-live";
+
+// TODO: this should use EvmSigner
+export interface CeloSigner {
+  getAddress: (
+    path: string,
+    boolDisplay?: boolean,
+    boolChaincode?: boolean,
+    chainId?: string,
+  ) => Promise<EvmAddress>;
+  signTransaction: (path: string, rawTxHex: string, resolution?: any) => Promise<EvmSignature>;
+  signPersonalMessage: (path: string, messageHex: string) => Promise<EvmSignature>;
+  signEIP712Message(
+    path: string,
+    jsonMessage: EIP712Message,
+    fullImplem?: boolean,
+  ): Promise<EvmSignature>;
+  setLoadConfig: (config: LoadConfig) => void;
+  clearSignTransaction: (
     path: string,
     rawTxHex: string,
-    resolution?: LedgerEthTransactionResolution | null,
-  ): Promise<EvmSignature>;
+    resolutionConfig: ResolutionConfig,
+    throwOnError: boolean,
+  ) => Promise<EvmSignature>;
+  signEIP712HashedMessage: (
+    path: string,
+    domainSeparatorHex: string,
+    hashStructMessageHex: string,
+  ) => Promise<EvmSignature>;
+  // Celo specific
   verifyTokenInfo(to: string, chainId: number): Promise<void>;
   rlpEncodedTxForLedger(txParams: CeloTx): Promise<RLPEncodedTx | LegacyEncodedTx>;
 }
