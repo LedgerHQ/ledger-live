a4a2db75a42e59372d23d988f4fc0865410c16e0
Elliott Coleman <elliott.coleman@yld.io>
2025-04-16 16:01:05 +0100

    feat(lld): test and use Text

--- libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.stories.tsx
+++ libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.stories.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { VirtualList } from "./VirtualList";
+import { PlayFnProps } from "src/pre-ldls/types";
+import { expect, within } from "@storybook/test";
 
 const meta: Meta<typeof VirtualList> = {
   component: VirtualList,
   title: "PreLdls/Components/VirtualList",
   tags: ["autodocs"],
-  args: {},
+  args: {
+    children: Array.from({ length: 50 }).map((_, i) => <h1>Item #{i}</h1>),
+    itemHeight: 64,
+    maxHeight: 250,
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof VirtualList>;
 
-export const Default: Story = {
-  args: {
-    children: Array.from({ length: 50 }).map((_, i) => <h1>Item {i}</h1>),
-    itemHeight: 64,
-    maxHeight: 250,
+export const Default: Story = {};
+
+export const TestVirtualList: Story = {
+  play: async ({ canvasElement }: PlayFnProps) => {
+    const canvas = within(canvasElement);
+
+    const visible = canvas.getAllByText(/Item #/i);
+
+    await expect(visible.length).toBeLessThanOrEqual(10);
   },
 };
