296ed54d9c678215b13fd3e77887d42c2cc3ce25
Elliott Coleman <elliott.coleman@yld.io>
2025-04-16 16:01:05 +0100

    feat(lld): test and use Text

--- libs/ui/packages/react/src/pre-ldls/components/AssetItem/AssetItem.stories.tsx
+++ libs/ui/packages/react/src/pre-ldls/components/AssetItem/AssetItem.stories.tsx
@@ -1,16 +1,28 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { AssetItem } from "./AssetItem";
+import { PlayFnProps } from "src/pre-ldls/types";
+import { expect, within } from "@storybook/test";
 
 const meta: Meta<typeof AssetItem> = {
   component: AssetItem,
   title: "PreLdls/Components/AssetItem",
   tags: ["autodocs"],
-  args: {},
+  args: { name: "Bitcoin", ticker: "BTC" },
 };
 export default meta;
 
 type Story = StoryObj<typeof AssetItem>;
 
-export const Default: Story = {
-  args: { name: "Bitcoin", ticker: "BTC" },
+export const Default: Story = {};
+
+export const TestAssetItem: Story = {
+  play: async ({ canvasElement }: PlayFnProps) => {
+    const canvas = within(canvasElement);
+
+    const name = canvas.getByText("Bitcoin");
+    const ticker = canvas.getByText("BTC");
+
+    await expect(name).toBeInTheDocument();
+    await expect(ticker).toBeInTheDocument();
+  },
 };
