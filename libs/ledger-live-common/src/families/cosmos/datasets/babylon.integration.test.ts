18697b5533b38a655a3c450442d44fad081ad171
Loïc Payol <loic.payol-ext@ledger.fr>
2025-04-16 17:41:59 +0200

    ci: add test snapshots

--- libs/ledger-live-common/src/families/cosmos/datasets/babylon.integration.test.ts
+++ libs/ledger-live-common/src/families/cosmos/datasets/babylon.integration.test.ts
@@ -1,8 +1,8 @@
 import { DatasetTest } from "@ledgerhq/types-live";
 import { testBridge } from "../../../__tests__/test-helpers/bridge";
 import "../../../__tests__/test-helpers/setup";
-import type { Transaction } from "../types";
 import babylon from "./babylon";
+import type { Transaction } from "../types";
 
 const dataset: DatasetTest<Transaction> = {
   implementations: ["js"],
