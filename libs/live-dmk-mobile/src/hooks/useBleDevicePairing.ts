d830c66253edeb98e1f905347d7f082c4765ee58
jdabbech-ledger <jalil.dabbech@ledger.com>
2025-04-17 15:17:56 +0200

    :recycle: (ble): Rename DeviceManagementKitTransport -> DeviceManagementKitBLETransport

--- libs/live-dmk-mobile/src/hooks/useBleDevicePairing.ts
+++ libs/live-dmk-mobile/src/hooks/useBleDevicePairing.ts
@@ -3,7 +3,7 @@ import { DmkError } from "@ledgerhq/device-management-kit";
 import { rnBleTransportIdentifier } from "@ledgerhq/device-transport-kit-react-native-ble";
 import { activeDeviceSessionSubject } from "@ledgerhq/live-dmk-shared";
 import { Device } from "@ledgerhq/types-devices";
-import { DeviceManagementKitTransport } from "../transport/DeviceManagementKitTransport";
+import { DeviceManagementKitBLETransport } from "../transport/DeviceManagementKitBLETransport";
 import { useDeviceManagementKit } from "./useDeviceManagementKit";
 
 type UseBleDevicePairingArgs = {
@@ -34,7 +34,7 @@ export const useBleDevicePairing = ({
         },
         sessionRefresherOptions: { isRefresherDisabled: true },
       });
-      const transport = new DeviceManagementKitTransport(dmk, sessionId);
+      const transport = new DeviceManagementKitBLETransport(dmk, sessionId);
       activeDeviceSessionSubject.next({ sessionId, transport });
       setIsPaired(true);
     } catch (error) {
