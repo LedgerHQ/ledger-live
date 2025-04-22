3fa16b8cbc2ac61067e97f722305f432acd24a20
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-04-22 17:39:39 +0200

    Test Virtual List scroll

--- libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.stories.tsx
+++ libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { VirtualList } from "./VirtualList";
-import { expect, within } from "@storybook/test";
+import { expect, userEvent, waitFor, within } from "@storybook/test";
 
 const meta: Meta<typeof VirtualList> = {
   component: VirtualList,
@@ -17,7 +17,7 @@ const meta: Meta<typeof VirtualList> = {
   args: {
     itemHeight: 64,
     count: 50,
-    renderItem: (i: number) => <h1>Item #{i}</h1>,
+    renderItem: (i: number) => <h1 tabIndex={i}>Item #{i}</h1>,
   },
 };
 export default meta;
@@ -31,7 +31,17 @@ export const TestVirtualList: Story = {
     const canvas = within(canvasElement);
 
     const visible = canvas.getAllByText(/Item #/i);
+    const firstVisible = visible[0];
+    const lastVisible = visible[visible.length - 1];
 
     await expect(visible.length).toBeLessThanOrEqual(10);
+
+    await expect(firstVisible).toBeInTheDocument();
+    await userEvent.pointer([
+      { keys: "[TouchA>]", target: lastVisible },
+      { target: firstVisible },
+      { keys: "[/TouchA]" },
+    ]);
+    await waitFor(() => expect(firstVisible).not.toBeInTheDocument());
   },
 };
