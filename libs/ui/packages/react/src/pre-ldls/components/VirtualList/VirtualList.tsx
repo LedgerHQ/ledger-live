3f999cfa208ecb791afc34b3a2f94afd7830fd07
Elliott Coleman <elliott.coleman@yld.io>
2025-04-22 10:43:47 +0100

    feat(LLD): autosizer and colour override

--- libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.tsx
+++ libs/ui/packages/react/src/pre-ldls/components/VirtualList/VirtualList.tsx
@@ -1,28 +1,29 @@
 import React, { ReactNode } from "react";
 import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 export const VirtualList = ({
   count,
   itemHeight,
-  maxHeight,
   renderRow,
 }: {
   count: number;
   itemHeight: number;
-  maxHeight: number;
   renderRow: (index: number) => ReactNode;
 }) => {
-  const minHeight = Math.min(count * itemHeight, maxHeight);
-
   return (
-    <List
-      height={minHeight}
-      itemCount={count}
-      itemSize={itemHeight}
-      layout="vertical"
-      width={"100%"}
-    >
-      {({ index, style }) => <div style={style}>{renderRow(index)}</div>}
-    </List>
+    <AutoSizer style={{ height: "100%", width: "100%" }}>
+      {({ height }: { height: number }) => (
+        <List
+          height={height}
+          itemCount={count}
+          itemSize={itemHeight}
+          layout="vertical"
+          width="100%"
+        >
+          {({ index, style }) => <div style={style}>{renderRow(index)}</div>}
+        </List>
+      )}
+    </AutoSizer>
   );
 };
