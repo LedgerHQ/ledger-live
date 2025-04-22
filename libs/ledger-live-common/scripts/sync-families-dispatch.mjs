4313f032bee1a8c284dfb2a343b67f3de9abee0a
Kant <quentin.jaccarino@ledger.fr>
2025-04-22 17:37:35 +0200

    chore: bring back zx v7 verbosity

--- libs/ledger-live-common/scripts/sync-families-dispatch.mjs
+++ libs/ledger-live-common/scripts/sync-families-dispatch.mjs
@@ -2,6 +2,8 @@
 import rimraf from "rimraf";
 import "zx/globals";
 
+$.verbose = true; // everything works like in v7
+
 if (os.platform() === "win32") {
   usePowerShell();
 }
@@ -18,7 +20,7 @@ const targets = [
   "mock.ts",
   "account.ts",
   "platformAdapter.ts",
-  "walletApiAdapter.ts"
+  "walletApiAdapter.ts",
 ];
 
 // Coins using coin-framework
