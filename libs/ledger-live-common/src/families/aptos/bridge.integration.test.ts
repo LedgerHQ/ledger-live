d2702e5c0556bf9c4d52f335440096b2d59f6096
scavassa-yld <gabriel.scavassa@yld.io>
2025-04-22 11:34:46 +0100

    feat: improving test

--- libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/aptos/bridge.integration.test.ts
@@ -1,11 +1,11 @@
 import "../../__tests__/test-helpers/setup";
-import { testBridge } from "../../__tests__/test-helpers/bridge";
-import { dataset } from "@ledgerhq/coin-aptos/test/index";
-
 jest.mock("blockies-ts", () => ({
   create: jest.fn(() => ({
     toDataURL: () => "data:image/png;base64,fakebase64string",
   })),
 }));
+
+import { testBridge } from "../../__tests__/test-helpers/bridge";
+import { dataset } from "@ledgerhq/coin-aptos/test/index";
 
 testBridge(dataset);
