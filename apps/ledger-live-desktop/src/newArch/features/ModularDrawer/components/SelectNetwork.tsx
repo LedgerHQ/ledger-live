98adc70de21ac0423e378ea2a7076e15e8fc8191
Elliott Coleman <elliott.coleman@yld.io>
2025-04-23 15:59:43 +0100

    feat(lld): select network component

---
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectNetwork.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { CryptoOrTokenCurrency } from "@ledgerhq/types-cryptoassets";
+import { NetworkList } from "@ledgerhq/react-ui/pre-ldls";
+import { track } from "~/renderer/analytics/segment";
+
+type SelectNetworkProps = {
+  networks: CryptoOrTokenCurrency[];
+  onNetworkSelected: (network: CryptoOrTokenCurrency) => void;
+  source: string;
+  flow: string;
+};
+
+export const SelectNetwork = ({
+  networks,
+  onNetworkSelected,
+  source,
+  flow,
+}: SelectNetworkProps) => {
+  useEffect(() => {
+    track("Modular Network Selection", { source, flow });
+  }, [source, flow]);
+
+  const onClick = (networkId: string) => {
+    track("network_clicked", { network: networkId, page: "Modular Asset Flow", flow });
+
+    const network = networks.find(({ id }) => id === networkId);
+    if (!network) return;
+
+    onNetworkSelected(network);
+  };
+
+  return <NetworkList networks={networks} onClick={onClick} />;
+};
