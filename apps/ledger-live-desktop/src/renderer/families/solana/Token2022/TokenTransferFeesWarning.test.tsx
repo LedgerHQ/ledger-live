c4695a0b21d75f47238237c6bd3dc3a04d20cf2a
François Guérin <francois.guerin@ledger.fr>
2025-04-29 18:39:17 +0200

    core(lld, llm): warn users sending SPLs with problematic extensions

---
+++ apps/ledger-live-desktop/src/renderer/families/solana/Token2022/TokenTransferFeesWarning.test.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { render, screen } from "tests/testSetup";
+import TokenTransferFeesWarning from "./TokenTransferFeesWarning";
+import { Transaction, SolanaTokenAccount } from "@ledgerhq/live-common/families/solana/types";
+
+describe("TokenTransferFeesWarning", () => {
+  it("displays a warning when the token embeds the transfer fee extension", () => {
+    render(
+      <TokenTransferFeesWarning
+        tokenAccount={
+          {
+            token: {
+              units: [{ code: "FAKE", magnitude: 8 }],
+            },
+          } as unknown as SolanaTokenAccount
+        }
+        transaction={
+          {
+            model: {
+              commandDescriptor: {
+                command: {
+                  kind: "token.transfer",
+                  extensions: {
+                    transferFee: {
+                      feeBps: 100,
+                      transferFee: 500,
+                    },
+                  },
+                },
+              },
+            },
+          } as unknown as Transaction
+        }
+      />,
+    );
+
+    expect(screen.queryByTestId("solana-token-transfer-fees-hint")).toHaveTextContent(
+      "Token transfer fee: 0.000005 FAKE",
+    );
+  });
+
+  it("does not display any warning when the token does not embed the transfer fee extension", () => {
+    render(
+      <TokenTransferFeesWarning
+        tokenAccount={
+          {
+            token: {
+              units: [{ code: "FAKE", magnitude: 8 }],
+            },
+          } as unknown as SolanaTokenAccount
+        }
+        transaction={
+          {
+            model: {
+              commandDescriptor: {
+                command: {
+                  kind: "token.transfer",
+                  extensions: {},
+                },
+              },
+            },
+          } as unknown as Transaction
+        }
+      />,
+    );
+
+    expect(screen.queryByTestId("solana-token-transfer-fees-hint")).toBeNull();
+  });
+});
