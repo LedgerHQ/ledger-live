f674982cc744246a8a43a7ca69286a07983f6e90
Pedro Semeano <semeano@gmail.com>
2025-04-30 09:14:42 +0100

    fix: add typeArguments to add stake payload

--- libs/coin-modules/coin-aptos/src/bridge/buildTransaction.ts
+++ libs/coin-modules/coin-aptos/src/bridge/buildTransaction.ts
@@ -57,7 +57,7 @@ const getPayload = (
       case "add":
         return {
           function: "0x1::delegation_pool::add_stake",
-          typeArguments: [],
+          typeArguments: [APTOS_ASSET_ID],
           functionArguments: [transaction.stake.poolAddr, transaction.amount.toString()],
         };
     }
