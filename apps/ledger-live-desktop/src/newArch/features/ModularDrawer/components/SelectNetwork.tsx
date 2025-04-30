c60814690c75633c2f662c3118ad62d3bb3b0fd5
Elliott Coleman <elliott.coleman@yld.io>
2025-04-30 11:31:16 +0100

    feat(lld): use TrackPage analytics

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectNetwork.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectNetwork.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { CryptoOrTokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { NetworkList } from "@ledgerhq/react-ui/pre-ldls";
 import { track } from "~/renderer/analytics/segment";
+import TrackPage from "~/renderer/analytics/TrackPage";
 
 type SelectNetworkProps = {
   networks: CryptoOrTokenCurrency[];
@@ -16,10 +17,6 @@ export const SelectNetwork = ({
   source,
   flow,
 }: SelectNetworkProps) => {
-  useEffect(() => {
-    track("Modular Network Selection", { source, flow });
-  }, [source, flow]);
-
   const onClick = (networkId: string) => {
     track("network_clicked", { network: networkId, page: "Modular Asset Flow", flow });
 
@@ -30,8 +27,11 @@ export const SelectNetwork = ({
   };
 
   return (
-    <div style={{ flex: "1 1 auto", width: "100%" }}>
-      <NetworkList networks={networks} onClick={onClick} />
-    </div>
+    <>
+      <TrackPage category={source} name="Modular Network Selection" flow={flow} />
+      <div style={{ flex: "1 1 auto", width: "100%" }}>
+        <NetworkList networks={networks} onClick={onClick} />
+      </div>
+    </>
   );
 };
