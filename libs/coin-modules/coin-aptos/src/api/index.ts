6938f88cdcb4e5b2fd62d5d9a963e876879ea5fc
Pedro Semeano <semeano@gmail.com>
2025-05-05 12:32:56 +0100

    chore: update alpaca api

--- libs/coin-modules/coin-aptos/src/api/index.ts
+++ libs/coin-modules/coin-aptos/src/api/index.ts
@@ -1,27 +1,32 @@
 import type { Api } from "@ledgerhq/coin-framework/api/index";
 import type { AptosConfig as AptosConfigApi } from "../config";
+import { Balance, FeeEstimation, Operation } from "@ledgerhq/coin-framework/api/types";
 import coinConfig from "../config";
 import type { AptosAsset } from "../types/assets";
-import {
-  broadcast as broadcastWrapper,
-  combine,
-  craftTransaction,
-  estimateFees,
-  getBalance,
-  lastBlock,
-  listOperations,
-} from "../logic";
+import { AptosAPI } from "../network";
 
 export function createApi(config: AptosConfigApi): Api<AptosAsset> {
   coinConfig.setCoinConfig(() => ({ ...config, status: { type: "active" } }));
 
+  const client = new AptosAPI(config.aptosSettings);
+
   return {
-    broadcast: tx => broadcastWrapper(config.aptosSettings, tx),
-    combine,
-    craftTransaction,
-    estimateFees,
-    getBalance,
-    lastBlock,
-    listOperations,
+    broadcast: (tx: string) => client.broadcast(tx),
+    combine: (_tx, _signature, _pubkey): string => {
+      throw new Error("Not Implemented");
+    },
+    craftTransaction: (_transactionIntent, _customFees): Promise<string> => {
+      throw new Error("Not Implemented");
+    },
+    estimateFees: (_transactionIntent): Promise<FeeEstimation<never>> => {
+      throw new Error("Not Implemented");
+    },
+    getBalance: (_address): Promise<Balance<AptosAsset>[]> => {
+      throw new Error("Not Implemented");
+    },
+    lastBlock: () => client.getLastBlock(),
+    listOperations: (_address, _pagination): Promise<[Operation<AptosAsset>[], string]> => {
+      throw new Error("Not Implemented");
+    },
   };
 }
