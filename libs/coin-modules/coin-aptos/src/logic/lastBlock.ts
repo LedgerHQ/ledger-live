7dd65c9ff7483a38303286487f5e50bbc3166703
Pedro Semeano <semeano@gmail.com>
2025-05-05 12:32:56 +0100

    chore: update alpaca api

--- libs/coin-modules/coin-aptos/src/logic/lastBlock.ts
+++
@@ -1,7 +0,0 @@
-import type { BlockInfo } from "@ledgerhq/coin-framework/api/index";
-import { AptosAPI } from "../network";
-
-export async function lastBlock(): Promise<BlockInfo> {
-  const client = new AptosAPI("aptos");
-  return await client.getLastBlock();
-}
