b61e1c54d3d4982bd81d0d465011c7233e17ebc8
Victor Alber <victor.alber@ledger.fr>
2025-05-06 16:10:58 +0200

    feat: test logs for earn live app

--- apps/ledger-live-desktop/src/renderer/screens/earn/index.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/earn/index.tsx
@@ -15,6 +15,7 @@ import {
   localeSelector,
 } from "~/renderer/reducers/settings";
 import { useDeepLinkListener } from "~/renderer/screens/earn/useDeepLinkListener";
+import { handlers as loggerHandlers } from "@ledgerhq/live-common/wallet-api/CustomLogger/server";
 
 const DEFAULT_EARN_APP_ID = "earn";
 
@@ -34,6 +35,16 @@ const Earn = () => {
   const stakeProgramsParam = useMemo(
     () => stakeProgramsToEarnParam(stakePrograms),
     [stakePrograms],
+  );
+
+  const customHandlers = useMemo(
+    () => ({
+      ...loggerHandlers,
+      "custom.isReady": async () => {
+        console.info("Earn Live App Loaded");
+      },
+    }),
+    [],
   );
 
   return (
@@ -59,6 +70,7 @@ const Earn = () => {
             OS: "web",
             stakeProgramsParam: stakeProgramsParam ? JSON.stringify(stakeProgramsParam) : undefined,
           }}
+          customHandlers={customHandlers}
         />
       ) : null}
     </Card>
