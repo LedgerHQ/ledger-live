fcabc4a651d3ddb2967f24755b5b00c869602918
Jérôme Prudent <jerome.prudent@ledger.fr>
2025-05-07 16:30:03 +0200

    fix(live-18576,coin-tron): retrieve oldest operations first
    - lama-adapter needs to fetch 15k+ operations on some accounts
    - this is about 150 calls made on trongrid where non of them should fail
    - looks like we missed a lot of operations trying to fetch all in one gulp
    - this commit let the possibility to fetch deepest operations first and
    still fixing a limit on the total number of operations
    - lama-adapter will be able to sync the whole set of operations by
      fetching bit by bit (1000 per 1000)

--- libs/coin-modules/coin-tron/src/logic/listOperations.ts
+++ libs/coin-modules/coin-tron/src/logic/listOperations.ts
@@ -8,18 +8,42 @@ import {
 import { fromTrongridTxInfoToOperation } from "../network/trongrid/trongrid-adapters";
 import { TronAsset } from "../types";
 
-const neverStop: FetchTxsContinuePredicate = () => true;
+export type Options = {
+  // the soft limit is an indicative number of transactions to fetch
+  // it is "soft" because it is not guaranteed that the number of transactions
+  // returned will be less than this number, but it is guaranteed to be:
+  //  <= 2 * softLimit (2 times that number)
+  softLimit: number;
+  minHeight: number;
+  order: "asc" | "desc";
+};
+
+export const defaultOptions: Options = {
+  softLimit: 1000,
+  minHeight: 0,
+  order: "desc",
+} as const;
 
 export async function listOperations(
   address: string,
-  minHeight: number,
+  options: Options,
 ): Promise<[Operation<TronAsset>[], string]> {
   // there is a possible optimisation here: when height is 0, set the minTimestamp to 0
-  const block = await getBlock(minHeight);
-  const fetchParams = block.time
-    ? { ...defaultFetchParams, minTimestamp: block.time?.getTime() }
-    : defaultFetchParams;
-  const txs = await fetchTronAccountTxs(address, neverStop, {}, fetchParams);
+  const block = await getBlock(options.minHeight);
+  const minTimestamp = block.time?.getTime() ?? defaultFetchParams.minTimestamp;
+  const fetchParams = {
+    ...defaultFetchParams,
+    minTimestamp: minTimestamp,
+    order: options.order,
+    hintGlobalLimit: options.softLimit,
+  };
+
+  // under the hood, the netwark fetches native transactions and trc20 transactions separately
+  // that same predicate is used to stop fetching both calls
+  // that's why we have a "soft" limit, with the guarantee of total number of transactions to be less than 2 * softLimit
+  const untilLimitReached: FetchTxsContinuePredicate = ops => ops.length < options.softLimit;
+
+  const txs = await fetchTronAccountTxs(address, untilLimitReached, {}, fetchParams);
   return [
     txs
       // TODO: adapt directly in network calls
