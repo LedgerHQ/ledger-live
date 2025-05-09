c8f87b7e7ceb0278f2a11a8b064481316a6beaaf
Elliott Coleman <elliott.coleman@yld.io>
2025-05-09 12:22:25 +0100

    feat(LLD): select account flow

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import SelectAccountAndCurrencyDrawer from "~/renderer/drawers/DataSelector/SelectAccountAndCurrencyDrawer";
+import React, { useState } from "react";
 
 import { CryptoOrTokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { AccountLike, Account } from "@ledgerhq/types-live";
 import { WalletAPIAccount } from "@ledgerhq/live-common/wallet-api/types";
 import { Observable } from "rxjs";
+import MemoizedSelectAssetFlow from "./SelectAssetFlow";
+import { SelectAccount } from "./SelectAccount";
+import styled from "styled-components";
 
 type SelectAccountDrawerProps = {
   onAccountSelected: (account: AccountLike, parentAccount?: Account) => void;
@@ -13,5 +15,31 @@ type SelectAccountDrawerProps = {
 };
 
 export function SelectAccountFlow(props: SelectAccountDrawerProps) {
-  return <SelectAccountAndCurrencyDrawer {...props} />;
+  const [selectedAsset, setSelectedAsset] = useState<undefined | CryptoOrTokenCurrency>();
+
+  const onAssetSelected = (asset: CryptoOrTokenCurrency) => {
+    setSelectedAsset(asset);
+  };
+
+  if (!selectedAsset) {
+    return (
+      <SelectorContent>
+        <MemoizedSelectAssetFlow onAssetSelected={onAssetSelected} {...props} />
+      </SelectorContent>
+    );
+  }
+
+  return (
+    <SelectorContent>
+      <SelectAccount {...props} asset={selectedAsset} source="source" flow="flow" />
+    </SelectorContent>
+  );
 }
+
+const SelectorContent = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  margin: 0px 16px 0px;
+  height: 100%;
+`;
