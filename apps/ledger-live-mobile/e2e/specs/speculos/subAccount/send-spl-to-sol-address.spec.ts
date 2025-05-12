35d5343bd2420d37f6831d4b214069f9109e9a57
aabbas-ledger <ayoub.abbas@ledger.fr>
2025-05-12 09:02:54 +0200

    added test files for llm subAccount

--- apps/ledger-live-mobile/e2e/specs/speculos/subAccount/send-spl-to-sol-address.spec.ts
+++
@@ -1,9 +0,0 @@
-import { runSendSPL } from "./subAccount";
-const transaction = new Transaction(
-  Account.SOL_GIGA_1,
-  Account.SOL_GIGA_2,
-  "0.5",
-  undefined,
-  "noTag",
-);
-runSendSPL(transaction, ["B2CQA-3055"]);
