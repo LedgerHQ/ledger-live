9cca21106cfda16a36b7df7c27a9181886c9925f
João Martins <joaomartinsfcup@gmail.com>
2025-05-13 17:51:09 +0100

    Saving past work.

--- apps/ledger-live-desktop/src/renderer/families/aptos/index.ts
+++ apps/ledger-live-desktop/src/renderer/families/aptos/index.ts
@@ -1,10 +1,13 @@
 import accountHeaderManageActions from "./AccountHeaderManageActions";
 import AccountBodyHeader from "./AccountBodyHeader";
+import AccountBalanceSummaryFooter from "./AccountBalanceSummaryFooter";
+
 import { AptosFamily } from "./types";
 
 const family: AptosFamily = {
   accountHeaderManageActions,
   AccountBodyHeader,
+  AccountBalanceSummaryFooter,
 };
 
 export default family;
