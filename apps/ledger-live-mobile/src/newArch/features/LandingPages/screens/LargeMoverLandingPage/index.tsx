d04bb8922ef25cde6dad346674d9262e2c3824c9
RobinVncnt <robin.vincent@ledger.fr>
2025-05-13 17:20:10 +0200

    error handling

--- apps/ledger-live-mobile/src/newArch/features/LandingPages/screens/LargeMoverLandingPage/index.tsx
+++ apps/ledger-live-mobile/src/newArch/features/LandingPages/screens/LargeMoverLandingPage/index.tsx
@@ -1,17 +1,20 @@
-import { Flex, InfiniteLoader, Text } from "@ledgerhq/native-ui";
+import { Flex } from "@ledgerhq/native-ui";
 import React, { useState, useMemo } from "react";
 import { useLargeMover } from "./hooks/useLargeMover";
 import getWindowDimensions from "~/logic/getWindowDimensions";
 import { Card } from "./components/Card";
 import { LandingPagesNavigatorParamList } from "~/components/RootNavigator/types/LandingPagesNavigator";
-import { ScreenName } from "~/const";
+import { NavigatorName, ScreenName } from "~/const";
 import { StackNavigatorProps } from "~/components/RootNavigator/types/helpers";
 import { StickyHeader } from "./components/StickyHeader";
 import { SafeAreaView } from "react-native";
 import { useTheme } from "styled-components/native";
 import { rangeMap } from "./utils";
 import { SwiperComponent } from "~/newArch/components/Swiper/components/Swiper";
-
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { WalletTabNavigatorStackParamList } from "~/components/RootNavigator/types/WalletTabNavigator";
+import { LoadingIndicator } from "./components/Loading";
+import { CardType } from "./types";
 type LargeMoverLandingPageProps = StackNavigatorProps<
   LandingPagesNavigatorParamList,
   ScreenName.LargeMoverLandingPage
@@ -21,10 +24,12 @@ export const LargeMoverLandingPage = ({ route }: LargeMoverLandingPageProps) => 
   const { currencyIds, initialRange = "day" } = route.params;
   const currencyIdsArray = currencyIds.toUpperCase().split(",");
   const initialKeyRange = rangeMap[initialRange];
-  const { range, setRange, currencies, loading } = useLargeMover({
+  const { range, setRange, currencies, loading, isError } = useLargeMover({
     currencyIdsArray,
     initialKeyRange,
   });
+
+  const navigation = useNavigation<NavigationProp<WalletTabNavigatorStackParamList>>();
 
   const { colors } = useTheme();
   const height = getWindowDimensions().height * 0.75;
@@ -33,43 +38,53 @@ export const LargeMoverLandingPage = ({ route }: LargeMoverLandingPageProps) => 
   const currenciesWithId = useMemo(
     () =>
       currencies.map((currency, index) => ({
-        ...currency,
+        data: currency.data,
+        chartData: currency.chartData,
         idCard: index,
+        range,
+        setRange,
       })),
-    [currencies],
+    [currencies, range, setRange],
   );
 
-  if (loading) {
-    return (
-      <Flex height={height} width="100%" justifyContent="center" alignItems="center">
-        <InfiniteLoader color="primary.c50" size={38} />
-      </Flex>
-    );
-  }
+  const renderCard = (card: CardType) => {
+    if (!card.data || !card.chartData) {
+      return null;
+    }
 
-  if (currencies.length === 0) {
     return (
-      <Flex>
-        <Text>{"No data available"}</Text>
-      </Flex>
+      <Card
+        data={card.data}
+        chartData={card.chartData}
+        range={range}
+        setRange={setRange}
+        height={height}
+      />
     );
+  };
+
+  if (isError) {
+    navigation.navigate(NavigatorName.Market, {
+      screen: ScreenName.MarketList,
+      params: { top100: true },
+    });
   }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: colors.neutral.c00 }}>
       <StickyHeader />
-      <Flex paddingTop={20}>
-        {currencies[0].data && currencies[0].chartData ? (
+      <Flex paddingTop={40}>
+        {loading ? (
+          <LoadingIndicator height={height} />
+        ) : (
           <SwiperComponent
             cardContainerStyle={{ justifyContent: "flex-start" }}
             currentIndex={currentIndex}
             onIndexChange={setCurrentIndex}
             initialCards={currenciesWithId}
-            renderCard={card => (
-              <Card {...card.data!} chartData={card.chartData!} range={range} setRange={setRange} />
-            )}
+            renderCard={renderCard}
           />
-        ) : null}
+        )}
       </Flex>
     </SafeAreaView>
   );
