bbf37896983690f1a5d2136033ea44b9be36a204
Elliott Coleman <elliott.coleman@yld.io>
2025-05-13 14:13:41 +0100

    feat(LLD): suggestions

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow/hooks/useDetailedAccounts.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow/hooks/useDetailedAccounts.tsx
@@ -7,15 +7,15 @@ import { Account, TokenAccount } from "@ledgerhq/types-live";
 import { CryptoOrTokenCurrency, Currency } from "@ledgerhq/types-cryptoassets";
 import { WalletAPIAccount } from "@ledgerhq/live-common/wallet-api/types";
 import { useGetAccountIds } from "@ledgerhq/live-common/wallet-api/react";
-import { formatCurrencyUnit } from "@ledgerhq/coin-framework/lib-es/currencies/formatCurrencyUnit";
-import { getTagDerivationMode } from "@ledgerhq/coin-framework/lib-es/derivation";
+import { formatCurrencyUnit } from "@ledgerhq/coin-framework/currencies/formatCurrencyUnit";
+import { getTagDerivationMode } from "@ledgerhq/coin-framework/derivation";
 
 import {
   getBalanceHistoryWithCountervalue,
   getPortfolioCount,
-} from "@ledgerhq/live-countervalues/lib-es/portfolio";
+} from "@ledgerhq/live-countervalues/portfolio";
 import { useCountervaluesState } from "@ledgerhq/live-countervalues-react";
-import { CounterValuesState } from "@ledgerhq/live-countervalues/lib-es/types";
+import { CounterValuesState } from "@ledgerhq/live-countervalues/types";
 
 import { getAccountTuplesForCurrency } from "~/renderer/components/PerCurrencySelectAccount/state";
 import { accountsSelector } from "~/renderer/reducers/accounts";
@@ -23,9 +23,11 @@ import { counterValueCurrencySelector } from "~/renderer/reducers/settings";
 
 import { Account as DetailedAccount } from "@ledgerhq/react-ui/pre-ldls/index";
 
+const formatAddress = (address: string) => `${address.slice(0, 5)}...${address.slice(-5)}`;
+
 const formatDetailedAccount = (
   account: Account | TokenAccount,
-  parentSeedIdentifier: string,
+  parentAddress: string,
   state: CounterValuesState,
   to: Currency,
 ): DetailedAccount => {
@@ -42,7 +44,7 @@ const formatDetailedAccount = (
     showCode: true,
   });
 
-  const address = `${parentSeedIdentifier.slice(0, 5)}...${parentSeedIdentifier.slice(-5)}`;
+  const address = formatAddress(parentAddress);
 
   return { name, id, ticker, balance, fiatValue, address };
 };
@@ -61,26 +63,17 @@ export const useDetailedAccounts = (
   return useMemo(
     () =>
       accounts.flatMap(({ account }) => {
-        const currencyAccount = formatDetailedAccount(account, account.seedIdentifier, state, to);
+        const currencyAccount = formatDetailedAccount(account, account.freshAddress, state, to);
 
-        const acc: DetailedAccount[] = [];
-
-        if (account.subAccounts?.length) {
-          account.subAccounts.map(subAccount => {
-            const tokenAccount = formatDetailedAccount(
-              subAccount,
-              account.seedIdentifier,
-              state,
-              to,
-            );
-
-            acc.push(tokenAccount);
-          });
-        }
+        const subAccounts = account.subAccounts?.length
+          ? account.subAccounts.map(subAccount =>
+              formatDetailedAccount(subAccount, account.freshAddress, state, to),
+            )
+          : [];
 
         const protocol = getTagDerivationMode(account.currency, account.derivationMode) || "";
 
-        return [{ ...currencyAccount, protocol }, ...acc];
+        return [{ ...currencyAccount, protocol }, ...subAccounts];
       }),
     [accounts, state, to],
   );
