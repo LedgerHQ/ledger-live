34c081aef73249ac43368cb29d5cd2bf3df7a1be
Paulo Oliveira <paulo.oliveira-ext@ledger.fr>
2025-05-14 15:18:10 +0100

    test: add validations on lastBlock result properties

--- libs/coin-modules/coin-aptos/src/__tests__/api/index.integ.test.ts
+++ libs/coin-modules/coin-aptos/src/__tests__/api/index.integ.test.ts
@@ -1,4 +1,4 @@
-import { Network } from "@aptos-labs/ts-sdk";
+import { Hex, Network } from "@aptos-labs/ts-sdk";
 import { createApi } from "../../api";
 
 describe("createApi", () => {
@@ -7,9 +7,18 @@ describe("createApi", () => {
   describe("lastBlock", () => {
     it("returns the last block information", async () => {
       const lastBlock = await api.lastBlock();
+
       expect(lastBlock).toHaveProperty("hash");
+      expect(Hex.isValid(lastBlock.hash ?? "").valid).toBeTruthy();
+
       expect(lastBlock).toHaveProperty("height");
+      expect(lastBlock.height).toBeGreaterThan(0);
+
       expect(lastBlock).toHaveProperty("time");
+      expect(lastBlock.time).not.toBeUndefined();
+      expect(lastBlock.time?.getFullYear()).toBeGreaterThan(0);
+      expect(lastBlock.time?.getMonth()).toBeGreaterThan(0);
+      expect(lastBlock.time?.getDay()).toBeGreaterThan(0);
     });
   });
 });
