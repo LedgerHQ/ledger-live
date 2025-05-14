6d483faca8ca54709d99b03dc9a8df60c4d2ca58
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-05-14 14:48:00 +0200

    rebase + fix

--- libs/ledger-live-common/src/bridge/generic-alpaca/alpaca/index.ts
+++ libs/ledger-live-common/src/bridge/generic-alpaca/alpaca/index.ts
@@ -5,7 +5,8 @@ import { getNetworkAlpacaApi } from "./network/network-alpaca";
 import { Api } from "@ledgerhq/coin-framework/api/types";
 import { XrpCoinConfig } from "@ledgerhq/coin-xrp/config";
 
-export function getAlpacaApi(network: string, kind: "local" | "remote"): Api<any> {
+// NOTE: Using Api<any, any, any> to allow integration with coin-specific APIs like XRP (which use structured sender types).
+export function getAlpacaApi(network: string, kind: "local" | "remote"): Api<any, any, any> {
   if (kind === "local") {
     // dynamic import ?
     switch (network) {
@@ -13,7 +14,8 @@ export function getAlpacaApi(network: string, kind: "local" | "remote"): Api<any
         return createXrpApi(
           getCurrencyConfiguration<XrpCoinConfig>(getCryptoCurrencyById("ripple")),
         );
+      // as unknown as Api<any>; // FIXME: createXrpApi returns a strongly typed Api<XrpSender>, fix Api<any> to allow it
     }
   }
-  return getNetworkAlpacaApi(network) satisfies Partial<Api<any>> as Api<any>;
+  return getNetworkAlpacaApi(network) satisfies Partial<Api<any, any, any>> as Api<any, any, any>;
 }
