e07ff1281e89cf314b0f50ceaa8732ee54c43325
Elliott Coleman <elliott.coleman@yld.io>
2025-05-15 11:12:52 +0100

    feat(LLD): select account constants

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow/components/SelectAccount.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/components/SelectAccountFlow/components/SelectAccount.tsx
@@ -5,6 +5,7 @@ import { Box, Flex } from "@ledgerhq/react-ui/index";
 import { Account, AccountLike, TokenAccount } from "@ledgerhq/types-live";
 import { AccountList, Account as DetailedAccount } from "@ledgerhq/react-ui/pre-ldls/index";
 import { AccountTuple } from "~/renderer/components/PerCurrencySelectAccount/state";
+import { ACCOUNT_PAGE, EXTRA_BOTTOM_MARGIN } from "./constants";
 
 type SelectAccountProps = {
   onAccountSelected: (account: AccountLike, parentAccount?: Account) => void;
@@ -13,13 +14,6 @@ type SelectAccountProps = {
   flow: string;
   detailedAccounts: DetailedAccount[];
 };
-
-const CURRENT_PAGE = "Modular Account Selection";
-const ROW_HEIGHT = 64;
-const SPACING = 8;
-const TWO_ROWS_HEIGHT = 2 * ROW_HEIGHT;
-const INPUT_HEIGHT = 40;
-const EXTRA_BOTTOM_MARGIN = TWO_ROWS_HEIGHT + INPUT_HEIGHT + SPACING;
 
 export const SelectAccount = ({
   detailedAccounts,
@@ -65,7 +59,7 @@ export const SelectAccount = ({
 
   return (
     <Box style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-      <TrackPage category={source} name={CURRENT_PAGE} flow={flow} />
+      <TrackPage category={source} name={ACCOUNT_PAGE} flow={flow} />
       <Flex
         style={{
           flex: "1",
