1dadf97809e2c9f7d4dd0318995645d1b0fa63b7
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-05-16 14:28:53 +0200

    fix: moved getTransactionExplorer to common space

--- .changeset/sour-teachers-joke.md
+++
@@ -1,8 +0,0 @@
----
-"@ledgerhq/coin-hedera": patch
-"ledger-live-desktop": patch
-"live-mobile": patch
----
-
-fix "view in explorer" button with unconfirmed hedera transactions
-support custom getTransactionExplorer in ledger-live-mobile
--- apps/ledger-live-mobile/src/families/hedera/operationDetails.ts
+++ apps/ledger-live-mobile/src/families/hedera/operationDetails.ts
@@ -1,15 +1,3 @@
-import { ExplorerView } from "@ledgerhq/types-cryptoassets";
-import { Operation } from "@ledgerhq/types-live";
-
-import { HederaOperationExtra } from "@ledgerhq/live-common/families/hedera/types";
-
-const getTransactionExplorer = (
-  explorerView: ExplorerView | null | undefined,
-  operation: Operation,
-): string | undefined => {
-  const extra = operation.extra as HederaOperationExtra;
-
-  return explorerView?.tx?.replace("$hash", extra.consensusTimestamp ?? extra.transactionId ?? "0");
-};
+import { getTransactionExplorer } from "@ledgerhq/live-common/families/hedera/logic";
 
 export default { getTransactionExplorer };
