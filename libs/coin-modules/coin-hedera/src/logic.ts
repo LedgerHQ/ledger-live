52e34fc0ca98917e8ac2846524828f1f0aaf701d
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-05-16 14:28:53 +0200

    fix: moved getTransactionExplorer to common space

--- libs/coin-modules/coin-hedera/src/logic.ts
+++ libs/coin-modules/coin-hedera/src/logic.ts
@@ -1,7 +1,7 @@
 import { ExplorerView } from "@ledgerhq/types-cryptoassets";
 import { Operation } from "@ledgerhq/types-live";
 
-import { HederaOperationExtra } from "@ledgerhq/live-common/families/hedera/types";
+import { HederaOperationExtra } from "./types";
 
 const getTransactionExplorer = (
   explorerView: ExplorerView | null | undefined,
@@ -12,4 +12,4 @@ const getTransactionExplorer = (
   return explorerView?.tx?.replace("$hash", extra.consensusTimestamp ?? extra.transactionId ?? "0");
 };
 
-export default getTransactionExplorer;
+export { getTransactionExplorer };
