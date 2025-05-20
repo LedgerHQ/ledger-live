4cb27078642f5ae5f296547f2ecf12d68d43eac4
cgrellard-ledger <come.grellard@ledger.fr>
2025-05-20 17:43:31 +0200

    :art: fix: move function to utils

--- e2e/desktop/tests/specs/rename.account.spec.ts
+++ e2e/desktop/tests/specs/rename.account.spec.ts
@@ -1,16 +1,12 @@
 import { test } from "../fixtures/common";
-import fsPromises from "fs/promises";
 import { expect } from "@playwright/test";
 import { Account } from "@ledgerhq/live-common/e2e/enum/Account";
 import { addTmsLink } from "../utils/allureUtils";
 import { getDescription } from "../utils/customJsonReporter";
 import { CLI } from "../utils/cliUtils";
+import { getUserdata } from "../utils/userdata";
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-const getUserdata = async (userdataFile: string) => {
-  const jsonFile = await fsPromises.readFile(userdataFile, "utf-8");
-  return JSON.parse(jsonFile);
-};
 
 const accounts = [{ account: Account.ATOM_1, xrayTicket: "B2CQA-2996" }];
 
@@ -51,7 +47,6 @@ for (const account of accounts) {
         await app.account.expectAccountVisibility(newName);
         await sleep(2000);
         await electronApp.close();
-        await sleep(2000);
         const userData = await getUserdata(userdataFile);
         expect(userData.data.wallet.accountNames[0][1]).toBe(newName);
       },
