0c5070ba6effeacbcb6a009d86738c39a9a831d2
François Guérin <francois.guerin@ledger.fr>
2025-05-22 14:07:53 +0200

    feat(coin-solana): warn correctly on not enough gas

--- libs/coin-modules/coin-solana/src/prepareTransaction.ts
+++ libs/coin-modules/coin-solana/src/prepareTransaction.ts
@@ -4,10 +4,11 @@ import {
   InvalidAddress,
   InvalidAddressBecauseDestinationIsAlsoSource,
   NotEnoughBalance,
+  NotEnoughGas,
   RecipientRequired,
 } from "@ledgerhq/errors";
 import type { Account } from "@ledgerhq/types-live";
-import { findSubAccountById } from "@ledgerhq/coin-framework/account/index";
+import { findSubAccountById, getFeesUnit } from "@ledgerhq/coin-framework/account/index";
 import { ChainAPI } from "./network";
 import {
   getMaybeMintAccount,
@@ -88,6 +89,7 @@ import {
   VersionedTransaction,
 } from "@solana/web3.js";
 import BigNumber from "bignumber.js";
+import { formatCurrencyUnit } from "@ledgerhq/coin-framework/currencies/formatCurrencyUnit";
 
 async function deriveCommandDescriptor(
   mainAccount: SolanaAccount,
@@ -319,7 +321,15 @@ const deriveTokenTransferCommandDescriptor = async (
   const { fee, spendable: spendableSol } = await estimateFeeAndSpendable(api, mainAccount, tx);
 
   if (spendableSol.lt(assocAccRentExempt) || spendableSol.isZero()) {
-    errors.fee = new NotEnoughBalance();
+    const query = new URLSearchParams({
+      ...(mainAccount?.id ? { account: mainAccount.id } : {}),
+    });
+    errors.gasPrice = new NotEnoughGas(undefined, {
+      fees: formatCurrencyUnit(getFeesUnit(mainAccount.currency), new BigNumber(fee)),
+      ticker: mainAccount.currency.ticker,
+      cryptoName: mainAccount.currency.name,
+      links: [`ledgerlive://buy?${query.toString()}`],
+    });
   }
 
   if (!tx.useAllAmount && tx.amount.lte(0)) {
