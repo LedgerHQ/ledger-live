94496878e7ef45134e2adfdcc5f312aec0a553a8
RobinVncnt <robin.vincent@ledger.fr>
2025-05-23 16:49:59 +0200

    split marge for buttons and delete $ for volume

--- libs/ui/packages/native/src/components/cta/QuickAction/QuickActionList/index.tsx
+++ libs/ui/packages/native/src/components/cta/QuickAction/QuickActionList/index.tsx
@@ -19,15 +19,16 @@ const QuickActionList = ({
 }: QuickActionListProps): React.ReactElement => {
   const renderItem = useCallback(
     ({ item, index }: { item: QuickActionButtonProps; index: number }) => {
+      const isNotLastColumn = (index + 1) % numColumns > 0;
+      const isNotLastItem = data && index !== data.length - 1;
+      const hasButtonToItsRight = isNotLastColumn && isNotLastItem;
+      const hasRowBelow = data?.length && index + numColumns < data.length;
+
+      const mr = hasButtonToItsRight ? 4 : 0;
+      const mb = hasRowBelow ? 4 : 0;
       return (
         <View style={{ flex: 1, minHeight: 30 }}>
-          <QuickActionButton
-            {...item}
-            isActive={isActive}
-            mr={(index + 1) % numColumns > 0 && data && index !== data.length - 1 ? 4 : 0}
-            mb={data?.length && index + numColumns < data.length ? 4 : 0}
-            testID={testID}
-          />
+          <QuickActionButton {...item} isActive={isActive} mr={mr} mb={mb} testID={testID} />
         </View>
       );
     },
