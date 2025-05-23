561f56c15befc09dd104c24bfe1e5d4064f10fb9
Monika Wiercinska <monika.wiercinska@yld.io>
2025-05-23 13:04:38 +0100

    chore: add tests

---
+++ apps/ledger-live-mobile/src/screens/PTX/BuySell/CustomLoaderBuySell.test.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { BUY_SELL_UI_APP_ID } from "@ledgerhq/live-common/wallet-api/constants";
+import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
+import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
+import { screen, act } from "@testing-library/react-native";
+import { render } from "@tests/test-renderer";
+import { CustomLoaderBuySell } from "./CustomLoaderBuySell";
+
+jest.useFakeTimers();
+
+jest.mock("@ledgerhq/live-common/featureFlags/useFeature", () => jest.fn());
+const mockedUseFeature = useFeature as jest.Mock;
+
+describe("CustomLoaderBuySell", () => {
+  beforeEach(() => {
+    mockedUseFeature.mockReturnValue({
+      enabled: true,
+      params: { durationMs: 300 },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    jest.clearAllTimers();
+  });
+
+  it("should not render when buySellLoader feature flag is disabled", () => {
+    mockedUseFeature.mockReturnValue({ enabled: false });
+    render(<CustomLoaderBuySell isLoading={true} manifest={getMockManifest({})} />);
+
+    expect(screen.queryByTestId("custom-buy-sell-loader")).toBeNull();
+  });
+
+  it("should not render when app is internal Buy/Sell", () => {
+    render(
+      <CustomLoaderBuySell
+        isLoading={true}
+        manifest={getMockManifest({ mockInternalApp: true })}
+      />,
+    );
+
+    expect(screen.queryByTestId("custom-buy-sell-loader")).toBeNull();
+  });
+
+  it("should not render when isLoading is false", () => {
+    render(<CustomLoaderBuySell isLoading={false} manifest={getMockManifest({})} />);
+
+    expect(screen.queryByTestId("custom-buy-sell-loader")).toBeNull();
+  });
+
+  it("should render correctly and persist for the duration specified in buySellLoader feature flag", () => {
+    const { rerender } = render(
+      <CustomLoaderBuySell isLoading={true} manifest={getMockManifest({})} />,
+    );
+
+    expect(screen.getByTestId("custom-buy-sell-loader")).toBeTruthy();
+    expect(screen.getByText("Connecting you to Moonpay")).toBeTruthy();
+
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    rerender(<CustomLoaderBuySell isLoading={false} manifest={getMockManifest({})} />);
+
+    expect(screen.getByTestId("custom-buy-sell-loader")).toBeTruthy();
+
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    expect(screen.getByTestId("custom-buy-sell-loader")).toBeTruthy();
+
+    act(() => {
+      jest.advanceTimersByTime(100);
+    });
+
+    expect(screen.queryByTestId("custom-buy-sell-loader")).toBeNull();
+  });
+
+  it("should persist over the set duration while isLoading is true", () => {
+    render(<CustomLoaderBuySell isLoading={true} manifest={getMockManifest({})} />);
+
+    expect(screen.getByTestId("custom-buy-sell-loader")).toBeTruthy();
+
+    act(() => {
+      jest.advanceTimersByTime(500);
+    });
+
+    expect(screen.getByTestId("custom-buy-sell-loader")).toBeTruthy();
+  });
+});
+
+const getMockManifest = ({ mockInternalApp }: { mockInternalApp?: boolean }): LiveAppManifest => {
+  return {
+    id: mockInternalApp ? BUY_SELL_UI_APP_ID : "moonpay",
+    name: "Moonpay",
+    url: "",
+    homepageUrl: "",
+    platforms: [],
+    apiVersion: "^2.0.0",
+    manifestVersion: "2",
+    branch: "experimental",
+    categories: [],
+    currencies: [],
+    content: {
+      shortDescription: {
+        en: "",
+      },
+      description: {
+        en: "",
+      },
+    },
+    permissions: [],
+    domains: [],
+    visibility: "complete",
+  };
+};
