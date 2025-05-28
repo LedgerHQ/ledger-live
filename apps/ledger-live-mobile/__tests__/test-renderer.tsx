a7192f0e1d579e9911fa5efd855ce6ab6ad05869
RobinVncnt <robin.vincent@ledger.fr>
2025-05-28 11:11:33 +0200

    fix tutorial storage

--- apps/ledger-live-mobile/__tests__/test-renderer.tsx
+++ apps/ledger-live-mobile/__tests__/test-renderer.tsx
@@ -38,7 +38,7 @@ import { initialState as WALLET_INITIAL_STATE } from "@ledgerhq/live-wallet/stor
 import QueuedDrawersContextProvider from "LLM/components/QueuedDrawer/QueuedDrawersContextProvider";
 import { INITIAL_STATE as TRUSTCHAIN_INITIAL_STATE } from "@ledgerhq/ledger-key-ring-protocol/store";
 import CustomLiveAppProvider from "./CustomLiveAppProvider";
-import { LARGE_MOVER_INITIAL_STATE } from "~/reducers/LargeMover";
+import { LARGE_MOVER_INITIAL_STATE } from "~/reducers/largeMover";
 
 const initialState = {
   accounts: ACCOUNTS_INITIAL_STATE,
