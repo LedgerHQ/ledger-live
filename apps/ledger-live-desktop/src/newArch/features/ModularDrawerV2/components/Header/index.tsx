2ca1377dfce293910c04bf13a7c5b1114d321521
Lucas Werey <lucas.werey@ledger.fr>
2025-05-28 21:00:55 +0200

    :lipstick: (ui) polish assetItem to avoid overflow

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawerV2/components/Header/index.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawerV2/components/Header/index.tsx
@@ -4,19 +4,12 @@ import styled from "styled-components";
 import { Title } from "./Title";
 import { ModularDrawerStep, MODULAR_DRAWER_STEP } from "../../types";
 import { track } from "~/renderer/analytics/segment";
-import { AnimatePresence, motion } from "framer-motion";
 
-type Props =
-  | {
-      step: ModularDrawerStep;
-      hidden: true;
-      onBackClick?: () => void;
-    }
-  | {
-      step: ModularDrawerStep;
-      hidden?: false | undefined;
-      onBackClick: () => void;
-    };
+type Props = {
+  step: ModularDrawerStep;
+  hidden?: boolean;
+  onBackClick?: () => void;
+};
 
 const TranslationKeyMap: Record<ModularDrawerStep, string> = {
   [MODULAR_DRAWER_STEP.ASSET_SELECTION]: "modularAssetDrawer.selectAsset",
@@ -28,25 +21,14 @@ export const Header = ({ step, hidden, onBackClick }: Props) => {
   const handleBackClick = () => {
     track("button_clicked", {
       step,
-      button: "ModularDrawerBackButton",
+      button: "modularDrawer_backButton",
     });
-    onBackClick?.();
+    onBackClick();
   };
 
   return (
     <HeaderContainer>
-      <AnimatePresence mode="wait">
-        <motion.span
-          key={`back-button-${step}`}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.15 }}
-          data-testid="select-asset-drawer-back-button"
-        >
-          <NavigateBackButton hidden={hidden} onClick={handleBackClick} />
-        </motion.span>
-      </AnimatePresence>
+      <NavigateBackButton hidden={hidden} onClick={handleBackClick} />
       <Title translationKey={TranslationKeyMap[step]} />
     </HeaderContainer>
   );
