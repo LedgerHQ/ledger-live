187d32c555949ffaae5298860c2b528ceb9ccbaf
RobinVncnt <robin.vincent@ledger.fr>
2025-05-28 11:11:33 +0200

    fix tutorial storage

--- apps/ledger-live-mobile/src/reducers/largeMover.ts
+++ apps/ledger-live-mobile/src/reducers/largeMover.ts
@@ -1,5 +1,5 @@
 import { handleActions } from "redux-actions";
-import type { ReducerMap } from "redux-actions";
+import type { Action, ReducerMap } from "redux-actions";
 
 export type LargeMoverState = {
   tutorial: boolean;
@@ -13,13 +13,13 @@ export const LARGE_MOVER_INITIAL_STATE: LargeMoverState = {
   tutorial: true,
 };
 
-const handlers: ReducerMap<LargeMoverState, boolean> = {
-  [LargeMoverActionTypes.SET_TUTORIAL]: (_state, action) => ({
-    tutorial: action.payload,
+const handlers: ReducerMap<LargeMoverState, LargeMoverState> = {
+  [LargeMoverActionTypes.SET_TUTORIAL]: (state, action) => ({
+    tutorial: (action as Action<LargeMoverState>).payload.tutorial,
   }),
 };
 
 export const tutorialSelector = (state: { largeMover: LargeMoverState }) =>
   state.largeMover.tutorial;
 
-export default handleActions<LargeMoverState, boolean>(handlers, LARGE_MOVER_INITIAL_STATE);
+export default handleActions<LargeMoverState, LargeMoverState>(handlers, LARGE_MOVER_INITIAL_STATE);
