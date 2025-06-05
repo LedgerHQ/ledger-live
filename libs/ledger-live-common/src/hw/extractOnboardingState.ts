c7e1b2ccbc0ae645e777f430b7ef62a6c34a8fa6
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-06-05 17:15:10 +0200

    🐛 (onboarding): Don't display Backup step after seeding with Recovery Key

--- libs/ledger-live-common/src/hw/extractOnboardingState.ts
+++ libs/ledger-live-common/src/hw/extractOnboardingState.ts
@@ -62,9 +62,9 @@ const fromBitsToOnboardingStep = new Map<number, OnboardingStep>([
   [0xe, OnboardingStep.SetupChoiceRestore],
   [0xf, OnboardingStep.OnboardingEarlyCheck],
   [0x10, OnboardingStep.RestoreRecoveryKey],
-  [RECOVERY_KEY_STEP_BIT_MASK + 0x0, OnboardingStep.BackupRecoveryKey], // default state, after boot, if no backup was pending
+  [RECOVERY_KEY_STEP_BIT_MASK + 0x0, OnboardingStep.Ready], // default state, after boot, if no backup was pending, this is also the state right after the device is seeded (if it was seeded with Recovery Key)
   [RECOVERY_KEY_STEP_BIT_MASK + 0x1, OnboardingStep.Ready], // backup fully refused
-  [RECOVERY_KEY_STEP_BIT_MASK + 0x2, OnboardingStep.BackupRecoveryKey], // backup not started or fully refused // TODO: probably change to BackupRecoveryKey
+  [RECOVERY_KEY_STEP_BIT_MASK + 0x2, OnboardingStep.BackupRecoveryKey], // backup not started or fully refused, this is the state right after the device is seeded (unless it was seeded with Recovery Key)
   [RECOVERY_KEY_STEP_BIT_MASK + 0x3, OnboardingStep.BackupRecoveryKey], // backup process started but not finished
   [RECOVERY_KEY_STEP_BIT_MASK + 0x4, OnboardingStep.BackupRecoveryKey], // backup done on RK and naming not finished
   [RECOVERY_KEY_STEP_BIT_MASK + 0x5, OnboardingStep.Ready], // backup done on RK and backup-process exited
