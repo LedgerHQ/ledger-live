3bef817da07c47d40b9357711f5022118c899f26
qperrot <quentin.perrot-minnot@ledger.fr>
2025-06-12 15:20:19 +0200

    Revert "Merge pull request #10425 from LedgerHQ/feat/address-sanctioned-swap"
    This reverts commit a137f2f4b19b2904d872a5b48b746abed0eb8014, reversing
    changes made to 348725a08352903a0a3e3ef8c0bc008b3d92a383.

--- libs/ledger-live-common/src/exchange/swap/completeExchange.ts
+++ libs/ledger-live-common/src/exchange/swap/completeExchange.ts
@@ -25,8 +25,6 @@ import { CompleteExchangeStep, convertTransportError } from "../error";
 import type { CompleteExchangeInputSwap, CompleteExchangeRequestEvent } from "../platform/types";
 import { convertToAppExchangePartnerKey, getSwapProvider } from "../providers";
 import { CEXProviderConfig } from "../providers/swap";
-import { isAddressSanctioned } from "../../sanction";
-import { RecipientAddressSanctionedError } from "../../sanction/errors";
 
 const COMPLETE_EXCHANGE_LOG = "SWAP-CompleteExchange";
 
@@ -66,19 +64,6 @@ const completeExchange = (
         const payoutCurrency = getAccountCurrency(toAccount);
         const refundCurrency = getAccountCurrency(fromAccount);
         const mainRefundCurrency = getAccountCurrency(refundAccount);
-
-        const isPayoutSanctioned = await isAddressSanctioned(
-          payoutAccount.currency,
-          payoutAccount.freshAddress,
-        );
-        const isRefundSanctioned = await isAddressSanctioned(
-          refundAccount.currency,
-          refundAccount.freshAddress,
-        );
-
-        if (isPayoutSanctioned || isRefundSanctioned) {
-          throw new RecipientAddressSanctionedError();
-        }
         if (mainPayoutCurrency.type !== "CryptoCurrency")
           throw new Error("This should be a cryptocurrency");
         if (mainRefundCurrency.type !== "CryptoCurrency")
