101af48a95c4c18185d45e7db5950ed342dd17de
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-06-12 16:35:18 +0200

    feat: fix getaccountinfo

--- libs/coin-modules/coin-xrp/src/logic/getAccountInfo.ts
+++ libs/coin-modules/coin-xrp/src/logic/getAccountInfo.ts
@@ -2,6 +2,6 @@ import { getAccountInfo as networkGetAccountInfo } from "../network";
 import { AccountInfo } from "../types";
 
 export async function getAccountInfo(address: string): Promise<AccountInfo> {
-  const accountInfo = await networkGetAccountInfo(address);
+  const accountInfo = await networkGetAccountInfo(address, true);
   return accountInfo;
 }
