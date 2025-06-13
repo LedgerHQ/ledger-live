e35f0d307adf7ec514b1a7d605580464bb62e2f4
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-06-13 21:55:03 +0200

    feat: ReceiveWithAssociationModal based on default Receive modal

---
+++ apps/ledger-live-desktop/src/renderer/families/hedera/ReceiveWithAssociationModal/steps/StepAccountFooter.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Trans } from "react-i18next";
+import { getReceiveFlowError } from "@ledgerhq/live-common/account/index";
+
+import Button from "~/renderer/components/Button";
+import type { StepId, StepProps } from "../Body";
+
+export function StepAccountFooter({
+  transitionTo,
+  isAssociationFlow,
+  receiveTokenMode,
+  token,
+  account,
+  parentAccount,
+  status,
+}: StepProps) {
+  const error = account ? getReceiveFlowError(account, parentAccount) : null;
+  const isMissingToken = receiveTokenMode && !token;
+  const isTransactionError = Object.keys(status.errors).length > 0;
+
+  const redirectToDeviceStep = () => {
+    const deviceStepId: StepId = isAssociationFlow ? "associationDevice" : "device";
+    transitionTo(deviceStepId);
+  };
+
+  return (
+    <Button
+      primary
+      data-testid="modal-continue-button"
+      disabled={!account || isMissingToken || !!error || isTransactionError}
+      onClick={redirectToDeviceStep}
+    >
+      <Trans i18nKey="common.continue" />
+    </Button>
+  );
+}
