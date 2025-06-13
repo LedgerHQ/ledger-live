6b8a1eb33744427b6bb69719fbc337b6452d9f99
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-06-13 12:38:15 +0200

    Revert ":bug: (llc): Unexpected DMK disconnection can occur during OpenApp"
    This reverts commit 3deee8b1266795f88fa2d2e31fb0b2eaf5bb9a1a.

--- libs/ledger-live-common/src/hw/connectAppEventMapper.ts
+++ libs/ledger-live-common/src/hw/connectAppEventMapper.ts
@@ -11,9 +11,9 @@ import type {
 } from "@ledgerhq/device-management-kit";
 import {
   DeviceActionStatus,
-  DeviceDisconnectedWhileSendingError,
   DeviceLockedError,
   DeviceSessionStateType,
+  DeviceStatus,
   UserInteractionRequired,
   OutOfMemoryDAError,
   SecureChannelError,
@@ -96,7 +96,9 @@ export class ConnectAppEventMapper {
       return;
     }
 
-    if (
+    if (deviceState.deviceStatus === DeviceStatus.NOT_CONNECTED) {
+      this.eventSubject.next({ type: "disconnected", expected: false });
+    } else if (
       deviceState.firmwareVersion?.metadata &&
       deviceState.firmwareUpdateContext &&
       !this.lastSeenDeviceSent
@@ -257,8 +259,6 @@ export class ConnectAppEventMapper {
       this.eventSubject.complete();
     } else if (error instanceof SecureChannelError) {
       this.eventSubject.error(new UserRefusedAllowManager());
-    } else if (error instanceof DeviceDisconnectedWhileSendingError) {
-      this.eventSubject.next({ type: "disconnected", expected: false });
     } else if ("_tag" in error && error._tag === "WebHidSendReportError") {
       this.eventSubject.next({ type: "disconnected", expected: false });
       this.eventSubject.complete();
