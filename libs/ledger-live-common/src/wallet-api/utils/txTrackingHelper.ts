bf5e39a95070b1518a25d98e99017b109fc7459d
adammino-ledger <andrea.dammino-ext@ledger.fr>
2025-06-24 15:11:03 +0100

    refactor function to not use Object.keys

--- libs/ledger-live-common/src/wallet-api/utils/txTrackingHelper.ts
+++ libs/ledger-live-common/src/wallet-api/utils/txTrackingHelper.ts
@@ -1,8 +1,14 @@
 import { Transaction as EvmTransaction } from "@ledgerhq/coin-evm/types/transaction";
 import { DAPP_SELECTORS } from "@ledgerhq/hw-app-eth";
 
-export const getTxType = (tx: EvmTransaction) => {
-  const txSelector = `0x${tx?.data?.toString("hex").substring(0, 8)}`;
+const fallback = "transfer";
 
-  return Object.keys(DAPP_SELECTORS).includes(txSelector) ? DAPP_SELECTORS[txSelector] : "transfer";
+export const getTxType = (tx: EvmTransaction): string => {
+  if (!tx || !tx.data) return fallback;
+
+  const txSelector = `0x${tx.data.toString("hex").substring(0, 8)}`;
+  const type = DAPP_SELECTORS[txSelector];
+
+  if (!type) return fallback;
+  return type;
 };
