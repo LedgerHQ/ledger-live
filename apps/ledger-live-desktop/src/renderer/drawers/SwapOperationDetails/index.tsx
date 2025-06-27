63b1ef438e0984b0b966fb5c6d11f59842ccdd10
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-06-27 15:20:53 +0200

    feat: squash all changes for hedera swaps

--- apps/ledger-live-desktop/src/renderer/drawers/SwapOperationDetails/index.tsx
+++ apps/ledger-live-desktop/src/renderer/drawers/SwapOperationDetails/index.tsx
@@ -4,7 +4,10 @@ import { AdditionalProviderConfig } from "@ledgerhq/live-common/exchange/provide
 import { isSwapOperationPending } from "@ledgerhq/live-common/exchange/swap/index";
 import { MappedSwapOperation } from "@ledgerhq/live-common/exchange/swap/types";
 import { getProviderName } from "@ledgerhq/live-common/exchange/swap/utils/index";
-import { getDefaultExplorerView, getTransactionExplorer } from "@ledgerhq/live-common/explorers";
+import {
+  getDefaultExplorerView,
+  getTransactionExplorer as getDefaultTransactionExplorer,
+} from "@ledgerhq/live-common/explorers";
 import { Account, TokenAccount } from "@ledgerhq/types-live";
 import uniq from "lodash/uniq";
 import React, { useCallback, useEffect, useState } from "react";
@@ -30,6 +33,7 @@ import {
   OpDetailsSection,
   OpDetailsTitle,
 } from "~/renderer/drawers/OperationDetails/styledComponents";
+import { getLLDCoinFamily } from "~/renderer/families";
 import { useAccountUnit } from "~/renderer/hooks/useAccountUnit";
 import { dayFormat, useDateFormatted } from "~/renderer/hooks/useDateFormatter";
 import useTheme from "~/renderer/hooks/useTheme";
@@ -128,11 +132,19 @@ const SwapOperationDetails = ({
   const { t } = useTranslation();
   const mainCurrency =
     fromCurrency.type === "CryptoCurrency" ? fromCurrency : fromCurrency.parentCurrency;
+
+  const specific = mainCurrency ? getLLDCoinFamily(mainCurrency.family) : null;
+  const getTransactionExplorer = specific?.getTransactionExplorer;
+
+  let url: string | null | undefined;
   //Temporary feature before adding history to swap live app
-  const url =
-    provider === "lifi"
-      ? "https://scan.li.fi/tx/$hash".replace("$hash", operation.hash)
-      : getTransactionExplorer(getDefaultExplorerView(mainCurrency), operation.hash);
+  if (provider === "lifi") {
+    url = "https://scan.li.fi/tx/$hash".replace("$hash", operation.hash);
+  } else if (getTransactionExplorer) {
+    url = getTransactionExplorer(getDefaultExplorerView(mainCurrency), operation);
+  } else {
+    url = getDefaultTransactionExplorer(getDefaultExplorerView(mainCurrency), operation.hash);
+  }
 
   useEffect(() => {
     const getProvideData = async () => {
