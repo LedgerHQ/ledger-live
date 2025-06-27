6ae35969bb93c80ce443be157b68252787abc578
Marco Toniut <marco.toniut-ext@ledger.fr>
2025-06-27 18:54:06 +0100

    Hide the “stop scan” button if no account has been scanned yet

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/ScanAccounts/components/Footer/index.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/ScanAccounts/components/Footer/index.tsx
@@ -1,31 +1,41 @@
 import { Button, Flex, Icons } from "@ledgerhq/react-ui";
+import { Account } from "@ledgerhq/types-live";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 export type FooterProps = {
+  handleConfirm: () => void;
+  importableAccounts: Account[];
   scanning: boolean;
   selectedIds: string[];
   stopSubscription: () => void;
-  handleConfirm: () => void;
 };
 
-export const Footer = ({ scanning, selectedIds, stopSubscription, handleConfirm }: FooterProps) => {
+export const Footer = ({
+  handleConfirm,
+  importableAccounts,
+  scanning,
+  selectedIds,
+  stopSubscription,
+}: FooterProps) => {
   const { t } = useTranslation();
 
   return (
     <Flex justifyContent="flex-end">
       {scanning ? (
-        <Button
-          alignItems="center"
-          flex={1}
-          Icon={<Icons.Pause />}
-          iconPosition="left"
-          onClick={() => stopSubscription()}
-          size="xl"
-          variant="main"
-        >
-          {t("modularAssetDrawer.scanAccounts.cta.stopScanning")}
-        </Button>
+        importableAccounts.length === 0 ? null : (
+          <Button
+            alignItems="center"
+            flex={1}
+            Icon={<Icons.Pause />}
+            iconPosition="left"
+            onClick={() => stopSubscription()}
+            size="xl"
+            variant="main"
+          >
+            {t("modularAssetDrawer.scanAccounts.cta.stopScanning")}
+          </Button>
+        )
       ) : (
         <Button
           alignItems="center"
