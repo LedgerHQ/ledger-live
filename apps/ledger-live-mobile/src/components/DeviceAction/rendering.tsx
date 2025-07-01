658abe7e90c119e52ed9dd44f807bb30e47475b3
Antoine Blaise <antoine.blaise@ledger.fr>
2025-07-01 15:03:33 +0200

    feat:deprecate device

--- apps/ledger-live-mobile/src/components/DeviceAction/rendering.tsx
+++ apps/ledger-live-mobile/src/components/DeviceAction/rendering.tsx
@@ -64,6 +64,10 @@ import ModalLock from "../ModalLock";
 import ProviderIcon from "../ProviderIcon";
 import { RootStackParamList } from "../RootNavigator/types/RootNavigator";
 import TermsFooter, { TermsProviders } from "../TermsFooter";
+import {
+  DeviceDeprecationScreen,
+  DeviceDeprecationScreens,
+} from "./Screen/DeviceDeprecationScreen";
 
 export const Wrapper = styled(Flex).attrs({
   flex: 1,
@@ -624,6 +628,7 @@ export function renderError({
   Icon,
   iconColor,
   device,
+  coinName = "",
   hasExportLogButton,
 }: RawProps & {
   navigation?: StackNavigationProp<RootStackParamList>;
@@ -633,6 +638,7 @@ export function renderError({
   Icon?: React.ComponentProps<typeof GenericErrorView>["Icon"];
   iconColor?: string;
   device?: Device;
+  coinName?: string;
   hasExportLogButton?: boolean;
 }) {
   const onPress = () => {
@@ -661,12 +667,22 @@ export function renderError({
   if (error instanceof LockedDeviceError) {
     return renderLockedDeviceError({ t, onRetry, device });
   }
-
   // TODO Once we have the aligned Error renderings, the CTA list should be determined
   // by the error class, not patched like here.
   let showRetryIfAvailable = true;
   if (error instanceof PeerRemovedPairing) {
     showRetryIfAvailable = false;
+  }
+  if (error.message === "device-deprecation") {
+    return (
+      <DeviceDeprecationScreen
+        coinName={coinName}
+        date={new Date()}
+        onContinue={() => {}}
+        productName={getDeviceModel(device!.modelId)?.productName}
+        screenName={DeviceDeprecationScreens.errorScreen}
+      />
+    );
   }
 
   return (
