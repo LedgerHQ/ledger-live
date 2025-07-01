737c4e54121b532786d54413101b71d36c502f7a
Antoine Blaise <antoine.blaise@ledger.fr>
2025-07-01 15:03:33 +0200

    feat:deprecate device

--- libs/live-dmk-shared/src/device-action/ConnectApp/types.ts
+++ libs/live-dmk-shared/src/device-action/ConnectApp/types.ts
@@ -8,32 +8,80 @@ import type {
   InstallPlan,
 } from "@ledgerhq/device-management-kit";
 import { UserInteractionRequired } from "@ledgerhq/device-management-kit";
-
+import { DeviceModelId as LLDeviceModelId } from "@ledgerhq/types-devices";
 export type ConnectAppDerivation = string;
 
 export type ConnectAppDAOutput = {
   readonly deviceMetadata?: GetDeviceMetadataDAOutput;
   readonly installResult?: InstallOrUpdateAppsDAOutput;
   readonly derivation?: ConnectAppDerivation;
+  readonly deviceDeprecation?: DeviceDeprecationPayload;
 };
 
 export type ConnectAppDAInput = OpenAppWithDependenciesDAInput & {
   readonly allowMissingApplication: boolean;
   readonly requiredDerivation?: () => Promise<ConnectAppDerivation>;
+  readonly deprecationConfig?: DeviceDeprecated;
 };
 
-export type ConnectAppDAError = OpenAppWithDependenciesDAError;
+export type DeviceDeprecationConfig = {
+  exception?: string[];
+  deprecatedFlow?: string[];
+};
+
+export type DeviceDeprecationPayload = {
+  warningScreenVisible: boolean;
+  clearSigningScreenVisible: boolean;
+  errorScreenVisible: boolean;
+  modelId: LLDeviceModelId;
+  date: Date;
+  warningScreenConfig?: DeviceDeprecationConfig;
+  clearSigningScreenConfig?: DeviceDeprecationConfig;
+  errorScreenConfig?: DeviceDeprecationConfig;
+  onContinue: (isError?: boolean) => void;
+};
+
+type DeprecationScreen = {
+  date: string;
+  deprecatedFlow: string[];
+  exception?: string[];
+};
+
+export type DeviceDeprecatedEntry = {
+  deviceModelId: string;
+  infoScreen: DeprecationScreen;
+  errorScreen: DeprecationScreen;
+  warningClearSigningScreen: DeprecationScreen;
+};
+
+export type DeviceDeprecated = DeviceDeprecatedEntry[];
+
+export class DeviceDeprecationError extends Error {
+  readonly _tag = "DeviceDeprecationError" as const;
+  constructor() {
+    super("device-deprecation");
+    this.name = "DeviceDeprecationError";
+  }
+}
+
+export type ConnectAppDAError = OpenAppWithDependenciesDAError | DeviceDeprecationError;
+
+export enum UserInteractionRequiredLL {
+  DeviceDeprecation = "device-deprecation",
+}
 
 export type ConnectAppDARequiredInteraction =
   | UserInteractionRequired.None
   | UserInteractionRequired.UnlockDevice
   | UserInteractionRequired.AllowSecureConnection
   | UserInteractionRequired.AllowListApps
+  | UserInteractionRequiredLL.DeviceDeprecation
   | OpenAppDARequiredInteraction;
 
 export type ConnectAppDAIntermediateValue = {
   requiredUserInteraction: ConnectAppDARequiredInteraction;
   installPlan: InstallPlan | null;
+  deviceDeprecation: DeviceDeprecationPayload | undefined;
 };
 
 export type ConnectAppDAState = DeviceActionState<
