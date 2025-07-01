70b614b6fe554929b4bd8cceab04db86e0a8eeae
Antoine Blaise <antoine.blaise@ledger.fr>
2025-07-01 15:03:33 +0200

    feat:deprecate device

--- apps/ledger-live-mobile/src/reducers/types.ts
+++ apps/ledger-live-mobile/src/reducers/types.ts
@@ -291,6 +291,7 @@ export type SettingsState = {
   fromLedgerSyncOnboarding: boolean;
   mevProtection: boolean;
   selectedTabPortfolioAssets: TabPortfolioAssetsType;
+  deprecationDoNotRemind: string[];
 };
 
 export type NotificationsSettings = {
