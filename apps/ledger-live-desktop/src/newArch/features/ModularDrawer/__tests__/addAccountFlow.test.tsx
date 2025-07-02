609b5fdfac912be4d508e2f81e5c76adbcffdad9
Elliott Coleman <elliott.coleman@yld.io>
2025-07-02 09:55:16 +0100

    feat(LLD): wip add account tests

---
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/__tests__/addAccountFlow.test.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+import { mockDomMeasurements } from "./shared";
+import { liveConfig } from "@ledgerhq/live-common/config/sharedConfig";
+import { LiveConfig } from "@ledgerhq/live-config/LiveConfig";
+import { bitcoinCurrency } from "../__mocks__/useSelectAssetFlow.mock";
+import { ARB_ACCOUNT, ETH_ACCOUNT } from "../__mocks__/accounts.mock";
+import BigNumber from "bignumber.js";
+import ModularDrawerAddAccountFlowManager from "../ModularDrawerAddAccountFlowManager";
+import { Provider } from "react-redux";
+import createStore from "~/renderer/createStore";
+import { render, screen } from "tests/testSetup";
+import { Account } from "@ledgerhq/types-live";
+import { act } from "react-dom/test-utils";
+import { State } from "~/renderer/reducers";
+
+beforeEach(async () => {
+  mockDomMeasurements();
+  LiveConfig.setConfig(liveConfig);
+});
+
+jest.mock("~/renderer/hooks/useConnectAppAction", () => ({
+  __esModule: true,
+  default: () => ({
+    useHook: () => {
+      const [locked, setLocked] = React.useState(true);
+
+      return {
+        device: { modelId: "stax" },
+        onResult: () => true,
+        isLocked: locked,
+        onRetry: () => {
+          setLocked(false);
+        },
+      };
+    },
+    mapResult: () => ({ device: { deviceId: 123456 } }),
+  }),
+}));
+
+let triggerNext: (account: Account) => void = () => null;
+let triggerComplete: () => void = () => null;
+
+jest.mock("@ledgerhq/live-common/bridge/index", () => ({
+  __esModule: true,
+  getCurrencyBridge: () => ({
+    scanAccounts: () => ({
+      subscribe: ({
+        next,
+        complete,
+      }: {
+        next: ({ account }: { account: Account }) => void;
+        complete: () => void;
+      }) => {
+        triggerNext = account => next({ account });
+        triggerComplete = () => complete();
+      },
+    }),
+    preload: () => true,
+    hydrate: () => true,
+  }),
+}));
+
+describe("ModularDrawerAddAccountFlowManager", () => {
+  it("should find an account and add it", async () => {
+    const { user } = render(
+      <Provider store={createStore({ state: undefined })}>
+        <ModularDrawerAddAccountFlowManager currency={bitcoinCurrency} source="MADSource" />
+      </Provider>,
+    );
+
+    expect(screen.getByText(/your device is locked/i)).toBeInTheDocument();
+    await user.click(screen.getByText(/retry/i));
+
+    expect(
+      screen.getByText(/looking for any existing accounts on the blockchain/i),
+    ).toBeInTheDocument();
+
+    act(() => triggerNext(ETH_ACCOUNT));
+    act(() => triggerComplete());
+    expect(screen.getByText(/we found 1 account/i)).toBeInTheDocument();
+
+    await user.click(screen.getByRole("button", { name: "Confirm" }));
+    expect(screen.getByText(/account added to your portfolio/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText(/close/i));
+  });
+
+  it("should create an account", async () => {
+    const { user } = render(
+      <Provider store={createStore({ state: undefined })}>
+        <ModularDrawerAddAccountFlowManager currency={bitcoinCurrency} source="MADSource" />
+      </Provider>,
+    );
+
+    await user.click(screen.getByText(/retry/i));
+    act(() => triggerNext(newAccount));
+    act(() => triggerComplete());
+
+    expect(screen.getByText(/new account/i)).toBeInTheDocument();
+    expect(screen.queryByText(/we found 1 account/i)).not.toBeInTheDocument();
+    await user.click(screen.getByRole("button", { name: "Confirm" }));
+
+    expect(screen.getByText(/account added to your portfolio/i)).toBeInTheDocument();
+
+    await user.click(screen.getByText(/close/i));
+  });
+
+  it("should hide previously added accounts and show new account", async () => {
+    const { user } = render(
+      <Provider store={createStore({ state: { accounts: [ARB_ACCOUNT] } as State })}>
+        <ModularDrawerAddAccountFlowManager currency={bitcoinCurrency} source="MADSource" />
+      </Provider>,
+    );
+
+    await user.click(screen.getByText(/retry/i));
+    act(() => triggerNext(ARB_ACCOUNT));
+    act(() => triggerNext(newAccount));
+    act(() => triggerComplete());
+
+    expect(screen.getByText(/new account/i)).toBeInTheDocument();
+    expect(screen.queryByText(/we found 1 account/i)).not.toBeInTheDocument();
+
+    const confirm = screen.getByRole("button", { name: "Confirm" });
+    expect(confirm).toBeDisabled();
+
+    await user.click(screen.getByRole("checkbox"));
+    expect(confirm).not.toBeDisabled();
+
+    await user.click(confirm);
+    expect(screen.getByText(/account added to your portfolio/i)).toBeInTheDocument();
+  });
+});
+
+const newAccount = {
+  type: "Account" as const,
+  id: "js:2:arbitrum:0xf97c24B5e0951db821b68CF7ce0e2F7BC9D93921:",
+  seedIdentifier:
+    "04618fc4d1ddca7549312fbb8d7f5567028e9023f633cb388f1e61473b1aa07e1871d31e20d544f1f4fd59b56d8ce0c4845010dde8b4504d28db650a5c4d1ed1a6",
+  freshAddress: "0xf97c24B5e0951db821b68CF7ce0e2F7BC9D93921",
+  freshAddressPath: "44'/60'/1'/0/0",
+  derivationMode: "" as const,
+  used: false,
+  index: 1,
+  currency: {
+    type: "CryptoCurrency" as const,
+    id: "arbitrum" as const,
+    coinType: 60,
+    name: "Arbitrum",
+    managerAppName: "Ethereum",
+    ticker: "ETH",
+    scheme: "arbitrum",
+    color: "#28a0f0",
+    family: "evm",
+    units: [
+      {
+        name: "ETH",
+        code: "ETH",
+        magnitude: 18,
+      },
+    ],
+    ethereumLikeInfo: {
+      chainId: 42161,
+    },
+    explorerViews: [],
+  },
+  operationsCount: 0,
+  operations: [],
+  swapHistory: [],
+  pendingOperations: [],
+  lastSyncDate: new Date(),
+  creationDate: new Date(),
+  balance: new BigNumber("0"),
+  spendableBalance: new BigNumber("0"),
+  blockHeight: 353794765,
+  balanceHistoryCache: {
+    HOUR: {
+      balances: [],
+      latestDate: 1751540400000,
+    },
+    DAY: {
+      balances: [],
+      latestDate: 1751497200000,
+    },
+    WEEK: {
+      balances: [],
+      latestDate: 1751151600000,
+    },
+  },
+  syncHash: "0x87394383",
+  subAccounts: [],
+  nfts: [],
+};
