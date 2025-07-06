4a97b151af7a0587f129fe00ac7ff4fae2f63283
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-07-07 00:56:34 +0200

    fix change

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/customHandlers/index.ts
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/customHandlers/index.ts
@@ -43,116 +43,12 @@ export function useSwapCustomHandlers(
     [navigation],
   );
 
-<<<<<<< HEAD
   const walletAPISwapHandlers = useCustomExchangeHandlers({
     manifest,
     accounts,
     onCompleteResult: navigateToSwapPendingOperation,
     sendAppReady: sendSwapLiveAppReady,
   });
-=======
-  const walletAPISwapHandlers = useMemo<WalletAPICustomHandlers>(() => {
-    return {
-      ...exchangeHandlers({
-        accounts,
-        tracking,
-        manifest,
-        uiHooks: {
-          "custom.exchange.start": ({ exchangeParams, onSuccess, onCancel }) => {
-            navigation.navigate(NavigatorName.PlatformExchange, {
-              screen: ScreenName.PlatformStartExchange,
-              params: {
-                request: {
-                  ...exchangeParams,
-                  exchangeType: ExchangeType[exchangeParams.exchangeType],
-                },
-                onResult: result => {
-                  if (result.startExchangeError) {
-                    onCancel(
-                      result.startExchangeError.error,
-                      result.startExchangeError.device || device,
-                    );
-                  }
-
-                  if (result.startExchangeResult) {
-                    setDevice(result.device);
-                    onSuccess(
-                      result.startExchangeResult.nonce,
-                      result.startExchangeResult.device || device,
-                    );
-                  }
-
-                  navigation.pop();
-                },
-              },
-            });
-          },
-          "custom.exchange.complete": ({ exchangeParams, onSuccess, onCancel }) => {
-            navigation.navigate(NavigatorName.PlatformExchange, {
-              screen: ScreenName.PlatformCompleteExchange,
-              params: {
-                request: {
-                  exchangeType: exchangeParams.exchangeType,
-                  provider: exchangeParams.provider,
-                  exchange: exchangeParams.exchange,
-                  transaction: exchangeParams.transaction,
-                  binaryPayload: exchangeParams.binaryPayload,
-                  signature: exchangeParams.signature,
-                  feesStrategy: exchangeParams.feesStrategy,
-                  amountExpectedTo: exchangeParams.amountExpectedTo,
-                },
-                device,
-                onResult: result => {
-                  if (result.error) {
-                    onCancel(result.error);
-                    navigation.pop();
-                    navigation.navigate(NavigatorName.CustomError, {
-                      screen: ScreenName.CustomErrorScreen,
-                      params: {
-                        error: result.error,
-                        displayError: result.error instanceof AddressesSanctionedError,
-                      },
-                    });
-                  }
-                  if (result.operation) {
-                    navigation.pop();
-                    navigation.navigate(ScreenName.SwapPendingOperation, {
-                      swapOperation: {
-                        provider: exchangeParams.provider,
-                        swapId: exchangeParams.swapId!,
-                        status: "pending",
-                        receiverAccountId: exchangeParams.transaction.recipient,
-                        operationId: result.operation.hash,
-                        fromAmount: exchangeParams.transaction.amount,
-                        toAmount: BigNumber(exchangeParams.amountExpectedTo!),
-                      },
-                    });
-                    onSuccess(result.operation.hash);
-                  }
-                  setDevice(undefined);
-                },
-              },
-            });
-          },
-          "custom.isReady": async () => {
-            if (Config.DETOX) {
-              sendSwapLiveAppReady();
-            }
-          },
-          "custom.exchange.error": ({ error }) => {
-            navigation.navigate(NavigatorName.CustomError, {
-              screen: ScreenName.CustomErrorScreen,
-              params: {
-                error,
-                displayError: error instanceof AddressesSanctionedError,
-              },
-            });
-          },
-        },
-      }),
-    };
-  }, [accounts, device, manifest, navigation, tracking]);
->>>>>>> 64a6a296fc (fix swap)
 
   const swapCustomHandlers = {
     "custom.getFee": getFee(accounts, navigation),
