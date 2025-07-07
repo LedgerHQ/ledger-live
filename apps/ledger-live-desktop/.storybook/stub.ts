924b01e95c97add44a41165ef15521de34c91378
Elliott Coleman <elliott.coleman@yld.io>
2025-07-07 13:45:17 +0100

    test(LLD): connect your device mock

--- apps/ledger-live-desktop/.storybook/stub.ts
+++ apps/ledger-live-desktop/.storybook/stub.ts
@@ -2,4 +2,4 @@ export default () => {};
 
 export const ipcRenderer = () => {};
 
-export const ModularDrawerAddAccountFlowManager = () => null; // TODO: This is a temporary patch to get storybook to work. This should be replaced by appropriate mocks of ModularDrawerAddAccountFlowManager's dependencies. Ticket LIVE-19799
+// export const ModularDrawerAddAccountFlowManager = () => null; // TODO: This is a temporary patch to get storybook to work. This should be replaced by appropriate mocks of ModularDrawerAddAccountFlowManager's dependencies. Ticket LIVE-19799
