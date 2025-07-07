19913d69d881612dec88dd8de2da32f4a453d891
Elliott Coleman <elliott.coleman@yld.io>
2025-07-07 15:45:10 +0100

    test(LLD): mock scan account progression

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/ScanAccounts/ScanAccounts.stories.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/ScanAccounts/ScanAccounts.stories.tsx
@@ -4,8 +4,23 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { bitcoinCurrency } from "../../__mocks__/useSelectAssetFlow.mock";
 import { legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
+import { triggerComplete, triggerNext } from "../../__mocks__/bridge.mock";
+import { BTC_ACCOUNT } from "../../__mocks__/accounts.mock";
+import BigNumber from "bignumber.js";
 
-const store = createStore(() => ({}));
+const detailedAccount = {
+  balance: new BigNumber(31918),
+  creationDate: "2024-12-10T09:27:22.000Z",
+  currency: bitcoinCurrency,
+  derivationMode: "native_segwit",
+  freshAddress: "mocked_fresh_address",
+  id: "bitcoin1",
+  type: "Account",
+};
+
+const store = createStore(() => ({
+  accounts: [detailedAccount],
+}));
 
 const meta: Meta<typeof ScanAccounts> = {
   title: "ModularDrawer/ScanAccounts",
@@ -16,6 +31,8 @@ const meta: Meta<typeof ScanAccounts> = {
   decorators: [
     Story => (
       <Provider store={store}>
+        <button onClick={() => triggerNext(BTC_ACCOUNT)}>trigger next</button>
+        <button onClick={() => triggerComplete()}>trigger complete</button>
         <Story />
       </Provider>
     ),
