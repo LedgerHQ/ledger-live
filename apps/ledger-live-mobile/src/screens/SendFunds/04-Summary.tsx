9131950822e953120f7a7346236298ef32cf29c6
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-07-08 10:25:44 +0200

    fix tests

--- apps/ledger-live-mobile/src/screens/SendFunds/04-Summary.tsx
+++ apps/ledger-live-mobile/src/screens/SendFunds/04-Summary.tsx
@@ -292,7 +292,7 @@ function SendSummary({ navigation, route }: Props) {
             bridgePending ||
             !!transactionError ||
             (!!error && error instanceof NotEnoughGas) ||
-            displayedError
+            !!displayedError
           }
           pending={bridgePending}
         />
