8a17e0a2eaab3d42a53b3f8cf489dc9d37761d80
Martin Cayuelas <martin.cayuelas@ledger.fr>
2025-07-09 14:07:27 +0200

    feat(😈): handle asset and network selection

---
+++ apps/ledger-live-mobile/src/newArch/features/ModularDrawer/hooks/__tests__/useProviders.test.ts
@@ -0,0 +1,21 @@
+import { renderHook } from "@tests/test-renderer";
+import { getProvider, useProviders } from "../useProviders";
+import { ethereumCurrency } from "../../__mocks__/currencies.mock";
+import { providers } from "../../__mocks__/useGroupedCurrenciesByProvider.mock";
+
+describe("getProvider", () => {
+  it("should return the provider containing the given currency", () => {
+    const result = getProvider(ethereumCurrency, providers);
+    expect(result).toBe(providers[1]);
+  });
+});
+
+describe("useProviders", () => {
+  it("getNetworksFromProvider should return correct network ids", () => {
+    const { result } = renderHook(() => useProviders());
+    const provider = providers[1]; //ETH
+    const ids = ["ethereum", "arbitrum"];
+    const networks = result.current.getNetworksFromProvider(provider, ids);
+    expect(networks).toEqual(ids);
+  });
+});
