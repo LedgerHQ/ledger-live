cd986bfe72b1053bad1b754ca699097896840e2f
bayo.makanjuola <bayo.makanjuola@ledger.com>
2025-07-09 09:39:28 +0100

    update: remove comments and format

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/AccountSelection/components/AddAccountButton.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/screens/AccountSelection/components/AddAccountButton.tsx
@@ -1,10 +1,9 @@
+import type { ComponentProps } from "react";
 import React from "react";
 import { CardButton } from "@ledgerhq/react-ui/pre-ldls/index";
 import { Icons } from "@ledgerhq/react-ui/index";
 import { useTranslation } from "react-i18next";
 
-// Omit onClick and title from CardButton props
-import type { ComponentProps } from "react";
 type CardButtonProps = Omit<ComponentProps<typeof CardButton>, "onClick" | "title">;
 
 type Props = {
