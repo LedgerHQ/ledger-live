35274fb9ef07105fc36eb9ab432c8b3cfef4c00c
Pedro Semeano <semeano@gmail.com>
2025-07-10 14:56:06 +0100

    feat: staking for aptos

--- libs/ledger-live-common/src/e2e/families/aptos.ts
+++ libs/ledger-live-common/src/e2e/families/aptos.ts
@@ -1,7 +1,17 @@
-import { pressBoth, pressUntilTextFound } from "../speculos";
+import expect from "expect";
+import { pressBoth, pressUntilTextFound, waitFor, containsSubstringInEvent } from "../speculos";
 import { DeviceLabels } from "../enum/DeviceLabels";
+import { Delegate } from "../models/Delegate";
 
 export async function sendAptos() {
   await pressUntilTextFound(DeviceLabels.APPROVE);
+  await pressBoth();
+}
+
+export async function delegateAptos(delegatingAccount: Delegate) {
+  await waitFor(DeviceLabels.REVIEW_TRANSACTION);
+  const events = await pressUntilTextFound(DeviceLabels.APPROVE);
+  const isAmountCorrect = containsSubstringInEvent(delegatingAccount.amount, events);
+  expect(isAmountCorrect).toBeTruthy();
   await pressBoth();
 }
