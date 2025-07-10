5c39ed22c1dbcee5df9999543b50123c3d8a58fc
Pedro Semeano <semeano@gmail.com>
2025-07-10 14:56:06 +0100

    feat: staking for aptos

---
+++ apps/ledger-live-desktop/src/renderer/families/aptos/UnstakingFlowModal/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from "react";
+import Modal from "~/renderer/components/Modal";
+import Body from "./Body";
+import { StepId } from "./types";
+
+export default function UnstakingModal() {
+  const [stepId, setStepId] = useState<StepId>("amount");
+
+  const onHide = useCallback(() => {
+    setStepId("amount");
+  }, []);
+
+  const onChange = useCallback((id: StepId) => {
+    setStepId(id);
+  }, []);
+
+  const isModalLocked = ["device", "confirmation"].includes(stepId);
+
+  return (
+    <Modal
+      name="MODAL_APTOS_UNSTAKE"
+      centered
+      onHide={onHide}
+      preventBackdropClick={isModalLocked}
+      render={({ onClose, data }) => (
+        <Body
+          account={data.account}
+          stepId={stepId}
+          onClose={onClose}
+          onChangeStepId={onChange}
+          validatorAddress={data.validatorAddress}
+        />
+      )}
+    />
+  );
+}
---
+++ apps/ledger-live-desktop/src/renderer/families/aptos/shared/components/ValidatorRow.tsx
@@ -0,0 +1,128 @@
+import { formatCurrencyUnit } from "@ledgerhq/coin-framework/currencies/formatCurrencyUnit";
+import { getAddressExplorer, getDefaultExplorerView } from "@ledgerhq/live-common/explorers";
+import { AptosValidator } from "@ledgerhq/live-common/families/aptos/types";
+import { CryptoCurrency, Unit } from "@ledgerhq/types-cryptoassets";
+import BigNumber from "bignumber.js";
+import React, { useCallback } from "react";
+import { Trans } from "react-i18next";
+import styled from "styled-components";
+import Box from "~/renderer/components/Box";
+import ValidatorRow from "~/renderer/components/Delegation/ValidatorRow";
+import Text from "~/renderer/components/Text";
+import ValidatorIcon from "~/renderer/families/aptos/shared/components/ValidatorIcon";
+import Check from "~/renderer/icons/Check";
+import { openURL } from "~/renderer/linking";
+
+type Props = {
+  currency: CryptoCurrency;
+  validator: AptosValidator;
+  active?: boolean;
+  onClick: (a: { address: string }) => void;
+  unit: Unit;
+};
+
+function AptosValidatorRow({ validator, active, onClick, unit, currency }: Readonly<Props>) {
+  const explorerView = getDefaultExplorerView(currency);
+
+  const onExternalLink = useCallback(
+    (address: string) => {
+      const srURL = explorerView && getAddressExplorer(explorerView, address);
+      if (srURL) openURL(srURL);
+    },
+    [explorerView],
+  );
+
+  return (
+    <StyledValidatorRow
+      onClick={onClick}
+      key={validator.address}
+      validator={{
+        address: validator.address,
+      }}
+      icon={<ValidatorIcon validatorAddress={validator.address} />}
+      title={validator.address}
+      onExternalLink={onExternalLink}
+      unit={unit}
+      sideInfo={
+        <Box
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <Box
+            style={{
+              flexDirection: "column",
+            }}
+          >
+            <Text ff="Inter|SemiBold" color="palette.text.shade100" fontSize={4}>
+              {formatCurrencyUnit(unit, new BigNumber(validator.activeStake?.toString()), {
+                showCode: true,
+              })}
+            </Text>
+            <Text fontSize={2} textAlign="right">
+              <Trans color="palette.text.shade50" i18nKey="aptos.stake.totalStake" />
+            </Text>
+          </Box>
+          <Box ml={2} justifyContent="center" alignContent="center">
+            <ChosenMark active={active ?? false} size={14} />
+          </Box>
+        </Box>
+      }
+      subtitle={
+        <Box>
+          <Text ff="Inter|Medium" fontSize={2} color="palette.text.shade50">
+            <Trans i18nKey="aptos.stake.commission" />
+            <Text
+              style={{
+                marginRight: 5,
+                fontSize: 11,
+              }}
+            >{` ${validator.commission}%`}</Text>
+            {!!validator.nextUnlockTime && (
+              <>
+                <Text
+                  style={{
+                    marginLeft: 8,
+                    fontSize: 11,
+                  }}
+                >
+                  {"\u{1F512}"}
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 3,
+                    fontSize: 11,
+                  }}
+                >
+                  <Trans i18nKey="aptos.stake.nextUnlockIn" />
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 2,
+                    fontSize: 11,
+                  }}
+                >{` ${validator.nextUnlockTime}`}</Text>
+              </>
+            )}
+          </Text>
+        </Box>
+      }
+    ></StyledValidatorRow>
+  );
+}
+
+const StyledValidatorRow = styled(ValidatorRow)`
+  border-color: transparent;
+  margin-bottom: 0;
+`;
+
+const ChosenMark = styled(Check).attrs<{
+  active?: boolean;
+}>(p => ({
+  color: p.active ? p.theme.colors.palette.primary.main : "transparent",
+  size: 14,
+}))<{
+  active?: boolean;
+}>``;
+
+export default AptosValidatorRow;
