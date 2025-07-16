92d28ffe30eb65e09bbfbfc1fc5999d5c0718fd3
Louis PAQUET <louis.paquet@ledger.fr>
2025-07-16 15:22:22 +0200

    fix(LIVE-19967): fix tests

--- libs/ledgerjs/packages/hw-app-exchange/src/ReturnCode.test.ts
+++ libs/ledgerjs/packages/hw-app-exchange/src/ReturnCode.test.ts
@@ -6,13 +6,16 @@ describe("getExchageErrorMessage", () => {
     const result = getExchangeErrorMessage(0x6a80);
 
     // Then
-    expect(result).toBe("Incorrect command data");
+    expect(result).toBe({
+      errorMessage: "Incorrect command data",
+      errorName: "incorrectCommandData",
+    });
   });
   it("returns undefined if the value is unknown", () => {
     // When
     const result = getExchangeErrorMessage(0x6a77);
 
     // Then
-    expect(result).toBeUndefined();
+    expect(result).toBe({ errorMessage: undefined, errorName: undefined });
   });
 });
