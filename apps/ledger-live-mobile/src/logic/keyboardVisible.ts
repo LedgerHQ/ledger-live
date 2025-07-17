175ad673fa276ac997b6db27cafabfb1cfca28e4
ysitbon <yoann.sitbon@ledger.fr>
2025-07-17 14:51:57 +0200

    build: upgrade android build target and fix related upgrade issues
    chore: add changeset
    
    chore: upgrade sub-deps
    
    fix: wip keyboard overlaps
    
    fix: wip keyboard (test change)
    
    chore: wip keyboard input fixes
    
    chore: wip text input fixes
    
    chore: fix eslint ts errors
    
    chore: remove statusbar height for and 15+
    
    chore: fix lint error and extra comp usage
    
    chore: remove unused style
    
    chore: remove accidental file
    
    chore: remove platform condition and remove extra node
    
    chore: restore some files
    
    chore: remove unused import
    
    chore: add comment
    
    chore: remove unused import
    
    chore: fix sonar error
    
    chore: ensure to handle android 15 and below
    
    chore: restore keyboardview on web3 catalog

--- apps/ledger-live-mobile/src/logic/keyboardVisible.ts
+++ apps/ledger-live-mobile/src/logic/keyboardVisible.ts
@@ -3,17 +3,23 @@ import { Keyboard } from "react-native";
 
 export function useKeyboardVisible() {
   const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
   useEffect(() => {
-    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", () =>
-      setKeyboardVisible(true),
-    );
-    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () =>
-      setKeyboardVisible(false),
-    );
+    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", event => {
+      setKeyboardVisible(true);
+      setKeyboardHeight(event.endCoordinates.height);
+    });
+    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardVisible(false);
+      setKeyboardHeight(0);
+    });
+
     return () => {
       keyboardDidHideListener.remove();
       keyboardDidShowListener.remove();
     };
   }, []);
-  return isKeyboardVisible;
+
+  return { isKeyboardVisible, keyboardHeight };
 }
