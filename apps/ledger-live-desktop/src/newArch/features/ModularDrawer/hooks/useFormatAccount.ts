f44070c78ede5f4894447681626439dba0f3ec10
Marco Toniut <marco.toniut-ext@ledger.fr>
2025-07-17 15:21:54 +0100

    chore: address PR comments

--- apps/ledger-live-desktop/src/newArch/features/ModularDrawer/hooks/useFormatAccount.ts
+++ apps/ledger-live-desktop/src/newArch/features/ModularDrawer/hooks/useFormatAccount.ts
@@ -1,50 +1,42 @@
+import { getTagDerivationMode } from "@ledgerhq/coin-framework/derivation";
+import { useCountervaluesState } from "@ledgerhq/live-countervalues-react";
 import { accountNameWithDefaultSelector } from "@ledgerhq/live-wallet/store";
-import { Account as AccountItem } from "@ledgerhq/react-ui/pre-ldls/components/AccountItem/AccountItem";
+import { Account as FormattedAccount } from "@ledgerhq/react-ui/pre-ldls/components/AccountItem/AccountItem";
+import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
 import { Account } from "@ledgerhq/types-live";
+import { formatAddress } from "LLD/utils/formatAddress";
+import { getBalanceAndFiatValue } from "LLD/utils/getBalanceAndFiatValue";
 import { useCallback } from "react";
 import { useSelector } from "react-redux";
-import { formatAddress } from "LLD/utils/formatAddress";
 import { counterValueCurrencySelector, discreetModeSelector } from "~/renderer/reducers/settings";
 import { walletSelector } from "~/renderer/reducers/wallet";
-import { getBalanceAndFiatValue } from "LLD/utils/getBalanceAndFiatValue";
-import { useCountervaluesState } from "@ledgerhq/live-countervalues-react";
-import { getTagDerivationMode } from "@ledgerhq/coin-framework/derivation";
-import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
 
-export const useFormatAccount = ({ currency }: { currency: CryptoCurrency }) => {
+export const useFormatAccount = (currency: CryptoCurrency) => {
   const walletState = useSelector(walletSelector);
   const counterValueCurrency = useSelector(counterValueCurrencySelector);
   const discreet = useSelector(discreetModeSelector);
   const counterValueState = useCountervaluesState();
 
-  const formatAccount = useCallback(
-    (account: Account): AccountItem => {
+  return useCallback(
+    (account: Account): FormattedAccount => {
       const { fiatValue, balance } = getBalanceAndFiatValue(
         account,
         counterValueState,
         counterValueCurrency,
         discreet,
       );
-      const protocol =
-        account.type === "Account" &&
-        account?.derivationMode !== undefined &&
-        account?.derivationMode !== null &&
-        currency.type === "CryptoCurrency" &&
-        getTagDerivationMode(currency, account.derivationMode);
 
       return {
         address: formatAddress(account.freshAddress),
-        cryptoId: account.currency.id,
-        fiatValue: fiatValue || "",
         balance,
-        protocol: protocol || "",
+        cryptoId: account.currency.id,
+        fiatValue: fiatValue ?? "",
         id: account.id,
         name: accountNameWithDefaultSelector(walletState, account),
+        protocol: getTagDerivationMode(currency, account.derivationMode) ?? "",
         ticker: account.currency.ticker,
       };
     },
     [counterValueCurrency, counterValueState, currency, discreet, walletState],
   );
-
-  return formatAccount;
 };
