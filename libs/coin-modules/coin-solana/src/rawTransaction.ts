723be2b3a208d5c3dd4aaccd660b2634da45fc5a
Kant <quentin.jaccarino@ledger.fr>
2025-07-18 12:28:24 +0200

    fix: improve solana raw tx handling [LIVE-19975] - Implemented handling of raw Solana transactions in the SendFunds summary screen. - Updated transaction building and signing processes to accommodate raw transaction types. - Introduced new types and command descriptors for raw transactions in the Solana module. - Enhanced error handling and user feedback for raw transactions. - Added mock data and tests to ensure proper functionality of raw transaction features.

---
+++ libs/coin-modules/coin-solana/src/rawTransaction.ts
@@ -0,0 +1,66 @@
+import { ChainAPI } from "./network";
+import { Transaction } from "./types";
+import { VersionedTransaction } from "@solana/web3.js";
+import BigNumber from "bignumber.js";
+
+function buildRawTransaction(raw: string, estimatedFees: number | null): Transaction {
+  return {
+    raw,
+    family: "solana",
+    amount: BigNumber(0),
+    recipient: "",
+    model: {
+      kind: "raw",
+      uiState: {},
+      commandDescriptor: {
+        command: {
+          kind: "raw",
+          raw,
+        },
+        fee: estimatedFees ?? 0,
+        warnings: {},
+        errors: {},
+      },
+    },
+  };
+}
+
+export async function toLiveTransaction(
+  api: ChainAPI,
+  serializedTransaction: string,
+): Promise<Transaction> {
+  const solanaTransaction = VersionedTransaction.deserialize(
+    Buffer.from(serializedTransaction, "base64"),
+  );
+
+  console.log("tx: ", serializedTransaction);
+  console.log("message: ", solanaTransaction.message);
+  console.log(
+    "message base64: ",
+    Buffer.from(solanaTransaction.message.serialize()).toString("base64"),
+  );
+
+  const estimatedFees = await api.getFeeForMessage(solanaTransaction.message);
+
+  return buildRawTransaction(serializedTransaction, estimatedFees);
+}
+
+export async function deriveRawCommandDescriptor(tx: Transaction, api: ChainAPI) {
+  if (!tx.raw) {
+    throw new Error("Raw transaction is required to derive command descriptor");
+  }
+
+  const liveTx = await toLiveTransaction(api, tx.raw);
+
+  return (
+    liveTx.model.commandDescriptor || {
+      command: {
+        kind: "raw",
+        raw: tx.raw,
+      },
+      fee: tx.model.commandDescriptor?.fee ?? 0,
+      warnings: {},
+      errors: {},
+    }
+  );
+}
