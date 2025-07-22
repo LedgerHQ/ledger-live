e5250cf027791c3f3d0c48c2802cf85ab9c201da
Teck <41478467+niodamian@users.noreply.github.com>
2025-07-22 19:16:34 +0300

    Create terraclassic.integration.test.ts

---
+++ libs/ledger-live-common/src/families/cosmos/datasets/terraclassic.integration.test.ts
@@ -0,0 +1,18 @@
+import { DatasetTest } from "@ledgerhq/types-live";
+import { testBridge } from "../../../__tests__/test-helpers/bridge";
+import "../../../__tests__/test-helpers/setup";
+import babylon from "./terraclassic";
+import type { Transaction } from "../types";
+
+const dataset: DatasetTest<Transaction> = {
+  implementations: ["js"],
+  currencies: {
+    uluna,
+        uluna,
+        lunc,
+        uusd,
+         ustc,
+  },
+};
+
+testBridge(dataset);
