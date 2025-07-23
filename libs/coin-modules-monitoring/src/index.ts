6e6fe811ec8fea2e0ce0a60ab4f947564d748bac
François Guérin <francois.guerin@ledger.fr>
2025-07-23 18:54:28 +0200

    test(coin-modules-monitoring): set breaks points between tests

--- libs/coin-modules-monitoring/src/index.ts
+++ libs/coin-modules-monitoring/src/index.ts
@@ -1,15 +1,21 @@
 import { client, v2 } from "@datadog/datadog-api-client";
+import type { AccountShapeInfo, GetAccountShape } from "@ledgerhq/coin-framework/bridge/jsHelpers";
+import type { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
+import type { Account } from "@ledgerhq/types-live";
 
-const apiV2 = new v2.LogsApi(
-  client.createConfiguration({
-    authMethods: {
-      apiKeyAuth: process.env.DD_API_KEY,
-      appKeyAuth: process.env.DD_APP_KEY,
-    },
-  }),
-);
+const configuration = client.createConfiguration({
+  authMethods: {
+    apiKeyAuth: process.env.DD_API_KEY,
+    appKeyAuth: process.env.DD_APP_KEY,
+  },
+});
+configuration.setServerVariables({
+  site: process.env.DD_DOMAIN ?? "datadoghq.eu",
+});
 
-export interface MonitorParam {
+const apiV2 = new v2.LogsApi(configuration);
+
+export interface LogEntry {
   duration: number;
   currencyName: string;
   coinModuleName: string;
@@ -19,12 +25,70 @@ export interface MonitorParam {
   accountAddress: string;
 }
 
-export async function monitor(params: MonitorParam[]) {
-  return apiV2.submitLog({
-    body: params.map(param => ({
+export interface AccountInfo<A extends Account> {
+  address: string;
+  initialAccount?: Partial<A>;
+  rest?: unknown;
+}
+
+export async function monitor<A extends Account>(
+  currency: CryptoCurrency,
+  accounts: {
+    pristine: AccountInfo<A>;
+    average: AccountInfo<A>;
+    big: AccountInfo<A>;
+  },
+  getAccountShape: GetAccountShape<A>,
+) {
+  const entries: LogEntry[] = [];
+
+  for (const [accountType, accountAddress] of Object.entries(accounts)) {
+    const info = {
+      currency,
+      index: 0,
+      derivationPath: "",
+      derivationMode: "",
+      ...accountAddress,
+    } as AccountShapeInfo<A>;
+    const pagination = { paginationConfig: {} };
+
+    const startScan = Date.now();
+    const initialAccount = (await getAccountShape(info, pagination)) as A;
+    const endScan = Date.now();
+
+    const startSync = Date.now();
+    await getAccountShape({ ...info, initialAccount }, pagination);
+    const endSync = Date.now();
+
+    entries.push(
+      {
+        duration: endScan - startScan,
+        currencyName: currency.id,
+        coinModuleName: currency.family,
+        operationType: "scan",
+        accountType: accountType as LogEntry["accountType"],
+        transactions: initialAccount.operationsCount,
+        accountAddress: accountAddress.address,
+      },
+      {
+        duration: endSync - startSync,
+        currencyName: currency.id,
+        coinModuleName: currency.family,
+        operationType: "sync",
+        accountType: accountType as LogEntry["accountType"],
+        transactions: initialAccount.operationsCount,
+        accountAddress: accountAddress.address,
+      },
+    );
+  }
+
+  await apiV2.submitLog({
+    body: entries.map(entries => ({
       ddtags: "env:prd",
-      message: JSON.stringify(param),
+      message: JSON.stringify(entries),
       service: "coin-modules-monitoring",
     })),
   });
+
+  return entries;
 }
