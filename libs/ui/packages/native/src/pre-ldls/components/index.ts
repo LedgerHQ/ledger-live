f4ced8da6a403a74e1ff0ed4b7858952ea4d828a
RobinVncnt <robin.vincent@ledger.fr>
2025-07-24 10:29:51 +0200

    feat: LLM MAD account selection

--- libs/ui/packages/native/src/pre-ldls/components/index.ts
+++ libs/ui/packages/native/src/pre-ldls/components/index.ts
@@ -5,3 +5,5 @@ export * from "./NetworkItem/NetworkItem";
 export * from "./NetworkList/NetworkList";
 export * from "./Tag/Tag";
 export * from "./Search/Search";
+export * from "./AccountItem/AccountItem";
+export * from "./Address/Address";
