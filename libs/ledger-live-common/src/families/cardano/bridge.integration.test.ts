03d06960a780bb494405a07aa1e8a148273ad413
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2025-07-31 12:53:22 +0200

    WIP

--- libs/ledger-live-common/src/families/cardano/bridge.integration.test.ts
+++
@@ -1,5 +0,0 @@
-import "../../__tests__/test-helpers/setup";
-import { testBridge } from "../../__tests__/test-helpers/bridge";
-import { dataset } from "@ledgerhq/coin-cardano/bridge.integration.test";
-
-testBridge(dataset);
--- libs/ledger-live-common/src/families/ton/bridge.integration.test.ts
+++
@@ -1,5 +0,0 @@
-import { dataset } from "@ledgerhq/coin-ton/__tests__/integration/bridge.integration.test";
-import { testBridge } from "../../__tests__/test-helpers/bridge";
-import "../../__tests__/test-helpers/setup";
-
-testBridge(dataset);
