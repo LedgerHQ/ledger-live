cf448d05a20b1d307402c65101d708696ea3ec46
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2025-07-31 12:15:38 +0200

    WIP

--- libs/ledger-live-common/src/families/evm/bridge.integration.test.ts
+++
@@ -1,5 +0,0 @@
-import { dataset } from "@ledgerhq/coin-evm/__tests__/integration/bridge.integration.test";
-import { testBridge } from "../../__tests__/test-helpers/bridge";
-import "../../__tests__/test-helpers/setup";
-
-testBridge(dataset);
