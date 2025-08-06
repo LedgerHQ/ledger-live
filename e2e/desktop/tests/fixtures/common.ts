8c480938af9ff32af381755aa294b350792a91ee
bayo.makanjuola <bayo.makanjuola@ledger.com>
2025-08-06 10:52:22 +0100

    fix: add temp feature flag util to enable drawer

--- e2e/desktop/tests/fixtures/common.ts
+++ e2e/desktop/tests/fixtures/common.ts
@@ -15,6 +15,7 @@ import { lastValueFrom, Observable } from "rxjs";
 import { CLI } from "../utils/cliUtils";
 import { launchSpeculos, killSpeculos } from "tests/utils/speculosUtils";
 import { SpeculosDevice } from "@ledgerhq/live-common/e2e/speculos";
+import { getDefaultFeatureFlags } from "tests/utils/featureUtils";
 
 type CliCommand = (appjsonPath: string) => Observable<unknown> | Promise<unknown> | string;
 
@@ -145,7 +146,7 @@ export const test = base.extend<TestFixtures>({
           PLAYWRIGHT_RUN: true,
           CRASH_ON_INTERNAL_CRASH: true,
           LEDGER_MIN_HEIGHT: 768,
-          FEATURE_FLAGS: JSON.stringify(featureFlags),
+          FEATURE_FLAGS: JSON.stringify({ ...getDefaultFeatureFlags(), ...featureFlags }),
           MANAGER_DEV_MODE: IS_NOT_MOCK ? true : undefined,
           SPECULOS_API_PORT: IS_NOT_MOCK ? getEnv("SPECULOS_API_PORT")?.toString() : undefined,
         },
