53d2ba48eafea321572c3f5d8a7685399b0a26a5
Marco Toniut <marco.toniut-ext@ledger.fr>
2025-08-13 18:04:00 +0100

    fix: extra space in JSX

--- apps/ledger-live-mobile/src/index.tsx
+++ apps/ledger-live-mobile/src/index.tsx
@@ -353,7 +353,7 @@ export default class Root extends Component {
               <LoadingApp />
             )
           }
-        </LedgerStoreProvider>{" "}
+        </LedgerStoreProvider>
       </RebootProvider>
     );
   }
