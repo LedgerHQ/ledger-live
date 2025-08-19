6ebe1f9e8e565e91afbb7c9db943c98425aaa311
gre-ledger <gaetan.renaudeau@ledger.fr>
2025-08-19 15:51:24 +0200

    WIP clean up of old feature: LiveQR import (LLD->LLM)

--- apps/ledger-live-mobile/src/newArch/features/Accounts/__integrations__/addAccount.integration.test.tsx
+++ apps/ledger-live-mobile/src/newArch/features/Accounts/__integrations__/addAccount.integration.test.tsx
@@ -98,33 +98,6 @@ describe("AddAccount", () => {
     expect(await screen.findByText(/Scan QR code/i));
   });
 
-  /**====== Import from desktop Test =======*/
-  it("Should open select add account method drawer without WS feature flag and navigate to import from desktop", async () => {
-    const { user } = render(<TestButtonPage />, {
-      overrideInitialState: (state: State) => ({
-        ...state,
-        settings: {
-          ...state.settings,
-          readOnlyModeEnabled: false,
-          overriddenFeatureFlags: { llmWalletSync: { enabled: false } },
-        },
-      }),
-    });
-
-    const addAssetButton = await screen.findByText(/add asset/i);
-    // Check if the add asset button is visible
-    expect(addAssetButton).toBeVisible();
-    // Open drawer
-    await user.press(addAssetButton);
-    // Wait for the drawer to open
-    expect(await screen.findByText(/add another account/i));
-    expect(await screen.findByText(/Use your Ledger device/i));
-    expect(await screen.findByText(/import from desktop/i));
-    // On press add from desktop
-    await user.press(screen.getByText(/import from desktop/i));
-    expect(await screen.findByText(/Scan and import your accounts from Ledger Live desktop/i));
-  });
-
   /**====== use ModularDrawer Test =======*/
   it("Should open ModularDrawer when selecting use Ledger device", async () => {
     const { user } = render(<TestButtonPage />, {
@@ -158,7 +131,6 @@ describe("AddAccount", () => {
     // Wait for the drawer to open
     expect(await screen.findByText(/add another account/i));
     expect(await screen.findByText(/Use your Ledger device/i));
-    expect(await screen.findByText(/import from desktop/i));
 
     // Press the "Use your Ledger device" button to trigger ModularDrawer
     await user.press(screen.getByText(/Use your Ledger device/i));
