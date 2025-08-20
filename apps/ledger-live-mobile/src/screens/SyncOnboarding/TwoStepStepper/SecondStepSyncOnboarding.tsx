7fca7b228c7e96682d2b31a20a6992a0690084e2
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2025-08-20 11:16:35 +0100

    feat: add install apps component to second step of two stepper

--- apps/ledger-live-mobile/src/screens/SyncOnboarding/TwoStepStepper/SecondStepSyncOnboarding.tsx
+++ apps/ledger-live-mobile/src/screens/SyncOnboarding/TwoStepStepper/SecondStepSyncOnboarding.tsx
@@ -1,37 +1,48 @@
 import React from "react";
 import CollapsibleStep from "./CollapsibleStep";
 import { useTranslation } from "react-i18next";
-import { Flex, Text } from "@ledgerhq/native-ui";
-// import { useDispatch } from "react-redux";
-// import { ScreenName } from "~/const";
-// import { SyncOnboardingScreenProps } from "../SyncOnboardingScreenProps";
-// import { Device } from "@ledgerhq/live-common/hw/actions/types";
-// import { CompanionStep } from "./TwoStepSyncOnboardingCompanion";
+import InstallSetOfApps from "~/components/DeviceAction/InstallSetOfApps";
+import { Device } from "@ledgerhq/live-common/hw/actions/types";
+import { CompanionStep } from "./TwoStepSyncOnboardingCompanion";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import BackgroundBlue from "../assets/BackgroundBlue";
+import { Box } from "@ledgerhq/native-ui";
+
+const fallbackDefaultAppsToInstall = ["Bitcoin", "Ethereum", "Polygon"];
 
 interface SecondStepSyncOnboardingProps {
-  //   navigation: SyncOnboardingScreenProps["navigation"];
-  //   device: Device;
-  //   companionStep: CompanionStep;
+  device: Device;
+  companionStep: CompanionStep;
   isCollapsed: boolean;
+  handleDone: () => void;
 }
 
 const SecondStepSyncOnboarding = ({
-  //   navigation,
-  //   device,
-  //   companionStep,
+  device,
+  companionStep,
   isCollapsed,
+  handleDone,
 }: SecondStepSyncOnboardingProps) => {
   const { t } = useTranslation();
+  const deviceInitialApps = useFeature("deviceInitialApps");
+  const initialAppsToInstall = deviceInitialApps?.params?.apps || fallbackDefaultAppsToInstall;
 
   return (
     <CollapsibleStep
       isCollapsed={isCollapsed}
       title={t("syncOnboarding.secureCryptoStep.title")}
       status="unfinished"
+      background={<BackgroundBlue />}
     >
-      <Flex>
-        <Text>OPEN SECOND STEP</Text>
-      </Flex>
+      <Box mt={3}>
+        <InstallSetOfApps
+          isNewSeed={companionStep === "new_seed"}
+          restore={companionStep === "restore"}
+          device={device}
+          onResult={handleDone}
+          dependencies={initialAppsToInstall}
+        />
+      </Box>
     </CollapsibleStep>
   );
 };
