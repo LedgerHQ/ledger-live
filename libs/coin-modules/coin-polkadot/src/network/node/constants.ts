112670373577372582d202328ecd5b33e1794793
fAnselmi-Ledger <francesco.anselmi@ledger.fr>
2025-08-25 12:14:49 +0200

    fix(coin-polkadot): patched build

--- libs/coin-modules/coin-polkadot/src/network/node/constants.ts
+++ libs/coin-modules/coin-polkadot/src/network/node/constants.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { decorateConstants } from "@polkadot/types";
 import getApiPromise from "./apiPromise";
 import { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
--- libs/coin-modules/coin-polkadot/src/network/node/nominations.ts
+++ libs/coin-modules/coin-polkadot/src/network/node/nominations.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { SidecarNominations } from "../types";
 import getApiPromise from "./apiPromise";
 
