fd5e501eb0aeedeb79c3f03738c4a8d376d5c9ae
Jérôme Prudent <jerome.prudent@ledger.fr>
2025-08-26 10:54:40 +0200

    chore(sui): document the combine function

--- libs/coin-modules/coin-sui/src/logic/combine.ts
+++ libs/coin-modules/coin-sui/src/logic/combine.ts
@@ -1,6 +1,8 @@
 /**
  * Returns a signed transaction for later used with {@link broadcast} function.
- * @returns Serialized Transaction (in its raw_data_tx form) and Signature
+ * @param tx - Serialized Transaction as base64 string
+ * @param signature - base64(00||$signature||$pubkey)
+ * @returns Serialized Transaction (in its raw_data_tx form) and Signature to be used with {@link broadcast} function
  */
 export function combine(tx: string, signature: string): string {
   return `${tx.length.toString(16).padStart(4, "0")}${tx}${signature}`;
