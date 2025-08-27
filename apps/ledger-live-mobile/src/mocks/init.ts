0e09c0b1925a6ab4206f60b12efbae03c523a37c
Lucas Werey <lucas.werey@ledger.fr>
2025-08-27 16:19:31 +0200

    :sparkles: (MAD-LLM) add MSW for dev

---
+++ apps/ledger-live-mobile/src/mocks/init.ts
@@ -0,0 +1,20 @@
+async function enableMocking() {
+  const mswEnabled = process.env.MSW_ENABLED === "true";
+
+  if (!mswEnabled) {
+    return;
+  }
+
+  try {
+    const { mswWorker } = await import("./server");
+    // eslint-disable-next-line no-console
+    console.log("\x1b[32m MSW: Starting Mock Service Worker \x1b[0m");
+    mswWorker.listen({
+      onUnhandledRequest: "bypass",
+    });
+  } catch (error) {
+    console.error("\x1b[41m MSW: Failed to start Mock Service Worker \x1b[0m", error);
+  }
+}
+
+enableMocking();
