21931ca0e598746fd061c8b2db2e0e3cac44f264
Diyaeddine LAOUID <diyaeddine.laouid-ext@ledger.fr>
2025-09-02 23:11:27 +0200

    fix: update staking contracts list format

---
+++ libs/coin-modules/coin-evm/src/staking/encoder.ts
@@ -0,0 +1,47 @@
+import { ethers } from "ethers";
+import type { StakingContractConfig, StakingOperation } from "../types/staking";
+import { getStakingABI } from "./abis";
+
+export interface EncodeStakingDataParams {
+  currencyId: string;
+  operation: StakingOperation;
+  config: StakingContractConfig;
+  params: unknown[];
+}
+
+export const encodeStakingData = (encodeParams: EncodeStakingDataParams): string => {
+  const { currencyId, operation, config, params } = encodeParams;
+
+  const abi = getStakingABI(currencyId);
+  if (!abi) {
+    throw new Error(`No ABI found for staking currency: ${currencyId}`);
+  }
+
+  const functionName = config.functions[operation];
+  if (!functionName) {
+    throw new Error(`Operation '${operation}' not supported for currency: ${currencyId}`);
+  }
+
+  const iface = new ethers.utils.Interface(abi);
+  return iface.encodeFunctionData(functionName, params);
+};
+
+export const decodeStakingResult = (
+  currencyId: string,
+  operation: StakingOperation,
+  config: StakingContractConfig,
+  result: string,
+): ethers.utils.Result => {
+  const abi = getStakingABI(currencyId);
+  if (!abi) {
+    throw new Error(`No ABI found for staking currency: ${currencyId}`);
+  }
+
+  const functionName = config.functions[operation];
+  if (!functionName) {
+    throw new Error(`Operation '${operation}' not supported for currency: ${currencyId}`);
+  }
+
+  const iface = new ethers.utils.Interface(abi);
+  return iface.decodeFunctionResult(functionName, result);
+};
