682033c632ebc876abd5a9eede87e33d23133ee9
Pedro Semeano <semeano@gmail.com>
2025-09-05 15:40:23 +0100

    feat: kaspa common libs

---
+++ libs/ledger-live-common/src/e2e/families/kaspa.ts
@@ -0,0 +1,17 @@
+import expect from "expect";
+import { pressBoth, pressUntilTextFound, waitFor, containsSubstringInEvent } from "../speculos";
+import { DeviceLabels } from "../enum/DeviceLabels";
+import { Delegate } from "../models/Delegate";
+
+export async function sendKaspa() {
+  await pressUntilTextFound(DeviceLabels.APPROVE);
+  await pressBoth();
+}
+
+export async function delegateKaspa(delegatingAccount: Delegate) {
+  await waitFor(DeviceLabels.REVIEW_OPERATION);
+  const events = await pressUntilTextFound(DeviceLabels.APPROVE);
+  const isAmountCorrect = containsSubstringInEvent(delegatingAccount.amount, events);
+  expect(isAmountCorrect).toBeTruthy();
+  await pressBoth();
+}
