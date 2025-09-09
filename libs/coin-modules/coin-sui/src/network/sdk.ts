d76e57411655fd0ba1f7a0a988e7e26d91c6a605
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2025-09-09 18:53:21 +0200

    feat(BACK-9645): workaround import

--- libs/coin-modules/coin-sui/src/network/sdk.ts
+++ libs/coin-modules/coin-sui/src/network/sdk.ts
@@ -46,7 +46,7 @@ import { getEnv } from "@ledgerhq/live-env";
 import { SUI_SYSTEM_STATE_OBJECT_ID } from "@mysten/sui/utils";
 import { getCurrentSuiPreloadData } from "../bridge/preload";
 import { ONE_SUI } from "../constants";
-import { SuiMoveObject } from "@mysten/signers/ledger/bcs";
+import { SuiMoveObject } from "../../node_modules/@mysten/signers/dist/cjs/ledger/bcs.js";
 import bs58 from "bs58";
 
 const apiMap: Record<string, SuiClient> = {};
