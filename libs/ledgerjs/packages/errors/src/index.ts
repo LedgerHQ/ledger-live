759064d4815c636af2d73ba548a85b4f53e7b491
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-09-09 15:04:09 +0200

    fix: hedera safeParseAccountId util + custom errors in coin-module

--- libs/ledgerjs/packages/errors/src/index.ts
+++ libs/ledgerjs/packages/errors/src/index.ts
@@ -201,17 +201,6 @@ export const ReplacementTransactionUnderpriced = createCustomErrorClass(
 export const OpReturnDataSizeLimit = createCustomErrorClass("OpReturnSizeLimit");
 export const DustLimit = createCustomErrorClass("DustLimit");
 
-// Hedera family
-export const HederaInsufficientFundsForAssociation = createCustomErrorClass(
-  "HederaInsufficientFundsForAssociation",
-);
-export const HederaRecipientTokenAssociationRequired = createCustomErrorClass(
-  "HederaRecipientTokenAssociationRequired",
-);
-export const HederaRecipientTokenAssociationUnverified = createCustomErrorClass(
-  "HederaRecipientTokenAssociationUnverified",
-);
-
 // Language
 export const LanguageNotFound = createCustomErrorClass("LanguageNotFound");
 
