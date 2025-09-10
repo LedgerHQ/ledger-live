a51f0b65e29638b37840990bb13d2d4f4735d8dc
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2025-09-10 17:08:02 +0200

    fix(lld): type issue

--- apps/ledger-live-desktop/src/renderer/components/CustomImage/ContrastChoice.tsx
+++ apps/ledger-live-desktop/src/renderer/components/CustomImage/ContrastChoice.tsx
@@ -10,32 +10,30 @@ type Props = {
   appearance: Appearance;
 };
 
-type ContrastOptionProps = {
-  topLeftColor: string;
-  bottomRightColor: string;
+type ContainerProps = {
+  selected: boolean;
+  appearanceType: Appearance["type"];
 };
 
-const Container = styled(Flex).attrs(
-  (p: { selected: boolean; appearanceType: Appearance["type"] }) => ({
-    position: "relative",
-    justifyContent: "center",
-    alignItems: "center",
-    border: "solid",
-    height: p.appearanceType === "two-colors" ? 48 : 56,
-    width: p.appearanceType === "two-colors" ? 48 : 56,
-    borderWidth: p.appearanceType === "two-colors" ? 1 : 2,
-    padding: p.appearanceType === "two-colors" ? 0 : 2,
-    borderColor: p.selected ? "neutral.c100" : "transparent",
-    borderRadius: p.appearanceType === "two-colors" ? "5px" : "8px",
-  }),
-)<{ selected: boolean }>``;
+const Container = styled(Flex).attrs((p: ContainerProps) => ({
+  position: "relative",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "solid",
+  height: p.appearanceType === "two-colors" ? 48 : 56,
+  width: p.appearanceType === "two-colors" ? 48 : 56,
+  borderWidth: p.appearanceType === "two-colors" ? 1 : 2,
+  padding: p.appearanceType === "two-colors" ? 0 : 2,
+  borderColor: p.selected ? "neutral.c100" : "transparent",
+  borderRadius: p.appearanceType === "two-colors" ? "5px" : "8px",
+}))<ContainerProps>``;
 
 const ContrastOptionColors = styled(Box).attrs({
   borderRadius: "4px",
   height: 46,
   width: 46,
   overflow: "hidden",
-})<ContrastOptionProps>`
+})<{ colors: { topLeft: string; bottomRight: string } }>`
   background: linear-gradient(
     to bottom right,
     ${p => p.colors.topLeft} calc(50% - 1px),
@@ -43,12 +41,12 @@ const ContrastOptionColors = styled(Box).attrs({
   );
 `;
 
-const ContrastOptionBackgroundPicture = styled(Box).attrs<{ backgroundSrc: string }>({
+const ContrastOptionBackgroundPicture = styled(Box).attrs({
   borderRadius: "4px",
   height: 48,
   width: 48,
   overflow: "hidden",
-})`
+})<{ backgroundSrc: string }>`
   background-image: url(${p => p.backgroundSrc});
   background-size: cover;
   background-position: center;
