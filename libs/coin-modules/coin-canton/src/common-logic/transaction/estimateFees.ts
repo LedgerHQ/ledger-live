a3bf70ea59fc31277a170d4cb5530f2f58d0a2ab
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2025-09-16 11:34:09 +0200

    update fee canton

--- libs/coin-modules/coin-canton/src/common-logic/transaction/estimateFees.ts
+++ libs/coin-modules/coin-canton/src/common-logic/transaction/estimateFees.ts
@@ -1,7 +1,8 @@
 import coinConfig from "../../config";
 import type { CryptoCurrency } from "@ledgerhq/types-cryptoassets";
 
-const feeValue = (currency: CryptoCurrency) => coinConfig.getCoinConfig(currency).fee ?? 1;
+const feeValue = (currency: CryptoCurrency) =>
+  coinConfig.getCoinConfig(currency).fee ?? 2n * 10n ** 38n;
 
 export async function estimateFees(currency: CryptoCurrency): Promise<bigint> {
   return Promise.resolve(BigInt(feeValue(currency)));
