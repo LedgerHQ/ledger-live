b720d11a8d4847a9a6cd7633636cabb02e28e361
qperrot <quentin.perrot-minnot@ledger.fr>
2025-09-17 17:14:20 +0200

    fix: check if defaultBaker exists

--- apps/ledger-live-desktop/src/renderer/families/tezos/DelegateFlowModal/Body.tsx
+++ apps/ledger-live-desktop/src/renderer/families/tezos/DelegateFlowModal/Body.tsx
@@ -132,7 +132,12 @@ const Body = ({ stepId, params, onChangeStepId, onClose }: Props) => {
     };
 
     // make sure that in delegate mode, a transaction recipient is set (random pick)
-    if (patch.mode === "delegate" && !transaction.recipient && stepId !== "custom") {
+    if (
+      patch.mode === "delegate" &&
+      !transaction.recipient &&
+      stepId !== "custom" &&
+      defaultBaker
+    ) {
       patch.recipient = defaultBaker.address;
     }
 
