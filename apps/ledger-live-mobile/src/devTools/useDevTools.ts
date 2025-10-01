777847535d2a3c4e98dedebbc1cee46ea12ab9a0
Martin Cayuelas <martin.cayuelas@ledger.fr>
2025-10-01 15:45:13 +0200

    chore: changeset & readme

--- apps/ledger-live-mobile/src/devTools/useDevTools.ts
+++ apps/ledger-live-mobile/src/devTools/useDevTools.ts
@@ -24,3 +24,15 @@ const HookDevTools = () => {
 };
 
 export default HookDevTools;
+
+/**
+ * For performance monitoring, we can use the following metrics:
+ *
+ * 0. import performance from 'react-native-performance';
+ *
+ * 1. performance.mark('app-start');
+ * 2. performance.mark('data-loaded');
+ * 3. performance.measure('app-initialization', 'app-start', 'data-loaded');
+ * 4. performance.metric('custom-metric', 42, { detail: 'Additional info' });
+ *
+ */
