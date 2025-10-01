e187bb08d388d4252be32c0249d8e6c802e5edb2
scavassa-yld <gabriel.scavassa@yld.io>
2025-10-01 14:16:53 +0100

    feat: adding core sui e2e code back

--- e2e/desktop/tests/page/drawer/send.drawer.ts
+++ e2e/desktop/tests/page/drawer/send.drawer.ts
@@ -1,13 +1,70 @@
 import { step } from "../../misc/reporters/step";
 import { Drawer } from "../../component/drawer.component";
 import { expect } from "@playwright/test";
+import { getAccountAddress } from "@ledgerhq/live-common/e2e/enum/Account";
+import { Transaction } from "@ledgerhq/live-common/e2e/models/Transaction";
 
 export class SendDrawer extends Drawer {
   private sendDrawer = this.page.getByTestId("drawer-content");
   private addressValue = (address: string) => this.sendDrawer.filter({ hasText: address });
+  private transactionTitle = this.page.getByTestId("modal-title").first();
+  private transactionMessageStatus = this.page.getByTestId("success-message-label");
+  private amountValue = this.page.getByTestId("amountReceived-drawer").first();
+  private transactionStatus = this.page.getByTestId("status-drawer");
+  private drawerOperationtype = this.page.getByTestId("operation-type");
+  private operationFromAccount = this.page.getByTestId("operation-from");
+  private operationToAccount = this.page.getByTestId("operation-to");
 
   @step("Verify address is visible")
   async addressValueIsVisible(address: string) {
     await expect(this.addressValue(address)).toBeVisible();
+  }
+
+  @step("Verify transaction title")
+  async expectTransactionTitle(transactionTitle: string) {
+    const displayedTransactionTitle = await this.transactionTitle.innerText();
+    expect(displayedTransactionTitle).toEqual(transactionTitle);
+  }
+
+  @step("Verify transaction message status")
+  async expectTransactionMessageStatus(transactionMessage: string) {
+    const displayedTransactionMessageStatus = await this.transactionMessageStatus.innerText();
+    expect(displayedTransactionMessageStatus).toEqual(transactionMessage);
+  }
+
+  @step("Verify that the information of the transaction is visible")
+  async expectReceiverInfos(tx: Transaction) {
+    await this.amountValue.waitFor();
+    await expect(this.addressValue(getAccountAddress(tx.accountToCredit))).toBeVisible();
+    await expect(this.amountValue).toBeVisible();
+    const displayedAmount = await this.amountValue.innerText();
+    expect(displayedAmount).toEqual(expect.stringContaining(tx.amount));
+    expect(displayedAmount).toEqual(expect.stringContaining(tx.accountToDebit.currency.ticker));
+  }
+
+  @step("Verify transaction status")
+  async expectTransactionStatus(transactionStatus: string) {
+    const displayedTransactionStatus = await this.transactionStatus.innerText();
+    expect(displayedTransactionStatus).toEqual(transactionStatus);
+  }
+
+  @step("Verify drawer operation type")
+  async expectDrawerOperationType(operationType: string) {
+    const displayedOperationType = await this.drawerOperationtype.innerText();
+    expect(displayedOperationType).toEqual(operationType);
+  }
+
+  @step("Verify drawer accounts")
+  async expectDrawerAccounts(tx: Transaction) {
+    await expect(this.operationToAccount).toContainText(tx.accountToCredit.address);
+    await expect(this.operationFromAccount).toContainText(tx.accountToDebit.address);
+  }
+
+  @step("Verify that the information of the token transaction is visible")
+  async expectTokenReceiverInfos(tx: Transaction) {
+    await expect(this.amountValue).toBeVisible();
+    const displayedAmount = await this.amountValue.innerText();
+    expect(displayedAmount).toEqual(expect.stringContaining(tx.amount));
+    expect(displayedAmount).toEqual(expect.stringContaining(tx.accountToDebit.currency.ticker));
   }
 }
