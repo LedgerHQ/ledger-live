a1982fd36453a6b270d918a0cb141e9fd9c76462
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-10-07 18:10:45 +0200

    feat: rename log files on desktop and mobile

--- apps/ledger-live-mobile/src/screens/Swap/History/index.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/History/index.tsx
@@ -159,7 +159,7 @@ const History = () => {
       }
     } else {
       try {
-        sendFile({ fileName: "ledgerlive-swap-history.csv", fileContent: mapped });
+        sendFile({ fileName: "ledgerwallet-swap-history.csv", fileContent: mapped });
       } catch (err) {
         logger.critical(err as Error);
       }
