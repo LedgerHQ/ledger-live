7310d1d2878cc7edc18ec69ff5d2a3b83ae813b6
Canestin <canestin.ndong@ledger.fr>
2025-10-07 16:33:16 +0200

    migration logic

--- libs/coin-modules/coin-polkadot/src/bridge/synchronization.ts
+++ libs/coin-modules/coin-polkadot/src/bridge/synchronization.ts
@@ -4,59 +4,76 @@ import type { GetAccountShape } from "@ledgerhq/coin-framework/bridge/jsHelpers"
 import { loadPolkadotCrypto } from "../logic/polkadot-crypto";
 import { PolkadotAccount } from "../types";
 import polkadotAPI from "../network";
+import { getCryptoCurrencyById } from "@ledgerhq/cryptoassets/currencies";
 
 export const getAccountShape: GetAccountShape<PolkadotAccount> = async info => {
   await loadPolkadotCrypto();
-  const { address, initialAccount, currency, derivationMode } = info;
 
-  // Retrieve account info
-  const {
-    blockHeight,
-    balance,
-    spendableBalance,
-    nonce,
-    lockedBalance,
-    controller,
-    stash,
-    unlockedBalance,
-    unlockingBalance,
-    unlockings,
-    nominations,
-    numSlashingSpans,
-  } = await polkadotAPI.getAccount(address, currency);
+  try {
+    const { address, initialAccount, currency, derivationMode } = info;
 
-  // Retrieve operations associated
-  const accountId = encodeAccountId({
-    type: "js",
-    version: "2",
-    currencyId: currency.id,
-    xpubOrAddress: address,
-    derivationMode,
-  });
-  const oldOperations = initialAccount?.operations || [];
-  const startAt = oldOperations.length ? (oldOperations[0].blockHeight || 0) + 1 : 0;
-  const newOperations = await polkadotAPI.getOperations(accountId, address, currency, startAt);
-  const operations = mergeOps(oldOperations, newOperations);
+    // Check if migration is needed and handle it automatically
+    const shouldMigrate = currency.id === "polkadot";
+    const currencyToUse = shouldMigrate ? getCryptoCurrencyById("assethub_polkadot") : currency;
 
-  return {
-    id: accountId,
-    balance,
-    spendableBalance,
-    operations,
-    operationsCount: operations.length,
-    blockHeight,
-    polkadotResources: {
+    // Retrieve account info
+    const {
+      blockHeight,
+      balance,
+      spendableBalance,
       nonce,
+      lockedBalance,
       controller,
       stash,
-      lockedBalance,
       unlockedBalance,
       unlockingBalance,
       unlockings,
       nominations,
       numSlashingSpans,
-    },
-  };
+    } = await polkadotAPI.getAccount(address, currencyToUse);
+
+    // Retrieve operations associated
+
+    const accountId = encodeAccountId({
+      type: "js",
+      version: "2",
+      currencyId: currencyToUse.id,
+      xpubOrAddress: address,
+      derivationMode,
+    });
+    const oldOperations = initialAccount?.operations || [];
+    const startAt = oldOperations.length ? (oldOperations[0].blockHeight || 0) + 1 : 0;
+    const newOperations = await polkadotAPI.getOperations(
+      accountId,
+      address,
+      currencyToUse,
+      startAt,
+    );
+    const operations = mergeOps(oldOperations, newOperations);
+
+    return {
+      id: accountId,
+      balance,
+      spendableBalance,
+      operations,
+      operationsCount: operations.length,
+      blockHeight,
+      polkadotResources: {
+        nonce,
+        controller,
+        stash,
+        lockedBalance,
+        unlockedBalance,
+        unlockingBalance,
+        unlockings,
+        nominations,
+        numSlashingSpans,
+      },
+    };
+  } catch (error) {
+    console.error("Error getting account shape", error);
+    throw error;
+  }
 };
 
 export const sync = makeSync({ getAccountShape });
