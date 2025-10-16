d86c1c0bc2ae6c7d630b67a0263511980371bffc
Martin Cayuelas <martin.cayuelas@ledger.fr>
2025-10-16 11:00:50 +0200

    fix: MAD - LLD - Extra step and incorrect display of non-stakable assets in Market staking flow

--- apps/ledger-live-desktop/src/renderer/screens/stake/index.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/stake/index.tsx
@@ -156,6 +156,7 @@ const useStakeFlow = () => {
           useCase: "earn",
           onSuccess,
           onCancel: handleRequestClose,
+          areCurrenciesFiltered: cryptoCurrencies.length > 0,
         });
       } else {
         setDrawer(
