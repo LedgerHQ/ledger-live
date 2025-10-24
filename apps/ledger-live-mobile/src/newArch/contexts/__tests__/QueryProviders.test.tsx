20820e22c2d3d8e6aa73bb7aa7747a1004f51cf6
Theophile Sandoz <theophile.sandoz@ledger.fr>
2025-10-24 09:49:23 +0200

    chore(llm): test new implementations

---
+++ apps/ledger-live-mobile/src/newArch/contexts/__tests__/QueryProviders.test.tsx
@@ -0,0 +1,71 @@
+import React, { useContext } from "react";
+import merge from "lodash/merge";
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/native";
+import { waitFor } from "@testing-library/react-native";
+import { render } from "@tests/test-renderer";
+import { InitialQueriesContext, QueryProviders } from "../QueryProviders";
+
+const contextSpy = jest.fn();
+const ofacResponse = jest.fn();
+
+describe("QueryProviders", () => {
+  const server = setupServer(
+    http.get("https://countervalues.live.ledger.com/v3/markets", ofacResponse),
+    http.all("*", () => HttpResponse.json({})),
+  );
+  server.listen();
+
+  beforeEach(() => {
+    contextSpy.mockClear();
+    ofacResponse.mockClear();
+  });
+
+  it("provides successful values for firebase and ofacResult", async () => {
+    ofacResponse.mockResolvedValueOnce(HttpResponse.json({}));
+    renderApp();
+    await waitFor(() =>
+      expect(contextSpy).toHaveBeenLastCalledWith({
+        firebaseIsReady: true,
+        ofacResult: { blocked: false, isLoading: false },
+      }),
+    );
+  });
+
+  it("provides blocked value for ofacResult", async () => {
+    ofacResponse.mockResolvedValueOnce(HttpResponse.json({}, { status: 451 }));
+    renderApp();
+    await waitFor(() =>
+      expect(contextSpy).toHaveBeenLastCalledWith({
+        firebaseIsReady: true,
+        ofacResult: { blocked: true, isLoading: false },
+      }),
+    );
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  function renderApp() {
+    return render(
+      <QueryProviders>
+        <ContextSpy />
+      </QueryProviders>,
+      {
+        overrideInitialState: state =>
+          merge({}, state, {
+            settings: {
+              overriddenFeatureFlags: { llmOfacGeoBlocking: { enabled: true } },
+            },
+          }),
+      },
+    );
+  }
+});
+
+function ContextSpy() {
+  const context = useContext(InitialQueriesContext);
+  contextSpy(context);
+  return null;
+}
