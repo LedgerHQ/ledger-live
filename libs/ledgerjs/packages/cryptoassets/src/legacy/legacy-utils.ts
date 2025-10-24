b3539db9557451f1b9ea99cd70b75090219b2aee
gre-ledger <gaetan.renaudeau@ledger.fr>
2025-10-24 19:05:59 +0200

    feat(FINAL): change CryptoAssetsStore interface to async

--- libs/ledgerjs/packages/cryptoassets/src/legacy/legacy-utils.ts
+++ libs/ledgerjs/packages/cryptoassets/src/legacy/legacy-utils.ts
@@ -388,6 +388,7 @@ export function convertStellarTokens([
 ]: StellarToken): TokenCurrency {
   const parentCurrency = getCryptoCurrencyById("stellar");
 
+  // FIXME: to be discussed with CAL service as values are Uppercase IRL
   return {
     type: "TokenCurrency",
     id: `stellar/asset/${assetCode.toUpperCase()}:${assetIssuer.toUpperCase()}`,
@@ -521,7 +522,6 @@ export function addTokens(list: (TokenCurrency | undefined)[]): void {
     if (!delisted) tokensArray.push(token);
     tokensArrayWithDelisted.push(token);
     tokensById[id] = token;
-
     tokensByCurrencyAddress[parentCurrency.id + ":" + lowCaseContract] = token;
 
     if (!(parentCurrency.id in tokensByCryptoCurrency)) {
