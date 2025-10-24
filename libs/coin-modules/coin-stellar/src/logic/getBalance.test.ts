49c9f0f859e7e334cefa3d0913df1a1f4edf5c53
gre-ledger <gaetan.renaudeau@ledger.fr>
2025-10-24 18:46:48 +0200

    feat(coin-stellar): add await to token lookups for async migration

--- libs/coin-modules/coin-stellar/src/logic/getBalance.test.ts
+++ libs/coin-modules/coin-stellar/src/logic/getBalance.test.ts
@@ -1,9 +1,18 @@
 import { getBalance } from "./getBalance";
 import { fetchAccount } from "../network";
+import { setCryptoAssetsStore } from "@ledgerhq/coin-framework/crypto-assets/index";
+import { legacyCryptoAssetsStore } from "@ledgerhq/cryptoassets/legacy/legacy-store";
+import { initializeLegacyTokens } from "@ledgerhq/cryptoassets/legacy/legacy-data";
+import { addTokens as addTokensLegacy } from "@ledgerhq/cryptoassets/legacy/legacy-utils";
 
 jest.mock("../network", () => ({
   fetchAccount: jest.fn(),
 }));
+
+beforeAll(() => {
+  initializeLegacyTokens(addTokensLegacy);
+  setCryptoAssetsStore(legacyCryptoAssetsStore);
+});
 
 describe("getBalance", () => {
   it("returns native balance when no assets are present", async () => {
@@ -32,7 +41,7 @@ describe("getBalance", () => {
         {
           asset_type: "credit_alphanum4",
           asset_code: "USDC",
-          asset_issuer: "issuer-address",
+          asset_issuer: "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
           balance: "50.1234567",
         },
       ],
@@ -51,7 +60,7 @@ describe("getBalance", () => {
         asset: {
           type: "credit_alphanum4",
           assetReference: "USDC",
-          assetOwner: "issuer-address",
+          assetOwner: "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
         },
       },
     ]);
