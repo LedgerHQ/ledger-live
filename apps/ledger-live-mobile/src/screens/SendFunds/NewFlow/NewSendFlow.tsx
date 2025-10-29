8f2c25be53df9590c7705ae9c9a5fd81c55314d3
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-10-29 18:28:30 +0100

    feature/Live-20725 [CoinConfig] Feature flag by currency

---
+++ apps/ledger-live-mobile/src/screens/SendFunds/NewFlow/NewSendFlow.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function NewSendFlow() {
+  return (
+    <View style={{ margin: "auto" }}>
+      <Text>Still under construction</Text>
+    </View>
+  );
+}
