2af2eb247c509f4edc1ce78cb6c52aa2251d8575
Martin Cayuelas <martin.cayuelas@ledger.fr>
2025-10-29 16:31:45 +0100

    fix: https://ledgerhq.atlassian.net/browse/LIVE-22676

--- apps/ledger-live-mobile/src/screens/Settings/Accounts/FilterTokenOperationsZeroAmountRow.tsx
+++ apps/ledger-live-mobile/src/screens/Settings/Accounts/FilterTokenOperationsZeroAmountRow.tsx
@@ -1,42 +1,35 @@
-import React from "react";
-import { Trans } from "react-i18next";
-import { compose } from "redux";
-import { connect } from "react-redux";
+import React, { memo, useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 import { Switch } from "@ledgerhq/native-ui";
 import { setFilterTokenOperationsZeroAmount } from "~/actions/settings";
 import SettingsRow from "~/components/SettingsRow";
-import withEnv from "~/logic/withEnv";
+import { filterTokenOperationsZeroAmountEnabledSelector } from "~/reducers/settings";
 
-type Props = {
-  filterTokenOperationsZeroAmountEnabled: boolean;
-  setFilterTokenOperationsZeroAmount: (_: boolean) => void;
-};
+function FilterTokenOperationsZeroAmountRow() {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
 
-const mapDispatchToProps = {
-  setFilterTokenOperationsZeroAmount,
-};
+  const filterTokenOperationsZeroAmountEnabled = useSelector(
+    filterTokenOperationsZeroAmountEnabledSelector,
+  );
 
-function FilterTokenOperationsZeroAmountRow({
-  filterTokenOperationsZeroAmountEnabled,
-  setFilterTokenOperationsZeroAmount,
-  ...props
-}: Props) {
+  const onChange = useCallback(
+    (enabled: boolean) => {
+      dispatch(setFilterTokenOperationsZeroAmount(enabled));
+    },
+    [dispatch],
+  );
+
   return (
     <SettingsRow
-      {...props}
       event="FilterTokenOperationsZeroAmountRow"
-      title={<Trans i18nKey="settings.display.filterTokenOperationsZeroAmount" />}
-      desc={<Trans i18nKey="settings.display.filterTokenOperationsZeroAmountDesc" />}
+      title={t("settings.display.filterTokenOperationsZeroAmount")}
+      desc={t("settings.display.filterTokenOperationsZeroAmountDesc")}
     >
-      <Switch
-        checked={filterTokenOperationsZeroAmountEnabled}
-        onChange={setFilterTokenOperationsZeroAmount}
-      />
+      <Switch checked={filterTokenOperationsZeroAmountEnabled} onChange={onChange} />
     </SettingsRow>
   );
 }
 
-export default compose<React.ComponentType<Record<string, unknown>>>(
-  withEnv("FILTER_ZERO_AMOUNT_ERC20_EVENTS", "filterTokenOperationsZeroAmountEnabled"),
-  connect(null, mapDispatchToProps),
-)(FilterTokenOperationsZeroAmountRow);
+export default memo(FilterTokenOperationsZeroAmountRow);
