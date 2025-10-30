b03f22ffc6e51d82524c143ad8d557ebca4813dd
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-10-30 15:23:52 +0100

    lint

--- apps/ledger-live-mobile/src/newArch/features/Send/index.tsx
+++ apps/ledger-live-mobile/src/newArch/features/Send/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function SendWorkflow() {
-  // eslint-disable-next-line i18next/no-literal-string
   return (
     <View style={{ margin: "auto" }}>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <Text>New Send flow</Text>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
       <Text>Still under construction</Text>
     </View>
   );
