c9019a287c5ad79f0ad124e7993d068761d7c6b8
gre-ledger <gaetan.renaudeau@ledger.fr>
2025-10-30 14:01:59 +0100

    feat(LLM): integrate CryptoAssetsStore

--- apps/ledger-live-mobile/src/config/bridge-setup.ts
+++ apps/ledger-live-mobile/src/config/bridge-setup.ts
@@ -1,17 +1,19 @@
-import { setCryptoAssetsStore as setCryptoAssetsStoreForCoinFramework } from "@ledgerhq/coin-framework/crypto-assets/index";
-import { getCryptoAssetsStore } from "@ledgerhq/live-common/bridge/crypto-assets/index";
+import { cryptoAssetsApi, createRtkCryptoAssetsStore } from "@ledgerhq/cryptoassets/cal-client";
 import { createCryptoAssetsHooks } from "@ledgerhq/cryptoassets/hooks";
-import { initializeLegacyTokens } from "@ledgerhq/cryptoassets/legacy/legacy-data";
-import { addTokens } from "@ledgerhq/cryptoassets/legacy/legacy-utils";
-import { setup } from "@ledgerhq/live-common/bridge/impl";
-import { legacyCryptoAssetsStore } from "@ledgerhq/cryptoassets/legacy/legacy-store";
+import { setCryptoAssetsStore } from "@ledgerhq/live-common/bridge/crypto-assets/index";
+import { setCryptoAssetsStore as setCryptoAssetsStoreForCoinFramework } from "@ledgerhq/coin-framework/crypto-assets/index";
 import { StoreType } from "../context/store";
 
-initializeLegacyTokens(addTokens);
+export const cryptoAssetsHooks = createCryptoAssetsHooks({
+  useCALBackend: true,
+});
 
-export const cryptoAssetsHooks = createCryptoAssetsHooks({});
+export function setupCryptoAssetsStore(store: StoreType) {
+  const cryptoAssetsStore = createRtkCryptoAssetsStore(cryptoAssetsApi, async <T>(action: T) => {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    return store.dispatch(action as Parameters<typeof store.dispatch>[0]) as unknown;
+  });
 
-export function setupCryptoAssetsStore(_store: StoreType) {
-  setCryptoAssetsStoreForCoinFramework(getCryptoAssetsStore());
-  setup(legacyCryptoAssetsStore);
+  setCryptoAssetsStore(cryptoAssetsStore);
+  setCryptoAssetsStoreForCoinFramework(cryptoAssetsStore);
 }
