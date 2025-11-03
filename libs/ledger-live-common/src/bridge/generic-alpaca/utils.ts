3e54d8442ecd95fd9cbaf140f80c4173b82cc152
Diyaeddine LAOUID <diyaeddine.laouid-ext@ledger.fr>
2025-11-03 14:35:11 +0100

    fix: only display fees on failed sent tx

--- libs/ledger-live-common/src/bridge/generic-alpaca/utils.ts
+++ libs/ledger-live-common/src/bridge/generic-alpaca/utils.ts
@@ -107,15 +107,26 @@ export function adaptCoreOperationToLiveOperation(accountId: string, op: CoreOpe
     extra.memo = op.details.memo as string;
   }
   const bnFees = new BigNumber(op.tx.fees.toString());
+  const hasFailed = op.details?.status === "failed";
+
+  let value: BigNumber;
+  if (hasFailed) {
+    value = bnFees;
+  } else if (
+    op.asset.type === "native" &&
+    ["OUT", "FEES", "DELEGATE", "UNDELEGATE"].includes(opType)
+  ) {
+    value = new BigNumber(op.value.toString()).plus(bnFees);
+  } else {
+    value = new BigNumber(op.value.toString());
+  }
+
   const res = {
     id: encodeOperationId(accountId, op.tx.hash, op.type),
     hash: op.tx.hash,
     accountId,
     type: opType,
-    value:
-      op.asset.type === "native" && ["OUT", "FEES", "DELEGATE", "UNDELEGATE"].includes(opType)
-        ? new BigNumber(op.value.toString()).plus(bnFees)
-        : new BigNumber(op.value.toString()),
+    value,
     fee: bnFees,
     blockHash: op.tx.block.hash,
     blockHeight: op.tx.block.height,
@@ -123,7 +134,7 @@ export function adaptCoreOperationToLiveOperation(accountId: string, op: CoreOpe
     recipients: extra.parentRecipients ?? op.recipients,
     date: op.tx.date,
     transactionSequenceNumber: op.details?.sequence as number,
-    hasFailed: op.details?.status === "failed",
+    hasFailed,
     extra,
   };
 
