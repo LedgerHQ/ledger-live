71a183505ba25bf83926580bb7d47c9ada51cb9f
Louis Aussedat <louis.aussedat@ledger.fr>
2025-11-17 10:40:52 +0100

    dmk: cleanup withDevice to remove queue

--- libs/live-dmk-shared/src/device-action/__test-utils__/makeInternalApi.ts
+++ libs/live-dmk-shared/src/device-action/__test-utils__/makeInternalApi.ts
@@ -9,7 +9,6 @@ const apiGetDeviceSessionStateObservableMock = vi.fn();
 const setDeviceSessionStateMock = vi.fn();
 const getManagerApiServiceMock = vi.fn();
 const getSecureChannelServiceMock = vi.fn();
-const disableRefresherMock = vi.fn();
 
 export function makeDeviceActionInternalApiMock(): Mocked<InternalApi> {
   return {
@@ -21,6 +20,5 @@ export function makeDeviceActionInternalApiMock(): Mocked<InternalApi> {
     setDeviceSessionState: setDeviceSessionStateMock,
     getManagerApiService: getManagerApiServiceMock,
     getSecureChannelService: getSecureChannelServiceMock,
-    disableRefresher: disableRefresherMock,
   };
 }
