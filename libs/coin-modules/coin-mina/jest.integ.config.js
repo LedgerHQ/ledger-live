d8ce2bdc5441a68f304db572f5f842f378a48398
Prateek Rathod <prateek@zondax.ch>
2025-11-19 14:21:40 +0100

    test: fix integ test failure

--- libs/coin-modules/coin-mina/jest.integ.config.js
+++ libs/coin-modules/coin-mina/jest.integ.config.js
@@ -1,8 +1,10 @@
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+// maxWorkers: 1 prevents worker serialization issues with HTTP clients that have circular references
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: ".integ.test.ts$",
   testPathIgnorePatterns: ["lib/", "lib-es/"],
   testTimeout: 60_000,
+  maxWorkers: 1,
 };
