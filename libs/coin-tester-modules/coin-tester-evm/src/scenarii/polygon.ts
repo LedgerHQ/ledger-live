20b83c71a0a73c506a85ce4c93483ae6878e2bde
François Guérin <francois.guerin@ledger.fr>
2025-11-19 19:36:11 +0100

    fix(coin-tester-evm): update Yoot token ID

--- libs/coin-tester-modules/coin-tester-evm/src/scenarii/polygon.ts
+++ libs/coin-tester-modules/coin-tester-evm/src/scenarii/polygon.ts
@@ -21,7 +21,7 @@ const usdcOnPolygon = tokensById["polygon/erc20/usd_coin_(pos)"];
 if (!usdcOnPolygon) throw new Error("USDC on Polygon token not found");
 const USDC_ON_POLYGON = usdcOnPolygon;
 const yootContract = "0x670fd103b1a08628e9557cD66B87DeD841115190";
-const yootTokenId = "1988";
+const yootTokenId = "1953";
 const emberContract = "0xa5511E9941E303101b50675926Fd4d9c1A8a8805";
 const platinumTokenId = "4";
 
