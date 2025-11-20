c33bfc2959710550a18cc6cf5cf1a560c2644f6f
Prateek Rathod <prateek@zondax.ch>
2025-11-20 12:19:08 +0100

    fix: codesmells

--- apps/ledger-live-desktop/src/renderer/families/mina/modals.ts
+++ apps/ledger-live-desktop/src/renderer/families/mina/modals.ts
@@ -1,6 +1,5 @@
 import { MakeModalsType } from "~/renderer/modals/types";
-import MODAL_MINA_STAKE from "./StakingFlowModal";
-import { Props as StakeProps } from "./StakingFlowModal";
+import MODAL_MINA_STAKE, { Props as StakeProps } from "./StakingFlowModal";
 
 export type ModalsData = {
   MODAL_MINA_STAKE: StakeProps;
