42cfde316a70d9178f7b25c76f9fdc7ce3db6073
Pedro Martins <pedro.martins-ext@ledger.fr>
2025-11-21 16:13:36 +0000

    chore: [LIVE-23484] remove getAccountDetails redundant call / add changeset

--- libs/coin-modules/coin-celo/src/bridge/synchronisation.ts
+++ libs/coin-modules/coin-celo/src/bridge/synchronisation.ts
@@ -3,10 +3,10 @@ import { makeSync, mergeOps } from "@ledgerhq/coin-framework/bridge/jsHelpers";
 import type { GetAccountShape } from "@ledgerhq/coin-framework/bridge/jsHelpers";
 import { encodeAccountId } from "@ledgerhq/coin-framework/account";
 import { getEnv } from "@ledgerhq/live-env";
-import { getAccountDetails } from "../network";
 import { CeloAccount } from "../types/types";
 import { celoKit } from "../network/sdk";
 import { getAccountShape as evmGetAccountShape } from "@ledgerhq/coin-evm/bridge/synchronization";
+import { BigNumber } from "bignumber.js";
 
 const kit = celoKit();
 
@@ -23,35 +23,41 @@ export const getAccountShape: GetAccountShape<CeloAccount> = async (info, config
     xpubOrAddress: address,
     derivationMode,
   });
-  const {
-    blockHeight,
-    balance,
-    spendableBalance,
-    operations: newOperations,
-    lockedBalance,
-    nonvotingLockedBalance,
-  } = await getAccountDetails(address, accountId);
 
   const accountRegistrationStatus = await getAccountRegistrationStatus(address);
-
   const pendingWithdrawals = accountRegistrationStatus ? await getPendingWithdrawals(address) : [];
-
   const votes = accountRegistrationStatus ? await getVotes(address) : [];
 
   const fromEvm = await evmGetAccountShape(info, config);
+  const lockedBalance = await lockedGold.getAccountTotalLockedGold(address);
+  const nonvotingLockedBalance = await lockedGold.getAccountNonvotingLockedGold(address);
 
-  const operations = mergeOps(oldOperations, newOperations);
+  const ops = fromEvm.operations || [];
+
+  const newOps = ops.filter((operation, index) => {
+    const includesLockedGoldAddress =
+      operation.recipients?.includes(lockedGold.address) ||
+      operation.senders?.includes(lockedGold.address);
+    const isBiggerThanZero = new BigNumber(operation.value.toString()).isGreaterThan(0);
+    const includeFailed = operation.hasFailed && operation.type !== "OUT";
+
+    return (
+      (!includesLockedGoldAddress && operation.type !== "NONE" && isBiggerThanZero) || includeFailed
+    );
+  });
+
+  const operations = mergeOps(oldOperations, newOps);
 
   const shape: Partial<CeloAccount> = {
     id: accountId,
-    balance,
-    blockHeight,
+    balance: fromEvm.balance?.plus(lockedBalance) || new BigNumber(0),
+    blockHeight: fromEvm.blockHeight || 0,
     operationsCount: operations.length,
-    spendableBalance,
+    spendableBalance: fromEvm.spendableBalance || new BigNumber(0),
     subAccounts: getEnv("ENABLE_CELO_TOKENS") === false ? [] : fromEvm.subAccounts || [],
     syncHash: fromEvm.syncHash,
     celoResources: {
-      registrationStatus: accountRegistrationStatus,
+      registrationStatus: false,
       lockedBalance,
       nonvotingLockedBalance,
       pendingWithdrawals,
