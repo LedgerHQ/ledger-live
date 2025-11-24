9251b77fcb01709723842f19220a2a41a6fc8f3b
gre-ledger <gaetan.renaudeau@ledger.fr>
2025-11-24 14:39:38 +0100

    fix: improve CryptoAssetsStore to return proper network errors
    LIVE-23633

--- libs/ledgerjs/packages/cryptoassets/src/cal-client/state-manager/store.ts
+++ libs/ledgerjs/packages/cryptoassets/src/cal-client/state-manager/store.ts
@@ -1,6 +1,7 @@
 import { TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import { CryptoAssetsStore } from "@ledgerhq/types-live";
 import { CryptoAssetsApi } from "./api";
+import { NetworkDown, LedgerAPI4xx, LedgerAPI5xx } from "@ledgerhq/errors";
 
 /**
  * Simple adapter that implements CryptoAssetsStore interface using RTK Query API directly.
@@ -17,8 +18,7 @@ export class RtkCryptoAssetsStore implements CryptoAssetsStore {
 
   async findTokenById(id: string): Promise<TokenCurrency | undefined> {
     const result = await this.dispatch(this.api.endpoints.findTokenById.initiate({ id }));
-
-    if (result.error) throw result.error;
+    if (result.error) throw remapRtkQueryError(result.error);
     return result.data;
   }
 
@@ -32,14 +32,13 @@ export class RtkCryptoAssetsStore implements CryptoAssetsStore {
         network: currencyId,
       }),
     );
-
-    if (result.error) throw result.error;
+    if (result.error) throw remapRtkQueryError(result.error);
     return result.data;
   }
 
   async getTokensSyncHash(currencyId: string): Promise<string> {
     const result = await this.dispatch(this.api.endpoints.getTokensSyncHash.initiate(currencyId));
-    if (result.error) throw result.error;
+    if (result.error) throw remapRtkQueryError(result.error);
     return result.data;
   }
 
@@ -53,4 +52,20 @@ export function createRtkCryptoAssetsStore(
   dispatch: <T>(action: T) => Promise<any>,
 ): RtkCryptoAssetsStore {
   return new RtkCryptoAssetsStore(api, dispatch);
+}
+
+function remapRtkQueryError(error: { status: string | number; error: string }): Error {
+  // see https://github.com/reduxjs/redux-toolkit/blob/39e7c8fb97e4e1da513d52925eda2898eb17426e/packages/toolkit/etc/rtk-query.api.md?plain=1#L208
+  if (typeof error.status === "number") {
+    const status = error.status;
+    if (status >= 400 && status < 500) {
+      return new LedgerAPI4xx();
+    } else if (status >= 500 && status < 600) {
+      return new LedgerAPI5xx();
+    }
+    return new NetworkDown();
+  } else if (error.status === "FETCH_ERROR") {
+    return new NetworkDown();
+  }
+  return new Error(error.error);
 }
