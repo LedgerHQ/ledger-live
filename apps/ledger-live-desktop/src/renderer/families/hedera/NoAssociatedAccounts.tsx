f2a313d8c6199d4db52b18160e338461350ca8a8
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-11-25 14:52:25 +0100

    fix: useLocalizedUrl usage in coin-hedera

--- apps/ledger-live-desktop/src/renderer/families/hedera/NoAssociatedAccounts.tsx
+++ apps/ledger-live-desktop/src/renderer/families/hedera/NoAssociatedAccounts.tsx
@@ -4,15 +4,18 @@ import { openURL } from "~/renderer/linking";
 import { urls } from "~/config/urls";
 import LinkWithExternalIcon from "~/renderer/components/LinkWithExternalIcon";
 import { StepProps } from "~/renderer/modals/AddAccounts/index";
+import { useLocalizedUrl } from "~/renderer/hooks/useLocalizedUrls";
 
 // "no associated accounts" text when adding/importing accounts
 const NoAssociatedAccounts = ({ t }: StepProps) => {
+  const createAccountSupportUrl = useLocalizedUrl(urls.hedera.supportArticleLink);
+
   return (
     <div>
       <Trans i18nKey="hedera.createHederaAccountHelp.text"></Trans>{" "}
       <LinkWithExternalIcon
         fontSize={3}
-        onClick={() => openURL(urls.hedera.supportArticleLink)}
+        onClick={() => openURL(createAccountSupportUrl)}
         label={t("hedera.createHederaAccountHelp.link")}
       />
     </div>
