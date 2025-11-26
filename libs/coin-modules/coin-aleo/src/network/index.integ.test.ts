293ec67375f4af8dfa6b184d3d7ac4ab7836c846
mateuszpalosz-ext <mateusz.palosz-ext@ledger.fr>
2025-11-26 08:42:28 +0100

    feat: mocked integration test

---
+++ libs/coin-modules/coin-aleo/src/network/index.integ.test.ts
@@ -0,0 +1,8 @@
+// MOCKED INTEGRATION TEST, REPLACE WITH PROPER IMPLEMENTATION LATER
+
+describe("mocked test", () => {
+  it("should always return true value", async () => {
+    const result = true;
+    expect(result).toEqual(true);
+  });
+});
