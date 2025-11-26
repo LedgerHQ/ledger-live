0f867b88c294bd392517ebf399891861b9754492
mateuszpalosz-ext <mateusz.palosz-ext@ledger.fr>
2025-11-26 09:38:43 +0100

    feat: minor fixes

--- libs/coin-modules/coin-aleo/src/test/index.ts
+++ libs/coin-modules/coin-aleo/src/test/index.ts
@@ -1,6 +1,4 @@
-import makeCliTools from "./cli";
-
 export * from "./bridgeDatasetTest";
 export * from "./bot-specs";
 export * from "./speculos-deviceActions";
-export { makeCliTools };
+export { default as makeCliTools } from "./cli";
