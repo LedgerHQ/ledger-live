31910b24a1f228c60540bdbd73c8c0deda10ac34
Raphael <raphael.da-fonseca@ledger.fr>
2025-11-27 08:59:57 +0100

    feat(AddAccountDrawer): log current step in ModularDrawerAddAccountFlowManager

--- apps/ledger-live-desktop/src/newArch/features/AddAccountDrawer/ModularDrawerAddAccountFlowManager.tsx
+++ apps/ledger-live-desktop/src/newArch/features/AddAccountDrawer/ModularDrawerAddAccountFlowManager.tsx
@@ -89,6 +89,8 @@ const ModularDrawerAddAccountFlowManager = ({
   const dispatch = useDispatch();
 
   const renderStepContent = (step: ModularDrawerAddAccountStep) => {
+
+    console.log("step", step);
     switch (step) {
       case MODULAR_DRAWER_ADD_ACCOUNT_STEP.CONNECT_YOUR_DEVICE:
         return (
