0fc191551a16ff1aa7e5a0b41c205cb429055067
Raphael <raphael.da-fonseca@ledger.fr>
2025-12-05 15:21:08 +0100

    feat(AssetSelector): use ldls tailwind config

--- apps/ledger-live-desktop/src/newArch/features/ModularDialog/screens/AssetSelector/components/AssetListItem/index.tsx
+++ apps/ledger-live-desktop/src/newArch/features/ModularDialog/screens/AssetSelector/components/AssetListItem/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { CryptoIcon, AssetType } from "@ledgerhq/react-ui/pre-ldls";
 import { ListItem } from "@ledgerhq/ldls-ui-react";
-import { Text } from "@ledgerhq/react-ui";
 
 type AssetListItemProps = AssetType & {
   onClick: (asset: AssetType) => void;
@@ -25,23 +24,14 @@ export const AssetListItem = ({
   const leadingContent = <CryptoIcon size="48px" ledgerId={id} ticker={ticker} />;
 
   const customDescription = (
-    <div className="flex items-center gap-2">
-      <Text
-        variant="body"
-        fontWeight="medium"
-        fontSize="12px"
-        lineHeight="16px"
-        color="neutral.c70"
-        data-testid={`asset-item-ticker-${ticker}`}
-      >
+    <div className="flex items-center gap-8">
+      <span className="text-muted body-3" data-testid={`asset-item-ticker-${ticker}`}>
         {ticker}
-      </Text>
+      </span>
       {leftElement}
       {shouldDisplayId && assetId && (
-        <div className="inline-flex px-2 py-0.5 rounded bg-palette-background-default shrink-0">
-          <Text variant="body" fontSize="12px" color="neutral.c70">
-            {`${assetId} (${numberOfNetworks} networks)`}
-          </Text>
+        <div className="bg-muted inline-flex rounded-sm px-8 py-2">
+          <span className="body-4 text-muted">{`${assetId} (${numberOfNetworks} networks)`}</span>
         </div>
       )}
     </div>
