0e76de8e1f73b75c55a3e047ea89482955affaca
Alessandro Vito <alessandro.vito84@gmail.com>
2025-12-05 17:24:45 +0000

    added dummy methods for estimating sync time, finding block height and decrypting a transaction

---
+++ libs/ledgerjs/packages/zcash-shielded/src/ZCash.ts
@@ -0,0 +1,37 @@
+/**
+ * ZCash API
+ */
+
+export default class ZCash {
+  static AVERAGE_BLOCK_SYNC_TIME = 5;
+
+  /**
+   * Estimates sync time given a starting block height.
+   *
+   * @param blockHeight
+   * @return the estimated sync time
+   */
+  async estimateSyncTime(startBlock: number, endBlock: number) {
+    return (endBlock - startBlock) * ZCash.AVERAGE_BLOCK_SYNC_TIME;
+  }
+
+  /**
+   * Decrypts a ZCash shielded - i.e., encrypted - transaction.
+   *
+   * @param encryptedTransaction string raw string representing an encrypted transaction.
+   * @return a decrypted transaction
+   */
+  async decryptTransaction(encryptedTransaction: string): Promise<string> {
+    return `decrypted_${encryptedTransaction}`;
+  }
+
+  /**
+   * Finds the lowest block height correspondent to a given timestamp
+   *
+   * @param timestamp
+   * @return a block height
+   */
+  async findBlockHeight(timestamp: number): Promise<number> {
+    return timestamp + 42;
+  }
+}
---
+++ libs/ledgerjs/packages/zcash-shielded/tests/ZCash.test.ts
@@ -0,0 +1,26 @@
+import { describe, test, expect } from "vitest";
+import ZCash from "../src/ZCash";
+
+describe("estimateSyncTime", () => {
+  test("estimates the sync time", async () => {
+    const zcash = new ZCash();
+    const estimatedSyncTime = await zcash.estimateSyncTime(5, 10);
+    expect(estimatedSyncTime).toEqual(25);
+  });
+});
+
+describe("findBlockHeight", () => {
+  test("finds the lowest block height", async () => {
+    const zcash = new ZCash();
+    const height = await zcash.findBlockHeight(1234);
+    expect(height).toEqual(1276);
+  });
+});
+
+describe("decryptTransaction", () => {
+  test("decrypts an encrypted transaction", async () => {
+    const zcash = new ZCash();
+    const height = await zcash.decryptTransaction("transaction");
+    expect(height).toEqual("decrypted_transaction");
+  });
+});
