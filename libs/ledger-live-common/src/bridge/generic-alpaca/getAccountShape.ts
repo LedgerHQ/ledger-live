2971a2678978b35fc3a242de330e8e3a3b3edbc0
François Guérin <francois.guerin@ledger.fr>
2025-12-05 08:21:40 +0100

    fix(llc): do not return failed incoming operations

--- libs/ledger-live-common/src/bridge/generic-alpaca/getAccountShape.ts
+++ libs/ledger-live-common/src/bridge/generic-alpaca/getAccountShape.ts
@@ -17,6 +17,15 @@ function isNftCoreOp(operation: Operation): boolean {
   );
 }
 
+function isIncomingCoreOp(operation: Operation): boolean {
+  const type =
+    typeof operation.details?.ledgerOpType === "string"
+      ? operation.details.ledgerOpType
+      : operation.type;
+
+  return type === "IN";
+}
+
 function isInternalLiveOp(operation: OperationCommon): boolean {
   return !!operation.extra?.internal;
 }
@@ -69,7 +78,7 @@ export function genericGetAccountShape(network: string, kind: string): GetAccoun
 
     const [newCoreOps] = await alpacaApi.listOperations(address, paginationParams);
     const newOps = newCoreOps
-      .filter(op => !isNftCoreOp(op))
+      .filter(op => !isNftCoreOp(op) && (!isIncomingCoreOp(op) || !op.tx.failed))
       .map(op => adaptCoreOperationToLiveOperation(accountId, op)) as OperationCommon[];
 
     const newAssetOperations = newOps.filter(
