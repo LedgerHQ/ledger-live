3ac5f26111f8596327fa7e588e514509de3f8a59
Mateusz Domański <mateusz.domanski-ext@ledger.fr>
2025-12-08 15:42:49 +0100

    feat: live-signer-aleo based with dmk mock

---
+++ .changeset/slow-candles-double.md
@@ -0,0 +1,14 @@
+---
+"@ledgerhq/types-cryptoassets": minor
+"@ledgerhq/cryptoassets": minor
+"@ledgerhq/types-live": minor
+"@ledgerhq/coin-aleo": minor
+"ledger-live-desktop": minor
+"live-mobile": minor
+"@ledgerhq/live-common": minor
+"@ledgerhq/live-signer-aleo": minor
+"@ledgerhq/coin-framework": minor
+"@ledgerhq/live-cli": minor
+---
+
+feat: coin-aleo template
--- libs/ledger-live-common/src/families/aleo/setup.ts
+++ libs/ledger-live-common/src/families/aleo/setup.ts
@@ -1,16 +1,27 @@
 // Goal of this file is to inject all necessary device/signer dependency to coin-modules
 
+import invariant from "invariant";
+import type { DeviceManagementKit } from "@ledgerhq/device-management-kit";
+import { DmkSignerAleo } from "@ledgerhq/live-signer-aleo";
 import { createBridges } from "@ledgerhq/coin-aleo/bridge/index";
 import makeCliTools from "@ledgerhq/coin-aleo/test/cli";
 import aleoResolver from "@ledgerhq/coin-aleo/signer/index";
 import type { AleoSigner, Transaction as AleoTransaction } from "@ledgerhq/coin-aleo/types/index";
-import Transport from "@ledgerhq/hw-transport";
+import type Transport from "@ledgerhq/hw-transport";
 import type { Bridge } from "@ledgerhq/types-live";
-import { CreateSigner, createResolver, executeWithSigner } from "../../bridge/setup";
-import { Resolver } from "../../hw/getAddress/types";
+import { type CreateSigner, createResolver, executeWithSigner } from "../../bridge/setup";
+import type { Resolver } from "../../hw/getAddress/types";
 
-const createSigner: CreateSigner<AleoSigner> = (_transport: Transport) => {
-  throw new Error("Aleo signer is not yet implemented");
+type TransportWithDmk = Transport &
+  Partial<{
+    dmk: DeviceManagementKit;
+    sessionId: string;
+  }>;
+
+const createSigner: CreateSigner<AleoSigner> = (transport: TransportWithDmk) => {
+  invariant(transport.dmk, "aleo: transport.dmk is missing");
+  invariant(transport.sessionId, "aleo: transport.sessionId is missing");
+  return new DmkSignerAleo(transport.dmk, transport.sessionId);
 };
 
 const bridge: Bridge<AleoTransaction> = createBridges(executeWithSigner(createSigner));
