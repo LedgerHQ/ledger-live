78592cb7e75eefb22c9e310668d7ef0ef57166ce
Raphael <raphael.da-fonseca@ledger.fr>
2025-12-11 16:25:51 +0100

    feat(accountList): update comment

--- apps/ledger-live-desktop/src/newArch/features/ModularDialog/components/Address/formatAddress.ts
+++ apps/ledger-live-desktop/src/newArch/features/ModularDialog/components/Address/formatAddress.ts
@@ -3,8 +3,8 @@
  *
  * @param address - The address to format
  * @param options - Configuration options
- * @param options.prefixLength - Number of characters to show at the start (default: 5)
- * @param options.suffixLength - Number of characters to show at the end (default: 5)
+ * @param options.prefixLength - Number of characters to show at the start (default: 4)
+ * @param options.suffixLength - Number of characters to show at the end (default: 4)
  * @param options.separator - String to use as separator (default: "...")
  * @param options.threshold - Minimum length before truncating (default: calculated from other params)
  * @returns Formatted address string
