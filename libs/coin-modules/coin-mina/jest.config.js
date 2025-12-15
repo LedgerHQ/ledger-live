57e8a027a24d3d79909572945d9048b1fea58e4d
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2025-12-15 15:17:43 +0100

    chore: add reporters setup in sui/mina jest config

--- libs/coin-modules/coin-mina/jest.config.js
+++ libs/coin-modules/coin-mina/jest.config.js
@@ -5,5 +5,26 @@ module.exports = {
   testEnvironment: "node",
   testPathIgnorePatterns: ["lib/", "lib-es/", ".*\\.integ\\.test\\.[tj]s"],
   workerThreads: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+    "!src/test/**/*.ts",
+    "!src/__tests__/**/*.ts",
+  ],
+  coverageReporters: ["json", ["lcov", { file: "lcov.info", projectRoot: "../../../" }], "text"],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["lib/", "lib-es/", ".integ.test.ts"],
+  reporters: [
+    "default",
+    ["jest-sonar", { outputName: "sonar-executionTests-report.xml", reportedFilePath: "absolute" }],
+  ],
+  coveragePathIgnorePatterns: [
+    "src/test",
+    "src/types",
+    "src/index.ts",
+    "src/bridge/bridge.fixture.ts",
+  ],
+
   setupFilesAfterEnv: ["@ledgerhq/disable-network-setup"],
 };
--- libs/coin-modules/coin-sui/jest.config.js
+++ libs/coin-modules/coin-sui/jest.config.js
@@ -3,8 +3,13 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts"],
   coveragePathIgnorePatterns: ["test/cli.ts", ".*\\.integration\\.test\\.[tj]s"],
   coverageDirectory: "coverage",
+  coverageReporters: ["json", ["lcov", { file: "lcov.info", projectRoot: "../../../" }], "text"],
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["lib/", "lib-es/", ".*\\.integration\\.test\\.[tj]s"],
+  reporters: [
+    "default",
+    ["jest-sonar", { outputName: "sonar-executionTests-report.xml", reportedFilePath: "absolute" }],
+  ],
   setupFilesAfterEnv: ["@ledgerhq/disable-network-setup"],
 };
