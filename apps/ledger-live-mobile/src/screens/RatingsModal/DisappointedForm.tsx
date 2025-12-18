b33de0c085220315b77812f31cafe1be6e9579ef
Tony Khaov <tony.khaov@gmail.com>
2025-12-18 10:13:48 +0100

    fix: feedbacks from typeform are not always received

--- apps/ledger-live-mobile/src/screens/RatingsModal/DisappointedForm.tsx
+++ apps/ledger-live-mobile/src/screens/RatingsModal/DisappointedForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { Flex } from "@ledgerhq/native-ui";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 import VersionNumber from "react-native-version-number";
@@ -14,36 +14,48 @@ import { useSettings } from "~/hooks";
 
 const { height } = getWindowDimensions();
 
-const appVersion = `${VersionNumber.appVersion || ""} (${VersionNumber.buildVersion || ""})`;
+const appVersion = `${VersionNumber.appVersion} (${VersionNumber.buildVersion})`;
 
-const injectedJavascript = `
-const submitInterval = setInterval(addListenerOnFormSubmitButton, 100);
-const alreadySubmittedInterval = setInterval(addListenerOnFormAlreadySubmittedButton, 100);
+const INTERNAL_FORM_COMPLETED_EVENT = "internal-form-completed";
+const TYPEFORM_COMPLETE_SUBMISSION_REQUEST_ENDPOINT = "/complete-submission";
 
-function addListenerOnFormSubmitButton() {
-  const submitButton = document.querySelector('button[data-qa*="submit-button"]');
-  if (submitButton) {
-    clearInterval(submitInterval);
-    clearInterval(alreadySubmittedInterval);
-    submitButton.addEventListener('click', () => {
-      window.ReactNativeWebView.postMessage('form-submit');
-    });
-  }
-}
+// TODO: Find a way to not code inside a string because we lose syntax highlighting, lint checking, etc.
+const formInjectedJavaScript = `
+  // Proxying network requests is the best way to handle typeform events as the website heavily relies on JS:
+  // We can't use the url to detect when the form is completed as it does not update the URL
+  // We can find a way to use the DOM to detect when the form is completed but we don't want users to see that
+  // Hence intercepting the fetch call and checking the url is the most reliable and UX friendly way to handle this.
 
-// If the user was too quick and we didn't catch the click on
-// the submit button but the form has already been submitted
-function addListenerOnFormAlreadySubmittedButton() {
-  const alreadySubmittedButton = document.querySelector('button[data-qa*="thank-you-button"]');
-  window.ReactNativeWebView.postMessage(alreadySubmittedButton);
-  if (alreadySubmittedButton) {
-    clearInterval(submitInterval);
-    clearInterval(alreadySubmittedInterval);
-    window.ReactNativeWebView.postMessage('form-submit');
-  }
-}
+  window.fetch = new Proxy(window.fetch, {
+    apply(actualFetch, that, args) {
+      // Forward function call to the original fetch
+      const result = Reflect.apply(actualFetch, that, args);
 
-true;
+      // Handle form events
+      result
+        .then(response => {
+          try {
+            const url = new URL(response.url);
+
+            if (url.hostname !== "form.typeform.com") {
+              return;
+            }
+
+            if (url.pathname.endsWith("${TYPEFORM_COMPLETE_SUBMISSION_REQUEST_ENDPOINT}")) {
+              window.ReactNativeWebView.postMessage("${INTERNAL_FORM_COMPLETED_EVENT}");
+              return;
+            }
+          } catch (e) {
+            // Silently ignore errors from URL parsing or missing properties
+          }
+        })
+        .catch(function () {
+          // Swallow errors to avoid unhandled promise rejections from the proxied fetch
+        });
+
+      return result;
+    },
+  });
 `;
 
 const StyledWebview = styled(WebView)`
@@ -60,14 +72,9 @@ const DisappointedForm = ({ setStep, equipmentId }: Props) => {
   const { ratingsHappyMoment, ratingsFeatureParams } = useRatings();
   const { language } = useSettings();
   const devices = useSelector(bleDevicesSelector);
-  const lastDevice = useSelector(lastSeenDeviceSelector) || devices[devices.length - 1];
+  const lastDevice = useSelector(lastSeenDeviceSelector) ?? devices.at(-1);
 
   const notifications = useSelector(notificationsSelector);
-  const notificationsAllowed = notifications.areNotificationsAllowed;
-  const notificationsBlacklisted = Object.entries(notifications)
-    .filter(([key, value]) => key !== "areNotificationsAllowed" && value === false)
-    .map(([key]) => key)
-    .join(",");
 
   const onLoadEnd = useCallback(() => {
     track("button_clicked", {
@@ -78,41 +85,74 @@ const DisappointedForm = ({ setStep, equipmentId }: Props) => {
       params: ratingsFeatureParams,
     });
   }, [ratingsFeatureParams, ratingsHappyMoment?.route_name]);
+
   const onMessage = useCallback(
     (event: WebViewMessageEvent) => {
       const { data } = event.nativeEvent;
 
-      if (data === "form-submit") {
-        track("button_clicked", {
-          flow: "review",
-          page: "review_disappointedstep2",
-          button: "form_submitted",
-          source: ratingsHappyMoment?.route_name,
-          params: ratingsFeatureParams,
-        });
-        setStep("disappointedDone");
+      switch (data) {
+        case INTERNAL_FORM_COMPLETED_EVENT: {
+          track("button_clicked", {
+            flow: "review",
+            page: "review_disappointedstep2",
+            button: "form_submitted",
+            source: ratingsHappyMoment?.route_name,
+            params: ratingsFeatureParams,
+          });
+          setStep("disappointedDone");
+          break;
+        }
+        default: {
+          console.warn("Unknown form event:", data);
+          break;
+        }
       }
     },
     [ratingsFeatureParams, ratingsHappyMoment?.route_name, setStep],
   );
 
-  const formUrlSplitted = ratingsFeatureParams?.typeform_url.split("?");
+  const formUrl = useMemo(() => {
+    if (!ratingsFeatureParams?.typeform_url) {
+      throw new Error(`Typeform URL is required, please set one in the ratings feature params`);
+    }
 
-  let formUrl =
-    formUrlSplitted?.[0] +
-    `#app_version=${appVersion}` +
-    `&app_language=${language}` +
-    `&platform_os=${Platform.OS}` +
-    `&platform_version=${Platform.Version}` +
-    `&model_id=${lastDevice?.modelId}` +
-    `&firmware_version=${lastDevice?.deviceInfo?.version}` +
-    `&notifications_allowed=${notificationsAllowed}` +
-    `&notifications_blacklisted=${notificationsBlacklisted}` +
-    `&done?${formUrlSplitted?.[1] || ""}`;
+    let url: URL;
+    try {
+      url = new URL(ratingsFeatureParams.typeform_url);
+    } catch {
+      throw new Error(
+        `Typeform URL is an invalid URL, Currently having: ${ratingsFeatureParams?.typeform_url}`,
+      );
+    }
 
-  if (equipmentId) {
-    formUrl += `&equipment_id=${equipmentId}`;
-  }
+    url.hash = `app_version=${appVersion}`;
+
+    url.searchParams.set("app_language", language);
+    url.searchParams.set("platform_os", Platform.OS);
+    url.searchParams.set("platform_version", Platform.Version.toString());
+    url.searchParams.set("model_id", lastDevice?.modelId ?? "");
+    url.searchParams.set("firmware_version", lastDevice?.deviceInfo?.version ?? "");
+
+    url.searchParams.set("notifications_allowed", notifications.areNotificationsAllowed.toString());
+    const notificationsBlacklisted = Object.entries(notifications)
+      .filter(([key, value]) => key !== "areNotificationsAllowed" && value === false)
+      .map(([key]) => key)
+      .join(",");
+    url.searchParams.set("notifications_blacklisted", notificationsBlacklisted);
+
+    if (equipmentId) {
+      url.searchParams.set("equipment_id", equipmentId);
+    }
+
+    return url.toString();
+  }, [
+    ratingsFeatureParams?.typeform_url,
+    equipmentId,
+    language,
+    lastDevice?.modelId,
+    lastDevice?.deviceInfo?.version,
+    notifications,
+  ]);
 
   return (
     <Flex flex={1} height={height * (4 / 5)}>
@@ -126,9 +166,9 @@ const DisappointedForm = ({ setStep, equipmentId }: Props) => {
       />
       <Flex flex={1} borderRadius={16} overflow="hidden">
         <StyledWebview
-          source={{ uri: encodeURI(formUrl) }}
-          originWhitelist={["*"]}
-          injectedJavaScript={injectedJavascript}
+          source={{ uri: formUrl }}
+          originWhitelist={["https://form.typeform.com"]}
+          injectedJavaScript={formInjectedJavaScript}
           onLoadEnd={onLoadEnd}
           onMessage={onMessage}
         />
