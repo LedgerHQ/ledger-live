c0a5d2f5b0706232479af5bcaa72ba07c8cd9df2
Ilia Shabanov <ishaba@me.com>
2025-12-24 17:08:23 +0300

    rebase fixes

--- libs/coin-modules/coin-canton/src/bridge/validateAddress.ts
+++
@@ -1,9 +0,0 @@
-import { isRecipientValid } from "../common-logic";
-import { AddressValidationCurrencyParameters } from "@ledgerhq/types-live";
-
-export async function validateAddress(
-  address: string,
-  _parameters: Partial<AddressValidationCurrencyParameters>,
-): Promise<boolean> {
-  return isRecipientValid(address);
-}
