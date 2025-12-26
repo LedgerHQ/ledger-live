2232735c71af148547a7bdfbc537549a091d09a2
Tony Khaov <tony.khaov@gmail.com>
2025-12-26 10:52:09 +0100

    feat: call on backdrop press

--- apps/ledger-live-mobile/src/screens/PushNotificationsModal/index.tsx
+++ apps/ledger-live-mobile/src/screens/PushNotificationsModal/index.tsx
@@ -14,6 +14,7 @@ const PushNotificationsModal = () => {
     isPushNotificationsModalOpen,
     handleAllowNotificationsPress,
     handleDelayLaterPress,
+    handleCloseFromBackdropPress,
   } = useNotifications();
 
   useEffect(() => {
@@ -22,7 +23,11 @@ const PushNotificationsModal = () => {
   }, []);
 
   return (
-    <QueuedDrawer isRequestingToBeOpened={isPushNotificationsModalOpen} noCloseButton>
+    <QueuedDrawer
+      isRequestingToBeOpened={isPushNotificationsModalOpen}
+      noCloseButton
+      onBackdropPress={handleCloseFromBackdropPress}
+    >
       <PushNotificationsModalTrackScreen type={pushNotificationsModalType} />
 
       <Flex mb={4}>
