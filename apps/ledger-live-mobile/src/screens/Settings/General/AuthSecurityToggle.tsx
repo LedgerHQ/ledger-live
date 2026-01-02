82d8b0145e634aba56a9737adf39b4dba12c3285
RobinVncnt <robin.vincent@ledger.fr>
2026-01-02 11:40:26 +0100

    fix: toggle app locked

--- apps/ledger-live-mobile/src/screens/Settings/General/AuthSecurityToggle.tsx
+++ apps/ledger-live-mobile/src/screens/Settings/General/AuthSecurityToggle.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useCallback } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { useSelector } from "~/context/store";
 import { useTranslation } from "react-i18next";
 import { Switch } from "@ledgerhq/native-ui";
@@ -16,12 +16,22 @@ export default function AuthSecurityToggle() {
   const privacy = useSelector(privacySelector);
   const { navigate } = useNavigation();
 
+  const [isToggleOn, setIsToggleOn] = useState(!!privacy?.hasPassword);
+
+  useFocusEffect(
+    useCallback(() => {
+      setIsToggleOn(!!privacy?.hasPassword);
+    }, [privacy?.hasPassword]),
+  );
+
   const onValueChange = (authSecurityEnabled: boolean): void => {
     track("toggle_clicked", {
       toggle: "Password Lock",
       page: ScreenName.GeneralSettings,
       enabled: !privacy?.hasPassword,
     });
+
+    setIsToggleOn(authSecurityEnabled);
 
     navigate(
       authSecurityEnabled ? NavigatorName.PasswordAddFlow : NavigatorName.PasswordModifyFlow,
@@ -40,11 +50,7 @@ export default function AuthSecurityToggle() {
         title={t("settings.display.password")}
         desc={getPasswordDesc()}
       >
-        <Switch
-          checked={!!privacy?.hasPassword}
-          onChange={onValueChange}
-          testID="password-settings-switch"
-        />
+        <Switch checked={isToggleOn} onChange={onValueChange} testID="password-settings-switch" />
       </SettingsRow>
       <BiometricsRow />
     </>
