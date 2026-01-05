54cd18e2d195dbc4779cf9046bc38c039a78122c
Lucas Werey <lucas.werey@ledger.fr>
2026-01-05 09:53:46 +0100

    :package: (chore) bump reselect

---
+++ .changeset/hip-paws-walk.md
@@ -0,0 +1,7 @@
+---
+"ledger-live-desktop": minor
+"live-mobile": minor
+"@ledgerhq/live-common": minor
+---
+
+Bump reselect to latest version and use the catalog
--- apps/ledger-live-mobile/src/components/SubAccountRow.tsx
+++ apps/ledger-live-mobile/src/components/SubAccountRow.tsx
@@ -2,9 +2,9 @@ import { getAccountCurrency } from "@ledgerhq/live-common/account/index";
 import React, { memo } from "react";
 import { RectButton, Gesture, GestureDetector } from "react-native-gesture-handler";
 import { TokenAccount, Account } from "@ledgerhq/types-live";
-import { createStructuredSelector } from "reselect";
-import { connect } from "react-redux";
 import { useSelector } from "~/context/store";
+import { createStructuredSelector } from "~/context/selectors";
+import { connect } from "react-redux";
 import { Box, Flex, Text } from "@ledgerhq/native-ui";
 import CurrencyUnitValue from "./CurrencyUnitValue";
 import CounterValue from "./CounterValue";
@@ -13,7 +13,6 @@ import { accountSelector } from "~/reducers/accounts";
 import { selectedTimeRangeSelector } from "~/reducers/settings";
 import { useBalanceHistoryWithCountervalue } from "~/hooks/portfolio";
 import Delta from "./Delta";
-import { State as RootState } from "~/reducers/types";
 import { useAccountName } from "~/reducers/wallet";
 import { useAccountUnit } from "LLM/hooks/useAccountUnit";
 
@@ -100,13 +99,7 @@ const AccountCv = ({ children }: { children?: React.ReactNode }) => (
   </Text>
 );
 
-const mapStateToProps = createStructuredSelector<
-  RootState,
-  { accountId?: string; parentAccount?: Account },
-  {
-    parentAccount: Account | undefined;
-  }
->({
+const mapStateToProps = createStructuredSelector({
   parentAccount: accountSelector,
 });
 
