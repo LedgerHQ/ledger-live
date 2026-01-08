3dfd1fe41766de744aae6a262966f35fc810d786
Abdurrahman Sastim <abdurrahman.sastim@ledger.fr>
2026-01-08 14:34:13 +0100

    test: clear connections

--- e2e/mobile/setup.ts
+++ e2e/mobile/setup.ts
@@ -4,9 +4,34 @@ import { close as closeBridge, getLogs } from "./bridge/server";
 import { launchApp, setupEnvironment } from "./helpers/commonHelpers";
 import { getEnv, setEnv } from "@ledgerhq/live-env";
 import { allure } from "jest-allure2-reporter/api";
+import https from "https";
+import http from "http";
 
 const broadcastOriginalValue = getEnv("DISABLE_TRANSACTION_BROADCAST");
 setupEnvironment();
+
+/**
+ * Cleans up HTTP/HTTPS connections and global state to prevent TLS socket serialization errors.
+ * This is needed because jest-worker uses @ungap/structured-clone to serialize test results,
+ * which can fail if the state contains references to destroyed TLS sockets from remote
+ * Speculos HTTPS connections.
+ */
+function cleanupConnections() {
+  // Force close any lingering HTTP/HTTPS connections
+  https.globalAgent.destroy();
+  http.globalAgent.destroy();
+
+  // Clear global state that might hold socket references
+  if (globalThis.speculosDevices) {
+    globalThis.speculosDevices.clear();
+  }
+  if (globalThis.proxySubscriptions) {
+    globalThis.proxySubscriptions.clear();
+  }
+
+  // Force garbage collection if available
+  global.gc?.();
+}
 
 beforeAll(
   async () => {
@@ -21,8 +46,12 @@ beforeAll(
 );
 
 afterAll(async () => {
+  if (IS_FAILED && process.env.CI) {
+    await allure.attachment("App logs", await getLogs(), "application/json");
+  }
   setEnv("DISABLE_TRANSACTION_BROADCAST", broadcastOriginalValue);
   closeBridge();
   closeProxy();
   await app.common.removeSpeculos();
+  cleanupConnections();
 });
