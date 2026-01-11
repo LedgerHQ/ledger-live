cdbeb45b5d1b153b4dd83e9d6c4114ee36f5c182
Abdurrahman Sastim <abdurrahman.sastim@ledger.fr>
2026-01-11 02:26:17 +0100

    ci: test 5 workers

--- e2e/mobile/detox.config.js
+++ e2e/mobile/detox.config.js
@@ -106,6 +106,18 @@ module.exports = {
         name: "iOS Simulator 3",
       },
     },
+    simulator4: {
+      type: "ios.simulator",
+      device: {
+        name: "iOS Simulator 4",
+      },
+    },
+    simulator5: {
+      type: "ios.simulator",
+      device: {
+        name: "iOS Simulator 5",
+      },
+    },
     emulator: {
       type: "android.emulator",
       device: {
