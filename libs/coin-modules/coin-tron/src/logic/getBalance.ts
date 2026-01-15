36aaf487c15da117a23332de376257ce8e6582a9
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2026-01-15 09:21:06 +0100

    return empty balance when account is pristine

--- libs/coin-modules/coin-tron/src/logic/getBalance.ts
+++ libs/coin-modules/coin-tron/src/logic/getBalance.ts
@@ -10,8 +10,8 @@ const bigIntOrZero = (val: number | BigNumber | undefined | null): bigint =>
 export async function getBalance(address: string): Promise<Balance[]> {
   const accounts = await fetchTronAccount(address);
 
-  // if account is not activated, an empty array is returned
-  if (accounts.length === 0) return [];
+  // if account is not activated, an empty balance is returned
+  if (accounts.length === 0) return [{ value: BigInt(0), asset: { type: "native" } }];
 
   const account = accounts[0];
 
