fa7fcefcf106ef3cb645fe825b882af54086e812
Martin Cayuelas <martin.cayuelas@ledger.fr>
2026-01-19 16:53:03 +0100

    feat: remove MarketWidget feature

--- apps/ledger-live-desktop/tests/specs/general/portfolio.spec.ts
+++ apps/ledger-live-desktop/tests/specs/general/portfolio.spec.ts
@@ -1,12 +1,10 @@
 import test from "../../fixtures/common";
 import { expect } from "@playwright/test";
-import { Layout } from "../../component/layout.component";
 import { PortfolioPage } from "../../page/portfolio.page";
 
 test.use({ userdata: "1AccountBTC1AccountETHStarred" });
 
 test("Portfolio @smoke", async ({ page }) => {
-  const layout = new Layout(page);
   const portfolioPage = new PortfolioPage(page);
 
   await test.step("load portfolio", async () => {
@@ -14,9 +12,7 @@ test("Portfolio @smoke", async ({ page }) => {
     await portfolioPage.checkBuySellButtonVisibility();
     await portfolioPage.checkSwapButtonVisibility();
     await portfolioPage.checkStakeButtonVisibility();
-    await expect.soft(page).toHaveScreenshot("portfolio.png", {
-      mask: [layout.marketPerformanceWidget],
-    });
+    await expect.soft(page).toHaveScreenshot("portfolio.png");
   });
 
   await test.step(`scroll to operations`, async () => {
