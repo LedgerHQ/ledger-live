244d83f4adbe3623ba16bdf9c06ea7bb603f3a93
Emil Kashkevich <emil.kashkevich@gmail.com>
2026-01-19 10:54:26 +0100

    fix: handle 404 on /v0/keyAccounts endpoint

--- libs/coin-modules/coin-concordium/src/bridge/sync.ts
+++ libs/coin-modules/coin-concordium/src/bridge/sync.ts
@@ -38,12 +38,61 @@ export const getAccountShape: GetAccountShape<ConcordiumAccount> = async info =>
     derivationMode,
   });
 
-  const accountsResponse = await getAccountsByPublicKey(currency, publicKey);
-
   let balance = new BigNumber(0);
   let spendableBalance = new BigNumber(0);
 
-  if (!accountsResponse || accountsResponse.length === 0) {
+  try {
+    const accountsResponse = await getAccountsByPublicKey(currency, publicKey);
+
+    if (!accountsResponse || accountsResponse.length === 0) {
+      throw new Error("No accounts found");
+    }
+
+    // The actual on-chain account
+    const account = accountsResponse[0];
+
+    const balanceResponse = await getAccountBalance(currency, account.address);
+    const { finalizedBalance: { accountAmount, accountAtDisposal } = {} } = balanceResponse;
+
+    balance = valueToBigNumber(accountAmount);
+
+    const minReserve = coinConfig.getCoinConfig(currency).minReserve;
+    spendableBalance = accountAtDisposal
+      ? valueToBigNumber(accountAtDisposal)
+      : balance.minus(minReserve);
+    spendableBalance = spendableBalance.isNegative() ? new BigNumber(0) : spendableBalance;
+
+    const oldOperations = initialAccount?.operations ?? [];
+    const startAt = oldOperations.length ? (oldOperations[0].blockHeight ?? 0) + 1 : 0;
+
+    const newOperations = await getOperations(currency, account.address, accountId, {
+      from: startAt,
+      size: 100,
+    }).catch((): Operation[] => []);
+
+    const operations = mergeOps(oldOperations, newOperations);
+
+    const used = !balance.isNegative() || operations.length > 0;
+
+    const accountShape = {
+      balance,
+      blockHeight: operations[0]?.blockHeight ?? 0,
+      concordiumResources: fillConcordiumResources(initialAccount?.concordiumResources, {
+        publicKey,
+      }),
+      derivationMode,
+      derivationPath,
+      id: accountId,
+      index,
+      operations,
+      operationsCount: operations.length,
+      spendableBalance,
+      used,
+      xpub: publicKey,
+    };
+
+    return accountShape;
+  } catch (error) {
     // No accounts found for this public key - return empty account shape. We're ready to create a new account.
     const newAccountShape = {
       balance,
@@ -65,49 +114,4 @@ export const getAccountShape: GetAccountShape<ConcordiumAccount> = async info =>
 
     return newAccountShape;
   }
-
-  // The actual on-chain account
-  const account = accountsResponse[0];
-
-  const balanceResponse = await getAccountBalance(currency, account.address);
-  const { finalizedBalance: { accountAmount, accountAtDisposal } = {} } = balanceResponse;
-
-  balance = valueToBigNumber(accountAmount);
-
-  const minReserve = coinConfig.getCoinConfig(currency).minReserve;
-  spendableBalance = accountAtDisposal
-    ? valueToBigNumber(accountAtDisposal)
-    : balance.minus(minReserve);
-  spendableBalance = spendableBalance.isNegative() ? new BigNumber(0) : spendableBalance;
-
-  const oldOperations = initialAccount?.operations ?? [];
-  const startAt = oldOperations.length ? (oldOperations[0].blockHeight ?? 0) + 1 : 0;
-
-  const newOperations = await getOperations(currency, account.address, accountId, {
-    from: startAt,
-    size: 100,
-  }).catch((): Operation[] => []);
-
-  const operations = mergeOps(oldOperations, newOperations);
-
-  const used = !balance.isNegative() || operations.length > 0;
-
-  const accountShape = {
-    balance,
-    blockHeight: operations[0]?.blockHeight ?? 0,
-    concordiumResources: fillConcordiumResources(initialAccount?.concordiumResources, {
-      publicKey,
-    }),
-    derivationMode,
-    derivationPath,
-    id: accountId,
-    index,
-    operations,
-    operationsCount: operations.length,
-    spendableBalance,
-    used,
-    xpub: publicKey,
-  };
-
-  return accountShape;
 };
