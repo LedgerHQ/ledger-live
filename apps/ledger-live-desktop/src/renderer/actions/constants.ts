8cf6fa8271d1a9780c692480752fe6ec84fddeca
Martin Cayuelas <martin.cayuelas@ledger.fr>
2026-01-19 16:53:03 +0100

    feat: remove MarketWidget feature

--- apps/ledger-live-desktop/src/renderer/actions/constants.ts
+++ apps/ledger-live-desktop/src/renderer/actions/constants.ts
@@ -3,7 +3,6 @@
 /** Settings --------- */
 export const PURGE_EXPIRED_ANONYMOUS_USER_NOTIFICATIONS =
   "settings/purgeExpiredAnonymousUserNotifications";
-export const TOGGLE_MARKET_WIDGET = "settings/toggleMarketWidget";
 export const TOGGLE_MEMOTAG_INFO = "settings/toggleShouldDisplayMemoTagInfo";
 export const TOGGLE_MEV = "settings/toggleMEV";
 export const UPDATE_ANONYMOUS_USER_NOTIFICATIONS = "settings/updateAnonymousUserNotifications";
