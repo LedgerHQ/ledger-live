8cd8de638cfdc17e4fb69e9e154099715b9bd3c6
mateuszpalosz-ext <mateusz.palosz-ext@ledger.fr>
2026-01-21 13:34:01 +0100

    feat: custom type translations

--- apps/ledger-live-desktop/src/renderer/families/aleo/CustomMetadataCell.tsx
+++ apps/ledger-live-desktop/src/renderer/families/aleo/CustomMetadataCell.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { AleoFamily } from "./types";
+import { Box, Text } from "@ledgerhq/react-ui/index";
+import { Trans } from "react-i18next";
 
-const MyCustomMetadataCell: AleoFamily["CustomMetadataCell"] = ({ operation }) => {
-  return <div>CUSTOM</div>;
+const mapFunctionIdToName: Record<string, string> = {
+  transfer_public: "aleo.operations.type.public",
+  transfer_private: "aleo.operations.type.private",
+  transfer_public_to_private: "aleo.operations.type.public_to_private",
+  transfer_private_to_public: "aleo.operations.type.private_to_public",
+};
+
+const MyCustomMetadataCell: AleoFamily["CustomMetadataCell"] = props => {
+  return (
+    <Box width={90}>
+      <Text color="neutral.c80" textAlign="center" display="block" variant="body" fontSize={3}>
+        <Trans i18nKey={mapFunctionIdToName[props.operation.extra.functionId ?? "public"]} />
+      </Text>
+    </Box>
+  );
 };
 
 export default MyCustomMetadataCell;
