e69662a9611b438f1cc5ac1e20d339ac76666390
Bruno Cunha <bruno.carlos-da-cunha@ledger.fr>
2026-01-22 13:10:14 +0000

    test(e2e): fix lwm buy sell e2e tests

--- apps/ledger-live-mobile/scripts/e2e-ci.mjs
+++ apps/ledger-live-mobile/scripts/e2e-ci.mjs
@@ -71,12 +71,12 @@ const build_android = async () => {
 const test_android = async () => {
   const result = await $`pnpm mobile ${testType}:test \\
       -c android.emu.${target} \\
-      --loglevel error \\
+      --loglevel debug \\
       --record-logs failing \\
       --take-screenshots failing \\
       --forceExit \\
       --headless \\
-      --retries 2 \\
+      --retries 0 \\
       --cleanup \\
       ${filteredArgs}`.nothrow();
   process.exitCode = result.exitCode;
