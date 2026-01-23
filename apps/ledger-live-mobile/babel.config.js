dca994de86166b58e361bd7c5cdf1a0251995e4f
Kevin Le Seigle <kevin.le-seigle@ledger.fr>
2026-01-23 17:38:32 +0100

    chore(lwm): migrate metro to repack for our bundler

--- apps/ledger-live-mobile/babel.config.js
+++ apps/ledger-live-mobile/babel.config.js
@@ -9,13 +9,25 @@ module.exports = {
     ],
   ],
   plugins: [
+    [
+      "metro-transform-plugins/private/inline-requires-plugin",
+      {
+        ignoredRequires: ["react-native"],
+      },
+    ],
     "@babel/plugin-transform-named-capturing-groups-regex",
     "@babel/plugin-transform-export-namespace-from",
     "@babel/plugin-transform-class-static-block",
     "@babel/plugin-transform-flow-strip-types",
     ["@babel/plugin-transform-private-methods", { loose: true }],
-    "babel-plugin-transform-inline-environment-variables",
-    // only inject collapsable={false} for builds running Detox tests
+    [
+      "babel-plugin-transform-inline-environment-variables",
+      // Exclude JEST_WORKER_ID and VITEST_WORKER_ID to avoid MMKV conflicts
+      // MMKV checks for these variables to determine if it runs for tests.
+      { exclude: ["JEST_WORKER_ID", "VITEST_WORKER_ID"] },
+    ],
+    // Fabric optimization are not supported by Detox and needs to be disabled.
+    // Only inject collapsable={false} for builds running Detox tests.
     process.env.DETOX === "1" || process.env.DETOX === "true"
       ? "./babel-plugin-inject-collapsable.js"
       : null,
