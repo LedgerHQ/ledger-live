0c0d7fcb7fc55da8a9458aed29d2b46bc68ccc16
Mateusz Domański <mateusz.domanski-ext@ledger.fr>
2026-01-28 10:34:33 +0100

    test: enable swap Hedera test

--- e2e/desktop/tests/specs/swap.send.spec.ts
+++ e2e/desktop/tests/specs/swap.send.spec.ts
@@ -176,12 +176,22 @@ const swaps = [
   // },
   //ToDo: enable once Hedera Swap E2E test issue fixed
   // Task https://ledgerhq.atlassian.net/browse/LIVE-21744
-  //{
-  //  fromAccount: Account.HEDERA_1,
-  //  toAccount: Account.XRP_1,
-  //  xrayTicket: "B2CQA-3753",
-  //  tag: ["@NanoSP", "@NanoX", "@Stax", "@Flex", "@NanoGen5", "@hedera", "@xrp", "@family-xrp", "@family-hedera"],
-  //},
+  {
+    fromAccount: Account.HEDERA_1,
+    toAccount: Account.XRP_1,
+    xrayTicket: "B2CQA-3753",
+    tag: [
+      "@NanoSP",
+      "@NanoX",
+      "@Stax",
+      "@Flex",
+      "@NanoGen5",
+      "@hedera",
+      "@xrp",
+      "@family-xrp",
+      "@family-hedera",
+    ],
+  },
   {
     fromAccount: TokenAccount.SUI_USDC_1,
     toAccount: Account.SOL_1,
