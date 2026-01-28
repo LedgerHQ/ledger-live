75793931a98d4b102b6573ce2aa5799756ccbca0
Olaoluwa Oke <olaoluwa.oke@ledger.fr>
2026-01-28 15:22:57 +0000

    feat: update props

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/WebView.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/WebView.tsx
@@ -60,10 +60,7 @@ export const WebView = forwardRef<WebviewAPI, Props>(
 
     const devMode = exportSettings.developerModeEnabled.toString();
     const lastSeenDevice = useSelector(lastSeenDeviceSelector);
-
-    const currentAccounts = useSelector(flattenAccountsSelector);
-    const stableCurrentAccounts = useRef(currentAccounts).current; // only consider accounts available upon initial WebView load
-    const swapParams = useTranslateToSwapAccount(params, stableCurrentAccounts);
+    const swapParams = useTranslateToSwapAccount(params);
     const llmModularDrawerFF = useFeature("llmModularDrawer");
 
     const isLlmModularDrawer = llmModularDrawerFF?.enabled && llmModularDrawerFF?.params?.live_app;
