16da950a3225ccf0687f0d633021a084f218bbf9
Thomas Brillard <thomas.brillard@ledger.fr>
2026-02-02 13:22:31 +0100

    feat: removed navbar from perps

--- apps/ledger-live-mobile/src/components/RootNavigator/PerpsNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/PerpsNavigator.tsx
@@ -1,18 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useMemo } from "react";
-import { useTranslation } from "~/context/Locale";
 import { useTheme } from "styled-components/native";
 import { ScreenName } from "~/const";
 import { getStackNavigatorConfig } from "~/navigation/navigatorConfig";
 import { PerpsNavigatorParamList } from "./types/PerpsNavigator";
-import { NavigationHeaderBackButton } from "../NavigationHeaderBackButton";
 import { PerpsLiveAppWrapper } from "LLM/features/Perps";
 import { useNoNanoBuyNanoWallScreenOptions } from "~/context/NoNanoBuyNanoWall";
 
 const Stack = createNativeStackNavigator<PerpsNavigatorParamList>();
 
 export default function PerpsNavigator() {
-  const { t } = useTranslation();
   const { colors } = useTheme();
   const stackNavigationConfig = useMemo(() => getStackNavigatorConfig(colors, true), [colors]);
   const noNanoBuyNanoWallScreenOptions = useNoNanoBuyNanoWallScreenOptions();
@@ -22,14 +19,14 @@ export default function PerpsNavigator() {
       ...("options" in noNanoBuyNanoWallScreenOptions
         ? noNanoBuyNanoWallScreenOptions.options
         : {}),
-      headerTitle: t("perps.title", { defaultValue: "Perps" }),
-      headerLeft: () => <NavigationHeaderBackButton />,
+      headerTitle: "",
+      headerLeft: () => null,
     }),
-    [noNanoBuyNanoWallScreenOptions, t],
+    [noNanoBuyNanoWallScreenOptions],
   );
 
   return (
-    <Stack.Navigator screenOptions={{ ...stackNavigationConfig, headerShown: true }}>
+    <Stack.Navigator screenOptions={{ ...stackNavigationConfig, headerShown: false }}>
       <Stack.Screen
         name={ScreenName.PerpsTab}
         component={PerpsLiveAppWrapper}
