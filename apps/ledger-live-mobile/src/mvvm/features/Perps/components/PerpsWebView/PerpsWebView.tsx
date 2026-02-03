82726a7c131f957b4ff89e9fe9d13b1a35b944f4
Bruno Cunha <bruno.carlos-da-cunha@ledger.fr>
2026-02-03 09:27:30 +0100

    fix(lwm): remove jotai-scope dependency

--- apps/ledger-live-mobile/src/mvvm/features/Perps/components/PerpsWebView/PerpsWebView.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Perps/components/PerpsWebView/PerpsWebView.tsx
@@ -1,7 +1,5 @@
 import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
-import { currentAccountAtom } from "@ledgerhq/live-common/wallet-api/useDappLogic";
 import React, { forwardRef } from "react";
-import { ScopeProvider } from "jotai-scope";
 import { Web3AppWebview } from "~/components/Web3AppWebview";
 import { WebviewAPI, WebviewState } from "~/components/Web3AppWebview/types";
 import SafeAreaView from "~/components/SafeAreaView";
@@ -15,18 +13,15 @@ type Props = {
 
 export const PerpsWebView = forwardRef<WebviewAPI, Props>(
   ({ manifest, setWebviewState, inputs }, ref) => {
-    // ScopeProvider required to prevent conflicts between Webview instance and deeplink instances
     return (
-      <ScopeProvider atoms={[currentAccountAtom]}>
-        <SafeAreaView edges={["bottom"]} isFlex>
-          <Web3AppWebview
-            ref={ref}
-            manifest={manifest}
-            onStateChange={setWebviewState}
-            inputs={inputs}
-          />
-        </SafeAreaView>
-      </ScopeProvider>
+      <SafeAreaView edges={["bottom"]} isFlex>
+        <Web3AppWebview
+          ref={ref}
+          manifest={manifest}
+          onStateChange={setWebviewState}
+          inputs={inputs}
+        />
+      </SafeAreaView>
     );
   },
 );
