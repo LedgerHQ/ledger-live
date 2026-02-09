1f4513e588cdb46c8bb1b1ed70dad7ced5f9e81c
Prateek Rathod <prateek@zondax.ch>
2026-02-09 15:35:19 +0100

    fix: typecheck and lint erros

--- apps/ledger-live-desktop/src/renderer/families/mina/StakingFlowModal/steps/StepValidator.tsx
+++ apps/ledger-live-desktop/src/renderer/families/mina/StakingFlowModal/steps/StepValidator.tsx
@@ -51,7 +51,7 @@ export function StepValidatorFooter({
     <>
       <AccountFooter parentAccount={parentAccount} account={account} status={status} />
       <Box horizontal>
-        <Button mr={1} secondary onClick={onClose}>
+        <Button mr={1} onClick={onClose}>
           {t("common.cancel")}
         </Button>
         <Button
