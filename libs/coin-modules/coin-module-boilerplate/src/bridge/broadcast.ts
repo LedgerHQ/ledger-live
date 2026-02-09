95b7d39eb341bc5a0658d970217dad5648947b6e
François Guérin <francois.guerin@ledger.fr>
2026-02-09 20:31:30 +0100

    refactor(coin-module-boilerplate): rename `common-logic` to `logic`

--- libs/coin-modules/coin-module-boilerplate/src/bridge/broadcast.ts
+++ libs/coin-modules/coin-module-boilerplate/src/bridge/broadcast.ts
@@ -1,6 +1,6 @@
 import { patchOperationWithHash } from "@ledgerhq/coin-framework/operation";
 import { AccountBridge } from "@ledgerhq/types-live";
-import { broadcast as broadcastLogic } from "../common-logic";
+import { broadcast as broadcastLogic } from "../logic";
 import { Transaction } from "../types";
 
 export const broadcast: AccountBridge<Transaction>["broadcast"] = async ({
