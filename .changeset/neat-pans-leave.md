1b664d54ede64e344970cac631cd532a7b526c1a
Diyaeddine LAOUID <diyaeddine.laouid-ext@ledger.fr>
2026-02-09 18:01:34 +0100

    feat(lwm): navigator for new send flow

---
+++ .changeset/neat-pans-leave.md
@@ -0,0 +1,5 @@
+---
+"live-mobile": minor
+---
+
+feat: create navigator for new send flow (LWM)
--- apps/ledger-live-mobile/src/mvvm/features/Send/index.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Send/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SEND_FLOW_STEP, type SendFlowStep } from "@ledgerhq/live-common/flows/send/types";
+import type { StepRegistry } from "@ledgerhq/live-common/flows/wizard/types";
 
-export default function SendWorkflow() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const stepRegistry: StepRegistry<SendFlowStep> = {
+  [SEND_FLOW_STEP.RECIPIENT]: () => <></>,
+  [SEND_FLOW_STEP.AMOUNT]: () => <></>,
+  [SEND_FLOW_STEP.CONFIRMATION]: () => <></>,
+  [SEND_FLOW_STEP.SIGNATURE]: () => <></>,
+};
+
+export function SendWorkflow() {
   return (
     <View style={{ margin: "auto" }}>
       {/* eslint-disable-next-line i18next/no-literal-string */}
