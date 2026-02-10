33d9a1daa6a4b4b9ce4081c44a416e9ac9858a41
Aleksei Maslakov <alexei.maslakov@gmail.com>
2026-02-10 22:00:57 +0000

    fix(concordium): remove no-op intersection type from ConcordiumFamily

--- apps/ledger-live-desktop/src/renderer/families/concordium/types.ts
+++ apps/ledger-live-desktop/src/renderer/families/concordium/types.ts
@@ -2,9 +2,4 @@ import { Account, Operation } from "@ledgerhq/types-live";
 import { LLDCoinFamily } from "../types";
 import { Transaction, TransactionStatus } from "@ledgerhq/live-common/families/concordium/types";
 
-export type ConcordiumFamily = LLDCoinFamily<
-  Account,
-  Transaction,
-  TransactionStatus,
-  Operation
-> & {};
+export type ConcordiumFamily = LLDCoinFamily<Account, Transaction, TransactionStatus, Operation>;
