a5be3b0bd8ccdcbd6b5fdb5817de6208104fa282
Bruno Cunha <bruno.carlos-da-cunha@ledger.fr>
2026-02-11 11:38:25 +0000

    test(e2e): lwd main navigation test improvements

--- e2e/desktop/tests/page/mainNavigation.page.ts
+++ e2e/desktop/tests/page/mainNavigation.page.ts
@@ -11,10 +11,19 @@ type NavigationTarget = {
   readonly selector: () => Locator;
 };
 
+export type TargetName =
+  | "portfolio"
+  | "accounts"
+  | "swap"
+  | "earn"
+  | "discover"
+  | "refer a friend"
+  | "card";
+
 export class MainNavigationPage extends AppPage {
-  private drawer = new Drawer(this.page);
-  private layout = new Layout(this.page);
-  private readonly sideBarButton: (name: string) => Locator = name =>
+  private readonly drawer = new Drawer(this.page);
+  private readonly layout = new Layout(this.page);
+  private readonly sideBarButton: (name: TargetName) => Locator = name =>
     this.page.getByRole("button", { name: new RegExp(`^${name}$`, "i") });
 
   private async expectPath(expectedPath: RegExp) {
@@ -25,107 +34,75 @@ export class MainNavigationPage extends AppPage {
     });
   }
 
-  private get sidebarTargets(): ReadonlyArray<NavigationTarget> {
-    return [
-      {
+  private get sidebarTargets(): Readonly<Record<TargetName, NavigationTarget>> {
+    return {
+      portfolio: {
         expectActive: true,
         selector: () => this.sideBarButton("portfolio"),
       },
-      {
+      accounts: {
         expectActive: true,
         expectedPath: /^\/accounts(?:\/|$|\?)/,
         selector: () => this.sideBarButton("accounts"),
       },
-      {
+      swap: {
         expectActive: true,
         expectedPath: /^\/swap(?:\/|$|\?)/,
         selector: () => this.sideBarButton("swap"),
       },
-      {
+      earn: {
         expectActive: true,
         expectedPath: /^\/earn(?:\/|$|\?)/,
         selector: () => this.page.getByRole("button", { name: /^(earn|stake)$/i }),
       },
-      {
+      discover: {
         expectActive: true,
         expectedPath: /^\/platform(?:\/|$|\?)/,
         selector: () => this.sideBarButton("discover"),
       },
-      {
+      "refer a friend": {
         expectActive: true,
         expectedPath: /^\/platform\/refer-a-friend(?:\/|$|\?)/,
         selector: () => this.sideBarButton("refer a friend"),
       },
-      {
+      card: {
         expectActive: true,
         expectedPath: /^\/card-new-wallet(?:\/|$|\?)/,
         selector: () => this.sideBarButton("card"),
       },
-    ];
+    };
+  }
+  @step("Open $0 from main navigation")
+  async openTargetFromMainNavigation(target: TargetName) {
+    await this.sidebarTargets[target].selector().click();
   }
 
-  private async verifySidebarTarget(target: NavigationTarget) {
-    const clickedItem = target.selector().first();
-    await clickedItem.click();
-
-    if (target.expectActive) {
-      await expect(clickedItem).toHaveAttribute("aria-current", "page");
+  @step("Validate $0 target from main navigation is selected and redirect to the expected path")
+  async validateTargetFromMainNavigation(target: TargetName) {
+    const targetConfig = this.sidebarTargets[target];
+    if (targetConfig.expectActive) {
+      await expect(targetConfig.selector()).toHaveAttribute("aria-current", "page");
     }
 
-    if (target.expectedPath) {
-      await this.expectPath(target.expectedPath);
+    if (targetConfig.expectedPath) {
+      await this.expectPath(targetConfig.expectedPath);
     }
-  }
-
-  @step("Open Portfolio from main navigation")
-  async verifyPortfolioNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[0]);
-  }
-
-  @step("Open Accounts from main navigation")
-  async verifyAccountsNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[1]);
-  }
-
-  @step("Open Swap from main navigation")
-  async verifySwapNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[2]);
-  }
-
-  @step("Open Earn from main navigation")
-  async verifyEarnNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[3]);
-  }
-
-  @step("Open Discover from main navigation")
-  async verifyDiscoverNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[4]);
-  }
-
-  @step("Open Refer a friend from main navigation")
-  async verifyReferAFriendNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[5]);
-  }
-
-  @step("Open Card from main navigation")
-  async verifyCardNavigation() {
-    await this.verifySidebarTarget(this.sidebarTargets[6]);
   }
 
   @step("Open Notification center from top navigation")
-  async verifyNotificationCenterNavigation() {
+  async openNotificationCenter() {
     await this.layout.topbarNotificationButton.click();
     await expect(this.drawer.content).toBeVisible();
     await this.drawer.closeDrawer();
   }
 
   @step("Click Activity indicator from top navigation")
-  async verifyActivityIndicatorNavigation() {
+  async clickActivityIndicator() {
     await this.layout.topbarSynchronizeButton.click();
   }
 
   @step("Open Settings from top navigation")
-  async verifySettingsNavigation() {
+  async openSettings() {
     await this.layout.topbarSettingsButton.click();
     await this.expectPath(/^\/settings(?:\/|$|\?)/);
   }
