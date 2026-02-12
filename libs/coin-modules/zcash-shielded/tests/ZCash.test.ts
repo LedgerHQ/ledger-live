a70150342c62795a783a5f693d92455bc81cc4af
Alessandro Vito <alessandro.vito84@gmail.com>
2026-02-12 16:59:18 +0000

    feat(zcash-shielded): get parameter nodeUrl

--- libs/coin-modules/zcash-shielded/tests/ZCash.test.ts
+++ libs/coin-modules/zcash-shielded/tests/ZCash.test.ts
@@ -12,6 +12,7 @@ import {
   txShieldedSapling,
 } from "./testAccounts";
 import { server } from "../mocks/node";
+import { JSON_RPC_SERVER } from "../src/consts";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -28,7 +29,7 @@ describe("estimatedSyncTime", () => {
 
   test("estimates the sync time", async () => {
     jest.setSystemTime(new Date("2016-10-28T00:00:00.000Z"));
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const estimatedSyncTime = await zcash.estimatedSyncTime(10);
     jest.setSystemTime(new Date("2016-10-28T00:20:00.000Z"));
     const estimatedSyncTimeResult = estimatedSyncTime();
@@ -38,7 +39,7 @@ describe("estimatedSyncTime", () => {
 
 describe("findBlockHeight", () => {
   test("finds the lowest block height", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const height = await zcash.findBlockHeight(1234);
     expect(height).toEqual(1276);
   });
@@ -46,31 +47,31 @@ describe("findBlockHeight", () => {
 
 describe("decryptTransaction", () => {
   test("fails to decrypt memo: UFVK decode failed", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const decryptedTx = await zcash.decryptTransaction("transaction", "viewingkey");
     expect(decryptedTx).toEqual(undefined);
   });
 
   test("fails to decrypt memo: hex decode failed", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const decryptedTx = await zcash.decryptTransaction("transaction", testAccount1.viewingKey);
     expect(decryptedTx).toEqual(undefined);
   });
 
   test("decrypts an orchard transaction", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const height = await zcash.decryptTransaction(txShieldedOrchard.hex, testAccount1.viewingKey);
     expect(height).toEqual(decryptedOrchardData);
   });
 
   test("decrypts a sapling transaction", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const height = await zcash.decryptTransaction(txShieldedSapling.hex, testAccount1.viewingKey);
     expect(height).toEqual(decryptedSaplingData);
   });
 
   test("returns empty list of actions when ufvk is not correct", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const height = await zcash.decryptTransaction(txShieldedSapling.hex, testAccount2.viewingKey);
     expect(height).toEqual({
       orchard_outputs: [],
@@ -81,7 +82,7 @@ describe("decryptTransaction", () => {
 
 describe("findShieldedTxsInBlock", () => {
   test("retrieves a list of transactions for the viewing key", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const transactions = await zcash.findShieldedTxsInBlock(
       blockWithMyTx.hash,
       testAccount1.viewingKey,
@@ -101,7 +102,7 @@ describe("findShieldedTxsInBlock", () => {
   });
 
   test("retrieves an empty list of transactions when viewing key doesn't match any", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const transactions = await zcash.findShieldedTxsInBlock(
       blockWithoutMyTx.hash,
       testAccount1.viewingKey,
@@ -123,7 +124,7 @@ describe("findShieldedTxsInBlock", () => {
   });
 
   test("edge case: retrieves an empty list when the block doesn't have tx", async () => {
-    const zcash = new ZCash();
+    const zcash = new ZCash({ nodeUrl: JSON_RPC_SERVER });
     const transactions = await zcash.findShieldedTxsInBlock(
       blockWithoutAnyTx.hash,
       testAccount1.viewingKey,
