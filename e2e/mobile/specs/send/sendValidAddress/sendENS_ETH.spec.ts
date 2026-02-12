9c83e00f665864b42831162ec634c3711fb19947
Victor Alber <victor.alber@ledger.fr>
2026-02-12 16:02:36 +0100

    test: file reorganization + buy/sell cleanup

--- e2e/mobile/specs/send/sendValidAddress/sendENS_ETH.spec.ts
+++ e2e/mobile/specs/send/sendValidAddress/sendENS_ETH.spec.ts
@@ -1,9 +1,9 @@
 import { Account } from "@ledgerhq/live-common/e2e/enum/Account";
-import { runSendMaxTest } from "./send";
+import { runSendENSTest } from "../send";
 
-const transaction = new Transaction(Account.ETH_1, Account.ETH_2, "max", Fee.MEDIUM);
-runSendMaxTest(
+const transaction = new Transaction(Account.ETH_1, Account.ETH_2_WITH_ENS, "0.0001", Fee.MEDIUM);
+runSendENSTest(
   transaction,
-  ["B2CQA-473"],
+  ["B2CQA-2202"],
   ["@NanoSP", "@LNS", "@NanoX", "@Stax", "@Flex", "@NanoGen5", "@ethereum", "@family-evm"],
 );
