0461d27332954d907aa1a70d44e22a7f0c6006b1
Valentin <valentin.lambert@ledger.fr>
2026-02-13 11:16:20 +0100

    chore(lw-feature-flag): display earn v4 depending on lwm40 or lwd40 feature flag

--- apps/ledger-live-mobile/src/components/Loading.tsx
+++ apps/ledger-live-mobile/src/components/Loading.tsx
@@ -6,9 +6,10 @@ import { Flex } from "@ledgerhq/native-ui";
 type Props = {
   size?: number;
   color?: string;
+  backgroundColor?: string;
 };
 
-export const Loading = ({ size = 40, color }: Props) => (
+export const Loading = ({ size = 40, color, backgroundColor = "background.main" }: Props) => (
   <Flex
     flex={1}
     position="absolute"
@@ -16,7 +17,7 @@ export const Loading = ({ size = 40, color }: Props) => (
     right="0"
     bottom="0"
     left="0"
-    backgroundColor="background.main"
+    backgroundColor={backgroundColor}
   >
     <Box flex={1} justifyContent="center">
       <InfiniteLoader size={size} color={color} />
