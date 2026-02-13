0b7ec96ef32c5e41737ed4fc474f7492acb2821e
Alessandro Vito <alessandro.vito84@gmail.com>
2026-02-13 13:08:18 +0000

    feat(zcash-shielded): update RPC server url to ledger service

--- libs/coin-modules/zcash-shielded/src/constants.ts
+++ libs/coin-modules/zcash-shielded/src/constants.ts
@@ -1,2 +1,3 @@
 export const LOG_TYPE = "zcash-shielded";
-export const JSON_RPC_SERVER = "http://localhost:18232";
+export const JSON_RPC_SERVER =
+  "https://explorers.api.vault.ledger-test.com/nodes/zec_testnet/zaino/jsonrpc";
