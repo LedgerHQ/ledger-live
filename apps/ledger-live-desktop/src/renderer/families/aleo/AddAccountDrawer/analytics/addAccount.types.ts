9c11664bf5690194b7436f8b23fee72ab74e7999
Mateusz Domański <mateusz.domanski-ext@ledger.fr>
2026-02-14 09:31:27 +0100

    feat: aleo track add account screen analytics

---
+++ apps/ledger-live-desktop/src/renderer/families/aleo/AddAccountDrawer/analytics/addAccount.types.ts
@@ -0,0 +1,28 @@
+import {
+  ADD_ACCOUNT_EVENTS_NAME,
+  ADD_ACCOUNT_PAGE_NAME,
+  type AddAccountEventParams,
+} from "LLD/features/AddAccountDrawer/analytics/addAccount.types";
+
+export const ALEO_ADD_ACCOUNT_EVENTS_NAME = {
+  ...ADD_ACCOUNT_EVENTS_NAME,
+  VIEW_KEY_WARNING: "confirm view key warning",
+} as const;
+
+export type AleoAddAccountEventName =
+  (typeof ALEO_ADD_ACCOUNT_EVENTS_NAME)[keyof typeof ALEO_ADD_ACCOUNT_EVENTS_NAME];
+
+export type AleoAddAccountEventsParams = AddAccountEventParams & {
+  [ALEO_ADD_ACCOUNT_EVENTS_NAME.VIEW_KEY_WARNING]: {
+    source: string;
+    flow: string;
+  };
+};
+
+export const ALEO_ADD_ACCOUNT_PAGE_NAME = {
+  ...ADD_ACCOUNT_PAGE_NAME,
+  VIEW_KEY_WARNING: "confirm view key warning",
+} as const;
+
+export type AleoAddAccountPageName =
+  (typeof ALEO_ADD_ACCOUNT_PAGE_NAME)[keyof typeof ALEO_ADD_ACCOUNT_PAGE_NAME];
