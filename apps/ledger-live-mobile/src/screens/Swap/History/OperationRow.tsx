2fdbb6bd5c98fad64fa7b6214653818bc0757746
Olaoluwa Oke <olaoluwa.oke@ledger.fr>
2026-02-18 21:35:42 +0000

    feat: display finalAmount in swap history

--- apps/ledger-live-mobile/src/screens/Swap/History/OperationRow.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/History/OperationRow.tsx
@@ -15,7 +15,7 @@ import { useAccountUnit } from "LLM/hooks/useAccountUnit";
 const OperationRow = ({ item }: { item: MappedSwapOperation }) => {
   const { colors } = useTheme();
   const { fromAccount, toAccount, ...routeParams } = item;
-  const { swapId, fromAmount, toAmount, status } = routeParams;
+  const { swapId, fromAmount, toAmount, finalAmount, status } = routeParams;
   const navigation = useNavigation<StackNavigatorNavigation<SwapNavigatorParamList>>();
 
   const onOpenOperationDetails = useCallback(() => {
@@ -70,7 +70,7 @@ const OperationRow = ({ item }: { item: MappedSwapOperation }) => {
             {toAccountName}
           </LText>
           <LText style={styles.amount} color="grey" testID={`swap-history-toAmount-${swapId}`}>
-            <CurrencyUnitValue showCode unit={unitTo} value={toAmount} />
+            <CurrencyUnitValue showCode unit={unitTo} value={finalAmount ?? toAmount} />
           </LText>
         </View>
       </View>
--- apps/ledger-live-mobile/src/screens/Swap/SubScreens/OperationDetails.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/SubScreens/OperationDetails.tsx
@@ -25,8 +25,16 @@ import { useMaybeAccountUnit } from "LLM/hooks/useAccountUnit";
 
 export function OperationDetails({ route }: OperationDetailsParamList) {
   const { swapOperation } = route.params;
-  const { swapId, provider, toAccountId, fromAccountId, fromAmount, toAmount, operation } =
-    swapOperation;
+  const {
+    swapId,
+    provider,
+    toAccountId,
+    fromAccountId,
+    fromAmount,
+    toAmount,
+    finalAmount,
+    operation,
+  } = swapOperation;
   const accounts = useSelector(flattenAccountsSelector);
   const fromAccount = useMemo(
     () => accounts.find(a => a.id === fromAccountId),
@@ -100,7 +108,7 @@ export function OperationDetails({ route }: OperationDetailsParamList) {
         </View>
         <LText style={styles.toAmount} color={statusColorKey}>
           {toAccount && unitTo ? (
-            <CurrencyUnitValue alwaysShowSign showCode unit={unitTo} value={toAmount} />
+            <CurrencyUnitValue alwaysShowSign showCode unit={unitTo} value={finalAmount ?? toAmount} />
           ) : null}
         </LText>
         <View style={styles.statusTextWrapper}>
@@ -201,7 +209,7 @@ export function OperationDetails({ route }: OperationDetailsParamList) {
           </LText>
           <LText style={styles.value} testID="swap-operation-details-toAmount">
             {toAccount && unitTo ? (
-              <CurrencyUnitValue showCode unit={unitTo} value={toAmount} />
+              <CurrencyUnitValue showCode unit={unitTo} value={finalAmount ?? toAmount} />
             ) : null}
           </LText>
         </View>
