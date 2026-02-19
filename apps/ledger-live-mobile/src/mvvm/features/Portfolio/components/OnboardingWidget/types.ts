0d09d79748700f614257171b68bb4f762916cb2f
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-02-19 14:54:16 +0000

    feat: add recover action to post onboarding hub
    chore: changeset
    
    feat: remove complete on start for recover and update copy for recover action
    
    feat: add canShowRecover to useOpenPostOnboardingCallback
    
    feat: add post onboarding action to add recover to hub state
    
    chore: remove unused code
    
    feat: add already complete async
    
    feat: add recover async fulfilled to onboarding widget utility
    
    chore: cr fixes
    
    chore: cr fixes
    
    chore: fix variable
    
    chore: cr fix
    
    chore: lint

--- apps/ledger-live-mobile/src/mvvm/features/Portfolio/components/OnboardingWidget/types.ts
+++ apps/ledger-live-mobile/src/mvvm/features/Portfolio/components/OnboardingWidget/types.ts
@@ -3,6 +3,7 @@ export interface OnboardingWidgetViewProps {
   readonly totalSteps: number;
   readonly stepperLabel: string;
   readonly onPress: () => void;
+  readonly loading: boolean;
 }
 
 export type UseOnboardingWidgetViewModelResult = OnboardingWidgetViewProps;
