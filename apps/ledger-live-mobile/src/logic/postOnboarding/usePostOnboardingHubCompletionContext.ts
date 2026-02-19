8dbf2f3808af7a5f1ffe279fc99d0b64350779ef
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-02-19 14:54:16 +0000

    feat: add recover action to post onboarding hub
    chore: changeset
    
    feat: remove complete on start for recover and update copy for recover action
    
    feat: add canShowRecover to useOpenPostOnboardingCallback
    
    feat: add post onboarding action to add recover to hub state
    
    chore: remove unused code
    
    feat: add already complete async
    
    feat: add recover async fulfilled to onboarding widget utility
    
    chore: cr fixes
    
    chore: cr fixes
    
    chore: fix variable
    
    chore: cr fix
    
    chore: lint
    
    chore: cr fix
    
    feat: add debug for postonboarding recover action
    
    chore: update changeset
    
    chore: lint

--- apps/ledger-live-mobile/src/logic/postOnboarding/usePostOnboardingHubCompletionContext.ts
+++ apps/ledger-live-mobile/src/logic/postOnboarding/usePostOnboardingHubCompletionContext.ts
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { trustchainSelector } from "@ledgerhq/ledger-key-ring-protocol/store";
+import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
 import type { Account } from "@ledgerhq/types-live";
 import { useSelector } from "~/context/hooks";
 import { accountsSelector } from "~/reducers/accounts";
@@ -7,17 +8,22 @@ import { accountsSelector } from "~/reducers/accounts";
 export type PostOnboardingHubCompletionContext = {
   readonly isLedgerSyncActive: boolean;
   readonly accounts: Account[];
+  readonly protectId: string;
 };
 
 export function usePostOnboardingHubCompletionContext(): PostOnboardingHubCompletionContext {
   const trustchain = useSelector(trustchainSelector);
   const accounts = useSelector(accountsSelector);
 
+  const recoverServices = useFeature("protectServicesMobile");
+  const protectId = recoverServices?.params?.protectId ?? "protect-prod";
+
   return useMemo(
     () => ({
       isLedgerSyncActive: Boolean(trustchain?.rootId),
       accounts,
+      protectId,
     }),
-    [trustchain, accounts],
+    [trustchain, accounts, protectId],
   );
 }
