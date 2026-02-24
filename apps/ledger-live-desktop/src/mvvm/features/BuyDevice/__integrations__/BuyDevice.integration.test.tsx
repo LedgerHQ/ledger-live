f2425cea03b1cea5b7a1ab4019e2573178469b84
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-02-24 15:55:56 +0000

    feat: add global buy device modal
    feat: add open drawer hook
    
    feat: add buy device dialog
    
    feat: full width cta buttons on buy device dialog
    
    feat: add useLazyOnboardingActions.ts
    
    feat: add useLazyOnboardingActions test
    
    chore: prettier
    
    chore: prettier
    
    chore: changeset
    
    feat: remove background and pointer on item list
    
    chore: CR updates
    
    chore: fixes from CR
    
    feat: add tracking
    
    chore: prettier

---
+++ apps/ledger-live-desktop/src/mvvm/features/BuyDevice/__integrations__/BuyDevice.integration.test.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Button } from "@ledgerhq/lumen-ui-react";
+import { render, screen } from "tests/testSetup";
+import Box from "~/renderer/components/Box";
+import useBuyDeviceDrawer from "../hooks/useBuyDeviceDrawer";
+import BuyDevice from "..";
+
+const mockConnect = jest.fn();
+const mockBuyDevice = jest.fn();
+
+jest.mock("LLD/hooks/useLazyOnboardingActions", () => ({
+  useLazyOnboardingActions: () => ({
+    handleConnect: mockConnect,
+    handleBuyDevice: mockBuyDevice,
+  }),
+}));
+
+const Setup = () => {
+  const { handleOpen } = useBuyDeviceDrawer();
+  return (
+    <Box>
+      <Button onClick={handleOpen}>OPEN</Button>
+      <BuyDevice />
+    </Box>
+  );
+};
+
+describe("Buy Device Dialog", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("displays dialog correctly", async () => {
+    const { user } = render(<Setup />);
+
+    const button = screen.getByRole("button", { name: "OPEN" });
+    await user.click(button);
+
+    expect(screen.getByText("Connect a Ledger device")).toBeVisible();
+
+    const connectButton = screen.getByRole("button", { name: "Connect" });
+    expect(connectButton).toBeVisible();
+
+    const buyDeviceButton = screen.getByRole("button", { name: "Buy your Ledger device" });
+    expect(buyDeviceButton).toBeVisible();
+  });
+
+  it("when connect button clicked it calls connect action", async () => {
+    const { user } = render(<Setup />);
+
+    const button = screen.getByRole("button", { name: "OPEN" });
+    await user.click(button);
+
+    expect(screen.getByText("Connect a Ledger device")).toBeVisible();
+
+    const connectButton = screen.getByRole("button", { name: "Connect" });
+    expect(connectButton).toBeVisible();
+
+    await user.click(connectButton);
+    expect(mockConnect).toHaveBeenCalled();
+    expect(screen.queryByText("Connect a Ledger device")).toBeNull();
+  });
+
+  it("when buy device button clicked it calls buy device action", async () => {
+    const { user } = render(<Setup />);
+
+    const button = screen.getByRole("button", { name: "OPEN" });
+    await user.click(button);
+
+    expect(screen.getByText("Connect a Ledger device")).toBeVisible();
+
+    const buyDeviceButton = screen.getByRole("button", { name: "Buy your Ledger device" });
+    expect(buyDeviceButton).toBeVisible();
+
+    await user.click(buyDeviceButton);
+    expect(mockBuyDevice).toHaveBeenCalled();
+    expect(screen.queryByText("Connect a Ledger device")).toBeNull();
+  });
+});
---
+++ apps/ledger-live-desktop/src/mvvm/features/BuyDevice/useBuyDeviceViewModel.ts
@@ -0,0 +1,51 @@
+import { useSelector, useDispatch } from "LLD/hooks/redux";
+import { closeBuyDevice, selectIsBuyDeviceOpen } from "./buyDeviceDialog";
+import { useCallback } from "react";
+import { useLazyOnboardingActions } from "LLD/hooks/useLazyOnboardingActions";
+import { track } from "~/renderer/analytics/segment";
+
+export interface BuyDeviceViewProps {
+  isOpen: boolean;
+  onClose: () => void;
+  handleConnect: () => void;
+  handleBuy: () => void;
+}
+
+const ANALYTICS_PAGE = "buy device dialog";
+
+const useBuyDeviceViewModel = (): BuyDeviceViewProps => {
+  const dispatch = useDispatch();
+  const isOpen = useSelector(selectIsBuyDeviceOpen);
+  const { handleBuyDevice, handleConnect: handleConnectDevice } = useLazyOnboardingActions();
+
+  const onClose = useCallback(() => {
+    dispatch(closeBuyDevice());
+  }, [dispatch]);
+
+  const handleBuy = useCallback(() => {
+    track("button_clicked", {
+      button: "Connect",
+      page: ANALYTICS_PAGE,
+    });
+    handleBuyDevice();
+    onClose();
+  }, [onClose, handleBuyDevice]);
+
+  const handleConnect = useCallback(() => {
+    track("button_clicked", {
+      button: "buy a ledger device",
+      page: ANALYTICS_PAGE,
+    });
+    handleConnectDevice();
+    onClose();
+  }, [onClose, handleConnectDevice]);
+
+  return {
+    isOpen,
+    onClose,
+    handleConnect,
+    handleBuy,
+  };
+};
+
+export default useBuyDeviceViewModel;
