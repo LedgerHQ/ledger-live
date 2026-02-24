89b001ab5a41854e38b34f612067c26d38fd9c29
Tony Khaov <tony.khaov@gmail.com>
2026-02-24 15:36:52 +0100

    fix: always trigger opt in notifications post onboarding

--- apps/ledger-live-mobile/src/logic/postOnboarding/useCompletePostOnboarding.ts
+++ apps/ledger-live-mobile/src/logic/postOnboarding/useCompletePostOnboarding.ts
@@ -4,19 +4,24 @@ import { useDispatch } from "~/context/hooks";
 import { useNavigation } from "@react-navigation/native";
 import { postOnboardingSetFinished } from "@ledgerhq/live-common/postOnboarding/actions";
 import { NavigatorName } from "~/const";
+import { useNotifications } from "LLM/features/NotificationsPrompt";
 
 export function useCompletePostOnboarding() {
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
+  const { tryTriggerPushNotificationDrawerAfterAction } = useNotifications();
+
   const closePostOnboarding = useCallback(() => {
+    tryTriggerPushNotificationDrawerAfterAction("onboarding");
+
     dispatch(postOnboardingSetFinished());
 
     navigation.navigate(NavigatorName.Main, {
       screen: NavigatorName.Portfolio,
       params: { screen: NavigatorName.WalletTab },
     });
-  }, [dispatch, navigation]);
+  }, [dispatch, navigation, tryTriggerPushNotificationDrawerAfterAction]);
 
   return closePostOnboarding;
 }
