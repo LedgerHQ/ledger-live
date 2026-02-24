ed8867eee76c5f66d2acc7cf72afafc92e10e4bb
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-02-24 15:55:56 +0000

    feat: add global buy device modal
    feat: add open drawer hook
    
    feat: add buy device dialog
    
    feat: full width cta buttons on buy device dialog
    
    feat: add useLazyOnboardingActions.ts
    
    feat: add useLazyOnboardingActions test
    
    chore: prettier
    
    chore: prettier
    
    chore: changeset
    
    feat: remove background and pointer on item list
    
    chore: CR updates
    
    chore: fixes from CR
    
    feat: add tracking
    
    chore: prettier
    
    feat: fix track events
    
    chore: fix test
    
    chore: test fix
    
    feat: rename drawer to dialog
    
    chore: fix lint
    
    chore: fix lint
    
    faet: add lazy import for buy device dialog
    
    chore: update buy device png to webp
    
    chore: CR fixes
    
    chore: lint
    
    feat: replace image with mobile version

---
+++ apps/ledger-live-desktop/src/mvvm/hooks/useLazyOnboardingActions.ts
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router";
+import { useLocalizedUrl } from "~/renderer/hooks/useLocalizedUrls";
+import { openURL } from "~/renderer/linking";
+import { urls } from "~/config/urls";
+
+interface LazyOnboardingActions {
+  handleConnect: () => void;
+  handleBuyDevice: () => void;
+}
+
+export function useLazyOnboardingActions(): LazyOnboardingActions {
+  const navigate = useNavigate();
+  const urlLedgerShop = useLocalizedUrl(urls.ledgerShop);
+
+  const handleConnect = useCallback(() => {
+    navigate("/onboarding/select-device", { state: { fromQuickAction: true } });
+  }, [navigate]);
+
+  const handleBuyDevice = useCallback(() => openURL(urlLedgerShop), [urlLedgerShop]);
+
+  return {
+    handleConnect,
+    handleBuyDevice,
+  };
+}
