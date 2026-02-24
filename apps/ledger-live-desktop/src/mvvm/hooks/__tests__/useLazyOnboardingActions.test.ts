cacebfef5721bc459feb6b37310ebe9750f3fc41
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-02-24 15:55:56 +0000

    feat: add global buy device modal
    feat: add open drawer hook
    
    feat: add buy device dialog
    
    feat: full width cta buttons on buy device dialog
    
    feat: add useLazyOnboardingActions.ts
    
    feat: add useLazyOnboardingActions test
    
    chore: prettier
    
    chore: prettier
    
    chore: changeset
    
    feat: remove background and pointer on item list
    
    chore: CR updates
    
    chore: fixes from CR
    
    feat: add tracking
    
    chore: prettier
    
    feat: fix track events
    
    chore: fix test
    
    chore: test fix
    
    feat: rename drawer to dialog

---
+++ apps/ledger-live-desktop/src/mvvm/hooks/__tests__/useLazyOnboardingActions.test.ts
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router";
+import { renderHook } from "tests/testSetup";
+import { useLazyOnboardingActions } from "../useLazyOnboardingActions";
+import * as LinkingHelpers from "~/renderer/linking";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: jest.fn(() => mockNavigate),
+}));
+
+jest.mock("~/renderer/linking", () => ({
+  openURL: jest.fn(),
+}));
+
+describe("useLazyOnboardingActions", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns a function for buy device action", async () => {
+    jest.spyOn(LinkingHelpers, "openURL");
+    const { result } = renderHook(() => useLazyOnboardingActions());
+    result.current.handleBuyDevice();
+    expect(LinkingHelpers.openURL).toHaveBeenCalled();
+  });
+
+  it("returns a function for connect action", async () => {
+    const { result } = renderHook(() => useLazyOnboardingActions());
+    result.current.handleConnect();
+    expect(mockNavigate).toHaveBeenCalledWith("/onboarding/select-device", {
+      state: { fromQuickAction: true },
+    });
+  });
+});
