476f480c1890486cf20a095c9471f964395fdf56
Lewis Phillips <lewis@d5icp.co.uk>
2026-02-26 09:44:34 +0000

    ci: lint cleanup

--- apps/ledger-live-mobile/e2e/jest.environment.ts
+++ apps/ledger-live-mobile/e2e/jest.environment.ts
@@ -2,23 +2,22 @@ import { config as detoxConfig } from "detox/internals";
 // @ts-expect-error detox doesn't provide type declarations for this module
 import DetoxEnvironment from "detox/runners/jest/testEnvironment";
 
-
-
 import { logMemoryUsage } from "./helpers/commonHelpers";
 
 export default class TestEnvironment extends DetoxEnvironment {
-
   async setup() {
     const workerId = Number(process.env.JEST_WORKER_ID ?? 1);
-    if (workerId > 1) this.setupDeviceForSecondaryWorker(workerId);
+    if (workerId > 1) await this.setupDeviceForSecondaryWorker(workerId);
     await super.setup();
   }
 
-  private setupDeviceForSecondaryWorker(workerId: number) {
+  private async setupDeviceForSecondaryWorker(workerId: number) {
     const configName = process.env.DETOX_CONFIGURATION;
     if (!configName) throw new Error("Missing DETOX_CONFIGURATION environment variable");
 
-    const detoxConfigFile = require("../detox.config.js");
+    const detoxConfigModule = await import("../detox.config.js");
+    const detoxConfigFile =
+      "default" in detoxConfigModule ? detoxConfigModule.default : detoxConfigModule;
     const targetConfig = detoxConfigFile.configurations[configName];
     if (!targetConfig || !targetConfig.device) {
       throw new Error(
@@ -50,5 +49,3 @@ export default class TestEnvironment extends DetoxEnvironment {
     }
   }
 }
-
-export default TestEnvironment;
