839852a4ad8baebee75f45f355c90e6b88a2ab97
RobinVncnt <robin.vincent@ledger.fr>
2026-02-27 13:18:40 +0100

    fix: lwm walletv4tour post onboarding

---
+++ .changeset/lemon-parrots-reply.md
@@ -0,0 +1,5 @@
+---
+"live-mobile": minor
+---
+
+fix: lwm walletv4tour post onboarding
--- apps/ledger-live-mobile/src/mvvm/features/WalletV4Tour/Drawer/hooks/useWalletV4TourDrawerViewModel.ts
+++ apps/ledger-live-mobile/src/mvvm/features/WalletV4Tour/Drawer/hooks/useWalletV4TourDrawerViewModel.ts
@@ -2,7 +2,7 @@ import { useState, useCallback, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "~/context/hooks";
 import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import { setHasSeenWalletV4Tour } from "~/actions/settings";
-import { hasCompletedOnboardingSelector, hasSeenWalletV4TourSelector } from "~/reducers/settings";
+import { hasSeenWalletV4TourSelector } from "~/reducers/settings";
 import type { WalletV4TourDrawerViewModel } from "../types";
 import { useTranslation } from "~/context/Locale";
 import animation01 from "../animations/01.lottie";
@@ -15,19 +15,16 @@ export const useWalletV4TourDrawerViewModel = (): WalletV4TourDrawerViewModel =>
   const dispatch = useDispatch();
   const currentIndexRef = useRef(0);
   const hasSeenTour = useSelector(hasSeenWalletV4TourSelector);
-  const hasCompletedOnboarding = useSelector(hasCompletedOnboardingSelector);
   const lwmWallet40 = useFeature("lwmWallet40");
   const isTourEnabled = (lwmWallet40?.enabled && lwmWallet40?.params?.tour) ?? false;
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(isTourEnabled && !hasSeenTour);
 
   const handleOpenDrawer = useCallback(() => {
-    if (!hasCompletedOnboarding) {
-      dispatch(setHasSeenWalletV4Tour(true));
-    } else if (!hasSeenTour) {
+    if (!hasSeenTour) {
       setIsDrawerOpen(true);
     }
-  }, [hasCompletedOnboarding, hasSeenTour, dispatch]);
+  }, [hasSeenTour]);
 
   const handleCloseDrawer = useCallback(() => {
     setIsDrawerOpen(false);
