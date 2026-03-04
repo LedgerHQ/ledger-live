ccd0609f4652563e5ffedb4923bcf59b43f08db6
Diyaeddine LAOUID <diyaeddine.laouid-ext@ledger.fr>
2026-03-04 07:28:25 +0100

    feat(lwd): apply dust protection tx history

---
+++ .changeset/gorgeous-balloons-tell.md
@@ -0,0 +1,9 @@
+---
+"@ledgerhq/types-live": minor
+"ledger-live-desktop": minor
+"@ledgerhq/live-common": minor
+"@ledgerhq/coin-framework": minor
+"@ledgerhq/live-env": minor
+---
+
+feat(lwd): apply dust protection tx history
--- apps/ledger-live-desktop/src/renderer/screens/settings/sections/Accounts/index.tsx
+++ apps/ledger-live-desktop/src/renderer/screens/settings/sections/Accounts/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
 import TrackPage from "~/renderer/analytics/TrackPage";
 import { SettingsSectionRow as Row, SettingsSectionBody as Body } from "../../SettingsSection";
 import HideEmptyTokenAccountsToggle from "./HideEmptyTokenAccountsToggle";
 import FilterTokenOperationsZeroAmount from "./FilterTokenOperationsZeroAmount";
+import FilterTokenOperationsThreshold from "./FilterTokenOperationsThreshold";
 import SectionExport from "./Export";
 import Currencies from "./Currencies";
 import BlacklistedTokens from "./BlacklistedTokens";
@@ -11,6 +13,7 @@ import DoNotAskAgainSkipMemo from "./DoNotAskAgainSkipMemo";
 
 export default function SectionAccounts() {
   const { t } = useTranslation();
+  const lldHideSmallValueTokenOperations = useFeature("lldHideSmallValueTokenOperations");
 
   return (
     <Body>
@@ -23,6 +26,7 @@ export default function SectionAccounts() {
         <HideEmptyTokenAccountsToggle />
       </Row>
       <FilterTokenOperationsZeroAmount />
+      {lldHideSmallValueTokenOperations?.enabled && <FilterTokenOperationsThreshold />}
       <DoNotAskAgainSkipMemo />
       <BlacklistedTokens />
       <Currencies />
