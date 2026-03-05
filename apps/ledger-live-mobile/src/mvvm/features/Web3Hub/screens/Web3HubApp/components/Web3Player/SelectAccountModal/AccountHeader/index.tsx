b9e75337adad11398c378fc59056959d50f1b406
Canestin <canestin.ndong@ledger.fr>
2026-03-05 10:05:14 +0100

    feat(web3Hub): rework broken ui

--- apps/ledger-live-mobile/src/mvvm/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountModal/AccountHeader/index.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountModal/AccountHeader/index.tsx
@@ -8,16 +8,13 @@ export default function AccountHeaderWrapper(onBackPress: () => void) {
     const { t } = useTranslation();
 
     return (
-      <>
-        <Flex mt={6} mx={6} flexDirection={"row"}>
-          <BackButton onPress={onBackPress} />
-          <Flex flex={1} height={32} justifyContent={"center"}>
-            <Text variant="h5">{t("web3hub.app.selectAccountModal.accountHeader.title")}</Text>
-          </Flex>
-          {children}
+      <Flex mt={6} mx={6} flexDirection={"row"}>
+        <BackButton onPress={onBackPress} />
+        <Flex flex={1} height={32} justifyContent={"center"}>
+          <Text variant="h5">{t("web3hub.app.selectAccountModal.accountHeader.title")}</Text>
         </Flex>
-        <Box height="1px" width="100%" backgroundColor={"translucentGrey"} />
-      </>
+        {children}
+      </Flex>
     );
   };
 }
