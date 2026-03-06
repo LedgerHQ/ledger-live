ce2c0a84a6674229e5840cef31335ffeb672501a
Canestin <canestin.ndong@ledger.fr>
2026-03-06 09:54:51 +0100

    feat(web3Hub): correct ui

--- apps/ledger-live-mobile/src/mvvm/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountModal/AccountHeader/index.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Web3Hub/screens/Web3HubApp/components/Web3Player/SelectAccountModal/AccountHeader/index.tsx
@@ -1,20 +1,39 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
 import { useTranslation } from "~/context/Locale";
 import { Box, Flex, Text } from "@ledgerhq/native-ui";
+import { IconButton } from "@ledgerhq/lumen-ui-rnative";
 import BackButton from "./BackButton";
+import { Close } from "@ledgerhq/lumen-ui-rnative/symbols";
 
-export default function AccountHeaderWrapper(onBackPress: () => void) {
-  return function AccountHeader({ children }: PropsWithChildren) {
+export default function AccountHeaderWrapper(onBackPress: () => void, onClose: () => void) {
+  return function AccountHeader() {
     const { t } = useTranslation();
 
     return (
-      <Flex mt={6} mx={6} flexDirection={"row"}>
-        <BackButton onPress={onBackPress} />
-        <Flex flex={1} height={32} justifyContent={"center"}>
-          <Text variant="h5">{t("web3hub.app.selectAccountModal.accountHeader.title")}</Text>
+      <Box width="100%">
+        <Flex
+          mt={6}
+          mx={6}
+          mb={6}
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <BackButton onPress={onBackPress} />
+          <Text flex={1} variant="h5">
+            {t("web3hub.app.selectAccountModal.accountHeader.title")}
+          </Text>
+          <IconButton
+            appearance="transparent"
+            style={{ borderRadius: 999 }}
+            size="sm"
+            icon={Close}
+            accessibilityLabel="Close"
+            onPress={onClose}
+          />
         </Flex>
-        {children}
-      </Flex>
+        <Box height="1px" width="100%" backgroundColor={"translucentGrey"} />
+      </Box>
     );
   };
 }
