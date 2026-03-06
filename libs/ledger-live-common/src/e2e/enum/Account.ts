76f6718c953e51d781d3610d4e639f0901fdad10
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-03-06 16:47:11 +0000

    test(address): update hedera

--- libs/ledger-live-common/src/e2e/enum/Account.ts
+++ libs/ledger-live-common/src/e2e/enum/Account.ts
@@ -167,7 +167,7 @@ export class Account {
     undefined,
     undefined,
     undefined,
-    "0.0.10285205",
+    "0.0.10310433",
   );
   static readonly HEDERA_2 = new Account(
     Currency.HBAR,
@@ -178,7 +178,7 @@ export class Account {
     undefined,
     undefined,
     undefined,
-    "0.0.10285245",
+    "0.0.10337251",
   );
 
   static readonly INJ_1 = new Account(Currency.INJ, "Injective 1", 0, "44'/60'/0'/0/0");
