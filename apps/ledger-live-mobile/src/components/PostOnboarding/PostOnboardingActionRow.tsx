6200f4c70821bd486fb01b50f225a5d04e1729b6
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-03-10 14:10:55 +0000

    feat: add canShowRecover to useOpenPostOnboardingCallback

--- apps/ledger-live-mobile/src/components/PostOnboarding/PostOnboardingActionRow.tsx
+++ apps/ledger-live-mobile/src/components/PostOnboarding/PostOnboardingActionRow.tsx
@@ -41,12 +41,11 @@ const PostOnboardingActionRow: React.FC<Props> = props => {
     openActivationDrawer,
     isLedgerSyncActive,
     accounts,
-    shouldShow,
   } = props;
   const { t } = useTranslation();
   const recoverServices = useFeature("protectServicesMobile");
-  const [canShow, setCanShow] = useState<boolean>(!shouldShow);
   const protectId = recoverServices?.params?.protectId ?? "protect-prod";
+
 
   const navigation =
     useNavigation<
@@ -68,17 +67,6 @@ const PostOnboardingActionRow: React.FC<Props> = props => {
     initIsActionCompleted();
   }, [initIsActionCompleted]);
 
-  const checkCanShow = useCallback(async () => {
-    if (shouldShow) {
-      const canShowAction = await shouldShow({ protectId });
-
-      setCanShow(canShowAction);
-    }
-  }, [shouldShow, protectId]);
-
-  useEffect(() => {
-    checkCanShow();
-  }, [checkCanShow]);
 
   const customActionHandlers = usePostOnboardingActionHandlers();
 
@@ -114,8 +102,6 @@ const PostOnboardingActionRow: React.FC<Props> = props => {
     trackAction();
     shouldCompleteOnStart && completeAction(id);
   };
-
-  if (!canShow) return null;
 
   return (
     <Touchable
