5fecd99d662a808aa3160ef91e27cc2cb24f15ff
Gabriel Scavassa <gabriel.scavassa@yld.com>
2026-03-13 11:20:23 +0000

    feat: adding index references

--- libs/coin-modules/coin-tezos/src/network/index.ts
+++ libs/coin-modules/coin-tezos/src/network/index.ts
@@ -1,4 +1,4 @@
 import * as bakers from "./bakers";
-import tzkt, { fetchAllTransactions } from "./tzkt";
+import tzkt, { fetchAllTransactions, fetchBlockTransactions, fetchBlockTokenTransfers } from "./tzkt";
 
-export { bakers, tzkt, fetchAllTransactions };
+export { bakers, tzkt, fetchAllTransactions, fetchBlockTransactions, fetchBlockTokenTransfers };
