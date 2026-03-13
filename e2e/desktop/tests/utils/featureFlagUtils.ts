89b39be20afa31168654e10d36a2ed3a52e3346f
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-03-13 00:25:37 +0000

    test(e2e-desktop): backward compatible main navigation
    test(e2e-desktop): apply to all delegate tests
    
    test(e2e-desktop): split legacy test case
    
    test(e2e-desktop): track tech debt
    
    test(e2e-desktop): stick to convention for readability
    
    test(e2e-desktop): prefer playwright test id

--- e2e/desktop/tests/utils/featureFlagUtils.ts
+++ e2e/desktop/tests/utils/featureFlagUtils.ts
@@ -1,4 +1,17 @@
 import { OptionalFeatureMap } from "@ledgerhq/types-live";
+import { Page } from "@playwright/test";
+
+export const getFeatureFlags = async (page: Page): Promise<OptionalFeatureMap> => {
+  const featureFlags = await page.evaluate(() => {
+    return window.getAllFeatureFlags("en");
+  });
+  return featureFlags;
+};
+
+export const isWallet40Enabled = async (page: Page): Promise<boolean> => {
+  const featureFlags = await getFeatureFlags(page);
+  return featureFlags.lwdWallet40?.enabled === true;
+};
 
 // TODO: remove when wallet 4.0 is default
 export const LWD_WALLET_40_FF_ENABLED: OptionalFeatureMap = {
