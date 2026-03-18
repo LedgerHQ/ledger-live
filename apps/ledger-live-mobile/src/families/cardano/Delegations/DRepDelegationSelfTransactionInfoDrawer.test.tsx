e207bc63557148299158d90737cc4363414ba4bd
Pedro Martins <pedro.martins-ext@ledger.fr>
2026-03-18 18:02:49 +0000

    fix: [LIVE-23185] resolve lint errors

--- apps/ledger-live-mobile/src/families/cardano/Delegations/DRepDelegationSelfTransactionInfoDrawer.test.tsx
+++ apps/ledger-live-mobile/src/families/cardano/Delegations/DRepDelegationSelfTransactionInfoDrawer.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { fireEvent, screen } from "@tests/test-renderer";
-import { render } from "@tests/test-renderer";
+import { fireEvent, screen, render } from "@tests/test-renderer";
 import DRepDelegationSelfTransactionInfoDrawer from "./DRepDelegationSelfTransactionInfoDrawer";
 import { CardanoAccount } from "@ledgerhq/live-common/families/cardano/types";
 import { getAccountBridge } from "@ledgerhq/live-common/bridge/index";
