dc0071f340872a351909edbb040f4829ffebf2bd
Victor Alber <victor.alber@ledger.fr>
2026-03-24 14:28:50 +0100

    test(e2e): re-enable account rename test - B2CQA-2996
    Remove describe.skip to activate the account rename persistence test,
    which verifies the renamed account is restored after app kill and restart.

--- e2e/mobile/specs/delegate/voteCELO.spec.ts
+++ e2e/mobile/specs/delegate/voteCELO.spec.ts
@@ -2,4 +2,4 @@ import { Account } from "@ledgerhq/live-common/e2e/enum/Account";
 import { runVoteCelo } from "./delegate";
 
 const delegation = new Delegate(Account.CELO_1, "0.001", "Ledger by Figment");
-runVoteCelo(delegation, ["B2CQA-todo"]);
+runVoteCelo(delegation, ["B2CQA-201"]);
