7e1c22ab71cae31784a5414c471392951ae32de6
Ilia Shabanov <ishaba@me.com>
2026-03-25 21:53:52 +0500

    chore: revert upstream bundle workaround, upgrade to 0.8.0

--- libs/ledgerjs/packages/hw-app-sui/src/Sui.ts
+++ libs/ledgerjs/packages/hw-app-sui/src/Sui.ts
@@ -1,15 +1,8 @@
-// Re-export the upstream @mysten/ledgerjs-hw-app-sui Sui class from inlined source.
-// The upstream package (0.7.1) ships a mega-bundle that inlines all dependencies
-// (RxJS, @ledgerhq/*, etc.), causing ~900KB of duplication in the final bundle.
-// By importing from source and resolving dependencies from the monorepo, we avoid
-// bundling duplicate copies.
-// Can be reverted to normal import once the upstream package is fixed.
-// See: https://github.com/MystenLabs/ts-sdks/issues/967
-export { default } from "./upstream/Sui";
+export { default } from "@mysten/ledgerjs-hw-app-sui";
 export type {
   GetPublicKeyResult,
   SignTransactionResult,
   GetVersionResult,
   Resolution,
   AppConfig,
-} from "./upstream/Sui";
+} from "@mysten/ledgerjs-hw-app-sui";
