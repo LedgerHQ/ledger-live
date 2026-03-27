21cb912215a7962def01798d02a2e279f2e69c1e
Victor Alber <victor.alber@ledger.fr>
2026-03-27 16:58:19 +0100

    chore(mobile): add changeset for disabled swap e2e test

--- e2e/mobile/specs/swap/swapETH_DOT.spec.ts
+++ e2e/mobile/specs/swap/swapETH_DOT.spec.ts
@@ -1,6 +1,3 @@
-import { Account } from "@ledgerhq/live-common/e2e/enum/Account";
-import { runSwapTest } from "./swap";
-
 //TODO: Reactivate when DOT is reactivated on swap prod
 // const swap = new Swap(Account.ETH_1, Account.DOT_1, "0.02", Fee.MEDIUM);
 // runSwapTest(
