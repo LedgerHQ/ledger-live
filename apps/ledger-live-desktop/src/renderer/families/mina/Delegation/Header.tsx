e383d42817fddfd6d9920c8fd13384633b12bb82
Teddy Chambard <teddy.chambard@ledger.fr>
2026-03-28 00:32:41 +0100

    feat(desktop): mina undelegate flow and delegate wording

---
+++ apps/ledger-live-desktop/src/renderer/families/mina/Delegation/Header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Trans } from "react-i18next";
+import styled from "styled-components";
+import Box from "~/renderer/components/Box/Box";
+import { HeaderWrapper } from "~/renderer/components/TableContainer";
+
+export const TableLine = styled(Box).attrs(() => ({
+  ff: "Inter|SemiBold",
+  color: "neutral.c70",
+  horizontal: true,
+  alignItems: "center",
+  justifyContent: "flex-start",
+  fontSize: 3,
+  flex: 1.125,
+  pr: 2,
+}))`
+  box-sizing: border-box;
+  &:last-child {
+    justify-content: flex-end;
+    flex: 0.5;
+    text-align: right;
+    white-space: nowrap;
+  }
+`;
+
+export const Header = () => (
+  <HeaderWrapper>
+    <TableLine>
+      <Trans i18nKey="delegation.validator" />
+    </TableLine>
+    <TableLine>
+      <Trans i18nKey="mina.delegation.amount" />
+    </TableLine>
+    <TableLine />
+  </HeaderWrapper>
+);
