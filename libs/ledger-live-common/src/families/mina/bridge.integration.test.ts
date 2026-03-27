d9a96a433761cb2f196ea2adb14f749b9a13b533
Teddy Chambard <teddy.chambard@ledger.fr>
2026-03-28 00:32:24 +0100

    feat(live-common): re-enable mina bridge integration tests

--- libs/ledger-live-common/src/families/mina/bridge.integration.test.ts
+++ libs/ledger-live-common/src/families/mina/bridge.integration.test.ts
@@ -2,7 +2,4 @@ import "../../__tests__/test-helpers/setup.integration";
 import { testBridge } from "../../__tests__/test-helpers/bridge";
 import { dataset } from "@ledgerhq/coin-mina/test/index";
 
-// FIXME: Disabled due to persistent 502 errors from Mina backend (Rosetta POST endpoints have no retry in live-network)
-describe.skip("mina integration", () => {
-  testBridge(dataset);
-});
+testBridge(dataset);
