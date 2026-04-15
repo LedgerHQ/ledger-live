aa3be649bfa9e930fab1d93cc054d1ac15407c57
Claudia <claudiiaguerreiro@hotmail.com>
2026-04-15 11:10:19 +0100

    feat: use UserAvatar as trigger for MyWallet context menu
    fix

---
+++ apps/ledger-live-desktop/src/mvvm/features/MyWallet/components/UserAvatar/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Avatar } from "@ledgerhq/lumen-ui-react";
+
+export function UserAvatar() {
+  return (
+    <Avatar size="sm" aria-label="Avatar" data-testid="my-wallet-avatar" className="text-white" />
+  );
+}
