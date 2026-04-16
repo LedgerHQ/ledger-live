651ae6bdfbb6e3e88e672125de59cd08f9c12564
Valentin <valentin.lambert@ledger.fr>
2026-04-16 11:35:18 +0200

    chore(safearea): remove safearea from borrow live-app

--- apps/ledger-live-mobile/src/mvvm/features/Borrow/components/BorrowWebView/BorrowWebView.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Borrow/components/BorrowWebView/BorrowWebView.tsx
@@ -2,7 +2,6 @@ import { LiveAppManifest } from "@ledgerhq/live-common/platform/types";
 import React, { forwardRef, useMemo } from "react";
 import { Web3AppWebview } from "~/components/Web3AppWebview";
 import { WebviewAPI, WebviewState } from "~/components/Web3AppWebview/types";
-import SafeAreaView from "~/components/SafeAreaView";
 import type { BorrowWebviewInputs } from "LLM/features/Borrow/screens/BorrowLiveApp/useBorrowLiveAppViewModel";
 import { WalletAPICustomHandlers } from "@ledgerhq/live-common/wallet-api/types";
 import { useDeeplinkCustomHandlers } from "~/components/WebPlatformPlayer/CustomHandlers";
@@ -22,15 +21,13 @@ export const BorrowWebView = forwardRef<WebviewAPI, Props>(
     );
 
     return (
-      <SafeAreaView isFlex>
-        <Web3AppWebview
-          ref={ref}
-          manifest={manifest}
-          onStateChange={setWebviewState}
-          inputs={inputs}
-          customHandlers={customHandlers}
-        />
-      </SafeAreaView>
+      <Web3AppWebview
+        ref={ref}
+        manifest={manifest}
+        onStateChange={setWebviewState}
+        inputs={inputs}
+        customHandlers={customHandlers}
+      />
     );
   },
 );
