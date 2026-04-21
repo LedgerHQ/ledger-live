dce0c2e255f6bca4730029903d9eca1d4a3808ab
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-04-21 16:34:02 +0100

    feat: add recover banner carousel to portfolio
    feat: add recover banner should display test
    
    feat: add recover banner view model test
    
    chore: add changeset
    
    chore: code review updates
    
    chore: fix lint
    
    chore: make content width constant
    
    chore: fix test
    
    chore: type fix
    
    chore: correct spacing of pagination
    
    chore: cr changes
    
    feat: add recover banner lumen
    
    feat: add styling for width of onboarding widget and recover banner
    
    feat: add tests
    
    feat: add tracking
    
    feat: improve test
    
    chore: update test
    
    chore: add event guard for close banner
    
    chore: remove unused code
    
    chore: remove unused prop
    
    chore: fix test
    
    feat: add rtk query for recover banner state
    
    chore: update test name
    
    lint
    
    chore: type
    
    feat: remove trk and use effect and state calls

---
+++ apps/ledger-live-mobile/src/mvvm/features/Portfolio/components/RecoverBanner/useShouldDisplayRecoverBanner.ts
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { addPostOnboardingAction } from "@ledgerhq/live-common/postOnboarding/actions";
+import { usePostOnboardingHubState } from "@ledgerhq/live-common/postOnboarding/hooks/index";
+import { PostOnboardingActionId } from "@ledgerhq/types-live";
+import { useDispatch } from "~/context/hooks";
+import {
+  LedgerRecoverSubscriptionStateEnum,
+  LedgerRecoverSubscriptionStateInProgressEnum,
+} from "~/types/recoverSubscriptionState";
+import useRecoverBannerStorage from "../../hooks/useRecoverBannerStorage";
+
+function useShouldDisplayRecoverBanner() {
+  const recoverServices = useFeature("protectServicesMobile");
+  const bannerIsEnabled = recoverServices?.params?.bannerSubscriptionNotification;
+  const protectID = recoverServices?.params?.protectId ?? "protect-prod";
+
+  const dispatch = useDispatch();
+
+  const { actionsState } = usePostOnboardingHubState();
+  const actionStateRecover = actionsState.find(
+    action => action.id === PostOnboardingActionId.recover,
+  );
+
+  const { data } = useRecoverBannerStorage(protectID);
+
+  useEffect(() => {
+    if (
+      data?.subscriptionState !== undefined &&
+      (data.subscriptionState in LedgerRecoverSubscriptionStateInProgressEnum ||
+        data.subscriptionState === LedgerRecoverSubscriptionStateEnum.BACKUP_DONE) &&
+      actionStateRecover === undefined
+    ) {
+      dispatch(addPostOnboardingAction({ actionId: PostOnboardingActionId.recover }));
+    }
+  }, [data?.subscriptionState, actionStateRecover, dispatch]);
+
+  const inProgress =
+    (data?.subscriptionState ?? LedgerRecoverSubscriptionStateEnum.NO_SUBSCRIPTION) in
+    LedgerRecoverSubscriptionStateInProgressEnum;
+  const shouldDisplay = bannerIsEnabled && inProgress && data?.displayBanner;
+
+  return !!shouldDisplay;
+}
+
+export default useShouldDisplayRecoverBanner;
