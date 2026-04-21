e165a0ad28160b658c901715996ce96d1878e86e
mitchellv-ledger <mitchell.vivanti-ext@ledger.fr>
2026-04-21 16:34:02 +0100

    feat: add recover banner carousel to portfolio
    feat: add recover banner should display test
    
    feat: add recover banner view model test
    
    chore: add changeset
    
    chore: code review updates
    
    chore: fix lint
    
    chore: make content width constant
    
    chore: fix test
    
    chore: type fix
    
    chore: correct spacing of pagination
    
    chore: cr changes
    
    feat: add recover banner lumen
    
    feat: add styling for width of onboarding widget and recover banner
    
    feat: add tests
    
    feat: add tracking
    
    feat: improve test
    
    chore: update test
    
    chore: add event guard for close banner
    
    chore: remove unused code
    
    chore: remove unused prop
    
    chore: fix test
    
    feat: add rtk query for recover banner state
    
    chore: update test name
    
    lint
    
    chore: type
    
    feat: remove trk and use effect and state calls

---
+++ apps/ledger-live-mobile/src/mvvm/features/Portfolio/components/RecoverBanner/useRecoverBannerViewModel.ts
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+import { GestureResponderEvent, Linking } from "react-native";
+import { useFeature } from "@ledgerhq/live-common/featureFlags/index";
+import { useCustomURI } from "@ledgerhq/live-common/hooks/recoverFeatureFlag";
+import { addPostOnboardingAction } from "@ledgerhq/live-common/postOnboarding/actions";
+import { usePostOnboardingHubState } from "@ledgerhq/live-common/postOnboarding/hooks/index";
+import { PostOnboardingActionId } from "@ledgerhq/types-live";
+import { useDispatch } from "~/context/hooks";
+import { useTranslation } from "~/context/Locale";
+import {
+  LedgerRecoverSubscriptionStateEnum,
+  LedgerRecoverSubscriptionStateInProgressEnum,
+} from "~/types/recoverSubscriptionState";
+import { track } from "~/analytics";
+import useRecoverBannerStorage from "../../hooks/useRecoverBannerStorage";
+
+function useRecoverBannerViewModel() {
+  const { t } = useTranslation();
+  const recoverServices = useFeature("protectServicesMobile");
+  const dispatch = useDispatch();
+  const bannerIsEnabled = recoverServices?.params?.bannerSubscriptionNotification;
+  const protectID = recoverServices?.params?.protectId ?? "protect-prod";
+
+  const { actionsState } = usePostOnboardingHubState();
+  const actionStateRecover = actionsState.find(
+    action => action.id === PostOnboardingActionId.recover,
+  );
+
+  const recoverResumeActivatePath = useCustomURI(
+    recoverServices,
+    "resumeActivate",
+    "llm-banner-unfinished-onboarding",
+    "recover-launch",
+  );
+
+  const { data, dismissBanner } = useRecoverBannerStorage(protectID);
+
+  const subscriptionState =
+    data?.subscriptionState ?? LedgerRecoverSubscriptionStateEnum.NO_SUBSCRIPTION;
+  const isInProgress = subscriptionState in LedgerRecoverSubscriptionStateInProgressEnum;
+
+  useEffect(() => {
+    if (
+      data?.subscriptionState !== undefined &&
+      (data.subscriptionState in LedgerRecoverSubscriptionStateInProgressEnum ||
+        data.subscriptionState === LedgerRecoverSubscriptionStateEnum.BACKUP_DONE) &&
+      actionStateRecover === undefined
+    ) {
+      dispatch(addPostOnboardingAction({ actionId: PostOnboardingActionId.recover }));
+    }
+  }, [data?.subscriptionState, actionStateRecover, dispatch]);
+
+  const onRedirectRecover = async () => {
+    track("banner_clicked", {
+      name: "recover",
+    });
+    if (!recoverResumeActivatePath) return;
+    try {
+      const canOpen = await Linking.canOpenURL(recoverResumeActivatePath);
+      if (canOpen) {
+        await Linking.openURL(recoverResumeActivatePath);
+      }
+    } catch {
+      // eslint-disable-next-line no-console
+      console.warn("Cannot open recover resume path");
+    }
+  };
+
+  const onCloseBanner = (event?: GestureResponderEvent) => {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    dismissBanner();
+    track("button_clicked", {
+      button: "close",
+      source: "recover banner",
+    });
+  };
+
+  const shouldDisplay = bannerIsEnabled && isInProgress && data?.displayBanner;
+
+  const title = t("portfolio.recoverBanner.title");
+  const description = t("portfolio.recoverBanner.description");
+
+  return {
+    onRedirectRecover,
+    onCloseBanner,
+    shouldDisplay,
+    title,
+    description,
+  };
+}
+
+export default useRecoverBannerViewModel;
