6c62fadb5b9eaf46d13b20b9c4dcb7cb0afa61e0
RobinVncnt <robin.vincent@ledger.fr>
2026-04-24 16:48:21 +0200

    feat: lwd product tour dialog

---
+++ apps/ledger-live-desktop/src/mvvm/features/ProductTour/Drawer/index.ts
@@ -0,0 +1,3 @@
+export { useProductTourDialogViewModel } from "./hooks/useProductTourDialogViewModel";
+export type { ProductTourDialogViewModel } from "./hooks/useProductTourDialogViewModel";
+export { ProductTourDialog } from "./ProductTourDialog";
