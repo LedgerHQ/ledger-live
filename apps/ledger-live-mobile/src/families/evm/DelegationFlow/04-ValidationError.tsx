1e76d287f14824cf86897badf9d0db516873f89d
qperrot <quentin.perrot-minnot@ledger.fr>
2026-04-27 16:43:58 +0200

    feat(lwm delegation): add delegation table ui on lwm

---
+++ apps/ledger-live-mobile/src/families/evm/DelegationFlow/04-ValidationError.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getAccountCurrency } from "@ledgerhq/live-common/account/index";
+import { useTheme } from "@react-navigation/native";
+import { TrackScreen } from "~/analytics";
+import ValidateError from "~/components/ValidateError";
+import {
+  BaseComposite,
+  StackNavigatorNavigation,
+  StackNavigatorProps,
+} from "~/components/RootNavigator/types/helpers";
+import { ScreenName } from "~/const";
+import type { BaseNavigatorStackParamList } from "~/components/RootNavigator/types/BaseNavigator";
+import type { EvmDelegationFlowParamList } from "./types";
+import { useAccountScreen } from "LLM/hooks/useAccountScreen";
+
+type Props = BaseComposite<
+  StackNavigatorProps<EvmDelegationFlowParamList, ScreenName.EvmDelegationValidationError>
+>;
+
+export default function ValidationError({ navigation, route }: Props) {
+  const { colors } = useTheme();
+  const { account } = useAccountScreen(route);
+  const { ticker } = getAccountCurrency(account);
+
+  const onClose = useCallback(() => {
+    navigation.getParent<StackNavigatorNavigation<BaseNavigatorStackParamList>>().pop();
+  }, [navigation]);
+
+  const retry = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const error = route.params.error;
+
+  return (
+    <SafeAreaView style={[styles.root, { backgroundColor: colors.background }]}>
+      <TrackScreen
+        category="EvmDelegation"
+        name="ValidationError"
+        flow="stake"
+        action="delegation"
+        currency={ticker}
+      />
+      <ValidateError error={error} onRetry={retry} onClose={onClose} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
