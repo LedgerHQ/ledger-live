74d3b32a1e93c21373ece5ebd27414147da3f964
Alessandro Vito <alessandro.vito84@gmail.com>
2026-04-28 14:03:25 +0100

    fix(coin-vechain): add missing unit tests for network/sdk module

--- libs/coin-modules/coin-vechain/src/network/sdk.test.ts
+++ libs/coin-modules/coin-vechain/src/network/sdk.test.ts
@@ -1,8 +1,38 @@
+import BigNumber from "bignumber.js";
 import { LedgerAPI4xx } from "@ledgerhq/errors";
-import { getOperations, getTokenOperations } from "./sdk";
+import {
+  getAccount,
+  getBlockRef,
+  getFees,
+  getLastBlockHeight,
+  getOperations,
+  getTokenOperations,
+} from "./sdk";
+import { AccountResponse } from "../types";
+import { Operation } from "@ledgerhq/types-live";
 
-const mockTransferLogs = jest.fn(() => ({ data: [] }));
-const mockEventLogs = jest.fn(() => ({ data: [] }));
+const LAST_BLOCK_COUNT = 24580112;
+const mockAccount = {
+  balance: "",
+  energy: "",
+  hasCode: false,
+};
+
+const mockGetTransferLogs = jest.fn(async (): Promise<{ data: Operation[] }> => ({ data: [] }));
+const mockGetEventLogs = jest.fn(async (): Promise<{ data: Operation[] }> => ({ data: [] }));
+const mockGetAccount = jest.fn(
+  async (): Promise<{ data: AccountResponse }> => ({ data: mockAccount }),
+);
+const mockGetLastBlockCount = jest.fn(
+  async (): Promise<{ data: { number: number; id: string } }> => ({
+    data: { number: LAST_BLOCK_COUNT, id: "abcdefghiklmnopqrstuvwxyz" },
+  }),
+);
+const mockGetFees = jest.fn(
+  async (): Promise<{ data: { paid: string } }> => ({
+    data: { paid: "42" },
+  }),
+);
 
 jest.mock("../common-logic", () => ({
   mapTokenTransfersToOperations: jest.fn(() => [{}]),
@@ -13,12 +43,26 @@ jest.mock("../common-logic", () => ({
 jest.mock("@ledgerhq/live-network", () => {
   return async (args: { method: string; url: string; data: string }) => {
     if (args.url.match(/\/logs\/transfer$/)) {
-      return mockTransferLogs();
+      return mockGetTransferLogs();
     }
 
     if (args.url.match(/\/logs\/event$/)) {
-      return mockEventLogs();
+      return mockGetEventLogs();
     }
+
+    if (args.url.match(/\/accounts/)) {
+      return mockGetAccount();
+    }
+
+    if (args.url.match(/\/blocks\/best$/)) {
+      return mockGetLastBlockCount();
+    }
+
+    if (args.url.match(/\/transactions\/([^/]+)\/receipt$/)) {
+      return mockGetFees();
+    }
+
+    return jest.fn();
   };
 });
 
@@ -33,9 +77,35 @@ const throws403ExceedsLimit = () => {
   });
 };
 
-const LAST_BLOCK_COUNT = 24580112;
-
 describe("sdk", () => {
+  describe("getAccount", () => {
+    test("retrieves an account", async () => {
+      const account = await getAccount("0xmy-address");
+      expect(account).toBe(mockAccount);
+    });
+  });
+
+  describe("getLastBlockHeight", () => {
+    test("retrieves the last block height", async () => {
+      const account = await getLastBlockHeight();
+      expect(account).toBe(LAST_BLOCK_COUNT);
+    });
+  });
+
+  describe("getBlockRef", () => {
+    test("retrieves the block ref to use in transaction", async () => {
+      const account = await getBlockRef();
+      expect(account).toBe("abcdefghiklmnopqrs");
+    });
+  });
+
+  describe("getFees", () => {
+    test("retrieves fees paid for the transaction", async () => {
+      const account = await getFees("0xtransaxtion");
+      expect(account).toStrictEqual(new BigNumber(42));
+    });
+  });
+
   describe("getOperations", () => {
     describe("when logs retrieved in each network request are within the maximum allowed limit", () => {
       test("retrieves all the operations", async () => {
@@ -53,7 +123,7 @@ describe("sdk", () => {
 
     describe("when logged operations exceed limit in the given range", () => {
       beforeEach(() => {
-        mockTransferLogs.mockImplementationOnce(throws403ExceedsLimit);
+        mockGetTransferLogs.mockImplementationOnce(throws403ExceedsLimit);
       });
 
       describe("and Vechain operations range cannot be split further", () => {
@@ -109,7 +179,7 @@ describe("sdk", () => {
 
     describe("when logged operations exceed limit in the given range", () => {
       beforeEach(() => {
-        mockEventLogs.mockImplementationOnce(throws403ExceedsLimit);
+        mockGetEventLogs.mockImplementationOnce(throws403ExceedsLimit);
       });
 
       describe("and Vechain operations range cannot be split further", () => {
