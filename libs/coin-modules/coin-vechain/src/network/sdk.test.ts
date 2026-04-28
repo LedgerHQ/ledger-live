c784c292d45f162e56d78276f5f575640ed297ce
Alessandro Vito <alessandro.vito84@gmail.com>
2026-04-28 15:15:34 +0100

    fix(coin-vechain): add 100% unit tests coverage to sdk.test

--- libs/coin-modules/coin-vechain/src/network/sdk.test.ts
+++ libs/coin-modules/coin-vechain/src/network/sdk.test.ts
@@ -1,4 +1,5 @@
 import BigNumber from "bignumber.js";
+import type { Operation } from "@ledgerhq/types-live";
 import { LedgerAPI4xx } from "@ledgerhq/errors";
 import {
   getAccount,
@@ -7,9 +8,9 @@ import {
   getLastBlockHeight,
   getOperations,
   getTokenOperations,
+  submit,
 } from "./sdk";
-import { AccountResponse } from "../types";
-import { Operation } from "@ledgerhq/types-live";
+import type { AccountResponse, VechainSDKTransaction } from "../types";
 
 const LAST_BLOCK_COUNT = 24580112;
 const mockAccount = {
@@ -17,6 +18,12 @@ const mockAccount = {
   energy: "",
   hasCode: false,
 };
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+const mockTransaction: VechainSDKTransaction = {
+  get encoded() {
+    return new Uint8Array();
+  },
+} as unknown as VechainSDKTransaction;
 
 const mockGetTransferLogs = jest.fn(async (): Promise<{ data: Operation[] }> => ({ data: [] }));
 const mockGetEventLogs = jest.fn(async (): Promise<{ data: Operation[] }> => ({ data: [] }));
@@ -29,8 +36,13 @@ const mockGetLastBlockCount = jest.fn(
   }),
 );
 const mockGetFees = jest.fn(
-  async (): Promise<{ data: { paid: string } }> => ({
+  async (): Promise<{ data: { paid?: string } }> => ({
     data: { paid: "42" },
+  }),
+);
+const mockSubmit = jest.fn(
+  async (): Promise<{ data: { id?: string } }> => ({
+    data: { id: "123" },
   }),
 );
 
@@ -62,6 +74,10 @@ jest.mock("@ledgerhq/live-network", () => {
       return mockGetFees();
     }
 
+    if (args.url.match(/\/transactions$/)) {
+      return mockSubmit();
+    }
+
     return jest.fn();
   };
 });
@@ -72,6 +88,16 @@ const throws403ExceedsLimit = () => {
 
   throw new LedgerAPI4xx(msg, {
     status: 403,
+    url: "",
+    method: "POST",
+  });
+};
+
+const throws500ServerError = () => {
+  const msg = "internal server error";
+
+  throw new LedgerAPI4xx(msg, {
+    status: 500,
     url: "",
     method: "POST",
   });
@@ -100,13 +126,72 @@ describe("sdk", () => {
   });
 
   describe("getFees", () => {
-    test("retrieves fees paid for the transaction", async () => {
-      const account = await getFees("0xtransaxtion");
-      expect(account).toStrictEqual(new BigNumber(42));
+    describe("when the transaction's receipt doesn't have any data.paid", () => {
+      test("retrieves fees paid for the transaction", async () => {
+        mockGetFees.mockImplementationOnce(async () => ({ data: {} }));
+
+        const account = await getFees("0xtransaxtion");
+        expect(account).toStrictEqual(new BigNumber(0));
+      });
+    });
+
+    describe("when the transaction's receipt has data.paid", () => {
+      test("retrieves fees paid for the transaction", async () => {
+        const account = await getFees("0xtransaxtion");
+        expect(account).toStrictEqual(new BigNumber(42));
+      });
+    });
+  });
+
+  describe("submit", () => {
+    describe("when transaction id is included in the transaction payload returned by the server", () => {
+      test("returns the transaction id", async () => {
+        const transactionId = await submit(mockTransaction);
+        expect(transactionId).toBe("123");
+      });
+    });
+
+    describe("when transaction id isn't included in the transaction payload returned by the server", () => {
+      test("throws an error", async () => {
+        mockSubmit.mockImplementationOnce(async () => ({ data: {} }));
+
+        try {
+          await submit(mockTransaction);
+        } catch (err: unknown) {
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          const error = err as { message: string };
+          expect(error.message).toMatch("Expected an ID");
+        }
+
+        expect.assertions(1);
+      });
     });
   });
 
   describe("getOperations", () => {
+    describe("when an unhandled error occurs", () => {
+      beforeEach(() => {
+        mockGetTransferLogs.mockImplementationOnce(throws500ServerError);
+      });
+
+      test("rethrows the error", async () => {
+        try {
+          await getOperations(
+            "my-account-id",
+            "0xmy-address",
+            LAST_BLOCK_COUNT - 1,
+            LAST_BLOCK_COUNT,
+          );
+        } catch (err: unknown) {
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          const error = err as { message: string };
+          expect(error.message).toMatch(/internal server error/);
+        }
+
+        expect.assertions(1);
+      });
+    });
+
     describe("when logs retrieved in each network request are within the maximum allowed limit", () => {
       test("retrieves all the operations", async () => {
         const operations = await getOperations(
@@ -162,6 +247,29 @@ describe("sdk", () => {
   });
 
   describe("getTokenOperations", () => {
+    describe("when an unhandled error occurs", () => {
+      beforeEach(() => {
+        mockGetTransferLogs.mockImplementationOnce(throws500ServerError);
+      });
+
+      test("rethrows the error", async () => {
+        try {
+          await getOperations(
+            "my-account-id",
+            "0xmy-address",
+            LAST_BLOCK_COUNT - 1,
+            LAST_BLOCK_COUNT,
+          );
+        } catch (err: unknown) {
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          const error = err as { message: string };
+          expect(error.message).toMatch(/internal server error/);
+        }
+
+        expect.assertions(1);
+      });
+    });
+
     describe("when logs retrieved in each network request are within the maximum allowed limit", () => {
       test("retrieves all the operations", async () => {
         const operations = await getTokenOperations(
