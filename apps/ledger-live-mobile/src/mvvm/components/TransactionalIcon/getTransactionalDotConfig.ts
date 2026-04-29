62f93e2b951ac4a9b3e215c5fdd09466c3940768
Lucas Werey <lucas.werey@ledger.fr>
2026-04-29 14:13:10 +0200

    :sparkles: feat(mobile): increase tx icon mapping

--- apps/ledger-live-mobile/src/mvvm/components/TransactionalIcon/getTransactionalDotConfig.ts
+++ apps/ledger-live-mobile/src/mvvm/components/TransactionalIcon/getTransactionalDotConfig.ts
@@ -40,21 +40,37 @@ export function getTransactionalDotConfig(
 
   switch (operationType) {
     case "IN":
+    case "NFT_IN":
       return { icon: ArrowDown, appearance: "success" };
     case "OUT":
+    case "NFT_OUT":
       return { icon: ArrowUp, appearance: "muted" };
     case "FEES":
       return { icon: Invoice, appearance: "muted" };
     case "REWARD":
     case "WITHDRAW":
+    case "WITHDRAW_EXPIRE_UNFREEZE":
+    case "REWARD_PAYOUT":
+    case "WITHDRAW_UNBONDED":
+    case "WITHDRAW_UNSTAKED":
       return { icon: Star, appearance: "success" };
     case "DELEGATE":
+    case "REDELEGATE":
+    case "BOND":
+    case "LOCK":
+    case "STAKE":
       return { icon: Link, appearance: "muted" };
     case "UNDELEGATE":
+    case "UNDELEGATE_RESOURCE":
+    case "UNBOND":
+    case "UNLOCK":
+    case "UNSTAKE":
       return { icon: Unlink, appearance: "muted" };
     case "APPROVE":
       return { icon: PenEdit, appearance: "muted" };
     case "FREEZE":
+    case "UNFREEZE":
+    case "LEGACY_UNFREEZE":
       return { icon: Snow, appearance: "muted" };
     case "VOTE":
       return { icon: Mailbox, appearance: "muted" };
