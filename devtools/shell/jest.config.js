90c610a8099571258b83adac0fe7aa8d62ce4841
Sebastien David <sebastien.david@ledger.fr>
2026-05-04 11:58:36 +0200

    feat: add mock renderer for mobile

--- devtools/shell/jest.config.js
+++ devtools/shell/jest.config.js
@@ -18,9 +18,6 @@ module.exports = {
   },
   moduleFileExtensions: ["web.tsx", "web.ts", "tsx", "ts", "js", "jsx", "json", "node"],
   testPathIgnorePatterns: ["\\.native\\.test\\."],
-  moduleNameMapper: {
-    "^@ledgerhq/lumen-ui-react/symbols$": "<rootDir>/jest/mocks/lumen-ui-react-symbols.tsx",
-  },
   modulePaths: ["<rootDir>"],
   transformIgnorePatterns: [
     "node_modules/.pnpm/(?!(@ledgerhq\\+lumen-ui-react|@ledgerhq\\+lumen-design-core))",
