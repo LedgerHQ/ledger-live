83f5a837f07458eb6990e58469c34abaac5ca795
Breban Sergiu <breban.sergiu-ext@ledger.fr>
2026-05-04 18:52:16 +0300

    fix: read earnUIVersion from ff directly

--- apps/ledger-live-mobile/src/mvvm/features/LiveAppModal/useEarnLiveAppModalContentViewModel.ts
+++ apps/ledger-live-mobile/src/mvvm/features/LiveAppModal/useEarnLiveAppModalContentViewModel.ts
@@ -4,6 +4,7 @@ import {
   getEthDepositScreenSetting,
 } from "@ledgerhq/live-common/featureFlags/stakePrograms/index";
 import { useWalletFeaturesConfig } from "@ledgerhq/live-common/featureFlags/index";
+import useFeature from "@ledgerhq/live-common/featureFlags/useFeature";
 import { useVersionedStakePrograms } from "LLM/hooks/useStake/useVersionedStakePrograms";
 import type { ExtraInputs } from "./useLiveAppModalContentViewModel";
 
@@ -14,13 +15,14 @@ export interface EarnLiveAppModalContentViewModel {
 const useEarnLiveAppModalContentViewModel = (): EarnLiveAppModalContentViewModel => {
   const { isEnabled: isLwm40Enabled } = useWalletFeaturesConfig("mobile");
   const stakePrograms = useVersionedStakePrograms();
+  const earnUiVersion = useFeature("ptxEarnUi")?.params?.value ?? "v1";
 
   const extraInputs = useMemo<ExtraInputs>(() => {
     const { stakeProgramsParam } = stakeProgramsToEarnParam(stakePrograms);
     const stakeCurrenciesParam = stakePrograms?.params?.list;
     const ethDepositCohort = getEthDepositScreenSetting(stakePrograms);
     return {
-      uiVersion: isLwm40Enabled ? "v2" : "v1",
+      uiVersion: earnUiVersion,
       lw40enabled: isLwm40Enabled ? "true" : "false",
       ethDepositCohort,
       stakeProgramsParam: stakeProgramsParam ? JSON.stringify(stakeProgramsParam) : undefined,
@@ -28,7 +30,7 @@ const useEarnLiveAppModalContentViewModel = (): EarnLiveAppModalContentViewModel
         ? JSON.stringify(stakeCurrenciesParam)
         : undefined,
     };
-  }, [stakePrograms, isLwm40Enabled]);
+  }, [stakePrograms, isLwm40Enabled, earnUiVersion]);
 
   return { extraInputs };
 };
