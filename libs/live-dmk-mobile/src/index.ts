e7f8cdb953e671f4f24d419ad844279306b5a67a
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2026-05-07 12:02:18 +0200

    feat(lwm): debug screen for new discovery service

--- libs/live-dmk-mobile/src/index.ts
+++ libs/live-dmk-mobile/src/index.ts
@@ -2,6 +2,14 @@ import { useDeviceSessionState } from "@ledgerhq/live-dmk-shared";
 
 export { DeviceManagementKitBLETransport } from "./transport/DeviceManagementKitBLETransport";
 export { DeviceManagementKitHIDTransport } from "./transport/DeviceManagementKitHIDTransport";
+export { DefaultDeviceDiscoveryService } from "./connectDevice/discoveryService/DefaultDeviceDiscoveryService";
+export {
+  DiscoveryErrors,
+  type DeviceDiscoveryService,
+  type DeviceDiscoveryStartArgs,
+  type DiscoveryError,
+  type DiscoveryErrorResolution,
+} from "./connectDevice/types";
 export * from "./errors";
 export * from "./hooks";
 export * from "./utils/matchDevicesByNameOrId";
