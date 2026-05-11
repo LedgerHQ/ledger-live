408f63315b3864738c82912d4e698f2697b6679e
Valentin <valentin.lambert@ledger.fr>
2026-05-11 16:07:42 +0200

    chore(earn-live-app-modal): add earn versioning into earn liveappmodal

--- apps/ledger-live-desktop/src/mvvm/features/LiveAppModal/useEarnLiveAppModalContentViewModel.ts
+++ apps/ledger-live-desktop/src/mvvm/features/LiveAppModal/useEarnLiveAppModalContentViewModel.ts
@@ -6,28 +6,37 @@ import {
 import { useFeature, useWalletFeaturesConfig } from "@ledgerhq/live-common/featureFlags/index";
 import { useVersionedStakePrograms } from "LLD/hooks/useVersionedStakePrograms";
 import type { ExtraInputs } from "./useLiveAppModalContentViewModel";
+import { computeEarnUiVersion } from "@ledgerhq/live-common/domain/computeEarnUiVersion";
 
 export interface EarnLiveAppModalContentViewModel {
   extraInputs: ExtraInputs;
 }
 
 const useEarnLiveAppModalContentViewModel = (): EarnLiveAppModalContentViewModel => {
-  const { isEnabled: isLwd40Enabled } = useWalletFeaturesConfig("desktop");
+  const {
+    isEnabled: isLwd40Enabled,
+    shouldDisplayEarnSimulator,
+    shouldDisplayEarnUpselling,
+  } = useWalletFeaturesConfig("desktop");
   const earnUiFlag = useFeature("ptxEarnUi");
-  const earnUiVersion = earnUiFlag?.params?.value ?? "v1";
+  const computedUiVersion = computeEarnUiVersion({
+    baseUiVersion: earnUiFlag?.params?.value ?? "v2",
+    shouldDisplayEarnUpselling,
+    shouldDisplayEarnSimulator,
+  });
   const stakePrograms = useVersionedStakePrograms();
 
   const extraInputs = useMemo<ExtraInputs>(() => {
     const { stakeProgramsParam, stakeCurrenciesParam } = stakeProgramsToEarnParam(stakePrograms);
     const ethDepositCohort = getEthDepositScreenSetting(stakePrograms);
     return {
-      uiVersion: isLwd40Enabled ? earnUiVersion : "v1",
+      uiVersion: isLwd40Enabled ? computedUiVersion : "v1",
       lw40enabled: isLwd40Enabled ? "true" : "false",
       ethDepositCohort,
       stakeProgramsParam: stakeProgramsParam ? JSON.stringify(stakeProgramsParam) : undefined,
       stakeCurrenciesParam: stakeCurrenciesParam ? JSON.stringify(stakeCurrenciesParam) : undefined,
     };
-  }, [stakePrograms, isLwd40Enabled, earnUiVersion]);
+  }, [stakePrograms, isLwd40Enabled, computedUiVersion]);
 
   return { extraInputs };
 };
