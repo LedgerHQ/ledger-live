40e8de23e3d80ef8dd61cf94c56c6a5585549f6c
Aleksei Maslakov <alexei.maslakov@gmail.com>
2026-05-12 22:08:56 +0100

    feat(lwd): add Tezos unstake flow modal (LIVE-29537)

--- apps/ledger-live-desktop/src/renderer/families/tezos/modals.ts
+++ apps/ledger-live-desktop/src/renderer/families/tezos/modals.ts
@@ -5,6 +5,8 @@ import MODAL_DELEGATE from "./DelegateFlowModal";
 import { Data as DelegateProps } from "./DelegateFlowModal/Body";
 import MODAL_TEZOS_EARNING_CHOICE from "./EarningChoiceModal";
 import { Data as EarningChoiceProps } from "./EarningChoiceModal/Body";
+import MODAL_TEZOS_UNSTAKE from "./UnstakeFlowModal";
+import { Data as UnstakeProps } from "./UnstakeFlowModal/Body";
 import { MakeModalsType } from "../../modals/types";
 
 // Pending LIVE-29536: replace this stub with the real Stake flow modal.
@@ -20,12 +22,14 @@ export type ModalsData = {
   MODAL_DELEGATE: DelegateProps;
   MODAL_TEZOS_EARNING_CHOICE: EarningChoiceProps;
   MODAL_TEZOS_STAKE: TezosStakeProps;
+  MODAL_TEZOS_UNSTAKE: UnstakeProps;
 };
 
 const modals: MakeModalsType<ModalsData> = {
   MODAL_DELEGATE,
   MODAL_TEZOS_EARNING_CHOICE,
   MODAL_TEZOS_STAKE,
+  MODAL_TEZOS_UNSTAKE,
 };
 
 export default modals;
