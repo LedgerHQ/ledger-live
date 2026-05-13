8ecb7e246e153377c04d734fc35ee489b9322da6
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2026-05-13 11:02:30 +0200

    style(lwm): use ~ for animation import paths

--- apps/ledger-live-mobile/src/mvvm/components/DeviceActionContent/getDeviceActionAnimation.test.ts
+++ apps/ledger-live-mobile/src/mvvm/components/DeviceActionContent/getDeviceActionAnimation.test.ts
@@ -10,30 +10,30 @@ import {
   supportedDeviceActionModelIds,
 } from "./getDeviceActionAnimation";
 
-import NanoSPPinDark from "../../../animations/device/nanoSP/dark/pin.json";
-import NanoSPPinLight from "../../../animations/device/nanoSP/light/pin.json";
-import NanoSPContinueDark from "../../../animations/device/nanoSP/dark/continue.json";
-import NanoSPContinueLight from "../../../animations/device/nanoSP/light/continue.json";
+import NanoSPPinDark from "~/animations/device/nanoSP/dark/pin.json";
+import NanoSPPinLight from "~/animations/device/nanoSP/light/pin.json";
+import NanoSPContinueDark from "~/animations/device/nanoSP/dark/continue.json";
+import NanoSPContinueLight from "~/animations/device/nanoSP/light/continue.json";
 
-import NanoXPinDark from "../../../animations/device/nanoX/dark/pin.json";
-import NanoXPinLight from "../../../animations/device/nanoX/light/pin.json";
-import NanoXContinueDark from "../../../animations/device/nanoX/dark/continue.json";
-import NanoXContinueLight from "../../../animations/device/nanoX/light/continue.json";
+import NanoXPinDark from "~/animations/device/nanoX/dark/pin.json";
+import NanoXPinLight from "~/animations/device/nanoX/light/pin.json";
+import NanoXContinueDark from "~/animations/device/nanoX/dark/continue.json";
+import NanoXContinueLight from "~/animations/device/nanoX/light/continue.json";
 
-import StaxPinDark from "../../../animations/device/stax/dark/pin.json";
-import StaxPinLight from "../../../animations/device/stax/light/pin.json";
-import StaxContinueDark from "../../../animations/device/stax/dark/continue.json";
-import StaxContinueLight from "../../../animations/device/stax/light/continue.json";
+import StaxPinDark from "~/animations/device/stax/dark/pin.json";
+import StaxPinLight from "~/animations/device/stax/light/pin.json";
+import StaxContinueDark from "~/animations/device/stax/dark/continue.json";
+import StaxContinueLight from "~/animations/device/stax/light/continue.json";
 
-import FlexPinDark from "../../../animations/device/flex/dark/pin.json";
-import FlexPinLight from "../../../animations/device/flex/light/pin.json";
-import FlexContinueDark from "../../../animations/device/flex/dark/continue.json";
-import FlexContinueLight from "../../../animations/device/flex/light/continue.json";
+import FlexPinDark from "~/animations/device/flex/dark/pin.json";
+import FlexPinLight from "~/animations/device/flex/light/pin.json";
+import FlexContinueDark from "~/animations/device/flex/dark/continue.json";
+import FlexContinueLight from "~/animations/device/flex/light/continue.json";
 
-import ApexPinDark from "../../../animations/device/apex/dark/pin.json";
-import ApexPinLight from "../../../animations/device/apex/light/pin.json";
-import ApexContinueDark from "../../../animations/device/apex/dark/continue.json";
-import ApexContinueLight from "../../../animations/device/apex/light/continue.json";
+import ApexPinDark from "~/animations/device/apex/dark/pin.json";
+import ApexPinLight from "~/animations/device/apex/light/pin.json";
+import ApexContinueDark from "~/animations/device/apex/dark/continue.json";
+import ApexContinueLight from "~/animations/device/apex/light/continue.json";
 
 type ExpectedAnimations = Record<
   DeviceActionContentAction,
