a15a69cc7f3ad735cfed214b070c0b176cdd1123
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-05-18 12:32:25 +0100

    test(e2e-mobile): swap spec file

--- e2e/poc-wdio/specs/onboarding.spec.ts
+++ e2e/poc-wdio/specs/onboarding.spec.ts
@@ -1,7 +1,7 @@
 import pages from "../pages/pages.ts";
 
 describe("Onboarding", () => {
-  it("Displays the Get Started button", async () => {
+  it("displays the Get Started button", async () => {
     await pages.onboarding.expectGetStartedButtonToBeDisplayed();
   });
 });
---
+++ e2e/poc-wdio/specs/swap.spec.ts
@@ -0,0 +1,8 @@
+describe("Swap", () => {
+  it("ETH to USDT (without broadcast)", async () => {
+    // disable broadcast
+    // setup accounts
+    // execute swap
+    expect("incomplete").toBe("complete");
+  });
+});
