5e87dbd63fdaa8eaf3f80012bcb1b80c4363fa52
Jerome Portier <jerome.portier@ledger.fr>
2026-05-19 15:59:23 +0200

    test(mobile): added last execute swap check that was missing in the flow

--- e2e/mobile/specs/swap/tokenApprovalFlow/swapTokenApprovalFlow.ts
+++ e2e/mobile/specs/swap/tokenApprovalFlow/swapTokenApprovalFlow.ts
@@ -61,6 +61,7 @@ export function runSwapTokenApprovalFlow(
         await app.speculos.signTypedMessage();
       }
       await app.swapLiveApp.expectTwoStepSignScreen();
+      await app.swapLiveApp.expectExecuteSwapOnStepApproval();
     });
   });
 }
