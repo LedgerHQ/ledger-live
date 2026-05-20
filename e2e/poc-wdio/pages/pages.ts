db2092df41bdddd2e9479b690125a26a034bd455
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-05-20 10:01:48 +0100

    test(e2e-mobile): refactor lazy init

--- e2e/poc-wdio/pages/pages.ts
+++ e2e/poc-wdio/pages/pages.ts
@@ -1,15 +1,17 @@
 import { OnboardingPage } from "./onboarding.page.ts";
 
-const lazyInit = <T>(PageClass: new () => T) => {
-  let instance: T | null = null;
-  return () => {
-    if (!instance) instance = new PageClass();
-    return instance;
-  };
-};
-
 class Pages {
-  private onboardingPageInstance = lazyInit(OnboardingPage);
+  private static readonly LAZY_INIT = <T>(PageClass: new () => T) => {
+    let instance: T | null = null;
+    return () => {
+      if (!instance) {
+        instance = new PageClass();
+      }
+      return instance;
+    };
+  };
+
+  private onboardingPageInstance = Pages.LAZY_INIT(OnboardingPage);
 
   public get onboarding() {
     return this.onboardingPageInstance();
