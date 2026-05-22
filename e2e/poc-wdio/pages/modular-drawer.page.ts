fafcf7c9a2c6ebab09ba2727e6a3357010271b30
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-05-22 17:42:26 +0100

    test(e2e-mobile): cleanup userdata json

--- e2e/poc-wdio/pages/modular-drawer.page.ts
+++ e2e/poc-wdio/pages/modular-drawer.page.ts
@@ -57,7 +57,7 @@ export class ModularDrawerPage {
     const networkName = this.getNetworkNameForAccount(account);
     await this.selectNetworkIfAsked(networkName);
     await this.selectFirstAccount();
-    await this.modularFlowView.waitForDisplayed({ reverse: true });
+    await this.modularFlowView.waitForDisplayed({ reverse: true, timeout: 30_000 });
   }
 
   // functions
