0e4b646c75fee35c67b0b565f82049abc507ac60
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-05-22 11:30:42 +0100

    test(e2e-mobile): add swap live app page

---
+++ e2e/poc-wdio/pages/swap-live-app.page.ts
@@ -0,0 +1,10 @@
+export class SwapLiveAppPage {
+  // components
+  // steps
+  //   async expectSwapLiveApp() {
+  //     await waitWebElementByTestId(this.fromSelector);
+  //     await detoxExpect(getWebElementByTestId(this.fromSelector)).toExist();
+  //     await detoxExpect(getWebElementByTestId(this.toSelector)).toExist();
+  //     await detoxExpect(getWebElementByTestId(this.quotesButtonDisabled)).toExist();
+  //   }
+}
