cc307769bfec277362c84e85b329092099178284
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-06-01 16:57:05 +0100

    test(e2e-mobile): run with one simulator only

--- tools/scripts/boot-android-emulators-ci.sh
+++ tools/scripts/boot-android-emulators-ci.sh
@@ -12,7 +12,7 @@
 
 set -uo pipefail
 
-readonly _EXPECTED_EMULATOR_COUNT=3
+readonly _EXPECTED_EMULATOR_COUNT=1
 
 log() {
   echo "[$(date +%H:%M:%S)] $*"
@@ -40,13 +40,13 @@ disable_ui_animations() {
 : "${AVD_NAME:?AVD_NAME is required}"
 : "${ANDROID_HOME:?ANDROID_HOME is required}"
 
-IFS=' ' read -r -a SERIALS <<<"${EMULATOR_SERIALS:-emulator-5554 emulator-5556 emulator-5558}"
+IFS=' ' read -r -a SERIALS <<<"${EMULATOR_SERIALS:-emulator-5554}"
 if [[ "${#SERIALS[@]}" -ne "$_EXPECTED_EMULATOR_COUNT" ]]; then
   log "ERROR: expected $_EXPECTED_EMULATOR_COUNT serials in EMULATOR_SERIALS, got ${#SERIALS[@]}"
   exit 1
 fi
 
-AVD_NAMES=("$AVD_NAME" "${AVD_NAME}_2" "${AVD_NAME}_3")
+AVD_NAMES=("$AVD_NAME")
 
 log "🛫 Starting emulators..."
 i=1
