b33e0933ad76f4290db2e774423122be74b93dbd
Martijn Kuhlemeier <martijn.kuhlemeier@ledger.fr>
2026-06-18 14:11:09 +0100

    test(e2e-mobile): take focus off input by tapping another element

--- e2e/poc-wdio/pages/swap-live-app.page.ts
+++ e2e/poc-wdio/pages/swap-live-app.page.ts
@@ -184,12 +184,8 @@ export class SwapLiveAppPage {
           await driver.pause(50);
         }
         if (driver.isAndroid) {
-          // workaround for ScreenContentWrapper crash on navigation
-          await driver.hideKeyboard();
-          await driver.waitUntil(async () => (await driver.isKeyboardShown()) === false, {
-            timeout: 5000,
-            timeoutMsg: "Expected keyboard to be hidden after inputting amount",
-          });
+          // workaround for ScreenContentWrapper crash on navigation - take focus off input
+          await this.toAmountInput.tap();
         }
       });
     });
