fa38593a6b9a4fc0b83f49b9248cc122f0187dc1
quantova-inc <botmintos@tuta.io>
2026-06-21 19:46:26 +0800

    coin-quantova: signer exports

---
+++ libs/coin-modules/coin-quantova/src/signer/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Home for the Quantova device-signer wiring (getAddress + signTransaction).
+ */
+import getAddressResolver from "./getAddress";
+
+export { getAddressResolver };
+export default getAddressResolver;
