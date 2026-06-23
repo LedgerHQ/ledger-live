7665fd92290fff0775870c9713bad1bf80c37968
Philipp Trentmann <philipp.trentmann@ledger.fr>
2026-06-23 10:13:06 +0200

    feat(LIVE-29443): add mobile swap transaction status drawer

---
+++ apps/ledger-live-mobile/src/mvvm/features/SwapTransactionStatus/components/TransactionHeader.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import type { CryptoOrTokenCurrency } from "@ledgerhq/types-cryptoassets";
+import { formatSwapTransactionStatusCreatedAt } from "@ledgerhq/live-common/exchange/swapTransactionStatus/index";
+import { Box, Skeleton, Text } from "@ledgerhq/lumen-ui-rnative";
+import CurrencyIcon from "~/components/CurrencyIcon";
+import { useTranslation } from "~/context/Locale";
+
+type TransactionHeaderProps = Readonly<{
+  sendCurrency?: CryptoOrTokenCurrency;
+  receiveCurrency?: CryptoOrTokenCurrency;
+  createdAt?: number;
+  locale: string;
+}>;
+
+export function TransactionHeader({
+  sendCurrency,
+  receiveCurrency,
+  createdAt,
+  locale,
+}: TransactionHeaderProps) {
+  const { t } = useTranslation();
+  const hasCurrencies = sendCurrency && receiveCurrency;
+
+  return (
+    <Box lx={{ alignItems: "center", gap: "s4" }}>
+      <Box lx={{ width: "s80", height: "s72" }}>
+        {sendCurrency ? (
+          <Box lx={{ position: "absolute", left: "s0", top: "s0" }}>
+            <CurrencyIcon currency={sendCurrency} size={48} hideNetwork />
+          </Box>
+        ) : null}
+        {receiveCurrency ? (
+          <Box lx={{ position: "absolute", right: "s0", top: "s16" }}>
+            <CurrencyIcon currency={receiveCurrency} size={48} hideNetwork />
+          </Box>
+        ) : null}
+        {hasCurrencies ? null : (
+          <Skeleton lx={{ height: "s48", width: "s48", borderRadius: "full" }} />
+        )}
+      </Box>
+      {hasCurrencies ? (
+        <Text typography="heading5SemiBold" lx={{ color: "base", textAlign: "center" }}>
+          {t("transfer.swap2.modals.transactionStatus.title", {
+            sendTicker: sendCurrency.ticker,
+            receiveTicker: receiveCurrency.ticker,
+          })}
+        </Text>
+      ) : (
+        <Skeleton lx={{ height: "s24", width: "s176" }} />
+      )}
+      {createdAt ? (
+        <Text typography="body3" lx={{ color: "muted", textAlign: "center" }}>
+          {formatSwapTransactionStatusCreatedAt(createdAt, locale)}
+        </Text>
+      ) : (
+        <Skeleton lx={{ height: "s16", width: "s144" }} />
+      )}
+    </Box>
+  );
+}
