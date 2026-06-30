e08b0e378e4b531f899f215b7fc8c706c092ecda
Kovalenko Roman <roman.kovalenko-ext@ledger.fr>
2026-06-30 13:19:50 +0500

    test(common): expect cardano self-transfer policy warning

--- libs/ledger-live-common/src/bridge/descriptor.test.ts
+++ libs/ledger-live-common/src/bridge/descriptor.test.ts
@@ -642,9 +642,9 @@ describe("sendFeatures", () => {
     ["bitcoin", "free"],
     ["ethereum", "free"],
     ["filecoin", "free"],
-    ["cardano", "free"],
     ["solana", "impossible"],
     ["cosmos", "impossible"],
+    ["cardano", "warning"],
     ["near", "warning"],
     ["vechain", "warning"],
   ])("should get self transfer policy for %s", (currencyId, expected) => {
