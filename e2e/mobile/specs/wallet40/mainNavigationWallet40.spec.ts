91524d6c00a2428bb52da25bc607b8dce601fcf0
Jerome Portier <jerome.portier@ledger.fr>
2026-07-03 17:38:59 +0200

    chore(mobile): wallet40 ff cleanup 2nd pass

--- e2e/mobile/specs/wallet40/mainNavigationWallet40.spec.ts
+++ e2e/mobile/specs/wallet40/mainNavigationWallet40.spec.ts
@@ -1,5 +1,6 @@
 import { Team } from "@ledgerhq/live-e2e-shared/enum/Team";
 import { setTeamOwner } from "../../helpers/allure/allure-helper";
+import { FF_LWM_WALLET_40_Q2 } from "utils/featureFlagUtils";
 
 setTeamOwner(Team.WALLET_XP);
 $TmsLink("B2CQA-4383");
@@ -11,6 +12,7 @@ describe("Wallet 4.0 - Main Navigation", () => {
   beforeAll(async () => {
     await app.init({
       userdata: "skip-onboarding",
+      featureFlags: FF_LWM_WALLET_40_Q2,
     });
     await app.mainNavigation.waitForWallet40Ready();
   });
