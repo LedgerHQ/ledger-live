c0d05a4393a8bfb71a4dece7bad0946be9833a97
Jerome Portier <jerome.portier@ledger.fr>
2026-07-06 13:44:58 +0200

    test(swap): add discreet mode e2e for desktop and mobile

---
+++ e2e/mobile/specs/swap/otherTestCases/swapDiscreetMode.spec.ts
@@ -0,0 +1,32 @@
+import { Account, type AccountType, TokenAccount } from "@ledgerhq/live-e2e-shared/enum/Account";
+import { runSwapDiscreetModeTest } from "./swap.other";
+
+const fundedAssetsAccounts: AccountType[] = [
+  Account.BTC_NATIVE_SEGWIT_1,
+  Account.ETH_1,
+  TokenAccount.ETH_USDC_1,
+  TokenAccount.ETH_USDT_1,
+];
+
+const swapDiscreetModeTestConfig = {
+  fundedAssetsAccounts,
+  tmsLinks: ["B2CQA-2457"],
+  tags: [
+    "@NanoSP",
+    "@LNS",
+    "@NanoX",
+    "@Stax",
+    "@Flex",
+    "@NanoGen5",
+    "@ethereum",
+    "@family-evm",
+    "@bitcoin",
+    "@family-bitcoin",
+  ],
+};
+
+runSwapDiscreetModeTest(
+  swapDiscreetModeTestConfig.fundedAssetsAccounts,
+  swapDiscreetModeTestConfig.tmsLinks,
+  swapDiscreetModeTestConfig.tags,
+);
