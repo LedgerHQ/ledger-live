a952f84063e5f791b9c757827570d59d048c43bf
François Guérin <francois.guerin@ledger.fr>
2026-07-06 09:52:51 +0200

    chore(coin-evm): clear remaining crypto asset store mentions

---
+++ .changeset/rude-impalas-promise.md
@@ -0,0 +1,5 @@
+---
+"@ledgerhq/coin-evm": patch
+---
+
+chore(coin-evm): clear remaining crypto assets store mentions
--- libs/coin-modules/coin-evm/src/api/index.integ.test.ts
+++ libs/coin-modules/coin-evm/src/api/index.integ.test.ts
@@ -6,7 +6,6 @@ import {
   Operation,
   StakingTransactionIntent,
 } from "@ledgerhq/coin-module-framework/api/types";
-import { setupCalClientStore } from "@ledgerhq/cryptoassets/cal-client/test-helpers";
 import { getEnv, setEnv } from "@ledgerhq/live-env";
 import { ethers } from "ethers";
 import { EvmConfig } from "../config";
@@ -39,8 +38,6 @@ describe.each([
   let module: CoinModuleApi<MemoNotSupported, BufferTxData>;
 
   beforeAll(() => {
-    // Setup CAL client store (automatically set as global store)
-    setupCalClientStore();
     module = createApi(config as EvmConfig, "ethereum");
   });
 
@@ -790,7 +787,6 @@ describe("EVM Api: external node and explorer ONLY", () => {
   let module: CoinModuleApi<MemoNotSupported, BufferTxData>;
 
   beforeAll(() => {
-    setupCalClientStore();
     module = createApi(
       {
         node: { type: "external", uri: "https://ethereum-rpc.publicnode.com" },
@@ -881,7 +877,6 @@ describe("EVM Api (Moonbeam Network)", () => {
   let module: CoinModuleApi<MemoNotSupported, BufferTxData>;
 
   beforeAll(() => {
-    setupCalClientStore();
     module = createApi(
       {
         node: { type: "external", uri: "https://rpc.api.moonbeam.network" },
@@ -929,8 +924,6 @@ describe("EVM Api (SEI Network)", () => {
   const seiStakingDelegationAmount = 1000000000000000n; // 0.001 SEI
 
   beforeAll(() => {
-    // Setup CAL client store (automatically set as global store)
-    setupCalClientStore();
     const config = {
       node: {
         type: "external",
@@ -1053,7 +1046,6 @@ describe("EVM Api (Zero Gravity)", () => {
   let module: CoinModuleApi<MemoNotSupported, BufferTxData>;
 
   beforeAll(() => {
-    setupCalClientStore();
     const config = {
       node: {
         type: "external",
