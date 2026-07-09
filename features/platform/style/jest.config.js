95fcba365a1fb9d920031b112963f3b02447c13e
ysitbon <yoann.sitbon@ledger.fr>
2026-07-09 10:43:02 +0200

    chore: add sonarqube coverage

--- features/platform/style/jest.config.js
+++ features/platform/style/jest.config.js
@@ -20,4 +20,8 @@ module.exports = {
   },
   passWithNoTests: true,
   coverageReporters: ["json", ["lcov", { file: "lcov.info", projectRoot: "../../../" }], "text"],
+  reporters: [
+    "default",
+    ["jest-sonar", { outputName: "sonar-executionTests-report.xml", reportedFilePath: "absolute" }],
+  ],
 };
