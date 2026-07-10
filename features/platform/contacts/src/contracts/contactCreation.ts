963f3ca398c961e33e95983c097dfaa45ccd5954
Raphael <raphael.da-fonseca@ledger.fr>
2026-07-10 16:28:47 +0200

    revert(contacts): remove platform contracts

--- features/platform/contacts/src/contracts/contactCreation.ts
+++
@@ -1,9 +0,0 @@
-import type { Contact, ContactInput } from "@domain/entity-contact";
-
-export type ContactCreationInput = Readonly<{
-  name: ContactInput["name"];
-}>;
-
-export type ContactCreationPort = Readonly<{
-  createContact(input: ContactCreationInput): Promise<Contact>;
-}>;
