16fa8e84b4e87b199682b2942e56d3ab4522bcd1
Raphael <raphael.da-fonseca@ledger.fr>
2026-07-15 10:51:55 +0200

    fix(contacts): use regex exec for initial

--- features/flow/contacts/src/list/utils/getContactInitial.ts
+++ features/flow/contacts/src/list/utils/getContactInitial.ts
@@ -1,7 +1,7 @@
 const ContactInitialPattern = /^\p{L}[\p{Mn}\p{Mc}]*/u;
 
 export function getContactInitial(name: string): string {
-  const initial = name.match(ContactInitialPattern)?.[0];
+  const initial = ContactInitialPattern.exec(name)?.[0];
 
   return initial?.toUpperCase() ?? "";
 }
