a48d0a45c8688f7872ec9bd10fd0c98fdead3694
Tony Khaov <tony.khaov@gmail.com>
2026-07-15 15:40:41 +0200

    feat(desktop): add large-screen upsell app-start modal

--- features/flow/large-screen-upsell/src/index.ts
+++ features/flow/large-screen-upsell/src/index.ts
@@ -1,3 +1,11 @@
 export * from "./types";
 export * from "./decision/getLargeScreenUpsellDecision";
 export * from "./hooks/useLargeScreenUpsellDecision";
+export * from "./utils/upsellCta";
+export * from "./utils/upsellContent";
+export * from "./utils/mapDevicesModelListToUpsellInputs";
+export { LARGE_SCREEN_UPSELL_IMAGES } from "./assets";
+export { LargeScreenUpsellModal } from "./screens/LargeScreenUpsellModal";
+export type { LargeScreenUpsellModalProps } from "./screens/LargeScreenUpsellModal";
+export type { LargeScreenUpsellModalViewModel } from "./screens/LargeScreenUpsellModal/types";
+export type { UseLargeScreenUpsellModalViewModelInput } from "./screens/LargeScreenUpsellModal/useLargeScreenUpsellModalViewModel";
