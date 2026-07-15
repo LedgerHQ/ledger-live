7c0d236ee7f2cc114e95334248170f0bb46206fe
Pedro Semeano <semeano@gmail.com>
2026-07-15 16:42:10 +0100

    feat: show shielded sync status on send modal

--- apps/ledger-live-desktop/src/renderer/families/bitcoin/SendRecipientFields.tsx
+++ apps/ledger-live-desktop/src/renderer/families/bitcoin/SendRecipientFields.tsx
@@ -1,13 +1,19 @@
 import { PendingOperation } from "@ledgerhq/errors";
 import { getMainAccount } from "@ledgerhq/live-common/account/index";
-import { BitcoinAccount } from "@ledgerhq/live-common/families/bitcoin/types";
+import {
+  BitcoinAccount,
+  Transaction,
+  ZcashAccount,
+} from "@ledgerhq/live-common/families/bitcoin/types";
 import { isConfirmedOperation } from "@ledgerhq/live-common/operation";
+import type { ZcashTransaction } from "@ledgerhq/coin-bitcoin/chain-adapters/zcash/types";
 import React from "react";
 import { connect } from "react-redux";
 import Alert from "~/renderer/components/Alert";
 import TranslatedError from "~/renderer/components/TranslatedError";
 import { State } from "~/renderer/reducers";
 import { confirmationsNbForCurrencySelector } from "~/renderer/reducers/settings";
+import ZcashSyncStateBanner from "./ZcashSyncStateBanner";
 
 // FIXME: ConfirmationNB seems to be specific.
 // So we can't do
@@ -15,26 +21,41 @@ import { confirmationsNbForCurrencySelector } from "~/renderer/reducers/settings
 type Props = {
   account: BitcoinAccount;
   confirmationsNb: number;
+  transaction: Transaction;
 };
 
 const SendRecipientFields = (props: Props) => {
-  const { confirmationsNb, account } = props;
+  const { confirmationsNb, account, transaction } = props;
   const pendingOperationError = new PendingOperation();
   const operations = account.pendingOperations.concat(account.operations);
   const incomingTransactionPending = operations.some(
     op => op.type === "IN" && !isConfirmedOperation(op, account, confirmationsNb),
   );
-  return incomingTransactionPending ? (
-    <Alert type={"warning"} mt={4}>
-      <TranslatedError error={pendingOperationError} field="description" />
-    </Alert>
-  ) : null;
+  return (
+    <>
+      {incomingTransactionPending && (
+        <Alert type={"warning"} mt={4}>
+          <TranslatedError error={pendingOperationError} field="description" />
+        </Alert>
+      )}
+      {account.currency.id === "zcash" ? (
+        <ZcashSyncStateBanner
+          account={account as ZcashAccount}
+          sender={(transaction as Partial<ZcashTransaction>).sender}
+        />
+      ) : null}
+    </>
+  );
 };
 
 const m = connect(
   (
     state: State,
-    props: { account: BitcoinAccount; parentAccount: BitcoinAccount | null | undefined },
+    props: {
+      account: BitcoinAccount;
+      parentAccount: BitcoinAccount | null | undefined;
+      transaction: Transaction;
+    },
   ) => {
     const confirmationsNb = confirmationsNbForCurrencySelector(state, {
       currency: getMainAccount(props.account, props.parentAccount).currency,
