35e952874f86878788d636d7d362d239374738cd
Claudia <claudiiaguerreiro@hotmail.com>
2026-07-21 11:31:47 +0100

    feat: contacts feature intro

--- features/flow/contacts/src/hooks/index.ts
+++ features/flow/contacts/src/hooks/index.ts
@@ -4,3 +4,8 @@ export type { UseAddContactViewModelResult } from "./useAddContactViewModel";
 export { useContactsListViewModel } from "./useContactsListViewModel";
 export { useContactsMeContact } from "./useContactsMeContact";
 export { useContactsSearchViewModel } from "./useContactsSearchViewModel";
+export { useContactsFeatureIntroductionState } from "./useContactsFeatureIntroductionState";
+export type {
+  ContactsFeatureIntroductionState,
+  UseContactsFeatureIntroductionStateInput,
+} from "./useContactsFeatureIntroductionState";
