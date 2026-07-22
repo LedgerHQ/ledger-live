1e56c8b3864060effed9ff6067cd35c67ce56c07
Laurence Lord <laurence.lord@ledger.fr>
2026-07-22 17:18:56 +0100

    chore(contributing): set max length for commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,12 @@
 const scopes = require("./commitlint.scopes");
 
+const LEVEL_ERROR = 2;
+const LEVEL_WARN = 1;
+
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [
-      2, // level: error
-      "always",
-      scopes,
-    ],
+    "scope-enum": [LEVEL_WARN, "always", scopes],
+    "header-max-length": [LEVEL_ERROR, "always", 72],
   },
 };
