03643fcde0600b033afc4bd8e538c25f7447dab1
Laurence Lord <laurence.lord@ledger.fr>
2026-07-22 17:06:28 +0100

    chore(contributing): define scope enum for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+const scopes = require("./commitlint.scopes");
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2, // level: error
+      "always",
+      scopes,
+    ],
+  },
+};
