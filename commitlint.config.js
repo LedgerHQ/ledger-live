d1b5f0812a86a520ee13dc29f690641898e13da9
Laurence Lord <laurence.lord@ledger.fr>
2026-07-23 11:08:02 +0100

    chore(commitlint): set header-max-length 72

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ const LEVEL_WARN = 1;
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "header-max-length": [LEVEL_ERROR, "always", 72],
     "scope-empty": [LEVEL_ERROR, "never"],
   },
 };
