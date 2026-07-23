38e87285f4207d493d922d6df10fbea544b44d51
Laurence Lord <laurence.lord@ledger.fr>
2026-07-23 11:07:17 +0100

    chore(commitlint): require scope in commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+const LEVEL_ERROR = 2;
+const LEVEL_WARN = 1;
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-empty": [LEVEL_ERROR, "never"],
+  },
+};
