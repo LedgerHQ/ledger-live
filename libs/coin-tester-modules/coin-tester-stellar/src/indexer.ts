67e84f1a74f3353734c18e47f2d2641552370605
François Guérin <francois.guerin@ledger.fr>
2026-07-24 12:03:35 +0200

    chore(coni-testers): add missing MSW guards

--- libs/coin-tester-modules/coin-tester-stellar/src/indexer.ts
+++ libs/coin-tester-modules/coin-tester-stellar/src/indexer.ts
@@ -21,11 +21,11 @@ export function initMswHandlers(): () => void {
   );
 
   server.listen({
-    onUnhandledRequest: req => {
-      const hostname = new URL(req.url).hostname;
+    onUnhandledRequest: request => {
+      const hostname = new URL(request.url).hostname;
       // Allow requests to the local Stellar Quickstart container.
       if (["127.0.0.1", "localhost"].includes(hostname)) return;
-      throw new Error(`Unhandled MSW request: ${req.method} ${req.url}`);
+      throw new Error(`Unhandled request: ${request.method} ${request.url}`);
     },
   });
 
