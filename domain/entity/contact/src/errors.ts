6fa7d8bc292da4d0bd00581da7ec2db027e9bcfe
Raphael <raphael.da-fonseca@ledger.fr>
2026-07-30 15:09:42 +0200

    refactor(contacts): reuse address label error names

--- domain/entity/contact/src/errors.ts
+++ domain/entity/contact/src/errors.ts
@@ -15,20 +15,18 @@ export type ContactNameValidationErrorName = "InvalidContactNameError";
 export const INVALID_CONTACT_NAME_ERROR_NAME =
   "InvalidContactNameError" satisfies ContactNameValidationErrorName;
 
+export const INVALID_CONTACT_ADDRESS_LABEL_ERROR_NAME = "InvalidContactAddressLabelError";
+
+export const DUPLICATE_CONTACT_ADDRESS_LABEL_ERROR_NAME = "DuplicateContactAddressLabelError";
+
+export type ContactAddressLabelValidationErrorName =
+  | typeof INVALID_CONTACT_ADDRESS_LABEL_ERROR_NAME
+  | typeof DUPLICATE_CONTACT_ADDRESS_LABEL_ERROR_NAME;
+
 export class InvalidContactAddressLabelError extends ContactError {
-  override name = "InvalidContactAddressLabelError";
+  override name = INVALID_CONTACT_ADDRESS_LABEL_ERROR_NAME;
 }
 
 export class DuplicateContactAddressLabelError extends ContactError {
-  override name = "DuplicateContactAddressLabelError";
+  override name = DUPLICATE_CONTACT_ADDRESS_LABEL_ERROR_NAME;
 }
-
-export type ContactAddressLabelValidationErrorName =
-  | "InvalidContactAddressLabelError"
-  | "DuplicateContactAddressLabelError";
-
-export const INVALID_CONTACT_ADDRESS_LABEL_ERROR_NAME =
-  "InvalidContactAddressLabelError" satisfies ContactAddressLabelValidationErrorName;
-
-export const DUPLICATE_CONTACT_ADDRESS_LABEL_ERROR_NAME =
-  "DuplicateContactAddressLabelError" satisfies ContactAddressLabelValidationErrorName;
