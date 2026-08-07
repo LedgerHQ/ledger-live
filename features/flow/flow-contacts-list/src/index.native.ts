9688614117c6f130b9c718863aacf112b92cf849
Raphael <raphael.da-fonseca@ledger.fr>
2026-08-07 09:39:01 +0200

    fix(contacts): restore native flow exports

--- features/flow/flow-contacts-list/src/index.native.ts
+++ features/flow/flow-contacts-list/src/index.native.ts
@@ -1,4 +1,25 @@
-export * from "./index";
+export {
+  createContactsSearchViewModel,
+  createContactsListViewModel,
+  createEmptyContactsListViewModel,
+  createPopulatedContactsListViewModel,
+} from "./model/viewModel";
+export { useContactsListViewModel } from "./hooks/useContactsListViewModel";
+export { useContactsSearchViewModel } from "./hooks/useContactsSearchViewModel";
+export { isContactsSearchNoResultsViewModel, isPopulatedContactsListViewModel } from "./types";
+export type {
+  ContactsListViewLabels,
+  ContactsListViewProps,
+  ContactsPageViewModel,
+  ContactsSearchNoResultsViewModel,
+  ContactsSearchResultsViewModel,
+  ContactsSearchViewModel,
+  ContactsListItem,
+  ContactsListSection,
+  ContactsListViewModel,
+  EmptyContactsListViewModel,
+  PopulatedContactsListViewModel,
+} from "./types";
 export { ContactsListView } from "./ContactsListView.native";
 export { ContactsAddContactHeaderButton } from "./components/ListHeader/ContactsAddContactHeaderButton.native";
 export type { ContactsAddContactHeaderButtonProps } from "./components/ListHeader/ContactsAddContactHeaderButton.native";
