e1b03116d129053d9b12ac367c2dbdf2a41e98d5
Raphael <raphael.da-fonseca@ledger.fr>
2026-08-07 09:59:43 +0200

    fix(contacts): use native avatar barrel

--- features/platform/contacts/src/index.native.ts
+++ features/platform/contacts/src/index.native.ts
@@ -1,4 +1,4 @@
 export { useContacts } from "./useContacts";
 export { useContactsMeContact } from "./useContactsMeContact";
 export { getContactAvatarColorClass } from "./utils/getContactAvatarColorClass";
-export { ContactAvatar } from "./components/ContactAvatar/ContactAvatar.native";
+export { ContactAvatar } from "./components/ContactAvatar/index.native";
