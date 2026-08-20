48d08fe5c292fa09d8ca32e44b4ce9815633762b
Laurence Lord <laurence.lord@ledger.fr>
2026-08-20 14:20:25 +0100

    chore(ci): consider merge conflict PR titles valid

--- tools/danger/validation/pr-title.ts
+++ tools/danger/validation/pr-title.ts
@@ -2,6 +2,8 @@ import lint from "@commitlint/lint";
 import config from "../../../commitlint.config.js";
 
 async function validatePrTitle() {
+  if (isMergeConflictsPR(danger.github.pr.title)) return;
+
   const report = await lint(stripPlatformPrefix(danger.github.pr.title), config.rules);
 
   const { errors, input, valid, warnings } = report;
@@ -25,9 +27,19 @@ async function validatePrTitle() {
         `- \`feat(ui): add dark mode toggle (LIVE-1234)\`\n` +
         `- \`fix(swap): resolve transaction signing issue\`\n` +
         `- \`ci(lint): harmonize git guidelines (LIVE-27608)\`\n\n` +
-        `See [Git conventions](https://github.com/LedgerHQ/ledger-live/blob/develop/docs/contributing/git-conventions.md).\n\n`
+        `See [Git conventions](https://github.com/LedgerHQ/ledger-live/blob/develop/docs/contributing/git-conventions.md).\n\n`,
     );
   }
+}
+
+function isMergeConflictsPR(title: string): boolean {
+  const RELEASE_CONFLICTS_TITLE = ":rotating_light: Release merge conflicts";
+  const HOTFIX_CONFLICTS_TITLE = ":rotating_light: Hotfix merge conflicts";
+
+  return (
+    title.trimEnd().endsWith(RELEASE_CONFLICTS_TITLE) ||
+    title.trimEnd().endsWith(HOTFIX_CONFLICTS_TITLE)
+  );
 }
 
 function stripPlatformPrefix(title: string) {
