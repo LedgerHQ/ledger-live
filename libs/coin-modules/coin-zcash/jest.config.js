fe2c5e26a0e46f75d0d1d1b74321800b7f0e380a
Lewis Phillips <lewis@d5icp.co.uk>
2026-08-20 17:59:10 +0100

    feat(test-quarantine): report flakes from blockchain-support jest suites
    Wires the flake reporter and CI-only retries into 8 jest packages.

--- libs/coin-modules/coin-zcash/jest.config.js
+++ libs/coin-modules/coin-zcash/jest.config.js
@@ -12,7 +12,11 @@ module.exports = {
     ],
   },
   passWithNoTests: true,
-  setupFilesAfterEnv: ["@ledgerhq/wallet-framework-test-setup", "@ledgerhq/disable-network-setup"],
+  setupFilesAfterEnv: [
+    "@ledgerhq/wallet-framework-test-setup",
+    "@ledgerhq/disable-network-setup",
+    "@ledgerhq/test-quarantine/jest-retries",
+  ],
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/**/*.test.ts",
@@ -36,5 +40,6 @@ module.exports = {
     "default",
     ...(process.env.CI ? ["github-actions"] : []),
     ["jest-sonar", { outputName: "sonar-executionTests-report.xml", reportedFilePath: "absolute" }],
+    "@ledgerhq/test-quarantine/jest",
   ],
 };
