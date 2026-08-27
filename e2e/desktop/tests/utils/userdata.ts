9d9ec27eba4b15147fe8026b88a0deae038e7290
Victor Alber <victor.alber@ledger.fr>
2026-08-27 16:16:06 +0200

    chore(e2e): remove dead code from desktop e2e utils

--- e2e/desktop/tests/utils/userdata.ts
+++ e2e/desktop/tests/utils/userdata.ts
@@ -1,6 +1,6 @@
 import { readFile } from "fs/promises";
 
-export const getUserdata = async (userdataFile: string) => {
+const getUserdata = async (userdataFile: string) => {
   const jsonFile = await readFile(userdataFile, "utf-8");
   return JSON.parse(jsonFile);
 };
