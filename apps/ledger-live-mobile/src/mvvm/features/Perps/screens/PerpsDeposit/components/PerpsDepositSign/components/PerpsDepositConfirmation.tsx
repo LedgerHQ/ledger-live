051e59d055a8b2d945f3e3e491a6d7055d13ec94
Olaoluwa Oke <olaoluwa.oke@ledger.fr>
2026-09-02 17:29:21 +0100

    chore(perps): replcae import source

--- apps/ledger-live-mobile/src/mvvm/features/Perps/screens/PerpsDeposit/components/PerpsDepositSign/components/PerpsDepositConfirmation.tsx
+++ apps/ledger-live-mobile/src/mvvm/features/Perps/screens/PerpsDeposit/components/PerpsDepositSign/components/PerpsDepositConfirmation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@ledgerhq/lumen-ui-rnative";
-import { getProductName } from "LLM/utils/getProductName";
+import { getProductName } from "@ledgerhq/devices";
 import type { Device } from "@ledgerhq/live-common/hw/actions/types";
 import { ContinueOnDevice } from "LLM/components/DeviceIntentExecutor/components/DeviceGenericStates/ContinueOnDevice";
 import { PerpsDepositSignTerms } from "./PerpsDepositSignTerms";
