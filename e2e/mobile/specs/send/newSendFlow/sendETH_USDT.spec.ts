8d6899a3af4a39efe7a690b52577bde25c4d92b3
Victor <162306106+VicAlbr@users.noreply.github.com>
2026-09-08 16:33:59 +0200

    Revert "test(e2e-mobile): tag ETH USDT send test with @smoke"

--- e2e/mobile/specs/send/newSendFlow/sendETH_USDT.spec.ts
+++ e2e/mobile/specs/send/newSendFlow/sendETH_USDT.spec.ts
@@ -5,16 +5,6 @@ import { FF_NEW_SEND_FLOW_ENABLED } from "@e2e/utils/featureFlagUtils";
 runNewSendFlowTokenTest(
   new Transaction(TokenAccount.ETH_USDT_1, TokenAccount.ETH_USDT_3, "0.01"),
   ["B2CQA-6111"],
-  [
-    "@NanoSP",
-    "@LNS",
-    "@NanoX",
-    "@Stax",
-    "@Flex",
-    "@NanoGen5",
-    "@smoke",
-    "@ethereum",
-    "@family-evm",
-  ],
+  ["@NanoSP", "@LNS", "@NanoX", "@Stax", "@Flex", "@NanoGen5", "@ethereum", "@family-evm"],
   { featureFlags: { ...FF_NEW_SEND_FLOW_ENABLED } },
 );
