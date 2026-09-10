c54e02107abb32d47c1364a9c16a91f877360cdf
Tony Khaov <tony.khaov@gmail.com>
2026-09-10 08:17:22 +0200

    feat(pay): add desktop card numbers flip UI

--- features/flow/pay-card-details/src/components/CardActions/CardActions.web.tsx
+++ features/flow/pay-card-details/src/components/CardActions/CardActions.web.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 import { Freeze } from "../Freeze/Freeze";
 import { More } from "../More/More";
 
-export function CardActions() {
+export function CardActions({ view }: { readonly view?: ReactNode }) {
   return (
     <div className="flex flex-row gap-8">
+      {view}
       <Freeze />
       <More />
     </div>
