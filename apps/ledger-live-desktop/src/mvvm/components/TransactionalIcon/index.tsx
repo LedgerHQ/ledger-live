4ca94cec67ef371a763b181232d9570f1ab678c5
Lucas Werey <lucas.werey@ledger.fr>
2026-09-15 15:53:21 +0200

    fix(lumen): derive mediaSize type from getDotIconProps

--- apps/ledger-live-desktop/src/mvvm/components/TransactionalIcon/index.tsx
+++ apps/ledger-live-desktop/src/mvvm/components/TransactionalIcon/index.tsx
@@ -12,7 +12,7 @@ export type TransactionalIconProps = {
   isPending: boolean;
   hasFailed?: boolean;
   currency: CryptoCurrency | TokenCurrency;
-  mediaSize?: 40 | 48 | 56 | 64 | 72;
+  mediaSize?: Parameters<typeof getDotIconProps<"mediaImage">>[1];
   network?: string;
 };
 
