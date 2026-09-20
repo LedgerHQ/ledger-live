ed48e8ab8c136a522c10d99574f6b03dbfc055a0
Abdurrahman Sastim <abdurrahman.sastim@ledger.fr>
2026-09-20 23:41:44 +0200

    Revert "test(e2e): test"
    This reverts commit 6ca224c53b26ca6b65cd6ff5d8872ae105e8c6b2.

--- e2e/mobile/jest.environment.ts
+++ e2e/mobile/jest.environment.ts
@@ -119,6 +119,11 @@ async function captureFailureDiagnostics(mergedFeatureFlags?: OptionalFeatureMap
     );
   logs = "";
 
+  await withTimeout(
+    captureNativeViewHierarchy(),
+    SLOW_DIAGNOSTIC_TIMEOUT_MS,
+    "captureNativeViewHierarchy",
+  );
   console.info("Failure diagnostics capture completed");
 }
 
