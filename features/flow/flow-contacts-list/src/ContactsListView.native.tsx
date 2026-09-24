7e4f88b848efd9214525663b17507e4bd0dde7e1
Tony Khaov <tony.khaov@gmail.com>
2026-09-24 21:31:05 +0700

    style(contacts): drop a stray blank line

--- features/flow/flow-contacts-list/src/ContactsListView.native.tsx
+++ features/flow/flow-contacts-list/src/ContactsListView.native.tsx
@@ -100,7 +100,6 @@ export function ContactsListView({
     <ContactsListHeader
       me={me}
       labels={labels}
-
       showAddContact={!isPopulated && !hasNoResults}
       onOpenContact={onOpenContact}
       onAddContact={onAddContact}
